9d4735f5dbf98ed7af810a7aa3ccbc3c9805b7b1
Jose Nino <jnino@lyft.com>
2016-08-26 12:03:36 -0700

    Add environment variables for service distinction

--- example/service.py
+++ example/service.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import socket
+import os
 
 app = Flask(__name__)
 
@@ -7,7 +8,8 @@ app = Flask(__name__)
 @app.route('/service/<service_number>')
 def hello(service_number):
     return ('Hello from behind Envoy (service {})! hostname: {} resolved'
-            'hostname: {}\n'.format(service_number, socket.gethostname(),
+            'hostname: {}\n'.format(os.environ['SERVICE_NAME'], 
+                                    socket.gethostname(),
                                     socket.gethostbyname(socket.gethostname())))
 
 if __name__ == "__main__":
