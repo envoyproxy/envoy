080071e5c087e8c7fcad693670152f6f2d2c1c83
Jose Nino <jnino@lyft.com>
2016-09-12 14:44:50 -0700

    Add default value due to conflicting test in /test/integration/fake_upstream.cc:175

--- source/server/connection_handler.h
+++ source/server/connection_handler.h
@@ -33,7 +33,7 @@ struct ListenerStats {
  */
 class ConnectionHandler final : NonCopyable {
 public:
-  ConnectionHandler(Stats::Store& stats_store, spdlog::logger& logger);
+  ConnectionHandler(Stats::Store& stats_store, spdlog::logger& logger, std::chrono::milliseconds flush_interval_msec = std::chrono::milliseconds(10000));
   ~ConnectionHandler();
 
   Api::Api& api() { return *api_; }
