b13e01263aa51d3c3fe00c6b2975f3aa51494563
Jose Ulises Nino Rivera <junr03@users.noreply.github.com>
2016-09-13 15:35:32 -0700

    Bandaid fix to fix CI failures (#64)

--- test/server/http/admin_test.cc
+++ test/server/http/admin_test.cc
@@ -9,8 +9,9 @@ namespace Server {
 
 class AdminFilterTest : public testing::Test {
 public:
+  // TODO: Switch to mocks and do not bind to a real port.
   AdminFilterTest()
-      : admin_("/dev/null", 9001, server_), filter_(admin_), request_headers_{{":path", "/"}} {
+      : admin_("/dev/null", 9002, server_), filter_(admin_), request_headers_{{":path", "/"}} {
     filter_.setDecoderFilterCallbacks(callbacks_);
   }
 
