313f2fea4dde88b26a19e7bdcf64c4664bd3ba65
Roman Dzhabarov <rdzhabarov@lyft.com>
2016-11-16 16:40:28 -0800

    make fix_format.

--- source/common/tracing/http_tracer_impl.cc
+++ source/common/tracing/http_tracer_impl.cc
@@ -246,7 +246,8 @@ void LightStepSink::flushTrace(const Http::HeaderMap& request_headers, const Htt
   lightstep::Span span = tls_.getTyped<TlsLightStepTracer>(tls_slot_).tracer_.StartSpan(
       tracing_context.operationName(),
       {lightstep::StartTimestamp(request_info.startTime()),
-       lightstep::SetTag("guid:x-request-id", std::string(request_headers.RequestId()->value().c_str())),
+       lightstep::SetTag("guid:x-request-id",
+                         std::string(request_headers.RequestId()->value().c_str())),
        lightstep::SetTag("request line", buildRequestLine(request_headers, request_info)),
        lightstep::SetTag("response code", buildResponseCode(request_info)),
        lightstep::SetTag("request size", request_info.bytesReceived()),
@@ -268,7 +269,8 @@ void LightStepSink::flushTrace(const Http::HeaderMap& request_headers, const Htt
   }
 
   if (request_headers.ClientTraceId()) {
-    span.SetTag("guid:x-client-trace-id", std::string(request_headers.ClientTraceId()->value().c_str()));
+    span.SetTag("guid:x-client-trace-id",
+                std::string(request_headers.ClientTraceId()->value().c_str()));
   }
 
   span.Finish();
