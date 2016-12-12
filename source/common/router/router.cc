envoyproxy/envoy

commit 7ac43497b74b66ecbe566f06f4c0d19cfadfa9c9

outlier detection: charge all failures to host (#279)
Previously we did only charged the final response code to a host and
ignored retires. For outlier detection purposes, we need to charge all
failures to the host to account for retries.

This commit also includes a small refactor to move upstream host into
the upstream request where it should be.

source/common/router/router.cc
@@ -91,45 +91,41 @@ Filter::~Filter() {
   ASSERT(!retry_state_);
 }
 
-const std::string& Filter::upstreamZone() {
-  return upstream_host_ ? upstream_host_->zone() : EMPTY_STRING;
+const std::string& Filter::upstreamZone(Upstream::HostDescriptionPtr upstream_host) {
+  return upstream_host ? upstream_host->zone() : EMPTY_STRING;
 }
 
-void Filter::chargeUpstreamCode(const Http::HeaderMap& response_headers) {
+void Filter::chargeUpstreamCode(const Http::HeaderMap& response_headers,
+                                Upstream::HostDescriptionPtr upstream_host) {
   if (config_.emit_dynamic_stats_ && !callbacks_->requestInfo().healthCheck()) {
     const Http::HeaderEntry* upstream_canary_header = response_headers.EnvoyUpstreamCanary();
     const Http::HeaderEntry* internal_request_header = downstream_headers_->EnvoyInternalRequest();
 
     bool is_canary = (upstream_canary_header && upstream_canary_header->value() == "true") ||
-                     (upstream_host_ ? upstream_host_->canary() : false);
+                     (upstream_host ? upstream_host->canary() : false);
     bool internal_request = internal_request_header && internal_request_header->value() == "true";
-
-    if (upstream_host_) {
-      upstream_host_->outlierDetector().putHttpResponseCode(
-          Http::Utility::getResponseStatus(response_headers));
-    }
 
     Http::CodeUtility::ResponseStatInfo info{
         config_.stats_store_, cluster_->statPrefix(), response_headers, internal_request,
         route_->virtualHostName(), request_vcluster_ ? request_vcluster_->name() : "",
-        config_.service_zone_, upstreamZone(), is_canary};
+        config_.service_zone_, upstreamZone(upstream_host), is_canary};
 
     Http::CodeUtility::chargeResponseStat(info);
 
     for (const std::string& alt_prefix : alt_stat_prefixes_) {
       Http::CodeUtility::ResponseStatInfo info{config_.stats_store_, alt_prefix, response_headers,
                                                internal_request, "", "", config_.service_zone_,
-                                               upstreamZone(), is_canary};
+                                               upstreamZone(upstream_host), is_canary};
 
       Http::CodeUtility::chargeResponseStat(info);
     }
   }
 }
 
-void Filter::chargeUpstreamCode(Http::Code code) {
+void Filter::chargeUpstreamCode(Http::Code code, Upstream::HostDescriptionPtr upstream_host) {
   Http::HeaderMapImpl fake_response_headers{
       {Http::Headers::get().Status, std::to_string(enumToInt(code))}};
-  chargeUpstreamCode(fake_response_headers);
+  chargeUpstreamCode(fake_response_headers, upstream_host);
 }
 
 Http::FilterHeadersStatus Filter::decodeHeaders(Http::HeaderMap& headers, bool end_stream) {
@@ -181,7 +177,7 @@ Http::FilterHeadersStatus Filter::decodeHeaders(Http::HeaderMap& headers, bool e
   // See if we are supposed to immediately kill some percentage of this cluster's traffic.
   if (cluster_->maintenanceMode()) {
     callbacks_->requestInfo().onFailedResponse(Http::AccessLog::FailureReason::UpstreamOverflow);
-    chargeUpstreamCode(Http::Code::ServiceUnavailable);
+    chargeUpstreamCode(Http::Code::ServiceUnavailable, nullptr);
     Http::Utility::sendLocalReply(*callbacks_, Http::Code::ServiceUnavailable, "maintenance mode");
     return Http::FilterHeadersStatus::StopIteration;
   }
@@ -227,7 +223,7 @@ Http::FilterHeadersStatus Filter::decodeHeaders(Http::HeaderMap& headers, bool e
 
 void Filter::sendNoHealthyUpstreamResponse() {
   callbacks_->requestInfo().onFailedResponse(Http::AccessLog::FailureReason::NoHealthyUpstream);
-  chargeUpstreamCode(Http::Code::ServiceUnavailable);
+  chargeUpstreamCode(Http::Code::ServiceUnavailable, nullptr);
   Http::Utility::sendLocalReply(*callbacks_, Http::Code::ServiceUnavailable, "no healthy upstream");
 }
 
@@ -343,6 +339,14 @@ void Filter::onUpstreamReset(UpstreamResetType type,
     stream_log_debug("upstream reset", *callbacks_);
   }
 
+  Upstream::HostDescriptionPtr upstream_host;
+  if (upstream_request_) {
+    upstream_host = upstream_request_->upstream_host_;
+    upstream_host->outlierDetector().putHttpResponseCode(
+        enumToInt(type == UpstreamResetType::Reset ? Http::Code::ServiceUnavailable
+                                                   : Http::Code::GatewayTimeout));
+  }
+
   // We don't retry on a global timeout or if we already started the response.
   if (type != UpstreamResetType::GlobalTimeout && !downstream_response_started_ && retry_state_ &&
       retry_state_->shouldRetry(nullptr, reset_reason, [this]() -> void { doRetry(); }) &&
@@ -373,7 +377,7 @@ void Filter::onUpstreamReset(UpstreamResetType type,
       body = "upstream connect error or disconnect/reset before headers";
     }
 
-    chargeUpstreamCode(code);
+    chargeUpstreamCode(code, upstream_host);
     Http::Utility::sendLocalReply(*callbacks_, code, body);
   }
 }
@@ -402,6 +406,9 @@ void Filter::onUpstreamHeaders(Http::HeaderMapPtr&& headers, bool end_stream) {
   stream_log_debug("upstream headers complete: end_stream={}", *callbacks_, end_stream);
   ASSERT(!downstream_response_started_);
 
+  upstream_request_->upstream_host_->outlierDetector().putHttpResponseCode(
+      Http::Utility::getResponseStatus(*headers));
+
   if (retry_state_ &&
       retry_state_->shouldRetry(headers.get(), Optional<Http::StreamResetReason>(),
                                 [this]() -> void { doRetry(); }) &&
@@ -426,8 +433,8 @@ void Filter::onUpstreamHeaders(Http::HeaderMapPtr&& headers, bool end_stream) {
 
   upstream_request_->upstream_canary_ =
       (headers->EnvoyUpstreamCanary() && headers->EnvoyUpstreamCanary()->value() == "true") ||
-      (upstream_host_ ? upstream_host_->canary() : false);
-  chargeUpstreamCode(*headers);
+      upstream_request_->upstream_host_->canary();
+  chargeUpstreamCode(*headers, upstream_request_->upstream_host_);
 
   downstream_response_started_ = true;
   if (end_stream) {
@@ -460,7 +467,7 @@ void Filter::onUpstreamComplete() {
     std::chrono::milliseconds response_time = std::chrono::duration_cast<std::chrono::milliseconds>(
         std::chrono::system_clock::now() - downstream_request_complete_time_);
 
-    upstream_host_->outlierDetector().putResponseTime(response_time);
+    upstream_request_->upstream_host_->outlierDetector().putResponseTime(response_time);
 
     const Http::HeaderEntry* internal_request_header = downstream_headers_->EnvoyInternalRequest();
     bool internal_request = internal_request_header && internal_request_header->value() == "true";
@@ -468,14 +475,16 @@ void Filter::onUpstreamComplete() {
     Http::CodeUtility::ResponseTimingInfo info{
         config_.stats_store_, cluster_->statPrefix(), response_time,
         upstream_request_->upstream_canary_, internal_request, route_->virtualHostName(),
-        request_vcluster_ ? request_vcluster_->name() : "", config_.service_zone_, upstreamZone()};
+        request_vcluster_ ? request_vcluster_->name() : "", config_.service_zone_,
+        upstreamZone(upstream_request_->upstream_host_)};
 
     Http::CodeUtility::chargeResponseTiming(info);
 
     for (const std::string& alt_prefix : alt_stat_prefixes_) {
-      Http::CodeUtility::ResponseTimingInfo info{
-          config_.stats_store_, alt_prefix, response_time, upstream_request_->upstream_canary_,
-          internal_request, "", "", config_.service_zone_, upstreamZone()};
+      Http::CodeUtility::ResponseTimingInfo info{config_.stats_store_, alt_prefix, response_time,
+                                                 upstream_request_->upstream_canary_,
+                                                 internal_request, "", "", config_.service_zone_,
+                                                 upstreamZone(upstream_request_->upstream_host_)};
 
       Http::CodeUtility::chargeResponseTiming(info);
     }

test/common/router/router_test.cc
@@ -176,6 +176,7 @@ TEST_F(RouterTest, ResetDuringEncodeHeaders) {
 
   Http::TestHeaderMapImpl headers;
   HttpTestUtility::addDefaultHeaders(headers);
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(503));
   router_.decodeHeaders(headers, true);
 }
 
@@ -209,6 +210,7 @@ TEST_F(RouterTest, UpstreamTimeout) {
   EXPECT_CALL(callbacks_, encodeHeaders_(HeaderMapEqualRef(&response_headers), false));
   EXPECT_CALL(callbacks_, encodeData(_, true));
   EXPECT_CALL(*router_.retry_state_, shouldRetry(_, _, _)).Times(0);
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(504));
   response_timeout_->callback_();
 
   EXPECT_EQ(1U,
@@ -246,6 +248,7 @@ TEST_F(RouterTest, UpstreamPerTryTimeout) {
       {":status", "504"}, {"content-length", "24"}, {"content-type", "text/plain"}};
   EXPECT_CALL(callbacks_, encodeHeaders_(HeaderMapEqualRef(&response_headers), false));
   EXPECT_CALL(callbacks_, encodeData(_, true));
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(504));
   per_try_timeout_->callback_();
 
   EXPECT_EQ(1U,
@@ -274,6 +277,7 @@ TEST_F(RouterTest, RetryRequestNotComplete) {
   router_.decodeHeaders(headers, false);
 
   router_.retry_state_->expectRetry();
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(503));
   encoder1.stream_.resetStream(Http::StreamResetReason::RemoteReset);
 }
 
@@ -298,6 +302,7 @@ TEST_F(RouterTest, RetryNoneHealthy) {
   router_.decodeHeaders(headers, true);
 
   router_.retry_state_->expectRetry();
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(503));
   encoder1.stream_.resetStream(Http::StreamResetReason::LocalReset);
 
   EXPECT_CALL(cm_, httpConnPoolForCluster(_, _)).WillOnce(Return(nullptr));
@@ -327,6 +332,7 @@ TEST_F(RouterTest, RetryUpstreamReset) {
   router_.decodeHeaders(headers, true);
 
   router_.retry_state_->expectRetry();
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(503));
   encoder1.stream_.resetStream(Http::StreamResetReason::RemoteReset);
 
   // We expect this reset to kick off a new request.
@@ -343,6 +349,7 @@ TEST_F(RouterTest, RetryUpstreamReset) {
   // Normal response.
   EXPECT_CALL(*router_.retry_state_, shouldRetry(_, _, _)).WillOnce(Return(false));
   Http::HeaderMapPtr response_headers(new Http::TestHeaderMapImpl{{":status", "200"}});
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(200));
   response_decoder->decodeHeaders(std::move(response_headers), true);
 }
 
@@ -366,6 +373,7 @@ TEST_F(RouterTest, RetryUpstreamPerTryTimeout) {
   router_.decodeHeaders(headers, true);
 
   router_.retry_state_->expectRetry();
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(504));
   per_try_timeout_->callback_();
 
   // We expect this reset to kick off a new request.
@@ -383,6 +391,7 @@ TEST_F(RouterTest, RetryUpstreamPerTryTimeout) {
   // Normal response.
   EXPECT_CALL(*router_.retry_state_, shouldRetry(_, _, _)).WillOnce(Return(false));
   Http::HeaderMapPtr response_headers(new Http::TestHeaderMapImpl{{":status", "200"}});
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(200));
   response_decoder->decodeHeaders(std::move(response_headers), true);
 }
 
@@ -406,7 +415,9 @@ TEST_F(RouterTest, RetryUpstreamResetResponseStarted) {
   EXPECT_CALL(*router_.retry_state_, shouldRetry(_, _, _)).WillOnce(Return(false));
   EXPECT_CALL(callbacks_, encodeHeaders_(_, false));
   Http::HeaderMapPtr response_headers(new Http::TestHeaderMapImpl{{":status", "200"}});
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(200));
   response_decoder->decodeHeaders(std::move(response_headers), false);
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(503));
   encoder1.stream_.resetStream(Http::StreamResetReason::RemoteReset);
 }
 
@@ -429,6 +440,7 @@ TEST_F(RouterTest, RetryUpstream5xx) {
   // 5xx response.
   router_.retry_state_->expectRetry();
   Http::HeaderMapPtr response_headers1(new Http::TestHeaderMapImpl{{":status", "503"}});
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(503));
   response_decoder->decodeHeaders(std::move(response_headers1), true);
 
   // We expect the 5xx response to kick off a new request.
@@ -446,6 +458,7 @@ TEST_F(RouterTest, RetryUpstream5xx) {
   // Normal response.
   EXPECT_CALL(*router_.retry_state_, shouldRetry(_, _, _)).WillOnce(Return(false));
   Http::HeaderMapPtr response_headers2(new Http::TestHeaderMapImpl{{":status", "200"}});
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(200));
   response_decoder->decodeHeaders(std::move(response_headers2), true);
 }
 
@@ -468,12 +481,12 @@ TEST_F(RouterTest, RetryTimeoutDuringRetryDelay) {
   // 5xx response.
   router_.retry_state_->expectRetry();
   Http::HeaderMapPtr response_headers1(new Http::TestHeaderMapImpl{{":status", "503"}});
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(503));
   response_decoder->decodeHeaders(std::move(response_headers1), true);
 
   // Fire timeout.
   EXPECT_CALL(callbacks_.request_info_,
               onFailedResponse(Http::AccessLog::FailureReason::UpstreamRequestTimeout));
-  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(504));
   EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putResponseTime(_)).Times(0);
   Http::TestHeaderMapImpl response_headers{
       {":status", "504"}, {"content-length", "24"}, {"content-type", "text/plain"}};
@@ -509,6 +522,7 @@ TEST_F(RouterTest, RetryUpstream5xxNotComplete) {
   router_.retry_state_->expectRetry();
   Http::HeaderMapPtr response_headers1(new Http::TestHeaderMapImpl{{":status", "503"}});
   EXPECT_CALL(encoder1.stream_, resetStream(Http::StreamResetReason::LocalReset));
+  EXPECT_CALL(cm_.conn_pool_.host_->outlier_detector_, putHttpResponseCode(503));
   response_decoder->decodeHeaders(std::move(response_headers1), false);
 
   // We expect the 5xx response to kick off a new request.

source/common/router/router.h
@@ -131,7 +131,7 @@ private:
     void onPerTryTimeout();
 
     void onUpstreamHostSelected(Upstream::HostDescriptionPtr host) {
-      parent_.upstream_host_ = host;
+      upstream_host_ = host;
       parent_.callbacks_->requestInfo().onUpstreamHostSelected(host);
     }
 
@@ -156,6 +156,7 @@ private:
     Http::StreamEncoder* request_encoder_{};
     Optional<Http::StreamResetReason> deferred_reset_reason_;
     Buffer::InstancePtr buffered_request_body_;
+    Upstream::HostDescriptionPtr upstream_host_;
 
     bool calling_encode_headers_ : 1;
     bool upstream_canary_ : 1;
@@ -170,9 +171,10 @@ private:
   Http::AccessLog::FailureReason
   streamResetReasonToFailureReason(Http::StreamResetReason reset_reason);
 
-  const std::string& upstreamZone();
-  void chargeUpstreamCode(const Http::HeaderMap& response_headers);
-  void chargeUpstreamCode(Http::Code code);
+  static const std::string& upstreamZone(Upstream::HostDescriptionPtr upstream_host);
+  void chargeUpstreamCode(const Http::HeaderMap& response_headers,
+                          Upstream::HostDescriptionPtr upstream_host);
+  void chargeUpstreamCode(Http::Code code, Upstream::HostDescriptionPtr upstream_host);
   void cleanup();
   virtual RetryStatePtr createRetryState(const RetryPolicy& policy,
                                          Http::HeaderMap& request_headers,
@@ -207,7 +209,6 @@ private:
   RetryStatePtr retry_state_;
   Http::HeaderMap* downstream_headers_{};
   Http::HeaderMap* downstream_trailers_{};
-  Upstream::HostDescriptionPtr upstream_host_;
   SystemTime downstream_request_complete_time_;
 
   bool downstream_response_started_ : 1;

commit 20841e2f9ba71e9aef3dda4b351c52eb15ded3bd

router: fix pool failure crash
regression from https://github.com/lyft/envoy/pull/279

source/common/router/router.cc
@@ -343,9 +343,11 @@ void Filter::onUpstreamReset(UpstreamResetType type,
   Upstream::HostDescriptionPtr upstream_host;
   if (upstream_request_) {
     upstream_host = upstream_request_->upstream_host_;
-    upstream_host->outlierDetector().putHttpResponseCode(
-        enumToInt(type == UpstreamResetType::Reset ? Http::Code::ServiceUnavailable
-                                                   : Http::Code::GatewayTimeout));
+    if (upstream_host) {
+      upstream_host->outlierDetector().putHttpResponseCode(
+          enumToInt(type == UpstreamResetType::Reset ? Http::Code::ServiceUnavailable
+                                                     : Http::Code::GatewayTimeout));
+    }
   }
 
   // We don't retry on a global timeout or if we already started the response.

test/common/http/async_client_impl_test.cc
@@ -256,8 +256,7 @@ TEST_F(AsyncClientImplTest, PoolFailure) {
   EXPECT_CALL(cm_.conn_pool_, newStream(_, _))
       .WillOnce(Invoke([&](StreamDecoder&,
                            ConnectionPool::Callbacks& callbacks) -> ConnectionPool::Cancellable* {
-        callbacks.onPoolFailure(ConnectionPool::PoolFailureReason::ConnectionFailure,
-                                cm_.conn_pool_.host_);
+        callbacks.onPoolFailure(ConnectionPool::PoolFailureReason::ConnectionFailure, nullptr);
         return nullptr;
       }));
 

test/common/router/router_test.cc
@@ -96,7 +96,7 @@ TEST_F(RouterTest, PoolFailureWithPriority) {
                            -> Http::ConnectionPool::Cancellable* {
                              callbacks.onPoolFailure(
                                  Http::ConnectionPool::PoolFailureReason::ConnectionFailure,
-                                 cm_.conn_pool_.host_);
+                                 nullptr);
                              return nullptr;
                            }));
 
@@ -106,9 +106,7 @@ TEST_F(RouterTest, PoolFailureWithPriority) {
   EXPECT_CALL(callbacks_, encodeData(_, true));
   EXPECT_CALL(callbacks_.request_info_,
               onFailedResponse(Http::AccessLog::FailureReason::UpstreamConnectionFailure));
-  EXPECT_CALL(callbacks_.request_info_, onUpstreamHostSelected(_))
-      .WillOnce(Invoke([&](const Upstream::HostDescriptionPtr host)
-                           -> void { EXPECT_EQ(host_url_, host->url()); }));
+  EXPECT_CALL(callbacks_.request_info_, onUpstreamHostSelected(Upstream::HostDescriptionPtr{}));
 
   Http::TestHeaderMapImpl headers;
   HttpTestUtility::addDefaultHeaders(headers);