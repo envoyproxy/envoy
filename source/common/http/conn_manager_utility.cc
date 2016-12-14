12c6a6c2c99344a0a8b2922f3fd833ca2a068439
Roman Dzhabarov <rdzhabarov@lyft.com>
2016-12-14 15:38:53 -0800

    fix method.

--- source/common/http/conn_manager_utility.cc
+++ source/common/http/conn_manager_utility.cc
@@ -148,7 +148,7 @@ bool ConnectionManagerUtility::shouldTraceRequest(
   case Http::TracingType::All:
     return true;
   case Http::TracingType::UpstreamFailure:
-    return isUpstreamFailure(request_info);
+    return isTraceableFailure(request_info);
   }
 
   // Compiler enforces switch above to cover all the cases and it's impossible to be here,
