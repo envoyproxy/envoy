90a6f955e0692d95c794e63bea2ccb3748c61b22
Piotr Sikora <piotrsikora@google.com>
2016-12-15 20:25:15 -0800

    Stop building libevent with OpenSSL support.
    Missed in commit a10174c29cf9106d081c06d6963d564dca3a4557.
    
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- ci/build_container/build_container.sh
+++ ci/build_container/build_container.sh
@@ -38,9 +38,7 @@ rm -fr openssl*
 wget https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
 tar xf libevent-2.0.22-stable.tar.gz
 cd libevent-2.0.22-stable
-./configure --prefix=$THIRDPARTY_BUILD --enable-shared=no --disable-libevent-regress \
-    CPPFLAGS=-I$THIRDPARTY_BUILD/include LDFLAGS=-L$THIRDPARTY_BUILD/lib \
-    OPENSSL_LIBADD=-ldl
+./configure --prefix=$THIRDPARTY_BUILD --enable-shared=no --disable-libevent-regress --disable-openssl
 make install
 cd ..
 rm -fr libevent*
