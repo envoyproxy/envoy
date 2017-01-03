6b1336a786ebe56c45a1a349ddf706e0526c1ec1
Lizan Zhou <zlizan@google.com>
2017-01-03 13:49:30 -0800

    Implement base64 encode (#306)

--- source/common/common/base64.cc
+++ source/common/common/base64.cc
@@ -1,35 +1,59 @@
 #include "base64.h"
 
-#include "openssl/bio.h"
-#include "openssl/buffer.h"
-#include "openssl/evp.h"
+static constexpr char CHAR_TABLE[] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 std::string Base64::encode(const Buffer::Instance& buffer, uint64_t length) {
-  BIO* bio;
-  BIO* b64;
-
-  b64 = BIO_new(BIO_f_base64());
-  bio = BIO_new(BIO_s_mem());
-  bio = BIO_push(b64, bio);
-  BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
+  uint64_t output_length = (std::min(length, buffer.length()) + 2) / 3 * 4;
+  std::string ret;
+  ret.reserve(output_length);
 
   uint64_t num_slices = buffer.getRawSlices(nullptr, 0);
   Buffer::RawSlice slices[num_slices];
   buffer.getRawSlices(slices, num_slices);
-  for (Buffer::RawSlice& slice : slices) {
-    uint64_t to_write = std::min(length, slice.len_);
-    length -= to_write;
-    BIO_write(bio, slice.mem_, to_write);
 
-    if (length == 0) {
+  uint64_t j = 0;
+  uint8_t next_c = 0;
+  for (Buffer::RawSlice& slice : slices) {
+    const uint8_t* slice_mem = static_cast<const uint8_t*>(slice.mem_);
+
+    for (uint64_t i = 0; i < slice.len_ && j < length; ++i, ++j) {
+      const uint8_t c = slice_mem[i];
+      switch (j % 3) {
+      case 0:
+        ret.push_back(CHAR_TABLE[c >> 2]);
+        next_c = (c & 0x03) << 4;
+        break;
+      case 1:
+        ret.push_back(CHAR_TABLE[next_c | (c >> 4)]);
+        next_c = (c & 0x0f) << 2;
+        break;
+      case 2:
+        ret.push_back(CHAR_TABLE[next_c | (c >> 6)]);
+        ret.push_back(CHAR_TABLE[c & 0x3f]);
+        next_c = 0;
+        break;
+      }
+    }
+
+    if (j == length) {
       break;
     }
   }
 
-  BIO_ctrl(bio, BIO_CTRL_FLUSH, 0, nullptr);
-  BUF_MEM* memory;
-  BIO_ctrl(bio, BIO_C_GET_BUF_MEM_PTR, 0, reinterpret_cast<char*>(&memory));
-  std::string ret(memory->data, memory->length);
-  BIO_free_all(bio);
+  switch (j % 3) {
+  case 1:
+    ret.push_back(CHAR_TABLE[next_c]);
+    ret.push_back('=');
+    ret.push_back('=');
+    break;
+  case 2:
+    ret.push_back(CHAR_TABLE[next_c]);
+    ret.push_back('=');
+    break;
+  default:
+    break;
+  }
+
   return ret;
 }
---
+++ test/common/common/base64_test.cc
@@ -0,0 +1,37 @@
+#include "common/buffer/buffer_impl.h"
+
+#include "common/common/base64.h"
+
+TEST(Base64, EmptyBufferEncode) {
+  Buffer::OwnedImpl buffer;
+  EXPECT_EQ("", Base64::encode(buffer, 0));
+}
+
+TEST(Base64, SingleSliceBufferEncode) {
+  Buffer::OwnedImpl buffer;
+  buffer.add("foo", 3);
+  EXPECT_EQ("Zm9v", Base64::encode(buffer, 3));
+  EXPECT_EQ("Zm8=", Base64::encode(buffer, 2));
+}
+
+TEST(Base64, MultiSlicesBufferEncode) {
+  Buffer::OwnedImpl buffer;
+  buffer.add("foob", 4);
+  buffer.add("ar", 2);
+  EXPECT_EQ("Zm9vYg==", Base64::encode(buffer, 4));
+  EXPECT_EQ("Zm9vYmE=", Base64::encode(buffer, 5));
+  EXPECT_EQ("Zm9vYmFy", Base64::encode(buffer, 6));
+  EXPECT_EQ("Zm9vYmFy", Base64::encode(buffer, 7));
+}
+
+TEST(Base64, BinaryBufferEncode) {
+  Buffer::OwnedImpl buffer;
+  buffer.add("\0\1\2\3", 4);
+  buffer.add("\b\n\t", 4);
+  buffer.add("\xaa\xbc\xde", 3);
+  EXPECT_EQ("AAECAwgKCQ==", Base64::encode(buffer, 7));
+  EXPECT_EQ("AAECAwgKCQA=", Base64::encode(buffer, 8));
+  EXPECT_EQ("AAECAwgKCQCq", Base64::encode(buffer, 9));
+  EXPECT_EQ("AAECAwgKCQCqvA==", Base64::encode(buffer, 10));
+  EXPECT_EQ("AAECAwgKCQCqvN4=", Base64::encode(buffer, 30));
+}
