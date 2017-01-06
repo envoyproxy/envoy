b0d001a585216c49cf042bcc9c6200bfa1c3ac5f
Matt Klein <mklein@lyft.com>
2017-01-06 10:23:32 -0800

    comments

--- include/envoy/local_info/local_info.h
+++ include/envoy/local_info/local_info.h
@@ -12,22 +12,22 @@ public:
   virtual ~LocalInfo() {}
 
   /**
-   * Human readable network address.
+   * Human readable network address. E.g., "127.0.0.1".
    */
   virtual const std::string& address() const PURE;
 
   /**
-   * Human readable zone name.
+   * Human readable zone name. E.g., "us-east-1a".
    */
   virtual const std::string& zoneName() const PURE;
 
   /**
-   * Human readable cluster name.
+   * Human readable cluster name. E.g., "eta".
    */
   virtual const std::string& clusterName() const PURE;
 
   /**
-   * Human readable individual node name.
+   * Human readable individual node name. E.g., "i-123456".
    */
   virtual const std::string& nodeName() const PURE;
 };
