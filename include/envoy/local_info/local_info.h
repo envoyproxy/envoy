27eaacc9cee99441e0576146da04e4bd2077badd
Matt Klein <mklein@lyft.com>
2017-01-06 10:16:16 -0800

    comments

--- include/envoy/local_info/local_info.h
+++ include/envoy/local_info/local_info.h
@@ -5,15 +5,30 @@
 namespace LocalInfo {
 
 /**
- *
+ * Information about the local environment.
  */
 class LocalInfo {
 public:
   virtual ~LocalInfo() {}
 
+  /**
+   * Human readable network address.
+   */
   virtual const std::string& address() const PURE;
+
+  /**
+   * Human readable zone name.
+   */
   virtual const std::string& zoneName() const PURE;
+
+  /**
+   * Human readable cluster name.
+   */
   virtual const std::string& clusterName() const PURE;
+
+  /**
+   * Human readable individual node name.
+   */
   virtual const std::string& nodeName() const PURE;
 };
 
