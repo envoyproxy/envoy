d8f6a7a29eef2e369cd9ef7c65158ce1d804eeb2
Matt Klein <mklein@lyft.com>
2017-01-26 20:44:47 -0800

    comment

--- source/common/ssl/openssl.cc
+++ source/common/ssl/openssl.cc
@@ -12,13 +12,13 @@ std::unique_ptr<std::mutex[]> OpenSsl::locks_;
 #endif
 
 void OpenSsl::initialize() {
-  SSL_load_error_strings();
-
   RELEASE_ASSERT(SSL_library_init());
+
+#ifndef OPENSSL_IS_BORINGSSL
+  SSL_load_error_strings();
   RELEASE_ASSERT(RAND_poll());
   OpenSSL_add_all_algorithms();
 
-#ifndef OPENSSL_IS_BORINGSSL
   locks_.reset(new std::mutex[CRYPTO_num_locks()]);
   CRYPTO_set_id_callback(getThreadIdCb);
   CRYPTO_set_locking_callback(threadLockCb);
