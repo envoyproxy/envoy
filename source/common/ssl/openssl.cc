b259fd5734008e5945814f8c8dc8f0099535466d
Matt Klein <mklein@lyft.com>
2017-01-26 17:31:27 -0800

    ssl: remove openssl threading code if using boringssl
    boringssl has internal support for threading.

--- source/common/ssl/openssl.cc
+++ source/common/ssl/openssl.cc
@@ -7,26 +7,25 @@
 
 namespace Ssl {
 
+#ifndef OPENSSL_IS_BORINGSSL
 std::unique_ptr<std::mutex[]> OpenSsl::locks_;
+#endif
 
 void OpenSsl::initialize() {
   SSL_load_error_strings();
 
-  if (!SSL_library_init()) {
-    PANIC("OpenSSL initialization failed");
-  }
-
-  // Seed PRNG from /dev/urandom
-  if (!RAND_poll()) {
-    PANIC("could not seed random number generator")
-  }
-
-  locks_.reset(new std::mutex[CRYPTO_num_locks()]);
-
+  RELEASE_ASSERT(SSL_library_init());
+  RELEASE_ASSERT(RAND_poll());
   OpenSSL_add_all_algorithms();
+
+#ifndef OPENSSL_IS_BORINGSSL
+  locks_.reset(new std::mutex[CRYPTO_num_locks()]);
   CRYPTO_set_id_callback(getThreadIdCb);
   CRYPTO_set_locking_callback(threadLockCb);
+#endif
 }
+
+#ifndef OPENSSL_IS_BORINGSSL
 
 void OpenSsl::threadLockCb(int mode, int which, const char*, int) {
   if (mode & CRYPTO_LOCK) {
@@ -35,5 +34,7 @@ void OpenSsl::threadLockCb(int mode, int which, const char*, int) {
     locks_[which].unlock();
   }
 }
+
+#endif
 
 } // Ssl
