envoyproxy/envoy

commit 14d3e97c6be8823420fe0d32dbe0afb9b79eb138

Extend AsyncClient to support streaming requests / responses (#353)

source/common/http/async_client_impl.cc
@@ -1,15 +1,14 @@
 #include "async_client_impl.h"
-#include "headers.h"
 
 namespace Http {
 
 const std::vector<std::reference_wrapper<const Router::RateLimitPolicyEntry>>
-    AsyncRequestImpl::NullRateLimitPolicy::rate_limit_policy_entry_;
-const AsyncRequestImpl::NullRateLimitPolicy AsyncRequestImpl::RouteEntryImpl::rate_limit_policy_;
-const AsyncRequestImpl::NullRetryPolicy AsyncRequestImpl::RouteEntryImpl::retry_policy_;
-const AsyncRequestImpl::NullShadowPolicy AsyncRequestImpl::RouteEntryImpl::shadow_policy_;
-const AsyncRequestImpl::NullVirtualHost AsyncRequestImpl::RouteEntryImpl::virtual_host_;
-const AsyncRequestImpl::NullRateLimitPolicy AsyncRequestImpl::NullVirtualHost::rate_limit_policy_;
+    AsyncStreamImpl::NullRateLimitPolicy::rate_limit_policy_entry_;
+const AsyncStreamImpl::NullRateLimitPolicy AsyncStreamImpl::RouteEntryImpl::rate_limit_policy_;
+const AsyncStreamImpl::NullRetryPolicy AsyncStreamImpl::RouteEntryImpl::retry_policy_;
+const AsyncStreamImpl::NullShadowPolicy AsyncStreamImpl::RouteEntryImpl::shadow_policy_;
+const AsyncStreamImpl::NullVirtualHost AsyncStreamImpl::RouteEntryImpl::virtual_host_;
+const AsyncStreamImpl::NullRateLimitPolicy AsyncStreamImpl::NullVirtualHost::rate_limit_policy_;
 
 AsyncClientImpl::AsyncClientImpl(const Upstream::ClusterInfo& cluster, Stats::Store& stats_store,
                                  Event::Dispatcher& dispatcher,
@@ -22,64 +21,158 @@ AsyncClientImpl::AsyncClientImpl(const Upstream::ClusterInfo& cluster, Stats::St
       dispatcher_(dispatcher) {}
 
 AsyncClientImpl::~AsyncClientImpl() {
-  while (!active_requests_.empty()) {
-    active_requests_.front()->failDueToClientDestroy();
+  while (!active_streams_.empty()) {
+    active_streams_.front()->reset();
   }
 }
 
 AsyncClient::Request* AsyncClientImpl::send(MessagePtr&& request, AsyncClient::Callbacks& callbacks,
                                             const Optional<std::chrono::milliseconds>& timeout) {
-  std::unique_ptr<AsyncRequestImpl> new_request{
-      new AsyncRequestImpl(std::move(request), *this, callbacks, timeout)};
+  AsyncRequestImpl* async_request =
+      new AsyncRequestImpl(std::move(request), *this, callbacks, timeout);
+  std::unique_ptr<AsyncStreamImpl> new_request{async_request};
 
   // The request may get immediately failed. If so, we will return nullptr.
-  if (!new_request->complete_) {
-    new_request->moveIntoList(std::move(new_request), active_requests_);
-    return active_requests_.front().get();
+  if (!new_request->remote_closed_) {
+    new_request->moveIntoList(std::move(new_request), active_streams_);
+    return async_request;
   } else {
     return nullptr;
   }
 }
 
-AsyncRequestImpl::AsyncRequestImpl(MessagePtr&& request, AsyncClientImpl& parent,
-                                   AsyncClient::Callbacks& callbacks,
-                                   const Optional<std::chrono::milliseconds>& timeout)
-    : request_(std::move(request)), parent_(parent), callbacks_(callbacks),
-      stream_id_(parent.config_.random_.random()), router_(parent.config_),
-      request_info_(Protocol::Http11), route_(parent_.cluster_.name(), timeout) {
+AsyncClient::Stream* AsyncClientImpl::start(AsyncClient::StreamCallbacks& callbacks,
+                                            const Optional<std::chrono::milliseconds>& timeout) {
+  std::unique_ptr<AsyncStreamImpl> new_stream{new AsyncStreamImpl(*this, callbacks, timeout)};
+
+  // The request may get immediately failed. If so, we will return nullptr.
+  if (!new_stream->remote_closed_) {
+    new_stream->moveIntoList(std::move(new_stream), active_streams_);
+    return active_streams_.front().get();
+  } else {
+    return nullptr;
+  }
+}
+
+AsyncStreamImpl::AsyncStreamImpl(AsyncClientImpl& parent, AsyncClient::StreamCallbacks& callbacks,
+                                 const Optional<std::chrono::milliseconds>& timeout)
+    : parent_(parent), stream_callbacks_(callbacks), stream_id_(parent.config_.random_.random()),
+      router_(parent.config_), request_info_(Protocol::Http11),
+      route_(parent_.cluster_.name(), timeout) {
 
   router_.setDecoderFilterCallbacks(*this);
-  request_->headers().insertEnvoyInternalRequest().value(
-      Headers::get().EnvoyInternalRequestValues.True);
-  request_->headers().insertForwardedFor().value(parent_.config_.local_info_.address());
-  router_.decodeHeaders(request_->headers(), !request_->body());
-  if (!complete_ && request_->body()) {
-    router_.decodeData(*request_->body(), true);
-  }
-
-  // TODO: Support request trailers.
   // TODO: Correctly set protocol in request info when we support access logging.
 }
 
-AsyncRequestImpl::~AsyncRequestImpl() { ASSERT(!reset_callback_); }
+AsyncStreamImpl::~AsyncStreamImpl() { ASSERT(!reset_callback_); }
 
-void AsyncRequestImpl::encodeHeaders(HeaderMapPtr&& headers, bool end_stream) {
-  response_.reset(new ResponseMessageImpl(std::move(headers)));
+void AsyncStreamImpl::encodeHeaders(HeaderMapPtr&& headers, bool end_stream) {
 #ifndef NDEBUG
   log_debug("async http request response headers (end_stream={}):", end_stream);
-  response_->headers().iterate([](const HeaderEntry& header, void*) -> void {
+  headers->iterate([](const HeaderEntry& header, void*) -> void {
     log_debug("  '{}':'{}'", header.key().c_str(), header.value().c_str());
   }, nullptr);
 #endif
+
+  stream_callbacks_.onHeaders(std::move(headers), end_stream);
+  closeRemote(end_stream);
+}
+
+void AsyncStreamImpl::encodeData(Buffer::Instance& data, bool end_stream) {
+  log_trace("async http request response data (length={} end_stream={})", data.length(),
+            end_stream);
+  stream_callbacks_.onData(data, end_stream);
+  closeRemote(end_stream);
+}
+
+void AsyncStreamImpl::encodeTrailers(HeaderMapPtr&& trailers) {
+#ifndef NDEBUG
+  log_debug("async http request response trailers:");
+  trailers->iterate([](const HeaderEntry& header, void*) -> void {
+    log_debug("  '{}':'{}'", header.key().c_str(), header.value().c_str());
+  }, nullptr);
+#endif
+
+  stream_callbacks_.onTrailers(std::move(trailers));
+  closeRemote(true);
+}
+
+void AsyncStreamImpl::sendHeaders(HeaderMap& headers, bool end_stream) {
+  headers.insertEnvoyInternalRequest().value(Headers::get().EnvoyInternalRequestValues.True);
+  headers.insertForwardedFor().value(parent_.config_.local_info_.address());
+  router_.decodeHeaders(headers, end_stream);
+  closeLocal(end_stream);
+}
+
+void AsyncStreamImpl::sendData(Buffer::Instance& data, bool end_stream) {
+  router_.decodeData(data, end_stream);
+  closeLocal(end_stream);
+}
+
+void AsyncStreamImpl::sendTrailers(HeaderMap& trailers) {
+  router_.decodeTrailers(trailers);
+  closeLocal(true);
+}
+
+void AsyncStreamImpl::closeLocal(bool end_stream) {
+  ASSERT(!(local_closed_ && end_stream));
+
+  local_closed_ |= end_stream;
+  if (complete()) {
+    cleanup();
+  }
+}
+
+void AsyncStreamImpl::closeRemote(bool end_stream) {
+  remote_closed_ |= end_stream;
+  if (complete()) {
+    cleanup();
+  }
+}
+
+void AsyncStreamImpl::reset() {
+  reset_callback_();
+  resetStream();
+}
+
+void AsyncStreamImpl::cleanup() {
+  reset_callback_ = nullptr;
+
+  // This will destroy us, but only do so if we are actually in a list. This does not happen in
+  // the immediate failure case.
+  if (inserted()) {
+    removeFromList(parent_.active_streams_);
+  }
+}
+
+void AsyncStreamImpl::resetStream() {
+  stream_callbacks_.onReset();
+  cleanup();
+}
+
+AsyncRequestImpl::AsyncRequestImpl(MessagePtr&& request, AsyncClientImpl& parent,
+                                   AsyncClient::Callbacks& callbacks,
+                                   const Optional<std::chrono::milliseconds>& timeout)
+    : AsyncStreamImpl(parent, *this, timeout), request_(std::move(request)), callbacks_(callbacks) {
+
+  sendHeaders(request_->headers(), !request_->body());
+  if (!complete() && request_->body()) {
+    sendData(*request_->body(), true);
+  }
+  // TODO: Support request trailers.
+}
+
+void AsyncRequestImpl::onComplete() { callbacks_.onSuccess(std::move(response_)); }
+
+void AsyncRequestImpl::onHeaders(HeaderMapPtr&& headers, bool end_stream) {
+  response_.reset(new ResponseMessageImpl(std::move(headers)));
 
   if (end_stream) {
     onComplete();
   }
 }
 
-void AsyncRequestImpl::encodeData(Buffer::Instance& data, bool end_stream) {
-  log_trace("async http request response data (length={} end_stream={})", data.length(),
-            end_stream);
+void AsyncRequestImpl::onData(Buffer::Instance& data, bool end_stream) {
   if (!response_->body()) {
     response_->body(Buffer::InstancePtr{new Buffer::OwnedImpl()});
   }
@@ -90,52 +183,21 @@ void AsyncRequestImpl::encodeData(Buffer::Instance& data, bool end_stream) {
   }
 }
 
-void AsyncRequestImpl::encodeTrailers(HeaderMapPtr&& trailers) {
+void AsyncRequestImpl::onTrailers(HeaderMapPtr&& trailers) {
   response_->trailers(std::move(trailers));
-#ifndef NDEBUG
-  log_debug("async http request response trailers:");
-  response_->trailers()->iterate([](const HeaderEntry& header, void*) -> void {
-    log_debug("  '{}':'{}'", header.key().c_str(), header.value().c_str());
-  }, nullptr);
-#endif
-
   onComplete();
 }
 
-void AsyncRequestImpl::cancel() {
-  reset_callback_();
-  cleanup();
-}
-
-void AsyncRequestImpl::onComplete() {
-  complete_ = true;
-  callbacks_.onSuccess(std::move(response_));
-  cleanup();
-}
-
-void AsyncRequestImpl::cleanup() {
-  response_.reset();
-  reset_callback_ = nullptr;
-
-  // This will destroy us, but only do so if we are actually in a list. This does not happen in
-  // the immediate failure case.
-  if (inserted()) {
-    removeFromList(parent_.active_requests_);
+void AsyncRequestImpl::onReset() {
+  if (!cancelled_) {
+    // In this case we don't have a valid response so we do need to raise a failure.
+    callbacks_.onFailure(AsyncClient::FailureReason::Reset);
   }
 }
 
-void AsyncRequestImpl::resetStream() {
-  // In this case we don't have a valid response so we do need to raise a failure.
-  callbacks_.onFailure(AsyncClient::FailureReason::Reset);
-  cleanup();
-}
-
-void AsyncRequestImpl::failDueToClientDestroy() {
-  // In this case we are going away because the client is being destroyed. We need to both reset
-  // the stream as well as raise a failure callback.
-  reset_callback_();
-  callbacks_.onFailure(AsyncClient::FailureReason::Reset);
-  cleanup();
+void AsyncRequestImpl::cancel() {
+  cancelled_ = true;
+  reset();
 }
 
 } // Http

source/common/http/async_client_impl.h
@@ -18,6 +18,7 @@
 
 namespace Http {
 
+class AsyncStreamImpl;
 class AsyncRequestImpl;
 
 class AsyncClientImpl final : public AsyncClient {
@@ -32,12 +33,16 @@ public:
   Request* send(MessagePtr&& request, Callbacks& callbacks,
                 const Optional<std::chrono::milliseconds>& timeout) override;
 
+  Stream* start(StreamCallbacks& callbacks,
+                const Optional<std::chrono::milliseconds>& timeout) override;
+
 private:
   const Upstream::ClusterInfo& cluster_;
   Router::FilterConfig config_;
   Event::Dispatcher& dispatcher_;
-  std::list<std::unique_ptr<AsyncRequestImpl>> active_requests_;
+  std::list<std::unique_ptr<AsyncStreamImpl>> active_streams_;
 
+  friend class AsyncStreamImpl;
   friend class AsyncRequestImpl;
 };
 
@@ -45,17 +50,24 @@ private:
  * Implementation of AsyncRequest. This implementation is capable of sending HTTP requests to a
  * ConnectionPool asynchronously.
  */
-class AsyncRequestImpl final : public AsyncClient::Request,
-                               StreamDecoderFilterCallbacks,
-                               Logger::Loggable<Logger::Id::http>,
-                               LinkedObject<AsyncRequestImpl> {
+class AsyncStreamImpl : public AsyncClient::Stream,
+                        StreamDecoderFilterCallbacks,
+                        Logger::Loggable<Logger::Id::http>,
+                        LinkedObject<AsyncStreamImpl> {
 public:
-  AsyncRequestImpl(MessagePtr&& request, AsyncClientImpl& parent, AsyncClient::Callbacks& callbacks,
-                   const Optional<std::chrono::milliseconds>& timeout);
-  ~AsyncRequestImpl();
+  AsyncStreamImpl(AsyncClientImpl& parent, AsyncClient::StreamCallbacks& callbacks,
+                  const Optional<std::chrono::milliseconds>& timeout);
+  virtual ~AsyncStreamImpl();
 
-  // Http::AsyncHttpRequest
-  void cancel() override;
+  // Http::AsyncClient::Stream
+  void sendHeaders(HeaderMap& headers, bool end_stream) override;
+  void sendData(Buffer::Instance& data, bool end_stream) override;
+  void sendTrailers(HeaderMap& trailers) override;
+  void reset() override;
+
+protected:
+  bool complete() { return local_closed_ && remote_closed_; }
+  AsyncClientImpl& parent_;
 
 private:
   struct NullRateLimitPolicy : public Router::RateLimitPolicy {
@@ -136,8 +148,9 @@ private:
   };
 
   void cleanup();
-  void failDueToClientDestroy();
-  void onComplete();
+
+  void closeLocal(bool end_stream);
+  void closeRemote(bool end_stream);
 
   // Http::StreamDecoderFilterCallbacks
   void addResetStreamCallback(std::function<void()> callback) override {
@@ -151,21 +164,51 @@ private:
   AccessLog::RequestInfo& requestInfo() override { return request_info_; }
   const std::string& downstreamAddress() override { return EMPTY_STRING; }
   void continueDecoding() override { NOT_IMPLEMENTED; }
-  const Buffer::Instance* decodingBuffer() override { return request_->body(); }
+  const Buffer::Instance* decodingBuffer() override {
+    throw EnvoyException("buffering is not supported in streaming");
+  }
   void encodeHeaders(HeaderMapPtr&& headers, bool end_stream) override;
   void encodeData(Buffer::Instance& data, bool end_stream) override;
   void encodeTrailers(HeaderMapPtr&& trailers) override;
 
-  MessagePtr request_;
-  AsyncClientImpl& parent_;
-  AsyncClient::Callbacks& callbacks_;
+  AsyncClient::StreamCallbacks& stream_callbacks_;
   const uint64_t stream_id_;
-  std::unique_ptr<MessageImpl> response_;
   Router::ProdFilter router_;
   std::function<void()> reset_callback_;
   AccessLog::RequestInfoImpl request_info_;
   RouteImpl route_;
-  bool complete_{};
+  bool local_closed_{};
+  bool remote_closed_{};
+
+  friend class AsyncClientImpl;
+};
+
+class AsyncRequestImpl final : public AsyncClient::Request,
+                               AsyncStreamImpl,
+                               AsyncClient::StreamCallbacks {
+public:
+  AsyncRequestImpl(MessagePtr&& request, AsyncClientImpl& parent, AsyncClient::Callbacks& callbacks,
+                   const Optional<std::chrono::milliseconds>& timeout);
+
+  // AsyncClient::Request
+  virtual void cancel() override;
+
+private:
+  void onComplete();
+
+  // AsyncClient::StreamCallbacks
+  void onHeaders(HeaderMapPtr&& headers, bool end_stream) override;
+  void onData(Buffer::Instance& data, bool end_stream) override;
+  void onTrailers(HeaderMapPtr&& trailers) override;
+  void onReset() override;
+
+  // Http::StreamDecoderFilterCallbacks
+  const Buffer::Instance* decodingBuffer() override { return request_->body(); }
+
+  MessagePtr request_;
+  AsyncClient::Callbacks& callbacks_;
+  std::unique_ptr<MessageImpl> response_;
+  bool cancelled_{};
 
   friend class AsyncClientImpl;
 };

include/envoy/http/async_client.h
@@ -38,7 +38,45 @@ public:
   };
 
   /**
-   * An in-flight HTTP request
+   * Notifies caller of async HTTP stream status.
+   * Note the HTTP stream is full-duplex, even if the local to remote stream has been ended
+   * by Stream.sendHeaders/sendData with end_stream=true or sendTrailers,
+   * StreamCallbacks can continue to receive events until the remote to local stream is closed,
+   * and vice versa.
+   */
+  class StreamCallbacks {
+  public:
+    virtual ~StreamCallbacks() {}
+
+    /**
+     * Called when all headers get received on the async HTTP stream.
+     * @param headers the headers received
+     * @param end_stream whether the response is header only
+     */
+    virtual void onHeaders(HeaderMapPtr&& headers, bool end_stream) PURE;
+
+    /**
+     * Called when a data frame get received on the async HTTP stream.
+     * This can be invoked multiple times if the data get streamed.
+     * @param data the data received
+     * @param end_stream whether the data is the last data frame
+     */
+    virtual void onData(Buffer::Instance& data, bool end_stream) PURE;
+
+    /**
+     * Called when all trailers get received on the async HTTP stream.
+     * @param trailers the trailers received.
+     */
+    virtual void onTrailers(HeaderMapPtr&& trailers) PURE;
+
+    /**
+     * Called when the async HTTP stream is reset.
+     */
+    virtual void onReset() PURE;
+  };
+
+  /**
+   * An in-flight HTTP request.
    */
   class Request {
   public:
@@ -50,17 +88,65 @@ public:
     virtual void cancel() PURE;
   };
 
+  /**
+   * An in-flight HTTP stream.
+   */
+  class Stream {
+  public:
+    virtual ~Stream() {}
+
+    /***
+     * Send headers to the stream. This method cannot be invoked more than once and
+     * need to be called before sendData.
+     * @param headers supplies the headers to send.
+     * @param end_stream supplies whether this is a header only request.
+     */
+    virtual void sendHeaders(HeaderMap& headers, bool end_stream) PURE;
+
+    /***
+     * Send data to the stream. This method can be invoked multiple times if it get streamed.
+     * To end the stream without data, call this method with empty buffer.
+     * @param data supplies the data to send.
+     * @param end_stream supplies whether this is the last data.
+     */
+    virtual void sendData(Buffer::Instance& data, bool end_stream) PURE;
+
+    /***
+     * Send trailers. This method cannot be invoked more than once, and implicitly ends the stream.
+     * @param trailers supplies the trailers to send.
+     */
+    virtual void sendTrailers(HeaderMap& trailers) PURE;
+
+    /***
+     * Reset the stream.
+     */
+    virtual void reset() PURE;
+  };
+
   virtual ~AsyncClient() {}
 
   /**
    * Send an HTTP request asynchronously
    * @param request the request to send.
    * @param callbacks the callbacks to be notified of request status.
+   * @param timeout supplies the request timeout
    * @return a request handle or nullptr if no request could be created. NOTE: In this case
    *         onFailure() has already been called inline. The client owns the request and the
    *         handle should just be used to cancel.
    */
   virtual Request* send(MessagePtr&& request, Callbacks& callbacks,
+                        const Optional<std::chrono::milliseconds>& timeout) PURE;
+
+  /**
+   * Start an HTTP stream asynchronously.
+   * @param callbacks the callbacks to be notified of stream status.
+   * @param timeout supplies the stream timeout, measured since when the frame with end_stream
+   *        flag is sent until when the first frame is received.
+   * @return a stream handle or nullptr if no stream could be started. NOTE: In this case
+   *         onResetStream() has already been called inline. The client owns the stream and
+   *         the handle can be used to send more messages or close the stream.
+   */
+  virtual Stream* start(StreamCallbacks& callbacks,
                         const Optional<std::chrono::milliseconds>& timeout) PURE;
 };
 

test/mocks/http/mocks.cc
@@ -112,6 +112,9 @@ MockAsyncClient::~MockAsyncClient() {}
 MockAsyncClientCallbacks::MockAsyncClientCallbacks() {}
 MockAsyncClientCallbacks::~MockAsyncClientCallbacks() {}
 
+MockAsyncClientStreamCallbacks::MockAsyncClientStreamCallbacks() {}
+MockAsyncClientStreamCallbacks::~MockAsyncClientStreamCallbacks() {}
+
 MockAsyncClientRequest::MockAsyncClientRequest(MockAsyncClient* client) : client_(client) {}
 MockAsyncClientRequest::~MockAsyncClientRequest() { client_->onRequestDestroy(); }
 

test/mocks/http/mocks.h
@@ -298,6 +298,9 @@ public:
 
   MOCK_METHOD3(send_, Request*(MessagePtr& request, Callbacks& callbacks,
                                const Optional<std::chrono::milliseconds>& timeout));
+
+  MOCK_METHOD2(start, Stream*(StreamCallbacks& callbacks,
+                              const Optional<std::chrono::milliseconds>& timeout));
 };
 
 class MockAsyncClientCallbacks : public AsyncClient::Callbacks {
@@ -310,6 +313,22 @@ public:
   // Http::AsyncClient::Callbacks
   MOCK_METHOD1(onSuccess_, void(Message* response));
   MOCK_METHOD1(onFailure, void(Http::AsyncClient::FailureReason reason));
+};
+
+class MockAsyncClientStreamCallbacks : public AsyncClient::StreamCallbacks {
+public:
+  MockAsyncClientStreamCallbacks();
+  ~MockAsyncClientStreamCallbacks();
+
+  void onHeaders(HeaderMapPtr&& headers, bool end_stream) override {
+    onHeaders_(*headers, end_stream);
+  }
+  void onTrailers(HeaderMapPtr&& trailers) override { onTrailers_(*trailers); }
+
+  MOCK_METHOD2(onHeaders_, void(HeaderMap& headers, bool end_stream));
+  MOCK_METHOD2(onData, void(Buffer::Instance& data, bool end_stream));
+  MOCK_METHOD1(onTrailers_, void(HeaderMap& headers));
+  MOCK_METHOD0(onReset, void());
 };
 
 class MockAsyncClientRequest : public AsyncClient::Request {

commit d4c0900db4a6edcb287d99036aaac8ef5ef954a7

async client: fix immediate response when request has body
regression from https://github.com/lyft/envoy/pull/353

source/common/http/async_client_impl.cc
@@ -37,6 +37,7 @@ AsyncClient::Request* AsyncClientImpl::send(MessagePtr&& request, AsyncClient::C
     new_request->moveIntoList(std::move(new_request), active_streams_);
     return async_request;
   } else {
+    new_request->cleanup();
     return nullptr;
   }
 }
@@ -44,14 +45,8 @@ AsyncClient::Request* AsyncClientImpl::send(MessagePtr&& request, AsyncClient::C
 AsyncClient::Stream* AsyncClientImpl::start(AsyncClient::StreamCallbacks& callbacks,
                                             const Optional<std::chrono::milliseconds>& timeout) {
   std::unique_ptr<AsyncStreamImpl> new_stream{new AsyncStreamImpl(*this, callbacks, timeout)};
-
-  // The request may get immediately failed. If so, we will return nullptr.
-  if (!new_stream->remote_closed_) {
-    new_stream->moveIntoList(std::move(new_stream), active_streams_);
-    return active_streams_.front().get();
-  } else {
-    return nullptr;
-  }
+  new_stream->moveIntoList(std::move(new_stream), active_streams_);
+  return active_streams_.front().get();
 }
 
 AsyncStreamImpl::AsyncStreamImpl(AsyncClientImpl& parent, AsyncClient::StreamCallbacks& callbacks,
@@ -156,7 +151,7 @@ AsyncRequestImpl::AsyncRequestImpl(MessagePtr&& request, AsyncClientImpl& parent
     : AsyncStreamImpl(parent, *this, timeout), request_(std::move(request)), callbacks_(callbacks) {
 
   sendHeaders(request_->headers(), !request_->body());
-  if (!complete() && request_->body()) {
+  if (!remoteClosed() && request_->body()) {
     sendData(*request_->body(), true);
   }
   // TODO: Support request trailers.

source/common/http/async_client_impl.h
@@ -66,7 +66,8 @@ public:
   void reset() override;
 
 protected:
-  bool complete() { return local_closed_ && remote_closed_; }
+  bool remoteClosed() { return remote_closed_; }
+
   AsyncClientImpl& parent_;
 
 private:
@@ -148,9 +149,9 @@ private:
   };
 
   void cleanup();
-
   void closeLocal(bool end_stream);
   void closeRemote(bool end_stream);
+  bool complete() { return local_closed_ && remote_closed_; }
 
   // Http::StreamDecoderFilterCallbacks
   void addResetStreamCallback(std::function<void()> callback) override {

test/common/http/async_client_impl_test.cc
@@ -610,6 +610,22 @@ TEST_F(AsyncClientImplTest, PoolFailure) {
   EXPECT_EQ(1UL, cm_.cluster_.info_->stats_store_.counter("upstream_rq_503").value());
 }
 
+TEST_F(AsyncClientImplTest, PoolFailureWithBody) {
+  EXPECT_CALL(cm_.conn_pool_, newStream(_, _))
+      .WillOnce(Invoke([&](StreamDecoder&,
+                           ConnectionPool::Callbacks& callbacks) -> ConnectionPool::Cancellable* {
+        callbacks.onPoolFailure(ConnectionPool::PoolFailureReason::Overflow, nullptr);
+        return nullptr;
+      }));
+
+  expectSuccess(503);
+  message_->body(Buffer::InstancePtr{new Buffer::OwnedImpl("hello")});
+  EXPECT_EQ(nullptr,
+            client_.send(std::move(message_), callbacks_, Optional<std::chrono::milliseconds>()));
+
+  EXPECT_EQ(1UL, cm_.cluster_.info_->stats_store_.counter("upstream_rq_503").value());
+}
+
 TEST_F(AsyncClientImplTest, StreamTimeout) {
   EXPECT_CALL(cm_.conn_pool_, newStream(_, _))
       .WillOnce(Invoke([&](StreamDecoder&, ConnectionPool::Callbacks& callbacks)