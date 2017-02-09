4d9c75119115a5654875aeb84375a515d0bd4d63
Roman Dzhabarov <rdzhabarov@lyft.com>
2017-02-09 15:54:09 -0800

    introduce Base64::encode on string

--- source/common/common/base64.h
+++ source/common/common/base64.h
@@ -12,6 +12,12 @@ public:
   static std::string encode(const Buffer::Instance& buffer, uint64_t length);
 
   /**
+   * Base64 encode an input string.
+   * @param input string to encode
+   */
+  static std::string encode(const std::string& input);
+
+  /**
    * Base64 decode an input string.
    * @param input supplies the input to decode.
    *
@@ -19,4 +25,15 @@ public:
    * bytes.
    */
   static std::string decode(const std::string& input);
+
+private:
+  /**
+   * Helper methods for encoding.
+   */
+  static inline void encode_base(const uint8_t cur_char, uint64_t pos, uint8_t& next_c,
+                                 std::string& ret);
+  /**
+   * Encode last characters if input string length is not divisible by 3.
+   */
+  static inline void encode_last(uint64_t pos, uint8_t last_char, std::string& ret);
 };
--- test/common/common/base64_test.cc
+++ test/common/common/base64_test.cc
@@ -22,6 +22,16 @@ TEST(Base64Test, SingleSliceBufferEncode) {
   EXPECT_EQ("Zm8=", Base64::encode(buffer, 2));
 }
 
+TEST(Base64Test, EncodeString) {
+  EXPECT_EQ("", Base64::encode(""));
+
+  std::string two_zero_bytes("\0\0", 2);
+  EXPECT_EQ("AAA=", Base64::encode(two_zero_bytes));
+
+  EXPECT_EQ("Zm9v", Base64::encode("foo"));
+  EXPECT_EQ("Zm8=", Base64::encode("fo"));
+}
+
 TEST(Base64Test, Decode) {
   EXPECT_EQ("", Base64::decode(""));
   EXPECT_EQ("foo", Base64::decode("Zm9v"));
@@ -43,10 +53,22 @@ TEST(Base64Test, Decode) {
   }
 
   {
+    std::string test_string("\0\0\0\0als;jkopqitu[\0opbjlcxnb35g]b[\xaa\b\n", 36);
+    EXPECT_FALSE(
+        memcmp(test_string.data(), Base64::decode(Base64::encode(test_string)).data(), 36));
+  }
+
+  {
     std::string test_string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     std::string decoded = Base64::decode(test_string);
     Buffer::OwnedImpl buffer(decoded);
     EXPECT_EQ(test_string, Base64::encode(buffer, decoded.length()));
+  }
+
+  {
+    std::string test_string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    std::string decoded = Base64::decode(test_string);
+    EXPECT_EQ(test_string, Base64::encode(decoded));
   }
 }
 
