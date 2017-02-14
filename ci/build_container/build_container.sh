d4610d5d7ac01b275612b14f0fbef72b1b374d87
Roman Dzhabarov <rdzhabarov@lyft.com>
2017-02-14 14:19:12 -0800

    Update LS library version to 0_33. (#480)

--- ci/build_container/build_container.sh
+++ ci/build_container/build_container.sh
@@ -113,13 +113,13 @@ tar xf tclap-1.2.1.tar.gz
 rm tclap-1.2.1.tar.gz
 
 # lightstep
-wget https://github.com/lightstep/lightstep-tracer-cpp/releases/download/v0_19/lightstep-tracer-cpp-0.19.tar.gz
-tar xf lightstep-tracer-cpp-0.19.tar.gz
-cd lightstep-tracer-cpp-0.19
+wget https://github.com/lightstep/lightstep-tracer-cpp/releases/download/v0_33/lightstep-tracer-cpp-0.33.tar.gz
+tar xf lightstep-tracer-cpp-0.33.tar.gz
+cd lightstep-tracer-cpp-0.33
 ./configure --disable-grpc --prefix=$THIRDPARTY_BUILD --enable-shared=no \
-	    protobuf_CFLAGS="-I$THIRDPARTY_BUILD/include" protobuf_LIBS="-L$THIRDPARTY_BUILD/lib -lprotobuf"
+	    protobuf_CFLAGS="-I$THIRDPARTY_BUILD/include" protobuf_LIBS="-L$THIRDPARTY_BUILD/lib -lprotobuf" PROTOC=$THIRDPARTY_BUILD/bin/protoc
 make install
-rm -rf lightstep-tracer-cpp-0.19
+rm -rf lightstep-tracer-cpp-0.33
 cd ..
 
 # rapidjson
