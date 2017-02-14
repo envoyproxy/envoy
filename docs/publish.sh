986ad3886a67f93b6a9fab0acffe949fef12082b
Jose Ulises Nino Rivera <junr03@users.noreply.github.com>
2017-02-14 14:41:05 -0800

    uncomment

--- docs/publish.sh
+++ docs/publish.sh
@@ -10,13 +10,13 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] &&\
   [ "$TEST_TYPE" == "normal" ]
 then
   git clone https://GH_TOKEN@github.com/lyft/envoy $PUBLISH_DIR
-  #git -C $PUBLISH_DIR config user.name "Publish Docs"
-  #git -C $PUBLISH_DIR config user.email GH_EMAIL
+  git -C $PUBLISH_DIR config user.name "Publish Docs"
+  git -C $PUBLISH_DIR config user.email GH_EMAIL
   git -C $PUBLISH_DIR fetch
   git -C $PUBLISH_DIR checkout -B gh-pages origin/gh-pages
   rm -fr $PUBLISH_DIR/*
   cp -r $DOCS_DIR/* $PUBLISH_DIR
   git -C $PUBLISH_DIR add .
   git -C $PUBLISH_DIR commit -m "docs @$BUILD_SHA"
-  #git -C $PUBLISH_DIR push origin gh-pages
+  git -C $PUBLISH_DIR push origin gh-pages
 fi
