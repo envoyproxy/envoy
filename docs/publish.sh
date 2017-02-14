c02d8cd16b7dae1005444a04033fa5974c6fa9a9
Jose Nino <jnino@lyft.com>
2017-02-14 14:21:15 -0800

    format

--- docs/publish.sh
+++ docs/publish.sh
@@ -6,16 +6,17 @@ DOCS_DIR=$1
 PUBLISH_DIR=$2
 BUILD_SHA=`git rev-parse HEAD`
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TEST_TYPE" == "normal" ]
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] &&\
+  [ "$TEST_TYPE" == "normal" ]
 then
   git clone https://GH_TOKEN@github.com/lyft/envoy $PUBLISH_DIR
-  git -C $PUBLISH_DIR config user.name "Publish Docs"
-  git -C $PUBLISH_DIR config user.email GH_EMAIL
+  #git -C $PUBLISH_DIR config user.name "Publish Docs"
+  #git -C $PUBLISH_DIR config user.email GH_EMAIL
   git -C $PUBLISH_DIR fetch
   git -C $PUBLISH_DIR checkout -B gh-pages origin/gh-pages
   rm -fr $PUBLISH_DIR/*
   cp -r $DOCS_DIR/* $PUBLISH_DIR
   git -C $PUBLISH_DIR add .
   git -C $PUBLISH_DIR commit -m "docs @$BUILD_SHA"
-  git -C $PUBLISH_DIR push origin gh-pages
+  #git -C $PUBLISH_DIR push origin gh-pages
 fi
