d9c39954ca4b348c0c49ae6b1a5438307510f244
Matt Klein <mattklein123@users.noreply.github.com>
2017-03-06 11:17:27 -0800

    test: raise coverage bar to 98% (#536)

--- test/run_envoy_coverage.sh
+++ test/run_envoy_coverage.sh
@@ -12,7 +12,7 @@ $PROJECT_SOURCE_DIR/test/run_envoy_tests.sh $PROJECT_SOURCE_DIR $PROJECT_BINARY_
 $GCOVR $GCOVR_EXTRA_ARGS -r $SOURCE_DIR --html --html-details -o $BINARY_DIR/coverage.html --exclude-unreachable-branches --print-summary > coverage_summary.txt
 
 coverage_value=`grep -Po 'lines: \K(\d|\.)*' coverage_summary.txt`
-coverage_threshold=95.0
+coverage_threshold=98.0
 coverage_failed=`echo "$coverage_value<$coverage_threshold" | bc`
 if test $coverage_failed -eq 1; then
     echo Code coverage $coverage_value is lower than limit of $coverage_threshold
