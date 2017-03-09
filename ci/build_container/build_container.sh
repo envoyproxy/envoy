446da589e2f44123057bfc1fe91b0ccdc0613a14
Roman Dzhabarov <rdzhabarov@lyft.com>
2017-03-09 12:22:39 -0800

    version 0.36.

--- ci/build_container/build_container.sh
+++ ci/build_container/build_container.sh
@@ -113,9 +113,9 @@ tar xf tclap-1.2.1.tar.gz
 rm tclap-1.2.1.tar.gz
 
 # lightstep
-wget https://github.com/lightstep/lightstep-tracer-cpp/releases/download/v0_33/lightstep-tracer-cpp-0.33.tar.gz
-tar xf lightstep-tracer-cpp-0.33.tar.gz
-cd lightstep-tracer-cpp-0.33
+wget https://github.com/lightstep/lightstep-tracer-cpp/releases/download/v0_36/lightstep-tracer-cpp-0.36.tar.gz
+tar xf lightstep-tracer-cpp-0.36.tar.gz
+cd lightstep-tracer-cpp-0.36
 ./configure --disable-grpc --prefix=$THIRDPARTY_BUILD --enable-shared=no \
 	    protobuf_CFLAGS="-I$THIRDPARTY_BUILD/include" protobuf_LIBS="-L$THIRDPARTY_BUILD/lib -lprotobuf" PROTOC=$THIRDPARTY_BUILD/bin/protoc
 make install
