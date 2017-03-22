1a36c01708e0e45f9603b88283ee61c78776ee62
Zack Hsi <zackhsi@gmail.com>
2017-03-22 11:45:57 -0700

    Exit nonzero when all children die
    When Envoy dies, for example when there is an invalid config schema, it
    should exit nonzero to indicate that something went wrong.

--- restarter/hot-restarter.py
+++ restarter/hot-restarter.py
@@ -98,7 +98,7 @@ def sigchld_handler(signum, frame):
   # Our last child died, so we have no purpose. Exit.
   if not pid_list:
     print "exiting due to lack of child processes"
-    sys.exit(0)
+    sys.exit(1)
 
 
 def fork_and_exec():
