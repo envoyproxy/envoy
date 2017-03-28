311f570f87166856905e8e9d8a24881aa3a0057a
Harvey Tuch <htuch@google.com>
2017-03-28 17:38:24 -0400

    Nit.

--- source/common/network/listen_socket_impl.cc
+++ source/common/network/listen_socket_impl.cc
@@ -15,8 +15,7 @@ void ListenSocketImpl::doBind() {
     throw EnvoyException(
         fmt::format("cannot bind '{}': {}", local_address_->asString(), strerror(errno)));
   }
-  if (local_address_->type() == Address::Type::Ip && local_address_->ip() != nullptr &&
-      local_address_->ip()->port() == 0) {
+  if (local_address_->type() == Address::Type::Ip && local_address_->ip()->port() == 0) {
     // If the port we bind is zero, then the OS will pick a free port for us (assuming there are
     // any), and we need to find out the port number that the OS picked.
     local_address_ = Address::addressFromFd(fd_);
