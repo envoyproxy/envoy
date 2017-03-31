e4f0f2f3fbe27efc6827104095b6c9077cc46dae
Venkata Siva Vijayendra Bhamidipati <vbhamidipati@ebay.com>
2017-03-31 14:25:31 -0700

    Update patch that builds both debug and non-debug alpine images.

--- ci/build_alpine_container/run_alpine_binary_verification.sh
+++ ci/build_alpine_container/run_alpine_binary_verification.sh
@@ -1,22 +1,48 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -z "${ENVOY_TAG}" ]; then
-	ENVOY_TAG="latest"
-fi
+usage() {
+    echo "Usage: $0 [debug|stripped]"
+    exit 1
+}
 
-ENVOY_IMAGE=envoy-alpine:${ENVOY_TAG}
-TEST_CONTAINER=alpine_envoy_test_ct
+check_args() {
+    if [ $# -lt 1 ]; then
+        usage
+    fi
 
-# Spin up a test container for the smoke test.
-set +e
-docker rm -fv ${TEST_CONTAINER}
-set -e
-docker run -d --name ${TEST_CONTAINER} ${ENVOY_IMAGE} /usr/local/bin/envoy -c /usr/local/conf/envoy/google_com_proxy.json
-output=`docker exec alpine_envoy_test_ct ps aux | grep google_com_proxy`
-if [ -z "$output" ]; then
-	echo "Failed to detect envoy binary to be running!"
-	exit 1
-fi
-echo "Successfully started envoy in container ${TEST_CONTAINER}"
-#TODO - add more tests, like a health check.
+    type=$1
+
+    if [ "$type" != "debug" ] && [ "$type" != "stripped" ]; then
+        usage
+    fi
+}
+
+main() {
+    check_args $*
+    if [ -z "${ENVOY_TAG}" ]; then
+        ENVOY_TAG="latest"
+    fi
+
+    if [ "${type}" == "debug" ]; then
+        ENVOY_IMAGE=envoy-alpine-debug:${ENVOY_TAG}
+    else
+        ENVOY_IMAGE=envoy-alpine:${ENVOY_TAG}
+    fi
+    TEST_CONTAINER=alpine_envoy_test_ct
+
+    # Spin up a test container for the smoke test.
+    set +e
+    docker rm -fv ${TEST_CONTAINER}
+    set -e
+    docker run -d --name ${TEST_CONTAINER} ${ENVOY_IMAGE} /usr/local/bin/envoy -c /usr/local/conf/envoy/google_com_proxy.json
+    output=`docker exec alpine_envoy_test_ct ps aux | grep google_com_proxy`
+    if [ -z "$output" ]; then
+	    echo "Failed to detect envoy binary to be running!"
+	    exit 1
+    fi
+    echo "Successfully started envoy in container ${TEST_CONTAINER}"
+    #TODO - add more tests, like a health check.
+}
+
+main $*
--- ci/docker_push.sh
+++ ci/docker_push.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
+set -x
 
 if [ "$TEST_TYPE" == "normal" ]
 then
   # this is needed to verify the example images
   docker build -f ci/Dockerfile-envoy-image -t lyft/envoy:latest .
+  make -C ci/build_alpine_container
 
   # push the envoy image on merge to master
   want_push='false'
@@ -21,6 +23,10 @@ then
       docker push lyft/envoy:latest
       docker tag lyft/envoy:latest lyft/envoy:$TRAVIS_COMMIT
       docker push lyft/envoy:$TRAVIS_COMMIT
+      docker tag lyft/envoy-alpine:latest lyft/envoy-alpine:$TRAVIS_COMMIT
+      docker push lyft/envoy-alpine:$TRAVIS_COMMIT
+      docker tag lyft/envoy-alpine-debug:latest lyft/envoy-alpine-debug:$TRAVIS_COMMIT
+      docker push lyft/envoy-alpine-debug:$TRAVIS_COMMIT
   else
       echo 'Ignoring PR branch for docker push.'
   fi
