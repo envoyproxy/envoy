5128ab196272274110d0898903d006f0c62c9437
Harvey Tuch <htuch@google.com>
2017-04-01 15:47:52 -0400

    guarddog: bazel build files and duration atomic type for portability.
    While building with various versions of Clang, duration worked better than time_point.

--- source/server/watchdog_impl.h
+++ source/server/watchdog_impl.h
@@ -9,12 +9,6 @@ namespace Server {
 /**
  * This class stores the actual data about when the WatchDog was last touched
  * along with thread metadata.
- *
- * Implementation note: This currently relies on the convenient fact that
- * std::atomic<SystemTime> works because SystemTime degrades internally to a
- * uint64. If this changes in the future and SystemTime becomes a nontrivial
- * object a compilation error will result and some implementation changes will
- * be required to deal with atomicizable canonical representations.
  */
 class WatchDogImpl : public WatchDog {
 public:
@@ -24,21 +18,24 @@ public:
    */
   WatchDogImpl(int32_t thread_id, SystemTimeSource& tsource, std::chrono::milliseconds interval)
       : thread_id_(thread_id), time_source_(tsource),
-        latest_touch_time_(tsource.currentSystemTime()), timer_interval_(interval) {}
+        latest_touch_time_(tsource.currentSystemTime().time_since_epoch()),
+        timer_interval_(interval) {}
 
-  int32_t threadId() const { return thread_id_; }
-  SystemTime lastTouchTime() const { return latest_touch_time_.load(); }
+  int32_t threadId() const override { return thread_id_; }
+  SystemTime lastTouchTime() const override { return SystemTime(latest_touch_time_.load()); }
 
   // Server::WatchDog
   void startWatchdog(Event::Dispatcher& dispatcher) override;
-  void touch() override { latest_touch_time_.store(time_source_.currentSystemTime()); }
+  void touch() override {
+    latest_touch_time_.store(time_source_.currentSystemTime().time_since_epoch());
+  }
 
 private:
   const int32_t thread_id_;
   SystemTimeSource& time_source_;
-  std::atomic<SystemTime> latest_touch_time_;
+  std::atomic<std::chrono::system_clock::duration> latest_touch_time_;
   Event::TimerPtr timer_;
   const std::chrono::milliseconds timer_interval_;
 };
 
-} // Server
+} // namespace Server
