766430e1a7293a2f5fd51f3821ac60c12613333f
Venkata Siva Vijayendra Bhamidipati <vbhamidipati@ebay.com>
2017-04-03 17:34:54 -0700

    Fail build if core gets dumped, change to server_only for quicker build. Build alpine image only when pushing to hub.

--- ci/docker_push.sh
+++ ci/docker_push.sh
@@ -6,7 +6,6 @@ if [ "$TEST_TYPE" == "normal" ]
 then
   # this is needed to verify the example images
   docker build -f ci/Dockerfile-envoy-image -t lyft/envoy:latest .
-  make -C ci/build_alpine_container
 
   # push the envoy image on merge to master
   want_push='false'
@@ -23,6 +22,7 @@ then
       docker push lyft/envoy:latest
       docker tag lyft/envoy:latest lyft/envoy:$TRAVIS_COMMIT
       docker push lyft/envoy:$TRAVIS_COMMIT
+      make -C ci/build_alpine_container
       docker tag lyft/envoy-alpine:latest lyft/envoy-alpine:$TRAVIS_COMMIT
       docker push lyft/envoy-alpine:$TRAVIS_COMMIT
       docker tag lyft/envoy-alpine-debug:latest lyft/envoy-alpine-debug:$TRAVIS_COMMIT
