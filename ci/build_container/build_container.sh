2be5ba7b4df1d226dc877bb16905fd1a57300cd6
htuch <htuch@users.noreply.github.com>
2017-04-11 11:11:29 -0400

    ci: build and install custom Bazel binary for coverage runs. (#727)
    This version of Bazel will only be used for code coverage purposes. It's necessary due to
    the regression in C++ support noted in https://github.com/bazelbuild/bazel/issues/1118.
    
    The plan is to use regular Bazel for build types other than coverage, since we want to make sure
    that we don't depend on anything in this snowflake.
    
    Also added lcov to the build, since Bazel coverage needs this to be available for generating
    trace files from the .gcno/.gcda for each test.

--- ci/build_container/build_container.sh
+++ ci/build_container/build_container.sh
@@ -4,7 +4,7 @@ set -e
 
 # Setup basic requirements and install them.
 apt-get update
-apt-get install -y wget software-properties-common make cmake git python python-pip clang-format-3.6 bc libtool automake
+apt-get install -y wget software-properties-common make cmake git python python-pip clang-format-3.6 bc libtool automake lcov zip
 apt-get install -y golang
 # For debugging.
 apt-get install -y gdb strace
@@ -18,6 +18,17 @@ curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
 apt-get update
 apt-get install -y bazel
 rm -rf /var/lib/apt/lists/*
+
+# Build a version of Bazel suitable for C++ code coverage collection. See
+# https://github.com/bazelbuild/bazel/issues/1118 for why we need this. This is the envoy-coverage
+# branch on the cloned repository.
+git clone https://github.com/htuch/bazel.git /tmp/bazel-coverage
+pushd /tmp/bazel-coverage
+git checkout 63f0542560773e973c9963845d5bbc30be75441a
+bazel build --spawn_strategy=standalone --genrule_strategy=standalone //src:bazel
+cp bazel-bin/src/bazel /usr/bin/bazel-coverage
+popd
+rm -rf /root/.cache /tmp/bazel-coverage
 
 # virtualenv
 pip install virtualenv
