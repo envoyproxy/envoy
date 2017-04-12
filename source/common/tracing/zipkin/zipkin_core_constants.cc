2c8884a2de46ae66adcaa5f2484f8ac02600af26
FABIO A. OLIVEIRA <fabolive@us.ibm.com>
2017-04-12 12:04:35 -0400

    Use file-level namespace for constant definitions (#430)

--- source/common/tracing/zipkin/zipkin_core_constants.cc
+++ source/common/tracing/zipkin/zipkin_core_constants.cc
@@ -1,32 +1,35 @@
 #include "common/tracing/zipkin/zipkin_core_constants.h"
 
-const std::string Zipkin::ZipkinCoreConstants::CLIENT_SEND = "cs";
-const std::string Zipkin::ZipkinCoreConstants::CLIENT_RECV = "cr";
-const std::string Zipkin::ZipkinCoreConstants::SERVER_SEND = "ss";
-const std::string Zipkin::ZipkinCoreConstants::SERVER_RECV = "sr";
-const std::string Zipkin::ZipkinCoreConstants::WIRE_SEND = "ws";
-const std::string Zipkin::ZipkinCoreConstants::WIRE_RECV = "wr";
-const std::string Zipkin::ZipkinCoreConstants::CLIENT_SEND_FRAGMENT = "csf";
-const std::string Zipkin::ZipkinCoreConstants::CLIENT_RECV_FRAGMENT = "crf";
-const std::string Zipkin::ZipkinCoreConstants::SERVER_SEND_FRAGMENT = "ssf";
-const std::string Zipkin::ZipkinCoreConstants::SERVER_RECV_FRAGMENT = "srf";
+namespace Zipkin {
 
-const std::string Zipkin::ZipkinCoreConstants::HTTP_HOST = "http.host";
-const std::string Zipkin::ZipkinCoreConstants::HTTP_METHOD = "http.method";
-const std::string Zipkin::ZipkinCoreConstants::HTTP_PATH = "http.path";
-const std::string Zipkin::ZipkinCoreConstants::HTTP_URL = "http.url";
-const std::string Zipkin::ZipkinCoreConstants::HTTP_STATUS_CODE = "http.status_code";
-const std::string Zipkin::ZipkinCoreConstants::HTTP_REQUEST_SIZE = "http.request.size";
-const std::string Zipkin::ZipkinCoreConstants::HTTP_RESPONSE_SIZE = "http.response.size";
+const std::string ZipkinCoreConstants::CLIENT_SEND = "cs";
+const std::string ZipkinCoreConstants::CLIENT_RECV = "cr";
+const std::string ZipkinCoreConstants::SERVER_SEND = "ss";
+const std::string ZipkinCoreConstants::SERVER_RECV = "sr";
+const std::string ZipkinCoreConstants::WIRE_SEND = "ws";
+const std::string ZipkinCoreConstants::WIRE_RECV = "wr";
+const std::string ZipkinCoreConstants::CLIENT_SEND_FRAGMENT = "csf";
+const std::string ZipkinCoreConstants::CLIENT_RECV_FRAGMENT = "crf";
+const std::string ZipkinCoreConstants::SERVER_SEND_FRAGMENT = "ssf";
+const std::string ZipkinCoreConstants::SERVER_RECV_FRAGMENT = "srf";
 
-const std::string Zipkin::ZipkinCoreConstants::LOCAL_COMPONENT = "lc";
-const std::string Zipkin::ZipkinCoreConstants::ERROR = "error";
-const std::string Zipkin::ZipkinCoreConstants::CLIENT_ADDR = "ca";
-const std::string Zipkin::ZipkinCoreConstants::SERVER_ADDR = "sa";
+const std::string ZipkinCoreConstants::HTTP_HOST = "http.host";
+const std::string ZipkinCoreConstants::HTTP_METHOD = "http.method";
+const std::string ZipkinCoreConstants::HTTP_PATH = "http.path";
+const std::string ZipkinCoreConstants::HTTP_URL = "http.url";
+const std::string ZipkinCoreConstants::HTTP_STATUS_CODE = "http.status_code";
+const std::string ZipkinCoreConstants::HTTP_REQUEST_SIZE = "http.request.size";
+const std::string ZipkinCoreConstants::HTTP_RESPONSE_SIZE = "http.response.size";
+
+const std::string ZipkinCoreConstants::LOCAL_COMPONENT = "lc";
+const std::string ZipkinCoreConstants::ERROR = "error";
+const std::string ZipkinCoreConstants::CLIENT_ADDR = "ca";
+const std::string ZipkinCoreConstants::SERVER_ADDR = "sa";
 
 // Zipkin B3 headers
-const std::string Zipkin::ZipkinCoreConstants::X_B3_TRACE_ID = "X-B3-TraceId";
-const std::string Zipkin::ZipkinCoreConstants::X_B3_SPAN_ID = "X-B3-SpanId";
-const std::string Zipkin::ZipkinCoreConstants::X_B3_PARENT_SPAN_ID = "X-B3-ParentSpanId";
-const std::string Zipkin::ZipkinCoreConstants::X_B3_SAMPLED = "X-B3-Sampled";
-const std::string Zipkin::ZipkinCoreConstants::X_B3_FLAGS = "X-B3-Flags";
+const std::string ZipkinCoreConstants::X_B3_TRACE_ID = "X-B3-TraceId";
+const std::string ZipkinCoreConstants::X_B3_SPAN_ID = "X-B3-SpanId";
+const std::string ZipkinCoreConstants::X_B3_PARENT_SPAN_ID = "X-B3-ParentSpanId";
+const std::string ZipkinCoreConstants::X_B3_SAMPLED = "X-B3-Sampled";
+const std::string ZipkinCoreConstants::X_B3_FLAGS = "X-B3-Flags";
+} // Zipkin
--- source/common/tracing/zipkin/zipkin_json_field_names.cc
+++ source/common/tracing/zipkin/zipkin_json_field_names.cc
@@ -1,23 +1,26 @@
 #include "common/tracing/zipkin/zipkin_json_field_names.h"
 
-const std::string Zipkin::ZipkinJsonFieldNames::SPAN_TRACE_ID = "traceId";
-const std::string Zipkin::ZipkinJsonFieldNames::SPAN_PARENT_ID = "parentId";
-const std::string Zipkin::ZipkinJsonFieldNames::SPAN_NAME = "name";
-const std::string Zipkin::ZipkinJsonFieldNames::SPAN_ID = "id";
-const std::string Zipkin::ZipkinJsonFieldNames::SPAN_TIMESTAMP = "timestamp";
-const std::string Zipkin::ZipkinJsonFieldNames::SPAN_DURATION = "duration";
-const std::string Zipkin::ZipkinJsonFieldNames::SPAN_ANNOTATIONS = "annotations";
-const std::string Zipkin::ZipkinJsonFieldNames::SPAN_BINARY_ANNOTATIONS = "binaryAnnotations";
+namespace Zipkin {
 
-const std::string Zipkin::ZipkinJsonFieldNames::ANNOTATION_ENDPOINT = "endpoint";
-const std::string Zipkin::ZipkinJsonFieldNames::ANNOTATION_TIMESTAMP = "timestamp";
-const std::string Zipkin::ZipkinJsonFieldNames::ANNOTATION_VALUE = "value";
+const std::string ZipkinJsonFieldNames::SPAN_TRACE_ID = "traceId";
+const std::string ZipkinJsonFieldNames::SPAN_PARENT_ID = "parentId";
+const std::string ZipkinJsonFieldNames::SPAN_NAME = "name";
+const std::string ZipkinJsonFieldNames::SPAN_ID = "id";
+const std::string ZipkinJsonFieldNames::SPAN_TIMESTAMP = "timestamp";
+const std::string ZipkinJsonFieldNames::SPAN_DURATION = "duration";
+const std::string ZipkinJsonFieldNames::SPAN_ANNOTATIONS = "annotations";
+const std::string ZipkinJsonFieldNames::SPAN_BINARY_ANNOTATIONS = "binaryAnnotations";
 
-const std::string Zipkin::ZipkinJsonFieldNames::BINARY_ANNOTATION_ENDPOINT = "endpoint";
-const std::string Zipkin::ZipkinJsonFieldNames::BINARY_ANNOTATION_KEY = "key";
-const std::string Zipkin::ZipkinJsonFieldNames::BINARY_ANNOTATION_VALUE = "value";
+const std::string ZipkinJsonFieldNames::ANNOTATION_ENDPOINT = "endpoint";
+const std::string ZipkinJsonFieldNames::ANNOTATION_TIMESTAMP = "timestamp";
+const std::string ZipkinJsonFieldNames::ANNOTATION_VALUE = "value";
 
-const std::string Zipkin::ZipkinJsonFieldNames::ENDPOINT_SERVICE_NAME = "serviceName";
-const std::string Zipkin::ZipkinJsonFieldNames::ENDPOINT_PORT = "port";
-const std::string Zipkin::ZipkinJsonFieldNames::ENDPOINT_IPV4 = "ipv4";
-const std::string Zipkin::ZipkinJsonFieldNames::ENDPOINT_IPV6 = "ipv6";
+const std::string ZipkinJsonFieldNames::BINARY_ANNOTATION_ENDPOINT = "endpoint";
+const std::string ZipkinJsonFieldNames::BINARY_ANNOTATION_KEY = "key";
+const std::string ZipkinJsonFieldNames::BINARY_ANNOTATION_VALUE = "value";
+
+const std::string ZipkinJsonFieldNames::ENDPOINT_SERVICE_NAME = "serviceName";
+const std::string ZipkinJsonFieldNames::ENDPOINT_PORT = "port";
+const std::string ZipkinJsonFieldNames::ENDPOINT_IPV4 = "ipv4";
+const std::string ZipkinJsonFieldNames::ENDPOINT_IPV6 = "ipv6";
+} // Zipkin
