d972917400cd9d0226678a022d820832e904c208
Harvey Tuch <htuch@google.com>
2017-04-13 10:39:14 -0400

    mattklein123@ review feedback and cleanup.

--- tools/gen_gdb_wrapper_script.py
+++ tools/gen_gdb_wrapper_script.py
@@ -5,35 +5,34 @@
 # in the bazel test environment. This is a workaround for the fact that --run_under does not attach
 # stdin.
 
-import base64
-import json
 import os
+import pipes
 import string
 import sys
 
 GDB_RUNNER_SCRIPT = string.Template("""#!/usr/bin/env python
 
-import base64
-import json
 import os
 
-env = json.loads(base64.b64decode('${b64env}'))
+env = ${b64env}
 for k, v in env.iteritems():
   os.environ[k] = v
 
 os.chdir('${working_dir}')
-os.system('gdb ${test_path}')
+os.system("${gdb} --args ${test_args}")
 """)
 
 if __name__ == '__main__':
-  generated_path = sys.argv[1]
-  test_path = sys.argv[2]
+  gdb = sys.argv[1]
+  generated_path = sys.argv[2]
+  test_args = sys.argv[3:]
   with open(generated_path, 'w') as f:
     f.write(
         GDB_RUNNER_SCRIPT.substitute(
-            b64env=base64.b64encode(json.dumps(dict(os.environ))),
+            b64env=str(dict(os.environ)),
             working_dir=os.getcwd(),
-            test_path=test_path))
+            gdb=gdb,
+            test_args=' '.join(pipes.quote(arg) for arg in test_args)))
   # To make bazel consider the test a failure we exit non-zero.
   print 'Test was not run, instead a gdb wrapper script was produced in %s' % generated_path
   sys.exit(1)
