91f1c03659becf71810c77ad35906d3cf2b8c75f
Harvey Tuch <htuch@google.com>
2017-04-18 06:40:56 -0400

    Hash CC/CXX/LD_LIBRARY_PATH into the cache path to capture dep.

--- bazel/repositories.sh
+++ bazel/repositories.sh
@@ -5,11 +5,14 @@ set -e
 # Tell build_and_install_deps.sh to build sequentially when performance debugging.
 # export BUILD_CONCURRENCY=0
 
+# Hash environment variables we care about to force rebuilds when they change.
+ENV_HASH=$(echo "${CC} ${CXX} ${LD_LIBRARY_PATH}" | md5sum - | cut -f 1 -d\ )
+
 # Don't build inside the directory Bazel believes the repository_rule output goes. Instead, do so in
 # a parallel directory. This allows the build artifacts to survive Bazel clobbering the repostory
 # directory when a small change to repositories.bzl or a build recipe happens. We then rely on make
 # dependency analysis to detect when stuff needs to be rebuilt.
-BASEDIR="${PWD}_cache"
+BASEDIR="${PWD}_cache_${ENV_HASH}"
 
 >&2 echo "External dependency cache directory ${BASEDIR}"
 mkdir -p  "${BASEDIR}"
