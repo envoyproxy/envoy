e656947acc1e714102b86368cf88342f8a88a816
Matt Klein <mklein@lyft.com>
2017-04-18 15:36:48 -0700

    bazel: allow coverage to be customized by consumer projects

--- test/run_envoy_bazel_coverage.sh
+++ test/run_envoy_bazel_coverage.sh
@@ -3,14 +3,15 @@
 set -e
 
 [[ -z "${SRCDIR}" ]] && SRCDIR="${PWD}"
-[[ -z "${REAL_SRCDIR}" ]] && REAL_SRCDIR="${SRCDIR}"
 [[ -z "${GCOVR_DIR}" ]] && GCOVR_DIR="${SRCDIR}/bazel-$(basename "${SRCDIR}")"
 [[ -z "${TESTLOGS_DIR}" ]] && TESTLOGS_DIR="${SRCDIR}/bazel-testlogs"
 [[ -z "${BAZEL_COVERAGE}" ]] && BAZEL_COVERAGE=bazel
 [[ -z "${GCOVR}" ]] && GCOVR=gcovr
+[[ -z "${COVERAGE_TARGET}" ]] && COVERAGE_TARGET="//test/coverage:coverage_tests"
 
 # Make sure //test/coverage is up-to-date.
-(BAZEL_BIN="${BAZEL_COVERAGE}" "${REAL_SRCDIR}"/test/coverage/gen_build.sh)
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+(BAZEL_BIN="${BAZEL_COVERAGE}" "${SCRIPT_DIR}"/coverage/gen_build.sh)
 
 echo "Cleaning .gcda/.gcov from previous coverage runs..."
 for f in $(find -L "${GCOVR_DIR}" -name "*.gcda" -o -name "*.gcov")
@@ -25,7 +26,7 @@ echo "Cleanup completed."
 # https://github.com/bazelbuild/bazel/issues/1118). This works today as we have
 # a single coverage test binary and do not require the "bazel coverage" support
 # for collecting multiple traces and glueing them together.
-"${BAZEL_COVERAGE}" test //test/coverage:coverage_tests ${BAZEL_BUILD_OPTIONS} \
+"${BAZEL_COVERAGE}" test "${COVERAGE_TARGET}" ${BAZEL_BUILD_OPTIONS} \
   --cache_test_results=no --cxxopt="--coverage" --linkopt="--coverage" \
   --test_output=all --strategy=Genrule=standalone --spawn_strategy=standalone
 
@@ -34,22 +35,23 @@ echo "Cleanup completed."
 # stats. The #foo# pattern is because gcov produces files such as
 # bazel-out#local-fastbuild#bin#external#spdlog_git#_virtual_includes#spdlog#spdlog#details#pattern_formatter_impl.h.gcov.
 # To find these while modifying this regex, perform a gcov run with -k set.
-GCOVR_EXCLUDE_REGEX=".*pb.h.gcov|.*#genfiles#.*|test#.*|external#.*|.*#external#.*|.*#prebuilt#.*"
+[[ -z "${GCOVR_EXCLUDE_REGEX}" ]] && GCOVR_EXCLUDE_REGEX=".*pb.h.gcov|.*#genfiles#.*|test#.*|external#.*|.*#external#.*|.*#prebuilt#.*"
+[[ -z "${GCOVR_EXCLUDE_DIR}" ]] && GCOVR_EXCLUDE_DIR=".*/external/.*"
 
 COVERAGE_DIR="${SRCDIR}"/generated/coverage
 mkdir -p "${COVERAGE_DIR}"
-COVERAGE_SUMMAY="${COVERAGE_DIR}/coverage_summary.txt"
+COVERAGE_SUMMARY="${COVERAGE_DIR}/coverage_summary.txt"
 
 # gcovr is extremely picky about where it is run and where the paths of the
 # original source are relative to its execution location.
 cd "${SRCDIR}"
 echo "Running gcovr..."
 time "${GCOVR}" --gcov-exclude="${GCOVR_EXCLUDE_REGEX}" \
-  --exclude-directories=".*/external/.*" --object-directory="${GCOVR_DIR}" -r "${SRCDIR}" \
+  --exclude-directories="${GCOVR_EXCLUDE_DIR}" --object-directory="${GCOVR_DIR}" -r "${SRCDIR}" \
   --html --html-details --exclude-unreachable-branches --print-summary \
-  -o "${COVERAGE_DIR}"/coverage.html > "${COVERAGE_SUMMAY}"
+  -o "${COVERAGE_DIR}"/coverage.html > "${COVERAGE_SUMMARY}"
 
-COVERAGE_VALUE=$(grep -Po 'lines: \K(\d|\.)*' "${COVERAGE_SUMMAY}")
+COVERAGE_VALUE=$(grep -Po 'lines: \K(\d|\.)*' "${COVERAGE_SUMMARY}")
 COVERAGE_THRESHOLD=98.0
 COVERAGE_FAILED=$(echo "${COVERAGE_VALUE}<${COVERAGE_THRESHOLD}" | bc)
 if test ${COVERAGE_FAILED} -eq 1; then
