ab3855f520ab57dee2cde60414c3fba9a084a632
Feng Li <fengli@google.com>
2017-04-25 15:09:18 -0700

    Local change for thirdparty.cmake, DO NOT commit to upstream!

--- thirdparty.cmake
+++ thirdparty.cmake
@@ -4,55 +4,55 @@
 
 # https://github.com/sakra/cotire
 # Last tested with 1.7.8
-set(ENVOY_COTIRE_MODULE_DIR "" CACHE FILEPATH "location of cotire cmake module")
+set(ENVOY_COTIRE_MODULE_DIR "/usr/local/google/home/fengli/fengli79/cotire/CMake" CACHE FILEPATH "location of cotire cmake module")
 
 # https://github.com/gabime/spdlog
 # Last tested with 0.11.0
-set(ENVOY_SPDLOG_INCLUDE_DIR "" CACHE FILEPATH "location of spdlog includes")
+set(ENVOY_SPDLOG_INCLUDE_DIR "/usr/local/google/home/fengli/fengli79/spdlog/include" CACHE FILEPATH "location of spdlog includes")
 
 # https://github.com/nodejs/http-parser
 # Last tested with 2.7.0
-set(ENVOY_HTTP_PARSER_INCLUDE_DIR "" CACHE FILEPATH "location of http-parser includes")
+set(ENVOY_HTTP_PARSER_INCLUDE_DIR "/usr/local/google/home/fengli/fengli79/http-parser" CACHE FILEPATH "location of http-parser includes")
 
 # https://github.com/nghttp2/nghttp2
 # Last tested with 1.20.0
-set(ENVOY_NGHTTP2_INCLUDE_DIR "" CACHE FILEPATH "location of nghttp2 includes")
+set(ENVOY_NGHTTP2_INCLUDE_DIR "/usr/local/google/home/fengli/fengli79/nghttp2/lib/includes" CACHE FILEPATH "location of nghttp2 includes")
 
 # http://libevent.org/
 # Last tested with 2.1.8
-set(ENVOY_LIBEVENT_INCLUDE_DIR "" CACHE FILEPATH "location of libevent includes")
+set(ENVOY_LIBEVENT_INCLUDE_DIR "/usr/local/google/home/fengli/fengli79/libevent/include" CACHE FILEPATH "location of libevent includes")
 
 # http://tclap.sourceforge.net/
 # Last tested with 1.2.1
-set(ENVOY_TCLAP_INCLUDE_DIR "" CACHE FILEPATH "location of tclap includes")
+set(ENVOY_TCLAP_INCLUDE_DIR "/usr/local/google/home/fengli/fengli79/tclap-code/include" CACHE FILEPATH "location of tclap includes")
 
 # https://github.com/gperftools/gperftools
 # Last tested with 2.5.0
-set(ENVOY_GPERFTOOLS_INCLUDE_DIR "" CACHE FILEPATH "location of gperftools includes")
+set(ENVOY_GPERFTOOLS_INCLUDE_DIR "/usr/local/google/home/fengli/fengli79/gperftools/src" CACHE FILEPATH "location of gperftools includes")
 
 # https://boringssl.googlesource.com/boringssl/+/chromium-stable
 # Last tested with sha be873e9f48b2a07269300282b69bb17d496c69ee
-set(ENVOY_OPENSSL_INCLUDE_DIR "" CACHE FILEPATH "location of openssl includes")
+set(ENVOY_OPENSSL_INCLUDE_DIR "/usr/local/google/home/fengli/fengli79/boringssl/include" CACHE FILEPATH "location of openssl includes")
 
 # https://github.com/c-ares/c-ares
 # Last tested with 1.12.0
-set(ENVOY_CARES_INCLUDE_DIR "" CACHE FILEPATH "location of c-ares includes")
+set(ENVOY_CARES_INCLUDE_DIR "/usr/local/google/home/fengli/fengli79/c-ares" CACHE FILEPATH "location of c-ares includes")
 
 # https://github.com/google/protobuf
 # Last tested with 3.0.0
-set(ENVOY_PROTOBUF_INCLUDE_DIR "" CACHE FILEPATH "location of protobuf includes")
-set(ENVOY_PROTOBUF_PROTOC "" CACHE FILEPATH "location of protoc")
+set(ENVOY_PROTOBUF_INCLUDE_DIR "/usr/local/google/home/fengli/fengli79/protobuf/src" CACHE FILEPATH "location of protobuf includes")
+set(ENVOY_PROTOBUF_PROTOC "/usr/local/google/home/fengli/fengli79/protobuf/src/protoc" CACHE FILEPATH "location of protoc")
 
 # http://lightstep.com/
 # Last tested with lightstep-tracer-cpp-0.36
-set(ENVOY_LIGHTSTEP_TRACER_INCLUDE_DIR "" CACHE FILEPATH "location of lighstep tracer includes")
+set(ENVOY_LIGHTSTEP_TRACER_INCLUDE_DIR "/usr/local/google/home/fengli/fengli79/lightstep-tracer-cpp/src/c++11" CACHE FILEPATH "location of lighstep tracer includes")
 
 # https://github.com/miloyip/rapidjson
 # Last tested with 1.1.0
-set(ENVOY_RAPIDJSON_INCLUDE_DIR "" CACHE FILEPATH "location of rapidjson includes")
+set(ENVOY_RAPIDJSON_INCLUDE_DIR "/usr/local/google/home/fengli/fengli79/rapidjson/include" CACHE FILEPATH "location of rapidjson includes")
 
 # Extra linker flags required to properly link envoy with all of the above libraries.
-set(ENVOY_EXE_EXTRA_LINKER_FLAGS "" CACHE STRING "envoy extra linker flags")
+set(ENVOY_EXE_EXTRA_LINKER_FLAGS "-L/usr/grte/v4/lib64 -L/usr/local/lib -L/usr/local/google/home/fengli/fengli79/gpertools -L/usr/local/google/home/fengli/fengli79/boringssl/build/ssl -L/usr/local/google/home/fengli/fengli79/boringssl/build/crypto -L/usr/local/google/home/fengli/fengli79/nghttp2/lib/.libs -L/usr/local/google/home/fengli/fengli79/lightstep-tracer-cpp/src/c++11/.libs" CACHE STRING "envoy extra linker flags")
 
 #
 # Test Requirements
@@ -60,12 +60,12 @@ set(ENVOY_EXE_EXTRA_LINKER_FLAGS "" CACHE STRING "envoy extra linker flags")
 
 # https://github.com/google/googletest
 # Last tested with 1.8.0
-set(ENVOY_GTEST_INCLUDE_DIR "" CACHE FILEPATH "location of gtest includes")
-set(ENVOY_GMOCK_INCLUDE_DIR "" CACHE FILEPATH "location of gmock includes")
+set(ENVOY_GTEST_INCLUDE_DIR "/usr/local/google/home/fengli/fengli79/googletest/googletest/include" CACHE FILEPATH "location of gtest includes")
+set(ENVOY_GMOCK_INCLUDE_DIR "/usr/local/google/home/fengli/fengli79/googletest/googlemock/include" CACHE FILEPATH "location of gmock includes")
 
 # http://gcovr.com/
 # Last tested with 3.3
-set(ENVOY_GCOVR "" CACHE FILEPATH "location of gcovr")
+set(ENVOY_GCOVR "/usr/local/google/home/fengli/fengli79/gcovr" CACHE FILEPATH "location of gcovr")
 set(ENVOY_GCOVR_EXTRA_ARGS "" CACHE STRING "extra arguments to pass to gcovr")
 
 # Extra linker flags required to properly link envoy-test with all of the above libraries.
