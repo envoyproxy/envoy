0e2951be41e90e8124317668b7cc847c07c42e6b
Harvey Tuch <htuch@google.com>
2017-04-27 11:37:51 -0400

    ci: fix some issues post #837.
    Alpine build was failing, fixed and changed to build even when not pushing to master to catch this
    next time.

--- ci/docker_push.sh
+++ ci/docker_push.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 set -e
-set -x
 
 # this is needed to verify the example images
 docker build -f ci/Dockerfile-envoy-image -t lyft/envoy:latest .
+# verify the Alpine build even when we're not pushing it
+make -C ci/build_alpine_container
 
 # push the envoy image on merge to master
 want_push='false'
@@ -21,7 +22,6 @@ then
     docker push lyft/envoy:latest
     docker tag lyft/envoy:latest lyft/envoy:$TRAVIS_COMMIT
     docker push lyft/envoy:$TRAVIS_COMMIT
-    make -C ci/build_alpine_container
     docker tag lyft/envoy-alpine:latest lyft/envoy-alpine:$TRAVIS_COMMIT
     docker push lyft/envoy-alpine:$TRAVIS_COMMIT
     docker push lyft/envoy-alpine:latest
