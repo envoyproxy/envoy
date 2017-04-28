ac817d5c92ed794c5cca5505bd69b8149f86c2a3
htuch <htuch@users.noreply.github.com>
2017-04-28 15:45:15 -0400

    build/test: some fixups for the google import. (#859)
    * Fix TestEnvironment::runfilesDirectory() to use TEST_RUNDIR, rather
      than the other way around.
    
    * Remove use of diff -Z in hotrestart_test, since it's not needed and
      isn't supported by all versions of diff.
    
    * Add license directives to all files.
    
    * Check/fix automation for BUILD license files.

--- test/main.cc
+++ test/main.cc
@@ -4,6 +4,8 @@
 // The main entry point (and the rest of this file) should have no logic in it,
 // this allows overriding by site specific versions of main.cc.
 int main(int argc, char** argv) {
-  ::setenv("TEST_RUNDIR", TestEnvironment::runfilesDirectory().c_str(), 1);
+  ::setenv("TEST_RUNDIR", (TestEnvironment::getCheckedEnvVar("TEST_SRCDIR") + "/" +
+                           TestEnvironment::getCheckedEnvVar("TEST_WORKSPACE")).c_str(),
+           1);
   return TestRunner::RunTests(argc, argv);
 }
