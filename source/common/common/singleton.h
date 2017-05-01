dd16fd9800d5856bcaf64ad57ce1d3bddf597586
htuch <htuch@users.noreply.github.com>
2017-05-01 16:01:43 -0400

    style: introduce singleton template class. (#869)
    This allows declaring a Singleton type while avoiding the static
    initialization order fiasco.

---
+++ source/common/common/singleton.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/**
+ * Immutable singleton pattern.
+ */
+template <class T> class ConstSingleton {
+public:
+  /**
+   * Obtain an instance of the singleton for class T.
+   * @return const T& a reference to the singleton for class T.
+   */
+  static const T& get() {
+    static T* instance = new T();
+    return *instance;
+  }
+};
+
+// TODO(htuch): Add a mutable Singleton if there is a need for this.
