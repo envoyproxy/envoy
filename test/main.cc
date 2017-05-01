3704b888e81b589ecd13743a75a5478213af6ee8
hennna <hennna@users.noreply.github.com>
2017-05-01 17:01:03 -0400

    Begin to support Ipv6 only or Ipv4 only testing (#861)
    This PR allows testing in environments that either don't support IPv4 or IPv6 networking. The way this is achieved is by setting an environment variable (ENVOY_IP_TEST_VERSIONS) in test/main.cc or externally to "v4only" or "v6only". We then construct parameterized gtests based on the value of ENVOY_IP_TEST_VERSIONS.
    
    This PR only modifies existing tests that were testing with both IPv4 and IPv6 addresses. If the approach taken in this PR is agreed upon, testing with IPv4 only and IPv6 only will be extended in subsequent PRs.
    
    This PR is related to #351

--- test/main.cc
+++ test/main.cc
@@ -7,5 +7,13 @@ int main(int argc, char** argv) {
   ::setenv("TEST_RUNDIR", (TestEnvironment::getCheckedEnvVar("TEST_SRCDIR") + "/" +
                            TestEnvironment::getCheckedEnvVar("TEST_WORKSPACE")).c_str(),
            1);
+  // Select whether to test only for IPv4, IPv6, or both. The default is to
+  // test for both. Options are {"v4only", "v6only", "all"}. Set
+  // ENVOY_IP_TEST_VERSIONS to "v4only" if the system currently does not support IPv6 network
+  // operations. Similary set ENVOY_IP_TEST_VERSIONS to "v6only" if IPv4 has already been
+  // phased out of network operations. Set to "all" (or don't set) if testing both
+  // v4 and v6 addresses is desired. This feature is in progress and will be rolled out to all tests
+  // in upcoming PRs.
+  ::setenv("ENVOY_IP_TEST_VERSIONS", "all", 0);
   return TestRunner::RunTests(argc, argv);
 }
