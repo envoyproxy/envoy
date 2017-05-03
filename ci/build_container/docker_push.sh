d7053f5ea5c502b4768217e8b437c6007b841413
Jose Nino <jnino@lyft.com>
2017-05-03 10:42:20 -0700

    last test

--- ci/build_container/docker_push.sh
+++ ci/build_container/docker_push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Do not ever set -x here, it is a security hazard as it will place the credentials below in the
+# Travis logs.
 set -e
 
 # push the envoy image on merge to master
@@ -17,7 +19,12 @@ then
     if [[ $(git diff HEAD^ ci/build_container/) ]]; then
         echo "There are changes in the ci/build_container directory"
         echo "Updating lyft/envoy-build image"
-        ./ci/build_container/update_build_container.sh
+        cd ci/build_container
+        ./docker_build.sh
+        docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+        # docker push lyft/envoy-build:$TRAVIS_COMMIT
+        docker tag lyft/envoy-build:$TRAVIS_COMMIT lyft/envoy-build-test:latest
+        docker push lyft/envoy-build-test:latest
     else
         echo "The ci/build_container directory has not changed"
     fi
