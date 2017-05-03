598a76caff7f27ab568f8272d0cdfc808f5880d5
Piotr Sikora <piotrsikora@google.com>
2017-05-03 15:38:35 -0700

    ci: preserve backward compatibility.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- ci/build_container/build_recipes/rapidjson.sh
+++ ci/build_container/build_recipes/rapidjson.sh
@@ -6,4 +6,5 @@ VERSION=1.1.0
 
 wget -O rapidjson-$VERSION.tar.gz https://github.com/miloyip/rapidjson/archive/v$VERSION.tar.gz
 tar xf rapidjson-$VERSION.tar.gz
+rsync -av rapidjson-$VERSION $THIRDPARTY_SRC
 rsync -av rapidjson-$VERSION/* $THIRDPARTY_SRC/rapidjson
