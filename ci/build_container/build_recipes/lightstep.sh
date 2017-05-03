9c6c34eb0f2180d0cb3070723228492705e6f369
Piotr Sikora <piotrsikora@google.com>
2017-05-03 12:38:05 -0700

    ci: cleanup build recipes. (#885)
    * ci: cleanup build recipes.
    
    The original intent of this change was to provide VERSION and COMMIT
    variables to avoid updating version strings in multiple places for a
    trivial update.
    
    However, while there, I made a few extra changes:
    
    - made scripts executable and added bash shebang,
    - changed Backward library to build from tarball,
    - forced wget to overwrite previously downloaded files,
    
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- ci/build_container/build_recipes/lightstep.sh
+++ ci/build_container/build_recipes/lightstep.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 set -e
 
-wget https://github.com/lightstep/lightstep-tracer-cpp/releases/download/v0_36/lightstep-tracer-cpp-0.36.tar.gz
-tar xf lightstep-tracer-cpp-0.36.tar.gz
-cd lightstep-tracer-cpp-0.36
+VERSION=0.36
+
+wget -O lightstep-tracer-cpp-$VERSION.tar.gz https://github.com/lightstep/lightstep-tracer-cpp/releases/download/v${VERSION//./_}/lightstep-tracer-cpp-$VERSION.tar.gz
+tar xf lightstep-tracer-cpp-$VERSION.tar.gz
+cd lightstep-tracer-cpp-$VERSION
 # Added for legacy compatibility, should not be needed in new build recipes.
 [ -z "$PROTOBUF_BUILD" ] && PROTOBUF_BUILD="$THIRDPARTY_BUILD"
 ./configure --disable-grpc --prefix=$THIRDPARTY_BUILD --enable-shared=no \
