8e31280492ccea6e940b54af7eef8c59e19e72cf
Piotr Sikora <piotrsikora@google.com>
2017-05-03 01:29:02 -0700

    ci: use version-less directories in /thirdparty.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- ci/build_container/build_recipes/gcovr.sh
+++ ci/build_container/build_recipes/gcovr.sh
@@ -6,5 +6,5 @@ VERSION=3.3
 
 wget -O gcovr-$VERSION.tar.gz https://github.com/gcovr/gcovr/archive/$VERSION.tar.gz
 tar xf gcovr-$VERSION.tar.gz
-rsync -av gcovr-$VERSION $THIRDPARTY_SRC
+rsync -av gcovr-$VERSION/* $THIRDPARTY_SRC/gcovr
 rm -rf gcovr-$VERSION*
--- ci/build_container/build_recipes/tclap.sh
+++ ci/build_container/build_recipes/tclap.sh
@@ -6,5 +6,5 @@ VERSION=1.2.1
 
 wget -O tclap-$VERSION.tar.gz https://sourceforge.net/projects/tclap/files/tclap-$VERSION.tar.gz/download
 tar xf tclap-$VERSION.tar.gz
-rsync -av tclap-$VERSION $THIRDPARTY_SRC
+rsync -av tclap-$VERSION/* $THIRDPARTY_SRC/tclap
 rm -rf tclap-$VERSION*
