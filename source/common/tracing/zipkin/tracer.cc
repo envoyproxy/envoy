ad84cc2b9bb78127dcdd4701048e828fc78574ba
FABIO A. OLIVEIRA <fabolive@us.ibm.com>
2017-05-04 12:29:10 -0400

    Merged master (#430)

---
+++ source/common/tracing/zipkin/tracer.cc
@@ -0,0 +1,122 @@
+#include "common/tracing/zipkin/tracer.h"
+
+#include <chrono>
+
+#include "common/common/utility.h"
+#include "common/tracing/zipkin/util.h"
+#include "common/tracing/zipkin/zipkin_core_constants.h"
+
+namespace Zipkin {
+
+SpanPtr Tracer::startSpan(const std::string& span_name, SystemTime timestamp) {
+  // Build the endpoint
+  Endpoint ep(service_name_, address_);
+
+  // Build the CS annotation
+  Annotation cs;
+  cs.setEndpoint(std::move(ep));
+  cs.setValue(ZipkinCoreConstants::get().CLIENT_SEND);
+
+  // Create an all-new span, with no parent id
+  SpanPtr span_ptr(new Span());
+  span_ptr->setName(span_name);
+  uint64_t random_number = generateRandomNumber();
+  span_ptr->setId(random_number);
+  span_ptr->setTraceId(random_number);
+  int64_t start_time_micro =
+      std::chrono::duration_cast<std::chrono::microseconds>(
+          ProdMonotonicTimeSource::instance_.currentTime().time_since_epoch()).count();
+  span_ptr->setStartTime(start_time_micro);
+
+  // Set the timestamp globally for the span and also for the CS annotation
+  uint64_t timestamp_micro =
+      std::chrono::duration_cast<std::chrono::microseconds>(timestamp.time_since_epoch()).count();
+  cs.setTimestamp(timestamp_micro);
+  span_ptr->setTimestamp(timestamp_micro);
+
+  // Add CS annotation to the span
+  span_ptr->addAnnotation(std::move(cs));
+
+  span_ptr->setTracer(this);
+
+  return span_ptr;
+}
+
+SpanPtr Tracer::startSpan(const std::string& span_name, SystemTime timestamp,
+                          SpanContext& previous_context) {
+  SpanPtr span_ptr(new Span());
+  Annotation annotation;
+  uint64_t timestamp_micro;
+
+  timestamp_micro =
+      std::chrono::duration_cast<std::chrono::microseconds>(timestamp.time_since_epoch()).count();
+
+  if (previous_context.annotationSet().sr_ && !previous_context.annotationSet().cs_) {
+    // We need to create a new span that is a child of the previous span; no shared context
+
+    // Create a new span id
+    uint64_t random_number = generateRandomNumber();
+    span_ptr->setId(random_number);
+
+    span_ptr->setName(span_name);
+
+    // Set the parent id to the id of the previous span
+    span_ptr->setParentId(previous_context.id());
+
+    // Set the CS annotation value
+    annotation.setValue(ZipkinCoreConstants::get().CLIENT_SEND);
+
+    // Set the timestamp globally for the span
+    span_ptr->setTimestamp(timestamp_micro);
+  } else if (previous_context.annotationSet().cs_ && !previous_context.annotationSet().sr_) {
+    // We need to create a new span that will share context with the previous span
+
+    // Initialize the shared context for the new span
+    span_ptr->setId(previous_context.id());
+    if (previous_context.parent_id()) {
+      span_ptr->setParentId(previous_context.parent_id());
+    }
+
+    // Set the SR annotation value
+    annotation.setValue(ZipkinCoreConstants::get().SERVER_RECV);
+  } else {
+    return span_ptr; // return an empty span
+  }
+
+  // Build the endpoint
+  Endpoint ep(service_name_, address_);
+
+  // Add the newly-created annotation to the span
+  annotation.setEndpoint(std::move(ep));
+  annotation.setTimestamp(timestamp_micro);
+  span_ptr->addAnnotation(std::move(annotation));
+
+  // Keep the same trace id
+  span_ptr->setTraceId(previous_context.trace_id());
+
+  int64_t start_time_micro =
+      std::chrono::duration_cast<std::chrono::microseconds>(
+          ProdMonotonicTimeSource::instance_.currentTime().time_since_epoch()).count();
+  span_ptr->setStartTime(start_time_micro);
+
+  span_ptr->setTracer(this);
+
+  return span_ptr;
+}
+
+void Tracer::reportSpan(Span&& span) {
+  if (reporter_) {
+    reporter_->reportSpan(std::move(span));
+  }
+}
+
+void Tracer::setReporter(ReporterPtr reporter) { reporter_ = std::move(reporter); }
+
+void Tracer::setRandomGenerator(Runtime::RandomGeneratorPtr random_generator) {
+  random_generator_ = std::move(random_generator);
+}
+
+uint64_t Tracer::generateRandomNumber() {
+  return random_generator_ ? random_generator_->random() : Util::generateRandom64();
+}
+} // Zipkin
---
+++ source/common/tracing/zipkin/tracer.h
@@ -0,0 +1,123 @@
+#pragma once
+
+#include "envoy/common/pure.h"
+#include "envoy/common/time.h"
+#include "envoy/runtime/runtime.h"
+
+#include "common/tracing/zipkin/span_context.h"
+#include "common/tracing/zipkin/tracer_interface.h"
+#include "common/tracing/zipkin/zipkin_core_types.h"
+
+namespace Zipkin {
+
+/**
+ * Abstract class that delegates to users of the Tracer class the responsibility
+ * of "reporting" a Zipkin span that has ended its life cycle. "Reporting" can mean that the
+ * span will be sent to out to Zipkin, or buffered so that it can be sent out later.
+ */
+class Reporter {
+public:
+  /**
+   * Destructor.
+   */
+  virtual ~Reporter() {}
+
+  /**
+   * Method that a concrete Reporter class must implement to handle finished spans.
+   * For example, a span-buffer management policy could be implemented.
+   *
+   * @param span The span that needs action.
+   */
+  virtual void reportSpan(Span&& span) PURE;
+};
+
+typedef std::unique_ptr<Reporter> ReporterPtr;
+
+/**
+ * This class implements the Zipkin tracer. It has methods to create the appropriate Zipkin span
+ * type, i.e., root span, child span, or shared-context span.
+ *
+ * This class allows its users to supply a concrete Reporter class whose reportSpan method
+ * is called by its own reportSpan method. By doing so, we have cleanly separated the logic
+ * of dealing with finished spans from the span-creation and tracing logic.
+ */
+class Tracer : public TracerInterface {
+public:
+  /**
+   * Move constructor
+   */
+  Tracer(Tracer&& tracer)
+      : service_name_(tracer.serviceName()), address_(tracer.address()),
+        reporter_(std::move(tracer.reporter())),
+        random_generator_(std::move(tracer.randomGenerator())) {}
+
+  /**
+   * Constructor.
+   *
+   * @param service_name The name of the service where the Tracer is running. This name is
+   * used in all annotations' endpoints of the spans created by the Tracer.
+   * @param address Pointer to a network-address object. The IP address and port are used
+   * in all annotations' endpoints of the spans created by the Tracer.
+   */
+  Tracer(const std::string& service_name, Network::Address::InstanceConstSharedPtr address)
+      : service_name_(service_name), address_(address), random_generator_(nullptr) {}
+
+  /**
+   * Creates a "root" Zipkin span.
+   *
+   * @param span_name Name of the new span.
+   * @param start_time The time indicating the beginning of the span.
+   */
+  SpanPtr startSpan(const std::string& span_name, SystemTime timestamp);
+
+  /**
+   * Depending on the given context, creates either a "child" or a "shared-context" Zipkin span.
+   *
+   * @param span_name Name of the new span.
+   * @param start_time The time indicating the beginning of the span.
+   * @param previous_context The context of the span preceding the one to be created.
+   */
+  SpanPtr startSpan(const std::string& span_name, SystemTime timestamp,
+                    SpanContext& previous_context);
+
+  /**
+   * TracerInterface::reportSpan.
+   */
+  void reportSpan(Span&& span) override;
+
+  const std::string& serviceName() const { return service_name_; }
+
+  const Network::Address::InstanceConstSharedPtr address() const { return address_; }
+
+  /**
+   * Associates a Reporter object with this Tracer.
+   */
+  void setReporter(ReporterPtr reporter);
+
+  ReporterPtr reporter() { return std::move(reporter_); }
+
+  /**
+   * Provides a random-number generator to be used by the Tracer.
+   * If this method is not used, the Tracer will use a default random-number generator.
+   *
+   * @param random_generator Random-number generator to be used.
+   */
+  void setRandomGenerator(Runtime::RandomGeneratorPtr random_generator);
+
+  Runtime::RandomGeneratorPtr randomGenerator() { return std::move(random_generator_); }
+
+private:
+  /**
+   * Uses the default random-number generator if one was not provided by the user
+   */
+  uint64_t generateRandomNumber();
+
+  const std::string service_name_;
+  Network::Address::InstanceConstSharedPtr address_;
+  ReporterPtr reporter_;
+  Runtime::RandomGeneratorPtr random_generator_;
+};
+
+typedef std::unique_ptr<Tracer> TracerPtr;
+
+} // Zipkin
