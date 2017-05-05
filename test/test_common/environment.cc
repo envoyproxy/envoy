fc45dde3c14f10844a2f8b726574af5b1ba305e2
Dan Noé <dpn@google.com>
2017-05-05 11:00:27 -0400

    Fail nice and hard if old GCC encountered.
    If GCC prior to 4.9 was used to compile the result was a mysterious
    runtime failure of 14 different tests that rely on the use of
    std::regex but are otherwise unremarkable.  This is because GCC 4.8 and
    below have a silently incomplete and buggy std::regex implementation. It
    compiles, but it doesn't actually do the right thing at runtime (!!).
    
    To prevent a repeat of the confusion this caused #error out if GCC that
    doesn't support regex properly is used to compile
    test/test_common/environment.cc.

--- test/test_common/environment.cc
+++ test/test_common/environment.cc
@@ -16,6 +16,14 @@
 
 #include "spdlog/spdlog.h"
 
+#include <regex>
+#if __cplusplus < 201103L ||                               \
+    (defined(__GLIBCXX__) && (__cplusplus < 201402L) &&    \
+        (!defined(_GLIBCXX_REGEX_DFS_QUANTIFIERS_LIMIT) && \
+         !defined(_GLIBCXX_REGEX_STATE_LIMIT)))
+#error "Your compiler does not support std::regex properly.  GCC 4.9+ or Clang required."
+#endif
+
 namespace {
 
 std::string getOrCreateUnixDomainSocketDirectory() {
