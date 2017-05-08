de91b1677fb66bbe9a06be864310f1e2093dc8cc
Dan Noé <dpn@google.com>
2017-05-08 11:16:28 -0400

    Fail nice and hard if old GCC encountered. (#901)
    If GCC prior to 4.9 was used to compile the result was a mysterious
    runtime failure of 14 different tests that rely on the use of
    std::regex but are otherwise unremarkable.  This is because GCC 4.8 and
    below have a silently incomplete and buggy std::regex implementation. It
    compiles, but it doesn't actually do the right thing at runtime (!!).
    
    To prevent a repeat of the confusion this caused #error out if GCC that
    doesn't support regex properly is used to compile
    test/test_common/environment.cc.

---
+++ source/common/common/compiler_requirements.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <regex>
+
+#if __cplusplus < 201103L ||                                                                       \
+    (defined(__GLIBCXX__) && (__cplusplus < 201402L) &&                                            \
+     (!defined(_GLIBCXX_REGEX_DFS_QUANTIFIERS_LIMIT) && !defined(_GLIBCXX_REGEX_STATE_LIMIT)))
+#error "Your compiler does not support std::regex properly.  GCC 4.9+ or Clang required."
+#endif
