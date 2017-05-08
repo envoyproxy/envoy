d22a3df5bb927b1b62114b60b370a7621dcd447e
Daniel Hochman <dhochman@lyft.com>
2017-05-08 11:30:35 -0700

    publish coverage

--- ci/coverage_publish.sh
+++ ci/coverage_publish.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-echo "Coverage"
-echo $(pwd)
+set -e
 
-echo "NOTE: envoy build dir $ENVOY_BUILD_DIR"
-ls -laR $ENVOY_BUILD_DIR
+if [ "${TRAVIS}" != "true" ]; then 
+  exit 0
+fi
 
-echo "NOTE: envoy build dir $TRAVIS_BUILD_DIR"
-ls -laR $TRAVIS_BUILD_DIR
+# available for master builds and PRs from originating repository (not forks)
+if [ "${TRAVIS_SECURE_ENV_VARS}" == "true" ]; then
+
+  mkdir -p ~/.aws
+  echo -e "[coverage]\naws_access_key_id=${COVERAGE_AWS_ACCESS_KEY_ID}\naws_secret_access_key=${COVERAGE_AWS_SECRET_ACCESS_KEY}\n" >> ~/.aws/credentials
+
+  COVERAGE_DIR="$(readlink -f ${ENVOY_BUILD_DIR}/bazel-envoy)/../envoy/generated/coverage"
+  VENV_DIR="${TRAVIS_BUILD_DIR}/coverage_venv"
+  S3_LOCATION="lyft-envoy/coverage/report-${TRAVIS_BRANCH}"
+
+  if [ ! -d "${VENV_DIR}" ]; then
+    virtualenv "${VENV_DIR}" --no-site-packages
+    "${VENV_DIR}/bin/pip" install awscli==1.11.83
+  fi
+
+  "${VENV_DIR}/bin/aws" s3 cp "${COVERAGE_DIR}" "s3://${S3_LOCATION}" --recursive --profile coverage --acl public-read --quiet
+  echo "Coverage report for this branch is available for viewing at https://s3.amazonaws.com/${S3_LOCATION}/coverage.html."
+else
+  echo "Coverage report will not be uploaded for this build."
+fi
