46e9fd6cd95364129d09a16529db777b2672a8ac
Piotr Sikora <piotrsikora@google.com>
2017-05-09 14:45:29 -0700

    tools: enforce presence of envoy_package().
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- tools/envoy_build_fixer.py
+++ tools/envoy_build_fixer.py
@@ -5,15 +5,45 @@
 import sys
 
 LICENSE_STRING = 'licenses(["notice"])  # Apache 2\n'
+ENVOY_PACKAGE_STRING = (
+    'load("//bazel:envoy_build_system.bzl", "envoy_package")\n'
+    '\n'
+    'envoy_package()\n')
+
 
 def FixBuild(path):
   with open(path, 'r') as f:
     outlines = [LICENSE_STRING]
+    load = 0
+    seen_ebs = 0
+    seen_epkg = 0
     for line in f:
+      if line.startswith('package(') and not path.endswith(
+          'bazel/BUILD') and not path.endswith('ci/prebuilt/BUILD'):
+        continue
+      if load == 0 and line.startswith('load('):
+        load = 1
+      if load == 1:
+        if 'envoy_build_system.bzl' in line:
+          seen_ebs = 1
+        if 'envoy_package' in line:
+          seen_epkg = 1
+        if line.strip().endswith(')'):
+          load = 2
+          if seen_ebs:
+            if not seen_epkg:
+              outlines.append(line.strip()[:-1] + ', "envoy_package")\n')
+              outlines.append('\nenvoy_package()\n')
+              continue
+          else:
+            outlines.append(line)
+            outlines.append(ENVOY_PACKAGE_STRING)
+            continue
       if not line.startswith('licenses'):
         outlines.append(line)
 
   return ''.join(outlines)
+
 
 if __name__ == '__main__':
   if len(sys.argv) == 2:
