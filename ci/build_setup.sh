ad4fe0a22ff4fc9278ddf7a98b3906b0592d24e5
hennna <hennna@users.noreply.github.com>
2017-05-10 12:02:12 -0400

    test/network: turn dual bind off and parameterize ipv6 tests (#934)
    This PR turns off dual bind for IPv6 sockets. Turning dual bind off causes some test/common/network/.. tests to fail in Ipv4 only enviroments. In addition, a segfault path in address_impl_test.cc is exercised. This PR addresses those issues.
    
    In Ipv4 only environments, users need to set the environment variable ENVOY_IP_TEST_VERSIONS to "v4only" to omit tests that bind or connect using Ipv6 addresses. This can be done:
    
    1. bazel test --test_env=ENVOY_IP_TEST_VERSIONS=v4only ...
    2. In main/test.cc https://github.com/lyft/envoy/blob/master/test/main.cc#L31

--- ci/build_setup.sh
+++ ci/build_setup.sh
@@ -81,7 +81,6 @@ fi
 cp -f "${ENVOY_SRCDIR}"/ci/WORKSPACE.filter.example "${ENVOY_FILTER_EXAMPLE_SRCDIR}"/WORKSPACE
 
 # This is the hash on https://github.com/lyft/envoy-filter-example.git we pin to.
-# TODO(hennna): Point to updated hash.
 (cd "${ENVOY_FILTER_EXAMPLE_SRCDIR}" && git checkout 9f006f6be519007e9be3b29ad531b8e8be5a18d6)
 
 # Also setup some space for building Envoy standalone.
