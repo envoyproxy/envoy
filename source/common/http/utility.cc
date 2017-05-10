3d818d753767ecbf0acdde9f8f007afa748c9f3a
Ryan McGowan <rmcgowan@lyft.com>
2017-05-10 13:37:56 -0700

    maybe fix style again

--- source/common/http/utility.cc
+++ source/common/http/utility.cc
@@ -80,7 +80,8 @@ std::string Utility::parseCookieValue(const HeaderMap& headers, const std::strin
       for (const std::string& s : StringUtil::split(std::string{header.value().c_str()}, ';')) {
         size_t first_non_space = s.find_first_not_of(" ");
         size_t equals_index = s.find('=');
-        if (first_non_space == std::string::npos || equals_index == std::string::npos || equals_index < first_non_space) {
+        if (first_non_space == std::string::npos || equals_index == std::string::npos ||
+            equals_index < first_non_space) {
             break;
         }
         std::string k = s.substr(first_non_space, equals_index - first_non_space);
