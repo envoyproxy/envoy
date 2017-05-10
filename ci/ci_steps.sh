a4e27ed0579ebaaf475e302846efeba9793e0245
Daniel Hochman <danielhochman@users.noreply.github.com>
2017-05-10 14:10:55 -0700

    build: publish coverage reports to S3 (#906)

--- ci/ci_steps.sh
+++ ci/ci_steps.sh
@@ -26,14 +26,18 @@ then
 else
   docker run -t -i -v "$ENVOY_BUILD_DIR":/build -v $TRAVIS_BUILD_DIR:/source \
     lyft/envoy-build:$ENVOY_BUILD_SHA /bin/bash -c "cd /source && ci/do_ci.sh $TEST_TYPE"
-fi
 
-if [ "$TEST_TYPE" == "bazel.release" ]
-then
-  mkdir -p build_release
-  cp -f "$ENVOY_BUILD_DIR"/envoy/source/exe/envoy ./build_release
-  # This script builds a lyft/envoy image and pushes that image on merge to master.
-  ./ci/docker_push.sh
-  # This script runs on every PRs release run to test the docker examples.
-  ./ci/verify_examples.sh
+  if [ "$TEST_TYPE" == "bazel.coverage" ]
+  then
+    ./ci/coverage_publish.sh
+  elif [ "$TEST_TYPE" == "bazel.release" ]
+  then
+    mkdir -p build_release
+    cp -f "$ENVOY_BUILD_DIR"/envoy/source/exe/envoy ./build_release
+    # This script builds a lyft/envoy image and pushes that image on merge to master.
+    ./ci/docker_push.sh
+    # This script runs on every PRs release run to test the docker examples.
+    ./ci/verify_examples.sh
+  fi
+
 fi
