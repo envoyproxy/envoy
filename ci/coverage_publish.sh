001ac5e92fea5a8c83692898aead80b44ce06d00
Daniel Hochman <dhochman@lyft.com>
2017-05-10 10:40:19 -0700

    use new symlink

--- ci/coverage_publish.sh
+++ ci/coverage_publish.sh
@@ -10,19 +10,14 @@ fi
 
 # available for master builds and PRs from originating repository (not forks)
 if [ "${TRAVIS_SECURE_ENV_VARS}" == "true" ]; then
-  mkdir -p ~/.aws
-  echo "[profile coverage]" >> ~/.aws/config
-  echo "aws_access_key_id=${COVERAGE_AWS_ACCESS_KEY_ID}" >> ~/.aws/config
-  echo "aws_secret_access_key=${COVERAGE_AWS_SECRET_ACCESS_KEY}" >> ~/.aws/config
-  echo "region=us-east-1" >> ~/.aws/config
+  echo "Uploading coverage report..."
+  
+  COVERAGE_FILE="${ENVOY_BUILD_DIR}/envoy/generated/coverage/coverage.html"
 
-  COVERAGE_FILE=$(find -L ${ENVOY_BUILD_DIR} -name "coverage\.html" 2>&1 | grep "coverage\.html" | tail -n1)
-  if [ -z "${COVERAGE_FILE}" ]; then
+  if [ ! -f "${COVERAGE_FILE}" ]; then
     echo "ERROR: Coverage file not found."
     exit 1
   fi
-
-  COVERAGE_DIR="$(dirname "${COVERAGE_FILE}")"
 
   if [ -n "${TRAVIS_PULL_REQUEST_BRANCH}" ]; then
     BRANCH_NAME="${TRAVIS_PULL_REQUEST_BRANCH}"
@@ -30,13 +25,18 @@ if [ "${TRAVIS_SECURE_ENV_VARS}" == "true" ]; then
     BRANCH_NAME="${TRAVIS_BRANCH}"
   fi
 
+  COVERAGE_DIR="$(dirname "${COVERAGE_FILE}")"
   S3_LOCATION="lyft-envoy/coverage/report-${BRANCH_NAME}"
+  
+  mkdir -p ~/.aws
+  echo "[profile coverage]" >> ~/.aws/config
+  echo "aws_access_key_id=${COVERAGE_AWS_ACCESS_KEY_ID}" >> ~/.aws/config
+  echo "aws_secret_access_key=${COVERAGE_AWS_SECRET_ACCESS_KEY}" >> ~/.aws/config
+  echo "region=us-east-1" >> ~/.aws/config
 
-  echo "Uploading coverage report..."
   aws s3 cp "${COVERAGE_DIR}" "s3://${S3_LOCATION}" --recursive --profile coverage --acl public-read --quiet
   echo "Coverage report for branch '${BRANCH_NAME}': https://s3.amazonaws.com/${S3_LOCATION}/coverage.html"
 
-  find -L ${ENVOY_BUILD_DIR} -name coverage.html
 else
   echo "Coverage report will not be uploaded for this build."
 fi
