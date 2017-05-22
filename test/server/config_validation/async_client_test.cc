47670a20c9c4d8022c23d999338bb0323f0c4f36
Reuven Lazarus <rzl@google.com>
2017-05-22 15:11:04 -0400

    Add tests for ValidationAsyncClient and ValidationClusterManager.
    These don't do anything except test that the mocked out methods are
    mocked out. They're purely to keep coverage from dropping.

---
+++ test/server/config_validation/async_client_test.cc
@@ -0,0 +1,24 @@
+#include "server/config_validation/async_client.h"
+
+#include "common/http/message_impl.h"
+
+#include "envoy/http/message.h"
+
+#include "test/mocks/http/mocks.h"
+#include "test/mocks/upstream/mocks.h"
+
+namespace Envoy {
+namespace Http {
+
+TEST(ValidationAsyncClientTest, MockedMethods) {
+  MessagePtr message{new RequestMessageImpl()};
+  MockAsyncClientCallbacks callbacks;
+  MockAsyncClientStreamCallbacks stream_callbacks;
+
+  ValidationAsyncClient client;
+  EXPECT_EQ(nullptr, client.send(std::move(message), callbacks, Optional<std::chrono::milliseconds>()));
+  EXPECT_EQ(nullptr, client.start(stream_callbacks, Optional<std::chrono::milliseconds>()));
+}
+
+} // Http
+} // Envoy
