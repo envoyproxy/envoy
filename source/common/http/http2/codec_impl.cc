36a917042fb60c2ca2f77f4cda638f4e19fffd1b
方俊武 <fangjunwu@xunlei.com>
2017-05-23 21:06:06 +0800

    add asserts for Http2Settings fields

--- source/common/http/http2/codec_impl.cc
+++ source/common/http/http2/codec_impl.cc
@@ -507,6 +507,13 @@ void ConnectionImpl::sendPendingFrames() {
 }
 
 void ConnectionImpl::sendSettings(const Http2Settings& http2_settings) {
+  ASSERT(Http2Settings::MIN_HPACK_TABLE_SIZE <= http2_settings.hpack_table_size_ &&
+         http2_settings.hpack_table_size_ <= Http2Settings::MAX_HPACK_TABLE_SIZE);
+  ASSERT(Http2Settings::MIN_MAX_CONCURRENT_STREAMS <= http2_settings.max_concurrent_streams_ &&
+         http2_settings.max_concurrent_streams_ <= Http2Settings::MAX_MAX_CONCURRENT_STREAMS);
+  ASSERT(Http2Settings::MIN_INITIAL_WINDOW_SIZE <= http2_settings.initial_window_size_ &&
+         http2_settings.initial_window_size_ <= Http2Settings::MAX_INITIAL_WINDOW_SIZE);
+
   std::vector<nghttp2_settings_entry> iv = {
       {NGHTTP2_SETTINGS_MAX_CONCURRENT_STREAMS, http2_settings.max_concurrent_streams_},
       {NGHTTP2_SETTINGS_INITIAL_WINDOW_SIZE, http2_settings.initial_window_size_}};
