64609e6dfe6ea433ef35ff7072e70b3e809c735c
方俊武 <fangjunwu@xunlei.com>
2017-05-25 14:34:19 +0800

    compare http2_settings.initial_window_size_ and NGHTTP2_INITIAL_CONNECTION_WINDOW_SIZE with not equal

--- source/common/http/http2/codec_impl.cc
+++ source/common/http/http2/codec_impl.cc
@@ -527,8 +527,7 @@ void ConnectionImpl::sendSettings(const Http2Settings& http2_settings) {
   UNREFERENCED_PARAMETER(rc);
 
   // Increase connection window size up to our default size.
-  ASSERT(http2_settings.initial_window_size_ >= NGHTTP2_INITIAL_CONNECTION_WINDOW_SIZE);
-  if (http2_settings.initial_window_size_ > NGHTTP2_INITIAL_CONNECTION_WINDOW_SIZE) {
+  if (http2_settings.initial_window_size_ != NGHTTP2_INITIAL_CONNECTION_WINDOW_SIZE) {
     rc = nghttp2_submit_window_update(session_, NGHTTP2_FLAG_NONE, 0,
                                       http2_settings.initial_window_size_ -
                                           NGHTTP2_INITIAL_CONNECTION_WINDOW_SIZE);
