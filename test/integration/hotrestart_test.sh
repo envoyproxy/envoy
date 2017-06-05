393b834419cab366e572bb808d2e044683547c34
Henna Huang <henna@google.com>
2017-06-05 16:41:39 -0400

    hot restart v6 test

--- test/integration/hotrestart_test.sh
+++ test/integration/hotrestart_test.sh
@@ -7,19 +7,40 @@ set -e
 # TODO(htuch): In this test script, we are duplicating work done in test_environment.cc via sed.
 # Instead, we can add a simple C++ binary that links against test_environment.cc and uses the
 # substitution methods provided there.
-# TODO(henna): Parameterize IPv4 and IPv6 testing.
-HOT_RESTART_JSON="${TEST_TMPDIR}"/hot_restart.json
-cat "${TEST_RUNDIR}"/test/config/integration/server.json |
-  sed -e "s#{{ upstream_. }}#0#g" | \
-  sed -e "s#{{ test_rundir }}#$TEST_RUNDIR#" | \
-  sed -e "s#{{ ip_loopback_address }}#127.0.0.1#" | \
-  sed -e "s#{{ dns_lookup_family }}#v4_only#" | \
-  cat > "${HOT_RESTART_JSON}"
+JSON_TEST_ARRAY=()
 
+# Parameterize IPv4 and IPv6 testing.
+if [[ "${ENVOY_IP_TEST_VERSIONS}" == "v4only" ]] || [[ "${ENVOY_IP_TEST_VERSIONS}" == "all" ]] \
+  || [[ -z "${ENVOY_IP_TEST_VERSIONS}" ]]; then
+  HOT_RESTART_JSON_V4="${TEST_TMPDIR}"/hot_restart_v4.json
+  cat "${TEST_RUNDIR}"/test/config/integration/server.json |
+    sed -e "s#{{ upstream_. }}#0#g" | \
+    sed -e "s#{{ test_rundir }}#$TEST_RUNDIR#" | \
+    sed -e "s#{{ ip_loopback_address }}#127.0.0.1#" | \
+    sed -e "s#{{ dns_lookup_family }}#v4_only#" | \
+    cat > "${HOT_RESTART_JSON_V4}"
+  JSON_TEST_ARRAY+=("${HOT_RESTART_JSON_V4}")
+fi
+
+if [[ "${ENVOY_IP_TEST_VERSIONS}" == "v6only" ]] || [[ "${ENVOY_IP_TEST_VERSIONS}" == "all" ]] \
+  || [[ -z "${ENVOY_IP_TEST_VERSIONS}" ]]; then
+  HOT_RESTART_JSON_V6="${TEST_TMPDIR}"/hot_restart_v6.json
+  cat "${TEST_RUNDIR}"/test/config/integration/server.json |
+    sed -e "s#{{ upstream_. }}#0#g" | \
+    sed -e "s#{{ test_rundir }}#$TEST_RUNDIR#" | \
+    sed -e "s#{{ ip_loopback_address }}#[::1]#" | \
+    sed -e "s#{{ dns_lookup_family }}#v6_only#" | \
+    cat > "${HOT_RESTART_JSON_V6}"
+  JSON_TEST_ARRAY+=("${HOT_RESTART_JSON_V6}")
+fi
+
+TEST_INDEX=0
+for HOT_RESTART_JSON in "${JSON_TEST_ARRAY[@]}"
+do
 # Now start the real server, hot restart it twice, and shut it all down as a basic hot restart
 # sanity test.
 echo "Starting epoch 0"
-ADMIN_ADDRESS_PATH_0="${TEST_TMPDIR}"/admin_0.address
+ADMIN_ADDRESS_PATH_0="${TEST_TMPDIR}"/admin.0."${TEST_INDEX}".address
 "${ENVOY_BIN}" -c "${HOT_RESTART_JSON}" \
     --restart-epoch 0 --base-id 1 --service-cluster cluster --service-node node \
     --admin-address-path "${ADMIN_ADDRESS_PATH_0}" &
@@ -28,8 +49,8 @@ FIRST_SERVER_PID=$!
 sleep 3
 
 echo "Updating original config json listener addresses"
-UPDATED_HOT_RESTART_JSON="${TEST_TMPDIR}"/hot_restart_updated.json
-"${TEST_RUNDIR}"/tools/socket_passing.py "-o" "${HOT_RESTART_JSON}" "-a" "${ADMIN_ADDRESS_PATH_0}" \
+UPDATED_HOT_RESTART_JSON="${TEST_TMPDIR}"/hot_restart_updated."${TEST_INDEX}".json
+"${TEST_RUNDIR}"/tools/socket_passing "-o" "${HOT_RESTART_JSON}" "-a" "${ADMIN_ADDRESS_PATH_0}" \
   "-u" "${UPDATED_HOT_RESTART_JSON}"
 
 # Send SIGUSR1 signal to the first server, this should not kill it. Also send SIGHUP which should
@@ -40,7 +61,7 @@ kill -SIGHUP ${FIRST_SERVER_PID}
 sleep 3
 
 echo "Starting epoch 1"
-ADMIN_ADDRESS_PATH_1="${TEST_TMPDIR}"/admin_1.address
+ADMIN_ADDRESS_PATH_1="${TEST_TMPDIR}"/admin.1."${TEST_INDEX}".address
 "${ENVOY_BIN}" -c "${UPDATED_HOT_RESTART_JSON}" \
     --restart-epoch 1 --base-id 1 --service-cluster cluster --service-node node \
     --admin-address-path "${ADMIN_ADDRESS_PATH_1}" &
@@ -50,13 +71,13 @@ SECOND_SERVER_PID=$!
 sleep 7
 
 echo "Checking that listener addresses have not changed"
-HOT_RESTART_JSON_1="${TEST_TMPDIR}"/hot_restart_1.json
-"${TEST_RUNDIR}"/tools/socket_passing.py "-o" "${UPDATED_HOT_RESTART_JSON}" "-a" "${ADMIN_ADDRESS_PATH_1}" \
+HOT_RESTART_JSON_1="${TEST_TMPDIR}"/hot_restart.1."${TEST_INDEX}".json
+"${TEST_RUNDIR}"/tools/socket_passing "-o" "${UPDATED_HOT_RESTART_JSON}" "-a" "${ADMIN_ADDRESS_PATH_1}" \
   "-u" "${HOT_RESTART_JSON_1}"
 CONFIG_DIFF=$(diff "${UPDATED_HOT_RESTART_JSON}" "${HOT_RESTART_JSON_1}")
 [[ -z "${CONFIG_DIFF}" ]]
 
-ADMIN_ADDRESS_PATH_2="${TEST_TMPDIR}"/admin_2.address
+ADMIN_ADDRESS_PATH_2="${TEST_TMPDIR}"/admin.2."${TEST_INDEX}".address
 echo "Starting epoch 2"
 "${ENVOY_BIN}" -c "${UPDATED_HOT_RESTART_JSON}" \
     --restart-epoch 2 --base-id 1 --service-cluster cluster --service-node node \
@@ -66,8 +87,8 @@ THIRD_SERVER_PID=$!
 sleep 3
 
 echo "Checking that listener addresses have not changed"
-HOT_RESTART_JSON_2="${TEST_TMPDIR}"/hot_restart_2.json
-"${TEST_RUNDIR}"/tools/socket_passing.py "-o" "${UPDATED_HOT_RESTART_JSON}" "-a" "${ADMIN_ADDRESS_PATH_2}" \
+HOT_RESTART_JSON_2="${TEST_TMPDIR}"/hot_restart.2."${TEST_INDEX}".json
+"${TEST_RUNDIR}"/tools/socket_passing "-o" "${UPDATED_HOT_RESTART_JSON}" "-a" "${ADMIN_ADDRESS_PATH_2}" \
   "-u" "${HOT_RESTART_JSON_2}"
 CONFIG_DIFF=$(diff "${UPDATED_HOT_RESTART_JSON}" "${HOT_RESTART_JSON_2}")
 [[ -z "${CONFIG_DIFF}" ]]
@@ -91,5 +112,7 @@ wait ${THIRD_SERVER_PID}
 echo "Waiting for epoch 1"
 wait ${SECOND_SERVER_PID}
 [[ $? == 0 ]]
+TEST_INDEX=$((TEST_INDEX+1))
+done
 
 echo "PASS"
