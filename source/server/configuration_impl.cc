68d1233d78fae04a7387ec446f7342ebcbe5413b
Matt Rice <mrice32@users.noreply.github.com>
2017-06-06 13:17:58 -0400

    Removed unnecessary includes for statically registered components (#1056)

--- source/server/configuration_impl.cc
+++ source/server/configuration_impl.cc
@@ -16,9 +16,6 @@
 #include "common/json/config_schemas.h"
 #include "common/ratelimit/ratelimit_impl.h"
 #include "common/ssl/context_config_impl.h"
-#include "common/tracing/http_tracer_impl.h"
-#include "common/tracing/lightstep_tracer_impl.h"
-#include "common/tracing/zipkin/zipkin_tracer_impl.h"
 #include "common/upstream/cluster_manager_impl.h"
 
 #include "spdlog/spdlog.h"
