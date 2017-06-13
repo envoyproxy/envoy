a43d5c668bdb07abadedc37781a47db13ca02c18
Harvey Tuch <htuch@google.com>
2017-06-13 17:45:25 -0400

    Constructor/destructor in .cc for better gmock compile performance.

--- test/mocks/http/mocks.h
+++ test/mocks/http/mocks.h
@@ -390,6 +390,9 @@ public:
 
 class MockAsyncClientStream : public AsyncClient::Stream {
 public:
+  MockAsyncClientStream();
+  ~MockAsyncClientStream();
+
   MOCK_METHOD2(sendHeaders, void(HeaderMap& headers, bool end_stream));
   MOCK_METHOD2(sendData, void(Buffer::Instance& data, bool end_stream));
   MOCK_METHOD1(sendTrailers, void(HeaderMap& trailers));
