envoyproxy/envoy

commit e9c56dc1074188578e753b7977c6e7513a6a3bb9

config: refactor filter registration (#1083)

source/server/configuration_impl.cc
@@ -33,8 +33,7 @@ bool FilterChainUtility::buildFilterChain(Network::FilterManager& filter_manager
   return filter_manager.initializeReadFilters();
 }
 
-MainImpl::MainImpl(Server::Instance& server,
-                   Upstream::ClusterManagerFactory& cluster_manager_factory)
+MainImpl::MainImpl(Instance& server, Upstream::ClusterManagerFactory& cluster_manager_factory)
     : server_(server), cluster_manager_factory_(cluster_manager_factory) {}
 
 void MainImpl::initialize(const Json::Object& json) {
@@ -46,8 +45,7 @@ void MainImpl::initialize(const Json::Object& json) {
   LOG(info, "loading {} listener(s)", listeners.size());
   for (size_t i = 0; i < listeners.size(); i++) {
     LOG(info, "listener #{}:", i);
-    listeners_.emplace_back(
-        Server::Configuration::ListenerPtr{new ListenerConfig(*this, *listeners[i])});
+    listeners_.emplace_back(ListenerPtr{new ListenerConfig(server_, *listeners[i])});
   }
 
   if (json.hasObject("statsd_local_udp_port") && json.hasObject("statsd_udp_ip_address")) {
@@ -131,9 +129,9 @@ void MainImpl::initializeTracers(const Json::Object& configuration) {
   }
 }
 
-const std::list<Server::Configuration::ListenerPtr>& MainImpl::listeners() { return listeners_; }
+const std::list<ListenerPtr>& MainImpl::listeners() { return listeners_; }
 
-MainImpl::ListenerConfig::ListenerConfig(MainImpl& parent, Json::Object& json) : parent_(parent) {
+MainImpl::ListenerConfig::ListenerConfig(Instance& server, Json::Object& json) : server_(server) {
   address_ = Network::Utility::resolveUrl(json.getString("address"));
 
   // ':' is a reserved char in statsd. Do the translation here to avoid costly inline translations
@@ -141,15 +139,14 @@ MainImpl::ListenerConfig::ListenerConfig(MainImpl& parent, Json::Object& json) :
   std::string final_stat_name = fmt::format("listener.{}.", address_->asString());
   std::replace(final_stat_name.begin(), final_stat_name.end(), ':', '_');
 
-  scope_ = parent_.server_.stats().createScope(final_stat_name);
+  scope_ = server.stats().createScope(final_stat_name);
   LOG(info, "  address={}", address_->asString());
 
   json.validateSchema(Json::Schema::LISTENER_SCHEMA);
 
   if (json.hasObject("ssl_context")) {
     Ssl::ContextConfigImpl context_config(*json.getObject("ssl_context"));
-    ssl_context_ =
-        parent_.server_.sslContextManager().createSslServerContext(*scope_, context_config);
+    ssl_context_ = server.sslContextManager().createSslServerContext(*scope_, context_config);
   }
 
   bind_to_port_ = json.getBoolean("bind_to_port", true);
@@ -180,9 +177,8 @@ MainImpl::ListenerConfig::ListenerConfig(MainImpl& parent, Json::Object& json) :
 
     // Now see if there is a factory that will accept the config.
     auto search_it = namedFilterConfigFactories().find(string_name);
-    if (search_it != namedFilterConfigFactories().end()) {
-      NetworkFilterFactoryCb callback =
-          search_it->second->createFilterFactory(type, *config, parent_.server_);
+    if (search_it != namedFilterConfigFactories().end() && search_it->second->type() == type) {
+      NetworkFilterFactoryCb callback = search_it->second->createFilterFactory(*config, *this);
       filter_factories_.push_back(callback);
     } else {
       // DEPRECATED
@@ -190,7 +186,7 @@ MainImpl::ListenerConfig::ListenerConfig(MainImpl& parent, Json::Object& json) :
       bool found_filter = false;
       for (NetworkFilterConfigFactory* config_factory : filterConfigFactories()) {
         NetworkFilterFactoryCb callback =
-            config_factory->tryCreateFilterFactory(type, string_name, *config, parent_.server_);
+            config_factory->tryCreateFilterFactory(type, string_name, *config, server);
         if (callback) {
           filter_factories_.push_back(callback);
           found_filter = true;

source/server/configuration_impl.h
@@ -12,6 +12,7 @@
 #include "envoy/http/filter.h"
 #include "envoy/network/filter.h"
 #include "envoy/server/configuration.h"
+#include "envoy/server/filter_config.h"
 #include "envoy/server/instance.h"
 #include "envoy/tracing/http_tracer.h"
 
@@ -22,15 +23,6 @@
 namespace Envoy {
 namespace Server {
 namespace Configuration {
-
-enum class NetworkFilterType { Read, Write, Both };
-
-/**
- * This lambda is used to wrap the creation of a network filter chain for new connections as they
- * come in. Filter factories create the lambda at configuration initialization time, and then they
- * are used at runtime. This maps JSON -> runtime configuration.
- */
-typedef std::function<void(Network::FilterManager& filter_manager)> NetworkFilterFactoryCb;
 
 /**
  * DEPRECATED - Implemented by each network filter and registered via
@@ -43,35 +35,7 @@ public:
   virtual NetworkFilterFactoryCb tryCreateFilterFactory(NetworkFilterType type,
                                                         const std::string& name,
                                                         const Json::Object& config,
-                                                        Server::Instance& server) PURE;
-};
-
-/**
- * Implemented by each network filter and registered via registerNamedNetworkFilterConfigFactory()
- * or the convenience class RegisterNamedNetworkFilterConfigFactory.
- */
-class NamedNetworkFilterConfigFactory {
-public:
-  virtual ~NamedNetworkFilterConfigFactory() {}
-
-  /**
-   * Create a particular network filter factory implementation.  If the implementation is unable to
-   * produce a factory with the provided parameters, it should throw an EnvoyException in the case
-   * of general error or a Json::Exception if the json configuration is erroneous.  The returned
-   * callback should always be initialized.
-   * @param type supplies type of filter to initialize (read, write, or both)
-   * @param config supplies the general json configuration for the filter
-   * @param server supplies the server instance
-   */
-  virtual NetworkFilterFactoryCb createFilterFactory(NetworkFilterType type,
-                                                     const Json::Object& config,
-                                                     Server::Instance& server) PURE;
-
-  /**
-  * Returns the identifying name for a particular implementation of a network filter produced by
-  * the factory.
-  */
-  virtual std::string name() PURE;
+                                                        Instance& server) PURE;
 };
 
 /**
@@ -91,8 +55,7 @@ public:
   * @param server supplies the server instance
   * @param cluster_manager supplies the cluster_manager instance
   */
-  virtual Tracing::HttpTracerPtr createHttpTracer(const Json::Object& json_config,
-                                                  Server::Instance& server,
+  virtual Tracing::HttpTracerPtr createHttpTracer(const Json::Object& json_config, Instance& server,
                                                   Upstream::ClusterManager& cluster_manager) PURE;
 
   /**
@@ -120,7 +83,7 @@ public:
  */
 class MainImpl : Logger::Loggable<Logger::Id::config>, public Main {
 public:
-  MainImpl(Server::Instance& server, Upstream::ClusterManagerFactory& cluster_manager_factory_);
+  MainImpl(Instance& server, Upstream::ClusterManagerFactory& cluster_manager_factory_);
 
   /**
    * DEPRECATED - Register an NetworkFilterConfigFactory implementation as an option to create
@@ -199,10 +162,11 @@ private:
   /**
    * Maps JSON config to runtime config for a listener with network filter chain.
    */
-  class ListenerConfig : public Server::Configuration::Listener,
+  class ListenerConfig : public Listener,
+                         public FactoryContext,
                          public Network::FilterChainFactory {
   public:
-    ListenerConfig(MainImpl& parent, Json::Object& json);
+    ListenerConfig(Instance& server, Json::Object& json);
 
     // Server::Configuration::Listener
     Network::FilterChainFactory& filterChainFactory() override { return *this; }
@@ -212,13 +176,31 @@ private:
     bool useProxyProto() override { return use_proxy_proto_; }
     bool useOriginalDst() override { return use_original_dst_; }
     uint32_t perConnectionBufferLimitBytes() override { return per_connection_buffer_limit_bytes_; }
+
+    // Server::Configuration::FactoryContext
+    AccessLog::AccessLogManager& accessLogManager() override { return server_.accessLogManager(); }
+    Upstream::ClusterManager& clusterManager() override { return server_.clusterManager(); }
+    Event::Dispatcher& dispatcher() override { return server_.dispatcher(); }
+    DrainManager& drainManager() override { return server_.drainManager(); }
+    bool healthCheckFailed() override { return server_.healthCheckFailed(); }
+    Tracing::HttpTracer& httpTracer() override { return server_.httpTracer(); }
+    Init::Manager& initManager() override { return server_.initManager(); }
+    const LocalInfo::LocalInfo& localInfo() override { return server_.localInfo(); }
+    Envoy::Runtime::RandomGenerator& random() override { return server_.random(); }
+    RateLimit::ClientPtr
+    rateLimitClient(const Optional<std::chrono::milliseconds>& timeout) override {
+      return server_.rateLimitClient(timeout);
+    }
+    Envoy::Runtime::Loader& runtime() override { return server_.runtime(); }
+    Instance& server() override { return server_; }
     Stats::Scope& scope() override { return *scope_; }
+    ThreadLocal::Instance& threadLocal() override { return server_.threadLocal(); }
 
     // Network::FilterChainFactory
     bool createFilterChain(Network::Connection& connection) override;
 
   private:
-    MainImpl& parent_;
+    Instance& server_;
     Network::Address::InstanceConstSharedPtr address_;
     bool bind_to_port_{};
     Stats::ScopePtr scope_;
@@ -258,11 +240,11 @@ private:
     return *http_tracer_factories;
   }
 
-  Server::Instance& server_;
+  Instance& server_;
   Upstream::ClusterManagerFactory& cluster_manager_factory_;
   std::unique_ptr<Upstream::ClusterManager> cluster_manager_;
   Tracing::HttpTracerPtr http_tracer_;
-  std::list<Server::Configuration::ListenerPtr> listeners_;
+  std::list<ListenerPtr> listeners_;
   Optional<std::string> statsd_tcp_cluster_name_;
   Optional<uint32_t> statsd_udp_port_;
   Optional<std::string> statsd_udp_ip_address_;

test/server/configuration_impl_test.cc
@@ -289,7 +289,7 @@ TEST_F(ConfigurationImplTest, BadFilterName) {
         "address": "tcp://127.0.0.1:1234",
         "filters": [
           {
-            "type" : "read",
+            "type" : "write",
             "name" : "invalid",
             "config" : {}
           }
@@ -306,7 +306,35 @@ TEST_F(ConfigurationImplTest, BadFilterName) {
 
   MainImpl config(server_, cluster_manager_factory_);
   EXPECT_THROW_WITH_MESSAGE(config.initialize(*loader), EnvoyException,
-                            "unable to create filter factory for 'invalid'/'read'");
+                            "unable to create filter factory for 'invalid'/'write'");
+}
+
+TEST_F(ConfigurationImplTest, BadFilterType) {
+  std::string json = R"EOF(
+  {
+    "listeners" : [
+      {
+        "address": "tcp://127.0.0.1:1234",
+        "filters": [
+          {
+            "type" : "write",
+            "name" : "echo",
+            "config" : {}
+          }
+        ]
+      }
+    ],
+    "cluster_manager": {
+      "clusters": []
+    }
+  }
+  )EOF";
+
+  Json::ObjectSharedPtr loader = Json::Factory::loadFromString(json);
+
+  MainImpl config(server_, cluster_manager_factory_);
+  EXPECT_THROW_WITH_MESSAGE(config.initialize(*loader), EnvoyException,
+                            "unable to create filter factory for 'echo'/'write'");
 }
 
 TEST_F(ConfigurationImplTest, ServiceClusterNotSetWhenLSTracing) {

commit 76b6fc80c355fc67747a64bac951a98d7101cb3a

stats: fix listener/filter scope prefixes
Regression from https://github.com/lyft/envoy/pull/1083. The listener
config needs two scopes. One for listener named stats, and one for global
named stats that need to be reaped during LDS processing.

include/envoy/server/configuration.h
@@ -68,7 +68,7 @@ public:
   /**
    * @return Stats::Scope& the stats scope to use for all listener specific stats.
    */
-  virtual Stats::Scope& scope() PURE;
+  virtual Stats::Scope& listenerScope() PURE;
 };
 
 typedef std::unique_ptr<Listener> ListenerPtr;

source/server/configuration_impl.cc
@@ -131,22 +131,24 @@ void MainImpl::initializeTracers(const Json::Object& configuration) {
 
 const std::list<ListenerPtr>& MainImpl::listeners() { return listeners_; }
 
-MainImpl::ListenerConfig::ListenerConfig(Instance& server, Json::Object& json) : server_(server) {
-  address_ = Network::Utility::resolveUrl(json.getString("address"));
+MainImpl::ListenerConfig::ListenerConfig(Instance& server, Json::Object& json)
+    : server_(server), address_(Network::Utility::resolveUrl(json.getString("address"))),
+      global_scope_(server.stats().createScope("")) {
 
   // ':' is a reserved char in statsd. Do the translation here to avoid costly inline translations
   // later.
   std::string final_stat_name = fmt::format("listener.{}.", address_->asString());
   std::replace(final_stat_name.begin(), final_stat_name.end(), ':', '_');
 
-  scope_ = server.stats().createScope(final_stat_name);
+  listener_scope_ = server.stats().createScope(final_stat_name);
   LOG(info, "  address={}", address_->asString());
 
   json.validateSchema(Json::Schema::LISTENER_SCHEMA);
 
   if (json.hasObject("ssl_context")) {
     Ssl::ContextConfigImpl context_config(*json.getObject("ssl_context"));
-    ssl_context_ = server.sslContextManager().createSslServerContext(*scope_, context_config);
+    ssl_context_ =
+        server.sslContextManager().createSslServerContext(*listener_scope_, context_config);
   }
 
   bind_to_port_ = json.getBoolean("bind_to_port", true);

source/server/configuration_impl.h
@@ -176,6 +176,7 @@ private:
     bool useProxyProto() override { return use_proxy_proto_; }
     bool useOriginalDst() override { return use_original_dst_; }
     uint32_t perConnectionBufferLimitBytes() override { return per_connection_buffer_limit_bytes_; }
+    Stats::Scope& listenerScope() override { return *listener_scope_; }
 
     // Server::Configuration::FactoryContext
     AccessLog::AccessLogManager& accessLogManager() override { return server_.accessLogManager(); }
@@ -193,7 +194,7 @@ private:
     }
     Envoy::Runtime::Loader& runtime() override { return server_.runtime(); }
     Instance& server() override { return server_; }
-    Stats::Scope& scope() override { return *scope_; }
+    Stats::Scope& scope() override { return *global_scope_; }
     ThreadLocal::Instance& threadLocal() override { return server_.threadLocal(); }
 
     // Network::FilterChainFactory
@@ -203,7 +204,8 @@ private:
     Instance& server_;
     Network::Address::InstanceConstSharedPtr address_;
     bool bind_to_port_{};
-    Stats::ScopePtr scope_;
+    Stats::ScopePtr global_scope_;   // Stats with global named scope, but needed for LDS cleanup.
+    Stats::ScopePtr listener_scope_; // Stats with listener named scope.
     Ssl::ServerContextPtr ssl_context_;
     bool use_proxy_proto_{};
     bool use_original_dst_{};

source/server/worker.cc
@@ -29,10 +29,11 @@ void Worker::initializeConfiguration(Server::Configuration::Main& config,
         .per_connection_buffer_limit_bytes_ = listener->perConnectionBufferLimitBytes()};
     if (listener->sslContext()) {
       handler_->addSslListener(listener->filterChainFactory(), *listener->sslContext(),
-                               *socket_map.at(listener.get()), listener->scope(), listener_options);
+                               *socket_map.at(listener.get()), listener->listenerScope(),
+                               listener_options);
     } else {
       handler_->addListener(listener->filterChainFactory(), *socket_map.at(listener.get()),
-                            listener->scope(), listener_options);
+                            listener->listenerScope(), listener_options);
     }
   }
 

test/server/configuration_impl_test.cc
@@ -459,6 +459,50 @@ TEST_F(ConfigurationImplTest, ConfigurationFailsWhenInvalidTracerSpecified) {
                             "No HttpTracerFactory found for type: invalid");
 }
 
+class TestStatsConfigFactory : public NamedNetworkFilterConfigFactory {
+public:
+  // NetworkFilterConfigFactory
+  NetworkFilterFactoryCb createFilterFactory(const Json::Object&,
+                                             FactoryContext& context) override {
+    context.scope().counter("bar").inc();
+    return [](Network::FilterManager&) -> void {};
+  }
+  std::string name() override { return "stats_test"; }
+  NetworkFilterType type() override { return NetworkFilterType::Read; }
+};
+
+TEST_F(ConfigurationImplTest, StatsScopeTest) {
+  RegisterNamedNetworkFilterConfigFactory<TestStatsConfigFactory> registered;
+
+  std::string json = R"EOF(
+  {
+    "listeners" : [
+      {
+        "address": "tcp://127.0.0.1:1234",
+        "filters": [
+          {
+            "type" : "read",
+            "name" : "stats_test",
+            "config" : {}
+          }
+        ]
+      }
+    ],
+    "cluster_manager": {
+      "clusters": []
+    }
+  }
+  )EOF";
+
+  Json::ObjectSharedPtr loader = Json::Factory::loadFromString(json);
+  MainImpl config(server_, cluster_manager_factory_);
+  config.initialize(*loader);
+  config.listeners().front()->listenerScope().counter("foo").inc();
+
+  EXPECT_EQ(1UL, server_.stats_store_.counter("bar").value());
+  EXPECT_EQ(1UL, server_.stats_store_.counter("listener.127.0.0.1_1234.foo").value());
+}
+
 /**
  * Config registration for the echo filter using the deprecated registration class.
  */
@@ -507,6 +551,7 @@ TEST_F(ConfigurationImplTest, DeprecatedFilterConfigFactoryRegistrationTest) {
   MainImpl config(server_, cluster_manager_factory_);
   config.initialize(*loader);
 }
+
 } // Configuration
 } // Server
 } // Envoy