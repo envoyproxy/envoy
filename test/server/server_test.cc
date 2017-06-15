83678123044d64bcc53c66fa1fa5c16053f49da8
Constance Caramanolis <ccaramanolis@lyft.com>
2017-06-14 17:28:17 -0700

    Update iter ptr before initialize is called.

--- test/server/server_test.cc
+++ test/server/server_test.cc
@@ -13,6 +13,7 @@
 namespace Envoy {
 using testing::_;
 using testing::InSequence;
+using testing::Invoke;
 
 namespace Server {
 
@@ -35,6 +36,19 @@ TEST(InitManagerImplTest, Targets) {
   manager.initialize([&]() -> void { initialized.ready(); });
   EXPECT_CALL(initialized, ready());
   target.callback_();
+}
+
+TEST(InitManagerImplTest, TargetRemoveWhile) {
+  InSequence s;
+  InitManagerImpl manager;
+  Init::MockTarget target1;
+  ReadyWatcher initialized;
+
+  manager.registerTarget(target1);
+  EXPECT_CALL(target1, initialize(_))
+      .WillOnce(Invoke([this](std::function<void()> callback) -> void { callback(); }));
+  EXPECT_CALL(initialized, ready());
+  manager.initialize([&]() -> void { initialized.ready(); });
 }
 
 // Class creates minimally viable server instance for testing.
