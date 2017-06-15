c9c7aa1125a3ea5d4c40b4d63f333ec6251cf08e
Derek <dschaller@users.noreply.github.com>
2017-06-15 10:56:24 -0700

    docs: fix favicon for landing and doc sites (#1119)

--- docs/conf.py
+++ docs/conf.py
@@ -139,7 +139,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'landing_source/source/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
