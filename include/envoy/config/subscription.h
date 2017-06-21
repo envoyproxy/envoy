5f72e5d13b93b556919096f76cdae14549e055f8
htuch <htuch@users.noreply.github.com>
2017-06-21 09:59:56 -0400

    config: inotify Subscription implementation. (#1150)
    Filesystem inotify implementation of the API Subscription interface. This allows the API to be consumed on filesystem changes to files containing the JSON canonical representation of lists of ResourceType.

--- include/envoy/config/subscription.h
+++ include/envoy/config/subscription.h
@@ -38,7 +38,8 @@ public:
    * Start a configuration subscription asynchronously. This should be called once and will continue
    * to fetch throughout the lifetime of the Subscription object.
    * @param resources vector of resource names to fetch.
-   * @param callbacks the callbacks to be notified of configuration updates.
+   * @param callbacks the callbacks to be notified of configuration updates. The callback must not
+   *        result in the deletion of the Subscription object.
    */
   virtual void start(const std::vector<std::string>& resources,
                      SubscriptionCallbacks<ResourceType>& callbacks) PURE;
