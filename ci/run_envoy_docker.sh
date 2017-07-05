ee1376ccd39a89a53de521bbd3155557bc2b32bb
Harvey Tuch <htuch@google.com>
2017-07-05 11:29:36 -0400

    run_envoy_docker: use CI hash by default.

--- ci/run_envoy_docker.sh
+++ ci/run_envoy_docker.sh
@@ -11,15 +11,13 @@ else
 	USER_GROUP=$(id -g)
 fi
 
-# The IMAGE_ID can be set to an arbitrary image ID (found with 'docker images').
-[[ -z "${IMAGE_ID}" ]] && IMAGE_ID="latest"
+# The IMAGE_ID defaults to the CI hash but can be set to an arbitrary image ID (found with 'docker
+# images').
+CI_ENVOY_BUILD_SHA=$(grep "^ENVOY_BUILD_SHA" ci/ci_steps.sh | cut -d\= -f 2)
+[[ -z "${IMAGE_ID}" ]] && IMAGE_ID="${CI_ENVOY_BUILD_SHA}"
 [[ -z "${ENVOY_DOCKER_BUILD_DIR}" ]] && ENVOY_DOCKER_BUILD_DIR=/tmp/envoy-docker-build
 
 mkdir -p "${ENVOY_DOCKER_BUILD_DIR}"
-# When not operating on an existing image ID, fetch latest.
-if [[ "${IMAGE_ID}" == "latest" ]]
-then
-  docker pull lyft/envoy-build:latest
-fi
+# Since we specify an explicit hash, docker-run will pull from the remote repo if missing.
 docker run -t -i -u "${USER}":"${USER_GROUP}" -v "${ENVOY_DOCKER_BUILD_DIR}":/build \
   -v "$PWD":/source lyft/envoy-build:"${IMAGE_ID}" /bin/bash -c "cd source && $*"
