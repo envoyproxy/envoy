46e0fd3f6b0526192726d749790124c21fa000c3
Daniel Hochman <dhochman@lyft.com>
2017-07-10 17:14:28 -0700

    revert formatting changes to conn_pool_impl

--- source/common/redis/conn_pool_impl.cc
+++ source/common/redis/conn_pool_impl.cc
@@ -179,10 +179,11 @@ InstanceImpl::InstanceImpl(const std::string& cluster_name, Upstream::ClusterMan
                            const Json::Object& config)
     : cm_(cm), client_factory_(client_factory), tls_(tls), tls_slot_(tls.allocateSlot()),
       config_(config) {
-  tls.set(tls_slot_, [this, cluster_name](
-                         Event::Dispatcher& dispatcher) -> ThreadLocal::ThreadLocalObjectSharedPtr {
-    return std::make_shared<ThreadLocalPool>(*this, dispatcher, cluster_name);
-  });
+  tls.set(tls_slot_,
+          [this,
+           cluster_name](Event::Dispatcher& dispatcher) -> ThreadLocal::ThreadLocalObjectSharedPtr {
+            return std::make_shared<ThreadLocalPool>(*this, dispatcher, cluster_name);
+          });
 }
 
 PoolRequest* InstanceImpl::makeRequest(const std::string& hash_key, const RespValue& value,
