bb1e2e33c2c80c947fe527b38a60d18ba5bdf0a0
Gabriel Sagula <gsagula@gmail.com>
2017-07-17 18:29:10 -0700

    deps: Set zlib build recipe with --prefix, removed unnecessary commands and added zconf.h in BUILD hrds

--- ci/build_container/build_recipes/zlib.sh
+++ ci/build_container/build_recipes/zlib.sh
@@ -7,7 +7,5 @@ VERSION=1.2.11
 wget -O zlib-$VERSION.tar.gz https://github.com/madler/zlib/archive/v$VERSION.tar.gz
 tar xf zlib-$VERSION.tar.gz
 cd zlib-$VERSION
-./configure
+./configure --prefix=$THIRDPARTY_BUILD
 make V=1 install
-cp libz.a $THIRDPARTY_BUILD/lib
-cp zlib.h $THIRDPARTY_BUILD/include
