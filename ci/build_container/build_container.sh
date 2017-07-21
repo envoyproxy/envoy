f92c8f883560dc9768c98cb4950f6bcc080ed283
Harvey Tuch <htuch@google.com>
2017-07-21 11:18:33 -0400

    build: switch protobuf version to pinned HEAD commit.
    Use protobuf from HEAD to ensure we have the
    https://github.com/google/protobuf/pull/3327 fix for CI and local
    builds. Users can opt to use a specific release tag with the
    ENVOY_PROTOBUF_COMMIT env var.

--- ci/build_container/build_container.sh
+++ ci/build_container/build_container.sh
@@ -41,11 +41,13 @@ DEPS=$(python <(cat target_recipes.bzl; \
   echo "print ' '.join(\"${THIRDPARTY_DEPS}/%s.dep\" % r for r in set(TARGET_RECIPES.values()))"))
 
 # TODO(htuch): We build twice as a workaround for https://github.com/google/protobuf/issues/3322.
-# Fix this.
-export THIRDPARTY_BUILD=/thirdparty_build_opt
+# Fix this. This will be gone real soon now.
+export THIRDPARTY_BUILD=/thirdparty_build
 export CPPFLAGS="-DNDEBUG"
 echo "Building opt deps ${DEPS}"
 "$(dirname "$0")"/build_and_install_deps.sh ${DEPS}
+
+ln -sf /thirdparty_build /thirdparty_build_opt
 
 export THIRDPARTY_BUILD=/thirdparty_build_dbg
 export CPPFLAGS=""
