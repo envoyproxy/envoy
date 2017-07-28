30a720766027342c2f7ec9f61424fbf1edc35dbd
Stephan Zuercher <stephan@turbinelabs.io>
2017-07-28 16:55:25 -0700

    signal (osx): hit #warning on 32bit OS X and handle possibility of MAP_STACK existing in the future

--- source/exe/signal_action.cc
+++ source/exe/signal_action.cc
@@ -16,7 +16,7 @@ void SignalAction::sigHandler(int sig, siginfo_t* info, void* context) {
 #ifdef REG_RIP
     // x86_64
     error_pc = reinterpret_cast<void*>(ucontext->uc_mcontext.gregs[REG_RIP]);
-#elif defined(__APPLE__)
+#elif defined(__APPLE__) && defined(__x86_64__)
     error_pc = reinterpret_cast<void*>(ucontext->uc_mcontext->__ss.__rip);
 #else
 #warning "Please enable and test PC retrieval code for your arch in signal_action.cc"
@@ -73,7 +73,7 @@ void SignalAction::removeSigHandlers() {
   }
 }
 
-#ifdef __APPLE__
+#if defined(__APPLE__) && !defined(MAP_STACK)
 #define MAP_STACK (0)
 #endif
 
