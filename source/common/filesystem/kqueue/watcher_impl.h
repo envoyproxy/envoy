40e0db90c3a0f70dba32352a68dbadd9746fbffc
Stephan Zuercher <stephan@turbinelabs.io>
2017-07-31 13:08:48 -0700

    filesystem: provide kqueue-based impl of Watcher

---
+++ source/common/filesystem/kqueue/watcher_impl.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <cstdint>
+#include <list>
+#include <string>
+
+#include "envoy/event/dispatcher.h"
+#include "envoy/filesystem/filesystem.h"
+
+#include "common/common/linked_object.h"
+#include "common/common/logger.h"
+
+namespace Envoy {
+namespace Filesystem {
+
+/**
+ * Implementation of Watcher that uses kqueue. If the file being watched doesn't exist, we watch
+ * the directory, and then try to add a file watch each time there's a write event to the
+ * directory.
+ */
+class WatcherImpl : public Watcher, Logger::Loggable<Logger::Id::file> {
+public:
+  WatcherImpl(Event::Dispatcher& dispatcher);
+  ~WatcherImpl();
+
+  // Filesystem::Watcher
+  void addWatch(const std::string& path, uint32_t events, OnChangedCb cb) override;
+
+private:
+  struct FileWatch : LinkedObject<FileWatch> {
+    int fd_;
+    uint32_t events_;
+    std::string file_;
+    OnChangedCb callback_;
+    bool watchingDir_;
+  };
+
+  typedef std::shared_ptr<FileWatch> FileWatchPtr;
+
+  void onKqueueEvent();
+  FileWatchPtr addWatch_(const std::string& path, uint32_t events, Watcher::OnChangedCb cb,
+                         bool pathMustExist);
+  void removeWatch_(FileWatchPtr& watch);
+
+  int queue_;
+  std::unordered_map<int, FileWatchPtr> watches_;
+  Event::FileEventPtr kqueue_event_;
+};
+
+} // namespace Filesystem
+} // namespace Envoy
