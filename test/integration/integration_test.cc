70f3f3c4ccfe68a7cc1576d5acbaa44a41326010
Shriram Rajagopalan <shriram@us.ibm.com>
2017-08-07 14:39:26 -0400

    fixing integration tests

--- test/integration/integration_test.cc
+++ test/integration/integration_test.cc
@@ -161,21 +161,25 @@ TEST_P(IntegrationTest, WebSocketConnectionDownstreamDisconnect) {
   executeActions({
       [&]() -> void { tcp_client = makeTcpConnection(lookupPort("http")); },
       // Send websocket upgrade request
+      // The request path gets rewritten from /websocket/test to /websocket.
+      // The size of headers received by the destination is 225 bytes.
       [&]() -> void { tcp_client->write(upgrade_req_str); },
       [&]() -> void { fake_upstream_connection = fake_upstreams_[0]->waitForRawConnection(); },
-      [&]() -> void { fake_upstream_connection->waitForData(190); },
+      [&]() -> void { fake_upstream_connection->waitForData(225); },
       // Accept websocket upgrade request
       [&]() -> void { fake_upstream_connection->write(upgrade_resp_str); },
       [&]() -> void { tcp_client->waitForData(upgrade_resp_str); },
       // Standard TCP proxy semantics post upgrade
       [&]() -> void { tcp_client->write("hello"); },
-      [&]() -> void { fake_upstream_connection->waitForData(195); },
+      // datalen = 225 + strlen(hello)
+      [&]() -> void { fake_upstream_connection->waitForData(230); },
       [&]() -> void { fake_upstream_connection->write("world"); },
       [&]() -> void { tcp_client->waitForData(upgrade_resp_str + "world"); },
       [&]() -> void { tcp_client->write("bye!"); },
       // downstream disconnect
       [&]() -> void { tcp_client->close(); },
-      [&]() -> void { fake_upstream_connection->waitForData(199); },
+      // datalen = 225 + strlen(hello) + strlen(bye!)
+      [&]() -> void { fake_upstream_connection->waitForData(234); },
       [&]() -> void { fake_upstream_connection->waitForDisconnect(); },
   });
 }
@@ -193,13 +197,16 @@ TEST_P(IntegrationTest, WebSocketConnectionUpstreamDisconnect) {
        // Send websocket upgrade request
        [&]() -> void { tcp_client->write(upgrade_req_str); },
        [&]() -> void { fake_upstream_connection = fake_upstreams_[0]->waitForRawConnection(); },
-       [&]() -> void { fake_upstream_connection->waitForData(190); },
+       // The request path gets rewritten from /websocket/test to /websocket.
+       // The size of headers received by the destination is 225 bytes.
+       [&]() -> void { fake_upstream_connection->waitForData(225); },
        // Accept websocket upgrade request
        [&]() -> void { fake_upstream_connection->write(upgrade_resp_str); },
        [&]() -> void { tcp_client->waitForData(upgrade_resp_str); },
        // Standard TCP proxy semantics post upgrade
        [&]() -> void { tcp_client->write("hello"); },
-       [&]() -> void { fake_upstream_connection->waitForData(195); },
+       // datalen = 225 + strlen(hello)
+       [&]() -> void { fake_upstream_connection->waitForData(230); },
        [&]() -> void { fake_upstream_connection->write("world"); },
        // upstream disconnect
        [&]() -> void { fake_upstream_connection->close(); },
