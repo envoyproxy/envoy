295cc0b7a3a4c3e6ca35cbe59861a23b8eb0d2fe
Jose Nino <jnino@lyft.com>
2017-08-16 16:18:15 -0700

    move macro

--- include/envoy/singleton/manager.h
+++ include/envoy/singleton/manager.h
@@ -37,6 +37,20 @@ public:
 };
 
 /**
+ * Macro used to statically register singletons managed by the singleton manager
+ * defined in envoy/singleton/manager.h. After the NAME has been registered use the
+ * SINGLETON_MANAGER_REGISTERED_NAME macro to access the name registered with the
+ * singleton manager.
+ */
+#define SINGLETON_MANAGER_REGISTRATION(NAME)                                                       \
+  static constexpr char NAME##_singleton_name[] = #NAME "_singleton";                              \
+  static Registry::RegisterFactory<Singleton::RegistrationImpl<NAME##_singleton_name>,             \
+                                   Singleton::Registration>                                        \
+      NAME##_singleton_registered_;
+
+#define SINGLETON_MANAGER_REGISTERED_NAME(NAME) NAME##_singleton_name
+
+/**
  * Callback function used to create a singleton.
  */
 typedef std::function<InstanceSharedPtr()> SingletonFactoryCb;
