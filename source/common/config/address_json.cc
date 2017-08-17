b1d0a093cf56b25d3e015f928896719f642f2ea7
Harvey Tuch <htuch@google.com>
2017-08-17 14:53:30 -0400

    config: plumb through recent Address changes in envoy-api.

--- source/common/config/address_json.cc
+++ source/common/config/address_json.cc
@@ -6,38 +6,26 @@
 namespace Envoy {
 namespace Config {
 
-void AddressJson::translateUnresolvedAddress(
-    const std::string& json_address, bool url,
-    envoy::api::v2::UnresolvedAddress& unresolved_address) {
-  // TODO(htuch): No parsing required yet for either pipes or non-URL unresolved addresses, add as
-  // needed.
-  ASSERT(url);
-  UNREFERENCED_PARAMETER(url);
-  auto* named_address = unresolved_address.mutable_named_address();
-  named_address->set_address(Network::Utility::hostFromTcpUrl(json_address));
-  named_address->mutable_port()->set_value(Network::Utility::portFromTcpUrl(json_address));
-}
-
-void AddressJson::translateResolvedAddress(const std::string& json_address, bool url,
-                                           envoy::api::v2::ResolvedAddress& resolved_address) {
-  Network::Address::InstanceConstSharedPtr address =
-      url ? Network::Utility::resolveUrl(json_address)
-          : Network::Utility::parseInternetAddressAndPort(json_address);
-  if (address->type() == Network::Address::Type::Ip) {
-    auto* socket_address = resolved_address.mutable_socket_address();
-    switch (address->ip()->version()) {
-    case Network::Address::IpVersion::v4:
-      socket_address->set_ip_address(address->ip()->addressAsString());
-      break;
-    case Network::Address::IpVersion::v6:
-      socket_address->set_ip_address(address->ip()->addressAsString());
-      break;
+void AddressJson::translateAddress(const std::string& json_address, bool url, bool resolved,
+                                   envoy::api::v2::Address& address) {
+  if (resolved) {
+    Network::Address::InstanceConstSharedPtr instance =
+        url ? Network::Utility::resolveUrl(json_address)
+            : Network::Utility::parseInternetAddressAndPort(json_address);
+    if (instance->type() == Network::Address::Type::Ip) {
+      address.mutable_socket_address()->set_address(instance->ip()->addressAsString());
+      address.mutable_socket_address()->set_port_value(instance->ip()->port());
+    } else {
+      ASSERT(instance->type() == Network::Address::Type::Pipe);
+      address.mutable_pipe()->set_path(instance->asString());
     }
-    socket_address->mutable_port()->set_value(address->ip()->port());
-  } else {
-    ASSERT(address->type() == Network::Address::Type::Pipe);
-    resolved_address.mutable_pipe()->set_path(address->asString());
+    return;
   }
+
+  // We don't have v1 JSON with unresolved addresses in non-URL form.
+  ASSERT(url);
+  address.mutable_socket_address()->set_address(Network::Utility::hostFromTcpUrl(json_address));
+  address.mutable_socket_address()->set_port_value(Network::Utility::portFromTcpUrl(json_address));
 }
 
 } // namespace Config
