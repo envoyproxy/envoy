0428cd4ff7f0c34576ea9fd7a9c09c303a3373df
Stephan Zuercher <stephan@turbinelabs.io>
2017-08-21 10:08:11 -0700

    build: fix gperftools build on OS X

--- ci/build_container/build_recipes/gperftools.sh
+++ ci/build_container/build_recipes/gperftools.sh
@@ -7,5 +7,12 @@ VERSION=2.6.1
 wget -O gperftools-$VERSION.tar.gz https://github.com/gperftools/gperftools/releases/download/gperftools-$VERSION/gperftools-$VERSION.tar.gz
 tar xf gperftools-$VERSION.tar.gz
 cd gperftools-$VERSION
+
+if [[ `uname` == "Darwin" ]];
+then
+  # enable ucontext(3)
+  export CPPFLAGS="-D_XOPEN_SOURCE=1"
+fi
+
 LDFLAGS="-lpthread" ./configure --prefix=$THIRDPARTY_BUILD --enable-shared=no --enable-frame-pointers --disable-libunwind
 make V=1 install
