b0d8095d0cd3c63667997124b9e8cee70d52f743
Piotr Sikora <piotrsikora@google.com>
2017-08-25 14:21:50 -0700

    config: sync with recent API changes in bootstrap and TLS. (#1548)
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- source/common/config/tls_context_json.cc
+++ source/common/config/tls_context_json.cc
@@ -10,8 +10,6 @@ void TlsContextJson::translateDownstreamTlsContext(
     const Json::Object& json_tls_context,
     envoy::api::v2::DownstreamTlsContext& downstream_tls_context) {
   translateCommonTlsContext(json_tls_context, *downstream_tls_context.mutable_common_tls_context());
-  translateTlsCertificate(json_tls_context,
-                          *downstream_tls_context.mutable_tls_certificates()->Add());
   JSON_UTIL_SET_BOOL(json_tls_context, downstream_tls_context, require_client_certificate);
 }
 
@@ -19,7 +17,6 @@ void TlsContextJson::translateUpstreamTlsContext(
     const Json::Object& json_tls_context,
     envoy::api::v2::UpstreamTlsContext& upstream_tls_context) {
   translateCommonTlsContext(json_tls_context, *upstream_tls_context.mutable_common_tls_context());
-  translateTlsCertificate(json_tls_context, *upstream_tls_context.mutable_client_certificate());
   upstream_tls_context.set_sni(json_tls_context.getString("sni", ""));
 }
 
@@ -34,8 +31,10 @@ void TlsContextJson::translateCommonTlsContext(
   common_tls_context.mutable_deprecated_v1()->set_alt_alpn_protocols(
       json_tls_context.getString("alt_alpn_protocols", ""));
 
+  translateTlsCertificate(json_tls_context, *common_tls_context.mutable_tls_certificates()->Add());
+
   auto* validation_context = common_tls_context.mutable_validation_context();
-  validation_context->mutable_ca_cert()->set_filename(
+  validation_context->mutable_trusted_ca()->set_filename(
       json_tls_context.getString("ca_cert_file", ""));
   if (json_tls_context.hasObject("verify_certificate_hash")) {
     validation_context->add_verify_certificate_hash(
@@ -59,7 +58,7 @@ void TlsContextJson::translateCommonTlsContext(
 
 void TlsContextJson::translateTlsCertificate(const Json::Object& json_tls_context,
                                              envoy::api::v2::TlsCertificate& tls_certificate) {
-  tls_certificate.mutable_cert_chain()->set_filename(
+  tls_certificate.mutable_certificate_chain()->set_filename(
       json_tls_context.getString("cert_chain_file", ""));
   tls_certificate.mutable_private_key()->set_filename(
       json_tls_context.getString("private_key_file", ""));
