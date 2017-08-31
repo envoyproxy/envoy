b5a58c5f9e223805193b08608194477d847f3b45
Jose Nino <jnino@lyft.com>
2017-08-31 16:46:30 -0700

    rds: allow v1 rds config to use a v2 subscription

--- source/common/config/utility.cc
+++ source/common/config/utility.cc
@@ -14,8 +14,15 @@ namespace Config {
 namespace {
 
 void translateApiConfigSource(const std::string& cluster, uint32_t refresh_delay_ms,
+                              const std::string& api_type,
                               envoy::api::v2::ApiConfigSource& api_config_source) {
-  api_config_source.set_api_type(envoy::api::v2::ApiConfigSource::REST_LEGACY);
+  if (api_type == "REST_LEGACY") {
+    api_config_source.set_api_type(envoy::api::v2::ApiConfigSource::REST_LEGACY);
+  } else if (api_type == "REST") {
+    api_config_source.set_api_type(envoy::api::v2::ApiConfigSource::REST);
+  } else if (api_type == "GRPC") {
+    api_config_source.set_api_type(envoy::api::v2::ApiConfigSource::GRPC);
+  }
   api_config_source.add_cluster_name(cluster);
   api_config_source.mutable_refresh_delay()->CopyFrom(
       Protobuf::util::TimeUtil::MillisecondsToDuration(refresh_delay_ms));
@@ -63,6 +70,7 @@ void Utility::translateEdsConfig(const Json::Object& json_config,
                                  envoy::api::v2::ConfigSource& eds_config) {
   translateApiConfigSource(json_config.getObject("cluster")->getString("name"),
                            json_config.getInteger("refresh_delay_ms", 30000),
+                           json_config.getString("api_type", "REST_LEGACY"),
                            *eds_config.mutable_api_config_source());
 }
 
@@ -70,6 +78,7 @@ void Utility::translateCdsConfig(const Json::Object& json_config,
                                  envoy::api::v2::ConfigSource& cds_config) {
   translateApiConfigSource(json_config.getObject("cluster")->getString("name"),
                            json_config.getInteger("refresh_delay_ms", 30000),
+                           json_config.getString("api_type", "REST_LEGACY"),
                            *cds_config.mutable_api_config_source());
 }
 
@@ -77,6 +86,7 @@ void Utility::translateRdsConfig(const Json::Object& json_rds, envoy::api::v2::f
   json_rds.validateSchema(Json::Schema::RDS_CONFIGURATION_SCHEMA);
   translateApiConfigSource(json_rds.getString("cluster"),
                            json_rds.getInteger("refresh_delay_ms", 30000),
+                           json_rds.getString("api_type", "REST_LEGACY"),
                            *rds.mutable_config_source()->mutable_api_config_source());
   JSON_UTIL_SET_STRING(json_rds, rds, route_config_name);
 }
@@ -86,6 +96,7 @@ void Utility::translateLdsConfig(const Json::Object& json_lds,
   json_lds.validateSchema(Json::Schema::LDS_CONFIG_SCHEMA);
   translateApiConfigSource(json_lds.getString("cluster"),
                            json_lds.getInteger("refresh_delay_ms", 30000),
+                           json_lds.getString("api_type", "REST_LEGACY"),
                            *lds_config.mutable_api_config_source());
 }
 
