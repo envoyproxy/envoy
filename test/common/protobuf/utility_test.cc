cb17b28beb89d3b8c8190965ed0b88332cc8abb2
John Millikin <jmillikin@stripe.com>
2017-09-05 14:01:05 -0700

    Add a test for loading text-formatted bootstrap configs.

--- test/common/protobuf/utility_test.cc
+++ test/common/protobuf/utility_test.cc
@@ -8,7 +8,7 @@
 
 namespace Envoy {
 
-TEST(UtilityTest, LoadProtoFromFile) {
+TEST(UtilityTest, LoadBinaryProtoFromFile) {
   envoy::api::v2::Bootstrap bootstrap;
   bootstrap.mutable_cluster_manager()
       ->mutable_upstream_bind_config()
@@ -17,6 +17,23 @@ TEST(UtilityTest, LoadProtoFromFile) {
 
   const std::string filename =
       TestEnvironment::writeStringToFileForTest("proto.pb", bootstrap.SerializeAsString());
+
+  envoy::api::v2::Bootstrap proto_from_file;
+  MessageUtil::loadFromFile(filename, proto_from_file);
+  EXPECT_TRUE(TestUtility::protoEqual(bootstrap, proto_from_file));
+}
+
+TEST(UtilityTest, LoadTextProtoFromFile) {
+  envoy::api::v2::Bootstrap bootstrap;
+  bootstrap.mutable_cluster_manager()
+      ->mutable_upstream_bind_config()
+      ->mutable_source_address()
+      ->set_address("1.1.1.1");
+
+  std::string bootstrap_text;
+  ASSERT_TRUE(Protobuf::TextFormat::PrintToString(bootstrap, &bootstrap_text));
+  const std::string filename =
+      TestEnvironment::writeStringToFileForTest("proto.pb_text", bootstrap_text);
 
   envoy::api::v2::Bootstrap proto_from_file;
   MessageUtil::loadFromFile(filename, proto_from_file);
