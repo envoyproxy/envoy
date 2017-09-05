f89d4d9697eb45336f576a81b4f913bd31f123e4
Daniel Hochman <dhochman@lyft.com>
2017-09-05 12:20:58 -0700

    const

--- source/common/config/utility.h
+++ source/common/config/utility.h
@@ -48,9 +48,9 @@ public:
   }
 
   /**
-   * Legacy configuration uses JSON and does not have an explicit version. Hash the body and append a user-friendly prefix
+   * Legacy configuration uses JSON and does not have an explicit version. Hash the body and append a user-friendly prefix.
    */
-  static std::string computeHashedVersion(const std::string& input) {
+  static const std::string computeHashedVersion(const std::string& input) {
     return "hash_" + std::to_string(std::hash<std::string>{}(input));
   }
 
