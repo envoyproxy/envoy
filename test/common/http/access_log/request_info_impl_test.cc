94b820e424bc4d129f4ea4a0267538b5cbe1523c
Matt Rice <mrice32@users.noreply.github.com>
2017-09-05 16:36:30 -0400

    fixed formatting

--- test/common/http/access_log/request_info_impl_test.cc
+++ test/common/http/access_log/request_info_impl_test.cc
@@ -19,19 +19,26 @@ namespace {
 
 class RequestInfoTimingWrapper {
 public:
-  RequestInfoTimingWrapper() : pre_start_(std::chrono::steady_clock::now()), request_info_(Protocol::Http2), post_start_(std::chrono::steady_clock::now()) {
-  }
+  RequestInfoTimingWrapper()
+      : pre_start_(std::chrono::steady_clock::now()), request_info_(Protocol::Http2),
+        post_start_(std::chrono::steady_clock::now()) {}
 
-  void checkTimingBounds(const std::function<std::chrono::microseconds(RequestInfoImpl&)> &measure_duration, const std::string &duration_name) {
+  void checkTimingBounds(
+      const std::function<std::chrono::microseconds(RequestInfoImpl&)>& measure_duration,
+      const std::string& duration_name) {
     MonotonicTime pre_measurement = std::chrono::steady_clock::now();
     std::chrono::microseconds duration = measure_duration(request_info_);
     MonotonicTime post_measurement = std::chrono::steady_clock::now();
 
-    std::chrono::microseconds lower_bound = std::chrono::duration_cast<std::chrono::microseconds>(pre_measurement - post_start_);
-    EXPECT_LE(lower_bound, duration) << fmt::format("Duration {} was lower than expected", duration_name);
+    std::chrono::microseconds lower_bound =
+        std::chrono::duration_cast<std::chrono::microseconds>(pre_measurement - post_start_);
+    EXPECT_LE(lower_bound, duration)
+        << fmt::format("Duration {} was lower than expected", duration_name);
 
-    std::chrono::microseconds upper_bound = std::chrono::duration_cast<std::chrono::microseconds>(post_measurement - pre_start_);
-    EXPECT_GE(upper_bound, duration) << fmt::format("Duration: {} was higher than expected", duration_name);
+    std::chrono::microseconds upper_bound =
+        std::chrono::duration_cast<std::chrono::microseconds>(post_measurement - pre_start_);
+    EXPECT_GE(upper_bound, duration)
+        << fmt::format("Duration: {} was higher than expected", duration_name);
   }
 
 private:
@@ -44,19 +51,22 @@ TEST(RequestInfoImplTest, TimingTest) {
 
   RequestInfoTimingWrapper wrapper;
 
-  wrapper.checkTimingBounds([](RequestInfoImpl& request_info) {
-    request_info.requestReceivedDuration(std::chrono::steady_clock::now());
-    return request_info.requestReceivedDuration();
-  }, "request received");
+  wrapper.checkTimingBounds(
+      [](RequestInfoImpl& request_info) {
+        request_info.requestReceivedDuration(std::chrono::steady_clock::now());
+        return request_info.requestReceivedDuration();
+      },
+      "request received");
 
-  wrapper.checkTimingBounds([](RequestInfoImpl& request_info) {
-    request_info.responseReceivedDuration(std::chrono::steady_clock::now());
-    return request_info.responseReceivedDuration();
-  }, "response received");
+  wrapper.checkTimingBounds(
+      [](RequestInfoImpl& request_info) {
+        request_info.responseReceivedDuration(std::chrono::steady_clock::now());
+        return request_info.responseReceivedDuration();
+      },
+      "response received");
 
-  wrapper.checkTimingBounds([](RequestInfoImpl& request_info) {
-    return request_info.duration();
-  }, "stream duration");
+  wrapper.checkTimingBounds([](RequestInfoImpl& request_info) { return request_info.duration(); },
+                            "stream duration");
 }
 
 TEST(RequestInfoImplTest, BytesTest) {
@@ -72,16 +82,28 @@ TEST(RequestInfoImplTest, BytesTest) {
 }
 
 TEST(RequestInfoImplTest, ResponseFlagTest) {
-  std::vector<ResponseFlag> responseFlags = {FailedLocalHealthCheck, NoHealthyUpstream, UpstreamRequestTimeout, LocalReset, UpstreamRemoteReset, UpstreamConnectionFailure, UpstreamConnectionTermination, UpstreamOverflow, NoRouteFound, DelayInjected, FaultInjected, RateLimited };
+  std::vector<ResponseFlag> responseFlags = {FailedLocalHealthCheck,
+                                             NoHealthyUpstream,
+                                             UpstreamRequestTimeout,
+                                             LocalReset,
+                                             UpstreamRemoteReset,
+                                             UpstreamConnectionFailure,
+                                             UpstreamConnectionTermination,
+                                             UpstreamOverflow,
+                                             NoRouteFound,
+                                             DelayInjected,
+                                             FaultInjected,
+                                             RateLimited};
 
   RequestInfoImpl request_info(Protocol::Http2);
   for (ResponseFlag flag : responseFlags) {
     // Test cumulative setting of response flags.
-    EXPECT_FALSE(request_info.getResponseFlag(flag)) << fmt::format("Flag: {} was already set", flag);
+    EXPECT_FALSE(request_info.getResponseFlag(flag))
+        << fmt::format("Flag: {} was already set", flag);
     request_info.setResponseFlag(flag);
-    EXPECT_TRUE(request_info.getResponseFlag(flag)) << fmt::format("Flag: {} was expected to be set", flag);
+    EXPECT_TRUE(request_info.getResponseFlag(flag))
+        << fmt::format("Flag: {} was expected to be set", flag);
   }
-
 }
 
 TEST(RequestInfoImplTest, MiscSettersAndGetters) {
@@ -105,9 +127,6 @@ TEST(RequestInfoImplTest, MiscSettersAndGetters) {
   request_info.healthCheck(true);
   EXPECT_TRUE(request_info.healthCheck());
 }
-
-
-
 
 } // namespace
 } // namespace AccessLog
