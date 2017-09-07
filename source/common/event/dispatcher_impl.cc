fa32989feee7e2eada48d5fc672c85eb8f42a834
alyssawilk <alyssar@google.com>
2017-09-07 15:35:14 -0400

    Changing watermark buffer to inherit rather than wrapping an OwnedImpl (#1562)
    Changing watermark buffer to inherit rather than wrapping an OwnedImpl

--- source/common/event/dispatcher_impl.cc
+++ source/common/event/dispatcher_impl.cc
@@ -26,9 +26,9 @@ namespace Envoy {
 namespace Event {
 
 DispatcherImpl::DispatcherImpl()
-    : DispatcherImpl(Buffer::FactoryPtr{new Buffer::OwnedImplFactory}) {}
+    : DispatcherImpl(Buffer::WatermarkFactoryPtr{new Buffer::WatermarkBufferFactory}) {}
 
-DispatcherImpl::DispatcherImpl(Buffer::FactoryPtr&& factory)
+DispatcherImpl::DispatcherImpl(Buffer::WatermarkFactoryPtr&& factory)
     : buffer_factory_(std::move(factory)), base_(event_base_new()),
       deferred_delete_timer_(createTimer([this]() -> void { clearDeferredDeleteList(); })),
       post_timer_(createTimer([this]() -> void { runPostCallbacks(); })),
