0dadf6a165fefdb16bd3443a44ef7108157dceca
alyssawilk <alyssar@google.com>
2017-09-11 16:27:16 -0400

    Adding a config util for bootstrap config (#1613)
    Creating a config util for integration tests using the bootstrap proto

--- test/integration/proto_integration_test.cc
+++ test/integration/proto_integration_test.cc
@@ -24,71 +24,17 @@ public:
     // Fake upstream.
     fake_upstreams_.emplace_back(new FakeUpstream(0, FakeHttpConnection::Type::HTTP1, version_));
 
-    // Below we build up the config completely dynamically.
-    // TODO(alyssawilk): Make some of this a static config as appropriate, i.e. the bits that won't
-    // change.
+    config_helper_.setUpstreamPorts({fake_upstreams_.back()->localAddress()->ip()->port()});
 
-    // Admin access log path and address.
-    auto* admin = bootstrap_.mutable_admin();
-    admin->set_access_log_path("/dev/null");
-    auto* admin_socket_addr = admin->mutable_address()->mutable_socket_address();
-    admin_socket_addr->set_address(Network::Test::getLoopbackAddressString(GetParam()));
-    admin_socket_addr->set_port_value(0);
-
-    // HTTP/1.1 Listener with HTTP connection manager filter.
-    auto* static_resources = bootstrap_.mutable_static_resources();
-    auto* listener = static_resources->mutable_listeners()->Add();
-    listener->set_name("listener_0");
-    auto* listener_socket_addr = listener->mutable_address()->mutable_socket_address();
-    listener_socket_addr->set_address(Network::Test::getLoopbackAddressString(GetParam()));
-    listener_socket_addr->set_port_value(0);
-    auto* hcm_filter = listener->mutable_filter_chains()->Add()->mutable_filters()->Add();
-    hcm_filter->set_name("http_connection_manager");
-    envoy::api::v2::filter::HttpConnectionManager hcm_config;
-    hcm_config.set_codec_type(envoy::api::v2::filter::HttpConnectionManager::HTTP1);
-    auto* router_filter = hcm_config.mutable_http_filters()->Add();
-    router_filter->set_name("router");
-    (*router_filter->mutable_config()->mutable_fields())["deprecated_v1"].set_bool_value(true);
-    // Route configuration.
-    auto* route_config = hcm_config.mutable_route_config();
-    route_config->set_name("route_config_0");
-    auto* virtual_host = route_config->mutable_virtual_hosts()->Add();
-    virtual_host->set_name("integration");
-    virtual_host->add_domains("*");
-    auto* route = virtual_host->mutable_routes()->Add();
-    route->mutable_match()->set_prefix("/");
-    route->mutable_route()->set_cluster("cluster_0");
-    MessageUtil::jsonConvert(hcm_config, *hcm_filter->mutable_config());
-
-    // Cluster for fake upstream.
-    auto* cluster = static_resources->mutable_clusters()->Add();
-    cluster->set_name("cluster_0");
-    cluster->mutable_connect_timeout()->set_seconds(5);
-    cluster->set_type(envoy::api::v2::Cluster::STATIC);
-    cluster->set_lb_policy(envoy::api::v2::Cluster::ROUND_ROBIN);
-    auto* host_socket_addr = cluster->mutable_hosts()->Add()->mutable_socket_address();
-    host_socket_addr->set_address(Network::Test::getLoopbackAddressString(GetParam()));
-    host_socket_addr->set_port_value(fake_upstreams_.back()->localAddress()->ip()->port());
-
-    // Generate bootstrap JSON.
     const std::string bootstrap_path = TestEnvironment::writeStringToFileForTest(
-        "bootstrap.json", MessageUtil::getJsonStringFromMessage(bootstrap_));
+        "bootstrap.json", MessageUtil::getJsonStringFromMessage(config_helper_.bootstrap()));
     createGeneratedApiTestServer(bootstrap_path, {"http"});
-  }
-
-  void set_source_address(std::string address_string) {
-    bootstrap_.mutable_cluster_manager()
-        ->mutable_upstream_bind_config()
-        ->mutable_source_address()
-        ->set_address(address_string);
   }
 
   void TearDown() override {
     test_server_.reset();
     fake_upstreams_.clear();
   }
-
-  envoy::api::v2::Bootstrap bootstrap_;
 };
 
 TEST_P(ProtoIntegrationTest, TestBind) {
@@ -98,7 +44,7 @@ TEST_P(ProtoIntegrationTest, TestBind) {
   } else {
     address_string = "::1";
   }
-  set_source_address(address_string);
+  config_helper_.setSourceAddress(address_string);
   initialize();
 
   executeActions(
@@ -128,7 +74,7 @@ TEST_P(ProtoIntegrationTest, TestBind) {
 }
 
 TEST_P(ProtoIntegrationTest, TestFailedBind) {
-  set_source_address("8.8.8.8");
+  config_helper_.setSourceAddress("8.8.8.8");
   initialize();
 
   executeActions({[&]() -> void {
