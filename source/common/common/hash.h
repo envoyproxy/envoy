a76e8c45f1743df203d8e4cb2e107f432ef8fef3
Daniel Hochman <dhochman@lyft.com>
2017-09-12 13:11:44 -0700

    nits

--- source/common/common/hash.h
+++ source/common/common/hash.h
@@ -10,6 +10,7 @@ class HashUtil {
 public:
   /**
    * Return 64-bit hash with seed of 0 from the xxHash algorithm.
+   * See https://github.com/Cyan4973/xxHash for details.
    */
   static uint64_t xxHash64(const std::string& input) {
     return XXH64(input.c_str(), input.size(), 0);
