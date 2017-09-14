envoyproxy/envoy

commit b0d8095d0cd3c63667997124b9e8cee70d52f743

config: sync with recent API changes in bootstrap and TLS. (#1548)
Signed-off-by: Piotr Sikora <piotrsikora@google.com>

source/common/ssl/context_config_impl.cc
@@ -31,17 +31,16 @@ const std::string ContextConfigImpl::DEFAULT_CIPHER_SUITES =
 
 const std::string ContextConfigImpl::DEFAULT_ECDH_CURVES = "X25519:P-256";
 
-ContextConfigImpl::ContextConfigImpl(const envoy::api::v2::CommonTlsContext& config,
-                                     const envoy::api::v2::TlsCertificate& cert)
+ContextConfigImpl::ContextConfigImpl(const envoy::api::v2::CommonTlsContext& config)
     : alpn_protocols_(RepeatedPtrUtil::join(config.alpn_protocols(), ",")),
       alt_alpn_protocols_(config.deprecated_v1().alt_alpn_protocols()),
       cipher_suites_(StringUtil::nonEmptyStringOrDefault(
           RepeatedPtrUtil::join(config.tls_params().cipher_suites(), ":"), DEFAULT_CIPHER_SUITES)),
       ecdh_curves_(StringUtil::nonEmptyStringOrDefault(
           RepeatedPtrUtil::join(config.tls_params().ecdh_curves(), ":"), DEFAULT_ECDH_CURVES)),
-      ca_cert_file_(config.validation_context().ca_cert().filename()),
-      cert_chain_file_(cert.cert_chain().filename()),
-      private_key_file_(cert.private_key().filename()),
+      ca_cert_file_(config.validation_context().trusted_ca().filename()),
+      cert_chain_file_(config.tls_certificates()[0].certificate_chain().filename()),
+      private_key_file_(config.tls_certificates()[0].private_key().filename()),
       verify_subject_alt_name_list_(config.validation_context().verify_subject_alt_name().begin(),
                                     config.validation_context().verify_subject_alt_name().end()),
       verify_certificate_hash_(config.validation_context().verify_certificate_hash().empty()
@@ -49,14 +48,17 @@ ContextConfigImpl::ContextConfigImpl(const envoy::api::v2::CommonTlsContext& con
                                    : config.validation_context().verify_certificate_hash()[0]) {
   // TODO(htuch): Support multiple hashes.
   ASSERT(config.validation_context().verify_certificate_hash().size() <= 1);
+  // TODO(PiotrSikora): Support multiple TLS certificates.
+  ASSERT(config.tls_certificates().size() == 1);
   // TODO(htuch): Support inline cert material delivery.
-  ASSERT(cert.cert_chain().specifier_case() == envoy::api::v2::DataSource::kFilename);
-  ASSERT(cert.private_key().specifier_case() == envoy::api::v2::DataSource::kFilename);
+  ASSERT(config.tls_certificates()[0].certificate_chain().specifier_case() ==
+         envoy::api::v2::DataSource::kFilename);
+  ASSERT(config.tls_certificates()[0].private_key().specifier_case() ==
+         envoy::api::v2::DataSource::kFilename);
 }
 
 ClientContextConfigImpl::ClientContextConfigImpl(const envoy::api::v2::UpstreamTlsContext& config)
-    : ContextConfigImpl(config.common_tls_context(), config.client_certificate()),
-      server_name_indication_(config.sni()) {}
+    : ContextConfigImpl(config.common_tls_context()), server_name_indication_(config.sni()) {}
 
 ClientContextConfigImpl::ClientContextConfigImpl(const Json::Object& config)
     : ClientContextConfigImpl([&config] {
@@ -66,13 +68,9 @@ ClientContextConfigImpl::ClientContextConfigImpl(const Json::Object& config)
       }()) {}
 
 ServerContextConfigImpl::ServerContextConfigImpl(const envoy::api::v2::DownstreamTlsContext& config)
-    : ContextConfigImpl(config.common_tls_context(), config.tls_certificates()[0]),
+    : ContextConfigImpl(config.common_tls_context()),
       require_client_certificate_(
-          PROTOBUF_GET_WRAPPED_OR_DEFAULT(config, require_client_certificate, false)) {
-  // TODO(htuch): Handle multiple certs #1319, add constraint for now to ensure we have at least one
-  // cert #1308.
-  ASSERT(config.tls_certificates().size() == 1);
-}
+          PROTOBUF_GET_WRAPPED_OR_DEFAULT(config, require_client_certificate, false)) {}
 
 ServerContextConfigImpl::ServerContextConfigImpl(const Json::Object& config)
     : ServerContextConfigImpl([&config] {

bazel/repositories.bzl
@@ -108,7 +108,7 @@ def envoy_api_deps(skip_targets):
     native.git_repository(
         name = "envoy_api",
         remote = REPO_LOCATIONS["envoy_api"],
-        commit = "32b1173cce4d49e9f26026f503d39a1192b98428",
+        commit = "b9e89d760d4849812b43a0979aa756137563bf83",
     )
     api_bind_targets = [
         "address",

source/common/config/tls_context_json.cc
@@ -10,8 +10,6 @@ void TlsContextJson::translateDownstreamTlsContext(
     const Json::Object& json_tls_context,
     envoy::api::v2::DownstreamTlsContext& downstream_tls_context) {
   translateCommonTlsContext(json_tls_context, *downstream_tls_context.mutable_common_tls_context());
-  translateTlsCertificate(json_tls_context,
-                          *downstream_tls_context.mutable_tls_certificates()->Add());
   JSON_UTIL_SET_BOOL(json_tls_context, downstream_tls_context, require_client_certificate);
 }
 
@@ -19,7 +17,6 @@ void TlsContextJson::translateUpstreamTlsContext(
     const Json::Object& json_tls_context,
     envoy::api::v2::UpstreamTlsContext& upstream_tls_context) {
   translateCommonTlsContext(json_tls_context, *upstream_tls_context.mutable_common_tls_context());
-  translateTlsCertificate(json_tls_context, *upstream_tls_context.mutable_client_certificate());
   upstream_tls_context.set_sni(json_tls_context.getString("sni", ""));
 }
 
@@ -34,8 +31,10 @@ void TlsContextJson::translateCommonTlsContext(
   common_tls_context.mutable_deprecated_v1()->set_alt_alpn_protocols(
       json_tls_context.getString("alt_alpn_protocols", ""));
 
+  translateTlsCertificate(json_tls_context, *common_tls_context.mutable_tls_certificates()->Add());
+
   auto* validation_context = common_tls_context.mutable_validation_context();
-  validation_context->mutable_ca_cert()->set_filename(
+  validation_context->mutable_trusted_ca()->set_filename(
       json_tls_context.getString("ca_cert_file", ""));
   if (json_tls_context.hasObject("verify_certificate_hash")) {
     validation_context->add_verify_certificate_hash(
@@ -59,7 +58,7 @@ void TlsContextJson::translateCommonTlsContext(
 
 void TlsContextJson::translateTlsCertificate(const Json::Object& json_tls_context,
                                              envoy::api::v2::TlsCertificate& tls_certificate) {
-  tls_certificate.mutable_cert_chain()->set_filename(
+  tls_certificate.mutable_certificate_chain()->set_filename(
       json_tls_context.getString("cert_chain_file", ""));
   tls_certificate.mutable_private_key()->set_filename(
       json_tls_context.getString("private_key_file", ""));

source/common/ssl/context_config_impl.h
@@ -28,8 +28,7 @@ public:
   const std::string& verifyCertificateHash() const override { return verify_certificate_hash_; };
 
 protected:
-  ContextConfigImpl(const envoy::api::v2::CommonTlsContext& config,
-                    const envoy::api::v2::TlsCertificate& cert);
+  ContextConfigImpl(const envoy::api::v2::CommonTlsContext& config);
 
 private:
   static const std::string DEFAULT_CIPHER_SUITES;

source/common/upstream/cluster_manager_impl.cc
@@ -179,13 +179,13 @@ void ClusterManagerImpl::initializeClustersFromV1Json(const Json::Object& config
 }
 
 void ClusterManagerImpl::initializeClustersFromV2Proto(const envoy::api::v2::Bootstrap& bootstrap) {
-  for (const auto& cluster : bootstrap.bootstrap_clusters()) {
+  for (const auto& cluster : bootstrap.static_resources().clusters()) {
     loadCluster(cluster, false);
   }
 
   // We can now potentially create the CDS API once the backing cluster exists.
-  if (bootstrap.has_cds_config()) {
-    cds_api_ = factory_.createCds(bootstrap.cds_config(), sds_config_, *this);
+  if (bootstrap.dynamic_resources().has_cds_config()) {
+    cds_api_ = factory_.createCds(bootstrap.dynamic_resources().cds_config(), sds_config_, *this);
     init_helper_.setCds(cds_api_.get());
   } else {
     init_helper_.setCds(nullptr);
@@ -227,14 +227,13 @@ ClusterManagerImpl::ClusterManagerImpl(const Json::Object& config,
     sds_config_.value(sds_config);
   }
 
-  if (bootstrap.has_upstream_bind_config()) {
-    if (bootstrap.upstream_bind_config().has_source_address()) {
-      source_address_ = Network::Utility::fromProtoSocketAddress(
-          bootstrap.upstream_bind_config().source_address());
-    }
+  if (bootstrap.cluster_manager().upstream_bind_config().has_source_address()) {
+    source_address_ = Network::Utility::fromProtoSocketAddress(
+        bootstrap.cluster_manager().upstream_bind_config().source_address());
   }
 
-  if (bootstrap.has_cds_config() || !bootstrap.bootstrap_clusters().empty()) {
+  if (bootstrap.dynamic_resources().has_cds_config() ||
+      !bootstrap.static_resources().clusters().empty()) {
     initializeClustersFromV2Proto(bootstrap);
   } else {
     // TODO(htuch): Make this similar to the v1 -> v2 translation elsewhere,

source/server/configuration_impl.cc
@@ -52,10 +52,10 @@ void MainImpl::initialize(const Json::Object& json, const envoy::api::v2::Bootst
     server.listenerManager().addOrUpdateListener(listener);
   }
 
-  if (bootstrap.has_lds_config()) {
-    lds_api_.reset(new LdsApi(bootstrap.lds_config(), *cluster_manager_, server.dispatcher(),
-                              server.random(), server.initManager(), server.localInfo(),
-                              server.stats(), server.listenerManager()));
+  if (bootstrap.dynamic_resources().has_lds_config()) {
+    lds_api_.reset(new LdsApi(bootstrap.dynamic_resources().lds_config(), *cluster_manager_,
+                              server.dispatcher(), server.random(), server.initManager(),
+                              server.localInfo(), server.stats(), server.listenerManager()));
   } else if (json.hasObject("lds")) {
     envoy::api::v2::ConfigSource lds_config;
     Config::Utility::translateLdsConfig(*json.getObject("lds"), lds_config);

test/common/protobuf/utility_test.cc
@@ -10,7 +10,10 @@ namespace Envoy {
 
 TEST(UtilityTest, LoadProtoFromFile) {
   envoy::api::v2::Bootstrap bootstrap;
-  bootstrap.mutable_upstream_bind_config()->mutable_source_address()->set_address("1.1.1.1");
+  bootstrap.mutable_cluster_manager()
+      ->mutable_upstream_bind_config()
+      ->mutable_source_address()
+      ->set_address("1.1.1.1");
 
   const std::string filename =
       TestEnvironment::writeStringToFileForTest("proto.pb", bootstrap.SerializeAsString());

test/common/upstream/cluster_manager_impl_test.cc
@@ -428,11 +428,12 @@ TEST_F(ClusterManagerImplTest, CdsBootstrap) {
                             "document key: #/name");
 
   envoy::api::v2::Bootstrap cds_config_bootstrap;
-  cds_config_bootstrap.mutable_cds_config();
+  cds_config_bootstrap.mutable_dynamic_resources()->mutable_cds_config();
   createWithBootstrap(*loader, cds_config_bootstrap);
 
   envoy::api::v2::Bootstrap clusters_bootstrap;
-  clusters_bootstrap.mutable_bootstrap_clusters()->Add()->CopyFrom(defaultStaticCluster("foo"));
+  clusters_bootstrap.mutable_static_resources()->mutable_clusters()->Add()->CopyFrom(
+      defaultStaticCluster("foo"));
   createWithBootstrap(*loader, clusters_bootstrap);
 }
 

commit 5bd6cd1d1746be87bbd91f4f50b94f434e3d69fd

config: don't require client certificates when using TLS.
Broken in commit b0d8095d0cd3c63667997124b9e8cee70d52f743.

Fixes #1604.

Signed-off-by: Piotr Sikora <piotrsikora@google.com>

configs/google_com_proxy.v2.yaml
@@ -30,4 +30,5 @@ static_resources:
     connect_timeout: 0.25s
     type: LOGICAL_DNS
     lb_policy: ROUND_ROBIN
-    hosts: [{ socket_address: { address: google.com, port_value: 80 }}]
+    hosts: [{ socket_address: { address: google.com, port_value: 443 }}]
+    tls_context: { sni: www.google.com }

configs/google_com_proxy.yaml
@@ -28,4 +28,5 @@ cluster_manager:
     connect_timeout_ms: 250
     type: logical_dns
     lb_type: round_robin
-    hosts: [{ url: tcp://google.com:80 }]
+    hosts: [{ url: tcp://google.com:443 }]
+    ssl_context: { sni: www.google.com }

source/common/ssl/context_config_impl.cc
@@ -39,8 +39,12 @@ ContextConfigImpl::ContextConfigImpl(const envoy::api::v2::CommonTlsContext& con
       ecdh_curves_(StringUtil::nonEmptyStringOrDefault(
           RepeatedPtrUtil::join(config.tls_params().ecdh_curves(), ":"), DEFAULT_ECDH_CURVES)),
       ca_cert_file_(config.validation_context().trusted_ca().filename()),
-      cert_chain_file_(config.tls_certificates()[0].certificate_chain().filename()),
-      private_key_file_(config.tls_certificates()[0].private_key().filename()),
+      cert_chain_file_(config.tls_certificates().size()
+                           ? config.tls_certificates()[0].certificate_chain().filename()
+                           : ""),
+      private_key_file_(config.tls_certificates().size()
+                            ? config.tls_certificates()[0].private_key().filename()
+                            : ""),
       verify_subject_alt_name_list_(config.validation_context().verify_subject_alt_name().begin(),
                                     config.validation_context().verify_subject_alt_name().end()),
       verify_certificate_hash_(config.validation_context().verify_certificate_hash().empty()
@@ -48,17 +52,20 @@ ContextConfigImpl::ContextConfigImpl(const envoy::api::v2::CommonTlsContext& con
                                    : config.validation_context().verify_certificate_hash()[0]) {
   // TODO(htuch): Support multiple hashes.
   ASSERT(config.validation_context().verify_certificate_hash().size() <= 1);
-  // TODO(PiotrSikora): Support multiple TLS certificates.
-  ASSERT(config.tls_certificates().size() == 1);
-  // TODO(htuch): Support inline cert material delivery.
-  ASSERT(config.tls_certificates()[0].certificate_chain().specifier_case() ==
-         envoy::api::v2::DataSource::kFilename);
-  ASSERT(config.tls_certificates()[0].private_key().specifier_case() ==
-         envoy::api::v2::DataSource::kFilename);
+  if (config.tls_certificates().size()) {
+    // TODO(htuch): Support inline cert material delivery.
+    ASSERT(config.tls_certificates()[0].certificate_chain().specifier_case() ==
+           envoy::api::v2::DataSource::kFilename);
+    ASSERT(config.tls_certificates()[0].private_key().specifier_case() ==
+           envoy::api::v2::DataSource::kFilename);
+  }
 }
 
 ClientContextConfigImpl::ClientContextConfigImpl(const envoy::api::v2::UpstreamTlsContext& config)
-    : ContextConfigImpl(config.common_tls_context()), server_name_indication_(config.sni()) {}
+    : ContextConfigImpl(config.common_tls_context()), server_name_indication_(config.sni()) {
+  // TODO(PiotrSikora): Support multiple TLS certificates.
+  ASSERT(config.common_tls_context().tls_certificates().size() <= 1);
+}
 
 ClientContextConfigImpl::ClientContextConfigImpl(const Json::Object& config)
     : ClientContextConfigImpl([&config] {
@@ -70,7 +77,10 @@ ClientContextConfigImpl::ClientContextConfigImpl(const Json::Object& config)
 ServerContextConfigImpl::ServerContextConfigImpl(const envoy::api::v2::DownstreamTlsContext& config)
     : ContextConfigImpl(config.common_tls_context()),
       require_client_certificate_(
-          PROTOBUF_GET_WRAPPED_OR_DEFAULT(config, require_client_certificate, false)) {}
+          PROTOBUF_GET_WRAPPED_OR_DEFAULT(config, require_client_certificate, false)) {
+  // TODO(PiotrSikora): Support multiple TLS certificates.
+  ASSERT(config.common_tls_context().tls_certificates().size() == 1);
+}
 
 ServerContextConfigImpl::ServerContextConfigImpl(const Json::Object& config)
     : ServerContextConfigImpl([&config] {