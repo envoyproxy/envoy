7133af38fb3ac7a715c23b56c9de428d7fd8f239
Matt Klein <mklein@lyft.com>
2017-09-15 11:18:32 -0700

    docs: fix fav icon

--- docs/conf.py
+++ docs/conf.py
@@ -139,8 +139,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-# TODO(mattklein123): Restore this.
-#html_favicon = 'landing_source/source/favicon.ico'
+html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
