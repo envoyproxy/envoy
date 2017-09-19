8b54c5a81126562ee7ad6d8b7c0e058ba3283f3a
jwfang <jwfang@github>
2017-09-19 21:06:27 +0800

    add clang-5.0.0 from copr
    Signed-off-by: jwfang <jwfang@github>

--- ci/build_container_centos/build_container.sh
+++ ci/build_container_centos/build_container.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -e
 
+# llvm-5.0.0 from copr
+curl -o /etc/yum.repos.d/alonid-llvm-5.0.0-epel-7.repo \
+  https://copr.fedorainfracloud.org/coprs/alonid/llvm-5.0.0/repo/epel-7/alonid-llvm-5.0.0-epel-7.repo
+
 # dependencies for bazel and build_recipes
 yum install -y java-1.8.0-openjdk-devel unzip which \
-               cmake git golang libtool make patch rsync wget
+               cmake git golang libtool make patch rsync wget \
+               clang-5.0.0 devtoolset-6-libatomic-devel llvm-5.0.0 python-virtualenv
 yum clean all
 
 # latest bazel installer
@@ -12,8 +17,17 @@ BAZEL_INSTALLER=bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
 curl -OL https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${BAZEL_INSTALLER} \
   && chmod ug+x ./${BAZEL_INSTALLER} && ./${BAZEL_INSTALLER} && rm ./${BAZEL_INSTALLER}
 
-# no suitable clang-format-5.0 for centos, so we also omit buildifier/virtualenv here.
-# we can use lyft/envoy-build image for format/docs.
+# symbolic links for clang
+pushd /opt/llvm-5.0.0/bin && ln -s clang++ clang++-5.0 \
+      && ln -s /opt/rh/devtoolset-6/root/bin/ld.gold . && popd
+pushd /opt/rh/devtoolset-6/root/usr/lib64 \
+      && ln -s /opt/rh/devtoolset-6/root/usr/lib/gcc/x86_64-redhat-linux/6.3.1/libatomic.a . && popd
+mkdir -p /usr/lib/llvm-5.0/bin
+pushd /usr/lib/llvm-5.0/bin && ln -s /opt/llvm-5.0.0/bin/llvm-symbolizer . && popd
+
+# buildifier
+export GOPATH=/usr/lib/go
+go get github.com/bazelbuild/buildifier/buildifier
 
 # GCC for everything.
 export CC=gcc
