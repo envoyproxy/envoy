b3944adef4aa60e0085dfd182a8ff002c2a08c2a
jwfang <jwfang@github>
2017-09-20 11:54:59 +0800

    shell style fix
    Signed-off-by: jwfang <jwfang@github>

--- ci/build_container/docker_push.sh
+++ ci/build_container/docker_push.sh
@@ -18,19 +18,19 @@ then
     if [[ $(git diff HEAD^ ci/build_container/) ]]; then
         echo "There are changes in the ci/build_container directory"
         cd ci/build_container
-        docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+        docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
 
         for distro in ubuntu centos
         do
-            echo "Updating lyft/envoy-build-$distro image"
+            echo "Updating lyft/envoy-build-${distro} image"
             LINUX_DISTRO=$distro ./docker_build.sh
-            docker push lyft/envoy-build-$distro:$TRAVIS_COMMIT
-            docker tag lyft/envoy-build-$distro:$TRAVIS_COMMIT lyft/envoy-build-$distro:latest
-            docker push lyft/envoy-build-$distro:latest
+            docker push lyft/envoy-build-${distro}:$TRAVIS_COMMIT
+            docker tag lyft/envoy-build-${distro}:$TRAVIS_COMMIT lyft/envoy-build-${distro}:latest
+            docker push lyft/envoy-build-${distro}:latest
 
             if [ "$distro" == "ubuntu" ]
                 echo "Updating lyft/envoy-build image"
-                docker tag lyft/envoy-build-$distro:$TRAVIS_COMMIT lyft/envoy-build:$TRAVIS_COMMIT
+                docker tag lyft/envoy-build-${distro}:$TRAVIS_COMMIT lyft/envoy-build:$TRAVIS_COMMIT
                 docker push lyft/envoy-build:$TRAVIS_COMMIT
                 docker tag lyft/envoy-build:$TRAVIS_COMMIT lyft/envoy-build:latest
                 docker push lyft/envoy-build:latest
