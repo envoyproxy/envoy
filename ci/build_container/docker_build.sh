b7795b589960fb45753612909cf0ca2a27c434ba
James Fang <54740235@qq.com>
2017-09-22 11:32:28 +0800

    fix ci LINUX_DISTRO variable name
    Signed-off-by: James Fang <54740235@qq.com>

--- ci/build_container/docker_build.sh
+++ ci/build_container/docker_build.sh
@@ -4,4 +4,4 @@
 # relative path workaround from https://github.com/docker/docker/issues/2745#issuecomment-253230025
 # to get this to work.
 tar cf - . -C ../../bazel target_recipes.bzl \
-  | docker build -f Dockerfile-${LINUX_DISTO} -t lyft/envoy-build-${LINUX_DISTO}:$TRAVIS_COMMIT -
+  | docker build -f Dockerfile-${LINUX_DISTRO} -t lyft/envoy-build-${LINUX_DISTRO}:$TRAVIS_COMMIT -
