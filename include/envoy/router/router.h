a0ef4e7b681d4e251008d56cc23ffdda58989fe3
Alex Konradi <akonradi@google.com>
2017-09-26 11:21:42 -0400

    Fix nits in HashPolicy::generateHash docs
    Signed-off-by: Alex Konradi <akonradi@google.com>

--- include/envoy/router/router.h
+++ include/envoy/router/router.h
@@ -216,11 +216,11 @@ public:
   virtual ~HashPolicy() {}
 
   /**
-   * @return Optional<uint64_t> an optional hash value to route on.
    * @param downstream_address contains the address of the connected client host, or an
-   * empty string if the request is initiated from within this host, and
+   * empty string if the request is initiated from within this host
    * @param headers stores the HTTP headers for the stream
-   * A hash value might not be returned if for example the specified HTTP header does not exist.
+   * @return Optional<uint64_t> an optional hash value to route on. A hash value might not be
+   * returned if for example the specified HTTP header does not exist.
    */
   virtual Optional<uint64_t> generateHash(const std::string& downstream_address,
                                           const Http::HeaderMap& headers) const PURE;
