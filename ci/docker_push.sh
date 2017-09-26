9d38cc461f03faac153f4a26585b690865cfabb5
Matt Klein <mklein@lyft.com>
2017-09-26 11:17:01 -0700

    fix
    Signed-off-by: Matt Klein <mklein@lyft.com>

--- ci/docker_push.sh
+++ ci/docker_push.sh
@@ -21,8 +21,8 @@ then
    # server also so we have to temporarily install docker here.
    # https://circleci.com/docs/2.0/building-docker-images/
    VER="17.03.0-ce"
-   curl -L -o /tmp/docker-$VER.tgz https://get.docker.com/builds/Linux/x86_64/docker-$VER.tgz
-   tar -xz -C /tmp -f /tmp/docker-$VER.tgz
+   curl -L -o /tmp/docker-"$VER".tgz https://get.docker.com/builds/Linux/x86_64/docker-"$VER".tgz
+   tar -xz -C /tmp -f /tmp/docker-"$VER".tgz
    mv /tmp/docker/* /usr/bin
 
    docker build -f ci/Dockerfile-envoy-image -t lyft/envoy:latest .
