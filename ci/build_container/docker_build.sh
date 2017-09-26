484a9e04ad6190f80a18de1e4d15146e777e6f46
jwfang <54740235@qq.com>
2017-09-26 22:55:44 +0800

    update ci/README for centos (#1731)
    Signed-off-by: James Fang <54740235@qq.com>

--- ci/build_container/docker_build.sh
+++ ci/build_container/docker_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[[ -z "${LINUX_DISTRO}" ]] && LINUX_DISTRO="ubuntu"
+
 # We need target_recipes.bzl for the build, but it's not in ci/build_container. Using Docker
 # relative path workaround from https://github.com/docker/docker/issues/2745#issuecomment-253230025
 # to get this to work.
