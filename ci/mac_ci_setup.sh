fe00e48ff7be71fdcbfaa1d23753470121215719
Stephan Zuercher <zuercher@gmail.com>
2017-09-26 18:29:05 -0700

    mac-ci: add a CI workflow for Mac OS builds (#1748)
    Signed-off-by: Stephan Zuercher <stephan@turbinelabs.io>

---
+++ ci/mac_ci_setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Installs the dependencies required for a Mac OS build via homebrew.
+# Tools are not upgraded to new versions.
+
+function is_installed {
+    brew ls --versions "$1" >/dev/null
+}
+
+function install {
+    echo "Installing $1"
+    if ! brew install "$1"
+    then
+        echo "Failed to install $1"
+        exit 1
+    fi
+}
+
+DEPS="coreutils wget cmake libtool go bazel"
+for DEP in ${DEPS}
+do
+    is_installed "${DEP}" || install "${DEP}"
+done
+
+if [ -n "$CIRCLECI" ]; then
+    # bazel uses jgit internally and the default circle-ci .gitconfig says to
+    # convert https://github.com to ssh://git@github.com, which jgit does not support.
+    mv ~/.gitconfig ~/.gitconfig_save
+
+    # Configure bazelrc for better output in mac/circle builds
+    echo >~/.bazelrc <<EOF
+build --curses=no --show_task_finish
+test --curses=no --show_task_finish
+EOF
+fi
