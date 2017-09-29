b2d9bc2cfb594cbd017bda54cc7bedc1e2009ae3
Alex Konradi <akonradi@google.com>
2017-09-29 10:54:48 -0400

    Add back accidentally removed const
    Signed-off-by: Alex Konradi <akonradi@google.com>

--- source/common/router/config_impl.cc
+++ source/common/router/config_impl.cc
@@ -155,11 +155,11 @@ Optional<uint64_t> HashPolicyImpl::generateHash(const std::string& downstream_ad
                                                 const AddCookieCallback add_cookie) const {
   Optional<uint64_t> hash;
   for (const HashMethodPtr& hash_impl : hash_impls_) {
-    Optional<uint64_t> new_hash = hash_impl->evaluate(downstream_addr, headers, add_cookie);
+    const Optional<uint64_t> new_hash = hash_impl->evaluate(downstream_addr, headers, add_cookie);
     if (new_hash.valid()) {
       // Rotating the old value prevents duplicate hash rules from cancelling each other out
       // and preserves all of the entropy
-      uint64_t old_value = hash.valid() ? ((hash.value() << 1) | (hash.value() >> 63)) : 0;
+      const uint64_t old_value = hash.valid() ? ((hash.value() << 1) | (hash.value() >> 63)) : 0;
       hash.value(old_value ^ new_hash.value());
     }
   }
