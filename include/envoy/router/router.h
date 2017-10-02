5ec91d97111b73885942fa6caad3d97d77a74f58
Stephan Zuercher <stephan@turbinelabs.io>
2017-10-02 14:41:27 -0700

    router: thread metadata_matches through to RouteEntryImplBase
    Signed-off-by: Stephan Zuercher <stephan@turbinelabs.io>

--- include/envoy/router/router.h
+++ include/envoy/router/router.h
@@ -15,6 +15,9 @@
 #include "envoy/tracing/http_tracer.h"
 #include "envoy/upstream/resource_manager.h"
 
+#include "common/protobuf/protobuf.h"
+#include "common/protobuf/utility.h"
+
 namespace Envoy {
 namespace Router {
 
@@ -226,6 +229,35 @@ public:
                                           const Http::HeaderMap& headers) const PURE;
 };
 
+class MetadataMatch {
+public:
+  virtual ~MetadataMatch() {}
+
+  /*
+   * @return const std::string& the name of the metadata key
+   */
+  virtual const std::string& name() const PURE;
+
+  /*
+   * @return const Envoy::HashedValue& the value for the metadata key
+   */
+  virtual const HashedValue& value() const PURE;
+};
+
+typedef std::shared_ptr<const MetadataMatch> MetadataMatchConstSharedPtr;
+
+class MetadataMatches {
+public:
+  virtual ~MetadataMatches() {}
+
+  /*
+   * @return std::vector<MetadataMatchConstSharedPtr>& a vector of
+   * metadata to be matched against upstream endpoints when load
+   * balancing, sorted lexically by name.
+   */
+  virtual const std::vector<MetadataMatchConstSharedPtr>& metadataMatches() const PURE;
+};
+
 /**
  * An individual resolved route entry.
  */
@@ -306,6 +338,12 @@ public:
    * @return bool true if this route should use WebSockets.
    */
   virtual bool useWebSocket() const PURE;
+
+  /**
+   * @return MetadataMatches* the metadata that a subset load balancer should match when selecting
+   * an upstream host
+   */
+  virtual const MetadataMatches* metadataMatches() const PURE;
 
   /**
    * @return const std::multimap<std::string, std::string> the opaque configuration associated
