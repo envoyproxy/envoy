80c4d17d97ff4764a69c69e55ef59f5293f3d260
Stephan Zuercher <stephan@turbinelabs.io>
2017-10-02 15:20:38 -0700

    protobuf: test new protobuf utilities
    Signed-off-by: Stephan Zuercher <stephan@turbinelabs.io>

--- test/common/protobuf/utility_test.cc
+++ test/common/protobuf/utility_test.cc
@@ -1,3 +1,5 @@
+#include <unordered_set>
+
 #include "common/protobuf/protobuf.h"
 #include "common/protobuf/utility.h"
 
@@ -49,6 +51,130 @@ TEST(UtilityTest, LoadTextProtoFromFile_Failure) {
   EXPECT_THROW_WITH_MESSAGE(MessageUtil::loadFromFile(filename, proto_from_file), EnvoyException,
                             "Unable to parse file \"" + filename +
                                 "\" as a text protobuf (type envoy.api.v2.Bootstrap)");
+}
+
+TEST(UtilityTest, ValueUtilEqual_NullValues) {
+  ProtobufWkt::Value v1, v2;
+  v1.set_null_value(ProtobufWkt::NULL_VALUE);
+  v2.set_null_value(ProtobufWkt::NULL_VALUE);
+
+  ProtobufWkt::Value other;
+  other.set_string_value("s");
+
+  EXPECT_TRUE(ValueUtil::equal(v1, v2));
+  EXPECT_FALSE(ValueUtil::equal(v1, other));
+}
+
+TEST(UtilityTest, ValueUtilEqual_StringValues) {
+  ProtobufWkt::Value v1, v2, v3;
+  v1.set_string_value("s");
+  v2.set_string_value("s");
+  v3.set_string_value("not_s");
+
+  EXPECT_TRUE(ValueUtil::equal(v1, v2));
+  EXPECT_FALSE(ValueUtil::equal(v1, v3));
+}
+
+TEST(UtilityTest, ValueUtilEqual_NumberValues) {
+  ProtobufWkt::Value v1, v2, v3;
+  v1.set_number_value(1.0);
+  v2.set_number_value(1.0);
+  v3.set_number_value(100.0);
+
+  EXPECT_TRUE(ValueUtil::equal(v1, v2));
+  EXPECT_FALSE(ValueUtil::equal(v1, v3));
+}
+
+TEST(UtilityTest, ValueUtilEqual_BoolValues) {
+  ProtobufWkt::Value v1, v2, v3;
+  v1.set_bool_value(true);
+  v2.set_bool_value(true);
+  v3.set_bool_value(false);
+
+  EXPECT_TRUE(ValueUtil::equal(v1, v2));
+  EXPECT_FALSE(ValueUtil::equal(v1, v3));
+}
+
+TEST(UtilityTest, ValueUtilEqual_StructValues) {
+  ProtobufWkt::Value string_val1, string_val2, bool_val;
+
+  string_val1.set_string_value("s1");
+  string_val2.set_string_value("s2");
+  bool_val.set_bool_value(true);
+
+  ProtobufWkt::Value v1, v2, v3, v4;
+  v1.mutable_struct_value()->mutable_fields()->insert({"f1", string_val1});
+  v1.mutable_struct_value()->mutable_fields()->insert({"f2", bool_val});
+
+  v2.mutable_struct_value()->mutable_fields()->insert({"f1", string_val1});
+  v2.mutable_struct_value()->mutable_fields()->insert({"f2", bool_val});
+
+  v3.mutable_struct_value()->mutable_fields()->insert({"f1", string_val2});
+  v3.mutable_struct_value()->mutable_fields()->insert({"f2", bool_val});
+
+  v4.mutable_struct_value()->mutable_fields()->insert({"f1", string_val1});
+
+  EXPECT_TRUE(ValueUtil::equal(v1, v2));
+  EXPECT_FALSE(ValueUtil::equal(v1, v3));
+  EXPECT_FALSE(ValueUtil::equal(v1, v4));
+}
+
+TEST(UtilityTest, ValueUtilEqual_ListValues) {
+  ProtobufWkt::Value v1, v2, v3, v4;
+  v1.mutable_list_value()->add_values()->set_string_value("s");
+  v1.mutable_list_value()->add_values()->set_bool_value(true);
+
+  v2.mutable_list_value()->add_values()->set_string_value("s");
+  v2.mutable_list_value()->add_values()->set_bool_value(true);
+
+  v3.mutable_list_value()->add_values()->set_bool_value(true);
+  v3.mutable_list_value()->add_values()->set_string_value("s");
+
+  v4.mutable_list_value()->add_values()->set_string_value("s");
+
+  EXPECT_TRUE(ValueUtil::equal(v1, v2));
+  EXPECT_FALSE(ValueUtil::equal(v1, v3));
+  EXPECT_FALSE(ValueUtil::equal(v1, v4));
+}
+
+TEST(UtilityTest, ValueUtilHash) {
+  ProtobufWkt::Value v;
+  v.set_string_value("s1");
+
+  EXPECT_NE(ValueUtil::hash(v), 0);
+}
+
+TEST(UtilityTest, HashedValue) {
+  ProtobufWkt::Value v1, v2, v3;
+  v1.set_string_value("s");
+  v2.set_string_value("s");
+  v3.set_string_value("not_s");
+
+  HashedValue hv1(v1), hv2(v2), hv3(v3);
+
+  EXPECT_EQ(hv1, hv2);
+  EXPECT_NE(hv1, hv3);
+
+  HashedValue copy(hv1);
+  EXPECT_EQ(hv1, copy);
+}
+
+TEST(UtilityTest, HashedValueStdHash) {
+  ProtobufWkt::Value v1, v2, v3;
+  v1.set_string_value("s");
+  v2.set_string_value("s");
+  v3.set_string_value("not_s");
+
+  HashedValue hv1(v1), hv2(v2), hv3(v3);
+
+  std::unordered_set<HashedValue> set;
+  set.emplace(hv1);
+  set.emplace(hv2);
+  set.emplace(hv3);
+
+  EXPECT_EQ(set.size(), 2); // hv1 == hv2
+  EXPECT_NE(set.find(hv1), set.end());
+  EXPECT_NE(set.find(hv3), set.end());
 }
 
 } // namespace Envoy
