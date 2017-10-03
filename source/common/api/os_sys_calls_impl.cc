3e5967f25e309c60e598e5b6387c7a4e89d370f7
Greg Greenway <ggreenway@users.noreply.github.com>
2017-10-02 17:09:25 -0700

    Fix an allocator bug in hot_restart_impl, and add a test (#1788)
    Fix hot_restart_impl allocator in the case when
    there are unused entries followed by used entries, and
    the requested stat name already exists.
    
    Signed-off-by: Greg Greenway <ggreenway@apple.com>

--- source/common/api/os_sys_calls_impl.cc
+++ source/common/api/os_sys_calls_impl.cc
@@ -17,5 +17,17 @@ ssize_t OsSysCallsImpl::write(int fd, const void* buffer, size_t num_bytes) {
   return ::write(fd, buffer, num_bytes);
 }
 
+int OsSysCallsImpl::shmOpen(const char* name, int oflag, mode_t mode) {
+  return ::shm_open(name, oflag, mode);
+}
+
+int OsSysCallsImpl::shmUnlink(const char* name) { return ::shm_unlink(name); }
+
+int OsSysCallsImpl::ftruncate(int fd, off_t length) { return ::ftruncate(fd, length); }
+
+void* OsSysCallsImpl::mmap(void* addr, size_t length, int prot, int flags, int fd, off_t offset) {
+  return ::mmap(addr, length, prot, flags, fd, offset);
+}
+
 } // namespace Api
 } // namespace Envoy
