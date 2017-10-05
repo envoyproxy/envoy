a75d7490f4c0c3feae21c7fdaf69350b44bd7cdd
Matt Rice <mattrice@google.com>
2017-10-05 18:25:09 -0400

    Corrected a few nits in Timespan and made methods non-virtual
    Signed-off-by: Matt Rice <mattrice@google.com>

--- include/envoy/stats/timespan.h
+++ include/envoy/stats/timespan.h
@@ -9,26 +9,24 @@ namespace Envoy {
 namespace Stats {
 
 /**
- * An individual timespan that flushes its measured value to a Duration histogram. The initial time
- * is captured on construction. A timespan must be completed via complete() for it to be stored. If
- * the timespan is deleted this will be treated as a cancellation.
+ * An individual timespan that flushes its measured value (in milliseconds) to a histogram. The
+ * initial time is captured on construction. A timespan must be completed via complete() for it to
+ * be stored. If the timespan is deleted this will be treated as a cancellation.
  */
 class Timespan {
 public:
   Timespan(Histogram& histogram)
       : histogram_(histogram), start_(std::chrono::steady_clock::now()) {}
 
-  virtual ~Timespan() {}
-
   /**
    * Complete the timespan and send the time to the histogram.
    */
-  virtual void complete() { histogram_.recordValue(getRawDuration().count()); }
+  void complete() { histogram_.recordValue(getRawDuration().count()); }
 
   /**
    * Get duration since the creation of the span.
    */
-  virtual std::chrono::milliseconds getRawDuration() {
+  std::chrono::milliseconds getRawDuration() {
     return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() -
                                                                  start_);
   }
