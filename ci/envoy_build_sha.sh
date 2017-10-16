bb96b403ab9197896e928ee4cbe1b2769d944d02
John Millikin <jmillikin@stripe.com>
2017-10-16 13:10:04 -0700

    Update CI build image to 42485dd6111408d43a911a5b1126316ef3151798.
    Signed-off-by: John Millikin <jmillikin@stripe.com>

--- ci/envoy_build_sha.sh
+++ ci/envoy_build_sha.sh
@@ -1,3 +1,3 @@
 # If you edit the SHA here you must also edit the SHA in .circleci/config.yml.
 
-ENVOY_BUILD_SHA=8839608c2f4b0d7c8311361ee5c8785e543d517a
+ENVOY_BUILD_SHA=42485dd6111408d43a911a5b1126316ef3151798
