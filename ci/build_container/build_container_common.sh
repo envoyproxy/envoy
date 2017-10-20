envoyproxy/envoy

commit 099083043e0aa5c11900104ce51215f60e451d8d

Start moving dependencies into their own Bazel externals. (#1682)
The eventual goal here is to have each dependency in its own @thing, so
Bazel can download and build them in parallel. This should also simplify
the build, because some dependencies (protobuf, boringssl) have their own
Bazel configs.

Reason these particular four deps were chosen as a starting point:

* `fmtlib` and `spdlog` are header-only libraries used by every
  `envoy_cc_library` target. Moving them out of the deps blob is trivial,
  and decouples most of the small libraries from the other huge deps.

* `protobuf` has its own BUILD file, which we should use.

* `lightstep` depends on `protobuf`, and therefore must also be moved
  out. This was relatively straightforward, by copying the existing
  "build recipe" shell script into a genrule.

I'm hopeful that we can eventually move these dependencies into
https://github.com/bazelbuild/bazel-packages when that project begins
accepting contributions.

Signed-off-by: John Millikin <jmillikin@stripe.com>

ci/build_container/build_container_common.sh
@@ -15,7 +15,7 @@ fi
 
 export THIRDPARTY_DEPS=/tmp
 export THIRDPARTY_SRC=/thirdparty
-DEPS=$(python <(cat target_recipes.bzl; \
+DEPS=$(python <(cat /bazel-prebuilt/bazel/target_recipes.bzl; \
   echo "print ' '.join(\"${THIRDPARTY_DEPS}/%s.dep\" % r for r in set(TARGET_RECIPES.values()))"))
 
 # TODO(htuch): We build twice as a workaround for https://github.com/google/protobuf/issues/3322.
@@ -24,3 +24,10 @@ export THIRDPARTY_BUILD=/thirdparty_build
 export CPPFLAGS="-DNDEBUG"
 echo "Building opt deps ${DEPS}"
 "$(dirname "$0")"/build_and_install_deps.sh ${DEPS}
+
+echo "Building Bazel-managed deps (//bazel/external:all_external)"
+BAZEL_OPTIONS="--output_user_root=/bazel-prebuilt-root --output_base=/bazel-prebuilt-output"
+cd /bazel-prebuilt
+for BAZEL_MODE in opt dbg fastbuild; do
+  bazel ${BAZEL_OPTIONS} build -c "${BAZEL_MODE}" //bazel/external:all_external
+done

bazel/external/BUILD
@@ -1,0 +1,15 @@
+# Use a wrapper cc_library with an empty source source file to force
+# compilation of other cc_library targets that only list *.a sources.
+cc_library(
+    name = "all_external",
+    srcs = [":empty.cc"],
+    deps = [
+        "@com_github_lightstep_lightstep_tracer_cpp//:lightstep",
+    ],
+)
+
+genrule(
+    name = "empty_cc",
+    outs = ["empty.cc"],
+    cmd = "touch \"$(@D)/empty.cc\"",
+)

bazel/external/fmtlib.BUILD
@@ -1,0 +1,10 @@
+cc_library(
+    name = "fmtlib",
+    hdrs = glob([
+        "fmt/*.cc",
+        "fmt/*.h",
+    ]),
+    defines = ["FMT_HEADER_ONLY"],
+    includes = ["."],
+    visibility = ["//visibility:public"],
+)

bazel/external/lightstep-missing-header.patch
@@ -1,0 +1,12 @@
+see https://github.com/envoyproxy/envoy/issues/1387 for progress
+
+--- ./src/c++11/lightstep/options.h.bak        2017-08-04 09:30:19.527076744 -0400
++++ ./src/c++11/lightstep/options.h    2017-08-04 09:30:33.742106924 -0400
+@@ -5,6 +5,7 @@
+ // Options for Tracer implementations, starting Spans, and finishing
+ // Spans.
+ 
++#include <functional>
+ #include <chrono>
+ #include <memory>
+ 

bazel/external/lightstep.BUILD
@@ -1,0 +1,50 @@
+load("@envoy//bazel:genrule_repository.bzl", "genrule_cc_deps", "genrule_environment")
+load(":genrule_cmd.bzl", "genrule_cmd")
+
+cc_library(
+    name = "lightstep",
+    srcs = [":_prefix/lib/liblightstep_core_cxx11.a"],
+    hdrs = glob([
+        "src/c++11/lightstep/**/*.h",
+        "src/c++11/mapbox_variant/**/*.hpp",
+    ]) + [
+        ":_prefix/include/collector.pb.h",
+        ":_prefix/include/lightstep_carrier.pb.h",
+    ],
+    includes = [
+        "_prefix/include",
+        "src/c++11",
+    ],
+    visibility = ["//visibility:public"],
+    deps = ["@protobuf_bzl//:protobuf"],
+)
+
+genrule_environment(
+    name = "lightstep_compiler_flags",
+)
+
+# This intermediate rule lets cc_library outputs be fed into a genrule.
+# Normally a cc_library creates a CcSkylarkApiProvider, but no direct
+# file outputs.
+genrule_cc_deps(
+    name = "protobuf_deps",
+    deps = ["@protobuf_bzl//:protobuf"],
+)
+
+genrule(
+    name = "install",
+    srcs = glob(["**"]) + [
+        ":lightstep_compiler_flags",
+        ":protobuf_deps",
+        "@protobuf_bzl//:well_known_protos",
+    ],
+    outs = [
+        "_prefix/lib/liblightstep_core_cxx11.a",
+        "_prefix/include/collector.pb.h",
+        "_prefix/include/lightstep_carrier.pb.h",
+    ],
+    cmd = genrule_cmd("@envoy//bazel/external:lightstep.genrule_cmd"),
+    tools = [
+        "@protobuf_bzl//:protoc",
+    ],
+)

bazel/external/lightstep.genrule_cmd
@@ -1,0 +1,23 @@
+# This is a Bazel genrule script. It acts like Bash, but has some extra
+# substitutions for locating dependencies.
+#
+# https://docs.bazel.build/versions/master/be/general.html#genrule
+
+cat "$(location :lightstep_compiler_flags)"
+source "$(location :lightstep_compiler_flags)"
+
+CONFIGURE="$${PWD}/$(location :configure)"
+PROTOC="$${PWD}/$(location @protobuf_bzl//:protoc)"
+PROTOBUF_SRC="$${PWD}/external/protobuf_bzl/src/"
+PROTOBUF_LIBDIR="$${PWD}/$(BINDIR)/external/protobuf_bzl/"
+mkdir "$(@D)/build"
+cd "$(@D)/build"
+"$${CONFIGURE}" \
+  --prefix="$${PWD}/../_prefix" \
+  --disable-grpc \
+  --enable-shared=no \
+  protobuf_CFLAGS="-I$${PROTOBUF_SRC}" \
+  protobuf_LIBS="-L$${PROTOBUF_LIBDIR} -lprotobuf -lprotobuf_lite" \
+  PKG_CONFIG="true" \
+  PROTOC="$${PROTOC} -I$${PROTOBUF_SRC}"
+make V=1 install

bazel/external/protobuf-memory-errors.patch
@@ -1,0 +1,65 @@
+Backport of https://github.com/google/protobuf/pull/3760 to fix undefined
+memory management behavior in Protobuf.
+
+--- ./src/google/protobuf/descriptor.cc
++++ ./src/google/protobuf/descriptor.cc
+@@ -4309,8 +4309,10 @@ FileDescriptor* DescriptorBuilder::BuildFileImpl(
+     result->dependencies_once_ = tables_->AllocateOnceDynamic();
+     result->dependencies_names_ =
+         tables_->AllocateArray<const string*>(proto.dependency_size());
+-    memset(result->dependencies_names_, 0,
+-           sizeof(*result->dependencies_names_) * proto.dependency_size());
++    if (proto.dependency_size() > 0) {
++      memset(result->dependencies_names_, 0,
++             sizeof(*result->dependencies_names_) * proto.dependency_size());
++    }
+   } else {
+     result->dependencies_once_ = NULL;
+     result->dependencies_names_ = NULL;
+--- ./src/google/protobuf/text_format.cc
++++ ./src/google/protobuf/text_format.cc
+@@ -1244,10 +1244,12 @@ class TextFormat::Printer::TextGenerator
+     while (size > buffer_size_) {
+       // Data exceeds space in the buffer.  Copy what we can and request a
+       // new buffer.
+-      memcpy(buffer_, data, buffer_size_);
+-      data += buffer_size_;
+-      size -= buffer_size_;
+-      void* void_buffer;
++      if (buffer_size_ > 0) {
++        memcpy(buffer_, data, buffer_size_);
++        data += buffer_size_;
++        size -= buffer_size_;
++      }
++      void* void_buffer = nullptr;
+       failed_ = !output_->Next(&void_buffer, &buffer_size_);
+       if (failed_) return;
+       buffer_ = reinterpret_cast<char*>(void_buffer);
+--- ./src/google/protobuf/util/json_util.cc
++++ ./src/google/protobuf/util/json_util.cc
+@@ -61,9 +61,11 @@ void ZeroCopyStreamByteSink::Append(const char* bytes, size_t len) {
+       buffer_size_ -= len;
+       return;
+     }
+-    memcpy(buffer_, bytes, buffer_size_);
+-    bytes += buffer_size_;
+-    len -= buffer_size_;
++    if (buffer_size_ > 0) {
++      memcpy(buffer_, bytes, buffer_size_);
++      bytes += buffer_size_;
++      len -= buffer_size_;
++    }
+     if (!stream_->Next(&buffer_, &buffer_size_)) {
+       // There isn't a way for ByteSink to report errors.
+       buffer_size_ = 0;
+--- ./src/google/protobuf/util/json_util.h
++++ ./src/google/protobuf/util/json_util.h
+@@ -179,7 +179,7 @@ namespace internal {
+ class LIBPROTOBUF_EXPORT ZeroCopyStreamByteSink : public strings::ByteSink {
+  public:
+   explicit ZeroCopyStreamByteSink(io::ZeroCopyOutputStream* stream)
+-      : stream_(stream), buffer_size_(0) {}
++      : stream_(stream), buffer_(nullptr), buffer_size_(0) {}
+   ~ZeroCopyStreamByteSink();
+ 
+   virtual void Append(const char* bytes, size_t len);

bazel/external/spdlog.BUILD
@@ -1,0 +1,11 @@
+cc_library(
+    name = "spdlog",
+    hdrs = glob([
+        "include/**/*.cc",
+        "include/**/*.h",
+    ]),
+    defines = ["SPDLOG_FMT_EXTERNAL"],
+    includes = ["include"],
+    visibility = ["//visibility:public"],
+    deps = ["@com_github_fmtlib_fmt//:fmtlib"],
+)

commit b72e5efaa0ac50f526aae898a83d0e12ffde0026

build_conainer: non-root base image fix.
Part of a fix for non-root invocation of do_ci.sh for regression
introduced in #1682.

Signed-off-by: Harvey Tuch <htuch@google.com>

ci/build_container/build_container_common.sh
@@ -32,3 +32,5 @@ cd /bazel-prebuilt
 for BAZEL_MODE in opt dbg fastbuild; do
   bazel ${BAZEL_OPTIONS} build -c "${BAZEL_MODE}" //bazel/external:all_external
 done
+# Allow access by non-root for building.
+chmod -R a+rX /bazel-prebuilt-root /bazel-prebuilt-output