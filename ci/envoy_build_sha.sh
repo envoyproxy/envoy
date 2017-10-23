27c4334747b57efe4eef4065320e435aa3755910
Harvey Tuch <htuch@google.com>
2017-10-23 17:03:40 -0400

    Validate hashes are consistent.
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- ci/envoy_build_sha.sh
+++ ci/envoy_build_sha.sh
@@ -1,1 +1,2 @@
-ENVOY_BUILD_SHA=$(grep lyft/envoy-build .circleci/config.yml | sed -e 's#.*lyft/envoy-build:\(.*\)#\1#' | head -n 1)
+ENVOY_BUILD_SHA=$(grep lyft/envoy-build .circleci/config.yml | sed -e 's#.*lyft/envoy-build:\(.*\)#\1#' | uniq)
+[[ $(wc -l <<< "${ENVOY_BUILD_SHA}") == 1 ]] || (echo ".circleci/config.yml hashes are inconsistent!" && exit 1)
