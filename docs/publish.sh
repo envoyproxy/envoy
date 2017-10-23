c958adbc94e12c280fdf78023c57abbd16ca9f53
Matt Klein <mklein@lyft.com>
2017-10-23 09:00:57 -0700

    docs: fix publish take 2 (#1921)
    Signed-off-by: Matt Klein <mklein@lyft.com>

--- docs/publish.sh
+++ docs/publish.sh
@@ -3,20 +3,21 @@
 set -e
 
 DOCS_DIR=generated/docs
-PUBLISH_DIR=../envoy-docs
+CHECKOUT_DIR=../envoy-docs
+PUBLISH_DIR="$CHECKOUT_DIR"/envoy
 BUILD_SHA=`git rev-parse HEAD`
 
 if [ -z "$CIRCLE_PULL_REQUEST" ] && [ "$CIRCLE_BRANCH" == "master" ]
 then
   echo 'cloning'
-  git clone git@github.com:envoyproxy/envoyproxy.github.io $PUBLISH_DIR
+  git clone git@github.com:envoyproxy/envoyproxy.github.io "$CHECKOUT_DIR"
 
-  git -C $PUBLISH_DIR fetch
-  git -C $PUBLISH_DIR checkout -B master origin/master
-  rm -fr $PUBLISH_DIR/envoy/*
-  mkdir -p $PUBLISH_DIR/envoy
-  cp -r $DOCS_DIR/envoy/* $PUBLISH_DIR
-  cd $PUBLISH_DIR
+  git -C "$CHECKOUT_DIR" fetch
+  git -C "$CHECKOUT_DIR" checkout -B master origin/master
+  rm -fr "$PUBLISH_DIR"
+  mkdir -p "$PUBLISH_DIR"
+  cp -r "$DOCS_DIR"/* "$PUBLISH_DIR"
+  cd "$CHECKOUT_DIR"
 
   git config user.name "envoy-docs(travis)"
   git config user.email envoy-docs@users.noreply.github.com
