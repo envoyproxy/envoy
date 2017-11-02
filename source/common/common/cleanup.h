8b1a821a75c1537c3a2076732424d1f23264f6ae
Harvey Tuch <htuch@google.com>
2017-11-02 14:08:24 -0400

    Add missing cleanup.h.
    Signed-off-by: Harvey Tuch <htuch@google.com>

---
+++ source/common/common/cleanup.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace Envoy {
+
+// RAII cleanup via functor.
+class Cleanup {
+public:
+  Cleanup(std::function<void()> f) : f_(std::move(f)) {}
+  ~Cleanup() { f_(); }
+
+private:
+  std::function<void()> f_;
+};
+
+} // namespace Envoy
