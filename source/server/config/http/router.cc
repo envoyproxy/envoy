11817d7e93df58ee8b93ff3f605728a0f0fa47ed
Shriram Rajagopalan <shriram@us.ibm.com>
2017-11-03 16:28:24 -0400

    merge prep
    Signed-off-by: Shriram Rajagopalan <shriram@us.ibm.com>

--- source/server/config/http/router.cc
+++ source/server/config/http/router.cc
@@ -5,7 +5,6 @@
 #include "envoy/registry/registry.h"
 
 #include "common/json/config_schemas.h"
-#include "common/protobuf/utility.h"
 #include "common/router/router.h"
 #include "common/router/shadow_writer_impl.h"
 
@@ -13,37 +12,21 @@ namespace Envoy {
 namespace Server {
 namespace Configuration {
 
-HttpFilterFactoryCb RouterFilterConfig::createRouterFilter(bool dynamic_stats,
-                                                           bool start_child_span,
-                                                           const std::string& stat_prefix,
-                                                           FactoryContext& context) {
-  Router::FilterConfigSharedPtr config(new Router::FilterConfig(
-      stat_prefix, context.localInfo(), context.scope(), context.clusterManager(),
-      context.runtime(), context.random(),
-      Router::ShadowWriterPtr{new Router::ShadowWriterImpl(context.clusterManager())},
-      dynamic_stats, start_child_span));
-
-  return [config](Http::FilterChainFactoryCallbacks& callbacks) -> void {
-    callbacks.addStreamDecoderFilter(std::make_shared<Router::ProdFilter>(*config));
-  };
-}
-
 HttpFilterFactoryCb RouterFilterConfig::createFilterFactory(const Json::Object& json_config,
                                                             const std::string& stat_prefix,
                                                             FactoryContext& context) {
   json_config.validateSchema(Json::Schema::ROUTER_HTTP_FILTER_SCHEMA);
-  return createRouterFilter(json_config.getBoolean("dynamic_stats", true),
-                            json_config.getBoolean("start_child_span", false), stat_prefix,
-                            context);
-}
 
-HttpFilterFactoryCb RouterFilterConfig::createFilterFactoryFromProto(
-    const Protobuf::Message& config, const std::string& stat_prefix, FactoryContext& context) {
-  const envoy::api::v2::filter::http::Router& router =
-      dynamic_cast<const envoy::api::v2::filter::http::Router&>(config);
-  const bool dynamic_stats = PROTOBUF_GET_WRAPPED_OR_DEFAULT(router, dynamic_stats, true);
-  const bool start_child_span = router.start_child_span();
-  return createRouterFilter(dynamic_stats, start_child_span, stat_prefix, context);
+  Router::FilterConfigSharedPtr config(new Router::FilterConfig(
+      stat_prefix, context.localInfo(), context.scope(), context.clusterManager(),
+      context.runtime(), context.random(),
+      Router::ShadowWriterPtr{new Router::ShadowWriterImpl(context.clusterManager())},
+      json_config.getBoolean("dynamic_stats", true),
+      json_config.getBoolean("start_child_span", false)));
+
+  return [config](Http::FilterChainFactoryCallbacks& callbacks) -> void {
+    callbacks.addStreamDecoderFilter(std::make_shared<Router::ProdFilter>(*config));
+  };
 }
 
 /**
--- source/server/config/http/router.h
+++ source/server/config/http/router.h
@@ -6,8 +6,6 @@
 
 #include "common/config/well_known_names.h"
 
-#include "api/filter/http/router.pb.h"
-
 namespace Envoy {
 namespace Server {
 namespace Configuration {
@@ -20,19 +18,7 @@ public:
   HttpFilterFactoryCb createFilterFactory(const Json::Object& json_config,
                                           const std::string& stat_prefix,
                                           FactoryContext& context) override;
-  HttpFilterFactoryCb createFilterFactoryFromProto(const Protobuf::Message& config,
-                                                   const std::string& stat_prefix,
-                                                   FactoryContext& context) override;
-
-  ProtobufTypes::MessagePtr createEmptyConfigProto() override {
-    return ProtobufTypes::MessagePtr{new envoy::api::v2::filter::http::Router()};
-  }
-
   std::string name() override { return Config::HttpFilterNames::get().ROUTER; }
-
-private:
-  HttpFilterFactoryCb createRouterFilter(bool dynamic_stats, bool start_child_span,
-                                         const std::string& stat_prefix, FactoryContext& context);
 };
 
 } // namespace Configuration
