442350cb553f70c87a5050fdcdd33227aae5b9f2
Nick Nellis <nick.nellis@weather.com>
2017-11-09 09:33:10 -0600

    fixed lower bound http status code as well
    Signed-off-by: Nick Nellis <nick.nellis@weather.com>

--- source/common/http/filter/lua/lua_filter.cc
+++ source/common/http/filter/lua/lua_filter.cc
@@ -116,7 +116,7 @@ int StreamHandleWrapper::luaRespond(lua_State* state) {
 
   uint64_t status;
   if (headers->Status() == nullptr ||
-      !StringUtil::atoul(headers->Status()->value().c_str(), status) || status < 200 ||
+      !StringUtil::atoul(headers->Status()->value().c_str(), status) || status < 100 ||
       status >= 600) {
     luaL_error(state, ":status must be between 100-599");
   }
