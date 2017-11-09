780ecf435d71e95c1b1a58dc6e9700f92dcac0d1
Nick <nick.nellis@weather.com>
2017-11-09 08:15:45 -0600

    Fixed status check
    Signed-off-by: Nick <nick.nellis@weather.com>

--- source/common/http/filter/lua/lua_filter.cc
+++ source/common/http/filter/lua/lua_filter.cc
@@ -117,7 +117,7 @@ int StreamHandleWrapper::luaRespond(lua_State* state) {
   uint64_t status;
   if (headers->Status() == nullptr ||
       !StringUtil::atoul(headers->Status()->value().c_str(), status) || status < 200 ||
-      status >= 500) {
+      status >= 600) {
     luaL_error(state, ":status must be between 100-599");
   }
 
