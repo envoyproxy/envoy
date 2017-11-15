33d27e789a038d475e9d1be0c8977a5ad80519e6
Piotr Sikora <piotrsikora@google.com>
2017-11-14 21:09:34 -0800

    review: remove "x".
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- include/envoy/ssl/context_manager.h
+++ include/envoy/ssl/context_manager.h
@@ -24,7 +24,7 @@ public:
 
   /**
    * Builds a ServerContext from a ServerContextConfig.
-   * The skip_context_update parameter is used for fast-path (avoiding lock & context looup)x
+   * The skip_context_update parameter is used for fast-path (avoiding lock & context looup)
    * on listeners with a single filter chain and no SNI restrictions.
    */
   virtual ServerContextPtr createSslServerContext(const std::string& listener_name,
