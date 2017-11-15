15c59e05313d3324f332743149abe072a500542c
Greg Greenway <ggreenway@apple.com>
2017-11-15 10:23:12 -0800

    Period at end of comment
    Signed-off-by: Greg Greenway <ggreenway@apple.com>

--- source/common/router/router.cc
+++ source/common/router/router.cc
@@ -895,7 +895,7 @@ void Filter::UpstreamRequest::onPoolReady(Http::StreamEncoder& request_encoder,
                                           Upstream::HostDescriptionConstSharedPtr host) {
   ENVOY_STREAM_LOG(debug, "pool ready", *parent_.callbacks_);
 
-  // TODO(ggreenway): set upstream local address in the RequestInfo
+  // TODO(ggreenway): set upstream local address in the RequestInfo.
   onUpstreamHostSelected(host);
   request_encoder.getStream().addCallbacks(*this);
 
