528889336462e1a38458bd72011168697a2e2568
Stephan Zuercher <stephan@turbinelabs.io>
2017-12-11 16:25:02 -0800

    add dynamic response header to header integration test
    Signed-off-by: Stephan Zuercher <stephan@turbinelabs.io>

--- test/integration/header_integration_test.cc
+++ test/integration/header_integration_test.cc
@@ -1,7 +1,11 @@
+#include "common/config/metadata.h"
+#include "common/config/resources.h"
 #include "common/protobuf/protobuf.h"
 
 #include "test/integration/http_integration.h"
+#include "test/test_common/network_utility.h"
 
+#include "api/eds.pb.h"
 #include "api/filter/network/http_connection_manager.pb.h"
 #include "gtest/gtest.h"
 
@@ -81,6 +85,11 @@ public:
     Replace = 2,
   };
 
+  void TearDown() override {
+    test_server_.reset();
+    fake_upstreams_.clear();
+  }
+
   void addHeader(Protobuf::RepeatedPtrField<envoy::api::v2::HeaderValueOption>* field,
                  const std::string& key, const std::string& value, bool append) {
     envoy::api::v2::HeaderValueOption* header_value_option = field->Add();
@@ -88,6 +97,45 @@ public:
     mutable_header->set_key(key);
     mutable_header->set_value(value);
     header_value_option->mutable_append()->set_value(append);
+  }
+
+  void prepareEDS() {
+    config_helper_.addConfigModifier([&](envoy::api::v2::Bootstrap& bootstrap) {
+      auto* static_resources = bootstrap.mutable_static_resources();
+      ASSERT(static_resources->clusters_size() == 1);
+
+      auto* cluster0 = static_resources->mutable_clusters(0);
+      cluster0->clear_hosts();
+      cluster0->set_type(envoy::api::v2::Cluster_DiscoveryType_EDS);
+      auto* eds_config =
+          cluster0->mutable_eds_cluster_config()->mutable_eds_config()->mutable_api_config_source();
+      eds_config->set_api_type(envoy::api::v2::ApiConfigSource_ApiType_GRPC);
+      eds_config->add_cluster_name("eds-cluster");
+
+      // Give the config utility a place to stuff the upstream host's port (must come before
+      // the eds-cluster to keep the upstreams and ports in the same order).)
+      auto* unused_cluster = static_resources->add_clusters();
+      unused_cluster->set_name("unused-cluster");
+      unused_cluster->set_type(envoy::api::v2::Cluster_DiscoveryType_STATIC);
+      unused_cluster->set_lb_policy(envoy::api::v2::Cluster_LbPolicy_ROUND_ROBIN);
+      auto* host = unused_cluster->add_hosts();
+      auto* sock_addr = host->mutable_socket_address();
+      sock_addr->set_address(Network::Test::getLoopbackAddressString(version_));
+      sock_addr->set_port_value(0);
+
+      auto* eds_cluster = static_resources->add_clusters();
+      eds_cluster->set_name("eds-cluster");
+      eds_cluster->set_type(envoy::api::v2::Cluster_DiscoveryType_STATIC);
+      eds_cluster->set_lb_policy(envoy::api::v2::Cluster_LbPolicy_ROUND_ROBIN);
+      eds_cluster->mutable_http2_protocol_options();
+      eds_cluster->mutable_connect_timeout()->set_seconds(5);
+      auto* eds_host = eds_cluster->add_hosts();
+      sock_addr = eds_host->mutable_socket_address();
+      sock_addr->set_address(Network::Test::getLoopbackAddressString(version_));
+      sock_addr->set_port_value(0);
+    });
+
+    use_eds_ = true;
   }
 
   void initializeFilter(HeaderMode mode, bool include_route_config_headers) {
@@ -106,6 +154,25 @@ public:
             route_config->add_response_headers_to_remove("x-routeconfig-response-remove");
             addHeader(route_config->mutable_request_headers_to_add(), "x-routeconfig-request",
                       "routeconfig", append);
+          }
+
+          if (use_eds_) {
+            addHeader(route_config->mutable_response_headers_to_add(), "x-routeconfig-dynamic",
+                      "%UPSTREAM_METADATA([\"test.namespace\", \"key\"])%", append);
+
+            // Iterate over VirtualHosts and nested Routes, adding a dynamic response header.
+            for (auto& vhost : *route_config->mutable_virtual_hosts()) {
+              addHeader(vhost.mutable_response_headers_to_add(), "x-vhost-dynamic",
+                        "%UPSTREAM_METADATA([\"test.namespace\", \"key\"])%", append);
+
+              for (auto& rte : *vhost.mutable_routes()) {
+                if (rte.has_route()) {
+                  addHeader(rte.mutable_route()->mutable_response_headers_to_add(),
+                            "x-route-dynamic", "%UPSTREAM_METADATA([\"test.namespace\", \"key\"])%",
+                            append);
+                }
+              }
+            }
           }
 
           if (append) {
@@ -132,11 +199,55 @@ public:
     initialize();
   }
 
+  virtual void createUpstreams() override {
+    HttpIntegrationTest::createUpstreams();
+
+    if (use_eds_) {
+      fake_upstreams_.emplace_back(new FakeUpstream(0, FakeHttpConnection::Type::HTTP2, version_));
+      ports_.push_back(fake_upstreams_.back()->localAddress()->ip()->port());
+    }
+  }
+
+  virtual void initialize() override {
+    HttpIntegrationTest::initialize();
+
+    if (use_eds_) {
+      eds_connection_ = fake_upstreams_[1]->waitForHttpConnection(*dispatcher_);
+      eds_stream_ = eds_connection_->waitForNewStream(*dispatcher_);
+      eds_stream_->startGrpcStream();
+
+      envoy::api::v2::DiscoveryRequest discovery_request;
+      eds_stream_->waitForGrpcMessage(*dispatcher_, discovery_request);
+
+      envoy::api::v2::DiscoveryResponse discovery_response;
+      discovery_response.set_version_info("1");
+      discovery_response.set_type_url(Config::TypeUrl::get().ClusterLoadAssignment);
+
+      envoy::api::v2::ClusterLoadAssignment cluster_load_assignment;
+      cluster_load_assignment.set_cluster_name("cluster_0");
+      auto* endpoint = cluster_load_assignment.add_endpoints()->add_lb_endpoints();
+      auto* sock_addr = endpoint->mutable_endpoint()->mutable_address()->mutable_socket_address();
+      sock_addr->set_protocol(envoy::api::v2::SocketAddress_Protocol_TCP);
+      sock_addr->set_address(Network::Test::getLoopbackAddressString(GetParam()));
+      sock_addr->set_port_value(fake_upstreams_[0]->localAddress()->ip()->port());
+      Config::Metadata::mutableMetadataValue(*endpoint->mutable_metadata(), "test.namespace", "key")
+          .set_string_value("metadata-value");
+
+      discovery_response.add_resources()->PackFrom(cluster_load_assignment);
+      eds_stream_->sendGrpcMessage(discovery_response);
+
+      // Wait for the next request to make sure the first response was consumed.
+      eds_stream_->waitForGrpcMessage(*dispatcher_, discovery_request);
+    }
+  }
+
 protected:
   void performRequest(Http::TestHeaderMapImpl&& request_headers,
                       Http::TestHeaderMapImpl&& expected_request_headers,
                       Http::TestHeaderMapImpl&& response_headers,
                       Http::TestHeaderMapImpl&& expected_response_headers) {
+    registerTestServerPorts({"http"});
+
     codec_client_ = makeHttpConnection(makeClientConnection(lookupPort("http")));
     codec_client_->makeHeaderOnlyRequest(request_headers, *response_);
     waitForNextUpstreamRequest();
@@ -159,6 +270,10 @@ protected:
 
     EXPECT_EQ(expected_headers, headers);
   }
+
+  bool use_eds_{0};
+  FakeHttpConnectionPtr eds_connection_;
+  FakeStreamPtr eds_stream_;
 };
 
 INSTANTIATE_TEST_CASE_P(IpVersions, HeaderIntegrationTest,
@@ -496,6 +611,52 @@ TEST_P(HeaderIntegrationTest, TestRouteConfigVirtualHostAndRouteReplaceHeaderMan
           {"x-route-response", "route"},
           {"x-vhost-response", "vhost"},
           {"x-routeconfig-response", "routeconfig"},
+          {":status", "200"},
+      });
+}
+
+// Validates that upstream host metadata can be emitted in headers.
+TEST_P(HeaderIntegrationTest, TestDynamicHeaders) {
+  prepareEDS();
+  initializeFilter(HeaderMode::Replace, true);
+  performRequest(
+      Http::TestHeaderMapImpl{
+          {":method", "GET"},
+          {":path", "/vhost-and-route"},
+          {":scheme", "http"},
+          {":authority", "vhost-headers.com"},
+          {"x-routeconfig-request", "downstream"},
+          {"x-vhost-request", "downstream"},
+          {"x-route-request", "downstream"},
+          {"x-unmodified", "request"},
+      },
+      Http::TestHeaderMapImpl{
+          {":authority", "vhost-headers.com"},
+          {"x-unmodified", "request"},
+          {"x-route-request", "route"},
+          {"x-vhost-request", "vhost"},
+          {"x-routeconfig-request", "routeconfig"},
+          {":path", "/vhost-and-route"},
+          {":method", "GET"},
+      },
+      Http::TestHeaderMapImpl{
+          {"server", "envoy"},
+          {"content-length", "0"},
+          {":status", "200"},
+          {"x-routeconfig-response", "upstream"},
+          {"x-vhost-response", "upstream"},
+          {"x-route-response", "upstream"},
+          {"x-unmodified", "response"},
+      },
+      Http::TestHeaderMapImpl{
+          {"server", "envoy"},
+          {"x-unmodified", "response"},
+          {"x-route-response", "route"},
+          {"x-route-dynamic", "metadata-value"},
+          {"x-vhost-response", "vhost"},
+          {"x-vhost-dynamic", "metadata-value"},
+          {"x-routeconfig-response", "routeconfig"},
+          {"x-routeconfig-dynamic", "metadata-value"},
           {":status", "200"},
       });
 }
