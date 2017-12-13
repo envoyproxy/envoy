bd163a81a09d0cd98278fb5e4f61d0136007d409
alyssawilk <alyssar@google.com>
2017-12-13 10:50:50 -0800

    Moving more tests over to the v2 API (#2204)
    Risk Level: Low (test only)
    Release Notes: (n/a)

--- test/integration/integration_admin_test.h
+++ test/integration/integration_admin_test.h
@@ -10,16 +10,10 @@ class IntegrationAdminTest : public HttpIntegrationTest,
                              public testing::TestWithParam<Network::Address::IpVersion> {
 public:
   IntegrationAdminTest() : HttpIntegrationTest(Http::CodecClient::Type::HTTP1, GetParam()) {}
-  /**
-   * Initializer for an individual test.
-   */
-  void SetUp() override {
-    fake_upstreams_.emplace_back(new FakeUpstream(0, FakeHttpConnection::Type::HTTP1, version_));
-    registerPort("upstream_0", fake_upstreams_.back()->localAddress()->ip()->port());
-    fake_upstreams_.emplace_back(new FakeUpstream(0, FakeHttpConnection::Type::HTTP1, version_));
-    registerPort("upstream_1", fake_upstreams_.back()->localAddress()->ip()->port());
-    createTestServer("test/config/integration/server.json",
-                     {"http", "http_buffer", "tcp_proxy", "rds"});
+
+  void initialize() override {
+    config_helper_.addFilter(ConfigHelper::DEFAULT_HEALTH_CHECK_FILTER);
+    HttpIntegrationTest::initialize();
   }
 
   /**
