f0291108df8f2855b9d0f48ff790cd9c21080fac
Matt Rice <mattrice@google.com>
2017-12-15 16:26:22 -0500

    Add some simple dispatcher unit tests
    Signed-off-by: Matt Rice <mattrice@google.com>

--- test/common/event/dispatcher_impl_test.cc
+++ test/common/event/dispatcher_impl_test.cc
@@ -1,5 +1,7 @@
 #include <functional>
+#include <mutex>
 
+#include "common/common/thread.h"
 #include "common/event/dispatcher_impl.h"
 
 #include "test/mocks/common.h"
@@ -21,7 +23,7 @@ private:
   std::function<void()> on_destroy_;
 };
 
-TEST(DispatcherImplTest, DeferredDelete) {
+TEST(DeferredDeleteTest, DeferredDelete) {
   InSequence s;
   DispatcherImpl dispatcher;
   ReadyWatcher watcher1;
@@ -49,6 +51,83 @@ TEST(DispatcherImplTest, DeferredDelete) {
 
   EXPECT_CALL(watcher3, ready());
   dispatcher.clearDeferredDeleteList();
+}
+
+class DispatcherImplTest : public ::testing::Test {
+protected:
+  DispatcherImplTest() {
+    {
+      std::lock_guard<std::mutex> lock(mu_);
+      work_id_ = 0;
+    }
+    dispatcher_ = std::make_unique<DispatcherImpl>();
+    dispatcher_thread_ = std::make_unique<Thread::Thread>([this]() {
+      // Must create a keepalive timer to keep the dispatcher from exiting.
+      std::chrono::milliseconds time_interval(500);
+      keepalive_timer_ = dispatcher_->createTimer(
+          [this, time_interval]() { keepalive_timer_->enableTimer(time_interval); });
+      keepalive_timer_->enableTimer(time_interval);
+
+      dispatcher_->run(Dispatcher::RunType::Block);
+    });
+  }
+
+  ~DispatcherImplTest() override {
+    dispatcher_->exit();
+    dispatcher_thread_->join();
+  }
+
+  std::unique_ptr<Thread::Thread> dispatcher_thread_;
+  DispatcherPtr dispatcher_;
+  std::mutex mu_;
+  int work_id_;
+  TimerPtr keepalive_timer_;
+};
+
+TEST_F(DispatcherImplTest, Post) {
+  dispatcher_->post([this]() {
+    std::lock_guard<std::mutex> lock(mu_);
+    work_id_ = 1;
+  });
+
+  bool reached_value = false;
+  while (!reached_value) {
+    std::lock_guard<std::mutex> lock(mu_);
+    reached_value = work_id_ == 1;
+  }
+}
+
+TEST_F(DispatcherImplTest, Timer) {
+  std::unique_ptr<Timer> timer;
+  dispatcher_->post([this, &timer]() {
+
+    std::lock_guard<std::mutex> lock(mu_);
+    timer = dispatcher_->createTimer([this]() {
+      std::lock_guard<std::mutex> lock(mu_);
+      work_id_ = 1;
+    });
+
+  });
+
+  bool reached_value = false;
+  while (!reached_value) {
+    std::lock_guard<std::mutex> lock(mu_);
+    if (timer) {
+      timer->enableTimer(std::chrono::milliseconds(50));
+      reached_value = true;
+    }
+  }
+
+  reached_value = false;
+  while (!reached_value) {
+    std::lock_guard<std::mutex> lock(mu_);
+    reached_value = work_id_ == 1;
+  }
+
+  {
+    std::lock_guard<std::mutex> lock(mu_);
+    EXPECT_TRUE(timer);
+  }
 }
 
 } // namespace Event
