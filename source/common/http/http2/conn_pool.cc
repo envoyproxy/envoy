envoyproxy/envoy

commit caf9c6f14a3e2105cc0879baffd985217dc9b492

http conn pool: rework closeConnections() -> drainConnections() (#2295)
This is a follow up from https://github.com/envoyproxy/envoy/pull/2005.
From production usage at Lyft it has become clear that immediately
closing connections on health check failure is too aggressive. During
normal upstream draining when using the x-envoy-immediate-health-check-fail
header, it is possible to close connections while they still have legitimate
draining requests.

This change reworks the functionality to drain but not close existing
connections. This will make it so that *new* streams occur on new connections,
but existing streams are allowed to finish on their existing connections.
This is the behavior we actually wanted in the first place.

Signed-off-by: Matt Klein <mklein@lyft.com>

source/common/http/http2/conn_pool.cc
@@ -19,13 +19,6 @@ ConnPoolImpl::ConnPoolImpl(Event::Dispatcher& dispatcher, Upstream::HostConstSha
     : dispatcher_(dispatcher), host_(host), priority_(priority) {}
 
 ConnPoolImpl::~ConnPoolImpl() {
-  closeConnections();
-
-  // Make sure all clients are destroyed before we are destroyed.
-  dispatcher_.clearDeferredDeleteList();
-}
-
-void ConnPoolImpl::ConnPoolImpl::closeConnections() {
   if (primary_client_) {
     primary_client_->client_->close();
   }
@@ -33,7 +26,12 @@ void ConnPoolImpl::ConnPoolImpl::closeConnections() {
   if (draining_client_) {
     draining_client_->client_->close();
   }
+
+  // Make sure all clients are destroyed before we are destroyed.
+  dispatcher_.clearDeferredDeleteList();
 }
+
+void ConnPoolImpl::ConnPoolImpl::drainConnections() { movePrimaryClientToDraining(); }
 
 void ConnPoolImpl::addDrainedCallback(DrainedCb cb) {
   drained_callbacks_.push_back(cb);

test/common/http/http2/conn_pool_test.cc
@@ -120,9 +120,9 @@ public:
 };
 
 /**
- * Verify that connections are closed when requested.
+ * Verify that connections are drained when requested.
  */
-TEST_F(Http2ConnPoolImplTest, CloseConnections) {
+TEST_F(Http2ConnPoolImplTest, DrainConnections) {
   InSequence s;
   pool_.max_streams_ = 1;
 
@@ -138,8 +138,14 @@ TEST_F(Http2ConnPoolImplTest, CloseConnections) {
   r2.callbacks_.outer_encoder_->encodeHeaders(HeaderMapImpl{}, true);
   expectClientConnect(1);
 
-  pool_.closeConnections();
-  EXPECT_CALL(*this, onClientDestroy()).Times(2);
+  // This will move primary to draining and destroy draining.
+  pool_.drainConnections();
+  EXPECT_CALL(*this, onClientDestroy());
+  dispatcher_.clearDeferredDeleteList();
+
+  // This will destroy draining.
+  test_clients_[1].connection_->raiseEvent(Network::ConnectionEvent::RemoteClose);
+  EXPECT_CALL(*this, onClientDestroy());
   dispatcher_.clearDeferredDeleteList();
 }
 

source/common/http/http1/conn_pool.cc
@@ -21,19 +21,27 @@ namespace Http {
 namespace Http1 {
 
 ConnPoolImpl::~ConnPoolImpl() {
-  closeConnections();
-
-  // Make sure all clients are destroyed before we are destroyed.
-  dispatcher_.clearDeferredDeleteList();
-}
-
-void ConnPoolImpl::closeConnections() {
   while (!ready_clients_.empty()) {
     ready_clients_.front()->codec_client_->close();
   }
 
   while (!busy_clients_.empty()) {
     busy_clients_.front()->codec_client_->close();
+  }
+
+  // Make sure all clients are destroyed before we are destroyed.
+  dispatcher_.clearDeferredDeleteList();
+}
+
+void ConnPoolImpl::drainConnections() {
+  while (!ready_clients_.empty()) {
+    ready_clients_.front()->codec_client_->close();
+  }
+
+  // We drain busy clients by manually setting remaining requests to 1. Thus, when the next
+  // response completes the client will be destroyed.
+  for (const auto& client : busy_clients_) {
+    client->remaining_requests_ = 1;
   }
 }
 

test/common/http/http1/conn_pool_test.cc
@@ -183,9 +183,9 @@ struct ActiveTestRequest {
 };
 
 /**
- * Verify that connections are closed when requested.
+ * Verify that connections are drained when requested.
  */
-TEST_F(Http1ConnPoolImplTest, CloseConnections) {
+TEST_F(Http1ConnPoolImplTest, DrainConnections) {
   cluster_->resource_manager_.reset(
       new Upstream::ResourceManagerImpl(runtime_, "fake_key", 2, 1024, 1024, 1));
   InSequence s;
@@ -198,8 +198,14 @@ TEST_F(Http1ConnPoolImplTest, CloseConnections) {
 
   r1.completeResponse(false);
 
-  conn_pool_.closeConnections();
-  EXPECT_CALL(conn_pool_, onClientDestroy()).Times(2);
+  // This will destroy the ready client and set requests remaining to 1 on the busy client.
+  conn_pool_.drainConnections();
+  EXPECT_CALL(conn_pool_, onClientDestroy());
+  dispatcher_.clearDeferredDeleteList();
+
+  // This will destroy the busy client when the response finishes.
+  r2.completeResponse(false);
+  EXPECT_CALL(conn_pool_, onClientDestroy());
   dispatcher_.clearDeferredDeleteList();
 }
 

include/envoy/http/conn_pool.h
@@ -81,15 +81,19 @@ public:
   typedef std::function<void()> DrainedCb;
 
   /**
-   * Invoke connection pool draining and register a callback that gets called when draining is
-   * complete.
+   * Register a callback that gets called when the connection pool is fully drained. No actual
+   * draining is done. The owner of the connection pool is responsible for not creating any
+   * new streams.
    */
   virtual void addDrainedCallback(DrainedCb cb) PURE;
 
   /**
-   * Close all connections currently owned by the pool.
+   * Actively drain all existing connection pool connections. This method can be used in cases
+   * where the connection pool is not being destroyed, but the caller wishes to make sure that
+   * all new streams take place on a new connection. For example, when a health check failure
+   * occurs.
    */
-  virtual void closeConnections() PURE;
+  virtual void drainConnections() PURE;
 
   /**
    * Create a new stream on the pool.

source/common/http/http1/conn_pool.h
@@ -37,7 +37,7 @@ public:
   // ConnectionPool::Instance
   Http::Protocol protocol() const override { return Http::Protocol::Http11; }
   void addDrainedCallback(DrainedCb cb) override;
-  void closeConnections() override;
+  void drainConnections() override;
   ConnectionPool::Cancellable* newStream(StreamDecoder& response_decoder,
                                          ConnectionPool::Callbacks& callbacks) override;
 

source/common/http/http2/conn_pool.h
@@ -30,7 +30,7 @@ public:
   // Http::ConnectionPool::Instance
   Http::Protocol protocol() const override { return Http::Protocol::Http2; }
   void addDrainedCallback(DrainedCb cb) override;
-  void closeConnections() override;
+  void drainConnections() override;
   ConnectionPool::Cancellable* newStream(Http::StreamDecoder& response_decoder,
                                          ConnectionPool::Callbacks& callbacks) override;
 

source/common/upstream/cluster_manager_impl.cc
@@ -666,7 +666,7 @@ void ClusterManagerImpl::ThreadLocalClusterManagerImpl::updateClusterMembership(
 void ClusterManagerImpl::ThreadLocalClusterManagerImpl::onHostHealthFailure(
     const HostSharedPtr& host, ThreadLocal::Slot& tls) {
 
-  // Close all HTTP connection pool connections in the case of a host health failure. If outlier/
+  // Drain all HTTP connection pool connections in the case of a host health failure. If outlier/
   // health is due to ECMP flow hashing issues for example, a new set of connections might do
   // better.
   // TODO(mattklein123): This function is currently very specific, but in the future when we do
@@ -680,7 +680,7 @@ void ClusterManagerImpl::ThreadLocalClusterManagerImpl::onHostHealthFailure(
         continue;
       }
 
-      pool->closeConnections();
+      pool->drainConnections();
     }
   }
 }

commit a060d0ca6516695712e3d85a1b0035c0375f826d

http/2 conn pool: fix crash when there is no primary connection
This is a regression from https://github.com/envoyproxy/envoy/pull/2295.

Signed-off-by: Matt Klein <mklein@lyft.com>

source/common/http/http2/conn_pool.cc
@@ -31,7 +31,11 @@ ConnPoolImpl::~ConnPoolImpl() {
   dispatcher_.clearDeferredDeleteList();
 }
 
-void ConnPoolImpl::ConnPoolImpl::drainConnections() { movePrimaryClientToDraining(); }
+void ConnPoolImpl::ConnPoolImpl::drainConnections() {
+  if (primary_client_ != nullptr) {
+    movePrimaryClientToDraining();
+  }
+}
 
 void ConnPoolImpl::addDrainedCallback(DrainedCb cb) {
   drained_callbacks_.push_back(cb);

test/common/http/http2/conn_pool_test.cc
@@ -126,6 +126,9 @@ TEST_F(Http2ConnPoolImplTest, DrainConnections) {
   InSequence s;
   pool_.max_streams_ = 1;
 
+  // Test drain connections call prior to any connections being created.
+  pool_.drainConnections();
+
   expectClientCreate();
   ActiveTestRequest r1(*this, 0);
   EXPECT_CALL(r1.inner_encoder_, encodeHeaders(_, true));