3e25a90a3655f78ef6a95a49feba4cd4370cc408
Mark McBride <mark.mcbride@gmail.com>
2018-01-04 09:09:39 -0800

    examples: convert configurations to v2 format (#2286)
    Signed-off-by: Mark McBride <mark@turbinelabs.io>

--- examples/front-proxy/start_service.sh
+++ examples/front-proxy/start_service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3 /code/service.py &
-envoy -c /etc/service-envoy.json --service-cluster service${SERVICE_NAME}
+envoy -c /etc/service-envoy.yaml --service-cluster service${SERVICE_NAME}
