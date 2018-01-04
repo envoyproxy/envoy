d20a390f211b6662da1c48dcea66e5715a70e7f5
Stephan Zuercher <stephan@turbinelabs.io>
2018-01-04 12:35:22 -0800

    test both combo read/close events and separate read and close events
    Signed-off-by: Stephan Zuercher <stephan@turbinelabs.io>

--- test/common/network/connection_impl_test.cc
+++ test/common/network/connection_impl_test.cc
@@ -575,15 +575,41 @@ TEST_P(ConnectionImplTest, BindFailureTest) {
   dispatcher_->run(Event::Dispatcher::RunType::NonBlock);
 }
 
-// IgnoreEmptyReadTest verifies that the read filter's onData function
-// is not invoked on spurious read events due to connection
-// closure. This is known to occur on OS X, but only about once in 100
-// invocations of this test.
-TEST_P(ConnectionImplTest, IgnoreEmptyReadTest) {
+// ReadOnCloseTest verifies that the read filter's onData function is invoked with available data
+// when the connection is closed.
+TEST_P(ConnectionImplTest, ReadOnCloseTest) {
   setUpBasicConnection();
   connect();
 
-  // Write some data and verify that it appears at the read filter. Leave the buffer undrained.
+  // Close without flush will immediately invokes this callback.
+  EXPECT_CALL(client_callbacks_, onEvent(ConnectionEvent::LocalClose));
+
+  const int buffer_size = 32;
+  Buffer::OwnedImpl data(std::string(buffer_size, 'a'));
+  client_connection_->write(data);
+  client_connection_->close(ConnectionCloseType::NoFlush);
+
+  EXPECT_CALL(*read_filter_, onNewConnection());
+  EXPECT_CALL(*read_filter_, onData(_))
+      .Times(1)
+      .WillOnce(Invoke([&](Buffer::Instance& data) -> FilterStatus {
+        EXPECT_EQ(buffer_size, data.length());
+        return FilterStatus::StopIteration;
+      }));
+
+  EXPECT_CALL(server_callbacks_, onEvent(ConnectionEvent::RemoteClose))
+      .WillOnce(Invoke([&](Network::ConnectionEvent) -> void { dispatcher_->exit(); }));
+
+  dispatcher_->run(Event::Dispatcher::RunType::Block);
+}
+
+// EmptyReadOnCloseTest verifies that the read filter's onData function is not invoked on empty
+// read events due to connection closure.
+TEST_P(ConnectionImplTest, EmptyReadOnCloseTest) {
+  setUpBasicConnection();
+  connect();
+
+  // Write some data and verify that the read filter's onData callback is invoked exactly once.
   const int buffer_size = 32;
   Buffer::OwnedImpl data(std::string(buffer_size, 'a'));
   EXPECT_CALL(*read_filter_, onNewConnection());
@@ -597,13 +623,7 @@ TEST_P(ConnectionImplTest, IgnoreEmptyReadTest) {
   client_connection_->write(data);
   dispatcher_->run(Event::Dispatcher::RunType::Block);
 
-  // Close the connection and verify that the undrained buffer is not passed to the read filter a
-  // second time.
-  EXPECT_CALL(client_callbacks_, onEvent(ConnectionEvent::LocalClose));
-  EXPECT_CALL(server_callbacks_, onEvent(ConnectionEvent::RemoteClose))
-      .WillOnce(Invoke([&](Network::ConnectionEvent) -> void { dispatcher_->exit(); }));
-  client_connection_->close(ConnectionCloseType::NoFlush);
-  dispatcher_->run(Event::Dispatcher::RunType::Block);
+  disconnect(true);
 }
 
 class ConnectionImplBytesSentTest : public testing::Test {
