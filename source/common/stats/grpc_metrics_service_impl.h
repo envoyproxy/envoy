de8e4a4d191f9e9454970282b281b42f0a98d9c7
Rama <ramaraochavali@gmail.com>
2018-01-08 16:23:35 +0530

    convert envoy stats to metrics proto structures
    Signed-off-by: Rama <ramaraochavali@gmail.com>

--- source/common/stats/grpc_metrics_service_impl.h
+++ source/common/stats/grpc_metrics_service_impl.h
@@ -129,82 +129,39 @@ public:
   MetricsServiceSink(const LocalInfo::LocalInfo& local_info, const std::string& cluster_name,
                      ThreadLocal::SlotAllocator& tls, Upstream::ClusterManager& cluster_manager,
                      Stats::Scope& scope, GrpcMetricsStreamerSharedPtr grpc_metrics_streamer);
- // TODO(ramachavali): Build MetricsFamily Object and construct the Stream Message
- // TODO(ramachavali): Remove couts
+
   // MetricsService::Sink
-  void beginFlush() override {
-    std::cout << "beginFlush() Called"
-              << "\n";
-   // TODO(ramachavali): Build MetricsFamily Object here           
-  }
+  void beginFlush() override { message.clear_envoy_metrics(); }
 
   void flushCounter(const Counter& counter, uint64_t delta) override {
-
-    std::cout << "flushCounter() Called" << counter.name() << delta << "\n";
-   // TODO(ramachavali): add these counter to the MetricsFamily Object
+    std::cout << "Delta:" << delta << "\n";
+    io::prometheus::client::MetricFamily* metrics_family = message.add_envoy_metrics();
+    metrics_family->set_name(counter.name());
+    auto* metric = metrics_family->add_metric();
+    auto* counter_metric = metric->mutable_counter();
+    counter_metric->set_value(counter.value());
   }
 
   void flushGauge(const Gauge& gauge, uint64_t value) override {
-    std::cout << "flushGauge() Called" << gauge.name() << value << "\n";
-   // TODO(ramachavali): add these gauges to the MetricsFamily Object
-
+    io::prometheus::client::MetricFamily* metrics_family = message.add_envoy_metrics();
+    metrics_family->set_name(gauge.name());
+    auto* metric = metrics_family->add_metric();
+    auto* gauage_metric = metric->mutable_gauge();
+    gauage_metric->set_value(value);
   }
 
-  void endFlush() override {
-    std::cout << "endFlush() Called"
-              << "\n";
-    //TODO(ramachavali): finish constructing the MetricsFamily object and send
-    envoy::api::v2::StreamMetricsMessage message =
-        envoy::api::v2::StreamMetricsMessage::default_instance();
-    grpc_metrics_streamer_->send(message);
-  }
+  void endFlush() override { grpc_metrics_streamer_->send(message); }
 
   void onHistogramComplete(const Histogram& histogram, uint64_t value) override {
-    std::cout << "onHistogramComplete() Called" << histogram.name() << value << "\n";
+    // TODO(ramaraochavali): Need to figure out how map existing histogram to Proto Model
+    std::cout << "Histogram Called" << histogram.name() << "value:" << value << "\n";
   }
 
 private:
   GrpcMetricsStreamerSharedPtr grpc_metrics_streamer_;
-  // TODO(ramachavali): remove unused code here
-  /*
-    struct TlsSink : public ThreadLocal::ThreadLocalObject, public Network::ConnectionCallbacks {
-      TlsSink(TcpStatsdSink& parent, Event::Dispatcher& dispatcher);
-      ~TlsSink();
+  envoy::api::v2::StreamMetricsMessage message;
 
-      void beginFlush(bool expect_empty_buffer);
-      void checkSize();
-      void commonFlush(const std::string& name, uint64_t value, char stat_type);
-      void flushCounter(const std::string& name, uint64_t delta);
-      void flushGauge(const std::string& name, uint64_t value);
-      void endFlush(bool do_write);
-      void onTimespanComplete(const std::string& name, std::chrono::milliseconds ms);
-      uint64_t usedBuffer();
-      void write(Buffer::Instance& buffer);
-
-      // Network::ConnectionCallbacks
-      void onEvent(Network::ConnectionEvent event) override;
-      void onAboveWriteBufferHighWatermark() override {}
-      void onBelowWriteBufferLowWatermark() override {}
-
-      TcpStatsdSink& parent_;
-      Event::Dispatcher& dispatcher_;
-      Network::ClientConnectionPtr connection_;
-      Buffer::OwnedImpl buffer_;
-      Buffer::RawSlice current_buffer_slice_;
-      char* current_slice_mem_{};
-    };
-  */
-
-   // TODO(ramaraochavali): Check if these are really needed.
-  // Somewhat arbitrary 16MiB limit for buffered stats.
-  static constexpr uint32_t MAX_BUFFERED_STATS_BYTES = (1024 * 1024 * 16);
-
-  // 16KiB intermediate buffer for flushing.
-  static constexpr uint32_t FLUSH_SLICE_SIZE_BYTES = (1024 * 16);
-
-  // Prefix for all flushed stats.
-  static char STAT_PREFIX[];
-
+  //TODO(ramaraochavali): check some of these things are not required
   Upstream::ClusterInfoConstSharedPtr cluster_info_;
   ThreadLocal::SlotPtr tls_;
   Upstream::ClusterManager& cluster_manager_;
