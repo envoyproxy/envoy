6c21077e6b1bda62f28f433c212b5af192a996d5
Taiki Ono <taiks.4559@gmail.com>
2018-01-08 16:43:29 +0900

    More flexible test for version outputs
    Signed-off-by: Taiki Ono <taiks.4559@gmail.com>

--- test/exe/version_out_test.sh
+++ test/exe/version_out_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e -o pipefail
 
-source/exe/envoy-static --version | grep --fixed-strings $(cat external/envoy_api/VERSION)
+source/exe/envoy-static --version | grep -E '[0-9]+\.[0-9]+\.[0-9]+'
