83d45391acf8a761d9deefefaf542bdd091bfe18
alyssawilk <alyssar@google.com>
2018-01-10 21:24:55 -0500

    Sanity checking libevent is initialized (#2339)
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- source/common/event/libevent.cc
+++ source/common/event/libevent.cc
@@ -10,11 +10,14 @@ namespace Envoy {
 namespace Event {
 namespace Libevent {
 
+bool Global::initialized_ = false;
+
 void Global::initialize() {
   evthread_use_pthreads();
 
   // Ignore SIGPIPE and allow errors to propagate through error codes.
   signal(SIGPIPE, SIG_IGN);
+  initialized_ = true;
 }
 
 } // namespace Libevent
