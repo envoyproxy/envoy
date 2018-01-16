a6d263f68a5ed218fcf5b7ab3db216ea54205bcc
Joshua Marantz <jmarantz@google.com>
2018-01-16 17:16:12 -0500

    Use absl::string_view::npos as needed.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/common/stats/stats_impl.cc
+++ source/common/stats/stats_impl.cc
@@ -131,7 +131,7 @@ void HeapRawStatDataAllocator::free(RawStatData& data) {
 void RawStatData::initialize(absl::string_view key) {
   ASSERT(!initialized());
   ASSERT(key.size() <= maxNameLength());
-  ASSERT(std::string::npos == key.find(':'));
+  ASSERT(absl::string_view::npos == key.find(':'));
   ref_count_ = 1;
 
   // key is not necessarily nul-terminated, but we want to make sure name_ is.
