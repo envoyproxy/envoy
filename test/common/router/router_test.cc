7062b9ac32ff499db39e7490e2c3e000a5ebc1c9
Matt Rice <mattrice@google.com>
2018-01-18 14:48:24 -0500

    Removed redundant Times(1)
    Signed-off-by: Matt Rice <mattrice@google.com>

--- test/common/router/router_test.cc
+++ test/common/router/router_test.cc
@@ -137,7 +137,7 @@ TEST_F(RouterTest, RouteNotFound) {
 
 TEST_F(RouterTest, ClusterNotFound) {
   EXPECT_CALL(callbacks_.request_info_, setResponseFlag(RequestInfo::ResponseFlag::NoRouteFound));
-  EXPECT_CALL(callbacks_.request_info_, onRouteSelected(_)).Times(1);
+  EXPECT_CALL(callbacks_.request_info_, onRouteSelected(_));
 
   Http::TestHeaderMapImpl headers;
   HttpTestUtility::addDefaultHeaders(headers);
@@ -171,7 +171,7 @@ TEST_F(RouterTest, PoolFailureWithPriority) {
       .WillOnce(Invoke([&](const Upstream::HostDescriptionConstSharedPtr host) -> void {
         EXPECT_EQ(host_address_, host->address());
       }));
-  EXPECT_CALL(callbacks_.request_info_, onRouteSelected(_)).Times(1);
+  EXPECT_CALL(callbacks_.request_info_, onRouteSelected(_));
 
   Http::TestHeaderMapImpl headers;
   HttpTestUtility::addDefaultHeaders(headers);
