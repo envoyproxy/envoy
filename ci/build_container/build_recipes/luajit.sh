1d50fdf241da52f5a4efb7d2db45be686ae95c81
Harvey Tuch <htuch@google.com>
2018-01-24 16:38:09 -0500

    fuzz: initial fuzzing infrastructure, docs and and Base64::encode() example.
    This is the groundwork for #508. See test/fuzz/README.md for the spiel
    on what we're integrating with, the fuzz test model and how to create
    new fuzz tests.
    
    There were a bunch of changes needed to the build infrastructure to get
    it to work in the oss-fuzz Docker build environment. In particular, we
    needed to allow clang + libc++ static linking on Linux and to supply
    extra flags that some of the external deps were not handling correctly.
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- ci/build_container/build_recipes/luajit.sh
+++ ci/build_container/build_recipes/luajit.sh
@@ -9,11 +9,20 @@ tar xf LuaJIT-"$VERSION".tar.gz
 cd LuaJIT-"$VERSION"
 
 # Fixup Makefile with things that cannot be set via env var.
-cat > ../luajit_make.diff << EOF
+cat > ../luajit_make.diff << 'EOF'
 diff --git a/src/Makefile b/src/Makefile
-index f7f81a4..e80c7ff 100644
+index f7f81a4..0a8feff 100644
 --- a/src/Makefile
 +++ b/src/Makefile
+@@ -27,7 +27,7 @@ NODOTABIVER= 51
+ DEFAULT_CC = gcc
+ #
+ # LuaJIT builds as a native 32 or 64 bit binary by default.
+-CC= $(DEFAULT_CC)
++CC ?= $(DEFAULT_CC)
+ #
+ # Use this if you want to force a 32 bit build on a 64 bit multilib OS.
+ #CC= $(DEFAULT_CC) -m32
 @@ -74,10 +74,10 @@ CCWARN= -Wall
  # as dynamic mode.
  #
@@ -36,16 +45,33 @@ index f7f81a4..e80c7ff 100644
  #
  # Disable the JIT compiler, i.e. turn LuaJIT into a pure interpreter.
  #XCFLAGS+= -DLUAJIT_DISABLE_JIT
-@@ -564,7 +564,7 @@ endif
-
+@@ -229,6 +229,7 @@ TARGET_ALDFLAGS= $(LDOPTIONS) $(TARGET_XLDFLAGS) $(TARGET_FLAGS) $(TARGET_LDFLAG
+ TARGET_ASHLDFLAGS= $(LDOPTIONS) $(TARGET_XSHLDFLAGS) $(TARGET_FLAGS) $(TARGET_SHLDFLAGS)
+ TARGET_ALIBS= $(TARGET_XLIBS) $(LIBS) $(TARGET_LIBS)
+ 
++$(warning $(TARGET_CC) $(TARGET_TCFLAGS) -E lj_arch.h -dM)
+ TARGET_TESTARCH=$(shell $(TARGET_CC) $(TARGET_TCFLAGS) -E lj_arch.h -dM)
+ ifneq (,$(findstring LJ_TARGET_X64 ,$(TARGET_TESTARCH)))
+   TARGET_LJARCH= x64
+@@ -251,7 +252,7 @@ ifneq (,$(findstring LJ_TARGET_MIPS ,$(TARGET_TESTARCH)))
+   endif
+   TARGET_LJARCH= mips
+ else
+-  $(error Unsupported target architecture)
++  $(error Unsupported target architecture $(TARGET_TESTARCH))
+ endif
+ endif
+ endif
+@@ -564,7 +565,7 @@ endif
+ 
  Q= @
  E= @echo
 -#Q=
 +Q=
  #E= @:
-
+ 
  ##############################################################################
 EOF
 patch -p1 < ../luajit_make.diff
 
-make PREFIX="$THIRDPARTY_BUILD" install
+DEFAULT_CC=${CC} TARGET_CFLAGS=${CFLAGS} TARGET_LDFLAGS=${CFLAGS} CFLAGS="" make V=1 PREFIX="$THIRDPARTY_BUILD" install
