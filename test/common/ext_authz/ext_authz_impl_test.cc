0df4b9ae5dac4b83e47ea7856128021c9a528152
Saurabh Mohan <saurabh+github@tigera.io>
2018-01-26 11:15:44 -0800

    Check generator class testing code.
    Signed-off-by: Saurabh Mohan <saurabh+github@tigera.io>

--- test/common/ext_authz/ext_authz_impl_test.cc
+++ test/common/ext_authz/ext_authz_impl_test.cc
@@ -5,8 +5,13 @@
 #include "common/ext_authz/ext_authz_impl.h"
 #include "common/http/header_map_impl.h"
 #include "common/http/headers.h"
+#include "common/network/address_impl.h"
 
 #include "test/mocks/grpc/mocks.h"
+#include "test/mocks/http/mocks.h"
+#include "test/mocks/network/mocks.h"
+#include "test/mocks/request_info/mocks.h"
+#include "test/mocks/ssl/mocks.h"
 #include "test/mocks/upstream/mocks.h"
 #include "test/test_common/printers.h"
 #include "test/test_common/utility.h"
@@ -18,6 +23,7 @@ using testing::AtLeast;
 using testing::Invoke;
 using testing::Ref;
 using testing::Return;
+using testing::ReturnRef;
 using testing::WithArg;
 using testing::_;
 
@@ -139,7 +145,52 @@ TEST(ExtAuthzNullFactoryTest, Basic) {
   client->cancel();
 }
 
-// TODO(saumoh): Add TEST for createHttpCheck and createTcpCheck
+class ExtAuthzCheckRequestGeneratorTest : public testing::Test {
+public:
+  ExtAuthzCheckRequestGeneratorTest() {
+    addr_ = std::make_shared<Network::Address::Ipv4Instance>("1.2.3.4", 1111);
+    protocol_ = Envoy::Http::Protocol::Http10;
+    // ssl_ = new Envoy::Ssl::MockConnection();
+    // Network::Address::InstanceConstSharedPtr{new Network::Address::Ipv4Instance("1.2.3.4", 111)};
+  };
+
+  Network::Address::InstanceConstSharedPtr addr_;
+  Optional<Http::Protocol> protocol_;
+  ExtAuthzCheckRequestGenerator check_request_generator_;
+  NiceMock<Envoy::Http::MockStreamDecoderFilterCallbacks> callbacks_;
+  NiceMock<Envoy::Network::MockReadFilterCallbacks> net_callbacks_;
+  NiceMock<Envoy::Network::MockConnection> connection_;
+  NiceMock<Envoy::Ssl::MockConnection> ssl_;
+  NiceMock<Envoy::RequestInfo::MockRequestInfo> req_info_;
+};
+
+TEST_F(ExtAuthzCheckRequestGeneratorTest, BasicTcp) {
+
+  envoy::api::v2::auth::CheckRequest request;
+
+  EXPECT_CALL(net_callbacks_, connection()).Times(2).WillRepeatedly(ReturnRef(connection_));
+  EXPECT_CALL(connection_, remoteAddress()).WillOnce(ReturnRef(addr_));
+  EXPECT_CALL(connection_, localAddress()).WillOnce(ReturnRef(addr_));
+  EXPECT_CALL(Const(connection_), ssl()).Times(2).WillRepeatedly(Return(&ssl_));
+
+  check_request_generator_.createTcpCheck(&net_callbacks_, request);
+}
+
+TEST_F(ExtAuthzCheckRequestGeneratorTest, BasicHttp) {
+
+  Http::HeaderMapImpl headers;
+  envoy::api::v2::auth::CheckRequest request;
+
+  EXPECT_CALL(callbacks_, connection()).Times(2).WillRepeatedly(Return(&connection_));
+  EXPECT_CALL(connection_, remoteAddress()).WillOnce(ReturnRef(addr_));
+  EXPECT_CALL(connection_, localAddress()).WillOnce(ReturnRef(addr_));
+  EXPECT_CALL(Const(connection_), ssl()).Times(2).WillRepeatedly(Return(&ssl_));
+
+  EXPECT_CALL(callbacks_, streamId()).WillOnce(Return(0));
+  EXPECT_CALL(callbacks_, requestInfo()).Times(2).WillRepeatedly(ReturnRef(req_info_));
+  EXPECT_CALL(req_info_, protocol()).WillOnce(ReturnRef(protocol_));
+  check_request_generator_.createHttpCheck(&callbacks_, headers, request);
+}
 
 } // namespace ExtAuthz
 } // namespace Envoy
