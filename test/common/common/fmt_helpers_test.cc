475786629e6f9e488bc445d52431410738bbbb05
Stephan Zuercher <stephan@turbinelabs.io>
2018-01-27 11:43:33 -0800

    fix ASAN error
    Signed-off-by: Stephan Zuercher <stephan@turbinelabs.io>

--- test/common/common/fmt_helpers_test.cc
+++ test/common/common/fmt_helpers_test.cc
@@ -7,9 +7,9 @@
 namespace Envoy {
 
 TEST(FormatHelpersTest, Format) {
-  std::string s = "This is my string.";
-  absl::string_view my_string = s.substr(8, 9);
-  absl::string_view is = s.substr(5, 2);
+  absl::string_view sv = "This is my string.";
+  absl::string_view my_string = sv.substr(8, 9);
+  absl::string_view is = sv.substr(5, 2);
 
   EXPECT_EQ("it's my string!", fmt::format("it's {}!", my_string));
   EXPECT_EQ("it's my string!", fmt::format("it's {:s}!", my_string));
@@ -17,7 +17,7 @@ TEST(FormatHelpersTest, Format) {
 }
 
 TEST(FormatHelpersTest, FormatLogMessages) {
-  absl::string_view sv("formatted");
+  absl::string_view sv = "formatted";
   ENVOY_LOG_MISC(info, "fake {} message", sv);
 }
 
