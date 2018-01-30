fe65e1a789cc3331e4e817ec6e607ff34aeeb9a4
Harvey Tuch <htuch@google.com>
2018-01-30 15:11:35 -0500

    Review feedback.
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- ci/build_container/build_recipes/luajit.sh
+++ ci/build_container/build_recipes/luajit.sh
@@ -11,7 +11,7 @@ cd LuaJIT-"$VERSION"
 # Fixup Makefile with things that cannot be set via env var.
 cat > ../luajit_make.diff << 'EOF'
 diff --git a/src/Makefile b/src/Makefile
-index f7f81a4..0a8feff 100644
+index f7f81a4..e698517 100644
 --- a/src/Makefile
 +++ b/src/Makefile
 @@ -27,7 +27,7 @@ NODOTABIVER= 51
@@ -45,24 +45,7 @@ index f7f81a4..0a8feff 100644
  #
  # Disable the JIT compiler, i.e. turn LuaJIT into a pure interpreter.
  #XCFLAGS+= -DLUAJIT_DISABLE_JIT
-@@ -229,6 +229,7 @@ TARGET_ALDFLAGS= $(LDOPTIONS) $(TARGET_XLDFLAGS) $(TARGET_FLAGS) $(TARGET_LDFLAG
- TARGET_ASHLDFLAGS= $(LDOPTIONS) $(TARGET_XSHLDFLAGS) $(TARGET_FLAGS) $(TARGET_SHLDFLAGS)
- TARGET_ALIBS= $(TARGET_XLIBS) $(LIBS) $(TARGET_LIBS)
- 
-+$(warning $(TARGET_CC) $(TARGET_TCFLAGS) -E lj_arch.h -dM)
- TARGET_TESTARCH=$(shell $(TARGET_CC) $(TARGET_TCFLAGS) -E lj_arch.h -dM)
- ifneq (,$(findstring LJ_TARGET_X64 ,$(TARGET_TESTARCH)))
-   TARGET_LJARCH= x64
-@@ -251,7 +252,7 @@ ifneq (,$(findstring LJ_TARGET_MIPS ,$(TARGET_TESTARCH)))
-   endif
-   TARGET_LJARCH= mips
- else
--  $(error Unsupported target architecture)
-+  $(error Unsupported target architecture $(TARGET_TESTARCH))
- endif
- endif
- endif
-@@ -564,7 +565,7 @@ endif
+@@ -564,7 +564,7 @@ endif
  
  Q= @
  E= @echo
