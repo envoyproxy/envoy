e0e6b77c59bf493b3fae8427ef1f7115cd7f3b30
Saurabh Mohan <saurabh+github@tigera.io>
2018-01-29 20:34:49 -0800

    Fix format.
    Signed-off-by: Saurabh Mohan <saurabh+github@tigera.io>

--- source/common/config/well_known_names.h
+++ source/common/config/well_known_names.h
@@ -136,7 +136,7 @@ public:
   const std::string SQUASH = "envoy.squash";
   // External Authorization filter
   const std::string EXT_AUTHORIZATION = "envoy.ext_authz";
-  
+
   // Converts names from v1 to v2
   const V1Converter v1_converter_;
 
