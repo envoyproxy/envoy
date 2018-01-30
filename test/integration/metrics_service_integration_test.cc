2e6402a1b25224298b2c1d474caf3d343e947e36
ramaraochavali <ramaraochavali@gmail.com>
2018-01-30 23:29:19 +0530

    metrics: populate metric type and time stamp in Metric Service (#2480)
    Metric Service does not populate the type and timestamp. It is easier to check the type in MetricFamily rather than checking has_counter() and has_gauge(). Also the time stamp field is not populated.
    
    Risk Level: Low
    Testing: Integration test upated
    Docs Changes: N/A
    Release Notes: N/A

--- test/integration/metrics_service_integration_test.cc
+++ test/integration/metrics_service_integration_test.cc
@@ -67,15 +67,16 @@ public:
     bool known_gauge_exists = false;
     for (::io::prometheus::client::MetricFamily metrics_family : envoy_metrics) {
       if (metrics_family.name() == "cluster.metrics_service.membership_change" &&
-          metrics_family.metric(0).has_counter()) {
+          metrics_family.type() == ::io::prometheus::client::MetricType::COUNTER) {
         known_counter_exists = true;
         EXPECT_EQ(1, metrics_family.metric(0).counter().value());
       }
       if (metrics_family.name() == "cluster.metrics_service.membership_total" &&
-          metrics_family.metric(0).has_gauge()) {
+          metrics_family.type() == ::io::prometheus::client::MetricType::GAUGE) {
         known_gauge_exists = true;
         EXPECT_EQ(1, metrics_family.metric(0).gauge().value());
       }
+      ASSERT(metrics_family.metric(0).has_timestamp_ms());
       if (known_counter_exists && known_gauge_exists) {
         break;
       }
