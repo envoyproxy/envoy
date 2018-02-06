98e6f3c54c580e7fa6e8aff5d3b283f5dd0f5809
Rama <rama.rao@salesforce.com>
2018-02-06 11:34:58 +0530

    formatted
    Signed-off-by: Rama <rama.rao@salesforce.com>

--- source/server/proto_descriptors.h
+++ source/server/proto_descriptors.h
@@ -2,9 +2,9 @@
 
 namespace Envoy {
 namespace Server {
-    
+
 // This function validates that the method descriptors for gRPC services and type descriptors that
-// are referenced in Any messages are available in the descriptor pool.    
+// are referenced in Any messages are available in the descriptor pool.
 bool validateProtoDescriptors();
 } // namespace Server
 } // namespace Envoy
