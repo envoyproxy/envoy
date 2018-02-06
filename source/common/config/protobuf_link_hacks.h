beb12c30b8e7503de5b1602c74ad6ac98ccbbda8
Rama <rama.rao@salesforce.com>
2018-02-06 11:33:16 +0530

    addressed review comments, added ifdef to avoid duplicate inclusions
    Signed-off-by: Rama <rama.rao@salesforce.com>

--- source/common/config/protobuf_link_hacks.h
+++ source/common/config/protobuf_link_hacks.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#ifndef PROTOBUF_LINKS_
+#define PROTOBUF_LINKS_
+
 #include "envoy/service/discovery/v2/ads.pb.h"
 #include "envoy/service/ratelimit/v2/rls.pb.h"
 
@@ -7,6 +10,7 @@ namespace Envoy {
 
 // Hack to force linking of the service: https://github.com/google/protobuf/issues/4221.
 // This file should be included ONLY if this hack is required.
-envoy::service::discovery::v2::AdsDummy dummy;
-envoy::service::ratelimit::v2::RateLimitRequest rls_dummy;
+const envoy::service::discovery::v2::AdsDummy _ads_dummy;
+const envoy::service::ratelimit::v2::RateLimitRequest _rls_dummy;
 } // namespace Envoy
+#endif
--- source/server/proto_descriptors.h
+++ source/server/proto_descriptors.h
@@ -2,6 +2,9 @@
 
 namespace Envoy {
 namespace Server {
+    
+// This function validates that the method descriptors for gRPC services and type descriptors that
+// are referenced in Any messages are available in the descriptor pool.    
 bool validateProtoDescriptors();
 } // namespace Server
 } // namespace Envoy
