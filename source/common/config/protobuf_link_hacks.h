8d64d92934657d5b173efa0b887d819c7f551e27
Rama <rama.rao@salesforce.com>
2018-02-06 10:36:41 +0530

    added Envoy namespace for protobuf_link_hacks.h
    Signed-off-by: Rama <rama.rao@salesforce.com>

--- source/common/config/protobuf_link_hacks.h
+++ source/common/config/protobuf_link_hacks.h
@@ -3,7 +3,10 @@
 #include "envoy/service/discovery/v2/ads.pb.h"
 #include "envoy/service/ratelimit/v2/rls.pb.h"
 
+namespace Envoy {
+
 // Hack to force linking of the service: https://github.com/google/protobuf/issues/4221.
 // This file should be included ONLY if this hack is required.
 envoy::service::discovery::v2::AdsDummy dummy;
 envoy::service::ratelimit::v2::RateLimitRequest rls_dummy;
+} // namespace Envoy
