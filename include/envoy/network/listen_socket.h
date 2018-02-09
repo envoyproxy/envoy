envoyproxy/envoy

commit eb02089b036f175f45f23b054f5b40688ed6bd43

Listener: Add listener filters. (#2346)
Network::ListenerImpl class implements an libevent socket
listener. Factor out all functionality that is not directly libevent
related to ConnectionHandlerImpl, which both reduces overall
complexity and makes it possible to refactor configurable listener
behavior to a new class of listener filters in the following patches.

This patch also reduces the special handling for SSL connections by
introducing a new dispatcher member for creating server
connections. This is used by the refactored code as the connection
creation is now shifted from Network::Listener to the connection
handler.

To further simplify the resulting code, this patch removes
Network::ListenerOptions class, as the only option that now needs to
be passed to Network::Listener is the "bind to port" boolean, which is
now passed in explicitly.  All other listener options are handled by
passing along Server::Listener instead.

Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

include/envoy/network/listen_socket.h
@@ -34,5 +34,65 @@ public:
 typedef std::unique_ptr<ListenSocket> ListenSocketPtr;
 typedef std::shared_ptr<ListenSocket> ListenSocketSharedPtr;
 
+/**
+ * A socket passed to a connection. For server connections this represents the accepted socket, and
+ * for client connections this represents the socket being connected to a remote address.
+ *
+ * TODO(jrajahalme): Hide internals (e.g., fd) from listener filters by providing callbacks filters
+ * may need (set/getsockopt(), peek(), recv(), etc.)
+ */
+class ConnectionSocket {
+public:
+  virtual ~ConnectionSocket() {}
+
+  /**
+   * @return the local address of the socket.
+   */
+  virtual const Address::InstanceConstSharedPtr& localAddress() const PURE;
+
+  /**
+   * @return the remote address of the socket.
+   */
+  virtual const Address::InstanceConstSharedPtr& remoteAddress() const PURE;
+
+  /**
+   * Set the local address of the socket. On accepted sockets the local address defaults to the
+   * one at which the connection was received at, which is the same as the listener's address, if
+   * the listener is bound to a specific address.
+   *
+   * @param local_address the new local address.
+   * @param restored a flag marking the local address as being restored to a value that is
+   *        different from the one the socket was initially accepted at. This should only be set
+   *        to 'true' when restoring the original destination address of a connection redirected
+   *        by iptables REDIRECT. The caller is responsible for making sure the new address is
+   *        actually different when passing restored as 'true'.
+   */
+  virtual void setLocalAddress(const Address::InstanceConstSharedPtr& local_address,
+                               bool restored = false) PURE;
+
+  /**
+   * Set the remote address of the socket.
+   */
+  virtual void setRemoteAddress(const Address::InstanceConstSharedPtr& remote_address) PURE;
+
+  /**
+   * @return true if the local address has been restored to a value that is different from the
+   *         address the socket was initially accepted at.
+   */
+  virtual bool localAddressRestored() const PURE;
+
+  /**
+   * @return fd the socket's file descriptor.
+   */
+  virtual int fd() const PURE;
+
+  /**
+   * Close the underlying socket.
+   */
+  virtual void close() PURE;
+};
+
+typedef std::unique_ptr<ConnectionSocket> ConnectionSocketPtr;
+
 } // namespace Network
 } // namespace Envoy

source/common/filter/listener/proxy_protocol.cc
@@ -1,0 +1,162 @@
+#include "common/filter/listener/proxy_protocol.h"
+
+#include <unistd.h>
+
+#include <cstdint>
+#include <memory>
+#include <string>
+
+#include "envoy/common/exception.h"
+#include "envoy/event/dispatcher.h"
+#include "envoy/network/listen_socket.h"
+#include "envoy/stats/stats.h"
+
+#include "common/common/assert.h"
+#include "common/common/empty_string.h"
+#include "common/common/utility.h"
+#include "common/network/address_impl.h"
+#include "common/network/utility.h"
+
+namespace Envoy {
+namespace Filter {
+namespace Listener {
+namespace ProxyProtocol {
+
+Config::Config(Stats::Scope& scope) : stats_{ALL_PROXY_PROTOCOL_STATS(POOL_COUNTER(scope))} {}
+
+Network::FilterStatus Instance::onAccept(Network::ListenerFilterCallbacks& cb) {
+  ENVOY_LOG(debug, "proxy_protocol: New connection accepted");
+  Network::ConnectionSocket& socket = cb.socket();
+  ASSERT(file_event_.get() == nullptr);
+  file_event_ =
+      cb.dispatcher().createFileEvent(socket.fd(),
+                                      [this](uint32_t events) {
+                                        ASSERT(events == Event::FileReadyType::Read);
+                                        UNREFERENCED_PARAMETER(events);
+                                        onRead();
+                                      },
+                                      Event::FileTriggerType::Edge, Event::FileReadyType::Read);
+  cb_ = &cb;
+  return Network::FilterStatus::StopIteration;
+}
+
+void Instance::onRead() {
+  try {
+    onReadWorker();
+  } catch (const EnvoyException& ee) {
+    config_->stats_.downstream_cx_proxy_proto_error_.inc();
+    cb_->continueFilterChain(false);
+  }
+}
+
+void Instance::onReadWorker() {
+  Network::ConnectionSocket& socket = cb_->socket();
+  std::string proxy_line;
+  if (!readLine(socket.fd(), proxy_line)) {
+    return;
+  }
+
+  const auto trimmed_proxy_line = StringUtil::rtrim(proxy_line);
+
+  // Parse proxy protocol line with format: PROXY TCP4/TCP6/UNKNOWN SOURCE_ADDRESS
+  // DESTINATION_ADDRESS SOURCE_PORT DESTINATION_PORT.
+  const auto line_parts = StringUtil::splitToken(trimmed_proxy_line, " ", true);
+  if (line_parts.size() < 2 || line_parts[0] != "PROXY") {
+    throw EnvoyException("failed to read proxy protocol");
+  }
+
+  // If the line starts with UNKNOWN we know it's a proxy protocol line, so we can remove it from
+  // the socket and continue. According to spec "real connection's parameters" should be used, so
+  // we should NOT restore the addresses in this case.
+  if (line_parts[1] != "UNKNOWN") {
+    // If protocol not UNKNOWN, src and dst addresses have to be present.
+    if (line_parts.size() != 6) {
+      throw EnvoyException("failed to read proxy protocol");
+    }
+
+    Network::Address::IpVersion protocol_version;
+    Network::Address::InstanceConstSharedPtr remote_address;
+    Network::Address::InstanceConstSharedPtr local_address;
+
+    // TODO(gsagula): parseInternetAddressAndPort() could be modified to take two string_view
+    // arguments, so we can eliminate allocation here.
+    if (line_parts[1] == "TCP4") {
+      protocol_version = Network::Address::IpVersion::v4;
+      remote_address = Network::Utility::parseInternetAddressAndPort(
+          std::string{line_parts[2]} + ":" + std::string{line_parts[4]});
+      local_address = Network::Utility::parseInternetAddressAndPort(
+          std::string{line_parts[3]} + ":" + std::string{line_parts[5]});
+    } else if (line_parts[1] == "TCP6") {
+      protocol_version = Network::Address::IpVersion::v6;
+      remote_address = Network::Utility::parseInternetAddressAndPort(
+          "[" + std::string{line_parts[2]} + "]:" + std::string{line_parts[4]});
+      local_address = Network::Utility::parseInternetAddressAndPort(
+          "[" + std::string{line_parts[3]} + "]:" + std::string{line_parts[5]});
+    } else {
+      throw EnvoyException("failed to read proxy protocol");
+    }
+
+    // Error check the source and destination fields. Most errors are caught by the address
+    // parsing above, but a malformed IPv6 address may combine with a malformed port and parse as
+    // an IPv6 address when parsing for an IPv4 address. Remote address refers to the source
+    // address.
+    const auto remote_version = remote_address->ip()->version();
+    const auto local_version = local_address->ip()->version();
+    if (remote_version != protocol_version || local_version != protocol_version) {
+      throw EnvoyException("failed to read proxy protocol");
+    }
+    // Check that both addresses are valid unicast addresses, as required for TCP
+    if (!remote_address->ip()->isUnicastAddress() || !local_address->ip()->isUnicastAddress()) {
+      throw EnvoyException("failed to read proxy protocol");
+    }
+
+    socket.setLocalAddress(local_address);
+    socket.setRemoteAddress(remote_address);
+  }
+
+  // Release the file event so that we do not interfere with the connection read events.
+  file_event_.reset();
+  cb_->continueFilterChain(true);
+}
+
+bool Instance::readLine(int fd, std::string& s) {
+  while (buf_off_ < MAX_PROXY_PROTO_LEN) {
+    ssize_t nread = recv(fd, buf_ + buf_off_, MAX_PROXY_PROTO_LEN - buf_off_, MSG_PEEK);
+
+    if (nread == -1 && errno == EAGAIN) {
+      return false;
+    } else if (nread < 1) {
+      throw EnvoyException("failed to read proxy protocol");
+    }
+
+    bool found = false;
+    // continue searching buf_ from where we left off
+    for (; search_index_ < buf_off_ + nread; search_index_++) {
+      if (buf_[search_index_] == '\n' && buf_[search_index_ - 1] == '\r') {
+        search_index_++;
+        found = true;
+        break;
+      }
+    }
+
+    // Read the data upto and including the line feed, if available, but not past it.
+    // This should never fail, as search_index_ - buf_off_ <= nread, so we're asking
+    // only for bytes we have already seen.
+    nread = recv(fd, buf_ + buf_off_, search_index_ - buf_off_, 0);
+    ASSERT(size_t(nread) == search_index_ - buf_off_);
+
+    buf_off_ += nread;
+
+    if (found) {
+      s.assign(buf_, buf_off_);
+      return true;
+    }
+  }
+
+  throw EnvoyException("failed to read proxy protocol");
+}
+
+} // namespace ProxyProtocol
+} // namespace Listener
+} // namespace Filter
+} // namespace Envoy

source/common/network/connection_impl.cc
@@ -16,21 +16,12 @@
 #include "common/common/empty_string.h"
 #include "common/common/enum_to_int.h"
 #include "common/network/address_impl.h"
+#include "common/network/listen_socket_impl.h"
 #include "common/network/raw_buffer_socket.h"
 #include "common/network/utility.h"
 
 namespace Envoy {
 namespace Network {
-
-namespace {
-Address::InstanceConstSharedPtr getNullLocalAddress(const Address::Instance& address) {
-  if (address.type() == Address::Type::Ip && address.ip()->version() == Address::IpVersion::v6) {
-    return Utility::getIpv6AnyAddress();
-  }
-  // Default to IPv4 any address.
-  return Utility::getIpv4AnyAddress();
-}
-} // namespace
 
 void ConnectionImplUtility::updateBufferStats(uint64_t delta, uint64_t new_total,
                                               uint64_t& previous_total, Stats::Counter& stat_total,
@@ -52,34 +43,22 @@ void ConnectionImplUtility::updateBufferStats(uint64_t delta, uint64_t new_total
 
 std::atomic<uint64_t> ConnectionImpl::next_global_id_;
 
-ConnectionImpl::ConnectionImpl(Event::Dispatcher& dispatcher, int fd,
-                               Address::InstanceConstSharedPtr remote_address,
-                               Address::InstanceConstSharedPtr local_address,
-                               Address::InstanceConstSharedPtr bind_to_address,
-                               bool using_original_dst, bool connected)
-    : ConnectionImpl(dispatcher, fd, remote_address, local_address, bind_to_address,
-                     TransportSocketPtr{new RawBufferSocket}, using_original_dst, connected) {}
-
-ConnectionImpl::ConnectionImpl(Event::Dispatcher& dispatcher, int fd,
-                               Address::InstanceConstSharedPtr remote_address,
-                               Address::InstanceConstSharedPtr local_address,
-                               Address::InstanceConstSharedPtr bind_to_address,
-                               TransportSocketPtr&& transport_socket, bool using_original_dst,
+ConnectionImpl::ConnectionImpl(Event::Dispatcher& dispatcher, ConnectionSocketPtr&& socket,
                                bool connected)
-    : transport_socket_(std::move(transport_socket)), filter_manager_(*this, *this),
-      remote_address_(remote_address),
-      local_address_((local_address == nullptr) ? getNullLocalAddress(*remote_address)
-                                                : local_address),
+    : ConnectionImpl(dispatcher, std::move(socket), std::make_unique<RawBufferSocket>(),
+                     connected) {}
 
-      write_buffer_(
-          dispatcher.getWatermarkFactory().create([this]() -> void { this->onLowWatermark(); },
-                                                  [this]() -> void { this->onHighWatermark(); })),
-      dispatcher_(dispatcher), fd_(fd), id_(++next_global_id_),
-      using_original_dst_(using_original_dst) {
+ConnectionImpl::ConnectionImpl(Event::Dispatcher& dispatcher, ConnectionSocketPtr&& socket,
+                               TransportSocketPtr&& transport_socket, bool connected)
+    : transport_socket_(std::move(transport_socket)), filter_manager_(*this, *this),
+      socket_(std::move(socket)), write_buffer_(dispatcher.getWatermarkFactory().create(
+                                      [this]() -> void { this->onLowWatermark(); },
+                                      [this]() -> void { this->onHighWatermark(); })),
+      dispatcher_(dispatcher), id_(++next_global_id_) {
 
   // Treat the lack of a valid fd (which in practice only happens if we run out of FDs) as an OOM
   // condition and just crash.
-  RELEASE_ASSERT(fd_ != -1);
+  RELEASE_ASSERT(fd() != -1);
 
   if (!connected) {
     connecting_ = true;
@@ -88,28 +67,14 @@ ConnectionImpl::ConnectionImpl(Event::Dispatcher& dispatcher, int fd,
   // We never ask for both early close and read at the same time. If we are reading, we want to
   // consume all available data.
   file_event_ = dispatcher_.createFileEvent(
-      fd_, [this](uint32_t events) -> void { onFileEvent(events); }, Event::FileTriggerType::Edge,
+      fd(), [this](uint32_t events) -> void { onFileEvent(events); }, Event::FileTriggerType::Edge,
       Event::FileReadyType::Read | Event::FileReadyType::Write);
-
-  if (bind_to_address != nullptr) {
-    int rc = bind_to_address->bind(fd);
-    if (rc < 0) {
-      ENVOY_LOG_MISC(debug, "Bind failure. Failed to bind to {}: {}", bind_to_address->asString(),
-                     strerror(errno));
-      // Set a special error state to ensure asynchronous close to give the owner of the
-      // ConnectionImpl a chance to add callbacks and detect the "disconnect"
-      bind_error_ = true;
-
-      // Trigger a write event to close this connection out-of-band.
-      file_event_->activate(Event::FileReadyType::Write);
-    }
-  }
 
   transport_socket_->setTransportSocketCallbacks(*this);
 }
 
 ConnectionImpl::~ConnectionImpl() {
-  ASSERT(fd_ == -1);
+  ASSERT(fd() == -1);
 
   // In general we assume that owning code has called close() previously to the destructor being
   // run. This generally must be done so that callbacks run in the correct context (vs. deferred
@@ -131,7 +96,7 @@ void ConnectionImpl::addReadFilter(ReadFilterSharedPtr filter) {
 bool ConnectionImpl::initializeReadFilters() { return filter_manager_.initializeReadFilters(); }
 
 void ConnectionImpl::close(ConnectionCloseType type) {
-  if (fd_ == -1) {
+  if (fd() == -1) {
     return;
   }
 
@@ -156,7 +121,7 @@ void ConnectionImpl::close(ConnectionCloseType type) {
 }
 
 Connection::State ConnectionImpl::state() const {
-  if (fd_ == -1) {
+  if (fd() == -1) {
     return State::Closed;
   } else if (close_with_flush_) {
     return State::Closing;
@@ -166,7 +131,7 @@ Connection::State ConnectionImpl::state() const {
 }
 
 void ConnectionImpl::closeSocket(ConnectionEvent close_type) {
-  if (fd_ == -1) {
+  if (fd() == -1) {
     return;
   }
 
@@ -179,8 +144,7 @@ void ConnectionImpl::closeSocket(ConnectionEvent close_type) {
   connection_stats_.reset();
 
   file_event_.reset();
-  ::close(fd_);
-  fd_ = -1;
+  socket_->close();
 
   raiseEvent(close_type);
 }
@@ -195,14 +159,14 @@ void ConnectionImpl::noDelay(bool enable) {
   // invalid. For this call instead of plumbing through logic that will immediately indicate that a
   // connect failed, we will just ignore the noDelay() call if the socket is invalid since error is
   // going to be raised shortly anyway and it makes the calling code simpler.
-  if (fd_ == -1) {
+  if (fd() == -1) {
     return;
   }
 
   // Don't set NODELAY for unix domain sockets
   sockaddr addr;
   socklen_t len = sizeof(addr);
-  int rc = getsockname(fd_, &addr, &len);
+  int rc = getsockname(fd(), &addr, &len);
   RELEASE_ASSERT(rc == 0);
 
   if (addr.sa_family == AF_UNIX) {
@@ -211,7 +175,7 @@ void ConnectionImpl::noDelay(bool enable) {
 
   // Set NODELAY
   int new_value = enable;
-  rc = setsockopt(fd_, IPPROTO_TCP, TCP_NODELAY, &new_value, sizeof(new_value));
+  rc = setsockopt(fd(), IPPROTO_TCP, TCP_NODELAY, &new_value, sizeof(new_value));
 #ifdef __APPLE__
   if (-1 == rc && errno == EINVAL) {
     // Sometimes occurs when the connection is not yet fully formed. Empirically, TCP_NODELAY is
@@ -409,7 +373,7 @@ void ConnectionImpl::onFileEvent(uint32_t events) {
 
   // It's possible for a write event callback to close the socket (which will cause fd_ to be -1).
   // In this case ignore write event processing.
-  if (fd_ != -1 && (events & Event::FileReadyType::Read)) {
+  if (fd() != -1 && (events & Event::FileReadyType::Read)) {
     onReadReady();
   }
 }
@@ -441,7 +405,7 @@ void ConnectionImpl::onWriteReady() {
   if (connecting_) {
     int error;
     socklen_t error_size = sizeof(error);
-    int rc = getsockopt(fd_, SOL_SOCKET, SO_ERROR, &error, &error_size);
+    int rc = getsockopt(fd(), SOL_SOCKET, SO_ERROR, &error, &error_size);
     ASSERT(0 == rc);
     UNREFERENCED_PARAMETER(rc);
 
@@ -478,36 +442,10 @@ void ConnectionImpl::onWriteReady() {
       cb(result.bytes_processed_);
 
       // If a callback closes the socket, stop iterating.
-      if (fd_ == -1) {
+      if (fd() == -1) {
         return;
       }
     }
-  }
-}
-
-void ConnectionImpl::doConnect() {
-  ENVOY_CONN_LOG(debug, "connecting to {}", *this, remote_address_->asString());
-  int rc = remote_address_->connect(fd_);
-  if (rc == 0) {
-    // write will become ready.
-    ASSERT(connecting_);
-  } else {
-    ASSERT(rc == -1);
-    if (errno == EINPROGRESS) {
-      ASSERT(connecting_);
-      ENVOY_CONN_LOG(debug, "connection in progress", *this);
-    } else {
-      // read/write will become ready.
-      immediate_connection_error_ = true;
-      connecting_ = false;
-      ENVOY_CONN_LOG(debug, "immediate connection error: {}", *this, errno);
-    }
-  }
-
-  // The local address can only be retrieved for IP connections. Other
-  // types, such as UDS, don't have a notion of a local address.
-  if (remote_address_->type() == Address::Type::Ip) {
-    local_address_ = Address::addressFromFd(fd_);
   }
 }
 
@@ -540,9 +478,48 @@ ClientConnectionImpl::ClientConnectionImpl(
     Event::Dispatcher& dispatcher, const Address::InstanceConstSharedPtr& remote_address,
     const Network::Address::InstanceConstSharedPtr& source_address,
     Network::TransportSocketPtr&& transport_socket)
-    : ConnectionImpl(dispatcher, remote_address->socket(Address::SocketType::Stream),
-                     remote_address, nullptr, source_address, std::move(transport_socket), false,
-                     false) {}
+    : ConnectionImpl(dispatcher, std::make_unique<ClientSocketImpl>(remote_address),
+                     std::move(transport_socket), false) {
+  if (source_address != nullptr) {
+    const int rc = source_address->bind(fd());
+    if (rc < 0) {
+      ENVOY_LOG_MISC(debug, "Bind failure. Failed to bind to {}: {}", source_address->asString(),
+                     strerror(errno));
+      // Set a special error state to ensure asynchronous close to give the owner of the
+      // ConnectionImpl a chance to add callbacks and detect the "disconnect"
+      bind_error_ = true;
+
+      // Trigger a write event to close this connection out-of-band.
+      file_event_->activate(Event::FileReadyType::Write);
+    }
+  }
+}
+
+void ClientConnectionImpl::connect() {
+  ENVOY_CONN_LOG(debug, "connecting to {}", *this, socket_->remoteAddress()->asString());
+  const int rc = socket_->remoteAddress()->connect(fd());
+  if (rc == 0) {
+    // write will become ready.
+    ASSERT(connecting_);
+  } else {
+    ASSERT(rc == -1);
+    if (errno == EINPROGRESS) {
+      ASSERT(connecting_);
+      ENVOY_CONN_LOG(debug, "connection in progress", *this);
+    } else {
+      // read/write will become ready.
+      immediate_connection_error_ = true;
+      connecting_ = false;
+      ENVOY_CONN_LOG(debug, "immediate connection error: {}", *this, errno);
+    }
+  }
+
+  // The local address can only be retrieved for IP connections. Other
+  // types, such as UDS, don't have a notion of a local address.
+  if (socket_->remoteAddress()->type() == Address::Type::Ip) {
+    socket_->setLocalAddress(Address::addressFromFd(fd()));
+  }
+}
 
 } // namespace Network
 } // namespace Envoy

commit 329daea09c0d866cc73382f5c1d327249714ca6c

listen_socket: Remove default value from virtual function parameter.
We should not have default parameter values on virtual functions, but
this one slipped through.

Fixes: eb02089b03 ("Listener: Add listener filters. (#2346)")
Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

include/envoy/network/listen_socket.h
@@ -96,7 +96,7 @@ public:
    *        actually different when passing restored as 'true'.
    */
   virtual void setLocalAddress(const Address::InstanceConstSharedPtr& local_address,
-                               bool restored = false) PURE;
+                               bool restored) PURE;
 
   /**
    * Set the remote address of the socket.

source/common/filter/listener/proxy_protocol.cc
@@ -110,7 +110,7 @@ void Instance::onReadWorker() {
       throw EnvoyException("failed to read proxy protocol");
     }
 
-    socket.setLocalAddress(local_address);
+    socket.setLocalAddress(local_address, false);
     socket.setRemoteAddress(remote_address);
   }
 

source/common/network/connection_impl.cc
@@ -579,7 +579,7 @@ void ClientConnectionImpl::connect() {
   // The local address can only be retrieved for IP connections. Other
   // types, such as UDS, don't have a notion of a local address.
   if (socket_->remoteAddress()->type() == Address::Type::Ip) {
-    socket_->setLocalAddress(Address::addressFromFd(fd()));
+    socket_->setLocalAddress(Address::addressFromFd(fd()), false);
   }
 }
 