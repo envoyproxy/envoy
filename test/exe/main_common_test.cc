4d2bc121bf5e236cd41eeef33578f630197614d4
Joshua Marantz <jmarantz@google.com>
2018-02-09 12:09:08 -0500

    Use TEST_RUNDIR rather than TEST_SRCDIR.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/exe/main_common_test.cc
+++ test/exe/main_common_test.cc
@@ -7,8 +7,8 @@
 namespace Envoy {
 
 TEST(MainCommon, ConstructDestruct) {
-  std::string config_file = Envoy::TestEnvironment::getCheckedEnvVar("TEST_SRCDIR") +
-                            "/envoy/configs/google_com_proxy.json";
+  std::string config_file =
+      Envoy::TestEnvironment::getCheckedEnvVar("TEST_RUNDIR") + "/configs/google_com_proxy.json";
   const char* argv[] = {"envoy-static", "-c", config_file.c_str(), nullptr};
   MainCommon main_common(3, argv, false);
 }
