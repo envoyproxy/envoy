845cf3377ec34587077cf9530a5c04ced5703ed2
Harvey Tuch <htuch@google.com>
2018-02-14 21:42:54 -0500

    Revert "server: factor out MainCommon as a class, with a run method (#2568)"
    This reverts commit deffec6a177bdb00586d1d7682aca5dbb395e68a, which was
    causing CI to flake today in TSAN runs. The proximate root cause is
    the change in #2568, which appears not to correctly setup logging in
    --mode=validate. This in turn leads to some log related races that I
    don't fully grok.
    
    Rolling back to unblock CI, we should make sure hotrestart_test passes
    TSAN with high confidence before rolling forward again.
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- source/exe/main_common.h
+++ source/exe/main_common.h
@@ -1,59 +1,8 @@
 #pragma once
 
-#include "common/stats/thread_local_store.h"
-#include "common/thread_local/thread_local_impl.h"
-
 #include "server/options_impl.h"
-#include "server/server.h"
-
-#ifdef ENVOY_HANDLE_SIGNALS
-#include "exe/signal_action.h"
-#endif
 
 namespace Envoy {
-
-class ProdComponentFactory : public Server::ComponentFactory {
-public:
-  // Server::DrainManagerFactory
-  Server::DrainManagerPtr createDrainManager(Server::Instance& server) override;
-  Runtime::LoaderPtr createRuntime(Server::Instance& server,
-                                   Server::Configuration::Initial& config) override;
-};
-
-class MainCommonBase {
-public:
-  MainCommonBase(OptionsImpl& options, bool hot_restart);
-  ~MainCommonBase();
-
-  bool run();
-
-protected:
-  Envoy::OptionsImpl& options_;
-  ProdComponentFactory component_factory_;
-  DefaultTestHooks default_test_hooks_;
-  std::unique_ptr<ThreadLocal::InstanceImpl> tls_;
-  std::unique_ptr<Server::HotRestart> restarter_;
-  std::unique_ptr<Stats::ThreadLocalStoreImpl> stats_store_;
-  std::unique_ptr<Server::InstanceImpl> server_;
-};
-
-class MainCommon {
-public:
-  MainCommon(int argc, char** argv, bool hot_restart);
-  bool run() { return base_.run(); }
-
-private:
-  static std::unique_ptr<Envoy::OptionsImpl> computeOptions(int argc, char** argv,
-                                                            bool hot_restart);
-
-#ifdef ENVOY_HANDLE_SIGNALS
-  Envoy::SignalAction handle_sigs;
-#endif
-
-  std::unique_ptr<Envoy::OptionsImpl> options_;
-  MainCommonBase base_;
-};
-
 /**
  * This is the real main body that executes after site-specific
  * main() runs.
@@ -61,6 +10,6 @@ private:
  * @param options Options object initialized by site-specific code
  * @return int Return code that should be returned from the actual main()
  */
-int main_common(OptionsImpl& options);
+int main_common(Envoy::OptionsImpl& options);
 
 } // namespace Envoy
