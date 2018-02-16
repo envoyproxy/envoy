7d7e613d8cacb4fd7aa6e73145d858c17df87315
Dhi Aurrahman <dio@rockybars.com>
2018-02-16 07:00:19 +0700

    Refactor shadow writer implementation
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- source/common/router/shadow_writer_impl.cc
+++ source/common/router/shadow_writer_impl.cc
@@ -6,7 +6,6 @@
 #include "common/common/assert.h"
 #include "common/http/headers.h"
 
-#include "absl/strings/match.h"
 #include "absl/strings/str_join.h"
 
 namespace Envoy {
@@ -14,18 +13,14 @@ namespace Router {
 
 void ShadowWriterImpl::shadow(const std::string& cluster, Http::MessagePtr&& request,
                               std::chrono::milliseconds timeout) {
-  ASSERT(request->headers().Host()->value().c_str() != "");
-
-  // Switch authority to add a shadow postfix. This allows upstream logging to make a more sense.
-  if (absl::StrContains(request->headers().Host()->value().c_str(), ":")) {
-    auto parts = StringUtil::splitToken(request->headers().Host()->value().c_str(), ":");
-    ASSERT(parts.size() == 2);
-    request->headers().Host()->value(absl::StrJoin(parts, "-shadow:"));
-  } else {
-    request->headers().Host()->value(absl::StrJoin(
-        std::forward_as_tuple(request->headers().Host()->value().c_str(), "-shadow"), ""));
-  }
-
+  ASSERT(!request->headers().Host()->value().empty());
+  // Switch authority to add a shadow postfix. This allows upstream logging to make a more
+  // sense.
+  auto parts = StringUtil::splitToken(request->headers().Host()->value().c_str(), ":");
+  ASSERT(parts.size() > 0 && parts.size() <= 2);
+  request->headers().Host()->value(
+      parts.size() == 2 ? absl::StrJoin(parts, "-shadow:")
+                        : absl::StrCat(request->headers().Host()->value().c_str(), "-shadow"));
   // Configuration should guarantee that cluster exists before calling here. This is basically
   // fire and forget. We don't handle cancelling.
   cm_.httpAsyncClientForCluster(cluster).send(std::move(request), *this,
