ebefe2fd673486d70b7f9ed1a3cb2fb487147f97
Ryan Burn <ryan.burn@gmail.com>
2018-02-22 14:20:22 -0500

    Fix coverage.
    Signed-off-by: Ryan Burn <ryan.burn@gmail.com>

--- source/common/tracing/dynamic_opentracing_driver_impl.cc
+++ source/common/tracing/dynamic_opentracing_driver_impl.cc
@@ -12,23 +12,15 @@ DynamicOpenTracingDriver::DynamicOpenTracingDriver(Stats::Store& stats, const st
   opentracing::expected<opentracing::DynamicTracingLibraryHandle> library_handle_maybe =
       opentracing::DynamicallyLoadTracingLibrary(library.c_str(), error_message);
   if (!library_handle_maybe) {
-    if (error_message.empty()) {
-      throw EnvoyException{fmt::format("{}", library_handle_maybe.error().message())};
-    } else {
-      throw EnvoyException{
-          fmt::format("{}: {}", library_handle_maybe.error().message(), error_message)};
-    }
+    throw EnvoyException{
+        fmt::format("{}: {}", library_handle_maybe.error().message(), error_message)};
   }
   library_handle_ = std::move(*library_handle_maybe);
 
   opentracing::expected<std::shared_ptr<opentracing::Tracer>> tracer_maybe =
       library_handle_.tracer_factory().MakeTracer(tracer_config.c_str(), error_message);
   if (!tracer_maybe) {
-    if (error_message.empty()) {
-      throw EnvoyException{fmt::format("{}", tracer_maybe.error().message())};
-    } else {
-      throw EnvoyException{fmt::format("{}: {}", tracer_maybe.error().message(), error_message)};
-    }
+    throw EnvoyException{fmt::format("{}: {}", tracer_maybe.error().message(), error_message)};
   }
   tracer_ = std::move(*tracer_maybe);
   RELEASE_ASSERT(tracer_ != nullptr);
