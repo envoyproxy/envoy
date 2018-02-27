7e149eea87e6c86c6e336fa5c3a7ba30b643e630
Joshua Marantz <jmarantz@google.com>
2018-02-26 20:03:46 -0500

    Unit-test both with hot restart enabled and disabled.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/exe/main_common_test.cc
+++ test/exe/main_common_test.cc
@@ -25,14 +25,25 @@
 
 namespace Envoy {
 
-TEST(MainCommon, ConstructDestruct) {
+TEST(MainCommon, ConstructDestructHotRestartEnabled) {
   if (!Envoy::TestEnvironment::shouldRunTestForIpVersion(Network::Address::IpVersion::v4)) {
     return;
   }
   std::string config_file = Envoy::TestEnvironment::getCheckedEnvVar("TEST_RUNDIR") +
                             "/test/config/integration/google_com_proxy_port_0.v2.yaml";
-  const char* argv[] = {"envoy-static", "-c", config_file.c_str(), nullptr};
-  MainCommon main_common(3, const_cast<char**>(argv));
+  const char* argv[] = {"envoy-static", "-c", config_file.c_str(), "--base-id", "1", nullptr};
+  MainCommon main_common(ARRAY_SIZE(argv) - 1, const_cast<char**>(argv));
+}
+
+TEST(MainCommon, ConstructDestructHotRestartDisabled) {
+  if (!Envoy::TestEnvironment::shouldRunTestForIpVersion(Network::Address::IpVersion::v4)) {
+    return;
+  }
+  std::string config_file = Envoy::TestEnvironment::getCheckedEnvVar("TEST_RUNDIR") +
+                            "/test/config/integration/google_com_proxy_port_0.v2.yaml";
+  const char* argv[] = {"envoy-static",          "-c",   config_file.c_str(), "--base-id", "2",
+                        "--disable-hot-restart", nullptr};
+  MainCommon main_common(ARRAY_SIZE(argv) - 1, const_cast<char**>(argv));
 }
 
 TEST(MainCommon, LegacyMain) {
