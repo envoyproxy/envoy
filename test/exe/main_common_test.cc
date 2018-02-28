d72ac0651dcd8e3494b6aae609e4e0a97e4b9d62
Joshua Marantz <jmarantz@google.com>
2018-02-28 17:55:32 -0500

    Use EXPECT_NO_THROW to more clearly show the expectation when constructing MainCommon.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/exe/main_common_test.cc
+++ test/exe/main_common_test.cc
@@ -32,7 +32,7 @@ TEST(MainCommon, ConstructDestructHotRestartEnabled) {
   std::string config_file = Envoy::TestEnvironment::getCheckedEnvVar("TEST_RUNDIR") +
                             "/test/config/integration/google_com_proxy_port_0.v2.yaml";
   const char* argv[] = {"envoy-static", "-c", config_file.c_str(), "--base-id", "1", nullptr};
-  MainCommon main_common(ARRAY_SIZE(argv) - 1, const_cast<char**>(argv));
+  EXPECT_NO_THROW(MainCommon main_common(ARRAY_SIZE(argv) - 1, const_cast<char**>(argv)));
 }
 
 TEST(MainCommon, ConstructDestructHotRestartDisabled) {
@@ -43,7 +43,7 @@ TEST(MainCommon, ConstructDestructHotRestartDisabled) {
                             "/test/config/integration/google_com_proxy_port_0.v2.yaml";
   const char* argv[] = {"envoy-static",          "-c",   config_file.c_str(), "--base-id", "2",
                         "--disable-hot-restart", nullptr};
-  MainCommon main_common(ARRAY_SIZE(argv) - 1, const_cast<char**>(argv));
+  EXPECT_NO_THROW(MainCommon main_common(ARRAY_SIZE(argv) - 1, const_cast<char**>(argv)));
 }
 
 TEST(MainCommon, LegacyMain) {
