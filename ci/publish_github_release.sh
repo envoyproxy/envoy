557e8ae3a0e80d98af79dfae705538c4026a77a0
Nicholas Johns <nicholas.a.johns5@gmail.com>
2018-02-28 17:27:54 -0500

    only attach a file to an existing release
    releases are done manually today through the github interface
    so we should only upload a file to the existing release.
    
    Signed-off-by: Nicholas Johns <nicholas.a.johns5@gmail.com>

--- ci/publish_github_release.sh
+++ ci/publish_github_release.sh
@@ -34,5 +34,4 @@ tar -xvjpf /tmp/ghrelease.tar.bz2 -C /tmp
 cp /tmp/bin/linux/amd64/github-release /usr/local/bin/ghrelease
 chmod +x /usr/local/bin/ghrelease
 
-ghrelease release --tag "${CIRCLE_TAG:-}" --name "${CIRCLE_TAG:-}"
 ghrelease upload --tag "${CIRCLE_TAG:-}" --name "envoy-linux-amd64" --file "${ENVOY_SRCDIR}/build_release_stripped/envoy"
