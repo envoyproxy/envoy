9ee83f69028131b01d92403611e5950d8c041fc9
Nicholas Johns <nicholas.a.johns5@gmail.com>
2018-02-28 16:53:01 -0500

    bugfix: ensure we only run when CIRCLE_TAG is populated
    Signed-off-by: Nicholas Johns <nicholas.a.johns5@gmail.com>

--- ci/publish_github_release.sh
+++ ci/publish_github_release.sh
@@ -24,19 +24,17 @@ if [[ -z "${GITHUB_REPO:-}" ]]; then
     exit 1
 fi
 
-if [[ -n "${CIRCLE_TAG:-}" ]]; then
-    echo "skipping tag events"
+if [[ -z "${CIRCLE_TAG:-}" ]]; then
+    echo "skipping non tag events"
     exit 0
 fi
-
-TAG=$(git describe --abbrev=0 --tags)
 
 wget https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2 -O /tmp/ghrelease.tar.bz2
 tar -xvjpf /tmp/ghrelease.tar.bz2 -C /tmp
 cp /tmp/bin/linux/amd64/github-release /usr/local/bin/ghrelease
 chmod +x /usr/local/bin/ghrelease
 
-if [[ -n "${TAG:-}" ]]; then
-    ghrelease release --tag "${TAG:-}" --name "${TAG:-}"
-    ghrelease upload --tag "${TAG:-}" --name "envoy-linux-amd64" --file "${ENVOY_SRCDIR}/build_release_stripped/envoy"
+if [[ -n "${CIRCLE_TAG:-}" ]]; then
+    ghrelease release --tag "${CIRCLE_TAG:-}" --name "${CIRCLE_TAG:-}"
+    ghrelease upload --tag "${CIRCLE_TAG:-}" --name "envoy-linux-amd64" --file "${ENVOY_SRCDIR}/build_release_stripped/envoy"
 fi
