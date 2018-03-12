7c530fcc547d39477c2bc10d95b57bd13c01efe5
Rama <rama.rao@salesforce.com>
2018-03-12 16:18:40 +0530

    added cleanup code
    Signed-off-by: Rama <rama.rao@salesforce.com>

--- test/integration/http2_integration_test.cc
+++ test/integration/http2_integration_test.cc
@@ -290,6 +290,13 @@ void Http2IntegrationTest::simultaneousRequestsWithIdleTimeout() {
   // Do not send any requests and validate idle timeout kicks in after both the requests are done.
   test_server_->waitForCounterGe("cluster.cluster_0.upstream_cx_idle_timeout", 1);
   test_server_->waitForCounterGe("cluster.cluster_0.upstream_cx_total", 0);
+
+  // Cleanup both downstream and upstream
+  codec_client_->close();
+  fake_upstream_connection1->close();
+  fake_upstream_connection1->waitForDisconnect();
+  fake_upstream_connection2->close();
+  fake_upstream_connection2->waitForDisconnect();
 }
 
 // Interleave two requests and responses and make sure the HTTP2 stack handles this correctly.
