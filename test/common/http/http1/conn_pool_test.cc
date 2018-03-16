37ff19a74ba711c1a8aff51f7505dbf0fccfddbc
Rama <rama.rao@salesforce.com>
2018-03-16 19:21:43 +0530

    fixed spaces
    Signed-off-by: Rama <rama.rao@salesforce.com>

--- test/common/http/http1/conn_pool_test.cc
+++ test/common/http/http1/conn_pool_test.cc
@@ -129,6 +129,7 @@ struct ActiveTestRequest {
 
   ActiveTestRequest(Http1ConnPoolImplTest& parent, size_t client_index, Type type)
       : parent_(parent), client_index_(client_index) {
+   
     if (type == Type::CreateConnection) {
       parent.conn_pool_.expectClientCreate();
     }
--- test/mocks/upstream/cluster_info.cc
+++ test/mocks/upstream/cluster_info.cc
@@ -27,6 +27,7 @@ MockClusterInfo::MockClusterInfo()
       transport_socket_factory_(new Network::RawBufferSocketFactory),
       load_report_stats_(ClusterInfoImpl::generateLoadReportStats(load_report_stats_store_)),
       resource_manager_(new Upstream::ResourceManagerImpl(runtime_, "fake_key", 1, 1024, 1024, 1)) {
+  
   ON_CALL(*this, connectTimeout()).WillByDefault(Return(std::chrono::milliseconds(1)));
   ON_CALL(*this, idleTimeout()).WillByDefault(Return(idle_timeout_));
   ON_CALL(*this, name()).WillByDefault(ReturnRef(name_));
