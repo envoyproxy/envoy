59cac9527d79dabc8c0df23fdfe825b6d1b7fb2d
Rama <rama.rao@salesforce.com>
2018-03-17 17:55:45 +0530

    added local close
    Signed-off-by: Rama <rama.rao@salesforce.com>

--- test/integration/http_integration.cc
+++ test/integration/http_integration.cc
@@ -28,11 +28,11 @@
 
 #include "gtest/gtest.h"
 
+using testing::_;
 using testing::AnyNumber;
 using testing::HasSubstr;
 using testing::Invoke;
 using testing::Not;
-using testing::_;
 
 namespace Envoy {
 
@@ -147,6 +147,9 @@ void IntegrationCodecClient::ConnectionCallbacks::onEvent(Network::ConnectionEve
     parent_.connected_ = true;
     parent_.connection_->dispatcher().exit();
   } else if (event == Network::ConnectionEvent::RemoteClose) {
+    parent_.disconnected_ = true;
+    parent_.connection_->dispatcher().exit();
+  } else if (event == Network::ConnectionEvent::LocalClose) {
     parent_.disconnected_ = true;
     parent_.connection_->dispatcher().exit();
   }
@@ -913,6 +916,7 @@ void HttpIntegrationTest::testIdleTimerDisabled() {
 
   // Close the Codec Client and validate Idle timer is also disabled.
   codec_client_->close();
+  codec_client_->waitForDisconnect();
   EXPECT_EQ(test_server_->counter("cluster.cluster_0.upstream_cx_idle_timeout")->value(), 0);
 }
 
