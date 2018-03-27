c400ea3165d37d109b43f00dcac0a672d308c34f
Nicolas Thiebaud <nicothieb@google.com>
2018-03-26 17:18:47 -0700

    Fixing test.
    Signed-off-by: Nicolas Thiebaud <nicothieb@google.com>

--- test/mocks/server/mocks.cc
+++ test/mocks/server/mocks.cc
@@ -21,6 +21,7 @@ namespace Server {
 MockOptions::MockOptions(const std::string& config_path)
     : config_path_(config_path), admin_address_path_("") {
   ON_CALL(*this, configPath()).WillByDefault(ReturnRef(config_path_));
+  ON_CALL(*this, configYaml()).WillByDefault(ReturnRef(""));
   ON_CALL(*this, v2ConfigOnly()).WillByDefault(Invoke([this] { return v2_config_only_; }));
   ON_CALL(*this, adminAddressPath()).WillByDefault(ReturnRef(admin_address_path_));
   ON_CALL(*this, serviceClusterName()).WillByDefault(ReturnRef(service_cluster_name_));
