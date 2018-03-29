0382c7ed8ad585e16cd4aa7773845ac3d74df1e4
Joshua Marantz <jmarantz@google.com>
2018-03-29 16:40:06 -0400

    Add RAII log-level set/restore, so we don't affect the rest of the binary when tweaking flags.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

---
+++ test/test_common/logging.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include "common/common/logger.h"
+
+#include "spdlog/spdlog.h"
+
+namespace Envoy {
+
+/**
+ * Provides a mechanism to temporarily set the logging level on
+ * construction, restoring its previous state on dstruction.
+ */
+class LogLevelSetter {
+public:
+  explicit LogLevelSetter(spdlog::level::level_enum log_level);
+  ~LogLevelSetter();
+
+private:
+  std::vector<spdlog::level::level_enum> previous_levels_;
+};
+
+/**
+ * Records log messages in a vector<string>, forwarding them to the previous
+ * delegate. This is useful for unit-testing log messages while still being able
+ * to see them on stderr.
+ */
+class LogRecordingSink : public Logger::SinkDelegate {
+public:
+  LogRecordingSink();
+  virtual ~LogRecordingSink();
+
+  // Logger::SinkDelgate
+  void log(absl::string_view msg) override;
+  void flush() override;
+
+  const std::vector<std::string>& messages() const { return messages_; }
+
+private:
+  std::vector<std::string> messages_;
+};
+
+} // namespace Envoy
