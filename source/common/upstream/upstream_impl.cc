2a2123d054415872df6ed8982e9063dc74722c9e
Harvey Tuch <htuch@google.com>
2018-03-29 10:09:07 -0400

    Fix upstream_impl_test TSAN fail.
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- source/common/upstream/upstream_impl.cc
+++ source/common/upstream/upstream_impl.cc
@@ -144,7 +144,7 @@ double HostSetImpl::effectiveLocalityWeight(uint32_t index) const {
   ASSERT(hosts_per_locality_ != nullptr);
   const auto& locality_hosts = hosts_per_locality_->get()[index];
   const auto& locality_healthy_hosts = healthy_hosts_per_locality_->get()[index];
-  ASSERT(!locality_healthy_hosts.empty());
+  ASSERT(!locality_hosts.empty());
   const double locality_healthy_ratio = 1.0 * locality_healthy_hosts.size() / locality_hosts.size();
   const uint32_t weight = (*locality_weights_)[index];
   // Health ranges from 0-1.0, and is the ratio of healthy hosts to total hosts, modified by the
