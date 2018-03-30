c4bc69fdb9d4d184d71e4afdf01a99510ceef3fa
Harvey Tuch <htuch@google.com>
2018-03-30 11:07:18 -0400

    Cleanup EDS related tests.
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- test/integration/eds_integration_test.cc
+++ test/integration/eds_integration_test.cc
@@ -1,7 +1,6 @@
 #include "envoy/api/v2/eds.pb.h"
 
-#include "common/config/resources.h"
-
+#include "test/config/utility.h"
 #include "test/integration/http_integration.h"
 #include "test/test_common/network_utility.h"
 
@@ -26,61 +25,30 @@ public:
 
     for (uint32_t i = 0; i < total_endpoints; ++i) {
       auto* endpoint = locality_lb_endpoints->add_lb_endpoints();
-      auto* socket_address =
-          endpoint->mutable_endpoint()->mutable_address()->mutable_socket_address();
-      socket_address->set_address(Network::Test::getLoopbackAddressString(GetParam()));
-      socket_address->set_port_value(fake_upstreams_[i]->localAddress()->ip()->port());
+      setUpstreamAddress(i, *endpoint);
       if (i >= healthy_endpoints) {
         endpoint->set_health_status(envoy::api::v2::core::HealthStatus::UNHEALTHY);
       }
     }
-
-    // Write to file the DiscoveryResponse and trigger inotify watch.
-    envoy::api::v2::DiscoveryResponse eds_response;
-    eds_response.set_version_info(std::to_string(eds_version_++));
-    eds_response.set_type_url(Config::TypeUrl::get().ClusterLoadAssignment);
-    eds_response.add_resources()->PackFrom(cluster_load_assignment);
-    // Past the initial write, need move semantics to trigger inotify move event that the
-    // FilesystemSubscriptionImpl is subscribed to.
-    if (eds_path_.empty()) {
-      eds_path_ =
-          TestEnvironment::writeStringToFileForTest("eds.pb_text", eds_response.DebugString());
-      // cluster.cluster_0.update_success will be incremented on the initial
-      // load when Envoy comes up.
-      ++update_successes_;
-    } else {
-      std::string path = TestEnvironment::writeStringToFileForTest("eds.update.pb_text",
-                                                                   eds_response.DebugString());
-      ASSERT_EQ(0, ::rename(path.c_str(), eds_path_.c_str()));
-      // Make sure Envoy has consumed the update now that it is running.
-      test_server_->waitForCounterGe("cluster.cluster_0.update_success", ++update_successes_);
-      EXPECT_EQ(update_successes_,
-                test_server_->counter("cluster.cluster_0.update_success")->value());
-    }
+    eds_helper_.setEds({cluster_load_assignment}, *test_server_);
   }
 
-  void createUpstreams() override {}
-
   void initialize() override {
-    setEndpoints(0, 0);
+    setUpstreamCount(3);
+    eds_helper_.setEds({}, *test_server_);
     config_helper_.addConfigModifier([this](envoy::config::bootstrap::v2::Bootstrap& bootstrap) {
       // Switch predefined cluster_0 to EDS filesystem sourcing.
       auto* cluster_0 = bootstrap.mutable_static_resources()->mutable_clusters(0);
       cluster_0->mutable_hosts()->Clear();
       cluster_0->set_type(envoy::api::v2::Cluster::EDS);
       auto* eds_cluster_config = cluster_0->mutable_eds_cluster_config();
-      eds_cluster_config->mutable_eds_config()->set_path(eds_path_);
+      eds_cluster_config->mutable_eds_config()->set_path(eds_helper_.eds_path());
     });
     HttpIntegrationTest::initialize();
-    for (uint32_t i = 0; i < upstream_endpoints_; ++i) {
-      fake_upstreams_.emplace_back(new FakeUpstream(0, FakeHttpConnection::Type::HTTP1, version_));
-    }
+    setEndpoints(0, 0);
   }
 
-  static constexpr uint32_t upstream_endpoints_{5};
-  std::string eds_path_;
-  uint32_t eds_version_{};
-  uint32_t update_successes_{};
+  EdsHelper eds_helper_;
 };
 
 INSTANTIATE_TEST_CASE_P(IpVersions, EdsIntegrationTest,
