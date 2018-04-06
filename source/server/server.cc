661a7e3df8582ddddda9cdc54c29ff7f6f98c8bf
Matt Klein <mklein@lyft.com>
2018-04-06 12:46:02 -0400

    fix
    Signed-off-by: Matt Klein <mklein@lyft.com>

--- source/server/server.cc
+++ source/server/server.cc
@@ -202,7 +202,7 @@ void InstanceImpl::initialize(Options& options,
   ENVOY_LOG(info, "  stat_sinks: {}",
             Registry::FactoryRegistry<Configuration::StatsSinkFactory>::allFactoryNames());
   ENVOY_LOG(info, "  tracers: {}",
-            Registry::FactoryRegistry<Configuration::TracerFactory>::allFactoryNames());
+            Registry::FactoryRegistry<Configuration::HttpTracerFactory>::allFactoryNames());
   ENVOY_LOG(info, "  transport_sockets.downstream: {}",
             Registry::FactoryRegistry<
                 Configuration::DownstreamTransportSocketConfigFactory>::allFactoryNames());
