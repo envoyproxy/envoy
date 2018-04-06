9739cfbfa122766b14f2d0de7306950711a54a4b
htuch <htuch@users.noreply.github.com>
2018-04-06 16:49:10 -0400

    upstream: locality weighted maglev load balancing. (#3004)
    This follows up from #2982 and adds the ability to weight at the locality level when performing
    Maglev LB. Unlike the non-affinity case, we don't do a two level LB (pick locality, pick host), but
    use a single Maglev table. This reduces memory cost when there are few localities relative to hosts,
    at the expense of reduced granularity. Ring hash LB is explicitly not supported for weighted,
    locality or otherwise.
    
    Fixes #2725.
    
    Risk Level: Low.
    Testing: Additional unit tests for locality weighted in maglev_lb_test.
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- source/common/upstream/maglev_lb.cc
+++ source/common/upstream/maglev_lb.cc
@@ -3,38 +3,59 @@
 namespace Envoy {
 namespace Upstream {
 
-MaglevTable::MaglevTable(const HostVector& hosts, uint64_t table_size) : table_size_(table_size) {
+MaglevTable::MaglevTable(const HostsPerLocality& hosts_per_locality,
+                         const LocalityWeightsConstSharedPtr& locality_weights, uint64_t table_size)
+    : table_size_(table_size) {
   // TODO(mattklein123): The Maglev table must have a size that is a prime number for the algorithm
   // to work. Currently, the table size is not user configurable. In the future, if the table size
   // is made user configurable, we will need proper error checking that the user cannot configure a
   // size that is not prime (the result is going to be an infinite loop with some inputs which is
   // not good!).
   ASSERT(Primes::isPrime(table_size));
-  if (hosts.empty()) {
-    return;
-  }
+
+  // Compute host weight combined with locality weight where applicable.
+  const auto effective_weight = [&locality_weights](uint32_t host_weight,
+                                                    uint32_t locality_index) -> uint32_t {
+    if (locality_weights == nullptr || locality_weights->empty()) {
+      return host_weight;
+    } else {
+      return host_weight * (*locality_weights)[locality_index];
+    }
+  };
 
   // Compute maximum host weight. If this is zero, we are doing unweighted Maglev.
   uint32_t max_host_weight = 0;
-  for (const auto& host : hosts) {
-    max_host_weight = std::max(host->weight(), max_host_weight);
+  uint32_t total_hosts = 0;
+  for (uint32_t i = 0; i < hosts_per_locality.get().size(); ++i) {
+    for (const auto& host : hosts_per_locality.get()[i]) {
+      max_host_weight = std::max(effective_weight(host->weight(), i), max_host_weight);
+      ++total_hosts;
+    }
+  }
+
+  // We can't do anything sensible with no hosts.
+  if (total_hosts == 0) {
+    return;
   }
 
   // Implementation of pseudocode listing 1 in the paper (see header file for more info).
   std::vector<TableBuildEntry> table_build_entries;
-  table_build_entries.reserve(hosts.size());
-  for (const auto& host : hosts) {
-    const std::string& address = host->address()->asString();
-    table_build_entries.emplace_back(HashUtil::xxHash64(address) % table_size_,
-                                     (HashUtil::xxHash64(address, 1) % (table_size_ - 1)) + 1,
-                                     max_host_weight > 0 ? host->weight() : 0);
+  table_build_entries.reserve(total_hosts);
+  for (uint32_t i = 0; i < hosts_per_locality.get().size(); ++i) {
+    for (const auto& host : hosts_per_locality.get()[i]) {
+      const std::string& address = host->address()->asString();
+      table_build_entries.emplace_back(host, HashUtil::xxHash64(address) % table_size_,
+                                       (HashUtil::xxHash64(address, 1) % (table_size_ - 1)) + 1,
+                                       max_host_weight > 0 ? effective_weight(host->weight(), i)
+                                                           : 0);
+    }
   }
 
   table_.resize(table_size_);
   uint64_t table_index = 0;
   uint32_t iteration = 1;
   while (true) {
-    for (uint64_t i = 0; i < hosts.size(); i++) {
+    for (uint64_t i = 0; i < table_build_entries.size(); i++) {
       TableBuildEntry& entry = table_build_entries[i];
       // Only consider weight if we are doing weighted Maglev.
       if (max_host_weight > 0) {
@@ -54,7 +75,7 @@ MaglevTable::MaglevTable(const HostVector& hosts, uint64_t table_size) : table_s
         c = permutation(entry);
       }
 
-      table_[c] = hosts[i];
+      table_[c] = entry.host_;
       entry.next_++;
       table_index++;
       if (table_index == table_size_) {
