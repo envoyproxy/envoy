de34d96df562ef6cb634d98c91fb2ba2a9f64540
sri kailash <sri.gandebathula@booking.com>
2018-04-08 15:26:07 +0200

    Fixed formatting issues for envoy_front_proxy Signed-off-by: sri kailash <sri.gandebathula@booking.com>

--- configs/generate_v2_config.py
+++ configs/generate_v2_config.py
@@ -1,5 +1,5 @@
 #Import necessary functions from Jinja2 module
-from jinja2 import Environment, FileSystemLoader
+import jinja2
 import yaml
 import os
 SCRIPT_DIR = os.path.dirname(__file__)
@@ -47,20 +47,19 @@ mongos_servers = {
 }
 
 def generate_v2_config(template_path, template, output_file, **context):
-	env = Environment(loader = FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
-	template = env.get_template(template)
-	data = template.render(**context)
-	# with open(output_file, 'w') as outfile:
-		# yaml.dump(data, outfile, default_flow_style=False)
-	print data
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_path, followlinks=True),
+                             undefined=jinja2.StrictUndefined, keep_trailing_newline=True)	
+    template = env.get_template(template)
+    data = template.render(**context)
+    print data
 
 generate_v2_config(SCRIPT_DIR, 'envoy_front_proxy.template.yaml',
                        './envoy_front_proxy.yaml', clusters=front_envoy_clusters)
-generate_v2_config(SCRIPT_DIR, 'envoy_double_proxy.template.yaml',
-                        './envoy_double_proxy.yaml')
+# generate_v2_config(SCRIPT_DIR, 'envoy_double_proxy.template.yaml',
+                        # './envoy_double_proxy.yaml')
 
-generate_v2_config("./", 'envoy_service_to_service.template.yaml',
-                    'envoy_service_to_service.yaml',
-                internal_virtual_hosts=service_to_service_envoy_clusters,
-                external_virtual_hosts=external_virtual_hosts,
-                mongos_servers=mongos_servers)
+# generate_v2_config("./", 'envoy_service_to_service.template.yaml',
+                    # 'envoy_service_to_service.yaml',
+                # internal_virtual_hosts=service_to_service_envoy_clusters,
+                # external_virtual_hosts=external_virtual_hosts,
+                # mongos_servers=mongos_servers)
