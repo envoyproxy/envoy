04dcdcd1bc4c404ea0217dfa9975b8a8284b79b5
Greg Greenway <ggreenway@apple.com>
2018-04-13 13:10:58 -0700

    Use hand-coded mocks for the benchmark so that benchmark time isn't dominated by gmock calls.
    Signed-off-by: Greg Greenway <ggreenway@apple.com>

--- test/extensions/filters/listener/tls_inspector/tls_inspector_benchmark.cc
+++ test/extensions/filters/listener/tls_inspector/tls_inspector_benchmark.cc
@@ -25,6 +25,61 @@ namespace Extensions {
 namespace ListenerFilters {
 namespace TlsInspector {
 
+class FastMockListenerFilterCallbacks : public Network::MockListenerFilterCallbacks {
+public:
+  FastMockListenerFilterCallbacks(Network::ConnectionSocket& socket, Event::Dispatcher& dispatcher)
+      : socket_(socket), dispatcher_(dispatcher) {}
+  Network::ConnectionSocket& socket() override { return socket_; }
+  Event::Dispatcher& dispatcher() override { return dispatcher_; }
+  void continueFilterChain(bool success) override { RELEASE_ASSERT(success); }
+
+  Network::ConnectionSocket& socket_;
+  Event::Dispatcher& dispatcher_;
+};
+
+class FastMockConnectionSocket : public Network::MockConnectionSocket {
+public:
+  int fd() const override { return 42; }
+  void setRequestedServerName(absl::string_view server_name) override {
+    if (server_name == "example.com") {
+      got_server_name_ = true;
+    }
+  }
+
+  bool got_server_name_{false};
+};
+
+// Don't inherit from the mock implementation at all, because this is instantiated
+// in the hot loop.
+class FastMockFileEvent : public Event::FileEvent {
+  void activate(uint32_t) override {}
+  void setEnabled(uint32_t) override {}
+};
+
+class FastMockDispatcher : public Event::MockDispatcher {
+public:
+  Event::FileEventPtr createFileEvent(int, Event::FileReadyCb cb, Event::FileTriggerType,
+                                      uint32_t) override {
+    file_event_callback_ = cb;
+    return std::make_unique<FastMockFileEvent>();
+  }
+
+  Event::FileReadyCb file_event_callback_;
+};
+
+class FastMockOsSysCalls : public Api::MockOsSysCalls {
+public:
+  FastMockOsSysCalls(const std::vector<uint8_t>& client_hello) : client_hello_(client_hello) {}
+
+  ssize_t recv(int, void* buffer, size_t length, int) override {
+    RELEASE_ASSERT(length >= client_hello_.size());
+    memcpy(buffer, client_hello_.data(), client_hello_.size());
+    return client_hello_.size();
+  }
+
+  const std::vector<uint8_t> client_hello_;
+};
+
 static std::vector<uint8_t> generateClientHello(const std::string& sni_name) {
   const SSL_METHOD* method = SSLv23_method();
   bssl::UniquePtr<SSL_CTX> ctx(SSL_CTX_new(method));
@@ -54,36 +109,20 @@ static std::vector<uint8_t> generateClientHello(const std::string& sni_name) {
 }
 
 static void BM_TlsInspector(benchmark::State& state) {
-  state.PauseTiming();
-  const std::vector<uint8_t> client_hello(generateClientHello("example.com"));
-  NiceMock<Api::MockOsSysCalls> os_sys_calls;
+  NiceMock<FastMockOsSysCalls> os_sys_calls(generateClientHello("example.com"));
   TestThreadsafeSingletonInjector<Api::OsSysCallsImpl> os_calls{&os_sys_calls};
   NiceMock<Stats::MockStore> store;
   ConfigSharedPtr cfg(std::make_shared<Config>(store));
-  Network::MockListenerFilterCallbacks cb;
-  Network::MockConnectionSocket socket;
-  NiceMock<Event::MockDispatcher> dispatcher;
-  Event::FileReadyCb file_event_callback;
-  EXPECT_CALL(cb, socket()).WillRepeatedly(ReturnRef(socket));
-  EXPECT_CALL(cb, dispatcher()).WillRepeatedly(ReturnRef(dispatcher));
-  EXPECT_CALL(cb, continueFilterChain(true)).Times(AtLeast(1));
-  EXPECT_CALL(socket, fd()).WillRepeatedly(Return(42));
-  EXPECT_CALL(socket, setRequestedServerName(_)).Times(AtLeast(1));
-  EXPECT_CALL(dispatcher, createFileEvent_(_, _, _, _))
-      .WillRepeatedly(
-          DoAll(SaveArg<1>(&file_event_callback), ReturnNew<NiceMock<Event::MockFileEvent>>()));
-  EXPECT_CALL(os_sys_calls, recv(42, _, _, MSG_PEEK))
-      .WillRepeatedly(Invoke([&client_hello](int, void* buffer, size_t length, int) -> int {
-        ASSERT(length >= client_hello.size());
-        memcpy(buffer, client_hello.data(), client_hello.size());
-        return client_hello.size();
-      }));
+  FastMockConnectionSocket socket;
+  NiceMock<FastMockDispatcher> dispatcher;
+  FastMockListenerFilterCallbacks cb(socket, dispatcher);
 
-  state.ResumeTiming();
   for (auto _ : state) {
     Filter filter(cfg);
     filter.onAccept(cb);
-    file_event_callback(Event::FileReadyType::Read);
+    dispatcher.file_event_callback_(Event::FileReadyType::Read);
+    RELEASE_ASSERT(socket.got_server_name_);
+    socket.got_server_name_ = false;
   }
 }
 
