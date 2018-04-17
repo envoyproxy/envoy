8f62fe286a8ad06ba857d4103b5b601a0d4af1fb
htuch <htuch@users.noreply.github.com>
2018-04-17 11:57:53 -0400

    ci: fix build_image build. (#3095)
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- ci/build_container/docker_build.sh
+++ ci/build_container/docker_build.sh
@@ -6,5 +6,5 @@
 # We need //bazel/... and WORKSPACE for the build, but it's not in ci/build_container. Using Docker
 # relative path workaround from https://github.com/docker/docker/issues/2745#issuecomment-253230025
 # to get this to work.
-tar cf - . -C ../../ bazel WORKSPACE \
+tar cf - . -C ../../ bazel WORKSPACE api \
   | docker build -f Dockerfile-${LINUX_DISTRO} -t ${IMAGE_NAME}:$CIRCLE_SHA1 -
