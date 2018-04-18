4494d2cad97d060aac215ff5e37ce939c0cae35e
Bjoern Metzdorf <bmetzdorf@apple.com>
2018-04-17 20:24:33 -0700

    ListenerSocketOptionImplTest should inherit from SocketOptionImplTest
    Signed-off-by: Bjoern Metzdorf <bmetzdorf@apple.com>

--- test/server/listener_socket_option_impl_test.cc
+++ test/server/listener_socket_option_impl_test.cc
@@ -1,59 +1,17 @@
 #include "server/listener_socket_option_impl.h"
 
-#include "test/mocks/server/mocks.h"
-#include "test/test_common/threadsafe_singleton_injector.h"
-
-#include "gtest/gtest.h"
-
-using testing::Invoke;
-using testing::Return;
-using testing::_;
+#include "test/common/network/socket_option_test_harness.h"
 
 namespace Envoy {
 namespace Server {
 
-class ListenerSocketOptionImplTest : public testing::Test {
+class ListenerSocketOptionImplTest : public Network::SocketOptionTestHarness {
 public:
-  ListenerSocketOptionImplTest() { socket_.local_address_.reset(); }
-
-  NiceMock<Network::MockListenSocket> socket_;
-  Api::MockOsSysCalls os_sys_calls_;
-  TestThreadsafeSingletonInjector<Api::OsSysCallsImpl> os_calls{&os_sys_calls_};
-
   void testSetSocketOptionSuccess(ListenerSocketOptionImpl& socket_option, int socket_level,
                                   Network::SocketOptionName option_name, int option_val,
                                   const std::set<Network::Socket::SocketState>& when) {
-    Network::Address::Ipv4Instance address("1.2.3.4", 5678);
-    const int fd = address.socket(Network::Address::SocketType::Stream);
-    EXPECT_CALL(socket_, fd()).WillRepeatedly(Return(fd));
-
-    for (Network::Socket::SocketState state : when) {
-      if (option_name.has_value()) {
-        EXPECT_CALL(os_sys_calls_,
-                    setsockopt_(_, socket_level, option_name.value(), _, sizeof(int)))
-            .WillOnce(Invoke([option_val](int, int, int, const void* optval, socklen_t) -> int {
-              EXPECT_EQ(option_val, *static_cast<const int*>(optval));
-              return 0;
-            }));
-        EXPECT_TRUE(socket_option.setOption(socket_, state));
-      } else {
-        EXPECT_FALSE(socket_option.setOption(socket_, state));
-      }
-    }
-
-    // The set of SocketState for which this option should not be set. Initialize to all
-    // the states, and remove states that are passed in.
-    std::list<Network::Socket::SocketState> unset_socketstates{
-        Network::Socket::SocketState::PreBind,
-        Network::Socket::SocketState::PostBind,
-        Network::Socket::SocketState::Listening,
-    };
-    unset_socketstates.remove_if(
-        [&](Network::Socket::SocketState state) -> bool { return when.find(state) != when.end(); });
-    for (Network::Socket::SocketState state : unset_socketstates) {
-      EXPECT_CALL(os_sys_calls_, setsockopt_(_, _, _, _, _)).Times(0);
-      EXPECT_TRUE(socket_option.setOption(socket_, state));
-    }
+    Network::SocketOptionTestHarness::testSetSocketOptionSuccess(socket_option, socket_level,
+                                                                 option_name, option_val, when);
   }
 };
 
