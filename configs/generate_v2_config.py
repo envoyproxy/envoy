5580ebd336ee439da8b292670c6b42ea2ac78993
sri kailash <sri.gandebathula@booking.com>
2018-04-19 09:03:45 +0200

    Removed all old json files Removed all incorrectly converted json to yaml files TODO: 1. Convert router template, service_to_service template, routing_helper, google_com_proxy, access_log_format to v2 2. Add tls context to front_proxy and double_proxy templates
    Signed-off-by: sri kailash <sri.gandebathula@booking.com>

--- configs/generate_v2_config.py
+++ configs/generate_v2_config.py
@@ -55,7 +55,7 @@ def generate_v2_config(template_path, template, output_file, **context):
 
 # generate_v2_config(SCRIPT_DIR, 'envoy_front_proxy.template.yaml',
                        # './envoy_front_proxy.yaml', clusters=front_envoy_clusters)
-generate_v2_config(SCRIPT_DIR, 'envoy_double_proxy.template_v2.yaml',
+generate_v2_config(SCRIPT_DIR, 'envoy_front_proxy_v2.template.yaml',
                         './envoy_double_proxy.yaml')
 #
 # generate_v2_config("./", 'envoy_service_to_service.template.yaml',
