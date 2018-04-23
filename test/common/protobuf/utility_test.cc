14c0c07c194d2bfb4c1de797d3ae66aa64905088
htuch <htuch@users.noreply.github.com>
2018-04-23 13:58:16 -0400

    protobuf: improve robustness of jsonConvert(). (#3168)
    Turns out that it can fail when you round-trip values that are out-of-range for things like
    duration.
    
    This was found via proto fuzzing the server config.
    
    Risk Level: Low
    Testing: New utility_test.
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- test/common/protobuf/utility_test.cc
+++ test/common/protobuf/utility_test.cc
@@ -203,4 +203,22 @@ TEST(UtilityTest, HashedValueStdHash) {
   EXPECT_NE(set.find(hv3), set.end());
 }
 
+TEST(UtilityTest, JsonConvertSuccess) {
+  ProtobufWkt::Duration source_duration;
+  source_duration.set_seconds(42);
+  ProtobufWkt::Duration dest_duration;
+  MessageUtil::jsonConvert(source_duration, dest_duration);
+  EXPECT_EQ(42, dest_duration.seconds());
+}
+
+TEST(UtilityTest, JsonConvertFail) {
+  ProtobufWkt::Duration source_duration;
+  source_duration.set_seconds(-281474976710656);
+  ProtobufWkt::Duration dest_duration;
+  EXPECT_THROW_WITH_MESSAGE(MessageUtil::jsonConvert(source_duration, dest_duration),
+                            EnvoyException,
+                            "Unable to convert protobuf message to JSON string: INTERNAL:Duration "
+                            "seconds exceeds limit for field:  seconds: -281474976710656\n");
+}
+
 } // namespace Envoy
