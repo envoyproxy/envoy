a149f7797b9064a87f4db74d736d21162c7d5cb0
Piotr Sikora <piotrsikora@google.com>
2018-05-01 16:30:46 -0700

    review: re-point example filter back at the origin; bump commit.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- ci/build_setup.sh
+++ ci/build_setup.sh
@@ -80,14 +80,13 @@ if [[ -d /bazel-prebuilt-output && ! -d "${TEST_TMPDIR}/_bazel_${USER}" ]]; then
 fi
 
 # Setup Envoy consuming project.
-rm  -rf "${ENVOY_FILTER_EXAMPLE_SRCDIR}"
 if [[ ! -a "${ENVOY_FILTER_EXAMPLE_SRCDIR}" ]]
 then
-  git clone https://github.com/PiotrSikora/envoy-filter-example.git "${ENVOY_FILTER_EXAMPLE_SRCDIR}"
+  git clone https://github.com/envoyproxy/envoy-filter-example.git "${ENVOY_FILTER_EXAMPLE_SRCDIR}"
 fi
 
 # This is the hash on https://github.com/envoyproxy/envoy-filter-example.git we pin to.
-(cd "${ENVOY_FILTER_EXAMPLE_SRCDIR}" && git fetch origin && git checkout -f 62f0cf9786689af8ee3f8c09f7111375175ee7f8)
+(cd "${ENVOY_FILTER_EXAMPLE_SRCDIR}" && git fetch origin && git checkout -f f377ae3f89a50a0c9c2327043897e31e56df2847)
 cp -f "${ENVOY_SRCDIR}"/ci/WORKSPACE.filter.example "${ENVOY_FILTER_EXAMPLE_SRCDIR}"/WORKSPACE
 
 # Also setup some space for building Envoy standalone.
