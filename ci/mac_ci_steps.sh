74a66235a3e1eefb4be9b7005af206704d4a6e18
Stephan Zuercher <stephan@turbinelabs.io>
2018-05-02 12:16:37 -0700

    ci: retry integration tests on OS X
    Signed-off-by: Stephan Zuercher <stephan@turbinelabs.io>

--- ci/mac_ci_steps.sh
+++ ci/mac_ci_steps.sh
@@ -3,7 +3,9 @@
 set -e
 
 BAZEL_BUILD_OPTIONS="--curses=no --show_task_finish --verbose_failures"
-BAZEL_TEST_OPTIONS="${BAZEL_BUILD_OPTIONS} --test_output=all"
+# TODO(zuercher): remove --flaky_test_attempts when https://github.com/envoyproxy/envoy/issues/2428
+# is resolved.
+BAZEL_TEST_OPTIONS="${BAZEL_BUILD_OPTIONS} --test_output=all --flaky_test_attempts=integration@2"
 
 # Build envoy and run tests as separate steps so that failure output
 # is somewhat more deterministic (rather than interleaving the build
