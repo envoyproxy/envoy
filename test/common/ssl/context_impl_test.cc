92188fd2f18c81f095dd4c690e21dedfd616d99a
Jae Kim <jaebong@jaebong0.svl.corp.google.com>
2018-05-15 17:22:11 -0700

    Added unit test for static secret in ClientContextConfigImpl

--- test/common/ssl/context_impl_test.cc
+++ test/common/ssl/context_impl_test.cc
@@ -2,6 +2,8 @@
 #include <vector>
 
 #include "common/json/json_loader.h"
+#include "common/secret/secret_impl.h"
+#include "common/secret/secret_manager_impl.h"
 #include "common/ssl/context_config_impl.h"
 #include "common/ssl/context_impl.h"
 #include "common/stats/stats_impl.h"
@@ -378,6 +380,96 @@ TEST(ClientContextConfigImplTest, MultipleTlsCertificates) {
   EXPECT_THROW_WITH_MESSAGE(
       ClientContextConfigImpl client_context_config(tls_context, secret_manager), EnvoyException,
       "Multiple TLS certificates are not supported for client contexts");
+}
+
+TEST(ClientContextConfigImplTest, StaticTlsCertificates) {
+  std::string kExpectedCertificateChain =
+      R"EOF(-----BEGIN CERTIFICATE-----
+MIIDEDCCAnmgAwIBAgIJAKnPQcNyJm/aMA0GCSqGSIb3DQEBCwUAMHoxCzAJBgNV
+BAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRYwFAYDVQQHEw1TYW4gRnJhbmNp
+c2NvMQ0wCwYDVQQKEwRMeWZ0MRkwFwYDVQQLExBMeWZ0IEVuZ2luZWVyaW5nMRQw
+EgYDVQQDEwtUZXN0IFNlcnZlcjAeFw0xNzA3MDkwMTM5MzJaFw0xOTA3MDkwMTM5
+MzJaMHoxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRYwFAYDVQQH
+Ew1TYW4gRnJhbmNpc2NvMQ0wCwYDVQQKEwRMeWZ0MRkwFwYDVQQLExBMeWZ0IEVu
+Z2luZWVyaW5nMRQwEgYDVQQDEwtUZXN0IFNlcnZlcjCBnzANBgkqhkiG9w0BAQEF
+AAOBjQAwgYkCgYEAqy+9qxHrAhi/o4GlshCoalUxMXxHBmE2vyxMs1rejBfwOl3y
+IyA9r7oaHtMrqXxfF5TdjRvKWpj7dbAwGjhSOrPKXRjhT543BCAbSisCpMlA/CP7
+GaNfYLOtgBHU5mz8BlXY2fLBUORnHRlFbL/myIl3oeNhuLsUNjIlJSSflL0CAwEA
+AaOBnTCBmjAMBgNVHRMBAf8EAjAAMAsGA1UdDwQEAwIF4DAdBgNVHSUEFjAUBggr
+BgEFBQcDAgYIKwYBBQUHAwEwHgYDVR0RBBcwFYITc2VydmVyMS5leGFtcGxlLmNv
+bTAdBgNVHQ4EFgQU8/1SRZup5ukZHvtfSaI/OXXXUJIwHwYDVR0jBBgwFoAU8/1S
+RZup5ukZHvtfSaI/OXXXUJIwDQYJKoZIhvcNAQELBQADgYEAhOZvHhxvktcKwgVF
+MoCp/sOlOV1NXHNndZxZl4uHpoUqXnTycp4VrniiQD5O6w5PjZliILpSyZTUm5HK
+uXF9gTlCv9G2Y8NMXPDV13G1UuGeS4nC/Pxe55+QgHL7xyReOpJvA8grWL+dCece
+Rk7e1/bKUaWuGEx0erYHNKEnpkY=
+-----END CERTIFICATE-----
+)EOF";
+
+  std::string kExpectedPrivateKey =
+      R"EOF(-----BEGIN RSA PRIVATE KEY-----
+MIICXQIBAAKBgQCrL72rEesCGL+jgaWyEKhqVTExfEcGYTa/LEyzWt6MF/A6XfIj
+ID2vuhoe0yupfF8XlN2NG8pamPt1sDAaOFI6s8pdGOFPnjcEIBtKKwKkyUD8I/sZ
+o19gs62AEdTmbPwGVdjZ8sFQ5GcdGUVsv+bIiXeh42G4uxQ2MiUlJJ+UvQIDAQAB
+AoGAGrFQBtu9ZE9NmoY9uv1D9YihKhEx1fnUmoyizRivOPMGn2NEvVtqovsG1aWh
+2kStYzTwMu+RZv0RwLEfXwdHMuTGEwcqLi0c/FskUIOXZvBl9Ev7P6Yr11C5SQHe
+U/Fm2rhPVcKs/UyUzT2R7dMtkhCc7Yl3koDZWX2XC9wjzsECQQDWf9T1UifSszrP
+Vb0QYyva4gniPPEUQJnqsCNfKo1AyzIzCBrdxgIeO44Izjourpvrs2/6BvvF0nxx
+/Y8ogfixAkEAzE6ewRohxnm0OBRL2Pcjj6EW7wJuxH4PS3E01lrwsKrgO1B04SgZ
+pqDA7qrEttya/O/OP02P1HfaZOEHqc4fzQJBAL/i85vStxViiQXZ6ZyzWxQgij79
+zZ0UfZzZnYsRAfQo0uucIIytClAJbvKpqpsAUTP1/gJqJOm/dtxyvJK8UsECQF5W
+Kx206EWR6rI+ROtw6h2m30ULVYQrRPqr0h7sLNkWfaVFuEJC1t1Guu85MM3SvUnv
+nMdEFBaiJNiRw40XnT0CQQCcwTdtTwWojjNZfzgSzzC2k0kjWXCWYfLD/OsEeaxB
+Hk8EP6nnwEi/312iSoo/BxuYUc9Y/XTKUpcMiwu7MA5b
+-----END RSA PRIVATE KEY-----
+)EOF";
+
+  envoy::api::v2::auth::Secret secret_config;
+
+  secret_config.set_name("abc.com");
+  auto tls_certificate = secret_config.mutable_tls_certificate();
+  tls_certificate->mutable_certificate_chain()->set_filename(
+      "test/common/ssl/test_data/selfsigned_cert.pem");
+  tls_certificate->mutable_private_key()->set_filename(
+      "test/common/ssl/test_data/selfsigned_key.pem");
+
+  Secret::SecretSharedPtr secret(new Secret::SecretImpl(secret_config));
+  Secret::SecretManagerImpl secret_manager;
+  secret_manager.addOrUpdateStaticSecret(secret);
+
+  envoy::api::v2::auth::UpstreamTlsContext tls_context;
+  tls_context.mutable_common_tls_context()
+      ->mutable_tls_certificate_sds_secret_configs()
+      ->Add()
+      ->set_name("abc.com");
+
+  ClientContextConfigImpl client_context_config(tls_context, secret_manager);
+
+  EXPECT_EQ(kExpectedCertificateChain, client_context_config.certChain());
+}
+
+TEST(ClientContextConfigImplTest, MissingStaticSecretTlsCertificates) {
+  envoy::api::v2::auth::Secret secret_config;
+
+  secret_config.set_name("abc.com");
+  auto tls_certificate = secret_config.mutable_tls_certificate();
+  tls_certificate->mutable_certificate_chain()->set_filename(
+      "test/common/ssl/test_data/selfsigned_cert.pem");
+  tls_certificate->mutable_private_key()->set_filename(
+      "test/common/ssl/test_data/selfsigned_key.pem");
+
+  Secret::SecretSharedPtr secret(new Secret::SecretImpl(secret_config));
+  Secret::SecretManagerImpl secret_manager;
+  secret_manager.addOrUpdateStaticSecret(secret);
+
+  envoy::api::v2::auth::UpstreamTlsContext tls_context;
+  tls_context.mutable_common_tls_context()
+      ->mutable_tls_certificate_sds_secret_configs()
+      ->Add()
+      ->set_name("missing");
+
+  EXPECT_THROW_WITH_MESSAGE(
+      ClientContextConfigImpl client_context_config(tls_context, secret_manager), EnvoyException,
+      "Static secret is not defined: missing");
 }
 
 // Multiple TLS certificates are not yet supported, but one is expected for
