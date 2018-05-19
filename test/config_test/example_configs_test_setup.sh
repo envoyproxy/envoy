045eeaeb28e76885e40fd50d264218cb343af583
Christoph Pakulski <paker8848@gmail.com>
2018-05-18 21:21:13 -0400

    Fixed bug in shutdown routine when running in config verification mode. Improper shutdown of threads triggered ASSERT.
    Added parameterized unit test to process all example config files.
    Tests may fail, but processing should not cause crash.
    
    Signed-off-by: Christoph Pakulski <paker8848@gmail.com>

--- test/config_test/example_configs_test_setup.sh
+++ test/config_test/example_configs_test_setup.sh
@@ -5,3 +5,6 @@ set -e
 DIR="$TEST_TMPDIR"/test/config_test
 mkdir -p "$DIR"
 tar -xvf "$TEST_RUNDIR"/configs/example_configs.tar -C "$DIR"
+
+# Extract the content of tar file, find all config files and save their names.
+tar -tf "$TEST_RUNDIR"/configs/example_configs.tar  | grep "yaml\|json" > "$DIR"/all_config_files.txt
--- test/server/config_validation/server_test.cc
+++ test/server/config_validation/server_test.cc
@@ -1,3 +1,5 @@
+#include <vector>
+
 #include "server/config_validation/server.h"
 
 #include "test/integration/server.h"
@@ -11,22 +13,59 @@ namespace Server {
 // Test param is the path to the config file to validate.
 class ValidationServerTest : public testing::TestWithParam<std::string> {
 public:
-  static void SetUpTestCase() {
+  static void SetupTestDirectory() {
     TestEnvironment::exec(
         {TestEnvironment::runfilesPath("test/config_test/example_configs_test_setup.sh")});
     directory_ = TestEnvironment::temporaryDirectory() + "/test/config_test/";
   }
 
+  static void SetUpTestCase() { SetupTestDirectory(); }
+
 protected:
   ValidationServerTest() : options_(directory_ + GetParam()) {}
 
   static std::string directory_;
-
   testing::NiceMock<MockOptions> options_;
   TestComponentFactory component_factory_;
 };
 
 std::string ValidationServerTest::directory_ = "";
+
+// ValidationServerTest_1 is created only to run different set of parameterized
+// tests than set of tests for ValidationServerTest.
+class ValidationServerTest_1 : public ValidationServerTest {
+public:
+  static const std::vector<std::string> GetAllConfigFiles() {
+    SetupTestDirectory();
+
+    std::string all_file_names =
+        TestEnvironment::readFileToStringForTest(directory_ + "all_config_files.txt");
+    std::vector<std::string> files;
+    // all_config_files.txt contains a list of all config files from tar file created by
+    // example_configs_test_setup.sh Names of the files are separated by EOL. Tokenize those  names
+    // and put them into a std container.
+    size_t pos = 0;
+    size_t prev_pos = pos;
+    while (pos != std::string::npos) {
+      pos = all_file_names.find('\n', pos);
+      if (pos != std::string::npos) {
+        files.emplace_back(all_file_names.substr(prev_pos, pos - prev_pos));
+        pos += 1;
+        prev_pos = pos;
+      } else {
+        // last item. Process it if not empty line.
+        if (prev_pos < all_file_names.length()) {
+          files.emplace_back(all_file_names.substr(prev_pos));
+        }
+      }
+    }
+    // Make sure that reading file with config file names and tokenizing was successful.
+    // If something went wrong, files container will be empty and parameterized tests will not be
+    // instantiated but there will be no warning.
+    ASSERT(!files.empty());
+    return files;
+  }
+};
 
 TEST_P(ValidationServerTest, Validate) {
   EXPECT_TRUE(
@@ -41,6 +80,16 @@ INSTANTIATE_TEST_CASE_P(ValidConfigs, ValidationServerTest,
                         ::testing::Values("front-envoy.yaml", "google_com_proxy.json",
                                           "google_com_proxy.yaml", "google_com_proxy.v2.yaml",
                                           "s2s-grpc-envoy.yaml", "service-envoy.yaml"));
+
+// Just make sure that all configs can be injested without a crash. Processing of config files
+// may not be successful, but there should be no crash.
+TEST_P(ValidationServerTest_1, RunWithoutCrash) {
+  validateConfig(options_, Network::Address::InstanceConstSharedPtr(), component_factory_);
+  SUCCEED();
+}
+
+INSTANTIATE_TEST_CASE_P(AllConfigs, ValidationServerTest_1,
+                        ::testing::ValuesIn(ValidationServerTest_1::GetAllConfigFiles()));
 
 } // namespace Server
 } // namespace Envoy
