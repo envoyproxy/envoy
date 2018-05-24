51d5a99125011131d41499377e0457616bd12063
Matthew Gumport <me@gum.pt>
2018-05-23 18:29:40 -0700

    Speculatively changing tests which failed when changing default
    "Speculative" because I'm not 100% sure that this is the right thing to do/if
    this follows the breaking changes policy.
    
    Would be worthwhile to go back through and clean up all header tests which use
    the value matching instead of exact_match if that's going to be deleted.
    
    Signed-off-by: Matthew Gumport <me@gum.pt>

--- test/common/http/header_utility_test.cc
+++ test/common/http/header_utility_test.cc
@@ -40,22 +40,7 @@ name: test-header
       HeaderUtility::HeaderData(parseHeaderMatcherFromYaml(yaml));
 
   EXPECT_EQ("test-header", header_data.name_.get());
-  EXPECT_EQ(HeaderUtility::HeaderMatchType::Value, header_data.header_match_type_);
-  EXPECT_EQ("", header_data.value_);
-}
-
-TEST(HeaderDataConstructorTest, ValueSet) {
-  const std::string yaml = R"EOF(
-name: test-header
-value: value
-  )EOF";
-
-  HeaderUtility::HeaderData header_data =
-      HeaderUtility::HeaderData(parseHeaderMatcherFromYaml(yaml));
-
-  EXPECT_EQ("test-header", header_data.name_.get());
-  EXPECT_EQ(HeaderUtility::HeaderMatchType::Value, header_data.header_match_type_);
-  EXPECT_EQ("value", header_data.value_);
+  EXPECT_EQ(HeaderUtility::HeaderMatchType::Present, header_data.header_match_type_);
 }
 
 TEST(HeaderDataConstructorTest, ValueAndRegexFlagSet) {
