4d70144718182d284e19332669f647259e24cede
Joshua Marantz <jmarantz@google.com>
2018-06-05 13:18:19 -0400

    fix a typo from merging the wrapper script.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- tools/check_format_test.sh
+++ tools/check_format_test.sh
@@ -5,4 +5,4 @@ root=$(realpath $tools/..)
 ci=$root/ci
 cd $root
 export BUILDIFIER_BIN="/usr/local/bin/buildifier"
-exec ./ci/run_envoy_docker.sh ./tools/check_format_test_run_under_docker.py "@"
+exec ./ci/run_envoy_docker.sh ./tools/check_format_test_helper.py "$@"
