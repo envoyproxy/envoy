e6bf9f896c3ecfb1875a5bff53244ebfcd101a5a
Piotr Sikora <piotrsikora@google.com>
2018-06-05 00:21:27 +0000

    tls: require trusted_ca when using verify_subject_alt_name.
    SAN-based verification without trusted CA is insecure, since provided
    values are easily spoofable.
    
    Becasue of how the existing verification code is structured, this was
    already enforced at run-time, and all certificates were rejected when
    trusted CA wasn't specified, but previously it wasn't obvious why.
    
    *Risk Level*: None
    *Testing*: bazel test //test/...
    *Docs Changes*: Added
    *Release Notes*: n/a
    
    Fixes #1268.
    
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- source/common/ssl/context_config_impl.cc
+++ source/common/ssl/context_config_impl.cc
@@ -67,9 +67,16 @@ ContextConfigImpl::ContextConfigImpl(const envoy::api::v2::auth::CommonTlsContex
           tlsVersionFromProto(config.tls_params().tls_minimum_protocol_version(), TLS1_VERSION)),
       max_protocol_version_(
           tlsVersionFromProto(config.tls_params().tls_maximum_protocol_version(), TLS1_2_VERSION)) {
-  if (ca_cert_.empty() && !certificate_revocation_list_.empty()) {
-    throw EnvoyException(fmt::format("Failed to load CRL from {} without trusted CA certificates",
-                                     certificateRevocationListPath()));
+  if (ca_cert_.empty()) {
+    if (!certificate_revocation_list_.empty()) {
+      throw EnvoyException(fmt::format("Failed to load CRL from {} without trusted CA",
+                                       certificateRevocationListPath()));
+    }
+    if (!verify_subject_alt_name_list_.empty()) {
+      throw EnvoyException(fmt::format("SAN-based verification of peer certificates without "
+                                       "trusted CA is insecure and not allowed",
+                                       certificateRevocationListPath()));
+    }
   }
 }
 
