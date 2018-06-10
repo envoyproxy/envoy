d464f8c1f9de8eb21775ee1768006008543a21c6
trabetti <talis@il.ibm.com>
2018-06-10 22:32:48 +0300

    factor out repeating behaviors to class/functions
    Signed-off-by: trabetti <talis@il.ibm.com>

--- test/extensions/stats_sinks/hystrix/hystrix_test.cc
+++ test/extensions/stats_sinks/hystrix/hystrix_test.cc
@@ -11,6 +11,8 @@
 #include "test/mocks/upstream/mocks.h"
 #include "test/test_common/utility.h"
 
+#include "absl/strings/str_replace.h"
+#include "absl/strings/str_split.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
@@ -24,6 +26,59 @@ namespace Envoy {
 namespace Extensions {
 namespace StatSinks {
 namespace Hystrix {
+
+class ClusterTestInfo {
+
+public:
+  ClusterTestInfo(const std::string cluster_name) : cluster_name_(cluster_name) {
+    ON_CALL(cluster_, info()).WillByDefault(Return(cluster_info_ptr_));
+    ON_CALL(*cluster_info_, name()).WillByDefault(testing::ReturnRefOfCopy(cluster_name_));
+    ON_CALL(*cluster_info_, statsScope()).WillByDefault(ReturnRef(cluster_stats_scope_));
+
+    // Set gauge value.
+    membership_total_gauge_.name_ = "membership_total";
+    ON_CALL(cluster_stats_scope_, gauge("membership_total"))
+        .WillByDefault(ReturnRef(membership_total_gauge_));
+    ON_CALL(membership_total_gauge_, value()).WillByDefault(Return(5));
+
+    // Attach counters.
+    setCounterForTest(success_counter_, "upstream_rq_2xx");
+    setCounterForTest(error_5xx_counter_, "upstream_rq_5xx");
+    setCounterForTest(retry_5xx_counter_, "retry.upstream_rq_5xx");
+    setCounterForTest(error_4xx_counter_, "upstream_rq_4xx");
+    setCounterForTest(retry_4xx_counter_, "retry.upstream_rq_4xx");
+    setCountersToZero();
+  }
+
+  // Attach the counter to cluster_stat_scope and set default value.
+  void setCounterForTest(NiceMock<Stats::MockCounter>& counter, std::string counter_name) {
+    counter.name_ = counter_name;
+    ON_CALL(cluster_stats_scope_, counter(counter_name)).WillByDefault(ReturnRef(counter));
+  }
+
+  void setCountersToZero() {
+    ON_CALL(error_5xx_counter_, value()).WillByDefault(Return(0));
+    ON_CALL(retry_5xx_counter_, value()).WillByDefault(Return(0));
+    ON_CALL(error_4xx_counter_, value()).WillByDefault(Return(0));
+    ON_CALL(retry_4xx_counter_, value()).WillByDefault(Return(0));
+    ON_CALL(success_counter_, value()).WillByDefault(Return(0));
+  }
+
+  NiceMock<Upstream::MockCluster> cluster_;
+  Upstream::MockClusterInfo* cluster_info_ = new NiceMock<Upstream::MockClusterInfo>();
+  Upstream::ClusterInfoConstSharedPtr cluster_info_ptr_{cluster_info_};
+
+  NiceMock<Stats::MockStore> stats_store_;
+  NiceMock<Stats::MockStore> cluster_stats_scope_;
+  const std::string cluster_name_;
+
+  NiceMock<Stats::MockGauge> membership_total_gauge_;
+  NiceMock<Stats::MockCounter> success_counter_;
+  NiceMock<Stats::MockCounter> error_5xx_counter_;
+  NiceMock<Stats::MockCounter> retry_5xx_counter_;
+  NiceMock<Stats::MockCounter> error_4xx_counter_;
+  NiceMock<Stats::MockCounter> retry_4xx_counter_;
+};
 
 class HystrixSinkTest : public testing::Test {
 public:
@@ -42,78 +97,59 @@ public:
     return actual;
   }
 
-  Buffer::OwnedImpl createClusterAndCallbacks() {
+  absl::string_view getStringStreamField(absl::string_view data_message, absl::string_view key) {
+    return absl::StrReplaceAll(getStreamField(data_message, key), {{"\"", ""}});
+  }
 
+  Buffer::OwnedImpl createClusterAndCallbacks() {
     // Set cluster.
-    cluster_map_.emplace("test_cluster1", cluster1_);
+    cluster_map_.emplace(cluster1_name_, cluster1_.cluster_);
     ON_CALL(server_, clusterManager()).WillByDefault(ReturnRef(cluster_manager_));
     ON_CALL(cluster_manager_, clusters()).WillByDefault(Return(cluster_map_));
-    ON_CALL(cluster1_, info()).WillByDefault(Return(cluster1_info_ptr_));
-
-    ON_CALL(*cluster1_info_, name()).WillByDefault(testing::ReturnRefOfCopy(cluster1_name_));
-    ON_CALL(*cluster1_info_, statsScope()).WillByDefault(ReturnRef(cluster1_stats_scope_));
-
-    // Set gauge value.
-    membership_total_gauge_.name_ = "membership_total";
-    ON_CALL(cluster1_stats_scope_, gauge("membership_total"))
-        .WillByDefault(ReturnRef(membership_total_gauge_));
-    ON_CALL(membership_total_gauge_, value()).WillByDefault(Return(5));
-
-    // Attach counters.
-    success_counter_.name_ = "upstream_rq_2xx";
-    ON_CALL(cluster1_stats_scope_, counter("upstream_rq_2xx"))
-        .WillByDefault(ReturnRef(success_counter_));
-    error_5xx_counter_.name_ = "upstream_rq_5xx";
-    ON_CALL(cluster1_stats_scope_, counter("upstream_rq_5xx"))
-        .WillByDefault(ReturnRef(error_5xx_counter_));
-    retry_5xx_counter_.name_ = "retry.upstream_rq_5xx";
-    ON_CALL(cluster1_stats_scope_, counter("retry.upstream_rq_5xx"))
-        .WillByDefault(ReturnRef(retry_5xx_counter_));
-    error_4xx_counter_.name_ = "upstream_rq_4xx";
-    ON_CALL(cluster1_stats_scope_, counter("upstream_rq_4xx"))
-        .WillByDefault(ReturnRef(error_4xx_counter_));
-    retry_4xx_counter_.name_ = "retry.upstream_rq_4xx";
-    ON_CALL(cluster1_stats_scope_, counter("tetry.upstream_rq_4xx"))
-        .WillByDefault(ReturnRef(retry_4xx_counter_));
-    ON_CALL(retry_5xx_counter_, value()).WillByDefault(Return(0));
-    ON_CALL(error_4xx_counter_, value()).WillByDefault(Return(0));
-    ON_CALL(retry_5xx_counter_, value()).WillByDefault(Return(0));
 
     Buffer::OwnedImpl buffer;
     auto encode_callback = [&buffer](Buffer::Instance& data, bool) {
       // Set callbacks to send data to buffer.
       buffer.add(
           data); // This will append to the end of the buffer, so multiple calls will all be dumped
-      // one after another into this buffer. See Buffer::Instance for other buffer
-      // buffer modification options.
+                 // one after another into this buffer.
     };
     ON_CALL(callbacks_, encodeData(_, _)).WillByDefault(Invoke(encode_callback));
-
     return buffer;
   }
 
+  void validateAllZero(std::string data_message) {
+    EXPECT_EQ(getStreamField(data_message, "errorPercentage"), "0");
+    EXPECT_EQ(getStreamField(data_message, "errorCount"), "0");
+    EXPECT_EQ(getStreamField(data_message, "requestCount"), "0");
+    EXPECT_EQ(getStreamField(data_message, "rollingCountSemaphoreRejected"), "0");
+    EXPECT_EQ(getStreamField(data_message, "rollingCountSuccess"), "0");
+    EXPECT_EQ(getStreamField(data_message, "rollingCountTimeout"), "0");
+  }
+
+  std::unordered_map<std::string, std::string> buildClusterMap(absl::string_view data_message) {
+    std::unordered_map<std::string, std::string> cluster_message_map;
+    std::vector<std::string> messages =
+        absl::StrSplit(data_message, "data:", absl::SkipWhitespace());
+    for (auto message : messages) {
+      if (absl::StrContains(getStreamField(message, "type"), "HystrixCommand")) {
+        std::string cluster_name(getStringStreamField(message, "name"));
+        cluster_message_map[cluster_name] = message;
+      }
+    }
+    return cluster_message_map;
+  }
+
+  const std::string cluster1_name_{"test_cluster1"};
+  ClusterTestInfo cluster1_{cluster1_name_};
+
   NiceMock<Http::MockStreamDecoderFilterCallbacks> callbacks_;
   NiceMock<Server::MockInstance> server_;
-  NiceMock<Upstream::MockCluster> cluster1_;
   Upstream::ClusterManager::ClusterInfoMap cluster_map_;
 
   std::unique_ptr<HystrixSink> sink_;
   NiceMock<Stats::MockSource> source_;
   NiceMock<Upstream::MockClusterManager> cluster_manager_;
-
-  Upstream::MockClusterInfo* cluster1_info_ = new NiceMock<Upstream::MockClusterInfo>();
-  Upstream::ClusterInfoConstSharedPtr cluster1_info_ptr_{cluster1_info_};
-
-  NiceMock<Stats::MockStore> stats_store_1_;
-  NiceMock<Stats::MockStore> cluster1_stats_scope_;
-  const std::string cluster1_name_{"test_cluster1"};
-
-  NiceMock<Stats::MockGauge> membership_total_gauge_;
-  NiceMock<Stats::MockCounter> success_counter_;
-  NiceMock<Stats::MockCounter> error_5xx_counter_;
-  NiceMock<Stats::MockCounter> retry_5xx_counter_;
-  NiceMock<Stats::MockCounter> error_4xx_counter_;
-  NiceMock<Stats::MockCounter> retry_4xx_counter_;
 };
 
 TEST_F(HystrixSinkTest, EmptyFlush) {
@@ -121,16 +157,9 @@ TEST_F(HystrixSinkTest, EmptyFlush) {
   Buffer::OwnedImpl buffer = createClusterAndCallbacks();
   // Register callback to sink.
   sink_->registerConnection(&callbacks_);
-
   sink_->flush(source_);
-
   std::string data_message = TestUtility::bufferToString(buffer);
-  EXPECT_EQ(getStreamField(data_message, "errorPercentage"), "0");
-  EXPECT_EQ(getStreamField(data_message, "errorCount"), "0");
-  EXPECT_EQ(getStreamField(data_message, "requestCount"), "0");
-  EXPECT_EQ(getStreamField(data_message, "rollingCountSemaphoreRejected"), "0");
-  EXPECT_EQ(getStreamField(data_message, "rollingCountSuccess"), "0");
-  EXPECT_EQ(getStreamField(data_message, "rollingCountTimeout"), "0");
+  validateAllZero(data_message);
 }
 
 TEST_F(HystrixSinkTest, BasicFlow) {
@@ -141,15 +170,15 @@ TEST_F(HystrixSinkTest, BasicFlow) {
 
   for (int i = 0; i < 12; i++) {
     buffer.drain(buffer.length());
-    ON_CALL(error_5xx_counter_, value()).WillByDefault(Return((i + 1) * 17));
-    ON_CALL(success_counter_, value()).WillByDefault(Return((i + 1) * 7));
-    cluster1_info_->stats().upstream_rq_timeout_.add(3);
-    cluster1_info_->stats().upstream_rq_pending_overflow_.add(8);
+    ON_CALL(cluster1_.error_5xx_counter_, value()).WillByDefault(Return((i + 1) * 17));
+    ON_CALL(cluster1_.success_counter_, value()).WillByDefault(Return((i + 1) * 7));
+    cluster1_.cluster_info_->stats().upstream_rq_timeout_.add(3);
+    cluster1_.cluster_info_->stats().upstream_rq_pending_overflow_.add(8);
     sink_->flush(source_);
   }
 
   std::string rolling_map = sink_->printRollingWindows();
-  EXPECT_NE(std::string::npos, rolling_map.find("test_cluster1.total"));
+  EXPECT_NE(std::string::npos, rolling_map.find(cluster1_name_ + ".total"));
 
   std::string data_message = TestUtility::bufferToString(buffer);
 
@@ -169,12 +198,7 @@ TEST_F(HystrixSinkTest, BasicFlow) {
   sink_->resetRollingWindow();
   sink_->flush(source_);
   data_message = TestUtility::bufferToString(buffer);
-  EXPECT_EQ(getStreamField(data_message, "errorPercentage"), "0");
-  EXPECT_EQ(getStreamField(data_message, "errorCount"), "0");
-  EXPECT_EQ(getStreamField(data_message, "requestCount"), "0");
-  EXPECT_EQ(getStreamField(data_message, "rollingCountSemaphoreRejected"), "0");
-  EXPECT_EQ(getStreamField(data_message, "rollingCountSuccess"), "0");
-  EXPECT_EQ(getStreamField(data_message, "rollingCountTimeout"), "0");
+  validateAllZero(data_message);
 }
 
 //
@@ -214,86 +238,46 @@ TEST_F(HystrixSinkTest, AddAndRemoveClusters) {
   sink_->registerConnection(&callbacks_);
 
   // New cluster.
-  NiceMock<Upstream::MockCluster> cluster2;
-  Upstream::MockClusterInfo* cluster2_info = new NiceMock<Upstream::MockClusterInfo>();
-  Upstream::ClusterInfoConstSharedPtr cluster2_info_ptr{cluster2_info};
-  NiceMock<Stats::MockStore> stats_store_2;
-  NiceMock<Stats::MockStore> cluster2_stats_scope;
   const std::string cluster2_name{"test_cluster2"};
-
-  ON_CALL(cluster2, info()).WillByDefault(Return(cluster2_info_ptr));
-
-  ON_CALL(*cluster2_info, name()).WillByDefault(testing::ReturnRefOfCopy(cluster2_name));
-  ON_CALL(*cluster2_info, statsScope()).WillByDefault(ReturnRef(cluster2_stats_scope));
-
-  // Set gauge value.
-  ON_CALL(cluster2_stats_scope, gauge("membership_total"))
-      .WillByDefault(ReturnRef(membership_total_gauge_));
-
-  // Attach counters.
-  NiceMock<Stats::MockCounter> success_counter2;
-  NiceMock<Stats::MockCounter> error_5xx_counter2;
-  NiceMock<Stats::MockCounter> retry_5xx_counter2;
-  NiceMock<Stats::MockCounter> error_4xx_counter2;
-  NiceMock<Stats::MockCounter> retry_4xx_counter2;
-  success_counter2.name_ = "upstream_rq_2xx";
-  ON_CALL(cluster2_stats_scope, counter("upstream_rq_2xx"))
-      .WillByDefault(ReturnRef(success_counter2));
-  error_5xx_counter2.name_ = "upstream_rq_5xx";
-  ON_CALL(cluster2_stats_scope, counter("upstream_rq_5xx"))
-      .WillByDefault(ReturnRef(error_5xx_counter2));
-  retry_5xx_counter2.name_ = "retry.upstream_rq_5xx";
-  ON_CALL(cluster2_stats_scope, counter("retry.upstream_rq_5xx"))
-      .WillByDefault(ReturnRef(retry_5xx_counter2));
-  error_4xx_counter2.name_ = "upstream_rq_4xx";
-  ON_CALL(cluster2_stats_scope, counter("upstream_rq_4xx"))
-      .WillByDefault(ReturnRef(error_4xx_counter2));
-  retry_4xx_counter2.name_ = "retry.upstream_rq_4xx";
-  ON_CALL(cluster2_stats_scope, counter("retry.upstream_rq_4xx"))
-      .WillByDefault(ReturnRef(retry_4xx_counter2));
-
-  cluster2_info->stats().upstream_rq_timeout_.reset();
-  cluster2_info->stats().upstream_rq_per_try_timeout_.reset();
-  cluster2_info->stats().upstream_rq_pending_overflow_.reset();
+  ClusterTestInfo cluster2(cluster2_name);
 
   // Add cluster.
-  cluster_map_.emplace(cluster2_name, cluster2);
   Buffer::OwnedImpl buffer = createClusterAndCallbacks();
+  cluster_map_.emplace(cluster2_name, cluster2.cluster_);
+  // Redefining since cluster_map_ is returned by value.
+  ON_CALL(cluster_manager_, clusters()).WillByDefault(Return(cluster_map_));
 
   // Generate data to both clusters.
+  sink_->flush(source_);
   for (int i = 0; i < 12; i++) {
     buffer.drain(buffer.length());
     // Cluster 1
-    ON_CALL(error_5xx_counter_, value()).WillByDefault(Return((i + 1) * 17));
-    ON_CALL(success_counter_, value()).WillByDefault(Return((i + 1) * 7));
-    cluster1_info_->stats().upstream_rq_timeout_.add(1);
-    cluster1_info_->stats().upstream_rq_per_try_timeout_.add(2);
-    cluster1_info_->stats().upstream_rq_pending_overflow_.add(8);
+    ON_CALL(cluster1_.error_5xx_counter_, value()).WillByDefault(Return((i + 1) * 17));
+    ON_CALL(cluster1_.success_counter_, value()).WillByDefault(Return((i + 1) * 7));
+    cluster1_.cluster_info_->stats().upstream_rq_timeout_.add(1);
+    cluster1_.cluster_info_->stats().upstream_rq_per_try_timeout_.add(2);
+    cluster1_.cluster_info_->stats().upstream_rq_pending_overflow_.add(8);
 
     // Cluster 2
-    ON_CALL(error_5xx_counter2, value()).WillByDefault(Return((i + 1) * 1));
-    ON_CALL(retry_5xx_counter2, value()).WillByDefault(Return((i + 1) * 2));
-    ON_CALL(error_4xx_counter2, value()).WillByDefault(Return((i + 1) * 3));
-    ON_CALL(retry_4xx_counter2, value()).WillByDefault(Return((i + 1) * 4));
-    ON_CALL(success_counter2, value()).WillByDefault(Return((i + 1) * 3));
-    cluster2_info->stats().upstream_rq_timeout_.add(3);
-    cluster2_info->stats().upstream_rq_per_try_timeout_.add(1);
-    cluster2_info->stats().upstream_rq_pending_overflow_.add(5);
+    ON_CALL(cluster2.error_5xx_counter_, value()).WillByDefault(Return((i + 1) * 1));
+    ON_CALL(cluster2.retry_5xx_counter_, value()).WillByDefault(Return((i + 1) * 2));
+    ON_CALL(cluster2.error_4xx_counter_, value()).WillByDefault(Return((i + 1) * 3));
+    ON_CALL(cluster2.retry_4xx_counter_, value()).WillByDefault(Return((i + 1) * 4));
+    ON_CALL(cluster2.success_counter_, value()).WillByDefault(Return((i + 1) * 3));
+    cluster2.cluster_info_->stats().upstream_rq_timeout_.add(3);
+    cluster2.cluster_info_->stats().upstream_rq_per_try_timeout_.add(1);
+    cluster2.cluster_info_->stats().upstream_rq_pending_overflow_.add(5);
 
     sink_->flush(source_);
   }
 
-  std::string data_message = TestUtility::bufferToString(buffer);
+  std::unordered_map<std::string, std::string> cluster_message_map =
+      buildClusterMap(TestUtility::bufferToString(buffer));
+  ASSERT_NE(cluster_message_map.find(cluster1_name_), cluster_message_map.end());
+  ASSERT_NE(cluster_message_map.find(cluster2_name), cluster_message_map.end());
 
-  std::size_t pos1 = data_message.find(cluster1_name_);
-  std::size_t pos2 = data_message.find(cluster2_name);
-  ASSERT_NE(std::string::npos, pos1);
-  ASSERT_NE(std::string::npos, pos2);
-
-  std::string data_message_1 =
-      (pos1 < pos2) ? data_message.substr(pos1, pos2) : data_message.substr(pos1);
-  std::string data_message_2 =
-      (pos2 < pos1) ? data_message.substr(pos2, pos1) : data_message.substr(pos2);
+  std::string data_message_1 = cluster_message_map[cluster1_name_];
+  std::string data_message_2 = cluster_message_map[cluster2_name];
 
   // Check stream format and data.
   EXPECT_EQ(getStreamField(data_message_1, "errorCount"), "160"); // Note that on regular operation,
@@ -319,42 +303,34 @@ TEST_F(HystrixSinkTest, AddAndRemoveClusters) {
 
   // Removing cluster.
   cluster_map_.erase(cluster2_name);
+  // Redefining since cluster_map_ is returned by value.
   ON_CALL(cluster_manager_, clusters()).WillByDefault(Return(cluster_map_));
   sink_->flush(source_);
-  data_message = TestUtility::bufferToString(buffer);
 
-  EXPECT_NE(std::string::npos, data_message.find(cluster1_name_));
-  EXPECT_EQ(std::string::npos, data_message.find(cluster2_name));
+  cluster_message_map.clear();
+  cluster_message_map = buildClusterMap(TestUtility::bufferToString(buffer));
+  ASSERT_NE(cluster_message_map.find(cluster1_name_), cluster_message_map.end());
+  ASSERT_EQ(cluster_message_map.find(cluster2_name), cluster_message_map.end());
 
   // Add cluster again.
   buffer.drain(buffer.length());
-  cluster_map_.emplace(cluster2_name, cluster2);
-  ON_CALL(error_5xx_counter2, value()).WillByDefault(Return(0));
-  ON_CALL(retry_5xx_counter2, value()).WillByDefault(Return(0));
-  ON_CALL(error_4xx_counter2, value()).WillByDefault(Return(0));
-  ON_CALL(retry_4xx_counter2, value()).WillByDefault(Return(0));
-  ON_CALL(success_counter2, value()).WillByDefault(Return(0));
-  cluster2_info->stats().upstream_rq_timeout_.reset();
-  cluster2_info->stats().upstream_rq_per_try_timeout_.reset();
-  cluster2_info->stats().upstream_rq_pending_overflow_.reset();
+  cluster2.setCountersToZero();
+  cluster_map_.emplace(cluster2_name, cluster2.cluster_);
+  // Redefining since cluster_map_ is returned by value.
   ON_CALL(cluster_manager_, clusters()).WillByDefault(Return(cluster_map_));
+
+  cluster2.cluster_info_->stats().upstream_rq_timeout_.reset();
+  cluster2.cluster_info_->stats().upstream_rq_per_try_timeout_.reset();
+  cluster2.cluster_info_->stats().upstream_rq_pending_overflow_.reset();
   sink_->flush(source_);
 
-  data_message = TestUtility::bufferToString(buffer);
-  pos1 = data_message.find(cluster1_name_);
-  pos2 = data_message.find(cluster2_name);
-  ASSERT_NE(std::string::npos, pos1);
-  ASSERT_NE(std::string::npos, pos2);
-
-  data_message_2 = (pos2 < pos1) ? data_message.substr(pos2, pos1) : data_message.substr(pos2);
+  cluster_message_map.clear();
+  cluster_message_map = buildClusterMap(TestUtility::bufferToString(buffer));
+  ASSERT_NE(cluster_message_map.find(cluster1_name_), cluster_message_map.end());
+  ASSERT_NE(cluster_message_map.find(cluster2_name), cluster_message_map.end());
 
   // Check that old values of test_cluster2 were deleted.
-  EXPECT_EQ(getStreamField(data_message_2, "errorPercentage"), "0");
-  EXPECT_EQ(getStreamField(data_message_2, "errorCount"), "0");
-  EXPECT_EQ(getStreamField(data_message_2, "requestCount"), "0");
-  EXPECT_EQ(getStreamField(data_message_2, "rollingCountSemaphoreRejected"), "0");
-  EXPECT_EQ(getStreamField(data_message_2, "rollingCountSuccess"), "0");
-  EXPECT_EQ(getStreamField(data_message_2, "rollingCountTimeout"), "0");
+  validateAllZero(cluster_message_map[cluster2_name]);
 }
 } // namespace Hystrix
 } // namespace StatSinks
