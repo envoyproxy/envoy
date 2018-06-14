envoyproxy/envoy

commit 763f8fdb097697165aab61557eff9b7864e91e05

grpc: serialize-to and parse-from zero copy streams (#3545)
*Description*:
This change takes advantage of newly-public grpc headers to make fewer copies to/from strings before using them.  Before this change, a message being serial/deserialization would involve copies between protobuf messages, vectors of slices, strings, and grpc byte-buffers.  Now extra step(s) are gone, and data are copied directly between protobuf messages and byte-buffers.

*Risk Level*:
Low due to quality of tests predating this change

*Testing*:
`bazel test //test/common/grpc:grpc_client_integration_test`, `bazel test //test/...`

*Docs Changes*:
N/A

*Release Notes*:
N/A

Fixes #3472 

Signed-off-by: Matthew Gumport <me@gum.pt>

source/common/grpc/google_async_client_impl.cc
@@ -6,6 +6,8 @@
 #include "common/grpc/google_grpc_creds_impl.h"
 #include "common/tracing/http_tracer_impl.h"
 
+#include "grpcpp/support/proto_buffer_reader.h"
+
 namespace Envoy {
 namespace Grpc {
 
@@ -298,26 +300,9 @@ void GoogleAsyncStreamImpl::handleOpCompletion(GoogleAsyncTag::Operation op, boo
   }
   case GoogleAsyncTag::Operation::Read: {
     ASSERT(ok);
-    std::vector<grpc::Slice> slices;
-    // Assuming this only fails due to OOM.
-    RELEASE_ASSERT(read_buf_.Dump(&slices).ok());
-    // TODO(htuch): As with PendingMessage serialization, the deserialization
-    // here is not optimal, as we are converting between string representation
-    // and have unnecessary copies. We should use ParseFromCodedStream as done
-    // by TensorFlow
-    // https://github.com/tensorflow/tensorflow/blob/f9462e82ac3981d7a3b5bf392477a585fb6e6912/tensorflow/core/distributed_runtime/rpc/grpc_serialization_traits.h#L210
-    // at the cost of some additional implementation complexity.
-    // Also see
-    // https://github.com/grpc/grpc/blob/5e82dddc056bd488e0ba1ba0057247ab23e442d4/include/grpc%2B%2B/impl/codegen/proto_utils.h#L113
-    // which gives us what we want for zero copy, but relies on grpc::internal details; we can't get
-    // a grpc_byte_buffer from grpc::ByteBuffer to use this.
-    grpc::string buf;
-    buf.reserve(read_buf_.Length());
-    for (const auto& slice : slices) {
-      buf.append(reinterpret_cast<const char*>(slice.begin()), slice.size());
-    }
     ProtobufTypes::MessagePtr response = callbacks_.createEmptyResponse();
-    if (!response->ParseFromString(buf)) {
+    grpc::ProtoBufferReader reader(&read_buf_);
+    if (!response->ParseFromZeroCopyStream(&reader)) {
       // This is basically streamError in Grpc::AsyncClientImpl.
       notifyRemoteClose(Status::GrpcStatus::Internal, nullptr, EMPTY_STRING);
       resetStream();

source/common/grpc/google_async_client_impl.h
@@ -13,6 +13,7 @@
 
 #include "grpc++/generic/generic_stub.h"
 #include "grpc++/grpc++.h"
+#include "grpcpp/support/proto_buffer_writer.h"
 
 namespace Envoy {
 namespace Grpc {
@@ -230,22 +231,19 @@ private:
   // point-in-time, so we queue pending writes here.
   struct PendingMessage {
     // We serialize the message to a grpc::ByteBuffer prior to queueing.
-    // TODO(htuch): We shouldn't need to do a string serialization & copy steps here (effectively,
-    // two copies!), this can be done more efficiently with SerializeToZeroCopyStream as done by
-    // TensorFlow
-    // https://github.com/tensorflow/tensorflow/blob/f9462e82ac3981d7a3b5bf392477a585fb6e6912/tensorflow/core/distributed_runtime/rpc/grpc_serialization_traits.h#L189
-    // at the cost of some additional implementation complexity.
-    // Also see
-    // https://github.com/grpc/grpc/blob/5e82dddc056bd488e0ba1ba0057247ab23e442d4/include/grpc%2B%2B/impl/codegen/proto_utils.h#L42
-    // which gives us what we want for zero copy, but relies on grpc::internal details; we can't get
-    // a grpc_byte_buffer from grpc::ByteBuffer to use this.
     PendingMessage(const Protobuf::Message& request, bool end_stream)
-        : slice_(request.SerializeAsString()), buf_(grpc::ByteBuffer(&slice_, 1)),
+        : buf_([](const Protobuf::Message& request) -> absl::optional<grpc::ByteBuffer> {
+            grpc::ByteBuffer buffer;
+            grpc::ProtoBufferWriter writer(&buffer, grpc::kProtoBufferWriterMaxBufferLength,
+                                           request.ByteSize());
+            return request.SerializeToZeroCopyStream(&writer)
+                       ? absl::make_optional<grpc::ByteBuffer>(buffer)
+                       : absl::nullopt;
+          }(request)),
           end_stream_(end_stream) {}
     // End-of-stream with no additional message.
     PendingMessage() : end_stream_(true) {}
 
-    const grpc::Slice slice_;
     const absl::optional<grpc::ByteBuffer> buf_;
     const bool end_stream_;
   };

commit 587840f2a97b091cc0b1794e4b8296fd42a3c0c7

grpc: fix TSAN race introduced in #3545.
This showed up in some internal tests.

Signed-off-by: Harvey Tuch <htuch@google.com>

source/common/grpc/google_async_client_impl.cc
@@ -301,13 +301,18 @@ void GoogleAsyncStreamImpl::handleOpCompletion(GoogleAsyncTag::Operation op, boo
   case GoogleAsyncTag::Operation::Read: {
     ASSERT(ok);
     ProtobufTypes::MessagePtr response = callbacks_.createEmptyResponse();
-    grpc::ProtoBufferReader reader(&read_buf_);
-    if (!response->ParseFromZeroCopyStream(&reader)) {
-      // This is basically streamError in Grpc::AsyncClientImpl.
-      notifyRemoteClose(Status::GrpcStatus::Internal, nullptr, EMPTY_STRING);
-      resetStream();
-      break;
-    };
+    {
+      // reader must be destructed before we queue up read_buf_ for the next
+      // Read op, otherwise we can race between this thread in the reader
+      // destructor and the gRPC op thread.
+      grpc::ProtoBufferReader reader(&read_buf_);
+      if (!response->ParseFromZeroCopyStream(&reader)) {
+        // This is basically streamError in Grpc::AsyncClientImpl.
+        notifyRemoteClose(Status::GrpcStatus::Internal, nullptr, EMPTY_STRING);
+        resetStream();
+        break;
+      };
+    }
     callbacks_.onReceiveMessageUntyped(std::move(response));
     rw_->Read(&read_buf_, &read_tag_);
     ++inflight_tags_;