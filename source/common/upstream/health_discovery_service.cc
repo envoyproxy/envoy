0c2ed45ab4010653b800856f013f073e93e9e7ca
Lilika Markatou <lilika@google.com>
2018-06-18 15:34:03 -0400

    Added skeleton code for HDS
    Signed-off-by: Lilika Markatou <lilika@google.com>

---
+++ source/common/upstream/health_discovery_service.cc
@@ -0,0 +1,84 @@
+#include "common/upstream/health_discovery_service.h"
+
+#include "common/protobuf/protobuf.h"
+
+namespace Envoy {
+namespace Upstream {
+
+HDSReporter::HDSReporter(const envoy::api::v2::core::Node& node,
+                                     ClusterManager& cluster_manager, Stats::Scope& scope,
+                                     Grpc::AsyncClientPtr async_client,
+                                     Event::Dispatcher& dispatcher)
+    : cm_(cluster_manager), stats_{ALL_HDS_STATS(
+                                POOL_COUNTER_PREFIX(scope, "load_reporter."))},
+      async_client_(std::move(async_client)),
+      service_method_(*Protobuf::DescriptorPool::generated_pool()->FindMethodByName(
+          "envoy.service.load_stats.v2.LoadReportingService.StreamLoadStats")) {
+  health_check_request_.mutable_node()->MergeFrom(node);
+  retry_timer_ = dispatcher.createTimer([this]() -> void { establishNewStream(); });
+  response_timer_ = dispatcher.createTimer([this]() -> void { sendHealthCheckRequest(); });
+  establishNewStream();
+}
+
+void HDSReporter::setRetryTimer() {
+  retry_timer_->enableTimer(std::chrono::milliseconds(RETRY_DELAY_MS));
+}
+
+void HDSReporter::establishNewStream() {
+  ENVOY_LOG(debug, "Establishing new gRPC bidi stream for {}", service_method_.DebugString());
+  stream_ = async_client_->start(service_method_, *this);
+  if (stream_ == nullptr) {
+    ENVOY_LOG(warn, "Unable to establish new stream");
+    handleFailure();
+    return;
+  }
+
+  sendHealthCheckRequest();
+}
+
+void HDSReporter::sendHealthCheckRequest() {
+ ENVOY_LOG(debug, "Sending HealthCheckRequest");
+  stream_->sendMessage(health_check_request_, false);
+  stats_.responses_.inc();
+  // When the connection is established, the message has not yet been read so we
+  // will not have a load reporting period.
+  if (health_check_message_.get()) {
+    ENVOY_LOG(debug, "We got a message wow!");
+  }
+}
+
+void HDSReporter::handleFailure() {
+  ENVOY_LOG(warn, "Load reporter stats stream/connection failure, will retry in {} ms.",
+            RETRY_DELAY_MS);
+  stats_.errors_.inc();
+  setRetryTimer();
+}
+
+void HDSReporter::onCreateInitialMetadata(Http::HeaderMap& metadata) {
+  UNREFERENCED_PARAMETER(metadata);
+}
+
+void HDSReporter::onReceiveInitialMetadata(Http::HeaderMapPtr&& metadata) {
+  UNREFERENCED_PARAMETER(metadata);
+}
+
+
+void HDSReporter::onReceiveMessage(
+    std::unique_ptr<envoy::service::discovery::v2::HealthCheckSpecifier>&& message) {
+  ENVOY_LOG(debug, "New health check response ", message->DebugString());
+  stats_.requests_.inc();
+}
+
+void HDSReporter::onReceiveTrailingMetadata(Http::HeaderMapPtr&& metadata) {
+  UNREFERENCED_PARAMETER(metadata);
+}
+
+void HDSReporter::onRemoteClose(Grpc::Status::GrpcStatus status, const std::string& message) {
+  ENVOY_LOG(warn, "gRPC config stream closed: {}, {}", status, message);
+  response_timer_->disableTimer();
+  stream_ = nullptr;
+  handleFailure();
+}
+
+} // namespace Upstream
+} // namespace Envoy
