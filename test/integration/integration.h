c5dc25613af163616632d692a7c125f6804a9d08
Xin Zhuang <stevenzzz@google.com>
2018-06-19 17:10:13 -0400

    Revert "network: adding error handling for calling readDisable on a closed connection (#3669)" revert to get clean master.
    This reverts commit b0a518d064c8255e0e20557a8f909b6ff457558f.

--- test/integration/integration.h
+++ test/integration/integration.h
@@ -77,7 +77,7 @@ public:
 
   void close();
   void waitForData(const std::string& data);
-  void waitForDisconnect(bool ignore_spurious_events = false);
+  void waitForDisconnect();
   void waitForHalfClose();
   void readDisable(bool disabled);
   void write(const std::string& data, bool end_stream = false, bool verify = true);
