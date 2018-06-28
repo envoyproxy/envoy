280fd985a19e2bc70248a00afb45ca4f87f3c987
Yangmin Zhu <ymzhu@google.com>
2018-06-28 10:58:41 -0700

    address comment.
    Signed-off-by: Yangmin Zhu <ymzhu@google.com>

--- source/common/common/logger.h
+++ source/common/common/logger.h
@@ -26,27 +26,27 @@ namespace Logger {
   FUNCTION(client)               \
   FUNCTION(config)               \
   FUNCTION(connection)           \
-  FUNCTION(misc)                 \
   FUNCTION(file)                 \
   FUNCTION(filter)               \
+  FUNCTION(grpc)                 \
   FUNCTION(hc)                   \
   FUNCTION(health_checker)       \
   FUNCTION(http)                 \
   FUNCTION(http2)                \
   FUNCTION(lua)                  \
   FUNCTION(main)                 \
+  FUNCTION(misc)                 \
   FUNCTION(mongo)                \
   FUNCTION(pool)                 \
+  FUNCTION(rbac)                 \
   FUNCTION(redis)                \
   FUNCTION(router)               \
   FUNCTION(runtime)              \
-  FUNCTION(testing)              \
-  FUNCTION(tracing)              \
-  FUNCTION(upstream)             \
-  FUNCTION(grpc)                 \
   FUNCTION(stats)                \
+  FUNCTION(testing)              \
   FUNCTION(thrift)               \
-  FUNCTION(rbac)
+  FUNCTION(tracing)              \
+  FUNCTION(upstream)
 
 enum class Id {
   ALL_LOGGER_IDS(GENERATE_ENUM)
