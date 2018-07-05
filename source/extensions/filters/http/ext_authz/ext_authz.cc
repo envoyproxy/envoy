8604c3c6faa80958dde674ed48b6fa34f7b9433e
Gabriel <gsagula@gmail.com>
2018-07-05 15:12:02 -0700

    authz_bugfix: fixed format
    Signed-off-by: Gabriel <gsagula@gmail.com>

--- source/extensions/filters/http/ext_authz/ext_authz.cc
+++ source/extensions/filters/http/ext_authz/ext_authz.cc
@@ -104,16 +104,16 @@ void Filter::onComplete(Filters::Common::ExtAuthz::ResponsePtr&& response) {
     ENVOY_STREAM_LOG(debug, "Ext_authz rejected the request", *callbacks_);
     ENVOY_STREAM_LOG(trace, "Ext_authz downstream header(s):", *callbacks_);
     const std::string body = std::move(response->body);
-    callbacks_->sendLocalReply(
-        response->status_code, body,
-        [& headers = response->headers_to_add,
-         &callbacks = *callbacks_ ](Http::HeaderMap & response_headers)
-            ->void {
-              for (const auto& header : headers) {
-                response_headers.addCopy(header.first, header.second);
-                ENVOY_STREAM_LOG(trace, " '{}':'{}'", callbacks, header.first.get(), header.second);
-              }
-            });
+    callbacks_->sendLocalReply(response->status_code, body,
+                               [& headers = response->headers_to_add,
+                                &callbacks = *callbacks_ ](Http::HeaderMap & response_headers)
+                                   ->void {
+                                     for (const auto& header : headers) {
+                                       response_headers.addCopy(header.first, header.second);
+                                       ENVOY_STREAM_LOG(trace, " '{}':'{}'", callbacks,
+                                                        header.first.get(), header.second);
+                                     }
+                                   });
     callbacks_->requestInfo().setResponseFlag(
         RequestInfo::ResponseFlag::UnauthorizedExternalService);
   } else {
