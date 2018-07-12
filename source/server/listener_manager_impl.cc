4cd5313fddef43a7e1ad80366216f6cab43f2cad
Piotr Sikora <piotrsikora@google.com>
2018-07-12 22:36:25 +0000

    review: replace EMPTY_STRING with empty() check.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- source/server/listener_manager_impl.cc
+++ source/server/listener_manager_impl.cc
@@ -379,7 +379,7 @@ void ListenerImpl::finishFilterChain() {
     std::vector<std::pair<ServerNamesMapSharedPtr, std::vector<Network::Address::CidrRange>>> list;
     for (const auto& entry : destination_ips_map) {
       std::vector<Network::Address::CidrRange> subnets;
-      if (entry.first == EMPTY_STRING) {
+      if (entry.first.empty()) {
         list.push_back(
             std::make_pair<ServerNamesMapSharedPtr, std::vector<Network::Address::CidrRange>>(
                 std::make_shared<ServerNamesMap>(entry.second),
