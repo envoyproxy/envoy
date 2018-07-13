89f27cb352d8ae90fdb31f328e4c333298e4cd03
Alex Konradi <akonradi@google.com>
2018-07-13 15:31:10 -0400

    Add ASSERT for cluster priority indexing.
    Signed-off-by: Alex Konradi <akonradi@google.com>

--- source/common/upstream/upstream_impl.cc
+++ source/common/upstream/upstream_impl.cc
@@ -696,6 +696,7 @@ void PriorityStateManager::updateClusterPrioritySet(
   LocalityWeightsMap empty_locality_map;
   LocalityWeightsMap& locality_weights_map =
       priority_state_.empty() ? empty_locality_map : priority_state_[priority].second;
+  ASSERT(priority_state_.empty() || priority_state_.size() > priority);
   LocalityWeightsSharedPtr locality_weights;
   std::vector<HostVector> per_locality;
 
