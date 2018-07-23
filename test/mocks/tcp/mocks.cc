7c11e9296535f2ab9d48df4b7606f08d9706df88
Stephan Zuercher <zuercher@gmail.com>
2018-07-23 14:13:03 -0700

    tcp/conn_pool: improve interface for callers (#3903)
    tcp/conn_pool: improve interface for callers
    
    Provides additional pool failure reasons to allow Tcp::ConnectionPool
    callers to distinguish between time outs and connection failures.
    Passes through connection and buffer watermark events.
    
    A subsequent PR will switch the TCP proxy to use the TCP connection
    pool (and makes use of these features). Relates to #3818.
    
    *Risk Level*: low
    *Testing*: unit tests
    *Docs Changes*: n/a
    *Release Notes*: n/a
    
    Signed-off-by: Stephan Zuercher <stephan@turbinelabs.io>

--- test/mocks/tcp/mocks.cc
+++ test/mocks/tcp/mocks.cc
@@ -3,6 +3,10 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
+using testing::Invoke;
+using testing::ReturnRef;
+using testing::_;
+
 namespace Envoy {
 namespace Tcp {
 namespace ConnectionPool {
@@ -13,8 +17,39 @@ MockCancellable::~MockCancellable() {}
 MockUpstreamCallbacks::MockUpstreamCallbacks() {}
 MockUpstreamCallbacks::~MockUpstreamCallbacks() {}
 
-MockInstance::MockInstance() {}
+MockConnectionData::MockConnectionData() {
+  ON_CALL(*this, connection()).WillByDefault(ReturnRef(connection_));
+}
+MockConnectionData::~MockConnectionData() {}
+
+MockInstance::MockInstance() {
+  ON_CALL(*this, newConnection(_)).WillByDefault(Invoke([&](Callbacks& cb) -> Cancellable* {
+    return newConnectionImpl(cb);
+  }));
+}
 MockInstance::~MockInstance() {}
+
+MockCancellable* MockInstance::newConnectionImpl(Callbacks& cb) {
+  handles_.emplace_back();
+  callbacks_.push_back(&cb);
+  return &handles_.back();
+}
+
+void MockInstance::poolFailure(PoolFailureReason reason) {
+  Callbacks* cb = callbacks_.front();
+  callbacks_.pop_front();
+  handles_.pop_front();
+
+  cb->onPoolFailure(reason, host_);
+}
+
+void MockInstance::poolReady() {
+  Callbacks* cb = callbacks_.front();
+  callbacks_.pop_front();
+  handles_.pop_front();
+
+  cb->onPoolReady(connection_data_, host_);
+}
 
 } // namespace ConnectionPool
 } // namespace Tcp
