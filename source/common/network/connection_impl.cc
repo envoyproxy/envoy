42d07710361156395acd73e23dbcf2c82220bca7
Venil Noronha <veniln@vmware.com>
2018-07-26 13:02:52 -0700

    Address review comments
    * Use const Api::SysCallResult in connection_impl.cc
    * Capture result value in place of rc_ for better debugging
    
    Signed-off-by: Venil Noronha <veniln@vmware.com>

--- source/common/network/connection_impl.cc
+++ source/common/network/connection_impl.cc
@@ -551,7 +551,7 @@ ClientConnectionImpl::ClientConnectionImpl(
   }
 
   if (source_address != nullptr) {
-    Api::SysCallResult result = source_address->bind(fd());
+    const Api::SysCallResult result = source_address->bind(fd());
     if (result.rc_ < 0) {
       ENVOY_LOG_MISC(debug, "Bind failure. Failed to bind to {}: {}", source_address->asString(),
                      strerror(result.errno_));
@@ -568,7 +568,7 @@ ClientConnectionImpl::ClientConnectionImpl(
 
 void ClientConnectionImpl::connect() {
   ENVOY_CONN_LOG(debug, "connecting to {}", *this, socket_->remoteAddress()->asString());
-  Api::SysCallResult result = socket_->remoteAddress()->connect(fd());
+  const Api::SysCallResult result = socket_->remoteAddress()->connect(fd());
   if (result.rc_ == 0) {
     // write will become ready.
     ASSERT(connecting_);
