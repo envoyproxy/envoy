324e628b7c85bcbeacfb49050dd7bfdcc86702b4
Venil Noronha <venil.noronha@gmail.com>
2018-07-27 11:26:25 -0700

    syscall: refactor address APIs for deeper errno latching (#3897)
    * Refactor address APIs for deeper errno latching
    
    The errno set by a syscall can be overwritten by code (e.g. logging) as
    it propagates up through the call stack. This commit refactors the
    bind and connect methods in the address API to allow for returning the
    errno from deeper down the call stack i.e. as soon as a syscall is
    performed.
    
    Signed-off-by: Venil Noronha <veniln@vmware.com>

--- source/common/network/address_impl.cc
+++ source/common/network/address_impl.cc
@@ -212,14 +212,16 @@ bool Ipv4Instance::operator==(const Instance& rhs) const {
           (ip_.port() == rhs_casted->ip_.port()));
 }
 
-int Ipv4Instance::bind(int fd) const {
-  return ::bind(fd, reinterpret_cast<const sockaddr*>(&ip_.ipv4_.address_),
-                sizeof(ip_.ipv4_.address_));
+Api::SysCallResult Ipv4Instance::bind(int fd) const {
+  const int rc = ::bind(fd, reinterpret_cast<const sockaddr*>(&ip_.ipv4_.address_),
+                        sizeof(ip_.ipv4_.address_));
+  return {rc, errno};
 }
 
-int Ipv4Instance::connect(int fd) const {
-  return ::connect(fd, reinterpret_cast<const sockaddr*>(&ip_.ipv4_.address_),
-                   sizeof(ip_.ipv4_.address_));
+Api::SysCallResult Ipv4Instance::connect(int fd) const {
+  const int rc = ::connect(fd, reinterpret_cast<const sockaddr*>(&ip_.ipv4_.address_),
+                           sizeof(ip_.ipv4_.address_));
+  return {rc, errno};
 }
 
 int Ipv4Instance::socket(SocketType type) const { return socketFromSocketType(type); }
@@ -275,19 +277,20 @@ bool Ipv6Instance::operator==(const Instance& rhs) const {
           (ip_.port() == rhs_casted->ip_.port()));
 }
 
-int Ipv6Instance::bind(int fd) const {
-  return ::bind(fd, reinterpret_cast<const sockaddr*>(&ip_.ipv6_.address_),
-                sizeof(ip_.ipv6_.address_));
+Api::SysCallResult Ipv6Instance::bind(int fd) const {
+  const int rc = ::bind(fd, reinterpret_cast<const sockaddr*>(&ip_.ipv6_.address_),
+                        sizeof(ip_.ipv6_.address_));
+  return {rc, errno};
 }
 
-int Ipv6Instance::connect(int fd) const {
-  return ::connect(fd, reinterpret_cast<const sockaddr*>(&ip_.ipv6_.address_),
-                   sizeof(ip_.ipv6_.address_));
+Api::SysCallResult Ipv6Instance::connect(int fd) const {
+  const int rc = ::connect(fd, reinterpret_cast<const sockaddr*>(&ip_.ipv6_.address_),
+                           sizeof(ip_.ipv6_.address_));
+  return {rc, errno};
 }
 
 int Ipv6Instance::socket(SocketType type) const {
   const int fd = socketFromSocketType(type);
-
   // Setting IPV6_V6ONLY resticts the IPv6 socket to IPv6 connections only.
   const int v6only = ip_.v6only_;
   RELEASE_ASSERT(::setsockopt(fd, IPPROTO_IPV6, IPV6_V6ONLY, &v6only, sizeof(v6only)) != -1, "");
@@ -333,24 +336,28 @@ PipeInstance::PipeInstance(const std::string& pipe_path) : InstanceBase(Type::Pi
 
 bool PipeInstance::operator==(const Instance& rhs) const { return asString() == rhs.asString(); }
 
-int PipeInstance::bind(int fd) const {
+Api::SysCallResult PipeInstance::bind(int fd) const {
   if (abstract_namespace_) {
-    return ::bind(fd, reinterpret_cast<const sockaddr*>(&address_),
-                  offsetof(struct sockaddr_un, sun_path) + address_length_);
+    const int rc = ::bind(fd, reinterpret_cast<const sockaddr*>(&address_),
+                          offsetof(struct sockaddr_un, sun_path) + address_length_);
+    return {rc, errno};
   }
   // Try to unlink an existing filesystem object at the requested path. Ignore
   // errors -- it's fine if the path doesn't exist, and if it exists but can't
   // be unlinked then `::bind()` will generate a reasonable errno.
   unlink(address_.sun_path);
-  return ::bind(fd, reinterpret_cast<const sockaddr*>(&address_), sizeof(address_));
+  const int rc = ::bind(fd, reinterpret_cast<const sockaddr*>(&address_), sizeof(address_));
+  return {rc, errno};
 }
 
-int PipeInstance::connect(int fd) const {
+Api::SysCallResult PipeInstance::connect(int fd) const {
   if (abstract_namespace_) {
-    return ::connect(fd, reinterpret_cast<const sockaddr*>(&address_),
-                     offsetof(struct sockaddr_un, sun_path) + address_length_);
+    const int rc = ::connect(fd, reinterpret_cast<const sockaddr*>(&address_),
+                             offsetof(struct sockaddr_un, sun_path) + address_length_);
+    return {rc, errno};
   }
-  return ::connect(fd, reinterpret_cast<const sockaddr*>(&address_), sizeof(address_));
+  const int rc = ::connect(fd, reinterpret_cast<const sockaddr*>(&address_), sizeof(address_));
+  return {rc, errno};
 }
 
 int PipeInstance::socket(SocketType type) const { return socketFromSocketType(type); }
