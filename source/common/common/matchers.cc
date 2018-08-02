a2afa7bb0889efc4c447f2007b1fd12b7fcec5bf
Limin Wang <liminwang@google.com>
2018-08-02 15:00:23 -0700

    Fix format.
    Signed-off-by: Limin Wang <liminwang@google.com>

--- source/common/common/matchers.cc
+++ source/common/common/matchers.cc
@@ -92,7 +92,7 @@ bool ListMatcher::match(const ProtobufWkt::Value& value) const {
 
   if (oneof_value_matcher_) {
     for (int i = 0; i < value.list_value().values_size(); ++i) {
-      const auto &lv = value.list_value().values(i);
+      const auto& lv = value.list_value().values(i);
 
       if (oneof_value_matcher_->match(lv)) {
         return true;
