bee31adb1b16503cb4ea4157113e1652b37e6707
Snow Pettersen <snowp@squareup.com>
2018-08-07 08:14:14 -0400

    formating
    Signed-off-by: Snow Pettersen <snowp@squareup.com>

--- source/common/upstream/eds.cc
+++ source/common/upstream/eds.cc
@@ -139,7 +139,8 @@ bool EdsClusterImpl::updateHostsPerLocality(
   // object for locality weights that we can update here, we should add something like this to
   // improve performance and scalability of locality weight updates.
 
-  if (updateDynamicHostList(new_hosts, *current_hosts_copy, hosts_added, hosts_removed, updated_hosts) ||
+  if (updateDynamicHostList(new_hosts, *current_hosts_copy, hosts_added, hosts_removed,
+                            updated_hosts) ||
       locality_weights_map != new_locality_weights_map) {
     locality_weights_map = new_locality_weights_map;
     ENVOY_LOG(debug, "EDS hosts or locality weights changed for cluster: {} ({}) priority {}",
