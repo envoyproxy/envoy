6e444dd70b42e37965ac16a4dce2e46a1adf1de9
Lilika Markatou <lilika@google.com>
2018-08-10 11:49:20 -0400

    Use jittered backoff for handling HdsDelegate stream/connection failures
    Signed-off-by: Lilika Markatou <lilika@google.com>

--- test/common/upstream/hds_test.cc
+++ test/common/upstream/hds_test.cc
@@ -227,12 +227,16 @@ TEST_F(HdsTest, TestMinimalSendResponse) {
 TEST_F(HdsTest, TestStreamConnectionFailure) {
   EXPECT_CALL(*async_client_, start(_, _))
       .WillOnce(Return(nullptr))
+      .WillOnce(Return(nullptr))
+      .WillOnce(Return(nullptr))
       .WillOnce(Return(&async_stream_));
-  EXPECT_CALL(*retry_timer_, enableTimer(_));
+  EXPECT_CALL(*retry_timer_, enableTimer(_)).Times(3);
   EXPECT_CALL(async_stream_, sendMessage(_, _));
 
   // Test connection failure and retry
   createHdsDelegate();
+  retry_timer_cb_();
+  retry_timer_cb_();
   retry_timer_cb_();
 }
 
