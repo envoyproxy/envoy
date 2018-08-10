bb70b42bb7eea4125be4502ef7fd7f73aab41bcf
htuch <htuch@users.noreply.github.com>
2018-08-10 14:27:12 -0400

    fuzz: router header formatter/parser fuzz test. (#4105)
    Risk level: Low
    Testing: corpus has 82.3% test coverage for header_formatter.cc and 82.5% for header_parser.cc
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

---
+++ test/common/router/header_parser_fuzz_test.cc
@@ -0,0 +1,24 @@
+#include "common/http/header_map_impl.h"
+#include "common/router/header_parser.h"
+
+#include "test/common/router/header_parser_fuzz.pb.h"
+#include "test/fuzz/fuzz_runner.h"
+#include "test/fuzz/utility.h"
+
+namespace Envoy {
+namespace Fuzz {
+
+DEFINE_PROTO_FUZZER(const test::common::router::TestCase& input) {
+  try {
+    Router::HeaderParserPtr parser =
+        Router::HeaderParser::configure(input.headers_to_add(), input.headers_to_remove());
+    Http::HeaderMapImpl header_map;
+    parser->evaluateHeaders(header_map, fromRequestInfo(input.request_info()));
+    ENVOY_LOG_MISC(trace, "Success");
+  } catch (const EnvoyException& e) {
+    ENVOY_LOG_MISC(debug, "EnvoyException: {}", e.what());
+  }
+}
+
+} // namespace Fuzz
+} // namespace Envoy
--- test/fuzz/utility.h
+++ test/fuzz/utility.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "common/network/utility.h"
+
 #include "test/common/access_log/test_util.h"
 #include "test/fuzz/common.pb.h"
+#include "test/mocks/upstream/host.h"
 #include "test/test_common/utility.h"
 
 namespace Envoy {
@@ -23,6 +26,15 @@ inline TestRequestInfo fromRequestInfo(const test::fuzz::RequestInfo& request_in
   if (request_info.has_response_code()) {
     test_request_info.response_code_ = request_info.response_code().value();
   }
+  auto upstream_host = std::make_shared<NiceMock<Upstream::MockHostDescription>>();
+  auto upstream_metadata =
+      std::make_shared<envoy::api::v2::core::Metadata>(request_info.upstream_metadata());
+  ON_CALL(*upstream_host, metadata()).WillByDefault(testing::Return(upstream_metadata));
+  test_request_info.upstream_host_ = upstream_host;
+  auto address = Network::Utility::resolveUrl("tcp://10.0.0.1:443");
+  test_request_info.upstream_local_address_ = address;
+  test_request_info.downstream_local_address_ = address;
+  test_request_info.downstream_remote_address_ = address;
   return test_request_info;
 }
 
