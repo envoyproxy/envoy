fc3cc916b7dd109da2b43771b8dc664bc7067e89
Snow Pettersen <snowp@squareup.com>
2018-08-16 14:36:47 -0400

    make LoadBalancerContext functions public to match parent
    Signed-off-by: Snow Pettersen <snowp@squareup.com>

--- source/common/upstream/load_balancer_impl.h
+++ source/common/upstream/load_balancer_impl.h
@@ -84,6 +84,7 @@ protected:
 };
 
 class LoadBalancerContextBase : public LoadBalancerContext {
+public:
   absl::optional<uint64_t> computeHashKey() override { return {}; }
 
   const Network::Connection* downstreamConnection() const override { return nullptr; }
