fa21faf4826faa4a7683b7ab4aa5a7a20287ab5b
Sam Smith <sesmith177@gmail.com>
2018-08-16 11:59:38 -0400

    Bump yaml-cpp so it builds with Visual Studio 15.8
    Version 0.6.2 of yaml-cpp does not build when using Visual Studio 15.8,
    for context see: https://github.com/jbeder/yaml-cpp/pull/597
    
    Updating to this commit fixes the issue.
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- ci/build_container/build_recipes/yaml-cpp.sh
+++ ci/build_container/build_recipes/yaml-cpp.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-VERSION=0.6.2
+# Pin to this commit to pick up fix for building on Visual Studio 15.8
+COMMIT=0f9a586ca1dc29c2ecb8dd715a315b93e3f40f79 # 2018-06-30
 
-curl https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-"$VERSION".tar.gz -sLo yaml-cpp-"$VERSION".tar.gz
-tar xf yaml-cpp-"$VERSION".tar.gz
-cd yaml-cpp-yaml-cpp-"$VERSION"
+git clone https://github.com/jbeder/yaml-cpp
+cd yaml-cpp
+git reset --hard "$COMMIT"
 
 mkdir build
 cd build
