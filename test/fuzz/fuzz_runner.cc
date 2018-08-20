5c058a88623254cf60b898117017f424238b9a9a
htuch <htuch@users.noreply.github.com>
2018-08-20 17:40:12 -0400

    fuzz: make temporary path and port handling less flaky for server fuz… (#4202)
    * Avoid reusing temporary paths across tests.
    
    * Force port zero binding on server_fuzz_test.
    
    * Force server_fuzz_test not to write access log to random places on FS.
    
    I don't think this is the last we'll see on the server_fuzz_test flake test, since this particular
    fuzz test is inherently non-hermetic. So, this seems to be the high payoff whackamole for now.
    
    Risk level: Low.
    Testing: Ran server_fuzz_test test under Bazel and oss-fuzz Docker image.
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- test/fuzz/fuzz_runner.cc
+++ test/fuzz/fuzz_runner.cc
@@ -2,6 +2,7 @@
 
 #include "common/common/logger.h"
 #include "common/common/thread.h"
+#include "common/common/utility.h"
 #include "common/event/libevent.h"
 
 #include "test/test_common/environment.h"
@@ -10,6 +11,15 @@ namespace Envoy {
 namespace Fuzz {
 
 spdlog::level::level_enum Runner::log_level_;
+
+PerTestEnvironment::PerTestEnvironment()
+    : test_tmpdir_([] {
+        const std::string fuzz_path = TestEnvironment::temporaryPath("fuzz_XXXXXX");
+        char test_tmpdir[fuzz_path.size() + 1];
+        StringUtil::strlcpy(test_tmpdir, fuzz_path.data(), fuzz_path.size() + 1);
+        RELEASE_ASSERT(::mkdtemp(test_tmpdir) != nullptr, "");
+        return std::string(test_tmpdir);
+      }()) {}
 
 void Runner::setupEnvironment(int argc, char** argv, spdlog::level::level_enum default_log_level) {
   Event::Libevent::Global::initialize();
