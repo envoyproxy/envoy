envoyproxy/envoy

commit f4ac32bfc884e418bd8c5bc7777e45f9d9f22b34

http: support appending to predefined inline headers. (#4211)
Previously we just ASSERTed when performing an addReferenceKey() with a
predefined header. This PR adds support for comma concatenation and
adjusts docs to make clear the contract for value lifetime across the
API surface.

Fixes #3919. Also resolves ClusterFuzz issue
https://oss-fuzz.com/v2/testcase-detail/5107723602493440?noredirect=1.

Risk level: High. This affects header processing and wire representation
of predefined headers when appending.
Testing: Unit/integration tests, corpus entry added.

Signed-off-by: Harvey Tuch <htuch@google.com>

include/envoy/http/header_map.h
@@ -321,10 +321,12 @@ public:
   /**
    * Add a reference header to the map. Both key and value MUST point to data that will live beyond
    * the lifetime of any request/response using the string (since a codec may optimize for zero
-   * copy). Nothing will be copied.
+   * copy). The key will not be copied and a best effort will be made not to
+   * copy the value (but this may happen when comma concatenating, see below).
    *
-   * Calling addReference multiple times for the same header will result in multiple headers being
-   * present in the HeaderMap.
+   * Calling addReference multiple times for the same header will result in:
+   * - Comma concatenation for predefined inline headers.
+   * - Multiple headers being present in the HeaderMap for other headers.
    *
    * @param key specifies the name of the header to add; it WILL NOT be copied.
    * @param value specifies the value of the header to add; it WILL NOT be copied.
@@ -336,8 +338,9 @@ public:
    * the lifetime of any request/response using the string (since a codec may optimize for zero
    * copy). The value will be copied.
    *
-   * Calling addReferenceKey multiple times for the same header will result in multiple headers
-   * being present in the HeaderMap.
+   * Calling addReference multiple times for the same header will result in:
+   * - Comma concatenation for predefined inline headers.
+   * - Multiple headers being present in the HeaderMap for other headers.
    *
    * @param key specifies the name of the header to add; it WILL NOT be copied.
    * @param value specifies the value of the header to add; it WILL be copied.
@@ -349,8 +352,9 @@ public:
    * live beyond the lifetime of any request/response using the string (since a codec may optimize
    * for zero copy). The value will be copied.
    *
-   * Calling addReferenceKey multiple times for the same header will result in multiple headers
-   * being present in the HeaderMap.
+   * Calling addReference multiple times for the same header will result in:
+   * - Comma concatenation for predefined inline headers.
+   * - Multiple headers being present in the HeaderMap for other headers.
    *
    * @param key specifies the name of the header to add; it WILL NOT be copied.
    * @param value specifies the value of the header to add; it WILL be copied.
@@ -360,8 +364,9 @@ public:
   /**
    * Add a header by copying both the header key and the value.
    *
-   * Calling addCopy multiple times for the same header will result in multiple headers being
-   * present in the HeaderMap.
+   * Calling addCopy multiple times for the same header will result in:
+   * - Comma concatenation for predefined inline headers.
+   * - Multiple headers being present in the HeaderMap for other headers.
    *
    * @param key specifies the name of the header to add; it WILL be copied.
    * @param value specifies the value of the header to add; it WILL be copied.
@@ -371,8 +376,9 @@ public:
   /**
    * Add a header by copying both the header key and the value.
    *
-   * Calling addCopy multiple times for the same header will result in multiple headers being
-   * present in the HeaderMap.
+   * Calling addCopy multiple times for the same header will result in:
+   * - Comma concatenation for predefined inline headers.
+   * - Multiple headers being present in the HeaderMap for other headers.
    *
    * @param key specifies the name of the header to add; it WILL be copied.
    * @param value specifies the value of the header to add; it WILL be copied.

source/common/http/header_map_impl.cc
@@ -67,13 +67,15 @@ void HeaderString::freeDynamic() {
 void HeaderString::append(const char* data, uint32_t size) {
   switch (type_) {
   case Type::Reference: {
-    // Switch back to inline and fall through. We do not actually append to the static string
-    // currently which would require a copy.
-    type_ = Type::Inline;
-    buffer_.dynamic_ = inline_buffer_;
-    string_length_ = 0;
-
-    FALLTHRU;
+    // Rather than be too clever and optimize this uncommon case, we dynamically
+    // allocate and copy.
+    type_ = Type::Dynamic;
+    dynamic_capacity_ = (string_length_ + size) * 2;
+    char* buf = static_cast<char*>(malloc(dynamic_capacity_));
+    RELEASE_ASSERT(buf != nullptr, "");
+    memcpy(buf, buffer_.ref_, string_length_);
+    buffer_.dynamic_ = buf;
+    break;
   }
 
   case Type::Inline: {
@@ -94,6 +96,7 @@ void HeaderString::append(const char* data, uint32_t size) {
       RELEASE_ASSERT(new_capacity <= std::numeric_limits<uint32_t>::max(), "");
       buffer_.dynamic_ = static_cast<char*>(malloc(new_capacity));
       memcpy(buffer_.dynamic_, inline_buffer_, string_length_);
+      RELEASE_ASSERT(buffer_.dynamic_ != nullptr, "");
       dynamic_capacity_ = new_capacity;
       type_ = Type::Dynamic;
     } else {
@@ -101,6 +104,7 @@ void HeaderString::append(const char* data, uint32_t size) {
         // Need to reallocate.
         dynamic_capacity_ = (string_length_ + size) * 2;
         buffer_.dynamic_ = static_cast<char*>(realloc(buffer_.dynamic_, dynamic_capacity_));
+        RELEASE_ASSERT(buffer_.dynamic_ != nullptr, "");
       }
     }
   }
@@ -323,8 +327,12 @@ void HeaderMapImpl::insertByKey(HeaderString&& key, HeaderString&& value) {
   if (cb) {
     key.clear();
     StaticLookupResponse ref_lookup_response = cb(*this);
-    ASSERT(*ref_lookup_response.entry_ == nullptr); // This function doesn't handle append.
-    maybeCreateInline(ref_lookup_response.entry_, *ref_lookup_response.key_, std::move(value));
+    if (*ref_lookup_response.entry_ == nullptr) {
+      maybeCreateInline(ref_lookup_response.entry_, *ref_lookup_response.key_, std::move(value));
+    } else {
+      appendToHeader((*ref_lookup_response.entry_)->value(), value.c_str());
+      value.clear();
+    }
   } else {
     std::list<HeaderEntryImpl>::iterator i = headers_.insert(std::move(key), std::move(value));
     i->entry_ = i;

test/common/http/header_map_impl_test.cc
@@ -110,7 +110,7 @@ TEST(HeaderStringTest, All) {
     HeaderString string(static_string);
     EXPECT_EQ(HeaderString::Type::Reference, string.type());
     string.append("a", 1);
-    EXPECT_STREQ("a", string.c_str());
+    EXPECT_STREQ("HELLOa", string.c_str());
   }
 
   // Copy inline
@@ -425,11 +425,37 @@ TEST(HeaderMapImplTest, SetRemovesAllValues) {
 }
 
 TEST(HeaderMapImplTest, DoubleInlineAdd) {
-  HeaderMapImpl headers;
-  headers.addReferenceKey(Headers::get().ContentLength, 5);
-  EXPECT_DEBUG_DEATH(headers.addReferenceKey(Headers::get().ContentLength, 6), "");
-  EXPECT_STREQ("5", headers.ContentLength()->value().c_str());
-  EXPECT_EQ(1UL, headers.size());
+  {
+    HeaderMapImpl headers;
+    const std::string foo("foo");
+    const std::string bar("bar");
+    headers.addReference(Headers::get().ContentLength, foo);
+    headers.addReference(Headers::get().ContentLength, bar);
+    EXPECT_STREQ("foo,bar", headers.ContentLength()->value().c_str());
+    EXPECT_EQ(1UL, headers.size());
+  }
+  {
+    HeaderMapImpl headers;
+    headers.addReferenceKey(Headers::get().ContentLength, "foo");
+    headers.addReferenceKey(Headers::get().ContentLength, "bar");
+    EXPECT_STREQ("foo,bar", headers.ContentLength()->value().c_str());
+    EXPECT_EQ(1UL, headers.size());
+  }
+  {
+    HeaderMapImpl headers;
+    headers.addReferenceKey(Headers::get().ContentLength, 5);
+    headers.addReferenceKey(Headers::get().ContentLength, 6);
+    EXPECT_STREQ("5,6", headers.ContentLength()->value().c_str());
+    EXPECT_EQ(1UL, headers.size());
+  }
+  {
+    HeaderMapImpl headers;
+    const std::string foo("foo");
+    headers.addReference(Headers::get().ContentLength, foo);
+    headers.addReferenceKey(Headers::get().ContentLength, 6);
+    EXPECT_STREQ("foo,6", headers.ContentLength()->value().c_str());
+    EXPECT_EQ(1UL, headers.size());
+  }
 }
 
 TEST(HeaderMapImplTest, DoubleInlineSet) {

test/common/router/header_parser_corpus/clusterfuzz-testcase-header_parser_fuzz_test-5107723602493440
@@ -1,0 +1,14 @@
+headers_to_add {
+}
+headers_to_add {
+  header {
+    key: "te"
+    value: "l"
+  }
+}
+headers_to_add {
+  header {
+    key: "te"
+    value: "@"
+  }
+}

test/integration/header_integration_test.cc
@@ -120,6 +120,26 @@ route_config:
               - header:
                   key: "x-real-ip"
                   value: "%DOWNSTREAM_REMOTE_ADDRESS_WITHOUT_PORT%"
+    - name: append-same-headers
+      domains: ["append-same-headers.com"]
+      request_headers_to_add:
+        - header:
+            key: "x-foo"
+            value: "value1"
+        - header:
+            key: "authorization"
+            value: "token1"
+      routes:
+        - match: { prefix: "/test" }
+          route:
+            cluster: cluster_0
+            request_headers_to_add:
+              - header:
+                  key: "x-foo"
+                  value: "value2"
+              - header:
+                  key: "authorization"
+                  value: "token2"
 )EOF";
 
 } // namespace
@@ -901,6 +921,41 @@ TEST_P(HeaderIntegrationTest, TestXFFParsing) {
           {"x-real-ip", "5.6.7.8"},
           {":path", "/test"},
           {":method", "GET"},
+      },
+      Http::TestHeaderMapImpl{
+          {"server", "envoy"},
+          {"content-length", "0"},
+          {":status", "200"},
+          {"x-unmodified", "response"},
+      },
+      Http::TestHeaderMapImpl{
+          {"server", "envoy"},
+          {"x-unmodified", "response"},
+          {":status", "200"},
+      });
+}
+
+// Validates behavior around same header appending (both predefined headers and
+// other).
+TEST_P(HeaderIntegrationTest, TestAppendSameHeaders) {
+  initializeFilter(HeaderMode::Append, false);
+  performRequest(
+      Http::TestHeaderMapImpl{
+          {":method", "GET"},
+          {":path", "/test"},
+          {":scheme", "http"},
+          {":authority", "append-same-headers.com"},
+          {"authorization", "token3"},
+          {"x-foo", "value3"},
+      },
+      Http::TestHeaderMapImpl{
+          {":authority", "append-same-headers.com"},
+          {":path", "/test"},
+          {":method", "GET"},
+          {"authorization", "token3,token2,token1"},
+          {"x-foo", "value3"},
+          {"x-foo", "value2"},
+          {"x-foo", "value1"},
       },
       Http::TestHeaderMapImpl{
           {"server", "envoy"},

commit 2c9cc74ccaf491d5162376c395ed13b51705e14e

http: fix allocation bug introduced in #4211.
There were some non-local invariants that header_map_impl_fuzz_test surfaced around minimum dynamic
buffer size. This PR improves comments and documentation of invariants and fixes the allocation issue
to maintain them.

Fixes oss-fuzz issue https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=10038.

Risk level: Low. It's recommended to bump to this for potential security reasons if you are already post
  #4211.
Testing: Unit test and corpus entry added.

Signed-off-by: Harvey Tuch <htuch@google.com>

include/envoy/http/header_map.h
@@ -155,10 +155,13 @@ public:
 
 private:
   union {
+    // This should reference inline_buffer_ for Type::Inline.
     char* dynamic_;
     const char* ref_;
   } buffer_;
 
+  // Capacity in both Type::Inline and Type::Dynamic cases must be at least MinDynamicCapacity in
+  // header_map_impl.cc.
   union {
     char inline_buffer_[128];
     uint32_t dynamic_capacity_;

source/common/http/header_map_impl.cc
@@ -14,9 +14,17 @@
 namespace Envoy {
 namespace Http {
 
+namespace {
+constexpr size_t MinDynamicCapacity{32};
+// This includes the NULL (StringUtil::itoa technically only needs 21).
+constexpr size_t MaxIntegerLength{32};
+} // namespace
+
 HeaderString::HeaderString() : type_(Type::Inline) {
   buffer_.dynamic_ = inline_buffer_;
   clear();
+  static_assert(sizeof(inline_buffer_) >= MaxIntegerLength, "");
+  static_assert(MinDynamicCapacity >= MaxIntegerLength, "");
 }
 
 HeaderString::HeaderString(const LowerCaseString& ref_value) : type_(Type::Reference) {
@@ -70,7 +78,8 @@ void HeaderString::append(const char* data, uint32_t size) {
     // Rather than be too clever and optimize this uncommon case, we dynamically
     // allocate and copy.
     type_ = Type::Dynamic;
-    dynamic_capacity_ = (string_length_ + size) * 2;
+    dynamic_capacity_ =
+        std::max(MinDynamicCapacity, static_cast<size_t>((string_length_ + size) * 2));
     char* buf = static_cast<char*>(malloc(dynamic_capacity_));
     RELEASE_ASSERT(buf != nullptr, "");
     memcpy(buf, buffer_.ref_, string_length_);
@@ -94,6 +103,7 @@ void HeaderString::append(const char* data, uint32_t size) {
       // If the resizing will cause buffer overflow due to hitting uint32_t::max, an OOM is likely
       // imminent. Fast-fail rather than allow a buffer overflow attack (issue #1421)
       RELEASE_ASSERT(new_capacity <= std::numeric_limits<uint32_t>::max(), "");
+      ASSERT(new_capacity >= MinDynamicCapacity);
       buffer_.dynamic_ = static_cast<char*>(malloc(new_capacity));
       memcpy(buffer_.dynamic_, inline_buffer_, string_length_);
       RELEASE_ASSERT(buffer_.dynamic_ != nullptr, "");
@@ -182,8 +192,13 @@ void HeaderString::setInteger(uint64_t value) {
   }
 
   case Type::Inline:
+    // buffer_.dynamic_ should always point at inline_buffer_ for Type::Inline.
+    ASSERT(buffer_.dynamic_ == inline_buffer_);
   case Type::Dynamic: {
     // Whether dynamic or inline the buffer is guaranteed to be large enough.
+    ASSERT(dynamic_capacity_ >= MaxIntegerLength);
+    // It's safe to use buffer.dynamic_, since buffer.ref_ is union aliased.
+    ASSERT(&buffer_.dynamic_ == &buffer_.ref_);
     string_length_ = StringUtil::itoa(buffer_.dynamic_, 32, value);
   }
   }

test/common/http/header_map_impl_corpus/clusterfuzz-testcase-minimized-header_map_impl_fuzz_test-5689833624698880
@@ -1,0 +1,1 @@
+actions { } actions { } actions { } actions { } actions { } actions { } actions { } actions { } actions { } actions { } actions { } actions { } actions { } actions { } actions { } actions { } actions { } actions { } actions {   set_reference {   } } actions { } actions {   get_and_mutate {     append: ""   } } actions { } actions {   get_and_mutate {     set_integer: 0   } } actions { } actions { } actions { } actions { } actions { } actions { } actions { } actions { } 

test/common/http/header_map_impl_test.cc
@@ -692,6 +692,15 @@ TEST(HeaderMapImplTest, TestAppendHeader) {
     HeaderMapImpl::appendToHeader(value3, "");
     EXPECT_EQ(value3, "empty");
   }
+  // Regression test for appending to an empty string with empty, then setting integer.
+  {
+    const std::string empty;
+    HeaderString value4(empty);
+    HeaderMapImpl::appendToHeader(value4, " ");
+    value4.setInteger(0);
+    EXPECT_STREQ("0", value4.c_str());
+    EXPECT_EQ(1U, value4.size());
+  }
 }
 
 TEST(HeaderMapImplTest, PseudoHeaderOrder) {