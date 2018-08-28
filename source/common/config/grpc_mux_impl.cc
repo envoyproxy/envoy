981914f94d7c2a03fa51823af695976d0ecbe550
Rama <rama.rao@salesforce.com>
2018-08-28 18:35:28 +0530

    fix ads integration test
    Signed-off-by: Rama <rama.rao@salesforce.com>

--- source/common/config/grpc_mux_impl.cc
+++ source/common/config/grpc_mux_impl.cc
@@ -3,7 +3,6 @@
 #include <unordered_set>
 
 #include "common/common/token_bucket_impl.h"
-#include "common/config/resources.h"
 #include "common/config/utility.h"
 #include "common/protobuf/protobuf.h"
 
@@ -207,31 +206,25 @@ void GrpcMuxImpl::onReceiveMessage(std::unique_ptr<envoy::api::v2::DiscoveryResp
       resources.emplace(resource_name, resource);
     }
     for (auto watch : api_state_[type_url].watches_) {
+      // onConfigUpdate should be called in all cases for single watch xDS (Cluster and Listener)
+      // even if the message does not have resources so that update_empty stat is properly
+      // incremented.
       if (watch->resources_.empty()) {
         watch->callbacks_.onConfigUpdate(message->resources(), message->version_info());
         continue;
       }
-      bool resource_found = false;
       Protobuf::RepeatedPtrField<ProtobufWkt::Any> found_resources;
       for (auto watched_resource_name : watch->resources_) {
         auto it = resources.find(watched_resource_name);
         if (it != resources.end()) {
           found_resources.Add()->MergeFrom(it->second);
-          resource_found = true;
         }
       }
-
-      // onConfigUpdate should be called even if the found_resources is empty for single watch xDS
-      // (Cluster and Listener) so that update_empty stat is incremented. onConfigUpdate with empty
-      // resources is called for every watch, only if message has no resources for multi watch xDS
-      // (ClusterLoadAssignment and RouteConfiguration).
-      if (type_url == Config::TypeUrl::get().Cluster ||
-          type_url == Config::TypeUrl::get().Listener) {
+      // onConfigUpdate should be called for all watches only if message has no resources for multi
+      // watch xDS (ClusterLoadAssignment and RouteConfiguration). Otherwise only call on
+      // watches(clusters/routes) that have updates in the message.
+      if (message->resources().empty() || found_resources.size() > 0) {
         watch->callbacks_.onConfigUpdate(found_resources, message->version_info());
-      } else {
-        if (message->resources().empty() || resource_found) {
-          watch->callbacks_.onConfigUpdate(found_resources, message->version_info());
-        }
       }
     }
     // TODO(mattklein123): In the future if we start tracking per-resource versions, we would do
