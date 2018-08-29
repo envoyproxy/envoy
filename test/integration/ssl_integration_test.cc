5e97bc96af0ee6fe4ca1734545c852aed53e4797
Michael Behr <mkbehr@google.com>
2018-08-29 15:48:18 -0400

    Skip SSL RouterDownstreamDisconnectBeforeRequestComplete on OS X
    Signed-off-by: Michael Behr <mkbehr@google.com>

--- test/integration/ssl_integration_test.cc
+++ test/integration/ssl_integration_test.cc
@@ -150,6 +150,13 @@ TEST_P(SslIntegrationTest, RouterUpstreamDisconnectBeforeResponseComplete) {
 }
 
 TEST_P(SslIntegrationTest, RouterDownstreamDisconnectBeforeRequestComplete) {
+#ifdef __APPLE__
+  // Skip this test on OS X: we can't detect the early close on OS X, and we
+  // won't clean up the upstream connection until it times out. See #4294.
+  if (downstream_protocol_ == Http::CodecClient::Type::HTTP1) {
+    return;
+  }
+#endif
   ConnectionCreationFunction creator = [&]() -> Network::ClientConnectionPtr {
     return makeSslClientConnection(false, false);
   };
