f36a870699d78646c9f7a2be56f06f58029eb81a
JimmyCYJ <jimmychen.0102@gmail.com>
2018-08-29 13:34:26 -0700

    Fix stats name.
    Signed-off-by: JimmyCYJ <jimmychen.0102@gmail.com>

--- source/common/ssl/ssl_socket.cc
+++ source/common/ssl/ssl_socket.cc
@@ -427,7 +427,7 @@ Network::TransportSocketPtr ClientSslSocketFactory::createTransportSocket() cons
     return std::make_unique<Ssl::SslSocket>(std::move(ssl_ctx), Ssl::InitialState::Client);
   } else {
     ENVOY_LOG(debug, "Create NotReadySslSocket");
-    stats_.upstream_context_secrets_not_ready.inc();
+    stats_.upstream_context_secrets_not_ready_.inc();
     return std::make_unique<NotReadySslSocket>();
   }
 }
@@ -459,7 +459,7 @@ Network::TransportSocketPtr ServerSslSocketFactory::createTransportSocket() cons
     return std::make_unique<Ssl::SslSocket>(std::move(ssl_ctx), Ssl::InitialState::Server);
   } else {
     ENVOY_LOG(debug, "Create NotReadySslSocket");
-    stats_.downstream_context_secrets_not_ready.inc();
+    stats_.downstream_context_secrets_not_ready_.inc();
     return std::make_unique<NotReadySslSocket>();
   }
 }
