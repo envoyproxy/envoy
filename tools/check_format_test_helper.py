a302856c0815ca551cc6c4166a81a6db27bcab19
Alex Konradi <akonradi@google.com>
2018-09-05 16:01:31 -0400

    Add check_format.py check for <shared_mutex>
    Signed-off-by: Alex Konradi <akonradi@google.com>

--- tools/check_format_test_helper.py
+++ tools/check_format_test_helper.py
@@ -143,6 +143,9 @@ if __name__ == "__main__":
                                     "Don't use <mutex> or <condition_variable*>")
   errors += fixFileExpectingFailure("condition_variable_any.cc",
                                     "Don't use <mutex> or <condition_variable*>")
+  errors += checkFileExpectingError("shared_mutex.cc", "shared_mutex")
+  errors += fixFileExpectingFailure("shared_mutex.cc", "shared_mutex")
+
   real_time_inject_error = (
       "Don't reference real-world time sources from production code; use injection")
   errors += fixFileExpectingFailure("real_time_source.cc", real_time_inject_error)
