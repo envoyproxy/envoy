b4150fdc0fee8cee3565b8d48b4b44d95a404c25
Snow Pettersen <snowp@squareup.com>
2018-09-07 15:22:32 -0400

    add username to TODOs and not-implemented-hide to protos
    Signed-off-by: Snow Pettersen <snowp@squareup.com>

--- source/common/router/config_impl.h
+++ source/common/router/config_impl.h
@@ -140,10 +140,12 @@ public:
   const RateLimitPolicy& rateLimitPolicy() const override { return rate_limit_policy_; }
   const Config& routeConfig() const override;
   const RouteSpecificFilterConfig* perFilterConfig(const std::string&) const override;
-  Upstream::RetryPrioritySharedPtr retryPriority() override { return nullptr; } // TODO: implement
+  Upstream::RetryPrioritySharedPtr retryPriority() override {
+    return nullptr;
+  } // TODO(snowp): implement
   Upstream::RetryHostPredicateSharedPtr retryHostPredicate() override {
     return nullptr;
-  } // TODO: implement
+  } // TODO(snowp): implement
 
 private:
   enum class SslRequirements { NONE, EXTERNAL_ONLY, ALL };
