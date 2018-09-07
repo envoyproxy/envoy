12957a29bd2627bc0bba921603bd5869a449be47
Elisha Ziskind <eziskind@google.com>
2018-09-07 09:19:19 -0400

    overload: add a file-based injected resource monitor (#4298)
    This allows injecting a synthetic resource pressure from a file, primarily intended for use in integration tests to force envoy into an overloaded state.
    
    Testing: unit tests
    Docs Changes: docs updated to reference new resource monitor
    
    Signed-off-by: Elisha Ziskind eziskind@google.com

---
+++ source/extensions/resource_monitors/injected_resource/config.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "envoy/config/resource_monitor/injected_resource/v2alpha/injected_resource.pb.validate.h"
+#include "envoy/server/resource_monitor_config.h"
+
+#include "extensions/resource_monitors/common/factory_base.h"
+#include "extensions/resource_monitors/well_known_names.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace ResourceMonitors {
+namespace InjectedResourceMonitor {
+
+class InjectedResourceMonitorFactory
+    : public Common::FactoryBase<
+          envoy::config::resource_monitor::injected_resource::v2alpha::InjectedResourceConfig> {
+public:
+  InjectedResourceMonitorFactory() : FactoryBase(ResourceMonitorNames::get().InjectedResource) {}
+
+private:
+  Server::ResourceMonitorPtr createResourceMonitorFromProtoTyped(
+      const envoy::config::resource_monitor::injected_resource::v2alpha::InjectedResourceConfig&
+          config,
+      Server::Configuration::ResourceMonitorFactoryContext& context) override;
+};
+
+} // namespace InjectedResourceMonitor
+} // namespace ResourceMonitors
+} // namespace Extensions
+} // namespace Envoy
