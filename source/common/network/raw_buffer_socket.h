3cc57db39d2bcc38489883ca87a195020ee25816
Vadim Eisenberg <vadime@il.ibm.com>
2018-09-09 04:26:30 +0300

    fix format
    Signed-off-by: Vadim Eisenberg <vadime@il.ibm.com>

--- source/common/network/raw_buffer_socket.h
+++ source/common/network/raw_buffer_socket.h
@@ -29,7 +29,8 @@ private:
 class RawBufferSocketFactory : public TransportSocketFactory {
 public:
   // Network::TransportSocketFactory
-  TransportSocketPtr createTransportSocket(const Network::ConnectionSocket::OptionsSharedPtr& options) const override;
+  TransportSocketPtr
+  createTransportSocket(const Network::ConnectionSocket::OptionsSharedPtr& options) const override;
   bool implementsSecureTransport() const override;
 };
 
