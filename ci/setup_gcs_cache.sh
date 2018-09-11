4aeb58cdf8cb0d8b7bd118fc450f6cacc5ab8500
Stephan Zuercher <stephan@turbinelabs.io>
2018-09-11 15:06:59 -0700

    ci: report compiler version when setting up cache
    Signed-off-by: Stephan Zuercher <stephan@turbinelabs.io>

--- ci/setup_gcs_cache.sh
+++ ci/setup_gcs_cache.sh
@@ -2,8 +2,22 @@
 
 set -e
 
+function dump_cc_version {
+    local XCC="${CC:-gcc}"
+
+    if [[ $(uname -s) == "Linux" ]]; then
+        local XCC_PATH=$(readlink -f $(which "${XCC}"))
+        local XCC_PKG=$(dpkg -S "${XCC}" | grep "${XCC_PATH}" | cut -d':' -f1)
+        local XCC_VERSION=$(dpkg-query --showformat='${Version}' --show "${XCC_PKG}")
+    else
+        local XCC_VERSION=$(clang -v 2>&1 | head -n 1)
+    fi
+    echo "Setting up $1 remote cache for ${XCC_VERSION}"
+}
+
 if [[ ! -z "${BAZEL_REMOTE_CACHE}" ]]; then
   if [[ ! -z "${GCP_SERVICE_ACCOUNT_KEY}" ]]; then
+    dump_cc_version "read/write"
     # mktemp will create a tempfile with u+rw permission minus umask, it will not be readable by all
     # users by default.
     GCP_SERVICE_ACCOUNT_KEY_FILE=$(mktemp -t gcp_service_account.XXXXXX.json)
@@ -22,6 +36,7 @@ if [[ ! -z "${BAZEL_REMOTE_CACHE}" ]]; then
       --google_credentials=${GCP_SERVICE_ACCOUNT_KEY_FILE}"
     echo "Set up bazel read/write HTTP cache at ${BAZEL_REMOTE_CACHE}."
   else
+    dump_cc_version "read-only"
     export BAZEL_BUILD_EXTRA_OPTIONS="${BAZEL_BUILD_EXTRA_OPTIONS} \
       --remote_http_cache=${BAZEL_REMOTE_CACHE} --noremote_upload_local_results"
     echo "Set up bazel read only HTTP cache at ${BAZEL_REMOTE_CACHE}."
