1457fb1b4cccb8513c218c1675c30030d6338843
Nick A. Smith <nick.a.smith@thales-esecurity.com>
2018-09-12 21:38:08 +0100

    Moved Mock constructors from header to translation unit
    Signed-off-by: Nick A. Smith <nick.a.smith@thales-esecurity.com>

--- test/extensions/filters/http/common/mock.h
+++ test/extensions/filters/http/common/mock.h
@@ -15,45 +15,22 @@ public:
   MOCK_METHOD2(fetch, void(const ::envoy::api::v2::core::HttpUri& uri, JwksReceiver& receiver));
 };
 
-// A mock HTTP upstream with response body.
+// A mock HTTP upstream.
 class MockUpstream {
 public:
+  /**
+   * Mock upstream which returns a given response body.
+   */
   MockUpstream(Upstream::MockClusterManager& mock_cm, const std::string& status,
-               const std::string& response_body)
-      : request_(&mock_cm.async_client_), status_(status), response_body_(response_body) {
-    ON_CALL(mock_cm.async_client_, send_(testing::_, testing::_, testing::_))
-        .WillByDefault(testing::Invoke([this](Http::MessagePtr&, Http::AsyncClient::Callbacks& cb,
-                                              const absl::optional<std::chrono::milliseconds>&)
-                                           -> Http::AsyncClient::Request* {
-          Http::MessagePtr response_message(new Http::ResponseMessageImpl(
-              Http::HeaderMapPtr{new Http::TestHeaderMapImpl{{":status", status_}}}));
-          if (response_body_.length()) {
-            response_message->body().reset(new Buffer::OwnedImpl(response_body_));
-          } else {
-            response_message->body().reset(nullptr);
-          }
-          cb.onSuccess(std::move(response_message));
-          return &request_;
-        }));
-  }
-
-  MockUpstream(Upstream::MockClusterManager& mock_cm, Http::AsyncClient::FailureReason reason)
-      : request_(&mock_cm.async_client_) {
-    ON_CALL(mock_cm.async_client_, send_(testing::_, testing::_, testing::_))
-        .WillByDefault(testing::Invoke(
-            [this, reason](
-                Http::MessagePtr&, Http::AsyncClient::Callbacks& cb,
-                const absl::optional<std::chrono::milliseconds>&) -> Http::AsyncClient::Request* {
-              cb.onFailure(reason);
-              return &request_;
-            }));
-  }
-
-  MockUpstream(Upstream::MockClusterManager& mock_cm, Http::MockAsyncClientRequest* request)
-      : request_(&mock_cm.async_client_) {
-    ON_CALL(mock_cm.async_client_, send_(testing::_, testing::_, testing::_))
-        .WillByDefault(testing::Return(request));
-  }
+               const std::string& response_body);
+  /**
+   * Mock upstream which returns a given failure.
+   */
+  MockUpstream(Upstream::MockClusterManager& mock_cm, Http::AsyncClient::FailureReason reason);
+  /**
+   * Mock upstream which returns the given request.
+   */
+  MockUpstream(Upstream::MockClusterManager& mock_cm, Http::MockAsyncClientRequest* request);
 
 private:
   Http::MockAsyncClientRequest request_;
