eeea28e61e2c8990313069282c29675befeab144
zyfjeff <tianqian.zyf@alibaba-inc.com>
2018-09-18 05:29:35 -0700

    Use fix version of misspeling and get ignore words from file
    Signed-off-by: zyfjeff <tianqian.zyf@alibaba-inc.com>

--- tools/check_spelling.sh
+++ tools/check_spelling.sh
@@ -5,8 +5,8 @@
 
 set -e
 
-# Used to filter a list of words that do not require spell checking
-IGNORE_STRING="Creater,creater,ect,overriden"
+VERSION="0.3.4"
+OS=""
 
 MISSPELL_ARGS="-error -o stderr"
 
@@ -14,20 +14,34 @@ if [[ $1 == "fix" ]];then
   MISSPELL_ARGS="-w"
 fi
 
+if [[ `uname` == "Darwin" ]];then
+  OS="mac"
+elif [[ `uname` == "Linux" ]];then
+  OS="linux"
+else
+  echo "Current only support mac/Linux"
+  exit 1
+fi
+
 SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
 ROOTDIR=$SCRIPTPATH/..
 cd "$ROOTDIR"
 
-GOPATH=$(cd "/tmp/"; pwd)
-export GOPATH
-export PATH=$GOPATH/bin:$PATH
-
 # Install tools we need
-go get -u github.com/client9/misspell/cmd/misspell
+if [[ ! -e "/tmp/misspell"  ]];then
+  wget -qO- https://github.com/client9/misspell/releases/download/v${VERSION}/misspell_${VERSION}_${OS}_64bit.tar.gz|tar xvz -C /tmp &> /dev/null
+fi
 
+chmod +x /tmp/misspell
+ 
 # Spell checking
-# All the skipping files are defined in tools/.spelling_failures
-skipping_file="${ROOTDIR}/tools/.spelling_failures"
+# All the skipping files are defined in tools/spelling_failures
+skipping_file="${ROOTDIR}/tools/spelling_failures"
+
+# All the ignore words ar defained in tools/ignore_words
+ignore_words_file="${ROOTDIR}/tools/ignore_words"
+
+ignore_words=$(echo -n `cat ${ignore_words_file} | grep -v "^#"|grep -v "^$"` | tr ' ' ',')
 failing_packages=$(echo `cat ${skipping_file}` | sed "s| | -e |g")
-git ls-files | grep -v -e ${failing_packages} | xargs misspell -i "${IGNORE_STRING}" ${MISSPELL_ARGS}
+git ls-files | grep -v -e ${failing_packages} | xargs /tmp/misspell -i "${ignore_words}" ${MISSPELL_ARGS}
 
