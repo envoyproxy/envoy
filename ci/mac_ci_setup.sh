c461fbb5091d55ba160c1e3d345983525fcad539
Lizan Zhou <lizan.j@gmail.com>
2018-09-21 11:09:09 -0700

    mac: install latest Bazel (#4472)
    Signed-off-by: Lizan Zhou <zlizan@google.com>

--- ci/mac_ci_setup.sh
+++ ci/mac_ci_setup.sh
@@ -21,7 +21,7 @@ if ! brew update; then
     exit 1
 fi
 
-DEPS="automake bazel cmake coreutils go libtool wget ninja"
+DEPS="automake bazelbuild/tap/bazel cmake coreutils go libtool wget ninja"
 for DEP in ${DEPS}
 do
     is_installed "${DEP}" || install "${DEP}"
