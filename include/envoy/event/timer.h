c89bb5148febfdff57e67ebfb4f37542b793ac16
Joshua Marantz <jmarantz@google.com>
2018-09-23 00:25:09 -0400

    time: Makes sleep() and waitFor() part of the TimeSystem interface, adding and testing SimulatedTimeSystem::waitFor. (#4488)
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- include/envoy/event/timer.h
+++ include/envoy/event/timer.h
@@ -7,6 +7,7 @@
 #include "envoy/common/pure.h"
 #include "envoy/common/time.h"
 
+#include "common/common/thread.h"
 #include "common/event/libevent.h"
 
 namespace Envoy {
@@ -59,11 +60,43 @@ class TimeSystem : public TimeSource {
 public:
   virtual ~TimeSystem() {}
 
+  using Duration = MonotonicTime::duration;
+
   /**
    * Creates a timer factory. This indirection enables thread-local timer-queue management,
    * so servers can have a separate timer-factory in each thread.
    */
   virtual SchedulerPtr createScheduler(Libevent::BasePtr&) PURE;
+
+  /**
+   * Advances time forward by the specified duration, running any timers
+   * along the way that have been scheduled to fire.
+   *
+   * @param duration The amount of time to sleep.
+   */
+  virtual void sleep(const Duration& duration) PURE;
+  template <class D> void sleep(const D& duration) {
+    sleep(std::chrono::duration_cast<Duration>(duration));
+  }
+
+  /**
+   * Waits for the specified duration to expire, or for a condvar to
+   * be notified, whichever comes first.
+   *
+   * @param mutex A mutex which must be held before calling this function.
+   * @param condvar The condition to wait on.
+   * @param duration The maximum amount of time to wait.
+   * @return Thread::CondVar::WaitStatus whether the condition timed out or not.
+   */
+  virtual Thread::CondVar::WaitStatus
+  waitFor(Thread::MutexBasicLockable& mutex, Thread::CondVar& condvar,
+          const Duration& duration) noexcept EXCLUSIVE_LOCKS_REQUIRED(mutex) PURE;
+
+  template <class D>
+  Thread::CondVar::WaitStatus waitFor(Thread::MutexBasicLockable& mutex, Thread::CondVar& condvar,
+                                      const D& duration) noexcept EXCLUSIVE_LOCKS_REQUIRED(mutex) {
+    return waitFor(mutex, condvar, std::chrono::duration_cast<Duration>(duration));
+  }
 };
 
 } // namespace Event
--- source/common/event/real_time_system.h
+++ source/common/event/real_time_system.h
@@ -14,6 +14,10 @@ class RealTimeSystem : public TimeSystem {
 public:
   // TimeSystem
   SchedulerPtr createScheduler(Libevent::BasePtr&) override;
+  void sleep(const Duration& duration) override;
+  Thread::CondVar::WaitStatus
+  waitFor(Thread::MutexBasicLockable& mutex, Thread::CondVar& condvar,
+          const Duration& duration) noexcept EXCLUSIVE_LOCKS_REQUIRED(mutex) override;
 
   // TimeSource
   SystemTime systemTime() override { return time_source_.systemTime(); }
