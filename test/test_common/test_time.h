d0dd376dfbf328e6dfa161c510046853913d694c
Joshua Marantz <jmarantz@google.com>
2018-09-23 22:56:44 -0400

    time: Do not provide TimeSystem::sleep and waitFor in production interface (#4508)
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/test_common/test_time.h
+++ test/test_common/test_time.h
@@ -2,7 +2,33 @@
 
 #include "common/event/real_time_system.h"
 
+#include "test/test_common/test_time_system.h"
+
 namespace Envoy {
+namespace Event {
+
+class TestRealTimeSystem : public TestTimeSystem {
+public:
+  // TestTimeSystem
+  void sleep(const Duration& duration) override;
+  Thread::CondVar::WaitStatus
+  waitFor(Thread::MutexBasicLockable& mutex, Thread::CondVar& condvar,
+          const Duration& duration) noexcept EXCLUSIVE_LOCKS_REQUIRED(mutex) override;
+
+  // Event::TimeSystem
+  Event::SchedulerPtr createScheduler(Event::Libevent::BasePtr& libevent) override {
+    return real_time_system_.createScheduler(libevent);
+  }
+
+  // TimeSource
+  SystemTime systemTime() override { return real_time_system_.systemTime(); }
+  MonotonicTime monotonicTime() override { return real_time_system_.monotonicTime(); }
+
+private:
+  Event::RealTimeSystem real_time_system_;
+};
+
+} // namespace Event
 
 // Instantiates real-time sources for testing purposes. In general, this is a
 // bad idea, and tests should use simulated or mock time.
@@ -16,7 +42,7 @@ public:
   Event::TimeSystem& timeSystem() { return time_system_; }
 
 private:
-  Event::RealTimeSystem time_system_;
+  Event::TestRealTimeSystem time_system_;
 };
 
 } // namespace Envoy
