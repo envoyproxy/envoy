f04e7dabdecb1f604fb9864a62c50c3084d1eb16
Snow Pettersen <aickck@gmail.com>
2018-09-25 19:56:18 -0400

    retry extensions: use vector to keep track of attempted hosts (#4509)
    Instead of using unordered_map, use a vector initialized with size equal
    to the number of allowed retries. As the number of retries attempted
    will generally be low, this is likely both faster and more memory
    efficient.
    
    Signed-off-by: Snow Pettersen <snowp@squareup.com>

--- source/extensions/retry/host/other_hosts/config.h
+++ source/extensions/retry/host/other_hosts/config.h
@@ -13,8 +13,8 @@ namespace Host {
 class OtherHostsRetryPredicateFactory : public Upstream::RetryHostPredicateFactory {
 public:
   void createHostPredicate(Upstream::RetryHostPredicateFactoryCallbacks& callbacks,
-                           const Protobuf::Message&) override {
-    callbacks.addHostPredicate(std::make_shared<OtherHostsRetryPredicate>());
+                           const Protobuf::Message&, uint32_t retry_count) override {
+    callbacks.addHostPredicate(std::make_shared<OtherHostsRetryPredicate>(retry_count));
   }
 
   std::string name() override { return RetryHostPredicateValues::get().PreviousHostsPredicate; }
