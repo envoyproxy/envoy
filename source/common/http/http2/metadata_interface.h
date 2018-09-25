422679112831476a569546fc9511ed4138abff45
yasong <yasong@google.com>
2018-09-25 15:14:20 -0400

    add metadata_interface.h
    Signed-off-by: yasong <yasong@google.com>

---
+++ source/common/http/http2/metadata_interface.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <unordered_map>
+
+namespace Envoy {
+namespace Http {
+namespace Http2 {
+
+/**
+ * Please refer to #2394 or go/envoy-metadata for more info about Envoy METADATA.
+ * TODO(soya3129): refer to Envoy document.
+ */
+const uint8_t METADATA_FRAME_TYPE = 0x4d;
+const uint8_t END_METADATA_FLAG = 0x4;
+
+typedef std::unordered_map<std::string, std::string> MetadataMap;
+
+} // namespace Http2
+} // namespace Http
+} // namespace Envoy
