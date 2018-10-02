48c09638836d35b6bc358d8d08319e328cfe8ed3
Snow Pettersen <snowp@squareup.com>
2018-10-02 11:07:04 -0700

    format
    Signed-off-by: Snow Pettersen <snowp@squareup.com>

--- source/extensions/retry/priority/other_priority/config.cc
+++ source/extensions/retry/priority/other_priority/config.cc
@@ -10,11 +10,13 @@ namespace Retry {
 namespace Priority {
 
 void OtherPriorityRetryPriorityFactory::createRetryPriority(
-    Upstream::RetryPriorityFactoryCallbacks& callbacks, const Protobuf::Message& config, uint32_t max_retries) {
+    Upstream::RetryPriorityFactoryCallbacks& callbacks, const Protobuf::Message& config,
+    uint32_t max_retries) {
   callbacks.addRetryPriority(std::make_shared<OtherPriorityRetryPriority>(
       MessageUtil::downcastAndValidate<
           const envoy::config::retry::other_priority::OtherPriorityConfig&>(config)
-          .update_frequency(), max_retries));
+          .update_frequency(),
+      max_retries));
 }
 
 static Registry::RegisterFactory<OtherPriorityRetryPriorityFactory, Upstream::RetryPriorityFactory>
