ba5d3f0c130bb21958cf093c368af0526a4740b7
Snow Pettersen <aickck@gmail.com>
2018-10-04 08:39:31 -0700

    retry extensions: implement "other priority" extension (#4529)
    Implements a RetryPriority which will keep track of attempted
    priorities and attempt to route retry requests to other priorities. The
    update frequency is configurable, allowing multiple requests to hit each
    priority if desired.
    
    As a fallback, when no healthy priorities remain, the list of attempted
    priorities will be reset and a host will selected again using the
    original priority load.
    
    Extracts out the recalculatePerPriorityState from LoadBalancerBase to
    recompute the priority load with the same code used by the LB.
    
    Signed-off-by: Snow Pettersen snowp@squareup.com
    
    Risk Level: Medium, new extension
    Testing: unit tests
    Docs Changes: n/a
    Release Notes: n/a
    Signed-off-by: Snow Pettersen <snowp@squareup.com>

---
+++ source/extensions/retry/priority/other_priority/other_priority.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "envoy/upstream/retry.h"
+
+#include "common/upstream/load_balancer_impl.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Retry {
+namespace Priority {
+
+class OtherPriorityRetryPriority : public Upstream::RetryPriority {
+public:
+  OtherPriorityRetryPriority(uint32_t update_frequency, uint32_t max_retries)
+      : update_frequency_(update_frequency) {
+    attempted_priorities_.reserve(max_retries);
+  }
+
+  const Upstream::PriorityLoad&
+  determinePriorityLoad(const Upstream::PrioritySet& priority_set,
+                        const Upstream::PriorityLoad& original_priority) override;
+
+  void onHostAttempted(Upstream::HostDescriptionConstSharedPtr attempted_host) override {
+    attempted_priorities_.emplace_back(attempted_host->priority());
+  }
+
+private:
+  void recalculatePerPriorityState(uint32_t priority, const Upstream::PrioritySet& priority_set) {
+    // Recalcuate health and priority the same way the load balancer does it.
+    Upstream::LoadBalancerBase::recalculatePerPriorityState(
+        priority, priority_set, per_priority_load_, per_priority_health_);
+  }
+
+  std::pair<std::vector<uint32_t>, uint32_t> adjustedHealth() const;
+
+  // Distributes priority load between priorities that should be considered after
+  // excluding attempted priorities.
+  void adjustForAttemptedPriorities(const Upstream::PrioritySet& priority_set);
+
+  const uint32_t update_frequency_;
+  std::vector<uint32_t> attempted_priorities_;
+  std::vector<bool> excluded_priorities_;
+  Upstream::PriorityLoad per_priority_load_;
+  std::vector<uint32_t> per_priority_health_;
+};
+
+} // namespace Priority
+} // namespace Retry
+} // namespace Extensions
+} // namespace Envoy
