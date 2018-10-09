a6c29ebe3f6d4b3d36fd40d8549d8cbf14e4428b
Taiki Ono <taiki-ono@cookpad.com>
2018-10-09 13:36:33 +0900

    review: remove .keep file
    Signed-off-by: Taiki Ono <taiki-ono@cookpad.com>

--- examples/fault-injection/enable_fault_injection.sh
+++ examples/fault-injection/enable_fault_injection.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
+mkdir -p /srv/runtime/v1/envoy/fault/http/abort
 echo '100' > /srv/runtime/v1/envoy/fault/http/abort/abort_percent
 echo '503' > /srv/runtime/v1/envoy/fault/http/abort/http_status
 
