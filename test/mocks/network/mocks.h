3f927b1b5fa87f98a00260c47238d2c834ad3d61
Vadim Eisenberg <vadime@il.ibm.com>
2018-10-11 13:34:08 +0300

    update MockTransportSocketFactory::createTransportSocket()
    Signed-off-by: Vadim Eisenberg <vadime@il.ibm.com>

--- test/mocks/network/mocks.h
+++ test/mocks/network/mocks.h
@@ -444,7 +444,7 @@ public:
   ~MockTransportSocketFactory();
 
   MOCK_CONST_METHOD0(implementsSecureTransport, bool());
-  MOCK_CONST_METHOD0(createTransportSocket, TransportSocketPtr());
+  MOCK_CONST_METHOD1(createTransportSocket, TransportSocketPtr(std::string));
 };
 
 class MockTransportSocketCallbacks : public TransportSocketCallbacks {
