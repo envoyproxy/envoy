6904c852908e4cdc2ffc7b395cd8b4eef2f18e25
Lizan Zhou <lizan@tetrate.io>
2018-10-12 20:16:24 +0000

    test clang-tidy
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- source/common/ssl/utility.cc
+++ source/common/ssl/utility.cc
@@ -34,9 +34,8 @@ std::vector<std::string> Utility::getSubjectAltNames(X509& cert, int type) {
   std::vector<std::string> subject_alt_names;
   bssl::UniquePtr<GENERAL_NAMES> san_names(
       static_cast<GENERAL_NAMES*>(X509_get_ext_d2i(&cert, NID_subject_alt_name, nullptr, nullptr)));
-  if (san_names == nullptr) {
+  if (san_names == nullptr)
     return subject_alt_names;
-  }
   for (const GENERAL_NAME* san : san_names.get()) {
     if (san->type == type) {
       ASN1_STRING* str = san->d.dNSName;
