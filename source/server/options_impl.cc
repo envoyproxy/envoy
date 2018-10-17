28aa6c7d8298f4728cabeda8cf1b116a9001749b
Randy Smith <rdsmith@google.com>
2018-10-17 15:22:16 -0400

    Got tests working and fixed format
    Signed-off-by: Randy Smith <rdsmith@google.com>

--- source/server/options_impl.cc
+++ source/server/options_impl.cc
@@ -255,8 +255,8 @@ void OptionsImpl::logError(const std::string& error) const {
 
 OptionsImpl::OptionsImpl(const std::string& service_cluster, const std::string& service_node,
                          const std::string& service_zone, spdlog::level::level_enum log_level)
-    : base_id_(0u), concurrency_(1u), config_path_(""), config_yaml_(""), v2_config_only_(false),
-      local_address_ip_version_(Network::Address::IpVersion::v6), log_level_(log_level),
+    : base_id_(0u), concurrency_(1u), config_path_(""), config_yaml_(""), v2_config_only_(true),
+      local_address_ip_version_(Network::Address::IpVersion::v4), log_level_(log_level),
       log_format_(Logger::Logger::DEFAULT_LOG_FORMAT), restart_epoch_(0u),
       service_cluster_(service_cluster), service_node_(service_node), service_zone_(service_zone),
       file_flush_interval_msec_(10000), drain_time_(600), parent_shutdown_time_(900),
