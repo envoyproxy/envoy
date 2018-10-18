db033c46f11a9ff040498a22f36ab715f422933e
Snow Pettersen <snowp@squareup.com>
2018-10-18 00:07:14 -0400

    use virtual inheritance
    Signed-off-by: Snow Pettersen <snowp@squareup.com>

--- source/extensions/filters/http/common/pass_through_filter.h
+++ source/extensions/filters/http/common/pass_through_filter.h
@@ -6,7 +6,8 @@ namespace Envoy {
 namespace Http {
 
 // A decoder filter which passes all data through with Continue status.
-class PassThroughDecoderFilter : public StreamDecoderFilter {
+class PassThroughDecoderFilter : public virtual StreamDecoderFilter {
+public:
   // Http::StreamFilterBase
   void onDestroy() override {}
 
@@ -30,7 +31,7 @@ protected:
 };
 
 // An encoder filter which passes all data through with Continue status.
-class PassThroughEncoderFilter : public StreamEncoderFilter {
+class PassThroughEncoderFilter : public virtual StreamEncoderFilter {
 public:
   // Http::StreamFilterBase
   void onDestroy() override {}
@@ -57,46 +58,8 @@ protected:
 };
 
 // A filter which passes all data through with Continue status.
-class PassThroughFilter : public StreamFilter {
-public:
-  // Http::StreamFilterBase
-  void onDestroy() override {}
-
-  // Http::StreamDecoderFilter
-  Http::FilterHeadersStatus decodeHeaders(Http::HeaderMap&, bool) override {
-    return Http::FilterHeadersStatus::Continue;
-  }
-  Http::FilterDataStatus decodeData(Buffer::Instance&, bool) override {
-    return Http::FilterDataStatus::Continue;
-  }
-
-  Http::FilterTrailersStatus decodeTrailers(Http::HeaderMap&) override {
-    return Http::FilterTrailersStatus::Continue;
-  }
-  void setDecoderFilterCallbacks(Http::StreamDecoderFilterCallbacks& callbacks) override {
-    decoder_callbacks_ = &callbacks;
-  }
-
-  // Http::StreamEncoderFilter
-  Http::FilterHeadersStatus encode100ContinueHeaders(Http::HeaderMap&) override {
-    return Http::FilterHeadersStatus::Continue;
-  }
-  Http::FilterHeadersStatus encodeHeaders(Http::HeaderMap&, bool) override {
-    return Http::FilterHeadersStatus::Continue;
-  }
-  Http::FilterDataStatus encodeData(Buffer::Instance&, bool) override {
-    return Http::FilterDataStatus::Continue;
-  }
-  Http::FilterTrailersStatus encodeTrailers(Http::HeaderMap&) override {
-    return Http::FilterTrailersStatus::Continue;
-  }
-  void setEncoderFilterCallbacks(Http::StreamEncoderFilterCallbacks& callbacks) override {
-    encoder_callbacks_ = &callbacks;
-  }
-
-protected:
-  Http::StreamDecoderFilterCallbacks* decoder_callbacks_{};
-  Http::StreamEncoderFilterCallbacks* encoder_callbacks_{};
-};
+class PassThroughFilter : public StreamFilter,
+                          public PassThroughDecoderFilter,
+                          public PassThroughEncoderFilter {};
 } // namespace Http
 } // namespace Envoy
