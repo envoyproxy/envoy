b83d3606fe822a7306bc6ab9e72a159720173924
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2018-10-24 15:08:26 -0700

    Added Doxygen comments to grpc_mux_factory.h

--- include/envoy/upstream/grpc_mux_factory.h
+++ include/envoy/upstream/grpc_mux_factory.h
@@ -16,6 +16,17 @@ class GrpcMuxFactory {
 public:
     virtual ~GrpcMuxFactory() {}
 
+    /**
+    * Returns an existing or creates a new GrpcMux.
+    * @param local_info LocalInfo::LocalInfo local info.
+    * @param async_client Grpc::AsyncClientPtr async client to use for the grpc connection
+    * @param dispatcher event dispatcher.
+    * @param _method fully qualified name of v2 gRPC API bidi streaming method (as per protobuf
+    *        service description).
+    * @param random random generator for jittering polling delays (when REST).
+    * @param config envoy::api::v2::core::ConfigSource to construct from.
+    * @param scope stats scope.
+    */
     virtual Config::GrpcMux*
     getOrCreateMux(const LocalInfo::LocalInfo &local_info, Grpc::AsyncClientPtr async_client,
                    Event::Dispatcher &dispatcher, const Protobuf::MethodDescriptor &service_method,
