bde73b8c58fd537a6216d3ed56e2b20b7cc8730d
JimmyCYJ <jimmychen.0102@gmail.com>
2018-10-28 19:47:18 -0700

    Fix compile error.
    Signed-off-by: JimmyCYJ <jimmychen.0102@gmail.com>

--- test/common/secret/sds_api_test.cc
+++ test/common/secret/sds_api_test.cc
@@ -195,7 +195,7 @@ TEST_F(SdsApiTest, DynamicTrustedCaUpdateSuccess) {
 
   const std::string ca_cert = "{{ test_rundir }}/test/common/ssl/test_data/ca_cert.pem";
   EXPECT_EQ(TestEnvironment::readFileToStringForTest(TestEnvironment::substitute(ca_cert)),
-            sds_api.secret()->trustedCa()->caCert());
+            sds_api.secret()->caCert());
 
   handle->remove();
 }
