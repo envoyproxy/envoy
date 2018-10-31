d2c8a0ec3c08caf0218fd77c1d0d756eb4dfb31b
Joshua Marantz <jmarantz@google.com>
2018-10-30 21:16:11 -0400

    Attempt to fix build problem with a cast.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/extensions/stat_sinks/hystrix/hystrix.cc
+++ source/extensions/stat_sinks/hystrix/hystrix.cc
@@ -336,7 +336,8 @@ void HystrixSink::flush(Stats::Source& source) {
 
       // Make sure we found the cluster name tag
       ASSERT(it != histogram->tags().end());
-      auto it_bool_pair = time_histograms.emplace(std::make_pair(it->value_, QuantileLatencyMap()));
+      auto it_bool_pair =
+          time_histograms.emplace(std::make_pair(std::string(it->value_), QuantileLatencyMap()));
       // Make sure histogram with this name was not already added
       ASSERT(it_bool_pair.second);
       QuantileLatencyMap& hist_map = it_bool_pair.first->second;
