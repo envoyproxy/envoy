e560b7412ba14b2f796eab512110685ba2cc507e
Vadim Eisenberg <vadime@il.ibm.com>
2018-11-04 09:39:59 +0200

    rename ForwardRequestedServerName::Key
    to be 'envoy.stream_info.forward_requested_server_name'
    Signed-off-by: Vadim Eisenberg <vadime@il.ibm.com>

--- source/common/stream_info/forward_requested_server_name.cc
+++ source/common/stream_info/forward_requested_server_name.cc
@@ -4,7 +4,7 @@ namespace Envoy {
 namespace StreamInfo {
 
 const std::string ForwardRequestedServerName::Key =
-    "envoy.stream_info.original_requested_server_name";
+    "envoy.stream_info.forward_requested_server_name";
 
 } // namespace StreamInfo
 } // namespace Envoy
