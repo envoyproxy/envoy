4ef8562b2194f222ce8a3d733fb04c629eaf0667
Raúl Gutiérrez Segalés <rgs@itevenworks.net>
2018-11-05 21:29:20 -0800

    Fix /server_info (#4966)
    When `/server_info` changed from a string to JSON we forgot
    to populate the restart epoch.
    
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>

--- source/server/http/admin.cc
+++ source/server/http/admin.cc
@@ -530,6 +530,7 @@ Http::Code AdminImpl::handlerServerInfo(absl::string_view, Http::HeaderMap& head
                                                           server_.startTimeCurrentEpoch());
   server_info.mutable_uptime_all_epochs()->set_seconds(current_time -
                                                        server_.startTimeFirstEpoch());
+  server_info.set_epoch(server_.options().restartEpoch());
   response.add(MessageUtil::getJsonStringFromMessage(server_info, true, true));
   headers.insertContentType().value().setReference(Http::Headers::get().ContentTypeValues.Json);
   return Http::Code::OK;
