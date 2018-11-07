524cf02cebcab2a0d34c8ae5373b51f649647835
Joshua Marantz <jmarantz@google.com>
2018-11-06 20:34:41 -0500

    format
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/exe/main_common.cc
+++ source/exe/main_common.cc
@@ -60,114 +60,114 @@ MainCommonBase::MainCommonBase(OptionsImpl& options, Event::TimeSystem& time_sys
     if (restarter_.get() == nullptr) {
       restarter_.reset(new Server::HotRestartNopImpl(symbol_table_));
 =======
-      restarter_ = std::make_unique<Server::HotRestartImpl>(options_);
-    }
-#endif
-    if (restarter_ == nullptr) {
-      restarter_ = std::make_unique<Server::HotRestartNopImpl>();
->>>>>>> master
-    }
-
-    tls_ = std::make_unique<ThreadLocal::InstanceImpl>();
-    Thread::BasicLockable& log_lock = restarter_->logLock();
-    Thread::BasicLockable& access_log_lock = restarter_->accessLogLock();
-    auto local_address = Network::Utility::getLocalAddress(options_.localAddressIpVersion());
-    logging_context_ =
-        std::make_unique<Logger::Context>(options_.logLevel(), options_.logFormat(), log_lock);
-
-    configureComponentLogLevels();
-
-    stats_store_ = std::make_unique<Stats::ThreadLocalStoreImpl>(options_.statsOptions(),
-                                                                 restarter_->statsAllocator());
-
-    server_ = std::make_unique<Server::InstanceImpl>(
-        options_, time_system, local_address, test_hooks, *restarter_, *stats_store_,
-        access_log_lock, component_factory, std::move(random_generator), *tls_);
-
-    break;
+    restarter_ = std::make_unique<Server::HotRestartImpl>(options_);
   }
+#endif
+      if (restarter_ == nullptr) {
+        restarter_ = std::make_unique<Server::HotRestartNopImpl>();
+>>>>>>> master
+      }
+
+      tls_ = std::make_unique<ThreadLocal::InstanceImpl>();
+      Thread::BasicLockable& log_lock = restarter_->logLock();
+      Thread::BasicLockable& access_log_lock = restarter_->accessLogLock();
+      auto local_address = Network::Utility::getLocalAddress(options_.localAddressIpVersion());
+      logging_context_ =
+          std::make_unique<Logger::Context>(options_.logLevel(), options_.logFormat(), log_lock);
+
+      configureComponentLogLevels();
+
+      stats_store_ = std::make_unique<Stats::ThreadLocalStoreImpl>(options_.statsOptions(),
+                                                                   restarter_->statsAllocator());
+
+      server_ = std::make_unique<Server::InstanceImpl>(
+          options_, time_system, local_address, test_hooks, *restarter_, *stats_store_,
+          access_log_lock, component_factory, std::move(random_generator), *tls_);
+
+      break;
+    }
   case Server::Mode::Validate:
     restarter_ = std::make_unique<Server::HotRestartNopImpl>(symbol_table_);
     logging_context_ = std::make_unique<Logger::Context>(options_.logLevel(), options_.logFormat(),
                                                          restarter_->logLock());
     break;
   }
-}
+  } // namespace Envoy
 
-MainCommonBase::~MainCommonBase() { ares_library_cleanup(); }
+  MainCommonBase::~MainCommonBase() { ares_library_cleanup(); }
 
-void MainCommonBase::configureComponentLogLevels() {
-  for (auto& component_log_level : options_.componentLogLevels()) {
-    Logger::Logger* logger_to_change = Logger::Registry::logger(component_log_level.first);
-    ASSERT(logger_to_change);
-    logger_to_change->setLevel(component_log_level.second);
+  void MainCommonBase::configureComponentLogLevels() {
+    for (auto& component_log_level : options_.componentLogLevels()) {
+      Logger::Logger* logger_to_change = Logger::Registry::logger(component_log_level.first);
+      ASSERT(logger_to_change);
+      logger_to_change->setLevel(component_log_level.second);
+    }
   }
-}
 
-bool MainCommonBase::run() {
-  switch (options_.mode()) {
-  case Server::Mode::Serve:
-    server_->run();
-    return true;
-  case Server::Mode::Validate: {
-    auto local_address = Network::Utility::getLocalAddress(options_.localAddressIpVersion());
-    return Server::validateConfig(options_, local_address, component_factory_);
+  bool MainCommonBase::run() {
+    switch (options_.mode()) {
+    case Server::Mode::Serve:
+      server_->run();
+      return true;
+    case Server::Mode::Validate: {
+      auto local_address = Network::Utility::getLocalAddress(options_.localAddressIpVersion());
+      return Server::validateConfig(options_, local_address, component_factory_);
+    }
+    case Server::Mode::InitOnly:
+      PERF_DUMP();
+      return true;
+    }
+    NOT_REACHED_GCOVR_EXCL_LINE;
   }
-  case Server::Mode::InitOnly:
-    PERF_DUMP();
-    return true;
+
+  void MainCommonBase::adminRequest(absl::string_view path_and_query, absl::string_view method,
+                                    const AdminRequestFn& handler) {
+    std::string path_and_query_buf = std::string(path_and_query);
+    std::string method_buf = std::string(method);
+    server_->dispatcher().post([this, path_and_query_buf, method_buf, handler]() {
+      Http::HeaderMapImpl response_headers;
+      std::string body;
+      server_->admin().request(path_and_query_buf, method_buf, response_headers, body);
+      handler(response_headers, body);
+    });
   }
-  NOT_REACHED_GCOVR_EXCL_LINE;
-}
 
-void MainCommonBase::adminRequest(absl::string_view path_and_query, absl::string_view method,
-                                  const AdminRequestFn& handler) {
-  std::string path_and_query_buf = std::string(path_and_query);
-  std::string method_buf = std::string(method);
-  server_->dispatcher().post([this, path_and_query_buf, method_buf, handler]() {
-    Http::HeaderMapImpl response_headers;
-    std::string body;
-    server_->admin().request(path_and_query_buf, method_buf, response_headers, body);
-    handler(response_headers, body);
-  });
-}
+  MainCommon::MainCommon(int argc, const char* const* argv)
+      : options_(argc, argv, &MainCommon::hotRestartVersion, spdlog::level::info),
+        base_(options_, real_time_system_, default_test_hooks_, prod_component_factory_,
+              std::make_unique<Runtime::RandomGeneratorImpl>()) {}
 
-MainCommon::MainCommon(int argc, const char* const* argv)
-    : options_(argc, argv, &MainCommon::hotRestartVersion, spdlog::level::info),
-      base_(options_, real_time_system_, default_test_hooks_, prod_component_factory_,
-            std::make_unique<Runtime::RandomGeneratorImpl>()) {}
-
-std::string MainCommon::hotRestartVersion(uint64_t max_num_stats, uint64_t max_stat_name_len,
-                                          bool hot_restart_enabled) {
+  std::string MainCommon::hotRestartVersion(uint64_t max_num_stats, uint64_t max_stat_name_len,
+                                            bool hot_restart_enabled) {
 #ifdef ENVOY_HOT_RESTART
-  if (hot_restart_enabled) {
-    return Server::HotRestartImpl::hotRestartVersion(max_num_stats, max_stat_name_len);
-  }
+    if (hot_restart_enabled) {
+      return Server::HotRestartImpl::hotRestartVersion(max_num_stats, max_stat_name_len);
+    }
 #else
   UNREFERENCED_PARAMETER(hot_restart_enabled);
   UNREFERENCED_PARAMETER(max_num_stats);
   UNREFERENCED_PARAMETER(max_stat_name_len);
 #endif
-  return "disabled";
-}
-
-// Legacy implementation of main_common.
-//
-// TODO(jmarantz): Remove this when all callers are removed. At that time, MainCommonBase
-// and MainCommon can be merged. The current theory is that only Google calls this.
-int main_common(OptionsImpl& options) {
-  try {
-    Event::RealTimeSystem real_time_system_;
-    DefaultTestHooks default_test_hooks_;
-    ProdComponentFactory prod_component_factory_;
-    MainCommonBase main_common(options, real_time_system_, default_test_hooks_,
-                               prod_component_factory_,
-                               std::make_unique<Runtime::RandomGeneratorImpl>());
-    return main_common.run() ? EXIT_SUCCESS : EXIT_FAILURE;
-  } catch (EnvoyException& e) {
-    return EXIT_FAILURE;
+    return "disabled";
   }
-  return EXIT_SUCCESS;
-}
+
+  // Legacy implementation of main_common.
+  //
+  // TODO(jmarantz): Remove this when all callers are removed. At that time, MainCommonBase
+  // and MainCommon can be merged. The current theory is that only Google calls this.
+  int main_common(OptionsImpl & options) {
+    try {
+      Event::RealTimeSystem real_time_system_;
+      DefaultTestHooks default_test_hooks_;
+      ProdComponentFactory prod_component_factory_;
+      MainCommonBase main_common(options, real_time_system_, default_test_hooks_,
+                                 prod_component_factory_,
+                                 std::make_unique<Runtime::RandomGeneratorImpl>());
+      return main_common.run() ? EXIT_SUCCESS : EXIT_FAILURE;
+    } catch (EnvoyException& e) {
+      return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+  }
 
 } // namespace Envoy
