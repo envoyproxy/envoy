89aaa07a4f99e285daa91c6397f1b91a999a6dd5
Lizan Zhou <lizan@tetrate.io>
2018-11-07 01:41:58 -0800

    introduce factory base
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

---
+++ source/extensions/tracers/common/factory_base.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "envoy/server/instance.h"
+#include "envoy/server/tracer_config.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Tracers {
+namespace Common {
+
+/**
+ * Common base class for tracer factory registrations. Removes a substantial amount of
+ * boilerplate.
+ */
+template <class ConfigProto> class FactoryBase : public Server::Configuration::TracerFactory {
+public:
+  // Server::Configuration::TracerFactory
+  virtual Tracing::HttpTracerPtr
+  createHttpTracer(const envoy::config::trace::v2::Tracing& configuration,
+                   Server::Instance& server) override {
+
+    ProtobufTypes::MessagePtr config_ptr = createEmptyConfigProto();
+
+    if (configuration.http().has_config()) {
+      MessageUtil::jsonConvert(configuration.http().config(), *config_ptr);
+    }
+
+    return createHttpTracerTyped(MessageUtil::downcastAndValidate<const ConfigProto&>(*config_ptr),
+                                 server);
+  }
+
+  ProtobufTypes::MessagePtr createEmptyConfigProto() override {
+    return std::make_unique<ConfigProto>();
+  }
+
+  std::string name() override { return name_; }
+
+protected:
+  FactoryBase(const std::string& name) : name_(name) {}
+
+private:
+  virtual Tracing::HttpTracerPtr createHttpTracerTyped(const ConfigProto& proto_config,
+                                                       Server::Instance& server) PURE;
+
+  const std::string name_;
+};
+
+} // namespace Common
+} // namespace Tracers
+} // namespace Extensions
+} // namespace Envoy
