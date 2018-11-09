29baef576a1b6dcd4453f5f1d8070174cb788f59
Derek Argueta <dereka@pinterest.com>
2018-11-08 17:20:08 -0800

    use initializer list for constructor (#4999)
    Signed-off-by: Derek Argueta <dereka@pinterest.com>
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- source/extensions/filters/network/thrift_proxy/twitter_protocol_impl.cc
+++ source/extensions/filters/network/thrift_proxy/twitter_protocol_impl.cc
@@ -572,9 +572,7 @@ public:
       }
     }
   }
-  ResponseHeader(const MessageMetadata& metadata) {
-    spans_ = metadata.spans();
-
+  ResponseHeader(const MessageMetadata& metadata) : spans_(metadata.spans()) {
     metadata.headers().iterate(
         [](const Http::HeaderEntry& header, void* cb) -> Http::HeaderMap::Iterate {
           absl::string_view key = header.key().getStringView();
