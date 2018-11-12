0acee1addd749cf444c7989520ad3f5896a9785c
Qi (Anna) Wang <qiwang@qiwang-macbookpro.roam.corp.google.com>
2018-11-11 20:43:20 -0800

    Add new tests based on reviewers comments.
    Signed-off-by: Qi (Anna) Wang <qiwang@qiwang-macbookpro.roam.corp.google.com>

--- test/mocks/network/mocks.h
+++ test/mocks/network/mocks.h
@@ -92,8 +92,12 @@ public:
   MOCK_CONST_METHOD0(streamInfo, const StreamInfo::StreamInfo&());
   MOCK_METHOD1(setDelayedCloseTimeout, void(std::chrono::milliseconds));
   MOCK_CONST_METHOD0(delayedCloseTimeout, std::chrono::milliseconds());
-  MOCK_METHOD1(setWriteFilterOrder, void(bool reversed));
-  bool reverseWriteFilterOrder() const override { return true; }
+
+  void setWriteFilterOrder(bool reversed) override { reversed_write_filter_order_ = reversed; }
+  bool reverseWriteFilterOrder() const override { return reversed_write_filter_order_; }
+
+private:
+  bool reversed_write_filter_order_{true};
 };
 
 /**
