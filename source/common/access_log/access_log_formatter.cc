2de1640aae6dad17f84a2f6acdea9dff421f8908
Aaltan Ahmad <aa@stripe.com>
2018-11-12 17:26:37 -0800

    Log a newline after each JSON access log line
    Signed-off-by: Aaltan Ahmad <aa@stripe.com>

--- source/common/access_log/access_log_formatter.cc
+++ source/common/access_log/access_log_formatter.cc
@@ -98,7 +98,7 @@ std::string JsonFormatterImpl::format(const Http::HeaderMap& request_headers,
         fmt::format("Error serializing access log to JSON: {}", conversion_status.ToString());
   }
 
-  return log_line;
+  return fmt::format("{}\n", log_line);
 }
 
 std::unordered_map<std::string, std::string> JsonFormatterImpl::toMap(
