0c92e8b7039ea4dee568cc2584e9064c1b6ce8ca
Snow Pettersen <snowp@squareup.com>
2018-11-14 15:05:22 -0500

    add files
    Signed-off-by: Snow Pettersen <snowp@squareup.com>

---
+++ test/integration/filters/common.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace Envoy {
+// DRYs up the creation of a simple filter config for a filter that requires no config.
+// TODO(snowp): make this reusable and use for other test filters.
+template <class T>
+class SimpleFilterConfig : public Extensions::HttpFilters::Common::EmptyHttpFilterConfig {
+public:
+  SimpleFilterConfig() : EmptyHttpFilterConfig(T::name) {}
+
+  Http::FilterFactoryCb createFilter(const std::string&, Server::Configuration::FactoryContext&) {
+    return [](Http::FilterChainFactoryCallbacks& callbacks) -> void {
+      callbacks.addStreamFilter(std::make_shared<T>());
+    };
+  }
+};
+
+}
