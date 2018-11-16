088f2d8cafcfd81dd0b646ffad727439cd8789f9
Vadim Eisenberg <vadime@il.ibm.com>
2018-11-16 22:11:33 +0200

    fix the hashKey() method
    Signed-off-by: Vadim Eisenberg <vadime@il.ibm.com>

--- source/common/network/transport_socket_options_impl.cc
+++ source/common/network/transport_socket_options_impl.cc
@@ -1,5 +1,7 @@
 #include "common/network/transport_socket_options_impl.h"
 
+#include "common/common/utility.h"
+
 namespace Envoy {
 namespace Network {
 void TransportSocketOptionsImpl::hashKey(std::vector<uint8_t>& key) const {
@@ -7,8 +9,13 @@ void TransportSocketOptionsImpl::hashKey(std::vector<uint8_t>& key) const {
     return;
   }
 
-  std::hash<std::string> hash_function;
-  key.push_back(hash_function(override_server_name_.value()));
+  uint64_t hash = StringUtil::CaseInsensitiveHash()(override_server_name_.value());
+
+  uint8_t* byte_ptr = reinterpret_cast<uint8_t*>(&hash);
+  for (int byte_index = 0; byte_index < 8; byte_index++) {
+    key.push_back(*byte_ptr);
+    byte_ptr++;
+  }
 }
 } // namespace Network
 } // namespace Envoy
