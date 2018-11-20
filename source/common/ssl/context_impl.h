505cce2d357b95c4caa7c15b3be4478e547c93ef
Vadim Eisenberg <vadime@il.ibm.com>
2018-11-20 23:31:39 +0200

    Revert "make the parameter of newSSL() const&"
    This reverts commit 6c9128dafa8a885c043f8cb55656c3ea6970a680.
    
    Signed-off-by: Vadim Eisenberg <vadime@il.ibm.com>

--- source/common/ssl/context_impl.h
+++ source/common/ssl/context_impl.h
@@ -42,8 +42,7 @@ struct SslStats {
 
 class ContextImpl : public virtual Context {
 public:
-  virtual bssl::UniquePtr<SSL>
-  newSsl(const absl::optional<std::string>& override_server_name) const;
+  virtual bssl::UniquePtr<SSL> newSsl(absl::optional<std::string> override_server_name) const;
 
   /**
    * Logs successful TLS handshake and updates stats.
@@ -144,8 +143,7 @@ public:
   ClientContextImpl(Stats::Scope& scope, const ClientContextConfig& config,
                     TimeSource& time_source);
 
-  bssl::UniquePtr<SSL>
-  newSsl(const absl::optional<std::string>& override_server_name) const override;
+  bssl::UniquePtr<SSL> newSsl(absl::optional<std::string> override_server_name) const override;
 
 private:
   const std::string server_name_indication_;
