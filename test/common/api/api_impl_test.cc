a59709cd0af94e2e810373884e10d2547ff3c115
Joshua Marantz <jmarantz@google.com>
2018-11-26 16:37:57 -0500

    stats: Make an object for the FIlesystem (Filesystem::Instance) to hold the stats counters. (#5031)
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/common/api/api_impl_test.cc
+++ test/common/api/api_impl_test.cc
@@ -2,6 +2,7 @@
 #include <string>
 
 #include "common/api/api_impl.h"
+#include "common/stats/isolated_store_impl.h"
 
 #include "test/test_common/environment.h"
 #include "test/test_common/utility.h"
@@ -11,20 +12,24 @@
 namespace Envoy {
 namespace Api {
 
-TEST(ApiImplTest, readFileToEnd) {
-  Impl api(std::chrono::milliseconds(1000), Thread::threadFactoryForTest());
+class ApiImplTest : public testing::Test {
+protected:
+  ApiImplTest() : api_(createApiForTest(store_)) {}
 
+  Stats::IsolatedStoreImpl store_;
+  ApiPtr api_;
+};
+
+TEST_F(ApiImplTest, readFileToEnd) {
   const std::string data = "test read To End\nWith new lines.";
   const std::string file_path = TestEnvironment::writeStringToFileForTest("test_api_envoy", data);
 
-  EXPECT_EQ(data, api.fileReadToEnd(file_path));
+  EXPECT_EQ(data, api_->fileReadToEnd(file_path));
 }
 
-TEST(ApiImplTest, fileExists) {
-  Impl api(std::chrono::milliseconds(1000), Thread::threadFactoryForTest());
-
-  EXPECT_TRUE(api.fileExists("/dev/null"));
-  EXPECT_FALSE(api.fileExists("/dev/blahblahblah"));
+TEST_F(ApiImplTest, fileExists) {
+  EXPECT_TRUE(api_->fileExists("/dev/null"));
+  EXPECT_FALSE(api_->fileExists("/dev/blahblahblah"));
 }
 
 } // namespace Api
