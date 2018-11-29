24f4f87be608c7436034c959eb091130da588504
Kyle Larose <kyle@agilicus.com>
2018-11-29 11:29:33 -0500

    Create SrcIpTransparentMapper in factory
    We hadn't yet hooked up the factory to the mapper. This change does so.
    Currently the default maximum number of pools is hardcoded. We should be
    able to change this later.
    
    Signed-off-by: Kyle Larose <kyle@agilicus.com>

--- source/common/http/wrapped/connection_mapper_factory.cc
+++ source/common/http/wrapped/connection_mapper_factory.cc
@@ -1,13 +1,19 @@
 #include "common/http/wrapped/connection_mapper_factory.h"
 
+#include "common/http/wrapped/src_ip_transparent_mapper.h"
+
 namespace Envoy {
 namespace Http {
+
+namespace {
+constexpr uint64_t DEFAULT_MAX_POOLS = 100;
+}
 
 ConnectionMapperFactory::~ConnectionMapperFactory() = default;
 
 ConnectionMapperPtr
-ConnectionMapperFactory::createSrcTransparentMapper(const ConnPoolBuilder& /*unused*/) {
-  return nullptr;
+ConnectionMapperFactory::createSrcTransparentMapper(const ConnPoolBuilder& builder) {
+  return std::make_unique<SrcIpTransparentMapper>(builder, DEFAULT_MAX_POOLS);
 }
 
 } // namespace Http
