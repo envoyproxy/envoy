340991f94dbacfa4489a6181aade21f1cb0149c2
Rama Chavali <rama.rao@salesforce.com>
2018-11-29 14:54:24 +0530

    fix test failure
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- test/extensions/filters/common/ratelimit/ratelimit_impl_test.cc
+++ test/extensions/filters/common/ratelimit/ratelimit_impl_test.cc
@@ -12,7 +12,6 @@
 #include "extensions/filters/common/ratelimit/ratelimit_impl.h"
 
 #include "test/mocks/grpc/mocks.h"
-#include "test/mocks/thread_local/mocks.h"
 #include "test/mocks/upstream/mocks.h"
 #include "test/test_common/printers.h"
 #include "test/test_common/utility.h"
@@ -44,32 +43,24 @@ public:
 
 class RateLimitGrpcClientTest : public testing::Test {
 public:
-  RateLimitGrpcClientTest() {
-    EXPECT_CALL(*factory_, create()).WillOnce(Invoke([this] {
-      return Grpc::AsyncClientPtr{async_client_};
-    }));
-    client_ = std::make_unique<GrpcClientImpl>(Grpc::AsyncClientFactoryPtr{factory_},
-                                               absl::optional<std::chrono::milliseconds>(), tls_);
-  }
+  RateLimitGrpcClientTest()
+      : async_client_(new Grpc::MockAsyncClient()),
+        client_(Grpc::AsyncClientPtr{async_client_}, absl::optional<std::chrono::milliseconds>()) {}
 
-  Grpc::MockAsyncClientFactory* factory_{new Grpc::MockAsyncClientFactory};
-  Grpc::MockAsyncClient* async_client_{new Grpc::MockAsyncClient};
+  Grpc::MockAsyncClient* async_client_;
   Grpc::MockAsyncRequest async_request_;
-  std::unique_ptr<GrpcClientImpl> client_;
+  GrpcClientImpl client_;
   MockRequestCallbacks request_callbacks_;
   Tracing::MockSpan span_;
-  NiceMock<ThreadLocal::MockInstance> tls_;
 };
 
 TEST_F(RateLimitGrpcClientTest, Basic) {
   std::unique_ptr<envoy::service::ratelimit::v2::RateLimitResponse> response;
 
   {
-    std::cout << "sending request..."
-              << "\n";
     envoy::service::ratelimit::v2::RateLimitRequest request;
     Http::HeaderMapImpl headers;
-    GrpcTlsClientImpl::createRequest(request, "foo", {{{{"foo", "bar"}}}});
+    GrpcClientImpl::createRequest(request, "foo", {{{{"foo", "bar"}}}});
     EXPECT_CALL(*async_client_, send(_, ProtoEq(request), _, _, _))
         .WillOnce(
             Invoke([this](const Protobuf::MethodDescriptor& service_method,
@@ -81,52 +72,52 @@ TEST_F(RateLimitGrpcClientTest, Basic) {
               return &async_request_;
             }));
 
-    client_->limit(request_callbacks_, "foo", {{{{"foo", "bar"}}}}, Tracing::NullSpan::instance());
+    client_.limit(request_callbacks_, "foo", {{{{"foo", "bar"}}}}, Tracing::NullSpan::instance());
 
-    client_->onCreateInitialMetadata(headers);
+    client_.onCreateInitialMetadata(headers);
     EXPECT_EQ(nullptr, headers.RequestId());
 
     response = std::make_unique<envoy::service::ratelimit::v2::RateLimitResponse>();
     response->set_overall_code(envoy::service::ratelimit::v2::RateLimitResponse_Code_OVER_LIMIT);
     EXPECT_CALL(span_, setTag("ratelimit_status", "over_limit"));
     EXPECT_CALL(request_callbacks_, complete_(LimitStatus::OverLimit, _));
-    client_->onSuccess(std::move(response), span_);
+    client_.onSuccess(std::move(response), span_);
   }
 
   {
     envoy::service::ratelimit::v2::RateLimitRequest request;
     Http::HeaderMapImpl headers;
-    GrpcTlsClientImpl::createRequest(request, "foo", {{{{"foo", "bar"}, {"bar", "baz"}}}});
+    GrpcClientImpl::createRequest(request, "foo", {{{{"foo", "bar"}, {"bar", "baz"}}}});
     EXPECT_CALL(*async_client_, send(_, ProtoEq(request), _, _, _))
         .WillOnce(Return(&async_request_));
 
-    client_->limit(request_callbacks_, "foo", {{{{"foo", "bar"}, {"bar", "baz"}}}},
-                   Tracing::NullSpan::instance());
+    client_.limit(request_callbacks_, "foo", {{{{"foo", "bar"}, {"bar", "baz"}}}},
+                  Tracing::NullSpan::instance());
 
-    client_->onCreateInitialMetadata(headers);
+    client_.onCreateInitialMetadata(headers);
 
     response = std::make_unique<envoy::service::ratelimit::v2::RateLimitResponse>();
     response->set_overall_code(envoy::service::ratelimit::v2::RateLimitResponse_Code_OK);
     EXPECT_CALL(span_, setTag("ratelimit_status", "ok"));
     EXPECT_CALL(request_callbacks_, complete_(LimitStatus::OK, _));
-    client_->onSuccess(std::move(response), span_);
+    client_.onSuccess(std::move(response), span_);
   }
 
   {
     envoy::service::ratelimit::v2::RateLimitRequest request;
-    GrpcTlsClientImpl::createRequest(
+    GrpcClientImpl::createRequest(
         request, "foo",
         {{{{"foo", "bar"}, {"bar", "baz"}}}, {{{"foo2", "bar2"}, {"bar2", "baz2"}}}});
     EXPECT_CALL(*async_client_, send(_, ProtoEq(request), _, _, _))
         .WillOnce(Return(&async_request_));
 
-    client_->limit(request_callbacks_, "foo",
-                   {{{{"foo", "bar"}, {"bar", "baz"}}}, {{{"foo2", "bar2"}, {"bar2", "baz2"}}}},
-                   Tracing::NullSpan::instance());
+    client_.limit(request_callbacks_, "foo",
+                  {{{{"foo", "bar"}, {"bar", "baz"}}}, {{{"foo2", "bar2"}, {"bar2", "baz2"}}}},
+                  Tracing::NullSpan::instance());
 
     response = std::make_unique<envoy::service::ratelimit::v2::RateLimitResponse>();
     EXPECT_CALL(request_callbacks_, complete_(LimitStatus::Error, _));
-    client_->onFailure(Grpc::Status::Unknown, "", span_);
+    client_.onFailure(Grpc::Status::Unknown, "", span_);
   }
 }
 
@@ -135,10 +126,10 @@ TEST_F(RateLimitGrpcClientTest, Cancel) {
 
   EXPECT_CALL(*async_client_, send(_, _, _, _, _)).WillOnce(Return(&async_request_));
 
-  client_->limit(request_callbacks_, "foo", {{{{"foo", "bar"}}}}, Tracing::NullSpan::instance());
+  client_.limit(request_callbacks_, "foo", {{{{"foo", "bar"}}}}, Tracing::NullSpan::instance());
 
   EXPECT_CALL(async_request_, cancel());
-  client_->cancel();
+  client_.cancel();
 }
 
 TEST(RateLimitGrpcFactoryTest, Create) {
@@ -151,8 +142,7 @@ TEST(RateLimitGrpcFactoryTest, Create) {
       .WillOnce(Invoke([](const envoy::api::v2::core::GrpcService&, Stats::Scope&, bool) {
         return std::make_unique<NiceMock<Grpc::MockAsyncClientFactory>>();
       }));
-  NiceMock<ThreadLocal::MockInstance> tls;
-  GrpcFactoryImpl factory(config, async_client_manager, tls, scope);
+  GrpcFactoryImpl factory(config, async_client_manager, scope);
   factory.create(absl::optional<std::chrono::milliseconds>());
 }
 
@@ -169,8 +159,7 @@ TEST(RateLimitGrpcFactoryTest, CreateLegacy) {
       .WillOnce(Invoke([](const envoy::api::v2::core::GrpcService&, Stats::Scope&, bool) {
         return std::make_unique<NiceMock<Grpc::MockAsyncClientFactory>>();
       }));
-  NiceMock<ThreadLocal::MockInstance> tls;
-  GrpcFactoryImpl factory(config, async_client_manager, tls, scope);
+  GrpcFactoryImpl factory(config, async_client_manager, scope);
   factory.create(absl::optional<std::chrono::milliseconds>());
 }
 
