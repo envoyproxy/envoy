0e3ff17ee98e366719ac9fd43ecad246e4c7837b
Lizan Zhou <lizan.j@gmail.com>
2018-12-05 11:42:21 -0500

    update bazel-compilation-database (#5219)
    To make compile database generation works with latest bazel.
    
    Risk Level: Low
    Testing: CI
    
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- bazel/gen_compilation_database.sh
+++ bazel/gen_compilation_database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RELEASE_VERSION=0.2.3
+RELEASE_VERSION=0.3.1
 
 if [[ ! -d bazel-compilation-database-${RELEASE_VERSION} ]]; then
   curl -L https://github.com/grailbio/bazel-compilation-database/archive/${RELEASE_VERSION}.tar.gz | tar -xz
