a3f6ba3cf9148c236ede1649168bec3e302d762a
Derek Schaller <dschaller@lyft.com>
2018-12-11 15:06:08 -0800

    use optional instead of string for runtimeKey return type
    Signed-off-by: Derek Schaller <dschaller@lyft.com>

--- source/extensions/filters/http/cors/cors_filter.cc
+++ source/extensions/filters/http/cors/cors_filter.cc
@@ -205,7 +205,7 @@ bool CorsFilter::allowCredentials() {
   return false;
 }
 
-std::string CorsFilter::runtimeKey(const std::string& key) {
+absl::optional<std::string> CorsFilter::runtimeKey(const std::string& key) {
   for (const auto policy : policies_) {
     if (!policy) {
       continue;
@@ -218,7 +218,7 @@ std::string CorsFilter::runtimeKey(const std::string& key) {
 }
 
 bool CorsFilter::shadowEnabled() {
-  const auto shadowMode = runtimeKey("shadow_enabled");
+  const auto shadowMode = runtimeKey("shadow_enabled").value();
   if (shadowMode == EMPTY_STRING) {
     return false;
   }
@@ -226,7 +226,7 @@ bool CorsFilter::shadowEnabled() {
 }
 
 bool CorsFilter::enabled() {
-  const auto filterEnabled = runtimeKey("filter_enabled");
+  const auto filterEnabled = runtimeKey("filter_enabled").value();
   for (const auto policy : policies_) {
     if (!policy) {
       continue;
