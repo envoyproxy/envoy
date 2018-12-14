1aa44014c35963164b0dec16ea6f0a9ab195262a
Auni Ahsan <auni.ahsan53@gmail.com>
2018-12-13 23:02:29 -0500

    StreamInfo: Add downstreamDirectRemoteAddress (#5064)
    The downstreamRemoteAddress field is modified when the remote address is inferred from proxy information or headers. This new field guarantees to hold the directly connected host's physical address.
    
    Risk Level: Low
    Testing: Searched around for references to downstreamRemoteAddress() and filled in similar tests/mocks.
    
    Fixes #4996.
    
    Signed-off-by: Auni Ahsan <auni@google.com>
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- source/common/http/conn_manager_impl.cc
+++ source/common/http/conn_manager_impl.cc
@@ -369,6 +369,8 @@ ConnectionManagerImpl::ActiveStream::ActiveStream(ConnectionManagerImpl& connect
   }
   stream_info_.setDownstreamLocalAddress(
       connection_manager_.read_callbacks_->connection().localAddress());
+  stream_info_.setDownstreamDirectRemoteAddress(
+      connection_manager_.read_callbacks_->connection().remoteAddress());
   // Initially, the downstream remote address is the source address of the
   // downstream connection. That can change later in the request's lifecycle,
   // based on XFF processing, but setting the downstream remote address here
