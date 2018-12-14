c3aa85f72679a0346f4ddb49abbc95809d8beb11
Kyu Chang <kyuc@google.com>
2018-12-14 19:52:17 +0000

    Add unit tests for ignoring query parameters in JSON gRPC transcoder
    Signed-off-by: Kyu Chang <kyuc@google.com>

--- test/extensions/filters/http/grpc_json_transcoder/json_transcoder_filter_test.cc
+++ test/extensions/filters/http/grpc_json_transcoder/json_transcoder_filter_test.cc
@@ -47,6 +47,7 @@ class GrpcJsonTranscoderConfigTest : public testing::Test {
 public:
   const envoy::config::filter::http::transcoder::v2::GrpcJsonTranscoder
   getProtoConfig(const std::string& descriptor_path, const std::string& service_name,
+                 const std::vector<std::string>& ignored_query_parameters,
                  bool match_incoming_request_route = false) {
     std::string json_string = "{\"proto_descriptor\": \"" + descriptor_path +
                               "\",\"services\": [\"" + service_name + "\"]}";
@@ -54,7 +55,19 @@ public:
     envoy::config::filter::http::transcoder::v2::GrpcJsonTranscoder proto_config;
     Envoy::Config::FilterJson::translateGrpcJsonTranscoder(*json_config, proto_config);
     proto_config.set_match_incoming_request_route(match_incoming_request_route);
+    for (const std::string& query_param : ignored_query_parameters) {
+      proto_config.add_ignored_query_parameters(query_param);
+    }
+
     return proto_config;
+  }
+
+  const envoy::config::filter::http::transcoder::v2::GrpcJsonTranscoder
+  getProtoConfig(const std::string& descriptor_path, const std::string& service_name,
+                 bool match_incoming_request_route = false) {
+    std::vector<std::string> emtpy_ignored_query_parameters;
+    return getProtoConfig(descriptor_path, service_name, emtpy_ignored_query_parameters,
+                          match_incoming_request_route);
   }
 
   std::string makeProtoDescriptor(std::function<void(FileDescriptorSet&)> process) {
@@ -173,6 +186,43 @@ TEST_F(GrpcJsonTranscoderConfigTest, CreateTranscoder) {
       TestEnvironment::runfilesPath("test/proto/bookstore.descriptor"), "bookstore.Bookstore"));
 
   Http::TestHeaderMapImpl headers{{":method", "GET"}, {":path", "/shelves"}};
+
+  TranscoderInputStreamImpl request_in, response_in;
+  std::unique_ptr<Transcoder> transcoder;
+  const MethodDescriptor* method_descriptor;
+  auto status =
+      config.createTranscoder(headers, request_in, response_in, transcoder, method_descriptor);
+
+  EXPECT_TRUE(status.ok());
+  EXPECT_TRUE(transcoder);
+  EXPECT_EQ("bookstore.Bookstore.ListShelves", method_descriptor->full_name());
+}
+
+TEST_F(GrpcJsonTranscoderConfigTest, InvalidQueryParameter) {
+  JsonTranscoderConfig config(getProtoConfig(
+      TestEnvironment::runfilesPath("test/proto/bookstore.descriptor"), "bookstore.Bookstore"));
+
+  Http::TestHeaderMapImpl headers{{":method", "GET"}, {":path", "/shelves?foo=bar"}};
+
+  TranscoderInputStreamImpl request_in, response_in;
+  std::unique_ptr<Transcoder> transcoder;
+  const MethodDescriptor* method_descriptor;
+  auto status =
+      config.createTranscoder(headers, request_in, response_in, transcoder, method_descriptor);
+
+  EXPECT_EQ(Code::INVALID_ARGUMENT, status.error_code());
+  EXPECT_EQ("Could not find field \"foo\" in the type \"google.protobuf.Empty\".",
+            status.error_message());
+  EXPECT_FALSE(transcoder);
+}
+
+TEST_F(GrpcJsonTranscoderConfigTest, IgnoredQueryParameter) {
+  std::vector<std::string> ignored_query_parameters = {"key"};
+  JsonTranscoderConfig config(
+      getProtoConfig(TestEnvironment::runfilesPath("test/proto/bookstore.descriptor"),
+                     "bookstore.Bookstore", ignored_query_parameters));
+
+  Http::TestHeaderMapImpl headers{{":method", "GET"}, {":path", "/shelves?key=API_KEY"}};
 
   TranscoderInputStreamImpl request_in, response_in;
   std::unique_ptr<Transcoder> transcoder;
