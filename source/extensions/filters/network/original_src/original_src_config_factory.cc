f9cd3a97603a37711c1ca325bdac2e75927910f8
Kyle Larose <kyle@agilicus.com>
2018-12-15 13:13:30 -0500

    Add config protobuf library
    Signed-off-by: Kyle Larose <kyle@agilicus.com>

--- source/extensions/filters/network/original_src/original_src_config_factory.cc
+++ source/extensions/filters/network/original_src/original_src_config_factory.cc
@@ -1,9 +1,11 @@
+#include "envoy/config/filter/network/original_src/v2alpha1/original_src.pb.h"
+#include "envoy/config/filter/network/original_src/v2alpha1/original_src.pb.validate.h"
 #include "envoy/registry/registry.h"
-#include "envoy/server/filter_config.h"
 
+#include "extensions/filters/network/common/factory_base.h"
+#include "extensions/filters/network/original_src/config.h"
 #include "extensions/filters/network/original_src/original_src.h"
 #include "extensions/filters/network/well_known_names.h"
-
 namespace Envoy {
 namespace Extensions {
 namespace NetworkFilters {
@@ -12,29 +14,21 @@ namespace OriginalSrc {
 /**
  * Config registration for the original_src filter. @see NamedNetworkFilterConfigFactory.
  */
-class OriginalSrcConfigFactory : public Server::Configuration::NamedNetworkFilterConfigFactory {
+class OriginalSrcConfigFactory
+ : public Common::FactoryBase<envoy::config::filter::network::original_src::v2alpha1::OriginalSrc>  {
 public:
+  OriginalSrcConfigFactory(): FactoryBase(NetworkFilterNames::get().OriginalSrc) {}
   // NamedNetworkFilterConfigFactory
-  Network::FilterFactoryCb createFilterFactory(const Json::Object&,
-                                               Server::Configuration::FactoryContext&) override {
-    return [](Network::FilterManager& filter_manager) -> void {
-      filter_manager.addReadFilter(std::make_shared<OriginalSrcFilter>());
-    };
-  }
 
   Network::FilterFactoryCb
-  createFilterFactoryFromProto(const Protobuf::Message&,
+  createFilterFactoryFromProtoTyped(
+    const envoy::config::filter::network::original_src::v2alpha1::OriginalSrc& proto_config,
                                Server::Configuration::FactoryContext&) override {
-    return [](Network::FilterManager& filter_manager) -> void {
-      filter_manager.addReadFilter(std::make_shared<OriginalSrcFilter>());
+    Config config(proto_config);
+    return [config](Network::FilterManager& filter_manager) -> void {
+      filter_manager.addReadFilter(std::make_shared<OriginalSrcFilter>(config));
     };
   }
-
-  ProtobufTypes::MessagePtr createEmptyConfigProto() override {
-    return ProtobufTypes::MessagePtr{new Envoy::ProtobufWkt::Empty()};
-  }
-
-  std::string name() override { return NetworkFilterNames::get().OriginalSrc; }
 };
 
 /**
