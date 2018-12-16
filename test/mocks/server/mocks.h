048581300dd24e8eab2626526e3f1ec88b9a3a0c
Jarno Rajahalme <jarno@covalent.io>
2018-12-15 21:08:17 -0800

    listener: Add listenerConfig() to ListenerFactoryContext. (#5306)
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- test/mocks/server/mocks.h
+++ test/mocks/server/mocks.h
@@ -473,6 +473,10 @@ public:
     addListenSocketOptions_(options);
   }
   MOCK_METHOD1(addListenSocketOptions_, void(const Network::Socket::OptionsSharedPtr&));
+  const Network::ListenerConfig& listenerConfig() const override { return _listenerConfig_; }
+  MOCK_CONST_METHOD0(listenerConfig_, const Network::ListenerConfig&());
+
+  Network::MockListenerConfig _listenerConfig_;
 };
 
 class MockHealthCheckerFactoryContext : public virtual HealthCheckerFactoryContext {
--- test/server/connection_handler_test.cc
+++ test/server/connection_handler_test.cc
@@ -44,10 +44,11 @@ public:
     Network::FilterChainManager& filterChainManager() override { return parent_.manager_; }
     Network::FilterChainFactory& filterChainFactory() override { return parent_.factory_; }
     Network::Socket& socket() override { return socket_; }
+    const Network::Socket& socket() const override { return socket_; }
     bool handOffRestoredDestinationConnections() const override {
       return hand_off_restored_destination_connections_;
     }
-    uint32_t perConnectionBufferLimitBytes() override { return 0; }
+    uint32_t perConnectionBufferLimitBytes() const override { return 0; }
     std::chrono::milliseconds listenerFiltersTimeout() const override {
       return listener_filters_timeout_;
     }
