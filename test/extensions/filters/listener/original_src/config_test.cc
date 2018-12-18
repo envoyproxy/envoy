f869a95c88a6fdb9eac60d975d86b062bdac2c6d
Kyle Larose <kyle@agilicus.com>
2018-12-18 09:42:48 -0500

    Give a unique name to the config test
    The anonymous namespace isn't suspicious to avoid coverage test
    collisions.  :(
    
    Signed-off-by: Kyle Larose <kyle@agilicus.com>

--- test/extensions/filters/listener/original_src/config_test.cc
+++ test/extensions/filters/listener/original_src/config_test.cc
@@ -13,7 +13,9 @@ namespace ListenerFilters {
 namespace OriginalSrc {
 namespace {
 
-class ConfigTest : public testing::Test {
+// In keeping with the class under test, it would have made sense to call this ConfigTest. However,
+// when running coverage tests, that conflicts with tests elsewhere in the codebase.
+class OriginalSrcConfigTest : public testing::Test {
 public:
   Config makeConfigFromProto(
       const envoy::config::filter::listener::original_src::v2alpha1::OriginalSrc& proto_config) {
@@ -21,7 +23,7 @@ public:
   }
 };
 
-TEST_F(ConfigTest, TestUsePortTrue) {
+TEST_F(OriginalSrcConfigTest, TestUsePortTrue) {
   envoy::config::filter::listener::original_src::v2alpha1::OriginalSrc config_proto;
   config_proto.set_bind_port(true);
   auto config = makeConfigFromProto(config_proto);
@@ -29,7 +31,7 @@ TEST_F(ConfigTest, TestUsePortTrue) {
   EXPECT_TRUE(config.usePort());
 }
 
-TEST_F(ConfigTest, TestUsePortFalse) {
+TEST_F(OriginalSrcConfigTest, TestUsePortFalse) {
   envoy::config::filter::listener::original_src::v2alpha1::OriginalSrc config_proto;
   config_proto.set_bind_port(false);
   auto config = makeConfigFromProto(config_proto);
@@ -37,7 +39,7 @@ TEST_F(ConfigTest, TestUsePortFalse) {
   EXPECT_FALSE(config.usePort());
 }
 
-TEST_F(ConfigTest, TestUseMark0) {
+TEST_F(OriginalSrcConfigTest, TestUseMark0) {
   envoy::config::filter::listener::original_src::v2alpha1::OriginalSrc config_proto;
   config_proto.set_mark(0);
   auto config = makeConfigFromProto(config_proto);
@@ -45,7 +47,7 @@ TEST_F(ConfigTest, TestUseMark0) {
   EXPECT_EQ(config.mark(), 0);
 }
 
-TEST_F(ConfigTest, TestUseMark1234) {
+TEST_F(OriginalSrcConfigTest, TestUseMark1234) {
   envoy::config::filter::listener::original_src::v2alpha1::OriginalSrc config_proto;
   config_proto.set_mark(1234);
   auto config = makeConfigFromProto(config_proto);
@@ -53,7 +55,7 @@ TEST_F(ConfigTest, TestUseMark1234) {
   EXPECT_EQ(config.mark(), 1234);
 }
 
-TEST_F(ConfigTest, TestUseMarkMax) {
+TEST_F(OriginalSrcConfigTest, TestUseMarkMax) {
   envoy::config::filter::listener::original_src::v2alpha1::OriginalSrc config_proto;
   config_proto.set_mark(std::numeric_limits<uint32_t>::max());
   auto config = makeConfigFromProto(config_proto);
