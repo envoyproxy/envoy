0623b196fcf0328bd3f9b9be0d9a688f40cbea76
Joshua Marantz <jmarantz@google.com>
2018-12-28 14:13:34 -0500

    Also test starting Envoy with no args, and with a valid base ID.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/integration/run_envoy_test.sh
+++ test/integration/run_envoy_test.sh
@@ -10,3 +10,22 @@ if [[ $EXIT_CODE -ne 1 ]]; then
     echo "Envoy exited with code: ${EXIT_CODE}"
     exit 1
 fi
+
+start_test Launching envoy with no args starts a server properly.
+log="${TEST_TMPDIR}/envoy.log"
+rm -f "$log"
+${ENVOY_BIN} >& "$log" &
+envoy_pid="$!"
+until grep "starting main dispatch loop" $log; do
+  sleep .2
+done
+kill -HUP "$envoy_pid"
+
+start_test Launching envoy with a valid base ID starts a server properly.
+rm -f "$log"
+${ENVOY_BIN} --base-id 1234 >& "$log" &
+envoy_pid="$!"
+until grep "starting main dispatch loop" $log; do
+  sleep .2
+done
+kill -HUP "$envoy_pid"
