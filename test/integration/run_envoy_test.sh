69c28327baf5708b4d995ad28d0ae20a73b65ef9
Joshua Marantz <jmarantz@google.com>
2018-12-30 20:35:50 -0500

    Report an error if no config is specified.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/integration/run_envoy_test.sh
+++ test/integration/run_envoy_test.sh
@@ -2,30 +2,28 @@
 
 source "$TEST_RUNDIR/test/integration/test_utility.sh"
 
+function expect_fail_with_error() {
+  log="${TEST_TMPDIR}/envoy.log"
+  rm -f "$log"
+  expected_error="$1"
+  shift
+  echo ${ENVOY_BIN} "$@" ">&" "$log"
+  ${ENVOY_BIN} "$@" >& "$log"
+  EXIT_CODE=$?
+  cat "$log"
+  check [ $EXIT_CODE -eq 1 ]
+  check grep "$expected_error" "$log"
+}
+
+
 start_test Launching envoy with a bogus command line flag.
-${ENVOY_BIN} --bogus-flag
-EXIT_CODE=$?
-# The test should fail if the Envoy binary exits with anything other than 1.
-if [[ $EXIT_CODE -ne 1 ]]; then
-    echo "Envoy exited with code: ${EXIT_CODE}"
-    exit 1
-fi
+expect_fail_with_error "PARSE ERROR: Argument: --bogus-flag" --bogus-flag
 
-start_test Launching envoy with no args starts a server properly.
-log="${TEST_TMPDIR}/envoy.log"
-rm -f "$log"
-${ENVOY_BIN} >& "$log" &
-envoy_pid="$!"
-until grep "starting main dispatch loop" $log; do
-  sleep .2
-done
-kill -HUP "$envoy_pid"
+start_test Launching envoy without --config-path or --config-yaml fails.
+expect_fail_with_error \
+  "Exactly one of --config-path '' and --config-yaml '' should be non-empty"
 
-start_test Launching envoy with a valid base ID starts a server properly.
-rm -f "$log"
-${ENVOY_BIN} --base-id 1234 >& "$log" &
-envoy_pid="$!"
-until grep "starting main dispatch loop" $log; do
-  sleep .2
-done
-kill -HUP "$envoy_pid"
+start_test Launching envoy with both --config-path and --config-yaml fails.
+expect_fail_with_error \
+  "Exactly one of --config-path 'x' and --config-yaml 'y' should be non-empty" \
+  --config-path x --config-yaml y
