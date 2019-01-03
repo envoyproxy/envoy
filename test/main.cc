6b0cc8baae5851e256f23e7802303b9f5d918319
Andres Guedez <aguedez@google.com>
2019-01-03 10:48:53 -0500

    Set the ThreadFactorySingleton only for debug builds.
    Signed-off-by: Andres Guedez <aguedez@google.com>

--- test/main.cc
+++ test/main.cc
@@ -22,7 +22,10 @@ int main(int argc, char** argv) {
   Envoy::SignalAction handle_sigs;
 #endif
 
+#ifndef NDEBUG
+  // The ThreadFactorySingleton is enabled only for debug builds.
   Envoy::Thread::ThreadFactorySingleton::set(&Envoy::Thread::threadFactoryForTest());
+#endif
 
   Envoy::TestEnvironment::setEnvVar("TEST_RUNDIR",
                                     (Envoy::TestEnvironment::getCheckedEnvVar("TEST_SRCDIR") + "/" +
