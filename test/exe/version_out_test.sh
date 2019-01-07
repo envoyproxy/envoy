888b27fc764a73dc4ada35ab69a9703bccb5af01
Piotr Sikora <piotrsikora@google.com>
2019-01-07 05:50:17 -0800

    test: more strict test for the --version output. (#5509)
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- test/exe/version_out_test.sh
+++ test/exe/version_out_test.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
 set -e -o pipefail
 
-source/exe/envoy-static --version | grep -E '[0-9]+\.[0-9]+\.[0-9]+'
+COMMIT=$(source/exe/envoy-static --version | \
+  sed -n -E 's/.*version: ([0-9a-f]{40})\/([0-9]+\.[0-9]+\.[0-9]+)(-dev)?\/(Clean|Modified)\/(RELEASE|DEBUG)\/([a-zA-Z-]+)$/\1/p')
+
+EXPECTED=$(cat bazel/raw_build_id.ldscript)
+
+if [[ ${COMMIT} != ${EXPECTED} ]]; then
+  echo "Commit mismatch, got: ${COMMIT}, expected: ${EXPECTED}".
+  exit 1
+fi
+
+VERSION=$(source/exe/envoy-static --version | \
+  sed -n -E 's/.*version: ([0-9a-f]{40})\/([0-9]+\.[0-9]+\.[0-9]+)(-dev)?\/(Clean|Modified)\/(RELEASE|DEBUG)\/([a-zA-Z-]+)$/\2\3/p')
+
+EXPECTED=$(cat VERSION)
+
+if [[ ${VERSION} != ${EXPECTED} ]]; then
+  echo "Version mismatch, got: ${VERSION}, expected: ${EXPECTED}".
+  exit 1
+fi
