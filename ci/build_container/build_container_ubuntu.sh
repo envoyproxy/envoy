e8bbce92a9262bef990dfc08fb9b942c9774fa45
Christopher M. Luciano <cmluciano@us.ibm.com>
2019-01-17 16:59:43 -0500

    Remove EXPECTED_CXX_VERSION check for gcc in build_container
    Signed-off-by: Christopher M. Luciano <cmluciano@us.ibm.com>

--- ci/build_container/build_container_ubuntu.sh
+++ ci/build_container/build_container_ubuntu.sh
@@ -6,7 +6,7 @@ set -e
 apt-get update
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y wget software-properties-common make cmake git python python-pip python3 python3-pip \
-  unzip bc libtool ninja-build automake zip time golang g++ gdb strace wireshark tshark tcpdump
+  unzip bc libtool ninja-build automake zip time golang gdb strace wireshark tshark tcpdump
 # clang 7.
 wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-7 main"
@@ -37,5 +37,5 @@ setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
 # virtualenv
 pip3 install virtualenv
 
-EXPECTED_CXX_VERSION="g++ (Ubuntu 7.4.0-1ubuntu1~16.04~ppa1) 7.4.0" ./build_container_common.sh
+./build_container_common.sh
 
