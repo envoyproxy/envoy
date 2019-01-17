c4bb24ebc144cd10a06fed6c9c95e9b12ffb53a0
cmluciano <cmluciano@cruznet.org>
2019-01-17 18:38:32 -0500

    Bump gcc version to 7.4 in build_container (#5641)
    Signed-off-by: Christopher M. Luciano <cmluciano@us.ibm.com>
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- ci/build_container/build_container_ubuntu.sh
+++ ci/build_container/build_container_ubuntu.sh
@@ -6,12 +6,20 @@ set -e
 apt-get update
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y wget software-properties-common make cmake git python python-pip python3 python3-pip \
-  unzip bc libtool ninja-build automake zip time golang g++ gdb strace wireshark tshark tcpdump
+  unzip bc libtool ninja-build automake zip time golang gdb strace wireshark tshark tcpdump
 # clang 7.
 wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-7 main"
 apt-get update
 apt-get install -y clang-7 clang-format-7 clang-tidy-7 lld-7 libc++-7-dev libc++abi-7-dev
+# gcc-7
+add-apt-repository ppa:ubuntu-toolchain-r/test
+apt update
+apt install -y g++-7
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 1000
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 1000
+update-alternatives --config gcc
+update-alternatives --config g++
 # Bazel and related dependencies.
 apt-get install -y openjdk-8-jdk curl
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
@@ -29,5 +37,5 @@ setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
 # virtualenv
 pip3 install virtualenv
 
-EXPECTED_CXX_VERSION="g++ (Ubuntu 5.4.0-6ubuntu1~16.04.11) 5.4.0 20160609" ./build_container_common.sh
+./build_container_common.sh
 
