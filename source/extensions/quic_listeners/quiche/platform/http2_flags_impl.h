020c72034dbbc8475d0ed3474411f9d43b93df56
Michael Warres <mpw@google.com>
2019-01-18 13:15:36 -0500

    Import current snapshot of QUICHE as an external dependency. (#5548)
    QUICHE is Google's implementation of
    QUIC and related protocols. It is the same code used in Chromium and Google's
    servers, but packaged in a form that is intended to be easier to incorporate
    into third-party projects.
    
    QUICHE is still a work in progress, with current efforts focusing on
    eliminating diffs between Google-internal and Chromium variants of the code, as
    well as providing buildfiles and a default implementation of the QUICHE
    platform API (the platform-dependent porting layer that sits underneath the
    main body of QUICHE, which is platform-independent).
    
    In the interim, the Google QUIC team has created a branch of QUICHE,
    envoy-integration,
    to provide access to the full set of QUICHE source files, while the work above
    continues. Since Envoy can overlay its own build rules on top of external
    dependencies, this should allow Envoy/QUICHE integration work to move forwards,
    without being blocked by completion of "full" QUICHE. When QUICHE is fully
    complete, we can just update the dependency to point to the official QUICHE
    release, making whatever buildfile adjustments are necessary.
    
    This PR is limited to setting up a basic import of QUICHE with a bare skeleton
    of build rules to build and test one element of the platform API. The plan is
    to add more build rules and platform implementation classes incrementally, in a
    series of follow-on CLs. The end milestone of the platform implementation phase
    will be when we can run the QUIC end-to-end tests included in QUICHE on top of
    the Envoy platform implementation.
    
    Risk Level: low--code not used yet
    Testing: unit test to exercise build rules
    
    Signed-off-by: Michael Warres <mpw@google.com>

---
+++ source/extensions/quic_listeners/quiche/platform/http2_flags_impl.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// NOLINT(namespace-envoy)
+
+// This file is part of the QUICHE platform implementation, and is not to be
+// consumed or referenced directly by other Envoy code. It serves purely as a
+// porting layer for QUICHE.
+
+// TODO: implement
+
+#define GetHttp2ReloadableFlagImpl(flag) 0
+#define SetHttp2ReloadableFlagImpl(flag, value) 0
