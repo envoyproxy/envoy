db600fd0370750d315417a47dfaaaabc5d79aecd
Michael Warres <mpw@google.com>
2019-01-21 11:24:01 -0500

    Misc additions for stubbing out QUICHE platform impls.
    - Add QUICHE spdy_platform and quic_platform build rules.
    - Prune not yet implemented platform implementation files, to reduce noise.
    - For partially complete platform implementation files, add "TODO: implement"
      comments to individual items that still need to be implemented.
    - Add unit tests for platform APIs that have already been implemented.
    
    Signed-off-by: Michael Warres <mpw@google.com>

--- source/extensions/quic_listeners/quiche/platform/spdy_macros_impl.h
+++ source/extensions/quic_listeners/quiche/platform/spdy_macros_impl.h
@@ -1,13 +1,15 @@
 #pragma once
 
+#include "absl/base/attributes.h"
+
 // NOLINT(namespace-envoy)
 
 // This file is part of the QUICHE platform implementation, and is not to be
 // consumed or referenced directly by other Envoy code. It serves purely as a
 // porting layer for QUICHE.
 
-// TODO: implement
+#define SPDY_MUST_USE_RESULT_IMPL ABSL_MUST_USE_RESULT
+#define SPDY_UNUSED_IMPL ABSL_ATTRIBUTE_UNUSED
 
-#define HTTP2_BUG_IMPL 0
-#define HTTP2_BUG_IF_IMPL 0
-#define FLAGS_http2_always_log_bugs_for_tests_IMPL 0
+// TODO: implement
+#define SPDY_DVLOG_IF_IMPL 0
