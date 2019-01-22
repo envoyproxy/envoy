2f0d285e85a14f047d46826ee975d87d437fcd32
Emil Mikulic <g-easy@users.noreply.github.com>
2019-01-22 15:20:42 +1100

    Remove debugging printfs.
    Signed-off-by: Emil Mikulic <g-easy@users.noreply.github.com>

--- source/extensions/tracers/opencensus/config.cc
+++ source/extensions/tracers/opencensus/config.cc
@@ -1,7 +1,5 @@
 #include "extensions/tracers/opencensus/config.h"
 
-#include <iostream>
-
 #include "envoy/registry/registry.h"
 
 #include "common/tracing/http_tracer_impl.h"
@@ -15,12 +13,10 @@ namespace Tracers {
 namespace OpenCensus {
 
 OpenCensusTracerFactory::OpenCensusTracerFactory() : FactoryBase(TracerNames::get().OpenCensus) {
-  std::cerr << "OpenCensusTracerFactory()\n";
 }
 
 Tracing::HttpTracerPtr OpenCensusTracerFactory::createHttpTracerTyped(
     const envoy::config::trace::v2::OpenCensusConfig& proto_config, Server::Instance& server) {
-  std::cerr << "OpenCensusTracerFactory::createHttpTracerTyped()\n";
   Tracing::DriverPtr driver = std::make_unique<Driver>(proto_config);
   return std::make_unique<Tracing::HttpTracerImpl>(std::move(driver), server.localInfo());
 }
