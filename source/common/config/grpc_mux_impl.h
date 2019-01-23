972f0f1fc96e09405752168e51d2516db51fdfab
Fred Douglas <fredlas@google.com>
2019-01-23 16:26:03 -0500

    move std queue include
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- source/common/config/grpc_mux_impl.h
+++ source/common/config/grpc_mux_impl.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <queue>
 #include <unordered_map>
 
 #include "envoy/common/time.h"
--- source/common/config/grpc_stream.h
+++ source/common/config/grpc_stream.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <functional>
+#include <queue>
 
 #include "envoy/grpc/async_client.h"
 
