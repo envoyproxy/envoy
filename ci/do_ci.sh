27531fd13585903e3c5fe7070ba4bd58b74bd26a
Alex Konradi <akonradi@google.com>
2019-01-23 21:14:44 -0500

    Control number of CPUs directly
    Try a heaver-handed approach to making Bazel keep the number of jobs
    down by reducing the number of CPUs used directly.
    
    Signed-off-by: Alex Konradi <akonradi@google.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -217,11 +217,13 @@ elif [[ "$1" == "bazel.coverage" ]]; then
   export TESTLOGS_DIR="${ENVOY_BUILD_DIR}/bazel-testlogs"
   export WORKSPACE=ci
 
-  # Reduce the amount of memory Bazel tries to use to prevent it from launching
-  # too many subprocesses. This should prevent the system from running out of
-  # memory and killing compilation tasks. See discussion on
-  # https://github.com/envoyproxy/envoy/pull/5611.
-  export BAZEL_TEST_OPTIONS="${BAZEL_TEST_OPTIONS} --ram_utilization_factor=40"
+  # Reduce the amount of memory and number of cores Bazel tries to use to
+  # prevent it from launching too many subprocesses. This should prevent the
+  # system from running out of memory and killing compilation tasks. See
+  # discussion on https://github.com/envoyproxy/envoy/pull/5611.
+  # TODO(akonradi): use --local_cpu_resources flag once Bazel has a release
+  # after 0.21.
+  export BAZEL_TEST_OPTIONS="${BAZEL_TEST_OPTIONS} --local_resources=12288,6,1"
 
   # There is a bug in gcovr 3.3, where it takes the -r path,
   # in our case /source, and does a regex replacement of various
