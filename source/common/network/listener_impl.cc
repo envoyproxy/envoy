ee78778d33c0ebf3f83e25625b2d980933b7b056
Jojy G Varghese <jojy_varghese@apple.com>
2019-01-24 22:27:48 -0800

    Fix format.
    Signed-off-by: Jojy G Varghese <jojy_varghese@apple.com>

--- source/common/network/listener_impl.cc
+++ source/common/network/listener_impl.cc
@@ -48,7 +48,8 @@ void ListenerImpl::listenCallback(evconnlistener*, evutil_socket_t fd, sockaddr*
 }
 
 void ListenerImpl::setupServerSocket(const Event::DispatcherImpl& dispatcher, Socket& socket) {
-  listener_.reset(evconnlistener_new(&dispatcher.base(), listenCallback, this, 0, -1, socket.ioHandle().fd()));
+  listener_.reset(
+      evconnlistener_new(&dispatcher.base(), listenCallback, this, 0, -1, socket.ioHandle().fd()));
 
   if (!listener_) {
     throw CreateListenerException(
