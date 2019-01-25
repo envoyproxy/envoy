8fd5e7a0fbce644e2c5f60f0b5747101f5abca2e
cclauss <cclauss@me.com>
2019-01-25 17:23:05 +0100

    CI: Run remaining format tests on Python 3 (#5714)
    Signed-off-by: cclauss <cclauss@me.com>
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- tools/format_python_tools.sh
+++ tools/format_python_tools.sh
@@ -11,12 +11,11 @@ cd "$SCRIPTPATH"
 if [ "${CIRCLECI}" != "true" ]; then
   source_venv "$VENV_DIR"
   echo "Installing requirements..."
-  pip install -r requirements.txt
+  pip3 install -r requirements.txt
 fi
 
 echo "Running Python format check..."
-python format_python_tools.py $1
+./format_python_tools.py $1
 
 echo "Running Python3 flake8 check..."
-pip3 install flake8
 flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics
