9786f052b14d64275fefb3bd98f5a87ee6a8db75
Matt Klein <mattklein123@gmail.com>
2019-01-25 11:16:55 -0700

    Revert "CI: Run remaining format tests on Python 3 (#5714)" (#5721)
    This reverts commit 5a7bf7c3d7fc8d0af017e78c6ed81cb1481ce763.
    
    Signed-off-by: Matt Klein <mklein@lyft.com>

--- tools/format_python_tools.sh
+++ tools/format_python_tools.sh
@@ -11,11 +11,12 @@ cd "$SCRIPTPATH"
 if [ "${CIRCLECI}" != "true" ]; then
   source_venv "$VENV_DIR"
   echo "Installing requirements..."
-  pip3 install -r requirements.txt
+  pip install -r requirements.txt
 fi
 
 echo "Running Python format check..."
-./format_python_tools.py $1
+python format_python_tools.py $1
 
 echo "Running Python3 flake8 check..."
+pip3 install flake8
 flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics
