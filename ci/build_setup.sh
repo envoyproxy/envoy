91c0adc5d3c1c972654b123eb10a2b4e531d8781
Lizan Zhou <lizan@tetrate.io>
2019-01-26 03:52:24 +0000

    retry
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/build_setup.sh
+++ ci/build_setup.sh
@@ -14,8 +14,6 @@ echo "ENVOY_SRCDIR=${ENVOY_SRCDIR}"
 function setup_gcc_toolchain() {
   export CC=gcc
   export CXX=g++
-  [ -z "$CIRCLECI" ] || export BAZEL_BUILD_OPTIONS="${BAZEL_BUILD_OPTIONS} --copt=--param=ggc-min-expand=30"
-  [ -z "$CIRCLECI" ] || export BAZEL_TEST_OPTIONS="${BAZEL_TEST_OPTIONS} --copt=--param=ggc-min-expand=30"
   echo "$CC/$CXX toolchain configured"
 }
 
