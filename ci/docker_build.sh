6702a4eb7e6a83300f754af4df40954881bede81
Jamie Hewland <jhewland@gmail.com>
2019-01-27 12:18:46 +0200

    circleci: Try build Docker images with docker image
    Signed-off-by: Jamie Hewland <jhewland@gmail.com>

--- ci/docker_build.sh
+++ ci/docker_build.sh
@@ -1,16 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 set -ex
-
-# TODO(mattklein123): Currently we are doing this push in the context of the release job which
-# happens inside of our build image. We should switch to using Circle caching so each of these
-# are discrete jobs that work with the binary. All of these commands run on a remote docker
-# server also so we have to temporarily install docker here.
-# https://circleci.com/docs/2.0/building-docker-images/
-VER="17.03.0-ce"
-curl -L -o /tmp/docker-"$VER".tgz https://get.docker.com/builds/Linux/x86_64/docker-"$VER".tgz
-tar -xz -C /tmp -f /tmp/docker-"$VER".tgz
-mv /tmp/docker/* /usr/bin
 
 docker build -f ci/Dockerfile-envoy-image -t envoyproxy/envoy:latest .
 docker build -f ci/Dockerfile-envoy-alpine -t envoyproxy/envoy-alpine:latest .
--- ci/docker_push.sh
+++ ci/docker_push.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Do not ever set -x here, it is a security hazard as it will place the credentials below in the
 # CircleCI logs.
@@ -15,16 +15,6 @@ do
 done
 if [ -z "$CIRCLE_PULL_REQUEST" ] && [ -z "$CIRCLE_TAG" ] && [ "$want_push" == "true" ]
 then
-   # TODO(mattklein123): Currently we are doing this push in the context of the release job which
-   # happens inside of our build image. We should switch to using Circle caching so each of these
-   # are discrete jobs that work with the binary. All of these commands run on a remote docker
-   # server also so we have to temporarily install docker here.
-   # https://circleci.com/docs/2.0/building-docker-images/
-   VER="17.03.0-ce"
-   curl -L -o /tmp/docker-"$VER".tgz https://get.docker.com/builds/Linux/x86_64/docker-"$VER".tgz
-   tar -xz -C /tmp -f /tmp/docker-"$VER".tgz
-   mv /tmp/docker/* /usr/bin
-
    docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
 
    docker push envoyproxy/envoy:latest
