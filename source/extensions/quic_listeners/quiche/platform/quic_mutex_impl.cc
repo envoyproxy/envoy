73610ae00feb6277a44260b00537da2615b77ad1
Dan Zhang <danzh@google.com>
2019-01-29 19:11:11 -0500

    Add impl

---
+++ source/extensions/quic_listeners/quiche/platform/quic_mutex_impl.cc
@@ -0,0 +1,25 @@
+#include "quiche/platform/quic_mutex_impl.h"
+
+namespace quic {
+
+void QuicLockImpl::WriterLock() {
+  mu_.WriterLock();
+}
+
+void QuicLockImpl::WriterUnlock() {
+  mu_.WriterUnlock();
+}
+
+void QuicLockImpl::ReaderLock() {
+  mu_.ReaderLock();
+}
+
+void QuicLockImpl::ReaderUnlock() {
+  mu_.ReaderUnlock();
+}
+
+void QuicLockImpl::AssertReaderHeld() const {
+  mu_.AssertReaderHeld();
+}
+
+}  // namespace quic
