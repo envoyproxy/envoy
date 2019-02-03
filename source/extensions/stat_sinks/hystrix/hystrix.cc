c238203f0fb6914e40eb8c8cc63aaf24c89ededd
Raúl Gutiérrez Segalés <rgs@itevenworks.net>
2019-02-03 14:17:47 -0800

    Fix setReference() call in hystrix stats sink (#5815)
    Per:
    
    https://github.com/envoyproxy/envoy/blob/master/include/envoy/http/header_map.h#L148
    
    `setReference()` needs a std::string ref that will live beyond
    the lifetime of a request/response, but it's currently receiving
    a string literal which is used to construct a temporary
    std::string.
    
    So instead pass an integer.
    
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- source/extensions/stat_sinks/hystrix/hystrix.cc
+++ source/extensions/stat_sinks/hystrix/hystrix.cc
@@ -290,7 +290,7 @@ Http::Code HystrixSink::handlerHystrixEventStream(absl::string_view,
       AccessControlAllowHeadersValue.AllowHeadersHystrix);
   response_headers.insertAccessControlAllowOrigin().value().setReference(
       Http::Headers::get().AccessControlAllowOriginValue.All);
-  response_headers.insertNoChunks().value().setReference("0");
+  response_headers.insertNoChunks().value().setInteger(0);
 
   Http::StreamDecoderFilterCallbacks& stream_decoder_filter_callbacks =
       admin_stream.getDecoderFilterCallbacks();
