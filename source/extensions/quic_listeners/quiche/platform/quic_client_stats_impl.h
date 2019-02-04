a7be9bbdc205693d5432de3c77062741363299ad
Bin Wu <wub@google.com>
2019-02-04 13:43:11 -0500

    Add quic_client_stats_impl.h to QUICHE platform implementation, with everything being no-op.
    Signed-off-by: Bin Wu <wub@google.com>

---
+++ source/extensions/quic_listeners/quiche/platform/quic_client_stats_impl.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// NOLINT(namespace-envoy)
+//
+// This file is part of the QUICHE platform implementation, and is not to be
+// consumed or referenced directly by other Envoy code. It serves purely as a
+// porting layer for QUICHE.
+
+#define QUIC_CLIENT_HISTOGRAM_ENUM_IMPL(name, sample, enum_size, docstring)                        \
+  do {                                                                                             \
+  } while (0)
+#define QUIC_CLIENT_HISTOGRAM_BOOL_IMPL(name, sample, docstring)                                   \
+  do {                                                                                             \
+  } while (0)
+#define QUIC_CLIENT_HISTOGRAM_TIMES_IMPL(name, sample, min, max, num_buckets, docstring)           \
+  do {                                                                                             \
+  } while (0)
+#define QUIC_CLIENT_HISTOGRAM_COUNTS_IMPL(name, sample, min, max, num_buckets, docstring)          \
+  do {                                                                                             \
+  } while (0)
+
+namespace quic {
+
+inline void QuicClientSparseHistogramImpl(const std::string& /*name*/, int /*sample*/) {}
+
+} // namespace quic
