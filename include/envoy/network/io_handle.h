3621f3a0f4a35496f336ada663092c2cc121ab38
Dan Zhang <danzh@google.com>
2019-02-07 10:44:55 -0500

    abstract more types
    Signed-off-by: Dan Zhang <danzh@google.com>

--- include/envoy/network/io_handle.h
+++ include/envoy/network/io_handle.h
@@ -1,15 +1,40 @@
 #pragma once
 
+#include <bitset>
 #include <memory>
 
 #include "envoy/common/pure.h"
 
 namespace Envoy {
+
+namespace Buffer {
+struct RawSlice;
+}
+
 namespace Network {
 
 namespace Address {
 class Instance;
 } // namespace Address
+
+class IoError {
+public:
+  enum IoErrorCode {
+    IO_NO_ERROR = 0,
+    IO_EAGAIN,
+    IO_ENOTSUP,
+    IO_EAFNOSUPPORT,
+    IO_EINPROGRESS,
+    IO_EPERM,
+    IO_UNKNOWN_ERROR
+  };
+  virtual ~IoError() {}
+
+  // Map platform specific error into IoErrorCode.
+  virtual IoErrorCode getErrorCode() PURE;
+  
+  virtual std::string getErrorDetails() PURE;
+};
 
 /**
  * Basic type for return result which has a return code and error code defined
@@ -19,17 +44,28 @@ template <typename T> struct IoHandleCallResult {
   virtual ~IoHandleCallResult() {}
 
   T rc_;
-  int errno_;
+  std::unique_ptr<IoError> err_;
 };
 
-typedef IoHandleCallResult<int> IoHandleCallIntResult;
-typedef IoHandleCallResult<ssize_t> IoHandleCallSizeResult;
+using IoHandleCallIntResult = IoHandleCallResult<int>;
+using IoHandleCallSizeResult = IoHandleCallResult<ssize_t>;
 
 /**
  * IoHandle: an abstract interface for all I/O operations
  */
 class IoHandle {
 public:
+  enum ShutdownType {
+    READ = 0,
+    WRITE,
+    BOTH
+  };
+
+  enum IoHandleFlag {
+    NONBLOCK = 1,
+    APPEND = 2
+  };
+
   virtual ~IoHandle() {}
 
   /**
@@ -49,9 +85,15 @@ public:
 
   virtual bool isClosed() PURE;
 
-  virtual IoHandleCallSizeResult readv(const iovec* iovec, int num_iovec) PURE;
+  virtual IoHandleCallSizeResult readv(Buffer::RawSlice* iovecs, int num_iovec) PURE;
 
-  virtual IoHandleCallSizeResult writev(const iovec* iovec, int num_iovec) PURE;
+  virtual IoHandleCallIntResult recvmmsg(struct mmsghdr *msgvec, unsigned int vlen,
+                    int flags, struct timespec *timeout) PURE;
+
+  virtual IoHandleCallSizeResult writev(const Buffer::RawSlice* iovec, int num_iovec) PURE;
+
+  virtual IoHandleCallIntResult sendmmsg(struct mmsghdr* msgvec, unsigned int vlen,
+                    int flags) PURE;
 
   virtual IoHandleCallIntResult bind(const Network::Address::Instance& address) PURE;
 
@@ -69,20 +111,23 @@ public:
                                                   socklen_t* optlen) PURE;
 
   /**
-   * Wrap getsockname()
+   * Wrap Address::addressFromFd()
    */
-  virtual IoHandleCallIntResult getIoHandleName(const Network::Address::Instance& address) PURE;
+  virtual IoHandleCallIntResult getBindToAddress(Network::Address::Instance** address) PURE;
 
-  virtual IoHandleCallIntResult getPeerName(const Network::Address::Instance& address) PURE;
+  /**
+   * Wrap Address::peerAddressFromFd()
+   */
+  virtual IoHandleCallIntResult getPeerAddress(Network::Address::Instance** address) PURE;
 
   /**
    * Wrap fcntl(fd_, F_SETFL...)
    */
-  virtual IoHandleCallIntResult setIoHandleFlag(int flag) PURE;
+  virtual IoHandleCallIntResult setIoHandleFlag(std::bitset<2> flag) PURE;
   /**
    * Wrap fcntl(fd_, F_GETFL...)
    */
-  virtual IoHandleCallIntResult getIoHandleFlag() PURE;
+  virtual IoHandleCallResult<std::bitset<2>> getIoHandleFlags() PURE;
 
   virtual IoHandleCallIntResult listen(int backlog) PURE;
 
@@ -91,7 +136,7 @@ public:
    */
   virtual std::unique_ptr<IoHandle> dup() PURE;
 
-  virtual IoHandleCallIntResult shutdown(int how) PURE;
+  virtual IoHandleCallIntResult shutdown(ShutdownType how) PURE;
 };
 
 typedef std::unique_ptr<IoHandle> IoHandlePtr;
