4308cb15c70290e7235f0bdcaca4dd598cfc5861
Dan Zhang <danzh@google.com>
2019-02-08 11:01:24 -0500

    NOLINT
    Signed-off-by: Dan Zhang <danzh@google.com>

--- source/extensions/quic_listeners/quiche/platform/quic_cert_utils_impl.cc
+++ source/extensions/quic_listeners/quiche/platform/quic_cert_utils_impl.cc
@@ -1,3 +1,9 @@
+// NOLINT(namespace-envoy)
+
+// This file is part of the QUICHE platform implementation, and is not to be
+// consumed or referenced directly by other Envoy code. It serves purely as a
+// porting layer for QUICHE.
+
 #include "extensions/quic_listeners/quiche/platform/quic_cert_utils_impl.h"
 
 #include "openssl/bytestring.h"
