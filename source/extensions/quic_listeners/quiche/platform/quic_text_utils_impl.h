ddcf019b2c6e1294cf56278dd273d906c80915cb
Michael Warres <mpw@google.com>
2019-02-11 13:25:53 -0500

    Address review comments.
    Also move non-trivial method definitions to quic_text_utils_impl.cc.
    
    Signed-off-by: Michael Warres <mpw@google.com>

--- source/extensions/quic_listeners/quiche/platform/quic_text_utils_impl.h
+++ source/extensions/quic_listeners/quiche/platform/quic_text_utils_impl.h
@@ -31,21 +31,7 @@ public:
   static QuicString ToLower(QuicStringPiece data) { return absl::AsciiStrToLower(data); }
 
   static void RemoveLeadingAndTrailingWhitespace(QuicStringPiece* data) {
-    size_t count = 0;
-    const char* ptr = data->data();
-    while (count < data->size() && absl::ascii_isspace(*ptr)) {
-      ++count;
-      ++ptr;
-    }
-    data->remove_prefix(count);
-
-    count = 0;
-    ptr = data->data() + data->size() - 1;
-    while (count < data->size() && absl::ascii_isspace(*ptr)) {
-      ++count;
-      --ptr;
-    }
-    data->remove_suffix(count);
+    *data = absl::StripAsciiWhitespace(*data);
   }
 
   static bool StringToUint64(QuicStringPiece in, uint64_t* out) {
@@ -68,53 +54,9 @@ public:
 
   static QuicString HexDecode(QuicStringPiece data) { return absl::HexStringToBytes(data); }
 
-  static void Base64Encode(const uint8_t* data, size_t data_len, QuicString* output) {
-    absl::Base64Escape(QuicString(reinterpret_cast<const char*>(data), data_len), output);
-    // Remove padding.
-    size_t len = output->size();
-    if (len >= 2) {
-      if ((*output)[len - 1] == '=') {
-        len--;
-        if ((*output)[len - 1] == '=') {
-          len--;
-        }
-        output->resize(len);
-      }
-    }
-  }
+  static void Base64Encode(const uint8_t* data, size_t data_len, QuicString* output);
 
-  static QuicString HexDump(QuicStringPiece binary_data) {
-    const int kBytesPerLine = 16;
-    const char* buf = binary_data.data();
-    int bytes_remaining = binary_data.size();
-    int offset = 0;
-    std::string out;
-    const char* p = buf;
-    while (bytes_remaining > 0) {
-      const int line_bytes = std::min(bytes_remaining, kBytesPerLine);
-      absl::StrAppendFormat(&out, "0x%04x:  ", offset); // Do the line header
-      for (int i = 0; i < kBytesPerLine; ++i) {
-        if (i < line_bytes) {
-          absl::StrAppendFormat(&out, "%02x", p[i]);
-        } else {
-          out += "  "; // two-space filler instead of two-space hex digits
-        }
-        if (i % 2) {
-          out += ' ';
-        }
-      }
-      out += ' ';
-      for (int i = 0; i < line_bytes; ++i) { // Do the ASCII dump
-        out += absl::ascii_isgraph(p[i]) ? p[i] : '.';
-      }
-
-      bytes_remaining -= line_bytes;
-      offset += line_bytes;
-      p += line_bytes;
-      out += '\n';
-    }
-    return out;
-  }
+  static QuicString HexDump(QuicStringPiece binary_data);
 
   static bool ContainsUpperCase(QuicStringPiece data) {
     return std::any_of(data.begin(), data.end(), absl::ascii_isupper);
