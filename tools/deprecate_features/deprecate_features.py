6282554f0152959c5ae60171a27f10b59b930871
Alyssa Wilk <alyssar@chromium.org>
2019-02-13 10:08:54 -0500

    bugfix, reviewer comments
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- tools/deprecate_features/deprecate_features.py
+++ tools/deprecate_features/deprecate_features.py
@@ -4,13 +4,7 @@ import re
 import subprocess
 import fileinput
 
-grep = subprocess.Popen(
-    'grep -r "deprecated = true" api/*',
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.STDOUT,
-    shell=True)
-grep_output = grep.communicate()[0]
+grep_output = subprocess.check_output("grep -r \"deprecated = true\" api/*", shell=True)
 
 filenames_and_fields = set()
 
@@ -25,12 +19,11 @@ for line in grep_output.splitlines():
 
 # Now discard any deprecated features already listed in runtime_features
 exiting_deprecated_regex = re.compile(r".*\"envoy.deprecated_features.(.*):(.*)\",.*")
-features = open('source/common/runtime/runtime_features.h', "r")
-for line in features.readlines():
-  match = exiting_deprecated_regex.match(line)
-  if match:
-    filenames_and_fields.discard(tuple([match.group(1), match.group(2)]))
-features.close()
+with open('source/common/runtime/runtime_features.h', "r") as features:
+  for line in features.readlines():
+    match = exiting_deprecated_regex.match(line)
+    if match:
+      filenames_and_fields.discard(tuple([match.group(1), match.group(2)]))
 
 # Finally sort out the code to add to runtime_features.h and a canned email for envoy-announce.
 code = ""
@@ -48,6 +41,7 @@ if not raw_input('Apply runtime changes? [yN] ').strip().lower() in ("y", "yes")
 for line in fileinput.FileInput('source/common/runtime/runtime_features.h', inplace=1):
   if "envoy.deprecated_features.deprecated.proto:is_deprecated_fatal" in line:
     line = line.replace(line, line + code)
+  print line,
 
 print "\nChanges applied.  Please create an upstream PR and send the following to envoy-announce:\n"
 
