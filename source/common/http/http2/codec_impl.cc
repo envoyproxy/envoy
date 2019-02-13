9f548866c1b3ecbde9c02277e7669c22eed66106
Harvey Tuch <htuch@google.com>
2019-02-13 16:30:20 -0500

    Comment.
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- source/common/http/http2/codec_impl.cc
+++ source/common/http/http2/codec_impl.cc
@@ -41,6 +41,8 @@ void initializeNghttp2Logging() {
   nghttp2_set_debug_vprintf_callback([](const char* format, va_list args) {
     char buf[2048];
     const int n = ::vsnprintf(buf, sizeof(buf), format, args);
+    // nghttp2 inserts new lines, but we also insert a new line in the ENVOY_LOG
+    // below, so avoid double \n.
     if (n >= 1 && static_cast<size_t>(n) < sizeof(buf) && buf[n - 1] == '\n') {
       buf[n - 1] = '\0';
     }
