73ba1b2ed9d3f32bcb5b393e63aa2ef325812251
Harvey Tuch <htuch@google.com>
2019-02-14 07:41:20 -0500

    WiP: evaluate Bazel native coverage in CI.
    For now, try and figure out if bazel native coverage will fly from a performance perspective.
    
    DO NOT SUBMIT
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- ci/build_setup.sh
+++ ci/build_setup.sh
@@ -70,8 +70,9 @@ fi
 # Not sandboxing, since non-privileged Docker can't do nested namespaces.
 BAZEL_OPTIONS="--package_path %workspace%:${ENVOY_SRCDIR}"
 export BAZEL_QUERY_OPTIONS="${BAZEL_OPTIONS}"
-export BAZEL_BUILD_OPTIONS="--strategy=Genrule=standalone --spawn_strategy=standalone \
-  --verbose_failures ${BAZEL_OPTIONS} --action_env=HOME --action_env=PYTHONUSERBASE \
+# Make sure not to include --strategy=Genrule=standalone --spawn_strategy=standalone below, they
+# break bazel coverage.
+export BAZEL_BUILD_OPTIONS="--verbose_failures ${BAZEL_OPTIONS} --action_env=HOME --action_env=PYTHONUSERBASE \
   --jobs=${NUM_CPUS} --show_task_finish --experimental_generate_json_trace_profile ${BAZEL_BUILD_EXTRA_OPTIONS}"
 export BAZEL_TEST_OPTIONS="${BAZEL_BUILD_OPTIONS} --test_env=HOME --test_env=PYTHONUSERBASE \
   --test_env=UBSAN_OPTIONS=print_stacktrace=1 \
