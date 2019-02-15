065b40f2c05195a298bd72e2cb3933139e0d0a5e
Lizan Zhou <lizan@tetrate.io>
2019-02-15 21:21:14 +0000

    path to dockerfile
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/build_container/build_container_centos.sh
+++ ci/build_container/build_container_centos.sh
@@ -5,7 +5,7 @@ set -e
 # Note: rh-git218 is needed to run `git -C` in docs build process.
 yum install -y centos-release-scl epel-release
 yum update -y
-yum install -y devtoolset-7-gcc devtoolset-7-gcc-c++ devtoolset-7-binutils java-1.8.0-openjdk-headless \
+yum install -y devtoolset-7-gcc devtoolset-7-gcc-c++ devtoolset-7-binutils java-1.8.0-openjdk-headless rsync \
     rh-git218 wget unzip which make cmake3 patch ninja-build devtoolset-7-libatomic-devel openssl python27
 
 ln -s /usr/bin/cmake3 /usr/bin/cmake
@@ -19,11 +19,9 @@ chmod u+x "./${BAZEL_INSTALLER}"
 "./${BAZEL_INSTALLER}"
 rm "./${BAZEL_INSTALLER}"
 
-# Required by rh-git218
+# httpd24 is equired by rh-git218
 echo "/opt/rh/httpd24/root/usr/lib64" > /etc/ld.so.conf.d/httpd24.conf
 ldconfig
-
-export PATH=/opt/rh/devtoolset-7/root/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # CentOS toolchains doesn't have C++11 ABI so needs ignore, also punting PATH into system bazelrc.
 echo "build --copt=-DENVOY_IGNORE_GLIBCXX_USE_CXX11_ABI_ERROR=1 --action_env=PATH=${PATH}" >> /etc/bazel.bazelrc
