028da2f6294477b06838691a36a68f56cafe4d3d
Harvey Tuch <htuch@google.com>
2019-02-21 08:16:19 -0500

    config_validation/server: fix initialization order redux.
    Another heap-user-after-free, this time we were missing a fix that had been applied to server.h but
    not config_validation/server.h (#4940). While working on this, attempted to make fully consistent and as
    simple/clear as possible the constraints on member ordering.
    
    This PR is in the tradition (!) of #5847, #4940, #4937. I think long-term we might want to think of
    more dynamic and explicit declaration of ordering constraints, it's evidently pretty fragile. Also,
    the lack of single-source-of-truth and duplication across prod and config server code bites again.
    
    Fixes oss-fuzz issue https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=13228.
    
    Risk level: Low
    Testing: Corpus entry added.
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- source/server/server.h
+++ source/server/server.h
@@ -200,6 +200,14 @@ private:
   void startWorkers();
   void terminate();
 
+  // init_manager_ must come before any member that participates in initialization, since
+  // initialization continuation can potentially occur at any point during member lifetime.
+  InitManagerImpl init_manager_;
+  // secret_manager_ must come before listener_manager_, config_ and dispatcher_, since:
+  // - There may be active filter chains referencing it in listener_manager_.
+  // - There may be active clusters referencing it in config_.cluster_manager_.
+  // - There may be active connections referencing it.
+  std::unique_ptr<Secret::SecretManager> secret_manager_;
   bool shutdown_;
   const Options& options_;
   TimeSource& time_source_;
@@ -211,9 +219,6 @@ private:
   Assert::ActionRegistrationPtr assert_action_registration_;
   ThreadLocal::Instance& thread_local_;
   Api::ApiPtr api_;
-  // secret_manager_ must come before dispatcher_, since there may be active connections
-  // referencing it, so need to destruct these first.
-  std::unique_ptr<Secret::SecretManager> secret_manager_;
   Event::DispatcherPtr dispatcher_;
   std::unique_ptr<AdminImpl> admin_;
   Singleton::ManagerPtr singleton_manager_;
@@ -231,7 +236,6 @@ private:
   DrainManagerPtr drain_manager_;
   AccessLog::AccessLogManagerImpl access_log_manager_;
   std::unique_ptr<Upstream::ClusterManagerFactory> cluster_manager_factory_;
-  InitManagerImpl init_manager_;
   std::unique_ptr<Server::GuardDog> guard_dog_;
   bool terminated_;
   std::unique_ptr<Logger::FileSinkDelegate> file_logger_;
