93a29e4189fde3fcd1090e92b6fc6ae36b67a712
Yael Harel <yharel@pivotal.io>
2019-02-21 16:17:00 -0500

    utility: use absl::Time library
    Replace strftime, mktime, gmtime with their absl::Time equivalents
    
    Prohibit use of strftime, strptime, mktime, gmtime, gmtime_r,
    localtime, localtime_r, std::get_time and std::put_time
    
    Fixes #5587
    
    Signed-off-by: Yael Harel <yharel@pivotal.io>
    Signed-off-by: Yechiel Kalmenson <ykalmenson@pivotal.io>
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- test/common/common/utility_test.cc
+++ test/common/common/utility_test.cc
@@ -812,11 +812,9 @@ TEST(DateFormatter, FromTime) {
   const SystemTime time1(std::chrono::seconds(1522796769));
   EXPECT_EQ("2018-04-03T23:06:09.000Z", DateFormatter("%Y-%m-%dT%H:%M:%S.000Z").fromTime(time1));
   EXPECT_EQ("aaa23", DateFormatter(std::string(3, 'a') + "%H").fromTime(time1));
-  EXPECT_EQ("", DateFormatter(std::string(1022, 'a') + "%H").fromTime(time1));
   const SystemTime time2(std::chrono::seconds(0));
   EXPECT_EQ("1970-01-01T00:00:00.000Z", DateFormatter("%Y-%m-%dT%H:%M:%S.000Z").fromTime(time2));
   EXPECT_EQ("aaa00", DateFormatter(std::string(3, 'a') + "%H").fromTime(time2));
-  EXPECT_EQ("", DateFormatter(std::string(1022, 'a') + "%H").fromTime(time2));
 }
 
 // Verify that two DateFormatter patterns with the same ??? patterns but
