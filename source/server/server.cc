6ff86030be9e9ca078ad7a1e45616dcd2e45e6b7
htuch <htuch@users.noreply.github.com>
2019-02-21 14:02:08 -0500

    config_validation/server: fix initialization order redux. (#6023)
    Another heap-user-after-free, this time we were missing a fix that had been applied to server.h but
    not config_validation/server.h (#4940). While working on this, attempted to make fully consistent and as
    simple/clear as possible the constraints on member ordering.
    
    This PR is in the tradition (!) of #5847, #4940, #4937. I think long-term we might want to think of
    more dynamic and explicit declaration of ordering constraints, it's evidently pretty fragile. Also,
    the lack of single-source-of-truth and duplication across prod and config server code bites again.
    
    Fixes oss-fuzz issue https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=13228.
    
    Risk level: Low
    Testing: Corpus entry added.
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- source/server/server.cc
+++ source/server/server.cc
@@ -52,11 +52,11 @@ InstanceImpl::InstanceImpl(const Options& options, Event::TimeSystem& time_syste
                            ComponentFactory& component_factory,
                            Runtime::RandomGeneratorPtr&& random_generator,
                            ThreadLocal::Instance& tls, Thread::ThreadFactory& thread_factory)
-    : shutdown_(false), options_(options), time_source_(time_system), restarter_(restarter),
+    : secret_manager_(std::make_unique<Secret::SecretManagerImpl>()), shutdown_(false),
+      options_(options), time_source_(time_system), restarter_(restarter),
       start_time_(time(nullptr)), original_start_time_(start_time_), stats_store_(store),
       thread_local_(tls),
       api_(new Api::Impl(options.fileFlushIntervalMsec(), thread_factory, store, time_system)),
-      secret_manager_(std::make_unique<Secret::SecretManagerImpl>()),
       dispatcher_(api_->allocateDispatcher()),
       singleton_manager_(new Singleton::ManagerImpl(api_->threadFactory().currentThreadId())),
       handler_(new ConnectionHandlerImpl(ENVOY_LOGGER(), *dispatcher_)),
