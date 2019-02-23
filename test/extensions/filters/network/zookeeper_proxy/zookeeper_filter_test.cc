98e689667ad9a873025b03500d617302b99bae15
Raul Gutierrez Segales <rgs@pinterest.com>
2019-02-23 14:27:10 -0800

    Move Buffer::OwnedImpl instead of returning pointers
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>

--- test/extensions/filters/network/zookeeper_proxy/zookeeper_filter_test.cc
+++ test/extensions/filters/network/zookeeper_proxy/zookeeper_filter_test.cc
@@ -25,104 +25,104 @@ public:
     filter_->initializeReadFilterCallbacks(filter_callbacks_);
   }
 
-  Buffer::OwnedImpl* encodeConnect(const bool readonly = false, const uint64_t zxid = 100,
-                                   const uint32_t session_timeout = 10,
-                                   const uint32_t session_id = 200,
-                                   const std::string& passwd = "") const {
-    Buffer::OwnedImpl* buffer = new Buffer::OwnedImpl();
+  Buffer::OwnedImpl encodeConnect(const bool readonly = false, const uint64_t zxid = 100,
+                                  const uint32_t session_timeout = 10,
+                                  const uint32_t session_id = 200,
+                                  const std::string& passwd = "") const {
+    Buffer::OwnedImpl buffer;
     const uint32_t message_size = readonly ? 28 + passwd.length() + 1 : 28 + passwd.length();
 
-    buffer->writeBEInt<uint32_t>(message_size);
-    buffer->writeBEInt<uint32_t>(0); // Protocol version.
-    buffer->writeBEInt<uint64_t>(zxid);
-    buffer->writeBEInt<uint32_t>(session_timeout);
-    buffer->writeBEInt<uint64_t>(session_id);
-    buffer->writeBEInt<uint32_t>(passwd.length());
-    buffer->add(passwd);
+    buffer.writeBEInt<uint32_t>(message_size);
+    buffer.writeBEInt<uint32_t>(0); // Protocol version.
+    buffer.writeBEInt<uint64_t>(zxid);
+    buffer.writeBEInt<uint32_t>(session_timeout);
+    buffer.writeBEInt<uint64_t>(session_id);
+    buffer.writeBEInt<uint32_t>(passwd.length());
+    buffer.add(passwd);
 
     if (readonly) {
       char readonly_flag = 0b1;
-      buffer->add(std::string(1, readonly_flag));
+      buffer.add(std::string(1, readonly_flag));
     }
 
     return buffer;
   }
 
-  Buffer::OwnedImpl* encodeBadMessage() const {
-    Buffer::OwnedImpl* buffer = new Buffer::OwnedImpl();
+  Buffer::OwnedImpl encodeBadMessage() const {
+    Buffer::OwnedImpl buffer;
 
     // Bad length.
-    buffer->writeBEInt<uint32_t>(1);
+    buffer.writeBEInt<uint32_t>(1);
     // Trailing int.
-    buffer->writeBEInt<uint32_t>(3);
+    buffer.writeBEInt<uint32_t>(3);
 
     return buffer;
   }
 
-  Buffer::OwnedImpl* encodePing() const {
-    Buffer::OwnedImpl* buffer = new Buffer::OwnedImpl();
+  Buffer::OwnedImpl encodePing() const {
+    Buffer::OwnedImpl buffer;
 
-    buffer->writeBEInt<uint32_t>(8);
-    buffer->writeBEInt<int32_t>(enumToInt(XidCodes::PING_XID));
+    buffer.writeBEInt<uint32_t>(8);
+    buffer.writeBEInt<int32_t>(enumToInt(XidCodes::PING_XID));
 
     return buffer;
   }
 
-  Buffer::OwnedImpl* encodeAuth(const std::string& scheme) const {
-    Buffer::OwnedImpl* buffer = new Buffer::OwnedImpl();
+  Buffer::OwnedImpl encodeAuth(const std::string& scheme) const {
+    Buffer::OwnedImpl buffer;
 
-    buffer->writeBEInt<uint32_t>(28 + scheme.length());
-    buffer->writeBEInt<int32_t>(enumToInt(XidCodes::AUTH_XID));
+    buffer.writeBEInt<uint32_t>(28 + scheme.length());
+    buffer.writeBEInt<int32_t>(enumToInt(XidCodes::AUTH_XID));
     // Opcode.
-    buffer->writeBEInt<int32_t>(enumToInt(OpCodes::SETAUTH));
+    buffer.writeBEInt<int32_t>(enumToInt(OpCodes::SETAUTH));
     // Type.
-    buffer->writeBEInt<int32_t>(0);
+    buffer.writeBEInt<int32_t>(0);
     // Scheme.
-    buffer->writeBEInt<uint32_t>(scheme.length());
-    buffer->add(scheme);
+    buffer.writeBEInt<uint32_t>(scheme.length());
+    buffer.add(scheme);
     // Credential.
-    buffer->writeBEInt<uint32_t>(6);
-    buffer->add("p@sswd");
+    buffer.writeBEInt<uint32_t>(6);
+    buffer.add("p@sswd");
 
     return buffer;
   }
 
-  Buffer::OwnedImpl* encodeGetData(const std::string& path, const bool watch,
-                                   const bool getchildren = false) const {
-    Buffer::OwnedImpl* buffer = new Buffer::OwnedImpl();
+  Buffer::OwnedImpl encodeGetData(const std::string& path, const bool watch,
+                                  const bool getchildren = false) const {
+    Buffer::OwnedImpl buffer;
 
-    buffer->writeBEInt<int32_t>(13 + path.length());
-    buffer->writeBEInt<int32_t>(1000);
+    buffer.writeBEInt<int32_t>(13 + path.length());
+    buffer.writeBEInt<int32_t>(1000);
     // Opcode.
     const int32_t opcode =
         getchildren ? enumToInt(OpCodes::GETCHILDREN) : enumToInt(OpCodes::GETDATA);
-    buffer->writeBEInt<int32_t>(opcode);
+    buffer.writeBEInt<int32_t>(opcode);
     // Path.
-    buffer->writeBEInt<int32_t>(path.length());
-    buffer->add(path);
+    buffer.writeBEInt<int32_t>(path.length());
+    buffer.add(path);
     // Watch.
     char watch_flag = watch ? 0b1 : 0b0;
-    buffer->add(std::string(1, watch_flag));
+    buffer.add(std::string(1, watch_flag));
 
     return buffer;
   }
 
-  Buffer::OwnedImpl* encodeCreateRequest(const std::string& path, const std::string& data,
-                                         const bool ephemeral, const bool sequence) const {
-    Buffer::OwnedImpl* buffer = new Buffer::OwnedImpl();
+  Buffer::OwnedImpl encodeCreateRequest(const std::string& path, const std::string& data,
+                                        const bool ephemeral, const bool sequence) const {
+    Buffer::OwnedImpl buffer;
 
-    buffer->writeBEInt<int32_t>(24 + path.length() + data.length());
-    buffer->writeBEInt<int32_t>(1000);
+    buffer.writeBEInt<int32_t>(24 + path.length() + data.length());
+    buffer.writeBEInt<int32_t>(1000);
     // Opcode.
-    buffer->writeBEInt<int32_t>(enumToInt(OpCodes::CREATE));
+    buffer.writeBEInt<int32_t>(enumToInt(OpCodes::CREATE));
     // Path.
-    buffer->writeBEInt<int32_t>(path.length());
-    buffer->add(path);
+    buffer.writeBEInt<int32_t>(path.length());
+    buffer.add(path);
     // Data.
-    buffer->writeBEInt<int32_t>(data.length());
-    buffer->add(data);
+    buffer.writeBEInt<int32_t>(data.length());
+    buffer.add(data);
     // Acls.
-    buffer->writeBEInt<int32_t>(0);
+    buffer.writeBEInt<int32_t>(0);
     // Flags.
     int flags = 0;
     if (ephemeral) {
@@ -131,27 +131,27 @@ public:
     if (sequence) {
       flags &= 0x2;
     }
-    buffer->writeBEInt<int32_t>(flags);
+    buffer.writeBEInt<int32_t>(flags);
 
     return buffer;
   }
 
-  Buffer::OwnedImpl* encodeSetRequest(const std::string& path, const std::string& data,
-                                      const int32_t version) const {
-    Buffer::OwnedImpl* buffer = new Buffer::OwnedImpl();
+  Buffer::OwnedImpl encodeSetRequest(const std::string& path, const std::string& data,
+                                     const int32_t version) const {
+    Buffer::OwnedImpl buffer;
 
-    buffer->writeBEInt<int32_t>(20 + path.length() + data.length());
-    buffer->writeBEInt<int32_t>(1000);
+    buffer.writeBEInt<int32_t>(20 + path.length() + data.length());
+    buffer.writeBEInt<int32_t>(1000);
     // Opcode.
-    buffer->writeBEInt<int32_t>(enumToInt(OpCodes::SETDATA));
+    buffer.writeBEInt<int32_t>(enumToInt(OpCodes::SETDATA));
     // Path.
-    buffer->writeBEInt<int32_t>(path.length());
-    buffer->add(path);
+    buffer.writeBEInt<int32_t>(path.length());
+    buffer.add(path);
     // Data.
-    buffer->writeBEInt<int32_t>(data.length());
-    buffer->add(data);
+    buffer.writeBEInt<int32_t>(data.length());
+    buffer.add(data);
     // Version.
-    buffer->writeBEInt<int32_t>(version);
+    buffer.writeBEInt<int32_t>(version);
 
     return buffer;
   }
@@ -167,7 +167,7 @@ public:
 TEST_F(ZooKeeperFilterTest, Connect) {
   initialize();
 
-  Buffer::InstancePtr data(encodeConnect());
+  Buffer::InstancePtr data(new Buffer::OwnedImpl(encodeConnect()));
 
   EXPECT_EQ(Envoy::Network::FilterStatus::Continue, filter_->onData(*data, false));
   EXPECT_EQ(1UL, config_->stats().connect_rq_.value());
@@ -178,7 +178,7 @@ TEST_F(ZooKeeperFilterTest, Connect) {
 TEST_F(ZooKeeperFilterTest, ConnectReadonly) {
   initialize();
 
-  Buffer::InstancePtr data(encodeConnect(true));
+  Buffer::InstancePtr data(new Buffer::OwnedImpl(encodeConnect(true)));
 
   EXPECT_EQ(Envoy::Network::FilterStatus::Continue, filter_->onData(*data, false));
   EXPECT_EQ(0UL, config_->stats().connect_rq_.value());
@@ -190,7 +190,7 @@ TEST_F(ZooKeeperFilterTest, ConnectReadonly) {
 TEST_F(ZooKeeperFilterTest, Fallback) {
   initialize();
 
-  Buffer::InstancePtr data(encodeBadMessage());
+  Buffer::InstancePtr data(new Buffer::OwnedImpl(encodeBadMessage()));
 
   EXPECT_EQ(Envoy::Network::FilterStatus::Continue, filter_->onData(*data, false));
   EXPECT_EQ(0UL, config_->stats().connect_rq_.value());
@@ -201,7 +201,7 @@ TEST_F(ZooKeeperFilterTest, Fallback) {
 TEST_F(ZooKeeperFilterTest, PingRequest) {
   initialize();
 
-  Buffer::InstancePtr data(encodePing());
+  Buffer::InstancePtr data(new Buffer::OwnedImpl(encodePing()));
 
   EXPECT_EQ(Envoy::Network::FilterStatus::Continue, filter_->onData(*data, false));
   EXPECT_EQ(1UL, config_->stats().ping_rq_.value());
@@ -211,7 +211,7 @@ TEST_F(ZooKeeperFilterTest, PingRequest) {
 TEST_F(ZooKeeperFilterTest, AuthRequest) {
   initialize();
 
-  Buffer::InstancePtr data(encodeAuth("digest"));
+  Buffer::InstancePtr data(new Buffer::OwnedImpl(encodeAuth("digest")));
 
   EXPECT_EQ(Envoy::Network::FilterStatus::Continue, filter_->onData(*data, false));
   EXPECT_EQ(scope_.counter("test.zookeeper.auth.digest_rq").value(), 1);
@@ -221,7 +221,7 @@ TEST_F(ZooKeeperFilterTest, AuthRequest) {
 TEST_F(ZooKeeperFilterTest, GetDataRequest) {
   initialize();
 
-  Buffer::InstancePtr data(encodeGetData("/foo", true));
+  Buffer::InstancePtr data(new Buffer::OwnedImpl(encodeGetData("/foo", true)));
 
   EXPECT_EQ(Envoy::Network::FilterStatus::Continue, filter_->onData(*data, false));
   EXPECT_EQ(1UL, config_->stats().getdata_rq_.value());
@@ -231,7 +231,7 @@ TEST_F(ZooKeeperFilterTest, GetDataRequest) {
 TEST_F(ZooKeeperFilterTest, CreateRequest) {
   initialize();
 
-  Buffer::InstancePtr data(encodeCreateRequest("/foo", "bar", false, false));
+  Buffer::InstancePtr data(new Buffer::OwnedImpl(encodeCreateRequest("/foo", "bar", false, false)));
 
   EXPECT_EQ(Envoy::Network::FilterStatus::Continue, filter_->onData(*data, false));
   EXPECT_EQ(1UL, config_->stats().create_rq_.value());
@@ -241,7 +241,7 @@ TEST_F(ZooKeeperFilterTest, CreateRequest) {
 TEST_F(ZooKeeperFilterTest, SetRequest) {
   initialize();
 
-  Buffer::InstancePtr data(encodeSetRequest("/foo", "bar", -1));
+  Buffer::InstancePtr data(new Buffer::OwnedImpl(encodeSetRequest("/foo", "bar", -1)));
 
   EXPECT_EQ(Envoy::Network::FilterStatus::Continue, filter_->onData(*data, false));
   EXPECT_EQ(1UL, config_->stats().setdata_rq_.value());
@@ -251,7 +251,7 @@ TEST_F(ZooKeeperFilterTest, SetRequest) {
 TEST_F(ZooKeeperFilterTest, GetChildrenRequest) {
   initialize();
 
-  Buffer::InstancePtr data(encodeGetData("/foo", false, true));
+  Buffer::InstancePtr data(new Buffer::OwnedImpl(encodeGetData("/foo", false, true)));
 
   EXPECT_EQ(Envoy::Network::FilterStatus::Continue, filter_->onData(*data, false));
   EXPECT_EQ(1UL, config_->stats().getchildren_rq_.value());
