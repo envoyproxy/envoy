cc3f123334fa92b3eca1c1c3f7fbc7e2d5f71efe
Nguyen Hung Phuong <phuongnh@vn.fujitsu.com>
2019-02-27 14:32:19 +0700

    Update bash script file because the link is not available
    Signed-off-by: Nguyen Hung Phuong <phuongnh@vn.fujitsu.com>

--- bazel/gen_compilation_database.sh
+++ bazel/gen_compilation_database.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "archive directory is not availabe on https://github.com/grailbio/bazel-compilation-database. Please check again!"
+exit 1
+
 RELEASE_VERSION=0.3.1
 
 if [[ ! -d bazel-compilation-database-${RELEASE_VERSION} ]]; then
