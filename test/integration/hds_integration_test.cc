6dc95ba43ca1e59a78ce32cd5b7a2b1c014fc175
Joshua Marantz <jmarantz@google.com>
2019-02-28 19:08:47 -0500

    Use simulated time in the hds_integration_test, and clean up some detritrus from an older sim-time integration mechanism.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/integration/hds_integration_test.cc
+++ test/integration/hds_integration_test.cc
@@ -16,6 +16,7 @@
 #include "test/config/utility.h"
 #include "test/integration/http_integration.h"
 #include "test/test_common/network_utility.h"
+#include "test/test_common/simulated_time_system.h"
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
@@ -23,11 +24,20 @@
 namespace Envoy {
 namespace {
 
+class TestUsingSimulatedTime {
+ public:
+  Event::SimulatedTimeSystem& simTime() { return sim_time_; }
+
+ private:
+  Event::SimulatedTimeSystem sim_time_;
+};
+
 class HdsIntegrationTest : public testing::TestWithParam<Network::Address::IpVersion>,
+                           public TestUsingSimulatedTime,
                            public HttpIntegrationTest {
 public:
   HdsIntegrationTest()
-      : HttpIntegrationTest(Http::CodecClient::Type::HTTP1, GetParam(), realTime()) {}
+      : HttpIntegrationTest(Http::CodecClient::Type::HTTP1, GetParam()) {}
 
   void createUpstreams() override {
     fake_upstreams_.emplace_back(
