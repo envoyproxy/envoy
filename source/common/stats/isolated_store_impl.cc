fd8db0ead8ece58beed45063561b9bd5671ce568
Joshua Marantz <jmarantz@google.com>
2019-03-03 16:53:17 -0500

    Use FakeSymbolTable in prod instances.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/common/stats/isolated_store_impl.cc
+++ source/common/stats/isolated_store_impl.cc
@@ -6,15 +6,15 @@
 #include <string>
 
 #include "common/common/utility.h"
+#include "common/stats/fake_symbol_table_impl.h"
 #include "common/stats/histogram_impl.h"
 #include "common/stats/scope_prefixer.h"
-#include "common/stats/symbol_table_impl.h"
 #include "common/stats/utility.h"
 
 namespace Envoy {
 namespace Stats {
 
-IsolatedStoreImpl::IsolatedStoreImpl() : IsolatedStoreImpl(std::make_unique<SymbolTableImpl>()) {}
+IsolatedStoreImpl::IsolatedStoreImpl() : IsolatedStoreImpl(std::make_unique<FakeSymbolTableImpl>()) {}
 
 IsolatedStoreImpl::IsolatedStoreImpl(std::unique_ptr<SymbolTable> symbol_table)
     : IsolatedStoreImpl(*symbol_table) {
