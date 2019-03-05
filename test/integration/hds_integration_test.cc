7ecdaaf5b7265fb7a70b7e08b0daa743ee0ba885
Joshua Marantz <jmarantz@google.com>
2019-03-05 14:42:07 -0500

    swtich the test to real time while flakes with sim-time are sorted out.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/integration/hds_integration_test.cc
+++ test/integration/hds_integration_test.cc
@@ -24,8 +24,9 @@
 namespace Envoy {
 namespace {
 
+// TODO(jmarantz): switch this to simulated-time after debugging flakes.
+
 class HdsIntegrationTest : public testing::TestWithParam<Network::Address::IpVersion>,
-                           public Event::TestUsingSimulatedTime,
                            public HttpIntegrationTest {
 public:
   HdsIntegrationTest() : HttpIntegrationTest(Http::CodecClient::Type::HTTP1, GetParam()) {}
