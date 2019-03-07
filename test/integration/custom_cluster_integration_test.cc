b06aca83c26ad4b2438616b5d40031277032afc4
Henry Yang <hyang@lyft.com>
2019-03-07 13:20:05 -0800

    remove CUSTOM_CLUSTER_CONFIG
    Signed-off-by: Henry Yang <hyang@lyft.com>

--- test/integration/custom_cluster_integration_test.cc
+++ test/integration/custom_cluster_integration_test.cc
@@ -12,69 +12,31 @@ namespace Envoy {
 namespace {
 
 const int UpstreamIndex = 0;
-const std::string CUSTOM_CLUSTER_CONFIG = R"EOF(
-admin:
-  access_log_path: /dev/null
-  address:
-    socket_address:
-      address: 127.0.0.1
-      port_value: 0
-static_resources:
-  clusters:
-    name: cluster_0
-    cluster_type:
-        name: envoy.clusters.custom_static
-        typed_config:
-          "@type": type.googleapis.com/test.integration.clusters.CustomStaticConfig
-          priority: 10
-          address: 127.0.0.1
-          port_value: 0
-  listeners:
-    name: listener_0
-    address:
-      socket_address:
-        address: 127.0.0.1
-        port_value: 0
-    filter_chains:
-      filters:
-        name: envoy.http_connection_manager
-        config:
-          stat_prefix: config_test
-          http_filters:
-            name: envoy.router
-          codec_type: HTTP1
-          route_config:
-            virtual_hosts:
-              name: integration
-              routes:
-                route:
-                  cluster: cluster_0
-                match:
-                  prefix: "/"
-              domains: "*"
-            name: route_config_0
-)EOF";
 
 // Integration test for cluster extension using CustomStaticCluster.
 class CustomClusterIntegrationTest : public testing::TestWithParam<Network::Address::IpVersion>,
                                      public HttpIntegrationTest {
 public:
   CustomClusterIntegrationTest()
-      : HttpIntegrationTest(Http::CodecClient::Type::HTTP1, GetParam(), realTime(),
-                            CUSTOM_CLUSTER_CONFIG) {}
+      : HttpIntegrationTest(Http::CodecClient::Type::HTTP1, GetParam(), realTime()) {}
 
   void initialize() override {
     setUpstreamCount(1);
-    // update the address and port in the cluster config
+    // change the configuration of the cluster_0 to a custom static cluster
     config_helper_.addConfigModifier([this](envoy::config::bootstrap::v2::Bootstrap& bootstrap) {
       auto* cluster_0 = bootstrap.mutable_static_resources()->mutable_clusters(0);
 
+      cluster_0->clear_hosts();
+
+      envoy::api::v2::Cluster_CustomClusterType cluster_type;
+      cluster_type.set_name("envoy.clusters.custom_static");
       test::integration::clusters::CustomStaticConfig config;
       config.set_priority(10);
       config.set_address(Network::Test::getLoopbackAddressString(ipVersion()));
       config.set_port_value(fake_upstreams_[UpstreamIndex]->localAddress()->ip()->port());
+      cluster_type.mutable_typed_config()->PackFrom(config);
 
-      cluster_0->mutable_cluster_type()->mutable_typed_config()->PackFrom(config);
+      cluster_0->mutable_cluster_type()->CopyFrom(cluster_type);
     });
     HttpIntegrationTest::initialize();
     test_server_->waitForGaugeGe("cluster_manager.active_clusters", 1);
