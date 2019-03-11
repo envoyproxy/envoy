c6405446ce950aedbc6c33567cedec8b95d2f754
Fred Douglas <fredlas@google.com>
2019-03-11 17:06:57 -0400

    consts
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- source/common/upstream/cds_api_impl.cc
+++ source/common/upstream/cds_api_impl.cc
@@ -30,10 +30,10 @@ CdsApiImpl::CdsApiImpl(const envoy::api::v2::core::ConfigSource& cds_config, Clu
     : cm_(cm), scope_(scope.createScope("cluster_manager.cds.")) {
   Config::Utility::checkLocalInfo("cds", local_info);
 
-  bool is_delta = (cds_config.api_config_source().api_type() ==
-                   envoy::api::v2::core::ApiConfigSource::DELTA_GRPC);
-  std::string grpc_method = is_delta ? "envoy.api.v2.ClusterDiscoveryService.DeltaClusters"
-                                     : "envoy.api.v2.ClusterDiscoveryService.StreamClusters";
+  const bool is_delta = (cds_config.api_config_source().api_type() ==
+                         envoy::api::v2::core::ApiConfigSource::DELTA_GRPC);
+  const std::string grpc_method = is_delta ? "envoy.api.v2.ClusterDiscoveryService.DeltaClusters"
+                                           : "envoy.api.v2.ClusterDiscoveryService.StreamClusters";
   subscription_ =
       Config::SubscriptionFactory::subscriptionFromConfigSource<envoy::api::v2::Cluster>(
           cds_config, local_info, dispatcher, cm, random, *scope_,
