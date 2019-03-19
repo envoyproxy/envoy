5f7ceeb74530034d711d8e3daff5a9ce73403dcf
Rama Chavali <rama.rao@salesforce.com>
2019-03-19 13:38:32 +0530

    fix eds test compilation
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- test/common/upstream/eds_test.cc
+++ test/common/upstream/eds_test.cc
@@ -63,7 +63,7 @@ protected:
         eds_cluster_.alt_stat_name().empty() ? eds_cluster_.name() : eds_cluster_.alt_stat_name()));
     Envoy::Server::Configuration::TransportSocketFactoryContextImpl factory_context(
         admin_, ssl_context_manager_, *scope, cm_, local_info_, dispatcher_, random_, stats_,
-        singleton_manager_, tls_, *api_, config_tracker_);
+        singleton_manager_, tls_, *api_);
     cluster_.reset(
         new EdsClusterImpl(eds_cluster_, runtime_, factory_context, std::move(scope), false));
     EXPECT_EQ(Cluster::InitializePhase::Secondary, cluster_->initializePhase());
