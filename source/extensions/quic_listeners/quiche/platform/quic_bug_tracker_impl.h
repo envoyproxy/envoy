f9f7cde414ea4c97a046b7a37dc66c26065c146b
Bin Wu <wub@google.com>
2019-03-25 11:32:03 -0400

    Add a TODO for QUIC_BUG to implement exponential back off.
    Signed-off-by: Bin Wu <wub@google.com>

--- source/extensions/quic_listeners/quiche/platform/quic_bug_tracker_impl.h
+++ source/extensions/quic_listeners/quiche/platform/quic_bug_tracker_impl.h
@@ -8,6 +8,8 @@
 
 #include "extensions/quic_listeners/quiche/platform/quic_logging_impl.h"
 
+// TODO(wub): Implement exponential back off to avoid performance problems due
+// to excessive QUIC_BUG.
 #define QUIC_BUG_IMPL QUIC_LOG_IMPL(DFATAL)
 #define QUIC_BUG_IF_IMPL(condition) QUIC_LOG_IF_IMPL(DFATAL, condition)
 #define QUIC_PEER_BUG_IMPL QUIC_LOG_IMPL(ERROR)
