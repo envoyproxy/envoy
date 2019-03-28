5d734e697e830e0e2dfec62b93ad2826ff07d003
Derek Argueta <dereka@pinterest.com>
2019-03-27 17:45:49 -0700

    remove route action header manipulation
    Signed-off-by: Derek Argueta <dereka@pinterest.com>

--- source/common/router/config_impl.cc
+++ source/common/router/config_impl.cc
@@ -332,10 +332,6 @@ RouteEntryImplBase::RouteEntryImplBase(const VirtualHostImpl& vhost,
       priority_(ConfigUtility::parsePriority(route.route().priority())),
       total_cluster_weight_(
           PROTOBUF_GET_WRAPPED_OR_DEFAULT(route.route().weighted_clusters(), total_weight, 100UL)),
-      route_action_request_headers_parser_(
-          HeaderParser::configure(route.route().request_headers_to_add())),
-      route_action_response_headers_parser_(HeaderParser::configure(
-          route.route().response_headers_to_add(), route.route().response_headers_to_remove())),
       request_headers_parser_(HeaderParser::configure(route.request_headers_to_add(),
                                                       route.request_headers_to_remove())),
       response_headers_parser_(HeaderParser::configure(route.response_headers_to_add(),
@@ -452,10 +448,8 @@ const std::string& RouteEntryImplBase::clusterName() const { return cluster_name
 void RouteEntryImplBase::finalizeRequestHeaders(Http::HeaderMap& headers,
                                                 const StreamInfo::StreamInfo& stream_info,
                                                 bool insert_envoy_original_path) const {
-  // Append user-specified request headers in the following order: route-action-level headers,
-  // route-level headers, virtual host level headers and finally global connection manager level
-  // headers.
-  route_action_request_headers_parser_->evaluateHeaders(headers, stream_info);
+  // Append user-specified request headers in the following order: route-level headers, virtual
+  // host level headers and finally global connection manager level headers.
   request_headers_parser_->evaluateHeaders(headers, stream_info);
   vhost_.requestHeaderParser().evaluateHeaders(headers, stream_info);
   vhost_.globalRouteConfig().requestHeaderParser().evaluateHeaders(headers, stream_info);
@@ -471,10 +465,8 @@ void RouteEntryImplBase::finalizeRequestHeaders(Http::HeaderMap& headers,
 
 void RouteEntryImplBase::finalizeResponseHeaders(Http::HeaderMap& headers,
                                                  const StreamInfo::StreamInfo& stream_info) const {
-  // Append user-specified response headers in the following order: route-action-level headers,
-  // route-level headers, virtual host level headers and finally global connection manager level
-  // headers.
-  route_action_response_headers_parser_->evaluateHeaders(headers, stream_info);
+  // Append user-specified response headers in the following order: route-level headers, virtual
+  // host level headers and finally global connection manager level headers.
   response_headers_parser_->evaluateHeaders(headers, stream_info);
   vhost_.responseHeaderParser().evaluateHeaders(headers, stream_info);
   vhost_.globalRouteConfig().responseHeaderParser().evaluateHeaders(headers, stream_info);
