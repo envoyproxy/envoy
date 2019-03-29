cb4735367bb292b6d22978ac43ece12108ba3000
Fred Douglas <fredlas@google.com>
2019-03-29 10:50:31 -0400

    class level comment
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- include/envoy/config/xds_grpc_context.h
+++ include/envoy/config/xds_grpc_context.h
@@ -8,25 +8,27 @@
 namespace Envoy {
 namespace Config {
 
+/**
+ * Interface to an object that uses a Config::GrpcStream to carry out the xDS protocol.
+ */
 class XdsGrpcContext {
 public:
   virtual ~XdsGrpcContext() = default;
 
   /**
-   * For the gRPC stream handler to prompt the context to take appropriate action in response to the
+   * For the GrpcStream to prompt the context to take appropriate action in response to the
    * gRPC stream having been successfully established.
    */
   virtual void handleStreamEstablished() PURE;
 
   /**
-   * For the gRPC stream handler to prompt the context to take appropriate action in response to
+   * For the GrpcStream to prompt the context to take appropriate action in response to
    * failure to establish the gRPC stream.
    */
   virtual void handleEstablishmentFailure() PURE;
 
   /**
-   * For the gRPC stream handler to call when its rate limiting logic allows more requests to be
-   * sent.
+   * For the GrpcStream to call when its rate limiting logic allows more requests to be sent.
    */
   virtual void drainRequests() PURE;
 };
