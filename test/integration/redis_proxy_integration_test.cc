38b0de6b6e8c6d21973dfbaf5991ca55b0ec3cfb
Mitch Sukalski <mitch.sukalski@workday.com>
2019-04-01 17:04:27 -0700

    redis: basic integration test for redis_proxy (#6450)
    Signed-off-by: Mitch Sukalski <mitch.sukalski@workday.com>

--- test/integration/redis_proxy_integration_test.cc
+++ test/integration/redis_proxy_integration_test.cc
@@ -1,6 +1,7 @@
 #include "test/integration/redis_proxy_integration_test.h"
 
-#include "common/buffer/buffer_impl.h"
+#include <sstream>
+#include <vector>
 
 #include "extensions/filters/network/redis_proxy/command_splitter_impl.h"
 
@@ -10,32 +11,16 @@ namespace RedisCmdSplitter = Envoy::Extensions::NetworkFilters::RedisProxy::Comm
 namespace Envoy {
 namespace {
 
-void makeBulkStringArray(RedisCommon::RespValue& value, std::vector<std::string>& command_strings) {
-  std::vector<RedisCommon::RespValue> values(command_strings.size());
+std::string makeBulkStringArray(std::vector<std::string>&& command_strings) {
+  std::stringstream result;
+
+  result << "*" << command_strings.size() << "\r\n";
   for (uint64_t i = 0; i < command_strings.size(); i++) {
-    values[i].type(RedisCommon::RespType::BulkString);
-    values[i].asString() = command_strings[i];
+    result << "$" << command_strings[i].size() << "\r\n";
+    result << command_strings[i] << "\r\n";
   }
 
-  value.type(RedisCommon::RespType::Array);
-  value.asArray().swap(values);
-}
-
-std::string valueToString(const RedisCommon::RespValue& value) {
-  RedisCommon::EncoderImpl encoder;
-  Buffer::OwnedImpl temp_buffer;
-
-  encoder.encode(value, temp_buffer);
-  return temp_buffer.toString();
-}
-
-RedisCommon::RespValuePtr stringToValue(const std::string& data) {
-  TestDecoderCallbacks callbacks;
-  RedisCommon::DecoderImpl decoder(callbacks);
-  Buffer::OwnedImpl temp_buffer(data);
-
-  decoder.decode(temp_buffer);
-  return std::move(callbacks.decoded());
+  return result.str();
 }
 
 INSTANTIATE_TEST_SUITE_P(IpVersions, RedisProxyIntegrationTest,
@@ -51,33 +36,20 @@ TEST_P(RedisProxyIntegrationTest, SimpleRequestAndResponse) {
   initialize();
   IntegrationTcpClientPtr tcp_client = makeTcpConnection(lookupPort("redis_proxy"));
 
-  std::vector<std::string> get_command = {"get", "foo"};
-  RedisCommon::RespValue request;
-  makeBulkStringArray(request, get_command);
-  std::string client_to_proxy = valueToString(request);
+  std::string client_to_proxy = makeBulkStringArray({"get", "foo"});
   std::string proxy_to_server;
 
   ASSERT_TRUE(client_to_proxy.size() > 0);
   ASSERT_TRUE(client_to_proxy.find("get") != std::string::npos);
+  ASSERT_TRUE(client_to_proxy.find("foo") != std::string::npos);
   tcp_client->write(client_to_proxy);
 
   FakeRawConnectionPtr fake_upstream_connection;
   ASSERT_TRUE(fake_upstreams_[0]->waitForRawConnection(fake_upstream_connection));
   ASSERT_TRUE(fake_upstream_connection->waitForData(client_to_proxy.size(), &proxy_to_server));
+  ASSERT_EQ(client_to_proxy, proxy_to_server);
 
-  RedisCommon::RespValuePtr received_request(stringToValue(proxy_to_server));
-  ASSERT_TRUE(received_request.get() != nullptr);
-  ASSERT_TRUE(received_request->type() == RedisCommon::RespType::Array);
-  ASSERT_EQ(received_request->asArray().size(), request.asArray().size());
-  for (unsigned int i = 0; i < received_request->asArray().size(); i++) {
-    ASSERT_TRUE(received_request->asArray()[i].type() == RedisCommon::RespType::BulkString);
-    ASSERT_EQ(received_request->asArray()[i].asString(), request.asArray()[i].asString());
-  }
-
-  RedisCommon::RespValue response;
-  response.type(RedisCommon::RespType::BulkString);
-  response.asString() = "bar";
-  std::string server_to_proxy = valueToString(response);
+  std::string server_to_proxy = "$3\r\nbar\r\n"; // bulkstring reply of "bar"
 
   ASSERT_TRUE(fake_upstream_connection->write(server_to_proxy));
   tcp_client->waitForData(server_to_proxy);
@@ -91,19 +63,15 @@ TEST_P(RedisProxyIntegrationTest, InvalidRequest) {
   initialize();
   IntegrationTcpClientPtr tcp_client = makeTcpConnection(lookupPort("redis_proxy"));
 
-  std::vector<std::string> invalid_command = {"foo"};
-  RedisCommon::RespValue request;
-  makeBulkStringArray(request, invalid_command);
-  std::string client_to_proxy = valueToString(request);
+  std::string client_to_proxy = makeBulkStringArray({"foo"});
 
   ASSERT_TRUE(client_to_proxy.size() > 0);
   ASSERT_TRUE(client_to_proxy.find("foo") != std::string::npos);
   tcp_client->write(client_to_proxy);
 
-  RedisCommon::RespValue error_response;
-  error_response.type(RedisCommon::RespType::Error);
-  error_response.asString() = RedisCmdSplitter::Response::get().InvalidRequest;
-  std::string proxy_to_client = valueToString(error_response);
+  std::stringstream error_response;
+  error_response << "-" << RedisCmdSplitter::Response::get().InvalidRequest << "\r\n";
+  std::string proxy_to_client = error_response.str();
 
   tcp_client->waitForData(proxy_to_client);
   ASSERT_EQ(proxy_to_client, tcp_client->data());
--- test/integration/redis_proxy_integration_test.h
+++ test/integration/redis_proxy_integration_test.h
@@ -1,10 +1,5 @@
 #pragma once
 
-#include <string>
-#include <vector>
-
-#include "extensions/filters/network/common/redis/codec_impl.h"
-
 #include "test/integration/integration.h"
 
 #include "gtest/gtest.h"
@@ -22,21 +17,6 @@ public:
   }
 
   void initialize() override;
-};
-
-class TestDecoderCallbacks : public Extensions::NetworkFilters::Common::Redis::DecoderCallbacks {
-public:
-  TestDecoderCallbacks() = default;
-  ~TestDecoderCallbacks() = default;
-
-  void onRespValue(Extensions::NetworkFilters::Common::Redis::RespValuePtr&& value) {
-    decoded_ = std::move(value);
-  }
-
-  Extensions::NetworkFilters::Common::Redis::RespValuePtr& decoded() { return decoded_; }
-
-private:
-  Extensions::NetworkFilters::Common::Redis::RespValuePtr decoded_;
 };
 
 } // namespace
