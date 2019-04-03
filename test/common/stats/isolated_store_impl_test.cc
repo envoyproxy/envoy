4f086c35490991e39327f28d74855b4947de7ebe
Joshua Marantz <jmarantz@google.com>
2019-04-03 18:22:45 -0400

    Add tests for new counterFromStatName() methods.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/common/stats/isolated_store_impl_test.cc
+++ test/common/stats/isolated_store_impl_test.cc
@@ -13,7 +13,27 @@ namespace Stats {
 
 class StatsIsolatedStoreImplTest : public testing::Test {
 protected:
+  ~StatsIsolatedStoreImplTest() override { clearStorage(); }
+
+  void clearStorage() {
+    for (auto& stat_name_storage : stat_name_storage_) {
+      stat_name_storage.free(store_.symbolTable());
+    }
+    stat_name_storage_.clear();
+    EXPECT_EQ(0, store_.symbolTable().numSymbols());
+  }
+
+  StatName makeStatName(absl::string_view name) {
+    stat_name_storage_.emplace_back(makeStatStorage(name));
+    return stat_name_storage_.back().statName();
+  }
+
+  StatNameStorage makeStatStorage(absl::string_view name) {
+    return StatNameStorage(name, store_.symbolTable());
+  }
+
   IsolatedStoreImpl store_;
+  std::vector<StatNameStorage> stat_name_storage_;
 };
 
 TEST_F(StatsIsolatedStoreImplTest, All) {
@@ -50,6 +70,49 @@ TEST_F(StatsIsolatedStoreImplTest, All) {
 
   ScopePtr scope2 = scope1->createScope("foo.");
   EXPECT_EQ("scope1.foo.bar", scope2->counter("bar").name());
+
+  // Validate that we sanitize away bad characters in the stats prefix.
+  ScopePtr scope3 = scope1->createScope(std::string("foo:\0:.", 7));
+  EXPECT_EQ("scope1.foo___.bar", scope3->counter("bar").name());
+
+  EXPECT_EQ(4UL, store_.counters().size());
+  EXPECT_EQ(2UL, store_.gauges().size());
+}
+
+TEST_F(StatsIsolatedStoreImplTest, AllWithSymbolTable) {
+  ScopePtr scope1 = store_.createScope("scope1.");
+  Counter& c1 = store_.counterFromStatName(makeStatName("c1"));
+  Counter& c2 = scope1->counterFromStatName(makeStatName("c2"));
+  EXPECT_EQ("c1", c1.name());
+  EXPECT_EQ("scope1.c2", c2.name());
+  EXPECT_EQ("c1", c1.tagExtractedName());
+  EXPECT_EQ("scope1.c2", c2.tagExtractedName());
+  EXPECT_EQ(0, c1.tags().size());
+  EXPECT_EQ(0, c1.tags().size());
+
+  Gauge& g1 = store_.gaugeFromStatName(makeStatName("g1"));
+  Gauge& g2 = scope1->gaugeFromStatName(makeStatName("g2"));
+  EXPECT_EQ("g1", g1.name());
+  EXPECT_EQ("scope1.g2", g2.name());
+  EXPECT_EQ("g1", g1.tagExtractedName());
+  EXPECT_EQ("scope1.g2", g2.tagExtractedName());
+  EXPECT_EQ(0, g1.tags().size());
+  EXPECT_EQ(0, g1.tags().size());
+
+  Histogram& h1 = store_.histogramFromStatName(makeStatName("h1"));
+  Histogram& h2 = scope1->histogramFromStatName(makeStatName("h2"));
+  scope1->deliverHistogramToSinks(h2, 0);
+  EXPECT_EQ("h1", h1.name());
+  EXPECT_EQ("scope1.h2", h2.name());
+  EXPECT_EQ("h1", h1.tagExtractedName());
+  EXPECT_EQ("scope1.h2", h2.tagExtractedName());
+  EXPECT_EQ(0, h1.tags().size());
+  EXPECT_EQ(0, h2.tags().size());
+  h1.recordValue(200);
+  h2.recordValue(200);
+
+  ScopePtr scope2 = scope1->createScope("foo.");
+  EXPECT_EQ("scope1.foo.bar", scope2->counterFromStatName(makeStatName("bar")).name());
 
   // Validate that we sanitize away bad characters in the stats prefix.
   ScopePtr scope3 = scope1->createScope(std::string("foo:\0:.", 7));
