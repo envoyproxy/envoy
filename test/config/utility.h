93a419b844d4d3422ad035100f342f4e913f73bc
Mitch Sukalski <mitch.sukalski@workday.com>
2019-04-02 21:09:48 -0700

    redis: basic integration test for redis_proxy (#6450)
    Signed-off-by: Mitch Sukalski <mitch.sukalski@workday.com>

--- test/config/utility.h
+++ test/config/utility.h
@@ -72,8 +72,6 @@ public:
   // A basic configuration (admin port, cluster_0, one listener) with no network filters.
   static const std::string BASE_CONFIG;
 
-  // A basic configuration for redis server proxying.
-  static const std::string REDIS_PROXY_CONFIG;
   // A basic configuration for L4 proxying.
   static const std::string TCP_PROXY_CONFIG;
   // A basic configuration for L7 proxying.
