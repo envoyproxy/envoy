116b7ae5c044e8b948b1f6dd5307d36bb3e946f5
Dan Zhang <danzh@google.com>
2019-04-05 14:20:27 -0400

    move definitions to cc
    Signed-off-by: Dan Zhang <danzh@google.com>

---
+++ source/extensions/quic_listeners/quiche/platform/quic_mem_slice_impl.cc
@@ -0,0 +1,51 @@
+// NOLINT(namespace-envoy)
+//
+// This file is part of the QUICHE platform implementation, and is not to be
+// consumed or referenced directly by other Envoy code. It serves purely as a
+// porting layer for QUICHE.
+
+#include "extensions/quic_listeners/quiche/platform/quic_mem_slice_impl.h"
+
+#include "common/common/assert.h"
+
+namespace quic {
+
+Envoy::Buffer::BufferFragmentImpl*
+QuicMemSliceImpl::allocateBufferAndFragment(QuicBufferAllocator* allocator, size_t length) {
+  auto bundle = reinterpret_cast<BufferFragmentBundle*>(
+      allocator->New(sizeof(BufferFragmentBundle) + length));
+  Envoy::Buffer::BufferFragmentImpl& fragment = bundle->fragment_with_padding_.fragment_;
+  return new (&fragment) Envoy::Buffer::BufferFragmentImpl(
+      bundle->buffer_, length,
+      [allocator](const void*, size_t, const Envoy::Buffer::BufferFragmentImpl* frag) {
+        // Delete will deallocate the bundle of fragment and buffer.
+        allocator->Delete(const_cast<char*>(reinterpret_cast<const char*>(frag)));
+      });
+}
+
+QuicMemSliceImpl::QuicMemSliceImpl(QuicBufferAllocator* allocator, size_t length) {
+  Envoy::Buffer::BufferFragmentImpl* fragment = allocateBufferAndFragment(allocator, length);
+  single_slice_buffer_.addBufferFragment(*fragment);
+}
+
+QuicMemSliceImpl::QuicMemSliceImpl(Envoy::Buffer::Instance& buffer, size_t length) {
+  ASSERT(firstSliceLength(buffer) == length);
+  single_slice_buffer_.move(buffer, length);
+  ASSERT(single_slice_buffer_.getRawSlices(nullptr, 0) == 1);
+}
+
+const char* QuicMemSliceImpl::data() const {
+  Envoy::Buffer::RawSlice out;
+  uint64_t num_slices = single_slice_buffer_.getRawSlices(&out, 1);
+  ASSERT(num_slices <= 1);
+  return static_cast<const char*>(out.mem_);
+}
+
+size_t QuicMemSliceImpl::firstSliceLength(Envoy::Buffer::Instance& buffer) {
+  Envoy::Buffer::RawSlice slice;
+  uint64_t total_num = buffer.getRawSlices(&slice, 1);
+  ASSERT(total_num != 0);
+  return slice.len_;
+}
+
+} // namespace quic
