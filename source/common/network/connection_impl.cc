aebc8cfb1ed63a72fa8d6867c3dce94e92352ef9
Andres Guedez <aguedez@google.com>
2019-04-09 09:47:36 -0400

    Fix spelling mistake in comment.
    Signed-off-by: Andres Guedez <aguedez@google.com>

--- source/common/network/connection_impl.cc
+++ source/common/network/connection_impl.cc
@@ -139,7 +139,7 @@ void ConnectionImpl::close(ConnectionCloseType type) {
       ASSERT(!delayed_close_timeout_set || delayed_close_timer_ != nullptr);
       // Validate that the same close type is used when multiple close()s are issued. An edge case
       // (checked first below) is when the delayed close timeout is disabled; in that case, the
-      // state is set to CloseAterFlush even if the requested type is FluhWriteAndDelay.
+      // state is set to CloseAterFlush even if the requested type is FlushWriteAndDelay.
       ASSERT((!delayed_close_timeout_set && type == ConnectionCloseType::FlushWriteAndDelay &&
               delayed_close_state_ == DelayedCloseState::CloseAfterFlush) ||
              (type == ConnectionCloseType::FlushWrite &&
