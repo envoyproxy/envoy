adb04884d8673d9b388aa9922602ff57978d89c0
Andres Guedez <aguedez@google.com>
2019-04-10 13:40:44 -0400

    Clarify the accepted use of the 'type' argument for close().
    Signed-off-by: Andres Guedez <aguedez@google.com>

--- include/envoy/network/connection.h
+++ include/envoy/network/connection.h
@@ -114,6 +114,8 @@ public:
 
   /**
    * Close the connection.
+   * @param type the type of close that is being issued; once a close() is issued on a given
+   * connection, the same type must be used on any subsequent close()s on that connection.
    */
   virtual void close(ConnectionCloseType type) PURE;
 
