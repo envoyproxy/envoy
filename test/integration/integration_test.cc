e612fc9a29ac501928a0c827e156a4633877394e
Kyle Larose <eomereadig@gmail.com>
2019-04-11 15:10:47 -0400

    upstream: add connection pool circuit breaker stats (#6516)
    Users will want to know how often connection pool overflows are
    happening. Count them.
    
    Signed-off-by: Kyle Larose <kyle@agilicus.com>

--- test/integration/integration_test.cc
+++ test/integration/integration_test.cc
@@ -778,6 +778,8 @@ TEST_P(IntegrationTest, NoConnectionPoolsFree) {
 
   EXPECT_STREQ("503", response->headers().Status()->value().c_str());
   test_server_->waitForCounterGe("cluster.cluster_0.upstream_rq_503", 1);
+
+  EXPECT_EQ(test_server_->counter("cluster.cluster_0.upstream_cx_pool_overflow")->value(), 1);
 }
 
 INSTANTIATE_TEST_SUITE_P(IpVersions, UpstreamEndpointIntegrationTest,
