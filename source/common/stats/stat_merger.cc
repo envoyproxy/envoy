c883fb5a150f5e7a4b089dd9380e3dcc93e64812
Fred Douglas <fredlas@google.com>
2019-04-18 13:17:01 -0400

    add comments to merge logic exceptions
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- source/common/stats/stat_merger.cc
+++ source/common/stats/stat_merger.cc
@@ -20,11 +20,17 @@ void StatMerger::mergeStats(const Protobuf::Map<std::string, uint64_t>& counters
 
   // Gauge name *substrings*, and special logic to use for combining those gauges' values.
   static const std::vector<std::pair<std::regex, CombineLogic>> combine_logic_exceptions{
+      // Any .version is either a static property of the binary, or an opaque identifier for
+      // resources that are not passed across hot restart.
       {std::regex(".*\\.version$"), CombineLogic::NoImport},
+      // Once the child is up and reporting stats, its own control plane state and liveness is what
+      // we're interested in.
       {std::regex(".*\\.control_plane.connected_state$"), CombineLogic::NoImport},
       {std::regex("^server.live$"), CombineLogic::NoImport},
-      {std::regex("^runtime.admin_overrides_active$"), CombineLogic::NoImport},
-      {std::regex("^runtime.num_keys$"), CombineLogic::NoImport},
+      // Properties that should reasonably have some continuity across hot restart. The parent's
+      // last value should be a relatively accurate starting point, and then the child can update
+      // from there when appropriate. (All of these exceptional stats used with set() rather than
+      // add()/sub(), so the child's new value will in fact overwrite.)
       {std::regex("^cluster_manager.active_clusters$"), CombineLogic::OnlyImportWhenUnused},
       {std::regex("^cluster_manager.warming_clusters$"), CombineLogic::OnlyImportWhenUnused},
       {std::regex("^cluster\\..*\\.membership_total$"), CombineLogic::OnlyImportWhenUnused},
@@ -32,13 +38,17 @@ void StatMerger::mergeStats(const Protobuf::Map<std::string, uint64_t>& counters
       {std::regex("^cluster\\..*\\.membership_degraded$"), CombineLogic::OnlyImportWhenUnused},
       {std::regex("^cluster\\..*\\.max_host_weight$"), CombineLogic::OnlyImportWhenUnused},
       {std::regex(".*\\.total_principals$"), CombineLogic::OnlyImportWhenUnused},
-      {std::regex("^listener_manager.total_listeners_draining$"), CombineLogic::NoImport},
-      {std::regex("^listener_manager.total_listeners_warming$"),
-       CombineLogic::OnlyImportWhenUnused},
       {std::regex("^listener_manager.total_listeners_active$"), CombineLogic::OnlyImportWhenUnused},
       {std::regex("^overload\\..*\\.pressure$"), CombineLogic::OnlyImportWhenUnused},
-      {std::regex("^server.concurrency$"), CombineLogic::OnlyImportWhenUnused},
+      // Due to the fd passing, the parent's view of whether its listeners are in transitive states
+      // is not useful.
+      {std::regex("^listener_manager.total_listeners_draining$"), CombineLogic::NoImport},
+      {std::regex("^listener_manager.total_listeners_warming$"), CombineLogic::NoImport},
+      // Static properties known at startup.
+      {std::regex("^server.concurrency$"), CombineLogic::NoImport},
       {std::regex("^server.hot_restart_epoch$"), CombineLogic::NoImport},
+      {std::regex("^runtime.admin_overrides_active$"), CombineLogic::NoImport},
+      {std::regex("^runtime.num_keys$"), CombineLogic::NoImport},
   };
   for (const auto& gauge : gauges) {
     uint64_t new_parent_value = gauge.second;
