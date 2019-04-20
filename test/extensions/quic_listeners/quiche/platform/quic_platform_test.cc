32e4d286668731594eb5c81ed664bd144d8d2d88
Andres Guedez <34292400+AndresGuedez@users.noreply.github.com>
2019-04-20 16:31:01 -0400

    thread: remove ThreadFactorySingleton (#6658)
    It is no longer needed since Api::Api is plumbed ubiquitiously throughout Envoy's core.
    
    The only user of the factory, QuicThreadImpl, has been modified to take the
    Envoy::Thread::ThreadFactory via QuicThreadImpl::setThreadFactory().
    
    Signed-off-by: Andres Guedez <aguedez@google.com>

--- test/extensions/quic_listeners/quiche/platform/quic_platform_test.cc
+++ test/extensions/quic_listeners/quiche/platform/quic_platform_test.cc
@@ -12,6 +12,8 @@
 #include "common/memory/stats.h"
 #include "common/network/utility.h"
 
+#include "exe/platform_impl.h"
+
 #include "test/common/stats/stat_test_utility.h"
 #include "test/extensions/transport_sockets/tls/ssl_test_utility.h"
 #include "test/mocks/api/mocks.h"
@@ -252,10 +254,14 @@ TEST_F(QuicPlatformTest, QuicStringPiece) {
 }
 
 TEST_F(QuicPlatformTest, QuicThread) {
+  Envoy::PlatformImpl platform_impl;
+
   class AdderThread : public QuicThread {
   public:
-    AdderThread(int* value, int increment)
-        : QuicThread("adder_thread"), value_(value), increment_(increment) {}
+    AdderThread(int* value, int increment, Envoy::Thread::ThreadFactory& thread_factory)
+        : QuicThread("adder_thread"), value_(value), increment_(increment) {
+      setThreadFactory(thread_factory);
+    }
 
     ~AdderThread() override = default;
 
@@ -270,19 +276,19 @@ TEST_F(QuicPlatformTest, QuicThread) {
   int value = 0;
 
   // A QuicThread that is never started, which is ok.
-  { AdderThread t0(&value, 1); }
+  { AdderThread t0(&value, 1, platform_impl.threadFactory()); }
   EXPECT_EQ(0, value);
 
   // A QuicThread that is started and joined as usual.
   {
-    AdderThread t1(&value, 1);
+    AdderThread t1(&value, 1, platform_impl.threadFactory());
     t1.Start();
     t1.Join();
   }
   EXPECT_EQ(1, value);
 
   // QuicThread will panic if it's started but not joined.
-  EXPECT_DEATH_LOG_TO_STDERR({ AdderThread(&value, 2).Start(); },
+  EXPECT_DEATH_LOG_TO_STDERR({ AdderThread(&value, 2, platform_impl.threadFactory()).Start(); },
                              "QuicThread should be joined before destruction");
 }
 
