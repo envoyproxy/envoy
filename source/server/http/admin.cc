bc2c8dba10a12386b73e4cbcaa7a8e2ccdbe215f
Jack Kleeman <jack@monzo.com>
2019-04-23 16:37:41 +0100

    Fix for rebase, use Uninitialized instead of NotInitialized
    Signed-off-by: Jack Kleeman <jack@monzo.com>

--- source/server/http/admin.cc
+++ source/server/http/admin.cc
@@ -666,7 +666,7 @@ Http::Code AdminImpl::handlerReadyz(absl::string_view, Http::HeaderMap&,
   envoy::admin::v2alpha::ServerInfo::State state;
 
   switch (server_.initManager().state()) {
-  case Init::Manager::State::NotInitialized:
+  case Init::Manager::State::Uninitialized:
     state = envoy::admin::v2alpha::ServerInfo::PRE_INITIALIZING;
     break;
   case Init::Manager::State::Initializing:
