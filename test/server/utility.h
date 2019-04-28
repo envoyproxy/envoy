a809849d18c16d98988195075f248843b89142ac
Derek Argueta <dereka@pinterest.com>
2019-04-28 00:44:39 -0700

    update listener configs for tests
    Signed-off-by: Derek Argueta <dereka@pinterest.com>

--- test/server/utility.h
+++ test/server/utility.h
@@ -9,14 +9,6 @@ namespace Envoy {
 namespace Server {
 namespace {
 
-inline envoy::api::v2::Listener parseListenerFromJson(const std::string& json_string) {
-  envoy::api::v2::Listener listener;
-  auto json_object_ptr = Json::Factory::loadFromString(json_string);
-  Stats::StatsOptionsImpl stats_options;
-  Config::LdsJson::translateListener(*json_object_ptr, listener, stats_options);
-  return listener;
-}
-
 inline envoy::api::v2::Listener parseListenerFromV2Yaml(const std::string& yaml) {
   envoy::api::v2::Listener listener;
   MessageUtil::loadFromYaml(yaml, listener);
