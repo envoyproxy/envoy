27b7d3f549dbec1ccad43f88fb1f79fc408f0c33
Fred Douglas <fredlas@google.com>
2019-04-29 18:57:01 -0400

    hot restart parent tests
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- source/server/hot_restarting_parent.cc
+++ source/server/hot_restarting_parent.cc
@@ -24,14 +24,14 @@ void HotRestartingParent::initialize(Event::Dispatcher& dispatcher, Server::Inst
         onSocketEvent();
       },
       Event::FileTriggerType::Edge, Event::FileReadyType::Read);
-  server_ = &server;
+  internal_ = std::make_unique<Internal>(&server);
 }
 
 void HotRestartingParent::onSocketEvent() {
   std::unique_ptr<HotRestartMessage> wrapped_request;
   while ((wrapped_request = receiveHotRestartMessage(Blocking::No))) {
     if (wrapped_request->requestreply_case() == HotRestartMessage::kReply) {
-      ENVOY_LOG(error, "child sent us a reply type HotRestartMessage; ignoring.");
+      ENVOY_LOG(error, "child sent us a HotRestartMessage reply (we want requests); ignoring.");
       HotRestartMessage wrapped_reply;
       wrapped_reply.set_didnt_recognize_your_last_message(true);
       sendHotRestartMessage(child_address_, wrapped_reply);
@@ -39,39 +39,25 @@ void HotRestartingParent::onSocketEvent() {
     }
     switch (wrapped_request->request().request_case()) {
     case HotRestartMessage::Request::kShutdownAdmin: {
-      server_->shutdownAdmin();
-      HotRestartMessage wrapped_reply;
-      wrapped_reply.mutable_reply()->mutable_shutdown_admin()->set_original_start_time_unix_seconds(
-          server_->startTimeFirstEpoch());
-      sendHotRestartMessage(child_address_, wrapped_reply);
+      sendHotRestartMessage(child_address_, internal_->shutdownAdmin());
       break;
     }
 
     case HotRestartMessage::Request::kPassListenSocket: {
-      HotRestartMessage wrapped_reply;
-      wrapped_reply.mutable_reply()->mutable_pass_listen_socket()->set_fd(-1);
-      Network::Address::InstanceConstSharedPtr addr =
-          Network::Utility::resolveUrl(wrapped_request->request().pass_listen_socket().address());
-      for (const auto& listener : server_->listenerManager().listeners()) {
-        if (*listener.get().socket().localAddress() == *addr) {
-          wrapped_reply.mutable_reply()->mutable_pass_listen_socket()->set_fd(
-              listener.get().socket().ioHandle().fd());
-          break;
-        }
-      }
-      sendHotRestartMessage(child_address_, wrapped_reply);
+      sendHotRestartMessage(child_address_,
+                            internal_->getListenSocketsForChild(wrapped_request->request()));
       break;
     }
 
     case HotRestartMessage::Request::kStats: {
       HotRestartMessage wrapped_reply;
-      exportStatsToChild(wrapped_reply.mutable_reply()->mutable_stats());
+      internal_->exportStatsToChild(wrapped_reply.mutable_reply()->mutable_stats());
       sendHotRestartMessage(child_address_, wrapped_reply);
       break;
     }
 
     case HotRestartMessage::Request::kDrainListeners: {
-      server_->drainListeners();
+      internal_->drainListeners();
       break;
     }
 
@@ -82,6 +68,7 @@ void HotRestartingParent::onSocketEvent() {
     }
 
     default: {
+      ENVOY_LOG(error, "child sent us an unfamiliar type of HotRestartMessage; ignoring.");
       HotRestartMessage wrapped_reply;
       wrapped_reply.set_didnt_recognize_your_last_message(true);
       sendHotRestartMessage(child_address_, wrapped_reply);
@@ -91,18 +78,44 @@ void HotRestartingParent::onSocketEvent() {
   }
 }
 
+void HotRestartingParent::shutdown() { socket_event_.reset(); }
+
+HotRestartMessage HotRestartingParent::Internal::shutdownAdmin() {
+  server_->shutdownAdmin();
+  HotRestartMessage wrapped_reply;
+  wrapped_reply.mutable_reply()->mutable_shutdown_admin()->set_original_start_time_unix_seconds(
+      server_->startTimeFirstEpoch());
+  return wrapped_reply;
+}
+
+HotRestartMessage
+HotRestartingParent::Internal::getListenSocketsForChild(const HotRestartMessage::Request& request) {
+  HotRestartMessage wrapped_reply;
+  wrapped_reply.mutable_reply()->mutable_pass_listen_socket()->set_fd(-1);
+  Network::Address::InstanceConstSharedPtr addr =
+      Network::Utility::resolveUrl(request.pass_listen_socket().address());
+  for (const auto& listener : server_->listenerManager().listeners()) {
+    if (*listener.get().socket().localAddress() == *addr) {
+      wrapped_reply.mutable_reply()->mutable_pass_listen_socket()->set_fd(
+          listener.get().socket().ioHandle().fd());
+      break;
+    }
+  }
+  return wrapped_reply;
+}
+
 // TODO(fredlas) if there are enough stats for stat name length to become an issue, this current
 // implementation can negate the benefit of symbolized stat names by periodically reaching the
 // magnitude of memory usage that they are meant to avoid, since this map holds full-string
 // names. The problem can be solved by splitting the export up over many chunks.
-void HotRestartingParent::exportStatsToChild(HotRestartMessage::Reply::Stats* stats) {
+void HotRestartingParent::Internal::exportStatsToChild(HotRestartMessage::Reply::Stats* stats) {
   for (const auto& gauge : server_->stats().gauges()) {
     (*stats->mutable_gauges())[gauge->name()] = gauge->value();
   }
   for (const auto& counter : server_->stats().counters()) {
     // The hot restart parent is expected to have stopped its normal stat exporting (and so
     // latching) by the time it begins exporting to the hot restart child.
-    int latched_value = counter->latch();
+    uint64_t latched_value = counter->latch();
     if (latched_value > 0) {
       (*stats->mutable_counter_deltas())[counter->name()] = latched_value;
     }
@@ -111,7 +124,7 @@ void HotRestartingParent::exportStatsToChild(HotRestartMessage::Reply::Stats* st
   stats->set_num_connections(server_->listenerManager().numConnections());
 }
 
-void HotRestartingParent::shutdown() { socket_event_.reset(); }
+void HotRestartingParent::Internal::drainListeners() { server_->drainListeners(); }
 
 } // namespace Server
 } // namespace Envoy
--- source/server/hot_restarting_parent.h
+++ source/server/hot_restarting_parent.h
@@ -9,6 +9,8 @@ namespace Server {
 
 /**
  * The parent half of hot restarting. Listens for requests and commands from the child.
+ * This outer class only handles evented socket I/O. The actual hot restart logic lives in
+ * HotRestartingParent::Internal.
  */
 class HotRestartingParent : HotRestartingBase, Logger::Loggable<Logger::Id::main> {
 public:
@@ -16,15 +18,31 @@ public:
   void initialize(Event::Dispatcher& dispatcher, Server::Instance& server);
   void shutdown();
 
+  // The hot restarting parent's hot restart logic. Each function is meant to be called to fulfill a
+  // request from the child for that action.
+  class Internal {
+  public:
+    explicit Internal(Server::Instance* server) : server_(server) {}
+    // Return value is the response to return to the child.
+    envoy::HotRestartMessage shutdownAdmin();
+    // Return value is the response to return to the child.
+    envoy::HotRestartMessage
+    getListenSocketsForChild(const envoy::HotRestartMessage::Request& request);
+    // 'stats' is a field in the reply protobuf to be sent to the child, which we should populate.
+    void exportStatsToChild(envoy::HotRestartMessage::Reply::Stats* stats);
+    void drainListeners();
+
+  private:
+    Server::Instance* const server_{};
+  };
+
 private:
   void onSocketEvent();
-  // Export all stats, to be given to our hot restart child.
-  void exportStatsToChild(envoy::HotRestartMessage::Reply::Stats* stats);
 
   const int restart_epoch_;
   sockaddr_un child_address_;
   Event::FileEventPtr socket_event_;
-  Server::Instance* server_{};
+  std::unique_ptr<Internal> internal_;
 };
 
 } // namespace Server
