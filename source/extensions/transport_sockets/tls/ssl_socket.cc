7f0e0bc7843bb507b5ff213747eb64da43306b59
Ismo Puustinen <ismo.puustinen@intel.com>
2019-04-30 14:44:05 +0300

    tls: make a failed private key method association fatal.
    Signed-off-by: Ismo Puustinen <ismo.puustinen@intel.com>

--- source/extensions/transport_sockets/tls/ssl_socket.cc
+++ source/extensions/transport_sockets/tls/ssl_socket.cc
@@ -67,13 +67,14 @@ void SslSocket::setTransportSocketCallbacks(Network::TransportSocketCallbacks& c
   for (auto const& provider : providers) {
     Ssl::PrivateKeyConnectionPtr pk_connection =
         provider->getPrivateKeyConnection(ssl_.get(), *this, callbacks_->connection().dispatcher());
-    if (pk_connection) {
-      // We keep track of the private key operations for memory management purposes (the operations
-      // object if destroyed when the SslSocket is destroyed). The operations objects are unique
-      // because they need to associated with the SSL objects so that user data can be passed to the
-      // BoringSSL private key methods.
-      pk_connections_.emplace_back(std::move(pk_connection));
-    }
+    // If a private key method provider has been assigned for this certificate, it's a fatal
+    // error if the connection can't be associated with the method.
+    ASSERT(pk_connection);
+    // We keep track of the private key operations for memory management purposes (the operations
+    // object if destroyed when the SslSocket is destroyed). The operations objects are unique
+    // because they need to associated with the SSL objects so that user data can be passed to the
+    // BoringSSL private key methods.
+    pk_connections_.emplace_back(std::move(pk_connection));
   }
 
   BIO* bio = BIO_new_socket(callbacks_->ioHandle().fd(), 0);
