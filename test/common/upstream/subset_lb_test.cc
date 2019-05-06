8a9153aea86403fff7d9f38ba6c5187e0bd59776
Snow Pettersen <snowp@squareup.com>
2019-05-06 19:46:41 -0400

    fix more updateHostsParams usages
    Signed-off-by: Snow Pettersen <snowp@squareup.com>

--- test/common/upstream/subset_lb_test.cc
+++ test/common/upstream/subset_lb_test.cc
@@ -317,9 +317,9 @@ public:
     if (GetParam() == REMOVES_FIRST && !remove.empty()) {
       local_priority_set_.updateHosts(
           0,
-          HostSetImpl::updateHostsParams(local_hosts_, local_hosts_per_locality_,
-                                         std::make_shared<HealthyHostVector>(*local_hosts_),
-                                         local_hosts_per_locality_),
+          updateHostsParams(local_hosts_, local_hosts_per_locality_,
+                            std::make_shared<HealthyHostVector>(*local_hosts_),
+                            local_hosts_per_locality_),
           {}, {}, remove, absl::nullopt);
     }
 
@@ -334,17 +334,17 @@ public:
       if (!add.empty()) {
         local_priority_set_.updateHosts(
             0,
-            HostSetImpl::updateHostsParams(local_hosts_, local_hosts_per_locality_,
-                                           std::make_shared<HealthyHostVector>(*local_hosts_),
-                                           local_hosts_per_locality_),
+            updateHostsParams(local_hosts_, local_hosts_per_locality_,
+                              std::make_shared<HealthyHostVector>(*local_hosts_),
+                              local_hosts_per_locality_),
             {}, add, {}, absl::nullopt);
       }
     } else if (!add.empty() || !remove.empty()) {
       local_priority_set_.updateHosts(
           0,
-          HostSetImpl::updateHostsParams(local_hosts_, local_hosts_per_locality_,
-                                         std::make_shared<const HealthyHostVector>(*local_hosts_),
-                                         local_hosts_per_locality_),
+          updateHostsParams(local_hosts_, local_hosts_per_locality_,
+                            std::make_shared<const HealthyHostVector>(*local_hosts_),
+                            local_hosts_per_locality_),
           {}, add, remove, absl::nullopt);
     }
   }
