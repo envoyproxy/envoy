66c6c3e8542dba7ccd1667a4b0f004425d3b9be4
Harvey Tuch <htuch@google.com>
2019-05-08 15:48:31 -0400

    My own personal spelling bee.
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- source/exe/process_wide.cc
+++ source/exe/process_wide.cc
@@ -17,11 +17,11 @@ namespace {
 // Static variable to count initialization pairs. For tests like
 // main_common_test, we need to count to avoid double initialization or
 // shutdown.
-uint32_t process_wide_initalized;
+uint32_t process_wide_initialized;
 } // namespace
 
-ProcessWide::ProcessWide() : initialization_depth_(process_wide_initalized) {
-  if (process_wide_initalized++ == 0) {
+ProcessWide::ProcessWide() : initialization_depth_(process_wide_initialized) {
+  if (process_wide_initialized++ == 0) {
 #ifdef ENVOY_GOOGLE_GRPC
     grpc_init();
 #endif
@@ -33,15 +33,15 @@ ProcessWide::ProcessWide() : initialization_depth_(process_wide_initalized) {
 }
 
 ProcessWide::~ProcessWide() {
-  ASSERT(process_wide_initalized > 0);
-  if (--process_wide_initalized == 0) {
-    process_wide_initalized = false;
+  ASSERT(process_wide_initialized > 0);
+  if (--process_wide_initialized == 0) {
+    process_wide_initialized = false;
     ares_library_cleanup();
 #ifdef ENVOY_GOOGLE_GRPC
     grpc_shutdown();
 #endif
   }
-  ASSERT(process_wide_initalized == initialization_depth_);
+  ASSERT(process_wide_initialized == initialization_depth_);
 }
 
 } // namespace Envoy
