7343fa59ae6d15bad7ce6409ed7d56ddc00dd678
Lizan Zhou <lizan@tetrate.io>
2019-05-11 00:34:33 +0000

    use grpc cache
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/setup_gcs_cache.sh
+++ ci/setup_gcs_cache.sh
@@ -18,13 +18,13 @@ if [[ ! -z "${BAZEL_REMOTE_CACHE}" ]]; then
     echo "${GCP_SERVICE_ACCOUNT_KEY}" | base64 --decode > "${GCP_SERVICE_ACCOUNT_KEY_FILE}"
 
     export BAZEL_BUILD_EXTRA_OPTIONS="${BAZEL_BUILD_EXTRA_OPTIONS} \
-      --remote_http_cache=${BAZEL_REMOTE_CACHE} \
+      --remote_cache=${BAZEL_REMOTE_CACHE} \
       --google_credentials=${GCP_SERVICE_ACCOUNT_KEY_FILE}"
-    echo "Set up bazel read/write HTTP cache at ${BAZEL_REMOTE_CACHE}."
+    echo "Set up bazel read/write cache at ${BAZEL_REMOTE_CACHE}."
   else
     export BAZEL_BUILD_EXTRA_OPTIONS="${BAZEL_BUILD_EXTRA_OPTIONS} \
-      --remote_http_cache=${BAZEL_REMOTE_CACHE} --noremote_upload_local_results"
-    echo "Set up bazel read only HTTP cache at ${BAZEL_REMOTE_CACHE}."
+      --remote_cache=${BAZEL_REMOTE_CACHE} --noremote_upload_local_results"
+    echo "Set up bazel read only cache at ${BAZEL_REMOTE_CACHE}."
   fi
 else
   echo "No remote cache bucket is set, skipping setup remote cache."
