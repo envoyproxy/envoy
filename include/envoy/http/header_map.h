64f9d6028267def4a7e44f33e19038ba14168d45
Dan Noé <dpn@google.com>
2019-05-14 10:43:25 -0400

    Improve the headermap fuzz test
    It is invariant that the input into the headermap code should not
    contain certain reserved characters, and this is enforced by assertion.
    The fuzzer would frequently hang up on these and just fail the valid()
    check, often just when constructing a LowerCaseString before doing
    anything interesting.
    
    To get past this, filter the reserved characters to spaces. The fuzzer
    ran for 30 minutes with this change.
    
    Signed-off-by: Dan Noé <dpn@google.com>

--- include/envoy/http/header_map.h
+++ include/envoy/http/header_map.h
@@ -23,6 +23,8 @@ namespace Http {
 // Used by ASSERTs to validate internal consistency. E.g. valid HTTP header keys/values should
 // never contain embedded NULLs.
 static inline bool validHeaderString(absl::string_view s) {
+  // If you modify this list of illegal embedded characters you will probably
+  // want to change header_map_fuzz_impl_test at the same time.
   for (const char c : {'\0', '\r', '\n'}) {
     if (s.find(c) != absl::string_view::npos) {
       return false;
