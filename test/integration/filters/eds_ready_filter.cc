e369e3025f09aeb16a6710fa62d5b496ebe882d2
Ashley Hedberg <ahedberg@google.com>
2019-05-16 09:38:30 -0400

    Use five-arg version of sendLocalReply to appease MacOS
    Signed-off-by: Ashley Hedberg <ahedberg@google.com>

--- test/integration/filters/eds_ready_filter.cc
+++ test/integration/filters/eds_ready_filter.cc
@@ -26,16 +26,16 @@ public:
         root_scope_.findGauge(stat_name_.statName());
     if (!gauge.has_value()) {
       decoder_callbacks_->sendLocalReply(Envoy::Http::Code::InternalServerError,
-                                         "Couldn't find stat", nullptr, absl::nullopt);
+                                         "Couldn't find stat", nullptr, absl::nullopt, "");
       return Http::FilterHeadersStatus::StopIteration;
     }
     if (gauge->get().value() == 0) {
       decoder_callbacks_->sendLocalReply(Envoy::Http::Code::InternalServerError, "EDS not ready",
-                                         nullptr, absl::nullopt);
+                                         nullptr, absl::nullopt, "");
       return Http::FilterHeadersStatus::StopIteration;
     }
     decoder_callbacks_->sendLocalReply(Envoy::Http::Code::OK, "EDS is ready", nullptr,
-                                       absl::nullopt);
+                                       absl::nullopt, "");
     return Http::FilterHeadersStatus::StopIteration;
   }
 
