4eb029b6da166aa925dd6b24c52d316128463311
Ismo Puustinen <ismo.puustinen@intel.com>
2019-05-17 12:46:07 +0300

    timer: check for int overflow in conversion (oss-fuzz #11852).
    Signed-off-by: Ismo Puustinen <ismo.puustinen@intel.com>

--- source/common/event/timer_impl.cc
+++ source/common/event/timer_impl.cc
@@ -23,7 +23,17 @@ void TimerImpl::enableTimer(const std::chrono::milliseconds& d) {
     event_active(&raw_event_, EV_TIMEOUT, 0);
   } else {
     // TODO(#4332): use duration_cast more nicely to clean up this code.
-    std::chrono::microseconds us = std::chrono::duration_cast<std::chrono::microseconds>(d);
+
+    // Since d is milliseconds, we need to check that the conversion
+    // doesn't overflow std::chrono::microseconds (since signed integer
+    // overflow is undefined behavior). If it would overflow, just use
+    // the maximum duration which can be expressed in microseconds.
+    std::chrono::microseconds us;
+    if (d > std::chrono::duration_values<std::chrono::microseconds>::max() / 1000) {
+      us = std::chrono::duration_values<std::chrono::microseconds>::max();
+    } else {
+      us = std::chrono::duration_cast<std::chrono::microseconds>(d);
+    }
     timeval tv;
     tv.tv_sec = us.count() / 1000000;
     tv.tv_usec = us.count() % 1000000;
