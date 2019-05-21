b6056c282023c45fccc6bbff8b56e30eb61d966c
Dan Zhang <danzh@google.com>
2019-05-21 13:47:00 -0400

    break circular deps
    Signed-off-by: Dan Zhang <danzh@google.com>

--- source/extensions/quic_listeners/quiche/platform/quiche_logging_impl.cc
+++
@@ -1,8 +0,0 @@
-// NOLINT(namespace-envoy)
-
-// This file is part of the QUICHE platform implementation, and is not to be
-// consumed or referenced directly by other Envoy code. It serves purely as a
-// porting layer for QUICHE.
-
-// Add a dummy .cc file to make sure .h file is compiled.
-#include "extensions/quic_listeners/quiche/platform/quiche_logging_impl.h"
