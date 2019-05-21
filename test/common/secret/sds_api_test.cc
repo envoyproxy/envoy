3c65309b8d872bb7663784d75c57a224dc8b8127
Fred Douglas <fredlas@google.com>
2019-05-21 18:04:30 -0400

    snapshot
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- test/common/secret/sds_api_test.cc
+++ test/common/secret/sds_api_test.cc
@@ -115,6 +115,48 @@ TEST_F(SdsApiTest, DynamicTlsCertificateUpdateSuccess) {
   handle->remove();
 }
 
+class PartialMockSds : public SdsApi {
+public:
+  PartialMockSds(NiceMock<Server::MockInstance>& server, Api::Api& api,
+                 NiceMock<Init::MockManager>& init_manager,
+                 envoy::api::v2::core::ConfigSource& config_source)
+      : SdsApi(
+            server.localInfo(), server.dispatcher(), server.random(), server.stats(),
+            server.clusterManager(), init_manager, config_source, "abc.com", []() {}, api) {}
+
+  MOCK_METHOD2(onConfigUpdate,
+               void(const Protobuf::RepeatedPtrField<ProtobufWkt::Any>&, const std::string&));
+  void setSecret(const envoy::api::v2::auth::Secret&) override {}
+  void validateConfig(const envoy::api::v2::auth::Secret&) override {}
+};
+
+// Basic test of delta's passthrough call to the state-of-the-world variant, to
+// increase coverage.
+TEST_F(SdsApiTest, Delta) {
+  Protobuf::RepeatedPtrField<envoy::api::v2::Resource> resources;
+  envoy::api::v2::auth::Secret secret;
+  secret.set_name("secret_1");
+  auto* resource = resources.Add();
+  resource->mutable_resource()->PackFrom(secret);
+  resource->set_name("secret_1");
+  resource->set_version("version1");
+
+  NiceMock<Server::MockInstance> server;
+  NiceMock<Init::MockManager> init_manager;
+  envoy::api::v2::core::ConfigSource config_source;
+  PartialMockSds sds(server, *api_, init_manager, config_source);
+  EXPECT_CALL(sds, onConfigUpdate(_, "version1"));
+  sds.SdsApi::onConfigUpdate(resources, {}, "ignored");
+
+  // An attempt to remove a resource logs an error, but otherwise just carries on (ignoring the
+  // removal attempt).
+  resource->set_version("version2");
+  EXPECT_CALL(sds, onConfigUpdate(_, "version2"));
+  Protobuf::RepeatedPtrField<std::string> removals;
+  *removals.Add() = "route_0";
+  sds.SdsApi::onConfigUpdate(resources, removals, "ignored");
+}
+
 // Tests SDS's use of the delta variant of onConfigUpdate().
 TEST_F(SdsApiTest, DeltaUpdateSuccess) {
   NiceMock<Server::MockInstance> server;
