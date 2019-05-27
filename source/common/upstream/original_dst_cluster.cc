1437b78336e667db850bdb0d2839988913a23a49
Dhi Aurrahman <dio@tetrate.io>
2019-05-27 08:53:31 +0700

    Fix clang_tidy
    Signed-off-by: Dhi Aurrahman <dio@tetrate.io>

--- source/common/upstream/original_dst_cluster.cc
+++ source/common/upstream/original_dst_cluster.cc
@@ -142,7 +142,7 @@ OriginalDstCluster::OriginalDstCluster(
           std::chrono::milliseconds(PROTOBUF_GET_MS_OR_DEFAULT(config, cleanup_interval, 5000))),
       cleanup_timer_(dispatcher_.createTimer([this]() -> void { cleanup(); })) {
   // TODO(dio): Remove hosts check once the hosts field is removed.
-  if (config.has_load_assignment() || config.hosts_size() > 0) {
+  if (config.has_load_assignment() || !config.hosts().empty()) {
     throw EnvoyException("ORIGINAL_DST clusters must have no load assignment or hosts configured");
   }
   cleanup_timer_->enableTimer(cleanup_interval_ms_);
