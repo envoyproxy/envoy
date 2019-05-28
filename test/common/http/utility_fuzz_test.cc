c7e109bcf1d50df1be3b1223b9f32aa84f2db32d
Asra Ali <asraa@google.com>
2019-05-28 10:24:00 -0400

    Add crashign utility fuzz testcase
    Signed-off-by: Asra Ali <asraa@google.com>

--- test/common/http/utility_fuzz_test.cc
+++ test/common/http/utility_fuzz_test.cc
@@ -1,5 +1,6 @@
 #include "common/http/utility.h"
 
+#include "test/common/http/common_fuzz.h"
 #include "test/common/http/utility_fuzz.pb.h"
 #include "test/fuzz/fuzz_runner.h"
 #include "test/test_common/utility.h"
