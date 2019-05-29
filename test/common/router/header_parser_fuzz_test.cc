1d0b7125cc901b47ace3571fb624d5e13d9d81f9
Asra Ali <asraa@google.com>
2019-05-29 14:31:42 -0400

    fuzz: remove invalid characters in header parser
    Signed-off-by: Asra Ali <asraa@google.com>

--- test/common/router/header_parser_fuzz_test.cc
+++ test/common/router/header_parser_fuzz_test.cc
@@ -1,6 +1,7 @@
 #include "common/http/header_map_impl.h"
 #include "common/router/header_parser.h"
 
+#include "test/common/http/common_fuzz.h"
 #include "test/common/router/header_parser_fuzz.pb.validate.h"
 #include "test/fuzz/fuzz_runner.h"
 #include "test/fuzz/utility.h"
@@ -9,11 +10,33 @@ namespace Envoy {
 namespace Fuzz {
 namespace {
 
+using Envoy::replaceInvalidCharacters;
+
+
+// Remove invalid characters from HeaderValueOptions in headers to add.
+Protobuf::RepeatedPtrField<envoy::api::v2::core::HeaderValueOption> processHeadersToAdd(
+    const Protobuf::RepeatedPtrField<envoy::api::v2::core::HeaderValueOption>& headers_to_add) {
+  Protobuf::RepeatedPtrField<envoy::api::v2::core::HeaderValueOption> processed;
+  for (auto header : headers_to_add) {
+    auto* header_value_option = processed.Add();
+    auto* mutable_header = header_value_option->mutable_header();
+    mutable_header->set_key(replaceInvalidCharacters(header.header().key()));
+    mutable_header->set_value(replaceInvalidCharacters(header.header().value()));
+    header_value_option->mutable_append()->CopyFrom(header.append());
+  }
+  return processed;
+}
+
 DEFINE_PROTO_FUZZER(const test::common::router::TestCase& input) {
   try {
     MessageUtil::validate(input);
+    auto headers_to_add = processHeadersToAdd(input.headers_to_add());
+    Protobuf::RepeatedPtrField<std::string> headers_to_remove;
+    for (auto s : input.headers_to_remove()) {
+      headers_to_remove.Add(replaceInvalidCharacters(s));
+    }
     Router::HeaderParserPtr parser =
-        Router::HeaderParser::configure(input.headers_to_add(), input.headers_to_remove());
+        Router::HeaderParser::configure(headers_to_add, headers_to_remove);
     Http::HeaderMapImpl header_map;
     parser->evaluateHeaders(header_map, fromStreamInfo(input.stream_info()));
     ENVOY_LOG_MISC(trace, "Success");
