38928521f43df9167873e8a8723ffc2611a64da8
Fred Douglas <fredlas@google.com>
2019-05-29 11:24:15 -0400

    move to and update proto_descriptors
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- source/common/config/subscription_factory.cc
+++ source/common/config/subscription_factory.cc
@@ -8,72 +8,10 @@
 #include "common/config/utility.h"
 #include "common/protobuf/protobuf.h"
 
+#include "server/proto_descriptors.h"
+
 namespace Envoy {
 namespace Config {
-
-const char UnknownMethod[] = "could_not_lookup_method_due_to_unknown_type_url";
-
-#define TYPE_URL_IS(x)                                                                             \
-  (type_url == Grpc::Common::typeUrl(envoy::api::v2::x().GetDescriptor()->full_name()))
-const Protobuf::MethodDescriptor& deltaGrpcMethod(absl::string_view type_url) {
-  std::string method_name = UnknownMethod;
-  if (TYPE_URL_IS(RouteConfiguration)) {
-    method_name = "envoy.api.v2.RouteDiscoveryService.DeltaRoutes";
-  } else if (TYPE_URL_IS(ScopedRouteConfiguration)) {
-    method_name = "envoy.api.v2.ScopedRoutesDiscoveryService.DeltaScopedRoutes";
-  } else if (TYPE_URL_IS(route::VirtualHost)) {
-    method_name = "envoy.api.v2.VirtualHostDiscoveryService.DeltaVirtualHosts";
-  } else if (TYPE_URL_IS(auth::Secret)) {
-    method_name = "envoy.service.discovery.v2.SecretDiscoveryService.DeltaSecrets";
-  } else if (TYPE_URL_IS(Cluster)) {
-    method_name = "envoy.api.v2.ClusterDiscoveryService.DeltaClusters";
-  } else if (TYPE_URL_IS(ClusterLoadAssignment)) {
-    method_name = "envoy.api.v2.EndpointDiscoveryService.DeltaEndpoints";
-  } else if (TYPE_URL_IS(Listener)) {
-    method_name = "envoy.api.v2.ListenerDiscoveryService.DeltaListeners";
-  }
-  ASSERT(method_name != UnknownMethod);
-  return *Protobuf::DescriptorPool::generated_pool()->FindMethodByName(method_name);
-}
-
-const Protobuf::MethodDescriptor& sotwGrpcMethod(absl::string_view type_url) {
-  std::string method_name = UnknownMethod;
-  if (TYPE_URL_IS(RouteConfiguration)) {
-    method_name = "envoy.api.v2.RouteDiscoveryService.StreamRoutes";
-  } else if (TYPE_URL_IS(ScopedRouteConfiguration)) {
-    method_name = "envoy.api.v2.ScopedRoutesDiscoveryService.StreamScopedRoutes";
-  } else if (TYPE_URL_IS(auth::Secret)) {
-    method_name = "envoy.service.discovery.v2.SecretDiscoveryService.StreamSecrets";
-  } else if (TYPE_URL_IS(Cluster)) {
-    method_name = "envoy.api.v2.ClusterDiscoveryService.StreamClusters";
-  } else if (TYPE_URL_IS(ClusterLoadAssignment)) {
-    method_name = "envoy.api.v2.EndpointDiscoveryService.StreamEndpoints";
-  } else if (TYPE_URL_IS(Listener)) {
-    method_name = "envoy.api.v2.ListenerDiscoveryService.StreamListeners";
-  }
-  ASSERT(method_name != UnknownMethod);
-  return *Protobuf::DescriptorPool::generated_pool()->FindMethodByName(method_name);
-}
-
-const Protobuf::MethodDescriptor& restMethod(absl::string_view type_url) {
-  std::string method_name = UnknownMethod;
-  if (TYPE_URL_IS(RouteConfiguration)) {
-    method_name = "envoy.api.v2.RouteDiscoveryService.FetchRoutes";
-  } else if (TYPE_URL_IS(ScopedRouteConfiguration)) {
-    method_name = "envoy.api.v2.ScopedRoutesDiscoveryService.FetchScopedRoutes";
-  } else if (TYPE_URL_IS(auth::Secret)) {
-    method_name = "envoy.service.discovery.v2.SecretDiscoveryService.FetchSecrets";
-  } else if (TYPE_URL_IS(Cluster)) {
-    method_name = "envoy.api.v2.ClusterDiscoveryService.FetchClusters";
-  } else if (TYPE_URL_IS(ClusterLoadAssignment)) {
-    method_name = "envoy.api.v2.EndpointDiscoveryService.FetchEndpoints";
-  } else if (TYPE_URL_IS(Listener)) {
-    method_name = "envoy.api.v2.ListenerDiscoveryService.FetchListeners";
-  }
-  ASSERT(method_name != UnknownMethod);
-  return *Protobuf::DescriptorPool::generated_pool()->FindMethodByName(method_name);
-}
-#undef TYPE_URL_IS
 
 std::unique_ptr<Subscription> SubscriptionFactory::subscriptionFromConfigSource(
     const envoy::api::v2::core::ConfigSource& config, const LocalInfo::LocalInfo& local_info,
@@ -101,8 +39,8 @@ std::unique_ptr<Subscription> SubscriptionFactory::subscriptionFromConfigSource(
       result = std::make_unique<HttpSubscriptionImpl>(
           local_info, cm, api_config_source.cluster_names()[0], dispatcher, random,
           Utility::apiConfigSourceRefreshDelay(api_config_source),
-          Utility::apiConfigSourceRequestTimeout(api_config_source), restMethod(type_url), stats,
-          Utility::configSourceInitialFetchTimeout(config));
+          Utility::apiConfigSourceRequestTimeout(api_config_source), Server::restMethod(type_url),
+          stats, Utility::configSourceInitialFetchTimeout(config));
       break;
     case envoy::api::v2::core::ApiConfigSource::GRPC:
       result = std::make_unique<GrpcSubscriptionImpl>(
@@ -110,7 +48,7 @@ std::unique_ptr<Subscription> SubscriptionFactory::subscriptionFromConfigSource(
           Config::Utility::factoryForGrpcApiConfigSource(cm.grpcAsyncClientManager(),
                                                          api_config_source, scope)
               ->create(),
-          dispatcher, random, sotwGrpcMethod(type_url), type_url, stats, scope,
+          dispatcher, random, Server::sotwGrpcMethod(type_url), type_url, stats, scope,
           Utility::parseRateLimitSettings(api_config_source),
           Utility::configSourceInitialFetchTimeout(config));
       break;
@@ -121,7 +59,7 @@ std::unique_ptr<Subscription> SubscriptionFactory::subscriptionFromConfigSource(
           Config::Utility::factoryForGrpcApiConfigSource(cm.grpcAsyncClientManager(),
                                                          api_config_source, scope)
               ->create(),
-          dispatcher, deltaGrpcMethod(type_url), type_url, random, scope,
+          dispatcher, Server::deltaGrpcMethod(type_url), type_url, random, scope,
           Utility::parseRateLimitSettings(api_config_source), stats,
           Utility::configSourceInitialFetchTimeout(config));
       break;
