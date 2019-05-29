d51e91bc544d7d52cb0d41c9cd9a7496690f5c33
asraa <asraa@google.com>
2019-05-28 23:12:43 -0400

    fuzz: add header string filter in utility_fuzz_test and cleanup dead code (#7088)
    Adds header string filter on cookie headers to fix crashing fuzz tests.
    Removes dead hasSetCookie code, and associated tests and fuzz tests.
    
    Fixes oss-fuzz issue
    https://oss-fuzz.com/testcase-detail/5735325211557888
    
    Risk Level: Low.
    Testing: Corpus entry added for crashing input.
    
    Signed-off-by: Asra Ali <asraa@google.com>

--- test/common/http/utility_test.cc
+++ test/common/http/utility_test.cc
@@ -410,27 +410,6 @@ TEST(HttpUtility, TestParseCookieWithQuotes) {
   EXPECT_EQ(Utility::parseCookieValue(headers, "leadingdquote"), "\"foobar");
 }
 
-TEST(HttpUtility, TestHasSetCookie) {
-  TestHeaderMapImpl headers{{"someheader", "10.0.0.1"},
-                            {"set-cookie", "somekey=somevalue"},
-                            {"set-cookie", "abc=def; Expires=Wed, 09 Jun 2021 10:18:14 GMT"},
-                            {"set-cookie", "key2=value2; Secure"}};
-
-  EXPECT_TRUE(Utility::hasSetCookie(headers, "abc"));
-  EXPECT_TRUE(Utility::hasSetCookie(headers, "somekey"));
-  EXPECT_FALSE(Utility::hasSetCookie(headers, "ghi"));
-}
-
-TEST(HttpUtility, TestHasSetCookieBadValues) {
-  TestHeaderMapImpl headers{{"someheader", "10.0.0.1"},
-                            {"set-cookie", "somekey =somevalue"},
-                            {"set-cookie", "abc"},
-                            {"set-cookie", "key2=value2; Secure"}};
-
-  EXPECT_FALSE(Utility::hasSetCookie(headers, "abc"));
-  EXPECT_TRUE(Utility::hasSetCookie(headers, "key2"));
-}
-
 TEST(HttpUtility, TestMakeSetCookieValue) {
   EXPECT_EQ("name=\"value\"; Max-Age=10",
             Utility::makeSetCookieValue("name", "value", "", std::chrono::seconds(10), false));
