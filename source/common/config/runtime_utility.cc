c4d1467a12aa61a0ae62013aa38ae898ca228d55
Harvey Tuch <htuch@google.com>
2019-05-29 15:37:35 -0400

    Runtime utility feedback.
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- source/common/config/runtime_utility.cc
+++ source/common/config/runtime_utility.cc
@@ -13,7 +13,7 @@ void translateRuntime(const envoy::config::bootstrap::v2::Runtime& runtime_confi
       layer->mutable_disk_layer()->set_symlink_root(runtime_config.symlink_root() + "/" +
                                                     runtime_config.subdirectory());
     }
-    {
+    if (!runtime_config.override_subdirectory().empty()) {
       auto* layer = layered_runtime_config.add_layers();
       layer->set_name("override");
       layer->mutable_disk_layer()->set_symlink_root(runtime_config.symlink_root() + "/" +
