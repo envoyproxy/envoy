8286635ffeb36fd7e09c04a7e63f0d0f17349105
Fred Douglas <43351173+fredlas@users.noreply.github.com>
2019-06-04 14:32:20 -0700

    config: centralize gRPC and REST method strings in SubscriptionFactory (#7092)
    Previously, constant strings like "envoy.api.v2.EndpointDiscoveryService.FetchEndpoints" were all over eds.cc, rds_impl.cc, etc, being fed into SubscriptionFactory. Now, they are all centralized in SubscriptionFactory, the only place that actually uses them. #4991
    
    Risk Level: low
    
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- source/common/config/subscription_factory.h
+++ source/common/config/subscription_factory.h
@@ -33,9 +33,9 @@ public:
   static std::unique_ptr<Subscription> subscriptionFromConfigSource(
       const envoy::api::v2::core::ConfigSource& config, const LocalInfo::LocalInfo& local_info,
       Event::Dispatcher& dispatcher, Upstream::ClusterManager& cm, Runtime::RandomGenerator& random,
-      Stats::Scope& scope, const std::string& rest_method, const std::string& grpc_method,
-      absl::string_view type_url, ProtobufMessage::ValidationVisitor& validation_visitor,
-      Api::Api& api, SubscriptionCallbacks& callbacks);
+      Stats::Scope& scope, absl::string_view type_url,
+      ProtobufMessage::ValidationVisitor& validation_visitor, Api::Api& api,
+      SubscriptionCallbacks& callbacks);
 };
 
 } // namespace Config
