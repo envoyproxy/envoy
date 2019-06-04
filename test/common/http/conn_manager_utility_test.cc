1fdaf8e74f7023cd40fa56919f44163241bd9b95
Andres Guedez <34292400+AndresGuedez@users.noreply.github.com>
2019-06-04 11:56:09 -0400

    router: scoped rds (2d): integrate SRDS API into HttpConnectionManager (#7068)
    Integrate the Scoped Route Discovery Service (SRDS) API into the HttpConnectionManager.
    
    NOTES:
    
    - This is the last PR in the chain originating from parent #5839.
    - The scoped routing logic which can be configured through this API has not yet been implemented; it will be done in follow up PRs.
    
    Risk Level: Low (API is not yet fully implemented and should not be enabled)
    Testing: Integration tests added.
    
    Signed-off-by: Andres Guedez <aguedez@google.com>

--- test/common/http/conn_manager_utility_test.cc
+++ test/common/http/conn_manager_utility_test.cc
@@ -56,7 +56,8 @@ public:
   MOCK_CONST_METHOD0(streamIdleTimeout, std::chrono::milliseconds());
   MOCK_CONST_METHOD0(requestTimeout, std::chrono::milliseconds());
   MOCK_CONST_METHOD0(delayedCloseTimeout, std::chrono::milliseconds());
-  MOCK_METHOD0(routeConfigProvider, Router::RouteConfigProvider&());
+  MOCK_METHOD0(routeConfigProvider, Router::RouteConfigProvider*());
+  MOCK_METHOD0(scopedRouteConfigProvider, Config::ConfigProvider*());
   MOCK_METHOD0(serverName, const std::string&());
   MOCK_METHOD0(stats, ConnectionManagerStats&());
   MOCK_METHOD0(tracingStats, ConnectionManagerTracingStats&());
