09f8e17c0d9d7111f6bd6bcbe8e18c8c7b1313ac
Lizan Zhou <lizan@tetrate.io>
2019-06-07 08:18:16 -0700

    update abseil (#7199)
    Update absl to latest and enable in macOS. Retrying #7146.
    
    Risk Level: Low
    
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- source/common/common/thread_annotations.h
+++ source/common/common/thread_annotations.h
@@ -3,9 +3,3 @@
 // NOLINT(namespace-envoy)
 
 #include "absl/base/thread_annotations.h"
-
-#ifdef __APPLE__
-#undef THREAD_ANNOTATION_ATTRIBUTE__
-// See #2571, we get problematic warnings on Clang + macOS.
-#define THREAD_ANNOTATION_ATTRIBUTE__(x) // no-op
-#endif
