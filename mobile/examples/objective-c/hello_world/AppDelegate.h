ffee37279729991e09a6d8a60b5630ae55ee091e
Michael Rebello <me@michaelrebello.com>
2019-06-07 14:41:20 -0700

    objective-c: run clang-format on Objective-C files (#54)
    - Replace the `.clang-format` symlink to upstream Envoy with a custom file for this repository
    - Rules in this `.clang-format` file mirror upstream and add Objective-C rules. 2 spaces were selected in order to match upstream's C++ formatting
    - Update the format check script to ignore `Envoy.framework` and start linting Objective-C
    - Run the formatter against Objective-C files
    
    https://github.com/lyft/envoy-mobile/issues/53
    
    Signed-off-by: Michael Rebello <mrebello@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/examples/objective-c/hello_world/AppDelegate.h
+++ mobile/examples/objective-c/hello_world/AppDelegate.h
@@ -1,8 +1,10 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+// NOLINT(namespace-envoy)
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UIWindow* window;
 
 @end
--- mobile/tools/check_format.sh
+++ mobile/tools/check_format.sh
@@ -12,10 +12,11 @@ fi
 
 # TODO(mattklein123): WORKSPACE is excluded due to warning about @bazel_tools reference. Fix here
 #                     or in the upstream checker.
-# TODO(mattklein123): Objective-C is excluded because the clang-format setup is not correct. Fix.
+# TODO(mattklein123): Add support upstream for whitelisting paths that don't need to have
+#                     NOLINT(namespace-envoy), such as Objective-C.
 # TODO(mattklein123): We don't need envoy_package() in various files. Somehow fix in upstream
 #                     checker.
 envoy/tools/check_format.py \
-    --add-excluded-prefixes ./envoy/ ./envoy_build_config/extensions_build_config.bzl ./WORKSPACE ./examples/objective-c/ \
+    --add-excluded-prefixes ./envoy/ ./envoy_build_config/extensions_build_config.bzl ./WORKSPACE ./dist/Envoy.framework/ \
     --skip_envoy_build_rule_check "$ENVOY_FORMAT_ACTION"
 envoy/tools/format_python_tools.sh check
