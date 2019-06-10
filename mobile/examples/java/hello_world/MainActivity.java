fa2f7f331eb36e16276f68eb13183e4871e0b67d
Michael Rebello <me@michaelrebello.com>
2019-06-10 09:53:07 -0700

    java: run clang-format for Java files (#56)
    `clang-format` can be used for auto-formatting Java files the same way it is for Objective-C and C++.
    
    2 spaces were selected for spacing to maintain parity with upstream.
    
    Depends on:
    envoyproxy/envoy#7210
    envoyproxy/envoy#7211
    
    https://github.com/lyft/envoy-mobile/issues/55
    
    Signed-off-by: Michael Rebello <mrebello@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/examples/java/hello_world/MainActivity.java
+++ mobile/examples/java/hello_world/MainActivity.java
@@ -26,92 +26,97 @@ import java.util.concurrent.TimeUnit;
 import io.envoyproxy.envoymobile.Envoy;
 
 public class MainActivity extends Activity {
-    private static final String ENDPOINT = "http://0.0.0.0:9001/api.lyft.com/static/demo/hello_world.txt";
-    private RecyclerView recyclerView;
+  private static final String ENDPOINT =
+      "http://0.0.0.0:9001/api.lyft.com/static/demo/hello_world.txt";
+  private RecyclerView recyclerView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        Envoy envoy = new Envoy();
-        envoy.load();
-        String config = null;
+    Envoy envoy = new Envoy();
+    envoy.load();
+    String config = null;
+    try {
+      config = loadEnvoyConfig(getBaseContext(), R.raw.config);
+    } catch (RuntimeException e) {
+      Log.d("MainActivity", "exception getting config.", e);
+      throw new RuntimeException("Can't get config to run envoy.");
+    }
+    envoy.run(getBaseContext(), config);
+
+    recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    final ResponseRecyclerViewAdapter adapter = new ResponseRecyclerViewAdapter();
+    recyclerView.setAdapter(adapter);
+    DividerItemDecoration dividerItemDecoration =
+        new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
+    recyclerView.addItemDecoration(dividerItemDecoration);
+    HandlerThread thread = new HandlerThread("");
+    thread.start();
+
+    final Handler handler = new Handler(thread.getLooper());
+    handler.postDelayed(new Runnable() {
+      @Override
+      public void run() {
         try {
-            config = loadEnvoyConfig(getBaseContext(), R.raw.config);
-        } catch (RuntimeException e) {
-            Log.d("MainActivity", "exception getting config.", e);
-            throw new RuntimeException("Can't get config to run envoy.");
-        }
-        envoy.run(getBaseContext(), config);
-
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        final ResponseRecyclerViewAdapter adapter = new ResponseRecyclerViewAdapter();
-        recyclerView.setAdapter(adapter);
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
-        recyclerView.addItemDecoration(dividerItemDecoration);
-        HandlerThread thread = new HandlerThread("");
-        thread.start();
-        final Handler handler = new Handler(thread.getLooper());
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    final Response response = makeRequest();
-                    recyclerView.post((Runnable) () -> adapter.add(response));
-                } catch (IOException e) {
-                    Log.d("MainActivity", "exception making request.", e);
-                }
-                // Make a call again
-                handler.postDelayed(this, TimeUnit.SECONDS.toMillis(1));
-            }
-        }, 0);
-    }
-
-    private Response makeRequest() throws IOException {
-        URL url = new URL(ENDPOINT);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        int status = connection.getResponseCode();
-        if (status != 200) {
-            throw new IOException("non 200 status: " + status);
-        }
-
-        List<String> serverHeaderField = connection.getHeaderFields().get("server");
-        InputStream inputStream = connection.getInputStream();
-        String body = deserialize(inputStream);
-        inputStream.close();
-        return new Response(body, serverHeaderField != null ? String.join(", ", serverHeaderField) : "");
-    }
-
-    private String deserialize(InputStream inputStream) throws IOException {
-        StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        String line = bufferedReader.readLine();
-        while (line != null) {
-            stringBuilder.append(line);
-            line = bufferedReader.readLine();
-        }
-        bufferedReader.close();
-        return stringBuilder.toString();
-    }
-
-    private String loadEnvoyConfig(Context context, int configResourceId) throws RuntimeException {
-        InputStream inputStream = context.getResources().openRawResource(configResourceId);
-        InputStreamReader inputReader = new InputStreamReader(inputStream);
-        BufferedReader bufReader = new BufferedReader(inputReader);
-        StringBuilder text = new StringBuilder();
-
-        try {
-            String line;
-            while ((line = bufReader.readLine()) != null) {
-                text.append(line);
-                text.append('\n');
-            }
+          final Response response = makeRequest();
+          recyclerView.post((Runnable)() -> adapter.add(response));
         } catch (IOException e) {
-            return null;
+          Log.d("MainActivity", "exception making request.", e);
         }
-        return text.toString();
+
+        // Make a call again
+        handler.postDelayed(this, TimeUnit.SECONDS.toMillis(1));
+      }
+    }, 0);
+  }
+
+  private Response makeRequest() throws IOException {
+    URL url = new URL(ENDPOINT);
+    HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+    int status = connection.getResponseCode();
+    if (status != 200) {
+      throw new IOException("non 200 status: " + status);
     }
+
+    List<String> serverHeaderField = connection.getHeaderFields().get("server");
+    InputStream inputStream = connection.getInputStream();
+    String body = deserialize(inputStream);
+    inputStream.close();
+    return new Response(body,
+                        serverHeaderField != null ? String.join(", ", serverHeaderField) : "");
+  }
+
+  private String deserialize(InputStream inputStream) throws IOException {
+    StringBuilder stringBuilder = new StringBuilder();
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+    String line = bufferedReader.readLine();
+    while (line != null) {
+      stringBuilder.append(line);
+      line = bufferedReader.readLine();
+    }
+    bufferedReader.close();
+    return stringBuilder.toString();
+  }
+
+  private String loadEnvoyConfig(Context context, int configResourceId) throws RuntimeException {
+    InputStream inputStream = context.getResources().openRawResource(configResourceId);
+    InputStreamReader inputReader = new InputStreamReader(inputStream);
+    BufferedReader bufReader = new BufferedReader(inputReader);
+    StringBuilder text = new StringBuilder();
+
+    try {
+      String line;
+      while ((line = bufReader.readLine()) != null) {
+        text.append(line);
+        text.append('\n');
+      }
+    } catch (IOException e) {
+      return null;
+    }
+    return text.toString();
+  }
 }
--- mobile/examples/java/hello_world/ResponseRecyclerViewAdapter.java
+++ mobile/examples/java/hello_world/ResponseRecyclerViewAdapter.java
@@ -10,28 +10,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ResponseRecyclerViewAdapter extends RecyclerView.Adapter<ResponseViewHolder> {
-    private final List<Response> data = new ArrayList<>();
+  private final List<Response> data = new ArrayList<>();
 
-    @Override
-    public ResponseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.item, parent, false);
-        return new ResponseViewHolder(view);
-    }
+  @Override
+  public ResponseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    Context context = parent.getContext();
+    LayoutInflater inflater = LayoutInflater.from(context);
+    View view = inflater.inflate(R.layout.item, parent, false);
+    return new ResponseViewHolder(view);
+  }
 
-    @Override
-    public void onBindViewHolder(ResponseViewHolder holder, int position) {
-        holder.setResult(data.get(position));
-    }
+  @Override
+  public void onBindViewHolder(ResponseViewHolder holder, int position) {
+    holder.setResult(data.get(position));
+  }
 
-    @Override
-    public int getItemCount() {
-        return data.size();
-    }
+  @Override
+  public int getItemCount() {
+    return data.size();
+  }
 
-    public void add(Response response) {
-        data.add(0, response);
-        notifyItemInserted(0);
-    }
+  public void add(Response response) {
+    data.add(0, response);
+    notifyItemInserted(0);
+  }
 }
