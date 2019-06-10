b46685e5d43696517fa4b333748f3fc4cb60d4d3
Michael Rebello <me@michaelrebello.com>
2019-06-10 15:32:17 -0700

    ci: run SwiftLint on CI (#62)
    Resolves https://github.com/lyft/envoy-mobile/issues/26
    
    Signed-off-by: Michael Rebello <mrebello@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/examples/swift/hello_world/AppDelegate.swift
+++ mobile/examples/swift/hello_world/AppDelegate.swift
@@ -2,41 +2,41 @@ import Envoy
 import UIKit
 
 private enum ConfigLoadError: Error {
-    case noFileAtPath
+  case noFileAtPath
 }
 
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
+  var window: UIWindow?
 
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?)
-        -> Bool
-    {
-        do {
-            let configYaml = try self.loadEnvoyConfig() as NSString
-            NSLog("Loading config:\n\(configYaml)")
-            Thread.detachNewThread {
-                run_envoy(configYaml.utf8String)
-            }
-        } catch let error {
-            NSLog("Failed to load config: \(error)")
-        }
-
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = ViewController()
-        window.makeKeyAndVisible()
-        self.window = window
-        NSLog("Finished launching!")
-
-        return true
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+  {
+    do {
+      let configYaml = try self.loadEnvoyConfig() as NSString
+      NSLog("Loading config:\n\(configYaml)")
+      Thread.detachNewThread {
+        run_envoy(configYaml.utf8String)
+      }
+    } catch let error {
+      NSLog("Failed to load config: \(error)")
     }
 
-    private func loadEnvoyConfig() throws -> String {
-        guard let configFile = Bundle.main.path(forResource: "config", ofType: "yaml") else {
-            throw ConfigLoadError.noFileAtPath
-        }
+    let window = UIWindow(frame: UIScreen.main.bounds)
+    window.rootViewController = ViewController()
+    window.makeKeyAndVisible()
+    self.window = window
+    NSLog("Finished launching!")
 
-        return try String(contentsOfFile: configFile, encoding: .utf8)
+    return true
+  }
+
+  private func loadEnvoyConfig() throws -> String {
+    guard let configFile = Bundle.main.path(forResource: "config", ofType: "yaml") else {
+      throw ConfigLoadError.noFileAtPath
     }
+
+    return try String(contentsOfFile: configFile, encoding: .utf8)
+  }
 }
--- mobile/examples/swift/hello_world/ViewController.swift
+++ mobile/examples/swift/hello_world/ViewController.swift
@@ -1,82 +1,86 @@
 import UIKit
 
 private let kCellID = "cell-id"
+// swiftlint:disable:next force_unwrapping
 private let kURL = URL(string: "http://0.0.0.0:9001/api.lyft.com/static/demo/hello_world.txt")!
 
 final class ViewController: UITableViewController {
-    private var responses = [Response]()
-    private var timer: Timer?
+  private var responses = [Response]()
+  private var timer: Timer?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.startRequests()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.startRequests()
+  }
+
+  deinit {
+    self.timer?.invalidate()
+  }
+
+  // MARK: - Requests
+
+  private func startRequests() {
+    // Note that the first delay will give Envoy time to start up.
+    self.timer = .scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
+      self?.performRequest()
+    }
+  }
+
+  private func performRequest() {
+    let request = URLRequest(url: kURL)
+    NSLog("Starting request to '\(kURL.path)")
+    let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+      if let response = response as? HTTPURLResponse, response.statusCode == 200, let data = data {
+        self?.handle(response: response, with: data)
+      } else if let error = error {
+        NSLog("Received error: \(error)")
+      } else {
+        NSLog("Failed to receive data from the server")
+      }
     }
 
-    deinit {
-        self.timer?.invalidate()
+    task.resume()
+  }
+
+  private func handle(response: HTTPURLResponse, with data: Data) {
+    guard let body = String(data: data, encoding: .utf8) else {
+      return NSLog("Failed to deserialize response string")
     }
 
-    // MARK: - Requests
+    let untypedHeaders = response.allHeaderFields
+    let headers = Dictionary(uniqueKeysWithValues: untypedHeaders.map
+    { header -> (String, String) in
+      return (header.key as? String ?? String(describing: header.key), "\(header.value)")
+    })
 
-    private func startRequests() {
-        // Note that the first delay will give Envoy time to start up.
-        self.timer = .scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
-            self?.performRequest()
-        }
+    NSLog("Response:\n\(data.count) bytes\n\(body)\n\(headers)")
+
+    let value = Response(body: body, serverHeader: headers["Server"] ?? "")
+    DispatchQueue.main.async {
+      self.responses.append(value)
+      self.tableView.reloadData()
     }
+  }
 
-    private func performRequest() {
-        let request = URLRequest(url: kURL)
-        NSLog("Starting request to '\(kURL.path)")
-        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
-            if let response = response as? HTTPURLResponse, response.statusCode == 200, let data = data {
-                self?.handle(response: response, with: data)
-            } else if let error = error {
-                NSLog("Received error: \(error)")
-            } else {
-                NSLog("Failed to receive data from the server")
-            }
-        }
+  // MARK: - UITableView
 
-        task.resume()
-    }
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
 
-    private func handle(response: HTTPURLResponse, with data: Data) {
-        guard let body = String(data: data, encoding: .utf8) else {
-            return NSLog("Failed to deserialize response string")
-        }
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.responses.count
+  }
 
-        let untypedHeaders = response.allHeaderFields
-        let headers = Dictionary(uniqueKeysWithValues: untypedHeaders.map { header -> (String, String) in
-            return (header.key as? String ?? String(describing: header.key), "\(header.value)")
-        })
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+    -> UITableViewCell
+  {
+    let cell = tableView.dequeueReusableCell(withIdentifier: kCellID) ??
+      UITableViewCell(style: .subtitle, reuseIdentifier: kCellID)
 
-        NSLog("Response:\n\(data.count) bytes\n\(body)\n\(headers)")
-
-        let value = Response(body: body, serverHeader: headers["Server"] ?? "")
-        DispatchQueue.main.async {
-            self.responses.append(value)
-            self.tableView.reloadData()
-        }
-    }
-
-    // MARK: - UITableView
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.responses.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: kCellID) ??
-            UITableViewCell(style: .subtitle, reuseIdentifier: kCellID)
-
-        let response = self.responses[indexPath.row]
-        cell.textLabel?.text = "Response: \(response.body)"
-        cell.detailTextLabel?.text = "'Server' header: \(response.serverHeader)"
-        return cell
-    }
+    let response = self.responses[indexPath.row]
+    cell.textLabel?.text = "Response: \(response.body)"
+    cell.detailTextLabel?.text = "'Server' header: \(response.serverHeader)"
+    return cell
+  }
 }
