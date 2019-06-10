b8788c0bb400a1916bc807f9e65f9d427e34e78d
Dan Zhang <danzh@google.com>
2019-06-10 12:22:29 -0400

    add #pragma GCC disagnostic push/pop
    Signed-off-by: Dan Zhang <danzh@google.com>

--- source/extensions/quic_listeners/quiche/envoy_quic_alarm_factory.h
+++ source/extensions/quic_listeners/quiche/envoy_quic_alarm_factory.h
@@ -1,16 +1,18 @@
 #pragma once
 
-// QUICHE allows unused parameters.
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-
 #include "common/common/non_copyable.h"
 
 #include "extensions/quic_listeners/quiche/envoy_quic_alarm.h"
 
-#include "quiche/quic/core/quic_alarm.h"
+#pragma GCC diagnostic push
+
+// QUICHE allows unused parameters.
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #include "quiche/quic/core/quic_alarm_factory.h"
 #include "quiche/quic/core/quic_arena_scoped_ptr.h"
 #include "quiche/quic/core/quic_one_block_arena.h"
+
+#pragma GCC diagnostic pop
 
 namespace Envoy {
 namespace Quic {
