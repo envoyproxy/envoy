ec1fe4fe33a0ce9f7f4e893fc815d9a2a07159d1
Vasili Syrakis <vsyrakis@atlassian.com>
2019-06-11 14:54:34 +1000

    Fixing tests for PR #6948
    * Add back expect_call lines to HttpSubscriptionImplTest
    * Update verifyStats calls to no longer expect failures
    
    Signed-off-by: Vasili Syrakis <vsyrakis@atlassian.com>

--- test/common/config/http_subscription_impl_test.cc
+++ test/common/config/http_subscription_impl_test.cc
@@ -44,18 +44,22 @@ TEST_F(HttpSubscriptionImplTest, BadJsonRecovery) {
 
 TEST_F(HttpSubscriptionImplTest, ConfigNotModified) {
   startSubscription({"cluster0", "cluster1"});
+  EXPECT_CALL(random_gen_, random()).WillOnce(Return(0));
+  EXPECT_CALL(*timer_, enableTimer(_));
+  EXPECT_CALL(callbacks_, onConfigUpdateFailed(_));
+  http_callbacks_->onFailure(Http::AsyncClient::FailureReason::Reset);
 
-  verifyStats(1, 0, 0, 1, 0);
+  verifyStats(1, 0, 0, 0, 0);
   timerTick();
-  verifyStats(2, 0, 0, 1, 0);
+  verifyStats(2, 0, 0, 0, 0);
 
   // accept and modify.
   deliverConfigUpdate({"cluster0", "cluster1"}, "0", true, true, "200");
-  verifyStats(3, 1, 0, 1, 7148434200721666028);
+  verifyStats(3, 1, 0, 0, 7148434200721666028);
 
   // accept and does not modify.
   deliverConfigUpdate({"cluster0", "cluster1"}, "0", true, false, "304");
-  verifyStats(4, 1, 0, 1, 7148434200721666028);
+  verifyStats(4, 1, 0, 0, 7148434200721666028);
 }
 
 } // namespace
