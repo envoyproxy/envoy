11e9649226ad627f9c17b717b807f254d1dbc90b
Matt Klein <mklein@lyft.com>
2019-06-11 14:43:04 -0700

    upstream: refactor logical/real host (#7233)
    This change refactors logical/real host to be a common
    concept that can be used by the dynamic forward proxy support.
    In doing so, the threading has been switched from using TLS to
    R/W locks to make dropping the code into multiple locations
    easier. It also fixes a longstanding issue with logical DNS
    where the current address is reported as "0.0.0.0" in admin
    output.
    
    Fixes https://github.com/envoyproxy/envoy/issues/5225
    Part of https://github.com/envoyproxy/envoy/issues/1606
    
    Signed-off-by: Matt Klein <mklein@lyft.com>

---
+++ source/common/upstream/logical_host.cc
@@ -0,0 +1,16 @@
+#include "common/upstream/logical_host.h"
+
+namespace Envoy {
+namespace Upstream {
+
+Upstream::Host::CreateConnectionData LogicalHost::createConnection(
+    Event::Dispatcher& dispatcher, const Network::ConnectionSocket::OptionsSharedPtr& options,
+    Network::TransportSocketOptionsSharedPtr transport_socket_options) const {
+  const auto current_address = address();
+  return {HostImpl::createConnection(dispatcher, cluster(), current_address, options,
+                                     transport_socket_options),
+          std::make_shared<RealHostDescription>(current_address, shared_from_this())};
+}
+
+} // namespace Upstream
+} // namespace Envoy
