f0227fcd19819ea4db224b91724aaacd122d0617
Joshua Marantz <jmarantz@google.com>
2019-06-12 10:14:01 -0400

    inject memory exact comparison bug to make sure 'release' fails.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/common/stats/symbol_table_impl_test.cc
+++ test/common/stats/symbol_table_impl_test.cc
@@ -577,7 +577,7 @@ TEST(SymbolTableTest, Memory) {
   // symbol_table_mem_used:  1726056 (3.9x) -- does not seem to depend on STL sizes.
   EXPECT_MEMORY_LE(string_mem_used, 7759488);
   EXPECT_MEMORY_LE(symbol_table_mem_used, string_mem_used / 3);
-  EXPECT_MEMORY_EQ(symbol_table_mem_used, 1726056);
+  EXPECT_MEMORY_EQ(symbol_table_mem_used, 1726057);
 }
 
 } // namespace Stats
