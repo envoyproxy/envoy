da015dfe6b0b0014fc99e1fdd53200796be54fa1
Hallie Lomax <hallie@lyft.com>
2019-06-12 17:58:47 -0700

    passed in wrong variable
    Signed-off-by: Hallie Lomax <hallie@lyft.com>

--- source/extensions/filters/http/dynamo/dynamo_request_parser.cc
+++ source/extensions/filters/http/dynamo/dynamo_request_parser.cc
@@ -105,7 +105,7 @@ RequestParser::TableDescriptor RequestParser::parseTable(const std::string& oper
         json_data.getObjectArray("TransactItems", true);
     std::string table_name = "";
     for (const Json::ObjectSharedPtr& transact_item : transact_items) {
-      table_name = getTableNameFromTransaction(*transact_items);
+      table_name = getTableNameFromTransaction(*transact_item);
       if (table_name == "") {
         // if an operation is missing a table name, we want to throw the normal set of errors
         table.table_name = "";
@@ -129,7 +129,7 @@ std::string RequestParser::getTableNameFromTransaction(const Json::Object& trans
   std::string table_name = "";
   for (const std::string& operation : TRANSACT_ITEM_OPERATIONS) {
     item = transact_item.getObject(operation, true);
-    table_name = item.getString("TableName", "");
+    table_name = item->getString("TableName", "");
     if (table_name != "") {
       return table_name;
     }
