ea63c3a9110558ef16ec032bd395a192c5eca316
Joshua Marantz <jmarantz@google.com>
2019-06-13 20:03:59 -0400

    stats: Resolve loss of importMode for gauges that are initialized first, then merged. (#7255)
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/common/stats/stat_merger.cc
+++ source/common/stats/stat_merger.cc
@@ -1,7 +1,5 @@
 #include "common/stats/stat_merger.h"
 
-#include <regex>
-
 namespace Envoy {
 namespace Stats {
 
@@ -15,15 +13,36 @@ void StatMerger::mergeCounters(const Protobuf::Map<std::string, uint64_t>& count
 
 void StatMerger::mergeGauges(const Protobuf::Map<std::string, uint64_t>& gauges) {
   for (const auto& gauge : gauges) {
+    // Merging gauges via RPC from the parent has 4 cases; case 2 and 4b are the
+    // most common.
+    //
+    // 1. Parent process thinks gauge is NeverImport: no data sent, and we
+    //    do not run this loop for such a gauge. Only if the parent process
+    //    thinks the gauge is Accumulate do we consider cases 2-4.
+    // 2. Child thinks gauge is Accumulate : data is combined in
+    //    gauge_ref.add() below.
+    // 3. Child thinks gauge is NeverImport: we skip this loop entry via
+    //    'continue'. This only happens with a code-change where the child
+    //    contains a code-change relative to parent, switching a Gauge from
+    //    Accumulate to NeverImport.
+    // 4. Child has not yet initialized gauge yet -- this merge is the
+    //    first time the child learns of the gauge. It's possible the child
+    //    will think the gauge is NeverImport due to a code change. But for
+    //    now we will leave the gauge in the child process as
+    //    import_mode==Uninitialized, and accumulate the parent value in
+    //    gauge_ref.add(). Gauges in this mode will not be included in
+    //    stats-sinks or the admin /stats calls, until the child initializes
+    //    the gauge, in which case:
+    // 4a. Child later initializes gauges as NeverImport: the parent value is
+    //     cleared during the mergeImportMode call.
+    // 4b. Child later initializes gauges as Accumulate: the parent value is
+    //     retained.
+
     StatNameManagedStorage storage(gauge.first, temp_scope_->symbolTable());
     StatName stat_name = storage.statName();
     absl::optional<std::reference_wrapper<const Gauge>> gauge_opt =
         temp_scope_->findGauge(stat_name);
 
-    // If the stat named in the protobuf map is already initialized, and has a
-    // mode of NeverImport, then we simply skip over the map entry. This is a
-    // case where a new revision of Envoy has been built where a previously
-    // Accumulated gauge has been switched to NeverImport mode.
     Gauge::ImportMode import_mode = Gauge::ImportMode::Uninitialized;
     if (gauge_opt) {
       import_mode = gauge_opt->get().importMode();
@@ -32,23 +51,16 @@ void StatMerger::mergeGauges(const Protobuf::Map<std::string, uint64_t>& gauges)
       }
     }
 
-    // We establish here a tentative import-mode of Uninitialized. Gauges in
-    // this mode will not be reported in stats sinks or in the admin /stats
-    // endpoint. However, we'll retain the transferred value, and if the running
-    // system initialized the stat as Accumulate, we'll have the accumulated
-    // value ready to go. If the system re-initializes it as NeverImport, we'll
-    // clear the value during the mergeImportMode call.
     auto& gauge_ref = temp_scope_->gaugeFromStatName(stat_name, import_mode);
     uint64_t& parent_value_ref = parent_gauge_values_[gauge_ref.statName()];
     uint64_t old_parent_value = parent_value_ref;
     uint64_t new_parent_value = gauge.second;
     parent_value_ref = new_parent_value;
 
-    if (new_parent_value > old_parent_value) {
-      gauge_ref.add(new_parent_value - old_parent_value);
-    } else {
-      gauge_ref.sub(old_parent_value - new_parent_value);
-    }
+    // Note that new_parent_value may be less than old_parent_value, in which
+    // case 2s complement does its magic (-1 == 0xffffffffffffffff) and adding
+    // that to the gauge's current value works the same as subtraction.
+    gauge_ref.add(new_parent_value - old_parent_value);
   }
 }
 
