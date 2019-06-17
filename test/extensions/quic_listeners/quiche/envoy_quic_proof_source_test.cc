02e4fd947985403a517ca2222dbe8b9016b33731
Dan Zhang <danzh@google.com>
2019-06-17 14:39:56 -0400

    actually add test file
    Signed-off-by: Dan Zhang <danzh@google.com>

---
+++ test/extensions/quic_listeners/quiche/envoy_quic_proof_source_test.cc
@@ -0,0 +1,71 @@
+#include <string>
+#include <vector>
+
+#include "extensions/quic_listeners/quiche/envoy_quic_fake_proof_source.h"
+#include "extensions/quic_listeners/quiche/envoy_quic_fake_proof_verifier.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace Envoy {
+
+namespace Quic {
+
+class TestGetProofCallback : public quic::ProofSource::Callback {
+public:
+  TestGetProofCallback(const std::string& signature, const std::string& leaf_cert_scts,
+                       const std::vector<std::string>& certs)
+      : expected_signature_(signature), expected_leaf_certs_scts_(leaf_cert_scts),
+        expected_certs_(certs) {}
+
+  // quic::ProofSource::Callback
+  void Run(bool ok, const quic::QuicReferenceCountedPointer<quic::ProofSource::Chain>& chain,
+           const quic::QuicCryptoProof& proof,
+           std::unique_ptr<quic::ProofSource::Details> details) override {
+    EXPECT_TRUE(ok);
+    EXPECT_EQ(expected_signature_, proof.signature);
+    EXPECT_EQ(expected_leaf_certs_scts_, proof.leaf_cert_scts);
+    EXPECT_EQ(expected_certs_, chain->certs);
+    EXPECT_EQ(nullptr, details);
+  }
+
+private:
+  std::string expected_signature_;
+  std::string expected_leaf_certs_scts_;
+  std::vector<std::string> expected_certs_;
+};
+
+class EnvoyQuicProofSourceTest : public ::testing::Test {
+protected:
+  std::string hostname_{"www.dummy.com"};
+  quic::QuicSocketAddress server_address_;
+  quic::QuicTransportVersion version_{quic::QUIC_VERSION_UNSUPPORTED};
+  quic::QuicStringPiece chlo_hash_{""};
+  std::string server_config_{"Server Config"};
+  std::vector<std::string> expected_certs_{absl::StrCat(dummy_cert_prefix, hostname_)};
+  std::string expected_signature_{absl::StrCat("Dummy signature for { ", server_config_, " }")};
+  EnvoyQuicFakeProofSource proof_source_;
+  EnvoyQuicFakeProofVerifier proof_verifier_;
+};
+
+TEST_F(EnvoyQuicProofSourceTest, TestGetProof) {
+  auto callback = std::make_unique<TestGetProofCallback>(expected_signature_, "Dummy timestamp",
+                                                         expected_certs_);
+  proof_source_.GetProof(server_address_, hostname_, server_config_, version_, chlo_hash_,
+                         std::move(callback));
+}
+
+TEST_F(EnvoyQuicProofSourceTest, TestVerifyProof) {
+  EXPECT_EQ(quic::QUIC_SUCCESS,
+            proof_verifier_.VerifyProof(hostname_, /*port=*/0, server_config_, version_, chlo_hash_,
+                                        expected_certs_, "Dummy timestamp", expected_signature_,
+                                        nullptr, nullptr, nullptr, nullptr));
+  std::vector<std::string> wrong_certs{"wrong cert"};
+  EXPECT_EQ(quic::QUIC_FAILURE,
+            proof_verifier_.VerifyProof(hostname_, /*port=*/0, server_config_, version_, chlo_hash_,
+                                        wrong_certs, "Dummy timestamp", expected_signature_,
+                                        nullptr, nullptr, nullptr, nullptr));
+}
+
+} // namespace Quic
+} // namespace Envoy
