e073771553f73b5ca074fe62b3a020c62512cf77
Dylan Carney <dcarney@gmail.com>
2019-06-18 11:36:49 -0700

    Adds overrides for new validateHeaders() method in fuzz test
    Signed-off-by: Dylan Carney <dcarney@gmail.com>

--- test/common/http/conn_manager_impl_fuzz_test.cc
+++ test/common/http/conn_manager_impl_fuzz_test.cc
@@ -110,6 +110,7 @@ public:
   bool proxy100Continue() const override { return proxy_100_continue_; }
   const Http::Http1Settings& http1Settings() const override { return http1_settings_; }
   bool shouldNormalizePath() const override { return false; }
+  bool validateHeaders() const override { return false; }
 
   const envoy::config::filter::network::http_connection_manager::v2::HttpConnectionManager config_;
   std::list<AccessLog::InstanceSharedPtr> access_logs_;
@@ -142,6 +143,7 @@ public:
   Http::Http1Settings http1_settings_;
   Http::DefaultInternalAddressConfig internal_address_config_;
   bool normalize_path_{true};
+  bool validate_headers_{false};
 };
 
 // Internal representation of stream state. Encapsulates the stream state, mocks
