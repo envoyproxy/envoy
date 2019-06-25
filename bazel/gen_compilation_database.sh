c0aecb691bdae36c129f7330bf9dfb131955f4a6
Keith Smiley <keithbsmiley@gmail.com>
2019-06-25 11:29:25 -0700

    Temporarily change compilation database URL
    Signed-off-by: Keith Smiley <keithbsmiley@gmail.com>

--- bazel/gen_compilation_database.sh
+++ bazel/gen_compilation_database.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #TODO(lizan): revert to released version once new version is released
-RELEASE_VERSION=d5a0ee259aa356886618eafae17ca05ebf79d6c2
+#TODO(keith): revert after https://github.com/grailbio/bazel-compilation-database/pull/30 is resolved
+RELEASE_VERSION=078319b6a2ed88915d817d4a4142e1adfb77d727
 
 if [[ ! -d bazel-compilation-database-${RELEASE_VERSION} ]]; then
-  curl -L https://github.com/grailbio/bazel-compilation-database/archive/${RELEASE_VERSION}.tar.gz | tar -xz
+  curl -L https://github.com/keith/bazel-compilation-database/archive/${RELEASE_VERSION}.tar.gz | tar -xz
 fi
 
 bazel-compilation-database-${RELEASE_VERSION}/generate.sh $@
