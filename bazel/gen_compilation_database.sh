c89d0b121b0d6aed534b321fd40c91a3f6d342ee
Keith Smiley <keithbsmiley@gmail.com>
2019-06-25 13:29:00 -0700

    Update version
    Signed-off-by: Keith Smiley <keithbsmiley@gmail.com>

--- bazel/gen_compilation_database.sh
+++ bazel/gen_compilation_database.sh
@@ -2,7 +2,7 @@
 
 #TODO(lizan): revert to released version once new version is released
 #TODO(keith): revert after https://github.com/grailbio/bazel-compilation-database/pull/30 is resolved
-RELEASE_VERSION=078319b6a2ed88915d817d4a4142e1adfb77d727
+RELEASE_VERSION=436399bfe9dbd6455d029722bff2948f2d7a1f04
 
 if [[ ! -d bazel-compilation-database-${RELEASE_VERSION} ]]; then
   curl -L https://github.com/keith/bazel-compilation-database/archive/${RELEASE_VERSION}.tar.gz | tar -xz
