269d4ade25c5ffc8997bf8946e137a933169886d
Keith Smiley <keithbsmiley@gmail.com>
2019-06-25 16:36:05 -0700

    Go back to upstream version with merged PR
    Signed-off-by: Keith Smiley <keithbsmiley@gmail.com>

--- bazel/gen_compilation_database.sh
+++ bazel/gen_compilation_database.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 #TODO(lizan): revert to released version once new version is released
-#TODO(keith): revert after https://github.com/grailbio/bazel-compilation-database/pull/30 is resolved
-RELEASE_VERSION=e65a17bef735ef64457221800207f6c295a6c8aa
+RELEASE_VERSION=fd92bf39666596cab609edf7b8e221307d6d325a
 
 if [[ ! -d bazel-compilation-database-${RELEASE_VERSION} ]]; then
-  curl -L https://github.com/keith/bazel-compilation-database/archive/${RELEASE_VERSION}.tar.gz | tar -xz
+  curl -L https://github.com/grailbio/bazel-compilation-database/archive/${RELEASE_VERSION}.tar.gz | tar -xz
 fi
 
 bazel-compilation-database-${RELEASE_VERSION}/generate.sh $@
