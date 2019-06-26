953284d56f42dbe9fe89805441f9b0fc7479e362
Keith Smiley <keithbsmiley@gmail.com>
2019-06-25 17:15:51 -0700

    Go back to tags
    Signed-off-by: Keith Smiley <keithbsmiley@gmail.com>

--- bazel/gen_compilation_database.sh
+++ bazel/gen_compilation_database.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#TODO(lizan): revert to released version once new version is released
-RELEASE_VERSION=fd92bf39666596cab609edf7b8e221307d6d325a
+RELEASE_VERSION=0.3.5
 
 if [[ ! -d bazel-compilation-database-${RELEASE_VERSION} ]]; then
   curl -L https://github.com/grailbio/bazel-compilation-database/archive/${RELEASE_VERSION}.tar.gz | tar -xz
