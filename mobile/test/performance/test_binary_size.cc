ecdda7d9a127bea984d33cc17dd0aecb66f092bb
Jose Ulises Nino Rivera <junr03@users.noreply.github.com>
2019-07-01 15:07:40 -0700

    perf: update test binary size target (#212)
    Signed-off-by: Jose Nino jnino@lyft.com
    
    Description: after #173 landed this target broke due to the breaking API change. This target will be added to CI once #181 closes, so breakages like this will not go undected.
    Risk Level: low - updating API, deleting old build rules.
    
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/test/performance/test_binary_size.cc
+++ mobile/test/performance/test_binary_size.cc
@@ -1,8 +1,8 @@
-#include "main_interface.h"
+#include "library/common/main_interface.h"
 
 // NOLINT(namespace-envoy)
 
 // This binary is used to perform stripped down binary size investigations of the Envoy codebase.
 // Please refer to the development docs for more information:
 // https://envoy-mobile.github.io/docs/envoy-mobile/latest/development/performance/binary_size.html
-int main() { return run_envoy(nullptr); }
+int main() { return run_envoy(nullptr, nullptr); }
