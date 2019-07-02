b20b985770e417d73a958cd5e670777faf1339d4
Dmitry Rozhkov <dmitry.rozhkov@linux.intel.com>
2019-07-02 20:44:25 +0300

    mark throwException() as noreturn
    Signed-off-by: Dmitry Rozhkov <dmitry.rozhkov@linux.intel.com>

--- source/server/ssl_context_manager.cc
+++ source/server/ssl_context_manager.cc
@@ -16,7 +16,6 @@ class SslContextManagerNoTlsStub final : public Envoy::Ssl::ContextManager {
   createSslClientContext(Stats::Scope& /* scope */,
                          const Envoy::Ssl::ClientContextConfig& /* config */) override {
     throwException();
-    return nullptr;
   }
 
   Ssl::ServerContextSharedPtr
@@ -24,7 +23,6 @@ class SslContextManagerNoTlsStub final : public Envoy::Ssl::ContextManager {
                          const Envoy::Ssl::ServerContextConfig& /* config */,
                          const std::vector<std::string>& /* server_names */) override {
     throwException();
-    return nullptr;
   }
 
   size_t daysUntilFirstCertExpires() const override { return std::numeric_limits<int>::max(); }
@@ -32,7 +30,9 @@ class SslContextManagerNoTlsStub final : public Envoy::Ssl::ContextManager {
   void iterateContexts(std::function<void(const Envoy::Ssl::Context&)> /* callback */) override{};
 
 private:
-  void throwException() { throw EnvoyException("SSL is not supported in this configuration"); }
+  [[noreturn]] void throwException() {
+    throw EnvoyException("SSL is not supported in this configuration");
+  }
 };
 
 Ssl::ContextManagerPtr createContextManager(const std::string& factory_name,
