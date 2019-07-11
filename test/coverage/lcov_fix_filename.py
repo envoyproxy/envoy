78747812dd7842090358e697ddbe98101bdb8a7a
Lizan Zhou <lizan@tetrate.io>
2019-07-11 19:31:39 +0000

    format
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- test/coverage/lcov_fix_filename.py
+++ test/coverage/lcov_fix_filename.py
@@ -4,10 +4,10 @@ import sys
 import os.path
 
 while True:
-    line = sys.stdin.readline()
-    if not line:
-        break
-    if line.startswith("SF:"):
-        filename = line[3:-1]
-        line = "SF:" + os.path.relpath(os.path.realpath(filename)) + "\n"
-    sys.stdout.write(line)
+  line = sys.stdin.readline()
+  if not line:
+    break
+  if line.startswith("SF:"):
+    filename = line[3:-1]
+    line = "SF:" + os.path.relpath(os.path.realpath(filename)) + "\n"
+  sys.stdout.write(line)
