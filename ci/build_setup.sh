fc58f446397dbb84b2f6624777d3d640ad7c3ad5
Lizan Zhou <lizan@tetrate.io>
2019-07-11 18:26:25 +0000

    fix filter-example WORKSPACE
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/build_setup.sh
+++ ci/build_setup.sh
@@ -85,7 +85,7 @@ if [ "$1" != "-nofetch" ]; then
 
   # This is the hash on https://github.com/envoyproxy/envoy-filter-example.git we pin to.
   (cd "${ENVOY_FILTER_EXAMPLE_SRCDIR}" && git fetch origin && git checkout -f dcd3374baa9365ab7ab505018232994d6c8a8d81)
-  cp -f "${ENVOY_SRCDIR}"/ci/WORKSPACE.filter.example "${ENVOY_FILTER_EXAMPLE_SRCDIR}"/WORKSPACE
+  sed -e "s|{ENVOY_SRCDIR}|${ENVOY_SRCDIR}|" "${ENVOY_SRCDIR}"/ci/WORKSPACE.filter.example > "${ENVOY_FILTER_EXAMPLE_SRCDIR}"/WORKSPACE
 fi
 
 # Also setup some space for building Envoy standalone.
