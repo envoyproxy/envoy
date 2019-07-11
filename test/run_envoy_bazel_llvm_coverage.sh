34885f39c91eb50986f126dd2e5938b5a37ac55f
Lizan Zhou <lizan@tetrate.io>
2019-07-11 21:20:13 +0000

    no lcov report
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- test/run_envoy_bazel_llvm_coverage.sh
+++ test/run_envoy_bazel_llvm_coverage.sh
@@ -4,21 +4,23 @@ set -e
 
 [[ -z "${SRCDIR}" ]] && SRCDIR="${PWD}"
 [[ -z "${BAZEL_COVERAGE}" ]] && BAZEL_COVERAGE=bazel
-[[ -z "${WORKSPACE}" ]] && WORKSPACE=envoy
 
 echo "Starting run_envoy_bazel_coverage.sh..."
 echo "    PWD=$(pwd)"
 echo "    SRCDIR=${SRCDIR}"
-echo "    GCOVR_DIR=${GCOVR_DIR}"
-echo "    TESTLOGS_DIR=${TESTLOGS_DIR}"
 echo "    BAZEL_COVERAGE=${BAZEL_COVERAGE}"
-echo "    GCOVR=${GCOVR}"
-echo "    WORKSPACE=${WORKSPACE}"
 echo "    VALIDATE_COVERAGE=${VALIDATE_COVERAGE}"
 
 # This is the target that will be run to generate coverage data. It can be overridden by consumer
 # projects that want to run coverage on a different/combined target.
-[[ -z "${COVERAGE_TARGET}" ]] && COVERAGE_TARGET="//test/..."
+# Command-line arguments take precedence over ${COVERAGE_TARGET}.
+if [[ $# -gt 0 ]]; then
+  COVERAGE_TARGETS=$*
+elif [[ -n "${COVERAGE_TARGET}" ]]; then
+  COVERAGE_TARGETS=${COVERAGE_TARGET}
+else
+  COVERAGE_TARGETS=//test/...
+fi
 
 rm -rf $(find -L bazel-bin -name "test-*.profraw")
 
@@ -35,16 +37,7 @@ llvm-profdata merge -sparse $(find -L bazel-bin -name "test-*.profraw") -o ${COV
 
 echo "Generating report..."
 llvm-cov show bazel-bin/source/exe/envoy-static -instr-profile=${COVERAGE_DIR}/coverage.profdata \
-  -ignore-filename-regex='(/external/|/k8-fastbuild/bin/|/chromium_url/)' -output-dir=${COVERAGE_DIR}/llvm-cov -format=html
-sed -i -e 's|>bazel-out/[^/]*/bin/\([^/]*\)/[^<]*/_virtual_includes/[^/]*|>\1|g' "${COVERAGE_DIR}/llvm-cov/index.html"
-
-echo "Generating lcov report..."
-llvm-cov export bazel-bin/source/exe/envoy-static -instr-profile=${COVERAGE_DIR}/coverage.profdata \
-  -ignore-filename-regex='(/external/|/k8-fastbuild/bin/|/chromium_url/)' -format=lcov | \
-  test/coverage/lcov_fix_filename.py > ${COVERAGE_DIR}/envoy.lcov
-
-echo "Generating HTML report from lcov..."
-genhtml ${COVERAGE_DIR}/envoy.lcov --output-directory=${COVERAGE_DIR}/lcov
-
+  -ignore-filename-regex='(/external/|/k8-fastbuild/bin/|/chromium_url/)' -output-dir=${COVERAGE_DIR} -format=html
+sed -i -e 's|>bazel-out/[^/]*/bin/\([^/]*\)/[^<]*/_virtual_includes/[^/]*|>\1|g' "${COVERAGE_DIR}/index.html"
 
 [[ -z "${ENVOY_COVERAGE_DIR}" ]] || rsync -av "${COVERAGE_DIR}"/ "${ENVOY_COVERAGE_DIR}"
