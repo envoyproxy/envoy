152f48c937fa9ff9170fadb7e814bf47e145b9f5
Michael Rebello <me@michaelrebello.com>
2019-07-12 12:50:55 -0700

    swift: add docstrings to main Envoy class (#253)
    Signed-off-by: Michael Rebello <mrebello@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/swift/src/Envoy.swift
+++ mobile/library/swift/src/Envoy.swift
@@ -4,14 +4,20 @@ import Foundation
 public final class Envoy: NSObject {
   private let runner: EnvoyRunner
 
+  /// Indicates whether this Envoy instance is currently active and running.
   public var isRunning: Bool {
     return self.runner.isExecuting
   }
 
+  /// Indicates whether the Envoy instance is terminated.
   public var isTerminated: Bool {
     return self.runner.isFinished
   }
 
+  /// Initialize a new Envoy instance.
+  ///
+  /// - parameter config:   Configuration file that is recognizable by Envoy (YAML).
+  /// - parameter logLevel: Log level to use for this instance.
   public init(config: String, logLevel: LogLevel = .info) {
     self.runner = EnvoyRunner(config: config, logLevel: logLevel)
     self.runner.start()
