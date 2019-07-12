aa03694c1655d4d3323c7a7a75602781ecc14286
Lizan Zhou <lizan@tetrate.io>
2019-07-12 01:02:39 +0000

    fix
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- test/run_envoy_bazel_llvm_coverage.sh
+++ test/run_envoy_bazel_llvm_coverage.sh
@@ -22,9 +22,10 @@ fi
 
 rm -rf $(find -L bazel-bin -name "test-*.profraw")
 
-BAZEL_USE_LLVM_NATIVE_COVERAGE=1 GCOV=llvm-profdata bazel coverage \
-    ${BAZEL_BUILD_OPTIONS} -c fastbuild --copt=-DNDEBUG --instrumentation_filter=//source/...,//include/... \
-    ${COVERAGE_TARGETS}
+BAZEL_USE_LLVM_NATIVE_COVERAGE=1 GCOV=llvm-profdata bazel coverage ${BAZEL_BUILD_OPTIONS} \
+    -c fastbuild --copt=-DNDEBUG --instrumentation_filter=//source/...,//include/... --dynamic_mode=off \
+    --strategy=TestRunner=local --test_sharding_strategy=disabled \
+    --test_env=HEAPCHECK= --test_filter='-QuicPlatformTest.QuicStackTraceTest' ${COVERAGE_TARGETS}
 
 COVERAGE_DIR="${SRCDIR}"/generated/coverage
 mkdir -p "${COVERAGE_DIR}"
@@ -34,7 +35,8 @@ llvm-profdata merge -sparse $(find -L bazel-out -name coverage.dat) -o ${COVERAG
 
 echo "Generating report..."
 llvm-cov show bazel-bin/source/exe/envoy-static -instr-profile=${COVERAGE_DIR}/coverage.profdata \
-  -ignore-filename-regex='(/external/|pb\.(validate\.)?(h|cc)|/chromium_url/)' -output-dir=${COVERAGE_DIR} -format=html
+  -ignore-filename-regex='(/external/|pb\.(validate\.)?(h|cc)|/chromium_url/|/tmp)' -output-dir=${COVERAGE_DIR} -format=html
+sed -i -e 's|>proc/self/cwd/|>|g' "${COVERAGE_DIR}/index.html"
 sed -i -e 's|>bazel-out/[^/]*/bin/\([^/]*\)/[^<]*/_virtual_includes/[^/]*|>\1|g' "${COVERAGE_DIR}/index.html"
 
 [[ -z "${ENVOY_COVERAGE_DIR}" ]] || rsync -av "${COVERAGE_DIR}"/ "${ENVOY_COVERAGE_DIR}"
