8f6c92f36fe6546e51f49d442ece8366e6b61ba1
Lizan Zhou <lizan@tetrate.io>
2019-07-16 05:54:53 +0000

    real memory fix
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- test/integration/stats_integration_test.cc
+++ test/integration/stats_integration_test.cc
@@ -220,7 +220,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithStats) {
   // On a local clang8/libstdc++/linux flow, the memory usage was observed in
   // June 2019 to be 64 bytes higher than it is in CI/release. Your mileage may
   // vary.
-  EXPECT_MEMORY_EQ(m_per_cluster, 42742); // 104 bytes higher than a debug build.
+  EXPECT_MEMORY_EQ(m_per_cluster, 42806); // 104 bytes higher than a debug build.
   EXPECT_MEMORY_LE(m_per_cluster, 43000);
 }
 
