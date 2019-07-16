9544379e651ea7bca0866961e96be44f833df10b
Lizan Zhou <lizan@tetrate.io>
2019-07-16 22:41:06 +0000

    remove old configs
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- bazel/toolchains/regenerate.sh
+++ bazel/toolchains/regenerate.sh
@@ -4,6 +4,7 @@ set -e
 
 export RBE_AUTOCONF_ROOT=$(bazel info workspace)
 
+rm -rf "${RBE_AUTOCONF_ROOT}/bazel/toolchains/configs/*"
 cp -vf "${RBE_AUTOCONF_ROOT}/bazel/toolchains/empty.bzl" "${RBE_AUTOCONF_ROOT}/bazel/toolchains/configs/versions.bzl"
 
 # Bazel query is the right command so bazel won't fail itself.
