88e5641dba56040b60091b1603abad029f668f54
Mike Schore <mike.schore@gmail.com>
2019-07-16 12:44:12 -0700

    build: remove extraneous inclusion of envoy_package (#228)
    Signed-off-by: Mike Schore <mike.schore@gmail.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/tools/check_format.sh
+++ mobile/tools/check_format.sh
@@ -10,10 +10,9 @@ fi
 
 # TODO(mattklein123): WORKSPACE is excluded due to warning about @bazel_tools reference. Fix here
 #                     or in the upstream checker.
-# TODO(mattklein123): We don't need envoy_package() in various files. Somehow fix in upstream
-#                     checker.
 envoy/tools/check_format.py \
     --add-excluded-prefixes ./envoy/ ./envoy_build_config/extensions_build_config.bzl ./WORKSPACE ./dist/Envoy.framework/ \
     --skip_envoy_build_rule_check "$ENVOY_FORMAT_ACTION" \
-    --namespace_check_excluded_paths ./examples/ ./library/java/ ./library/kotlin ./library/objective-c
+    --namespace_check_excluded_paths ./examples/ ./library/java/ ./library/kotlin ./library/objective-c \
+    --build_fixer_check_excluded_paths ./BUILD ./dist ./examples ./library/java ./library/kotlin ./library/objective-c ./library/swift
 envoy/tools/format_python_tools.sh check
