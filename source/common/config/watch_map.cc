c3628c414b4d06df71727dbc940537d3a7d59083
Fred Douglas <fredlas@google.com>
2019-07-19 16:08:59 -0400

    add watch_map_test
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- source/common/config/watch_map.cc
+++ source/common/config/watch_map.cc
@@ -154,10 +154,8 @@ WatchMap::findRemovals(const std::vector<std::string>& newly_removed_from_watch,
   std::set<std::string> newly_removed_from_subscription;
   for (const auto& name : newly_removed_from_watch) {
     auto entry = watch_interest_.find(name);
-    if (entry == watch_interest_.end()) {
-      ENVOY_LOG(warn, "WatchMap: tried to remove a watch from untracked resource {}", name);
-      continue;
-    }
+    ASSERT(entry != watch_interest_.end(),
+           fmt::format("WatchMap: tried to remove a watch from untracked resource {}", name));
 
     entry->second.erase(watch);
     if (entry->second.empty()) {
