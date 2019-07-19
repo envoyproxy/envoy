764736508ae06f6f526449ec28231e47cb257875
Rama Chavali <rama.rao@salesforce.com>
2019-07-19 22:15:08 +0530

    upstream: improve original dst logs (#7633)
    Currently when original dst validation occurs, it does not very clear what lb type_cluster type combination is causing that. This PR improves that log line.
    Risk Level: Low
    Testing: Added automated tests
    Docs Changes: N/A
    Release Notes: N/A
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- source/common/upstream/upstream_impl.cc
+++ source/common/upstream/upstream_impl.cc
@@ -595,7 +595,10 @@ ClusterInfoImpl::ClusterInfoImpl(const envoy::api::v2::Cluster& config,
   case envoy::api::v2::Cluster::ORIGINAL_DST_LB:
     if (config.type() != envoy::api::v2::Cluster::ORIGINAL_DST) {
       throw EnvoyException(fmt::format(
-          "cluster: LB type 'original_dst_lb' may only be used with cluster type 'original_dst'"));
+          "cluster: LB policy {} is not valid for Cluster type {}. Only 'original_dst_lb' "
+          "is allowed with cluster type 'original_dst'",
+          envoy::api::v2::Cluster_LbPolicy_Name(config.lb_policy()),
+          envoy::api::v2::Cluster_DiscoveryType_Name(config.type())));
     }
     lb_type_ = LoadBalancerType::OriginalDst;
     break;
