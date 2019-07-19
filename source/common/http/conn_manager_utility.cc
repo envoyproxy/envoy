eb0c73162d2cebbd46ab1a32f566b19603028c6a
Ruslan Nigmatullin <elessar@dropbox.com>
2019-07-18 17:19:18 -0700

    address cr for comments
    Signed-off-by: Ruslan Nigmatullin <elessar@dropbox.com>

--- source/common/http/conn_manager_utility.cc
+++ source/common/http/conn_manager_utility.cc
@@ -396,6 +396,7 @@ bool ConnectionManagerUtility::maybeNormalizePath(HeaderMap& request_headers,
   if (config.shouldNormalizePath()) {
     is_valid_path = PathUtil::canonicalPath(*request_headers.Path());
   }
+  // Merge slashes after path normarlization to catch potential edge cases with percent encoding.
   if (config.shouldMergeSlashes()) {
     PathUtil::mergeSlashes(*request_headers.Path());
   }
