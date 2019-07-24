7257256af16fe880faa5dc2bf200e1a2d147989d
Michael Rebello <me@michaelrebello.com>
2019-07-24 13:53:07 -0700

    objc: use headerdoc for EnvoyEngine
    Signed-off-by: Michael Rebello <me@michaelrebello.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/objective-c/EnvoyEngine.h
+++ mobile/library/objective-c/EnvoyEngine.h
@@ -3,12 +3,21 @@
 /// Wrapper layer to simplify calling into Envoy's C++ API.
 @interface EnvoyEngine : NSObject
 
-/// Run the Envoy engine with the provided config and log level. This call is synchronous
-/// and will not yield.
+/**
+ Run the Envoy engine with the provided config and log level.
+
+ @param config The configuration file with which to start Envoy.
+ @return A status indicating if the action was successful.
+ */
 + (int)runWithConfig:(NSString *)config;
 
-/// Run the Envoy engine with the provided config and log level. This call is synchronous
-/// and will not yield.
+/**
+ Run the Envoy engine with the provided config and log level.
+
+ @param config The configuration file with which to start Envoy.
+ @param logLevel The log level to use when starting Envoy.
+ @return A status indicating if the action was successful.
+ */
 + (int)runWithConfig:(NSString *)config logLevel:(NSString *)logLevel;
 
 @end
