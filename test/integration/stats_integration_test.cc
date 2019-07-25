e1787cb6eed17c57af1a0dc65516aa3d1d24c559
Joshua Marantz <jmarantz@google.com>
2019-07-25 06:20:59 -0400

    pedantic spelling
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/integration/stats_integration_test.cc
+++ test/integration/stats_integration_test.cc
@@ -211,7 +211,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithStats) {
   // 2019/06/30  7428     42742       43000   remove stats multiple inheritance, inline HeapStatData
   // 2019/07/06  7477     42742       43000   fork gauge representation to drop pending_increment_
   // 2019/07/15  7555     42806       43000   static link libstdc++ in tests
-  // 2019/07/24  xxxx     42798       43000   share main symtab for ClusterInfo's isolated stats.
+  // 2019/07/24  xxxx     42798       43000   Share SymbolTable for ClusterInfo's isolated stats
 
   // Note: when adjusting this value: EXPECT_MEMORY_EQ is active only in CI
   // 'release' builds, where we control the platform and tool-chain. So you
