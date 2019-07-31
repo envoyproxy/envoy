614bc5b116252ef09a0c843b51b912b99ac46081
alyssawilk <alyssar@chromium.org>
2019-07-31 13:04:51 -0700

    runtime: making runtime accessible from non-worker threads (#7695) (#27)
    Making runtime accessible for non-worker threads, and using the new accessor for runtime features.
    
    This allows the work done in #7601, moving the strict HTTP checks out of the HCM and into the codec, where the integration tests use them from client/server threads, and other downstream Envoys might use them from non-worker threads as well.
    
    Risk Level: High (affects runtime access for all runtime features)
    Testing: new unit tests, integration tests use in #7601
    Docs Changes: n/a
    Release Notes: n/a
    
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/mocks/thread_local/mocks.h
+++ test/mocks/thread_local/mocks.h
@@ -58,6 +58,7 @@ public:
 
     // ThreadLocal::Slot
     ThreadLocalObjectSharedPtr get() override { return parent_.data_[index_]; }
+    bool currentThreadRegistered() override { return parent_.registered_; }
     void runOnAllThreads(Event::PostCb cb) override { parent_.runOnAllThreads(cb); }
     void runOnAllThreads(Event::PostCb cb, Event::PostCb main_callback) override {
       parent_.runOnAllThreads(cb, main_callback);
@@ -72,6 +73,7 @@ public:
   testing::NiceMock<Event::MockDispatcher> dispatcher_;
   std::vector<ThreadLocalObjectSharedPtr> data_;
   bool shutdown_{};
+  bool registered_{true};
 };
 
 } // namespace ThreadLocal
