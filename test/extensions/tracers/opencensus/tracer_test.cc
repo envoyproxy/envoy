9603803f31952483952eed948be1613205d6ee57
Emil Mikulic <g-easy@users.noreply.github.com>
2019-08-01 14:43:45 +1000

    tracing: Add B3 support in OpenCensus driver.
    This makes it possible to propagate, generate, and translate
    B3-format trace context headers.
    
    Signed-off-by: Emil Mikulic <g-easy@users.noreply.github.com>

--- test/extensions/tracers/opencensus/tracer_test.cc
+++ test/extensions/tracers/opencensus/tracer_test.cc
@@ -19,6 +19,7 @@
 #include "gtest/gtest.h"
 #include "opencensus/trace/exporter/span_data.h"
 #include "opencensus/trace/exporter/span_exporter.h"
+#include "opencensus/trace/propagation/b3.h"
 #include "opencensus/trace/propagation/cloud_trace_context.h"
 #include "opencensus/trace/propagation/grpc_trace_bin.h"
 #include "opencensus/trace/propagation/trace_context.h"
@@ -165,74 +166,86 @@ TEST(OpenCensusTracerTest, Span) {
 TEST(OpenCensusTracerTest, PropagateTraceContext) {
   registerSpanCatcher();
   // The test calls the helper with each kind of incoming context in turn.
-  auto helper = [](const std::string& header, const std::string& value) {
-    OpenCensusConfig oc_config;
-    NiceMock<LocalInfo::MockLocalInfo> local_info;
-    oc_config.add_incoming_trace_context(OpenCensusConfig::NONE);
-    oc_config.add_incoming_trace_context(OpenCensusConfig::TRACE_CONTEXT);
-    oc_config.add_incoming_trace_context(OpenCensusConfig::GRPC_TRACE_BIN);
-    oc_config.add_incoming_trace_context(OpenCensusConfig::CLOUD_TRACE_CONTEXT);
-    oc_config.add_outgoing_trace_context(OpenCensusConfig::NONE);
-    oc_config.add_outgoing_trace_context(OpenCensusConfig::TRACE_CONTEXT);
-    oc_config.add_outgoing_trace_context(OpenCensusConfig::GRPC_TRACE_BIN);
-    oc_config.add_outgoing_trace_context(OpenCensusConfig::CLOUD_TRACE_CONTEXT);
-    std::unique_ptr<Tracing::Driver> driver(new OpenCensus::Driver(oc_config, local_info));
-    NiceMock<Tracing::MockConfig> config;
-    Http::TestHeaderMapImpl request_headers{
-        {":path", "/"},
-        {":method", "GET"},
-        {"x-request-id", "foo"},
-        {header, value},
-    };
-    const std::string operation_name{"my_operation_2"};
-    SystemTime start_time;
-    Http::TestHeaderMapImpl injected_headers;
-    {
-      Tracing::SpanPtr span = driver->startSpan(config, request_headers, operation_name, start_time,
-                                                {Tracing::Reason::Sampling, false});
-      span->injectContext(injected_headers);
-      span->finishSpan();
-    }
+  auto test_incoming =
+      [](const std::initializer_list<std::pair<const char*, const char*>>& headers) {
+        using Envoy::Http::LowerCaseString;
+        OpenCensusConfig oc_config;
+        NiceMock<LocalInfo::MockLocalInfo> local_info;
+        oc_config.add_incoming_trace_context(OpenCensusConfig::NONE);
+        oc_config.add_incoming_trace_context(OpenCensusConfig::B3);
+        oc_config.add_incoming_trace_context(OpenCensusConfig::TRACE_CONTEXT);
+        oc_config.add_incoming_trace_context(OpenCensusConfig::GRPC_TRACE_BIN);
+        oc_config.add_incoming_trace_context(OpenCensusConfig::CLOUD_TRACE_CONTEXT);
+        oc_config.add_outgoing_trace_context(OpenCensusConfig::NONE);
+        oc_config.add_outgoing_trace_context(OpenCensusConfig::B3);
+        oc_config.add_outgoing_trace_context(OpenCensusConfig::TRACE_CONTEXT);
+        oc_config.add_outgoing_trace_context(OpenCensusConfig::GRPC_TRACE_BIN);
+        oc_config.add_outgoing_trace_context(OpenCensusConfig::CLOUD_TRACE_CONTEXT);
+        std::unique_ptr<Tracing::Driver> driver(new OpenCensus::Driver(oc_config, local_info));
+        NiceMock<Tracing::MockConfig> config;
+        Http::TestHeaderMapImpl request_headers{
+            {":path", "/"},
+            {":method", "GET"},
+            {"x-request-id", "foo"},
+        };
+        for (const auto& kv : headers) {
+          request_headers.addCopy(LowerCaseString(kv.first), kv.second);
+        }
 
-    // Retrieve SpanData from the OpenCensus trace exporter.
-    std::vector<SpanData> spans = getSpanCatcher()->catchSpans();
-    ASSERT_EQ(1, spans.size());
-    const auto& sd = spans[0];
-    ENVOY_LOG_MISC(debug, "{}", sd.DebugString());
+        const std::string operation_name{"my_operation_2"};
+        SystemTime start_time;
+        Http::TestHeaderMapImpl injected_headers;
+        {
+          Tracing::SpanPtr span = driver->startSpan(config, request_headers, operation_name,
+                                                    start_time, {Tracing::Reason::Sampling, false});
+          span->injectContext(injected_headers);
+          span->finishSpan();
+        }
 
-    // Check contents.
-    EXPECT_TRUE(sd.has_remote_parent());
-    EXPECT_EQ("6162636465666768", sd.parent_span_id().ToHex());
-    EXPECT_EQ("404142434445464748494a4b4c4d4e4f", sd.context().trace_id().ToHex());
-    EXPECT_TRUE(sd.context().trace_options().IsSampled())
-        << "parent was sampled, child should be also";
+        // Retrieve SpanData from the OpenCensus trace exporter.
+        std::vector<SpanData> spans = getSpanCatcher()->catchSpans();
+        ASSERT_EQ(1, spans.size());
+        const auto& sd = spans[0];
+        ENVOY_LOG_MISC(debug, "{}", sd.DebugString());
 
-    // Check injectContext.
-    using Envoy::Http::LowerCaseString;
-    {
-      auto val = injected_headers.get(LowerCaseString("traceparent"));
-      ASSERT_NE(nullptr, val);
-      EXPECT_EQ(::opencensus::trace::propagation::ToTraceParentHeader(sd.context()),
-                val->value().getStringView());
-    }
-    {
-      auto val = injected_headers.get(LowerCaseString("grpc-trace-bin"));
-      ASSERT_NE(nullptr, val);
-      std::string expected = ::opencensus::trace::propagation::ToGrpcTraceBinHeader(sd.context());
-      expected = Base64::encode(expected.data(), expected.size(), /*add_padding=*/false);
-      EXPECT_EQ(expected, val->value().getStringView());
-    }
-    {
-      auto val = injected_headers.get(LowerCaseString("x-cloud-trace-context"));
-      ASSERT_NE(nullptr, val);
-      EXPECT_EQ(::opencensus::trace::propagation::ToCloudTraceContextHeader(sd.context()),
-                val->value().getStringView());
-    }
-  };
+        // Check contents.
+        EXPECT_TRUE(sd.has_remote_parent());
+        EXPECT_EQ("6162636465666768", sd.parent_span_id().ToHex());
+        EXPECT_EQ("404142434445464748494a4b4c4d4e4f", sd.context().trace_id().ToHex());
+        EXPECT_TRUE(sd.context().trace_options().IsSampled())
+            << "parent was sampled, child should be also";
 
-  helper("traceparent", "00-404142434445464748494a4b4c4d4e4f-6162636465666768-01");
-  helper("grpc-trace-bin", "AABAQUJDREVGR0hJSktMTU5PAWFiY2RlZmdoAgE");
-  helper("x-cloud-trace-context", "404142434445464748494a4b4c4d4e4f/7017280452245743464;o=1");
+        // Check injectContext.
+        // The SpanID is unpredictable so re-serialize context to check it.
+        const auto& ctx = sd.context();
+        auto test_outgoing = [&injected_headers](const char* header, std::string expected_value) {
+          auto val = injected_headers.get(LowerCaseString(header));
+          ASSERT_NE(nullptr, val);
+          EXPECT_EQ(expected_value, val->value().getStringView());
+        };
+        test_outgoing("traceparent", ::opencensus::trace::propagation::ToTraceParentHeader(ctx));
+        {
+          std::string expected = ::opencensus::trace::propagation::ToGrpcTraceBinHeader(ctx);
+          expected = Base64::encode(expected.data(), expected.size(), /*add_padding=*/false);
+          test_outgoing("grpc-trace-bin", expected);
+        }
+        test_outgoing("x-cloud-trace-context",
+                      ::opencensus::trace::propagation::ToCloudTraceContextHeader(ctx));
+        test_outgoing("x-b3-traceid", "404142434445464748494a4b4c4d4e4f");
+        test_outgoing("x-b3-spanid", ::opencensus::trace::propagation::ToB3SpanIdHeader(ctx));
+        test_outgoing("x-b3-sampled", "1");
+      };
+
+  test_incoming({{"traceparent", "00-404142434445464748494a4b4c4d4e4f-6162636465666768-01"}});
+  test_incoming({{"grpc-trace-bin", "AABAQUJDREVGR0hJSktMTU5PAWFiY2RlZmdoAgE"}});
+  test_incoming(
+      {{"x-cloud-trace-context", "404142434445464748494a4b4c4d4e4f/7017280452245743464;o=1"}});
+  test_incoming({{"x-b3-traceid", "404142434445464748494a4b4c4d4e4f"},
+                 {"x-b3-spanid", "6162636465666768"},
+                 {"x-b3-sampled", "1"}});
+  test_incoming({{"x-b3-traceid", "404142434445464748494a4b4c4d4e4f"},
+                 {"x-b3-spanid", "6162636465666768"},
+                 {"x-b3-flags", "1"}}); // Debug flag causes sampling.
 }
 
 namespace {
