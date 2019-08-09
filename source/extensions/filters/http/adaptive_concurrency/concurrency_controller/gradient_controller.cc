1e29caa32214953f8dcecb82bdbbc2b19927570d
Tony Allen <tony@allen.gg>
2019-08-09 01:50:47 -0700

    builds, still wip
    Signed-off-by: Tony Allen <tony@allen.gg>

--- source/extensions/filters/http/adaptive_concurrency/concurrency_controller/gradient_controller.cc
+++ source/extensions/filters/http/adaptive_concurrency/concurrency_controller/gradient_controller.cc
@@ -1,6 +1,12 @@
 #include <chrono>
 
 #include "extensions/filters/http/adaptive_concurrency/concurrency_controller/concurrency_controller.h"
+#include "envoy/runtime/runtime.h"
+#include "common/protobuf/utility.h"
+#include "extensions/filters/http/adaptive_concurrency/concurrency_controller/gradient_controller.h"
+#include "envoy/config/filter/http/adaptive_concurrency/v2alpha/adaptive_concurrency.pb.h"
+#include "envoy/event/dispatcher.h"
+#include "envoy/stats/stats.h"
 #include "common/protobuf/protobuf.h"
 
 #include "absl/synchronization/mutex.h"
@@ -11,42 +17,107 @@ namespace HttpFilters {
 namespace AdaptiveConcurrency {
 namespace ConcurrencyController {
 
-  GradientControllerConfig::GradientControllerConfig(
-      const envoy::config::filter::http::adaptive_concurrency::v2alpha::GradientControllerConfig& proto_config) :
-    // @tallen need to convert proto to actual class here.
-   min_rtt_calc_interval_(
-       std::chrono::milliseconds(
-         DurationUtil::durationToMilliseconds(
-           proto_config.min_rtt_calc_params().interval()))),
-   sample_rtt_calc_interval_(
-       std::chrono::milliseconds(
-         DurationUtil::durationToMilliseconds(
-           proto_config.concurrency_limit_params().concurrency_update_interval()))),
-   max_limit_(proto_config.concurrency_limit_params().max_concurrency_limit())
-  {
+GradientControllerConfig::GradientControllerConfig(
+  const envoy::config::filter::http::adaptive_concurrency::v2alpha::GradientControllerConfig& proto_config) :
+  min_rtt_calc_interval_(
+      std::chrono::milliseconds(
+        DurationUtil::durationToMilliseconds(
+          proto_config.min_rtt_calc_params().interval()))),
+  sample_rtt_calc_interval_(
+      std::chrono::milliseconds(
+        DurationUtil::durationToMilliseconds(
+          proto_config.concurrency_limit_params().concurrency_update_interval()))),
+  max_concurrency_limit_(PROTOBUF_GET_WRAPPED_REQUIRED(proto_config.concurrency_limit_params(), max_concurrency_limit)) { 
 
-GradientController::GradientController(
-    Event::Dispatcher& dispatcher, 
-    Runtime::Loader& runtime, 
-    std::string stats_prefix, 
-    Stats::Scope& scope,
-    TimeSource& time_source) :
-  dispatcher_(dispatcher),
-  runtime_(runtime),
-  stats_prefix_(stats_prefix),
-  scope_(scope),
-  time_source_(time_source) { 
+ switch (proto_config.sample_aggregate_percentile()) {
+   case envoy::config::filter::http::adaptive_concurrency::v2alpha::GradientControllerConfig::P50:
+     sample_aggregate_percentile_ = SampleAggregatePercentile::P50;
+     break;
+   case envoy::config::filter::http::adaptive_concurrency::v2alpha::GradientControllerConfig::P75:
+     sample_aggregate_percentile_ = SampleAggregatePercentile::P75;
+     break;
+   case envoy::config::filter::http::adaptive_concurrency::v2alpha::GradientControllerConfig::P90:
+     sample_aggregate_percentile_ = SampleAggregatePercentile::P90;
+     break;
+   case envoy::config::filter::http::adaptive_concurrency::v2alpha::GradientControllerConfig::P95:
+     sample_aggregate_percentile_ = SampleAggregatePercentile::P95;
+     break;
+   case envoy::config::filter::http::adaptive_concurrency::v2alpha::GradientControllerConfig::P99:
+     sample_aggregate_percentile_ = SampleAggregatePercentile::P99;
+     break;
+   default:
+    NOT_REACHED_GCOVR_EXCL_LINE;
+  }
+}
 
-min_rtt_calc_timer_
-sample_reset_timer_
+GradientController::GradientController(GradientControllerConfigSharedPtr config,
+                                       Event::Dispatcher& dispatcher, 
+                                       Runtime::Loader& runtime, 
+                                       std::string stats_prefix, 
+                                       Stats::Scope& scope,
+                                       TimeSource& time_source) :
+
+                                       config_(config),
+                                       dispatcher_(dispatcher),
+                                       runtime_(runtime),
+                                       stats_prefix_(stats_prefix),
+                                       scope_(scope),
+                                       time_source_(time_source),
+                                       num_rq_outstanding_(0),
+                                       concurrency_limit_(config_->starting_concurrency_limit())
+//                                       latency_sample_hist_(/* TODO @tallen */)
+                                       { 
+
+  min_rtt_calc_timer_ = dispatcher_.createTimer([this]() -> void { calculateMinRTT(); });
+  sample_reset_timer_= dispatcher_.createTimer([this]() -> void { updateConcurrencyLimit(); });
+
+  calculateMinRTT();
+  updateConcurrencyLimit();
+}
+
+GradientController::~GradientController() {
+  min_rtt_calc_timer_.reset();
+  sample_reset_timer_.reset();
+}
+
+void GradientController::calculateMinRTT() {
+
+}
+
+void GradientController::updateConcurrencyLimit() {
+
 }
 
 RequestForwardingAction GradientController::forwardingDecision() {
-
+  absl::MutexLock ml(&limit_mtx_);
+  if (num_rq_outstanding_ < concurrency_limit_) {
+    ++num_rq_outstanding_;
+    return RequestForwardingAction::Forward;
+  }
+  return RequestForwardingAction::Block;
 }
 
 void GradientController::recordLatencySample(const std::chrono::nanoseconds& rq_latency) {
+  if (!min_rtt_calculation_mtx_.ReaderTryLock()) {
+    // Failing to grab the read lock means there is an on-going
+    // min_rtt_recalculation.
+    recordLatencySampleForMinRTT(rq_latency);
+    return;
+  }
 
+  {
+    absl::MutexLock ml(&limit_mtx_);
+    const uint32_t latency_usec = std::chrono::duration_cast<std::chrono::microseconds>(rq_latency).count();
+    //    @tallen
+//    latency_sample_hist_.recordValue(latency_usec);
+    latency_sample_hist_.emplace_back(latency_usec);
+    --num_rq_outstanding_;
+  }
+
+  min_rtt_calculation_mtx_.ReaderUnlock();
+}
+
+void GradientController::recordLatencySampleForMinRTT(const std::chrono::nanoseconds& /*rq_latency*/) {
 }
 
 } // namespace ConcurrencyController
--- source/extensions/filters/http/adaptive_concurrency/concurrency_controller/gradient_controller.h
+++ source/extensions/filters/http/adaptive_concurrency/concurrency_controller/gradient_controller.h
@@ -4,9 +4,14 @@
 
 #include "envoy/config/filter/http/adaptive_concurrency/v2alpha/adaptive_concurrency.pb.h"
 #include "envoy/config/filter/http/adaptive_concurrency/v2alpha/adaptive_concurrency.pb.validate.h"
+#include "envoy/runtime/runtime.h"
+#include "envoy/event/dispatcher.h"
 #include "extensions/filters/http/adaptive_concurrency/concurrency_controller/concurrency_controller.h"
+//@tallen
+//#include "include/envoy/stats/histogram.h"
 
 #include "absl/synchronization/mutex.h"
+#include "absl/base/thread_annotations.h"
 
 namespace Envoy {
 namespace Extensions {
@@ -14,13 +19,24 @@ namespace HttpFilters {
 namespace AdaptiveConcurrency {
 namespace ConcurrencyController {
 
+enum class SampleAggregatePercentile {
+  P50,
+  P75,
+  P90,
+  P95,
+  P99
+};
+
 class GradientControllerConfig {
 public:
   GradientControllerConfig(
-    const envoy::config::filter::http::adaptive_concurrency::v2alpha::GradientControllerConfig& proto_config);
+    const envoy::config::filter::http::adaptive_concurrency::v2alpha::GradientControllerConfig&
+      proto_config);
 
-  // accessors.
-  std::chrono::nanoseconds min_rtt() const { return min_rtt_;}
+  std::chrono::milliseconds min_rtt_calc_interval() const { return min_rtt_calc_interval_; }
+  std::chrono::milliseconds sample_rtt_calc_interval() const { return sample_rtt_calc_interval_; }
+  uint64_t max_concurrency_limit() const { return max_concurrency_limit_; }
+  int starting_concurrency_limit() const { return starting_concurrency_limit_; }
 
 private:
   // The measured request round-trip time under ideal conditions.
@@ -29,8 +45,16 @@ private:
   // The measured sample round-trip time from the previous time window.
   std::chrono::milliseconds sample_rtt_calc_interval_;
 
-  int32_t max_limit_;
+  // The maximum allowed concurrency value.
+  uint64_t max_concurrency_limit_;
+
+  // Initial value for the concurrency limit.
+  int starting_concurrency_limit_;
+
+  // The percentile considered when aggregating latency samples.
+  SampleAggregatePercentile sample_aggregate_percentile_;
 };
+typedef std::shared_ptr<GradientControllerConfig> GradientControllerConfigSharedPtr;
 
 /**
  * A concurrency controller that implements the a variation of the Gradient algorithm to control the
@@ -57,25 +81,45 @@ private:
  */
 class GradientController : public ConcurrencyController {
 public:
-  GradientController(AdaptiveConcurrencyFilterConfigSharedPtr config_);
+  GradientController(GradientControllerConfigSharedPtr config,
+                     Event::Dispatcher& dispatcher,
+                     Runtime::Loader& runtime,
+                     std::string stats_prefix,
+                     Stats::Scope& scope,
+                     TimeSource& time_source);
+
+  ~GradientController();
 
   // ConcurrencyController.
   RequestForwardingAction forwardingDecision() override;
   void recordLatencySample(const std::chrono::nanoseconds& rq_latency) override;
 
 private:
+  void recordLatencySampleForMinRTT(const std::chrono::nanoseconds& rq_latency);
+  void calculateMinRTT();
+  void updateConcurrencyLimit();
+  uint32_t processLatencySamples() ABSL_EXCLUSIVE_LOCKS_REQUIRED(min_rtt_calculation_mtx_, limit_mtx_);
+
+  GradientControllerConfigSharedPtr config_;
+  Event::Dispatcher& dispatcher_;
   Runtime::Loader& runtime_;
   const std::string stats_prefix_;
   Stats::Scope& scope_;
   TimeSource& time_source_;
 
-  std::chrono::nanoseconds min_rtt_;
-  absl::Mutex min_rtt_calc_mtx_;
-  std::chrono::nanoseconds sample_rtt_;
-  int32_t concurrency_headroom_; // @tallen may not even need this..
+  std::chrono::nanoseconds min_rtt_ ABSL_GUARDED_BY(min_rtt_calculation_mtx_);
+  absl::Mutex min_rtt_calculation_mtx_;
+  absl::Mutex limit_mtx_;
+  std::chrono::nanoseconds sample_rtt_ ABSL_GUARDED_BY(/*min_rtt_calculation_mtx_,*/ limit_mtx_);
+  int num_rq_outstanding_ ABSL_GUARDED_BY(limit_mtx_);
+  int concurrency_limit_ ABSL_GUARDED_BY(limit_mtx_);
 
-  TimerPtr min_rtt_calc_timer_;
-  TimerPtr sample_reset_timer_;
+  // TODO @tallen figure out the deal with this histogram and stats sinks.
+  //Stats::Histogram latency_samples_;
+  std::vector<uint32_t> latency_sample_hist_;
+
+  Event::TimerPtr min_rtt_calc_timer_;
+  Event::TimerPtr sample_reset_timer_;
 
 };
 
