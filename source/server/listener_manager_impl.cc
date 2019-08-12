4bbc8c3ad3e247107a0ffdb6db9d16c3df4867c8
Alyssa Wilk <alyssar@chromium.org>
2019-08-12 18:22:42 -0400

    doing the repo dance
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- source/server/listener_manager_impl.cc
+++ source/server/listener_manager_impl.cc
@@ -65,8 +65,11 @@ std::vector<Network::FilterFactoryCb> ProdListenerComponentFactory::createNetwor
         Config::Utility::getAndCheckFactory<Configuration::NamedNetworkFilterConfigFactory>(
             string_name);
 
-    Config::Utility::validateTerminalFilters(filters[i].name(), "network",
-                                             factory.isTerminalFilter(), i == filters.size() - 1);
+    // TODO(alyssar) replace this block and reinstate TerminalNotLast test once echo2 is updated
+    if (factory.isTerminalFilter() && i != filters.size() - 1) {
+      throw EnvoyException(
+          fmt::format("Error: {} must be the terminal network filter.", filters[i].name()));
+    }
 
     Network::FilterFactoryCb callback;
     if (Config::Utility::allowDeprecatedV1Config(context.runtime(), *filter_config)) {
