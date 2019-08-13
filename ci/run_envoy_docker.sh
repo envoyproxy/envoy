05d0d6c8bf3aea447b0f2cdecfeacd96e255f07b
Santosh Kumar Cheler <scheler@arubanetworks.com>
2019-08-13 18:40:12 +0000

    Update references to local scripts to enable using build container for filter repos

--- ci/run_envoy_docker.sh
+++ ci/run_envoy_docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-. ci/envoy_build_sha.sh
+. $(dirname $0)/envoy_build_sha.sh
 
 # We run as root and later drop permissions. This is requried to setup the USER
 # in useradd below, which is need for correct Python execution in the Docker
