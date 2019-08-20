c7b6fb200187dcb2e3b03f93f83ac91c43de809d
Alan Chiu <achiu@lyft.com>
2019-08-20 13:49:25 -0700

    kotlin: remove body and trailers from request object to Client i… (#352)
    The request object is no longer going to contain the body and trailers.
    For the streaming case, the headers will be sent upon connection but the data/trailers will be handled by the user
    
    For the unary case, the headers and body will be sent upon connection. The trailers will be sent once a response is sent back.
    
    Signed-off-by: Alan Chiu <achiu@lyft.com>
    
    For an explanation of how to fill out the fields, please see the relevant section
    in [PULL_REQUESTS.md](https://github.com/envoyproxy/envoy/blob/master/PULL_REQUESTS.md)
    
    Description: kotlin: remove body and trailers from request object to Client interfaces
    Risk Level: low
    Testing: unit
    Docs Changes: n/a
    Release Notes: n/a
    [Optional Fixes #Issue]
    [Optional Deprecated:]
    
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/kotlin/test/io/envoyproxy/envoymobile/RequestBuilderTest.kt
+++ mobile/library/kotlin/test/io/envoyproxy/envoymobile/RequestBuilderTest.kt
@@ -6,24 +6,6 @@ import org.junit.Test
 class RequestBuilderTest {
 
   @Test
-  fun `adding request data should have body present in request`() {
-    val body = "data".toByteArray()
-
-    val request = RequestBuilder(method = RequestMethod.POST, scheme = "https", authority = "api.foo.com", path = "foo")
-        .addBody(body)
-        .build()
-    assertThat(request.body).isEqualTo(body)
-  }
-
-  @Test
-  fun `not adding request data should have null body in request`() {
-    val request = RequestBuilder(method = RequestMethod.POST, scheme = "https", authority = "api.foo.com", path = "foo")
-        .build()
-
-    assertThat(request.body).isNull()
-  }
-
-  @Test
   fun `adding retry policy should have policy present in request`() {
 
     val retryPolicy = RetryPolicy(maxRetryCount = 23, retryOn = listOf(RetryRule.STATUS_5XX, RetryRule.CONNECT_FAILURE), perRetryTimeoutMs = 1234)
@@ -49,15 +31,6 @@ class RequestBuilderTest {
         .build()
 
     assertThat(request.headers["header_a"]).contains("value_a1")
-  }
-
-  @Test
-  fun `adding new trailers should append to the list of trailers keys`() {
-    val request = RequestBuilder(method = RequestMethod.POST, scheme = "https", authority = "api.foo.com", path = "foo")
-        .addTrailer("trailer_a", "value_a1")
-        .build()
-
-    assertThat(request.trailers["trailer_a"]).contains("value_a1")
   }
 
   @Test
@@ -110,57 +83,5 @@ class RequestBuilderTest {
         .build()
 
     assertThat(request.headers["header_a"]).isNull()
-  }
-
-  @Test
-  fun `removing trailers should clear trailers in request`() {
-    val request = RequestBuilder(method = RequestMethod.POST, scheme = "https", authority = "api.foo.com", path = "foo")
-        .addTrailer("trailer_a", "value_a1")
-        .removeTrailers("trailer_a")
-        .build()
-
-    assertThat(request.trailers).doesNotContainKey("trailer_a")
-  }
-
-  @Test
-  fun `removing a specific trailer value should not be in request`() {
-    val request = RequestBuilder(method = RequestMethod.POST, scheme = "https", authority = "api.foo.com", path = "foo")
-        .addTrailer("trailer_a", "value_a1")
-        .addTrailer("trailer_a", "value_a2")
-        .removeTrailer("trailer_a", "value_a1")
-        .build()
-
-    assertThat(request.trailers["trailer_a"]).doesNotContain("value_a1")
-  }
-
-  @Test
-  fun `removing a specific trailer value should keep the other trailer values in request`() {
-    val request = RequestBuilder(method = RequestMethod.POST, scheme = "https", authority = "api.foo.com", path = "foo")
-        .addTrailer("trailer_a", "value_a1")
-        .addTrailer("trailer_a", "value_a2")
-        .removeTrailer("trailer_a", "value_a1")
-        .build()
-
-    assertThat(request.trailers["trailer_a"]).contains("value_a2")
-  }
-
-  @Test
-  fun `adding a specific trailer value should keep the other trailer values in request`() {
-    val request = RequestBuilder(method = RequestMethod.POST, scheme = "https", authority = "api.foo.com", path = "foo")
-        .addTrailer("trailer_a", "value_a1")
-        .addTrailer("trailer_a", "value_a2")
-        .build()
-
-    assertThat(request.trailers["trailer_a"]).containsExactly("value_a1", "value_a2")
-  }
-
-  @Test
-  fun `removing all trailer values should remove trailer list in request`() {
-    val request = RequestBuilder(method = RequestMethod.POST, scheme = "https", authority = "api.foo.com", path = "foo")
-        .addTrailer("trailer_a", "value_a1")
-        .removeTrailer("trailer_a", "value_a1")
-        .build()
-
-    assertThat(request.trailers["trailer_a"]).isNull()
   }
 }
--- mobile/library/kotlin/test/io/envoyproxy/envoymobile/RequestTest.kt
+++ mobile/library/kotlin/test/io/envoyproxy/envoymobile/RequestTest.kt
@@ -9,23 +9,15 @@ class RequestTest {
   @Test
   fun `requests with the same properties should be equal`() {
     val request1 = RequestBuilder(method = RequestMethod.POST, scheme = "https", authority = "api.foo.com", path = "foo")
-        .addBody("data".toByteArray())
         .addHeader("header_a", "value_a1")
         .addHeader("header_a", "value_a2")
         .addHeader("header_b", "value_b1")
-        .addTrailer("trailer_a", "value_a1")
-        .addTrailer("trailer_a", "value_a2")
-        .addTrailer("trailer_b", "value_b1")
         .build()
 
     val request2 = RequestBuilder(method = RequestMethod.POST, scheme = "https", authority = "api.foo.com", path = "foo")
-        .addBody("data".toByteArray())
         .addHeader("header_a", "value_a1")
         .addHeader("header_a", "value_a2")
         .addHeader("header_b", "value_b1")
-        .addTrailer("trailer_a", "value_a1")
-        .addTrailer("trailer_a", "value_a2")
-        .addTrailer("trailer_b", "value_b1")
         .build()
 
     assertThat(request1).isEqualTo(request2)
@@ -34,14 +26,10 @@ class RequestTest {
   @Test
   fun `requests converted to a builder should build to the same request`() {
     val request = RequestBuilder(method = RequestMethod.POST, scheme = "https", authority = "api.foo.com", path = "foo")
-        .addBody("data".toByteArray())
         .addRetryPolicy(RetryPolicy(23, listOf(RetryRule.STATUS_5XX, RetryRule.CONNECT_FAILURE), 1234))
         .addHeader("header_a", "value_a1")
         .addHeader("header_a", "value_a2")
         .addHeader("header_b", "value_b1")
-        .addTrailer("trailer_a", "value_a1")
-        .addTrailer("trailer_a", "value_a2")
-        .addTrailer("trailer_b", "value_b1")
         .build()
 
     assertThat(request).isEqualTo(request.toBuilder().build())
