df7645d72d14cddb1cc39656af26eb3d4aebe65d
Harvey Tuch <htuch@google.com>
2019-08-22 16:45:34 -0400

    tools: sync tool for envoyproxy/assignable team.
    Bulk update of team to match envoyproxy organization. While at it, cleaned up some venv stuff in
    shell_utils.sh.
    
    Risk level: Low
    Testing: Synced 157 members from envoyproxy to envoyproxy/assignable.
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- tools/shell_utils.sh
+++ tools/shell_utils.sh
@@ -9,3 +9,15 @@ source_venv() {
     echo "Found existing virtualenv"
   fi
 }
+
+python_venv() {
+  SCRIPT_DIR=$(realpath "$(dirname "$0")")
+
+  BUILD_DIR=build_tools
+  VENV_DIR="$BUILD_DIR/$1"
+
+  source_venv "$VENV_DIR"
+  pip install -r "${SCRIPT_DIR}"/requirements.txt
+
+  python3 "${SCRIPT_DIR}/$1.py" $*
+}
