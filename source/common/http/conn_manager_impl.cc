e958cf9e58742d0caf4d07b59810f688c0212fe3
alyssawilk <alyssar@chromium.org>
2019-08-23 12:18:38 -0400

    http: tracking active session under L7 timers (#7782)
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- source/common/http/conn_manager_impl.cc
+++ source/common/http/conn_manager_impl.cc
@@ -471,7 +471,7 @@ ConnectionManagerImpl::ActiveStream::ActiveStream(ConnectionManagerImpl& connect
     std::chrono::milliseconds request_timeout_ms_ = connection_manager_.config_.requestTimeout();
     request_timer_ = connection_manager.read_callbacks_->connection().dispatcher().createTimer(
         [this]() -> void { onRequestTimeout(); });
-    request_timer_->enableTimer(request_timeout_ms_);
+    request_timer_->enableTimer(request_timeout_ms_, this);
   }
 
   stream_info_.setRequestedServerName(
