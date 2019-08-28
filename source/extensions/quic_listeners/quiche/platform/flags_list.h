cde302275178c24a7db62e3b2f89a6d63d7139a4
Dan Zhang <danzh@google.com>
2019-08-28 13:57:46 -0400

    fix misfire of s/QUIC/QUICHE
    Signed-off-by: Dan Zhang <danzh@google.com>

--- source/extensions/quic_listeners/quiche/platform/flags_list.h
+++ source/extensions/quic_listeners/quiche/platform/flags_list.h
@@ -3,7 +3,7 @@
 
 // NOLINT(namespace-envoy)
 
-// This file is part of the QUIC platform implementation, and is not to be
+// This file is part of the QUICHE platform implementation, and is not to be
 // consumed or referenced directly by other Envoy code. It serves purely as a
 // porting layer for QUICHE.
 
