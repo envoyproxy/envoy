38e515af6938ef48727a5c6ba702c0cbbbe26301
Andres Guedez <aguedez@google.com>
2019-08-30 14:53:56 -0400

    Avoid removing warming clusters from the init helper.
    This properly fixes the invalid iterator access whe removing clusters from the init helper.
    
    Warming clusters are never added to the init helper; added an assertion to validate this invariant.
    
    Signed-off-by: Andres Guedez <aguedez@google.com>

--- source/common/upstream/cluster_manager_impl.cc
+++ source/common/upstream/cluster_manager_impl.cc
@@ -487,12 +487,21 @@ bool ClusterManagerImpl::addOrUpdateCluster(const envoy::api::v2::Cluster& clust
 
   if (existing_active_cluster != active_clusters_.end() ||
       existing_warming_cluster != warming_clusters_.end()) {
-    const auto cluster_it = (existing_active_cluster != active_clusters_.end())
-                                ? existing_active_cluster
-                                : existing_warming_cluster;
-    // The following init manager remove call is a NOP in the case we are already initialized. It's
-    // just kept here to avoid additional logic.
-    init_helper_.removeCluster(*cluster_it->second->cluster_);
+    if (existing_active_cluster != active_clusters_.end()) {
+      // The following init manager remove call is a NOP in the case we are already initialized.
+      // It's just kept here to avoid additional logic.
+      init_helper_.removeCluster(*existing_active_cluster->second->cluster_);
+    } else {
+      // Validate that warming clusters are not added to the init_helper_.
+      for (const std::list<Cluster*>& cluster_list :
+           {std::cref(init_helper_.primary_init_clusters_),
+            std::cref(init_helper_.secondary_init_clusters_)}) {
+        ASSERT(!std::any_of(cluster_list.begin(), cluster_list.end(),
+                            [&existing_warming_cluster](Cluster* cluster) {
+                              return existing_warming_cluster->second->cluster_.get() == cluster;
+                            }));
+      }
+    }
     cm_stats_.cluster_modified_.inc();
   } else {
     cm_stats_.cluster_added_.inc();
