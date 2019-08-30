08a96f0f4fa9ad2c1c887294af926082f20380b9
Jose Ulises Nino Rivera <junr03@users.noreply.github.com>
2019-08-30 12:36:12 -0700

    cleanup: remove stale todos (#396)
    Description: remove todos that were done already.
    Risk Level: low
    
    Signed-off-by: Jose Nino <jnino@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/common/buffer/utility.cc
+++ mobile/library/common/buffer/utility.cc
@@ -11,8 +11,6 @@ namespace Utility {
 Buffer::InstancePtr toInternalData(envoy_data data) {
   // This fragment only needs to live until done is called.
   // Therefore, it is sufficient to allocate on the heap, and delete in the done method.
-  // TODO: this method leaks the implementation of Buffer::BridgeFragment and could be improved to
-  // avoid new.
   Buffer::BridgeFragment* fragment = Buffer::BridgeFragment::createBridgeFragment(data);
   InstancePtr buf = std::make_unique<Buffer::OwnedImpl>();
   buf->addBufferFragment(*fragment);
