402ffbafe8d6e8c2291c06418cc0f92d303cf489
Joshua Marantz <jmarantz@google.com>
2019-09-02 11:22:53 -0400

    update PR #.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/integration/stats_integration_test.cc
+++ test/integration/stats_integration_test.cc
@@ -225,7 +225,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithFakeSymbolTable) {
   // 2019/07/15  7555     42806       43000   static link libstdc++ in tests
   // 2019/07/24  7503     43030       44000   add upstream filters to clusters
   // 2019/08/13  7877     42838       44000   skip EdfScheduler creation if all host weights equal
-  // 2019/09/02  xxxx     42830       43000   Share symbol-tables in cluster/host stats.
+  // 2019/09/02  8118     42830       43000   Share symbol-tables in cluster/host stats.
 
   // Note: when adjusting this value: EXPECT_MEMORY_EQ is active only in CI
   // 'release' builds, where we control the platform and tool-chain. So you
@@ -261,7 +261,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithRealSymbolTable) {
   //                      exact upper-bound
   // ----------  -----    -----------------   -----
   // 2019/08/09  7882     35489       36000   Initial version
-  // 2019/09/02  xxxx     34585       34500   Share symbol-tables in cluster/host stats.
+  // 2019/09/02  8118     34585       34500   Share symbol-tables in cluster/host stats.
 
   // Note: when adjusting this value: EXPECT_MEMORY_EQ is active only in CI
   // 'release' builds, where we control the platform and tool-chain. So you
