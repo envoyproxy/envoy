8891a1458f1e85df9b969e6434aa2cbe83ca6af1
Dan Zhang <danzh@google.com>
2019-09-04 11:32:50 -0400

    fix coverage test failure
    Signed-off-by: Dan Zhang <danzh@google.com>

--- test/extensions/quic_listeners/quiche/quic_io_handle_wrapper_test.cc
+++ test/extensions/quic_listeners/quiche/quic_io_handle_wrapper_test.cc
@@ -1,3 +1,5 @@
+#include <sys/socket.h>
+
 #include <memory>
 
 #include "common/network/address_impl.h"
@@ -55,7 +57,21 @@ TEST_F(QuicIoHandleWrapperTest, DelegateIoHandleCalls) {
   wrapper_->sendmsg(&slice, 1, 0, /*self_ip=*/nullptr, *addr);
 
   Network::IoHandle::RecvMsgOutput output(nullptr);
-  EXPECT_DEATH(wrapper_->recvmsg(&slice, 1, /*self_port=*/12345, output), "Invalid remote address");
+  EXPECT_CALL(os_sys_calls_, recvmsg(fd, _, 0)).WillOnce(Invoke([](int, struct msghdr* msg, int) {
+    sockaddr_storage ss;
+    auto ipv6_addr = reinterpret_cast<sockaddr_in6*>(&ss);
+    memset(ipv6_addr, 0, sizeof(sockaddr_in6));
+    ipv6_addr->sin6_family = AF_INET6;
+    ipv6_addr->sin6_addr = in6addr_loopback;
+    ipv6_addr->sin6_port = htons(54321);
+    *reinterpret_cast<sockaddr_in6*>(msg->msg_name) = *ipv6_addr;
+    msg->msg_namelen = sizeof(sockaddr_in6);
+    return Api::SysCallSizeResult{5u, 0};
+  }));
+  EXPECT_CALL(os_sys_calls_, socket(AF_INET6, SOCK_STREAM, 0))
+      .WillOnce(Return(Api::SysCallIntResult{1, 0}));
+  EXPECT_CALL(os_sys_calls_, close(1)).WillOnce(Return(Api::SysCallIntResult{0, 0}));
+  wrapper_->recvmsg(&slice, 1, /*self_port=*/12345, output);
 
   EXPECT_TRUE(wrapper_->close().ok());
 
