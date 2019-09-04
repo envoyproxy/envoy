118c47a7027c05c8374a8e4ffc4f0e1286e8a04f
Yan Xue <yxyan@google.com>
2019-09-04 12:09:48 -0700

    move test cluster manager to common code
    Signed-off-by: Yan Xue <yxyan@google.com>

--- test/extensions/clusters/aggregate/cluster_integration_test.cc
+++ test/extensions/clusters/aggregate/cluster_integration_test.cc
@@ -4,6 +4,7 @@
 
 #include "extensions/clusters/aggregate/cluster.h"
 
+#include "test/common/upstream/test_cluster_manager.h"
 #include "test/common/upstream/utility.h"
 #include "test/mocks/protobuf/mocks.h"
 #include "test/mocks/server/mocks.h"
@@ -23,120 +24,6 @@ namespace Envoy {
 namespace Extensions {
 namespace Clusters {
 namespace Aggregate {
-namespace {
-
-// The tests in this file are split between testing with real clusters and some with mock clusters.
-// By default we setup to call the real cluster creation function. Individual tests can override
-// the expectations when needed.
-class TestClusterManagerFactory : public Upstream::ClusterManagerFactory {
-public:
-  TestClusterManagerFactory() : api_(Api::createApiForTest(stats_)) {
-    ON_CALL(*this, clusterFromProto_(_, _, _, _))
-        .WillByDefault(Invoke(
-            [&](const envoy::api::v2::Cluster& cluster, Upstream::ClusterManager& cm,
-                Upstream::Outlier::EventLoggerSharedPtr outlier_event_logger, bool added_via_api)
-                -> std::pair<Upstream::ClusterSharedPtr, Upstream::ThreadAwareLoadBalancer*> {
-              auto result = Upstream::ClusterFactoryImplBase::create(
-                  cluster, cm, stats_, tls_, dns_resolver_, ssl_context_manager_, runtime_, random_,
-                  dispatcher_, log_manager_, local_info_, admin_, singleton_manager_,
-                  outlier_event_logger, added_via_api, validation_visitor_, *api_);
-              // Convert from load balancer unique_ptr -> raw pointer -> unique_ptr.
-              return std::make_pair(result.first, result.second.release());
-            }));
-  }
-
-  Http::ConnectionPool::InstancePtr
-  allocateConnPool(Event::Dispatcher&, Upstream::HostConstSharedPtr host,
-                   Upstream::ResourcePriority, Http::Protocol,
-                   const Network::ConnectionSocket::OptionsSharedPtr& options) override {
-    return Http::ConnectionPool::InstancePtr{allocateConnPool_(host, options)};
-  }
-
-  Tcp::ConnectionPool::InstancePtr
-  allocateTcpConnPool(Event::Dispatcher&, Upstream::HostConstSharedPtr host,
-                      Upstream::ResourcePriority,
-                      const Network::ConnectionSocket::OptionsSharedPtr&,
-                      Network::TransportSocketOptionsSharedPtr) override {
-    return Tcp::ConnectionPool::InstancePtr{allocateTcpConnPool_(host)};
-  }
-
-  std::pair<Upstream::ClusterSharedPtr, Upstream::ThreadAwareLoadBalancerPtr>
-  clusterFromProto(const envoy::api::v2::Cluster& cluster, Upstream::ClusterManager& cm,
-                   Upstream::Outlier::EventLoggerSharedPtr outlier_event_logger,
-                   bool added_via_api) override {
-    auto result = clusterFromProto_(cluster, cm, outlier_event_logger, added_via_api);
-    return std::make_pair(result.first, Upstream::ThreadAwareLoadBalancerPtr(result.second));
-  }
-
-  Upstream::CdsApiPtr createCds(const envoy::api::v2::core::ConfigSource&,
-                                Upstream::ClusterManager&) override {
-    return Upstream::CdsApiPtr{createCds_()};
-  }
-
-  Upstream::ClusterManagerPtr
-  clusterManagerFromProto(const envoy::config::bootstrap::v2::Bootstrap& bootstrap) override {
-    return Upstream::ClusterManagerPtr{clusterManagerFromProto_(bootstrap)};
-  }
-
-  Secret::SecretManager& secretManager() override { return secret_manager_; }
-
-  MOCK_METHOD1(clusterManagerFromProto_,
-               Upstream::ClusterManager*(const envoy::config::bootstrap::v2::Bootstrap& bootstrap));
-  MOCK_METHOD2(allocateConnPool_,
-               Http::ConnectionPool::Instance*(Upstream::HostConstSharedPtr host,
-                                               Network::ConnectionSocket::OptionsSharedPtr));
-  MOCK_METHOD1(allocateTcpConnPool_,
-               Tcp::ConnectionPool::Instance*(Upstream::HostConstSharedPtr host));
-  MOCK_METHOD4(clusterFromProto_,
-               std::pair<Upstream::ClusterSharedPtr, Upstream::ThreadAwareLoadBalancer*>(
-                   const envoy::api::v2::Cluster& cluster, Upstream::ClusterManager& cm,
-                   Upstream::Outlier::EventLoggerSharedPtr outlier_event_logger,
-                   bool added_via_api));
-  MOCK_METHOD0(createCds_, Upstream::CdsApi*());
-
-  Stats::IsolatedStoreImpl stats_;
-  NiceMock<ThreadLocal::MockInstance> tls_;
-  std::shared_ptr<NiceMock<Network::MockDnsResolver>> dns_resolver_{
-      new NiceMock<Network::MockDnsResolver>};
-  NiceMock<Runtime::MockLoader> runtime_;
-  NiceMock<Runtime::MockRandomGenerator> random_;
-  NiceMock<Event::MockDispatcher> dispatcher_;
-  Extensions::TransportSockets::Tls::ContextManagerImpl ssl_context_manager_{
-      dispatcher_.timeSource()};
-  NiceMock<LocalInfo::MockLocalInfo> local_info_;
-  NiceMock<Server::MockAdmin> admin_;
-  NiceMock<Secret::MockSecretManager> secret_manager_;
-  NiceMock<AccessLog::MockAccessLogManager> log_manager_;
-  Singleton::ManagerImpl singleton_manager_{Thread::threadFactoryForTest()};
-  NiceMock<ProtobufMessage::MockValidationVisitor> validation_visitor_;
-  Api::ApiPtr api_;
-};
-
-// A test version of ClusterManagerImpl that provides a way to get a non-const handle to the
-// clusters, which is necessary in order to call updateHosts on the priority set.
-class TestClusterManagerImpl : public Upstream::ClusterManagerImpl {
-public:
-  using Upstream::ClusterManagerImpl::ClusterManagerImpl;
-
-  TestClusterManagerImpl(const envoy::config::bootstrap::v2::Bootstrap& bootstrap,
-                         Upstream::ClusterManagerFactory& factory, Stats::Store& stats,
-                         ThreadLocal::Instance& tls, Runtime::Loader& runtime,
-                         Runtime::RandomGenerator& random, const LocalInfo::LocalInfo& local_info,
-                         AccessLog::AccessLogManager& log_manager,
-                         Event::Dispatcher& main_thread_dispatcher, Server::Admin& admin,
-                         ProtobufMessage::ValidationContext& validation_context, Api::Api& api,
-                         Http::Context& http_context)
-      : ClusterManagerImpl(bootstrap, factory, stats, tls, runtime, random, local_info, log_manager,
-                           main_thread_dispatcher, admin, validation_context, api, http_context) {}
-
-  std::map<std::string, std::reference_wrapper<Upstream::Cluster>> activeClusters() {
-    std::map<std::string, std::reference_wrapper<Upstream::Cluster>> clusters;
-    for (auto& cluster : active_clusters_) {
-      clusters.emplace(cluster.first, *cluster.second->cluster_);
-    }
-    return clusters;
-  }
-};
 
 envoy::config::bootstrap::v2::Bootstrap parseBootstrapFromV2Yaml(const std::string& yaml) {
   envoy::config::bootstrap::v2::Bootstrap bootstrap;
@@ -149,7 +36,7 @@ public:
   AggregateClusterIntegrationTest() : http_context_(stats_store_.symbolTable()) {}
 
   void initialize(const std::string& yaml_config) {
-    cluster_manager_ = std::make_unique<TestClusterManagerImpl>(
+    cluster_manager_ = std::make_unique<Upstream::TestClusterManagerImpl>(
         parseBootstrapFromV2Yaml(yaml_config), factory_, factory_.stats_, factory_.tls_,
         factory_.runtime_, factory_.random_, factory_.local_info_, log_manager_,
         factory_.dispatcher_, admin_, validation_context_, *api_, http_context_);
@@ -164,9 +51,9 @@ public:
   Upstream::ThreadLocalCluster* cluster_;
 
   Event::SimulatedTimeSystem time_system_;
-  NiceMock<TestClusterManagerFactory> factory_;
+  NiceMock<Upstream::TestClusterManagerFactory> factory_;
   NiceMock<ProtobufMessage::MockValidationContext> validation_context_;
-  std::unique_ptr<TestClusterManagerImpl> cluster_manager_;
+  std::unique_ptr<Upstream::TestClusterManagerImpl> cluster_manager_;
   AccessLog::MockAccessLogManager log_manager_;
   Http::ContextImpl http_context_;
 
@@ -295,8 +182,6 @@ TEST_F(AggregateClusterIntegrationTest, BasicFlow) {
     EXPECT_EQ(Upstream::Host::Health::Degraded, host->health());
   }
 }
-
-} // namespace
 
 } // namespace Aggregate
 } // namespace Clusters
