ae0df9aa1f91f3f1d04a26f3e285a174d9fdcda3
Rama Chavali <rama.rao@salesforce.com>
2019-09-11 09:36:10 +0530

    add todo
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- source/extensions/stat_sinks/metrics_service/grpc_metrics_service_impl.cc
+++ source/extensions/stat_sinks/metrics_service/grpc_metrics_service_impl.cc
@@ -61,6 +61,9 @@ void MetricsServiceSink::flushGauge(const Stats::Gauge& gauge) {
 }
 
 void MetricsServiceSink::flushHistogram(const Stats::ParentHistogram& envoy_histogram) {
+  // TODO(ramaraochavali): Currently we are sending both quantile information and bucket
+  // information. We should make sending both summary and bucket information configurable
+
   // Add summary information for histograms.
   io::prometheus::client::MetricFamily* summary_metrics_family = message_.add_envoy_metrics();
   summary_metrics_family->set_type(io::prometheus::client::MetricType::SUMMARY);
