36446323c46c46254b2d67f9e5d494f97f4d19cd
Dan Zhang <danzh@google.com>
2019-09-11 17:05:08 -0400

    fix build error
    Signed-off-by: Dan Zhang <danzh@google.com>

--- source/server/filter_chain_manager_impl.h
+++ source/server/filter_chain_manager_impl.h
@@ -179,7 +179,7 @@ public:
   // Network::FilterChain
   const Network::TransportSocketFactory* transportSocketFactory() const override { return nullptr; }
 
-  const Ssl::ContextConfig& tlsContextConfig() const { return tls_context_config_.get(); }
+  const Ssl::ContextConfig& tlsContextConfig() const { return *tls_context_config_.get(); }
 
 private:
   std::unique_ptr<const Ssl::ContextConfig> tls_context_config_;
