571c31a16aefb1c7d4aaff6431bbb8b55c986a12
Pengyuan Bian <bianpengyuan@google.com>
2019-09-10 21:05:35 -0700

    format
    Signed-off-by: Pengyuan Bian <bianpengyuan@google.com>

--- source/extensions/filters/network/http_connection_manager/config.cc
+++ source/extensions/filters/network/http_connection_manager/config.cc
@@ -283,7 +283,7 @@ HttpConnectionManagerConfig::HttpConnectionManagerConfig(
     // TODO: Random sampling historically was an integer and default to out of 10,000. We should
     // deprecate that and move to a straight fractional percent config.
     uint64_t random_sampling_numerator{PROTOBUF_PERCENT_TO_ROUNDED_INTEGER_OR_DEFAULT(
-      tracing_config, random_sampling, 10000, 10000)};
+        tracing_config, random_sampling, 10000, 10000)};
     random_sampling.set_numerator(random_sampling_numerator);
     random_sampling.set_denominator(envoy::type::FractionalPercent::TEN_THOUSAND);
     envoy::type::FractionalPercent overall_sampling;
