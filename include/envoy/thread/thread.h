0fc1b32c380e3e20631e17e8a1148c9e2a1ce236
yanavlasov <yavlasov@google.com>
2019-09-13 17:37:33 -0400

    Replace deprecated thread annotations macros. (#8237)
    Abseil thread annotation macros are now prefixed by ABSL_.
    
    There is no semantic change; this is just a rename.
    
    Signed-off-by: Yan Avlasov <yavlasov@google.com>
    Signed-off-by: Garden Windows <garden-windows-eng@pivotal.io>

--- include/envoy/thread/thread.h
+++ include/envoy/thread/thread.h
@@ -67,13 +67,13 @@ public:
  * Like the C++11 "basic lockable concept" but a pure virtual interface vs. a template, and
  * with thread annotations.
  */
-class LOCKABLE BasicLockable {
+class ABSL_LOCKABLE BasicLockable {
 public:
   virtual ~BasicLockable() = default;
 
-  virtual void lock() EXCLUSIVE_LOCK_FUNCTION() PURE;
-  virtual bool tryLock() EXCLUSIVE_TRYLOCK_FUNCTION(true) PURE;
-  virtual void unlock() UNLOCK_FUNCTION() PURE;
+  virtual void lock() ABSL_EXCLUSIVE_LOCK_FUNCTION() PURE;
+  virtual bool tryLock() ABSL_EXCLUSIVE_TRYLOCK_FUNCTION(true) PURE;
+  virtual void unlock() ABSL_UNLOCK_FUNCTION() PURE;
 };
 
 } // namespace Thread
