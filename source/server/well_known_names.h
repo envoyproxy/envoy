94dc9a38cefc68beaec7e19c9af8bd990c4674a0
Dan Zhang <danzh@google.com>
2019-09-13 16:45:26 -0400

    rename quic listener
    Signed-off-by: Dan Zhang <danzh@google.com>

--- source/server/well_known_names.h
+++ source/server/well_known_names.h
@@ -13,7 +13,7 @@ namespace Server {
 class UdpListenerNameValues {
 public:
   const std::string RawUdp = "raw_udp_listener";
-  const std::string Quic = "quic_listener";
+  const std::string Quic = "quiche_quic_listener";
 };
 
 using UdpListenerNames = ConstSingleton<UdpListenerNameValues>;
