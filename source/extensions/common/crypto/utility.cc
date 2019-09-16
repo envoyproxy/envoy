0621fcc882fbf093c002a279c3c5330e5f375e67
Bill DeCoste <bdecoste@gmail.com>
2019-09-16 18:00:06 -0400

    move common/crypto impl to extensions for openssl (#7344)
    Description: Move common/crypto impl (i.e. utility.cc) to extensions to make it clear that the impl is ssl-impl specific (e.g. boringgsl vs openssl) and easier to plug in an openssl impl. 
    Risk Level: Low
    Testing: Passes all standard tests
    Docs Changes: None
    Release Notes: None
    
    Signed-off-by: William DeCoste <bdecoste@gmail.com>

--- source/extensions/common/crypto/utility.cc
+++ source/extensions/common/crypto/utility.cc
@@ -3,6 +3,8 @@
 #include "common/common/assert.h"
 #include "common/common/stack_array.h"
 
+#include "extensions/common/crypto/crypto_impl.h"
+
 #include "absl/strings/ascii.h"
 #include "absl/strings/str_cat.h"
 #include "openssl/bytestring.h"
@@ -13,7 +15,11 @@ namespace Envoy {
 namespace Common {
 namespace Crypto {
 
-std::vector<uint8_t> Utility::getSha256Digest(const Buffer::Instance& buffer) {
+namespace Utility {
+
+const EVP_MD* getHashFunction(absl::string_view name);
+
+std::vector<uint8_t> getSha256Digest(const Buffer::Instance& buffer) {
   std::vector<uint8_t> digest(SHA256_DIGEST_LENGTH);
   EVP_MD_CTX* ctx(EVP_MD_CTX_new());
   auto rc = EVP_DigestInit(ctx, EVP_sha256());
@@ -31,8 +37,7 @@ std::vector<uint8_t> Utility::getSha256Digest(const Buffer::Instance& buffer) {
   return digest;
 }
 
-std::vector<uint8_t> Utility::getSha256Hmac(const std::vector<uint8_t>& key,
-                                            absl::string_view message) {
+std::vector<uint8_t> getSha256Hmac(const std::vector<uint8_t>& key, absl::string_view message) {
   std::vector<uint8_t> hmac(SHA256_DIGEST_LENGTH);
   const auto ret =
       HMAC(EVP_sha256(), key.data(), key.size(), reinterpret_cast<const uint8_t*>(message.data()),
@@ -41,21 +46,28 @@ std::vector<uint8_t> Utility::getSha256Hmac(const std::vector<uint8_t>& key,
   return hmac;
 }
 
-const VerificationOutput Utility::verifySignature(absl::string_view hash, EVP_PKEY* pubKey,
-                                                  const std::vector<uint8_t>& signature,
-                                                  const std::vector<uint8_t>& text) {
+const VerificationOutput verifySignature(absl::string_view hash, CryptoObject& key,
+                                         const std::vector<uint8_t>& signature,
+                                         const std::vector<uint8_t>& text) {
   // Step 1: initialize EVP_MD_CTX
   bssl::ScopedEVP_MD_CTX ctx;
 
   // Step 2: initialize EVP_MD
-  const EVP_MD* md = Utility::getHashFunction(hash);
+  const EVP_MD* md = getHashFunction(hash);
 
   if (md == nullptr) {
     return {false, absl::StrCat(hash, " is not supported.")};
   }
-
   // Step 3: initialize EVP_DigestVerify
-  int ok = EVP_DigestVerifyInit(ctx.get(), nullptr, md, nullptr, pubKey);
+  auto pkey_wrapper = Common::Crypto::Access::getTyped<Common::Crypto::PublicKeyObject>(key);
+  EVP_PKEY* pkey = pkey_wrapper->getEVP_PKEY();
+
+  if (pkey == nullptr) {
+    free(pkey_wrapper);
+    return {false, "Failed to initialize digest verify."};
+  }
+
+  int ok = EVP_DigestVerifyInit(ctx.get(), nullptr, md, nullptr, pkey);
   if (!ok) {
     return {false, "Failed to initialize digest verify."};
   }
@@ -71,12 +83,13 @@ const VerificationOutput Utility::verifySignature(absl::string_view hash, EVP_PK
   return {false, absl::StrCat("Failed to verify digest. Error code: ", ok)};
 }
 
-PublicKeyPtr Utility::importPublicKey(const std::vector<uint8_t>& key) {
+CryptoObjectPtr importPublicKey(const std::vector<uint8_t>& key) {
   CBS cbs({key.data(), key.size()});
-  return PublicKeyPtr(EVP_parse_public_key(&cbs));
+
+  return std::make_unique<PublicKeyObject>(EVP_parse_public_key(&cbs));
 }
 
-const EVP_MD* Utility::getHashFunction(absl::string_view name) {
+const EVP_MD* getHashFunction(absl::string_view name) {
   const std::string hash = absl::AsciiStrToLower(name);
 
   // Hash algorithms set refers
@@ -96,6 +109,7 @@ const EVP_MD* Utility::getHashFunction(absl::string_view name) {
   }
 }
 
+} // namespace Utility
 } // namespace Crypto
 } // namespace Common
 } // namespace Envoy
