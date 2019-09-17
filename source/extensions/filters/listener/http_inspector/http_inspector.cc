443b94595c48ad643edb73781d72592040629349
Yan Xue <3491507+yxue@users.noreply.github.com>
2019-09-17 13:18:04 -0700

    http inspector: rename h2 to h2c (#8227)
    Description: Rename h2 to h2c to respect the standard
    Risk Level: Low
    Testing: Unit test
    Docs Changes: N/A
    Release Notes: N/A
    
    Signed-off-by: crazyxy <yxyan@google.com>

--- source/extensions/filters/listener/http_inspector/http_inspector.cc
+++ source/extensions/filters/listener/http_inspector/http_inspector.cc
@@ -122,7 +122,9 @@ void Filter::done(bool success) {
     } else {
       ASSERT(protocol_ == "HTTP/2");
       config_->stats().http2_found_.inc();
-      protocol = "h2";
+      // h2 HTTP/2 over TLS, h2c HTTP/2 over TCP
+      // TODO(yxue): use detected protocol from http inspector and support h2c token in HCM
+      protocol = "h2c";
     }
 
     cb_->socket().setRequestedApplicationProtocols({protocol});
