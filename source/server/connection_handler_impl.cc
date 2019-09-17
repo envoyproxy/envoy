4bbdcb07fb768c9775ea86ddf50b7796102a3357
Dan Zhang <danzh@google.com>
2019-09-17 11:28:36 -0400

    typo
    Signed-off-by: Dan Zhang <danzh@google.com>

--- source/server/connection_handler_impl.cc
+++ source/server/connection_handler_impl.cc
@@ -32,7 +32,7 @@ void ConnectionHandlerImpl::addListener(Network::ListenerConfig& config) {
     listener = std::make_unique<ActiveTcpListener>(*this, config);
   } else {
     RELEASE_ASSERT(config.udpListenerFactory() != nullptr,
-                   "UDP listner factory is not initialized.");
+                   "UDP listener factory is not initialized.");
     listener =
         config.udpListenerFactory()->createActiveUdpListener(*this, dispatcher_, logger_, config);
   }
