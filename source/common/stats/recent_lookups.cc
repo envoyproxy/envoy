14f9eae0b76cf835a2d6d2bd7c3ab6fa5fbb6828
Joshua Marantz <jmarantz@google.com>
2019-09-18 16:05:00 -0400

    cleanup test deque behavior.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/common/stats/recent_lookups.cc
+++ source/common/stats/recent_lookups.cc
@@ -3,11 +3,7 @@
 #include <functional>
 #include <utility>
 
-//#include "envoy/common/time.h"
-
-#include "common/common/assert.h"
-//#include "common/common/logger.h"
-
+#include "absl/container/flat_hash_map.h"
 #include "absl/strings/str_join.h"
 
 namespace Envoy {
@@ -20,32 +16,30 @@ constexpr size_t Capacity = 10;
 
 void RecentLookups::lookup(absl::string_view str) {
   ++total_;
-
-  Map::iterator map_iter = map_.find(str);
-  if (map_iter != map_.end()) {
-    // The item is already in the list, but we need to bump its count and move
-    // it to the front, so we must re-order the list, which will invalidate the
-    // iterators to i.
-    List::iterator list_iter = map_iter->second;
-    ItemCount item_count = std::move(*list_iter);
-    list_.erase(list_iter);
-    ++item_count.count_;
-    list_.push_front(std::move(item_count));
-    map_iter->second = list_.begin();
-  } else {
-    ASSERT(list_.size() <= Capacity);
-    // Evict oldest item if needed.
-    if (list_.size() >= Capacity) {
-      ItemCount item_count = std::move(list_.back());
-      list_.pop_back();
-      map_.erase(item_count.item_);
-    }
-
-    // The string storage is in the list entry.
-    list_.push_front(ItemCount{std::string(str), 1});
-    List::iterator list_iter = list_.begin();
-    map_[list_iter->item_] = list_iter;
+  if (queue_.size() >= Capacity) {
+    queue_.pop_back();
   }
+  queue_.push_front(std::string(str));
+  /*
+  SystemTime now = time_source_.systemTime();
+  queue_.push_front(ItemTime(std::string(str), now));
+  if (now <= last_log_time_) { // handle black-swan event for non-monotonic time.
+    return;
+  }
+  std::chrono::seconds duration =
+      std::chrono::duration_cast<std::chrono::seconds>(now - last_log_time_);
+
+  if (duration >= std::chrono::seconds(LogIntervalSec)) {
+    std::vector<absl::string_view> message;
+    forEach([&message, this](absl::string_view item, SystemTime time) {
+      if (time > last_log_time_) {
+        message.push_back(item);
+      }
+    });
+    ENVOY_LOG_MISC(warn, "Recent lookups for {}", absl::StrJoin(message, ", "));
+    last_log_time_ = now;
+  }
+  */
 }
 
 /**
@@ -54,8 +48,12 @@ void RecentLookups::lookup(absl::string_view str) {
  * @param fn The function to call for every recently looked up item.
  */
 void RecentLookups::forEach(IterFn fn) const {
-  for (const ItemCount& item_count : list_) {
-    fn(item_count.item_, item_count.count_);
+  absl::flat_hash_map<absl::string_view, uint64_t> counts;
+  for (const std::string& item : queue_) {
+    ++counts[item];
+  }
+  for (auto iter : counts) {
+    fn(iter.first, iter.second);
   }
 }
 
--- source/common/stats/recent_lookups.h
+++ source/common/stats/recent_lookups.h
@@ -1,14 +1,9 @@
 #pragma once
 
+#include <deque>
 #include <functional>
-#include <map>
 #include <utility>
 
-//#include "envoy/common/time.h"
-
-//#include "common/common/hash.h"
-
-#include "absl/container/flat_hash_map.h"
 #include "absl/strings/string_view.h"
 
 namespace Envoy {
@@ -17,8 +12,6 @@ namespace Stats {
 // Remembers the last 'Capacity' items passed to lookup().
 class RecentLookups {
 public:
-  //explicit RecentLookups(TimeSource& time_source) : time_source_(time_source) {}
-
   /**
    * Records a lookup of a string. Only the last 'Capacity' lookups are remembered.
    *
@@ -36,36 +29,12 @@ public:
   void forEach(IterFn fn) const;
 
   /**
-   * @return the time-source associated with the object.
-   */
-  //TimeSource& timeSource() { return time_source_; }
-
-  /**
    * @return the total number of lookups since tracking began.
    */
   uint64_t total() const { return total_; }
 
 private:
-  struct ItemCount {
-    std::string item_;
-    int64_t count_;
-
-    /*bool operator<(const ItemCount& that) const {
-      if (count_ == that.count_) {
-        return item_ < that.item_;
-      }
-      return count_ < that.count_;
-    }
-
-    template <typename H> friend H AbslHashValue(H h, const ItemCount& item_count) {
-      return H::combine(std::move(h), item_count.item_);
-      }*/
-  };
-
-  using List = std::list<ItemCount>;
-  List list_;
-  using Map = absl::flat_hash_map<absl::string_view, List::iterator>;
-  Map map_;
+  std::deque<std::string> queue_;
   uint64_t total_{0};
 };
 
