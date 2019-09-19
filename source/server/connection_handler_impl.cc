272ee70a6d4da36f5a4fe398db055a2fc395d1ba
danzh <danzh2010@users.noreply.github.com>
2019-09-18 23:33:41 -0400

    quiche: implement QUIC specific TransportSocketFactory for TLS context (#8091)
    Signed-off-by: Dan Zhang <danzh@google.com>

--- source/server/connection_handler_impl.cc
+++ source/server/connection_handler_impl.cc
@@ -31,8 +31,7 @@ void ConnectionHandlerImpl::addListener(Network::ListenerConfig& config) {
   if (socket_type == Network::Address::SocketType::Stream) {
     listener = std::make_unique<ActiveTcpListener>(*this, config);
   } else {
-    ASSERT(socket_type == Network::Address::SocketType::Datagram,
-           "Only datagram/stream listener supported");
+    ASSERT(config.udpListenerFactory() != nullptr, "UDP listener factory is not initialized.");
     listener =
         config.udpListenerFactory()->createActiveUdpListener(*this, dispatcher_, logger_, config);
   }
