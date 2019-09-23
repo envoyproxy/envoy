2ac0d1bcd487e6699fe4b91169f2d6aaaf23e54c
Joshua Marantz <jmarantz@google.com>
2019-09-23 11:50:56 -0700

    grpc: Separate out grpc_init into its own class (#8293)
    * Separate out grpc init to a separate class.
    
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/exe/process_wide.h
+++ source/exe/process_wide.h
@@ -5,7 +5,7 @@
 namespace Envoy {
 
 // Process-wide lifecycle events for global state in third-party dependencies,
-// e.g. gRPC, c-ares. There should only ever be a single instance of this.
+// e.g. c-ares. There should only ever be a single instance of this.
 class ProcessWide {
 public:
   ProcessWide();
