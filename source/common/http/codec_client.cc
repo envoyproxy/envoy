f758d5ea4449761aa67b642343e90480299dc733
Dan Zhang <danzh@google.com>
2019-09-24 12:22:45 -0400

    Revert "remove client side impl"
    This reverts commit 6fe087a3dc52bf3196212377c697087fcc0f951a.
    
    Signed-off-by: Dan Zhang <danzh@google.com>

--- source/common/http/codec_client.cc
+++ source/common/http/codec_client.cc
@@ -9,6 +9,8 @@
 #include "common/http/http2/codec_impl.h"
 #include "common/http/utility.h"
 
+#include "extensions/quic_listeners/quiche/codec_impl.h"
+
 namespace Envoy {
 namespace Http {
 
@@ -17,9 +19,12 @@ CodecClient::CodecClient(Type type, Network::ClientConnectionPtr&& connection,
                          Event::Dispatcher& dispatcher)
     : type_(type), connection_(std::move(connection)), host_(host),
       idle_timeout_(host_->cluster().idleTimeout()) {
-  // Make sure upstream connections process data and then the FIN, rather than processing
-  // TCP disconnects immediately. (see https://github.com/envoyproxy/envoy/issues/1679 for details)
-  connection_->detectEarlyCloseWhenReadDisabled(false);
+  if (type_ != Type::HTTP3) {
+    // Make sure upstream connections process data and then the FIN, rather than processing
+    // TCP disconnects immediately. (see https://github.com/envoyproxy/envoy/issues/1679 for
+    // details)
+    connection_->detectEarlyCloseWhenReadDisabled(false);
+  }
   connection_->addConnectionCallbacks(*this);
   connection_->addReadFilter(Network::ReadFilterSharedPtr{new CodecReadFilter(*this)});
 
@@ -150,6 +155,10 @@ CodecClientProd::CodecClientProd(Type type, Network::ClientConnectionPtr&& conne
         *connection_, *this, host->cluster().statsScope(), host->cluster().http2Settings(),
         Http::DEFAULT_MAX_REQUEST_HEADERS_KB);
     break;
+  }
+  case Type::HTTP3: {
+    codec_ = std::make_unique<Quic::QuicHttpClientConnectionImpl>(
+        dynamic_cast<Quic::EnvoyQuicClientSession&>(*connection_), *this);
   }
   }
 }
