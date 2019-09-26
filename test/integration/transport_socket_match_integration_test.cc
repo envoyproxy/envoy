68083e73c221fb2c194653e95cece8d8a021ed3a
Jianfei Hu <jianfeih@google.com>
2019-09-26 18:13:16 +0000

    finish  ssl upstream creation, fails as expected.
    Signed-off-by: Jianfei Hu <jianfeih@google.com>

--- test/integration/transport_socket_match_integration_test.cc
+++ test/integration/transport_socket_match_integration_test.cc
@@ -1,3 +1,7 @@
+#include "extensions/transport_sockets/tls/context_config_impl.h"
+#include "extensions/transport_sockets/tls/context_impl.h"
+#include "extensions/transport_sockets/tls/ssl_socket.h"
+
 #include "test/integration/http_integration.h"
 
 #include "absl/strings/str_replace.h"
@@ -24,12 +28,7 @@ public:
                             TestEnvironment::getIpVersionsForTest().front(),
                             ConfigHelper::HTTP_PROXY_CONFIG),
         num_hosts_{1} {
-    // xds_upstream is the control plane management server, not the upstream...
-    // create_xds_upstream_ = true;
-    // tls_xds_upstream_ = true;
-    autonomous_upstream_ = false;
     setUpstreamCount(num_hosts_);
-
     config_helper_.addConfigModifier([&](envoy::config::bootstrap::v2::Bootstrap& bootstrap) {
       auto* static_resources = bootstrap.mutable_static_resources();
       auto* cluster = static_resources->mutable_clusters(0);
@@ -53,54 +52,40 @@ public:
     });
   }
 
-  // void SetUp() override {
-  // setDownstreamProtocol(Http::CodecClient::Type::HTTP1);
-  // setUpstreamProtocol(FakeHttpConnection::Type::HTTP1);
-  //}
+  Network::TransportSocketFactoryPtr createUpstreamSslContext() {
+    // copied from grpc_*_hardness.*.h
+    envoy::api::v2::auth::DownstreamTlsContext tls_context;
+    auto* common_tls_context = tls_context.mutable_common_tls_context();
+    common_tls_context->add_alpn_protocols("h2");
+    auto* tls_cert = common_tls_context->add_tls_certificates();
+    tls_cert->mutable_certificate_chain()->set_filename(
+        TestEnvironment::runfilesPath("test/config/integration/certs/upstreamcert.pem"));
+    tls_cert->mutable_private_key()->set_filename(
+        TestEnvironment::runfilesPath("test/config/integration/certs/upstreamkey.pem"));
+    // if (use_client_cert_) {
+    tls_context.mutable_require_client_certificate()->set_value(true);
+    auto* validation_context = common_tls_context->mutable_validation_context();
+    validation_context->mutable_trusted_ca()->set_filename(
+        TestEnvironment::runfilesPath("test/config/integration/certs/cacert.pem"));
+    //}
+    auto cfg = std::make_unique<Extensions::TransportSockets::Tls::ServerContextConfigImpl>(
+        tls_context, factory_context_);
+    static Stats::Scope* upstream_stats_store = new Stats::IsolatedStoreImpl();
+    return std::make_unique<Extensions::TransportSockets::Tls::ServerSslSocketFactory>(
+        std::move(cfg), context_manager_, *upstream_stats_store, std::vector<std::string>{});
+  }
 
   void createUpstreams() override {
     for (uint32_t i = 0; i < num_hosts_; i++) {
+      // Use below ssl upstream fails as expected...
+      // fake_upstreams_.emplace_back(
+      // std::make_unique<FakeUpstream>(createUpstreamSslContext(), 0,
+      // FakeHttpConnection::Type::HTTP1, version_, timeSystem()));
       fake_upstreams_.emplace_back(
           new FakeUpstream(0, FakeHttpConnection::Type::HTTP1, version_, timeSystem()));
     }
   }
-
-  void runTest(Http::TestHeaderMapImpl&) {
-    // TODO: here... how to get the fake upstream working with real response...
-    // look at the fake upstream, waitForHTTPConnection, needs to manually manage the upstream
-    // side...
-    //IntegrationStreamDecoderPtr response = codec_client_->makeHeaderOnlyRequest(request_headers);
-    //auto upstream_idx = waitForNextUpstreamRequest({0,1});
-    //ASSERT_TRUE(fake_upstream_connection_->waitForDisconnect());
-    //ASSERT_TRUE(fake_upstreams_[upstream_idx]->waitForHttpConnection(*dispatcher_,
-                                                                     //fake_upstream_connection_));
-    //waitForNextUpstreamRequest(upstream_idx);
-		//upstream_request_->encodeHeaders(default_response_headers_, false);
-		//upstream_request_->encodeData(512, true);
-    //response->waitForEndStream(); // note to myself required to obtain response.
-		//EXPECT_TRUE(upstream_request_->complete());
-    //EXPECT_EQ("200", response->headers().Status()->value().getStringView());
-  //const uint64_t second_id = Network::ConnectionImpl::nextGlobalIdForTest() + 1;
-  //codec_client_ = makeHttpConnection(creator());
-  //Http::TestHeaderMapImpl get_request_headers{
-      //{":method", "GET"},     {":path", "/test/long/url"}, {":scheme", "http"},
-      //{":authority", "host"}, {"x-lyft-user-id", "123"},   {"x-forwarded-for", "10.0.0.1"}};
-  //response =
-      //sendRequestAndWaitForResponse(get_request_headers, 128, default_response_headers_, 256);
-  //EXPECT_TRUE(upstream_request_->complete());
-  //EXPECT_EQ(128, upstream_request_->bodyLength());
-  //ASSERT_TRUE(response->complete());
-  //EXPECT_EQ("200", response->headers().Status()->value().getStringView());
-  }
   const uint32_t num_hosts_;
-  const std::string host_type_header_{"x-host-type"};
-  const std::string host_header_{"x-host"};
-  const std::string type_key_{"type"};
-  Http::TestHeaderMapImpl type_a_request_headers_{{":method", "GET"},
-                                                  {":path", "/test"},
-                                                  {":scheme", "http"},
-                                                  {":authority", "host"},
-                                                  {"x-type", "a"}};
 };
 
 TEST_F(TransportSockeMatchIntegrationTest, BasicMatch) {
@@ -109,13 +94,6 @@ TEST_F(TransportSockeMatchIntegrationTest, BasicMatch) {
   auto response =
       sendRequestAndWaitForResponse(default_request_headers_, 0, default_response_headers_, 0);
   EXPECT_EQ("200", response->headers().Status()->value().getStringView());
-  //EXPECT_TRUE(response->headers().get(
-                  //Envoy::Http::LowerCaseString{"x-custom-upstream-service-time"}) != nullptr);
-  //EXPECT_EQ("x-custom-upstream-service-time",
-            //response->headers().EnvoyUpstreamServiceTime()->key().getStringView());
-  //codec_client_ = makeHttpConnection(lookupPort("http"));
-  //runTest(type_a_request_headers_);
-  //timeSystem().sleep(std::chrono::milliseconds(10000000));
 }
 
 } // namespace Envoy
