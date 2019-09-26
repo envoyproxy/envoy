e4e94cd29262648618a64e4b5d36a724ebad2d49
Joshua Marantz <jmarantz@google.com>
2019-09-26 14:55:58 -0400

    const
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/common/stats/symbol_table_impl.h
+++ source/common/stats/symbol_table_impl.h
@@ -718,7 +718,7 @@ private:
 
   StatNameSet(SymbolTable& symbol_table, absl::string_view name);
 
-  std::string name_;
+  const std::string name_;
   Stats::SymbolTable& symbol_table_;
   Stats::StatNamePool pool_ GUARDED_BY(mutex_);
   absl::Mutex mutex_;
