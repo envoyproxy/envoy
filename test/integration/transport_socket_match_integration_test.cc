ae7cfb6db7783c2f6510be57c2a92f4bee64252b
Jianfei Hu <jianfeih@google.com>
2019-09-26 06:35:22 +0000

    figured out the fakeusptream need to managed by myself...
    Signed-off-by: Jianfei Hu <jianfeih@google.com>

--- test/integration/transport_socket_match_integration_test.cc
+++ test/integration/transport_socket_match_integration_test.cc
@@ -7,22 +7,27 @@ namespace Envoy {
 
 // TODO(incfly):
 // - upstream setup
-//     src/test/integration/integration.cc:517 is the example of creating the fake upstream
-//      with tls configuration, also see how Network::Test::createRawBufferSocket is invoked.
+//    use the FakeUpstream with transport socket factory typed constructor.
+//    override the createUpstreams()
+//    src/test/integration/integration.cc:517 is the example of creating the fake upstream
+//     with tls configuration, also see how Network::Test::createRawBufferSocket is invoked.
 //    just creating the fake upstream right away here...inline code for now.
+//    get rid of autonomous_upstream_
+//    line 71
 // - Client envoy configuration modifying
-// bazel test //test/integration:transport_socket_match_integration_test --test_output=streamed   --test_arg='-l info'
+// bazel test //test/integration:transport_socket_match_integration_test --test_output=streamed
+// --test_arg='-l info'
 class TransportSockeMatchIntegrationTest : public testing::Test, public HttpIntegrationTest {
 public:
   TransportSockeMatchIntegrationTest()
       : HttpIntegrationTest(Http::CodecClient::Type::HTTP1,
                             TestEnvironment::getIpVersionsForTest().front(),
                             ConfigHelper::HTTP_PROXY_CONFIG),
-        num_hosts_{4} {
+        num_hosts_{2} {
     // xds_upstream is the control plane management server, not the upstream...
-   // create_xds_upstream_ = true;
+    // create_xds_upstream_ = true;
     // tls_xds_upstream_ = true;
-    autonomous_upstream_ = true;
+    autonomous_upstream_ = false;
     setUpstreamCount(num_hosts_);
 
     config_helper_.addConfigModifier([&](envoy::config::bootstrap::v2::Bootstrap& bootstrap) {
@@ -41,38 +46,49 @@ public:
             TestEnvironment::getIpVersionsForTest().front()));
         addr->set_port_value(0);
         // Assign type metadata based on i.
-        //auto* metadata = lb_endpoint->mutable_metadata();
-        //Envoy::Config::Metadata::mutableMetadataValue(*metadata, "envoy.lb", type_key_)
-            //.set_string_value((i % 2 == 0) ? "a" : "b");
+        // auto* metadata = lb_endpoint->mutable_metadata();
+        // Envoy::Config::Metadata::mutableMetadataValue(*metadata, "envoy.lb", type_key_)
+        //.set_string_value((i % 2 == 0) ? "a" : "b");
       }
     });
-}
+  }
 
-  void SetUp() override {
-    setDownstreamProtocol(Http::CodecClient::Type::HTTP1);
-    setUpstreamProtocol(FakeHttpConnection::Type::HTTP1);
+  // void SetUp() override {
+  // setDownstreamProtocol(Http::CodecClient::Type::HTTP1);
+  // setUpstreamProtocol(FakeHttpConnection::Type::HTTP1);
+  //}
+
+  void createUpstreams() override {
+    for (uint32_t i = 0; i < num_hosts_; i++) {
+      fake_upstreams_.emplace_back(
+          new FakeUpstream(0, FakeHttpConnection::Type::HTTP1, version_, timeSystem()));
+    }
   }
 
   void runTest(Http::TestHeaderMapImpl& request_headers) {
-    // Send header only request.
+    // TODO: here... how to get the fake upstream working with real response...
+    // look at the fake upstream, waitForHTTPConnection, needs to manually manage the upstream
+    // side...
     IntegrationStreamDecoderPtr response = codec_client_->makeHeaderOnlyRequest(request_headers);
-    response->waitForEndStream();
+    response->waitForEndStream(); // note to myself required to obtain response.
     EXPECT_EQ("200", response->headers().Status()->value().getStringView());
-
   }
   const uint32_t num_hosts_;
   const std::string host_type_header_{"x-host-type"};
   const std::string host_header_{"x-host"};
   const std::string type_key_{"type"};
-  Http::TestHeaderMapImpl type_a_request_headers_{{":method", "GET"},  {":path", "/test"},
-                                                  {":scheme", "http"}, {":authority", "host"},
+  Http::TestHeaderMapImpl type_a_request_headers_{{":method", "GET"},
+                                                  {":path", "/test"},
+                                                  {":scheme", "http"},
+                                                  {":authority", "host"},
                                                   {"x-type", "a"}};
 };
 
 TEST_F(TransportSockeMatchIntegrationTest, BasicMatch) {
   initialize();
-  codec_client_ = makeHttpConnection(lookupPort("http"));
-  runTest(type_a_request_headers_);
+  // codec_client_ = makeHttpConnection(lookupPort("http"));
+  // runTest(type_a_request_headers_);
+  timeSystem().sleep(std::chrono::milliseconds(10000000));
 }
 
 } // namespace Envoy
