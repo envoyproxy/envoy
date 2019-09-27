5100ab22ec8fea4f80883969bf126acf76f3b741
Rama Chavali <rama.rao@salesforce.com>
2019-09-27 18:38:46 +0530

    fix test compilation
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- test/extensions/filters/listener/proxy_protocol/proxy_protocol_test.cc
+++ test/extensions/filters/listener/proxy_protocol/proxy_protocol_test.cc
@@ -77,6 +77,9 @@ public:
   uint64_t listenerTag() const override { return 1; }
   const std::string& name() const override { return name_; }
   const Network::ActiveUdpListenerFactory* udpListenerFactory() override { return nullptr; }
+  envoy::api::v2::core::TrafficDirection direction() const override {
+    return envoy::api::v2::core::TrafficDirection::UNSPECIFIED;
+  }
 
   // Network::FilterChainManager
   const Network::FilterChain* findFilterChain(const Network::ConnectionSocket&) const override {
