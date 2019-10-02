57ac960c54389dfa55df1d5c7db2754e3e326ffd
danzh <danzh2010@users.noreply.github.com>
2019-10-02 00:22:59 -0400

    quiche: modify some envoy quic implementation for code sharing (#8349)
    Signed-off-by: Dan Zhang <danzh@google.com>

--- test/extensions/quic_listeners/quiche/envoy_quic_writer_test.cc
+++ test/extensions/quic_listeners/quiche/envoy_quic_writer_test.cc
@@ -1,42 +1,54 @@
+#include <sys/types.h>
+
 #include <memory>
 #include <string>
 
+#include "common/network/address_impl.h"
 #include "common/network/io_socket_error_impl.h"
 
 #include "extensions/quic_listeners/quiche/envoy_quic_packet_writer.h"
 
+#include "test/mocks/api/mocks.h"
 #include "test/mocks/network/mocks.h"
+#include "test/test_common/threadsafe_singleton_injector.h"
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+
+using testing::Return;
 
 namespace Envoy {
 namespace Quic {
 
 class EnvoyQuicWriterTest : public ::testing::Test {
 public:
-  EnvoyQuicWriterTest() : envoy_quic_writer_(udp_listener_) {
-    self_address_.FromString("0.0.0.0");
+  EnvoyQuicWriterTest() : envoy_quic_writer_(socket_) {
+    self_address_.FromString("::");
     quic::QuicIpAddress peer_ip;
-    peer_ip.FromString("127.0.0.1");
+    peer_ip.FromString("::1");
     peer_address_ = quic::QuicSocketAddress(peer_ip, /*port=*/123);
-    EXPECT_CALL(udp_listener_, onDestroy());
-    ON_CALL(udp_listener_, send(_))
-        .WillByDefault(testing::Invoke([](const Network::UdpSendData& send_data) {
-          return Api::IoCallUint64Result(
-              send_data.buffer_.length(),
-              Api::IoErrorPtr(nullptr, Network::IoSocketError::deleteIoError));
-        }));
+    ON_CALL(os_sys_calls_, socket(_, _, _)).WillByDefault(Return(Api::SysCallIntResult{3, 0}));
+    ON_CALL(os_sys_calls_, close(3)).WillByDefault(Return(Api::SysCallIntResult{0, 0}));
   }
 
-  void verifySendData(const std::string& content, const Network::UdpSendData send_data) {
-    EXPECT_EQ(peer_address_.ToString(), send_data.peer_address_.asString());
-    EXPECT_EQ(self_address_.ToString(), send_data.local_ip_->addressAsString());
-    EXPECT_EQ(content, send_data.buffer_.toString());
+  void verifySendData(const std::string& content, const msghdr* message) {
+    EXPECT_EQ(peer_address_.ToString(), Network::Address::addressFromSockAddr(
+                                            *reinterpret_cast<sockaddr_storage*>(message->msg_name),
+                                            message->msg_namelen, /*v6only=*/false)
+                                            ->asString());
+    cmsghdr* const cmsg = CMSG_FIRSTHDR(message);
+    auto pktinfo = reinterpret_cast<in6_pktinfo*>(CMSG_DATA(cmsg));
+    EXPECT_EQ(0, memcmp(self_address_.GetIPv6().s6_addr, pktinfo->ipi6_addr.s6_addr,
+                        sizeof(pktinfo->ipi6_addr.s6_addr)));
+    EXPECT_EQ(1, message->msg_iovlen);
+    iovec iov = message->msg_iov[0];
+    EXPECT_EQ(content, std::string(reinterpret_cast<char*>(iov.iov_base), iov.iov_len));
   }
 
 protected:
-  testing::NiceMock<Network::MockUdpListener> udp_listener_;
+  testing::NiceMock<Api::MockOsSysCalls> os_sys_calls_;
+  TestThreadsafeSingletonInjector<Api::OsSysCallsImpl> os_calls_{&os_sys_calls_};
+  testing::NiceMock<Network::MockListenSocket> socket_;
   quic::QuicIpAddress self_address_;
   quic::QuicSocketAddress peer_address_;
   EnvoyQuicPacketWriter envoy_quic_writer_;
@@ -47,16 +59,16 @@ TEST_F(EnvoyQuicWriterTest, AssertOnNonNullPacketOption) {
   EXPECT_DEBUG_DEATH(envoy_quic_writer_.WritePacket(str.data(), str.length(), self_address_,
                                                     peer_address_,
                                                     reinterpret_cast<quic::PerPacketOptions*>(0x1)),
-                     "Per packet option is not supported yet.");
+                     "");
 }
 
 TEST_F(EnvoyQuicWriterTest, SendSuccessfully) {
   std::string str("Hello World!");
-  EXPECT_CALL(udp_listener_, send(_))
-      .WillOnce(testing::Invoke([this, str](const Network::UdpSendData& send_data) {
-        verifySendData(str, send_data);
-        return Api::IoCallUint64Result(
-            str.length(), Api::IoErrorPtr(nullptr, Network::IoSocketError::deleteIoError));
+
+  EXPECT_CALL(os_sys_calls_, sendmsg(_, _, _))
+      .WillOnce(testing::Invoke([this, str](int, const msghdr* message, int) {
+        verifySendData(str, message);
+        return Api::SysCallSizeResult{static_cast<ssize_t>(str.length()), 0};
       }));
   quic::WriteResult result = envoy_quic_writer_.WritePacket(str.data(), str.length(), self_address_,
                                                             peer_address_, nullptr);
@@ -67,12 +79,10 @@ TEST_F(EnvoyQuicWriterTest, SendSuccessfully) {
 
 TEST_F(EnvoyQuicWriterTest, SendBlocked) {
   std::string str("Hello World!");
-  EXPECT_CALL(udp_listener_, send(_))
-      .WillOnce(testing::Invoke([this, str](const Network::UdpSendData& send_data) {
-        verifySendData(str, send_data);
-        return Api::IoCallUint64Result(
-            0u, Api::IoErrorPtr(Network::IoSocketError::getIoSocketEagainInstance(),
-                                Network::IoSocketError::deleteIoError));
+  EXPECT_CALL(os_sys_calls_, sendmsg(_, _, _))
+      .WillOnce(testing::Invoke([this, str](int, const msghdr* message, int) {
+        verifySendData(str, message);
+        return Api::SysCallSizeResult{-1, EAGAIN};
       }));
   quic::WriteResult result = envoy_quic_writer_.WritePacket(str.data(), str.length(), self_address_,
                                                             peer_address_, nullptr);
@@ -81,31 +91,26 @@ TEST_F(EnvoyQuicWriterTest, SendBlocked) {
   EXPECT_TRUE(envoy_quic_writer_.IsWriteBlocked());
   // Writing while blocked is not allowed.
 #ifdef NDEBUG
-  EXPECT_CALL(udp_listener_, send(_))
-      .WillOnce(testing::Invoke([this, str](const Network::UdpSendData& send_data) {
-        verifySendData(str, send_data);
-        return Api::IoCallUint64Result(
-            0u, Api::IoErrorPtr(Network::IoSocketError::getIoSocketEagainInstance(),
-                                Network::IoSocketError::deleteIoError));
+  EXPECT_CALL(os_sys_calls_, sendmsg(_, _, _))
+      .WillOnce(testing::Invoke([this, str](int, const msghdr* message, int) {
+        verifySendData(str, message);
+        return Api::SysCallSizeResult{-1, EAGAIN};
       }));
 #endif
   EXPECT_DEBUG_DEATH(envoy_quic_writer_.WritePacket(str.data(), str.length(), self_address_,
                                                     peer_address_, nullptr),
-                     "Cannot write while IO handle is blocked.");
+                     "");
   envoy_quic_writer_.SetWritable();
   EXPECT_FALSE(envoy_quic_writer_.IsWriteBlocked());
 }
 
 TEST_F(EnvoyQuicWriterTest, SendFailure) {
   std::string str("Hello World!");
-  EXPECT_CALL(udp_listener_, send(_))
-      .WillOnce(testing::Invoke(
-          [this, str](const Network::UdpSendData& send_data) -> Api::IoCallUint64Result {
-            verifySendData(str, send_data);
-            return Api::IoCallUint64Result(0u,
-                                           Api::IoErrorPtr(new Network::IoSocketError(ENOTSUP),
-                                                           Network::IoSocketError::deleteIoError));
-          }));
+  EXPECT_CALL(os_sys_calls_, sendmsg(_, _, _))
+      .WillOnce(testing::Invoke([this, str](int, const msghdr* message, int) {
+        verifySendData(str, message);
+        return Api::SysCallSizeResult{-1, ENOTSUP};
+      }));
   quic::WriteResult result = envoy_quic_writer_.WritePacket(str.data(), str.length(), self_address_,
                                                             peer_address_, nullptr);
   EXPECT_EQ(quic::WRITE_STATUS_ERROR, result.status);
@@ -115,11 +120,10 @@ TEST_F(EnvoyQuicWriterTest, SendFailure) {
 
 TEST_F(EnvoyQuicWriterTest, SendFailureMessageTooBig) {
   std::string str("Hello World!");
-  EXPECT_CALL(udp_listener_, send(_))
-      .WillOnce(testing::Invoke([this, str](const Network::UdpSendData& send_data) {
-        verifySendData(str, send_data);
-        return Api::IoCallUint64Result(0u, Api::IoErrorPtr(new Network::IoSocketError(EMSGSIZE),
-                                                           Network::IoSocketError::deleteIoError));
+  EXPECT_CALL(os_sys_calls_, sendmsg(_, _, _))
+      .WillOnce(testing::Invoke([this, str](int, const msghdr* message, int) {
+        verifySendData(str, message);
+        return Api::SysCallSizeResult{-1, EMSGSIZE};
       }));
   quic::WriteResult result = envoy_quic_writer_.WritePacket(str.data(), str.length(), self_address_,
                                                             peer_address_, nullptr);
