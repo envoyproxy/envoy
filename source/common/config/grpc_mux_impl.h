52c798e2c4d8b65e3d9d655668b4602bf3a603a3
Fred Douglas <fredlas@google.com>
2019-10-02 12:01:45 -0400

    get github to diff grpc_mux_impl against new_grpc_mux impl, part 2
    Signed-off-by: Fred Douglas <fredlas@google.com>

--- source/common/config/grpc_mux_impl.h
+++ source/common/config/grpc_mux_impl.h
@@ -1,155 +1,117 @@
 #pragma once
 
-#include <queue>
-#include <unordered_map>
-
-#include "envoy/common/time.h"
+#include "envoy/api/v2/discovery.pb.h"
+#include "envoy/common/token_bucket.h"
 #include "envoy/config/grpc_mux.h"
 #include "envoy/config/subscription.h"
-#include "envoy/event/dispatcher.h"
-#include "envoy/grpc/status.h"
-#include "envoy/upstream/cluster_manager.h"
 
-#include "common/common/cleanup.h"
 #include "common/common/logger.h"
+#include "common/config/delta_subscription_state.h"
 #include "common/config/grpc_stream.h"
-#include "common/config/utility.h"
+#include "common/config/pausable_ack_queue.h"
+#include "common/config/watch_map.h"
+#include "common/grpc/common.h"
 
 namespace Envoy {
 namespace Config {
 
-/**
- * ADS API implementation that fetches via gRPC.
- */
-class GrpcMuxImpl : public GrpcMux,
-                    public GrpcStreamCallbacks<envoy::api::v2::DiscoveryResponse>,
-                    public Logger::Loggable<Logger::Id::config> {
+// Manages subscriptions to one or more type of resource. The logical protocol
+// state of those subscription(s) is handled by DeltaSubscriptionState.
+// This class owns the GrpcStream used to talk to the server, maintains queuing
+// logic to properly order the subscription(s)' various messages, and allows
+// starting/stopping/pausing of the subscriptions.
+class NewGrpcMuxImpl : public GrpcMux,
+                       public GrpcStreamCallbacks<envoy::api::v2::DeltaDiscoveryResponse>,
+                       Logger::Loggable<Logger::Id::config> {
 public:
-  GrpcMuxImpl(const LocalInfo::LocalInfo& local_info, Grpc::RawAsyncClientPtr async_client,
-              Event::Dispatcher& dispatcher, const Protobuf::MethodDescriptor& service_method,
-              Runtime::RandomGenerator& random, Stats::Scope& scope,
-              const RateLimitSettings& rate_limit_settings, bool skip_subsequent_node);
-  ~GrpcMuxImpl() override;
+  NewGrpcMuxImpl(Grpc::RawAsyncClientPtr&& async_client, Event::Dispatcher& dispatcher,
+                 const Protobuf::MethodDescriptor& service_method, Runtime::RandomGenerator& random,
+                 Stats::Scope& scope, const RateLimitSettings& rate_limit_settings,
+                 const LocalInfo::LocalInfo& local_info);
 
-  void start() override;
-  GrpcMuxWatchPtr subscribe(const std::string& type_url, const std::set<std::string>& resources,
-                            GrpcMuxCallbacks& callbacks) override;
+  Watch* addOrUpdateWatch(const std::string& type_url, Watch* watch,
+                          const std::set<std::string>& resources, SubscriptionCallbacks& callbacks,
+                          std::chrono::milliseconds init_fetch_timeout) override;
+  void removeWatch(const std::string& type_url, Watch* watch) override;
 
-  // GrpcMux
   void pause(const std::string& type_url) override;
   void resume(const std::string& type_url) override;
   bool paused(const std::string& type_url) const override;
+  void
+  onDiscoveryResponse(std::unique_ptr<envoy::api::v2::DeltaDiscoveryResponse>&& message) override;
 
-  Watch* addOrUpdateWatch(const std::string&, Watch*, const std::set<std::string>&,
-                          SubscriptionCallbacks&, std::chrono::milliseconds) override {
-    NOT_IMPLEMENTED_GCOVR_EXCL_LINE;
-  }
-  void removeWatch(const std::string&, Watch*) override { NOT_IMPLEMENTED_GCOVR_EXCL_LINE; }
-
-  void handleDiscoveryResponse(std::unique_ptr<envoy::api::v2::DiscoveryResponse>&& message);
-
-  void sendDiscoveryRequest(const std::string& type_url);
-
-  // Config::GrpcStreamCallbacks
   void onStreamEstablished() override;
+
   void onEstablishmentFailure() override;
-  void onDiscoveryResponse(std::unique_ptr<envoy::api::v2::DiscoveryResponse>&& message) override;
+
   void onWriteable() override;
 
-  GrpcStream<envoy::api::v2::DiscoveryRequest, envoy::api::v2::DiscoveryResponse>&
-  grpcStreamForTest() {
-    return grpc_stream_;
-  }
+  void kickOffAck(UpdateAck ack);
+
+  // TODO(fredlas) remove these two from the GrpcMux interface.
+  GrpcMuxWatchPtr subscribe(const std::string&, const std::set<std::string>&,
+                            GrpcMuxCallbacks&) override;
+  void start() override;
 
 private:
-  void drainRequests();
-  void setRetryTimer();
+  Watch* addWatch(const std::string& type_url, const std::set<std::string>& resources,
+                  SubscriptionCallbacks& callbacks, std::chrono::milliseconds init_fetch_timeout);
 
-  struct GrpcMuxWatchImpl : public GrpcMuxWatch, RaiiListElement<GrpcMuxWatchImpl*> {
-    GrpcMuxWatchImpl(const std::set<std::string>& resources, GrpcMuxCallbacks& callbacks,
-                     const std::string& type_url, GrpcMuxImpl& parent)
-        : RaiiListElement<GrpcMuxWatchImpl*>(parent.api_state_[type_url].watches_, this),
-          resources_(resources), callbacks_(callbacks), type_url_(type_url), parent_(parent),
-          inserted_(true) {}
-    ~GrpcMuxWatchImpl() override {
-      if (inserted_) {
-        erase();
-        if (!resources_.empty()) {
-          parent_.sendDiscoveryRequest(type_url_);
-        }
-      }
-    }
+  // Updates the list of resource names watched by the given watch. If an added name is new across
+  // the whole subscription, or if a removed name has no other watch interested in it, then the
+  // subscription will enqueue and attempt to send an appropriate discovery request.
+  void updateWatch(const std::string& type_url, Watch* watch,
+                   const std::set<std::string>& resources);
 
-    void clear() {
-      inserted_ = false;
-      cancel();
-    }
+  void addSubscription(const std::string& type_url, std::chrono::milliseconds init_fetch_timeout);
 
-    std::set<std::string> resources_;
-    GrpcMuxCallbacks& callbacks_;
-    const std::string type_url_;
-    GrpcMuxImpl& parent_;
+  void trySendDiscoveryRequests();
 
-  private:
-    bool inserted_;
-  };
+  // Checks whether external conditions allow sending a DeltaDiscoveryRequest. (Does not check
+  // whether we *want* to send a DeltaDiscoveryRequest).
+  bool canSendDiscoveryRequest(const std::string& type_url);
 
-  // Per muxed API state.
-  struct ApiState {
-    // Watches on the returned resources for the API;
-    std::list<GrpcMuxWatchImpl*> watches_;
-    // Current DiscoveryRequest for API.
-    envoy::api::v2::DiscoveryRequest request_;
-    // Paused via pause()?
-    bool paused_{};
-    // Was a DiscoveryRequest elided during a pause?
-    bool pending_{};
-    // Has this API been tracked in subscriptions_?
-    bool subscribed_{};
-  };
+  // Checks whether we have something to say in a DeltaDiscoveryRequest, which can be an ACK and/or
+  // a subscription update. (Does not check whether we *can* send that DeltaDiscoveryRequest).
+  // Returns the type_url we should send the DeltaDiscoveryRequest for (if any).
+  // First, prioritizes ACKs over non-ACK subscription interest updates.
+  // Then, prioritizes non-ACK updates in the order the various types
+  // of subscriptions were activated.
+  absl::optional<std::string> whoWantsToSendDiscoveryRequest();
 
-  // Request queue management logic.
-  void queueDiscoveryRequest(const std::string& queue_item);
-  void clearRequestQueue();
-
-  GrpcStream<envoy::api::v2::DiscoveryRequest, envoy::api::v2::DiscoveryResponse> grpc_stream_;
+  Event::Dispatcher& dispatcher_;
   const LocalInfo::LocalInfo& local_info_;
-  const bool skip_subsequent_node_;
-  bool first_stream_request_;
-  std::unordered_map<std::string, ApiState> api_state_;
-  // Envoy's dependency ordering.
-  std::list<std::string> subscriptions_;
 
-  // A queue to store requests while rate limited. Note that when requests cannot be sent due to the
-  // gRPC stream being down, this queue does not store them; rather, they are simply dropped.
-  // This string is a type URL.
-  std::queue<std::string> request_queue_;
+  // Resource (N)ACKs we're waiting to send, stored in the order that they should be sent in. All
+  // of our different resource types' ACKs are mixed together in this queue. See class for
+  // description of how it interacts with pause() and resume().
+  PausableAckQueue pausable_ack_queue_;
+
+  struct SubscriptionStuff {
+    SubscriptionStuff(const std::string& type_url, std::chrono::milliseconds init_fetch_timeout,
+                      Event::Dispatcher& dispatcher, const LocalInfo::LocalInfo& local_info)
+        : sub_state_(type_url, watch_map_, local_info, init_fetch_timeout, dispatcher),
+          init_fetch_timeout_(init_fetch_timeout) {}
+
+    WatchMap watch_map_;
+    DeltaSubscriptionState sub_state_;
+    const std::chrono::milliseconds init_fetch_timeout_;
+
+    SubscriptionStuff(const SubscriptionStuff&) = delete;
+    SubscriptionStuff& operator=(const SubscriptionStuff&) = delete;
+  };
+  // Map key is type_url.
+  absl::flat_hash_map<std::string, std::unique_ptr<SubscriptionStuff>> subscriptions_;
+
+  // Determines the order of initial discovery requests. (Assumes that subscriptions are added in
+  // the order of Envoy's dependency ordering).
+  std::list<std::string> subscription_ordering_;
+
+  GrpcStream<envoy::api::v2::DeltaDiscoveryRequest, envoy::api::v2::DeltaDiscoveryResponse>
+      grpc_stream_;
 };
 
-class NullGrpcMuxImpl : public GrpcMux, GrpcStreamCallbacks<envoy::api::v2::DiscoveryResponse> {
-public:
-  void start() override {}
-  GrpcMuxWatchPtr subscribe(const std::string&, const std::set<std::string>&,
-                            GrpcMuxCallbacks&) override {
-    throw EnvoyException("ADS must be configured to support an ADS config source");
-  }
-  void pause(const std::string&) override {}
-  void resume(const std::string&) override {}
-  bool paused(const std::string&) const override { return false; }
-
-  Watch* addOrUpdateWatch(const std::string&, Watch*, const std::set<std::string>&,
-                          SubscriptionCallbacks&, std::chrono::milliseconds) override {
-    throw EnvoyException("ADS must be configured to support an ADS config source");
-  }
-  void removeWatch(const std::string&, Watch*) override {
-    throw EnvoyException("ADS must be configured to support an ADS config source");
-  }
-
-  void onWriteable() override {}
-  void onStreamEstablished() override {}
-  void onEstablishmentFailure() override {}
-  void onDiscoveryResponse(std::unique_ptr<envoy::api::v2::DiscoveryResponse>&&) override {}
-};
+using NewGrpcMuxImplSharedPtr = std::shared_ptr<NewGrpcMuxImpl>;
 
 } // namespace Config
 } // namespace Envoy
