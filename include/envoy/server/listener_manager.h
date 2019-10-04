35a6240f97090f79ed3293f5e59b71b2ece8c3e3
Rama Chavali <rama.rao@salesforce.com>
2019-10-04 13:30:27 +0530

    format
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- include/envoy/server/listener_manager.h
+++ include/envoy/server/listener_manager.h
@@ -157,11 +157,11 @@ public:
    * Stops a listener from accepting new connections without actually removing any of them. This is
    * used by /drain_listeners admin endpoint. This methods directly stops the listeners on workers.
    * There is a potential race condition, where the listener that is being stopped here, might have
-   * got updated configuration and there by may be in warming state that might add listeners back to
-   * workers. Given that this API is intenteded to be used from admin endpoint that is supposedly
-   * used when Envoy is terminating, that is considered as an edge case. The complexity of handling
-   * warmed listeners and preventing it being added back by name is not worth the usecase of this
-   * API.
+   * got updated configuration from management server and there by may be in warming state and that
+   * might add listeners back to workers. Given that this API is intenteded to be used from admin
+   * endpoint that is typically used when an Envoy is terminating, this is considered an edge case.
+   * The complexity of handling warmed listeners and preventing them being added back by name is not
+   * worth the usecase of this API.
    * @param name supplies the listener config to stop.
    */
   virtual void stopListener(Network::ListenerConfig& listener) PURE;
