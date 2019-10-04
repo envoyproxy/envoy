dca089823fa6453b9323243f831053a1d1460c59
Bence Béky <bnc@google.com>
2019-10-04 13:32:21 -0400

    quiche: use event loop prepare time in QuicClock (#7177)
    Signed-off-by: Bence Béky <bnc@google.com>

--- test/mocks/event/mocks.h
+++ test/mocks/event/mocks.h
@@ -119,6 +119,7 @@ public:
   MOCK_CONST_METHOD0(isThreadSafe, bool());
   Buffer::WatermarkFactory& getWatermarkFactory() override { return buffer_factory_; }
   MOCK_METHOD0(getCurrentThreadId, Thread::ThreadId());
+  MOCK_CONST_METHOD0(getSchedulerPrepareTime, timeval());
 
   GlobalTimeSystem time_system_;
   std::list<DeferredDeletablePtr> to_delete_;
