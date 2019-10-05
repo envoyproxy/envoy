9438ff429fe5c4ff58f3ee305e4f6237e5cf7c8e
Yan Avlasov <yavlasov@google.com>
2019-10-04 21:37:47 -0400

    Fix tests after merging master
    Signed-off-by: Yan Avlasov <yavlasov@google.com>

--- test/extensions/filters/http/grpc_json_transcoder/grpc_json_transcoder_integration_test.cc
+++ test/extensions/filters/http/grpc_json_transcoder/grpc_json_transcoder_integration_test.cc
@@ -384,7 +384,8 @@ TEST_P(GrpcJsonTranscoderIntegrationTest, UnaryErrorInTrailerConvertedToJson) {
   const std::string filter =
       R"EOF(
             name: envoy.grpc_json_transcoder
-            config:
+            typed_config:
+              "@type": type.googleapis.com/envoy.config.filter.http.transcoder.v2.GrpcJsonTranscoder
               proto_descriptor: "{}"
               services: "bookstore.Bookstore"
               convert_grpc_status: true
@@ -408,7 +409,8 @@ TEST_P(GrpcJsonTranscoderIntegrationTest, StreamingErrorConvertedToJson) {
   const std::string filter =
       R"EOF(
             name: envoy.grpc_json_transcoder
-            config:
+            typed_config:
+              "@type": type.googleapis.com/envoy.config.filter.http.transcoder.v2.GrpcJsonTranscoder
               proto_descriptor: "{}"
               services: "bookstore.Bookstore"
               convert_grpc_status: true
