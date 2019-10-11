ca60948e961cf46104905780e9d7df47c186ff76
Snow Pettersen <snowp@squareup.com>
2019-10-11 10:33:14 -0400

    format
    Signed-off-by: Snow Pettersen <snowp@squareup.com>

--- test/common/http/http1/header_formatter_test.cc
+++ test/common/http/http1/header_formatter_test.cc
@@ -1,18 +1,18 @@
 #include "common/http/http1/header_formatter.h"
- 
+
 #include "gtest/gtest.h"
 
 namespace Envoy {
 namespace Http {
 namespace Http1 {
-  TEST(ProperCaseHeaderKeyFormatterTest, Formatting) {
-    ProperCaseHeaderKeyFormatter formatter;
+TEST(ProperCaseHeaderKeyFormatterTest, Formatting) {
+  ProperCaseHeaderKeyFormatter formatter;
 
-    const std::string downcased = "content-type";
-    const std::string specialCharacters = "a!d#sa-lo";
-    EXPECT_EQ(formatter.format(downcased), "Content-Type");
-    EXPECT_EQ(formatter.format(specialCharacters), "A!D#Sa-Lo");
-  }
+  const std::string downcased = "content-type";
+  const std::string specialCharacters = "a!d#sa-lo";
+  EXPECT_EQ(formatter.format(downcased), "Content-Type");
+  EXPECT_EQ(formatter.format(specialCharacters), "A!D#Sa-Lo");
 }
+} // namespace Http1
 } // namespace Http
 } // namespace Envoy
