97fcf597b338a9933ff7aafc85e12fd1fa6f9531
Jose Ulises Nino Rivera <junr03@users.noreply.github.com>
2019-10-15 14:07:36 -0700

    grpc: close stream with data frame (#500)
    Description: the gRPC protocol requires closing the client stream in a DATA frame. This PR fixes the GRPCStreamEmitter for iOS.
    Risk Level: med - protocol level changes.
    Testing: local.
    
    Signed-off-by: Jose Nino <jnino@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/kotlin/src/io/envoyproxy/envoymobile/StreamEmitter.kt
+++ mobile/library/kotlin/src/io/envoyproxy/envoymobile/StreamEmitter.kt
@@ -44,10 +44,10 @@ interface StreamEmitter : CancelableStream {
   /**
    * For ending an associated stream and sending trailers.
    *
-   * @param trailers to send with ending a stream. If no trailers are needed, empty map will be the default.
+   * @param trailers to send with ending a stream. If null, stream will be closed with an empty data frame.
    * @throws IllegalStateException when the stream is not active.
    * @throws EnvoyException when there is an exception ending the stream or sending trailers.
    */
   @Throws(EnvoyException::class)
-  fun close(trailers: Map<String, List<String>> = emptyMap())
+  fun close(trailers: Map<String, List<String>>?)
 }
