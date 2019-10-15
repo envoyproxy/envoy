2a5f7e2072a56500c4cf5e644e146d43ed08114d
Marco Magdy <mmagdy@gmail.com>
2019-10-15 15:17:25 -0700

    Added comments to the wildcard matching function
    Signed-off-by: Marco Magdy <mmagdy@gmail.com>

--- source/extensions/tracers/xray/util.cc
+++ source/extensions/tracers/xray/util.cc
@@ -7,6 +7,22 @@ namespace Extensions {
 namespace Tracers {
 namespace XRay {
 
+// AWS X-Ray has two sources of sampling rules (wildcard patterns):
+//
+// 1- The manifest file (static config)
+// 2- The X-Ray Service - periodically polled for new rules. (dynamic config)
+//
+// X-Ray will inspect every single request and try to match it against the set of rules it has to
+// decide whether or not a given request should be sampled. That means this wildcard matching
+// routine is on the hot path. I've spent a great deal of time to make this function optimal and not
+// allocate.
+// Using regex matching here has many downsides and it would require us to:
+// 1- escape/lint the user input pattern to avoid messing up its meaning (think '.' character is a
+// valid regex and URL character) 2- compile the regex and store it with every corresponding part of
+// the rule
+//
+// Those two steps would add significant overhead to the tracer. Meanwhile, the following function
+// has a comprehensive test suite and fuzz tests.
 bool wildcardMatch(absl::string_view pattern, absl::string_view input) {
   if (pattern.empty()) {
     return input.empty();
