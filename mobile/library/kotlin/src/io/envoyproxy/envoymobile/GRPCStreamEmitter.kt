5e96a17f8f8f27c049428a75e886e0563d61ac8d
Alan Chiu <achiu@lyft.com>
2019-10-16 18:22:59 -0700

    kotlin: grpc codec (#472)
    gRPC codec for kotlin
    
    We are able to hit an Envoy backed gRPC server from `lyft.com`
    
    We ran into some issues with testing (debug pull: https://github.com/lyft/envoy-mobile/pull/495). The first is outgoing ALPN is required for gRPC connections: https://github.com/lyft/envoy-mobile/issues/502. gRPC doesn't support disabling this option when starting up a service. We'll have to revisit this effort in the future.
    
    For now, we are primarily missing https://github.com/lyft/envoy-mobile/pull/494 for local Envoy library e2e testing
    
    Signed-off-by: Alan Chiu <achiu@lyft.com>
    
    Description: gRPC codec for kotlin
    Risk Level: low
    Testing: unit/end-to-end
    Docs Changes: n/a
    Release Notes: n/a
    [Optional Fixes #Issue]
    [Optional Deprecated:]
    
    Signed-off-by: JP Simard <jp@jpsim.com>

---
+++ mobile/library/kotlin/src/io/envoyproxy/envoymobile/GRPCStreamEmitter.kt
@@ -0,0 +1,41 @@
+package io.envoyproxy.envoymobile
+
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+
+class GRPCStreamEmitter(
+    private val emitter: StreamEmitter
+) {
+  /**
+   * Send a protobuf messageData's binary data over the gRPC stream.
+   *
+   * @param messageData Binary data of a protobuf messageData to send.
+   */
+  fun sendMessage(messageData: ByteBuffer) {
+    // https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests
+    // Length-Prefixed-Message = Compressed-Flag | Message-Length | Message
+    // Compressed-Flag = 0 / 1, encoded as 1 byte unsigned integer
+    // Message-Length = length of Message, encoded as 4 byte unsigned integer (big endian)
+    // Message = binary representation of protobuf messageData
+    val byteBuffer = ByteBuffer.allocate(GRPC_PREFIX_LENGTH)
+
+    // Compression flag (1 byte) - 0, not compressed
+    byteBuffer.put(0)
+
+    // Message length
+    val messageLength = messageData.remaining()
+    byteBuffer.order(ByteOrder.BIG_ENDIAN)
+    byteBuffer.putInt(messageLength)
+
+    emitter.sendData(byteBuffer)
+    emitter.sendData(messageData)
+  }
+
+  /**
+   * Close this connection.
+   */
+  fun close() {
+    emitter.close(null)
+  }
+}
