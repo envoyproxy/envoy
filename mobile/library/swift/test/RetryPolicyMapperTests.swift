ee11558f9274053cdacd68c4ecc94752abb7de05
Michael Rebello <me@michaelrebello.com>
2019-10-17 16:17:14 -0700

    swift: add support for total upstream timeouts (#518)
    When users set a timeout via `perRetryTimeoutMS`, they silently get limited to 15 seconds due to the fact that Envoy assigns a [default timeout of 15 seconds](https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/route/route.proto#envoy-api-field-route-routeaction-timeout). We should expose this through our `RetryPolicy` to allow consumers to override it using the already-supported `x-envoy-upstream-rq-timeout-ms` header.
    
    Related Kotlin PR: https://github.com/lyft/envoy-mobile/pull/519
    
    Details on retry semantics: [Envoy docs](https://www.envoyproxy.io/docs/envoy/latest/intro/arch_overview/http/http_routing.html?highlight=exponential#retry-semantics)
    
    Signed-off-by: Michael Rebello <me@michaelrebello.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/swift/test/RetryPolicyMapperTests.swift
+++ mobile/library/swift/test/RetryPolicyMapperTests.swift
@@ -3,30 +3,24 @@ import XCTest
 
 final class RetryPolicyMapperTests: XCTestCase {
   func testConvertingToHeadersWithPerRetryTimeoutIncludesAllHeaders() {
-    let policy = RetryPolicy(maxRetryCount: 123,
+    let policy = RetryPolicy(maxRetryCount: 3,
                              retryOn: RetryRule.allCases,
-                             perRetryTimeoutMS: 9001)
+                             perRetryTimeoutMS: 15_000,
+                             totalUpstreamTimeoutMS: 60_000)
     let expectedHeaders = [
-      "x-envoy-max-retries": ["123"],
+      "x-envoy-max-retries": ["3"],
       "x-envoy-retry-on": [
         "5xx", "gateway-error", "connect-failure", "retriable-4xx", "refused-upstream",
       ],
-      "x-envoy-upstream-rq-per-try-timeout-ms": ["9001"],
+      "x-envoy-upstream-rq-per-try-timeout-ms": ["15000"],
+      "x-envoy-upstream-rq-timeout-ms": ["60000"],
     ]
 
     XCTAssertEqual(expectedHeaders, policy.outboundHeaders())
   }
 
   func testConvertingToHeadersWithoutRetryTimeoutExcludesPerRetryTimeoutHeader() {
-    let policy = RetryPolicy(maxRetryCount: 123,
-                             retryOn: RetryRule.allCases)
-    let expectedHeaders = [
-      "x-envoy-max-retries": ["123"],
-      "x-envoy-retry-on": [
-        "5xx", "gateway-error", "connect-failure", "retriable-4xx", "refused-upstream",
-      ],
-    ]
-
-    XCTAssertEqual(expectedHeaders, policy.outboundHeaders())
+    let policy = RetryPolicy(maxRetryCount: 123, retryOn: RetryRule.allCases)
+    XCTAssertNil(policy.outboundHeaders()["x-envoy-upstream-rq-per-try-timeout-ms"])
   }
 }
