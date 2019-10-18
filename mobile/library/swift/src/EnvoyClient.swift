85bd4fd6abc97606d7d294df586d04b761c096a6
Michael Rebello <me@michaelrebello.com>
2019-10-18 12:38:16 -0700

    ios: only buffer when requests support retries (#521)
    Updates to utilize the options exposed in https://github.com/lyft/envoy-mobile/pull/520 to prevent unnecessarily buffering requests that will never be retried. This can be especially useful for preventing buffering of long-lived [gRPC] streams, for example.
    
    We discussed having a full fledged platform level struct to parallel `envoy_stream_options`, but decided that while we only expose one option we can leave the API as a boolean argument.
    
    Matching Android functionality is being tracked here: https://github.com/lyft/envoy-mobile/issues/524.
    
    Signed-off-by: Michael Rebello <me@michaelrebello.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/swift/src/EnvoyClient.swift
+++ mobile/library/swift/src/EnvoyClient.swift
@@ -69,7 +69,8 @@ public final class EnvoyClient: NSObject {
 
 extension EnvoyClient: HTTPClient {
   public func send(_ request: Request, handler: ResponseHandler) -> StreamEmitter {
-    let httpStream = self.engine.startStream(with: handler.underlyingCallbacks)
+    let httpStream = self.engine.startStream(
+      with: handler.underlyingCallbacks, bufferForRetry: request.retryPolicy != nil)
     httpStream.sendHeaders(request.outboundHeaders(), close: false)
     return EnvoyStreamEmitter(stream: httpStream)
   }
--- mobile/library/swift/test/EnvoyClientTests.swift
+++ mobile/library/swift/test/EnvoyClientTests.swift
@@ -11,17 +11,18 @@ private final class MockEnvoyEngine: EnvoyEngine {
     return 0
   }
 
-  func startStream(with callbacks: EnvoyHTTPCallbacks) -> EnvoyHTTPStream {
-    return MockEnvoyHTTPStream(handle: 0, callbacks: callbacks)
+  func startStream(with callbacks: EnvoyHTTPCallbacks, bufferForRetry: Bool)
+    -> EnvoyHTTPStream
+  {
+    return MockEnvoyHTTPStream(handle: 0, callbacks: callbacks,
+                               bufferForRetry: bufferForRetry)
   }
 }
 
 final class EnvoyClientTests: XCTestCase {
   override func tearDown() {
     super.tearDown()
-    MockEnvoyHTTPStream.onHeaders = nil
-    MockEnvoyHTTPStream.onData = nil
-    MockEnvoyHTTPStream.onTrailers = nil
+    MockEnvoyHTTPStream.reset()
   }
 
   func testNonStreamingExtensionSendsRequestDetailsThroughStream() throws {
@@ -59,5 +60,30 @@ final class EnvoyClientTests: XCTestCase {
                handler: ResponseHandler())
     self.wait(for: [requestExpectation, dataExpectation, closeExpectation],
               timeout: 0.1, enforceOrder: true)
+  }
+
+  func testBuffersForRetriesWhenRetryPolicyIsSet() throws {
+    let request = RequestBuilder(
+      method: .get, scheme: "https", authority: "www.envoyproxy.io", path: "/docs")
+      .addRetryPolicy(RetryPolicy(maxRetryCount: 3, retryOn: RetryRule.allCases))
+      .build()
+    let envoy = try EnvoyClientBuilder(domain: "api.foo.com")
+      .addEngineType(MockEnvoyEngine.self)
+      .build()
+    envoy.send(request, body: Data(), trailers: [:], handler: ResponseHandler())
+
+    XCTAssertEqual(true, MockEnvoyHTTPStream.bufferForRetry)
+  }
+
+  func testDoesNotBufferForRetriesWhenRetryPolicyIsNil() throws {
+    let request = RequestBuilder(
+      method: .get, scheme: "https", authority: "www.envoyproxy.io", path: "/docs")
+      .build()
+    let envoy = try EnvoyClientBuilder(domain: "api.foo.com")
+      .addEngineType(MockEnvoyEngine.self)
+      .build()
+    envoy.send(request, body: Data(), trailers: [:], handler: ResponseHandler())
+
+    XCTAssertEqual(false, MockEnvoyHTTPStream.bufferForRetry)
   }
 }
