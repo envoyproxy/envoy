e5b324b8e8e50a182c690c62013a6e9cd35f0e03
Kuat Yessenov <kuat@google.com>
2019-10-21 14:38:19 -0700

    ignore git commit error code
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- tools/api/generate_go_protobuf.py
+++ tools/api/generate_go_protobuf.py
@@ -97,8 +97,11 @@ def publishGoProtobufs(repo, sha):
   git(repo, 'config', 'user.name', USER_NAME)
   git(repo, 'config', 'user.email', USER_EMAIL)
   git(repo, 'add', 'envoy')
-  git(repo, 'commit', '-s', '-m', MIRROR_MSG + sha)
-  git(repo, 'push', 'origin', BRANCH)
+  try:
+    git(repo, 'commit', '-s', '-m', MIRROR_MSG + sha)
+    git(repo, 'push', 'origin', BRANCH)
+  except:
+    print('Failed to commit changes, skipping push')
 
 
 if __name__ == "__main__":
