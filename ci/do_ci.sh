envoyproxy/envoy

commit a88e1b4cc51051e0ca939aba240278ae117d8b42

ci: sanitize vptr and function (#8631)
Risk Level: Low
Testing: CI
Docs Changes:
Release Notes:

Signed-off-by: Lizan Zhou <lizan@tetrate.io>

ci/do_ci.sh
@@ -145,12 +145,17 @@ elif [[ "$CI_TARGET" == "bazel.debug.server_only" ]]; then
   exit 0
 elif [[ "$CI_TARGET" == "bazel.asan" ]]; then
   setup_clang_toolchain
+  BAZEL_BUILD_OPTIONS+="-c dbg --config=clang-asan \
+    --copt=-fsanitize=vptr,function --linkopt=-fsanitize=vptr,function \
+    --linkopt=-l:libclang_rt.ubsan_standalone-x86_64.a \
+    --linkopt=-l:libclang_rt.ubsan_standalone_cxx-x86_64.a \
+    --linkopt=-L$(dirname $(find $(llvm-config --libdir) -name libclang_rt.ubsan_standalone_cxx-x86_64.a | head -1))" 
   echo "bazel ASAN/UBSAN debug build with tests"
   echo "Building and testing envoy tests ${TEST_TARGETS}"
-  bazel_with_collection test ${BAZEL_BUILD_OPTIONS} -c dbg --config=clang-asan ${TEST_TARGETS}
+  bazel_with_collection test ${BAZEL_BUILD_OPTIONS} ${TEST_TARGETS}
   echo "Building and testing envoy-filter-example tests..."
   pushd "${ENVOY_FILTER_EXAMPLE_SRCDIR}"
-  bazel_with_collection test ${BAZEL_BUILD_OPTIONS} -c dbg --config=clang-asan \
+  bazel_with_collection test ${BAZEL_BUILD_OPTIONS} \
     //:echo2_integration_test //:envoy_binary_test
   popd
   # Also validate that integration test traffic tapping (useful when debugging etc.)
@@ -160,7 +165,7 @@ elif [[ "$CI_TARGET" == "bazel.asan" ]]; then
   TAP_TMP=/tmp/tap/
   rm -rf "${TAP_TMP}"
   mkdir -p "${TAP_TMP}"
-  bazel_with_collection test ${BAZEL_BUILD_OPTIONS} -c dbg --config=clang-asan \
+  bazel_with_collection test ${BAZEL_BUILD_OPTIONS} \
     --strategy=TestRunner=local --test_env=TAP_PATH="${TAP_TMP}/tap" \
     --test_env=PATH="/usr/sbin:${PATH}" \
     //test/extensions/transport_sockets/tls/integration:ssl_integration_test

.bazelrc
@@ -42,11 +42,9 @@ build:asan --define ENVOY_CONFIG_ASAN=1
 build:asan --copt -fsanitize=address,undefined
 build:asan --linkopt -fsanitize=address,undefined
 # TODO(lizan): vptr and function requires C++ UBSAN runtime which we're not currently linking to.
-# Enable them when bazel has better support for that.
-build:asan --copt -fno-sanitize=vptr
-build:asan --linkopt -fno-sanitize=vptr
-build:asan --copt -fno-sanitize=function
-build:asan --linkopt -fno-sanitize=function
+# Enable them when bazel has better support for that or with explicit linker options.
+build:asan --copt -fno-sanitize=vptr,function
+build:asan --linkopt -fno-sanitize=vptr,function
 build:asan --copt -DADDRESS_SANITIZER=1
 build:asan --copt -D__SANITIZE_ADDRESS__
 build:asan --test_env=ASAN_OPTIONS=handle_abort=1:allow_addr2line=true:check_initialization_order=true:strict_init_order=true:detect_odr_violation=1

test/extensions/filters/network/redis_proxy/proxy_filter_test.cc
@@ -111,7 +111,7 @@ TEST_F(RedisProxyFilterConfigTest, DownstreamAuthPasswordSet) {
 
 class RedisProxyFilterTest : public testing::Test, public Common::Redis::DecoderFactory {
 public:
-  const std::string default_config = R"EOF(
+  static constexpr const char* DefaultConfig = R"EOF(
     {
       "cluster_name": "fake_cluster",
       "stat_prefix": "foo",
@@ -135,7 +135,7 @@ public:
     filter_->onBelowWriteBufferLowWatermark();
   }
 
-  RedisProxyFilterTest() : RedisProxyFilterTest(default_config) {}
+  RedisProxyFilterTest() : RedisProxyFilterTest(DefaultConfig) {}
 
   ~RedisProxyFilterTest() override {
     filter_.reset();

commit 828ca62b61d9f09596ab126f97dc545bad46fced

ci: fix bazel.asan target.
BAZEL_BUILD_OPTIONS was expanded without a leading whitespace,
which broke build with non-empty BAZEL_BUILD_EXTRA_OPTIONS.

Broken in #8631.

Signed-off-by: Piotr Sikora <piotrsikora@google.com>

ci/do_ci.sh
@@ -145,7 +145,7 @@ elif [[ "$CI_TARGET" == "bazel.debug.server_only" ]]; then
   exit 0
 elif [[ "$CI_TARGET" == "bazel.asan" ]]; then
   setup_clang_toolchain
-  BAZEL_BUILD_OPTIONS+="-c dbg --config=clang-asan \
+  BAZEL_BUILD_OPTIONS="${BAZEL_BUILD_OPTIONS} -c dbg --config=clang-asan \
     --copt=-fsanitize=vptr,function --linkopt=-fsanitize=vptr,function \
     --linkopt=-l:libclang_rt.ubsan_standalone-x86_64.a \
     --linkopt=-l:libclang_rt.ubsan_standalone_cxx-x86_64.a \