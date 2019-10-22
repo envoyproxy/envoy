9a5f296579918ff6cb503cc02bb46cade7701940
Rama Chavali <rama.rao@salesforce.com>
2019-10-22 09:03:43 +0530

    add line break
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- source/server/listener_manager_impl.h
+++ source/server/listener_manager_impl.h
@@ -105,6 +105,7 @@ using ListenerImplPtr = std::unique_ptr<ListenerImpl>;
   GAUGE(total_listeners_active, NeverImport)                                                       \
   GAUGE(total_listeners_draining, NeverImport)                                                     \
   GAUGE(total_listeners_warming, NeverImport)
+
 /**
  * Struct definition for all listener manager stats. @see stats_macros.h
  */
