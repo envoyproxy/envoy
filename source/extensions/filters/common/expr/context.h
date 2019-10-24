d20c099fe9f7afed15b746aee8ae3a73b71026e4
Kuat Yessenov <kuat@google.com>
2019-10-24 16:08:47 -0700

    expose TLS properties in CEL
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- source/extensions/filters/common/expr/context.h
+++ source/extensions/filters/common/expr/context.h
@@ -43,6 +43,12 @@ constexpr absl::string_view Connection = "connection";
 constexpr absl::string_view MTLS = "mtls";
 constexpr absl::string_view RequestedServerName = "requested_server_name";
 constexpr absl::string_view TLSVersion = "tls_version";
+constexpr absl::string_view SubjectLocalCertificate = "subject_local_certificate";
+constexpr absl::string_view SubjectPeerCertificate = "subject_peer_certificate";
+constexpr absl::string_view URISanLocalCertificate = "uri_san_local_certificate";
+constexpr absl::string_view URISanPeerCertificate = "uri_san_peer_certificate";
+constexpr absl::string_view DNSSanLocalCertificate = "dns_san_local_certificate";
+constexpr absl::string_view DNSSanPeerCertificate = "dns_san_peer_certificate";
 
 // Source properties
 constexpr absl::string_view Source = "source";
