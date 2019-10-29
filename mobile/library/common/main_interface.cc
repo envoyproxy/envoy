b48d33b1208388c5679b500f51a76995e7571870
Mike Schore <mike.schore@gmail.com>
2019-10-29 03:17:01 -0700

    core: expose engine via weak_ptr (#555)
    Description: Access Engine via weak_ptr so that threads can extend its lifetime for the durations of their calls to it. Despite the fact that all incoming calls are ultimately dispatched to the underlying Envoy main thread, there exists a window where the thread must leverage Engine state (e.g. to access the dispatcher) that can otherwise race with the Engine lifecycle. While it's probably possible to cover these edge cases at the platform layer, this provides a common solution.
    Risk Level: Moderate
    Testing: local and CI
    
    Signed-off-by: Mike Schore <mike.schore@gmail.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/common/main_interface.cc
+++ mobile/library/common/main_interface.cc
@@ -7,7 +7,7 @@
 
 // NOLINT(namespace-envoy)
 
-static std::unique_ptr<Envoy::Engine> engine_;
+static std::weak_ptr<Envoy::Engine> engine_;
 static std::atomic<envoy_stream_t> current_stream_handle_{0};
 static std::atomic<envoy_network_t> preferred_network_{ENVOY_NET_GENERIC};
 
@@ -15,27 +15,42 @@ envoy_stream_t init_stream(envoy_engine_t) { return current_stream_handle_++; }
 
 envoy_status_t start_stream(envoy_stream_t stream, envoy_http_callbacks callbacks,
                             envoy_stream_options stream_options) {
-  engine_->httpDispatcher().startStream(stream, callbacks, stream_options);
-  return ENVOY_SUCCESS;
+  if (auto e = engine_.lock()) {
+    e->httpDispatcher().startStream(stream, callbacks, stream_options);
+    return ENVOY_SUCCESS;
+  }
+  return ENVOY_FAILURE;
 }
 
 envoy_status_t send_headers(envoy_stream_t stream, envoy_headers headers, bool end_stream) {
-  return engine_->httpDispatcher().sendHeaders(stream, headers, end_stream);
+  if (auto e = engine_.lock()) {
+    return e->httpDispatcher().sendHeaders(stream, headers, end_stream);
+  }
+  return ENVOY_FAILURE;
 }
 
 envoy_status_t send_data(envoy_stream_t stream, envoy_data data, bool end_stream) {
-  return engine_->httpDispatcher().sendData(stream, data, end_stream);
+  if (auto e = engine_.lock()) {
+    return e->httpDispatcher().sendData(stream, data, end_stream);
+  }
+  return ENVOY_FAILURE;
 }
 
 // TODO: implement.
 envoy_status_t send_metadata(envoy_stream_t, envoy_headers) { return ENVOY_FAILURE; }
 
 envoy_status_t send_trailers(envoy_stream_t stream, envoy_headers trailers) {
-  return engine_->httpDispatcher().sendTrailers(stream, trailers);
+  if (auto e = engine_.lock()) {
+    return e->httpDispatcher().sendTrailers(stream, trailers);
+  }
+  return ENVOY_FAILURE;
 }
 
 envoy_status_t reset_stream(envoy_stream_t stream) {
-  return engine_->httpDispatcher().resetStream(stream);
+  if (auto e = engine_.lock()) {
+    return e->httpDispatcher().resetStream(stream);
+  }
+  return ENVOY_FAILURE;
 }
 
 envoy_engine_t init_engine() {
@@ -56,6 +71,14 @@ envoy_status_t run_engine(envoy_engine_t, envoy_engine_callbacks callbacks, cons
                           const char* log_level) {
   // This will change once multiple engine support is in place.
   // https://github.com/lyft/envoy-mobile/issues/332
-  engine_ = std::make_unique<Envoy::Engine>(callbacks, config, log_level, preferred_network_);
+
+  // The shared pointer created here will keep the engine alive until static destruction occurs.
+  static auto strong_ref =
+      std::make_shared<Envoy::Engine>(callbacks, config, log_level, preferred_network_);
+
+  // The weak pointer we actually expose allows calling threads to atomically check if the engine
+  // still exists and acquire a shared pointer to it - ensuring the engine persists at least for
+  // the duration of the call.
+  engine_ = strong_ref;
   return ENVOY_SUCCESS;
 }
