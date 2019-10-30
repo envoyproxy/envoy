08619569178f26e1e5c99e4f76d03dd76d9cb6cc
Derek Argueta <dereka@pinterest.com>
2019-10-29 18:52:08 -0700

    fix
    Signed-off-by: Derek Argueta <dereka@pinterest.com>

--- source/common/config/json_utility.h
+++ source/common/config/json_utility.h
@@ -6,13 +6,12 @@
 
 // NOLINT(namespace-envoy)
 
-// Set a string field in a protobuf message with the corresponding field's string	
-// value from a JSON object.	
-#define JSON_UTIL_SET_STRING(json, message, field_name)                                            \	
-  do {                                                                                             \	
-    (message).set_##field_name((json).getString(#field_name, ""));                                 \	
-  } while (0)	
-
+// Set a string field in a protobuf message with the corresponding field's string
+// value from a JSON object.
+#define JSON_UTIL_SET_STRING(json, message, field_name)                                            \
+  do {                                                                                             \
+    (message).set_##field_name((json).getString(#field_name, ""));                                 \
+  } while (0)
 
 // Set a google.protobuf.Duration field in a protobuf message with the corresponding field's
 // milliseconds value from a JSON object if the field is set in the JSON object.
