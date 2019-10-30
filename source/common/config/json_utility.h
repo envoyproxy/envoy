5ceb0e7c73e990a7aaa43b0b77586d1ed4ed8ef1
Derek Argueta <dereka@pinterest.com>
2019-10-29 17:47:50 -0700

    add back JSON_UTIL_SET_STRING for envoy-filter-example
    Signed-off-by: Derek Argueta <dereka@pinterest.com>

--- source/common/config/json_utility.h
+++ source/common/config/json_utility.h
@@ -6,6 +6,14 @@
 
 // NOLINT(namespace-envoy)
 
+// Set a string field in a protobuf message with the corresponding field's string	
+// value from a JSON object.	
+#define JSON_UTIL_SET_STRING(json, message, field_name)                                            \	
+  do {                                                                                             \	
+    (message).set_##field_name((json).getString(#field_name, ""));                                 \	
+  } while (0)	
+
+
 // Set a google.protobuf.Duration field in a protobuf message with the corresponding field's
 // milliseconds value from a JSON object if the field is set in the JSON object.
 #define JSON_UTIL_SET_DURATION(json, message, field_name)                                          \
