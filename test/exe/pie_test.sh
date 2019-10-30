b8c4088e9d7fd70a6adf7e72b7d3ea432a2e33b6
Lizan Zhou <lizan@tetrate.io>
2019-10-30 07:21:22 +0000

    review
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- test/exe/pie_test.sh
+++ test/exe/pie_test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-ENVOY_BIN=${TEST_SRCDIR}/envoy/source/exe/envoy-static
+set -e
+
+ENVOY_BIN="${TEST_SRCDIR}/envoy/source/exe/envoy-static"
 
 if [[ `uname` == "Darwin" ]]; then
   echo "Skipping on macOS."
   exit 0
 fi
 
-if readelf -hW ${ENVOY_BIN} | grep "Type" | grep -o "DYN (Shared object file)"; then
+if readelf -hW "${ENVOY_BIN}" | grep "Type" | grep -o "DYN (Shared object file)"; then
   echo "${ENVOY_BIN} is a PIE!"
   exit 0
 fi
