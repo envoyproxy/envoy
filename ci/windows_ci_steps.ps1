3eaa3087f824836831d8c96807efdae3fe394047
William A Rowe Jr <wrowe@pivotal.io>
2019-10-31 11:03:06 -0400

    Windows build tests for PR8280/PR8754 components
    This tests compilation of several components in the Windows CI
    which were impacted by the recent foreign_cc cmake build changes
    and proposed fixes to boringssl's internal bazel build schema.
    
    Signed-off-by: William A Rowe Jr <wrowe@pivotal.io>
    Signed-off-by: Yechiel Kalmenson <ykalmenson@pivotal.io>
    
    Signed-off-by: William A Rowe Jr <wrowe@pivotal.io>

--- ci/windows_ci_steps.ps1
+++ ci/windows_ci_steps.ps1
@@ -1,1 +1,6 @@
 bazel --bazelrc=windows\.bazelrc test @envoy_api//test/build/...
+bazel --bazelrc=windows\.bazelrc build @boringssl//:ssl
+bazel --bazelrc=windows\.bazelrc build //external:ares
+bazel --bazelrc=windows\.bazelrc build //external:event
+bazel --bazelrc=windows\.bazelrc build //external:yaml_cpp
+bazel --bazelrc=windows\.bazelrc build //external:zlib
