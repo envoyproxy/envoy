837944804f53747fa3a2702e95e6c4211815257a
Shikugawa <Shikugawa@gmail.com>
2019-11-09 16:13:30 +0000

    fix checking format
    Signed-off-by: Shikugawa <Shikugawa@gmail.com>

--- test/integration/integration.h
+++ test/integration/integration.h
@@ -222,14 +222,13 @@ public:
   // available if you're writing a SotW/delta-specific test.
   // TODO(fredlas) expect_node was defaulting false here; the delta+SotW unification work restores
   // it.
-  AssertionResult
-  compareDiscoveryRequest(const std::string& expected_type_url, const std::string& expected_version,
-                          const std::vector<std::string>& expected_resource_names,
-                          const std::vector<std::string>& expected_resource_names_added,
-                          const std::vector<std::string>& expected_resource_names_removed,
-                          bool expect_node = true,
-                          const Protobuf::int32 expected_error_code = Grpc::Status::WellKnownGrpcStatus::Ok,
-                          const std::string& expected_error_message = "");
+  AssertionResult compareDiscoveryRequest(
+      const std::string& expected_type_url, const std::string& expected_version,
+      const std::vector<std::string>& expected_resource_names,
+      const std::vector<std::string>& expected_resource_names_added,
+      const std::vector<std::string>& expected_resource_names_removed, bool expect_node = true,
+      const Protobuf::int32 expected_error_code = Grpc::Status::WellKnownGrpcStatus::Ok,
+      const std::string& expected_error_message = "");
   template <class T>
   void sendDiscoveryResponse(const std::string& type_url, const std::vector<T>& state_of_the_world,
                              const std::vector<T>& added_or_updated,
