2824e9686a65fa40125fc1b8965fa108f633e3eb
Sriduth Jayhari <sriduth.j@compile.com>
2019-11-14 10:19:18 +0530

    platform checks:
      Remove log message for win32.
    
    Signed-off-by: Sriduth Jayhari <sriduth.j@compile.com>

--- source/exe/win32/platform_checks.cc
+++ source/exe/win32/platform_checks.cc
@@ -7,11 +7,6 @@ namespace Envoy {
 // Check system wide configurations such as user level
 // file watches, file limit permissions etc...
 // TODO(windows maintainers)
-void checkPlatformSettings(Filesystem::Instance&) {
-  ENVOY_LOG_MISC(
-      warn,
-      "platform specific checks are not implemented for win32 - please ensure that adequate limits "
-      "are set for the envoy process - for example number of max open files and file watches");
-}
+void checkPlatformSettings(Filesystem::Instance&) {}
 
 } // namespace Envoy
