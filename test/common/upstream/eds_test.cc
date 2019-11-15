f976c174b3c91a08e8b66a1b2fb95ae373474e71
Snow Pettersen <snowp@squareup.com>
2019-11-15 09:50:41 -0500

    fix eds_test
    Signed-off-by: Snow Pettersen <snowp@squareup.com>

--- test/common/upstream/eds_test.cc
+++ test/common/upstream/eds_test.cc
@@ -90,8 +90,8 @@ protected:
     Envoy::Server::Configuration::TransportSocketFactoryContextImpl factory_context(
         admin_, ssl_context_manager_, *scope, cm_, local_info_, dispatcher_, random_, stats_,
         singleton_manager_, tls_, validation_visitor_, *api_);
-    cluster_.reset(
-        new EdsClusterImpl(eds_cluster_, runtime_, factory_context, std::move(scope), false));
+    cluster_.reset(new EdsClusterImpl(eds_cluster_, runtime_, factory_context, std::move(scope),
+                                      false, zone_aware_));
     EXPECT_EQ(initialize_phase, cluster_->initializePhase());
     eds_callbacks_ = cm_.subscription_factory_.callbacks_;
   }
@@ -124,6 +124,7 @@ protected:
   NiceMock<ThreadLocal::MockInstance> tls_;
   NiceMock<ProtobufMessage::MockValidationVisitor> validation_visitor_;
   Api::ApiPtr api_;
+  bool zone_aware_;
 };
 
 class EdsWithHealthCheckUpdateTest : public EdsTest {
@@ -1371,6 +1372,8 @@ TEST_F(EdsTest, EndpointHostsPerPriority) {
 
 // Make sure config updates with P!=0 are rejected for the local cluster.
 TEST_F(EdsTest, NoPriorityForLocalCluster) {
+  zone_aware_ = true;
+  resetCluster();
   envoy::api::v2::ClusterLoadAssignment cluster_load_assignment;
   cluster_load_assignment.set_cluster_name("fare");
   cm_.local_cluster_name_ = "fare";
