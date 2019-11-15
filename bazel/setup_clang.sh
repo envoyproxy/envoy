envoyproxy/envoy

commit be5e7a565b3571556f2d06035ad743923e87b48c

wasm: fix V8 running under UBSan with vptr checks. (#8905)
* wasm: fix wee8 running under UBSan with vptr checks.

Signed-off-by: Piotr Sikora <piotrsikora@google.com>

bazel/external/wee8.genrule_cmd
@@ -33,6 +33,10 @@ export NM=$${NM:-nm}
 if [[ $${ENVOY_ASAN-} == "1" ]]; then
   WEE8_BUILD_ARGS+=" is_asan=true"
   WEE8_BUILD_ARGS+=" is_lsan=true"
+  WEE8_BUILD_ARGS+=" is_ubsan=true"
+fi
+if [[ $${ENVOY_UBSAN_VPTR-} == "1" ]]; then
+  WEE8_BUILD_ARGS+=" is_ubsan_vptr=true"
 fi
 if [[ $${ENVOY_MSAN-} == "1" ]]; then
   WEE8_BUILD_ARGS+=" is_msan=true"

bazel/setup_clang.sh
@@ -20,6 +20,7 @@ build:clang --action_env=CXX=clang++
 build:clang --action_env=LD_LIBRARY_PATH=$(llvm-config --libdir)
 build:clang --test_env=LD_LIBRARY_PATH=$(llvm-config --libdir)
 
+build:clang-asan --action_env=ENVOY_UBSAN_VPTR=1
 build:clang-asan --copt=-fsanitize=vptr,function
 build:clang-asan --linkopt=-fsanitize=vptr,function
 build:clang-asan --linkopt=-L${RT_LIBRARY_PATH}

commit b6a1b54998185e0d9a0744c14ccc502115e2a0b7

wasm: fix V8 running under ASan without setup_clang.
Broken in #8905.

Signed-off-by: Piotr Sikora <piotrsikora@google.com>

bazel/external/wee8.genrule_cmd
@@ -33,9 +33,9 @@ export NM=$${NM:-nm}
 if [[ $${ENVOY_ASAN-} == "1" ]]; then
   WEE8_BUILD_ARGS+=" is_asan=true"
   WEE8_BUILD_ARGS+=" is_lsan=true"
-  WEE8_BUILD_ARGS+=" is_ubsan=true"
 fi
-if [[ $${ENVOY_UBSAN_VPTR-} == "1" ]]; then
+if [[ $${ENVOY_UBSAN-} == "1" ]]; then
+  WEE8_BUILD_ARGS+=" is_ubsan=true"
   WEE8_BUILD_ARGS+=" is_ubsan_vptr=true"
 fi
 if [[ $${ENVOY_MSAN-} == "1" ]]; then

bazel/setup_clang.sh
@@ -20,7 +20,7 @@ build:clang --action_env=CXX=clang++
 build:clang --action_env=LD_LIBRARY_PATH=$(llvm-config --libdir)
 build:clang --test_env=LD_LIBRARY_PATH=$(llvm-config --libdir)
 
-build:clang-asan --action_env=ENVOY_UBSAN_VPTR=1
+build:clang-asan --action_env=ENVOY_UBSAN=1
 build:clang-asan --copt=-fsanitize=vptr,function
 build:clang-asan --linkopt=-fsanitize=vptr,function
 build:clang-asan --linkopt=-L${RT_LIBRARY_PATH}