c3f1125bd27a24f3fb21bc3f0206272e872ad425
Matt Klein <mklein@lyft.com>
2019-11-15 14:43:25 -0800

    Revert "config: minor cleanup: remove DeltaSubscriptionState::resource_names_ (#8918)" This reverts commit 80aedc1c4a1aecc1616bd1563450c69d04e9568f.
    Revert "config: rename NewGrpcMuxImpl -> GrpcMuxImpl (#8919)"
    This reverts commit 6d505533304731fcc97041adce1f735431a703d7.
    
    Revert "config: reinstate #8478 (unification of delta and SotW xDS), reverted by #8939 (#8974)"
    This reverts commit a37522cf3f15639c8afeb7402f505044815fcf85.
    
    Signed-off-by: Matt Klein <mklein@lyft.com>

--- source/common/config/delta_subscription_state.cc
+++ source/common/config/delta_subscription_state.cc
@@ -8,23 +8,18 @@ namespace Config {
 
 DeltaSubscriptionState::DeltaSubscriptionState(std::string type_url,
                                                SubscriptionCallbacks& callbacks,
+                                               const LocalInfo::LocalInfo& local_info,
                                                std::chrono::milliseconds init_fetch_timeout,
                                                Event::Dispatcher& dispatcher)
-    : SubscriptionState(std::move(type_url), callbacks, init_fetch_timeout, dispatcher) {}
-
-DeltaSubscriptionState::~DeltaSubscriptionState() = default;
-
-DeltaSubscriptionStateFactory::DeltaSubscriptionStateFactory(Event::Dispatcher& dispatcher)
-    : dispatcher_(dispatcher) {}
-
-DeltaSubscriptionStateFactory::~DeltaSubscriptionStateFactory() = default;
-
-std::unique_ptr<SubscriptionState>
-DeltaSubscriptionStateFactory::makeSubscriptionState(const std::string& type_url,
-                                                     SubscriptionCallbacks& callbacks,
-                                                     std::chrono::milliseconds init_fetch_timeout) {
-  return std::make_unique<DeltaSubscriptionState>(type_url, callbacks, init_fetch_timeout,
-                                                  dispatcher_);
+    : type_url_(std::move(type_url)), callbacks_(callbacks), local_info_(local_info),
+      init_fetch_timeout_(init_fetch_timeout) {
+  if (init_fetch_timeout_.count() > 0 && !init_fetch_timeout_timer_) {
+    init_fetch_timeout_timer_ = dispatcher.createTimer([this]() -> void {
+      ENVOY_LOG(warn, "delta config: initial fetch timed out for {}", type_url_);
+      callbacks_.onConfigUpdateFailed(ConfigUpdateFailureReason::FetchTimedout, nullptr);
+    });
+    init_fetch_timeout_timer_->enableTimer(init_fetch_timeout_);
+  }
 }
 
 void DeltaSubscriptionState::updateSubscriptionInterest(const std::set<std::string>& cur_added,
@@ -56,13 +51,13 @@ bool DeltaSubscriptionState::subscriptionUpdatePending() const {
          !any_request_sent_yet_in_current_stream_;
 }
 
-UpdateAck DeltaSubscriptionState::handleResponse(const void* response_proto_ptr) {
-  auto* response = static_cast<const envoy::api::v2::DeltaDiscoveryResponse*>(response_proto_ptr);
+UpdateAck
+DeltaSubscriptionState::handleResponse(const envoy::api::v2::DeltaDiscoveryResponse& message) {
   // We *always* copy the response's nonce into the next request, even if we're going to make that
   // request a NACK by setting error_detail.
-  UpdateAck ack(response->nonce(), type_url());
+  UpdateAck ack(message.nonce(), type_url_);
   try {
-    handleGoodResponse(*response);
+    handleGoodResponse(message);
   } catch (const EnvoyException& e) {
     handleBadResponse(e, ack);
   }
@@ -91,8 +86,8 @@ void DeltaSubscriptionState::handleGoodResponse(
           fmt::format("duplicate name {} found in the union of added+removed resources", name));
     }
   }
-  callbacks().onConfigUpdate(message.resources(), message.removed_resources(),
-                             message.system_version_info());
+  callbacks_.onConfigUpdate(message.resources(), message.removed_resources(),
+                            message.system_version_info());
   for (const auto& resource : message.resources()) {
     setResourceVersion(resource.name(), resource.version());
   }
@@ -105,11 +100,11 @@ void DeltaSubscriptionState::handleGoodResponse(
   // initial_resource_versions messages, but will remind us to explicitly tell the server "I'm
   // cancelling my subscription" when we lose interest.
   for (const auto& resource_name : message.removed_resources()) {
-    if (resource_versions_.find(resource_name) != resource_versions_.end()) {
+    if (resource_names_.find(resource_name) != resource_names_.end()) {
       setResourceWaitingForServer(resource_name);
     }
   }
-  ENVOY_LOG(debug, "Delta config for {} accepted with {} resources added, {} removed", type_url(),
+  ENVOY_LOG(debug, "Delta config for {} accepted with {} resources added, {} removed", type_url_,
             message.resources().size(), message.removed_resources().size());
 }
 
@@ -118,19 +113,17 @@ void DeltaSubscriptionState::handleBadResponse(const EnvoyException& e, UpdateAc
   ack.error_detail_.set_code(Grpc::Status::WellKnownGrpcStatus::Internal);
   ack.error_detail_.set_message(e.what());
   disableInitFetchTimeoutTimer();
-  ENVOY_LOG(warn, "delta config for {} rejected: {}", type_url(), e.what());
-  callbacks().onConfigUpdateFailed(Envoy::Config::ConfigUpdateFailureReason::UpdateRejected, &e);
+  ENVOY_LOG(warn, "delta config for {} rejected: {}", type_url_, e.what());
+  callbacks_.onConfigUpdateFailed(Envoy::Config::ConfigUpdateFailureReason::UpdateRejected, &e);
 }
 
 void DeltaSubscriptionState::handleEstablishmentFailure() {
-  callbacks().onConfigUpdateFailed(Envoy::Config::ConfigUpdateFailureReason::ConnectionFailure,
-                                   nullptr);
+  callbacks_.onConfigUpdateFailed(Envoy::Config::ConfigUpdateFailureReason::ConnectionFailure,
+                                  nullptr);
 }
 
-envoy::api::v2::DeltaDiscoveryRequest* DeltaSubscriptionState::getNextRequestInternal() {
-  auto* request = new envoy::api::v2::DeltaDiscoveryRequest;
-  request->set_type_url(type_url());
-
+envoy::api::v2::DeltaDiscoveryRequest DeltaSubscriptionState::getNextRequestAckless() {
+  envoy::api::v2::DeltaDiscoveryRequest request;
   if (!any_request_sent_yet_in_current_stream_) {
     any_request_sent_yet_in_current_stream_ = true;
     // initial_resource_versions "must be populated for first request in a stream".
@@ -141,7 +134,7 @@ envoy::api::v2::DeltaDiscoveryRequest* DeltaSubscriptionState::getNextRequestInt
       // Resources we are interested in, but are still waiting to get any version of from the
       // server, do not belong in initial_resource_versions. (But do belong in new subscriptions!)
       if (!resource.second.waitingForServer()) {
-        (*request->mutable_initial_resource_versions())[resource.first] = resource.second.version();
+        (*request.mutable_initial_resource_versions())[resource.first] = resource.second.version();
       }
       // As mentioned above, fill resource_names_subscribe with everything, including names we
       // have yet to receive any resource for.
@@ -149,40 +142,50 @@ envoy::api::v2::DeltaDiscoveryRequest* DeltaSubscriptionState::getNextRequestInt
     }
     names_removed_.clear();
   }
-
   std::copy(names_added_.begin(), names_added_.end(),
-            Protobuf::RepeatedFieldBackInserter(request->mutable_resource_names_subscribe()));
+            Protobuf::RepeatedFieldBackInserter(request.mutable_resource_names_subscribe()));
   std::copy(names_removed_.begin(), names_removed_.end(),
-            Protobuf::RepeatedFieldBackInserter(request->mutable_resource_names_unsubscribe()));
+            Protobuf::RepeatedFieldBackInserter(request.mutable_resource_names_unsubscribe()));
   names_added_.clear();
   names_removed_.clear();
 
+  request.set_type_url(type_url_);
+  request.mutable_node()->MergeFrom(local_info_.node());
   return request;
 }
 
-void* DeltaSubscriptionState::getNextRequestAckless() { return getNextRequestInternal(); }
-
-void* DeltaSubscriptionState::getNextRequestWithAck(const UpdateAck& ack) {
-  envoy::api::v2::DeltaDiscoveryRequest* request = getNextRequestInternal();
-  request->set_response_nonce(ack.nonce_);
+envoy::api::v2::DeltaDiscoveryRequest
+DeltaSubscriptionState::getNextRequestWithAck(const UpdateAck& ack) {
+  envoy::api::v2::DeltaDiscoveryRequest request = getNextRequestAckless();
+  request.set_response_nonce(ack.nonce_);
   if (ack.error_detail_.code() != Grpc::Status::WellKnownGrpcStatus::Ok) {
     // Don't needlessly make the field present-but-empty if status is ok.
-    request->mutable_error_detail()->CopyFrom(ack.error_detail_);
+    request.mutable_error_detail()->CopyFrom(ack.error_detail_);
   }
   return request;
+}
+
+void DeltaSubscriptionState::disableInitFetchTimeoutTimer() {
+  if (init_fetch_timeout_timer_) {
+    init_fetch_timeout_timer_->disableTimer();
+    init_fetch_timeout_timer_.reset();
+  }
 }
 
 void DeltaSubscriptionState::setResourceVersion(const std::string& resource_name,
                                                 const std::string& resource_version) {
   resource_versions_[resource_name] = ResourceVersion(resource_version);
+  resource_names_.insert(resource_name);
 }
 
 void DeltaSubscriptionState::setResourceWaitingForServer(const std::string& resource_name) {
   resource_versions_[resource_name] = ResourceVersion();
+  resource_names_.insert(resource_name);
 }
 
 void DeltaSubscriptionState::setLostInterestInResource(const std::string& resource_name) {
   resource_versions_.erase(resource_name);
+  resource_names_.erase(resource_name);
 }
 
 } // namespace Config
--- source/common/config/subscription_factory_impl.cc
+++ source/common/config/subscription_factory_impl.cc
@@ -1,9 +1,11 @@
 #include "common/config/subscription_factory_impl.h"
 
+#include "common/config/delta_subscription_impl.h"
 #include "common/config/filesystem_subscription_impl.h"
-#include "common/config/grpc_mux_impl.h"
+#include "common/config/grpc_mux_subscription_impl.h"
 #include "common/config/grpc_subscription_impl.h"
 #include "common/config/http_subscription_impl.h"
+#include "common/config/new_grpc_mux_impl.h"
 #include "common/config/type_to_endpoint.h"
 #include "common/config/utility.h"
 #include "common/protobuf/protobuf.h"
@@ -27,8 +29,9 @@ SubscriptionPtr SubscriptionFactoryImpl::subscriptionFromConfigSource(
   switch (config.config_source_specifier_case()) {
   case envoy::api::v2::core::ConfigSource::kPath: {
     Utility::checkFilesystemSubscriptionBackingPath(config.path(), api_);
-    return std::make_unique<FilesystemSubscriptionImpl>(dispatcher_, config.path(), callbacks,
-                                                        stats, validation_visitor_, api_);
+    result = std::make_unique<Config::FilesystemSubscriptionImpl>(
+        dispatcher_, config.path(), callbacks, stats, validation_visitor_, api_);
+    break;
   }
   case envoy::api::v2::core::ConfigSource::kApiConfigSource: {
     const envoy::api::v2::core::ApiConfigSource& api_config_source = config.api_config_source();
@@ -40,46 +43,56 @@ SubscriptionPtr SubscriptionFactoryImpl::subscriptionFromConfigSource(
           "Please specify an explicit supported api_type in the following config:\n" +
           config.DebugString());
     case envoy::api::v2::core::ApiConfigSource::REST:
-      return std::make_unique<HttpSubscriptionImpl>(
+      result = std::make_unique<HttpSubscriptionImpl>(
           local_info_, cm_, api_config_source.cluster_names()[0], dispatcher_, random_,
           Utility::apiConfigSourceRefreshDelay(api_config_source),
           Utility::apiConfigSourceRequestTimeout(api_config_source), restMethod(type_url),
           callbacks, stats, Utility::configSourceInitialFetchTimeout(config), validation_visitor_);
+      break;
     case envoy::api::v2::core::ApiConfigSource::GRPC:
-      return std::make_unique<GrpcSubscriptionImpl>(
-          std::make_shared<GrpcMuxSotw>(Utility::factoryForGrpcApiConfigSource(
-                                            cm_.grpcAsyncClientManager(), api_config_source, scope)
-                                            ->create(),
-                                        dispatcher_, sotwGrpcMethod(type_url), random_, scope,
-                                        Utility::parseRateLimitSettings(api_config_source),
-                                        local_info_,
-                                        api_config_source.set_node_on_first_message_only()),
-          type_url, callbacks, stats, Utility::configSourceInitialFetchTimeout(config),
-          /*is_aggregated=*/false);
-    case envoy::api::v2::core::ApiConfigSource::DELTA_GRPC:
-      return std::make_unique<GrpcSubscriptionImpl>(
-          std::make_shared<GrpcMuxDelta>(Utility::factoryForGrpcApiConfigSource(
-                                             cm_.grpcAsyncClientManager(), api_config_source, scope)
-                                             ->create(),
-                                         dispatcher_, deltaGrpcMethod(type_url), random_, scope,
-                                         Utility::parseRateLimitSettings(api_config_source),
-                                         local_info_,
-                                         api_config_source.set_node_on_first_message_only()),
-          type_url, callbacks, stats, Utility::configSourceInitialFetchTimeout(config),
-          /*is_aggregated=*/false);
+      result = std::make_unique<GrpcSubscriptionImpl>(
+          local_info_,
+          Config::Utility::factoryForGrpcApiConfigSource(cm_.grpcAsyncClientManager(),
+                                                         api_config_source, scope)
+              ->create(),
+          dispatcher_, random_, sotwGrpcMethod(type_url), type_url, callbacks, stats, scope,
+          Utility::parseRateLimitSettings(api_config_source),
+          Utility::configSourceInitialFetchTimeout(config),
+          api_config_source.set_node_on_first_message_only());
+      break;
+    case envoy::api::v2::core::ApiConfigSource::DELTA_GRPC: {
+      Utility::checkApiConfigSourceSubscriptionBackingCluster(cm_.clusters(), api_config_source);
+      result = std::make_unique<DeltaSubscriptionImpl>(
+          std::make_shared<Config::NewGrpcMuxImpl>(
+              Config::Utility::factoryForGrpcApiConfigSource(cm_.grpcAsyncClientManager(),
+                                                             api_config_source, scope)
+                  ->create(),
+              dispatcher_, deltaGrpcMethod(type_url), random_, scope,
+              Utility::parseRateLimitSettings(api_config_source), local_info_),
+          type_url, callbacks, stats, Utility::configSourceInitialFetchTimeout(config), false);
+      break;
+    }
     default:
       NOT_REACHED_GCOVR_EXCL_LINE;
     }
+    break;
   }
   case envoy::api::v2::core::ConfigSource::kAds: {
-    return std::make_unique<GrpcSubscriptionImpl>(cm_.adsMux(), type_url, callbacks, stats,
-                                                  Utility::configSourceInitialFetchTimeout(config),
-                                                  /*is_aggregated=*/true);
+    if (cm_.adsMux()->isDelta()) {
+      result = std::make_unique<DeltaSubscriptionImpl>(
+          cm_.adsMux(), type_url, callbacks, stats,
+          Utility::configSourceInitialFetchTimeout(config), true);
+    } else {
+      result = std::make_unique<GrpcMuxSubscriptionImpl>(
+          cm_.adsMux(), callbacks, stats, type_url, dispatcher_,
+          Utility::configSourceInitialFetchTimeout(config));
+    }
+    break;
   }
   default:
     throw EnvoyException("Missing config source specifier in envoy::api::v2::core::ConfigSource");
   }
-  NOT_REACHED_GCOVR_EXCL_LINE;
+  return result;
 }
 
 } // namespace Config
