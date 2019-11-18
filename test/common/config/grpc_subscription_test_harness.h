4c678ecb683ef788f0d1dfa3a17171ca81ac3b79
Bill Gallagher <bgallagher@lyft.com>
2019-11-18 13:04:37 -0500

    Revert "Revert "config: minor cleanup: remove DeltaSubscriptionState::resource_names_ (#8918)" (#9044)"
    This reverts commit 38adf1f02e95cf7a7078cdaa39032b62ca1e2ebf.
    
    Signed-off-by: Bill Gallagher <bgallagher@lyft.com>

--- test/common/config/grpc_subscription_test_harness.h
+++ test/common/config/grpc_subscription_test_harness.h
@@ -36,16 +36,22 @@ public:
       : method_descriptor_(Protobuf::DescriptorPool::generated_pool()->FindMethodByName(
             "envoy.api.v2.EndpointDiscoveryService.StreamEndpoints")),
         async_client_(new NiceMock<Grpc::MockAsyncClient>()), timer_(new Event::MockTimer()) {
-    node_.set_id("fo0");
-    EXPECT_CALL(local_info_, node()).WillOnce(testing::ReturnRef(node_));
+    node_.set_id("node_name");
+    node_.set_cluster("cluster_name");
+    node_.mutable_locality()->set_zone("zone_name");
+    EXPECT_CALL(local_info_, node()).WillRepeatedly(testing::ReturnRef(node_));
     EXPECT_CALL(dispatcher_, createTimer_(_)).WillOnce(Invoke([this](Event::TimerCb timer_cb) {
       timer_cb_ = timer_cb;
       return timer_;
     }));
+
     subscription_ = std::make_unique<GrpcSubscriptionImpl>(
-        local_info_, std::unique_ptr<Grpc::MockAsyncClient>(async_client_), dispatcher_, random_,
-        *method_descriptor_, Config::TypeUrl::get().ClusterLoadAssignment, callbacks_, stats_,
-        stats_store_, rate_limit_settings_, init_fetch_timeout, true);
+        std::make_shared<GrpcMuxSotw>(std::unique_ptr<Grpc::MockAsyncClient>(async_client_),
+                                      dispatcher_, *method_descriptor_, random_, stats_store_,
+                                      rate_limit_settings_, local_info_,
+                                      /*skip_subsequent_node=*/true),
+        Config::TypeUrl::get().ClusterLoadAssignment, callbacks_, stats_, init_fetch_timeout,
+        /*is_aggregated=*/false);
   }
 
   ~GrpcSubscriptionTestHarness() override { EXPECT_CALL(async_stream_, sendMessageRaw_(_, false)); }
@@ -77,7 +83,9 @@ public:
       error_detail->set_code(error_code);
       error_detail->set_message(error_message);
     }
-    EXPECT_CALL(async_stream_, sendMessageRaw_(Grpc::ProtoBufferEq(expected_request), false));
+    EXPECT_CALL(
+        async_stream_,
+        sendMessageRaw_(Grpc::ProtoBufferEqIgnoreRepeatedFieldOrdering(expected_request), false));
   }
 
   void startSubscription(const std::set<std::string>& cluster_names) override {
@@ -89,8 +97,7 @@ public:
 
   void deliverConfigUpdate(const std::vector<std::string>& cluster_names,
                            const std::string& version, bool accept) override {
-    std::unique_ptr<envoy::api::v2::DiscoveryResponse> response(
-        new envoy::api::v2::DiscoveryResponse());
+    auto response = std::make_unique<envoy::api::v2::DiscoveryResponse>();
     response->set_version_info(version);
     last_response_nonce_ = std::to_string(HashUtil::xxHash64(version));
     response->set_nonce(last_response_nonce_);
@@ -115,35 +122,19 @@ public:
       expectSendMessage(last_cluster_names_, version_, false,
                         Grpc::Status::WellKnownGrpcStatus::Internal, "bad config");
     }
-    subscription_->grpcMux()->onDiscoveryResponse(std::move(response));
+    auto shared_mux = subscription_->getGrpcMuxForTest();
+    static_cast<GrpcMuxSotw*>(shared_mux.get())->onDiscoveryResponse(std::move(response));
     Mock::VerifyAndClearExpectations(&async_stream_);
   }
 
   void updateResourceInterest(const std::set<std::string>& cluster_names) override {
-    // The "watch" mechanism means that updates that lose interest in a resource
-    // will first generate a request for [still watched resources, i.e. without newly unwatched
-    // ones] before generating the request for all of cluster_names.
-    // TODO(fredlas) this unnecessary second request will stop happening once the watch mechanism is
-    // no longer internally used by GrpcSubscriptionImpl.
-    std::set<std::string> both;
-    for (const auto& n : cluster_names) {
-      if (last_cluster_names_.find(n) != last_cluster_names_.end()) {
-        both.insert(n);
-      }
-    }
-    expectSendMessage(both, version_);
     expectSendMessage(cluster_names, version_);
     subscription_->updateResourceInterest(cluster_names);
     last_cluster_names_ = cluster_names;
   }
 
   void expectConfigUpdateFailed() override {
-    EXPECT_CALL(callbacks_, onConfigUpdateFailed(_, nullptr))
-        .WillOnce([this](ConfigUpdateFailureReason reason, const EnvoyException*) {
-          if (reason == ConfigUpdateFailureReason::FetchTimedout) {
-            stats_.init_fetch_timeout_.inc();
-          }
-        });
+    EXPECT_CALL(callbacks_, onConfigUpdateFailed(_, nullptr));
   }
 
   void expectEnableInitFetchTimeoutTimer(std::chrono::milliseconds timeout) override {
