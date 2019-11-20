36352f67bb45ef1da095f4aba6a72ed4d45c718c
Dmitri Dolguikh <ddolguik@redhat.com>
2019-11-20 10:09:52 -0800

    Fixed spelling errors
    Signed-off-by: Dmitri Dolguikh <ddolguik@redhat.com>

--- test/integration/vhds_integration_test.cc
+++ test/integration/vhds_integration_test.cc
@@ -248,7 +248,7 @@ INSTANTIATE_TEST_SUITE_P(IpVersionsClientType, VhdsIntegrationTest, GRPC_CLIENT_
 //  - a spontaneous VHDS DiscoveryResponse adds two virtual hosts
 //  - the next spontaneous VHDS DiscoveryResponse removes newly added virtual hosts
 //  - Upstream makes a request to an (now) unknown domain
-//  - A VHDS DiscoveryReponse received containing update for the domain
+//  - A VHDS DiscoveryResponse received containing update for the domain
 //  - Upstream receives a 200 response
 TEST_P(VhdsIntegrationTest, VhdsVirtualHostAddUpdateRemove) {
   // Calls our initialize(), which includes establishing a listener, route, and cluster.
