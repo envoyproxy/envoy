4104f46a8e471bb1ce603d454981358ab25e40e9
Asra Ali <asraa@google.com>
2019-11-26 11:03:51 -0500

    address comments
    Signed-off-by: Asra Ali <asraa@google.com>

--- test/extensions/filters/common/expr/evaluator_fuzz_test.cc
+++ test/extensions/filters/common/expr/evaluator_fuzz_test.cc
@@ -18,28 +18,6 @@ namespace Common {
 namespace Expr {
 namespace {
 
-// Set stream_info using information from input.
-// Duplicates most of fromStreamInfo, except is more lightweight without the use of mocks.
-TestStreamInfo makeStreamInfo(const test::fuzz::StreamInfo& info) {
-  TestStreamInfo stream_info;
-  // TODO(asraa): Set upstreamHost() and downstreamSslConnection().
-  stream_info.metadata_ = info.dynamic_metadata();
-  const auto start_time =
-      static_cast<uint64_t>(std::numeric_limits<std::chrono::nanoseconds::rep>::max()) <
-              info.start_time()
-          ? 0
-          : info.start_time() / 1000;
-  stream_info.start_time_ = SystemTime(std::chrono::microseconds(start_time));
-  stream_info.setRequestedServerName(info.requested_server_name());
-  stream_info.response_code_ = info.has_response_code() ? info.response_code().value() : 200;
-  auto address = Network::Utility::resolveUrl("tcp://10.0.0.1:443");
-  stream_info.upstream_local_address_ = address;
-  stream_info.downstream_local_address_ = address;
-  stream_info.downstream_direct_remote_address_ = address;
-  stream_info.downstream_remote_address_ = address;
-  return stream_info;
-}
-
 DEFINE_PROTO_FUZZER(const test::extensions::filters::common::expr::EvaluatorTestCase& input) {
   // Create builder without constant folding.
   static Expr::BuilderPtr builder = Expr::createBuilder(nullptr);
@@ -52,7 +30,7 @@ DEFINE_PROTO_FUZZER(const test::extensions::filters::common::expr::EvaluatorTest
     Expr::ExpressionPtr expr = Expr::createExpression(*builder, input.expression());
 
     // Create the headers and stream_info to test against.
-    TestStreamInfo stream_info = makeStreamInfo(input.stream_info());
+    TestStreamInfo stream_info = Fuzz::fromStreamInfo(input.stream_info());
     Http::TestHeaderMapImpl request_headers = Fuzz::fromHeaders(input.request_headers());
     Http::TestHeaderMapImpl response_headers = Fuzz::fromHeaders(input.response_headers());
     Http::TestHeaderMapImpl response_trailers = Fuzz::fromHeaders(input.trailers());
