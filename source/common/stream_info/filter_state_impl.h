125cc54e308fea513e388bc617b9444037b713fc
Peng Gao <pengg@google.com>
2019-12-03 10:49:11 -0500

    Add default life_span value to FilterStateImpl as well since tests use it instead of a pointer to FilterState.
    Signed-off-by: Peng Gao <pengg@google.com>

--- source/common/stream_info/filter_state_impl.h
+++ source/common/stream_info/filter_state_impl.h
@@ -15,7 +15,8 @@ class FilterStateImpl : public FilterState {
 public:
   // FilterState
   void setData(absl::string_view data_name, std::shared_ptr<Object> data,
-               FilterState::StateType state_type, FilterState::LifeSpan life_span) override;
+               FilterState::StateType state_type,
+               FilterState::LifeSpan life_span = FilterState::LifeSpan::FilterChain) override;
   void copyInto(FilterState& other, FilterState::LifeSpan life_span) override;
   bool hasDataWithName(absl::string_view) const override;
   const Object* getDataReadOnlyGeneric(absl::string_view data_name) const override;
