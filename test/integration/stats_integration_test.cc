a5195edc6b6cc94f9aeb6bb270254ee82d5f5f4b
Matthew Gumport <mgumport@lyft.com>
2019-12-05 14:31:08 -0800

    ci disagrees with local box on the correct value
    I'm going to agree with ci.
    
    Signed-off-by: Matthew Gumport <mgumport@lyft.com>

--- test/integration/stats_integration_test.cc
+++ test/integration/stats_integration_test.cc
@@ -308,7 +308,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithRealSymbolTable) {
   // 2019/11/01  8859     35221       36000   build: switch to libc++ by default
   // 2019/11/15  9040     35029       35500   build: update protobuf to 3.10.1
   // 2019/11/15  9040     35061       35500   upstream: track whether cluster is local
-  // 2019/12/04  9227     40758       42000   router: per-cluster histograms w/ timeout budget
+  // 2019/12/04  9227     40949       42000   router: per-cluster histograms w/ timeout budget
 
   // Note: when adjusting this value: EXPECT_MEMORY_EQ is active only in CI
   // 'release' builds, where we control the platform and tool-chain. So you
@@ -322,7 +322,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithRealSymbolTable) {
   // If you encounter a failure here, please see
   // https://github.com/envoyproxy/envoy/blob/master/source/docs/stats.md#stats-memory-tests
   // for details on how to fix.
-  EXPECT_MEMORY_EQ(m_per_cluster, 40758); // 104 bytes higher than a debug build.
+  EXPECT_MEMORY_EQ(m_per_cluster, 40949); // 104 bytes higher than a debug build.
   EXPECT_MEMORY_LE(m_per_cluster, 42000);
 }
 
