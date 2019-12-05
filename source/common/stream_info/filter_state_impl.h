72ed2b9d9f42f865aa1df6b29ae7de94d2718b76
Peng Gao <pengg@google.com>
2019-12-05 13:24:48 -0500

    Add default parameter back to FilterStateImpl::setData
    Signed-off-by: Peng Gao <pengg@google.com>

--- source/common/stream_info/filter_state_impl.h
+++ source/common/stream_info/filter_state_impl.h
@@ -17,7 +17,8 @@ public:
 
   // FilterState
   void setData(absl::string_view data_name, std::shared_ptr<Object> data,
-               FilterState::StateType state_type, FilterState::LifeSpan life_span) override;
+               FilterState::StateType state_type,
+               FilterState::LifeSpan life_span = FilterState::LifeSpan::FilterChain) override;
   bool hasDataWithName(absl::string_view) const override;
   const Object* getDataReadOnlyGeneric(absl::string_view data_name) const override;
   Object* getDataMutableGeneric(absl::string_view data_name) override;
