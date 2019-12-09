54ea4b027ac8f40a5f13fdc6ddb6df5675a50ef7
Lizan Zhou <lizan@tetrate.io>
2019-12-09 06:35:09 +0000

    typedb library fix
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- tools/protoxform/utils.py
+++ tools/protoxform/utils.py
@@ -4,16 +4,16 @@ from tools.type_whisperer.api_type_db_pb2 import TypeDb
 
 from google.protobuf import text_format
 
-typedb = None
+_typedb = None
 
 
 def GetTypeDb():
-  assert typedb != None
-  return typedb
+  assert _typedb != None
+  return _typedb
 
 
 def LoadTypeDb(type_db_path):
-  global typedb
-  typedb = TypeDb()
+  global _typedb
+  _typedb = TypeDb()
   with open(type_db_path, 'r') as f:
-    text_format.Merge(f.read(), typedb)
+    text_format.Merge(f.read(), _typedb)
