593135719feda2758e2564b1c351fc1b6bdc2ebc
tehnerd <tehnerd@tehnerd.com>
2019-12-10 20:43:52 +0000

    [tls][tickets]: fixing PROTOBUF_GET_SECONDS_OR_DEFAULT macro
    Signed-off-by: tehnerd <tehnerd@tehnerd.com>

--- source/common/protobuf/utility.h
+++ source/common/protobuf/utility.h
@@ -58,7 +58,7 @@
 
 // Obtain the seconds value of a google.protobuf.Duration field if set. Otherwise, return the
 // default value.
-#define PROTOBUF_GET_SECONDS_OR_DEFAULT(message, field_name)                                       \
+#define PROTOBUF_GET_SECONDS_OR_DEFAULT(message, field_name, default_value)                        \
   ((message).has_##field_name() ? DurationUtil::durationToSeconds((message).field_name())          \
                                 : (default_value))
 
