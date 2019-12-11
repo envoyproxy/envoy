aeb0ebcd5f9a923e715aaf3c052eb109ae15b6cd
Lizan Zhou <lizan@tetrate.io>
2019-12-11 09:35:14 +0000

    debug
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/windows_ci_setup.ps1
+++ ci/windows_ci_setup.ps1
@@ -2,8 +2,6 @@
 # The list of installed software can be found at:
 # https://github.com/Microsoft/azure-pipelines-image-generation/blob/master/images/win/Vs2019-Server2019-Readme.md
 
-mkdir "$env:TOOLS_BIN_DIR"
-
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile("https://github.com/bazelbuild/bazelisk/releases/download/v1.0/bazelisk-windows-amd64.exe", "$env:TOOLS_BIN_DIR\bazel.exe")
 
