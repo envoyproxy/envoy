81d7fb66e8819b36ff1e29d38f8d54e03ea71875
Lizan Zhou <lizan@tetrate.io>
2019-12-11 01:57:09 +0000

    ci: install Ninja explicitly in Windows
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/windows_ci_setup.ps1
+++ ci/windows_ci_setup.ps1
@@ -6,3 +6,5 @@ mkdir "$env:TOOLS_BIN_DIR"
 
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile("https://github.com/bazelbuild/bazelisk/releases/download/v1.0/bazelisk-windows-amd64.exe", "$env:TOOLS_BIN_DIR\bazel.exe")
+
+choco install ninja
