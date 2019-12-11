911bac6ed3b5d32d7f358978f87648c2b633e744
Otto van der Schaaf <oschaaf@we-amp.com>
2019-12-11 08:52:58 +0100

    Review feedback: update comment
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- source/common/event/timer_impl.h
+++ source/common/event/timer_impl.h
@@ -21,7 +21,8 @@ public:
    * duration conversions for its input arguments. This lets us have an opportunity to check bounds
    * before doing any conversions. When the passed in duration exceeds INT32_MAX max seconds, the
    * output will be clipped to yield INT32_MAX seconds and 0 microseconds for the
-   * output argument. Throws an EnvoyException on negative duration input.
+   * output argument. We clip to INT32_MAX to guard against overflowing the timeval structure.
+   * Throws an EnvoyException on negative duration input.
    * @tparam Duration std::chrono duration type, e.g. seconds, milliseconds, ...
    * @param d duration value
    * @param tv output parameter that will be updated
