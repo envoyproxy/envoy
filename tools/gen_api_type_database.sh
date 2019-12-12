6c9881c7e8692fa0e8b9852c7152053d0e38f2cd
Harvey Tuch <htuch@google.com>
2019-12-12 06:37:48 -0500

    Consolidate API type database Bazel invocations.
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- tools/gen_api_type_database.sh
+++ tools/gen_api_type_database.sh
@@ -6,17 +6,11 @@ set -e
 # files still.
 rm -rf bazel-bin/external/envoy_api
 
-# Find all source protos.
-declare -r PROTO_TARGETS=$(bazel query "labels(srcs, labels(deps, @envoy_api//docs:protos))")
-
 # TODO(htuch): This script started life by cloning docs/build.sh. It depends on
 # the @envoy_api//docs:protos target in a few places as a result. This is not
 # guaranteed to be the precise set of protos we want to format, but as a
 # starting place it seems reasonable. In the future, we should change the logic
 # here.
-bazel build ${BAZEL_BUILD_OPTIONS} @envoy_api//docs:protos --aspects \
-  tools/type_whisperer/type_whisperer.bzl%type_whisperer_aspect --output_groups=types_pb_text \
-  --host_force_python=PY3
-declare -x -r TYPE_DB_PATH="${PWD}"/tools/type_whisperer/api_type_db.generated.pb_text
-bazel run ${BAZEL_BUILD_OPTIONS} //tools/type_whisperer:typedb_gen -- \
-  ${PWD} ${TYPE_DB_PATH} ${PROTO_TARGETS}
+bazel build ${BAZEL_BUILD_OPTIONS} --//tools/api_proto_plugin:default_type_db_target=@envoy_api//docs:protos \
+  @envoy_api//docs:protos --aspects //tools/protoxform:protoxform.bzl%protoxform_aspect --output_groups=proto \
+  --action_env=CPROFILE_ENABLED=1 --host_force_python=PY3
