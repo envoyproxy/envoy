61d7057dc3db478ef99bd6eb82abc080327b256c
Tony Allen <tallen@lyft.com>
2019-12-13 14:40:12 -0800

    fix stats integration test
    Signed-off-by: Tony Allen <tallen@lyft.com>

--- test/integration/stats_integration_test.cc
+++ test/integration/stats_integration_test.cc
@@ -307,6 +307,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithRealSymbolTable) {
   // 2019/11/01  8859     35221       36000   build: switch to libc++ by default
   // 2019/11/15  9040     35029       35500   build: update protobuf to 3.10.1
   // 2019/11/15  9040     35061       35500   upstream: track whether cluster is local
+  // 2019/12/13  9069     35606       35700   upstream: Implement retry concurrency budgets
 
   // Note: when adjusting this value: EXPECT_MEMORY_EQ is active only in CI
   // 'release' builds, where we control the platform and tool-chain. So you
@@ -320,8 +321,8 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithRealSymbolTable) {
   // If you encounter a failure here, please see
   // https://github.com/envoyproxy/envoy/blob/master/source/docs/stats.md#stats-memory-tests
   // for details on how to fix.
-  EXPECT_MEMORY_EQ(m_per_cluster, 35061); // 104 bytes higher than a debug build.
-  EXPECT_MEMORY_LE(m_per_cluster, 35500);
+  EXPECT_MEMORY_EQ(m_per_cluster, 35606); // 104 bytes higher than a debug build.
+  EXPECT_MEMORY_LE(m_per_cluster, 35700);
 }
 
 TEST_P(ClusterMemoryTestRunner, MemoryLargeHostSizeWithStats) {
