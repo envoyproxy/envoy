3edfac7cbace275f33b28713676748dbc239c525
Joshua Marantz <jmarantz@google.com>
2019-12-17 23:02:56 -0500

    add direct fuzzing of the encoder.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/common/stats/symbol_table_fuzz_test.cc
+++ test/common/stats/symbol_table_fuzz_test.cc
@@ -24,6 +24,20 @@ DEFINE_FUZZER(const uint8_t* buf, size_t len) {
     // string before comparing.
     absl::string_view trimmed_fuzz_data = StringUtil::removeTrailingCharacters(next_data, '.');
     FUZZ_ASSERT(trimmed_fuzz_data == symbol_table.toString(stat_name));
+
+    // Also encode the string directly, without symbolizing it.
+    MemBlockBuilder<uint8_t> mem_block(SymbolTableImpl::Encoding::totalSizeBytes(next_data.size()));
+    SymbolTableImpl::Encoding::appendEncoding(next_data.size(), mem_block);
+    const uint8_t* data = reinterpret_cast<const uint8_t*>(next_data.data());
+    mem_block.appendData(absl::MakeSpan(data, data + next_data.size()));
+    FUZZ_ASSERT(mem_block.capacityRemaining() == 0);
+    absl::Span<uint8_t> span = mem_block.span();
+    std::pair<uint64_t, uint64_t> number_consumed =
+        SymbolTableImpl::Encoding::decodeNumber(span.data());
+    FUZZ_ASSERT(number_consumed.first == next_data.size());
+    span.remove_prefix(number_consumed.second);
+    FUZZ_ASSERT(absl::string_view(reinterpret_cast<const char*>(span.data()), span.size()) ==
+                next_data);
   }
 }
 
