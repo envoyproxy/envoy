505bb16fb03f11026c101e41cee71d658a51b31c
Dmitri Dolguikh <ddolguik@redhat.com>
2019-12-18 15:42:44 -0800

    fixed spelling
    Signed-off-by: Dmitri Dolguikh <ddolguik@redhat.com>

--- include/envoy/http/filter.h
+++ include/envoy/http/filter.h
@@ -451,8 +451,9 @@ public:
   /**
    *
    * @return absl::optional<Router::ConfigConstSharedPtr>. Contains a value if a non-scoped RDS
-   * route config provider is used. Scoped RDS provides are not supported atm, as retrieval of a
-   * route configuration in their case requires passing of http request headers as a parameter.
+   * route config provider is used. Scoped RDS provides are not supported at the moment, as
+   * retrieval of a route configuration in their case requires passing of http request headers
+   * as a parameter.
    */
   virtual absl::optional<Router::ConfigConstSharedPtr> routeConfig() PURE;
 };
