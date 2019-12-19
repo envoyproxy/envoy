0b9fc3b1dec8dae01b468ec4b4ceeecb79c64a1e
Asra Ali <asraa@google.com>
2019-12-19 10:47:09 -0500

    fix fuzz bugs
    Signed-off-by: Asra Ali <asraa@google.com>

--- source/common/protobuf/utility.cc
+++ source/common/protobuf/utility.cc
@@ -426,7 +426,10 @@ std::string MessageUtil::getJsonStringFromMessage(const Protobuf::Message& messa
   std::string json;
   const auto status = Protobuf::util::MessageToJsonString(message, &json, json_options);
   // This should always succeed unless something crash-worthy such as out-of-memory.
-  RELEASE_ASSERT(status.ok(), "");
+  if (!status.ok()) {
+    throw EnvoyException(fmt::format("Unable to convert protobuf message to JSON string: {} {}",
+                                     status.ToString(), message.DebugString()));
+  }
   return json;
 }
 
