envoyproxy/envoy

commit da5fb5ce6ec7cc6e4693656a5ba2161107f6ac4a

api: shadowing for API protos. (#9429)
This is intended to simplify the internal handling of deprecations during API boosting.

See https://docs.google.com/document/d/1mGO9LtVo7t4Lph7WlmyGCxXye3h6j29z3JZvIBbs_D0/edit

Ultimately the plan is to hide this all as a build artifact in Bazel cache, but due to the technical complexity of the pure Bazel solution (involving changes spanning PGV, protoxform, API build rules), we will use checked-in artifacts for 1.13.0.

Risk level: Low
Testing: Additional API test and protoxform golden test.

Part of #8082

This should be unwound in the future with #9479

Signed-off-by: Harvey Tuch <htuch@google.com>

generated_api_shadow/bazel
@@ -1,0 +1,1 @@
+../api/bazel

api/bazel/api_build_system.bzl
@@ -149,11 +149,15 @@ def api_go_test(name, **kwargs):
         **kwargs
     )
 
-def api_proto_package(srcs = [], deps = [], has_services = False, visibility = ["//visibility:public"]):
+def api_proto_package(
+        name = "pkg",
+        srcs = [],
+        deps = [],
+        has_services = False,
+        visibility = ["//visibility:public"]):
     if srcs == []:
         srcs = native.glob(["*.proto"])
 
-    name = "pkg"
     api_cc_py_proto_library(
         name = name,
         visibility = visibility,
@@ -162,9 +166,9 @@ def api_proto_package(srcs = [], deps = [], has_services = False, visibility = [
         has_services = has_services,
     )
 
-    compilers = ["@io_bazel_rules_go//proto:go_proto", "//bazel:pgv_plugin_go"]
+    compilers = ["@io_bazel_rules_go//proto:go_proto", "@envoy_api//bazel:pgv_plugin_go"]
     if has_services:
-        compilers = ["@io_bazel_rules_go//proto:go_grpc", "//bazel:pgv_plugin_go"]
+        compilers = ["@io_bazel_rules_go//proto:go_grpc", "@envoy_api//bazel:pgv_plugin_go"]
 
     go_proto_library(
         name = name + _GO_PROTO_SUFFIX,

generated_api_shadow/BUILD
@@ -1,0 +1,157 @@
+# DO NOT EDIT. This file is generated by tools/proto_sync.py.
+
+load("@rules_proto//proto:defs.bzl", "proto_library")
+
+licenses(["notice"])  # Apache 2
+
+proto_library(
+    name = "protos",
+    visibility = ["//visibility:public"],
+    deps = [
+        "//envoy/admin/v2alpha:pkg",
+        "//envoy/admin/v3alpha:pkg",
+        "//envoy/api/v2:pkg",
+        "//envoy/api/v2/auth:pkg",
+        "//envoy/api/v2/cluster:pkg",
+        "//envoy/api/v2/core:pkg",
+        "//envoy/api/v2/endpoint:pkg",
+        "//envoy/api/v2/listener:pkg",
+        "//envoy/api/v2/ratelimit:pkg",
+        "//envoy/api/v2/route:pkg",
+        "//envoy/api/v3alpha:pkg",
+        "//envoy/api/v3alpha/auth:pkg",
+        "//envoy/api/v3alpha/cluster:pkg",
+        "//envoy/api/v3alpha/core:pkg",
+        "//envoy/api/v3alpha/endpoint:pkg",
+        "//envoy/api/v3alpha/listener:pkg",
+        "//envoy/api/v3alpha/route:pkg",
+        "//envoy/config/accesslog/v2:pkg",
+        "//envoy/config/accesslog/v3alpha:pkg",
+        "//envoy/config/bootstrap/v2:pkg",
+        "//envoy/config/bootstrap/v3alpha:pkg",
+        "//envoy/config/cluster/aggregate/v2alpha:pkg",
+        "//envoy/config/cluster/dynamic_forward_proxy/v2alpha:pkg",
+        "//envoy/config/cluster/dynamic_forward_proxy/v3alpha:pkg",
+        "//envoy/config/cluster/redis:pkg",
+        "//envoy/config/common/dynamic_forward_proxy/v2alpha:pkg",
+        "//envoy/config/common/dynamic_forward_proxy/v3alpha:pkg",
+        "//envoy/config/common/tap/v2alpha:pkg",
+        "//envoy/config/common/tap/v3alpha:pkg",
+        "//envoy/config/filter/accesslog/v2:pkg",
+        "//envoy/config/filter/accesslog/v3alpha:pkg",
+        "//envoy/config/filter/dubbo/router/v2alpha1:pkg",
+        "//envoy/config/filter/fault/v2:pkg",
+        "//envoy/config/filter/fault/v3alpha:pkg",
+        "//envoy/config/filter/http/adaptive_concurrency/v2alpha:pkg",
+        "//envoy/config/filter/http/adaptive_concurrency/v3alpha:pkg",
+        "//envoy/config/filter/http/buffer/v2:pkg",
+        "//envoy/config/filter/http/csrf/v2:pkg",
+        "//envoy/config/filter/http/csrf/v3alpha:pkg",
+        "//envoy/config/filter/http/dynamic_forward_proxy/v2alpha:pkg",
+        "//envoy/config/filter/http/dynamic_forward_proxy/v3alpha:pkg",
+        "//envoy/config/filter/http/ext_authz/v2:pkg",
+        "//envoy/config/filter/http/ext_authz/v3alpha:pkg",
+        "//envoy/config/filter/http/fault/v2:pkg",
+        "//envoy/config/filter/http/fault/v3alpha:pkg",
+        "//envoy/config/filter/http/grpc_http1_reverse_bridge/v2alpha1:pkg",
+        "//envoy/config/filter/http/grpc_stats/v2alpha:pkg",
+        "//envoy/config/filter/http/gzip/v2:pkg",
+        "//envoy/config/filter/http/header_to_metadata/v2:pkg",
+        "//envoy/config/filter/http/health_check/v2:pkg",
+        "//envoy/config/filter/http/health_check/v3alpha:pkg",
+        "//envoy/config/filter/http/ip_tagging/v2:pkg",
+        "//envoy/config/filter/http/ip_tagging/v3alpha:pkg",
+        "//envoy/config/filter/http/jwt_authn/v2alpha:pkg",
+        "//envoy/config/filter/http/jwt_authn/v3alpha:pkg",
+        "//envoy/config/filter/http/lua/v2:pkg",
+        "//envoy/config/filter/http/original_src/v2alpha1:pkg",
+        "//envoy/config/filter/http/rate_limit/v2:pkg",
+        "//envoy/config/filter/http/rate_limit/v3alpha:pkg",
+        "//envoy/config/filter/http/rbac/v2:pkg",
+        "//envoy/config/filter/http/rbac/v3alpha:pkg",
+        "//envoy/config/filter/http/router/v2:pkg",
+        "//envoy/config/filter/http/router/v3alpha:pkg",
+        "//envoy/config/filter/http/squash/v2:pkg",
+        "//envoy/config/filter/http/tap/v2alpha:pkg",
+        "//envoy/config/filter/http/tap/v3alpha:pkg",
+        "//envoy/config/filter/http/transcoder/v2:pkg",
+        "//envoy/config/filter/listener/original_src/v2alpha1:pkg",
+        "//envoy/config/filter/network/client_ssl_auth/v2:pkg",
+        "//envoy/config/filter/network/client_ssl_auth/v3alpha:pkg",
+        "//envoy/config/filter/network/dubbo_proxy/v2alpha1:pkg",
+        "//envoy/config/filter/network/dubbo_proxy/v3alpha:pkg",
+        "//envoy/config/filter/network/ext_authz/v2:pkg",
+        "//envoy/config/filter/network/ext_authz/v3alpha:pkg",
+        "//envoy/config/filter/network/http_connection_manager/v2:pkg",
+        "//envoy/config/filter/network/http_connection_manager/v3alpha:pkg",
+        "//envoy/config/filter/network/mongo_proxy/v2:pkg",
+        "//envoy/config/filter/network/mongo_proxy/v3alpha:pkg",
+        "//envoy/config/filter/network/mysql_proxy/v1alpha1:pkg",
+        "//envoy/config/filter/network/rate_limit/v2:pkg",
+        "//envoy/config/filter/network/rate_limit/v3alpha:pkg",
+        "//envoy/config/filter/network/rbac/v2:pkg",
+        "//envoy/config/filter/network/rbac/v3alpha:pkg",
+        "//envoy/config/filter/network/redis_proxy/v2:pkg",
+        "//envoy/config/filter/network/redis_proxy/v3alpha:pkg",
+        "//envoy/config/filter/network/tcp_proxy/v2:pkg",
+        "//envoy/config/filter/network/tcp_proxy/v3alpha:pkg",
+        "//envoy/config/filter/network/thrift_proxy/v2alpha1:pkg",
+        "//envoy/config/filter/network/thrift_proxy/v3alpha:pkg",
+        "//envoy/config/filter/network/zookeeper_proxy/v1alpha1:pkg",
+        "//envoy/config/filter/thrift/rate_limit/v2alpha1:pkg",
+        "//envoy/config/filter/thrift/rate_limit/v3alpha:pkg",
+        "//envoy/config/filter/thrift/router/v2alpha1:pkg",
+        "//envoy/config/filter/udp/udp_proxy/v2alpha:pkg",
+        "//envoy/config/grpc_credential/v2alpha:pkg",
+        "//envoy/config/grpc_credential/v3alpha:pkg",
+        "//envoy/config/health_checker/redis/v2:pkg",
+        "//envoy/config/listener/v2:pkg",
+        "//envoy/config/metrics/v2:pkg",
+        "//envoy/config/metrics/v3alpha:pkg",
+        "//envoy/config/overload/v2alpha:pkg",
+        "//envoy/config/overload/v3alpha:pkg",
+        "//envoy/config/ratelimit/v2:pkg",
+        "//envoy/config/ratelimit/v3alpha:pkg",
+        "//envoy/config/rbac/v2:pkg",
+        "//envoy/config/rbac/v3alpha:pkg",
+        "//envoy/config/resource_monitor/fixed_heap/v2alpha:pkg",
+        "//envoy/config/resource_monitor/injected_resource/v2alpha:pkg",
+        "//envoy/config/retry/previous_priorities:pkg",
+        "//envoy/config/trace/v2:pkg",
+        "//envoy/config/trace/v2alpha:pkg",
+        "//envoy/config/trace/v3alpha:pkg",
+        "//envoy/config/transport_socket/alts/v2alpha:pkg",
+        "//envoy/config/transport_socket/tap/v2alpha:pkg",
+        "//envoy/config/transport_socket/tap/v3alpha:pkg",
+        "//envoy/data/accesslog/v2:pkg",
+        "//envoy/data/accesslog/v3alpha:pkg",
+        "//envoy/data/cluster/v2alpha:pkg",
+        "//envoy/data/core/v2alpha:pkg",
+        "//envoy/data/core/v3alpha:pkg",
+        "//envoy/data/tap/v2alpha:pkg",
+        "//envoy/data/tap/v3alpha:pkg",
+        "//envoy/service/accesslog/v2:pkg",
+        "//envoy/service/accesslog/v3alpha:pkg",
+        "//envoy/service/auth/v2:pkg",
+        "//envoy/service/auth/v3alpha:pkg",
+        "//envoy/service/discovery/v2:pkg",
+        "//envoy/service/discovery/v3alpha:pkg",
+        "//envoy/service/load_stats/v2:pkg",
+        "//envoy/service/load_stats/v3alpha:pkg",
+        "//envoy/service/metrics/v2:pkg",
+        "//envoy/service/metrics/v3alpha:pkg",
+        "//envoy/service/ratelimit/v2:pkg",
+        "//envoy/service/ratelimit/v3alpha:pkg",
+        "//envoy/service/route/v3alpha:pkg",
+        "//envoy/service/tap/v2alpha:pkg",
+        "//envoy/service/tap/v3alpha:pkg",
+        "//envoy/service/trace/v2:pkg",
+        "//envoy/service/trace/v3alpha:pkg",
+        "//envoy/type:pkg",
+        "//envoy/type/matcher:pkg",
+        "//envoy/type/matcher/v3alpha:pkg",
+        "//envoy/type/metadata/v2:pkg",
+        "//envoy/type/tracing/v2:pkg",
+        "//envoy/type/v3alpha:pkg",
+    ],
+)

generated_api_shadow/envoy/admin/v2alpha/BUILD
@@ -1,0 +1,16 @@
+# DO NOT EDIT. This file is generated by tools/proto_sync.py.
+
+load("@envoy_api//bazel:api_build_system.bzl", "api_proto_package")
+
+licenses(["notice"])  # Apache 2
+
+api_proto_package(
+    deps = [
+        "//envoy/api/v2:pkg",
+        "//envoy/api/v2/auth:pkg",
+        "//envoy/api/v2/core:pkg",
+        "//envoy/config/bootstrap/v2:pkg",
+        "//envoy/service/tap/v2alpha:pkg",
+        "//envoy/type:pkg",
+    ],
+)

generated_api_shadow/envoy/admin/v3alpha/BUILD
@@ -1,0 +1,19 @@
+# DO NOT EDIT. This file is generated by tools/proto_sync.py.
+
+load("@envoy_api//bazel:api_build_system.bzl", "api_proto_package")
+
+licenses(["notice"])  # Apache 2
+
+api_proto_package(
+    deps = [
+        "//envoy/admin/v2alpha:pkg",
+        "//envoy/api/v3alpha:pkg",
+        "//envoy/api/v3alpha/auth:pkg",
+        "//envoy/api/v3alpha/core:pkg",
+        "//envoy/config/bootstrap/v3alpha:pkg",
+        "//envoy/service/route/v3alpha:pkg",
+        "//envoy/service/tap/v3alpha:pkg",
+        "//envoy/type/v3alpha:pkg",
+        "@com_github_cncf_udpa//udpa/annotations:pkg",
+    ],
+)

generated_api_shadow/envoy/api/v2/BUILD
@@ -1,0 +1,20 @@
+# DO NOT EDIT. This file is generated by tools/proto_sync.py.
+
+load("@envoy_api//bazel:api_build_system.bzl", "api_proto_package")
+
+licenses(["notice"])  # Apache 2
+
+api_proto_package(
+    has_services = True,
+    deps = [
+        "//envoy/api/v2/auth:pkg",
+        "//envoy/api/v2/cluster:pkg",
+        "//envoy/api/v2/core:pkg",
+        "//envoy/api/v2/endpoint:pkg",
+        "//envoy/api/v2/listener:pkg",
+        "//envoy/api/v2/route:pkg",
+        "//envoy/config/listener/v2:pkg",
+        "//envoy/type:pkg",
+        "@com_github_cncf_udpa//udpa/annotations:pkg",
+    ],
+)

generated_api_shadow/envoy/api/v2/auth/BUILD
@@ -1,0 +1,12 @@
+# DO NOT EDIT. This file is generated by tools/proto_sync.py.
+
+load("@envoy_api//bazel:api_build_system.bzl", "api_proto_package")
+
+licenses(["notice"])  # Apache 2
+
+api_proto_package(
+    deps = [
+        "//envoy/api/v2/core:pkg",
+        "//envoy/type/matcher:pkg",
+    ],
+)

generated_api_shadow/envoy/api/v2/cluster/BUILD
@@ -1,0 +1,9 @@
+# DO NOT EDIT. This file is generated by tools/proto_sync.py.
+
+load("@envoy_api//bazel:api_build_system.bzl", "api_proto_package")
+
+licenses(["notice"])  # Apache 2
+
+api_proto_package(
+    deps = ["//envoy/api/v2/core:pkg"],
+)

commit 39c67b34029a9cf884fe3dc845b15e45d8162896

ci: fix Windows target broken by #9429.
Signed-off-by: Harvey Tuch <htuch@google.com>

generated_api_shadow/bazel
@@ -1,1 +1,0 @@
-../api/bazel

generated_api_shadow/bazel/BUILD
@@ -1,0 +1,1 @@
+../../api/bazel/BUILD

generated_api_shadow/bazel/api_build_system.bzl
@@ -1,0 +1,1 @@
+../../api/bazel/api_build_system.bzl

generated_api_shadow/bazel/envoy_http_archive.bzl
@@ -1,0 +1,1 @@
+../../api/bazel/envoy_http_archive.bzl

generated_api_shadow/bazel/external_proto_deps.bzl
@@ -1,0 +1,1 @@
+../../api/bazel/external_proto_deps.bzl

generated_api_shadow/bazel/repositories.bzl
@@ -1,0 +1,1 @@
+../../api/bazel/repositories.bzl

generated_api_shadow/bazel/repository_locations.bzl
@@ -1,0 +1,1 @@
+../../api/bazel/repository_locations.bzl