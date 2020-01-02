0e5c1e1e0a6387cc0afb649fdc24d3749d4277de
Lizan Zhou <lizan@tetrate.io>
2020-01-02 12:35:03 +0000

    fix format
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- test/common/config/api_type_oracle_test.cc
+++ test/common/config/api_type_oracle_test.cc
@@ -15,7 +15,8 @@ namespace {
 TEST(ApiTypeOracleTest, All) {
   // For proto descriptors only
   static_cast<void>(envoy::config::filter::http::ip_tagging::v2::IPTagging::RequestType());
-  static_cast<void>(envoy::extensions::filters::http::ip_tagging::v3alpha::IPTagging::RequestType());
+  static_cast<void>(
+      envoy::extensions::filters::http::ip_tagging::v3alpha::IPTagging::RequestType());
 
   EXPECT_EQ(nullptr, ApiTypeOracle::inferEarlierVersionDescriptor("foo", {}, ""));
   EXPECT_EQ(nullptr, ApiTypeOracle::inferEarlierVersionDescriptor("envoy.ip_tagging", {}, ""));
