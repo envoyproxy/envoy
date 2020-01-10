224d5cf9837b2ecc4b46a9d81711bebb11a675d5
Kuat Yessenov <kuat@google.com>
2020-01-09 16:44:48 -0800

    fix asan
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- include/envoy/registry/registry.h
+++ include/envoy/registry/registry.h
@@ -214,6 +214,7 @@ public:
           auto it = mapping->find(config_type);
           if (it != mapping->end() && it->second != factory.second) {
             delete mapping;
+            mapping = nullptr;
             throw EnvoyException(fmt::format("Double registration for type: '{}' by '{}' and '{}'",
                                              config_type, factory.second->name(),
                                              it->second->name()));
