442f9fcf21a5f091cec3fe9913ff309e02288659
Jose Ulises Nino Rivera <junr03@users.noreply.github.com>
2020-01-09 20:22:59 -0800

    proto: force link missing v2 protos (#9615)
    #9526 force linked most method descriptors. However, HDS was missing. This PR force links HDS v2.
    
    Risk Level: low
    Testing: compiled with Envoy Mobile's iOS dist target which, as discussed here, requires the hack currently in place.
    
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- source/common/config/protobuf_link_hacks.h
+++ source/common/config/protobuf_link_hacks.h
@@ -5,8 +5,10 @@
 #include "envoy/api/v2/lds.pb.h"
 #include "envoy/api/v2/rds.pb.h"
 #include "envoy/api/v2/srds.pb.h"
+#include "envoy/config/bootstrap/v2/bootstrap.pb.h"
 #include "envoy/service/cluster/v3alpha/cds.pb.h"
 #include "envoy/service/discovery/v2/ads.pb.h"
+#include "envoy/service/discovery/v2/hds.pb.h"
 #include "envoy/service/discovery/v2/rtds.pb.h"
 #include "envoy/service/discovery/v2/sds.pb.h"
 #include "envoy/service/discovery/v3alpha/ads.pb.h"
@@ -43,5 +45,20 @@ const envoy::service::listener::v3alpha::LdsDummy _lds_dummy_v3;
 const envoy::service::route::v3alpha::RdsDummy _rds_dummy_v3;
 const envoy::service::cluster::v3alpha::CdsDummy _cds_dummy_v3;
 const envoy::service::endpoint::v3alpha::EdsDummy _eds_dummy_v3;
-const envoy::service::route::v3alpha::SrdsDummy _srds_dummy_v4;
+const envoy::service::route::v3alpha::SrdsDummy _srds_dummy_v3;
+
+// With the v2 -> v3 migration there is another, related linking issue.
+// Symbols for v2 protos which headers are not included in any file in the codebase are being
+// dropped by the linker in some circumstances. For example, in the Envoy Mobile iOS build system.
+// Even though all v2 packages are included as a dependency in their corresponding v3 package, and
+// `always_link` is set for all proto bazel targets.
+// Further proof of this can be seen by way of counter example with the envoy.api.v2.Cluster type,
+// which is checked for by proto_descriptors.cc. This type **is** getting linked because its headers
+// is still included in cds_api_impl.cc. On the other side because the v2 hds header is not included
+// anywhere the v2 service type is getting dropped, and thus the descriptor is not present in the
+// descriptor pool.
+// https://github.com/envoyproxy/envoy/issues/9639
+const envoy::config::bootstrap::v2::Bootstrap _bootstrap_dummy_v2;
+const envoy::service::discovery::v2::Capability _hds_dummy_v2;
+
 } // namespace Envoy
