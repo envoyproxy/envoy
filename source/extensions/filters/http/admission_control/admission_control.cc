84c1b5172c07ac46d6b76727ab3a7cef81100a7a
Tony Allen <tallen@lyft.com>
2020-01-10 18:05:14 -0800

    wip
    Signed-off-by: Tony Allen <tallen@lyft.com>

--- source/extensions/filters/http/admission_control/admission_control.cc
+++ source/extensions/filters/http/admission_control/admission_control.cc
@@ -1,77 +1,82 @@
-#include "extensions/filters/http/adaptive_concurrency/adaptive_concurrency_filter.h"
+#include "extensions/filters/http/admission_control/admission_control_filter.h"
 
 #include <chrono>
 #include <cstdint>
 #include <string>
 #include <vector>
 
-#include "envoy/extensions/filters/http/adaptive_concurrency/v3alpha/adaptive_concurrency.pb.h"
+#include "envoy/extensions/filters/http/admission_control/v3alpha/admission_control.pb.h"
 
 #include "common/common/assert.h"
 #include "common/protobuf/utility.h"
 
-#include "extensions/filters/http/adaptive_concurrency/concurrency_controller/concurrency_controller.h"
 #include "extensions/filters/http/well_known_names.h"
 
 namespace Envoy {
 namespace Extensions {
 namespace HttpFilters {
-namespace AdaptiveConcurrency {
+namespace AdmissionControl {
 
-AdaptiveConcurrencyFilterConfig::AdaptiveConcurrencyFilterConfig(
-    const envoy::extensions::filters::http::adaptive_concurrency::v3alpha::AdaptiveConcurrency&
+static constexpr double defaultAggression = 1.5;
+static constexpr uint32_t defaultSamplingWindowSeconds = 120;
+static constexpr uint32_t defaultMinRequestSamples = 100;
+
+AdmissionControlFilterConfig::AdmissionControlFilterConfig(
+    const envoy::extensions::filters::http::admission_control::v3alpha::AdmissionControl&
         proto_config,
     Runtime::Loader& runtime, std::string stats_prefix, Stats::Scope&, TimeSource& time_source)
     : stats_prefix_(std::move(stats_prefix)), time_source_(time_source),
-      adaptive_concurrency_feature_(proto_config.enabled(), runtime) {}
+      admission_control_feature_(proto_config.enabled(), runtime),
+      sampling_window_seconds_(proto_config.has_sampling_window() ?
+          DurationUtil::durationToSeconds(proto_config.sampling_window()) : defaultSamplingWindowSeconds),
+      aggression_(
+          PROTOBUF_PERCENT_TO_DOUBLE_OR_DEFAULT(proto_config, aggression, defaultAggression)),
+      min_request_samples_(
+          PROTOBUF_GET_WRAPPED_OR_DEFAULT(proto_config, min_request_samples, defaultMinRequestSamples))
+      {}
 
-AdaptiveConcurrencyFilter::AdaptiveConcurrencyFilter(
-    AdaptiveConcurrencyFilterConfigSharedPtr config, ConcurrencyControllerSharedPtr controller)
-    : config_(std::move(config)), controller_(std::move(controller)) {}
+AdmissionControlFilter::AdmissionControlFilter(AdmissionControlFilterConfigSharedPtr config)
+    : config_(std::move(config)) {}
 
-Http::FilterHeadersStatus AdaptiveConcurrencyFilter::decodeHeaders(Http::HeaderMap&, bool) {
-  // In addition to not sampling if the filter is disabled, health checks should also not be sampled
-  // by the concurrency controller since they may potentially bias the sample aggregate to lower
-  // latency measurements.
-  if (!config_->filterEnabled() || decoder_callbacks_->streamInfo().healthCheck()) {
+Http::FilterHeadersStatus AdmissionControlFilter::decodeHeaders(Http::HeaderMap&, bool) {
+  if (!config_->filterEnabled() ||
+      decoder_callbacks_->streamInfo().healthCheck() ||
+      total_rq_count_.load() < config->minRequestSamples()) {
     return Http::FilterHeadersStatus::Continue;
   }
 
-  if (controller_->forwardingDecision() == ConcurrencyController::RequestForwardingAction::Block) {
-    decoder_callbacks_->sendLocalReply(Http::Code::ServiceUnavailable, "", nullptr, absl::nullopt,
-                                       "reached concurrency limit");
+  if (shouldRejectRequest()) {
+    decoder_callbacks_->sendLocalReply(Http::Code::ServiceUnavailable, "", nullptr, absl::nullopt,   
+                                       "throttling request");                                 
     return Http::FilterHeadersStatus::StopIteration;
   }
-
-  // When the deferred_sample_task_ object is destroyed, the time difference between its destruction
-  // and the request start time is measured as the request latency. This value is sampled by the
-  // concurrency controller either when encoding is complete or during destruction of this filter
-  // object.
-  deferred_sample_task_ =
-      std::make_unique<Cleanup>([this, rq_start_time = config_->timeSource().monotonicTime()]() {
-        const auto now = config_->timeSource().monotonicTime();
-        const std::chrono::nanoseconds rq_latency = now - rq_start_time;
-        controller_->recordLatencySample(rq_latency);
-      });
 
   return Http::FilterHeadersStatus::Continue;
 }
 
-void AdaptiveConcurrencyFilter::encodeComplete() { deferred_sample_task_.reset(); }
+bool AdmissionControlFilter::shouldRejectRequest() const {
+  const double rq_count = total_rq_count_.load();
+  const double rq_success_count = total_success_count_.load();
+  
 
-void AdaptiveConcurrencyFilter::onDestroy() {
+  // TODO @tallen: cleanup types and random thing
+  const double rejection_pct = 100 * std::max<double>(0,
+    (rq_count - config_->aggression() * rq_success_count) / (rq_count + 1));
+
+  return config_->runtime().snapshot().featureEnabled("", rejection_probability);
+}
+
+void AdmissionControlFilter::encodeComplete() { deferred_sample_task_.reset(); }
+
+void AdmissionControlFilter::onDestroy() {
   if (deferred_sample_task_) {
     // The sampling task hasn't been destroyed yet, so this implies we did not complete encoding.
-    // Let's stop the sampling from happening and perform request cleanup inside the controller.
-    //
-    // TODO (tonya11en): Return some RAII handle from the concurrency controller that performs this
-    // logic as part of its lifecycle.
+    // Let's stop the sampling from happening.
     deferred_sample_task_->cancel();
-    controller_->cancelLatencySample();
   }
 }
 
-} // namespace AdaptiveConcurrency
+} // namespace AdmissionControl
 } // namespace HttpFilters
 } // namespace Extensions
 } // namespace Envoy
--- source/extensions/filters/http/admission_control/admission_control.h
+++ source/extensions/filters/http/admission_control/admission_control.h
@@ -5,7 +5,7 @@
 #include <string>
 
 #include "envoy/common/time.h"
-#include "envoy/extensions/filters/http/adaptive_concurrency/v3alpha/adaptive_concurrency.pb.h"
+#include "envoy/extensions/filters/http/admission_control/v3alpha/admission_control.pb.h"
 #include "envoy/http/filter.h"
 #include "envoy/runtime/runtime.h"
 #include "envoy/stats/scope.h"
@@ -14,48 +14,65 @@
 #include "common/common/cleanup.h"
 #include "common/runtime/runtime_protos.h"
 
-#include "extensions/filters/http/adaptive_concurrency/concurrency_controller/concurrency_controller.h"
 #include "extensions/filters/http/common/pass_through_filter.h"
 
 namespace Envoy {
 namespace Extensions {
 namespace HttpFilters {
-namespace AdaptiveConcurrency {
+namespace AdmissionControl {
 
 /**
- * Configuration for the adaptive concurrency limit filter.
+ * All stats for the admission control filter.
  */
-class AdaptiveConcurrencyFilterConfig {
+#define ALL_ADMISSION_CONTROL_STATS(COUNTER, GAUGE)
+  COUNTER(rq_rejected) \
+  GAUGE(success_rate_pct, Accumulate)
+
+/**
+ * Wrapper struct for admission control filter stats. @see stats_macros.h
+ */
+struct AdmissionControlStats {
+  ALL_ADMISSION_CONTROL_STATS(GENERATE_COUNTER_STRUCT, GENERATE_GAUGE_STRUCT)
+};
+
+/**
+ * Configuration for the admission control filter.
+ */
+class AdmissionControlFilterConfig {
 public:
-  AdaptiveConcurrencyFilterConfig(
-      const envoy::extensions::filters::http::adaptive_concurrency::v3alpha::AdaptiveConcurrency&
+  AdmissionControlFilterConfig(
+      const envoy::extensions::filters::http::admission_control::v3alpha::AdmissionControl&
           proto_config,
       Runtime::Loader& runtime, std::string stats_prefix, Stats::Scope& scope,
       TimeSource& time_source);
 
-  bool filterEnabled() const { return adaptive_concurrency_feature_.enabled(); }
+  Runtime::Loader& runtime() { return runtime_; }
+  bool filterEnabled() const { return admission_control_feature_.enabled(); }
   TimeSource& timeSource() const { return time_source_; }
+  uint32_t samplingWindowSeconds() const { return sampling_window_seconds_; }
+  double aggression() const { return aggression_; }
+  uint32_t minRequestSamples() const { return min_request_samples_; }
 
 private:
+  Runtime::Loader& runtime_;
   const std::string stats_prefix_;
   TimeSource& time_source_;
-  Runtime::FeatureFlag adaptive_concurrency_feature_;
+  Runtime::FeatureFlag admission_control_feature_;
+  uint32_t sampling_window_seconds_;
+  double aggression_;
+  uint32_t min_request_samples_;
 };
 
-using AdaptiveConcurrencyFilterConfigSharedPtr =
-    std::shared_ptr<const AdaptiveConcurrencyFilterConfig>;
-using ConcurrencyControllerSharedPtr =
-    std::shared_ptr<ConcurrencyController::ConcurrencyController>;
+using AdmissionControlFilterConfigSharedPtr =
+    std::shared_ptr<const AdmissionControlFilterConfig>;
 
 /**
- * A filter that samples request latencies and dynamically adjusts the request
- * concurrency window.
+ * A filter that probabilistically rejects requests based on upstream success-rate.
  */
-class AdaptiveConcurrencyFilter : public Http::PassThroughFilter,
+class AdmissionControlFilter : public Http::PassThroughFilter,
                                   Logger::Loggable<Logger::Id::filter> {
 public:
-  AdaptiveConcurrencyFilter(AdaptiveConcurrencyFilterConfigSharedPtr config,
-                            ConcurrencyControllerSharedPtr controller);
+  AdmissionControlFilter(AdmissionControlFilterConfigSharedPtr config);
 
   // Http::StreamDecoderFilter
   Http::FilterHeadersStatus decodeHeaders(Http::HeaderMap&, bool) override;
@@ -64,13 +81,24 @@ public:
   void encodeComplete() override;
   void onDestroy() override;
 
+  bool shouldRejectRequest() const;
+
 private:
-  AdaptiveConcurrencyFilterConfigSharedPtr config_;
-  const ConcurrencyControllerSharedPtr controller_;
+  struct SuccessRateAggregate {
+    uint32_t total_rq;
+    uint32_t total_success;
+  }
+
+  AdmissionControlFilterConfigSharedPtr config_;
   std::unique_ptr<Cleanup> deferred_sample_task_;
+  std::queue<std::pair<std::chrono::seconds, SuccessRateAggregate>> sr_aggregates_;
+  std::atomic<uint32_t> current_total_rq;
+  std::atomic<uint32_t> current_total_success;
+
+
 };
 
-} // namespace AdaptiveConcurrency
+} // namespace AdmissionControl
 } // namespace HttpFilters
 } // namespace Extensions
 } // namespace Envoy
