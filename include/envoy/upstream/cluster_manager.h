05ba93a48e781e9c5813c0e12a462c5ecc32d673
aaron-ai <yangkun.ayk@alibaba-inc.com>
2020-01-15 15:04:48 +0800

    Remove useless code
    Signed-off-by: aaron-ai <yangkun.ayk@alibaba-inc.com>

--- include/envoy/upstream/cluster_manager.h
+++ include/envoy/upstream/cluster_manager.h
@@ -257,11 +257,6 @@ public:
   virtual const std::string versionInfo() const PURE;
 
   /**
-   * Update watch set of cluster resources interested.
-   */
-  virtual void updateClusterInterest(const std::set<std::string>& update_to_these_names) PURE;
-
-  /**
    * Add watch set of cluster resources interested.
    */
   virtual void addToClusterInterest(const std::set<std::string>& add_these_names) PURE;
