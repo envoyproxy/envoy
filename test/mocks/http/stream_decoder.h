ea65c4e935e6af4eef75d39c1e03b9a64ac30d70
Derek Argueta <dereka@pinterest.com>
2020-01-23 09:29:48 -0800

    test: batch 3/4 of updating MOCK_METHOD (#9771)
    Signed-off-by: Derek Argueta <dereka@pinterest.com>

--- test/mocks/http/stream_decoder.h
+++ test/mocks/http/stream_decoder.h
@@ -22,11 +22,11 @@ public:
   void decodeMetadata(MetadataMapPtr&& metadata_map) override { decodeMetadata_(metadata_map); }
 
   // Http::StreamDecoder
-  MOCK_METHOD2(decodeHeaders_, void(HeaderMapPtr& headers, bool end_stream));
-  MOCK_METHOD1(decode100ContinueHeaders_, void(HeaderMapPtr& headers));
-  MOCK_METHOD2(decodeData, void(Buffer::Instance& data, bool end_stream));
-  MOCK_METHOD1(decodeTrailers_, void(HeaderMapPtr& trailers));
-  MOCK_METHOD1(decodeMetadata_, void(MetadataMapPtr& metadata_map));
+  MOCK_METHOD(void, decodeHeaders_, (HeaderMapPtr & headers, bool end_stream));
+  MOCK_METHOD(void, decode100ContinueHeaders_, (HeaderMapPtr & headers));
+  MOCK_METHOD(void, decodeData, (Buffer::Instance & data, bool end_stream));
+  MOCK_METHOD(void, decodeTrailers_, (HeaderMapPtr & trailers));
+  MOCK_METHOD(void, decodeMetadata_, (MetadataMapPtr & metadata_map));
 };
 
 } // namespace Http
