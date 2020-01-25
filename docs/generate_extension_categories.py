5862bccf8a42d577f158bc9fb9dce1276cf705cb
Yan Avlasov <yavlasov@google.com>
2020-01-24 19:44:36 -0500

    Fix format
    Signed-off-by: Yan Avlasov <yavlasov@google.com>

--- docs/generate_extension_categories.py
+++ docs/generate_extension_categories.py
@@ -12,10 +12,9 @@ import sys
 
 
 def GetCurrentCommitSHA():
-  r = subprocess.run(
-      ['git', 'rev-list', 'HEAD', '-n', '1'],
-      stdout=subprocess.PIPE,
-      stderr=subprocess.PIPE)
+  r = subprocess.run(['git', 'rev-list', 'HEAD', '-n', '1'],
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE)
 
   commit_sha = r.stdout.decode('utf-8').strip()
   return commit_sha
@@ -25,11 +24,12 @@ if __name__ == '__main__':
   output_file_path = os.path.join(sys.argv[1], 'configuration', 'factory_categories.csv')
   commit_sha = GetCurrentCommitSHA()
 
-  r = subprocess.run(
-      ['find . -name *.h -exec grep -HP "static\s+const\s+char\s+FACTORY_CATEGORY\[\s*\]\s*=\s*\{\s*\".*\"\s*\}\s*;" {} \;'],
-      stdout=subprocess.PIPE,
-      stderr=subprocess.PIPE,
-      shell=True)
+  r = subprocess.run([
+      'find . -name *.h -exec grep -HP "static\s+const\s+char\s+FACTORY_CATEGORY\[\s*\]\s*=\s*\{\s*\".*\"\s*\}\s*;" {} \;'
+  ],
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE,
+                     shell=True)
 
   lines = r.stdout.decode('utf-8').strip().split('\n')
   output = '"Category Name", "Source File"\n'
@@ -40,6 +40,7 @@ if __name__ == '__main__':
     file_path = file_path[2:]
     # The category name is the quoted string in the declaration. Tease it out.
     prefix, category_name, suffix = declaration.split('"')
-    output += '"%s", "`%s <https://github.com/envoyproxy/envoy/blob/%s/%s>`_"\n' % (category_name, file_path, commit_sha, file_path)
+    output += '"%s", "`%s <https://github.com/envoyproxy/envoy/blob/%s/%s>`_"\n' % (
+        category_name, file_path, commit_sha, file_path)
 
   pathlib.Path(output_file_path).write_text(output)
