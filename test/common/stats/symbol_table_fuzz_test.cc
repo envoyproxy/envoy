d7f131b84a5e13eb9f498a3e84fb0603e98be03c
Joshua Marantz <jmarantz@google.com>
2020-01-24 22:24:46 -0500

    flip the order of the stanzas to avoid a level of if-nesting
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/common/stats/symbol_table_fuzz_test.cc
+++ test/common/stats/symbol_table_fuzz_test.cc
@@ -30,50 +30,7 @@ DEFINE_FUZZER(const uint8_t* buf, size_t len) {
     StatName dynamic_stat_name = dynamic_pool.add(next_data);
     StatName fake_dynamic_stat_name = fake_dynamic_pool.add(next_data);
 
-    // We can add stat-names with trailing dots, but note that they will be
-    // trimmed by the Symbol Table implementation, so we must trim the input
-    // string before comparing.
-    absl::string_view trimmed_fuzz_data = StringUtil::removeTrailingCharacters(next_data, '.');
-    FUZZ_ASSERT(trimmed_fuzz_data == symbol_table.toString(stat_name));
-    FUZZ_ASSERT(trimmed_fuzz_data == fake_symbol_table.toString(fake_stat_name));
-    FUZZ_ASSERT(trimmed_fuzz_data == symbol_table.toString(dynamic_stat_name));
-    FUZZ_ASSERT(trimmed_fuzz_data == fake_symbol_table.toString(fake_dynamic_stat_name));
-
-    // Test all combinations of joins within each symbol table.
-    if (!trimmed_fuzz_data.empty()) {
-      std::string joined = absl::StrCat(trimmed_fuzz_data, ".", trimmed_fuzz_data);
-      auto join = [joined](SymbolTable& table, StatName name1, StatName name2) -> bool {
-        bool ok = true;
-        SymbolTable::StoragePtr storage = table.join({name1, name2});
-        StatName stat_name(storage.get());
-        std::string name1_name2 = table.toString(stat_name);
-        if (joined.size() != name1_name2.size()) {
-          std::cerr << "lengths don't match: " << joined.size() << " != " << name1_name2.size()
-                    << std::endl;
-          ok = false;
-        } else {
-          for (uint32_t i = 0; i < joined.size(); ++i) {
-            if (joined[i] != name1_name2[i]) {
-              std::cerr << "char [" << i << "] mismatch: " << joined[i] << "("
-                        << static_cast<uint32_t>(joined[i]) << ") != " << name1_name2[i] << "("
-                        << static_cast<uint32_t>(name1_name2[i]) << ")" << std::endl;
-              ok = false;
-            }
-          }
-        }
-        return ok;
-      };
-      FUZZ_ASSERT(join(symbol_table, stat_name, stat_name));
-      FUZZ_ASSERT(join(symbol_table, stat_name, dynamic_stat_name));
-      FUZZ_ASSERT(join(symbol_table, dynamic_stat_name, dynamic_stat_name));
-      FUZZ_ASSERT(join(symbol_table, dynamic_stat_name, stat_name));
-      FUZZ_ASSERT(join(fake_symbol_table, fake_stat_name, fake_stat_name));
-      FUZZ_ASSERT(join(fake_symbol_table, fake_stat_name, fake_dynamic_stat_name));
-      FUZZ_ASSERT(join(fake_symbol_table, fake_dynamic_stat_name, fake_dynamic_stat_name));
-      FUZZ_ASSERT(join(fake_symbol_table, fake_dynamic_stat_name, fake_stat_name));
-    }
-
-    // Also encode the string directly, without symbolizing it.
+    // Encode the string directly first.
     TestUtil::serializeDeserializeString(next_data);
 
     // Grab the first few bytes from next_data to synthesize together a random uint64_t.
@@ -87,6 +44,51 @@ DEFINE_FUZZER(const uint8_t* buf, size_t len) {
       }
       TestUtil::serializeDeserializeNumber(number);
     }
+
+    // We can add stat-names with trailing dots, but note that they will be
+    // trimmed by the Symbol Table implementation, so we must trim the input
+    // string before comparing.
+    absl::string_view trimmed_fuzz_data = StringUtil::removeTrailingCharacters(next_data, '.');
+    FUZZ_ASSERT(trimmed_fuzz_data == symbol_table.toString(stat_name));
+    FUZZ_ASSERT(trimmed_fuzz_data == fake_symbol_table.toString(fake_stat_name));
+    FUZZ_ASSERT(trimmed_fuzz_data == symbol_table.toString(dynamic_stat_name));
+    FUZZ_ASSERT(trimmed_fuzz_data == fake_symbol_table.toString(fake_dynamic_stat_name));
+
+    // The 'join' tests only work if the trimmed fuzz data is not empty.
+    if (trimmed_fuzz_data.empty()) {
+      continue;
+    }
+
+    std::string joined = absl::StrCat(trimmed_fuzz_data, ".", trimmed_fuzz_data);
+    auto join = [joined](SymbolTable& table, StatName name1, StatName name2) -> bool {
+      bool ok = true;
+      SymbolTable::StoragePtr storage = table.join({name1, name2});
+      StatName stat_name(storage.get());
+      std::string name1_name2 = table.toString(stat_name);
+      if (joined.size() != name1_name2.size()) {
+        std::cerr << "lengths don't match: " << joined.size() << " != " << name1_name2.size()
+                  << std::endl;
+        ok = false;
+      } else {
+        for (uint32_t i = 0; i < joined.size(); ++i) {
+          if (joined[i] != name1_name2[i]) {
+            std::cerr << "char [" << i << "] mismatch: " << joined[i] << "("
+                      << static_cast<uint32_t>(joined[i]) << ") != " << name1_name2[i] << "("
+                      << static_cast<uint32_t>(name1_name2[i]) << ")" << std::endl;
+            ok = false;
+          }
+        }
+      }
+      return ok;
+    };
+    FUZZ_ASSERT(join(symbol_table, stat_name, stat_name));
+    FUZZ_ASSERT(join(symbol_table, stat_name, dynamic_stat_name));
+    FUZZ_ASSERT(join(symbol_table, dynamic_stat_name, dynamic_stat_name));
+    FUZZ_ASSERT(join(symbol_table, dynamic_stat_name, stat_name));
+    FUZZ_ASSERT(join(fake_symbol_table, fake_stat_name, fake_stat_name));
+    FUZZ_ASSERT(join(fake_symbol_table, fake_stat_name, fake_dynamic_stat_name));
+    FUZZ_ASSERT(join(fake_symbol_table, fake_dynamic_stat_name, fake_dynamic_stat_name));
+    FUZZ_ASSERT(join(fake_symbol_table, fake_dynamic_stat_name, fake_stat_name));
   }
 }
 
