9b9be97fc66023eb1d0573e1fe92a6d9c535b500
Derek Argueta <dereka@pinterest.com>
2020-01-31 18:40:20 -0800

    Revert "remove onDestroy from rate limit filter, which is now handled by PassThroughDecoderFilter"
    This reverts commit f9599baa29c4922e6929887d0aec009fcb86dc76.
    
    Signed-off-by: Derek Argueta <dereka@pinterest.com>

--- source/extensions/filters/network/thrift_proxy/filters/ratelimit/ratelimit.h
+++ source/extensions/filters/network/thrift_proxy/filters/ratelimit/ratelimit.h
@@ -73,6 +73,7 @@ public:
   ~Filter() override = default;
 
   // ThriftFilters::PassThroughDecoderFilter
+  void onDestroy() override;
   ThriftProxy::FilterStatus messageBegin(ThriftProxy::MessageMetadataSharedPtr) override;
 
   // RateLimit::RequestCallbacks
