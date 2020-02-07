e176b307bd2ab475574fc7c4b0f92e8575129573
danzh <danzh2010@users.noreply.github.com>
2020-02-07 10:49:33 -0500

    quiche: add multi-worker support for QUIC via BPF (#9424)
    Signed-off-by: Dan Zhang <danzh@google.com>

--- test/mocks/network/mocks.h
+++ test/mocks/network/mocks.h
@@ -312,7 +312,7 @@ public:
   MOCK_METHOD(Stats::Scope&, listenerScope, ());
   MOCK_METHOD(uint64_t, listenerTag, (), (const));
   MOCK_METHOD(const std::string&, name, (), (const));
-  MOCK_METHOD(const Network::ActiveUdpListenerFactory*, udpListenerFactory, ());
+  MOCK_METHOD(Network::ActiveUdpListenerFactory*, udpListenerFactory, ());
   MOCK_METHOD(ConnectionBalancer&, connectionBalancer, ());
 
   envoy::config::core::v3::TrafficDirection direction() const override {
