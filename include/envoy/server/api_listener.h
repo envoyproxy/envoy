751ade4340b67115d656abb45177ef7fac6ea1d6
Jose Nino <jnino@lyft.com>
2020-02-07 13:30:16 -0800

    typo
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- include/envoy/server/api_listener.h
+++ include/envoy/server/api_listener.h
@@ -23,7 +23,7 @@ public:
 
   /**
    * Shutdown the ApiListener. This is an interrupt, not a drain. In other words, calling this
-   * function results in termination of all active streams vs. draining were no new streams are
+   * function results in termination of all active streams vs. draining where no new streams are
    * allowed, but already existing streams are allowed to finish.
    */
   virtual void shutdown() PURE;
