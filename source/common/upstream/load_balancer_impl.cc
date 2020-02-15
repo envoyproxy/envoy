ec7aacedc03ae652867681af62f14fd35c155789
maziang <maziang@bytedance.com>
2020-02-15 12:45:38 +0800

    fix comment
    Signed-off-by: maziang <maziang@bytedance.com>

--- source/common/upstream/load_balancer_impl.cc
+++ source/common/upstream/load_balancer_impl.cc
@@ -728,8 +728,8 @@ void EdfLoadBalancerBase::refresh(uint32_t priority) {
     }
 
     // Current time is initialized as a float ranges [0.0, 1.0).
-    // Accuracy 1e-5 is enough because (1 / 128) ^ 2 > 1e-5.
-    // 128 is the largest host weight (see envoy.api.v2.endpoint.Endpoint.load_balancing_weight).
+    // Accuracy 1e-5 is enough because (1 / 128) ^ 2 > 1e-5. 128 is the largest weight for hosts
+    // (see envoy.api.v2.endpoint.Endpoint.load_balancing_weight).
     scheduler.edf_ = std::make_unique<EdfScheduler<const Host>>(seed_ % 100000 / 100000.0);
 
     // Populate scheduler with host list.
