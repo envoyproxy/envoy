49ad3f5c8209403110a0140e0ec2be2a0b46491f
Jingzhao <Jingzhao.Ni@arm.com>
2020-02-18 16:56:02 +0000

    Enable docker build on Arm64 platform
    For supporting docker build tools on Arm64 platform, some modification
    was done in this patch.
    1. Add arm64 envoy-build-tools image's sha256 value in bazelrc.
    2. Modify some script for supporting different platform.
    
    Signed-off-by: Jingzhao <Jingzhao.Ni@arm.com>

--- ci/envoy_build_sha.sh
+++ ci/envoy_build_sha.sh
@@ -1,2 +1,6 @@
-ENVOY_BUILD_SHA=$(grep envoyproxy/envoy-build-ubuntu@sha256 $(dirname $0)/../.bazelrc | sed -e 's#.*envoyproxy/envoy-build-ubuntu@sha256:\(.*\)#\1#' | uniq)
+if [[ $(uname -m) == "x86_64" ]]; then
+	ENVOY_BUILD_SHA=$(grep experimental_docker_image_amd64 $(dirname $0)/../.bazelrc | sed -e 's#.*envoyproxy/envoy-build-ubuntu@sha256:\(.*\)#\1#' | uniq)
+elif [[ $(uname -m) == "aarch64" ]]; then
+	ENVOY_BUILD_SHA=$(grep experimental_docker_image_arm64 $(dirname $0)/../.bazelrc | sed -e 's#.*envoyproxy/envoy-build-ubuntu@sha256:\(.*\)#\1#' | uniq)
+fi
 [[ $(wc -l <<< "${ENVOY_BUILD_SHA}" | awk '{$1=$1};1') == 1 ]] || (echo ".bazelrc envoyproxy/envoy-build-ubuntu hashes are inconsistent!" && exit 1)
