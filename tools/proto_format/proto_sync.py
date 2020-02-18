f63c90b68ddd4b493958f74c8c7b77ff19b5aa38
Erica Manno <erica.manno@gmail.com>
2020-02-18 17:51:01 +0100

    Update refs to proto format files
    Signed-off-by: Erica Manno <erica.manno@gmail.com>

--- tools/proto_format/proto_sync.py
+++ tools/proto_format/proto_sync.py
@@ -58,7 +58,7 @@ class RequiresReformatError(ProtoSyncError):
 
   def __init__(self, message):
     super(RequiresReformatError, self).__init__(
-        '%s; either run ./ci/do_ci.sh fix_format or ./tools/proto_format.sh fix to reformat.\n' %
+        '%s; either run ./ci/do_ci.sh fix_format or ./tools/proto_format/proto_format.sh fix to reformat.\n' %
         message)
 
 
