059f366ffa250ba09a4ef985c899bdd3ec19debb
Kuat Yessenov <kuat@google.com>
2020-02-27 13:26:35 -0800

    initial cut
    Signed-off-by: Kuat Yessenov <kuat@google.com>

---
+++ test/integration/direct_response_integration_test.cc
@@ -0,0 +1,59 @@
+#include "test/integration/integration.h"
+#include "test/integration/utility.h"
+#include "test/test_common/utility.h"
+
+namespace Envoy {
+
+std::string direct_response_config;
+
+class DirectResponseIntegrationTest : public testing::TestWithParam<Network::Address::IpVersion>,
+                                      public BaseIntegrationTest {
+public:
+  DirectResponseIntegrationTest() : BaseIntegrationTest(GetParam(), direct_response_config) {}
+
+  // Called once by the gtest framework before any DirectResponseIntegrationTests are run.
+  static void SetUpTestSuite() {
+    direct_response_config = ConfigHelper::BASE_CONFIG + R"EOF(
+    filter_chains:
+      filters:
+      - name: direct_response
+        typed_config:
+          "@type": type.googleapis.com/envoy.extensions.filters.network.direct_response.v3.Config
+          response: aGVsbG8sIHdvcmxkIQo=
+      )EOF";
+  }
+
+  /**
+   * Initializer for an individual test.
+   */
+  void SetUp() override { BaseIntegrationTest::initialize(); }
+
+  /**
+   *  Destructor for an individual test.
+   */
+  void TearDown() override {
+    test_server_.reset();
+    fake_upstreams_.clear();
+  }
+};
+
+INSTANTIATE_TEST_SUITE_P(IpVersions, DirectResponseIntegrationTest,
+                         testing::ValuesIn(TestEnvironment::getIpVersionsForTest()),
+                         TestUtility::ipTestParamsToString);
+
+TEST_P(DirectResponseIntegrationTest, Hello) {
+  Buffer::OwnedImpl buffer("hello");
+  std::string response;
+  RawConnectionDriver connection(
+      lookupPort("listener_0"), buffer,
+      [&](Network::ClientConnection&, const Buffer::Instance& data) -> void {
+        response.append(data.toString());
+        connection.close();
+      },
+      version_);
+
+  connection.run();
+  EXPECT_EQ("hello, world!\n", response);
+}
+
+} // namespace Envoy
