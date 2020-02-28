64384d03a608ec33a66acb0fe0fb48b28d8be0d5
Snow Pettersen <aickck@gmail.com>
2020-02-28 13:10:11 -0800

    stats: allow specifying tags when creating metric objects (#9743)
    Signed-off-by: Snow Pettersen <aickck@gmail.com>

--- source/common/stats/metric_impl.h
+++ source/common/stats/metric_impl.h
@@ -21,13 +21,13 @@ namespace Stats {
  */
 class MetricHelper {
 public:
-  MetricHelper(StatName name, absl::string_view tag_extracted_name, const std::vector<Tag>& tags,
-               SymbolTable& symbol_table);
+  MetricHelper(StatName name, absl::string_view tag_extracted_name,
+               const StatNameTagVector& stat_name_tags, SymbolTable& symbol_table);
   ~MetricHelper();
 
   StatName statName() const;
   std::string name(const SymbolTable& symbol_table) const;
-  std::vector<Tag> tags(const SymbolTable& symbol_table) const;
+  TagVector tags(const SymbolTable& symbol_table) const;
   StatName tagExtractedStatName() const;
   void iterateTagStatNames(const Metric::TagStatNameIterFn& fn) const;
   void clear(SymbolTable& symbol_table) { stat_names_.clear(symbol_table); }
@@ -56,15 +56,15 @@ private:
 template <class BaseClass> class MetricImpl : public BaseClass {
 public:
   // TODO(jmarantz): Use StatName for tag_extracted_name.
-  MetricImpl(StatName name, absl::string_view tag_extracted_name, const std::vector<Tag>& tags,
-             SymbolTable& symbol_table)
-      : helper_(name, tag_extracted_name, tags, symbol_table) {}
+  MetricImpl(StatName name, absl::string_view tag_extracted_name,
+             const StatNameTagVector& stat_name_tags, SymbolTable& symbol_table)
+      : helper_(name, tag_extracted_name, stat_name_tags, symbol_table) {}
 
   // Empty construction of a MetricImpl; used for null stats.
   explicit MetricImpl(SymbolTable& symbol_table)
-      : MetricImpl(StatNameManagedStorage("", symbol_table).statName(), "", std::vector<Tag>(),
+      : MetricImpl(StatNameManagedStorage("", symbol_table).statName(), "", StatNameTagVector{},
                    symbol_table) {}
-  std::vector<Tag> tags() const override { return helper_.tags(constSymbolTable()); }
+  TagVector tags() const override { return helper_.tags(constSymbolTable()); }
   StatName statName() const override { return helper_.statName(); }
   StatName tagExtractedStatName() const override { return helper_.tagExtractedStatName(); }
   void iterateTagStatNames(const Metric::TagStatNameIterFn& fn) const override {
