ab6baf036af4bd24e7c7e03baba214632abd8555
Dan Zhang <danzh@google.com>
2020-02-28 20:08:54 -0500

    update tar
    Signed-off-by: Dan Zhang <danzh@google.com>

--- test/extensions/quic_listeners/quiche/envoy_quic_server_stream_test.cc
+++ test/extensions/quic_listeners/quiche/envoy_quic_server_stream_test.cc
@@ -50,9 +50,10 @@ public:
     quic_stream_->setRequestDecoder(stream_decoder_);
     quic_stream_->addCallbacks(stream_callbacks_);
     quic_session_.ActivateStream(std::unique_ptr<EnvoyQuicServerStream>(quic_stream_));
-    EXPECT_CALL(quic_session_, WritevData(_, _, _, _, _))
-        .WillRepeatedly(Invoke([](quic::QuicStream*, quic::QuicStreamId, size_t write_length,
-                                  quic::QuicStreamOffset, quic::StreamSendingState state) {
+    EXPECT_CALL(quic_session_, WritevData(_, _, _, _, _, _))
+        .WillRepeatedly(Invoke([](quic::QuicStreamId, size_t write_length, quic::QuicStreamOffset,
+                                  quic::StreamSendingState state, bool,
+                                  quiche::QuicheOptional<quic::EncryptionLevel>) {
           return quic::QuicConsumedData{write_length, state != quic::NO_FIN};
         }));
     EXPECT_CALL(writer_, WritePacket(_, _, _, _, _))
