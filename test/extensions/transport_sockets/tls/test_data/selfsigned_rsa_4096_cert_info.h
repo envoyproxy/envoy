a5604662d9765a1292187946abdbbf5024d01798
Snow Pettersen <aickck@gmail.com>
2020-02-29 08:59:03 -0800

    format
    Signed-off-by: Snow Pettersen <aickck@gmail.com>

--- test/extensions/transport_sockets/tls/test_data/selfsigned_rsa_4096_cert_info.h
+++ test/extensions/transport_sockets/tls/test_data/selfsigned_rsa_4096_cert_info.h
@@ -1,7 +1,8 @@
 // NOLINT(namespace-envoy)
 constexpr char TEST_SELFSIGNED_RSA_4096_CERT_HASH[] =
     "82eb3c09e6aaa4cf55f24f6f6b0c6cdba60ab2b0f67c0937ce17166b81a988bc";
-constexpr char TEST_SELFSIGNED_RSA_4096_CERT_SPKI[] = "9iot4XADYlXoT1RDAR4kfOi+/DYNiy0k1nGc1wD8mvs=";
+constexpr char TEST_SELFSIGNED_RSA_4096_CERT_SPKI[] =
+    "9iot4XADYlXoT1RDAR4kfOi+/DYNiy0k1nGc1wD8mvs=";
 constexpr char TEST_SELFSIGNED_RSA_4096_CERT_SERIAL[] = "b2fe5ebacd2af1d7";
 constexpr char TEST_SELFSIGNED_RSA_4096_CERT_NOT_BEFORE[] = "Feb 29 16:52:38 2020 GMT";
 constexpr char TEST_SELFSIGNED_RSA_4096_CERT_NOT_AFTER[] = "Feb 28 16:52:38 2022 GMT";
