0d03e6fa982d36607b85abf4ccd459a8ffe3939a
Michael Rebello <me@michaelrebello.com>
2020-03-02 16:33:11 -0800

    core: add missing header docstrings (#708)
    Signed-off-by: Michael Rebello <me@michaelrebello.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/common/engine.h
+++ mobile/library/common/engine.h
@@ -21,11 +21,25 @@ namespace Envoy {
 
 class Engine {
 public:
+  /**
+   * Constructor for a new engine instance.
+   * @param callbacks, the callbacks to use for engine lifecycle monitoring.
+   * @param config, the Envoy configuration to use when starting the instance.
+   * @param log_level, the log level with which to configure the engine.
+   * @param preferred_network, hook to obtain the preferred network for new streams.
+   */
   Engine(envoy_engine_callbacks callbacks, const char* config, const char* log_level,
          std::atomic<envoy_network_t>& preferred_network);
 
+  /**
+   * Engine destructor.
+   */
   ~Engine();
 
+  /**
+   * Accessor for the http dispatcher.
+   * @return Http::Dispatcher&, the dispatcher being used by the engine.
+   */
   Http::Dispatcher& httpDispatcher();
 
   /**
