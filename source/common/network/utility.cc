dfca526a18768332af0bbb0dffe8936e4cafbd30
Ilya Leoshkevich <45998171+iii-i@users.noreply.github.com>
2020-03-04 18:35:33 +0100

    common: add big-endian support to Ip6ntohl and Ip6htonl (#10250)
    This patch resolves the TODO from
    https://github.com/envoyproxy/envoy/pull/2431.
    Tests pass on a big-endian machine (IBM Z).
    
    Signed-off-by: Ilya Leoshkevich <iii@linux.ibm.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- source/common/network/utility.cc
+++ source/common/network/utility.cc
@@ -424,17 +424,19 @@ bool Utility::portInRangeList(const Address::Instance& address, const std::list<
 }
 
 absl::uint128 Utility::Ip6ntohl(const absl::uint128& address) {
-  // TODO(ccaraman): Support Ip6ntohl for big-endian.
-  static_assert(ABSL_IS_LITTLE_ENDIAN,
-                "Machines using big-endian byte order is not supported for IPv6.");
+#ifdef ABSL_IS_LITTLE_ENDIAN
   return flipOrder(address);
+#else
+  return address;
+#endif
 }
 
 absl::uint128 Utility::Ip6htonl(const absl::uint128& address) {
-  // TODO(ccaraman): Support Ip6ntohl for big-endian.
-  static_assert(ABSL_IS_LITTLE_ENDIAN,
-                "Machines using big-endian byte order is not supported for IPv6.");
+#ifdef ABSL_IS_LITTLE_ENDIAN
   return flipOrder(address);
+#else
+  return address;
+#endif
 }
 
 absl::uint128 Utility::flipOrder(const absl::uint128& input) {
