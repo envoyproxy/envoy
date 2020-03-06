9076fc2eb568f00f3bb45e77f797e8e6f35d913f
Kevin Burek <kburek@lyft.com>
2020-03-06 14:36:32 -0800

    a little templating
    Signed-off-by: Kevin Burek <kburek@lyft.com>

--- test/tools/router_check/router.h
+++ test/tools/router_check/router.h
@@ -134,17 +134,17 @@ private:
   bool compareRedirectPath(ToolConfig& tool_config,
                            const envoy::RouterCheckToolSchema::ValidationAssert& expected);
   bool compareRequestHeaderFields(ToolConfig& tool_config,
-                                 const envoy::RouterCheckToolSchema::ValidationAssert& expected);
-  bool compareRequestHeaderField(ToolConfig& tool_config, const std::string& field,
-                                 const std::string& expected, const bool expect_match);
-  bool expectRequestHeaderField(ToolConfig& tool_config, const std::string& field,
-                                  const bool expected_present);
-  bool compareResponseHeaderFields(ToolConfig& tool_config,
                                   const envoy::RouterCheckToolSchema::ValidationAssert& expected);
-  bool compareResponseHeaderField(ToolConfig& tool_config, const std::string& field,
-                                  const std::string& expected, const bool expect_match);
-  bool expectResponseHeaderField(ToolConfig& tool_config, const std::string& field,
-                                  const bool expected_present);
+  bool compareResponseHeaderFields(ToolConfig& tool_config,
+                                   const envoy::RouterCheckToolSchema::ValidationAssert& expected);
+  template <typename HM>
+  bool compareHeaderField(const HM& header_map, const std::string& field,
+                          const std::string& expected, const std::string& test_type,
+                          const bool expect_match);
+  template <typename HM>
+  bool expectHeaderField(const HM& header_map, const std::string& field,
+                         const std::string& test_type, const bool expected_present);
+
   /**
    * Compare the expected and actual route parameter values. Print out match details if details_
    * flag is set.
