6347834d3dcf955d112d4aca20a988275ce04f35
Snow Pettersen <kpettersen@netflix.com>
2020-03-05 18:43:31 -0800

    update new cert to reference old CA
    Signed-off-by: Snow Pettersen <kpettersen@netflix.com>

--- test/extensions/transport_sockets/tls/test_data/extensions_cert_info.h
+++ test/extensions/transport_sockets/tls/test_data/extensions_cert_info.h
@@ -1,7 +1,7 @@
 // NOLINT(namespace-envoy)
 constexpr char TEST_EXTENSIONS_CERT_HASH[] =
-    "673fae477168386b5066a7b7aa1e2550a19f1f982f8a49fcf3c0c6e4765cd8f3";
-constexpr char TEST_EXTENSIONS_CERT_SPKI[] = "Ghn1W+L7esjMSLKQEzrI6MosrApveMRUQlMATGXk6eg=";
-constexpr char TEST_EXTENSIONS_CERT_SERIAL[] = "f0fd8464bdbe7d0c";
-constexpr char TEST_EXTENSIONS_CERT_NOT_BEFORE[] = "Mar  6 02:33:05 2020 GMT";
-constexpr char TEST_EXTENSIONS_CERT_NOT_AFTER[] = "Mar  6 02:33:05 2022 GMT";
+    "5a24be0c5ca6c89c10f0905f9810d08db06b49b9029db77622b050340de3400b";
+constexpr char TEST_EXTENSIONS_CERT_SPKI[] = "r4AA/+y2tvwjcrBpByHVqkR1Rg0k3Fw3U7YyCYKvjXc=";
+constexpr char TEST_EXTENSIONS_CERT_SERIAL[] = "f490a17388dcd3a9";
+constexpr char TEST_EXTENSIONS_CERT_NOT_BEFORE[] = "Mar  6 02:41:10 2020 GMT";
+constexpr char TEST_EXTENSIONS_CERT_NOT_AFTER[] = "Mar  6 02:41:10 2022 GMT";
