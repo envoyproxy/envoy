5112f82c926c6c8278311aa116c5786a36b36d9c
Lin Lin <linlineric@google.com>
2020-03-09 12:25:36 -0400

    Add fuzz tests for more functions
    Signed-off-by: Lin Lin <linlineric@google.com>

--- test/common/common/hex_fuzz_test.cc
+++ test/common/common/hex_fuzz_test.cc
@@ -1,6 +1,7 @@
 #include "common/common/hex.h"
 
 #include "test/fuzz/fuzz_runner.h"
+#include "test/fuzz/utility.h"
 
 namespace Envoy {
 namespace Fuzz {
@@ -8,6 +9,13 @@ namespace Fuzz {
 DEFINE_FUZZER(const uint8_t* buf, size_t len) {
   Envoy::Hex::encode(buf, len);
   Envoy::Hex::decode(std::string(reinterpret_cast<const char*>(buf), len));
+
+  FuzzedDataProvider fuzz_data(buf, len);
+  auto decoded = Envoy::Hex::decode(Envoy::Hex::encode(buf, len));
+  FUZZ_ASSERT(fuzz_data.ConsumeBytes<uint8_t>(len) == decoded);
+
+  Envoy::Hex::uint64ToHex(fuzz_data.ConsumeIntegral<uint64_t>());
+  Envoy::Hex::uint32ToHex(fuzz_data.ConsumeIntegral<uint32_t>());
 }
 
 } // namespace Fuzz
