ff3a379c8a503dde1e1f9f88c9aa00de50583db0
Lin Lin <linlineric@google.com>
2020-03-09 14:40:29 -0400

    Put fuzz ConsumeBytes to last
    Signed-off-by: Lin Lin <linlineric@google.com>

--- test/common/common/hex_fuzz_test.cc
+++ test/common/common/hex_fuzz_test.cc
@@ -7,15 +7,16 @@ namespace Envoy {
 namespace Fuzz {
 
 DEFINE_FUZZER(const uint8_t* buf, size_t len) {
+
+  FuzzedDataProvider fuzz_data(buf, len);
+  Envoy::Hex::uint64ToHex(fuzz_data.ConsumeIntegral<uint64_t>());
+  Envoy::Hex::uint32ToHex(fuzz_data.ConsumeIntegral<uint32_t>());
+
   Envoy::Hex::encode(buf, len);
   Envoy::Hex::decode(std::string(reinterpret_cast<const char*>(buf), len));
 
-  FuzzedDataProvider fuzz_data(buf, len);
   auto decoded = Envoy::Hex::decode(Envoy::Hex::encode(buf, len));
   FUZZ_ASSERT(fuzz_data.ConsumeBytes<uint8_t>(len) == decoded);
-
-  Envoy::Hex::uint64ToHex(fuzz_data.ConsumeIntegral<uint64_t>());
-  Envoy::Hex::uint32ToHex(fuzz_data.ConsumeIntegral<uint32_t>());
 }
 
 } // namespace Fuzz
