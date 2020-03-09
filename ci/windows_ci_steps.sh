54309797f364c928d108f337aa1b3d79faba4f1e
William A Rowe Jr <wrowe@pivotal.io>
2020-03-09 11:12:53 -0400

    Restore PATH until we have a better solution
    - This PATH is too explicit, and we likely want to use devenv in the
      VC toolkit to resolve the correct build envvars in conjunction with
      the specific build instance. Perhaps a build machine local .bazelrc
      is a possible workaround?
    
    Signed-off-by: William A Rowe Jr <wrowe@pivotal.io>

--- ci/windows_ci_steps.sh
+++ ci/windows_ci_steps.sh
@@ -13,6 +13,9 @@ df -h
 
 . "$(dirname "$0")"/setup_cache.sh
 
+# TODO(dio): Put in windows/.bazelrc.
+export PATH="/c/Program Files (x86)/Windows Kits/10/bin/10.0.17763.0/x64":$PATH
+
 BAZEL_STARTUP_OPTIONS="--noworkspace_rc --bazelrc=windows/.bazelrc --output_base=c:/_eb"
 BAZEL_BUILD_OPTIONS="-c fastbuild --config=msvc-cl --show_task_finish --verbose_failures \
   --test_output=all ${BAZEL_BUILD_EXTRA_OPTIONS} ${BAZEL_EXTRA_TEST_OPTIONS}"
