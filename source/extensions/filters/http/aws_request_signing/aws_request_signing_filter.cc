28f33a776960dd717d4b57da73b0dbce7dbb3e85
Raúl Gutiérrez Segalés <rgs@pinterest.com>
2020-03-09 17:18:53 -0300

    aws_request_signing: a few fixes (#10280)
    There were a few things missing to make this filter work with S3:
    
    * empty body requests (e.g.: GET) still need a sha256 content header
      (the empty string hash)
    * requests for s3 shouldn't sign all headers (especially because some
      of them might change, e.g.: x-forwarded-for)
    * when proxying requests to s3, HCM's host rewrite won't work so
      support it as a filter config option
    * ditto for prefix rewrite
    
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>

--- source/extensions/filters/http/aws_request_signing/aws_request_signing_filter.cc
+++ source/extensions/filters/http/aws_request_signing/aws_request_signing_filter.cc
@@ -8,8 +8,10 @@ namespace HttpFilters {
 namespace AwsRequestSigningFilter {
 
 FilterConfigImpl::FilterConfigImpl(Extensions::Common::Aws::SignerPtr&& signer,
-                                   const std::string& stats_prefix, Stats::Scope& scope)
-    : signer_(std::move(signer)), stats_(Filter::generateStats(stats_prefix, scope)) {}
+                                   const std::string& stats_prefix, Stats::Scope& scope,
+                                   const std::string& host_rewrite)
+    : signer_(std::move(signer)), stats_(Filter::generateStats(stats_prefix, scope)),
+      host_rewrite_(host_rewrite) {}
 
 Filter::Filter(const std::shared_ptr<FilterConfig>& config) : config_(config) {}
 
@@ -17,12 +19,19 @@ Extensions::Common::Aws::Signer& FilterConfigImpl::signer() { return *signer_; }
 
 FilterStats& FilterConfigImpl::stats() { return stats_; }
 
+const std::string& FilterConfigImpl::hostRewrite() const { return host_rewrite_; }
+
 FilterStats Filter::generateStats(const std::string& prefix, Stats::Scope& scope) {
   const std::string final_prefix = prefix + "aws_request_signing.";
   return {ALL_AWS_REQUEST_SIGNING_FILTER_STATS(POOL_COUNTER_PREFIX(scope, final_prefix))};
 }
 
 Http::FilterHeadersStatus Filter::decodeHeaders(Http::RequestHeaderMap& headers, bool) {
+  const auto& host_rewrite = config_->hostRewrite();
+  if (!host_rewrite.empty()) {
+    headers.setHost(host_rewrite);
+  }
+
   try {
     config_->signer().sign(headers);
     config_->stats().signing_added_.inc();
