b269c762ef11e8a0db959133b651a159108f247d
Yaroslav Skopets <y.skopets@gmail.com>
2020-03-10 17:01:23 +0100

    unit tests: add test cases for HttpTracerManagerImpl
    Signed-off-by: Yaroslav Skopets <y.skopets@gmail.com>

--- test/common/tracing/http_tracer_manager_impl_test.cc
+++ test/common/tracing/http_tracer_manager_impl_test.cc
@@ -3,6 +3,7 @@
 #include "common/tracing/http_tracer_manager_impl.h"
 
 #include "test/mocks/server/mocks.h"
+#include "test/test_common/registry.h"
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
@@ -15,18 +16,56 @@ namespace Envoy {
 namespace Tracing {
 namespace {
 
+class SampleTracer : public HttpTracer {
+public:
+  SpanPtr startSpan(const Config&, Http::RequestHeaderMap&, const StreamInfo::StreamInfo&,
+                    const Tracing::Decision) override {
+    return nullptr;
+  }
+};
+
+class SampleTracerFactory : public Server::Configuration::TracerFactory {
+public:
+  Tracing::HttpTracerPtr createHttpTracer(const Protobuf::Message&,
+                                          Server::Configuration::TracerFactoryContext&) override {
+    return std::make_unique<SampleTracer>();
+  }
+
+  std::string name() const override { return "envoy.tracers.sample"; }
+
+  ProtobufTypes::MessagePtr createEmptyConfigProto() override {
+    return std::make_unique<ProtobufWkt::Struct>();
+  }
+};
+
 class HttpTracerManagerImplTest : public testing::Test {
 public:
   NiceMock<Server::Configuration::MockServerFactoryContext> server_factory_context_;
-  HttpTracerManagerImpl http_tracer_manager_{
-      std::make_unique<TracerFactoryContextImpl>(server_factory_context_, ProtobufMessage::getStrictValidationVisitor())};
+  HttpTracerManagerImpl http_tracer_manager_{std::make_unique<TracerFactoryContextImpl>(
+      server_factory_context_, ProtobufMessage::getStrictValidationVisitor())};
+
+private:
+  SampleTracerFactory sample_tracer_factory_;
+  Registry::InjectFactory<Server::Configuration::TracerFactory> registered_sample_tracer_factory_{
+      sample_tracer_factory_};
 };
 
-TEST_F(HttpTracerManagerImplTest, ShouldReturnHttpNullTracerWhenNoTracingProviderHasBeenConfigured) {
+TEST_F(HttpTracerManagerImplTest,
+       ShouldReturnHttpNullTracerWhenNoTracingProviderHasBeenConfigured) {
   auto http_tracer = http_tracer_manager_.getOrCreateHttpTracer(nullptr);
 
   // Should return a null object (Tracing::HttpNullTracer) rather than nullptr.
   EXPECT_THAT(http_tracer.get(), WhenDynamicCastTo<Tracing::HttpNullTracer*>(NotNull()));
+}
+
+TEST_F(HttpTracerManagerImplTest, ShouldUseProperTracerFactory) {
+  envoy::config::trace::v3::Tracing_Http tracing_config;
+  tracing_config.set_name("envoy.tracers.sample");
+
+  auto http_tracer = http_tracer_manager_.getOrCreateHttpTracer(&tracing_config);
+
+  // Should use proper TracerFactory.
+  EXPECT_THAT(http_tracer.get(), WhenDynamicCastTo<SampleTracer*>(NotNull()));
 }
 
 } // namespace
