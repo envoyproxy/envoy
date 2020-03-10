dba0039c1e48c814533e886ef3938970f278fb40
Jose Nino <jnino@lyft.com>
2020-03-10 15:37:06 -0700

    spelling
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- source/common/router/router.cc
+++ source/common/router/router.cc
@@ -1232,7 +1232,7 @@ void Filter::onUpstreamHeaders(uint64_t response_code, Http::ResponseHeaderMapPt
   // This header is added without checking for config_.suppress_envoy_headers_ to mirror what is
   // done for upstream requests. Note: this work is done here instead of in
   // route_entry_->finalizeResponse headers because the attempt_count_ data pertains to the router.
-  // This could be moved, if desired, by charging callbacks_->streamInfo with the attemp count.
+  // This could be moved, if desired, by charging callbacks_->streamInfo with the attempt count.
   // Adding the information to the stream info would also be necessary if we wanted this header to
   // be added on sendLocalReply.
   if (include_attempt_count_) {
