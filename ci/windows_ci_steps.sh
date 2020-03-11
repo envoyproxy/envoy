9477979f615707e7ee66a1a8079dc8b5e96c74d2
William A Rowe Jr <wrowe@pivotal.io>
2020-03-11 12:40:28 -0400

    fix quoting
    Co-authored-by: Sunjay Bhatia <sbhatia@pivotal.io>
    Co-authored-by: William A Rowe Jr <wrowe@pivotal.io>
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>
    Signed-off-by: William A Rowe Jr <wrowe@pivotal.io>

--- ci/windows_ci_steps.sh
+++ ci/windows_ci_steps.sh
@@ -11,7 +11,7 @@ trap finish EXIT
 echo "disk space at beginning of build:"
 df -h
 
-/c/windows/system32/cmd.exe /c "rmdir /s /q C:\Program Files\Git\tmp && mkdir C:\tmp && mklink /d C:\Program Files\Git\tmp C:\tmp && mklink /d D:\tmp C:\tmp"
+/c/windows/system32/cmd.exe "/c rmdir /s /q C:\Program Files\Git\tmp && mkdir C:\tmp && mklink /d C:\Program Files\Git\tmp C:\tmp && mklink /d D:\tmp C:\tmp"
 
 echo "check on the directory setup"
 ls -lah / && ls -lah /tmp
