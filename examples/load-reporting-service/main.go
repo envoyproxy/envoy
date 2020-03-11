f3fe2fca9bf3fc7c0b8795cad7981eccca9785c3
kathan24 <kshah@lyft.com>
2020-03-11 16:18:00 -0700

    Simplifying the example even further
    Signed-off-by: kathan24 <kshah@lyft.com>

--- examples/load-reporting-service/main.go
+++ examples/load-reporting-service/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net"
-	"time"
 
 	"github.com/envoyproxy/envoy/examples/load-reporting-service/server"
 	gcpLoadStats "github.com/envoyproxy/go-control-plane/envoy/service/load_stats/v2"
@@ -11,9 +10,6 @@ import (
 )
 
 func main() {
-	// This is how often Envoy will send the load report
-	const StatsFrequencyInSeconds = 2
-
 	// Listening on port 18000
 	address := ":18000"
 	lis, err := net.Listen("tcp", address)
@@ -24,19 +20,10 @@ func main() {
 	grpcServer := grpc.NewServer()
 	xdsServer := server.NewServer()
 	gcpLoadStats.RegisterLoadReportingServiceServer(grpcServer, xdsServer)
-	startCollectingStats(xdsServer, "http_service", []string{"local_service"}, StatsFrequencyInSeconds)
 
 	log.Printf("LRS Server is up and running on %s", address)
 	err = grpcServer.Serve(lis)
 	if err != nil {
 		panic(err)
 	}
-}
-
-func startCollectingStats(server server.Server, cluster string, upstreamClusters []string, frequency int64) {
-	// Send LoadStatsResponse after 10 seconds to initiate the Load Reporting
-	go func() {
-		time.Sleep(10 * time.Second)
-		server.SendResponse(cluster, upstreamClusters, frequency)
-	}()
 }
--- examples/load-reporting-service/server/lrs_server.go
+++ examples/load-reporting-service/server/lrs_server.go
@@ -4,42 +4,29 @@ import (
 	"log"
 	"sync"
 
-	core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	gcpLoadStats "github.com/envoyproxy/go-control-plane/envoy/service/load_stats/v2"
 	"github.com/golang/protobuf/ptypes/duration"
-	"google.golang.org/grpc"
 )
+
+// This is how often Envoy will send the load report
+const StatsFrequencyInSeconds = 2
 
 // Server handling Load Stats communication
 type Server interface {
 	gcpLoadStats.LoadReportingServiceServer
-	SendResponse(cluster string, upstreamCluster []string, frequency int64)
 	HandleRequest(stream gcpLoadStats.LoadReportingService_StreamLoadStatsServer, request *gcpLoadStats.LoadStatsRequest)
 }
 
 func NewServer() Server {
-	return &server{lrsCache: make(map[string]map[string]NodeMetadata)}
+	return &server{nodesConnected: make(map[string]bool)}
 }
 
 type server struct {
-	// protects lrsCache
+	// protects nodesConnected
 	mu sync.Mutex
 
-	// This cache stores stream objects (and Node data) for every node (within a cluster) upon connection
-	lrsCache map[string]map[string]NodeMetadata
-}
-
-// Struct to hold stream object and node details
-type NodeMetadata struct {
-	stream stream
-	node   *core.Node
-}
-
-type stream interface {
-	grpc.ServerStream
-
-	Send(*gcpLoadStats.LoadStatsResponse) error
-	Recv() (*gcpLoadStats.LoadStatsRequest, error)
+	// This cache stores nodes connected to the LRS server
+	nodesConnected map[string]bool
 }
 
 // Handles incoming stream connections and LoadStatsRequests
@@ -56,32 +43,27 @@ func (s *server) StreamLoadStats(stream gcpLoadStats.LoadReportingService_Stream
 }
 
 func (s *server) HandleRequest(stream gcpLoadStats.LoadReportingService_StreamLoadStatsServer, request *gcpLoadStats.LoadStatsRequest) {
-	clusterName := request.GetNode().GetCluster()
 	nodeID := request.GetNode().GetId()
 
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	// Check whether any Node from Cluster has already connected or not.
-	// If yes, Cluster should be present in the cache
-	// If not, add Cluster <-> Node <-> Stream object in cache
-	if _, exist := s.lrsCache[clusterName]; !exist {
-		// Add all Nodes and its stream objects into the cache
-		log.Printf("Adding new cluster to cache `%s` with node `%s`", clusterName, nodeID)
-		s.lrsCache[clusterName] = make(map[string]NodeMetadata)
-		s.lrsCache[clusterName][nodeID] = NodeMetadata{
-			node:   request.GetNode(),
-			stream: stream,
-		}
-		return
-	}
+	// Check whether any Node has already connected or not.
+	// If not, add the NodeID to nodesConnected and enable Load Report with given frequency
+	// If yes, log stats
+	if _, exist := s.nodesConnected[nodeID]; !exist {
+		// Add NodeID to the nodesConnected
+		log.Printf("Adding new new node to cache `%s`", nodeID)
+		s.nodesConnected[nodeID] = true
 
-	if _, exist := s.lrsCache[clusterName][nodeID]; !exist {
-		// Add remaining Nodes of a Cluster and its stream objects into the cache
-		log.Printf("Adding new node `%s` to existing cluster `%s`", nodeID, clusterName)
-		s.lrsCache[clusterName][nodeID] = NodeMetadata{
-			node:   request.GetNode(),
-			stream: stream,
+		// Initialize Load Reporting
+		err := stream.Send(&gcpLoadStats.LoadStatsResponse{
+			Clusters:                  []string{"local_service"},
+			LoadReportingInterval:     &duration.Duration{Seconds: StatsFrequencyInSeconds},
+			ReportEndpointGranularity: true,
+		})
+		if err != nil {
+			log.Panicf("Unable to send response to node %s due to err: %s", nodeID, err)
 		}
 		return
 	}
@@ -90,32 +72,6 @@ func (s *server) HandleRequest(stream gcpLoadStats.LoadReportingService_StreamLo
 	for _, clusterStats := range request.ClusterStats {
 		if len(clusterStats.UpstreamLocalityStats) > 0 {
 			log.Printf("Got stats from cluster `%s` node `%s` - %s", request.Node.Cluster, request.Node.Id, clusterStats)
-		}
-	}
-}
-
-// Initialize Load Reporting for a given cluster to a list of UpStreamClusters
-func (s *server) SendResponse(cluster string, upstreamClusters []string, frequency int64) {
-	s.mu.Lock()
-	// Check whether any Node from given Cluster is connected or not.
-	clusterDetails, exist := s.lrsCache[cluster]
-	s.mu.Unlock()
-	if !exist {
-		log.Printf("Cannot send response as cluster `%s` because is not connected", cluster)
-		return
-	}
-
-	// To enable Load Report, send LoadStatsResponse to all Nodes within a Cluster
-	for nodeID, nodeDetails := range clusterDetails {
-		log.Printf("Creating LRS response for cluster %s, node %s with frequency %d secs", nodeDetails.node.Cluster, nodeID, frequency)
-		err := nodeDetails.stream.Send(&gcpLoadStats.LoadStatsResponse{
-			Clusters:                  upstreamClusters,
-			LoadReportingInterval:     &duration.Duration{Seconds: frequency},
-			ReportEndpointGranularity: true,
-		})
-
-		if err != nil {
-			log.Panicf("Unable to send response to cluster %s node %s due to err: %s", nodeDetails.node.Cluster, nodeID, err)
 		}
 	}
 }
