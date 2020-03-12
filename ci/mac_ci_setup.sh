ef5c6045744565536c6448f489fc32dd0449fbf2
Sunjay Bhatia <sbhatia@pivotal.io>
2020-03-12 14:05:15 -0400

    Force install homebrew packages
    So bazelisk can overwrite pre-installed bazel
    
    Co-authored-by: Sunjay Bhatia <sbhatia@pivotal.io>
    Co-authored-by: William A Rowe Jr <wrowe@pivotal.io>
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>
    Signed-off-by: William A Rowe Jr <wrowe@pivotal.io>

--- ci/mac_ci_setup.sh
+++ ci/mac_ci_setup.sh
@@ -15,7 +15,7 @@ function is_installed {
 
 function install {
     echo "Installing $1"
-    brew install "$1"
+    brew install --force "$1"
     if ! brew link --overwrite "$1"; then
         echo "Failed to install and link $1"
         exit 1
