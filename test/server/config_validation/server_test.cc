0af0d8e8307523aec91efe9e2ebd5aaa446fd8a3
Kathan <5263542+kathan24@users.noreply.github.com>
2020-03-12 11:09:21 -0700

    Lrs example (#9920)
    Signed-off-by: kathan24 <kshah@lyft.com>
    Signed-off-by: Kathan <5263542+kathan24@users.noreply.github.com>

--- test/server/config_validation/server_test.cc
+++ test/server/config_validation/server_test.cc
@@ -82,8 +82,9 @@ INSTANTIATE_TEST_SUITE_P(ValidConfigs, ValidationServerTest,
 // Just make sure that all configs can be ingested without a crash. Processing of config files
 // may not be successful, but there should be no crash.
 TEST_P(ValidationServerTest_1, RunWithoutCrash) {
-  validateConfig(options_, Network::Address::InstanceConstSharedPtr(), component_factory_,
-                 Thread::threadFactoryForTest(), Filesystem::fileSystemForTest());
+  auto local_address = Network::Utility::getLocalAddress(options_.localAddressIpVersion());
+  validateConfig(options_, local_address, component_factory_, Thread::threadFactoryForTest(),
+                 Filesystem::fileSystemForTest());
   SUCCEED();
 }
 
