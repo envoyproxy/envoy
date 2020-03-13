f9dd0d2e3d87599737864d8c4ff26e2b0fbe8677
Joshua Marantz <jmarantz@google.com>
2020-03-13 13:36:03 -0400

    stats: Predefine symbols needed for user agent stats (#10302)
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/common/http/conn_manager_impl_test.cc
+++ test/common/http/conn_manager_impl_test.cc
@@ -89,10 +89,9 @@ public:
                 access_log_path_, {}, AccessLog::AccessLogFormatUtils::defaultAccessLogFormatter(),
                 log_manager_)}},
         codec_(new NiceMock<MockServerConnection>()),
-        stats_{{ALL_HTTP_CONN_MAN_STATS(POOL_COUNTER(fake_stats_), POOL_GAUGE(fake_stats_),
+        stats_({ALL_HTTP_CONN_MAN_STATS(POOL_COUNTER(fake_stats_), POOL_GAUGE(fake_stats_),
                                         POOL_HISTOGRAM(fake_stats_))},
-               "",
-               fake_stats_},
+               "", fake_stats_),
         tracing_stats_{CONN_MAN_TRACING_STATS(POOL_COUNTER(fake_stats_))},
         listener_stats_{CONN_MAN_LISTENER_STATS(POOL_COUNTER(fake_listener_stats_))} {
 
