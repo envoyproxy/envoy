e7fc5fc19a4208ac364c3c300c6ed23e0ad5ca7b
William A. Rowe Jr <wrowe@pivotal.io>
2020-03-14 01:04:30 -0500

    Enable Windows CI //source/exe:envoy-static build (#10293)
    Description:
     - Build and CI tooling to get windows compiling
     - (Reintroduces an experimental ci/windows_build_steps.ps1)
     - Source changes required for Windows compilation on msvc
     - Pick up a flavor of nameser.h for Windows only from c-ares dependency
     - Excludes all source fixes unrelated to compilation passing on Windows
    
    Risk Level: Low
    Testing: local on linux/gcc windows/msvc
    Docs Changes: n/a
    Release Notes: n/a
    
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>
    Signed-off-by: William A Rowe Jr <wrowe@pivotal.io>

--- source/server/listener_manager_impl.cc
+++ source/server/listener_manager_impl.cc
@@ -184,10 +184,6 @@ Network::SocketSharedPtr ProdListenerComponentFactory::createListenSocket(
   // For each listener config we share a single socket among all threaded listeners.
   // First we try to get the socket from our parent if applicable.
   if (address->type() == Network::Address::Type::Pipe) {
-// No such thing as AF_UNIX on Windows
-#ifdef WIN32
-    throw EnvoyException("network type pipe not supported on Windows");
-#else
     if (socket_type != Network::Address::SocketType::Stream) {
       // This could be implemented in the future, since Unix domain sockets
       // support SOCK_DGRAM, but there would need to be a way to specify it in
@@ -203,7 +199,6 @@ Network::SocketSharedPtr ProdListenerComponentFactory::createListenSocket(
       return std::make_shared<Network::UdsListenSocket>(std::move(io_handle), address);
     }
     return std::make_shared<Network::UdsListenSocket>(address);
-#endif
   }
 
   const std::string scheme = (socket_type == Network::Address::SocketType::Stream)
