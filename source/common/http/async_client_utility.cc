cdc881fed9fb5b3960062cbc12e3dff6be025be7
Yaroslav Skopets <yaroslav@tetrate.io>
2020-03-18 01:26:30 +0100

    code review: add comments
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>

--- source/common/http/async_client_utility.cc
+++ source/common/http/async_client_utility.cc
@@ -15,6 +15,9 @@ void AsyncClientRequestTracker::add(AsyncClient::Request& request) {
 }
 
 void AsyncClientRequestTracker::remove(const AsyncClient::Request& request) {
+  // Notice that use of "const_cast" here is motivated by keeping API convenient for client code.
+  // In the context where remove() will be typically called, request.cancel() is no longer
+  // desirable and therefore get prevented by means of "const" modifier.
   auto it = active_requests_.find(const_cast<AsyncClient::Request*>(&request));
   // Support a use case where request callbacks might get called prior to a request handle
   // is returned from AsyncClient::send().
--- source/common/http/async_client_utility.h
+++ source/common/http/async_client_utility.h
@@ -21,6 +21,15 @@ public:
   void add(AsyncClient::Request& request);
   /**
    * Excludes a given async HTTP request from a set of known active requests.
+   *
+   * NOTE: Asymmetry between signatures of add() and remove() is caused by the difference
+   *       between contexts in which these methods will be used.
+   *       add() will be called right after AsyncClient::send() when request.cancel() is
+   *       perfectly valid and desirable.
+   *       However, remove() will be called in the context of
+   *       AsyncClient::Callbacks::[onSuccess | onFailure] where request.cancel() is no longer
+   *       expected and therefore get prevented by means of "const" modifier.
+   *
    * @param request request handle
    */
   void remove(const AsyncClient::Request& request);
