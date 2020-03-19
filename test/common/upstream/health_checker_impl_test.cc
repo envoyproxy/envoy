af655f4ade29a3247241720483378268605aff0e
Yuval Kohavi <yuval.kohavi@gmail.com>
2020-03-19 14:46:41 -0400

    format
    Signed-off-by: Yuval Kohavi <yuval.kohavi@gmail.com>

--- test/common/upstream/health_checker_impl_test.cc
+++ test/common/upstream/health_checker_impl_test.cc
@@ -1298,7 +1298,7 @@ TEST_F(HttpHealthCheckerImplTest,
       envoy::config::core::v3::Locality(), health_check_config, 0,
       envoy::config::core::v3::UNKNOWN);
   const std::string path = "/healthcheck";
-  // setup health check config with a differnet host, to check that we still get hte host configured
+  // setup health check config with a different host, to check that we still get the host configured
   // on the endpoint.
   setupServiceValidationWithCustomHostValueHC("foo.com");
   // requires non-empty `service_name` in config.
