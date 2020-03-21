871acf4a845e323d5e6ed6729c59a7c31e577998
Ruslan Nigmatullin <elessar@dropbox.com>
2020-03-20 18:01:30 -0700

    more uuid-impl-specific tests
    Signed-off-by: Ruslan Nigmatullin <elessar@dropbox.com>

--- test/common/request_id_extension/uuid_impl_test.cc
+++ test/common/request_id_extension/uuid_impl_test.cc
@@ -4,18 +4,80 @@
 #include "common/request_id_extension/uuid_impl.h"
 
 #include "test/test_common/utility.h"
+#include "test/mocks/runtime/mocks.h"
 
 #include "gtest/gtest.h"
+
+using testing::Return;
 
 namespace Envoy {
 namespace RequestIDExtension {
 
-TEST(UUIDUtilsTest, mod) {
+TEST(UUIDUtilsTest, SetRequestID) {
+  testing::StrictMock<Runtime::MockRandomGenerator> random;
+  RequestIDExtension::UUIDUtils uuid_utils(random);
+  Http::TestRequestHeaderMapImpl request_headers;
+
+  EXPECT_CALL(random, uuid()).Times(1).WillOnce(Return("first-request-id"));
+  uuid_utils.setRequestID(request_headers);
+  EXPECT_EQ("first-request-id", request_headers.get_(Http::Headers::get().RequestId));
+
+  EXPECT_CALL(random, uuid()).Times(1).WillOnce(Return("second-request-id"));
+  uuid_utils.setRequestID(request_headers);
+  EXPECT_EQ("second-request-id", request_headers.get_(Http::Headers::get().RequestId));
+}
+
+TEST(UUIDUtilsTest, EnsureRequestID) {
+  testing::StrictMock<Runtime::MockRandomGenerator> random;
+  RequestIDExtension::UUIDUtils uuid_utils(random);
+  Http::TestRequestHeaderMapImpl request_headers;
+
+  EXPECT_CALL(random, uuid()).Times(1).WillOnce(Return("first-request-id"));
+  uuid_utils.ensureRequestID(request_headers);
+  EXPECT_EQ("first-request-id", request_headers.get_(Http::Headers::get().RequestId));
+
+  EXPECT_CALL(random, uuid()).Times(0);
+  uuid_utils.ensureRequestID(request_headers);
+  EXPECT_EQ("first-request-id", request_headers.get_(Http::Headers::get().RequestId));
+}
+
+TEST(UUIDUtilsTest, PreserveRequestIDInResponse) {
+  testing::StrictMock<Runtime::MockRandomGenerator> random;
+  RequestIDExtension::UUIDUtils uuid_utils(random);
+  Http::TestRequestHeaderMapImpl request_headers;
+  Http::TestResponseHeaderMapImpl response_headers;
+
+  uuid_utils.preserveRequestIDInResponse(response_headers, request_headers);
+  EXPECT_EQ(nullptr, response_headers.get(Http::Headers::get().RequestId));
+
+  request_headers.setRequestId("some-request-id");
+  uuid_utils.preserveRequestIDInResponse(response_headers, request_headers);
+  EXPECT_EQ("some-request-id", response_headers.get_(Http::Headers::get().RequestId));
+
+  request_headers.removeRequestId();
+  response_headers.setRequestId("another-request-id");
+  uuid_utils.preserveRequestIDInResponse(response_headers, request_headers);
+  EXPECT_EQ("another-request-id", response_headers.get_(Http::Headers::get().RequestId));
+
+  request_headers.setRequestId("");
+  uuid_utils.preserveRequestIDInResponse(response_headers, request_headers);
+  EXPECT_EQ("", response_headers.get_(Http::Headers::get().RequestId));
+}
+
+TEST(UUIDUtilsTest, ModRequestIDBy) {
   Runtime::RandomGeneratorImpl random;
   RequestIDExtension::UUIDUtils uuid_utils(random);
   Http::TestRequestHeaderMapImpl request_headers;
 
   uint64_t result;
+  EXPECT_FALSE(uuid_utils.modRequestIDBy(request_headers, result, 10000));
+
+  request_headers.setRequestId("fffffff");
+  EXPECT_FALSE(uuid_utils.modRequestIDBy(request_headers, result, 10000));
+
+  request_headers.setRequestId("fffffffz-0012-0110-00ff-0c00400600ff");
+  EXPECT_FALSE(uuid_utils.modRequestIDBy(request_headers, result, 10000));
+
   request_headers.setRequestId("00000000-0000-0000-0000-000000000000");
   EXPECT_TRUE(uuid_utils.modRequestIDBy(request_headers, result, 100));
   EXPECT_EQ(0, result);
@@ -52,7 +114,7 @@ TEST(UUIDUtilsTest, mod) {
   EXPECT_EQ(7295, result);
 }
 
-TEST(UUIDUtilsTest, checkDistribution) {
+TEST(UUIDUtilsTest, RequestIDModDistribution) {
   Runtime::RandomGeneratorImpl random;
   RequestIDExtension::UUIDUtils uuid_utils(random);
   Http::TestRequestHeaderMapImpl request_headers;
@@ -90,7 +152,7 @@ TEST(UUIDUtilsTest, DISABLED_benchmark) {
   }
 }
 
-TEST(UUIDUtilsTest, setAndCheckTraceable) {
+TEST(UUIDUtilsTest, SetTraceStatus) {
   Runtime::RandomGeneratorImpl random;
   RequestIDExtension::UUIDUtils uuid_utils(random);
   Http::TestRequestHeaderMapImpl request_headers;
