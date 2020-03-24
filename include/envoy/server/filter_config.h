08590076337d561e18acc494d80921300f89e2a8
Yuchen Dai <silentdai@gmail.com>
2020-03-24 19:43:26 +0000

    amend
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- include/envoy/server/filter_config.h
+++ include/envoy/server/filter_config.h
@@ -119,7 +119,7 @@ public:
   virtual Grpc::Context& grpcContext() PURE;
 
   /** 
-   * @return DrainManager& the server-wide singleton manager.
+   * @return DrainManager& the server-wide drain manager.
    */
   virtual Envoy::Server::DrainManager& drainManager() PURE;
 };
