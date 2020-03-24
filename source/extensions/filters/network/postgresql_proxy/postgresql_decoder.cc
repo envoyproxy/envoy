b584eada341b81b08b785f5a3f892e65d0445889
Fabrízio de Royes Mello <fabrizio@ongres.com>
2020-03-24 11:54:09 -0300

    Added all Backend/Frontend known commands and some minor code cleanup.
    Signed-off-by: Fabrízio de Royes Mello <fabrizio@ongres.com>

--- source/extensions/filters/network/postgresql_proxy/postgresql_decoder.cc
+++ source/extensions/filters/network/postgresql_proxy/postgresql_decoder.cc
@@ -11,33 +11,64 @@ namespace PostgreSQLProxy {
 
 void DecoderImpl::initialize() {
   // Special handler for first message of the transaction
-  first_ = Message{"Initial", {}};
+  first_ = Message{"Startup", {}};
 
   // Frontend messages
   std::get<0>(FEmessages_) = "Frontend";
+
   // Setup handlers for known messages
   absl::flat_hash_map<char, Message>& FE_known_msgs = std::get<1>(FEmessages_);
-  FE_known_msgs['Q'] = Message{"Q(Query)", {&DecoderImpl::incFrontend}};
-  FE_known_msgs['P'] = Message{"P(Parse)", {&DecoderImpl::incFrontend}};
-  FE_known_msgs['B'] = Message{"B(Bind)", {&DecoderImpl::incFrontend}};
+
+  // Handler for know messages
+  FE_known_msgs['B'] = Message{"Bind", {&DecoderImpl::incFrontend}};
+  FE_known_msgs['F'] = Message{"Close", {&DecoderImpl::incFrontend}};
+  FE_known_msgs['d'] = Message{"CopyData", {}};
+  FE_known_msgs['c'] = Message{"CopyDone", {}};
+  FE_known_msgs['f'] = Message{"CopyFail", {&DecoderImpl::incFrontend}};
+  FE_known_msgs['D'] = Message{"Describe", {&DecoderImpl::incFrontend}};
+  FE_known_msgs['E'] = Message{"Execute", {&DecoderImpl::incFrontend}};
+  FE_known_msgs['H'] = Message{"Flush", {&DecoderImpl::incFrontend}};
+  FE_known_msgs['F'] = Message{"FunctionCall", {&DecoderImpl::incFrontend}};
+  FE_known_msgs['p'] = Message{"GSSResponse", {&DecoderImpl::incFrontend}};
+  FE_known_msgs['P'] = Message{"Parse", {&DecoderImpl::incFrontend}};
+  FE_known_msgs['p'] = Message{"PasswordMessage", {&DecoderImpl::incFrontend}};
+  FE_known_msgs['Q'] = Message{"Query", {&DecoderImpl::incFrontend}};
+  FE_known_msgs['S'] = Message{"Sync", {&DecoderImpl::incFrontend}};
+  FE_known_msgs['X'] = Message{"Terminate", {&DecoderImpl::decodeFrontendTerminate}};
+
   // Handler for unknown messages
   std::get<2>(FEmessages_) = Message{"Other", {&DecoderImpl::incUnrecognized}};
 
   // Backend messages
   std::get<0>(BEmessages_) = "Backend";
+
   // Setup handlers for known messages
   absl::flat_hash_map<char, Message>& BE_known_msgs = std::get<1>(BEmessages_);
-  BE_known_msgs['C'] = Message{"C(CommandComplete)", {&DecoderImpl::decodeBackendStatements}};
-  BE_known_msgs['2'] = Message{"2(BindComplete)", {&DecoderImpl::decodeBackendStatements}};
-  BE_known_msgs['T'] = Message{"T(RowDescription)", {&DecoderImpl::decodeBackendRowDescription}};
-  BE_known_msgs['1'] =
-      Message{"1(ParseComplete)", {&DecoderImpl::incStatements, &DecoderImpl::incStatementsOther}};
-  BE_known_msgs['R'] = Message{"R(Authentication)", {&DecoderImpl::onAuthentication}};
-  BE_known_msgs['E'] = Message{"E(ErrorResponse)", {&DecoderImpl::decodeBackendErrorResponse}};
-  BE_known_msgs['N'] = Message{"N(NoticeResponse)", {&DecoderImpl::decodeBackendNoticeResponse}};
-  // messages for which there is no action, but we do not want them to go into Unrecognized category
-  BE_known_msgs['S'] = Message{"S(ParameterStatus)", {}};
-  BE_known_msgs['Z'] = Message{"Z(ReadyForQuery)", {}};
+
+  // Handler for know messages
+  BE_known_msgs['R'] = Message{"Authentication", {&DecoderImpl::decodeAuthentication}};
+  BE_known_msgs['K'] = Message{"BackendKeyData", {}};
+  BE_known_msgs['2'] = Message{"BindComplete", {}};
+  BE_known_msgs['3'] = Message{"CloseComplete", {}};
+  BE_known_msgs['C'] = Message{"CommandComplete", {&DecoderImpl::decodeBackendStatements}};
+  BE_known_msgs['d'] = Message{"CopyData", {}};
+  BE_known_msgs['c'] = Message{"CopyDone", {}};
+  BE_known_msgs['G'] = Message{"CopyInResponse", {}};
+  BE_known_msgs['H'] = Message{"CopyOutResponse", {}};
+  BE_known_msgs['D'] = Message{"DataRow", {}};
+  BE_known_msgs['I'] = Message{"EmptyQueryResponse", {}};
+  BE_known_msgs['E'] = Message{"ErrorResponse", {&DecoderImpl::decodeBackendErrorResponse}};
+  BE_known_msgs['V'] = Message{"FunctionCallResponse", {}};
+  BE_known_msgs['v'] = Message{"NegotiateProtocolVersion", {}};
+  BE_known_msgs['n'] = Message{"NoData", {}};
+  BE_known_msgs['N'] = Message{"NoticeResponse", {&DecoderImpl::decodeBackendNoticeResponse}};
+  BE_known_msgs['A'] = Message{"NotificationResponse", {}};
+  BE_known_msgs['t'] = Message{"ParameterDescription", {}};
+  BE_known_msgs['S'] = Message{"ParameterStatus", {}};
+  BE_known_msgs['1'] = Message{"ParseComplete", {}};
+  BE_known_msgs['s'] = Message{"PortalSuspend", {}};
+  BE_known_msgs['Z'] = Message{"ReadyForQuery", {}};
+  BE_known_msgs['T'] = Message{"RowDescription", {}};
 
   // Handler for unknown messages
   std::get<2>(BEmessages_) = Message{"Other", {&DecoderImpl::incUnrecognized}};
@@ -52,7 +83,7 @@ bool DecoderImpl::parseMessage(Buffer::Instance& data) {
     return false;
   }
 
-  if (!initial_) {
+  if (!startup_) {
     char com;
     data.copyOut(0, 1, &com);
     ENVOY_LOG(trace, "postgresql_proxy: command is {}", com);
@@ -63,22 +94,21 @@ bool DecoderImpl::parseMessage(Buffer::Instance& data) {
   std::string cmd;
   std::string message;
   uint32_t length;
-  data.copyOut(initial_ ? 0 : 1, 4, &length);
+  data.copyOut(startup_ ? 0 : 1, 4, &length);
   length = ntohl(length);
-  if (data.length() < (length + (initial_ ? 0 : 1))) {
+  if (data.length() < (length + (startup_ ? 0 : 1))) {
     ENVOY_LOG(trace, "postgresql_proxy: cannot parse message. Need {} bytes in buffer",
-              length + (initial_ ? 0 : 1));
+              length + (startup_ ? 0 : 1));
     // not enough data in the buffer
     return false;
   }
 
   setMessageLength(length);
 
-  if (!initial_) {
+  if (!startup_) {
     BufferHelper::readStringBySize(data, 1, cmd);
     command_ = cmd[0];
   }
-  // setCommand(cmd);
 
   data.drain(4); // this is length which we already know.
 
@@ -101,9 +131,9 @@ bool DecoderImpl::onData(Buffer::Instance& data, bool frontend) {
 
   std::reference_wrapper<Message> msg = std::get<2>(msg_processor);
   ;
-  if (initial_) {
+  if (startup_) {
     msg = std::ref(first_);
-    initial_ = false;
+    startup_ = false;
   } else {
     auto it = std::get<1>(msg_processor).find(command_);
     if (it != std::get<1>(msg_processor).end()) {
@@ -116,7 +146,7 @@ bool DecoderImpl::onData(Buffer::Instance& data, bool frontend) {
     action(this);
   }
 
-  ENVOY_LOG(debug, "{} command = {}", std::get<0>(msg_processor), std::get<0>(msg.get()));
+  ENVOY_LOG(debug, "{} command = {} ({})", std::get<0>(msg_processor), command_, std::get<0>(msg.get()));
   ENVOY_LOG(debug, "{} length = {}", std::get<0>(msg_processor), getMessageLength());
   ENVOY_LOG(debug, "{} message = {}", std::get<0>(msg_processor), getMessage());
 
@@ -141,6 +171,9 @@ void DecoderImpl::decodeBackendStatements() {
     callbacks_->incStatementsOther();
     session_.setInTransaction(false);
     callbacks_->incTransactionsCommit();
+  } else if (getMessage().find("SELECT") != std::string::npos) {
+    callbacks_->incStatementsSelect();
+    callbacks_->incTransactionsCommit();
   } else if (getMessage().find("INSERT") != std::string::npos) {
     callbacks_->incStatementsInsert();
     callbacks_->incTransactionsCommit();
@@ -155,13 +188,22 @@ void DecoderImpl::decodeBackendStatements() {
     callbacks_->incTransactionsCommit();
   }
 }
-void DecoderImpl::incFrontend() { callbacks_->incFrontend(); }
 
+void DecoderImpl::decodeFrontendTerminate() {
+  DecoderImpl::incFrontend();
+  if (session_.inTransaction()) {
+    session_.setInTransaction(false);
+    callbacks_->incTransactionsRollback();
+  }
+}
+
+void DecoderImpl::incFrontend() { callbacks_->incFrontend(); }
 void DecoderImpl::incUnrecognized() { callbacks_->incUnrecognized(); }
 void DecoderImpl::incStatements() { callbacks_->incStatements(); }
 void DecoderImpl::incStatementsOther() { callbacks_->incStatementsOther(); }
 void DecoderImpl::incSessions() { callbacks_->incSessions(); }
-void DecoderImpl::onAuthentication() {
+
+void DecoderImpl::decodeAuthentication() {
   // check if auth message indicates successful authentication
   // Length must be 8 and payload must be 0
   if ((8 == message_len_) && (0 == *(reinterpret_cast<const uint32_t*>(message_.data())))) {
@@ -179,12 +221,6 @@ void DecoderImpl::decodeBackendNoticeResponse() {
   if (getMessage().find("VWARNING") != std::string::npos) {
     callbacks_->incWarnings();
   }
-}
-
-void DecoderImpl::decodeBackendRowDescription() {
-  callbacks_->incStatements();
-  callbacks_->incStatementsSelect();
-  callbacks_->incTransactionsCommit();
 }
 
 } // namespace PostgreSQLProxy
--- test/extensions/filters/network/postgresql_proxy/sql/errors_and_warnings.sql
+++ test/extensions/filters/network/postgresql_proxy/sql/errors_and_warnings.sql
@@ -9,6 +9,7 @@ COMMIT;                      -- (+) statements, statements_other and transaction
 
 /*
 postgresql.egress_postgresql.errors: 1
+postgresql.egress_postgresql.frontend_commands: 6
 postgresql.egress_postgresql.sessions: 1
 postgresql.egress_postgresql.statements: 5
 postgresql.egress_postgresql.statements_delete: 0
@@ -19,27 +20,30 @@ postgresql.egress_postgresql.statements_update: 0
 postgresql.egress_postgresql.transactions: 1
 postgresql.egress_postgresql.transactions_commit: 0
 postgresql.egress_postgresql.transactions_rollback: 1
+postgresql.egress_postgresql.unrecognized: 0
 postgresql.egress_postgresql.warnings: 0
 */
 
 /* WARNING:  there is already a transaction in progress */
 BEGIN;     -- (+) statements, statements_other and transactions
-BEGIN;     -- (+) warnings
+BEGIN;     -- (+) statements and warnings
 SELECT 1;  -- (+) statements and statements_select
 COMMIT;    -- (+) statements, statements_other and transactions_commit
 
 /*
-postgresql.egress_postgresql.errors: 1
-postgresql.egress_postgresql.sessions: 2
-postgresql.egress_postgresql.statements: 8
+postgresql.egress_postgresql.errors: 0
+postgresql.egress_postgresql.frontend_commands: 4
+postgresql.egress_postgresql.sessions: 1
+postgresql.egress_postgresql.statements: 4
 postgresql.egress_postgresql.statements_delete: 0
 postgresql.egress_postgresql.statements_insert: 0
-postgresql.egress_postgresql.statements_other: 6
-postgresql.egress_postgresql.statements_select: 2
+postgresql.egress_postgresql.statements_other: 3
+postgresql.egress_postgresql.statements_select: 1
 postgresql.egress_postgresql.statements_update: 0
-postgresql.egress_postgresql.transactions: 2
+postgresql.egress_postgresql.transactions: 1
 postgresql.egress_postgresql.transactions_commit: 1
-postgresql.egress_postgresql.transactions_rollback: 1
+postgresql.egress_postgresql.transactions_rollback: 0
+postgresql.egress_postgresql.unrecognized: 0
 postgresql.egress_postgresql.warnings: 1
 */
 
@@ -50,16 +54,18 @@ SELECT 1+1;  -- (+) statements and statements_select
 ROLLBACK;    -- (+) statements, statements_other and transactions_rollback
 
 /*
-postgresql.egress_postgresql.errors: 1
+postgresql.egress_postgresql.errors: 0
+postgresql.egress_postgresql.frontend_commands: 3
 postgresql.egress_postgresql.sessions: 1
-postgresql.egress_postgresql.statements: 12
+postgresql.egress_postgresql.statements: 3
 postgresql.egress_postgresql.statements_delete: 0
 postgresql.egress_postgresql.statements_insert: 0
-postgresql.egress_postgresql.statements_other: 9
-postgresql.egress_postgresql.statements_select: 3
+postgresql.egress_postgresql.statements_other: 2
+postgresql.egress_postgresql.statements_select: 1
 postgresql.egress_postgresql.statements_update: 0
-postgresql.egress_postgresql.transactions: 3
-postgresql.egress_postgresql.transactions_commit: 1
-postgresql.egress_postgresql.transactions_rollback: 2
-postgresql.egress_postgresql.warnings: 1
+postgresql.egress_postgresql.transactions: 1
+postgresql.egress_postgresql.transactions_commit: 0
+postgresql.egress_postgresql.transactions_rollback: 1
+postgresql.egress_postgresql.unrecognized: 0
+postgresql.egress_postgresql.warnings: 0
 */
