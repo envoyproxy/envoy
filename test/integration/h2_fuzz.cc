14bb67729ae651d36a1ea1c6a5810b1e3ebb8d68
Adi Suissa-Peleg <adip@google.com>
2020-03-24 22:40:40 -0400

    Changing h2 integration fuzz test by specifically describing messages for each type of frame in the proto file, and adding their possible inner fields
    Signed-off-by: Adi Suissa-Peleg <adip@google.com>

--- test/integration/h2_fuzz.cc
+++ test/integration/h2_fuzz.cc
@@ -6,12 +6,92 @@
 #include "common/common/base64.h"
 #include "common/common/logger.h"
 
-//#include "test/integration/http_integration.h"
 #include "test/test_common/environment.h"
 
 namespace Envoy {
 
 using namespace Envoy::Http::Http2;
+
+void H2FuzzIntegrationTest::sendFrame(const test::integration::H2TestFrame& proto_frame,
+                                      std::function<void(const Http2Frame&)> write_func) {
+  Http2Frame h2_frame;
+  switch (proto_frame.frame_type_case()) {
+  case test::integration::H2TestFrame::kPing:
+    ENVOY_LOG_MISC(trace, "Sending ping frame");
+    h2_frame = Http2Frame::makePingFrame(proto_frame.ping().data());
+    break;
+  case test::integration::H2TestFrame::kSettings: {
+    const Http2Frame::SettingsFlags settings_flags =
+        static_cast<Http2Frame::SettingsFlags>(proto_frame.settings().flags());
+    ENVOY_LOG_MISC(trace, "Sending settings frame");
+    h2_frame = Http2Frame::makeEmptySettingsFrame(settings_flags);
+  } break;
+  case test::integration::H2TestFrame::kHeaders: {
+    const Http2Frame::HeadersFlags headers_flags =
+        static_cast<Http2Frame::HeadersFlags>(proto_frame.headers().flags());
+    uint32_t stream_idx = proto_frame.headers().stream_index();
+    ENVOY_LOG_MISC(trace, "Sending headers frame");
+    h2_frame = Http2Frame::makeEmptyHeadersFrame(stream_idx, headers_flags);
+  } break;
+  case test::integration::H2TestFrame::kContinuation: {
+    const Http2Frame::HeadersFlags headers_flags =
+        static_cast<Http2Frame::HeadersFlags>(proto_frame.continuation().flags());
+    uint32_t stream_idx = proto_frame.continuation().stream_index();
+    ENVOY_LOG_MISC(trace, "Sending continuation frame");
+    h2_frame = Http2Frame::makeEmptyContinuationFrame(stream_idx, headers_flags);
+  } break;
+  case test::integration::H2TestFrame::kData: {
+    const Http2Frame::DataFlags data_flags =
+        static_cast<Http2Frame::DataFlags>(proto_frame.data().flags());
+    uint32_t stream_idx = proto_frame.data().stream_index();
+    ENVOY_LOG_MISC(trace, "Sending data frame");
+    h2_frame = Http2Frame::makeEmptyDataFrame(stream_idx, data_flags);
+  } break;
+  case test::integration::H2TestFrame::kPriority: {
+    uint32_t stream_idx = proto_frame.priority().stream_index();
+    uint32_t dependent_idx = proto_frame.priority().dependent_index();
+    ENVOY_LOG_MISC(trace, "Sending priority frame");
+    h2_frame = Http2Frame::makePriorityFrame(stream_idx, dependent_idx);
+  } break;
+  case test::integration::H2TestFrame::kWindowUpdate: {
+    uint32_t stream_idx = proto_frame.window_update().stream_index();
+    uint32_t increment = proto_frame.window_update().increment();
+    ENVOY_LOG_MISC(trace, "Sending windows_update frame");
+    h2_frame = Http2Frame::makeWindowUpdateFrame(stream_idx, increment);
+  } break;
+  case test::integration::H2TestFrame::kMalformedRequest: {
+    uint32_t stream_idx = proto_frame.malformed_request().stream_index();
+    ENVOY_LOG_MISC(trace, "Sending malformed_request frame");
+    h2_frame = Http2Frame::makeMalformedRequest(stream_idx);
+  } break;
+  case test::integration::H2TestFrame::kMalformedRequestWithZerolenHeader: {
+    uint32_t stream_idx = proto_frame.malformed_request_with_zerolen_header().stream_index();
+    absl::string_view host = proto_frame.malformed_request_with_zerolen_header().host();
+    absl::string_view path = proto_frame.malformed_request_with_zerolen_header().path();
+    ENVOY_LOG_MISC(trace, "Sending malformed_request_with_zerolen_header");
+    h2_frame = Http2Frame::makeMalformedRequestWithZerolenHeader(stream_idx, host, path);
+  } break;
+  case test::integration::H2TestFrame::kRequest: {
+    uint32_t stream_idx = proto_frame.request().stream_index();
+    absl::string_view host = proto_frame.request().host();
+    absl::string_view path = proto_frame.request().path();
+    ENVOY_LOG_MISC(trace, "Sending request");
+    h2_frame = Http2Frame::makeRequest(stream_idx, host, path);
+  } break;
+  case test::integration::H2TestFrame::kPostRequest: {
+    uint32_t stream_idx = proto_frame.post_request().stream_index();
+    absl::string_view host = proto_frame.post_request().host();
+    absl::string_view path = proto_frame.post_request().path();
+    ENVOY_LOG_MISC(trace, "Sending post request");
+    h2_frame = Http2Frame::makePostRequest(stream_idx, host, path);
+  } break;
+  default:
+    ENVOY_LOG_MISC(debug, "Proto-frame not supported!");
+    break;
+  }
+
+  write_func(h2_frame);
+}
 
 void H2FuzzIntegrationTest::replay(const test::integration::H2CaptureFuzzTestCase& input) {
   initialize();
@@ -32,26 +112,26 @@ void H2FuzzIntegrationTest::replay(const test::integration::H2CaptureFuzzTestCas
     }
     switch (event.event_selector_case()) {
     // TODO(adip): Add ability to test complex interactions when receiving data
-    case test::integration::Event::kDownstreamSendEvent:
+    case test::integration::Event::kDownstreamSendEvent: {
+      auto downstream_write_func = [&](const Http2Frame& h2_frame) -> void {
+        tcp_client->write(std::string(h2_frame), false, false);
+      };
       // Start H2 session - send hello string
       tcp_client->write(Http2Frame::Preamble, false, false);
-      for (auto& frame_func_idx : event.downstream_send_event().frames()) {
+      for (auto& frame : event.downstream_send_event().h2_frames()) {
         if (!tcp_client->connected()) {
           ENVOY_LOG_MISC(debug,
                          "Disconnected, avoiding sending data, no further event processing.");
           break;
         }
 
-        const Http2Frame frame = frameMakers[frame_func_idx]();
-        ENVOY_LOG_MISC(trace, "sending downstream frame idx: {}", frame_func_idx);
-        tcp_client->write(std::string(frame), false, false);
+        ENVOY_LOG_MISC(trace, "sending downstream frame");
+        sendFrame(frame, downstream_write_func);
       }
-      break;
+    } break;
     case test::integration::Event::kUpstreamSendEvent:
       if (fake_upstream_connection == nullptr) {
-        // TODO(adip): The H1 fuzzer uses a short max_wait_ms_ when calling this function. When
-        // trying to do so with H2, an ABORT happens in FileEventImpl::assignEvents
-        if (!fake_upstreams_[0]->waitForRawConnection(fake_upstream_connection)) {
+        if (!fake_upstreams_[0]->waitForRawConnection(fake_upstream_connection, max_wait_ms_)) {
           // If we timed out, we fail out.
           if (tcp_client->connected()) {
             tcp_client->close();
@@ -68,18 +148,23 @@ void H2FuzzIntegrationTest::replay(const test::integration::H2CaptureFuzzTestCas
         stop_further_inputs = true;
         break;
       }
-      for (auto& frame_func_idx : event.upstream_send_event().frames()) {
-        if (!fake_upstream_connection->connected()) {
-          ENVOY_LOG_MISC(
-              debug, "Upstream disconnected, avoiding sending data, no further event processing.");
-          stop_further_inputs = true;
-          break;
-        }
+      {
+        auto upstream_write_func = [&](const Http2Frame& h2_frame) -> void {
+          AssertionResult result = fake_upstream_connection->write(std::string(h2_frame));
+          RELEASE_ASSERT(result, result.message());
+        };
+        for (auto& frame : event.upstream_send_event().h2_frames()) {
+          if (!fake_upstream_connection->connected()) {
+            ENVOY_LOG_MISC(
+                debug,
+                "Upstream disconnected, avoiding sending data, no further event processing.");
+            stop_further_inputs = true;
+            break;
+          }
 
-        const Http2Frame frame = frameMakers[frame_func_idx]();
-        ENVOY_LOG_MISC(trace, "sending upstream frame idx: {}", frame_func_idx);
-        AssertionResult result = fake_upstream_connection->write(std::string(frame));
-        RELEASE_ASSERT(result, result.message());
+          ENVOY_LOG_MISC(trace, "sending upstream frame");
+          sendFrame(frame, upstream_write_func);
+        }
       }
       break;
     default:
--- test/integration/h2_fuzz.h
+++ test/integration/h2_fuzz.h
@@ -18,73 +18,12 @@ public:
 
   void initialize() override;
   void replay(const test::integration::H2CaptureFuzzTestCase&);
+  // TODO(adip): The H1 fuzzer uses a short max_wait_ms_ when calling this function. When
+  // trying to do so with H2, an ABORT happens in FileEventImpl::assignEvents
+  const std::chrono::milliseconds max_wait_ms_{1500};
 
 private:
-  // Maps between the enum value in h2_capture_fuzz.proto to the correct
-  // function in test/common/http/http2/http2_frame.h
-  // TODO(adip): some methods receive the stream index, which is currently set to 1, this should
-  // be fuzzed further
-  static constexpr uint32_t stream_index_ = 1;
-  static constexpr const char* host_ = "host";
-  static constexpr const char* path_ = "/test/long/url";
-  Envoy::Http::Http2::Http2Frame (*const frameMakers[17])(){
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makePingFrame();
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeEmptySettingsFrame();
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeEmptySettingsFrame(
-            Envoy::Http::Http2::Http2Frame::SettingsFlags::Ack);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeEmptyHeadersFrame(stream_index_);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeEmptyHeadersFrame(
-            stream_index_, Envoy::Http::Http2::Http2Frame::HeadersFlags::EndStream);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeEmptyHeadersFrame(
-            stream_index_, Envoy::Http::Http2::Http2Frame::HeadersFlags::EndHeaders);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeEmptyContinuationFrame(stream_index_);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeEmptyContinuationFrame(
-            stream_index_, Envoy::Http::Http2::Http2Frame::HeadersFlags::EndStream);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeEmptyContinuationFrame(
-            stream_index_, Envoy::Http::Http2::Http2Frame::HeadersFlags::EndHeaders);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeEmptyDataFrame(stream_index_);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeEmptyDataFrame(
-            stream_index_, Envoy::Http::Http2::Http2Frame::DataFlags::EndStream);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makePriorityFrame(stream_index_, 1);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeWindowUpdateFrame(stream_index_, 60);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeMalformedRequest(stream_index_);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeMalformedRequestWithZerolenHeader(stream_index_,
-                                                                                     host_, path_);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makeRequest(stream_index_, host_, path_);
-      },
-      []() -> Envoy::Http::Http2::Http2Frame {
-        return Envoy::Http::Http2::Http2Frame::makePostRequest(stream_index_, host_, path_);
-      }};
+  void sendFrame(const test::integration::H2TestFrame&,
+                 std::function<void(const Envoy::Http::Http2::Http2Frame&)>);
 };
 } // namespace Envoy
