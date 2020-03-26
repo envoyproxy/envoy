41013a4eaf16351be46933657f0d1e87996241be
Yaroslav Skopets <yaroslav@tetrate.io>
2020-03-26 14:17:19 +0100

    unit tests: mark unit tests the involve `v2.Bootstrap.tracing` as `DEPRECATED_FEATURE_TEST()`
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>

--- test/server/server_test.cc
+++ test/server/server_test.cc
@@ -939,7 +939,7 @@ TEST_P(ServerInstanceImplTest, NoHttpTracing) {
               ProtoEq(server_->httpContext().defaultTracingConfig()));
 }
 
-TEST_P(ServerInstanceImplTest, ZipkinHttpTracingEnabled) {
+TEST_P(ServerInstanceImplTest, DEPRECATED_FEATURE_TEST(ZipkinHttpTracingEnabled)) {
   options_.service_cluster_name_ = "some_cluster_name";
   options_.service_node_name_ = "some_node_name";
   EXPECT_NO_THROW(initialize("test/server/test_data/server/zipkin_tracing.yaml"));
