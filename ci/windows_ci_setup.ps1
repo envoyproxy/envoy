f79a6945acee7f4577e2e93b99f27d4015d20316
Sunjay Bhatia <sbhatia@pivotal.io>
2020-03-27 15:13:31 -0400

    Fix BAZEL_SH path
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>
    Signed-off-by: William A Rowe Jr <wrowe@pivotal.io>

--- ci/windows_ci_setup.ps1
+++ ci/windows_ci_setup.ps1
@@ -44,7 +44,7 @@ $msys2TarPathClean = "/$env:TEMP/msys2.tar.xz".replace(':', '').replace('\', '/'
 $outDirClean = "/$env:TOOLS_BIN_DIR".replace(':', '').replace('\', '/')
 &"$tarpath" -Jxf $msys2TarPathClean -C $outDirClean --strip-components=1
 # Add utils to the path for msys2 setup
-$env:PATH = "$env:TOOLS_BIN_DIR\usr\bin;$env:TOOLS_BIN_DIR\mingw64\bin;$env:Path"
+$env:PATH = "$env:TOOLS_BIN_DIR\usr\bin;$env:TOOLS_BIN_DIR\mingw64\bin;$env:PATH"
 bash.exe -c "pacman-key --init 2>&1"
 bash.exe -c "pacman-key --populate msys2 2>&1"
 bash.exe -c "pacman.exe -Syyuu --noconfirm 2>&1"
