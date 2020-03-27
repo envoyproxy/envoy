210ee5df7261d78ce5e220b479cd78400e532131
William A Rowe Jr <wrowe@pivotal.io>
2020-03-27 13:19:02 -0400

    Fix download path for msys2.tar.xz
    Add comment explaining why we update the PATH temporarily
    
    Signed-off-by: William A Rowe Jr <wrowe@pivotal.io>
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- ci/windows_ci_setup.ps1
+++ ci/windows_ci_setup.ps1
@@ -33,7 +33,7 @@ $wc.DownloadFile("http://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-2019052
 
 # Check the SHA256 file hash of each downloaded file.
 Checksum $env:TOOLS_BIN_DIR\bazel.exe 96395ee9e3fb9f4499fcaffa8a94dd72b0748f495f366bc4be44dbf09d6827fc SHA256
-Checksum $env:TOOLS_BIN_DIR\msys2.tar.xz 168e156fa9f00d90a8445676c023c63be6e82f71487f4e2688ab5cb13b345383 SHA256
+Checksum $env:TEMP\msys2.tar.xz 168e156fa9f00d90a8445676c023c63be6e82f71487f4e2688ab5cb13b345383 SHA256
 # Checksum $env:TOOLS_BIN_DIR\ninja-win.zip 2d70010633ddaacc3af4ffbd21e22fae90d158674a09e132e06424ba3ab036e9 SHA256
 
 # Unzip "$env:TOOLS_BIN_DIR\ninja-win.zip" "$env:TOOLS_BIN_DIR"
@@ -41,6 +41,7 @@ Checksum $env:TOOLS_BIN_DIR\msys2.tar.xz 168e156fa9f00d90a8445676c023c63be6e82f7
 # Unpack and install msys2 and required packages
 $tarpath="$env:ProgramFiles\Git\usr\bin\tar.exe"
 &"$tarpath" -xJf "$env:TEMP\msys2.tar.xz" -C "$env:TOOLS_BIN_DIR"
+# Add utils to the path for msys2 setup
 $env:PATH = "$env:TOOLS_BIN_DIR\mingw64\bin;$env:TOOLS_BIN_DIR\usr\bin;$env:Path"
 bash.exe -c "pacman-key --init 2>&1"
 bash.exe -c "pacman-key --populate msys2 2>&1"
