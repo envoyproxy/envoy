5ab3896b6e4225b9e8f68411372c689d68a448a2
Marco Magdy <marcomagdy@users.noreply.github.com>
2020-03-30 13:39:54 -0700

    AWS Lambda integration test fix (#10558)
    The lambda integration test is timing out because it's attempting to
    fetch the aws creds using the default provider. The default provider
    checks the environment variables and falls-back to querying ec2 instance
    metadata. Since the CI does not have the environment variables, nor does
    it run on ec2, the provider spends 5s before it times out.
    
    The fix is to simply set environment variables. The values of the
    variables does not matter since we never actually send the request to
    aws.
    
    Signed-off-by: Marco Magdy <mmagdy@gmail.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/extensions/filters/http/aws_lambda/aws_lambda_filter_integration_test.cc
+++ test/extensions/filters/http/aws_lambda/aws_lambda_filter_integration_test.cc
@@ -3,6 +3,7 @@
 #include "source/extensions/filters/http/aws_lambda/request_response.pb.h"
 
 #include "test/integration/http_integration.h"
+#include "test/test_common/environment.h"
 #include "test/test_common/utility.h"
 
 #include "gtest/gtest.h"
@@ -18,7 +19,13 @@ public:
   AwsLambdaFilterIntegrationTest()
       : HttpIntegrationTest(Http::CodecClient::Type::HTTP2, GetParam()) {}
 
-  void SetUp() override { setUpstreamProtocol(FakeHttpConnection::Type::HTTP1); }
+  void SetUp() override {
+    // Set these environment variables to quickly sign credentials instead of attempting to query
+    // instance metadata and timing-out.
+    TestEnvironment::setEnvVar("AWS_ACCESS_KEY_ID", "aws-user", 1 /*overwrite*/);
+    TestEnvironment::setEnvVar("AWS_SECRET_ACCESS_KEY", "secret", 1 /*overwrite*/);
+    setUpstreamProtocol(FakeHttpConnection::Type::HTTP1);
+  }
 
   void TearDown() override {
     test_server_.reset();
@@ -77,7 +84,16 @@ public:
       auto encoder_decoder = codec_client_->startRequest(request_headers);
       request_encoder_ = &encoder_decoder.first;
       response = std::move(encoder_decoder.second);
-      Buffer::OwnedImpl buffer(request_body);
+      // Chunk the data to simulate a real request.
+      const size_t chunk_size = 5;
+      size_t i = 0;
+      for (; i < request_body.length() / chunk_size; i++) {
+        Buffer::OwnedImpl buffer(request_body.substr(i * chunk_size, chunk_size));
+        codec_client_->sendData(*request_encoder_, buffer, false);
+      }
+      // Send the last chunk flagged as end_stream.
+      Buffer::OwnedImpl buffer(
+          request_body.substr(i * chunk_size, request_body.length() % chunk_size));
       codec_client_->sendData(*request_encoder_, buffer, true);
     }
 
