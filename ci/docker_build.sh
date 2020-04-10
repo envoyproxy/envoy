968ce601d784e923c73d83214ac2f614277353de
Matt Klein <mklein@lyft.com>
2020-04-10 16:26:00 -0700

    build: refine docker image CI process (#10729)
    Signed-off-by: Matt Klein <mklein@lyft.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- ci/docker_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-DOCKER_IMAGE_PREFIX="${DOCKER_IMAGE_PREFIX:-envoyproxy/envoy}"
-
-docker build -f ci/Dockerfile-envoy-image -t "${DOCKER_IMAGE_PREFIX}-dev:${CIRCLE_SHA1}" .
-docker build -f ci/Dockerfile-envoy-alpine -t "${DOCKER_IMAGE_PREFIX}-alpine-dev:${CIRCLE_SHA1}" .
-docker build -f ci/Dockerfile-envoy-alpine-debug -t "${DOCKER_IMAGE_PREFIX}-alpine-debug-dev:${CIRCLE_SHA1}" .
