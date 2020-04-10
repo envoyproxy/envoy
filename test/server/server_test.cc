67668e44b7958e5207a0c46bfc2e5b3a62c10468
Yaroslav Skopets <yaroslav@tetrate.io>
2020-04-10 16:55:09 +0200

    code review: rename `test_data/server/zipkin_tracing.yaml` into `test_data/server/zipkin_tracing_deprecated_config.yaml`
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>

--- test/server/server_test.cc
+++ test/server/server_test.cc
@@ -943,7 +943,7 @@ TEST_P(ServerInstanceImplTest, NoHttpTracing) {
 TEST_P(ServerInstanceImplTest, DEPRECATED_FEATURE_TEST(ZipkinHttpTracingEnabled)) {
   options_.service_cluster_name_ = "some_cluster_name";
   options_.service_node_name_ = "some_node_name";
-  EXPECT_NO_THROW(initialize("test/server/test_data/server/zipkin_tracing.yaml"));
+  EXPECT_NO_THROW(initialize("test/server/test_data/server/zipkin_tracing_deprecated_config.yaml"));
   EXPECT_EQ("zipkin", server_->httpContext().defaultTracingConfig().http().name());
 }
 
