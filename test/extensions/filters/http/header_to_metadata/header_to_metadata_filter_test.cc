03eeb76f5dab157cb767243482d06049d76aa5cf
Raul Gutierrez Segales <rgs@pinterest.com>
2020-04-15 08:35:25 -0400

    Test empty rules are not allowed in the per route case
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>

--- test/extensions/filters/http/header_to_metadata/header_to_metadata_filter_test.cc
+++ test/extensions/filters/http/header_to_metadata/header_to_metadata_filter_test.cc
@@ -417,6 +417,11 @@ request_rules:
   EXPECT_THROW_WITH_MESSAGE(initializeFilter(config), Envoy::EnvoyException, expected);
 }
 
+TEST_F(HeaderToMetadataTest, PerRouteEmtpyRules) {
+  envoy::extensions::filters::http::header_to_metadata::v3::Config config_proto;
+  EXPECT_THROW(std::make_shared<Config>(config_proto, true), EnvoyException);
+}
+
 /**
  * Empty values not added to metadata.
  */
