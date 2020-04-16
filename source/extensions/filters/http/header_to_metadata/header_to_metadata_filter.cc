724e620897bf2b21b2af2ba6ae5ab1748c57c43b
Raul Gutierrez Segales <rgs@pinterest.com>
2020-04-16 12:02:18 -0400

    Clarify comment around config validation
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>

--- source/extensions/filters/http/header_to_metadata/header_to_metadata_filter.cc
+++ source/extensions/filters/http/header_to_metadata/header_to_metadata_filter.cc
@@ -21,7 +21,10 @@ Config::Config(const envoy::extensions::filters::http::header_to_metadata::v3::C
   request_set_ = Config::configToVector(config.request_rules(), request_rules_);
   response_set_ = Config::configToVector(config.response_rules(), response_rules_);
 
-  // Note: empty configs are fine for the non per route case.
+  // Note: empty configs are fine for the global config, which would be the case for enabling
+  //       the filter globally without rules and then applying them at the virtual host or
+  //       route level. At the virtual or route level, it makes no sense to have an empty
+  //       config so we throw an error.
   if (per_route && !response_set_ && !request_set_) {
     throw EnvoyException("Must at least specify either response or request config");
   }
