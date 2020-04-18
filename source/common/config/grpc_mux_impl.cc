bd93cfbcaa6b6133cfc138bd047fd77db19709a1
Venil Noronha <veniln@vmware.com>
2020-04-18 12:14:17 -0700

    Simplify logic
    Signed-off-by: Venil Noronha <veniln@vmware.com>

--- source/common/config/grpc_mux_impl.cc
+++ source/common/config/grpc_mux_impl.cc
@@ -216,21 +216,15 @@ void GrpcMuxImpl::onStreamEstablished() {
 }
 
 void GrpcMuxImpl::onEstablishmentFailure() {
-  for (auto& api_state : api_state_) {
-    if (api_state.second.pending_) {
-      ENVOY_LOG(trace, "API {} pending during onEstablishmentFailure(), unsetting pending.",
-                api_state.first);
-      api_state.second.pending_ = false;
-    }
-    if (api_state.second.paused_) {
-      ENVOY_LOG(trace, "API {} paused during onEstablishmentFailure(), unpausing.",
-                api_state.first);
-      api_state.second.paused_ = false;
-    }
-
+  for (const auto& api_state : api_state_) {
     for (auto watch : api_state.second.watches_) {
       watch->callbacks_.onConfigUpdateFailed(
           Envoy::Config::ConfigUpdateFailureReason::ConnectionFailure, nullptr);
+    }
+    if (api_state.second.paused_) {
+      ENVOY_LOG(trace, "API {} paused during onEstablishmentFailure(), resuming.",
+                api_state.first);
+      resume(api_state.first);
     }
   }
 }
