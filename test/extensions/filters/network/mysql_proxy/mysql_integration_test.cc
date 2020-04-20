b016a5e5d572b80f42d08cfaffb658678af5189f
Sunjay Bhatia <sbhatia@pivotal.io>
2020-04-20 18:24:24 -0400

    Drop unnecessary header as observed by mattklein123
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>
    
    Signed-off-by: William A Rowe Jr <wrowe@pivotal.io>

--- test/extensions/filters/network/mysql_proxy/mysql_integration_test.cc
+++ test/extensions/filters/network/mysql_proxy/mysql_integration_test.cc
@@ -1,8 +1,3 @@
-#ifndef WIN32
-#include <pthread.h>
-
-#endif
-
 #include "extensions/filters/network/mysql_proxy/mysql_codec.h"
 #include "extensions/filters/network/mysql_proxy/mysql_codec_clogin.h"
 #include "extensions/filters/network/mysql_proxy/mysql_codec_clogin_resp.h"
