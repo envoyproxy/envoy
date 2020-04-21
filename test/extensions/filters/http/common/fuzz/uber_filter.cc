bec4ab302c3ad3d5a9cec38ff6fe773be9e386b6
Asra Ali <asraa@google.com>
2020-04-21 15:27:59 -0400

    split into source file
    Signed-off-by: Asra Ali <asraa@google.com>

---
+++ test/extensions/filters/http/common/fuzz/uber_filter.cc
@@ -0,0 +1,170 @@
+#include "test/extensions/filters/http/common/fuzz/uber_filter.h"
+
+#include "envoy/extensions/filters/http/grpc_json_transcoder/v3/transcoder.pb.h"
+
+#include "common/config/utility.h"
+#include "common/config/version_converter.h"
+#include "common/http/message_impl.h"
+#include "common/protobuf/protobuf.h"
+#include "common/protobuf/utility.h"
+
+#include "test/proto/bookstore.pb.h"
+#include "test/test_common/utility.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace HttpFilters {
+
+UberFilterFuzzer::UberFilterFuzzer() {
+  // Need to set for both a decoder filter and an encoder/decoder filter.
+  ON_CALL(filter_callback_, addStreamDecoderFilter(_))
+      .WillByDefault(Invoke([&](std::shared_ptr<Envoy::Http::StreamDecoderFilter> filter) -> void {
+        filter_ = filter;
+        filter_->setDecoderFilterCallbacks(callbacks_);
+      }));
+  ON_CALL(filter_callback_, addStreamFilter(_))
+      .WillByDefault(Invoke([&](std::shared_ptr<Envoy::Http::StreamDecoderFilter> filter) -> void {
+        filter_ = filter;
+        filter_->setDecoderFilterCallbacks(callbacks_);
+      }));
+  setExpectations();
+}
+
+void UberFilterFuzzer::setExpectations() {
+  // Ext-authz setup
+  prepareExtAuthz();
+  prepareCache();
+  prepareTap();
+}
+
+void UberFilterFuzzer::prepareExtAuthz() {
+  // Preparing the expectations for the ext_authz filter.
+  addr_ = std::make_shared<Network::Address::Ipv4Instance>("1.2.3.4", 1111);
+  ON_CALL(connection_, remoteAddress()).WillByDefault(testing::ReturnRef(addr_));
+  ON_CALL(connection_, localAddress()).WillByDefault(testing::ReturnRef(addr_));
+  ON_CALL(callbacks_, connection()).WillByDefault(testing::Return(&connection_));
+  ON_CALL(callbacks_, activeSpan())
+      .WillByDefault(testing::ReturnRef(Tracing::NullSpan::instance()));
+  callbacks_.stream_info_.protocol_ = Envoy::Http::Protocol::Http2;
+}
+
+void UberFilterFuzzer::prepareCache() {
+  // Prepare expectations for dynamic forward proxy.
+  ON_CALL(factory_context_.dispatcher_, createDnsResolver(_, _))
+      .WillByDefault(testing::Return(resolver_));
+}
+
+void UberFilterFuzzer::prepareTap() {
+  ON_CALL(factory_context_.admin_, addHandler(_, _, _, _, _)).WillByDefault(testing::Return(true));
+  ON_CALL(factory_context_.admin_, removeHandler(_)).WillByDefault(testing::Return(true));
+}
+
+void UberFilterFuzzer::addFileDescriptorsRecursively(
+    const Protobuf::FileDescriptor* descriptor, Protobuf::FileDescriptorSet* set,
+    absl::flat_hash_set<absl::string_view>* added_descriptors) {
+  if (!added_descriptors->insert(descriptor->name()).second) {
+    // Already added.
+    return;
+  }
+  for (int i = 0; i < descriptor->dependency_count(); i++) {
+    addFileDescriptorsRecursively(descriptor->dependency(i), set, added_descriptors);
+  }
+  descriptor->CopyTo(set->add_file());
+}
+
+void UberFilterFuzzer::addProtoDescriptor(absl::string_view filter_name,
+                                          Protobuf::Message* message) {
+  if (filter_name.find("grpc_json_transcoder") != absl::string_view::npos) {
+    envoy::extensions::filters::http::grpc_json_transcoder::v3::GrpcJsonTranscoder& config =
+        dynamic_cast<
+            envoy::extensions::filters::http::grpc_json_transcoder::v3::GrpcJsonTranscoder&>(
+            *message);
+    config.clear_services();
+    config.add_services("bookstore.Bookstore");
+
+    Protobuf::FileDescriptorSet descriptor_set;
+    const auto* file_descriptor =
+        Protobuf::DescriptorPool::generated_pool()->FindFileByName("test/proto/bookstore.proto");
+    ASSERT(file_descriptor != nullptr);
+    absl::flat_hash_set<absl::string_view> added_descriptors;
+    addFileDescriptorsRecursively(file_descriptor, &descriptor_set, &added_descriptors);
+    descriptor_set.SerializeToString(config.mutable_proto_descriptor_bin());
+  }
+}
+
+void UberFilterFuzzer::decode(Http::StreamDecoderFilter* filter, const test::fuzz::HttpData& data) {
+  bool end_stream = false;
+
+  auto headers = Fuzz::fromHeaders<Http::TestRequestHeaderMapImpl>(data.headers());
+  if (headers.Path() == nullptr) {
+    headers.setPath("/foo");
+  }
+  if (headers.Method() == nullptr) {
+    headers.setMethod("GET");
+  }
+  if (headers.Host() == nullptr) {
+    headers.setHost("foo.com");
+  }
+
+  if (data.data().empty() && !data.has_trailers()) {
+    end_stream = true;
+  }
+  ENVOY_LOG_MISC(debug, "Decoding headers: {} ", data.headers().DebugString());
+  const auto& headersStatus = filter->decodeHeaders(headers, end_stream);
+  if (headersStatus != Http::FilterHeadersStatus::Continue &&
+      headersStatus != Http::FilterHeadersStatus::StopIteration) {
+    return;
+  }
+
+  for (int i = 0; i < data.data().size(); i++) {
+    if (i == data.data().size() - 1 && !data.has_trailers()) {
+      end_stream = true;
+    }
+    Buffer::OwnedImpl buffer(data.data().Get(i));
+    ENVOY_LOG_MISC(debug, "Decoding data: {} ", buffer.toString());
+    if (filter->decodeData(buffer, end_stream) != Http::FilterDataStatus::Continue) {
+      return;
+    }
+  }
+
+  if (data.has_trailers()) {
+    ENVOY_LOG_MISC(debug, "Decoding trailers: {} ", data.trailers().DebugString());
+    auto trailers = Fuzz::fromHeaders<Http::TestRequestTrailerMapImpl>(data.trailers());
+    filter->decodeTrailers(trailers);
+  }
+}
+
+void UberFilterFuzzer::fuzz(
+    const envoy::extensions::filters::network::http_connection_manager::v3::HttpFilter&
+        proto_config,
+    const test::fuzz::HttpData& data) {
+  try {
+    // Try to create the filter. Exit early if the config is invalid or violates PGV constraints.
+    ENVOY_LOG_MISC(info, "filter name {}", proto_config.name());
+    auto& factory = Config::Utility::getAndCheckFactoryByName<
+        Server::Configuration::NamedHttpFilterConfigFactory>(proto_config.name());
+    ProtobufTypes::MessagePtr message = Config::Utility::translateToFactoryConfig(
+        proto_config, factory_context_.messageValidationVisitor(), factory);
+    // Add a valid service and proto descriptor.
+    addProtoDescriptor(proto_config.name(), message.get());
+    cb_ = factory.createFilterFactoryFromProto(*message, "stats", factory_context_);
+    cb_(filter_callback_);
+  } catch (const EnvoyException& e) {
+    ENVOY_LOG_MISC(debug, "Controlled exception {}", e.what());
+    return;
+  }
+
+  decode(filter_.get(), data);
+  reset();
+}
+
+void UberFilterFuzzer::reset() {
+  if (filter_ != nullptr) {
+    filter_->onDestroy();
+  }
+  filter_.reset();
+}
+
+} // namespace HttpFilters
+} // namespace Extensions
+} // namespace Envoy
--- test/extensions/filters/http/common/fuzz/uber_filter.h
+++ test/extensions/filters/http/common/fuzz/uber_filter.h
@@ -1,17 +1,7 @@
-#include "envoy/extensions/filters/http/grpc_json_transcoder/v3/transcoder.pb.h"
-
-#include "common/config/utility.h"
-#include "common/config/version_converter.h"
-#include "common/http/message_impl.h"
-#include "common/protobuf/protobuf.h"
-#include "common/protobuf/utility.h"
-
 #include "test/fuzz/utility.h"
 #include "test/mocks/buffer/mocks.h"
 #include "test/mocks/http/mocks.h"
 #include "test/mocks/server/mocks.h"
-#include "test/proto/bookstore.pb.h"
-#include "test/test_common/utility.h"
 
 namespace Envoy {
 namespace Extensions {
@@ -19,159 +9,31 @@ namespace HttpFilters {
 
 class UberFilterFuzzer {
 public:
-  UberFilterFuzzer() {
-    // Need to set for both a decoder filter and an encoder/decoder filter.
-    ON_CALL(filter_callback_, addStreamDecoderFilter(_))
-        .WillByDefault(
-            Invoke([&](std::shared_ptr<Envoy::Http::StreamDecoderFilter> filter) -> void {
-              filter_ = filter;
-              filter_->setDecoderFilterCallbacks(callbacks_);
-            }));
-    ON_CALL(filter_callback_, addStreamFilter(_))
-        .WillByDefault(
-            Invoke([&](std::shared_ptr<Envoy::Http::StreamDecoderFilter> filter) -> void {
-              filter_ = filter;
-              filter_->setDecoderFilterCallbacks(callbacks_);
-            }));
-    setExpectations();
-  }
+  UberFilterFuzzer();
 
-  void setExpectations() {
-    // Ext-authz setup
-    prepareExtAuthz();
-    prepareCache();
-    prepareTap();
-  }
+  // This creates the filter config and runs the decode methods.
+  void fuzz(const envoy::extensions::filters::network::http_connection_manager::v3::HttpFilter&
+                proto_config,
+            const test::fuzz::HttpData& data);
 
-  void prepareExtAuthz() {
-    // Preparing the expectations for the ext_authz filter.
-    addr_ = std::make_shared<Network::Address::Ipv4Instance>("1.2.3.4", 1111);
-    ON_CALL(connection_, remoteAddress()).WillByDefault(testing::ReturnRef(addr_));
-    ON_CALL(connection_, localAddress()).WillByDefault(testing::ReturnRef(addr_));
-    ON_CALL(callbacks_, connection()).WillByDefault(testing::Return(&connection_));
-    ON_CALL(callbacks_, activeSpan())
-        .WillByDefault(testing::ReturnRef(Tracing::NullSpan::instance()));
-    callbacks_.stream_info_.protocol_ = Envoy::Http::Protocol::Http2;
-  }
-
-  void prepareCache() {
-    // Prepare expectations for dynamic forward proxy.
-    ON_CALL(factory_context_.dispatcher_, createDnsResolver(_, _))
-        .WillByDefault(testing::Return(resolver_));
-  }
-
-  void prepareTap() {
-    ON_CALL(factory_context_.admin_, addHandler(_, _, _, _, _))
-        .WillByDefault(testing::Return(true));
-    ON_CALL(factory_context_.admin_, removeHandler(_)).WillByDefault(testing::Return(true));
-  }
+protected:
+  // This executes the decode methods to be fuzzed.
+  void decode(Http::StreamDecoderFilter* filter, const test::fuzz::HttpData& data);
+  void reset();
 
   void addFileDescriptorsRecursively(const Protobuf::FileDescriptor* descriptor,
                                      Protobuf::FileDescriptorSet* set,
-                                     absl::flat_hash_set<absl::string_view>* added_descriptors) {
-    if (!added_descriptors->insert(descriptor->name()).second) {
-      // Already added.
-      return;
-    }
-    for (int i = 0; i < descriptor->dependency_count(); i++) {
-      addFileDescriptorsRecursively(descriptor->dependency(i), set, added_descriptors);
-    }
-    descriptor->CopyTo(set->add_file());
-  }
+                                     absl::flat_hash_set<absl::string_view>* added_descriptors);
 
-  void addProtoDescriptor(absl::string_view filter_name, Protobuf::Message* message) {
-    if (filter_name.find("grpc_json_transcoder") != absl::string_view::npos) {
-      envoy::extensions::filters::http::grpc_json_transcoder::v3::GrpcJsonTranscoder& config =
-          dynamic_cast<
-              envoy::extensions::filters::http::grpc_json_transcoder::v3::GrpcJsonTranscoder&>(
-              *message);
-      config.clear_services();
-      config.add_services("bookstore.Bookstore");
+  void addProtoDescriptor(absl::string_view filter_name, Protobuf::Message* message);
 
-      Protobuf::FileDescriptorSet descriptor_set;
-      const auto* file_descriptor =
-          Protobuf::DescriptorPool::generated_pool()->FindFileByName("test/proto/bookstore.proto");
-      ASSERT(file_descriptor != nullptr);
-      absl::flat_hash_set<absl::string_view> added_descriptors;
-      addFileDescriptorsRecursively(file_descriptor, &descriptor_set, &added_descriptors);
-      descriptor_set.SerializeToString(config.mutable_proto_descriptor_bin());
-    }
-  }
+  // Set expectations in constructor.
+  void setExpectations();
+  void prepareExtAuthz();
+  void prepareCache();
+  void prepareTap();
 
-  // This executes the decode methods to be fuzzed.
-  void decode(Http::StreamDecoderFilter* filter, const test::fuzz::HttpData& data) {
-    bool end_stream = false;
-
-    auto headers = Fuzz::fromHeaders<Http::TestRequestHeaderMapImpl>(data.headers());
-    if (headers.Path() == nullptr) {
-      headers.setPath("/foo");
-    }
-    if (headers.Method() == nullptr) {
-      headers.setMethod("GET");
-    }
-    if (headers.Host() == nullptr) {
-      headers.setHost("foo.com");
-    }
-
-    if (data.data().empty() && !data.has_trailers()) {
-      end_stream = true;
-    }
-    ENVOY_LOG_MISC(debug, "Decoding headers: {} ", data.headers().DebugString());
-    const auto& headersStatus = filter->decodeHeaders(headers, end_stream);
-    if (headersStatus != Http::FilterHeadersStatus::Continue &&
-        headersStatus != Http::FilterHeadersStatus::StopIteration) {
-      return;
-    }
-
-    for (int i = 0; i < data.data().size(); i++) {
-      if (i == data.data().size() - 1 && !data.has_trailers()) {
-        end_stream = true;
-      }
-      Buffer::OwnedImpl buffer(data.data().Get(i));
-      ENVOY_LOG_MISC(debug, "Decoding data: {} ", buffer.toString());
-      if (filter->decodeData(buffer, end_stream) != Http::FilterDataStatus::Continue) {
-        return;
-      }
-    }
-
-    if (data.has_trailers()) {
-      ENVOY_LOG_MISC(debug, "Decoding trailers: {} ", data.trailers().DebugString());
-      auto trailers = Fuzz::fromHeaders<Http::TestRequestTrailerMapImpl>(data.trailers());
-      filter->decodeTrailers(trailers);
-    }
-  }
-
-  // This creates the filter config and runs decode.
-  void fuzz(const envoy::extensions::filters::network::http_connection_manager::v3::HttpFilter&
-                proto_config,
-            const test::fuzz::HttpData& data) {
-    try {
-      // Try to create the filter. Exit early if the config is invalid or violates PGV constraints.
-      ENVOY_LOG_MISC(info, "filter name {}", proto_config.name());
-      auto& factory = Config::Utility::getAndCheckFactoryByName<
-          Server::Configuration::NamedHttpFilterConfigFactory>(proto_config.name());
-      ProtobufTypes::MessagePtr message = Config::Utility::translateToFactoryConfig(
-          proto_config, factory_context_.messageValidationVisitor(), factory);
-      // Add a valid service and proto descriptor.
-      addProtoDescriptor(proto_config.name(), message.get());
-      cb_ = factory.createFilterFactoryFromProto(*message, "stats", factory_context_);
-      cb_(filter_callback_);
-    } catch (const EnvoyException& e) {
-      ENVOY_LOG_MISC(debug, "Controlled exception {}", e.what());
-      return;
-    }
-
-    decode(filter_.get(), data);
-    reset();
-  }
-
-  void reset() {
-    if (filter_ != nullptr) {
-      filter_->onDestroy();
-    }
-    filter_.reset();
-  }
-
+private:
   NiceMock<Server::Configuration::MockFactoryContext> factory_context_;
   NiceMock<Http::MockStreamDecoderFilterCallbacks> callbacks_;
   NiceMock<Http::MockFilterChainFactoryCallbacks> filter_callback_;
