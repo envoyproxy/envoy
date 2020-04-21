3841158dbb6909c4c67c4c4dcffa76bbfcc58bf2
Alan Chiu <achiu@lyft.com>
2020-04-21 16:45:21 -0700

    [kotlin] update detekt to 1.8.0 (#803)
    Signed-off-by: Alan Chiu <achiu@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/ci/kotlin_lint.sh
+++ mobile/ci/kotlin_lint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-version="1.0.0-RC15"
+version="1.8.0"
 detekt_jar="detekt-cli-$version-all.jar"
-wget -q "https://github.com/arturbosch/detekt/releases/download/$version/$detekt_jar"
+wget -q "https://github.com/detekt/detekt/releases/download/v$version/$detekt_jar"
 
 java -jar ${detekt_jar} --build-upon-default-config -c .kotlinlint.yml -i examples/kotlin/hello_world
