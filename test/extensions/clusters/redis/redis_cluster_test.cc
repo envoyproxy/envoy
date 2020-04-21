abc27986d2506817a42a3ffc7e0ead66751da591
Ruslan Nigmatullin <elessar@dropbox.com>
2020-04-20 17:03:54 -0700

    redis: Fix stack-use-after-scope in test (#10840)
    Signed-off-by: Ruslan Nigmatullin <elessar@dropbox.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/extensions/clusters/redis/redis_cluster_test.cc
+++ test/extensions/clusters/redis/redis_cluster_test.cc
@@ -150,7 +150,8 @@ protected:
                               Network::DnsResolver::ResolutionStatus status =
                                   Network::DnsResolver::ResolutionStatus::Success) {
     EXPECT_CALL(*dns_resolver_, resolve(expected_address, dns_lookup_family, _))
-        .WillOnce(Invoke([&](const std::string&, Network::DnsLookupFamily,
+        .WillOnce(Invoke([status, resolved_addresses](
+                             const std::string&, Network::DnsLookupFamily,
                              Network::DnsResolver::ResolveCb cb) -> Network::ActiveDnsQuery* {
           cb(status, TestUtility::makeDnsResponse(resolved_addresses));
           return nullptr;
@@ -685,7 +686,7 @@ TEST_F(RedisClusterTest, FailedDnsResponse) {
 
   EXPECT_EQ(0UL, cluster_->prioritySet().hostSetsPerPriority()[0]->hosts().size());
   EXPECT_EQ(0UL, cluster_->prioritySet().hostSetsPerPriority()[0]->healthyHosts().size());
-  EXPECT_EQ(1U, cluster_->info()->stats().update_empty_.value());
+  EXPECT_EQ(0U, cluster_->info()->stats().update_empty_.value());
 
   // Does not recreate the timer on subsequent DNS resolve calls.
   EXPECT_CALL(*dns_timer, enableTimer(_, _));
@@ -694,7 +695,7 @@ TEST_F(RedisClusterTest, FailedDnsResponse) {
 
   EXPECT_EQ(0UL, cluster_->prioritySet().hostSetsPerPriority()[0]->hosts().size());
   EXPECT_EQ(0UL, cluster_->prioritySet().hostSetsPerPriority()[0]->healthyHosts().size());
-  EXPECT_EQ(2U, cluster_->info()->stats().update_empty_.value());
+  EXPECT_EQ(1U, cluster_->info()->stats().update_empty_.value());
 }
 
 TEST_F(RedisClusterTest, Basic) {
