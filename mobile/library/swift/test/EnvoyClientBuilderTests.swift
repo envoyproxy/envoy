18f3c1c295f9a35b40543658524db2e2875daf2c
Michael Rebello <me@michaelrebello.com>
2020-04-21 20:52:09 -0700

    ios: centralize MockEnvoyEngine (#804)
    We don't need to replicate this type in multiple places.
    
    Signed-off-by: Michael Rebello <me@michaelrebello.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/swift/test/EnvoyClientBuilderTests.swift
+++ mobile/library/swift/test/EnvoyClientBuilderTests.swift
@@ -18,25 +18,6 @@ mock_template:
   virtual_clusters: {{ virtual_clusters }}
 """
 
-private final class MockEnvoyEngine: NSObject, EnvoyEngine {
-  static var onRunWithConfig: ((_ config: EnvoyConfiguration, _ logLevel: String?) -> Void)?
-  static var onRunWithYAML: ((_ configYAML: String, _ logLevel: String?) -> Void)?
-
-  func run(withConfig config: EnvoyConfiguration, logLevel: String) -> Int32 {
-    MockEnvoyEngine.onRunWithConfig?(config, logLevel)
-    return 0
-  }
-
-  func run(withConfigYAML configYAML: String, logLevel: String) -> Int32 {
-    MockEnvoyEngine.onRunWithYAML?(configYAML, logLevel)
-    return 0
-  }
-
-  func startStream(with callbacks: EnvoyHTTPCallbacks) -> EnvoyHTTPStream {
-    return MockEnvoyHTTPStream(handle: 0, callbacks: callbacks)
-  }
-}
-
 final class EnvoyClientBuilderTests: XCTestCase {
   override func tearDown() {
     super.tearDown()
