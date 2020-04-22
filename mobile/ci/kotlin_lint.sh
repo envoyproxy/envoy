a61a7ca8a0c4661cbd4ba2d81fa5f8c4c708eef3
Michael Rebello <me@michaelrebello.com>
2020-04-22 11:48:05 -0700

    kotlin: improve linter script (#807)
    - Update the script to download to a temp directory instead of the project root
    - This makes it easier to run locally since users won't end up with a git diff
    - Add some logs to the script to make it easier to identify issues in CI logs
    
    New output:
    
    ```
    mrebello-mbp151:envoy-mobile-2 mrebello$ ./ci/kotlin_lint.sh
    Downloading detekt...
    detekt-cli-1.8.0-all.jar                            100%[===================================================================================================================>]  46.86M  5.23MB/s    in 12s
    Running linter...
    Done
    ```
    
    Signed-off-by: Michael Rebello <me@michaelrebello.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/ci/kotlin_lint.sh
+++ mobile/ci/kotlin_lint.sh
@@ -2,9 +2,20 @@
 
 version="1.8.0"
 detekt_jar="detekt-cli-$version-all.jar"
-wget -q "https://github.com/detekt/detekt/releases/download/v$version/$detekt_jar"
+temp_dir="$(mktemp -d)"
 
-java -jar ${detekt_jar} \
+echo "Downloading detekt..."
+wget --directory-prefix ${temp_dir} \
+  -q \
+  --show-progress \
+  "https://github.com/detekt/detekt/releases/download/v$version/$detekt_jar"
+
+echo "Running linter..."
+detekt_path="${temp_dir}/${detekt_jar}"
+java -jar ${detekt_path} \
   --build-upon-default-config \
   -c .kotlinlint.yml \
   -i examples/kotlin,library/kotlin
+
+rm -rf ${temp_dir}
+echo "Done"
