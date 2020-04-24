81e8e194262e07e6674014ac9cb0f40a6be8f9f0
Lizan Zhou <lizan@tetrate.io>
2020-04-24 13:14:14 -0700

    ci: update before purge in cleanup (#10938)
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- .azure-pipelines/cleanup.sh
+++ .azure-pipelines/cleanup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Temporary script to remove tools from Azure pipelines agent to create more disk space room.
-
+sudo apt-get -y update
 sudo apt-get purge -y 'ghc-*' 'zulu-*-azure-jdk' 'libllvm*' 'mysql-*' 'dotnet-*' 'cpp-*'
 
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -rn
