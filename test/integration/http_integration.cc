c26b07c16e6509e679a8a6c79f2bf8447f9b4696
Alyssa Wilk <alyssar@chromium.org>
2020-04-29 13:02:29 -0400

    alter TODO
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/integration/http_integration.cc
+++ test/integration/http_integration.cc
@@ -1204,7 +1204,7 @@ void HttpIntegrationTest::testAdminDrain(Http::CodecClient::Type admin_request_t
 
   // Validate that port is closed and can be bound by other sockets.
   // This does not work for HTTP/3 because the port is not closed until the listener is completely
-  // destroyed. TODO(danzh) fix at some point.
+  // destroyed. TODO(danzh) Match TCP behavior as much as possible.
   if (downstreamProtocol() != Http::CodecClient::Type::HTTP3) {
     EXPECT_NO_THROW(Network::TcpListenSocket(
         Network::Utility::getAddressWithPort(*Network::Test::getCanonicalLoopbackAddress(version_),
