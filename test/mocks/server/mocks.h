bba33f9916a005601fb799dbc067ecb0619b9717
Dmitri Dolguikh <ddolguik@redhat.com>
2020-04-30 09:43:17 -0700

    Fixed a test failure due to the the oder in which destructors were called. (#11001)
    Commit Message: Fixed a test failure due to the the order in which destructors were called.
    
    MockIsolatedStatsStore is being destroyed before ScopePrefixer, which
    holds a reference to it, resulting in a "virtual method called" error.
    
    Signed-off-by: Dmitri Dolguikh <ddolguik@redhat.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/mocks/server/mocks.h
+++ test/mocks/server/mocks.h
@@ -432,8 +432,8 @@ public:
 
   TimeSource& timeSource() override { return time_system_; }
 
-  testing::NiceMock<ThreadLocal::MockInstance> thread_local_;
   NiceMock<Stats::MockIsolatedStatsStore> stats_store_;
+  testing::NiceMock<ThreadLocal::MockInstance> thread_local_;
   std::shared_ptr<testing::NiceMock<Network::MockDnsResolver>> dns_resolver_{
       new testing::NiceMock<Network::MockDnsResolver>()};
   testing::NiceMock<Api::MockApi> api_;
