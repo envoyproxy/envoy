6c014617060bc3d4a3183549585915fe34d89bdb
Lizan Zhou <lizan@tetrate.io>
2020-05-02 00:47:09 +0000

    use 18.04 azp image
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- .azure-pipelines/cleanup.sh
+++ .azure-pipelines/cleanup.sh
@@ -4,6 +4,6 @@ set -e
 
 # Temporary script to remove tools from Azure pipelines agent to create more disk space room.
 sudo apt-get -y update
-sudo apt-get purge -y 'ghc-*' 'zulu-*-azure-jdk' 'libllvm*' 'mysql-*' 'dotnet-*' 'cpp-*'
+sudo apt-get purge --no-upgrade -y 'ghc-*' 'zulu-*-azure-jdk' 'libllvm*' 'mysql-*' 'dotnet-*' 'cpp-*' 'php*' 'libgl0'
 
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -rn
