c31a64a86262a571cbc0fbbfcbe97053a9e15fc7
Sunjay Bhatia <sbhatia@pivotal.io>
2020-05-05 15:25:07 -0400

    Remove docker build dir
    Not needed at the moment as we're not outputting any binaries to publish
    
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- ci/run_envoy_docker_windows.sh
+++ ci/run_envoy_docker_windows.sh
@@ -10,7 +10,6 @@ ENVOY_BUILD_SHA="3cbc11e373dc4e3a523b9273ed010c5e0f197874"
 # The IMAGE_ID defaults to the CI hash but can be set to an arbitrary image ID (found with 'docker
 # images').
 [[ -z "${IMAGE_ID}" ]] && IMAGE_ID="${ENVOY_BUILD_SHA}"
-[[ -z "${ENVOY_DOCKER_BUILD_DIR}" ]] && ENVOY_DOCKER_BUILD_DIR=$(echo "${TEMP}/envoy-docker-build" | sed "s#/c/#C:/#")
 
 ENVOY_SOURCE_DIR=$(echo "${PWD}" | sed "s#/c/#C:/#")
 
@@ -20,12 +19,10 @@ ENVOY_SOURCE_DIR=$(echo "${PWD}" | sed "s#/c/#C:/#")
 
 export ENVOY_BUILD_IMAGE="${IMAGE_NAME}:${IMAGE_ID}"
 
-mkdir -p "${ENVOY_DOCKER_BUILD_DIR}"
 # Since we specify an explicit hash, docker-run will pull from the remote repo if missing.
 docker run --rm ${DOCKER_TTY_OPTION} -e HTTP_PROXY=${http_proxy} -e HTTPS_PROXY=${https_proxy} \
-  -v "${ENVOY_DOCKER_BUILD_DIR}":C:/build ${GIT_VOLUME_OPTION} -e BAZEL_BUILD_EXTRA_OPTIONS \
-  -e BAZEL_EXTRA_TEST_OPTIONS -e BAZEL_REMOTE_CACHE -e ENVOY_STDLIB -e BUILD_REASON \
-  -e BAZEL_REMOTE_INSTANCE -e GCP_SERVICE_ACCOUNT_KEY -e NUM_CPUS -e ENVOY_RBE -e ENVOY_BUILD_IMAGE \
-  -e ENVOY_SRCDIR -e ENVOY_BUILD_TARGET -e SYSTEM_PULLREQUEST_TARGETBRANCH -v ${ENVOY_SOURCE_DIR}:C:/source \
+  ${GIT_VOLUME_OPTION} -e BAZEL_BUILD_EXTRA_OPTIONS -e BAZEL_EXTRA_TEST_OPTIONS -e BAZEL_REMOTE_CACHE \
+  -e ENVOY_STDLIB -e BUILD_REASON -e BAZEL_REMOTE_INSTANCE -e GCP_SERVICE_ACCOUNT_KEY -e NUM_CPUS -e ENVOY_RBE \
+  -e ENVOY_BUILD_IMAGE -e ENVOY_SRCDIR -e ENVOY_BUILD_TARGET -e SYSTEM_PULLREQUEST_TARGETBRANCH -v ${ENVOY_SOURCE_DIR}:C:/source \
   "${ENVOY_BUILD_IMAGE}" \
   bash -c "cd source && $*"
