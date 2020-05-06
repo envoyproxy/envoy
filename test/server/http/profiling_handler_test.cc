357a8a2e2ca7acbab768cc17cb8a4fdc14920228
Erica Manno <erica.manno@gmail.com>
2020-05-06 10:27:47 +0200

    Extract tests to separate files
    Signed-off-by: Erica Manno <erica.manno@gmail.com>

---
+++ test/server/http/profiling_handler_test.cc
@@ -0,0 +1,11 @@
+#include "test/server/http/admin_instance.h"
+
+namespace Envoy {
+namespace Server {
+
+INSTANTIATE_TEST_SUITE_P(IpVersions, AdminInstanceTest,
+                         testing::ValuesIn(TestEnvironment::getIpVersionsForTest()),
+                         TestUtility::ipTestParamsToString);
+
+} // namespace Server
+} // namespace Envoy
