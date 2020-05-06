9c469dcc2159cdde2c0b2509bb136eb2007c4a9c
Teju Nareddy <nareddyt@google.com>
2020-05-06 16:16:51 -0700

    Implement any guidance
    Signed-off-by: Teju Nareddy <nareddyt@google.com>

--- test/extensions/filters/http/common/fuzz/filter_fuzz_test.cc
+++ test/extensions/filters/http/common/fuzz/filter_fuzz_test.cc
@@ -36,6 +36,36 @@ DEFINE_PROTO_FUZZER(const test::extensions::filters::http::FilterFuzzTestCase& i
         input->mutable_config()->mutable_typed_config()->set_type_url(
             absl::StrCat("type.googleapis.com/",
                          factory->createEmptyConfigProto()->GetDescriptor()->full_name()));
+
+        // For fuzzing proto data, guide the mutator to useful 'Any' types half
+        // the time. The other half the time, let the fuzzing engine choose
+        // any message to serialize.
+        if (seed % 2 == 0 && input->data().has_proto_body()) {
+          // These types are request/response from the test Bookstore service
+          // for the gRPC Transcoding filter.
+          static const std::vector<std::string> expected_types = {
+              "type.googleapis.com/bookstore.ListShelvesResponse",
+              "type.googleapis.com/bookstore.CreateShelfRequest",
+              "type.googleapis.com/bookstore.GetShelfRequest",
+              "type.googleapis.com/bookstore.DeleteShelfRequest",
+              "type.googleapis.com/bookstore.ListBooksRequest",
+              "type.googleapis.com/bookstore.CreateBookRequest",
+              "type.googleapis.com/bookstore.GetBookRequest",
+              "type.googleapis.com/bookstore.UpdateBookRequest",
+              "type.googleapis.com/bookstore.DeleteBookRequest",
+              "type.googleapis.com/bookstore.GetAuthorRequest",
+              "type.googleapis.com/bookstore.EchoBodyRequest",
+              "type.googleapis.com/bookstore.EchoStructReqResp",
+              "type.googleapis.com/bookstore.Shelf",
+              "type.googleapis.com/bookstore.Book",
+              "type.googleapis.com/google.protobuf.Empty",
+              "type.googleapis.com/google.api.HttpBody",
+          };
+          ProtobufWkt::Any* mutable_any =
+              input->mutable_data()->mutable_proto_body()->mutable_message();
+          const std::string& type_url = expected_types[(seed / 2) % expected_types.size()];
+          mutable_any->set_type_url(type_url);
+        }
       }};
 
   try {
