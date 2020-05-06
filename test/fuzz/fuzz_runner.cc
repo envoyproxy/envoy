867dd02b25f219348a9a811c11c6a3951baf8da3
Teju Nareddy <nareddyt@google.com>
2020-05-06 13:21:08 -0700

    Review comments + fix
    Signed-off-by: Teju Nareddy <nareddyt@google.com>

--- test/fuzz/fuzz_runner.cc
+++ test/fuzz/fuzz_runner.cc
@@ -48,9 +48,11 @@ void Runner::setupEnvironment(int argc, char** argv, spdlog::level::level_enum d
       new Logger::Context(log_level_, TestEnvironment::getOptions().logFormat(), *lock, false);
   UNREFERENCED_PARAMETER(logging_context);
 
-  // Suppress all libprotobuf logging as long as this object exists.
-  // For fuzzing, this disables all logs when parsing text-format corpus fails.
-  static auto* log_silencer = new Protobuf::LogSilencer();
+  // Suppress all libprotobuf non-fatal logging as long as this object exists.
+  // For fuzzing, this prevents logging when parsing text-format protos fails,
+  // deprecated fields are used, etc.
+  // https://github.com/protocolbuffers/protobuf/blob/204f99488ce1ef74565239cf3963111ae4c774b7/src/google/protobuf/stubs/logging.h#L223
+  ABSL_ATTRIBUTE_UNUSED static auto* log_silencer = new Protobuf::LogSilencer();
 }
 
 } // namespace Fuzz
