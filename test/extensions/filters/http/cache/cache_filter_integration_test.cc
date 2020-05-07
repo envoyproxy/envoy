90e7949370fa9216cc2eecda6028f6d041a9ab7b
Alyssa Wilk <alyssar@chromium.org>
2020-05-07 15:25:05 -0400

    simtime
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/extensions/filters/http/cache/cache_filter_integration_test.cc
+++ test/extensions/filters/http/cache/cache_filter_integration_test.cc
@@ -83,7 +83,7 @@ TEST_P(CacheIntegrationTest, MissInsertHit) {
   EXPECT_EQ(request->body(), std::string(42, 'a'));
   EXPECT_NE(request->headers().get(Http::Headers::get().Age), nullptr);
   // Advance time to force a log flush.
-  simTime().advanceTimeAsync(std::chrono::seconds(1));
+  simTime().advanceTimeWait(std::chrono::seconds(1));
   EXPECT_EQ(waitForAccessLog(access_log_name_, 1), "RFCF cache.response_from_cache_filter\n");
 }
 
