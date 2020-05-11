59fe97c73f4559f275b51b6c0cf7807b845d29f9
Tony Allen <tony@allen.gg>
2020-05-11 14:58:57 -0700

    safer vector array pointer
    Signed-off-by: Tony Allen <tony@allen.gg>

--- source/common/http/http2/codec_impl.cc
+++ source/common/http/http2/codec_impl.cc
@@ -318,7 +318,7 @@ void ConnectionImpl::StreamImpl::submitTrailers(const HeaderMap& trailers) {
   std::vector<nghttp2_nv> final_headers;
   buildHeaders(final_headers, trailers);
   int rc =
-      nghttp2_submit_trailer(parent_.session_, stream_id_, &final_headers[0], final_headers.size());
+      nghttp2_submit_trailer(parent_.session_, stream_id_, final_headers.data(), final_headers.size());
   ASSERT(rc == 0);
 }
 
