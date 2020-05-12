430da5b68c12707810d6fee3a82e183f03fb9a8e
Sunjay Bhatia <sbhatia@pivotal.io>
2020-05-12 17:13:14 -0400

    Fix the access log formatter tests for Windows
    formatter test:
    - Anchor the command regex at the beginning of the search space to replace
      the existing invalid use of the match position logic (match should not
      be used at all when the regex search fails).
    - add test case that demonstrates anchor is needed, removal of anchor
      causes new test case to fail
    
    benchmark test:
    - benchmark main now matches test/main.cc to initialize the socket stack
      on Windows and to disable the CRT invalid parameter abort behavior
    
    Co-authored-by: William A Rowe Jr <wrowe@pivotal.io>
    Co-authored-by: Sunjay Bhatia <sbhatia@pivotal.io>
    Signed-off-by: William A Rowe Jr <wrowe@pivotal.io>
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- test/benchmark/main.cc
+++ test/benchmark/main.cc
@@ -1,16 +1,36 @@
 // NOLINT(namespace-envoy)
 // This is an Envoy driver for benchmarks.
+#include "envoy/common/platform.h"
 
 #include "benchmark/benchmark.h"
 
 #ifdef ENVOY_HANDLE_SIGNALS
 #include "common/signal/signal_action.h"
 #endif
+#include "common/common/assert.h"
 
 #include "absl/debugging/symbolize.h"
 
+#if defined(WIN32)
+static void NoopInvalidParameterHandler(const wchar_t* expression, const wchar_t* function,
+                                        const wchar_t* file, unsigned int line,
+                                        uintptr_t pReserved) {
+  return;
+}
+#endif
+
 // Boilerplate main(), which discovers benchmarks and runs them.
 int main(int argc, char** argv) {
+#if defined(WIN32)
+  _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
+
+  _set_invalid_parameter_handler(NoopInvalidParameterHandler);
+
+  WSADATA wsa_data;
+  const WORD version_requested = MAKEWORD(2, 2);
+  RELEASE_ASSERT(WSAStartup(version_requested, &wsa_data) == 0, "");
+#endif
+
 #ifndef __APPLE__
   absl::InitializeSymbolizer(argv[0]);
 #endif
