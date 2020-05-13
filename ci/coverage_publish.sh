8fea925d4653562c990405b8b8cf8917fd95b001
Lizan Zhou <lizan@tetrate.io>
2020-05-13 22:35:12 +0000

    try report
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/coverage_publish.sh
+++ ci/coverage_publish.sh
@@ -4,12 +4,8 @@
 # CircleCI logs.
 set -e
 
-if [ "${CIRCLECI}" != "true" ]; then
-  exit 0
-fi
-
-[[ -z "${ENVOY_BUILD_DIR}" ]] && ENVOY_BUILD_DIR=/build
-COVERAGE_FILE="${ENVOY_BUILD_DIR}/envoy/generated/coverage/index.html"
+[[ -z "${ENVOY_BUILD_DIR}" ]] && ENVOY_BUILD_DIR=/build/envoy
+COVERAGE_FILE="${ENVOY_BUILD_DIR}/generated/coverage/index.html"
 
 if [ ! -f "${COVERAGE_FILE}" ]; then
   echo "ERROR: Coverage file not found."
@@ -17,11 +13,10 @@ if [ ! -f "${COVERAGE_FILE}" ]; then
 fi
 
 # available for master builds
-if [ -z "$CIRCLE_PR_NUMBER" ]
-then
+if [[ "${BUILD_REASON}" != "PullRequest" ]]; then
   echo "Uploading coverage report..."
 
-  BRANCH_NAME="${CIRCLE_BRANCH}"
+  BRANCH_NAME="${BUILD_SOURCEBRANCH/refs\/heads\//}"
   COVERAGE_DIR="$(dirname "${COVERAGE_FILE}")"
   GCS_LOCATION="envoy-coverage/report-${BRANCH_NAME}"
 
@@ -31,3 +26,10 @@ then
 else
   echo "Coverage report will not be uploaded for this build."
 fi
+
+pip3 install -I https://github.com/lizan/lcov-to-cobertura-xml/archive/e4c27b8dc3e63cbb6d39f7bedfe2e1e6bec625f2.tar.gz
+
+COVERAGE_DATA="${ENVOY_BUILD_DIR}/generated/coverage/coverage.dat"
+COVERAGE_XML="${ENVOY_BUILD_DIR}/coverage.xml"
+
+python3 -m lcov_cobertura -b ${PWD} ${COVERAGE_DATA} -o ${COVERAGE_XML}
