7bf7744add5d51b1fda3a5a09ef50171939048e5
Lizan Zhou <lizan@tetrate.io>
2020-05-13 20:06:43 +0000

    copy corpus recursively
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- bazel/coverage/fuzz_coverage_wrapper.sh
+++ bazel/coverage/fuzz_coverage_wrapper.sh
@@ -6,7 +6,7 @@ TEST_BINARY=$1
 shift
 
 mkdir -p fuzz_corpus/seed_corpus
-cp $@ fuzz_corpus/seed_corpus
+cp -r $@ fuzz_corpus/seed_corpus
 
 LLVM_PROFILE_FILE= ${TEST_BINARY} fuzz_corpus -max_total_time=60
 
