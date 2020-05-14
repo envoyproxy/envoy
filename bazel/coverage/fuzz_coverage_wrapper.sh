1aa9f8676a2627ae4f27d386add812228679cd0b
Lizan Zhou <lizan@tetrate.io>
2020-05-14 19:39:22 +0000

    collect coverage at the beginning
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- bazel/coverage/fuzz_coverage_wrapper.sh
+++ bazel/coverage/fuzz_coverage_wrapper.sh
@@ -5,10 +5,10 @@ set -ex
 TEST_BINARY=$1
 shift
 
+# Clear existing corpus if previous run wasn't in sandbox
+rm -rf fuzz_corpus
+
 mkdir -p fuzz_corpus/seed_corpus
 cp -r $@ fuzz_corpus/seed_corpus
 
-# Don't collect coverage when generating corpus
-LLVM_PROFILE_FILE= ${TEST_BINARY} fuzz_corpus -seed=${FUZZ_CORPUS_SEED:-1} -max_total_time=${FUZZ_CORPUS_TIME:-60}
-
-${TEST_BINARY} fuzz_corpus -runs=0
+${TEST_BINARY} fuzz_corpus -seed=${FUZZ_CORPUS_SEED:-1} -max_total_time=${FUZZ_CORPUS_TIME:-60}
