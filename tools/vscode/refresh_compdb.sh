45f0d1c068c8f84ba72005e2720cb76578639b3b
Lizan Zhou <lizan@tetrate.io>
2020-05-13 19:15:55 -0700

    allow refresh compdb even there's compile error
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- tools/vscode/refresh_compdb.sh
+++ tools/vscode/refresh_compdb.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-TEST_TMPDIR=${TEST_TMPDIR:-/tmp}/compdb tools/gen_compilation_database.py --run_bazel_build
+# Setting platform suffix here so the compdb headers won't be overwritten by another bazel run
+BAZEL_BUILD_OPTIONS=--platform_suffix=-compdb tools/gen_compilation_database.py --run_bazel_build -k
 
 # Kill clangd to reload the compilation database
 killall -v /opt/llvm/bin/clangd
