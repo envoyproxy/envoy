0c06e1b67c050434c9a0000ecffe9dc2e70a8989
mrSeoDavid <seodavid221@gmail.com>
2020-05-15 15:49:05 -0700

    Fix just for DCO error #2
    Signed-off-by: mrSeoDavid <seodavid221@gmail.com>

--- source/extensions/filters/listener/proxy_protocol/proxy_protocol.cc
+++ source/extensions/filters/listener/proxy_protocol/proxy_protocol.cc
@@ -124,14 +124,14 @@ ProxyState Filter::parseV2Header(char* buf) {
   size_t hdr_addr_len = (upper_byte << 8) + lower_byte;
 
   if ((ver_cmd & 0xf) == PROXY_PROTO_V2_LOCAL) {
-    // This is locally-initiated, e.g. health-check, and should not override remote address
+    // This is locally-initiated, e.g. health-check, and should not override remote address.
     proxy_protocol_header_.emplace(WireHeader{hdr_addr_len});
     return ProxyState::Done;
   }
 
   // Only do connections on behalf of another user, not internally-driven health-checks. If
   // its not on behalf of someone, or its not AF_INET{6} / STREAM/DGRAM, ignore and
-  // use the real-remote info
+  // use the real-remote info.
   if ((ver_cmd & 0xf) == PROXY_PROTO_V2_ONBEHALF_OF) {
     uint8_t proto_family = buf[PROXY_PROTO_V2_SIGNATURE_LEN + 1];
     if (((proto_family & 0x0f) == PROXY_PROTO_V2_TRANSPORT_STREAM) ||
