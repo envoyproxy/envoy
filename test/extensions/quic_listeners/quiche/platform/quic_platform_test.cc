bd810939bd0f409c07391ec5729cbe88e8bd010f
Dmitri Dolguikh <ddolguik@redhat.com>
2020-05-27 13:46:32 -0700

    Fixed formatting
    Signed-off-by: Dmitri Dolguikh <ddolguik@redhat.com>

--- test/extensions/quic_listeners/quiche/platform/quic_platform_test.cc
+++ test/extensions/quic_listeners/quiche/platform/quic_platform_test.cc
@@ -239,7 +239,7 @@ TEST_F(QuicPlatformTest, QuicServerStats) {
 }
 
 TEST_F(QuicPlatformTest, QuicStackTraceTest) {
-#if !defined(ENVOY_CONFIG_COVERAGE) && !defined(GCC_COMPILER) 
+#if !defined(ENVOY_CONFIG_COVERAGE) && !defined(GCC_COMPILER)
   // This doesn't work in coverage build because part of the stacktrace will be overwritten by
   // __llvm_coverage_mapping
   // Stack trace under gcc with optimizations on (-c opt) doesn't include the test name
