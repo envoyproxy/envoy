f78aa3dcd0cae5657e935b921cc6a362730044e2
Lizan Zhou <lizan@tetrate.io>
2020-05-29 14:05:45 -0700

    add skip_proto_format
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- tools/vscode/refresh_compdb.sh
+++ tools/vscode/refresh_compdb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-tools/proto_format/proto_format.sh fix
+[[ -z "${SKIP_PROTO_FORMAT}" ]] && tools/proto_format/proto_format.sh fix
 
 # Setting TEST_TMPDIR here so the compdb headers won't be overwritten by another bazel run
 TEST_TMPDIR=${BUILD_DIR:-/tmp}/envoy-compdb tools/gen_compilation_database.py --run_bazel_build -k
