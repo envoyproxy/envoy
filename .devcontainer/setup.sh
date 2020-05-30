3612d04dff2aee717bbd21429adb0d410217307d
Lizan Zhou <lizan@tetrate.io>
2020-05-30 14:19:31 -0700

    devex: several fix for devcontainer (#11342)
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,8 +8,11 @@ BAZELRC_FILE=~/.bazelrc bazel/setup_clang.sh /opt/llvm
 # Use generated toolchain config because we know the base container is the one we're using in RBE.
 # Not using libc++ here because clangd will raise some tidy issue in libc++ header as of version 9.
 echo "build --config=rbe-toolchain-clang" >> ~/.bazelrc
-echo "build --symlink_prefix=/" >> ~/.bazelrc
 echo "build ${BAZEL_BUILD_EXTRA_OPTIONS}" | tee -a ~/.bazelrc
-echo "startup --output_base=/build/tmp"
+
+# Ideally we want this line so bazel doesn't pollute things outside of the devcontainer, but some of
+# API tooling (proto_sync) depends on symlink like bazel-bin.
+# TODO(lizan): Fix API tooling and enable this again
+#echo "build --symlink_prefix=/" >> ~/.bazelrc
 
 [[ ! -z "${BUILD_DIR}" ]] && sudo chown -R "$(id -u):$(id -g)" ${BUILD_DIR}
