e464ba40df98a45615889d6de3545556525e4861
Lizan Zhou <lizan@tetrate.io>
2020-06-02 14:24:56 -0700

    ci: opensuse outage workaround
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- .azure-pipelines/cleanup.sh
+++ .azure-pipelines/cleanup.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Workaround suggested by https://github.com/actions/virtual-environments/issues/987
+sudo rm -rf /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+
 # Temporary script to remove tools from Azure pipelines agent to create more disk space room.
 sudo apt-get update -y
 sudo apt-get purge -y --no-upgrade 'ghc-*' 'zulu-*-azure-jdk' 'libllvm*' 'mysql-*' 'dotnet-*' 'libgl1'
