d2db3222ddff3b0d3a6c8eaa9b9769999a9beb36
Wayne Zhang <qiwzhang@google.com>
2020-06-03 07:48:08 -0700

    grpc_transcoder: implement Skip function in transcoder input stream (#11387)
    Skip function in Buffer::ZeroCopyInputStream is not implemented. It is used in grpc transcoder filter.
    
    Signed-off-by: Wayne Zhang <qiwzhang@google.com>
    Signed-off-by: yashwant121 <yadavyashwant36@gmail.com>

--- test/extensions/filters/http/grpc_json_transcoder/grpc_json_transcoder_integration_test.cc
+++ test/extensions/filters/http/grpc_json_transcoder/grpc_json_transcoder_integration_test.cc
@@ -809,7 +809,9 @@ TEST_P(GrpcJsonTranscoderIntegrationTest, LargeStruct) {
       R"({"content":)" + largeJson + R"(})");
 }
 
-TEST_P(GrpcJsonTranscoderIntegrationTest, UnknownField) {
+TEST_P(GrpcJsonTranscoderIntegrationTest, UnknownFieldInRequest) {
+  // Request JSON has many fields that are unknown to the request proto message.
+  // They are discarded.
   HttpIntegrationTest::initialize();
   testTranscoding<bookstore::CreateShelfRequest, bookstore::Shelf>(
       Http::TestRequestHeaderMapImpl{{":method", "POST"},
@@ -823,6 +825,33 @@ TEST_P(GrpcJsonTranscoderIntegrationTest, UnknownField) {
                                       {"content-length", "30"},
                                       {"grpc-status", "0"}},
       R"({"id":"20","theme":"Children"})");
+}
+
+// Test proto to json transcoding with an unknown field in the response message.
+// gRPC server may use a updated proto with a new field, but Envoy transcoding
+// filter could use an old proto descriptor without that field. That fields is unknown
+// to the Envoy transcoder filter. Expected result: the unknown field is discarded,
+// other fields should be transcoded properly.
+TEST_P(GrpcJsonTranscoderIntegrationTest, UnknownResponse) {
+  // The mocked upstream proto response message is bookstore::BigBook which has
+  // all 3 fields. But the proto descriptor used by the Envoy transcoder filter is using
+  // bookstore::OldBigBook which is missing the `field1` field.
+  HttpIntegrationTest::initialize();
+  // The bug is ZeroCopyInputStreamImpl::Skip() which is not implemented.
+  // In order to trigger a call to that function, the response message has to be big enough
+  // so it is stored in multiple slices.
+  const std::string field1_value = std::string(32 * 1024, 'O');
+  const std::string response_body =
+      fmt::format(R"(field1: "{}" field2: "field2_value" field3: "field3_value" )", field1_value);
+  testTranscoding<Empty, bookstore::BigBook>(
+      Http::TestRequestHeaderMapImpl{
+          {":method", "GET"}, {":path", "/bigbook"}, {":authority", "host"}},
+      "", {""}, {response_body}, Status(),
+      Http::TestResponseHeaderMapImpl{{":status", "200"},
+                                      {"content-type", "application/json"},
+                                      {"content-length", "49"},
+                                      {"grpc-status", "0"}},
+      R"({"field2":"field2_value","field3":"field3_value"})");
 }
 
 TEST_P(GrpcJsonTranscoderIntegrationTest, UTF8) {
