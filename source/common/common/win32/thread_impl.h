7f36e99fbbcd74c0295abd49b6a6371c2185bec0
Joshua Marantz <jmarantz@google.com>
2020-06-04 15:14:28 -0400

    re-expose the win32 thread class.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/common/common/win32/thread_impl.h
+++ source/common/common/win32/thread_impl.h
@@ -9,6 +9,28 @@ namespace Envoy {
 namespace Thread {
 
 /**
+ * Wrapper for a win32 thread. We don't use std::thread because it eats exceptions and leads to
+ * unusable stack traces.
+ */
+class ThreadImplWin32 : public Thread {
+public:
+  ThreadImplWin32(std::function<void()> thread_routine, OptionsOptConstRef options);
+  ~ThreadImplWin32();
+
+  // Thread::Thread
+  void join() override;
+  std::string name() const override { return name_; }
+
+  // Needed for WatcherImpl for the QueueUserAPC callback context
+  HANDLE handle() const { return thread_handle_; }
+
+private:
+  std::function<void()> thread_routine_;
+  HANDLE thread_handle_;
+  std::string name_;
+};
+
+/**
  * Implementation of ThreadFactory
  */
 class ThreadFactoryImplWin32 : public ThreadFactory {
