3a7cf3b2243ae009807af8b845bba4ac7e66f985
asraa <asraa@google.com>
2020-06-04 15:40:18 -0400

    fix all filter fuzz bugs (#11422)
    Closes fuzz bugs related to configuration and test issues in filter_fuzz_test
    
    * Require that encodeComplete() is called after end_stream/trailers per the HCM/filter contract. Otherwise, you run into destruction issues.
    * TAP configuration can't use unimplemented TapDS configuration. Had to enforce in code because it's a oneof, which can only be constrained to be required
    * Squash filter configuration requires some JSON attachment template. Scrubbed if invalid JSON
    
    Risk Level: Low
    Testing: Regression corpuses added, fuzzer runs for a 2 minutes
    Fixes OSS-fuzz issues:
    * https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=18938
    * https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=20844
    * https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=22428
    
    Signed-off-by: Asra Ali <asraa@google.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/extensions/filters/http/common/fuzz/uber_per_filter.cc
+++ test/extensions/filters/http/common/fuzz/uber_per_filter.cc
@@ -1,5 +1,8 @@
 #include "envoy/extensions/filters/http/grpc_json_transcoder/v3/transcoder.pb.h"
+#include "envoy/extensions/filters/http/squash/v3/squash.pb.h"
+#include "envoy/extensions/filters/http/tap/v3/tap.pb.h"
 
+#include "extensions/filters/http/common/utility.h"
 #include "extensions/filters/http/well_known_names.h"
 
 #include "test/extensions/filters/http/common/fuzz/uber_filter.h"
@@ -70,11 +73,39 @@ void UberFilterFuzzer::guideAnyProtoType(test::fuzz::HttpData* mutable_data, uin
   mutable_any->set_type_url(type_url);
 }
 
+void cleanAttachmentTemplate(Protobuf::Message* message) {
+  envoy::extensions::filters::http::squash::v3::Squash& config =
+      dynamic_cast<envoy::extensions::filters::http::squash::v3::Squash&>(*message);
+  std::string json;
+  Protobuf::util::JsonPrintOptions json_options;
+  if (!Protobuf::util::MessageToJsonString(config.attachment_template(), &json, json_options)
+           .ok()) {
+    config.clear_attachment_template();
+  }
+}
+
+void cleanTapConfig(Protobuf::Message* message) {
+  envoy::extensions::filters::http::tap::v3::Tap& config =
+      dynamic_cast<envoy::extensions::filters::http::tap::v3::Tap&>(*message);
+  if (config.common_config().config_type_case() ==
+      envoy::extensions::common::tap::v3::CommonExtensionConfig::ConfigTypeCase::kTapdsConfig) {
+    config.mutable_common_config()->mutable_static_config()->mutable_match_config()->set_any_match(
+        true);
+  }
+}
+
 void UberFilterFuzzer::cleanFuzzedConfig(absl::string_view filter_name,
                                          Protobuf::Message* message) {
+  const std::string name = Extensions::HttpFilters::Common::FilterNameUtil::canonicalFilterName(
+      std::string(filter_name));
   // Map filter name to clean-up function.
-  if (filter_name == HttpFilterNames::get().GrpcJsonTranscoder) {
+  if (name == HttpFilterNames::get().GrpcJsonTranscoder) {
     addBookstoreProtoDescriptor(message);
+  } else if (name == HttpFilterNames::get().Squash) {
+    cleanAttachmentTemplate(message);
+  } else if (name == HttpFilterNames::get().Tap) {
+    // TapDS oneof field not implemented.
+    cleanTapConfig(message);
   }
 }
 
