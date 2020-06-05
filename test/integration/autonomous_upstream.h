1123eefcf3a304ae61986613a70f198077d52067
Tony Allen <tony@allen.gg>
2020-06-05 14:21:33 -0700

    remove autonomous upstream changes
    Signed-off-by: Tony Allen <tony@allen.gg>

--- test/integration/autonomous_upstream.h
+++ test/integration/autonomous_upstream.h
@@ -56,7 +56,6 @@ public:
                      bool allow_incomplete_streams)
       : FakeUpstream(address, type, time_system),
         allow_incomplete_streams_(allow_incomplete_streams),
-        response_trailers_(std::make_unique<Http::TestResponseTrailerMapImpl>()),
         response_headers_(std::make_unique<Http::TestResponseHeaderMapImpl>(
             Http::TestResponseHeaderMapImpl({{":status", "200"}}))) {}
 
@@ -65,7 +64,6 @@ public:
                      Event::TestTimeSystem& time_system, bool allow_incomplete_streams)
       : FakeUpstream(std::move(transport_socket_factory), port, type, version, time_system),
         allow_incomplete_streams_(allow_incomplete_streams),
-        response_trailers_(std::make_unique<Http::TestResponseTrailerMapImpl>()),
         response_headers_(std::make_unique<Http::TestResponseHeaderMapImpl>(
             Http::TestResponseHeaderMapImpl({{":status", "200"}}))) {}
 
@@ -79,16 +77,13 @@ public:
 
   void setLastRequestHeaders(const Http::HeaderMap& headers);
   std::unique_ptr<Http::TestRequestHeaderMapImpl> lastRequestHeaders();
-  void setResponseTrailers(std::unique_ptr<Http::TestResponseTrailerMapImpl>&& response_trailers);
   void setResponseHeaders(std::unique_ptr<Http::TestResponseHeaderMapImpl>&& response_headers);
-  Http::TestResponseTrailerMapImpl responseTrailers();
   Http::TestResponseHeaderMapImpl responseHeaders();
   const bool allow_incomplete_streams_{false};
 
 private:
   Thread::MutexBasicLockable headers_lock_;
   std::unique_ptr<Http::TestRequestHeaderMapImpl> last_request_headers_;
-  std::unique_ptr<Http::TestResponseTrailerMapImpl> response_trailers_;
   std::unique_ptr<Http::TestResponseHeaderMapImpl> response_headers_;
   std::vector<AutonomousHttpConnectionPtr> http_connections_;
   std::vector<SharedConnectionWrapperPtr> shared_connections_;
