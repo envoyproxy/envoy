62a45df20d8322177e63e980444fd49ff3771f21
Snow Pettersen <kpettersen@netflix.com>
2020-06-06 14:11:59 -0700

    test: print out what to set ip version env variable to
    Also includes a minor cleanup where we remove an unneccessary
    const-qualifier from a return type.
    
    Signed-off-by: Snow Pettersen <kpettersen@netflix.com>

--- test/test_common/environment.cc
+++ test/test_common/environment.cc
@@ -243,10 +243,11 @@ std::vector<Network::Address::IpVersion> TestEnvironment::getIpVersionsForTest()
     if (TestEnvironment::shouldRunTestForIpVersion(version)) {
       parameters.push_back(version);
       if (!Network::Test::supportsIpVersion(version)) {
+        const auto version_string = Network::Test::addressVersionAsString(version);
         ENVOY_LOG_TO_LOGGER(Logger::Registry::getLog(Logger::Id::testing), warn,
                             "Testing with IP{} addresses may not be supported on this machine. If "
-                            "testing fails, set the environment variable ENVOY_IP_TEST_VERSIONS.",
-                            Network::Test::addressVersionAsString(version));
+                            "testing fails, set the environment variable ENVOY_IP_TEST_VERSIONS to 'v{}only'.",
+                            version_string, version_string);
       }
     }
   }
