6679d57dd218b369fb39068177b25977a857f355
Florin Coras <fcoras@cisco.com>
2020-06-08 13:25:05 -0700

    network: move socket type from address to socket (#11486)
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- source/extensions/stat_sinks/common/statsd/statsd.cc
+++ source/extensions/stat_sinks/common/statsd/statsd.cc
@@ -29,7 +29,7 @@ namespace Statsd {
 
 UdpStatsdSink::WriterImpl::WriterImpl(UdpStatsdSink& parent)
     : parent_(parent), io_handle_(Network::SocketInterfaceSingleton::get().socket(
-                           Network::Address::SocketType::Datagram, parent_.server_address_)) {}
+                           Network::Socket::Type::Datagram, parent_.server_address_)) {}
 
 void UdpStatsdSink::WriterImpl::write(const std::string& message) {
   // TODO(mattklein123): We can avoid this const_cast pattern by having a constant variant of
