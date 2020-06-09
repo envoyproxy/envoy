b889da6513718b9b1ff54d1457544f17528fa960
Konstantin Belyalov <arsenicus@mail.ru>
2020-06-09 11:23:48 -0600

    Simplify: remove redundant itests, fix one itest
    Signed-off-by: Konstantin Belyalov <arsenicus@mail.ru>

--- test/extensions/filters/http/grpc_json_transcoder/grpc_json_transcoder_integration_test.cc
+++ test/extensions/filters/http/grpc_json_transcoder/grpc_json_transcoder_integration_test.cc
@@ -330,6 +330,7 @@ TEST_P(GrpcJsonTranscoderIntegrationTest, UnaryGetHttpBody) {
 TEST_P(GrpcJsonTranscoderIntegrationTest, StreamGetHttpBody) {
   HttpIntegrationTest::initialize();
 
+  // 1. Normal streaming get
   testTranscoding<Empty, google::api::HttpBody>(
       Http::TestRequestHeaderMapImpl{
           {":method", "GET"}, {":path", "/indexStream"}, {":authority", "host"}},
@@ -339,6 +340,14 @@ TEST_P(GrpcJsonTranscoderIntegrationTest, StreamGetHttpBody) {
       Status(), Http::TestResponseHeaderMapImpl{{":status", "200"}, {"content-type", "text/html"}},
       R"(<h1>Hello!</h1>)"
       R"(Hello!)");
+
+  // 2. Empty response (trailers only) from streaming backend, with a gRPC error.
+  testTranscoding<Empty, google::api::HttpBody>(
+      Http::TestRequestHeaderMapImpl{
+          {":method", "GET"}, {":path", "/indexStream"}, {":authority", "host"}},
+      "", {""}, {}, Status(Code::NOT_FOUND, "Not Found"),
+      Http::TestResponseHeaderMapImpl{{":status", "404"}, {"content-type", "application/json"}},
+      "");
 }
 
 TEST_P(GrpcJsonTranscoderIntegrationTest, StreamGetHttpBodyMultipleFramesInData) {
@@ -432,62 +441,6 @@ TEST_P(GrpcJsonTranscoderIntegrationTest, StreamGetHttpBodyFragmented) {
   // As well as content-type header
   auto content_type = response->headers().get(Http::LowerCaseString("content-type"));
   EXPECT_EQ("text/plain", content_type->value().getStringView());
-}
-
-TEST_P(GrpcJsonTranscoderIntegrationTest, StreamTrailersOnlyResponse) {
-  HttpIntegrationTest::initialize();
-
-  // Make request to gRPC upstream
-  codec_client_ = makeHttpConnection(lookupPort("http"));
-  auto response = codec_client_->makeHeaderOnlyRequest(Http::TestRequestHeaderMapImpl{
-      {":method", "GET"},
-      {":path", "/shelves/1/books"},
-      {":authority", "host"},
-  });
-  waitForNextUpstreamRequest();
-
-  // Make headers only response
-  Http::TestResponseHeaderMapImpl response_headers;
-  response_headers.setStatus(200);
-  response_headers.setContentType("application/grpc");
-  response_headers.setGrpcStatus(Code::NOT_FOUND);
-  response_headers.setGrpcMessage("JustError");
-  upstream_request_->encodeHeaders(response_headers, true);
-
-  // Wait for complete
-  response->waitForEndStream();
-  EXPECT_TRUE(response->complete());
-
-  // Ensure that grpc-status converted to http status for headers/trailers only response
-  EXPECT_EQ("404", response->headers().Status()->value().getStringView());
-}
-
-TEST_P(GrpcJsonTranscoderIntegrationTest, StreamTrailersOnlyResponseHttpBody) {
-  HttpIntegrationTest::initialize();
-
-  // Make request to gRPC upstream
-  codec_client_ = makeHttpConnection(lookupPort("http"));
-  auto response = codec_client_->makeHeaderOnlyRequest(Http::TestRequestHeaderMapImpl{
-      {":method", "GET"},
-      {":path", "/indexStream"},
-      {":authority", "host"},
-  });
-  waitForNextUpstreamRequest();
-
-  // Make headers only response
-  Http::TestResponseHeaderMapImpl response_headers;
-  response_headers.setStatus(200);
-  response_headers.setContentType("application/grpc");
-  response_headers.setGrpcStatus(Code::NOT_FOUND);
-  response_headers.setGrpcMessage("JustError");
-  upstream_request_->encodeHeaders(response_headers, true);
-
-  // Wait for complete
-  response->waitForEndStream();
-  EXPECT_TRUE(response->complete());
-
-  // Ensure that grpc-status converted to http status for headers/trailers only response
-  EXPECT_EQ("404", response->headers().Status()->value().getStringView());
 }
 
 TEST_P(GrpcJsonTranscoderIntegrationTest, UnaryEchoHttpBody) {
@@ -698,7 +651,7 @@ TEST_P(GrpcJsonTranscoderIntegrationTest, ServerStreamingGet) {
       Http::TestRequestHeaderMapImpl{
           {":method", "GET"}, {":path", "/shelves/37/books"}, {":authority", "host"}},
       "", {"shelf: 37"}, {}, Status(Code::NOT_FOUND, "Shelf 37 not found"),
-      Http::TestResponseHeaderMapImpl{{":status", "200"}, {"content-type", "application/json"}},
+      Http::TestResponseHeaderMapImpl{{":status", "404"}, {"content-type", "application/json"}},
       "[]");
 }
 
