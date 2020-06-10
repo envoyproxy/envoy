61cdc9601b9d8c4f1daea189ab827e950cd6a865
jianwen <jianwendong@google.com>
2020-06-10 16:04:35 -0500

    Renamed utility_fuzz_test.cc to get_sha_256_digest_fuzz_test.cc Added fuzz test for function importPublicKey() and added the corpus for it. Modified BUILD to match above changes.
    Signed-off-by: jianwen <jianwendong@google.com>

--- test/common/crypto/get_sha_256_digest_fuzz_test.cc
+++ test/common/crypto/get_sha_256_digest_fuzz_test.cc
@@ -9,6 +9,7 @@ namespace Fuzz {
 DEFINE_FUZZER(const uint8_t* buf, size_t len) {
   Buffer::OwnedImpl buffer(buf, len);
   auto digest = Common::Crypto::UtilitySingleton::get().getSha256Digest(buffer);
+  
 }
 
 } // namespace Fuzz
---
+++ test/common/crypto/import_public_key_fuzz_test.cc
@@ -0,0 +1,16 @@
+#include "common/buffer/buffer_impl.h"
+#include "common/crypto/utility.h"
+
+#include "test/fuzz/fuzz_runner.h"
+
+namespace Envoy {
+namespace Fuzz {
+
+DEFINE_FUZZER(const uint8_t* buf, size_t len) {
+  std::vector<uint8_t> key(buf, buf+len);
+  auto digest = Common::Crypto::UtilitySingleton::get().importPublicKey(key);
+  
+}
+
+} // namespace Fuzz
+} // namespace Envoy
