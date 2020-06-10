e61817fe9a2a7612d16414d718ab8dacf14c6e9b
Taras Galkovskyi <elimgta@google.com>
2020-06-10 09:29:36 -0400

    drop unneccessary include
    Signed-off-by: Taras Galkovskyi <elimgta@google.com>

--- source/extensions/transport_sockets/alts/config.cc
+++ source/extensions/transport_sockets/alts/config.cc
@@ -13,8 +13,6 @@
 #include "extensions/transport_sockets/alts/grpc_tsi.h"
 #include "extensions/transport_sockets/alts/tsi_socket.h"
 
-#include "grpc/grpc_security.h"
-
 #include "absl/strings/str_join.h"
 
 namespace Envoy {
