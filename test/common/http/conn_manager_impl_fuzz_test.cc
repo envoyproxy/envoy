b0445555668309abfe953c27a38a8ca68f26c1fe
jianwen <jianwendong@google.com>
2020-06-10 12:54:15 -0500

    fix style problem
    Signed-off-by: jianwen <jianwendong@google.com>

--- test/common/http/conn_manager_impl_fuzz_test.cc
+++ test/common/http/conn_manager_impl_fuzz_test.cc
@@ -90,11 +90,11 @@ public:
     EXPECT_CALL(*decoder_filter_, setDecoderFilterCallbacks(_));
     EXPECT_CALL(*encoder_filter_, setEncoderFilterCallbacks(_));
     EXPECT_CALL(filter_factory_, createUpgradeFilterChain("WebSocket", _, _))
-    .WillRepeatedly(Invoke([&](absl::string_view, const Http::FilterChainFactory::UpgradeMap*,
-                                FilterChainFactoryCallbacks& callbacks) -> bool {
-      filter_factory_.createFilterChain(callbacks);
-      return true;
-    }));
+        .WillRepeatedly(Invoke([&](absl::string_view, const Http::FilterChainFactory::UpgradeMap*,
+                                   FilterChainFactoryCallbacks& callbacks) -> bool {
+          filter_factory_.createFilterChain(callbacks);
+          return true;
+        }));
   }
 
   // Http::ConnectionManagerConfig
