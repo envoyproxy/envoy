ea4065cb1156a86fe51bd2bfd046aa71bf05a42e
Michael Rebello <me@michaelrebello.com>
2020-06-09 17:30:55 -0700

    swift: lazily compute {Request/Response}Headers properties (#897)
    Android's implementation lazily computes these properties, then stores them so that they are not recalculated each time they're accessed. Conversely, iOS recomputes them each time.
    
    This PR updates iOS to mirror Android's behavior since we actually don't need to recompute these each time since `Headers` types are immutable (modifications are done via builders).
    
    Signed-off-by: Michael Rebello <me@michaelrebello.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/swift/src/ResponseHeaders.swift
+++ mobile/library/swift/src/ResponseHeaders.swift
@@ -4,9 +4,8 @@ import Foundation
 @objcMembers
 public final class ResponseHeaders: Headers {
   /// HTTP status code received with the response.
-  public var httpStatus: Int? {
-    return self.value(forName: ":status")?.first.flatMap(Int.init)
-  }
+  public private(set) lazy var httpStatus: Int? =
+    self.value(forName: ":status")?.first.flatMap(Int.init)
 
   /// Convert the headers back to a builder for mutation.
   ///
