1a0ef25152fa4de7328bbfc15d388c0d0c931edc
Asra Ali <asraa@google.com>
2020-06-11 16:00:07 -0400

    only instrument coverage over successful corpus
    Signed-off-by: Asra Ali <asraa@google.com>

--- bazel/coverage/fuzz_coverage_wrapper.sh
+++ bazel/coverage/fuzz_coverage_wrapper.sh
@@ -11,4 +11,6 @@ rm -rf fuzz_corpus
 mkdir -p fuzz_corpus/seed_corpus
 cp -r $@ fuzz_corpus/seed_corpus
 
-${TEST_BINARY} fuzz_corpus -seed=${FUZZ_CORPUS_SEED:-1} -max_total_time=${FUZZ_CORPUS_TIME:-60}
+LLVM_PROFILE_FILE= ${TEST_BINARY} fuzz_corpus -seed=${FUZZ_CORPUS_SEED:-1} -max_total_time=${FUZZ_CORPUS_TIME:-60} || true
+
+${TEST_BINARY} fuzz_corpus -runs=0
