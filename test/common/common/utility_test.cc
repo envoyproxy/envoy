e5b8af67fcb6131344d9288ef46575a87e204bbe
jianwen <jianwendong@google.com>
2020-06-12 17:49:16 -0500

    DateFormatter.parse() timeout because of its O(n^2) time complexity The issue is in https://oss-fuzz.com/testcase-detail/5700644951556096(or https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=21682)
    It shows Timeout (exceeds 60 secs).
    
    This is because DateFormatter.parse() is costing O(n^2) time. The original implementation uses "string.replace" to generate a new string and to collect specifiers in while(regex_search) loop.
    However, the new string is completely unnecessary and will never not used after this function call. Each iteration in the loop, the regex needs to search for the next matched string from the beginning. This causes it to be O(n^2).
    
    My implementation is to loop on suffix. In each iteration, set `suffix=matched.suffix()` and continue searching from the end of the matched string. So when collecting specifiers, we need to use different indices there to make sure they still point to the same positions in the string after being formatted.
    
    After this,  the timeout test case passed. The running time of 400 layers cascading which cost 45 seconds in my local machine in the past, now can finish in around 300ms. The failed, timeout(exceeds 61s) test case now can pass in 525ms.
    
    Besides, I added a new unit test against long, messy string in utility_test.cc.
    
    Signed-off-by: jianwen <jianwendong@google.com>

--- test/common/common/utility_test.cc
+++ test/common/common/utility_test.cc
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <cmath>
 #include <cstdint>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -811,6 +812,19 @@ TEST(DateFormatter, FromTime) {
   const SystemTime time2(std::chrono::seconds(0));
   EXPECT_EQ("1970-01-01T00:00:00.000Z", DateFormatter("%Y-%m-%dT%H:%M:%S.000Z").fromTime(time2));
   EXPECT_EQ("aaa00", DateFormatter(std::string(3, 'a') + "%H").fromTime(time2));
+}
+
+//Check the time complexity. Make sure DateFormatter can finish parsing long messy string without crashing/freezing
+//Finish in 0-2 seconds if O(n). Finish in 30-120 seconds if O(n^2)
+TEST(DateFormatter, ParseLongString){
+  std::stringstream ss;
+  int num_duplicates=400;
+  std::string duplicate="%%1f %1f, %2f, %3f, %4f, ";
+  for(int i=0;i<num_duplicates;i++){
+    ss<<duplicate<<'(';
+  }
+  ss<<duplicate;
+  DateFormatter(ss.str());
 }
 
 // Verify that two DateFormatter patterns with the same ??? patterns but
