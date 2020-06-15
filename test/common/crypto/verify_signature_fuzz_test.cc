80cf82dc7a115210c90dad0c69d127aa33c04f23
jianwen <jianwendong@google.com>
2020-06-15 09:32:09 -0500

    Combined verify_signatrure with importPublicKey. Added the corresponding corpus and covered all related lines except line 61 in utility.cc. Added .proto for the test.
    Signed-off-by: jianwen <jianwendong@google.com>

---
+++ test/common/crypto/verify_signature_fuzz_test.cc
@@ -0,0 +1,43 @@
+#include "common/common/hex.h"
+#include "common/crypto/utility.h"
+#include "common/protobuf/utility.h"
+
+#include "test/common/crypto/verify_signature_fuzz.pb.validate.h"
+#include "test/fuzz/fuzz_runner.h"
+#include "test/test_common/utility.h"
+
+// #include "common/common/logger.h"
+
+namespace Envoy {
+namespace Common {
+namespace Crypto {
+namespace {
+
+DEFINE_PROTO_FUZZER(const test::common::crypto::VerifySignatureFuzzTestCase& input) {
+  // Exit early if the protobuf input does not satisfy the constraints.
+  try {
+    TestUtility::validate(input);
+  } catch (const ProtoValidationException& e) {
+    ENVOY_LOG_MISC(debug, "ProtoValidationException: {}", e.what());
+    return;
+  }
+
+  auto key = input.key();
+  auto hash_func = input.hash_func();
+  auto signature = input.signature();
+  auto data = input.data();
+
+  Common::Crypto::CryptoObjectPtr crypto_ptr(
+      Common::Crypto::UtilitySingleton::get().importPublicKey(Hex::decode(key)));
+  Common::Crypto::CryptoObject* crypto(crypto_ptr.get());
+
+  std::vector<uint8_t> text(data.begin(), data.end());
+
+  auto sig = Hex::decode(signature);
+  auto result = UtilitySingleton::get().verifySignature(hash_func, *crypto, sig, text);
+}
+
+} // namespace
+} // namespace Crypto
+} // namespace Common
+} // namespace Envoy
