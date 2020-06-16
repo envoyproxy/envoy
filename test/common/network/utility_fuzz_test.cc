b636586055238641dd56a20784091800ecb5ce86
Arthur Yan <arthuryan@google.com>
2020-06-16 20:27:37 +0000

    added fuzz test for protobufAddressToAddress (pipe)
    Signed-off-by: Arthur Yan <arthuryan@google.com>

--- test/common/network/utility_fuzz_test.cc
+++ test/common/network/utility_fuzz_test.cc
@@ -1,3 +1,5 @@
+#include "envoy/config/core/v3/address.pb.h"
+
 #include "common/network/utility.h"
 
 #include "test/fuzz/fuzz_runner.h"
@@ -29,6 +31,17 @@ DEFINE_FUZZER(const uint8_t* buf, size_t len) {
     std::list<Network::PortRange> port_range_list;
     try {
       Network::Utility::parsePortRangeList(string_buffer, port_range_list);
+    } catch (const EnvoyException& e) {
+      ENVOY_LOG_MISC(debug, "EnvoyException: {}", e.what());
+    }
+  }
+
+  {
+    const std::string string_buffer(reinterpret_cast<const char*>(buf), len);
+    envoy::config::core::v3::Address proto_address;
+    try {
+      proto_address.mutable_pipe()->set_path(string_buffer);
+      Network::Utility::protobufAddressToAddress(proto_address);
     } catch (const EnvoyException& e) {
       ENVOY_LOG_MISC(debug, "EnvoyException: {}", e.what());
     }
