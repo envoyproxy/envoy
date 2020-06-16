791c785505aa05e6c70d2939fb2d2efcedc2e3e2
jianwen <jianwendong@google.com>
2020-06-16 15:25:07 -0500

    Added catch block for the constructors and actions. Fixed the style
    Signed-off-by: jianwen <jianwendong@google.com>

--- test/extensions/filters/network/local_ratelimit/local_ratelimit_fuzz_test.cc
+++ test/extensions/filters/network/local_ratelimit/local_ratelimit_fuzz_test.cc
@@ -1,12 +1,16 @@
-#include "test/fuzz/fuzz_runner.h"
-#include "test/fuzz/utility.h"
-#include "test/extensions/filters/network/local_ratelimit/local_ratelimit_fuzz.pb.h"
-#include "test/extensions/filters/network/local_ratelimit/local_ratelimit_fuzz.pb.validate.h"
+#include <memory>
+
 #include "envoy/extensions/filters/network/local_ratelimit/v3/local_rate_limit.pb.h"
-#include "common/stats/isolated_store_impl.h"
+
 #include "common/buffer/buffer_impl.h"
+#include "common/stats/isolated_store_impl.h"
+
 #include "extensions/filters/network/local_ratelimit/local_ratelimit.h"
 
+#include "test/extensions/filters/network/local_ratelimit/local_ratelimit_fuzz.pb.h"
+#include "test/extensions/filters/network/local_ratelimit/local_ratelimit_fuzz.pb.validate.h"
+#include "test/fuzz/fuzz_runner.h"
+#include "test/fuzz/utility.h"
 #include "test/mocks/event/mocks.h"
 #include "test/mocks/network/mocks.h"
 #include "test/mocks/runtime/mocks.h"
@@ -23,7 +27,7 @@ namespace Envoy {
 namespace Extensions {
 namespace NetworkFilters {
 namespace LocalRateLimitFilter {
-  struct ActiveFilter {
+struct ActiveFilter {
   ActiveFilter(const ConfigSharedPtr& config) : filter_(config) {
     filter_.initializeReadFilterCallbacks(read_filter_callbacks_);
   }
@@ -32,8 +36,9 @@ namespace LocalRateLimitFilter {
   Filter filter_;
 };
 
-DEFINE_PROTO_FUZZER(const envoy::extensions::filters::network::local_ratelimit::LocalRateLimitTestCase& input) {
-  
+DEFINE_PROTO_FUZZER(
+    const envoy::extensions::filters::network::local_ratelimit::LocalRateLimitTestCase& input) {
+
   try {
     TestUtility::validate(input);
   } catch (const ProtoValidationException& e) {
@@ -42,53 +47,53 @@ DEFINE_PROTO_FUZZER(const envoy::extensions::filters::network::local_ratelimit::
   } catch (const ProtobufMessage::DeprecatedProtoFieldException& e) {
     ENVOY_LOG_MISC(debug, "DeprecatedProtoFieldException: {}", e.what());
     return;
-  } 
-  if(input.config().token_bucket().fill_interval().nanos()<0){
-      ENVOY_LOG_MISC(debug, "Nanos should not be negative!");
-      return;
   }
-
-  NiceMock<Event::MockDispatcher> dispatcher_;
-  Stats::IsolatedStoreImpl stats_store_;
-  NiceMock<Runtime::MockLoader> runtime_;
-  Event::MockTimer* fill_timer_=new Event::MockTimer(&dispatcher_);
-  ConfigSharedPtr config_;
-  ActiveFilter* active_filter;
- 
-  envoy::extensions::filters::network::local_ratelimit::v3::LocalRateLimit proto_config = input.config();
-  config_ = ::std::make_shared<Config>(proto_config, dispatcher_, stats_store_, runtime_);
-
-  try{
-    active_filter=new active_filter(config_);
-  }catch (const EnvoyException e){
-    ENVOY_LOG_MISC(debug, "EnvoyException in constructor of ActiveFilter: {}", e.what());
+  if (input.config().token_bucket().fill_interval().nanos() < 0) {
+    ENVOY_LOG_MISC(debug, "In fill_interval, nanos should not be negative!");
     return;
   }
 
-  std::chrono::milliseconds fill_interval_(PROTOBUF_GET_MS_REQUIRED(proto_config.token_bucket(), fill_interval));
+  try {
+    NiceMock<Event::MockDispatcher> dispatcher_;
+    Stats::IsolatedStoreImpl stats_store_;
+    NiceMock<Runtime::MockLoader> runtime_;
+    Event::MockTimer* fill_timer_ = new Event::MockTimer(&dispatcher_);
+    // auto fill_timer_=std::make_shared<Event::MockTimer>(&dispatcher_);
+    ConfigSharedPtr config_;
 
-  for (const auto& action : input.actions()) {
-    ENVOY_LOG_MISC(trace, "action {}", action.DebugString());
+    envoy::extensions::filters::network::local_ratelimit::v3::LocalRateLimit proto_config =
+        input.config();
+    config_ = ::std::make_shared<Config>(proto_config, dispatcher_, stats_store_, runtime_);
+    ActiveFilter active_filter(config_);
+    std::chrono::milliseconds fill_interval_(
+        PROTOBUF_GET_MS_REQUIRED(proto_config.token_bucket(), fill_interval));
 
-    switch (action.action_selector_case()) {
-    case envoy::extensions::filters::network::local_ratelimit::Action::kOnData: {
-      Buffer::OwnedImpl buffer(action.on_data().data());
-      active_filter->filter_.onData(buffer, action.on_data().end_stream());
-      break;
+    for (const auto& action : input.actions()) {
+      ENVOY_LOG_MISC(trace, "action {}", action.DebugString());
+
+      switch (action.action_selector_case()) {
+      case envoy::extensions::filters::network::local_ratelimit::Action::kOnData: {
+        Buffer::OwnedImpl buffer(action.on_data().data());
+        active_filter.filter_.onData(buffer, action.on_data().end_stream());
+        break;
+      }
+      case envoy::extensions::filters::network::local_ratelimit::Action::kOnNewConnection: {
+        active_filter.filter_.onNewConnection();
+        break;
+      }
+      case envoy::extensions::filters::network::local_ratelimit::Action::kRefill: {
+        EXPECT_CALL(*fill_timer_, enableTimer(fill_interval_, nullptr));
+        fill_timer_->invokeCallback();
+        break;
+      }
+      default:
+        // other actions
+        break;
+      }
     }
-    case envoy::extensions::filters::network::local_ratelimit::Action::kOnNewConnection: {
-      active_filter->filter_.onNewConnection();
-      break;
-    }
-    case envoy::extensions::filters::network::local_ratelimit::Action::kRefill:{
-      EXPECT_CALL(*fill_timer_, enableTimer(fill_interval_, nullptr));
-      fill_timer_->invokeCallback();
-      break;
-    }
-    default:
-      // Maybe nothing is set?
-      break;
-    }
+  } catch (const EnvoyException e) {
+    ENVOY_LOG_MISC(debug, "EnvoyException in fuzz test: {}", e.what());
+    return;
   }
 }
 
