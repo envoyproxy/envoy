b5da4c786690e44cbbc98152eeeecfe39cb95931
Arthur Yan <arthuryan@google.com>
2020-06-16 21:31:32 +0000

    added fuzz test for addressToProtobufAddress
    Signed-off-by: Arthur Yan <arthuryan@google.com>

--- test/common/network/utility_fuzz_test.cc
+++ test/common/network/utility_fuzz_test.cc
@@ -1,5 +1,6 @@
 #include "envoy/config/core/v3/address.pb.h"
 
+#include "common/network/address_impl.h"
 #include "common/network/utility.h"
 
 #include "test/fuzz/fuzz_runner.h"
@@ -56,6 +57,28 @@ DEFINE_FUZZER(const uint8_t* buf, size_t len) {
       proto_address.mutable_socket_address()->set_address(address_value);
       proto_address.mutable_socket_address()->set_port_value(port_value);
       Network::Utility::protobufAddressToAddress(proto_address);
+    } catch (const EnvoyException& e) {
+      ENVOY_LOG_MISC(debug, "EnvoyException: {}", e.what());
+    }
+  }
+
+  {
+    const std::string string_buffer(reinterpret_cast<const char*>(buf), len);
+    envoy::config::core::v3::Address proto_address;
+    try {
+      Network::Address::Ipv4Instance address(string_buffer);
+      Network::Utility::addressToProtobufAddress(address, proto_address);
+    } catch (const EnvoyException& e) {
+      ENVOY_LOG_MISC(debug, "EnvoyException: {}", e.what());
+    }
+  }
+
+  {
+    const std::string string_buffer(reinterpret_cast<const char*>(buf), len);
+    envoy::config::core::v3::Address proto_address;
+    try {
+      Network::Address::PipeInstance address(string_buffer);
+      Network::Utility::addressToProtobufAddress(address, proto_address);
     } catch (const EnvoyException& e) {
       ENVOY_LOG_MISC(debug, "EnvoyException: {}", e.what());
     }
