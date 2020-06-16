0b8f1d7d617c89ff2c9b661ac0b7db2a119f5448
Snow Pettersen <aickck@gmail.com>
2020-06-16 15:02:12 -0400

    http: make it possible to match upstream alpn with selected protocol (#10812)
    Updates the HTTP connection pools to specify the upstream ALPN when one is not specified
    through the TLS context. This simplifies configuration as Envoy can reuse the protocol selection
    logic to determine what ALPN to negotiate on upstream connections.
    
    Signed-off-by: Snow Pettersen <kpettersen@netflix.com>
    Signed-off-by: yashwant121 <yadavyashwant36@gmail.com>

--- source/extensions/transport_sockets/tls/context_impl.cc
+++ source/extensions/transport_sockets/tls/context_impl.cc
@@ -865,6 +865,18 @@ ClientContextImpl::ClientContextImpl(Stats::Scope& scope,
   }
 }
 
+bool ContextImpl::parseAndSetAlpn(const std::vector<std::string>& alpn, SSL& ssl) {
+  std::vector<uint8_t> parsed_alpn = parseAlpnProtocols(absl::StrJoin(alpn, ","));
+  if (!parsed_alpn.empty()) {
+    const int rc = SSL_set_alpn_protos(&ssl, parsed_alpn.data(), parsed_alpn.size());
+    // This should only if memory allocation fails, e.g. OOM.
+    RELEASE_ASSERT(rc == 0, Utility::getLastCryptoError().value_or(""));
+    return true;
+  }
+
+  return false;
+}
+
 bssl::UniquePtr<SSL> ClientContextImpl::newSsl(const Network::TransportSocketOptions* options) {
   bssl::UniquePtr<SSL> ssl_con(ContextImpl::newSsl(options));
 
@@ -882,14 +894,23 @@ bssl::UniquePtr<SSL> ClientContextImpl::newSsl(const Network::TransportSocketOpt
     SSL_set_verify(ssl_con.get(), SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, nullptr);
   }
 
-  if (options && !options->applicationProtocolListOverride().empty()) {
-    std::vector<uint8_t> parsed_override_alpn =
-        parseAlpnProtocols(absl::StrJoin(options->applicationProtocolListOverride(), ","));
-    if (!parsed_override_alpn.empty()) {
-      const int rc = SSL_set_alpn_protos(ssl_con.get(), parsed_override_alpn.data(),
-                                         parsed_override_alpn.size());
-      RELEASE_ASSERT(rc == 0, Utility::getLastCryptoError().value_or(""));
-    }
+  // We determine what ALPN using the following precedence:
+  // 1. Option-provided ALPN override.
+  // 2. ALPN statically configured in the upstream TLS context.
+  // 3. Option-provided ALPN fallback.
+
+  // At this point in the code the ALPN has already been set (if present) to the value specified in
+  // the TLS context. We've stored this value in parsed_alpn_protocols_ so we can check that to see
+  // if it's already been set.
+  bool has_alpn_defined = !parsed_alpn_protocols_.empty();
+  if (options) {
+    // ALPN override takes precedence over TLS context specified, so blindly overwrite it.
+    has_alpn_defined |= parseAndSetAlpn(options->applicationProtocolListOverride(), *ssl_con);
+  }
+
+  if (options && !has_alpn_defined && options->applicationProtocolFallback().has_value()) {
+    // If ALPN hasn't already been set (either through TLS context or override), use the fallback.
+    parseAndSetAlpn({*options->applicationProtocolFallback()}, *ssl_con);
   }
 
   if (allow_renegotiation_) {
