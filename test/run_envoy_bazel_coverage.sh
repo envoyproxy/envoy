74d635a33f58bdeea8721063f2130ada3506ffc8
Lizan Zhou <lizan@tetrate.io>
2020-06-18 20:17:07 -0700

    collect testlogs instead of print
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- test/run_envoy_bazel_coverage.sh
+++ test/run_envoy_bazel_coverage.sh
@@ -30,7 +30,11 @@ else
   BAZEL_BUILD_OPTIONS+=" --config=test-coverage --test_tag_filters=-nocoverage,-fuzz_target"
 fi
 
-bazel coverage ${BAZEL_BUILD_OPTIONS} --test_output=all ${COVERAGE_TARGETS}
+bazel coverage ${BAZEL_BUILD_OPTIONS} ${COVERAGE_TARGETS}
+
+# Collecting profile and testlogs
+[[ -z "${ENVOY_BUILD_PROFILE}" ]] || cp -f "$(bazel info output_base)/command.profile.gz" "${ENVOY_BUILD_PROFILE}/coverage.profile.gz" || true
+[[ -z "${ENVOY_BUILD_DIR}"]] || find bazel-testlogs/ -name test.log | tar zcf "${ENVOY_BUILD_DIR}/testlogs.tar.gz" -T -
 
 COVERAGE_DIR="${SRCDIR}"/generated/coverage
 
