3356969b691bf3ff0d17683226a6f3f43dc49c39
tengpeng <tengpeng.li2020@gmail.com>
2020-06-19 15:08:05 -0400

    1. fix format problem; 2. exit fuzz test when data length is 0; 3. add StatName and Element randomly.
    Signed-off-by: tengpeng <tengpeng.li2020@gmail.com>

--- test/common/stats/utility_fuzz_test.cc
+++ test/common/stats/utility_fuzz_test.cc
@@ -16,14 +16,21 @@ DEFINE_FUZZER(const uint8_t* buf, size_t len) {
     const absl::string_view string_buffer(reinterpret_cast<const char*>(buf), len);
     Stats::Utility::sanitizeStatsName(string_buffer);
   }
+  if (len < 1)
+    return;
 
   // generate a random number as the maximum length of the stat name
-  const size_t max_len = *reinterpret_cast<const uint8_t*>(buf) % len;
+  const size_t max_len = 1 + *reinterpret_cast<const uint8_t*>(buf) % len;
   {
     FuzzedDataProvider provider(buf, len);
 
-    // model common/stats/utility_test.cc, initilize those objects to create random elements as input
-    Stats::SymbolTablePtr symbol_table = Stats::SymbolTableCreator::makeSymbolTable();
+    // model common/stats/utility_test.cc, initilize those objects to create random elements as
+    // input
+    Stats::SymbolTablePtr symbol_table;
+    if (len % 2 == 1)
+      symbol_table = std::make_unique<Stats::FakeSymbolTableImpl>();
+    else
+      symbol_table = std::make_unique<Stats::SymbolTableImpl>();
     std::unique_ptr<Stats::IsolatedStoreImpl> store =
         std::make_unique<Stats::IsolatedStoreImpl>(*symbol_table);
     Stats::StatNamePool pool(*symbol_table);
@@ -38,15 +45,18 @@ DEFINE_FUZZER(const uint8_t* buf, size_t len) {
     } else {
       // add ranndom length string in each loop
       while (provider.remaining_bytes() > 0) {
-        size_t random = provider.ConsumeIntegral<uint8_t>();
-        if (random % 2 == 1) {
+        if (provider.ConsumeBool()) {
           std::string str = provider.ConsumeRandomLengthString(max_len);
-          ele_vec.push_back(Stats::DynamicName(str));
-          Stats::StatName sn = pool.add(str);
-          sn_vec.push_back(sn);
+          if (provider.ConsumeBool())
+            ele_vec.push_back(Stats::DynamicName(str));
+          if (provider.ConsumeBool()) {
+            Stats::StatName sn = pool.add(str);
+            sn_vec.push_back(sn);
+          }
         } else {
-          tags.push_back({pool.add(provider.ConsumeRandomLengthString(max_len)),
-                          pool.add(provider.ConsumeRandomLengthString(max_len))});
+          Stats::StatName key = pool.add(provider.ConsumeRandomLengthString(max_len));
+          Stats::StatName val = pool.add(provider.ConsumeRandomLengthString(max_len));
+          tags.push_back({key, val});
         }
         Stats::Utility::counterFromStatNames(*scope, sn_vec, tags);
         Stats::Utility::counterFromElements(*scope, ele_vec, tags);
