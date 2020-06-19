0e16ed873212bd9fe6e71edd8f870e9acdb2ec62
jianwen <jianwendong@google.com>
2020-06-19 17:37:50 -0500

    modify auto const& to const auto&. removed result for verifySignature().
    Signed-off-by: jianwen <jianwendong@google.com>

--- test/common/crypto/verify_signature_fuzz_test.cc
+++ test/common/crypto/verify_signature_fuzz_test.cc
@@ -10,10 +10,10 @@ namespace Crypto {
 namespace {
 
 DEFINE_PROTO_FUZZER(const test::common::crypto::VerifySignatureFuzzTestCase& input) {
-  auto const& key = input.key();
-  auto const& hash_func = input.hash_func();
-  auto const& signature = input.signature();
-  auto const& data = input.data();
+  const auto& key = input.key();
+  const auto& hash_func = input.hash_func();
+  const auto& signature = input.signature();
+  const auto& data = input.data();
 
   Common::Crypto::CryptoObjectPtr crypto_ptr(
       Common::Crypto::UtilitySingleton::get().importPublicKey(Hex::decode(key)));
@@ -21,8 +21,8 @@ DEFINE_PROTO_FUZZER(const test::common::crypto::VerifySignatureFuzzTestCase& inp
 
   std::vector<uint8_t> text(data.begin(), data.end());
 
-  auto sig = Hex::decode(signature);
-  auto result = UtilitySingleton::get().verifySignature(hash_func, *crypto, sig, text);
+  const auto sig = Hex::decode(signature);
+  UtilitySingleton::get().verifySignature(hash_func, *crypto, sig, text);
 }
 
 } // namespace
