3779b37423a3beffd23ef7ca4a49a56c9f27da87
Muge Chen <mugechen@google.com>
2020-06-19 14:55:02 +0000

    Remove unsed using statements
    Signed-off-by: Muge Chen <mugechen@google.com>

--- test/mocks/server/config_tracker.cc
+++ test/mocks/server/config_tracker.cc
@@ -10,13 +10,6 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
-using testing::_;
-using testing::Invoke;
-using testing::Return;
-using testing::ReturnPointee;
-using testing::ReturnRef;
-using testing::SaveArg;
-
 namespace Envoy {
 namespace Server {
 MockConfigTracker::MockConfigTracker() {
