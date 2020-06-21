fdd3765fef5163df3def8d13f1092c3b208c9792
Abhay Narayan Katare <abhay.katare@india.nec.com>
2020-06-22 01:38:52 +0530

    avoid boosting for parsefooFromV3yaml
    Signed-off-by: Abhay Narayan Katare <abhay.katare@india.nec.com>

--- test/test_common/utility.h
+++ test/test_common/utility.h
@@ -534,13 +534,21 @@ public:
   }
 
   static void loadFromYaml(const std::string& yaml, Protobuf::Message& message,
-                           bool preserve_original_type = false, bool avoid_boosting = true) {
+                           bool preserve_original_type, bool avoid_boosting) {
     if (avoid_boosting) {
       MessageUtil::loadFromYaml(yaml, message, ProtobufMessage::getStrictValidationVisitor(),
                                 false);
     } else {
       MessageUtil::loadFromYaml(yaml, message, ProtobufMessage::getStrictValidationVisitor());
     }
+    if (!preserve_original_type) {
+      Config::VersionConverter::eraseOriginalTypeInformation(message);
+    }
+  }
+
+  static void loadFromYaml(const std::string& yaml, Protobuf::Message& message,
+                           bool preserve_original_type = false) {
+    MessageUtil::loadFromYaml(yaml, message, ProtobufMessage::getStrictValidationVisitor());
     if (!preserve_original_type) {
       Config::VersionConverter::eraseOriginalTypeInformation(message);
     }
