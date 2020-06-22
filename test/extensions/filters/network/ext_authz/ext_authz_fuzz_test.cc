d4fd2fbd59c664d9718d53c9ade1d9b80190a291
jianwen <jianwendong@google.com>
2020-06-22 15:08:26 -0500

    removed local variable suffix "_" and modified test case
    Signed-off-by: jianwen <jianwendong@google.com>

--- test/extensions/filters/network/ext_authz/ext_authz_fuzz_test.cc
+++ test/extensions/filters/network/ext_authz/ext_authz_fuzz_test.cc
@@ -41,21 +41,21 @@ DEFINE_PROTO_FUZZER(const envoy::extensions::filters::network::ext_authz::ExtAut
     return;
   }
 
-  Stats::TestUtil::TestStore stats_store_;
-  Filters::Common::ExtAuthz::MockClient* client_ = new Filters::Common::ExtAuthz::MockClient();
+  Stats::TestUtil::TestStore stats_store;
+  Filters::Common::ExtAuthz::MockClient* client = new Filters::Common::ExtAuthz::MockClient();
   envoy::extensions::filters::network::ext_authz::v3::ExtAuthz proto_config = input.config();
 
-  ConfigSharedPtr config_ = std::make_shared<Config>(proto_config, stats_store_);
-  std::unique_ptr<Filter> filter_ =
-      std::make_unique<Filter>(config_, Filters::Common::ExtAuthz::ClientPtr{client_});
+  ConfigSharedPtr config = std::make_shared<Config>(proto_config, stats_store);
+  std::unique_ptr<Filter> filter =
+      std::make_unique<Filter>(config, Filters::Common::ExtAuthz::ClientPtr{client});
 
-  NiceMock<Network::MockReadFilterCallbacks> filter_callbacks_;
-  filter_->initializeReadFilterCallbacks(filter_callbacks_);
-  Network::Address::InstanceConstSharedPtr addr_ =
+  NiceMock<Network::MockReadFilterCallbacks> filter_callbacks;
+  filter->initializeReadFilterCallbacks(filter_callbacks);
+  Network::Address::InstanceConstSharedPtr addr =
       std::make_shared<Network::Address::PipeInstance>("/test/test.sock");
 
-  ON_CALL(filter_callbacks_.connection_, remoteAddress()).WillByDefault(ReturnRef(addr_));
-  ON_CALL(filter_callbacks_.connection_, localAddress()).WillByDefault(ReturnRef(addr_));
+  ON_CALL(filter_callbacks.connection_, remoteAddress()).WillByDefault(ReturnRef(addr));
+  ON_CALL(filter_callbacks.connection_, localAddress()).WillByDefault(ReturnRef(addr));
 
   for (const auto& action : input.actions()) {
     switch (action.action_selector_case()) {
@@ -64,16 +64,16 @@ DEFINE_PROTO_FUZZER(const envoy::extensions::filters::network::ext_authz::ExtAut
       if (action.on_data().has_result()) {
         switch (action.on_data().result().result_selector_case()) {
         case envoy::extensions::filters::network::ext_authz::Result::kCheckStatusOk: {
-          ON_CALL(*client_, check(_, _, _, _))
+          ON_CALL(*client, check(_, _, _, _))
               .WillByDefault(WithArgs<0>(
                   Invoke([&](Filters::Common::ExtAuthz::RequestCallbacks& callbacks) -> void {
                     callbacks.onComplete(
-                        makeAuthzResponse(Filters::Common::ExtAuthz::CheckStatus::OK));
+                        makeAuthzResponse(Filters::Common::ExtAuthz::CheckStatus::Error));
                   })));
           break;
         }
         case envoy::extensions::filters::network::ext_authz::Result::kCheckStatusError: {
-          ON_CALL(*client_, check(_, _, _, _))
+          ON_CALL(*client, check(_, _, _, _))
               .WillByDefault(WithArgs<0>(
                   Invoke([&](Filters::Common::ExtAuthz::RequestCallbacks& callbacks) -> void {
                     callbacks.onComplete(
@@ -82,7 +82,7 @@ DEFINE_PROTO_FUZZER(const envoy::extensions::filters::network::ext_authz::ExtAut
           break;
         }
         case envoy::extensions::filters::network::ext_authz::Result::kCheckStatusDenied: {
-          ON_CALL(*client_, check(_, _, _, _))
+          ON_CALL(*client, check(_, _, _, _))
               .WillByDefault(WithArgs<0>(
                   Invoke([&](Filters::Common::ExtAuthz::RequestCallbacks& callbacks) -> void {
                     callbacks.onComplete(
@@ -97,19 +97,19 @@ DEFINE_PROTO_FUZZER(const envoy::extensions::filters::network::ext_authz::ExtAut
         }
       }
       Buffer::OwnedImpl buffer(action.on_data().data());
-      filter_->onData(buffer, action.on_data().end_stream());
+      filter->onData(buffer, action.on_data().end_stream());
       break;
     }
     case envoy::extensions::filters::network::ext_authz::Action::kOnNewConnection: {
-      filter_->onNewConnection();
+      filter->onNewConnection();
       break;
     }
     case envoy::extensions::filters::network::ext_authz::Action::kRemoteClose: {
-      filter_callbacks_.connection_.raiseEvent(Network::ConnectionEvent::RemoteClose);
+      filter_callbacks.connection_.raiseEvent(Network::ConnectionEvent::RemoteClose);
       break;
     }
     case envoy::extensions::filters::network::ext_authz::Action::kLocalClose: {
-      filter_callbacks_.connection_.raiseEvent(Network::ConnectionEvent::LocalClose);
+      filter_callbacks.connection_.raiseEvent(Network::ConnectionEvent::LocalClose);
       break;
     }
     default: {
