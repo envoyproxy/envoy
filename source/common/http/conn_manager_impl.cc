dd373a532d3dac65ce0fd45606f6aa45ee8ba640
Ranjith Kumar <ranjith.dakshana2015@gmail.com>
2020-06-22 21:15:42 +0530

    check if host is not null
    Signed-off-by: Ranjith Kumar <ranjith.dakshana2015@gmail.com>

--- source/common/http/conn_manager_impl.cc
+++ source/common/http/conn_manager_impl.cc
@@ -771,10 +771,13 @@ void ConnectionManagerImpl::ActiveStream::decodeHeaders(RequestHeaderMapPtr&& he
   ScopeTrackerScopeState scope(this,
                                connection_manager_.read_callbacks_->connection().dispatcher());
   request_headers_ = std::move(headers);
-  connection_manager_.read_callbacks_->upstreamHost()
-      ->cluster()
-      .stats()
-      .upstream_rq_headers_size_.recordValue(request_headers_->byteSize());
+
+  if (connection_manager_.read_callbacks_->upstreamHost() != nullptr) {
+    connection_manager_.read_callbacks_->upstreamHost()
+        ->cluster()
+        .stats()
+        .upstream_rq_headers_size_.recordValue(request_headers_->byteSize());
+  }
 
   // Both saw_connection_close_ and is_head_request_ affect local replies: set
   // them as early as possible.
