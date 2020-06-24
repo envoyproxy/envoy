ad7002f362658ebc7de83fa444e58ce03c677207
Michael Rebello <me@michaelrebello.com>
2020-06-23 17:00:28 -0700

    ci: fix liveliness flakiness (#927)
    We've seen some flakiness on the iOS liveliness CI jobs which appear to be caused by the fact that the first response log from the hello world projects is not showing up when the script greps for `Response status (1): 200`. This PR aims to:
    
    - Fix these flakes by grepping for any 200 response in the logs (rather than specifically the first one)
    - Unify the logging and CI behavior for all 4 languages
    - Fix error messages not being displayed in the Objective-C hello world app
    
    Signed-off-by: Michael Rebello <me@michaelrebello.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/examples/objective-c/hello_world/Result.h
+++ mobile/examples/objective-c/hello_world/Result.h
@@ -2,8 +2,7 @@
 
 /// Represents a response from the server or an error.
 @interface Result : NSObject
-@property (nonatomic, assign) int identifier;
-@property (nonatomic, strong) NSString *body;
+@property (nonatomic, strong) NSString *message;
 @property (nonatomic, strong) NSString *serverHeader;
 @property (nonatomic, strong) NSString *error;
 @end
