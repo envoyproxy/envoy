f87e68f8a7eef471b1e12b8ef1f662ab845356fb
alyssawilk <alyssar@chromium.org>
2020-06-24 16:14:44 -0400

    upstream: cleaning up APIs (#11713)
    Risk Level: Low (API change)
    Testing: n/a
    Docs Changes: n/a
    Release Notes: n/a
    
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/extensions/upstreams/http/tcp/upstream_request_test.cc
+++ test/extensions/upstreams/http/tcp/upstream_request_test.cc
@@ -122,7 +122,7 @@ TEST_F(TcpConnPoolTest, Basic) {
   EXPECT_CALL(mock_pool_, newConnection(_)).WillOnce(Return(&cancellable_));
   conn_pool_->newStream(&mock_generic_callbacks_);
 
-  EXPECT_CALL(mock_generic_callbacks_, upstreamRequest());
+  EXPECT_CALL(mock_generic_callbacks_, upstreamToDownstream());
   EXPECT_CALL(mock_generic_callbacks_, onPoolReady(_, _, _, _));
   auto data = std::make_unique<NiceMock<Envoy::Tcp::ConnectionPool::MockConnectionData>>();
   EXPECT_CALL(*data, connection()).Times(AnyNumber()).WillRepeatedly(ReturnRef(connection));
