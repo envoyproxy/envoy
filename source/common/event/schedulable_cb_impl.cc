6cda16f665eb43f7578cd9acb4cf0fc6e3349a89
Antonio Vicente <avd@google.com>
2020-06-24 21:28:03 -0400

    Delay fd activation until the next itertion of the event loop. Protected by runtime feature "envoy.reloadable_features.activate_fds_next_event_loop".
    Signed-off-by: Antonio Vicente <avd@google.com>

--- source/common/event/schedulable_cb_impl.cc
+++ source/common/event/schedulable_cb_impl.cc
@@ -24,6 +24,11 @@ void SchedulableCallbackImpl::scheduleCallbackCurrentIteration() {
   event_active(&raw_event_, EV_TIMEOUT, 0);
 }
 
+void SchedulableCallbackImpl::scheduleCallbackNextIteration() {
+  const timeval zero_tv{};
+  event_add(&raw_event_, &zero_tv);
+}
+
 void SchedulableCallbackImpl::cancel() { event_del(&raw_event_); }
 
 bool SchedulableCallbackImpl::enabled() { return 0 != evtimer_pending(&raw_event_, nullptr); }
