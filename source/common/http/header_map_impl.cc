215456bdee2e4fc9f0aa494182a7139648018f10
Matt Klein <mklein@lyft.com>
2020-06-25 21:23:11 +0000

    comments
    Signed-off-by: Matt Klein <mklein@lyft.com>

--- source/common/http/header_map_impl.cc
+++ source/common/http/header_map_impl.cc
@@ -359,6 +359,8 @@ void HeaderMapImpl::addReferenceKey(const LowerCaseString& key, absl::string_vie
 }
 
 void HeaderMapImpl::addCopy(const LowerCaseString& key, uint64_t value) {
+  // In the case that the header is appended, we will perform a needless copy of the key and value.
+  // This is done on purpose to keep the code simple and should be rare.
   HeaderString new_key;
   new_key.setCopy(key.get());
   HeaderString new_value;
@@ -369,6 +371,8 @@ void HeaderMapImpl::addCopy(const LowerCaseString& key, uint64_t value) {
 }
 
 void HeaderMapImpl::addCopy(const LowerCaseString& key, absl::string_view value) {
+  // In the case that the header is appended, we will perform a needless copy of the key and value.
+  // This is done on purpose to keep the code simple and should be rare.
   HeaderString new_key;
   new_key.setCopy(key.get());
   HeaderString new_value;
@@ -434,11 +438,21 @@ const HeaderEntry* HeaderMapImpl::get(const LowerCaseString& key) const {
 }
 
 HeaderEntry* HeaderMapImpl::getExisting(const LowerCaseString& key) {
+  // Attempt a trie lookup first to see if the user is requesting an O(1) header. This may be
+  // relatively common in certain header matching / routing patterns.
+  // TODO(mattklein123): Add inline handle support directly to the header matcher code to support
+  // this use case more directly.
   auto lookup = staticLookup(key.get());
   if (lookup.has_value()) {
     return *lookup.value().entry_;
   }
 
+  // If the requested header is not an O(1) header we do a full scan. Doing the trie lookup is
+  // wasteful in the miss case, but is present for code consistency with other functions that do
+  // similar things.
+  // TODO(mattklein123): The full scan here and in remove() are the biggest issues with this
+  // implementation for certain use cases. We can either replace this with a totally different
+  // implementation or potentially create a lazy map if the size of the map is above a threshold.
   for (HeaderEntryImpl& header : headers_) {
     if (header.key() == key.get().c_str()) {
       return &header;
