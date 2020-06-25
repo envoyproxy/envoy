808d885482d6811b7b1b816a02c59f4fd9a1c68e
Ariane van der Steldt <avandersteldt@slack-corp.com>
2020-06-25 14:40:24 +0100

    Add a test case.
    Signed-off-by: Ariane van der Steldt <avandersteldt@slack-corp.com>

--- test/extensions/filters/http/header_to_metadata/header_to_metadata_filter_test.cc
+++ test/extensions/filters/http/header_to_metadata/header_to_metadata_filter_test.cc
@@ -463,6 +463,27 @@ request_rules:
   EXPECT_EQ(Http::FilterHeadersStatus::Continue, filter_->decodeHeaders(headers, false));
 }
 
+/**
+ * Missing case is not executed when header is present.
+ */
+TEST_F(HeaderToMetadataTest, NoMissingWhenHeaderIsPresent) {
+  const std::string config = R"EOF(
+request_rules:
+  - header: x-version
+    on_header_missing:
+      metadata_namespace: envoy.lb
+      key: version
+      value: some_value
+      type: STRING
+)EOF";
+  initializeFilter(config);
+  Http::TestRequestHeaderMapImpl headers{{"x-version", ""}};
+
+  EXPECT_CALL(decoder_callbacks_, streamInfo()).WillRepeatedly(ReturnRef(req_info_));
+  EXPECT_CALL(req_info_, setDynamicMetadata(_, _)).Times(0);
+  EXPECT_EQ(Http::FilterHeadersStatus::Continue, filter_->decodeHeaders(headers, false));
+}
+
 } // namespace HeaderToMetadataFilter
 } // namespace HttpFilters
 } // namespace Extensions
