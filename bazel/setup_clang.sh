d1456d990422c81ccacea05b9a93e852b4e328aa
Kotaro Inoue <kotaro@tetrate.io>
2020-06-26 17:25:48 +0900

    Add single quotes to all environment values that passed as arguments
    Signed-off-by: Kotaro Inoue <kotaro@tetrate.io>

--- bazel/setup_clang.sh
+++ bazel/setup_clang.sh
@@ -17,15 +17,15 @@ echo "# Generated file, do not edit. If you want to disable clang, just delete t
 build:clang --action_env='PATH=${PATH}'
 build:clang --action_env=CC=clang
 build:clang --action_env=CXX=clang++
-build:clang --action_env=LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config
-build:clang --repo_env=LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config
-build:clang --linkopt=-L$(llvm-config --libdir)
-build:clang --linkopt=-Wl,-rpath,$(llvm-config --libdir)
+build:clang --action_env='LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config'
+build:clang --repo_env='LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config'
+build:clang --linkopt='-L$(llvm-config --libdir)'
+build:clang --linkopt='-Wl,-rpath,$(llvm-config --libdir)'
 
 build:clang-asan --action_env=ENVOY_UBSAN_VPTR=1
 build:clang-asan --copt=-fsanitize=vptr,function
 build:clang-asan --linkopt=-fsanitize=vptr,function
-build:clang-asan --linkopt=-L${RT_LIBRARY_PATH}
+build:clang-asan --linkopt='-L${RT_LIBRARY_PATH}'
 build:clang-asan --linkopt=-l:libclang_rt.ubsan_standalone-x86_64.a
 build:clang-asan --linkopt=-l:libclang_rt.ubsan_standalone_cxx-x86_64.a
 " > ${BAZELRC_FILE}
