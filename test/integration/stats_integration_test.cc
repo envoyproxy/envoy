4dc4356cdaa81ea6e830f3ce6408d522e2c80e69
Joshua Marantz <jmarantz@google.com>
2020-06-26 13:18:21 -0400

    add more slack
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/integration/stats_integration_test.cc
+++ test/integration/stats_integration_test.cc
@@ -350,7 +350,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithRealSymbolTable) {
   // https://github.com/envoyproxy/envoy/blob/master/source/docs/stats.md#stats-memory-tests
   // for details on how to fix.
   EXPECT_MEMORY_EQ(m_per_cluster, 36603);
-  EXPECT_MEMORY_LE(m_per_cluster, 37000); // Round up to allow platform variations.
+  EXPECT_MEMORY_LE(m_per_cluster, 38000); // Round up to allow platform variations.
 }
 
 TEST_P(ClusterMemoryTestRunner, MemoryLargeHostSizeWithStats) {
@@ -392,7 +392,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeHostSizeWithStats) {
   // https://github.com/envoyproxy/envoy/blob/master/source/docs/stats.md#stats-memory-tests
   // for details on how to fix.
   EXPECT_MEMORY_EQ(m_per_host, 1380);
-  EXPECT_MEMORY_LE(m_per_host, 1700); // Round up to allow platform variations.
+  EXPECT_MEMORY_LE(m_per_host, 1800); // Round up to allow platform variations.
 }
 
 } // namespace
