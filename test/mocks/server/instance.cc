ba1be16c1b757a41319974f7ab8c3f7d0a86c0ec
Muge Chen <mugechen@google.com>
2020-06-29 06:44:37 +0000

    fix duplicate symbol error
    Signed-off-by: Muge Chen <mugechen@google.com>

--- test/mocks/server/instance.cc
+++ test/mocks/server/instance.cc
@@ -57,18 +57,33 @@ MockInstance::MockInstance()
 
 MockInstance::~MockInstance() = default;
 
+
 namespace Configuration {
 
-MockTransportSocketFactoryContext::MockTransportSocketFactoryContext()
-    : secret_manager_(std::make_unique<Secret::SecretManagerImpl>(config_tracker_)) {
+
+MockServerFactoryContext::MockServerFactoryContext()
+    : singleton_manager_(new Singleton::ManagerImpl(Thread::threadFactoryForTest())),
+      grpc_context_(scope_.symbolTable()) {
   ON_CALL(*this, clusterManager()).WillByDefault(ReturnRef(cluster_manager_));
+  ON_CALL(*this, dispatcher()).WillByDefault(ReturnRef(dispatcher_));
+  ON_CALL(*this, drainDecision()).WillByDefault(ReturnRef(drain_manager_));
+  ON_CALL(*this, localInfo()).WillByDefault(ReturnRef(local_info_));
+  ON_CALL(*this, random()).WillByDefault(ReturnRef(random_));
+  ON_CALL(*this, runtime()).WillByDefault(ReturnRef(runtime_loader_));
+  ON_CALL(*this, scope()).WillByDefault(ReturnRef(scope_));
+  ON_CALL(*this, singletonManager()).WillByDefault(ReturnRef(*singleton_manager_));
+  ON_CALL(*this, threadLocal()).WillByDefault(ReturnRef(thread_local_));
+  ON_CALL(*this, admin()).WillByDefault(ReturnRef(admin_));
   ON_CALL(*this, api()).WillByDefault(ReturnRef(api_));
+  ON_CALL(*this, timeSource()).WillByDefault(ReturnRef(time_system_));
+  ON_CALL(*this, messageValidationContext()).WillByDefault(ReturnRef(validation_context_));
   ON_CALL(*this, messageValidationVisitor())
       .WillByDefault(ReturnRef(ProtobufMessage::getStrictValidationVisitor()));
+  ON_CALL(*this, api()).WillByDefault(ReturnRef(api_));
+  ON_CALL(*this, drainManager()).WillByDefault(ReturnRef(drain_manager_));
 }
+MockServerFactoryContext::~MockServerFactoryContext() = default;
 
-
-MockTransportSocketFactoryContext::~MockTransportSocketFactoryContext() = default;
 
 
 }
