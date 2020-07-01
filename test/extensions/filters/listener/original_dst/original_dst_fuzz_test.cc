676047e888930d9a9b48efd6b71526d1a83f345d
Arthur Yan <arthuryan@google.com>
2020-07-01 15:41:47 +0000

    Route original_dst_fuzz_test.cc correctly thru network utilities
    Signed-off-by: Arthur Yan <arthuryan@google.com>

--- test/extensions/filters/listener/original_dst/original_dst_fuzz_test.cc
+++ test/extensions/filters/listener/original_dst/original_dst_fuzz_test.cc
@@ -15,7 +15,6 @@ namespace Extensions {
 namespace ListenerFilters {
 namespace OriginalDst {
 
-
 DEFINE_PROTO_FUZZER(
     const envoy::extensions::filters::listener::original_dst::v3::OriginalDstTestCase& input) {
 
@@ -27,19 +26,38 @@ DEFINE_PROTO_FUZZER(
   }
 
   NiceMock<Network::MockListenerFilterCallbacks> callbacks_;
-  try {
-    callbacks_.socket_.local_address_ = Network::Utility::resolveUrl(input.address());
-  } catch (const EnvoyException& e) {
-    ENVOY_LOG_MISC(debug, "EnvoyException: {}", e.what());
-    return;
-  }
-
-  auto filter = std::make_unique<OriginalDstFilter>();
-
+  NiceMock<Network::MockConnectionSocket> socket_;
 
   try {
+    auto address_ = Network::Utility::resolveUrl(input.address());
+    ON_CALL(socket_, localAddress()).WillByDefault(testing::ReturnRef(address_));
+    ON_CALL(callbacks_, socket()).WillByDefault(testing::ReturnRef(socket_));
+    if (address_ != nullptr)
+      ON_CALL(socket_, addressType()).WillByDefault(testing::Return(address_->type()));
+    if (socket_.addressType() == Network::Address::Type::Ip)
+      ON_CALL(socket_, ipVersion()).WillByDefault(testing::Return(address_->ip()->version()));
+
+    auto filter = std::make_unique<OriginalDstFilter>();
+
+    // Set address family of mock socket so that it routes correctly thru addressFromSockAddr
+    ON_CALL(socket_, getSocketOption(_, _, _, _))
+        .WillByDefault(testing::WithArgs<0, 2>(Invoke([](int level, void *optval) {
+      switch (level) {
+        case SOL_IPV6:
+          static_cast<sockaddr_storage*>(optval)->ss_family = AF_INET6;
+          break;
+        case SOL_IP:
+          static_cast<sockaddr_storage*>(optval)->ss_family = AF_INET;
+          break;
+        default:
+          NOT_REACHED_GCOVR_EXCL_LINE;
+      }
+
+      return Api::SysCallIntResult{0, 0};
+    })));
+
     filter->onAccept(callbacks_);
-  } catch (const EnvoyException& e) {
+  } catch(const EnvoyException& e) {
     ENVOY_LOG_MISC(debug, "EnvoyException: {}", e.what());
     return;
   }
