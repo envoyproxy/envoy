424b9bd5e6a464ba66089c958eb0ee186910bf96
Arthur Yan <arthuryan@google.com>
2020-07-01 16:23:19 +0000

    Fixed formatting issues
    Signed-off-by: Arthur Yan <arthuryan@google.com>

--- test/extensions/filters/listener/original_dst/original_dst_fuzz_test.cc
+++ test/extensions/filters/listener/original_dst/original_dst_fuzz_test.cc
@@ -41,23 +41,23 @@ DEFINE_PROTO_FUZZER(
 
     // Set address family of mock socket so that it routes correctly thru addressFromSockAddr
     ON_CALL(socket_, getSocketOption(_, _, _, _))
-        .WillByDefault(testing::WithArgs<0, 2>(Invoke([](int level, void *optval) {
-      switch (level) {
-        case SOL_IPV6:
-          static_cast<sockaddr_storage*>(optval)->ss_family = AF_INET6;
-          break;
-        case SOL_IP:
-          static_cast<sockaddr_storage*>(optval)->ss_family = AF_INET;
-          break;
-        default:
-          NOT_REACHED_GCOVR_EXCL_LINE;
-      }
+        .WillByDefault(testing::WithArgs<0, 2>(Invoke([](int level, void* optval) {
+          switch (level) {
+          case SOL_IPV6:
+            static_cast<sockaddr_storage*>(optval)->ss_family = AF_INET6;
+            break;
+          case SOL_IP:
+            static_cast<sockaddr_storage*>(optval)->ss_family = AF_INET;
+            break;
+          default:
+            NOT_REACHED_GCOVR_EXCL_LINE;
+          }
 
-      return Api::SysCallIntResult{0, 0};
-    })));
+          return Api::SysCallIntResult{0, 0};
+        })));
 
     filter->onAccept(callbacks_);
-  } catch(const EnvoyException& e) {
+  } catch (const EnvoyException& e) {
     ENVOY_LOG_MISC(debug, "EnvoyException: {}", e.what());
     return;
   }
