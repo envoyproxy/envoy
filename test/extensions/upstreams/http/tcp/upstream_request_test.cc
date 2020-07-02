d548b1c8398b7c15a99771e1848980e2fea769e1
Ranjith Kumar <ranjith.dakshana2015@gmail.com>
2020-07-02 16:09:16 +0530

    fix nullptr
    Signed-off-by: Ranjith Kumar <ranjith.dakshana2015@gmail.com>

--- test/extensions/upstreams/http/tcp/upstream_request_test.cc
+++ test/extensions/upstreams/http/tcp/upstream_request_test.cc
@@ -34,7 +34,7 @@ public:
   MockRouterFilterInterface()
       : config_("prefix.", context_, ShadowWriterPtr(new MockShadowWriter()), router_proto) {
     auto cluster_info = new NiceMock<Upstream::MockClusterInfo>();
-    cluster_info->timeout_budget_stats_ = absl::nullopt;
+    cluster_info->timeout_budget_stats_ = nullptr;
     cluster_info_.reset(cluster_info);
     ON_CALL(*this, callbacks()).WillByDefault(Return(&callbacks_));
     ON_CALL(*this, config()).WillByDefault(ReturnRef(config_));
