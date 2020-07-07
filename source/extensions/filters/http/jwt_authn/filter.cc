943c473e0471aa60a7f5117f3db55f604375be45
Wayne Zhang <qiwzhang@google.com>
2020-07-07 11:53:02 -0700

    jwt_authn: fix a header name typo (#11911)
    Signed-off-by: Wayne Zhang <qiwzhang@google.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- source/extensions/filters/http/jwt_authn/filter.cc
+++ source/extensions/filters/http/jwt_authn/filter.cc
@@ -19,7 +19,7 @@ namespace {
 Http::RegisterCustomInlineHeader<Http::CustomInlineHeaderRegistry::Type::RequestHeaders>
     access_control_request_method_handle(Http::CustomHeaders::get().AccessControlRequestMethod);
 Http::RegisterCustomInlineHeader<Http::CustomInlineHeaderRegistry::Type::RequestHeaders>
-    origin_handle(Http::CustomHeaders::get().AccessControlRequestMethod);
+    origin_handle(Http::CustomHeaders::get().Origin);
 
 bool isCorsPreflightRequest(const Http::RequestHeaderMap& headers) {
   return headers.getMethodValue() == Http::Headers::get().MethodValues.Options &&
