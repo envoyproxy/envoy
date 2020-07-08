bdc345aa902405c6682923da9a3268b48a0c0b09
Martin Matusiak <numerodix@gmail.com>
2020-07-09 06:43:15 +1000

    router: consume all retry related headers (#11913)
    Also remove `x-envoy-retriable-header-names` and
    `x-envoy-retriable-status-codes` to avoid these headers being
    propagated to the upstream.
    
    Signed-off-by: Martin Matusiak <numerodix@gmail.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -61,6 +61,7 @@ constexpr const char* runtime_features[] = {
     // Begin alphabetically sorted section.
     "envoy.reloadable_features.activate_fds_next_event_loop",
     "envoy.deprecated_features.allow_deprecated_extension_names",
+    "envoy.reloadable_features.consume_all_retry_headers",
     "envoy.reloadable_features.disallow_unbounded_access_logs",
     "envoy.reloadable_features.early_errors_via_hcm",
     "envoy.reloadable_features.enable_deprecated_v2_api_warning",
