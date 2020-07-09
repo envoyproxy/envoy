1f8c6c032c23828fd5444459800e3380775542e3
Harvey Tuch <htuch@google.com>
2020-07-09 19:16:55 -0400

    nghttp2: don't log unless ENVOY_NGHTTP2_TRACE is set.
    Avoid nghttp2 debug logs by default is good for two reasons:
    
    1. In regular developer integration test debugging at trace level, it's
       a pain to filter the very spammy nghttp2 traces.
    
    2. Coverage runs -l trace, it's hard to debug test output without generating
       O(100k) log lines, which results in massive CI log files.
    
    Risk level: Low
    Testing: No new tests, happy to add if it makes sense for this tweak.
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- source/common/http/http2/nghttp2.cc
+++ source/common/http/http2/nghttp2.cc
@@ -15,14 +15,16 @@ namespace Http2 {
 
 void initializeNghttp2Logging() {
   nghttp2_set_debug_vprintf_callback([](const char* format, va_list args) {
-    char buf[2048];
-    const int n = ::vsnprintf(buf, sizeof(buf), format, args);
-    // nghttp2 inserts new lines, but we also insert a new line in the ENVOY_LOG
-    // below, so avoid double \n.
-    if (n >= 1 && static_cast<size_t>(n) < sizeof(buf) && buf[n - 1] == '\n') {
-      buf[n - 1] = '\0';
+    if (std::getenv("ENVOY_NGHTTP2_TRACE") != nullptr) {
+      char buf[2048];
+      const int n = ::vsnprintf(buf, sizeof(buf), format, args);
+      // nghttp2 inserts new lines, but we also insert a new line in the ENVOY_LOG
+      // below, so avoid double \n.
+      if (n >= 1 && static_cast<size_t>(n) < sizeof(buf) && buf[n - 1] == '\n') {
+        buf[n - 1] = '\0';
+      }
+      ENVOY_LOG_TO_LOGGER(Logger::Registry::getLog(Logger::Id::http2), trace, "nghttp2: {}", buf);
     }
-    ENVOY_LOG_TO_LOGGER(Logger::Registry::getLog(Logger::Id::http2), trace, "nghttp2: {}", buf);
   });
 }
 
