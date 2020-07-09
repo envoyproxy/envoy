6e83b2fdc4fc2c3ea833604ae8d2c1c9cd1f05e3
Phil Genera <pgenera@google.com>
2020-07-09 14:48:06 -0400

    eds: decrease computational complexity of updates (#11442)
    Makes BaseDynamicClusterImpl::updateDynamicHostList O(n) rather than O(n^2)
    
    Instead of calling .erase() on list iterators as we find them, we swap with the end of the list and erase after iterating over the list. This shows a ~3x improvement in execution time in the included benchmark test.
    
    Risk Level: Medium. No reordering happens to the endpoint list. Not runtime guarded.
    Testing: New benchmark, existing unit tests pass (and cover the affected function).
    Docs Changes: N/A
    Release Notes: N/A
    
    Relates to #2874 #11362
    
    Signed-off-by: Phil Genera <pgenera@google.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/benchmark/main.cc
+++ test/benchmark/main.cc
@@ -1,16 +1,40 @@
 // NOLINT(namespace-envoy)
 // This is an Envoy driver for benchmarks.
+#include "test/benchmark/main.h"
+
 #include "test/test_common/environment.h"
 
 #include "benchmark/benchmark.h"
+#include "tclap/CmdLine.h"
 
-// Boilerplate main(), which discovers benchmarks and runs them.
+static bool skip_expensive_benchmarks = false;
+
+// Boilerplate main(), which discovers benchmarks and runs them. This uses two
+// different flag parsers, so the order of flags matters: flags defined here
+// must be passed first, and flags defined in benchmark::Initialize second,
+// separated by --.
+// TODO(pgenera): convert this to abseil/flags/ when benchmark also adopts abseil.
 int main(int argc, char** argv) {
   Envoy::TestEnvironment::initializeTestMain(argv[0]);
 
-  benchmark::Initialize(&argc, argv);
-  if (benchmark::ReportUnrecognizedArguments(argc, argv)) {
-    return 1;
+  // NOLINTNEXTLINE(clang-analyzer-optin.cplusplus.VirtualCall)
+  TCLAP::CmdLine cmd("envoy-benchmark-test", ' ', "0.1");
+  TCLAP::SwitchArg skip_switch("s", "skip_expensive_benchmarks",
+                               "skip or minimize expensive benchmarks", cmd, false);
+
+  cmd.setExceptionHandling(false);
+  try {
+    cmd.parse(argc, argv);
+  } catch (const TCLAP::ExitException& e) {
+    // parse() throws an ExitException with status 0 after printing the output
+    // for --help and --version.
+    return 0;
   }
+
+  skip_expensive_benchmarks = skip_switch.getValue();
+
+  benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
 }
+
+bool Envoy::benchmark::skipExpensiveBenchmarks() { return skip_expensive_benchmarks; }
