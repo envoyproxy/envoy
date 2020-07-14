1a90ebd57f3284f51516a55da78de027f7c086f7
foreseeable <mugechen@google.com>
2020-07-14 20:15:20 +0000

    test: refactor header inclusion to speed up building (for test/extensions/[upstream|common|tracer]/...) (#12049)
    Commit Message: refactor header inclusion to speed up building
    Additional Description:
    Risk Level: low
    Testing: exsiting tests
    Docs Changes: N/A
    Release Notes: no
    Related Issues: #10917
    
    Signed-off-by: Muge Chen <mugechen@google.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/extensions/tracers/xray/config_test.cc
+++ test/extensions/tracers/xray/config_test.cc
@@ -5,7 +5,9 @@
 
 #include "extensions/tracers/xray/config.h"
 
-#include "test/mocks/server/mocks.h"
+#include "test/mocks/server/instance.h"
+#include "test/mocks/server/tracer_factory.h"
+#include "test/mocks/server/tracer_factory_context.h"
 #include "test/test_common/environment.h"
 
 #include "gmock/gmock.h"
