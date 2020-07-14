0ffb9417246869afbd6e14c3ec8178ab3043d209
Arthur Yan <arthuryan@google.com>
2020-07-14 19:50:04 +0000

    Rewrote original_src fuzzer to utilize generic uber_filter
    Signed-off-by: Arthur Yan <arthuryan@google.com>

--- test/extensions/filters/listener/original_src/original_src_fuzz_test.cc
+++ test/extensions/filters/listener/original_src/original_src_fuzz_test.cc
@@ -1,15 +1,9 @@
-#include "envoy/extensions/filters/listener/original_src/v3/original_src.pb.h"
-
-#include "common/network/utility.h"
-
 #include "extensions/filters/listener/original_src/original_src.h"
 
 #include "test/extensions/filters/listener/original_src/original_src_fuzz_test.pb.validate.h"
-#include "test/fuzz/fuzz_runner.h"
-#include "test/mocks/network/mocks.h"
-#include "test/mocks/network/fakes.h"
+#include "test/extensions/filters/listener/common/uber_filter.h"
 
-#include "gmock/gmock.h"
+#include "test/fuzz/fuzz_runner.h"
 
 namespace Envoy {
 namespace Extensions {
@@ -26,23 +20,16 @@ DEFINE_PROTO_FUZZER(
     return;
   }
 
-  NiceMock<Network::MockListenerFilterCallbacks> callbacks;
-  Network::Address::InstanceConstSharedPtr address = nullptr;
+  Config config(input.config());
+  auto filter = std::make_unique<OriginalSrcFilter>(config);
 
   try {
-    address = Network::Utility::resolveUrl(input.address());
+    UberFilterFuzzer fuzzer;
+    fuzzer.fuzz(*filter, input.data());
   } catch (const EnvoyException& e) {
     ENVOY_LOG_MISC(debug, "EnvoyException: {}", e.what());
     return;
   }
-
-  Network::FakeConnectionSocket socket(nullptr, address);
-  ON_CALL(callbacks, socket()).WillByDefault(testing::ReturnRef(socket));
-
-  Config config(input.config());
-  auto filter = std::make_unique<OriginalSrcFilter>(config);
-
-  filter->onAccept(callbacks);
 }
 
 } // namespace OriginalSrc
