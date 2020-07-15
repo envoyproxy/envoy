1b85e44d3b2efa0ac4ae3e7429a21aa4b9918e3e
Dan Zhang <danzh@google.com>
2020-07-15 13:15:43 -0400

    fix windows
    Signed-off-by: Dan Zhang <danzh@google.com>

--- test/extensions/quic_listeners/quiche/envoy_quic_proof_verifier_test.cc
+++ test/extensions/quic_listeners/quiche/envoy_quic_proof_verifier_test.cc
@@ -49,37 +49,36 @@ public:
     // Getting the last cert in the chain as the root CA cert.
     const std::string& root_ca_cert =
         cert_chain_.substr(cert_chain_.rfind("-----BEGIN CERTIFICATE-----"));
-    const std::string path_string("some_path");
     EXPECT_CALL(cert_validation_ctx_config_, caCert()).WillRepeatedly(ReturnRef(root_ca_cert));
-    EXPECT_CALL(cert_validation_ctx_config_, caCertPath()).WillRepeatedly(ReturnRef(path_string));
+    EXPECT_CALL(cert_validation_ctx_config_, caCertPath()).WillRepeatedly(ReturnRef(path_string_));
     EXPECT_CALL(cert_validation_ctx_config_, trustChainVerification)
         .WillRepeatedly(Return(envoy::extensions::transport_sockets::tls::v3::
                                    CertificateValidationContext::VERIFY_TRUST_CHAIN));
     EXPECT_CALL(cert_validation_ctx_config_, allowExpiredCertificate())
         .WillRepeatedly(Return(allow_expired_cert));
-    const std::string crl_list;
     EXPECT_CALL(cert_validation_ctx_config_, certificateRevocationList())
-        .WillRepeatedly(ReturnRef(crl_list));
+        .WillRepeatedly(ReturnRef(crl_list_));
     EXPECT_CALL(cert_validation_ctx_config_, certificateRevocationListPath())
-        .WillRepeatedly(ReturnRef(path_string));
-    const std::vector<std::string> empty_string_list;
+        .WillRepeatedly(ReturnRef(path_string_));
     EXPECT_CALL(cert_validation_ctx_config_, verifySubjectAltNameList())
-        .WillRepeatedly(ReturnRef(empty_string_list));
-    const std::vector<envoy::type::matcher::v3::StringMatcher> san_matchers;
+        .WillRepeatedly(ReturnRef(empty_string_list_));
     EXPECT_CALL(cert_validation_ctx_config_, subjectAltNameMatchers())
-        .WillRepeatedly(ReturnRef(san_matchers));
+        .WillRepeatedly(ReturnRef(san_matchers_));
     EXPECT_CALL(cert_validation_ctx_config_, verifyCertificateHashList())
-        .WillRepeatedly(ReturnRef(empty_string_list));
+        .WillRepeatedly(ReturnRef(empty_string_list_));
     EXPECT_CALL(cert_validation_ctx_config_, verifyCertificateSpkiList())
-        .WillRepeatedly(ReturnRef(empty_string_list));
-
+        .WillRepeatedly(ReturnRef(empty_string_list_));
     verifier_ =
         std::make_unique<EnvoyQuicProofVerifier>(store_, client_context_config_, time_system_);
   }
 
 protected:
+  const std::string path_string_{"some_path"};
   const std::string alpn_{"h2,http/1.1"};
   const std::string sig_algs_{"rsa_pss_rsae_sha256"};
+  const std::vector<envoy::type::matcher::v3::StringMatcher> san_matchers_;
+  const std::string crl_list_;
+  const std::vector<std::string> empty_string_list_;
   const std::string cert_chain_{quic::test::kTestCertificateChainPem};
   std::string leaf_cert_;
   NiceMock<Stats::MockStore> store_;
@@ -89,7 +88,7 @@ protected:
   std::unique_ptr<EnvoyQuicProofVerifier> verifier_;
 };
 
-TEST_F(EnvoyQuicProofVerifierTest, VerifyFilterChainSuccess) {
+TEST_F(EnvoyQuicProofVerifierTest, VerifyCertChainSuccess) {
   configCertVerificationDetails(true);
   std::unique_ptr<quic::CertificateView> cert_view =
       quic::CertificateView::ParseSingleCertificate(leaf_cert_);
@@ -103,7 +102,7 @@ TEST_F(EnvoyQuicProofVerifierTest, VerifyFilterChainSuccess) {
       << error_details;
 }
 
-TEST_F(EnvoyQuicProofVerifierTest, VerifyFilterChainFailureFromSsl) {
+TEST_F(EnvoyQuicProofVerifierTest, VerifyCertChainFailureFromSsl) {
   configCertVerificationDetails(false);
   std::unique_ptr<quic::CertificateView> cert_view =
       quic::CertificateView::ParseSingleCertificate(leaf_cert_);
@@ -119,7 +118,7 @@ TEST_F(EnvoyQuicProofVerifierTest, VerifyFilterChainFailureFromSsl) {
             error_details);
 }
 
-TEST_F(EnvoyQuicProofVerifierTest, VerifyFilterChainFailureInvalidHost) {
+TEST_F(EnvoyQuicProofVerifierTest, VerifyCertChainFailureInvalidHost) {
   configCertVerificationDetails(true);
   std::unique_ptr<quic::CertificateView> cert_view =
       quic::CertificateView::ParseSingleCertificate(leaf_cert_);
