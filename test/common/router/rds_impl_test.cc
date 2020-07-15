c51d06c0d1b07a9be9811132b7dbffb8f40c702f
ankatare <abhay.katare@india.nec.com>
2020-07-15 22:28:58 +0530

    V2 v3 fragment change for router directory (#11973)
    V2 to V3 changes related to test/commom/router/ directory.
    
    Risk Level: Low
    Testing: unit, integration and format testing
    Docs Changes: NA
    
    Relates to #10843
    
    Signed-off-by: Abhay Narayan Katare <abhay.katare@india.nec.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/common/router/rds_impl_test.cc
+++ test/common/router/rds_impl_test.cc
@@ -358,9 +358,9 @@ public:
 };
 
 envoy::config::route::v3::RouteConfiguration
-parseRouteConfigurationFromV2Yaml(const std::string& yaml) {
+parseRouteConfigurationFromV3Yaml(const std::string& yaml, bool avoid_boosting = true) {
   envoy::config::route::v3::RouteConfiguration route_config;
-  TestUtility::loadFromYaml(yaml, route_config, true);
+  TestUtility::loadFromYaml(yaml, route_config, true, avoid_boosting);
   return route_config;
 }
 
@@ -394,7 +394,7 @@ virtual_hosts:
   // Only static route.
   RouteConfigProviderPtr static_config =
       route_config_provider_manager_->createStaticRouteConfigProvider(
-          parseRouteConfigurationFromV2Yaml(config_yaml), server_factory_context_,
+          parseRouteConfigurationFromV3Yaml(config_yaml), server_factory_context_,
           validation_visitor_);
   message_ptr =
       server_factory_context_.admin_.config_tracker_.config_tracker_callbacks_["routes"]();
@@ -403,7 +403,7 @@ virtual_hosts:
   TestUtility::loadFromYaml(R"EOF(
 static_route_configs:
   - route_config:
-      "@type": type.googleapis.com/envoy.api.v2.RouteConfiguration
+      "@type": type.googleapis.com/envoy.config.route.v3.RouteConfiguration
       name: foo
       virtual_hosts:
         - name: bar
@@ -430,7 +430,7 @@ dynamic_route_configs:
   "version_info": "1",
   "resources": [
     {
-      "@type": "type.googleapis.com/envoy.api.v2.RouteConfiguration",
+      "@type": "type.googleapis.com/envoy.config.route.v3.RouteConfiguration",
       "name": "foo_route_config",
       "virtual_hosts": null
     }
@@ -451,7 +451,7 @@ dynamic_route_configs:
   TestUtility::loadFromYaml(R"EOF(
 static_route_configs:
   - route_config:
-      "@type": type.googleapis.com/envoy.api.v2.RouteConfiguration
+      "@type": type.googleapis.com/envoy.config.route.v3.RouteConfiguration
       name: foo
       virtual_hosts:
         - name: bar
@@ -465,7 +465,7 @@ static_route_configs:
 dynamic_route_configs:
   - version_info: "1"
     route_config:
-      "@type": type.googleapis.com/envoy.api.v2.RouteConfiguration
+      "@type": type.googleapis.com/envoy.config.route.v3.RouteConfiguration
       name: foo_route_config
       virtual_hosts:
     last_updated:
@@ -484,7 +484,7 @@ TEST_F(RouteConfigProviderManagerImplTest, Basic) {
 
   EXPECT_FALSE(provider_->configInfo().has_value());
 
-  const auto route_config = parseRouteConfigurationFromV2Yaml(R"EOF(
+  const auto route_config = parseRouteConfigurationFromV3Yaml(R"EOF(
 name: foo_route_config
 virtual_hosts:
   - name: bar
@@ -565,7 +565,7 @@ TEST_F(RouteConfigProviderManagerImplTest, SameProviderOnTwoInitManager) {
   EXPECT_EQ(Init::Manager::State::Initializing, real_init_manager.state());
 
   {
-    const auto route_config = parseRouteConfigurationFromV2Yaml(R"EOF(
+    const auto route_config = parseRouteConfigurationFromV3Yaml(R"EOF(
 name: foo_route_config
 virtual_hosts:
   - name: bar
