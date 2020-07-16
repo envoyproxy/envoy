8cd7fd61321fe4022f173609785adc68366aba87
pingsun <pingsun@google.com>
2020-07-16 00:04:52 +0000

    update stats_integration_test memory usage
    Signed-off-by: pingsun <pingsun@google.com>

--- test/integration/stats_integration_test.cc
+++ test/integration/stats_integration_test.cc
@@ -283,6 +283,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithFakeSymbolTable) {
   // 2020/06/29  11751    44715       46000   Improve time complexity of removing callback handle
   //                                          in callback manager.
   // 2020/07/07  11252    44971       46000   Introduce Least Request LB active request bias config
+  // 2020/07/10  12035    45083       46000   Init manager store unready targets in hash map.
 
   // Note: when adjusting this value: EXPECT_MEMORY_EQ is active only in CI
   // 'release' builds, where we control the platform and tool-chain. So you
@@ -300,7 +301,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithFakeSymbolTable) {
   // We only run the exact test for ipv6 because ipv4 in some cases may allocate a
   // different number of bytes. We still run the approximate test.
   if (ip_version_ != Network::Address::IpVersion::v6) {
-    EXPECT_MEMORY_EQ(m_per_cluster, 44971);
+    EXPECT_MEMORY_EQ(m_per_cluster, 45083);
   }
   EXPECT_MEMORY_LE(m_per_cluster, 46000); // Round up to allow platform variations.
 }
@@ -355,6 +356,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithRealSymbolTable) {
   // 2020/06/29  11751    36827       38000   Improve time complexity of removing callback handle.
   //                                          in callback manager.
   // 2020/07/07  11252    37083       38000   Introduce Least Request LB active request bias config
+  // 2020/07/10  12035    37195       38000   Init manager store unready targets in hash map.
 
   // Note: when adjusting this value: EXPECT_MEMORY_EQ is active only in CI
   // 'release' builds, where we control the platform and tool-chain. So you
@@ -372,7 +374,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithRealSymbolTable) {
   // We only run the exact test for ipv6 because ipv4 in some cases may allocate a
   // different number of bytes. We still run the approximate test.
   if (ip_version_ != Network::Address::IpVersion::v6) {
-    EXPECT_MEMORY_EQ(m_per_cluster, 37083);
+    EXPECT_MEMORY_EQ(m_per_cluster, 37195);
   }
   EXPECT_MEMORY_LE(m_per_cluster, 38000); // Round up to allow platform variations.
 }
