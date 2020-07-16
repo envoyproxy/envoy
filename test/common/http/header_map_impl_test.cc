72e21a493bace47af2c012e40823c4df88bf342d
Alex Konradi <akonradi@google.com>
2020-07-16 11:50:52 -0400

    Fix method name case for clang-tidy
    Signed-off-by: Alex Konradi <akonradi@google.com>

--- test/common/http/header_map_impl_test.cc
+++ test/common/http/header_map_impl_test.cc
@@ -564,7 +564,7 @@ TEST(HeaderMapImplTest, RemoveRegex) {
 class HeaderAndValueCb
     : public testing::MockFunction<void(const std::string&, const std::string&)> {
 public:
-  HeaderMap::ConstIterateCb AsIterateCb() {
+  HeaderMap::ConstIterateCb asIterateCb() {
     return [this](const Http::HeaderEntry& header) -> HeaderMap::Iterate {
       Call(std::string(header.key().getStringView()), std::string(header.value().getStringView()));
       return HeaderMap::Iterate::Continue;
