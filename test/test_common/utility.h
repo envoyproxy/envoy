1c6bb55ace752e93de9e9ead5ccc81d6f6ecf084
ankatare <abhay.katare@india.nec.com>
2020-07-17 00:15:35 +0530

    V2 v3 changes for parse foo from v2 json (#12113)
    V2 to V3 changes for parseFoofromV2json
    :
    Risk Level: NA
    Testing: unit and format
    Docs Changes: NA
    
    Part of #10843
    
    Signed-off-by: Abhay Narayan Katare <abhay.katare@india.nec.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/test_common/utility.h
+++ test/test_common/utility.h
@@ -538,8 +538,9 @@ public:
 
   // Strict variants of Protobuf::MessageUtil
   static void loadFromJson(const std::string& json, Protobuf::Message& message,
-                           bool preserve_original_type = false) {
-    MessageUtil::loadFromJson(json, message, ProtobufMessage::getStrictValidationVisitor());
+                           bool preserve_original_type = false, bool avoid_boosting = false) {
+    MessageUtil::loadFromJson(json, message, ProtobufMessage::getStrictValidationVisitor(),
+                              !avoid_boosting);
     if (!preserve_original_type) {
       Config::VersionConverter::eraseOriginalTypeInformation(message);
     }
