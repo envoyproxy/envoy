9579352cf2ee425dca48acfcab4057526af6f521
Muge Chen <mugechen@google.com>
2020-07-17 15:50:46 +0000

    fix format issues
    Signed-off-by: Muge Chen <mugechen@google.com>

--- tools/envoy_headersplit/headersplit_test.py
+++ tools/envoy_headersplit/headersplit_test.py
@@ -25,7 +25,6 @@ class test{
 };
   """
 
-  
   # A C++ source code contains definition for several classes
   # class_definitions() should return a list of cursors, each pointing to one of those classes
 
@@ -96,13 +95,15 @@ class test{
 
   def test_to_filename(self):
     # Test class name with one "mock"
-    self.assertEqual(headersplit.to_filename("MockAdminStream"),"admin_stream")
+    self.assertEqual(headersplit.to_filename("MockAdminStream"), "admin_stream")
 
     # Test class name with two "Mock"
-    self.assertEqual(headersplit.to_filename("MockClusterMockPrioritySet"), "cluster_mock_priority_set")
+    self.assertEqual(headersplit.to_filename("MockClusterMockPrioritySet"),
+                     "cluster_mock_priority_set")
 
     # Test class name with no "Mock"
-    self.assertEqual(headersplit.to_filename("TestRetryHostPredicateFactory"), "test_retry_host_predicate_factory")
+    self.assertEqual(headersplit.to_filename("TestRetryHostPredicateFactory"),
+                     "test_retry_host_predicate_factory")
 
   def test_get_headers(self):
     includes = """
@@ -114,52 +115,44 @@ class test{
 #include "foo/bar"
 
 """
-    with open("hello.h","w") as source:
+    with open("hello.h", "w") as source:
       source.write(self.source_code_hello_world)
 
-    translation_unit_hello_world = TranslationUnit.from_source("hello.h", options=TranslationUnit.PARSE_SKIP_FUNCTION_BODIES)
+    translation_unit_hello_world = TranslationUnit.from_source(
+        "hello.h", options=TranslationUnit.PARSE_SKIP_FUNCTION_BODIES)
     self.assertEqual(headersplit.get_headers(translation_unit_hello_world), includes)
 
-
   def test_class_definitions(self):
-    with open("test_class_defn.h","w") as source:
+    with open("test_class_defn.h", "w") as source:
       source.write(self.source_class_defn)
 
     idx = Index.create()
     translation_unit_class_defn = idx.parse("test_class_defn.h", ['-x', 'c++'])
     defns_cursors = headersplit.class_definitions(translation_unit_class_defn.cursor)
     defns_names = [cursor.spelling for cursor in defns_cursors]
-    
-    self.assertEqual(defns_names, ['Foo','Bar','FooBar','DeadBeaf'])
-    with open("test_class_defn_without_namespace.h","w") as source:
+
+    self.assertEqual(defns_names, ['Foo', 'Bar', 'FooBar', 'DeadBeaf'])
+    with open("test_class_defn_without_namespace.h", "w") as source:
       source.write(self.source_class_defn_without_namespace)
 
     idx = Index.create()
     translation_unit_class_defn = idx.parse("test_class_defn_without_namespace.h", ['-x', 'c++'])
     defns_cursors = headersplit.class_definitions(translation_unit_class_defn.cursor)
     defns_names = [cursor.spelling for cursor in defns_cursors]
-    
+
     # class_definitions() should ignore all classes outside the namespaces
     self.assertEqual(defns_names, [])
-    
+
   def test_class_implementations(self):
-    with open("test_class_impl.cc","w") as source:
+    with open("test_class_impl.cc", "w") as source:
       source.write(self.source_class_impl)
 
     translation_unit_class_impl = TranslationUnit.from_source(
         "test_class_impl.cc", options=TranslationUnit.PARSE_SKIP_FUNCTION_BODIES)
     impls_cursors = headersplit.class_implementations(translation_unit_class_impl.cursor)
     impls_names = [cursor.spelling for cursor in impls_cursors]
-    
+
     self.assertEqual(impls_names, ['getFoo', 'val', 'DeadBeaf'])
-
-    
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
--- tools/envoy_headersplit/replace_includes.py
+++ tools/envoy_headersplit/replace_includes.py
@@ -17,19 +17,21 @@ this script need to be executed in the Envoy directory
 from pathlib import Path
 import argparse
 
+
 def replace_includes(mock_name):
   dir = Path("test/mocks/{}/".format(mock_name))
-  filenames = list(map(str,dir.glob('*.h')))
+  filenames = list(map(str, dir.glob('*.h')))
   classnames = []
   classname2filename = dict()
   classname2bazelname = dict()
   for filename in filenames:
-    classname_tokens = filename.split('/')[-1].replace('.h','').split('_')
-    classname = "Mock" + ''.join(map(lambda x: x[:1].upper()+x[1:],classname_tokens))
+    classname_tokens = filename.split('/')[-1].replace('.h', '').split('_')
+    classname = "Mock" + ''.join(map(lambda x: x[:1].upper() + x[1:], classname_tokens))
     classnames.append(classname)
-    classname2filename[classname]=filename
-    bazelname = "//test/mocks/{}:"+filename.split('/')[-1].replace('.h','')+'_mocks'.format(mock_name)
-    classname2bazelname[classname]=bazelname
+    classname2filename[classname] = filename
+    bazelname = "//test/mocks/{}:" + filename.split('/')[-1].replace(
+        '.h', '') + '_mocks'.format(mock_name)
+    classname2bazelname[classname] = bazelname
 
   print(classnames)
 
@@ -48,29 +50,22 @@ def replace_includes(mock_name):
         replace_includes = ""
         for classname in classnames:
           if classname in content:
-              replace_includes += '#include "{}"\n'.format(classname2filename[classname])
-              used+= '"{}",'.format(classname2bazelname[classname])
+            replace_includes += '#include "{}"\n'.format(classname2filename[classname])
+            used += '"{}",'.format(classname2bazelname[classname])
     if flag:
       with i.open(mode='w') as f:
-        f.write(content.replace('#include "test/mocks/{}/mocks.h"'.format(mock_name),replace_includes))
+        f.write(
+            content.replace('#include "test/mocks/{}/mocks.h"'.format(mock_name), replace_includes))
       with (i.parent / 'BUILD').open() as f:
         # write building files
         content = f.read()
-        content = content.replace('"//test/mocks/{}:{}_mocks",'.format(mock_name,mock_name),used)
+        content = content.replace('"//test/mocks/{}:{}_mocks",'.format(mock_name, mock_name), used)
       with (i.parent / 'BUILD').open('w') as f:
         f.write(content)
 
 
 if __name__ == '__main__':
   PARSER = argparse.ArgumentParser()
-  PARSER.add_argument(
-      '-m',
-      '--mock_name',
-      default="server",
-      help="mock folder that been divided"
-  )
+  PARSER.add_argument('-m', '--mock_name', default="server", help="mock folder that been divided")
   mock_name = vars(PARSER.parse_args())['mock_name']
   replace_includes(mock_name)
-
-
-
