1519d3fcd67bcca3968ec3b7d0781b29d4bfb920
James Fish <jfish@pinterest.com>
2020-07-20 11:07:47 -0700

    thrift_proxy: Handle thrift void response as a success (#12070)
    When a successful thrift response was receive we never called fieldBegin
    for void response because fieldBegin is skipped if the field type is
    stop. Therefore check at the message end if we never saw a field begin
    callback so we can mark a void response as a success.
    
    Signed-off-by: James Fish <jfish@pinterest.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- source/extensions/filters/network/thrift_proxy/conn_manager.cc
+++ source/extensions/filters/network/thrift_proxy/conn_manager.cc
@@ -201,11 +201,28 @@ FilterStatus ConnectionManager::ResponseDecoder::fieldBegin(absl::string_view na
     // Reply messages contain a struct where field 0 is the call result and fields 1+ are
     // exceptions, if defined. At most one field may be set. Therefore, the very first field we
     // encounter in a reply is either field 0 (success) or not (IDL exception returned).
-    success_ = field_id == 0 && field_type != FieldType::Stop;
+    // If first fieldType is FieldType::Stop then it is a void success and handled in messageEnd()
+    // because decoder state machine does not call decoder event callback fieldBegin on
+    // FieldType::Stop.
+    success_ = (field_id == 0);
     first_reply_field_ = false;
   }
 
   return ProtocolConverter::fieldBegin(name, field_type, field_id);
+}
+
+FilterStatus ConnectionManager::ResponseDecoder::messageEnd() {
+  if (first_reply_field_) {
+    // When the response is thrift void type there is never a fieldBegin call on a success
+    // because the response struct has no fields and so the first field type is FieldType::Stop.
+    // The decoder state machine handles FieldType::Stop by going immediately to structEnd,
+    // skipping fieldBegin callback. Therefore if we are still waiting for the first reply field
+    // at end of message then it is a void success.
+    success_ = true;
+    first_reply_field_ = false;
+  }
+
+  return ProtocolConverter::messageEnd();
 }
 
 FilterStatus ConnectionManager::ResponseDecoder::transportEnd() {
