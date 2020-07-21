5f3d9b5cec5d2dc90e9bf3bcfbd618e05222799f
Yugant <yugant@google.com>
2020-07-21 06:51:41 +0000

    Added common library udp_listener_impl_test_base to house common code from udp_listener_impl_tests
    Signed-off-by: Yugant <yugant@google.com>

--- test/common/network/udp_listener_impl_batch_writer_test.cc
+++ test/common/network/udp_listener_impl_batch_writer_test.cc
@@ -9,12 +9,11 @@
 #include "common/network/socket_option_factory.h"
 #include "common/network/socket_option_impl.h"
 #include "common/network/udp_listener_impl.h"
-#include "common/network/udp_packet_writer_handler_impl.h"
 #include "common/network/utility.h"
 
 #include "extensions/quic_listeners/quiche/udp_gso_batch_writer.h"
 
-#include "test/common/network/listener_impl_test_base.h"
+#include "test/common/network/udp_listener_impl_test_base.h"
 #include "test/mocks/api/mocks.h"
 #include "test/mocks/network/mocks.h"
 #include "test/mocks/server/mocks.h"
@@ -35,15 +34,10 @@ namespace Envoy {
 namespace Network {
 namespace {
 
-class UdpListenerImplBatchWriterTest : public ListenerImplTestBase {
+class UdpListenerImplBatchWriterTest : public UdpListenerImplTestBase {
 public:
-  UdpListenerImplBatchWriterTest()
-      : server_socket_(createServerSocket(true)), send_to_addr_(getServerLoopbackAddress()) {
-    time_system_.advanceTimeWait(std::chrono::milliseconds(100));
-  }
-
   void SetUp() override {
-    // Set listening socket options.
+    // Set listening socket options and set UdpGsoBatchWriter
     server_socket_->addOptions(SocketOptionFactory::buildIpPacketInfoOptions());
     server_socket_->addOptions(SocketOptionFactory::buildRxQueueOverFlowOptions());
 
@@ -61,63 +55,6 @@ public:
         std::make_unique<UdpListenerImpl>(dispatcherImpl(), server_socket_, listener_callbacks_,
                                           dispatcherImpl().timeSource(), listener_config_);
   }
-
-protected:
-  Address::Instance* getServerLoopbackAddress() {
-    if (version_ == Address::IpVersion::v4) {
-      return new Address::Ipv4Instance(Network::Test::getLoopbackAddressString(version_),
-                                       server_socket_->localAddress()->ip()->port());
-    }
-    return new Address::Ipv6Instance(Network::Test::getLoopbackAddressString(version_),
-                                     server_socket_->localAddress()->ip()->port());
-  }
-
-  SocketSharedPtr createServerSocket(bool bind) {
-    // Set IP_FREEBIND to allow sendmsg to send with non-local IPv6 source address.
-    return std::make_shared<UdpListenSocket>(Network::Test::getAnyAddress(version_),
-#ifdef IP_FREEBIND
-                                             SocketOptionFactory::buildIpFreebindOptions(),
-#else
-                                             nullptr,
-#endif
-                                             bind);
-  }
-
-  // Validates receive data, source/destination address and received time.
-  void validateRecvCallbackParams(const UdpRecvData& data) {
-    ASSERT_NE(data.addresses_.local_, nullptr);
-
-    ASSERT_NE(data.addresses_.peer_, nullptr);
-    ASSERT_NE(data.addresses_.peer_->ip(), nullptr);
-
-    EXPECT_EQ(data.addresses_.local_->asString(), send_to_addr_->asString());
-
-    EXPECT_EQ(data.addresses_.peer_->ip()->addressAsString(),
-              client_.localAddress()->ip()->addressAsString());
-
-    EXPECT_EQ(*data.addresses_.local_, *send_to_addr_);
-
-    size_t num_packet_per_recv = 1u;
-    if (Api::OsSysCallsSingleton::get().supportsMmsg()) {
-      num_packet_per_recv = 16u;
-    }
-    EXPECT_EQ(time_system_.monotonicTime(),
-              data.receive_time_ +
-                  std::chrono::milliseconds(
-                      (num_packets_received_by_listener_ % num_packet_per_recv) * 100));
-    // Advance time so that next onData() should have different received time.
-    time_system_.advanceTimeWait(std::chrono::milliseconds(100));
-    ++num_packets_received_by_listener_;
-  }
-
-  SocketSharedPtr server_socket_;
-  Network::Test::UdpSyncPeer client_{GetParam()};
-  Address::InstanceConstSharedPtr send_to_addr_;
-  MockUdpListenerCallbacks listener_callbacks_;
-  NiceMock<MockListenerConfig> listener_config_;
-  NiceMock<MockUdpPacketWriterFactory> udp_packet_writer_factory_;
-  std::unique_ptr<UdpListenerImpl> listener_;
-  size_t num_packets_received_by_listener_{0};
 };
 
 INSTANTIATE_TEST_SUITE_P(IpVersions, UdpListenerImplBatchWriterTest,
@@ -136,32 +73,8 @@ INSTANTIATE_TEST_SUITE_P(IpVersions, UdpListenerImplBatchWriterTest,
  *       sent along with the already buffered payloads.
  */
 TEST_P(UdpListenerImplBatchWriterTest, SendData) {
-  // Use a self address that is unlikely to be picked by source address discovery
-  // algorithm if not specified in recvmsg/recvmmsg. Port is not taken into
-  // consideration.
-  Address::InstanceConstSharedPtr send_from_addr;
-  if (version_ == Address::IpVersion::v4) {
-    // Linux kernel regards any 127.x.x.x as local address. But Mac OS doesn't.
-    send_from_addr = std::make_shared<Address::Ipv4Instance>(
-#ifndef __APPLE__
-        "127.1.2.3",
-#else
-        "127.0.0.1",
-#endif
-        server_socket_->localAddress()->ip()->port());
-  } else {
-    // Only use non-local v6 address if IP_FREEBIND is supported. Otherwise use
-    // ::1 to avoid EINVAL error. Unfortunately this can't verify that sendmsg with
-    // customized source address is doing the work because kernel also picks ::1
-    // if it's not specified in cmsghdr.
-    send_from_addr = std::make_shared<Address::Ipv6Instance>(
-#ifdef IP_FREEBIND
-        "::9",
-#else
-        "::1",
-#endif
-        server_socket_->localAddress()->ip()->port());
-  }
+
+  Address::InstanceConstSharedPtr send_from_addr = getUnlikelySourceAddress();
 
   absl::FixedArray<std::string> payloads{"length7", "length7", "len<7",
                                          "length7", "length7", "length>7"};
