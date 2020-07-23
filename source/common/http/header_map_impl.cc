322a5a39eb430e3817d652cab1c65cb432721c44
Matt Klein <mklein@lyft.com>
2020-07-23 10:36:58 -0700

    http: restore previous HeaderMap::remove() behavior (#12244)
    https://github.com/envoyproxy/envoy/pull/12160 changed
    the behavior of remove() to not first look in the inline
    header map for a header. This is a subtle change in
    behavior that specifically breaks attempting to remove
    ":authority" via the "host" mapping. This restores that
    behavior and is thus a bug fix and low risk.
    
    Signed-off-by: Matt Klein <mklein@lyft.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- source/common/http/header_map_impl.cc
+++ source/common/http/header_map_impl.cc
@@ -508,10 +508,17 @@ size_t HeaderMapImpl::removeIf(const HeaderMap::HeaderMatchPredicate& predicate)
 }
 
 size_t HeaderMapImpl::remove(const LowerCaseString& key) {
-  // TODO(mattklein123): When the lazy map is implemented we can stop using removeIf() here.
-  return HeaderMapImpl::removeIf([&key](const HeaderEntry& entry) -> bool {
-    return key.get() == entry.key().getStringView();
-  });
+  auto lookup = staticLookup(key.get());
+  if (lookup.has_value()) {
+    const size_t old_size = headers_.size();
+    removeInline(lookup.value().entry_);
+    return old_size - headers_.size();
+  } else {
+    // TODO(mattklein123): When the lazy map is implemented we can stop using removeIf() here.
+    return HeaderMapImpl::removeIf([&key](const HeaderEntry& entry) -> bool {
+      return key.get() == entry.key().getStringView();
+    });
+  }
 }
 
 size_t HeaderMapImpl::removePrefix(const LowerCaseString& prefix) {
