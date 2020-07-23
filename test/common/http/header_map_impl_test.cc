d358ca64ed4b838c826a27eaf2409b63faca9c4f
Matt Klein <mklein@lyft.com>
2020-07-23 10:36:58 -0700

    http: restore previous HeaderMap::remove() behavior (#12244)
    https://github.com/envoyproxy/envoy/pull/12160 changed
    the behavior of remove() to not first look in the inline
    header map for a header. This is a subtle change in
    behavior that specifically breaks attempting to remove
    ":authority" via the "host" mapping. This restores that
    behavior and is thus a bug fix and low risk.
    
    Signed-off-by: Matt Klein <mklein@lyft.com>
    Signed-off-by: Kevin Baichoo <kbaichoo@google.com>

--- test/common/http/header_map_impl_test.cc
+++ test/common/http/header_map_impl_test.cc
@@ -520,6 +520,20 @@ TEST(HeaderMapImplTest, Remove) {
   EXPECT_EQ(0UL, headers.remove(Headers::get().ContentLength));
 }
 
+TEST(HeaderMapImplTest, RemoveHost) {
+  TestRequestHeaderMapImpl headers;
+  headers.setHost("foo");
+  EXPECT_EQ("foo", headers.get_("host"));
+  EXPECT_EQ("foo", headers.get_(":authority"));
+  // Make sure that when we remove by "host" without using the inline functions, the mapping to
+  // ":authority" still takes place.
+  // https://github.com/envoyproxy/envoy/pull/12160
+  EXPECT_EQ(1UL, headers.remove("host"));
+  EXPECT_EQ("", headers.get_("host"));
+  EXPECT_EQ("", headers.get_(":authority"));
+  EXPECT_EQ(nullptr, headers.Host());
+}
+
 TEST(HeaderMapImplTest, RemoveIf) {
   LowerCaseString key1 = LowerCaseString("X-postfix-foo");
   LowerCaseString key2 = LowerCaseString("X-postfix-");
