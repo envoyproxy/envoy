d7b01b691c8010c696514a7bc6c07968eac39c4a
Matt Klein <mklein@lyft.com>
2020-07-23 21:03:54 -0700

    load stats: fix integration test flake (#12265)
    Waiting on a load stats response can race with resetting
    the counters when initializing a watch. Moving the counter
    increment prevents the race.
    
    Fixes https://github.com/envoyproxy/envoy/issues/11784
    
    Signed-off-by: Matt Klein <mklein@lyft.com>
    Signed-off-by: chaoqinli <chaoqinli@google.com>

--- source/common/upstream/load_stats_reporter.cc
+++ source/common/upstream/load_stats_reporter.cc
@@ -140,9 +140,9 @@ void LoadStatsReporter::onReceiveInitialMetadata(Http::ResponseHeaderMapPtr&& me
 void LoadStatsReporter::onReceiveMessage(
     std::unique_ptr<envoy::service::load_stats::v3::LoadStatsResponse>&& message) {
   ENVOY_LOG(debug, "New load report epoch: {}", message->DebugString());
-  stats_.requests_.inc();
   message_ = std::move(message);
   startLoadReportPeriod();
+  stats_.requests_.inc();
 }
 
 void LoadStatsReporter::startLoadReportPeriod() {
