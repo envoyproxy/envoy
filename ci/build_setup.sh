8d9644148f62bc2bbb0092b842d7feb556c79b96
Lizan Zhou <lizan@tetrate.io>
2020-07-25 22:13:50 -0700

    remove duplicates
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/build_setup.sh
+++ ci/build_setup.sh
@@ -76,6 +76,8 @@ export LLVM_ROOT="${LLVM_ROOT:-/opt/llvm}"
 [[ "${BUILD_REASON}" != "PullRequest" ]] && BAZEL_EXTRA_TEST_OPTIONS+=" --nocache_test_results"
 
 export BAZEL_QUERY_OPTIONS="${BAZEL_OPTIONS}"
+# Use https://docs.bazel.build/versions/master/command-line-reference.html#flag--experimental_repository_cache_hardlinks
+# to save disk space.
 export BAZEL_BUILD_OPTIONS=" ${BAZEL_OPTIONS} --verbose_failures --show_task_finish --experimental_generate_json_trace_profile \
   --build_event_json_file=${BUILD_DIR}/build_event.json \
   --test_output=errors --repository_cache=${BUILD_DIR}/repository_cache --experimental_repository_cache_hardlinks \
