74aa42ae36f4bef4ac3bceae60c63daee96dc0c4
Keith Smiley <keithbsmiley@gmail.com>
2020-07-27 15:10:18 -0700

    Swift: Workaround Swift 5.2 + 5.3 swiftinterface bug (#975)
    With envoy-mobile built from Xcode 11.5, we are unable to consume it
    with Xcode 12.0 beta 3. I'm hoping this fixes it, the errors appear
    related:
    
    ```
    deps/cache/dependencies/Envoy/current/Envoy.framework/Modules/Envoy.swiftmodule/x86_64.swiftinterface:137:28: error: overriding declaration requires an 'override' keyword
      @objc convenience public init()
                               ^
                               override
    deps/cache/dependencies/Envoy/current/Envoy.framework/Modules/Envoy.swiftmodule/x86_64.swiftinterface:38:33: note: overridden declaration is here
      @objc override dynamic public init()
                                    ^
    Modules/EnvoyFilters/Sources/EnvoyFilterManager.swift:1:8: error: failed to build module 'Envoy' from its module interface; the compiler that produced it, 'Apple Swift version 5.2.4 (swiftlang-1103.0.32.9 clang-1103.0.32.53)', may have used features that aren't supported by this compiler, 'Apple Swift version 5.3 (swiftlang-1200.0.22.4 clang-1200.0.25.1)'
    import Envoy
           ^
    ```
    
    This reverts commit ec6dfae8ae3ac8cbd0eef8571ca71b636a64a2df.
    
    Signed-off-by: Keith Smiley <keithbsmiley@gmail.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/swift/src/ResponseHeadersBuilder.swift
+++ mobile/library/swift/src/ResponseHeadersBuilder.swift
@@ -4,7 +4,7 @@ import Foundation
 @objcMembers
 public final class ResponseHeadersBuilder: HeadersBuilder {
   /// Initialize a new instance of the builder.
-  public convenience init() {
+  public override convenience init() {
     self.init(headers: [:])
   }
 
