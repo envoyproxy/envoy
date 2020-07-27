b529bba0fc681cfec3c13f60616c727441bb0e63
Yifan Yang <needyyang@google.com>
2020-07-27 16:40:59 +0000

    remove the conditional check and change the proto rules
    Signed-off-by: Yifan Yang <needyyang@google.com>

--- source/extensions/common/tap/tap_matcher.cc
+++ source/extensions/common/tap/tap_matcher.cc
@@ -152,9 +152,7 @@ HttpGenericBodyMatcher::HttpGenericBodyMatcher(
       NOT_REACHED_GCOVR_EXCL_LINE;
     }
     // overlap_size_ indicates how many bytes from previous data chunk(s) are buffered.
-    if (patterns_->back().length() != 0) {
-      overlap_size_ = std::max(overlap_size_, patterns_->back().length() - 1);
-    }
+    overlap_size_ = std::max(overlap_size_, patterns_->back().length() - 1);
   }
   limit_ = config.bytes_limit();
 }
