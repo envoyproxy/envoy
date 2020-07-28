c4c30ec8b5a15b5517d2bca1a4f24508655a4750
gargnupur <gargnupur@google.com>
2020-07-27 17:34:28 -0700

    Add upstream_local_address and upstream_transport_failure_reason fields
    Signed-off-by: gargnupur <gargnupur@google.com>

--- source/extensions/filters/common/expr/context.h
+++ source/extensions/filters/common/expr/context.h
@@ -66,6 +66,8 @@ constexpr absl::string_view Destination = "destination";
 
 // Upstream properties
 constexpr absl::string_view Upstream = "upstream";
+constexpr absl::string_view UpstreamLocalAddress = "upstream_local_address";
+constexpr absl::string_view UpstreamTransportFailureReason = "upstream_transport_failure_reason";
 
 class RequestWrapper;
 
