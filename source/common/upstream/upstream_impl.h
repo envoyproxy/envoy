53a5286b62c85ce16c59df05fc6cf7a0ba914192
alyssawilk <alyssar@chromium.org>
2020-07-29 08:02:52 -0400

    upstream: per-upstream prefetching (#12135)
    Implementing per-upstream prefetching, useful for high QPS or latency-sensitive services.
    
    Risk Level: low (off by default)
    Testing: new unit tests, integration test
    Docs Changes: n/a
    Release Notes: not yet
    
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>
    Signed-off-by: chaoqinli <chaoqinli@google.com>

--- source/common/upstream/upstream_impl.h
+++ source/common/upstream/upstream_impl.h
@@ -536,6 +536,7 @@ public:
   const absl::optional<std::chrono::milliseconds> idleTimeout() const override {
     return idle_timeout_;
   }
+  float prefetchRatio() const override { return prefetch_ratio_; }
   uint32_t perConnectionBufferLimitBytes() const override {
     return per_connection_buffer_limit_bytes_;
   }
@@ -655,6 +656,7 @@ private:
   const uint32_t max_response_headers_count_;
   const std::chrono::milliseconds connect_timeout_;
   absl::optional<std::chrono::milliseconds> idle_timeout_;
+  const float prefetch_ratio_;
   const uint32_t per_connection_buffer_limit_bytes_;
   TransportSocketMatcherPtr socket_matcher_;
   Stats::ScopePtr stats_scope_;
