861cfca3bb6e45491663e5a5e19c539bc9a7ae2a
Jinhui Song <jinhuis2@illinois.edu>
2020-07-29 16:02:22 -0500

    logger: implement Fancy Logger with fine-grained log control (#11822)
    Implement a new logger called Fancy Logger with fine-grained control, e.g. file, function and line level.
    
    Additional Description: Some new macros are defined for basic usage but not hooked with any existing log macros. API for the logger is still in progress.
    
    Risk Level: Low
    Testing: unit test & benchmark
    Docs Changes: None
    Release Notes: None
    
    Signed-off-by: Jinhui Song <jinhuisong@google.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

---
+++ source/common/common/fancy_logger.cc
@@ -0,0 +1,102 @@
+#include "common/common/fancy_logger.h"
+
+#include <atomic>
+#include <memory>
+
+#include "common/common/logger.h"
+
+using spdlog::level::level_enum;
+
+namespace Envoy {
+
+/**
+ * Implements a lock from BasicLockable, to avoid dependency problem of thread.h.
+ */
+class FancyBasicLockable : public Thread::BasicLockable {
+public:
+  // BasicLockable
+  void lock() ABSL_EXCLUSIVE_LOCK_FUNCTION() override { mutex_.Lock(); }
+  bool tryLock() ABSL_EXCLUSIVE_TRYLOCK_FUNCTION(true) override { return mutex_.TryLock(); }
+  void unlock() ABSL_UNLOCK_FUNCTION() override { mutex_.Unlock(); }
+
+private:
+  absl::Mutex mutex_;
+};
+
+SpdLoggerSharedPtr FancyContext::getFancyLogEntry(std::string key)
+    ABSL_LOCKS_EXCLUDED(fancy_log_lock_) {
+  absl::ReaderMutexLock l(&fancy_log_lock_);
+  return fancy_log_map_->find(key)->second;
+}
+
+void FancyContext::initFancyLogger(std::string key, std::atomic<spdlog::logger*>& logger)
+    ABSL_LOCKS_EXCLUDED(fancy_log_lock_) {
+  absl::WriterMutexLock l(&fancy_log_lock_);
+  auto it = fancy_log_map_->find(key);
+  spdlog::logger* target;
+  if (it == fancy_log_map_->end()) {
+    target = createLogger(key);
+  } else {
+    target = it->second.get();
+  }
+  logger.store(target);
+}
+
+bool FancyContext::setFancyLogger(std::string key, level_enum log_level)
+    ABSL_LOCKS_EXCLUDED(fancy_log_lock_) {
+  absl::ReaderMutexLock l(&fancy_log_lock_);
+  auto it = fancy_log_map_->find(key);
+  if (it != fancy_log_map_->end()) {
+    it->second->set_level(log_level);
+    return true;
+  }
+  return false;
+}
+
+void FancyContext::setDefaultFancyLevelFormat(spdlog::level::level_enum level, std::string format)
+    ABSL_LOCKS_EXCLUDED(fancy_log_lock_) {
+  if (level == Logger::Context::getFancyDefaultLevel() &&
+      format == Logger::Context::getFancyLogFormat()) {
+    return;
+  }
+  absl::ReaderMutexLock l(&fancy_log_lock_);
+  for (const auto& it : *fancy_log_map_) {
+    if (it.second->level() == Logger::Context::getFancyDefaultLevel()) {
+      // if logger is default level now
+      it.second->set_level(level);
+    }
+    it.second->set_pattern(format);
+  }
+}
+
+void FancyContext::initSink() {
+  spdlog::sink_ptr sink = Logger::Registry::getSink();
+  Logger::DelegatingLogSinkSharedPtr sp = std::static_pointer_cast<Logger::DelegatingLogSink>(sink);
+  if (!sp->hasLock()) {
+    static FancyBasicLockable tlock;
+    sp->setLock(tlock);
+    sp->setShouldEscape(false);
+  }
+}
+
+spdlog::logger* FancyContext::createLogger(std::string key, int level)
+    ABSL_EXCLUSIVE_LOCKS_REQUIRED(fancy_log_lock_) {
+  SpdLoggerSharedPtr new_logger =
+      std::make_shared<spdlog::logger>(key, Logger::Registry::getSink());
+  if (!Logger::Registry::getSink()->hasLock()) { // occurs in benchmark test
+    initSink();
+  }
+  level_enum lv = Logger::Context::getFancyDefaultLevel();
+  if (level > -1) {
+    lv = static_cast<level_enum>(level);
+  }
+  new_logger->set_level(lv);
+  new_logger->set_pattern(Logger::Context::getFancyLogFormat());
+  new_logger->flush_on(level_enum::critical);
+  fancy_log_map_->insert(std::make_pair(key, new_logger));
+  return new_logger.get();
+}
+
+FancyContext& getFancyContext() { MUTABLE_CONSTRUCT_ON_FIRST_USE(FancyContext); }
+
+} // namespace Envoy
