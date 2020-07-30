bc7378597e2e849e9dd37e26fc967ae43ab07e42
Sunjay Bhatia <sunjayb@vmware.com>
2020-07-30 15:03:24 -0400

    Refer to std::chrono only in TimeSystem as suggested by Lizan
    - Makes it more trivial to switch to absl::Duration at some point
    
    Co-authored-by: Sunjay Bhatia <sunjayb@vmware.com>
    Co-authored-by: William A Rowe Jr <wrowe@vmware.com>
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>
    Signed-off-by: William A Rowe Jr <wrowe@vmware.com>
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>

--- test/extensions/filters/network/kafka/broker/filter_unit_test.cc
+++ test/extensions/filters/network/kafka/broker/filter_unit_test.cc
@@ -218,7 +218,7 @@ TEST_F(KafkaMetricsFacadeImplUnitTest, ShouldRegisterRequest) {
 
   EXPECT_CALL(*request_metrics_, onRequest(api_key));
 
-  MonotonicTime time_point{std::chrono::milliseconds(1234)};
+  MonotonicTime time_point{TimeSystem::Milliseconds(1234)};
   EXPECT_CALL(time_source_, monotonicTime()).WillOnce(Return(time_point));
 
   // when
@@ -248,10 +248,10 @@ TEST_F(KafkaMetricsFacadeImplUnitTest, ShouldRegisterResponse) {
   const int32_t correlation_id = 1234;
   AbstractResponseSharedPtr response = std::make_shared<MockResponse>(api_key, correlation_id);
 
-  MonotonicTime request_time_point{std::chrono::milliseconds(1234)};
+  MonotonicTime request_time_point{TimeSystem::Milliseconds(1234)};
   testee_.getRequestArrivalsForTest()[correlation_id] = request_time_point;
 
-  MonotonicTime response_time_point{std::chrono::milliseconds(2345)};
+  MonotonicTime response_time_point{TimeSystem::Milliseconds(2345)};
 
   EXPECT_CALL(*response_metrics_, onResponse(api_key, 1111));
   EXPECT_CALL(time_source_, monotonicTime()).WillOnce(Return(response_time_point));
