2756e2ee24593ac326f9a43b00e78b02fcc0e65a
pingsun <pingsun@google.com>
2020-07-31 21:25:26 +0000

    solve integration stats conflict
    Signed-off-by: pingsun <pingsun@google.com>

--- test/integration/stats_integration_test.cc
+++ test/integration/stats_integration_test.cc
@@ -286,7 +286,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithFakeSymbolTable) {
   // 2020/07/20  11559    44747       46000   stats: add histograms for request/response headers
   //                                          and body sizes.
   // 2020/07/21  12034    44811       46000   Add configurable histogram buckets.
-  // 2020/07/30  12035    44923       46000   Init manager store unready targets in hash map.
+  // 2020/07/30  12035    44938       46000   Init manager store unready targets in hash map.
 
   // Note: when adjusting this value: EXPECT_MEMORY_EQ is active only in CI
   // 'release' builds, where we control the platform and tool-chain. So you
@@ -304,7 +304,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithFakeSymbolTable) {
   // We only run the exact test for ipv6 because ipv4 in some cases may allocate a
   // different number of bytes. We still run the approximate test.
   if (ip_version_ != Network::Address::IpVersion::v6) {
-    EXPECT_MEMORY_EQ(m_per_cluster, 44923);
+    EXPECT_MEMORY_EQ(m_per_cluster, 44938);
   }
   EXPECT_MEMORY_LE(m_per_cluster, 46000); // Round up to allow platform variations.
 }
@@ -362,7 +362,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithRealSymbolTable) {
   // 2020/07/20  11559    36859       38000   stats: add histograms for request/response headers
   //                                          and body sizes.
   // 2020/07/21  12034    36923       38000   Add configurable histogram buckets.
-  // 2020/07/30  12035    37035       38000   Init manager store unready targets in hash map.
+  // 2020/07/30  12035    37050       38000   Init manager store unready targets in hash map.
 
   // Note: when adjusting this value: EXPECT_MEMORY_EQ is active only in CI
   // 'release' builds, where we control the platform and tool-chain. So you
@@ -380,7 +380,7 @@ TEST_P(ClusterMemoryTestRunner, MemoryLargeClusterSizeWithRealSymbolTable) {
   // We only run the exact test for ipv6 because ipv4 in some cases may allocate a
   // different number of bytes. We still run the approximate test.
   if (ip_version_ != Network::Address::IpVersion::v6) {
-    EXPECT_MEMORY_EQ(m_per_cluster, 37035);
+    EXPECT_MEMORY_EQ(m_per_cluster, 37050);
   }
   EXPECT_MEMORY_LE(m_per_cluster, 38000); // Round up to allow platform variations.
 }
