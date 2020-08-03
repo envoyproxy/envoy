1fd2778a24217535d9807b7851b8be4c1e7f2415
Yifan Yang <stedyyf@gmail.com>
2020-08-03 14:28:17 -0400

    lint: add guards to prevent the use of std::any/optional/variant (#12421)
    Per the discussion on #12341, use of these standard library functions are temporarily banned to prevent runtime failure when running in iOS11.
    
    Signed-off-by: Yifan Yang <needyyang@google.com>
    Signed-off-by: chaoqinli <chaoqinli@google.com>

--- tools/code_format/check_format_test_helper.py
+++ tools/code_format/check_format_test_helper.py
@@ -238,6 +238,11 @@ def runChecks():
   errors += checkUnfixableError(
       "std_unordered_set.cc", "Don't use std::unordered_set; use absl::flat_hash_set instead " +
       "or absl::node_hash_set if pointer stability of keys/values is required")
+  errors += checkUnfixableError("std_any.cc", "Don't use std::any; use absl::any instead")
+  errors += checkUnfixableError("std_optional.cc",
+                                "Don't use std::optional; use absl::optional instead")
+  errors += checkUnfixableError("std_variant.cc",
+                                "Don't use std::variant; use absl::variant instead")
 
   # The following files have errors that can be automatically fixed.
   errors += checkAndFixError("over_enthusiastic_spaces.cc",
