5f13f375403d46c66a289a31e0a18208f3e5f2a6
Oleg Guba <oleg@dropbox.com>
2020-08-06 15:53:55 -0700

    update field description with requests/responses
    Signed-off-by: Oleg Guba <oleg@dropbox.com>

--- include/envoy/http/codec.h
+++ include/envoy/http/codec.h
@@ -374,9 +374,9 @@ struct Http1Settings {
   //  - Is neither a HEAD only request nor a HTTP Upgrade
   //  - Not a HEAD request
   bool enable_trailers_{false};
-  // Allow serving requests with both Content-Length and Transfer-Encoding: chunked.
-  // If enabled and such a request is served - Content-Length is ignored and removed from
-  // the request headers.
+  // Allows Envoy to process requests/responses with both `Content-Length` and `Transfer-Encoding`
+  // headers set. By default such messages are rejected, but if option is enabled - Envoy will
+  // remove Content-Length header and process message.
   bool allow_chunked_length_{false};
 
   enum class HeaderKeyFormat {
