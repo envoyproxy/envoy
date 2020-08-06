871ad1a904aaf29c9f2011b8bb76a151bf34344b
Kuat <kyessenov@users.noreply.github.com>
2020-08-05 17:04:49 -0700

    xds: keep a reference to old config in the filter config provider (#12479)
    Some filter factories allocate TLS slots shared via shared pointers. On a filter config update, the last filter factory reference happens to be deleted on a worker thread, which causes a runtime failure since TLS slots must be deleted on the main thread. The solution is to prolong the life of the filter factory using main thread completion callback.
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

---
+++ test/integration/filters/set_response_code_filter.cc
@@ -0,0 +1,64 @@
+#include <string>
+
+#include "envoy/http/filter.h"
+#include "envoy/registry/registry.h"
+
+#include "extensions/filters/http/common/factory_base.h"
+#include "extensions/filters/http/common/pass_through_filter.h"
+
+#include "test/integration/filters/set_response_code_filter_config.pb.h"
+#include "test/integration/filters/set_response_code_filter_config.pb.validate.h"
+
+#include "absl/strings/match.h"
+
+namespace Envoy {
+
+// A test filter that responds directly with a code on a prefix match.
+class SetResponseCodeFilterConfig {
+public:
+  SetResponseCodeFilterConfig(const std::string& prefix, uint32_t code,
+                              Server::Configuration::FactoryContext& context)
+      : prefix_(prefix), code_(code), tls_slot_(context.threadLocal().allocateSlot()) {}
+
+  const std::string prefix_;
+  const uint32_t code_;
+  // Allocate a slot to validate that it is destroyed on a main thread only.
+  ThreadLocal::SlotPtr tls_slot_;
+};
+
+class SetResponseCodeFilter : public Http::PassThroughFilter {
+public:
+  SetResponseCodeFilter(std::shared_ptr<SetResponseCodeFilterConfig> config) : config_(config) {}
+
+  Http::FilterHeadersStatus decodeHeaders(Http::RequestHeaderMap& headers, bool) override {
+    if (absl::StartsWith(headers.Path()->value().getStringView(), config_->prefix_)) {
+      decoder_callbacks_->sendLocalReply(static_cast<Http::Code>(config_->code_), "", nullptr,
+                                         absl::nullopt, "");
+      return Http::FilterHeadersStatus::StopIteration;
+    }
+    return Http::FilterHeadersStatus::Continue;
+  }
+
+private:
+  const std::shared_ptr<SetResponseCodeFilterConfig> config_;
+};
+
+class SetResponseCodeFilterFactory : public Extensions::HttpFilters::Common::FactoryBase<
+                                         test::integration::filters::SetResponseCodeFilterConfig> {
+public:
+  SetResponseCodeFilterFactory() : FactoryBase("set-response-code-filter") {}
+
+private:
+  Http::FilterFactoryCb createFilterFactoryFromProtoTyped(
+      const test::integration::filters::SetResponseCodeFilterConfig& proto_config,
+      const std::string&, Server::Configuration::FactoryContext& context) override {
+    auto filter_config = std::make_shared<SetResponseCodeFilterConfig>(
+        proto_config.prefix(), proto_config.code(), context);
+    return [filter_config](Http::FilterChainFactoryCallbacks& callbacks) -> void {
+      callbacks.addStreamFilter(std::make_shared<SetResponseCodeFilter>(filter_config));
+    };
+  }
+};
+
+REGISTER_FACTORY(SetResponseCodeFilterFactory, Server::Configuration::NamedHttpFilterConfigFactory);
+} // namespace Envoy
