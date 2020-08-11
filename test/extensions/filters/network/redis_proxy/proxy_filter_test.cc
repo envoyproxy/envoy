043eb2e0cc2f4d05d7cb9116a6d06051f7f42251
FAYiEKcbD0XFqF2QK2E4viAHg8rMm2VbjYKdjTg <nflacco@lyft.com>
2020-08-11 14:31:14 -0700

    fix casing
    Signed-off-by: FAYiEKcbD0XFqF2QK2E4viAHg8rMm2VbjYKdjTg <nflacco@lyft.com>

--- test/extensions/filters/network/redis_proxy/proxy_filter_test.cc
+++ test/extensions/filters/network/redis_proxy/proxy_filter_test.cc
@@ -178,7 +178,7 @@ public:
   CommandSplitter::SplitCallbacks* request_callbacks1_;
   CommandSplitter::SplitCallbacks* request_callbacks2_;
 
-  void DecodeHelper(Buffer::Instance&) {
+  void decodeHelper(Buffer::Instance&) {
     Common::Redis::RespValuePtr request1(new Common::Redis::RespValue());
     EXPECT_CALL(splitter_, makeRequest_(Ref(*request1), _, _))
         .WillOnce(
@@ -198,7 +198,7 @@ TEST_F(RedisProxyFilterTestWithTwoCallbacks, OutOfOrderResponseWithDrainClose) {
 
   Buffer::OwnedImpl fake_data;
   EXPECT_CALL(*decoder_, decode(Ref(fake_data)))
-      .WillOnce(Invoke(this, &RedisProxyFilterTestWithTwoCallbacks::DecodeHelper));
+      .WillOnce(Invoke(this, &RedisProxyFilterTestWithTwoCallbacks::decodeHelper));
   EXPECT_EQ(Network::FilterStatus::Continue, filter_->onData(fake_data, false));
 
   EXPECT_EQ(2UL, config_->stats_.downstream_rq_total_.value());
@@ -226,7 +226,7 @@ TEST_F(RedisProxyFilterTestWithTwoCallbacks, OutOfOrderResponseDownstreamDisconn
 
   Buffer::OwnedImpl fake_data;
   EXPECT_CALL(*decoder_, decode(Ref(fake_data)))
-      .WillOnce(Invoke(this, &RedisProxyFilterTestWithTwoCallbacks::DecodeHelper));
+      .WillOnce(Invoke(this, &RedisProxyFilterTestWithTwoCallbacks::decodeHelper));
   EXPECT_EQ(Network::FilterStatus::Continue, filter_->onData(fake_data, false));
 
   EXPECT_EQ(2UL, config_->stats_.downstream_rq_total_.value());
