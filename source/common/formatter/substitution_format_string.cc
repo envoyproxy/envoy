f36fe9646bfe2f467a419a4cb8be0c2a265cb2ad
Petr Pchelko <ppchelko@wikimedia.org>
2020-08-11 21:23:17 -0700

    Revert removal of createJsonFormatter
    Signed-off-by: Petr Pchelko <ppchelko@wikimedia.org>

--- source/common/formatter/substitution_format_string.cc
+++ source/common/formatter/substitution_format_string.cc
@@ -5,13 +5,19 @@
 namespace Envoy {
 namespace Formatter {
 
+FormatterPtr
+SubstitutionFormatStringUtils::createJsonFormatter(const ProtobufWkt::Struct& struct_format,
+                                                   bool preserve_types) {
+  return std::make_unique<JsonFormatterImpl>(struct_format, preserve_types);
+}
+
 FormatterPtr SubstitutionFormatStringUtils::fromProtoConfig(
     const envoy::config::core::v3::SubstitutionFormatString& config) {
   switch (config.format_case()) {
   case envoy::config::core::v3::SubstitutionFormatString::FormatCase::kTextFormat:
     return std::make_unique<FormatterImpl>(config.text_format());
   case envoy::config::core::v3::SubstitutionFormatString::FormatCase::kJsonFormat: {
-    return std::make_unique<JsonFormatterImpl>(config.json_format(), true);
+    return createJsonFormatter(config.json_format(), true);
   }
   default:
     NOT_REACHED_GCOVR_EXCL_LINE;
