9f955825993d2d357e249d2909ab3c300ee8cd0a
jianwen <jianwendong@google.com>
2020-08-13 12:29:04 -0500

    fixed comments
    Signed-off-by: jianwen <jianwendong@google.com>

--- test/extensions/filters/network/common/fuzz/uber_per_writefilter.cc
+++ test/extensions/filters/network/common/fuzz/uber_per_writefilter.cc
@@ -34,20 +34,25 @@ std::vector<absl::string_view> UberWriteFilterFuzzer::filterNames() {
 
 void UberWriteFilterFuzzer::checkInvalidInputForFuzzer(const std::string& filter_name,
                                                        Protobuf::Message* config_message) {
-  // System calls such as reading files are prohibited in this fuzzer. Some input that crashes the
+  // System calls such as reading files are prohibited in this fuzzer. Some inputs that crash the
   // mock/fake objects are also prohibited. We could also avoid fuzzing some unfinished features by
   // checking them here. For now there is only one filter {MongoProxy} on which we have a
   // constraint.
   const std::string name = Extensions::NetworkFilters::Common::FilterNameUtil::canonicalFilterName(
       std::string(filter_name));
-  if (filter_name == NetworkFilterNames::get().MongoProxy) {
+  else if (filter_name == NetworkFilterNames::get().MongoProxy) {
     envoy::extensions::filters::network::mongo_proxy::v3::MongoProxy& config =
         dynamic_cast<envoy::extensions::filters::network::mongo_proxy::v3::MongoProxy&>(
             *config_message);
     if (config.has_delay() && config.mutable_delay()->has_header_delay()) {
-      // Mongo has no header map. Only fixed_delay is supported.
-      throw EnvoyException(
-          absl::StrCat("Header_delay is not supported here. Config:\n{}", config.DebugString()));
+      // MongoProxy filter doesn't allow header_delay because it will pass nullptr to percentage()
+      // which will cause "runtime error: member call on null pointer". (See:
+      // https://github.com/envoyproxy/envoy/blob/master/source/extensions/filters/network/mongo_proxy/proxy.cc#L403
+      // and
+      // https://github.com/envoyproxy/envoy/blob/master/source/extensions/filters/common/fault/fault_config.cc#L16)
+      throw EnvoyException(absl::StrCat(
+          "header delay is not supported in the config of a MongoProxy filter.. Config:\n{}",
+          config.DebugString()));
     }
   }
 }
