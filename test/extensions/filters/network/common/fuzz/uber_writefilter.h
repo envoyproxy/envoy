cf5c77a7fb4b0a79b9fb2f9f4196d893611a39de
jianwen <jianwendong@google.com>
2020-08-12 22:23:42 -0500

    fixed a comment
    Signed-off-by: jianwen <jianwendong@google.com>

--- test/extensions/filters/network/common/fuzz/uber_writefilter.h
+++ test/extensions/filters/network/common/fuzz/uber_writefilter.h
@@ -19,7 +19,7 @@ public:
       const Protobuf::RepeatedPtrField<::test::extensions::filters::network::WriteAction>& actions);
   // Get the name of filters which has been covered by this fuzzer.
   static std::vector<absl::string_view> filterNames();
-  // Check whether the filter's config is invalid for fuzzer(e.g. system call).
+  // Check whether the filter's config is invalid for fuzzer.
   void checkInvalidInputForFuzzer(const std::string& filter_name,
                                   Protobuf::Message* config_message);
 
