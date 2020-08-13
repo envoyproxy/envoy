ff024e7afb737fbe7d4f477401f94b51faf63b9f
Oleg Guba <oleg@dropbox.com>
2020-08-12 17:25:34 -0700

    add response test
    Signed-off-by: Oleg Guba <oleg@dropbox.com>

--- test/common/http/http1/codec_impl_test.cc
+++ test/common/http/http1/codec_impl_test.cc
@@ -95,7 +95,7 @@ public:
   // Used to test if trailers are decoded/encoded
   void expectTrailersTest(bool enable_trailers);
 
-  void testAllowChunkedContentLength(uint32_t content_length, bool allow_chunked_length);
+  void testServerAllowChunkedContentLength(uint32_t content_length, bool allow_chunked_length);
 
   // Send the request, and validate the received request headers.
   // Then send a response just to clean up.
@@ -328,8 +328,8 @@ void Http1ServerConnectionImplTest::testRequestHeadersAccepted(std::string heade
   EXPECT_TRUE(status.ok());
 }
 
-void Http1ServerConnectionImplTest::testAllowChunkedContentLength(uint32_t content_length,
-                                                                  bool allow_chunked_length) {
+void Http1ServerConnectionImplTest::testServerAllowChunkedContentLength(uint32_t content_length,
+                                                                        bool allow_chunked_length) {
   codec_settings_.allow_chunked_length_ = allow_chunked_length;
   if (GetParam()) {
     codec_ = std::make_unique<Http1::ServerConnectionImpl>(
@@ -1898,6 +1898,30 @@ TEST_P(Http1ServerConnectionImplTest, WatermarkTest) {
       ->onUnderlyingConnectionBelowWriteBufferLowWatermark();
 }
 
+TEST_P(Http1ServerConnectionImplTest, TestSmugglingDisallowChunkedContentLength0) {
+  testServerAllowChunkedContentLength(0, false);
+}
+TEST_P(Http1ServerConnectionImplTest, TestSmugglingDisallowChunkedContentLength1) {
+  // content-length less than POST body size
+  testServerAllowChunkedContentLength(1, false);
+}
+TEST_P(Http1ServerConnectionImplTest, TestSmugglingDisallowChunkedContentLength100) {
+  // content-length greater than POST body size
+  testServerAllowChunkedContentLength(100, false);
+}
+
+TEST_P(Http1ServerConnectionImplTest, TestSmugglingAllowChunkedContentLength0) {
+  testServerAllowChunkedContentLength(0, true);
+}
+TEST_P(Http1ServerConnectionImplTest, TestSmugglingAllowChunkedContentLength1) {
+  // content-length less than POST body size
+  testServerAllowChunkedContentLength(1, true);
+}
+TEST_P(Http1ServerConnectionImplTest, TestSmugglingAllowChunkedContentLength100) {
+  // content-length greater than POST body size
+  testServerAllowChunkedContentLength(100, true);
+}
+
 class Http1ClientConnectionImplTest : public Http1CodecTestBase,
                                       public testing::TestWithParam<bool> {
 public:
@@ -1924,6 +1948,8 @@ public:
   NiceMock<Http1Settings> codec_settings_;
   Http::ClientConnectionPtr codec_;
 
+  void testClientAllowChunkedContentLength(uint32_t content_length, bool allow_chunked_length);
+
 protected:
   Stats::TestUtil::TestStore store_;
   uint32_t max_response_headers_count_{Http::DEFAULT_MAX_HEADERS_COUNT};
@@ -1933,6 +1959,52 @@ INSTANTIATE_TEST_SUITE_P(Codecs, Http1ClientConnectionImplTest, testing::Bool(),
                          [](const testing::TestParamInfo<bool>& param) {
                            return param.param ? "New" : "Legacy";
                          });
+
+void Http1ClientConnectionImplTest::testClientAllowChunkedContentLength(uint32_t content_length,
+                                                                        bool allow_chunked_length) {
+  codec_settings_.allow_chunked_length_ = allow_chunked_length;
+  if (GetParam()) {
+    codec_ = std::make_unique<Http1::ClientConnectionImpl>(
+        connection_, http1CodecStats(), callbacks_, codec_settings_, max_response_headers_count_);
+  } else {
+    codec_ = std::make_unique<Legacy::Http1::ClientConnectionImpl>(
+        connection_, http1CodecStats(), callbacks_, codec_settings_, max_response_headers_count_);
+  }
+
+  NiceMock<MockResponseDecoder> response_decoder;
+  Http::RequestEncoder& request_encoder = codec_->newStream(response_decoder);
+
+  TestRequestHeaderMapImpl headers{{":method", "GET"}, {":path", "/"}, {":authority", "host"}};
+  request_encoder.encodeHeaders(headers, true);
+
+  TestResponseHeaderMapImpl expected_headers{{":status", "200"}, {"transfer-encoding", "chunked"}};
+  Buffer::OwnedImpl expected_data("Hello World");
+
+  if (allow_chunked_length) {
+    EXPECT_CALL(response_decoder, decodeHeaders_(HeaderMapEqual(&expected_headers), false));
+    EXPECT_CALL(response_decoder, decodeData(BufferEqual(&expected_data), false));
+    EXPECT_CALL(response_decoder, decodeData(_, true));
+  } else {
+    EXPECT_CALL(response_decoder, decodeHeaders_(_, _)).Times(0);
+    EXPECT_CALL(response_decoder, decodeData(_, _)).Times(0);
+  }
+
+  Buffer::OwnedImpl buffer(
+      fmt::format("HTTP/1.1 200 OK\r\ntransfer-encoding: chunked\r\ncontent-length: {}\r\n\r\n"
+                  "6\r\nHello \r\n"
+                  "5\r\nWorld\r\n"
+                  "0\r\n\r\n",
+                  content_length));
+  auto status = codec_->dispatch(buffer);
+
+  if (allow_chunked_length) {
+    EXPECT_TRUE(status.ok());
+  } else {
+    EXPECT_TRUE(isCodecProtocolError(status));
+    EXPECT_EQ(status.message(),
+              "http/1.1 protocol error: both 'Content-Length' and 'Transfer-Encoding' are set.");
+  };
+}
 
 TEST_P(Http1ClientConnectionImplTest, SimpleGet) {
   initialize();
@@ -2886,28 +2958,59 @@ TEST_P(Http1ClientConnectionImplTest, ManyResponseHeadersAccepted) {
   status = codec_->dispatch(buffer);
 }
 
-TEST_P(Http1ServerConnectionImplTest, TestSmugglingDisallowChunkedContentLength0) {
-  testAllowChunkedContentLength(0, false);
-}
-TEST_P(Http1ServerConnectionImplTest, TestSmugglingDisallowChunkedContentLength1) {
-  // content-length less than POST body size
-  testAllowChunkedContentLength(1, false);
-}
-TEST_P(Http1ServerConnectionImplTest, TestSmugglingDisallowChunkedContentLength100) {
-  // content-length greater than POST body size
-  testAllowChunkedContentLength(100, false);
+TEST_P(Http1ClientConnectionImplTest, TestResponseSplit0) {
+  testClientAllowChunkedContentLength(0, false);
 }
 
-TEST_P(Http1ServerConnectionImplTest, TestSmugglingAllowChunkedContentLength0) {
-  testAllowChunkedContentLength(0, true);
+TEST_P(Http1ClientConnectionImplTest, TestResponseSplit1) {
+  testClientAllowChunkedContentLength(1, false);
 }
-TEST_P(Http1ServerConnectionImplTest, TestSmugglingAllowChunkedContentLength1) {
-  // content-length less than POST body size
-  testAllowChunkedContentLength(1, true);
+
+TEST_P(Http1ClientConnectionImplTest, TestResponseSplit100) {
+  testClientAllowChunkedContentLength(100, false);
 }
-TEST_P(Http1ServerConnectionImplTest, TestSmugglingAllowChunkedContentLength100) {
-  // content-length greater than POST body size
-  testAllowChunkedContentLength(100, true);
+
+TEST_P(Http1ClientConnectionImplTest, TestResponseSplitAllowChunkedLength0) {
+  testClientAllowChunkedContentLength(0, true);
+}
+
+TEST_P(Http1ClientConnectionImplTest, TestResponseSplitAllowChunkedLength1) {
+  testClientAllowChunkedContentLength(1, true);
+}
+
+TEST_P(Http1ClientConnectionImplTest, TestResponseSplitAllowChunkedLength100) {
+  testClientAllowChunkedContentLength(100, true);
+}
+
+TEST_P(Http1ClientConnectionImplTest, TestUnknown) {
+  codec_settings_.allow_chunked_length_ = true;
+  initialize();
+
+  NiceMock<MockResponseDecoder> response_decoder;
+  Http::RequestEncoder& request_encoder = codec_->newStream(response_decoder);
+
+  TestRequestHeaderMapImpl headers{{":method", "GET"}, {":path", "/"}, {":authority", "host"}};
+  request_encoder.encodeHeaders(headers, true);
+
+  TestResponseHeaderMapImpl expected_headers{{":status", "200"}, {"transfer-encoding", "chunked"}};
+  Buffer::OwnedImpl expected_data("Hello World");
+
+  EXPECT_CALL(response_decoder, decodeHeaders_(HeaderMapEqual(&expected_headers), false));
+  EXPECT_CALL(response_decoder, decodeData(BufferEqual(&expected_data), false));
+  EXPECT_CALL(response_decoder, decodeData(_, true));
+
+  Buffer::OwnedImpl buffer(
+      "HTTP/1.1 200 OK\r\n"
+      "Content-Length: 0\r\n"
+      "Transfer-Encoding: chunked\r\n"
+      "\r\n"
+      "6\r\nHello \r\n"
+      "5\r\nWorld\r\n"
+      "0\r\n\r\n");
+  auto status = codec_->dispatch(buffer);
+
+  EXPECT_EQ(status.message(), "NONE");
+  // EXPECT_TRUE(status.ok());
 }
 
 } // namespace Http
