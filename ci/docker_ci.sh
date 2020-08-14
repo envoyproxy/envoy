f30e6cfbc6ad7f9814caf204308a2c25c83765c0
Lizan Zhou <lizan@tetrate.io>
2020-08-14 04:13:14 -0700

    enable docker in presubmit
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/docker_ci.sh
+++ ci/docker_ci.sh
@@ -4,6 +4,8 @@
 # CI logs.
 set -e
 
+ENVOY_DOCKER_IMAGE_DIRECTORY="${ENVOY_DOCKER_IMAGE_DIRECTORY:-${BUILD_STAGINGDIRECTORY:-.}/docker_images}"
+
 # Setting environments for buildx tools
 config_env(){
     # Qemu configurations
@@ -19,11 +21,11 @@ build_images(){
     BUILD_TAG=$2
 
     # Only build/push envoyproxy/envoy multi-arch images since others still do not support.
-    if [ -z "${TYPE}" ]; then
+    if [[ -z "${TYPE}" ]]; then
         docker buildx build --platform linux/arm64 -f ci/Dockerfile-envoy"${TYPE}" -t ${BUILD_TAG} .
         # Export envoyproxy/envoy amd64 image which will be used for building envoyproxy/envoy-google-vrp
         docker buildx build --platform linux/amd64 -f ci/Dockerfile-envoy"${TYPE}" -o type=docker -t ${BUILD_TAG} .
-    elif [ "${TYPE}" == "-google-vrp" ]; then
+    elif [[ "${TYPE}" == "-google-vrp" ]]; then
         # The envoyproxy/envoy-google-vrp is based on envoyproxy/envoy image. So it is built from cache envoyproxy/envoy:local
         docker build -f ci/Dockerfile-envoy"${TYPE}" --cache-from "${DOCKER_IMAGE_PREFIX}:local" -t ${BUILD_TAG} .
     else
@@ -35,12 +37,16 @@ push_images(){
     TYPE=$1
     BUILD_TAG=$2
 
-    if [ -z "${TYPE}" ]; then
+    if [[ -z "${TYPE}" ]]; then
         # Only push envoyproxy/envoy multi-arch images since others still do not support.
         docker buildx build --platform linux/arm64,linux/amd64 --push -f ci/Dockerfile-envoy"${TYPE}" -t ${BUILD_TAG} .
     else
         docker tag "${DOCKER_IMAGE_PREFIX}${TYPE}:local" ${BUILD_TAG}
-        docker push ${BUILD_TAG}
+        if [[ -z "${SAVE_IMAGES}" ]]; then
+            docker push ${BUILD_TAG}
+        else
+            docker save ${BUILD_TAG} | xz -T0 -1 > "${ENVOY_DOCKER_IMAGE_DIRECTORY}/envoy-docker${TYPE}-${BUILD_TAG}.tar.xz"
+        fi
     fi
 }
 
@@ -48,7 +54,7 @@ push_images(){
 DOCKER_IMAGE_PREFIX="${DOCKER_IMAGE_PREFIX:-envoyproxy/envoy}"
 
 # "-google-vrp" must come afer "" to ensure we rebuild the local base image dependency.
-BUILD_TYPES=("" "-alpine" "-alpine-debug" "-google-vrp")
+BUILD_TYPES=("") # "-alpine" "-alpine-debug" "-google-vrp")
 
 # Configure docker-buildx tools
 config_env
@@ -67,8 +73,13 @@ RELEASE_TAG_REGEX="^refs/tags/v.*"
 if [[ "${AZP_BRANCH}" != "${MASTER_BRANCH}" ]] && \
    ! [[ "${AZP_BRANCH}" =~ ${RELEASE_BRANCH_REGEX} ]] && \
    ! [[ "${AZP_BRANCH}" =~ ${RELEASE_TAG_REGEX} ]]; then
-    echo 'Ignoring non-master branch or tag for docker push.'
-    exit 0
+    if [[ -z "${BUILD_REASON}" ]]; then
+        echo 'Ignoring non-master branch or tag for docker push.'
+        exit 0
+    else
+        echo 'Running CI in non-master branch or tag for docker push, save images instead.'
+        SAVE_IMAGES=true
+    fi
 fi
 
 # For master builds and release branch builds use the dev repo. Otherwise we assume it's a tag and
@@ -82,7 +93,7 @@ else
   IMAGE_NAME="${AZP_BRANCH/refs\/tags\//}"
 fi
 
-docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
+[[ -z "${SAVE_IMAGES}" ]] && docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
 
 for BUILD_TYPE in "${BUILD_TYPES[@]}"; do
     push_images "${BUILD_TYPE}" "${DOCKER_IMAGE_PREFIX}${BUILD_TYPE}${IMAGE_POSTFIX}:${IMAGE_NAME}"
