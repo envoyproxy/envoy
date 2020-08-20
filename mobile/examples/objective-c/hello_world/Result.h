eac6b8856de01c4db21a39ff43734c52b5aae87d
Mike Schore <mike.schore@gmail.com>
2020-08-20 11:43:57 +0800

    iOS example apps: improve header rendering (#1041)
    Description: Allows rendering of additional headers. Leftover from testing platform filters; seemed worth cleaning up and merging.
    
    Signed-off-by: Mike Schore <mike.schore@gmail.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/examples/objective-c/hello_world/Result.h
+++ mobile/examples/objective-c/hello_world/Result.h
@@ -3,6 +3,6 @@
 /// Represents a response from the server or an error.
 @interface Result : NSObject
 @property (nonatomic, strong) NSString *message;
-@property (nonatomic, strong) NSString *serverHeader;
+@property (nonatomic, strong) NSString *headerMessage;
 @property (nonatomic, strong) NSString *error;
 @end
