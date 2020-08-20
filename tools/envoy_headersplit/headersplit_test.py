604023009958d45e79e2bffcbe112cde8f9a622b
Muge Chen <mugechen@google.com>
2020-08-20 08:18:02 +0000

    format fix
    Signed-off-by: Muge Chen <mugechen@google.com>

--- tools/envoy_headersplit/headersplit_test.py
+++ tools/envoy_headersplit/headersplit_test.py
@@ -8,20 +8,16 @@ import clang.cindex
 from clang.cindex import TranslationUnit, Index, CursorKind
 clang.cindex.Config.set_library_path("/opt/llvm/lib")
 
-
 class HeadersplitTest(unittest.TestCase):
   # A header contains a simple class print hello world
-  source_code_hello_world = open('code_corpus/hello.h', 'r').read()
-
+  source_code_hello_world = open("code_corpus/hello.h", "r").read()
   # A C++ source code contains definition for several classes
-  source_class_defn = open('code_corpus/class_defn.h', 'r').read()
-
+  source_class_defn = open("code_corpus/class_defn.h", "r").read()
   # almost the same as above, but classes are not enclosed by namespace
-  source_class_defn_without_namespace = open('code_corpus/class_defn_without_namespace.h',
-                                             'r').read()
-
+  source_class_defn_without_namespace = open("code_corpus/class_defn_without_namespace.h",
+                                             "r").read()
   # A C++ source code contains method implementaions for class_defn.h
-  source_class_impl = open('code_corpus/class_impl.cc', 'r').read()
+  source_class_impl = open("code_corpus/class_impl.cc", "r").read()
 
   def test_to_filename(self):
     # Test class name with one "mock"
@@ -45,28 +41,21 @@ class HeadersplitTest(unittest.TestCase):
 #include "foo/bar"
 
 """
-
     translation_unit_hello_world = TranslationUnit.from_source(
         "code_corpus/hello.h", options=TranslationUnit.PARSE_SKIP_FUNCTION_BODIES)
     self.assertEqual(headersplit.get_directives(translation_unit_hello_world), includes)
 
   def test_class_definitions(self):
-    # class_definitions() should return a list of cursors, each pointing to one of those classes
-
     idx = Index.create()
-    translation_unit_class_defn = idx.parse("code_corpus/class_defn.h", ['-x', 'c++'])
+    translation_unit_class_defn = idx.parse("code_corpus/class_defn.h", ["-x", "c++"])
     defns_cursors = headersplit.class_definitions(translation_unit_class_defn.cursor)
     defns_names = [cursor.spelling for cursor in defns_cursors]
-
-    self.assertEqual(defns_names, ['Foo', 'Bar', 'FooBar', 'DeadBeaf'])
-
+    self.assertEqual(defns_names, ["Foo", "Bar", "FooBar", "DeadBeaf"])
     idx = Index.create()
     translation_unit_class_defn = idx.parse("code_corpus/class_defn_without_namespace.h",
-                                            ['-x', 'c++'])
+                                            ["-x", "c++"])
     defns_cursors = headersplit.class_definitions(translation_unit_class_defn.cursor)
     defns_names = [cursor.spelling for cursor in defns_cursors]
-
-    # class_definitions() should ignore all classes outside the namespaces
     self.assertEqual(defns_names, [])
 
   def test_class_implementations(self):
@@ -74,34 +63,29 @@ class HeadersplitTest(unittest.TestCase):
         "code_corpus/class_impl.cc", options=TranslationUnit.PARSE_SKIP_FUNCTION_BODIES)
     impls_cursors = headersplit.class_implementations(translation_unit_class_impl.cursor)
     impls_names = [cursor.spelling for cursor in impls_cursors]
-
-    self.assertEqual(impls_names, ['getFoo', 'val', 'DeadBeaf'])
+    self.assertEqual(impls_names, ["getFoo", "val", "DeadBeaf"])
 
   def test_class_implementations_error(self):
-    # LibClang will fail in parse this source file (it's the origin test/server/mocks.cc from Envoy repository)
+    # LibClang will fail in parse this source file (it's modified from the original test/server/mocks.cc from Envoy repository)
     # if we don't add flag PARSE_SKIP_FUNCTION_BODIES to ignore function bodies.
-    impl_translation_unit = TranslationUnit.from_source('code_corpus/fail_mocks.cc')
-
+    impl_translation_unit = TranslationUnit.from_source("code_corpus/fail_mocks.cc")
     impls_cursors = headersplit.class_implementations(impl_translation_unit.cursor)
     # impls_name is not complete in this case
     impls_names = [cursor.spelling for cursor in impls_cursors]
-
     # LibClang will stop parsing at
     # MockListenerComponentFactory::MockListenerComponentFactory()
     #     : socket_(std::make_shared<NiceMock<Network::MockListenSocket>>()) {
     #       ^
     # Since parsing stops early, we will have incomplete method list.
-    #
     # The reason is not clear, however, this issue can be addressed by adding parsing flag to ignore function body
 
     # get correct list of member methods
     impl_translation_unit_correct = TranslationUnit.from_source(
-        'code_corpus/fail_mocks.cc', options=TranslationUnit.PARSE_SKIP_FUNCTION_BODIES)
+        "code_corpus/fail_mocks.cc", options=TranslationUnit.PARSE_SKIP_FUNCTION_BODIES)
     impls_cursors_correct = headersplit.class_implementations(impl_translation_unit_correct.cursor)
     impls_names_correct = [cursor.spelling for cursor in impls_cursors_correct]
-
     self.assertNotEqual(impls_names, impls_names_correct)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   unittest.main()
