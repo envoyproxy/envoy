1939e5990dd398c6b5b9e966f1798b930e4aff69
Yifan Yang <needyyang@google.com>
2020-08-24 18:50:52 +0000

    condensing the shell script
    Signed-off-by: Yifan Yang <needyyang@google.com>

--- .github/workflows/get_build_targets.sh
+++ .github/workflows/get_build_targets.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 
-TMP_OUTPUT_FILE="tmp.txt"
-QUERY_OUTPUT_FILE="tmp_query.txt"
+readonly TMP_OUTPUT_FILE="tmp.txt"
+readonly QUERY_OUTPUT_FILE="tmp_query.txt"
 
 # This limits the directory that bazel query is going to search under.
-SEARCH_FOLDER="//source/common/..."
-git fetch
-git diff --name-only ..origin/master > $TMP_OUTPUT_FILE
+readonly SEARCH_FOLDER="//source/common/..."
 
-declare -A query_result
+set -eu -o pipefail
 
-while IFS= read -r line
+git fetch https://github.com/envoyproxy/envoy.git master 2>/dev/null
+
+git diff --name-only $ORIGINAL_HEAD FETCH_HEAD | while IFS= read -r line
 do
-  # Split the line by '/'.
-  splited_line=(${line//// })
   # Only targets under those folders.
-  if [[ "$splited_line" == "source" || "$splited_line" == "include" ]]; then
-    bazel query "rdeps($SEARCH_FOLDER, $line, 1)" > $QUERY_OUTPUT_FILE
-    while IFS= read -r query_line
-    do
-      query_result["$query_line"]="$query_line"
-    done < $QUERY_OUTPUT_FILE
-  fi
-done < $TMP_OUTPUT_FILE
-
-RES=""
-for result in "${!query_result[@]}"; 
-do 
-  RES+="$result " 
-done
-
-export BUILD_TARGETS="$RES"
-rm $TMP_OUTPUT_FILE
-rm $QUERY_OUTPUT_FILE
+  case "$line" in
+    source/*|include/*)
+      bazel query "rdeps($SEARCH_FOLDER, $line, 1)" 2>/dev/null
+      ;;
+  esac
+done | sort -u
