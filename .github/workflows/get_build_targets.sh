a2f7243678dc496eaadd63fae3eb783075ba1b2a
Yifan Yang <needyyang@google.com>
2020-08-24 15:55:13 +0000

    update on tmp output files name
    Signed-off-by: Yifan Yang <needyyang@google.com>

--- .github/workflows/get_build_targets.sh
+++ .github/workflows/get_build_targets.sh
@@ -4,7 +4,7 @@ TMP_OUTPUT_FILE="tmp.txt"
 QUERY_OUTPUT_FILE="tmp_query.txt"
 SEARCH_FOLDER="//source/common/..."
 git fetch
-git diff --name-only ..origin/master > TMP_OUTPUT_FILE
+git diff --name-only ..origin/master > $TMP_OUTPUT_FILE
 
 declare -A query_result
 
@@ -14,14 +14,14 @@ do
   arrIN=(${line//// })
   #echo "$arrIN"
   if [[ "$arrIN" == "source" || "$arrIN" == "include" ]]; then
-    bazel query "rdeps($SEARCH_FOLDER, $line, 1)" > QUERY_OUTPUT_FILE
+    bazel query "rdeps($SEARCH_FOLDER, $line, 1)" > $QUERY_OUTPUT_FILE
     while IFS= read -r query_line
     do
       #echo "$query_line"
       query_result["$query_line"]="$query_line"
-    done < QUERY_OUTPUT_FILE
+    done < $QUERY_OUTPUT_FILE
   fi
-done < TMP_OUTPUT_FILE
+done < $TMP_OUTPUT_FILE
 
 RES=""
 for result in "${!query_result[@]}"; 
