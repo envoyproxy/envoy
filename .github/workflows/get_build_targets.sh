5753ac6c8dfe66b26080384b12ff6921fc08a76f
Yifan Yang <needyyang@google.com>
2020-08-24 16:02:01 +0000

    add some comments as well as actually ask codeql to use the build targets generated
    Signed-off-by: Yifan Yang <needyyang@google.com>

--- .github/workflows/get_build_targets.sh
+++ .github/workflows/get_build_targets.sh
@@ -2,6 +2,8 @@
 
 TMP_OUTPUT_FILE="tmp.txt"
 QUERY_OUTPUT_FILE="tmp_query.txt"
+
+# This limits the directory that bazel query is going to search under.
 SEARCH_FOLDER="//source/common/..."
 git fetch
 git diff --name-only ..origin/master > $TMP_OUTPUT_FILE
@@ -10,14 +12,13 @@ declare -A query_result
 
 while IFS= read -r line
 do
-  #echo "$line"
-  arrIN=(${line//// })
-  #echo "$arrIN"
-  if [[ "$arrIN" == "source" || "$arrIN" == "include" ]]; then
+  # Split the line by '/'.
+  splited_line=(${line//// })
+  # Only targets under those folders.
+  if [[ "$splited_line" == "source" || "$splited_line" == "include" ]]; then
     bazel query "rdeps($SEARCH_FOLDER, $line, 1)" > $QUERY_OUTPUT_FILE
     while IFS= read -r query_line
     do
-      #echo "$query_line"
       query_result["$query_line"]="$query_line"
     done < $QUERY_OUTPUT_FILE
   fi
@@ -26,7 +27,6 @@ done < $TMP_OUTPUT_FILE
 RES=""
 for result in "${!query_result[@]}"; 
 do 
-  echo "$result"; 
   RES+="$result " 
 done
 
