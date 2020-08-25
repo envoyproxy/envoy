afbc147150f35c1233abe6dac8c75b14ffb9e4ca
Mike Schore <mike.schore@gmail.com>
2020-08-25 10:55:56 -0700

    jni: separate Android and base JNI interfaces (#1049)
    Description: Split interfaces to allow for creation of a non-Android build target.
    Risk Level: Low
    Testing: Local and CI
    
    Signed-off-by: Mike Schore <mike.schore@gmail.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/common/jni/jni_interface.cc
+++ mobile/library/common/jni/jni_interface.cc
@@ -62,42 +62,11 @@ Java_io_envoyproxy_envoymobile_engine_JniLibrary_filterTemplateString(JNIEnv* en
   return result;
 }
 
-// AndroidJniLibrary
-
-extern "C" JNIEXPORT jint JNICALL
-Java_io_envoyproxy_envoymobile_engine_AndroidJniLibrary_initialize(JNIEnv* env,
-                                                                   jclass, // class
-                                                                   jobject connectivity_manager) {
-  // See note above about c-ares.
-  // c-ares jvm init is necessary in order to let c-ares perform DNS resolution in Envoy.
-  // More information can be found at:
-  // https://c-ares.haxx.se/ares_library_init_android.html
-  ares_library_init_jvm(get_vm());
-
-  return ares_library_init_android(connectivity_manager);
-}
-
-extern "C" JNIEXPORT jint JNICALL
-Java_io_envoyproxy_envoymobile_engine_AndroidJniLibrary_setPreferredNetwork(JNIEnv* env,
-                                                                            jclass, // class
-                                                                            jint network) {
-  __android_log_write(ANDROID_LOG_INFO, "[Envoy]", "setting preferred network");
-  return set_preferred_network(static_cast<envoy_network_t>(network));
-}
-
 extern "C" JNIEXPORT void JNICALL
 Java_io_envoyproxy_envoymobile_engine_JniLibrary_recordCounter(JNIEnv* env,
                                                                jclass, // class
                                                                jstring elements, jint count) {
   record_counter(env->GetStringUTFChars(elements, nullptr), count);
-}
-
-extern "C" JNIEXPORT void JNICALL
-Java_io_envoyproxy_envoymobile_engine_AndroidJniLibrary_flushStats(JNIEnv* env,
-                                                                   jclass // class
-) {
-  __android_log_write(ANDROID_LOG_INFO, "[Envoy]", "triggering stats flush");
-  flush_stats();
 }
 
 // JvmCallbackContext
