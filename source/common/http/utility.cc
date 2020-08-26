a3e180aec37d61664cc892a32e84fb75bc54dcc0
Wayne Zhang <qiwzhang@google.com>
2020-08-26 13:51:29 -0700

    fix format
    Signed-off-by: Wayne Zhang <qiwzhang@google.com>

--- source/common/http/utility.cc
+++ source/common/http/utility.cc
@@ -476,7 +476,8 @@ void Utility::sendLocalReply(const bool& is_reset, const EncodeFunctions& encode
     response_headers->setContentLength(body_text.size());
     // If the `rewrite` function has changed body_text or content-type is not set, set it.
     // This allows `modify_headers` function to set content-type for the body. For example,
-    // router.direct_response is calling sendLocalReply and wants to set conent-type for the body.
+    // router direct response is calling sendLocalReply and may need to set conent-type for
+    // the body.
     if (body_text != local_reply_data.body_text_ || response_headers->ContentType() == nullptr) {
       response_headers->setReferenceContentType(content_type);
     }
