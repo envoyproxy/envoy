d0a09a05c16c348f8ea0231e1ca128ac6f68ac17
Joshua Rutherford <joshua.rutherford@deciphernow.com>
2020-08-26 11:32:16 -0400

    tls: add intermediate ca crl tests and documentation (#12788)
    Adds intermediate certificate authority revocation list documentation and tests that cover revoked, unrevoked and incomplete certificate revocation list chains.
    
    Signed-off-by: Joshua Rutherford <joshua.rutherford@greymatter.io>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/extensions/transport_sockets/tls/test_data/certs.sh
+++ test/extensions/transport_sockets/tls/test_data/certs.sh
@@ -12,6 +12,7 @@ cleanup() {
   rm *csr
   rm *srl
   rm crl_*
+  rm intermediate_csr_*
 }
 
 # $1=<CA name> $2=[issuer name]
@@ -83,6 +84,9 @@ generate_x509_cert ca ca
 # Generate intermediate_ca_cert.pem.
 generate_ca intermediate_ca ca
 
+# Concatenate intermediate_ca_cert.pem and ca_cert.pem to create valid certificate chain.
+cat intermediate_ca_cert.pem ca_cert.pem > intermediate_ca_cert_chain.pem
+
 # Generate fake_ca_cert.pem.
 generate_ca fake_ca
 
@@ -111,6 +115,12 @@ cp -f san_dns_cert.cfg san_dns3_cert.cfg
 generate_rsa_key san_dns3
 generate_x509_cert san_dns3 intermediate_ca
 rm -f san_dns3_cert.cfg
+
+# Generate san_dns4_cert.pm (signed by intermediate_ca_cert.pem).
+cp -f san_dns_cert.cfg san_dns4_cert.cfg
+generate_rsa_key san_dns4
+generate_x509_cert san_dns4 intermediate_ca
+rm -f san_dns4_cert.cfg
 
 # Concatenate san_dns3_cert.pem and Test Intermediate CA (intermediate_ca_cert.pem) to create valid certificate chain.
 cat san_dns3_cert.pem intermediate_ca_cert.pem > san_dns3_chain.pem
@@ -191,14 +201,25 @@ generate_rsa_key expired_san_uri
 generate_x509_cert expired_san_uri ca -365
 rm -f expired_san_uri_cert.cfg
 
-# Initialize information for CRL process
+# Initialize information for root CRL process
 touch crl_index.txt crl_index.txt.attr
 echo 00 > crl_number
 
-# Revoke the certificate and generate a CRL
+# Revoke the certificate and generate a CRL (using root)
 openssl ca -revoke san_dns_cert.pem -keyfile ca_key.pem -cert ca_cert.pem -config ca_cert.cfg
 openssl ca -gencrl -keyfile ca_key.pem -cert ca_cert.pem -out ca_cert.crl -config ca_cert.cfg
 cat ca_cert.pem ca_cert.crl > ca_cert_with_crl.pem
+
+# Initialize information for intermediate CRL process
+touch intermediate_crl_index.txt intermediate_crl_index.txt.attr
+echo 00 > intermediate_crl_number
+
+# Revoke the certificate and generate a CRL (using intermediate)
+openssl ca -revoke san_dns3_cert.pem -keyfile intermediate_ca_key.pem -cert intermediate_ca_cert.pem -config intermediate_ca_cert.cfg
+openssl ca -gencrl -keyfile intermediate_ca_key.pem -cert intermediate_ca_cert.pem -out intermediate_ca_cert.crl -config intermediate_ca_cert.cfg
+cat ca_cert.crl intermediate_ca_cert.crl > intermediate_ca_cert_chain.crl
+cat ca_cert.pem intermediate_ca_cert.pem intermediate_ca_cert.crl > intermediate_ca_cert_chain_with_crl.pem
+cat ca_cert.pem intermediate_ca_cert.pem ca_cert.crl intermediate_ca_cert.crl > intermediate_ca_cert_chain_with_crl_chain.pem
 
 # Write session ticket key files
 openssl rand 80 > ticket_key_a
