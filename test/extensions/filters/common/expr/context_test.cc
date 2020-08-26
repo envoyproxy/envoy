76c2028b90ce9e7cd55d7f7794d6399e3a5864af
Arthur Yan <arthuryan@google.com>
2020-08-26 03:40:42 +0000

    Fixed newline character in unit test
    Signed-off-by: Arthur Yan <arthuryan@google.com>

--- test/extensions/filters/common/expr/context_test.cc
+++ test/extensions/filters/common/expr/context_test.cc
@@ -33,7 +33,7 @@ TEST(Context, EmptyHeadersAttributes) {
 TEST(Context, InvalidRequest) {
   Http::TestRequestHeaderMapImpl header_map{{"referer", "dogs.com"}};
   HeadersWrapper<Http::RequestHeaderMap> headers(&header_map);
-  auto header = headers[CelValue::CreateStringView("dogs.com/n")];
+  auto header = headers[CelValue::CreateStringView("dogs.com\n")];
   EXPECT_FALSE(header.has_value());
 }
 
