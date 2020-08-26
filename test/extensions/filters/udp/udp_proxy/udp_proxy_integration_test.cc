6f96811124c8f4517f841cc9069ba3aa1416d228
Sunjay Bhatia <5337253+sunjayBhatia@users.noreply.github.com>
2020-08-26 19:05:48 -0400

    Fix and enable now-passing UDP tests on Windows (#12820)
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/extensions/filters/udp/udp_proxy/udp_proxy_integration_test.cc
+++ test/extensions/filters/udp/udp_proxy/udp_proxy_integration_test.cc
@@ -112,10 +112,10 @@ TEST_P(UdpProxyIntegrationTest, HelloWorldOnNonLocalAddress) {
   if (version_ == Network::Address::IpVersion::v4) {
     // Kernel regards any 127.x.x.x as local address.
     listener_address = std::make_shared<Network::Address::Ipv4Instance>(
-#ifndef __APPLE__
-        "127.0.0.3",
-#else
+#if defined(__APPLE__) || defined(WIN32)
         "127.0.0.1",
+#else
+        "127.0.0.3",
 #endif
         port);
   } else {
