61b8ec3fddf110d4e76b9ae431f65c4ff11d5c78
Zach <zasweq@google.com>
2020-08-27 19:32:19 +0000

    Fuzzer started
    Signed-off-by: Zach <zasweq@google.com>

---
+++ test/common/http/path_utility_fuzz_test.cc
@@ -0,0 +1,37 @@
+#include "common/http/path_utility.h"
+
+#include "test/common/http/path_utility_fuzz.pb.h"
+#include "test/fuzz/fuzz_runner.h"
+#include "test/fuzz/utility.h"
+
+
+namespace Envoy {
+namespace Fuzz {
+namespace {
+DEFINE_PROTO_FUZZER(const test::common::http::PathUtilityTestCase& input) {
+    /*try {
+
+    } catch () {
+        
+    }*/
+    switch (input.path_utility_selector_case()) {
+        case test::common::http::PathUtilityTestCase::kCanonicalPath: { //BIG QUESTION: HOW TO REPRESENT HEADER MAPS WITH PROTO
+            auto request_headers = fromHeaders<Http::TestRequestHeaderMapImpl>(input.canonical_path().request_headers(),
+                                                      {}, {});
+            Http::PathUtil::canonicalPath(request_headers);
+            break;
+        }
+        case test::common::http::PathUtilityTestCase::kMergeSlashes: {
+            auto request_headers = fromHeaders<Http::TestRequestHeaderMapImpl>(input.canonical_path().request_headers(),
+                                                      {}, {});
+            Http::PathUtil::mergeSlashes(request_headers);
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+}
+} //namespace Fuzz
+} //namespace Envoy
