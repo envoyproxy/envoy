cf2ee726d3ff25d4661cd8c611c9f20d7c97e67f
Zach <zasweq@google.com>
2020-08-27 13:28:02 +0000

    Removed unused part of test
    Signed-off-by: Zach <zasweq@google.com>

--- test/common/http/http2/http2_frame_test.cc
+++ test/common/http/http2/http2_frame_test.cc
@@ -16,12 +16,6 @@ TEST(EqualityMetadataFrame, Http2FrameTest) {
   MetadataMap metadataMap = {{"Connections", "15"}, {"Timeout Seconds", "10"}};
   Http2Frame http2FrameFromUtility = Http2Frame::makeMetadataFrameFromMetadataMap(
       1, metadataMap, Http2Frame::MetadataFlags::EndMetadata);
-  MetadataEncoder metadataEncoder{};
-  Http::MetadataMapPtr metadataMapPtr = std::make_unique<Http::MetadataMap>(metadataMap);
-  Http::MetadataMapVector metadata_map_vector;
-  metadata_map_vector.push_back(std::move(metadataMapPtr));
-  metadataEncoder.createPayload(metadata_map_vector);
-  std::string payloadFromEncoder = metadataEncoder.payload();
   std::string payloadFromHttp2Frame(http2FrameFromUtility);
   // Note: the actual encoding of the metadata map is nondeterministic and flaky. This is okay!
   ASSERT_EQ(static_cast<int>(http2FrameFromUtility.type()), 0x4D); // type
