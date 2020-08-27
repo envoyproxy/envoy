80fb292be93f395c1fef96f8f24bcd6915a4e686
Yifan Yang <needyyang@google.com>
2020-08-27 21:24:48 +0000

    using absolute path
    Signed-off-by: Yifan Yang <needyyang@google.com>

--- ci/flaky_test/run_process_xml.sh
+++ ci/flaky_test/run_process_xml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. "${ENVOY_SRCDIR}"/ci/flaky_test/tools/shell_utils.sh
+. "${ENVOY_SRCDIR}"/tools/shell_utils.sh
 
 if [[ "${ENVOY_BUILD_ARCH}" == "aarch64" ]]; then
   export MULTIDICT_NO_EXTENSIONS=1
