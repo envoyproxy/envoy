1f270391b9406d1544a5f0a4f852fa70b70ada93
Rafał Augustyniak <Augustyniak@users.noreply.github.com>
2020-08-27 14:55:50 -0700

    Introduce Counter protocol (#1052)
    Description: Make `StatsClient` protocol's interface return `Counter` protocol (as opposed to `Counter` class) to make mocking of the `StatsClient` easier.
    
    Use case: in our Lyft application we need a way to mock `StatsClient` (protocol). An instance of the object conforming to this protocol can be created with the use of Envoy engine. In unit tests, we do not want to use Envoy engine and hence we need a way to create a custom object conforming to `StatsClient` protocol.
    To implement a `StatsClient` protocol one needs to implements `func counter(_ elements: [Element]) -> Counter` method. In order to implement this method a developer needs to be able to initialize a new instance of `Counter`, it's currently not possible to do from outside of the module in which `Counter` is defined because:
    1. `Counter` does  not provide any public initializer.
    2. `Counter` cannot be subclassed since public classes don't allow for subclassing from outside of the module in which they are defined.
    
    Risk Level: Low, additive change
    Testing: No testing performed since it shouldn't influence the way framework works
    Docs Changes: No changes
    Release Notes: No changes
    
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/swift/src/stats/Counter.swift
+++ mobile/library/swift/src/stats/Counter.swift
@@ -1,24 +1,15 @@
-@_implementationOnly import EnvoyEngine
 import Foundation
 
 /// A time series counter.
-@objcMembers
-public class Counter: NSObject {
-  private let series: String
-  private weak var engine: EnvoyEngine?
-
-  init(elements: [Element], engine: EnvoyEngine) {
-    self.series = elements.map { $0.value }.joined(separator: ".")
-    self.engine = engine
-    super.init()
-  }
-
+@objc
+public protocol Counter: AnyObject {
   /// Increment the counter by the given count.
-  public func increment(count: Int = 1) {
-    guard let engine = self.engine else {
-      return
-    }
+  func increment(count: Int)
+}
 
-    engine.recordCounter(self.series, count: numericCast(count))
+extension Counter {
+  /// Increment the counter by 1.
+  public func increment() {
+    self.increment(count: 1)
   }
 }
