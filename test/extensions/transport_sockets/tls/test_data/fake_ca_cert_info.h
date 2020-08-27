27abe9184c753a16de6bd4783c4d6e9c42aa1074
Piotr Sikora <piotrsikora@google.com>
2020-08-27 13:17:21 -0700

    test: don't generate duplicate x509v3 extensions in intermediate CAs. (#12842)
    While there, add missing rules for generating "san_ip" test certificate.
    
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

---
+++ test/extensions/transport_sockets/tls/test_data/fake_ca_cert_info.h
@@ -0,0 +1,8 @@
+// NOLINT(namespace-envoy)
+constexpr char TEST_FAKE_CA_CERT_256_HASH[] =
+    "c765f9b756e41de667ec7622765100383dbd18723afa1903ed59ec2ae05e9dd8";
+constexpr char TEST_FAKE_CA_CERT_1_HASH[] = "5a5b6a7148d8649db0c20469a8d2db245edd95cb";
+constexpr char TEST_FAKE_CA_CERT_SPKI[] = "UmI2PwrPQmISzkZopazA3nRtCGvWZ8r26UBYNHjYEOI=";
+constexpr char TEST_FAKE_CA_CERT_SERIAL[] = "16985c0f6a5e6fc93823cce75d0950ae5794dede";
+constexpr char TEST_FAKE_CA_CERT_NOT_BEFORE[] = "Aug 20 16:57:47 2020 GMT";
+constexpr char TEST_FAKE_CA_CERT_NOT_AFTER[] = "Aug 20 16:57:47 2022 GMT";
