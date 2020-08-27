4876e1b06d57953affd9eeff4e35db5d502e4de6
Arthur Yan <arthuryan@google.com>
2020-08-27 14:32:33 +0000

    Added const qualifier to size()
    Signed-off-by: Arthur Yan <arthuryan@google.com>

--- test/extensions/filters/listener/common/fuzz/listener_filter_fuzzer.h
+++ test/extensions/filters/listener/common/fuzz/listener_filter_fuzzer.h
@@ -48,7 +48,7 @@ public:
   Api::SysCallSizeResult read(void* buffer, size_t length, bool peek);
 
   // Returns the number of bytes currently available to read()
-  size_t size();
+  size_t size() const;
 
   // Returns true if end of stream reached (no more reads)
   bool done();
