3335f91b0198f12dfce157f78f6e9c798e66478c
Petr Pchelko <ppchelko@wikimedia.org>
2020-08-28 16:16:52 -0700

    Fix tests
    Signed-off-by: Petr Pchelko <ppchelko@wikimedia.org>

--- test/common/formatter/substitution_formatter_test.cc
+++ test/common/formatter/substitution_formatter_test.cc
@@ -93,9 +93,9 @@ private:
 };
 
 TEST(SubstitutionFormatUtilsTest, protocolToString) {
-  EXPECT_EQ("HTTP/1.0", SubstitutionFormatUtils::protocolToString(Http::Protocol::Http10));
-  EXPECT_EQ("HTTP/1.1", SubstitutionFormatUtils::protocolToString(Http::Protocol::Http11));
-  EXPECT_EQ("HTTP/2", SubstitutionFormatUtils::protocolToString(Http::Protocol::Http2));
+  EXPECT_EQ("HTTP/1.0", SubstitutionFormatUtils::protocolToString(Http::Protocol::Http10).value().get());
+  EXPECT_EQ("HTTP/1.1", SubstitutionFormatUtils::protocolToString(Http::Protocol::Http11).value().get());
+  EXPECT_EQ("HTTP/2", SubstitutionFormatUtils::protocolToString(Http::Protocol::Http2).value().get());
   EXPECT_EQ(absl::nullopt, SubstitutionFormatUtils::protocolToString({}));
 }
 
