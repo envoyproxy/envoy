fb8bf3ffd8bf0a7b520f66604130112c71eed4d0
Frank Fort <ffort@google.com>
2020-08-28 15:30:18 -0400

    add more tests
    Signed-off-by: Frank Fort <ffort@google.com>

--- test/common/tcp/conn_info_test.cc
+++ test/common/tcp/conn_info_test.cc
@@ -11,19 +11,50 @@ namespace Tcp {
 namespace {
 
 using testing::_;
+using testing::Eq;
 using testing::Invoke;
 using testing::InvokeWithoutArgs;
+using testing::DoAll;
+using testing::SetArgPointee;
+using testing::SetArgReferee;
 using testing::NiceMock;
 using testing::Property;
-using testing::Eq;
 using testing::Return;
+
+#if defined(__linux__)
 
 TEST(ConnectionInfo, ReturnsEmptyOptionalIfGetSocketFails) {
   NiceMock<Envoy::Network::MockSocket> socket;
-  EXPECT_CALL(socket, getSocketOption(_, _, _, _)).WillOnce(Return(Api::SysCallIntResult{0, 0}));
-  EXPECT_THAT(ConnectionInfo::lastRoundTripTime(&socket), Eq(absl::optional<std::chrono::milliseconds>{}));
+  EXPECT_CALL(socket, getSocketOption(_, _, _, _)).WillOnce(Return(Api::SysCallIntResult{-1, -1})); 
+  EXPECT_THAT(ConnectionInfo::lastRoundTripTime(&socket),
+              Eq(absl::optional<std::chrono::milliseconds>{}));
 }
 
+TEST(ConnectionInfo, ReturnsRttIfSuccessful) {
+  NiceMock<Envoy::Network::MockSocket> socket;
+  struct tcp_info ti;
+  ti.tcpi_rtt = 34;
+  EXPECT_CALL(socket, getSocketOption(_, _, _, _)).WillOnce(
+  	DoAll(SetArgPointee<2>(ti), Return(Api::SysCallIntResult{0, 0})));
+  EXPECT_THAT(ConnectionInfo::lastRoundTripTime(&socket),
+              Eq(absl::optional<std::chrono::milliseconds>{34}));
 }
+
+
+#endif
+
+#if !defined(__linux__)
+
+TEST(ConnectionInfo, AlwaysReturnsEmptyOptional) {
+  NiceMock<Envoy::Network::MockSocket> socket;
+  EXPECT_CALL(socket, getSocketOption(_, _, _, _)).WillOnce(Return(Api::SysCallIntResult{-1, -1}));
+  EXPECT_THAT(ConnectionInfo::lastRoundTripTime(&socket),
+              Eq(absl::optional<std::chrono::milliseconds>{}));
+}
+
+#endif
+
+} // namespace
 } // namespace Tcp
 } // namespace Envoy
+
