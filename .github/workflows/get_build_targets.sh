8a18023c96191e1cfdf69c2ee051ea0464234dcf
Yifan Yang <needyyang@google.com>
2020-08-28 15:16:59 +0000

    get rid of tmp file and put it in a function
    Signed-off-by: Yifan Yang <needyyang@google.com>

--- .github/workflows/get_build_targets.sh
+++ .github/workflows/get_build_targets.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-readonly TMP_OUTPUT_FILE="tmp.txt"
-
 # This limits the directory that bazel query is going to search under.
 readonly SEARCH_FOLDER="//source/common/..."
 
 set -e -o pipefail
 
+function get_targets() {
+  git diff --name-only HEAD FETCH_HEAD | while IFS= read -r line
+  do
+    # Only targets under those folders.
+    case "$line" in
+      source/*|include/*)
+        bazel query "rdeps($SEARCH_FOLDER, $line, 1)" 2>/dev/null
+        ;;
+    esac
+    # Limit to the first 10 targets.
+  done | sort -u | head -n 10
+}
+
 # Fetching the upstream HEAD to compare with.
 git fetch https://github.com/envoyproxy/envoy.git master 2>/dev/null
 
-git diff --name-only HEAD FETCH_HEAD | while IFS= read -r line
-do
-  # Only targets under those folders.
-  case "$line" in
-    source/*|include/*)
-      bazel query "rdeps($SEARCH_FOLDER, $line, 1)" 2>/dev/null
-      ;;
-  esac
-  # Limit to the first 10 targets.
-done | sort -u | head -n 10 | tee $TMP_OUTPUT_FILE
-
-export BUILD_TARGETS_LOCAL=$(cat $TMP_OUTPUT_FILE)
-rm $TMP_OUTPUT_FILE
+export BUILD_TARGETS_LOCAL=$(echo $(get_targets))
