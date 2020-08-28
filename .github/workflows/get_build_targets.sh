a918059c87eea331c623c175bbc6a47eaaa4375e
Yifan Yang <needyyang@google.com>
2020-08-28 21:00:06 +0000

    added the filter to filter out .cc/.h file build targets and needed comments
    Signed-off-by: Yifan Yang <needyyang@google.com>

--- .github/workflows/get_build_targets.sh
+++ .github/workflows/get_build_targets.sh
@@ -5,7 +5,23 @@ readonly SEARCH_FOLDER="//source/common/..."
 
 set -e -o pipefail
 
+# This is to filter out the redudant .cc or .h build targets that bazel
+# queryemits when rdeps = 0
+function filter_line() {
+  while read line
+  do
+    case "$line" in
+        *.cc|*.h)
+          ;;
+        *)
+          echo "$line"
+          ;;
+    esac
+  done < "${1:-/dev/stdin}"
+}
+
 function get_targets() {
+  # Comparing the PR HEAD with the upstream master HEAD.
   git diff --name-only HEAD FETCH_HEAD | while IFS= read -r line
   do
     # Only targets under those folders.
@@ -15,10 +31,10 @@ function get_targets() {
         ;;
     esac
     # Limit to the first 10 targets.
-  done | sort -u | head -n 10
+  done | filter_line | sort -u | head -n 10
 }
 
-# Fetching the upstream HEAD to compare with.
+# Fetching the upstream HEAD to compare with and stored in FETCH_HEAD.
 git fetch https://github.com/envoyproxy/envoy.git master 2>/dev/null
 
 export BUILD_TARGETS_LOCAL=$(echo $(get_targets))
