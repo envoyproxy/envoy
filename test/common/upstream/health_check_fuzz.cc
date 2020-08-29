4b38d4bbcbb86cc96bab6e0946ef5c9084b384f2
Zach <zasweq@google.com>
2020-08-29 18:57:27 +0000

    Save work so far
    Signed-off-by: Zach <zasweq@google.com>

--- test/common/upstream/health_check_fuzz.cc
+++ test/common/upstream/health_check_fuzz.cc
@@ -1,26 +1,67 @@
-
+#include "health_check_fuzz.h"
+#include "test/common/health_check_fuzz.pb.h"
 
 namespace Envoy {
 
 //EITHER HAVE THIS HERE OR IN INITALIZE CALL
-HealthCheckFuzz::HealthCheckFuzz(test::common::upstream::HealthCheckTestCase input) {
+HealthCheckFuzz::HealthCheckFuzz() {
     //use input to construct config
     std::string yaml = R"EOF(
-        timeout: X
-        interval: X
-        interval_jitter: X
-        unhealthy_threshold: X
-        healthy_threshold": X
+        timeout: %us
+        interval: %us
+        initial_jitter: %us
+        interval_jitter: %us
+        interval_jitter_percent: %u
+        unhealthy_threshold: %u
+        healthy_threshold: %u
+        reuse_connection: %s //x ? "true" : "false"
+        no_traffic_interval: %us
+        unhealthy_interval: %us
+        unhealthy_edge_interval: %us
+        healthy_edge_interval: %us
+        always_log_health_check_failures: %s //x ? "true" : "false"
         http_health_check: //this is from the http message protobuf, specific to only http checking
-            path: XSPECIFIC
-            host: XSPECIFIC
-            request_headers_to_add: ETC
-        ETC (other common fields)
+            host: %s
+            path: %s
+            service_name: %s
     )EOF";
 
+    //sprintf(sqlAnswers, "select key from answer WHERE key = %s LIMIT 5;", tmp);
 
     allocHealthChecker(yaml);
     addCompletionCallback();
+}
+
+std::string HealthCheckFuzz::constructYamlFromProtoInput(test::common::upstream::HealthCheckTestCase input) {
+    std::string yaml;
+    sprintf(yaml, R"EOF(
+        timeout: %us
+        interval: %us
+        initial_jitter: %us
+        interval_jitter: %us
+        interval_jitter_percent: %u
+        unhealthy_threshold: %u
+        healthy_threshold: %u
+        reuse_connection: %s //x ? "true" : "false"
+        no_traffic_interval: %us
+        unhealthy_interval: %us
+        unhealthy_edge_interval: %us
+        healthy_edge_interval: %us
+        always_log_health_check_failures: %s //x ? "true" : "false"
+        http_health_check: //this is from the http message protobuf, specific to only http checking
+            host: %s
+            path: %s
+            service_name: %s
+    )EOF")
+}
+
+void HealthCheckFuzz::initalize(test::common::upstream::HealthCheckTestCase input) {
+    allocHealthChecker(constructYamlFromProtoInput(input));
+    addCompletionCallback();
+    cluster_->prioritySet().getMockHostSet(0)->hosts_ = {
+      makeTestHost(cluster_->info_, "tcp://127.0.0.1:80")};
+    expectSessionCreate();
+    expectStreamCreate(0);
 }
 
 /**
@@ -44,26 +85,29 @@ Questions for Asra: I will have my proto have the exact same constraints? (talk 
 Iterated proto for hardcoded response, however body and trailers never have anything. I feel like it should have valid options for body and data that the upstream could send that would make it go through codec
 */
 
-void HealthCheckFuzz::respond() {
+void HealthCheckFuzz::respond(test::common::upstream::HealthCheckTestCase respond) {
+    respond(0, respond.code(), respond.conn_close(), respond.proxy_close(), respond.body(), respond.traliers(), respond.degraded()); //TODO: HEALTH CHECK CLUSTER
+}
 
+void HealthCheckFuzz::streamCreate() {
+    expectStreamCreate(0);
 }
 
 HealthCheckFuzz::replay() { //call this with the
     for (int i = 0; i < input.actions.size(); ++i) {
         const auto& event = input.actions(i);
         ENVOY_LOG_MISC(trace, "Action: {}", event.DebugString());
-        switch (event.action_selector_case()) {
-            case : { //Respond
-                event.respond().code();
-                event.respond().conn_close();
-                event.respond().proxy_close();
-                //body
-                //trailers
-                //service cluster
-                //degraded
+        switch (event.action_selector_case()) { //TODO: Once added implementations for tcp and gRPC, move this to a seperate method, handleHttp
+            case test::common::upstream::HttpAction::kRespond: { //Respond
+                respond(event.respond());
                 break;
             }
-            case : {
+            case test::common::upstream::HttpAction::kStreamCreate: { //Expect Stream Create
+                streamCreate();
+                break;
+            }
+            //TODO: Expect degradation behavior
+            default : {
                 break;
             }
         }
--- test/common/upstream/health_check_fuzz.h
+++ test/common/upstream/health_check_fuzz.h
@@ -1,13 +1,16 @@
 #include "test/common/upstream/health_checker_impl_test.h"
 
 namespace Envoy {
-    class HealthCheckFuzz: public HttpHealthCheckerImplTest {
-        HealthCheckFuzz(test::common::upstream::HealthCheckTestCase input);
-        void initialize(); //can pipe in proto configs in either/or of both of these methods
-        void replay();
+    class HealthCheckFuzz: public Upstream::HttpHealthCheckerImplTest {
+        public:
+            HealthCheckFuzz();
+            void initialize(test::common::upstream::HealthCheckTestCase input); //can pipe in proto configs in either/or of both of these methods
+            void replay();
 
 
-        
+        private:
+            void respond();
+            void expectStreamCreate();
 
         //state?
         //Helper function?
