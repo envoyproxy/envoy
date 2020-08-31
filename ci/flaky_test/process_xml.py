4a6f9054b145667f4ed8d0dd1a83e8a6f70cce59
asraa <asraa@google.com>
2020-08-31 16:53:08 -0400

    ci: fix flaky test error (#12902)
    Signed-off-by: Asra Ali <asraa@google.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- ci/flaky_test/process_xml.py
+++ ci/flaky_test/process_xml.py
@@ -70,7 +70,7 @@ def getGitInfo(CI_TARGET):
   os.system("git remote -v > ${TMP_OUTPUT_PROCESS_XML}")
   os.system("git describe --all >> ${TMP_OUTPUT_PROCESS_XML}")
   os.system("git show >> ${TMP_OUTPUT_PROCESS_XML}")
-  f = open(os.environ['TMP_OUTPUT_PROCESS_XML'], 'r+')
+  f = open(os.environ['TMP_OUTPUT_PROCESS_XML'], 'r+', encoding='utf-8')
   # Fetching the URL from predefined env variable
   envoy_link = os.environ["REPO_URI"]
   for line in [next(f) for x in range(6)]:
