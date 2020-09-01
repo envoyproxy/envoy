c743baf634d18a97a99c3770fbce484e6f839998
Kuat Yessenov <kuat@google.com>
2020-09-01 13:46:58 -0700

    initial commit
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- source/server/connection_handler_impl.cc
+++ source/server/connection_handler_impl.cc
@@ -289,7 +289,7 @@ void ConnectionHandlerImpl::ActiveTcpSocket::continueFilterChain(bool success) {
 
 void ConnectionHandlerImpl::ActiveTcpSocket::setDynamicMetadata(const std::string& name,
                                                                 const ProtobufWkt::Struct& value) {
-  (*metadata_.mutable_filter_metadata())[name].MergeFrom(value);
+  stream_info_->setDynamicMetadata(name, value);
 }
 
 void ConnectionHandlerImpl::ActiveTcpSocket::newConnection() {
@@ -323,7 +323,7 @@ void ConnectionHandlerImpl::ActiveTcpSocket::newConnection() {
     // Particularly the assigned events need to reset before assigning new events in the follow up.
     accept_filters_.clear();
     // Create a new connection on this listener.
-    listener_.newConnection(std::move(socket_), dynamicMetadata());
+    listener_.newConnection(std::move(socket_), std::move(stream_info_));
   }
 }
 
@@ -377,18 +377,7 @@ void emitLogs(Network::ListenerConfig& config, StreamInfo::StreamInfo& stream_in
 
 void ConnectionHandlerImpl::ActiveTcpListener::newConnection(
     Network::ConnectionSocketPtr&& socket,
-    const envoy::config::core::v3::Metadata& dynamic_metadata) {
-  auto stream_info = std::make_unique<StreamInfo::StreamInfoImpl>(
-      parent_.dispatcher_.timeSource(), StreamInfo::FilterState::LifeSpan::Connection);
-  stream_info->setDownstreamLocalAddress(socket->localAddress());
-  stream_info->setDownstreamRemoteAddress(socket->remoteAddress());
-  stream_info->setDownstreamDirectRemoteAddress(socket->directRemoteAddress());
-
-  // merge from the given dynamic metadata if it's not empty
-  if (dynamic_metadata.filter_metadata_size() > 0) {
-    stream_info->dynamicMetadata().MergeFrom(dynamic_metadata);
-  }
-
+    std::unique_ptr<StreamInfo::StreamInfo> stream_info) {
   // Find matching filter chain.
   const auto filter_chain = config_->filterChainManager().findFilterChain(*socket);
   if (filter_chain == nullptr) {
