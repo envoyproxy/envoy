ba4b6719889db52ab44fdb2bfde9ca77caaa63f7
Justin Mazzola Paluska <justinmp@google.com>
2020-09-03 14:09:47 -0400

    http: add CDN-Loop header filter
    The CdnLoopFilter implements an HTTP filter that detects and prevents
    CDN loops using the RFC 8586 CDN-Loop header.  The filter can be
    configured with the CDN identifier to look for as well as the number
    of times the CDN identifier can be seen before responding with an
    error.
    
    Risk Level: Low
    Testing: bazel test //test/extensions/filters/http/cdn_loop/...
    Docs Changes: N/A
    Release Notes: added note about new filter
    
    Signed-off-by: Justin Mazzola Paluska <justinmp@google.com>

---
+++ source/extensions/filters/http/cdn_loop/filter.cc
@@ -0,0 +1,48 @@
+#include "extensions/filters/http/cdn_loop/filter.h"
+
+#include "envoy/http/codes.h"
+#include "envoy/http/filter.h"
+#include "envoy/http/header_map.h"
+
+#include "common/common/statusor.h"
+
+#include "extensions/filters/http/cdn_loop/utils.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace HttpFilters {
+namespace CdnLoop {
+
+namespace {
+const Http::LowerCaseString CdnLoopHeaderName("CDN-Loop");
+constexpr absl::string_view ParseErrorMessage = "Invalid CDN-Loop header in request.";
+constexpr absl::string_view ParseErrorDetails = "The CDN-Loop header could not be parsed.";
+constexpr absl::string_view LoopDetectedMessage = "CDN-Loop detected.";
+constexpr absl::string_view LoopDetectedDetails = "The server has detected a loop between CDNs.";
+} // namespace
+
+Http::FilterHeadersStatus CdnLoopFilter::decodeHeaders(Http::RequestHeaderMap& headers,
+                                                       bool /*end_stream*/) {
+  if (const Http::HeaderEntry* header_entry = headers.get(CdnLoopHeaderName);
+      header_entry != nullptr) {
+    if (StatusOr<int> count =
+            countCdnLoopOccurrences(header_entry->value().getStringView(), cdn_id_);
+        !count) {
+      decoder_callbacks_->sendLocalReply(Http::Code::BadRequest, ParseErrorMessage, nullptr,
+                                         absl::nullopt, ParseErrorDetails);
+      return Http::FilterHeadersStatus::StopIteration;
+    } else if (*count > max_allowed_occurences_) {
+      decoder_callbacks_->sendLocalReply(Http::Code::BadGateway, LoopDetectedMessage, nullptr,
+                                         absl::nullopt, LoopDetectedDetails);
+      return Http::FilterHeadersStatus::StopIteration;
+    }
+  }
+
+  headers.appendCopy(CdnLoopHeaderName, cdn_id_);
+  return Http::FilterHeadersStatus::Continue;
+}
+
+} // namespace CdnLoop
+} // namespace HttpFilters
+} // namespace Extensions
+} // namespace Envoy
