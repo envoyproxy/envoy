9d466c71ab217317d3e989b261eb496877348a47
Petr Pchelko <ppchelko@wikimedia.org>
2020-09-03 19:10:45 -0700

    access_log, formatter: Implement omit_empty_values (#12801)
    This introduces an option to entirely omit null values from the access log.
    
    Risk Level: Low
    Testing: Unit and integration tests
    Docs Changes: New option documented in proto file
    Release Notes: Updated
    Fixes #12735 
    
    Signed-off-by: Petr Pchelko <ppchelko@wikimedia.org>

--- source/common/formatter/substitution_format_string.cc
+++ source/common/formatter/substitution_format_string.cc
@@ -7,17 +7,17 @@ namespace Formatter {
 
 FormatterPtr
 SubstitutionFormatStringUtils::createJsonFormatter(const ProtobufWkt::Struct& struct_format,
-                                                   bool preserve_types) {
-  return std::make_unique<JsonFormatterImpl>(struct_format, preserve_types);
+                                                   bool preserve_types, bool omit_empty_values) {
+  return std::make_unique<JsonFormatterImpl>(struct_format, preserve_types, omit_empty_values);
 }
 
 FormatterPtr SubstitutionFormatStringUtils::fromProtoConfig(
     const envoy::config::core::v3::SubstitutionFormatString& config) {
   switch (config.format_case()) {
   case envoy::config::core::v3::SubstitutionFormatString::FormatCase::kTextFormat:
-    return std::make_unique<FormatterImpl>(config.text_format());
+    return std::make_unique<FormatterImpl>(config.text_format(), config.omit_empty_values());
   case envoy::config::core::v3::SubstitutionFormatString::FormatCase::kJsonFormat: {
-    return createJsonFormatter(config.json_format(), true);
+    return createJsonFormatter(config.json_format(), true, config.omit_empty_values());
   }
   default:
     NOT_REACHED_GCOVR_EXCL_LINE;
