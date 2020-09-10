52f41c228a2550cd53046f58236e9286dba3bb84
Asra Ali <asraa@google.com>
2020-09-10 12:58:14 -0400

    lower for watchdog subdr
    Signed-off-by: Asra Ali <asraa@google.com>

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -67,7 +67,7 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/extensions/transport_sockets/tls/ocsp:95.3"
 "source/extensions/transport_sockets/tls/private_key:76.9"
 "source/extensions/watchdog:84.9"
-"source/extensions/watchdog/profile_action:86.1"
+"source/extensions/watchdog/profile_action:84.9"
 "source/server:94.7"
 "source/server/config_validation:76.8"
 "source/server/admin:95.5"
