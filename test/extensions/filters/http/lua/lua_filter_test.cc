50df5dc5bf9179a03d444a48c4d6748e7ecbcb23
Lihao Luo <lihao@squareup.com>
2020-09-10 21:01:55 -0700

    Support binary data in lua base64 encoding
    Signed-off-by: Lihao Luo <lihao@squareup.com>

--- test/extensions/filters/http/lua/lua_filter_test.cc
+++ test/extensions/filters/http/lua/lua_filter_test.cc
@@ -2202,8 +2202,10 @@ TEST_F(LuaHttpFilterTest, LuaFilterBase64Escape) {
     end
 
     function envoy_on_response(response_handle)
-      local base64Encoded = response_handle:base64Escape("barfoo")
-      response_handle:logTrace(base64Encoded)
+      local resp_body_buf = response_handle:body()
+      local resp_body = resp_body_buf:getBytes(0, resp_body_buf:length())
+      local b64_resp_body = response_handle:base64Escape(resp_body)
+      response_handle:logTrace(b64_resp_body)
     end
   )EOF"};
 
@@ -2216,9 +2218,16 @@ TEST_F(LuaHttpFilterTest, LuaFilterBase64Escape) {
   EXPECT_EQ(Http::FilterHeadersStatus::Continue, filter_->decodeHeaders(request_headers, true));
 
   Http::TestResponseHeaderMapImpl response_headers{{":status", "200"}};
+  EXPECT_EQ(Http::FilterHeadersStatus::StopIteration,
+            filter_->encodeHeaders(response_headers, false));
 
-  EXPECT_CALL(*filter_, scriptLog(spdlog::level::trace, StrEq("YmFyZm9v")));
-  EXPECT_EQ(Http::FilterHeadersStatus::Continue, filter_->encodeHeaders(response_headers, true));
+  // base64 encoding should also work for binary data
+  uint8_t buffer[34] = {31, 139, 8,  0, 0, 0, 0, 0,   0,   255, 202, 72,  205, 201, 201, 47, 207,
+                        47, 202, 73, 1, 4, 0, 0, 255, 255, 173, 32,  235, 249, 10,  0,   0,  0};
+  Buffer::OwnedImpl response_body(buffer, 34);
+  EXPECT_CALL(*filter_, scriptLog(spdlog::level::trace,
+                                  StrEq("H4sIAAAAAAAA/8pIzcnJL88vykkBBAAA//+tIOv5CgAAAA==")));
+  EXPECT_EQ(Http::FilterDataStatus::Continue, filter_->encodeData(response_body, true));
 }
 
 } // namespace
