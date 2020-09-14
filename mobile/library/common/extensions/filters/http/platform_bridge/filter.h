a1691c0d01d04de05666e137c19403c3cc91eb7a
Jose Ulises Nino Rivera <junr03@users.noreply.github.com>
2020-09-14 16:26:19 -0700

    platform bridge filter: offload no-ops to pass through filter (#1096)
    Description: these functions are no-ops in the current implementation. They can be not overridden in order to delete uncovered code here.
    Risk Level: low
    Testing: no-op
    
    Signed-off-by: Jose Nino <jnino@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/common/extensions/filters/http/platform_bridge/filter.h
+++ mobile/library/common/extensions/filters/http/platform_bridge/filter.h
@@ -45,15 +45,12 @@ public:
                                           bool end_stream) override;
   Http::FilterDataStatus decodeData(Buffer::Instance& data, bool end_stream) override;
   Http::FilterTrailersStatus decodeTrailers(Http::RequestTrailerMap& trailers) override;
-  Http::FilterMetadataStatus decodeMetadata(Http::MetadataMap& metadata) override;
 
   // StreamEncoderFilter
-  Http::FilterHeadersStatus encode100ContinueHeaders(Http::ResponseHeaderMap& headers) override;
   Http::FilterHeadersStatus encodeHeaders(Http::ResponseHeaderMap& headers,
                                           bool end_stream) override;
   Http::FilterDataStatus encodeData(Buffer::Instance& data, bool end_stream) override;
   Http::FilterTrailersStatus encodeTrailers(Http::ResponseTrailerMap& trailers) override;
-  Http::FilterMetadataStatus encodeMetadata(Http::MetadataMap& metadata) override;
 
 private:
   Http::FilterHeadersStatus onHeaders(Http::HeaderMap& headers, bool end_stream,
