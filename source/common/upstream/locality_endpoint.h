7777e06fded7a43db55597a3d92f27bdd363faeb
Drew S. Ortega <drewortega@google.com>
2020-09-15 02:08:20 +0000

    simplify LocalityEndpoint hashing
    Signed-off-by: Drew S. Ortega <drewortega@google.com>

--- source/common/upstream/locality_endpoint.h
+++ source/common/upstream/locality_endpoint.h
@@ -12,20 +12,9 @@ using LocalityEndpointTuple = std::tuple<const envoy::config::core::v3::Locality
                                          const envoy::config::endpoint::v3::LbEndpoint&>;
 struct LocalityEndpointHash {
   size_t operator()(const LocalityEndpointTuple& values) const {
-    const envoy::config::core::v3::Locality& locality = std::get<0>(values);
-    const envoy::config::endpoint::v3::LbEndpoint& endpoint = std::get<1>(values);
-
-    std::string text;
-    {
-      // For memory safety, the StringOutputStream needs to be destroyed before
-      // we read the string.
-      Protobuf::io::StringOutputStream string_stream(&text);
-      Protobuf::io::CodedOutputStream coded_stream(&string_stream);
-      coded_stream.SetSerializationDeterministic(true);
-      locality.SerializeToCodedStream(&coded_stream);
-      endpoint.SerializeToCodedStream(&coded_stream);
-    }
-    return HashUtil::xxHash64(text);
+    const auto locality_hash = MessageUtil::hash(std::get<0>(values));
+    const auto endpoint_hash = MessageUtil::hash(std::get<1>(values));
+    return locality_hash ^ endpoint_hash;
   }
 };
 
