7473508146ec56be3d459b04d14a031c7a93a019
Alyssa Wilk <alyssar@chromium.org>
2020-09-17 15:51:25 -0400

    http: applying route header rules on local replies.
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -57,6 +57,7 @@ constexpr const char* runtime_features[] = {
     "envoy.reloadable_features.test_feature_true",
     // Begin alphabetically sorted section.
     "envoy.deprecated_features.allow_deprecated_extension_names",
+    "envoy.reloadable_features.always_apply_route_header_rules",
     "envoy.reloadable_features.activate_fds_next_event_loop",
     "envoy.reloadable_features.activate_timers_next_event_loop",
     "envoy.reloadable_features.allow_500_after_100",
