005159417206bbfbd6e0c51ec62d63685901a7a6
Justin Mazzola Paluska <justinmp@google.com>
2020-09-18 17:02:09 -0400

    Add fuzz test for the CDN-Loop header parser
    The CDN-Loop header parser is a non-trivial parser of untrusted user
    data (headers!), so it’s worth fuzzing it.
    
    Fixes #13179.
    
    Risk Level: Low
    Testing: bazel run //test/extensions/filters/http/cdn_loop:parser_fuzz_test --config asan-fuzzer
    Docs Changes: N/A
    Release Notes: N/A
    
    Signed-off-by: Justin Mazzola Paluska <justinmp@google.com>

---
+++ test/extensions/filters/http/cdn_loop/parser_fuzz_test.cc
@@ -0,0 +1,19 @@
+#include "extensions/filters/http/cdn_loop/parser.h"
+
+#include "test/fuzz/fuzz_runner.h"
+
+#include "absl/strings/string_view.h"
+
+namespace Envoy {
+namespace Fuzz {
+
+DEFINE_FUZZER(const uint8_t* buf, size_t len) {
+  {
+    Envoy::Extensions::HttpFilters::CdnLoop::Parser::ParseContext input(
+        absl::string_view(reinterpret_cast<const char*>(buf), len));
+    Envoy::Extensions::HttpFilters::CdnLoop::Parser::parseCdnInfoList(input).IgnoreError();
+  }
+}
+
+} // namespace Fuzz
+} // namespace Envoy
