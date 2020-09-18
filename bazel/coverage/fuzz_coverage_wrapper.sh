aa7d401f43fd80ad2fa6baceea32629930fbf48d
Asra Ali <asraa@google.com>
2020-09-18 13:15:20 -0400

    try fix
    Signed-off-by: Asra Ali <asraa@google.com>

--- bazel/coverage/fuzz_coverage_wrapper.sh
+++ bazel/coverage/fuzz_coverage_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 TEST_BINARY=$1
 shift
@@ -14,4 +14,6 @@ cp -r $@ fuzz_corpus/seed_corpus
 # TODO(asraa): When fuzz targets are stable, remove error suppression and run coverage while fuzzing.
 LLVM_PROFILE_FILE= ${TEST_BINARY} fuzz_corpus -seed=${FUZZ_CORPUS_SEED:-1} -max_total_time=${FUZZ_CORPUS_TIME:-60} -max_len=2048 -rss_limit_mb=8192 -timeout=30 || true
 
-${TEST_BINARY} fuzz_corpus -rss_limit_mb=8192 -runs=0 
+# Passing files instead of a directory will run fuzzing as a regression test.
+# TODO(asraa): Add a manual `|| true`, but this shouldn't be necessary. 
+${TEST_BINARY} fuzz_corpus/* 
