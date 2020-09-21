129795900500f6c89d79f3369eb16cba6a720c9b
Zach <zasweq@google.com>
2020-09-21 16:17:37 +0000

    Builds correctly, save progress
    Signed-off-by: Zach <zasweq@google.com>

--- test/common/upstream/health_check_fuzz.h
+++ test/common/upstream/health_check_fuzz.h
@@ -39,20 +39,26 @@ public:
   bool empty_response_ = true;
 };
 
-class GrpcHealthCheckFuzz : GrpcHealthCheckerImplTestBase { //TODO: What methods will I use here
+class GrpcHealthCheckFuzz : GrpcHealthCheckerImplTestBase { // TODO: What methods will I use here
 public:
   void allocGrpcHealthCheckerFromProto(const envoy::config::core::v3::HealthCheck& config);
   void initialize(test::common::upstream::HealthCheckTestCase input);
   void respondHeaders(test::common::upstream::GrpcRespondHeaders grpc_respond_headers);
   void respondBytes(test::common::upstream::GrpcRespondBytes grpc_respond_bytes);
   void respondTrailers(test::common::upstream::GrpcRespondTrailers grpc_respond_headers);
-  void respond(test::common::upstream::GrpcRespond grpc_respond); //This has three options, headers, raw bytes, or trailers
+  void respond(test::common::upstream::GrpcRespond
+                   grpc_respond); // This has three options, headers, raw bytes, or trailers
   void triggerIntervalTimer(bool expect_client_create);
   void triggerTimeoutTimer(bool last_action);
   void raiseEvent(const Network::ConnectionEvent& event_type, bool last_action);
+  void raiseGoAway(bool no_error);
 
   // Determines whether the client gets reused or not after response
   bool reuse_connection_ = true;
+
+  // Determines whether a client closes after responds and timeouts. Exactly maps to
+  // received_no_error_goaway_ in source code.
+  bool received_no_error_goaway_ = false;
 };
 
 class HealthCheckFuzz { // TODO: once added tcp/grpc, switch this to an
