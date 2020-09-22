74919ab2310688ad36f3efecce78468efb0cfaf8
Ryan Northey <ryan@synca.io>
2020-09-22 07:21:07 +0100

    use full path for find (for windows)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- test/config_test/example_configs_test_setup.sh
+++ test/config_test/example_configs_test_setup.sh
@@ -6,4 +6,5 @@ DIR="$TEST_TMPDIR"/test/config_test
 mkdir -p "$DIR"
 tar -xvf "$TEST_SRCDIR"/envoy/configs/example_configs.tar -C "$DIR"
 
-find "$DIR" -type f | grep -c .yaml > "$TEST_TMPDIR"/config-file-count.txt
+# find uses full path to prevent using windows find on windows
+/usr/bin/find "$DIR" -type f | grep -c .yaml > "$TEST_TMPDIR"/config-file-count.txt
