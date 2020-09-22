8baf86d77d396b0af0d6054fc30e4630247ea556
Ryan Northey <ryan@synca.io>
2020-09-22 05:45:19 +0100

    script
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- test/config_test/example_configs_test_setup.sh
+++ test/config_test/example_configs_test_setup.sh
@@ -6,5 +6,4 @@ DIR="$TEST_TMPDIR"/test/config_test
 mkdir -p "$DIR"
 tar -xvf "$TEST_SRCDIR"/envoy/configs/example_configs.tar -C "$DIR"
 
-CONFIG_FILE_COUNT=$(find "$DIR" -type f | grep .yaml | wc -l)
-echo "$CONFIG_FILE_COUNT" > "$TEST_TMPDIR"/config-file-count.txt
+find "$DIR" -type f | grep -c .yaml > "$TEST_TMPDIR"/config-file-count.txt
