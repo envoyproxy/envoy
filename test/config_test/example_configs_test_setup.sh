d4207ed411b7acdd02f94ad4cba22f6c04928874
Ryan Northey <ryan@synca.io>
2020-09-22 08:25:45 +0100

    cleanup: comment nit
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- test/config_test/example_configs_test_setup.sh
+++ test/config_test/example_configs_test_setup.sh
@@ -6,5 +6,5 @@ DIR="$TEST_TMPDIR"/test/config_test
 mkdir -p "$DIR"
 tar -xvf "$TEST_SRCDIR"/envoy/configs/example_configs.tar -C "$DIR"
 
-# find uses full path to prevent using windows find on windows
+# find uses full path to prevent using Windows find on Windows.
 /usr/bin/find "$DIR" -type f | grep -c .yaml > "$TEST_TMPDIR"/config-file-count.txt
