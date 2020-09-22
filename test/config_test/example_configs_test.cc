b6be8788b638129aa3ba79899fc8fb794f5a9ef8
Ryan Northey <ryan@synca.io>
2020-09-22 04:42:53 +0100

    set count dynamically and move mac/windows decrement to constant
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- test/config_test/example_configs_test.cc
+++ test/config_test/example_configs_test.cc
@@ -1,17 +1,27 @@
 #include "test/config_test/config_test.h"
 #include "test/test_common/environment.h"
 #include "test/test_common/utility.h"
+#include "common/filesystem/filesystem_impl.h"
 
 #include "gtest/gtest.h"
 
 namespace Envoy {
 
-// The total number of available configuration files under the examples directory.
-constexpr uint64_t NumberOfConfigurationFiles = 38UL;
+// freebind/freebind.yaml is not supported on macOS or Windows and is disabled via Bazel.
+constexpr uint64_t LinuxOnlyConfigFileTestCount = 1UL;
 
 TEST(ExampleConfigsTest, All) {
   TestEnvironment::exec(
       {TestEnvironment::runfilesPath("test/config_test/example_configs_test_setup.sh")});
+
+#ifdef WIN32
+  Filesystem::InstanceImplWin32 file_system;
+#else
+  Filesystem::InstanceImplPosix file_system;
+#endif
+
+  const auto config_file_count = std::stoi(
+      file_system.fileReadToEnd(TestEnvironment::temporaryDirectory() + "/config-file-count.txt"));
 
   // Change working directory, otherwise we won't be able to read files using relative paths.
 #ifdef PATH_MAX
@@ -24,10 +34,9 @@ TEST(ExampleConfigsTest, All) {
   RELEASE_ASSERT(::chdir(directory.c_str()) == 0, "");
 
 #if defined(__APPLE__) || defined(WIN32)
-  // freebind/freebind.yaml is not supported on macOS or Windows and is disabled via Bazel.
-  EXPECT_EQ(NumberOfConfigurationFiles - 1, ConfigTest::run(directory));
+  EXPECT_EQ(config_file_count - LinuxOnlyConfigFileTestCount, ConfigTest::run(directory));
 #else
-  EXPECT_EQ(NumberOfConfigurationFiles, ConfigTest::run(directory));
+  EXPECT_EQ(config_file_count, ConfigTest::run(directory));
 #endif
 
   ConfigTest::testMerge();
--- test/config_test/example_configs_test_setup.sh
+++ test/config_test/example_configs_test_setup.sh
@@ -5,3 +5,6 @@ set -e
 DIR="$TEST_TMPDIR"/test/config_test
 mkdir -p "$DIR"
 tar -xvf "$TEST_SRCDIR"/envoy/configs/example_configs.tar -C "$DIR"
+
+CONFIG_FILE_COUNT=$(find "$DIR" -type f | grep .yaml | wc -l)
+echo "$CONFIG_FILE_COUNT" > "$TEST_TMPDIR"/config-file-count.txt
