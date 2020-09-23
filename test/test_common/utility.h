06e9a906018ad64cd8e37a8f5a9c1c633dd9d1f6
Snow Pettersen <snowp@lyft.com>
2020-09-23 21:08:14 +0000

    fix build
    Signed-off-by: Snow Pettersen <snowp@lyft.com>

--- test/test_common/utility.h
+++ test/test_common/utility.h
@@ -645,8 +645,7 @@ public:
     for (const auto& resource : resources) {
       auto owned_resource = std::make_unique<MessageType>(resource);
       decoded_resources.owned_resources_.emplace_back(new Config::DecodedResourceImpl(
-          std::move(owned_resource), MessageUtil::getStringField(resource, name_field), {}, "",
-          absl::nullopt));
+          std::move(owned_resource), MessageUtil::getStringField(resource, name_field), {}, ""));
       decoded_resources.refvec_.emplace_back(*decoded_resources.owned_resources_.back());
     }
     return decoded_resources;
