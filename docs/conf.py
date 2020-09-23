e6e851a749ca8e8ec06baec2a430d52ebd2d5345
Sotiris Nanopoulos <sonanopo@microsoft.com>
2020-09-23 12:38:55 -0700

    fixes format
    Signed-off-by: Sotiris Nanopoulos <sonanopo@microsoft.com>

--- docs/conf.py
+++ docs/conf.py
@@ -71,12 +71,8 @@ blob_sha = os.environ['ENVOY_BLOB_SHA']
 sys.path.append(os.path.abspath("./_ext"))
 
 extensions = [
-    'sphinxcontrib.httpdomain',
-    'sphinx.ext.extlinks',
-    'sphinx.ext.ifconfig',
-    'sphinx_tabs.tabs',
-    'sphinx_copybutton',
-    'validating_code_block'
+    'sphinxcontrib.httpdomain', 'sphinx.ext.extlinks', 'sphinx.ext.ifconfig', 'sphinx_tabs.tabs',
+    'sphinx_copybutton', 'validating_code_block'
 ]
 extlinks = {
     'repo': ('https://github.com/envoyproxy/envoy/blob/{}/%s'.format(blob_sha), ''),
