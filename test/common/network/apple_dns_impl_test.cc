04e286115cb4ec1391ad3048a11164f801879642
Jose Nino <jnino@lyft.com>
2020-09-24 10:27:37 -0700

    updates
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- test/common/network/apple_dns_impl_test.cc
+++ test/common/network/apple_dns_impl_test.cc
@@ -25,8 +25,6 @@
 #include "test/test_common/printers.h"
 #include "test/test_common/utility.h"
 
-// #include "test/common/network/dns_utility.h"
-
 #include "absl/container/fixed_array.h"
 #include "absl/container/node_hash_map.h"
 #include "gtest/gtest.h"
@@ -43,7 +41,14 @@ namespace Envoy {
 namespace Network {
 namespace {
 
-class AppleDnsImplTest : public testing::TestWithParam<Address::IpVersion> {
+// Note: this test suite is, unfortunately, not hermetic. Apple's APIs do not allow overriding the
+// IP address used for resolution programatically via API calls (only in system settings), and worse
+// yet does not allow overriding the port number used _at all_. Therefore, the tests do not use a
+// test DNS server like in dns_impl_test, and thus affords less flexibility in testing scenarios: no
+// concurrent requests, no expressive error responses, etc. Further experiments could be done in
+// order to create a test connection that is reachable locally (potentially by binding port 53 --
+// default for DNS). However, @junr03's initial attempts were not successful.
+class AppleDnsImplTest : public testing::Test {
 public:
   AppleDnsImplTest()
       : api_(Api::createApiForTest()), dispatcher_(api_->allocateDispatcher("test_thread")) {}
@@ -52,11 +57,22 @@ public:
 
   ActiveDnsQuery* resolveWithExpectations(const std::string& address,
                                           const DnsLookupFamily lookup_family,
-                                          const DnsResolver::ResolutionStatus expected_status) {
+                                          const DnsResolver::ResolutionStatus expected_status,
+                                          const bool expected_results) {
     return resolver_->resolve(
         address, lookup_family,
-        [=](DnsResolver::ResolutionStatus status, std::list<DnsResponse> &&) -> void {
+        [=](DnsResolver::ResolutionStatus status, std::list<DnsResponse>&& results) -> void {
           EXPECT_EQ(expected_status, status);
+          if (expected_results) {
+            EXPECT_FALSE(results.empty());
+            for (const auto& result : results) {
+              if (lookup_family == DnsLookupFamily::V4Only) {
+                EXPECT_NE(nullptr, result.address_->ip()->ipv4());
+              } else if (lookup_family == DnsLookupFamily::V6Only) {
+                EXPECT_NE(nullptr, result.address_->ip()->ipv6());
+              }
+            }
+          }
           dispatcher_->exit();
         });
   }
@@ -93,123 +109,63 @@ protected:
   DnsResolverSharedPtr resolver_;
 };
 
-// Parameterize the DNS test server socket address.
-INSTANTIATE_TEST_SUITE_P(IpVersions, AppleDnsImplTest,
-                         testing::ValuesIn({Network::Address::IpVersion::v4}),
-                         TestUtility::ipTestParamsToString);
-
-// Validate that when DnsResolverImpl is destructed with outstanding requests,
+// Validate that when AppleDnsResolverImpl is destructed with outstanding requests,
 // that we don't invoke any callbacks if the query was cancelled. This is a regression test from
 // development, where segfaults were encountered due to callback invocations on
 // destruction.
-TEST_P(AppleDnsImplTest, DestructPending) {
+TEST_F(AppleDnsImplTest, DestructPending) {
   ActiveDnsQuery* query = resolveWithUnreferencedParameters("", DnsLookupFamily::V4Only, 0);
   ASSERT_NE(nullptr, query);
   query->cancel();
 }
 
-TEST_P(AppleDnsImplTest, LocalLookup) {
-  if (GetParam() == Address::IpVersion::v4) {
-    EXPECT_NE(nullptr, resolveWithExpectations("localhost", DnsLookupFamily::V4Only,
-                                               DnsResolver::ResolutionStatus::Success));
-  }
+TEST_F(AppleDnsImplTest, LocalLookup) {
+  EXPECT_NE(nullptr, resolveWithExpectations("localhost", DnsLookupFamily::Auto,
+                                             DnsResolver::ResolutionStatus::Success, true));
 }
 
-TEST_P(AppleDnsImplTest, DnsIpAddressVersion) {
+TEST_F(AppleDnsImplTest, DnsIpAddressVersion) {
   EXPECT_NE(nullptr, resolveWithExpectations("google.com", DnsLookupFamily::Auto,
-                                             DnsResolver::ResolutionStatus::Success));
+                                             DnsResolver::ResolutionStatus::Success, true));
   dispatcher_->run(Event::Dispatcher::RunType::Block);
 
   EXPECT_NE(nullptr, resolveWithExpectations("google.com", DnsLookupFamily::V4Only,
-                                             DnsResolver::ResolutionStatus::Success));
+                                             DnsResolver::ResolutionStatus::Success, true));
   dispatcher_->run(Event::Dispatcher::RunType::Block);
 
   EXPECT_NE(nullptr, resolveWithExpectations("google.com", DnsLookupFamily::V6Only,
-                                             DnsResolver::ResolutionStatus::Success));
+                                             DnsResolver::ResolutionStatus::Success, true));
   dispatcher_->run(Event::Dispatcher::RunType::Block);
 }
 
-TEST_P(AppleDnsImplTest, CallbackException) {
+TEST_F(AppleDnsImplTest, CallbackException) {
   EXPECT_NE(nullptr, resolveWithException<EnvoyException>("1.2.3.4", DnsLookupFamily::V4Only,
                                                           EnvoyException("Envoy exception")));
   EXPECT_THROW_WITH_MESSAGE(dispatcher_->run(Event::Dispatcher::RunType::Block), EnvoyException,
                             "Envoy exception");
 }
 
-TEST_P(AppleDnsImplTest, CallbackException2) {
+TEST_F(AppleDnsImplTest, CallbackException2) {
   EXPECT_NE(nullptr, resolveWithException<std::runtime_error>("1.2.3.4", DnsLookupFamily::V4Only,
                                                               std::runtime_error("runtime error")));
   EXPECT_THROW_WITH_MESSAGE(dispatcher_->run(Event::Dispatcher::RunType::Block), EnvoyException,
                             "runtime error");
 }
 
-TEST_P(AppleDnsImplTest, CallbackException3) {
+TEST_F(AppleDnsImplTest, CallbackException3) {
   EXPECT_NE(nullptr,
             resolveWithException<std::string>("1.2.3.4", DnsLookupFamily::V4Only, std::string()));
   EXPECT_THROW_WITH_MESSAGE(dispatcher_->run(Event::Dispatcher::RunType::Block), EnvoyException,
                             "unknown");
 }
 
-// // Validate that the c-ares channel is destroyed and re-initialized when c-ares returns
-// // ARES_ECONNREFUSED as its callback status.
-// TEST_P(AppleDnsImplTest, DestroyChannelOnRefused) {
-//   ASSERT_FALSE(peer_->isChannelDirty());
-//   server_->addHosts("some.good.domain", {"201.134.56.7"}, RecordType::A);
-//   server_->setRefused(true);
-
-//   EXPECT_NE(nullptr,
-//             resolveWithExpectations("", DnsLookupFamily::V4Only,
-//                                     DnsResolver::ResolutionStatus::Failure, {}, {},
-//                                     absl::nullopt));
-//   dispatcher_->run(Event::Dispatcher::RunType::Block);
-//   // The c-ares channel should be dirty because the TestDnsServer replied with return code
-//   REFUSED;
-//   // This test, and the way the TestDnsServerQuery is setup, relies on the fact that Envoy's
-//   // c-ares channel is configured **without** the ARES_FLAG_NOCHECKRESP flag. This causes c-ares
-//   to
-//   // discard packets with REFUSED, and thus Envoy receives ARES_ECONNREFUSED due to the code
-//   here:
-//   //
-//   https://github.com/c-ares/c-ares/blob/d7e070e7283f822b1d2787903cce3615536c5610/ares_process.c#L654
-//   // If that flag needs to be set, or c-ares changes its handling this test will need to be
-//   updated
-//   // to create another condition where c-ares invokes onAresGetAddrInfoCallback with status ==
-//   // ARES_ECONNREFUSED.
-//   EXPECT_TRUE(peer_->isChannelDirty());
-
-//   server_->setRefused(false);
-
-//   // Resolve will destroy the original channel and create a new one.
-//   EXPECT_NE(nullptr,
-//             resolveWithExpectations("some.good.domain", DnsLookupFamily::V4Only,
-//                                     DnsResolver::ResolutionStatus::Failure, {}, {},
-//                                     absl::nullopt));
-//   dispatcher_->run(Event::Dispatcher::RunType::Block);
-//   // However, the fresh channel initialized by production code does not point to the
-//   TestDnsServer.
-//   // This means that resolution will return ARES_ENOTFOUND. This should not dirty the channel.
-//   EXPECT_FALSE(peer_->isChannelDirty());
-
-//   // Reset the channel to point to the TestDnsServer, and make sure resolution is healthy.
-//   if (tcp_only()) {
-//     peer_->resetChannelTcpOnly(zero_timeout());
-//   }
-//   ares_set_servers_ports_csv(peer_->channel(), socket_->localAddress()->asString().c_str());
-
-//   EXPECT_NE(nullptr, resolveWithExpectations("some.good.domain", DnsLookupFamily::Auto,
-//                                              DnsResolver::ResolutionStatus::Success,
-//                                              {"201.134.56.7"}, {}, absl::nullopt));
-//   dispatcher_->run(Event::Dispatcher::RunType::Block);
-//   EXPECT_FALSE(peer_->isChannelDirty());
-// }
-
 // Validate working of cancellation provided by ActiveDnsQuery return.
-TEST_P(AppleDnsImplTest, Cancel) {
+TEST_F(AppleDnsImplTest, Cancel) {
   ActiveDnsQuery* query =
       resolveWithUnreferencedParameters("google.com", DnsLookupFamily::Auto, false);
 
   EXPECT_NE(nullptr, resolveWithExpectations("some.domain", DnsLookupFamily::Auto,
-                                             DnsResolver::ResolutionStatus::Success));
+                                             DnsResolver::ResolutionStatus::Success, true));
 
   ASSERT_NE(nullptr, query);
   query->cancel();
@@ -217,9 +173,9 @@ TEST_P(AppleDnsImplTest, Cancel) {
   dispatcher_->run(Event::Dispatcher::RunType::Block);
 }
 
-TEST_P(AppleDnsImplTest, Timeout) {
+TEST_F(AppleDnsImplTest, Timeout) {
   EXPECT_NE(nullptr, resolveWithExpectations("some.domain", DnsLookupFamily::V6Only,
-                                             DnsResolver::ResolutionStatus::Failure));
+                                             DnsResolver::ResolutionStatus::Failure, false));
   dispatcher_->run(Event::Dispatcher::RunType::Block);
 }
 
