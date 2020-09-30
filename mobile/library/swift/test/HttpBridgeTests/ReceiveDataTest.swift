f7f7f8951d7dcf895df2f4fb3d962b493e93bc7e
Jose Ulises Nino Rivera <junr03@users.noreply.github.com>
2020-09-30 13:15:21 -0700

    swift: http acceptance tests with assertion filter (#1069)
    Description: this PR add end-to-end tests for (almost) all http streaming functionality in the library from the swift platform layer to the core layer.
    Risk Level: low
    Testing: adds new testing strategy.
    
    Signed-off-by: Jose Nino <jnino@lyft.com>
    Co-authored-by: Michael Rebello <me@michaelrebello.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

---
+++ mobile/library/swift/test/HttpBridgeTests/ReceiveDataTest.swift
@@ -0,0 +1,84 @@
+import Envoy
+import EnvoyEngine
+import Foundation
+import XCTest
+
+final class ReceiveDataTests: XCTestCase {
+  func testReceiveData() throws {
+    // swiftlint:disable:next line_length
+    let apiListenerType = "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager"
+    // swiftlint:disable:next line_length
+    let assertionFilterType = "type.googleapis.com/envoymobile.extensions.filters.http.assertion.Assertion"
+    let config =
+    """
+    static_resources:
+      listeners:
+      - name: base_api_listener
+        address:
+          socket_address:
+            protocol: TCP
+            address: 0.0.0.0
+            port_value: 10000
+        api_listener:
+          api_listener:
+            "@type": \(apiListenerType)
+            stat_prefix: hcm
+            route_config:
+              name: api_router
+              virtual_hosts:
+                - name: api
+                  domains:
+                    - "*"
+                  routes:
+                    - match:
+                        prefix: "/"
+                      direct_response:
+                        status: 200
+                        body:
+                          inline_string: response_body
+            http_filters:
+              - name: envoy.filters.http.assertion
+                typed_config:
+                  "@type": \(assertionFilterType)
+                  match_config:
+                    http_request_headers_match:
+                      headers:
+                        - name: ":authority"
+                          exact_match: example.com
+              - name: envoy.router
+                typed_config:
+                  "@type": type.googleapis.com/envoy.extensions.filters.http.router.v3.Router
+    """
+    let client = try EngineBuilder(yaml: config)
+      .addLogLevel(.debug)
+      .addFilter(factory: DemoFilter.init)
+      .build()
+      .streamClient()
+
+    let requestHeaders = RequestHeadersBuilder(method: .get, scheme: "https",
+                                               authority: "example.com", path: "/test")
+      .addUpstreamHttpProtocol(.http2)
+      .build()
+
+    let headersExpectation = self.expectation(description: "Run called with expected headers")
+    let dataExpectation = self.expectation(description: "Run called with expected data")
+
+    client
+      .newStreamPrototype()
+      .setOnResponseHeaders { responseHeaders, _ in
+         XCTAssertEqual(200, responseHeaders.httpStatus)
+         headersExpectation.fulfill()
+      }
+      .setOnResponseData { data, _ in
+        let responseBody = String(data: data, encoding: .utf8)
+        XCTAssertEqual("response_body", responseBody)
+        dataExpectation.fulfill()
+      }
+      .start()
+      .sendHeaders(requestHeaders, endStream: true)
+
+    XCTAssertEqual(XCTWaiter.wait(for: [headersExpectation, dataExpectation], timeout: 1,
+                                  enforceOrder: true),
+                   .completed)
+  }
+}
---
+++ mobile/library/swift/test/HttpBridgeTests/SendHeadersTest.swift
@@ -0,0 +1,72 @@
+import Envoy
+import EnvoyEngine
+import Foundation
+import XCTest
+
+final class SendHeadersTests: XCTestCase {
+  func testSendHeaders() throws {
+    // swiftlint:disable:next line_length
+    let apiListenerType = "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager"
+    // swiftlint:disable:next line_length
+    let assertionFilterType = "type.googleapis.com/envoymobile.extensions.filters.http.assertion.Assertion"
+    let config =
+    """
+    static_resources:
+      listeners:
+      - name: base_api_listener
+        address:
+          socket_address:
+            protocol: TCP
+            address: 0.0.0.0
+            port_value: 10000
+        api_listener:
+          api_listener:
+            "@type": \(apiListenerType)
+            stat_prefix: hcm
+            route_config:
+              name: api_router
+              virtual_hosts:
+                - name: api
+                  domains:
+                    - "*"
+                  routes:
+                    - match:
+                        prefix: "/"
+                      direct_response:
+                        status: 200
+            http_filters:
+              - name: envoy.filters.http.assertion
+                typed_config:
+                  "@type": \(assertionFilterType)
+                  match_config:
+                    http_request_headers_match:
+                      headers:
+                        - name: ":authority"
+                          exact_match: example.com
+              - name: envoy.router
+                typed_config:
+                  "@type": type.googleapis.com/envoy.extensions.filters.http.router.v3.Router
+    """
+    let expectation = self.expectation(description: "Run called with expected http status")
+    let client = try EngineBuilder(yaml: config)
+      .addLogLevel(.debug)
+      .addFilter(factory: DemoFilter.init)
+      .build()
+      .streamClient()
+
+    let requestHeaders = RequestHeadersBuilder(method: .get, scheme: "https",
+                                               authority: "example.com", path: "/test")
+      .addUpstreamHttpProtocol(.http2)
+      .build()
+    client
+      .newStreamPrototype()
+      .setOnResponseHeaders { responseHeaders, _ in
+         XCTAssertEqual(200, responseHeaders.httpStatus)
+         expectation.fulfill()
+      }
+      .start()
+      .sendHeaders(requestHeaders, endStream: true)
+
+    XCTAssertEqual(XCTWaiter.wait(for: [expectation], timeout: 1), .completed)
+  }
+}
