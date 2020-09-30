ad5563e0445668c133d260682049717945249cae
Snow Pettersen <snowp@lyft.com>
2020-09-30 20:19:57 +0000

    format
    Signed-off-by: Snow Pettersen <snowp@lyft.com>

--- source/common/http/filter_manager.cc
+++ source/common/http/filter_manager.cc
@@ -1449,7 +1449,8 @@ void ActiveStreamEncoderFilter::sendLocalReply(
     Code code, absl::string_view body,
     std::function<void(ResponseHeaderMap& headers)> modify_headers,
     const absl::optional<Grpc::Status::GrpcStatus> grpc_status, absl::string_view details) {
-  parent_.sendLocalReply(parent_.state_.is_grpc_request_, code, body, modify_headers, grpc_status, details);
+  parent_.sendLocalReply(parent_.state_.is_grpc_request_, code, body, modify_headers, grpc_status,
+                         details);
 }
 
 Http1StreamEncoderOptionsOptRef ActiveStreamEncoderFilter::http1StreamEncoderOptions() {
