envoyproxy/envoy

commit 442b3f168384f7a1a714d9478f1512e9152706c3

[http] Headermap Lazy map addition (#12656)
This is a continuation of #9261. Adding a map to headermap's HeaderList to access headers by key in O(1) instead of O(n).
In addition some benchmarks that emulate encoding and decoding of headers, and route matching by headers are added.

Signed-off-by: Adi Suissa-Peleg <adip@google.com>

source/common/http/header_map_impl.cc
@@ -179,6 +179,48 @@ template <> bool HeaderMapImpl::HeaderList::isPseudoHeader(const LowerCaseString
   return key.get().c_str()[0] == ':';
 }
 
+bool HeaderMapImpl::HeaderList::maybeMakeMap() {
+  if (lazy_map_.empty()) {
+    if (headers_.size() < lazy_map_min_size_) {
+      return false;
+    }
+    // Add all entries from the list into the map.
+    for (auto node = headers_.begin(); node != headers_.end(); ++node) {
+      HeaderNodeVector& v = lazy_map_[node->key().getStringView()];
+      v.push_back(node);
+    }
+  }
+  return true;
+}
+
+size_t HeaderMapImpl::HeaderList::remove(absl::string_view key) {
+  size_t removed_bytes = 0;
+  if (maybeMakeMap()) {
+    auto iter = lazy_map_.find(key);
+    if (iter != lazy_map_.end()) {
+      // Erase from the map, and all same key entries from the list.
+      HeaderNodeVector header_nodes = std::move(iter->second);
+      lazy_map_.erase(iter);
+      for (const HeaderNode& node : header_nodes) {
+        ASSERT(node->key() == key);
+        removed_bytes += node->key().size() + node->value().size();
+        erase(node, false /* remove_from_map */);
+      }
+    }
+  } else {
+    // Erase all same key entries from the list.
+    for (auto i = headers_.begin(); i != headers_.end();) {
+      if (i->key() == key) {
+        removed_bytes += i->key().size() + i->value().size();
+        i = erase(i, false /* remove_from_map */);
+      } else {
+        ++i;
+      }
+    }
+  }
+  return removed_bytes;
+}
+
 HeaderMapImpl::HeaderEntryImpl::HeaderEntryImpl(const LowerCaseString& key) : key_(key) {}
 
 HeaderMapImpl::HeaderEntryImpl::HeaderEntryImpl(const LowerCaseString& key, HeaderString&& value)
@@ -327,7 +369,7 @@ void HeaderMapImpl::insertByKey(HeaderString&& key, HeaderString&& value) {
     }
   } else {
     addSize(key.size() + value.size());
-    std::list<HeaderEntryImpl>::iterator i = headers_.insert(std::move(key), std::move(value));
+    HeaderNode i = headers_.insert(std::move(key), std::move(value));
     i->entry_ = i;
   }
 }
@@ -454,12 +496,24 @@ HeaderMap::NonConstGetResult HeaderMapImpl::getExisting(const LowerCaseString& k
     return ret;
   }
 
-  // If the requested header is not an O(1) header we do a full scan. Doing the trie lookup is
-  // wasteful in the miss case, but is present for code consistency with other functions that do
-  // similar things.
-  // TODO(mattklein123): The full scan here and in remove() are the biggest issues with this
-  // implementation for certain use cases. We can either replace this with a totally different
-  // implementation or potentially create a lazy map if the size of the map is above a threshold.
+  // If the requested header is not an O(1) header try using the lazy map to
+  // search for it instead of iterating the headers list.
+  if (headers_.maybeMakeMap()) {
+    HeaderList::HeaderLazyMap::iterator iter = headers_.mapFind(key.get());
+    if (iter != headers_.mapEnd()) {
+      const HeaderList::HeaderNodeVector& v = iter->second;
+      ASSERT(!v.empty()); // It's impossible to have a map entry with an empty vector as its value.
+      for (const auto values_it : v) {
+        // Convert the iterated value to a HeaderEntry*.
+        ret.push_back(&(*values_it));
+      }
+    }
+    return ret;
+  }
+
+  // If the requested header is not an O(1) header and the lazy map is not in use, we do a full
+  // scan. Doing the trie lookup is wasteful in the miss case, but is present for code consistency
+  // with other functions that do similar things.
   for (HeaderEntryImpl& header : headers_) {
     if (header.key() == key.get().c_str()) {
       ret.push_back(&header);
@@ -516,17 +570,14 @@ size_t HeaderMapImpl::removeIf(const HeaderMap::HeaderMatchPredicate& predicate)
 }
 
 size_t HeaderMapImpl::remove(const LowerCaseString& key) {
+  const size_t old_size = headers_.size();
   auto lookup = staticLookup(key.get());
   if (lookup.has_value()) {
-    const size_t old_size = headers_.size();
     removeInline(lookup.value().entry_);
-    return old_size - headers_.size();
   } else {
-    // TODO(mattklein123): When the lazy map is implemented we can stop using removeIf() here.
-    return HeaderMapImpl::removeIf([&key](const HeaderEntry& entry) -> bool {
-      return key.get() == entry.key().getStringView();
-    });
+    subtractSize(headers_.remove(key.get()));
   }
+  return old_size - headers_.size();
 }
 
 size_t HeaderMapImpl::removePrefix(const LowerCaseString& prefix) {
@@ -551,7 +602,7 @@ HeaderMapImpl::HeaderEntryImpl& HeaderMapImpl::maybeCreateInline(HeaderEntryImpl
   }
 
   addSize(key.get().size());
-  std::list<HeaderEntryImpl>::iterator i = headers_.insert(key);
+  HeaderNode i = headers_.insert(key);
   i->entry_ = i;
   *entry = &(*i);
   return **entry;
@@ -566,7 +617,7 @@ HeaderMapImpl::HeaderEntryImpl& HeaderMapImpl::maybeCreateInline(HeaderEntryImpl
   }
 
   addSize(key.get().size() + value.size());
-  std::list<HeaderEntryImpl>::iterator i = headers_.insert(key, std::move(value));
+  HeaderNode i = headers_.insert(key, std::move(value));
   i->entry_ = i;
   *entry = &(*i);
   return **entry;
@@ -581,7 +632,7 @@ size_t HeaderMapImpl::removeInline(HeaderEntryImpl** ptr_to_entry) {
   const uint64_t size_to_subtract = entry->entry_->key().size() + entry->entry_->value().size();
   subtractSize(size_to_subtract);
   *ptr_to_entry = nullptr;
-  headers_.erase(entry->entry_);
+  headers_.erase(entry->entry_, true);
   return 1;
 }
 

source/common/http/header_map_impl.h
@@ -12,6 +12,7 @@
 #include "common/common/non_copyable.h"
 #include "common/common/utility.h"
 #include "common/http/headers.h"
+#include "common/runtime/runtime_features.h"
 
 namespace Envoy {
 namespace Http {
@@ -115,6 +116,7 @@ protected:
     HeaderString value_;
     std::list<HeaderEntryImpl>::iterator entry_;
   };
+  using HeaderNode = std::list<HeaderEntryImpl>::iterator;
 
   /**
    * This is the static lookup table that is used to determine whether a header is one of the O(1)
@@ -171,6 +173,10 @@ protected:
   /**
    * List of HeaderEntryImpl that keeps the pseudo headers (key starting with ':') in the front
    * of the list (as required by nghttp2) and otherwise maintains insertion order.
+   * When the list size is greater or equal to the envoy.http.headermap.lazy_map_min_size runtime
+   * feature value (or uint32_t max value if not set), all headers are added to a map, to allow
+   * fast access given a header key. Once the map is initialized, it will be used even if the number
+   * of headers decreases below the threshold.
    *
    * Note: the internal iterators held in fields make this unsafe to copy and move, since the
    * reference to end() is not preserved across a move (see Notes in
@@ -181,27 +187,37 @@ protected:
    */
   class HeaderList : NonCopyable {
   public:
-    HeaderList() : pseudo_headers_end_(headers_.end()) {}
+    using HeaderNodeVector = absl::InlinedVector<HeaderNode, 1>;
+    using HeaderLazyMap = absl::flat_hash_map<absl::string_view, HeaderNodeVector>;
+
+    HeaderList()
+        : pseudo_headers_end_(headers_.end()),
+          lazy_map_min_size_(static_cast<uint32_t>(Runtime::getInteger(
+              "envoy.http.headermap.lazy_map_min_size", std::numeric_limits<uint32_t>::max()))) {}
 
     template <class Key> bool isPseudoHeader(const Key& key) {
       return !key.getStringView().empty() && key.getStringView()[0] == ':';
     }
 
-    template <class Key, class... Value>
-    std::list<HeaderEntryImpl>::iterator insert(Key&& key, Value&&... value) {
+    template <class Key, class... Value> HeaderNode insert(Key&& key, Value&&... value) {
       const bool is_pseudo_header = isPseudoHeader(key);
-      std::list<HeaderEntryImpl>::iterator i =
-          headers_.emplace(is_pseudo_header ? pseudo_headers_end_ : headers_.end(),
-                           std::forward<Key>(key), std::forward<Value>(value)...);
+      HeaderNode i = headers_.emplace(is_pseudo_header ? pseudo_headers_end_ : headers_.end(),
+                                      std::forward<Key>(key), std::forward<Value>(value)...);
+      if (!lazy_map_.empty()) {
+        lazy_map_[i->key().getStringView()].push_back(i);
+      }
       if (!is_pseudo_header && pseudo_headers_end_ == headers_.end()) {
         pseudo_headers_end_ = i;
       }
       return i;
     }
 
-    std::list<HeaderEntryImpl>::iterator erase(std::list<HeaderEntryImpl>::iterator i) {
+    HeaderNode erase(HeaderNode i, bool remove_from_map) {
       if (pseudo_headers_end_ == i) {
         pseudo_headers_end_++;
+      }
+      if (remove_from_map) {
+        lazy_map_.erase(i->key().getStringView());
       }
       return headers_.erase(i);
     }
@@ -213,10 +229,54 @@ protected:
           if (pseudo_headers_end_ == entry.entry_) {
             pseudo_headers_end_++;
           }
+          if (!lazy_map_.empty()) {
+            ASSERT(lazy_map_.find(entry.key().getStringView()) != lazy_map_.end());
+            auto& values_vec = lazy_map_[entry.key().getStringView()];
+            if (values_vec.size() == 1) {
+              // If the map contains a single value with the same key of the entry, it is certain
+              // that this is the only value that needs to be removed, and can be erased from the
+              // map.
+              lazy_map_.erase(entry.key().getStringView());
+            } else {
+              // Otherwise, find the exact element that is removed from headers_ (entry) in the
+              // vector, and remove it. This keeps an invariant that only the element that is
+              // currently removed from the headers list will also be removed from the map.
+              // In case the predicate holds for all the vector elements, the size of the vector
+              // will eventually decrease to 1 and the entire vector will be erased by the if
+              // clause above.
+              // Note: it is possible to remove all elements in the vector that satisfy the
+              // predicate during this iteration, but it will complicate the code by requiring
+              // handling of missing values in the lazy map.
+
+              // The call to std::remove_if removes the elements that satisfy the predicate and
+              // shifts the vector elements, but does not resize the vector. The call to erase that
+              // follows erases the unneeded cells (from remove_pos to the end) and modifies the
+              // vector's size.
+              const auto remove_pos =
+                  std::remove_if(values_vec.begin(), values_vec.end(),
+                                 [&](HeaderNode it) { return it == entry.entry_; });
+              values_vec.erase(remove_pos, values_vec.end());
+            }
+          }
         }
         return to_remove;
       });
     }
+
+    /*
+     * Creates and populates a map if the number of headers is at least the
+     * envoy.http.headermap.lazy_map_min_size runtime feature value.
+     *
+     * @return if a map was created.
+     */
+    bool maybeMakeMap();
+
+    /*
+     * Removes a given key and its values from the HeaderList.
+     *
+     * @return the number of bytes that were removed.
+     */
+    size_t remove(absl::string_view key);
 
     std::list<HeaderEntryImpl>::iterator begin() { return headers_.begin(); }
     std::list<HeaderEntryImpl>::iterator end() { return headers_.end(); }
@@ -224,16 +284,22 @@ protected:
     std::list<HeaderEntryImpl>::const_iterator end() const { return headers_.end(); }
     std::list<HeaderEntryImpl>::const_reverse_iterator rbegin() const { return headers_.rbegin(); }
     std::list<HeaderEntryImpl>::const_reverse_iterator rend() const { return headers_.rend(); }
+    HeaderLazyMap::iterator mapFind(absl::string_view key) { return lazy_map_.find(key); }
+    HeaderLazyMap::iterator mapEnd() { return lazy_map_.end(); }
     size_t size() const { return headers_.size(); }
     bool empty() const { return headers_.empty(); }
     void clear() {
       headers_.clear();
       pseudo_headers_end_ = headers_.end();
+      lazy_map_.clear();
     }
 
   private:
     std::list<HeaderEntryImpl> headers_;
-    std::list<HeaderEntryImpl>::iterator pseudo_headers_end_;
+    HeaderNode pseudo_headers_end_;
+    // The number of headers threshold for lazy map usage.
+    const uint32_t lazy_map_min_size_;
+    HeaderLazyMap lazy_map_;
   };
 
   void insertByKey(HeaderString&& key, HeaderString&& value);

test/benchmark/BUILD
@@ -19,5 +19,7 @@ envoy_cc_test_library(
     deps = [
         "//source/common/common:minimal_logger_lib",
         "//test/test_common:environment_lib",
+        "//test/test_common:printers_lib",
+        "//test/test_common:test_runtime_lib",
     ],
 )

test/benchmark/main.cc
@@ -5,6 +5,7 @@
 #include "common/common/logger.h"
 
 #include "test/test_common/environment.h"
+#include "test/test_common/test_runtime.h"
 
 #include "benchmark/benchmark.h"
 #include "tclap/CmdLine.h"
@@ -21,10 +22,20 @@ static bool skip_expensive_benchmarks = false;
 int main(int argc, char** argv) {
   TestEnvironment::initializeTestMain(argv[0]);
 
+  // Suppressing non-error messages in benchmark tests. This hides warning
+  // messages that appear when using a runtime feature when there isn't an initialized
+  // runtime, and may have non-negligible impact on performance.
+  // TODO(adisuissa): This should be configurable, similarly to unit tests.
+  const spdlog::level::level_enum default_log_level = spdlog::level::err;
+  Envoy::Logger::Registry::setLogLevel(default_log_level);
+
   // NOLINTNEXTLINE(clang-analyzer-optin.cplusplus.VirtualCall)
   TCLAP::CmdLine cmd("envoy-benchmark-test", ' ', "0.1");
   TCLAP::SwitchArg skip_switch("s", "skip_expensive_benchmarks",
                                "skip or minimize expensive benchmarks", cmd, false);
+  TCLAP::MultiArg<std::string> runtime_features(
+      "r", "runtime_feature", "runtime feature settings each of the form: <flag_name>:<flag_value>",
+      false, "string", cmd);
 
   cmd.setExceptionHandling(false);
   try {
@@ -36,6 +47,29 @@ int main(int argc, char** argv) {
   }
 
   skip_expensive_benchmarks = skip_switch.getValue();
+
+  // Initialize scoped_runtime if a runtime_feature argument is present. This
+  // allows benchmarks to use their own scoped_runtime in case no runtime flag is
+  // passed as an argument.
+  std::unique_ptr<TestScopedRuntime> scoped_runtime = nullptr;
+  const auto& runtime_features_args = runtime_features.getValue();
+  for (const absl::string_view runtime_feature_arg : runtime_features_args) {
+    if (scoped_runtime == nullptr) {
+      scoped_runtime = std::make_unique<TestScopedRuntime>();
+    }
+    // Make sure the argument contains a single ":" character.
+    const std::vector<std::string> runtime_feature_split = absl::StrSplit(runtime_feature_arg, ':');
+    if (runtime_feature_split.size() != 2) {
+      ENVOY_LOG_MISC(critical,
+                     "Given runtime flag \"{}\" should have a single ':' separating the flag name "
+                     "and its value.",
+                     runtime_feature_arg);
+      return 1;
+    }
+    const auto feature_name = runtime_feature_split[0];
+    const auto feature_val = runtime_feature_split[1];
+    Runtime::LoaderSingleton::getExisting()->mergeValues({{feature_name, feature_val}});
+  }
 
   ::benchmark::Initialize(&argc, argv);
 

test/common/http/header_map_impl_speed_test.cc
@@ -1,4 +1,5 @@
 #include "common/http/header_map_impl.h"
+#include "common/http/headers.h"
 
 #include "benchmark/benchmark.h"
 
@@ -9,8 +10,8 @@ namespace Http {
  * Add several dummy headers to a HeaderMap.
  * @param num_headers the number of dummy headers to add.
  */
-static void addDummyHeaders(HeaderMap& headers, size_t num_headers) {
-  const std::string prefix("dummy-key-");
+static void addDummyHeaders(HeaderMap& headers, size_t num_headers,
+                            const std::string prefix = "dummy-key-") {
   for (size_t i = 0; i < num_headers; i++) {
     headers.addCopy(LowerCaseString(prefix + std::to_string(i)), "abcd");
   }
@@ -44,7 +45,7 @@ static void headerMapImplSetReference(benchmark::State& state) {
   }
   benchmark::DoNotOptimize(headers->size());
 }
-BENCHMARK(headerMapImplSetReference)->Arg(0)->Arg(1)->Arg(10)->Arg(50);
+BENCHMARK(headerMapImplSetReference)->Arg(0)->Arg(1)->Arg(5)->Arg(10)->Arg(50);
 
 /**
  * Measure the speed of retrieving a header value. The numeric Arg passed by the
@@ -66,7 +67,7 @@ static void headerMapImplGet(benchmark::State& state) {
   }
   benchmark::DoNotOptimize(successes);
 }
-BENCHMARK(headerMapImplGet)->Arg(0)->Arg(1)->Arg(10)->Arg(50);
+BENCHMARK(headerMapImplGet)->Arg(0)->Arg(1)->Arg(5)->Arg(10)->Arg(50);
 
 /**
  * Measure the retrieval speed of a header for which HeaderMapImpl is expected to
@@ -83,7 +84,7 @@ static void headerMapImplGetInline(benchmark::State& state) {
   }
   benchmark::DoNotOptimize(size);
 }
-BENCHMARK(headerMapImplGetInline)->Arg(0)->Arg(1)->Arg(10)->Arg(50);
+BENCHMARK(headerMapImplGetInline)->Arg(0)->Arg(1)->Arg(5)->Arg(10)->Arg(50);
 
 /**
  * Measure the speed of writing to a header for which HeaderMapImpl is expected to
@@ -98,7 +99,7 @@ static void headerMapImplSetInlineMacro(benchmark::State& state) {
   }
   benchmark::DoNotOptimize(headers->size());
 }
-BENCHMARK(headerMapImplSetInlineMacro)->Arg(0)->Arg(1)->Arg(10)->Arg(50);
+BENCHMARK(headerMapImplSetInlineMacro)->Arg(0)->Arg(1)->Arg(5)->Arg(10)->Arg(50);
 
 /**
  * Measure the speed of writing to a header for which HeaderMapImpl is expected to
@@ -113,7 +114,7 @@ static void headerMapImplSetInlineInteger(benchmark::State& state) {
   }
   benchmark::DoNotOptimize(headers->size());
 }
-BENCHMARK(headerMapImplSetInlineInteger)->Arg(0)->Arg(1)->Arg(10)->Arg(50);
+BENCHMARK(headerMapImplSetInlineInteger)->Arg(0)->Arg(1)->Arg(5)->Arg(10)->Arg(50);
 
 /** Measure the speed of the byteSize() estimation method. */
 static void headerMapImplGetByteSize(benchmark::State& state) {
@@ -125,7 +126,7 @@ static void headerMapImplGetByteSize(benchmark::State& state) {
   }
   benchmark::DoNotOptimize(size);
 }
-BENCHMARK(headerMapImplGetByteSize)->Arg(0)->Arg(1)->Arg(10)->Arg(50);
+BENCHMARK(headerMapImplGetByteSize)->Arg(0)->Arg(1)->Arg(5)->Arg(10)->Arg(50);
 
 /** Measure the speed of iteration with a lightweight callback. */
 static void headerMapImplIterate(benchmark::State& state) {
@@ -141,7 +142,7 @@ static void headerMapImplIterate(benchmark::State& state) {
   }
   benchmark::DoNotOptimize(num_callbacks);
 }
-BENCHMARK(headerMapImplIterate)->Arg(0)->Arg(1)->Arg(10)->Arg(50);
+BENCHMARK(headerMapImplIterate)->Arg(0)->Arg(1)->Arg(5)->Arg(10)->Arg(50);
 
 /**
  * Measure the speed of removing a header by key name.
@@ -159,7 +160,7 @@ static void headerMapImplRemove(benchmark::State& state) {
   }
   benchmark::DoNotOptimize(headers->size());
 }
-BENCHMARK(headerMapImplRemove)->Arg(0)->Arg(1)->Arg(10)->Arg(50);
+BENCHMARK(headerMapImplRemove)->Arg(0)->Arg(1)->Arg(5)->Arg(10)->Arg(50);
 
 /**
  * Measure the speed of removing a header by key name, for the special case of
@@ -178,7 +179,7 @@ static void headerMapImplRemoveInline(benchmark::State& state) {
   }
   benchmark::DoNotOptimize(headers->size());
 }
-BENCHMARK(headerMapImplRemoveInline)->Arg(0)->Arg(1)->Arg(10)->Arg(50);
+BENCHMARK(headerMapImplRemoveInline)->Arg(0)->Arg(1)->Arg(5)->Arg(10)->Arg(50);
 
 /**
  * Measure the speed of creating a HeaderMapImpl and populating it with a realistic
@@ -206,6 +207,93 @@ static void headerMapImplPopulate(benchmark::State& state) {
   }
 }
 BENCHMARK(headerMapImplPopulate);
+
+/**
+ * Measure the speed of encoding headers as part of upgraded requests (HTTP/1 to HTTP/2)
+ * @note The measured time for each iteration includes the time needed to add
+ *       a varying number of headers (set by the benchmark's argument).
+ */
+static void headerMapImplEmulateH1toH2Upgrade(benchmark::State& state) {
+  uint32_t total_len = 0; // Accumulates the length of all header keys and values.
+  auto headers = Http::RequestHeaderMapImpl::create();
+  addDummyHeaders(*headers, state.range(0));
+  headers->setConnection(Http::Headers::get().ConnectionValues.Upgrade);
+  headers->setUpgrade(Http::Headers::get().UpgradeValues.H2c);
+
+  for (auto _ : state) { // NOLINT
+    // Emulate the encodeHeaders method upgrade part.
+    Http::RequestHeaderMapPtr modified_headers = createHeaderMap<RequestHeaderMapImpl>(*headers);
+    benchmark::DoNotOptimize(headers->getUpgradeValue());
+    // Emulate the Http::Utility::transformUpgradeRequestFromH1toH2 function.
+    modified_headers->setReferenceMethod(Http::Headers::get().MethodValues.Connect);
+    modified_headers->setProtocol(headers->getUpgradeValue());
+    modified_headers->removeUpgrade();
+    modified_headers->removeConnection();
+    if (modified_headers->getContentLengthValue() == "0") {
+      modified_headers->removeContentLength();
+    }
+    // Emulate the headers iteration in the buildHeaders method.
+    modified_headers->iterate([&total_len](const HeaderEntry& header) -> HeaderMap::Iterate {
+      const absl::string_view header_key = header.key().getStringView();
+      const absl::string_view header_value = header.value().getStringView();
+      total_len += header_key.length() + header_value.length();
+      return HeaderMap::Iterate::Continue;
+    });
+    // modified_headers destruction time also being measured.
+  }
+  benchmark::DoNotOptimize(headers->size());
+  benchmark::DoNotOptimize(total_len);
+}
+BENCHMARK(headerMapImplEmulateH1toH2Upgrade)->Arg(0)->Arg(1)->Arg(5)->Arg(10)->Arg(50);
+
+/**
+ * Measure the speed of decoding headers as part of upgraded responses (HTTP/2 to HTTP/1)
+ * @note The measured time for each iteration includes the time needed to add
+ *       a varying number of headers (set by the benchmark's argument).
+ */
+static void headerMapImplEmulateH2toH1Upgrade(benchmark::State& state) {
+  uint32_t total_len = 0; // Accumulates the length of all header keys and values.
+  auto headers = Http::ResponseHeaderMapImpl::create();
+  addDummyHeaders(*headers, state.range(0));
+  headers->setStatus(200);
+
+  for (auto _ : state) { // NOLINT
+    // Emulate the Http::Utility::transformUpgradeResponseFromH2toH1 function.
+    benchmark::DoNotOptimize(headers->getStatusValue());
+    headers->setUpgrade(Http::Headers::get().UpgradeValues.H2c);
+    headers->setReferenceConnection(Http::Headers::get().ConnectionValues.Upgrade);
+    headers->setStatus(101);
+    // Emulate a decodeHeaders function that iterates over the headers.
+    headers->iterate([&total_len](const HeaderEntry& header) -> HeaderMap::Iterate {
+      const absl::string_view header_key = header.key().getStringView();
+      const absl::string_view header_value = header.value().getStringView();
+      total_len += header_key.length() + header_value.length();
+      return HeaderMap::Iterate::Continue;
+    });
+  }
+  benchmark::DoNotOptimize(headers->size());
+  benchmark::DoNotOptimize(total_len);
+}
+BENCHMARK(headerMapImplEmulateH2toH1Upgrade)->Arg(0)->Arg(1)->Arg(5)->Arg(10)->Arg(50);
+
+/**
+ * Measure the speed of removing a varying number of headers by key name prefix from
+ * a header-map that contains 80 headers that do not have that prefix.
+ */
+static void headerMapImplRemovePrefix(benchmark::State& state) {
+  const LowerCaseString prefix("X-prefix");
+  auto headers = Http::ResponseHeaderMapImpl::create();
+  addDummyHeaders(*headers, 80);
+  for (auto _ : state) { // NOLINT
+    // Add the headers with the prefix
+    state.PauseTiming();
+    addDummyHeaders(*headers, state.range(0), prefix.get());
+    state.ResumeTiming();
+    headers->removePrefix(prefix);
+  }
+  benchmark::DoNotOptimize(headers->size());
+}
+BENCHMARK(headerMapImplRemovePrefix)->Arg(0)->Arg(1)->Arg(5)->Arg(10)->Arg(50);
 
 } // namespace Http
 } // namespace Envoy

test/common/http/header_map_impl_test.cc
@@ -540,20 +540,42 @@ TEST(HeaderMapImplTest, RemoveIf) {
   LowerCaseString key2 = LowerCaseString("X-postfix-");
   LowerCaseString key3 = LowerCaseString("x-postfix-eep");
 
-  TestRequestHeaderMapImpl headers;
-  headers.addReference(key1, "value");
-  headers.addReference(key2, "value");
-  headers.addReference(key3, "value");
+  {
+    TestRequestHeaderMapImpl headers;
+    headers.addReference(key1, "value");
+    headers.addReference(key2, "value");
+    headers.addReference(key3, "value");
 
-  EXPECT_EQ(0UL, headers.removeIf([](const HeaderEntry&) -> bool { return false; }));
+    EXPECT_EQ(0UL, headers.removeIf([](const HeaderEntry&) -> bool { return false; }));
 
-  EXPECT_EQ(2UL, headers.removeIf([](const HeaderEntry& entry) -> bool {
-    return absl::EndsWith(entry.key().getStringView(), "foo") ||
-           absl::EndsWith(entry.key().getStringView(), "eep");
-  }));
+    EXPECT_EQ(2UL, headers.removeIf([](const HeaderEntry& entry) -> bool {
+      return absl::EndsWith(entry.key().getStringView(), "foo") ||
+             absl::EndsWith(entry.key().getStringView(), "eep");
+    }));
 
-  TestRequestHeaderMapImpl expected{{"X-postfix-", "value"}};
-  EXPECT_EQ(expected, headers);
+    TestRequestHeaderMapImpl expected{{"X-postfix-", "value"}};
+    EXPECT_EQ(expected, headers);
+  }
+
+  // Test multiple entries with same key but different value.
+  {
+    TestRequestHeaderMapImpl headers;
+    headers.addReference(key1, "valueA");
+    headers.addReference(key1, "valueB");
+    headers.addReference(key1, "valueC");
+    headers.addReference(key2, "valueB");
+    headers.addReference(key3, "valueC");
+
+    EXPECT_EQ(5UL, headers.size());
+    EXPECT_EQ(2UL, headers.removeIf([](const HeaderEntry& entry) -> bool {
+      return absl::EndsWith(entry.value().getStringView(), "B");
+    }));
+
+    // Make sure key1 other values still exist.
+    TestRequestHeaderMapImpl expected{
+        {key1.get(), "valueA"}, {key1.get(), "valueC"}, {key3.get(), "valueC"}};
+    EXPECT_EQ(expected, headers);
+  }
 }
 
 TEST(HeaderMapImplTest, RemovePrefix) {
@@ -1213,7 +1235,7 @@ TEST(HeaderMapImplTest, PseudoHeaderOrder) {
 // Validate that TestRequestHeaderMapImpl copy construction and assignment works. This is a
 // regression for where we were missing a valid copy constructor and had the
 // default (dangerous) move semantics takeover.
-TEST(HeaderMapImplTest, TestRequestHeaderMapImplyCopy) {
+TEST(HeaderMapImplTest, TestRequestHeaderMapImplCopy) {
   TestRequestHeaderMapImpl foo;
   foo.addCopy(LowerCaseString("foo"), "bar");
   auto headers = std::make_unique<TestRequestHeaderMapImpl>(foo);

test/common/router/BUILD
@@ -1,5 +1,6 @@
 load(
     "//bazel:envoy_build_system.bzl",
+    "envoy_cc_benchmark_binary",
     "envoy_cc_fuzz_test",
     "envoy_cc_test",
     "envoy_cc_test_binary",
@@ -38,6 +39,21 @@ envoy_cc_test_library(
         "//test/test_common:utility_lib",
         "@envoy_api//envoy/config/route/v3:pkg_cc_proto",
         "@envoy_api//envoy/type/v3:pkg_cc_proto",
+    ],
+)
+
+envoy_cc_benchmark_binary(
+    name = "config_impl_headermap_benchmark_test",
+    srcs = ["config_impl_headermap_benchmark_test.cc"],
+    external_deps = [
+        "benchmark",
+    ],
+    deps = [
+        "//source/common/http:header_map_lib",
+        "//source/common/router:config_lib",
+        "//test/mocks/server:server_mocks",
+        "//test/test_common:utility_lib",
+        "@envoy_api//envoy/config/route/v3:pkg_cc_proto",
     ],
 )
 

commit 83e6c070ced7cff869c9b975ac3ee0100cee4ed1

common: fix `-Wrange-loop-analysis` error on Envoy Mobile
Envoy Mobile is failing compilation with this error:

```
external/envoy/source/common/http/header_map_impl.cc:506:23: error: loop variable 'values_it' of type 'const std::__1::__list_iterator<Envoy::Http::HeaderMapImpl::HeaderEntryImpl, void *>' creates a copy from type 'const std::__1::__list_iterator<Envoy::Http::HeaderMapImpl::HeaderEntryImpl, void *>' [-Werror,-Wrange-loop-analysis]
      for (const auto values_it : v) {
                      ^
external/envoy/source/common/http/header_map_impl.cc:506:12: note: use reference type 'const std::__1::__list_iterator<Envoy::Http::HeaderMapImpl::HeaderEntryImpl, void *> &' to prevent copying
      for (const auto values_it : v) {
           ^~~~~~~~~~~~~~~~~~~~~~
                      &
```

This regression was introduced in https://github.com/envoyproxy/envoy/commit/442b3f168384f7a1a714d9478f1512e9152706c3.

Signed-off-by: Michael Rebello <me@michaelrebello.com>

source/common/http/header_map_impl.cc
@@ -503,7 +503,7 @@ HeaderMap::NonConstGetResult HeaderMapImpl::getExisting(const LowerCaseString& k
     if (iter != headers_.mapEnd()) {
       const HeaderList::HeaderNodeVector& v = iter->second;
       ASSERT(!v.empty()); // It's impossible to have a map entry with an empty vector as its value.
-      for (const auto values_it : v) {
+      for (const auto& values_it : v) {
         // Convert the iterated value to a HeaderEntry*.
         ret.push_back(&(*values_it));
       }