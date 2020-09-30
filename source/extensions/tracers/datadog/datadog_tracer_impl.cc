eb5a0895d307408bb2152845065969a262945696
alyssawilk <alyssar@chromium.org>
2020-09-30 14:56:13 -0400

    http: fix datadog and squash handling of responses without bod (#13328)
    Commit Message: Fixing bugs in datadog and sqaush where unexpected bodyless responses would crash Envoy
    Risk Level: low
    Testing: new unit tests
    Docs Changes: n/a
    Release Notes: inline
    
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>
    Signed-off-by: Christoph Pakulski <christoph@tetrate.io>

--- source/extensions/tracers/datadog/datadog_tracer_impl.cc
+++ source/extensions/tracers/datadog/datadog_tracer_impl.cc
@@ -140,7 +140,7 @@ void TraceReporter::onSuccess(const Http::AsyncClient::Request& request,
   } else {
     ENVOY_LOG(debug, "traces successfully submitted to datadog agent");
     driver_.tracerStats().reports_sent_.inc();
-    encoder_->handleResponse(http_response->body()->toString());
+    encoder_->handleResponse(http_response->bodyAsString());
   }
 }
 
