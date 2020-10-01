99276bc1e765ac7f43e981d7cc8139974cd4691a
Shikugawa <rei@tetrate.io>
2020-10-01 20:38:17 +0900

    fix build
    Signed-off-by: Shikugawa <rei@tetrate.io>

--- test/extensions/clusters/aggregate/cluster_update_test.cc
+++ test/extensions/clusters/aggregate/cluster_update_test.cc
@@ -40,7 +40,7 @@ public:
     cluster_manager_ = std::make_unique<Upstream::TestClusterManagerImpl>(
         bootstrap, factory_, factory_.stats_, factory_.tls_, factory_.runtime_, factory_.random_,
         factory_.local_info_, log_manager_, factory_.dispatcher_, admin_, validation_context_,
-        *api_, http_context_, grpc_context_);
+        *api_, http_context_, grpc_context_, secret_manager_);
     cluster_manager_->initializeSecondaryClusters(bootstrap);
     EXPECT_EQ(cluster_manager_->activeClusters().size(), 1);
     cluster_ = cluster_manager_->get("aggregate_cluster");
@@ -49,6 +49,7 @@ public:
   Stats::IsolatedStoreImpl stats_store_;
   NiceMock<Server::MockAdmin> admin_;
   NiceMock<Upstream::TestClusterManagerFactory> factory_;
+  NiceMock<Secret::MockSecretManager> secret_manager_;
   Api::ApiPtr api_{Api::createApiForTest(stats_store_, factory_.random_)};
   Upstream::ThreadLocalCluster* cluster_;
 
@@ -264,7 +265,7 @@ TEST_F(AggregateClusterUpdateTest, InitializeAggregateClusterAfterOtherClusters)
   cluster_manager_ = std::make_unique<Upstream::TestClusterManagerImpl>(
       bootstrap, factory_, factory_.stats_, factory_.tls_, factory_.runtime_, factory_.random_,
       factory_.local_info_, log_manager_, factory_.dispatcher_, admin_, validation_context_, *api_,
-      http_context_, grpc_context_);
+      http_context_, grpc_context_, secret_manager_);
   cluster_manager_->initializeSecondaryClusters(bootstrap);
   EXPECT_EQ(cluster_manager_->activeClusters().size(), 2);
   cluster_ = cluster_manager_->get("aggregate_cluster");
