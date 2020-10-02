4d96f1dd265055a5b208c647dfb878de105072ab
Justin Mazzola Paluska <justinmp@google.com>
2020-10-02 08:00:33 -0400

    Remove unnecessary scope
    Signed-off-by: Justin Mazzola Paluska <justinmp@google.com>

--- test/extensions/filters/http/cdn_loop/parser_fuzz_test.cc
+++ test/extensions/filters/http/cdn_loop/parser_fuzz_test.cc
@@ -8,11 +8,9 @@ namespace Envoy {
 namespace Fuzz {
 
 DEFINE_FUZZER(const uint8_t* buf, size_t len) {
-  {
-    Envoy::Extensions::HttpFilters::CdnLoop::Parser::ParseContext input(
-        absl::string_view(reinterpret_cast<const char*>(buf), len));
-    Envoy::Extensions::HttpFilters::CdnLoop::Parser::parseCdnInfoList(input).IgnoreError();
-  }
+  Envoy::Extensions::HttpFilters::CdnLoop::Parser::ParseContext input(
+      absl::string_view(reinterpret_cast<const char*>(buf), len));
+  Envoy::Extensions::HttpFilters::CdnLoop::Parser::parseCdnInfoList(input).IgnoreError();
 }
 
 } // namespace Fuzz
