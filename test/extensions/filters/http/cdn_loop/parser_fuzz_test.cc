19abf7ec63fc67259fbfe3bba1c871f308cf99be
Justin Mazzola Paluska <justinmp@google.com>
2020-10-02 08:25:46 -0400

    Check that the return value of parseCdnInfoList makes sense
    Signed-off-by: Justin Mazzola Paluska <justinmp@google.com>

--- test/extensions/filters/http/cdn_loop/parser_fuzz_test.cc
+++ test/extensions/filters/http/cdn_loop/parser_fuzz_test.cc
@@ -1,16 +1,33 @@
+#include "common/common/statusor.h"
+
 #include "extensions/filters/http/cdn_loop/parser.h"
 
 #include "test/fuzz/fuzz_runner.h"
 
 #include "absl/strings/string_view.h"
+#include "gtest/gtest.h"
 
 namespace Envoy {
 namespace Fuzz {
 
+using Envoy::Extensions::HttpFilters::CdnLoop::Parser::parseCdnInfoList;
+using Envoy::Extensions::HttpFilters::CdnLoop::Parser::ParseContext;
+using Envoy::Extensions::HttpFilters::CdnLoop::Parser::ParsedCdnInfoList;
+
 DEFINE_FUZZER(const uint8_t* buf, size_t len) {
-  Envoy::Extensions::HttpFilters::CdnLoop::Parser::ParseContext input(
-      absl::string_view(reinterpret_cast<const char*>(buf), len));
-  Envoy::Extensions::HttpFilters::CdnLoop::Parser::parseCdnInfoList(input).IgnoreError();
+  absl::string_view input(reinterpret_cast<const char*>(buf), len);
+  StatusOr<ParsedCdnInfoList> list = parseCdnInfoList(ParseContext(input));
+  if (list) {
+    // If we successfully parse input, we should make sure that cdn_ids we find appear in the input
+    // string in order.
+    size_t start = 0;
+    for (const absl::string_view cdn_id : list->cdnIds()) {
+      size_t pos = input.find(cdn_id, start);
+      EXPECT_NE(pos, absl::string_view::npos);
+      EXPECT_GE(pos, start);
+      start = pos + cdn_id.length();
+    }
+  }
 }
 
 } // namespace Fuzz
