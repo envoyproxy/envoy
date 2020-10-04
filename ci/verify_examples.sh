48b24d5091671b4da97e66c700ee219ac0291cd6
Ryan Northey <ryan@synca.io>
2020-10-04 06:05:21 +0100

    remove unnecessary verify_build_configs function
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ci/verify_examples.sh
+++ ci/verify_examples.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -E
 
-TESTCOMMAND="${1:-}"
-TESTFILTER="${2:-*}"
+TESTFILTER="${1:-*}"
 FAILED=()
 SRCDIR="${SRCDIR:-$(pwd)}"
-EXCLUDED_BUILD_CONFIGS=${EXCLUDED_BUILD_CONFIGS:-"^./cache/responses.yaml|^./jaeger-native-tracing|docker-compose"}
 
 
 trap_errors () {
@@ -39,39 +37,7 @@ run_examples () {
     done
 }
 
-verify_build_configs () {
-    local config configs missing
-    missing=()
-    cd "${SRCDIR}/examples" || return 1
-    configs="$(find . -name "*.yaml" -o -name "*.lua" | grep -vE "${EXCLUDED_BUILD_CONFIGS}" | cut  -d/ -f2-)"
-    for config in $configs; do
-        grep "\"$config\"" BUILD || missing+=("$config")
-    done
-    if [[ -n "${missing[*]}" ]]; then
-       for config in "${missing[@]}"; do
-           echo "Missing config: $config" >&2
-       done
-       return 1
-    fi
-}
-
-case "$TESTCOMMAND" in
-    build_config)
-	echo "Running build_configs test"
-	verify_build_configs
-	;;
-    sandbox)
-	echo "Running sandbox test"
-	run_examples
-	;;
-    ""|all)
-	echo "Running all tests"
-	verify_build_configs
-	run_examples
-	;;
-    *)
-	echo "Usage: ./ci/verify_examples.sh [build_config|sandbox|all] [<test_filter>]"
-esac
+run_examples
 
 if [[ "${#FAILED[@]}" -ne "0" ]]; then
     echo "TESTS FAILED:"
