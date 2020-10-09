fb3908b5bb39ae5a79ab735aa1ace2c6938e4a70
Snow Pettersen <snowp@lyft.com>
2020-10-09 17:52:17 +0000

    more impl fixes
    Signed-off-by: Snow Pettersen <snowp@lyft.com>

--- source/common/filter/http/filter_config_discovery_impl.h
+++ source/common/filter/http/filter_config_discovery_impl.h
@@ -110,7 +110,7 @@ private:
                       const std::string&) override;
   void onConfigUpdateFailed(Config::ConfigUpdateFailureReason reason,
                             const EnvoyException*) override;
-  void onConfigExpired(const std::vector<std::string>&) {
+  void onConfigExpired(const std::vector<std::string>&) override {
     // TODO(snowp): Implement.
   }
 
