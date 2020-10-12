773ded226ed5d3ff528580aace1013db0aee6d75
Alex Konradi <akonradi@google.com>
2020-10-12 10:36:14 -0400

    Use unique_ptr::reset() instead of disabling
    Signed-off-by: Alex Konradi <akonradi@google.com>

--- source/common/http/conn_manager_impl.cc
+++ source/common/http/conn_manager_impl.cc
@@ -193,19 +193,10 @@ void ConnectionManagerImpl::doEndStream(ActiveStream& stream) {
 }
 
 void ConnectionManagerImpl::doDeferredStreamDestroy(ActiveStream& stream) {
-  if (stream.max_stream_duration_timer_) {
-    stream.max_stream_duration_timer_->disableTimer();
-    stream.max_stream_duration_timer_ = nullptr;
-  }
-  if (stream.stream_idle_timer_ != nullptr) {
-    stream.stream_idle_timer_->disableTimer();
-    stream.stream_idle_timer_ = nullptr;
-  }
+  stream.max_stream_duration_timer_.reset();
+  stream.stream_idle_timer_.reset();
   stream.filter_manager_.disarmRequestTimeout();
-  if (stream.request_header_timer_ != nullptr) {
-    stream.request_header_timer_->disableTimer();
-    stream.request_header_timer_ = nullptr;
-  }
+  stream.request_header_timer_.reset();
 
   stream.completeRequest();
   stream.filter_manager_.onStreamComplete();
