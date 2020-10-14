b7a47564ca2ee6844f6344a4f8152d40c052da86
Lizan Zhou <lizan@tetrate.io>
2020-10-14 08:16:42 -0700

    ci: use azp for api and go-control-plane sync (#13550)
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/go_mirror.sh
+++ ci/go_mirror.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-if [ -z "$CIRCLE_PULL_REQUEST" ] && [ "$CIRCLE_BRANCH" == "master" ]
-then
-  tools/api/generate_go_protobuf.py
+MAIN_BRANCH="refs/heads/master"
+
+# shellcheck source=ci/setup_cache.sh
+. "$(dirname "$0")"/setup_cache.sh
+
+if [[ "${AZP_BRANCH}" == "${MAIN_BRANCH}" ]]; then
+  BAZEL_BUILD_OPTIONS="${BAZEL_BUILD_EXTRA_OPTIONS}" tools/api/generate_go_protobuf.py
 fi
