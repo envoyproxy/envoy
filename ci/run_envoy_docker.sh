1cfa053334e660dc1d3ee0a19b7227fd58bd0642
Lizan Zhou <lizan@tetrate.io>
2020-10-14 08:16:42 -0700

    ci: use azp for api and go-control-plane sync (#13550)
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- ci/run_envoy_docker.sh
+++ ci/run_envoy_docker.sh
@@ -56,6 +56,7 @@ mkdir -p "${ENVOY_DOCKER_BUILD_DIR}"
 
 [[ -t 1 ]] && ENVOY_DOCKER_OPTIONS+=("-it")
 [[ -f .git ]] && [[ ! -d .git ]] && ENVOY_DOCKER_OPTIONS+=(-v "$(git rev-parse --git-common-dir):$(git rev-parse --git-common-dir)")
+[[ -n "${SSH_AUTH_SOCK}" ]] && ENVOY_DOCKER_OPTIONS+=(-v "${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK}" -e SSH_AUTH_SOCK)
 
 export ENVOY_BUILD_IMAGE="${IMAGE_NAME}:${IMAGE_ID}"
 
