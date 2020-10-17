3c88d29e8fb35887acf379db55d139712b357f5d
phlax <phlax@users.noreply.github.com>
2020-10-17 21:02:19 +0100

    ci/examples: Fix umask/permissions in verify_examples (#13617)
    Signed-off-by: Ryan Northey <ryan@synca.io>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -438,6 +438,7 @@ elif [[ "$CI_TARGET" == "verify_examples" ]]; then
   sudo apt-get install -y -qq --no-install-recommends redis-tools
   export DOCKER_NO_PULL=1
   umask 027
+  chmod -R o-rwx examples/
   ci/verify_examples.sh
   exit 0
 else
