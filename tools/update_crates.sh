cfa1e67100bbf3d8236f83e6dc998048952f836c
Piotr Sikora <piotrsikora@google.com>
2020-10-17 02:04:48 +0000

    review: fix shellcheck.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- tools/update_crates.sh
+++ tools/update_crates.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 ROOT="$(dirname "$(dirname "$(realpath "$0")")")"
 
 # Pin to a specific cargo-raze version, since the output might change between versions.
 cargo install cargo-raze --version 0.3.8
 
 # Regenerate BUILD files.
-cd ${ROOT}/bazel/external/cargo
+cd "${ROOT}"/bazel/external/cargo
 rm -rf remote/
 cargo generate-lockfile
 cargo raze
