71c962f898872000595e036d157df2f27276b518
Zach Reyes <39203661+zasweq@users.noreply.github.com>
2020-10-19 16:15:51 -0400

    [fuzz] Fixed divide by zero bug (#13545)
    Fixed divide by zero bug in admission control filter for HTTP.
    
    Fixes issues: https://oss-fuzz.com/testcase-detail/5914972389113856
    
    Risk Level: Low
    Testing: Added regression test
    
    Signed-off-by: Zach <zasweq@google.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/extensions/filters/http/admission_control/config_test.cc
+++ test/extensions/filters/http/admission_control/config_test.cc
@@ -6,6 +6,7 @@
 #include "common/stats/isolated_store_impl.h"
 
 #include "extensions/filters/http/admission_control/admission_control.h"
+#include "extensions/filters/http/admission_control/config.h"
 #include "extensions/filters/http/admission_control/evaluators/success_criteria_evaluator.h"
 
 #include "test/mocks/runtime/mocks.h"
@@ -45,6 +46,36 @@ protected:
   Stats::IsolatedStoreImpl scope_;
   NiceMock<Random::MockRandomGenerator> random_;
 };
+
+// Ensure the filter ingest throws an exception if it is passed a config with a default value of 0
+// for sr_threshold If exception was not thrown, a default value of 0 for sr_threshold induces a
+// divide by zero error
+TEST_F(AdmissionControlConfigTest, ZeroSuccessRateThreshold) {
+  AdmissionControlFilterFactory admission_control_filter_factory;
+  const std::string yaml = R"EOF(
+enabled:
+  default_value: false
+  runtime_key: "foo.enabled"
+sampling_window: 1337s
+sr_threshold:
+  default_value:
+    value: 0
+  runtime_key: "foo.sr_threshold"
+aggression:
+  default_value: 4.2
+  runtime_key: "foo.aggression"
+success_criteria:
+  http_criteria:
+  grpc_criteria:
+)EOF";
+
+  AdmissionControlProto proto;
+  TestUtility::loadFromYamlAndValidate(yaml, proto);
+  NiceMock<Server::Configuration::MockFactoryContext> factory_context;
+  EXPECT_THROW_WITH_MESSAGE(admission_control_filter_factory.createFilterFactoryFromProtoTyped(
+                                proto, "whatever", factory_context),
+                            EnvoyException, "Success Rate Threshold cannot be zero percent");
+}
 
 // Verify the configuration when all fields are set.
 TEST_F(AdmissionControlConfigTest, BasicTestAllConfigured) {
