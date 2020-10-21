f3346fb67abeacb8d346119d6fbeb1a1e8427254
Piotr Sikora <piotrsikora@google.com>
2020-10-21 20:51:22 +0000

    review: update cargo-raze to 2e7f0d8.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- tools/update_crates.sh
+++ tools/update_crates.sh
@@ -5,12 +5,12 @@ set -e
 ROOT="$(dirname "$(dirname "$(realpath "$0")")")"
 
 # Pin to a specific cargo-raze version, since the output might change between versions.
-cargo install cargo-raze --version 0.3.8
+# TODO(PiotrSikora): update to a released version, once there is one containing this commit.
+cargo install cargo-raze --git https://github.com/google/cargo-raze --rev 2e7f0d82d88ab0a1b698de52b604579ce49aa883
 
 # Regenerate BUILD files.
 cd "${ROOT}"/bazel/external/cargo
 rm -rf remote/
 cargo generate-lockfile
 cargo raze
-buildifier -lint=fix crates.bzl && find . -type f -name "*BUILD" -exec buildifier -lint=fix {} \;
 git add .
