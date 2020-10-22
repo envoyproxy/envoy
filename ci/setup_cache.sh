e56ef8a621315b2a0bb924868385e2d12440ec70
Lizan Zhou <lizan@tetrate.io>
2020-10-21 21:38:08 -0700

    cherry-pick 0d862f639696b483f8a5f4262d72a8b0011b4859
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/setup_cache.sh
+++ ci/setup_cache.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ ! -z "${GCP_SERVICE_ACCOUNT_KEY}" ]]; then
+if [[ ! -z "${GCP_SERVICE_ACCOUNT_KEY:0:1}" ]]; then
   # mktemp will create a tempfile with u+rw permission minus umask, it will not be readable by all
   # users by default.
   GCP_SERVICE_ACCOUNT_KEY_FILE=$(mktemp -t gcp_service_account.XXXXXX.json)
@@ -14,27 +14,24 @@ if [[ ! -z "${GCP_SERVICE_ACCOUNT_KEY}" ]]; then
 
   trap gcp_service_account_cleanup EXIT
 
-  echo "${GCP_SERVICE_ACCOUNT_KEY}" | base64 --decode > "${GCP_SERVICE_ACCOUNT_KEY_FILE}"
+  bash -c 'echo "${GCP_SERVICE_ACCOUNT_KEY}"' | base64 --decode > "${GCP_SERVICE_ACCOUNT_KEY_FILE}"
+
+  export BAZEL_BUILD_EXTRA_OPTIONS+=" --google_credentials=${GCP_SERVICE_ACCOUNT_KEY_FILE}"
 fi
 
-if [[ "${BAZEL_REMOTE_CACHE}" =~ ^http ]]; then
-  if [[ ! -z "${GCP_SERVICE_ACCOUNT_KEY}" ]]; then
-    export BAZEL_BUILD_EXTRA_OPTIONS="${BAZEL_BUILD_EXTRA_OPTIONS} \
-      --remote_http_cache=${BAZEL_REMOTE_CACHE} \
-      --google_credentials=${GCP_SERVICE_ACCOUNT_KEY_FILE}"
-    echo "Set up bazel HTTP read/write cache at ${BAZEL_REMOTE_CACHE}."
-  else
-    export BAZEL_BUILD_EXTRA_OPTIONS="${BAZEL_BUILD_EXTRA_OPTIONS} \
-      --remote_http_cache=${BAZEL_REMOTE_CACHE} --noremote_upload_local_results"
-    echo "Set up bazel HTTP read only cache at ${BAZEL_REMOTE_CACHE}."
+
+if [[ ! -z "${BAZEL_REMOTE_CACHE}" ]]; then
+  export BAZEL_BUILD_EXTRA_OPTIONS+=" --remote_cache=${BAZEL_REMOTE_CACHE}"
+  echo "Set up bazel remote read/write cache at ${BAZEL_REMOTE_CACHE}."
+
+  if [[ ! -z "${BAZEL_REMOTE_INSTANCE}" ]]; then
+    export BAZEL_BUILD_EXTRA_OPTIONS+=" --remote_instance_name=${BAZEL_REMOTE_INSTANCE}"
+    echo "instance_name: ${BAZEL_REMOTE_INSTANCE}."
+  elif [[ -z "${ENVOY_RBE}" ]]; then
+    export BAZEL_BUILD_EXTRA_OPTIONS+=" --jobs=HOST_CPUS*.9 --remote_timeout=600"
+    echo "using local build cache."
   fi
-elif [[ ! -z "${BAZEL_REMOTE_CACHE}" ]]; then
-  export BAZEL_BUILD_EXTRA_OPTIONS="${BAZEL_BUILD_EXTRA_OPTIONS} \
-    --remote_cache=${BAZEL_REMOTE_CACHE} \
-    --remote_instance_name=${BAZEL_REMOTE_INSTANCE} \
-    --google_credentials=${GCP_SERVICE_ACCOUNT_KEY_FILE} \
-    --auth_enabled=true"
-  echo "Set up bazel remote read/write cache at ${BAZEL_REMOTE_CACHE} instance: ${BAZEL_REMOTE_INSTANCE}."
+
 else
-  echo "No remote cache bucket is set, skipping setup remote cache."
+  echo "No remote cache is set, skipping setup remote cache."
 fi
--- ci/upload_gcs_artifact.sh
+++ ci/upload_gcs_artifact.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Do not ever set -x here, it is a security hazard as it will place the credentials below in the
-# CI logs.
 set -e -o pipefail
 
 if [[ -z "${GCS_ARTIFACT_BUCKET}" ]]; then
@@ -10,7 +8,7 @@ if [[ -z "${GCS_ARTIFACT_BUCKET}" ]]; then
 fi
 
 # Fail when service account key is not specified
-echo ${GCP_SERVICE_ACCOUNT_KEY} | base64 --decode | gcloud auth activate-service-account --key-file=-
+bash -c 'echo ${GCP_SERVICE_ACCOUNT_KEY}' | base64 --decode | gcloud auth activate-service-account --key-file=-
 
 SOURCE_DIRECTORY="$1"
 TARGET_SUFFIX="$2"
