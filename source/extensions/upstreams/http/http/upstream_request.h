5b209b14ffd098c99468bb3d8f0e445e3aa96b61
Raul Gutierrez Segales <rgs@pinterest.com>
2020-10-23 16:05:33 -0400

    clang-format
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>

--- source/extensions/upstreams/http/http/upstream_request.h
+++ source/extensions/upstreams/http/http/upstream_request.h
@@ -27,7 +27,9 @@ public:
     conn_pool_ = cm.httpConnPoolForCluster(route_entry.clusterName(), route_entry.priority(),
                                            downstream_protocol, ctx);
   }
-  ~HttpConnPool() { RELEASE_ASSERT(conn_pool_stream_handle_ == nullptr, "conn_pool_stream_handle not null"); }
+  ~HttpConnPool() {
+    RELEASE_ASSERT(conn_pool_stream_handle_ == nullptr, "conn_pool_stream_handle not null");
+  }
   void newStream(Router::GenericConnectionPoolCallbacks* callbacks) override;
   bool cancelAnyPendingStream() override;
   absl::optional<Envoy::Http::Protocol> protocol() const override;
