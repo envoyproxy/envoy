7bab9e957e9a29e79a2d21d6fa13e8c670ba0cb6
Ryan Northey <ryan@synca.io>
2020-10-24 19:46:35 +0100

    examples/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- examples/dynamic-configuration/verify.sh
+++ examples/dynamic-configuration/verify.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -e
 
 export NAME=dynamic-configuration
+
+# shellcheck source=examples/verify-common.sh
+. "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
+
+# curl http://localhost:19000/config_dump
+
+curl http://localhost:19000/config_dump  | jq '.configs[1].dynamic_active_clusters' | grep null
+
+sleep 20
+
+curl http://localhost:19000/config_dump  | jq '.configs[1].dynamic_active_clusters'
