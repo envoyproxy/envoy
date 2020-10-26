82ef1fe0bfdcf1b95000e53f767bbc09c4e131cd
wbpcode <comems@msn.com>
2020-10-26 20:40:35 +0800

    remove depends_on.condition and add 60s delay in verify.sh
    Signed-off-by: wbpcode <comems@msn.com>

--- examples/skywalking-tracing/verify.sh
+++ examples/skywalking-tracing/verify.sh
@@ -5,6 +5,8 @@ export NAME=skywalking
 # shellcheck source=examples/verify-common.sh
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
+# Waiting SkyWalking server to be ready.
+sleep 60
 
 run_log "Test connection"
 responds_with \
@@ -12,8 +14,6 @@ responds_with \
     http://localhost:8000/trace/1
 
 run_log "Test stats"
-# Waiting SkyWalking server to be ready.
-sleep 20
 responds_with \
     "tracing.skywalking.segments_sent: 1" \
     http://localhost:8001/stats
