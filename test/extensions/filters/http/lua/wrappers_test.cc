aec5a3ca8282afbb08995c932efac0bc8d0315fa
Lizan Zhou <lizan@tetrate.io>
2020-10-26 23:42:01 -0700

    build: update protobuf to 3.13.0 (#13712)
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/extensions/filters/http/lua/wrappers_test.cc
+++ test/extensions/filters/http/lua/wrappers_test.cc
@@ -10,6 +10,7 @@
 #include "test/mocks/stream_info/mocks.h"
 #include "test/test_common/utility.h"
 
+using testing::Expectation;
 using testing::InSequence;
 using testing::ReturnPointee;
 using testing::ReturnRef;
@@ -451,36 +452,34 @@ TEST_F(LuaStreamInfoWrapperTest, ModifyAfterIterationForDynamicMetadata) {
     function callMe(object)
       object:dynamicMetadata():set("envoy.lb", "hello", "world")
       for filter, entry in pairs(object:dynamicMetadata()) do
-        testPrint(filter)
         for key, value in pairs(entry) do
-          testPrint(string.format("'%s' '%s'", key, value))
+          testPrint(string.format("'%s' '%s' '%s'", filter, key, value))
         end
       end
 
       object:dynamicMetadata():set("envoy.lb", "hello", "envoy")
       object:dynamicMetadata():set("envoy.proxy", "proto", "grpc")
+
+      testPrint("modified")
+
       for filter, entry in pairs(object:dynamicMetadata()) do
-        testPrint(filter)
         for key, value in pairs(entry) do
-          testPrint(string.format("'%s' '%s'", key, value))
+          testPrint(string.format("'%s' '%s' '%s'", filter, key, value))
         end
       end
     end
   )EOF"};
 
-  InSequence s;
   setup(SCRIPT);
 
   StreamInfo::StreamInfoImpl stream_info(Http::Protocol::Http2, test_time_.timeSystem());
   EXPECT_EQ(0, stream_info.dynamicMetadata().filter_metadata_size());
   Filters::Common::Lua::LuaDeathRef<StreamInfoWrapper> wrapper(
       StreamInfoWrapper::create(coroutine_->luaState(), stream_info), true);
-  EXPECT_CALL(printer_, testPrint("envoy.lb"));
-  EXPECT_CALL(printer_, testPrint("'hello' 'world'"));
-  EXPECT_CALL(printer_, testPrint("envoy.proxy"));
-  EXPECT_CALL(printer_, testPrint("'proto' 'grpc'"));
-  EXPECT_CALL(printer_, testPrint("envoy.lb"));
-  EXPECT_CALL(printer_, testPrint("'hello' 'envoy'"));
+  Expectation expect_1 = EXPECT_CALL(printer_, testPrint("'envoy.lb' 'hello' 'world'"));
+  Expectation expect_2 = EXPECT_CALL(printer_, testPrint("modified")).After(expect_1);
+  EXPECT_CALL(printer_, testPrint("'envoy.proxy' 'proto' 'grpc'")).After(expect_2);
+  EXPECT_CALL(printer_, testPrint("'envoy.lb' 'hello' 'envoy'")).After(expect_2);
   start("callMe");
 }
 
