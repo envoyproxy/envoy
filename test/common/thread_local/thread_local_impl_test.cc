fb496a9ecbdc2185ce66a752094e4e539e148709
Joshua Marantz <jmarantz@google.com>
2020-10-29 12:36:28 -0400

    tls: Typesafe tls slots (#13789)
    Commit Message: Adds a new TypedSlot API, where the slot data is strongly typed. Also removes an unused capability to replace slot data with a new instance via the callbacks in runOnAllThreads.
    
    Use this new API rather than the untyped Slot API for one of the sites (stats thread_local_store.cc).
    
    This is a partial fix for #13313
    Risk Level: low
    Testing: //test/...
    Docs Changes: n/a
    Release Notes: n/a
    Platform Specific Features: n/a
    
    Signed-off-by: Joshua Marantz <jmarantz@google.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/common/thread_local/thread_local_impl_test.cc
+++ test/common/thread_local/thread_local_impl_test.cc
@@ -149,35 +149,56 @@ TEST_F(ThreadLocalInstanceImplTest, CallbackNotInvokedAfterDeletion) {
   tls_.shutdownGlobalThreading();
 }
 
-// Test that the config passed into the update callback is the previous version stored in the slot.
+// Test that the update callback is called as expected, for the worker and main threads.
 TEST_F(ThreadLocalInstanceImplTest, UpdateCallback) {
   InSequence s;
 
   SlotPtr slot = tls_.allocateSlot();
 
-  auto newer_version = std::make_shared<TestThreadLocalObject>();
-  bool update_called = false;
+  uint32_t update_called = 0;
 
   TestThreadLocalObject& object_ref = setObject(*slot);
-  auto update_cb = [&object_ref, &update_called,
-                    newer_version](ThreadLocalObjectSharedPtr obj) -> ThreadLocalObjectSharedPtr {
-    // The unit test setup have two dispatchers registered, but only one thread, this lambda will be
-    // called twice in the same thread.
-    if (!update_called) {
-      EXPECT_EQ(obj.get(), &object_ref);
-      update_called = true;
-    } else {
-      EXPECT_EQ(obj.get(), newer_version.get());
-    }
-
-    return newer_version;
+  auto update_cb = [&update_called](ThreadLocalObjectSharedPtr obj) -> ThreadLocalObjectSharedPtr {
+    ++update_called;
+    return obj;
   };
   EXPECT_CALL(thread_dispatcher_, post(_));
   EXPECT_CALL(object_ref, onDestroy());
-  EXPECT_CALL(*newer_version, onDestroy());
   slot->runOnAllThreads(update_cb);
 
-  EXPECT_EQ(newer_version.get(), &slot->getTyped<TestThreadLocalObject>());
+  EXPECT_EQ(2, update_called); // 1 worker, 1 main thread.
+
+  tls_.shutdownGlobalThreading();
+  tls_.shutdownThread();
+}
+
+struct StringSlotObject : public ThreadLocalObject {
+  std::string str_;
+};
+
+TEST_F(ThreadLocalInstanceImplTest, TypedUpdateCallback) {
+  InSequence s;
+
+  TypedSlot<StringSlotObject> slot(tls_);
+
+  uint32_t update_called = 0;
+  EXPECT_CALL(thread_dispatcher_, post(_));
+  slot.set([](Event::Dispatcher&) -> std::shared_ptr<StringSlotObject> {
+    auto s = std::make_shared<StringSlotObject>();
+    s->str_ = "hello";
+    return s;
+  });
+  EXPECT_EQ("hello", slot.get().str_);
+
+  auto update_cb = [&update_called](StringSlotObject& s) {
+    ++update_called;
+    s.str_ = "goodbye";
+  };
+  EXPECT_CALL(thread_dispatcher_, post(_));
+  slot.runOnAllThreads(update_cb);
+
+  EXPECT_EQ("goodbye", slot.get().str_);
+  EXPECT_EQ(2, update_called); // 1 worker, 1 main thread.
 
   tls_.shutdownGlobalThreading();
   tls_.shutdownThread();
