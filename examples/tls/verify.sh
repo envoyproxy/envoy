e39f44a57529eb184baf99b04d2cc6e32f1b2de0
Ryan Northey <ryan@synca.io>
2020-10-31 12:50:33 +0000

    examples/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- examples/tls/verify.sh
+++ examples/tls/verify.sh
@@ -5,19 +5,23 @@ export NAME=tls
 # shellcheck source=examples/verify-common.sh
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
-run_log "Test http -> https"
-responds_with \
-    '"x-forwarded-proto": "http",' \
-    http://localhost:10000
-
 run_log "Test https -> http"
 responds_with \
     '"x-forwarded-proto": "https",' \
     -k \
-    https://localhost:10001
+    https://localhost:10000
+curl -sk https://localhost:10000  | jq  '.os.hostname' | grep '"service-http"'
 
 run_log "Test https -> https"
 responds_with \
     '"x-forwarded-proto": "https",' \
     -k \
-    https://localhost:10002
+    https://localhost:10001
+curl -sk https://localhost:10001  | jq  '.os.hostname' | grep '"service-https"'
+
+run_log "Test http -> https"
+responds_with \
+    '"x-forwarded-proto": "http",' \
+    http://localhost:10002
+
+curl -s http://localhost:10002  | jq  '.os.hostname' | grep '"service-https"'
