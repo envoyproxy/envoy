a69238fe2887056da9f0fcbfe02d91f34abd596d
Dhi Aurrahman <dio@tetrate.io>
2020-11-03 20:18:36 +0000

    Fix
    Signed-off-by: Dhi Aurrahman <dio@tetrate.io>

--- third_party/icu/googleurl/test.cc
+++ third_party/icu/googleurl/test.cc
@@ -2,6 +2,8 @@
 
 #include "gtest/gtest.h"
 
+namespace {
+
 // Basic smoke test to ensure that GURL (with shimmed ICU) works properly.
 TEST(GoogleUrl, SmokeTest) {
   GURL url("https://example.org/test?foo=bar#section");
@@ -16,16 +18,18 @@ TEST(GoogleUrl, SmokeTest) {
   GURL punycode("https://xn--c1yn36f.example");
   EXPECT_TRUE(punycode.is_valid());
 
+  GURL percent_encoded_valid("https://%20.example");
+  EXPECT_TRUE(percent_encoded_valid.is_valid());
+
   // Ensure ICU shim is functioning correctly, i.e. not crashing and resulting invalid parsed URL.
   GURL idn_url("https://\xe5\x85\x89.example/");
   EXPECT_FALSE(idn_url.is_valid());
 
-  GURL percent_encoded("https://%Da%aa.example");
-  EXPECT_FALSE(percent_encoded.is_valid());
-
-  GURL percent_encoded_more("https://%20.example");
-  EXPECT_FALSE(percent_encoded.is_valid());
+  GURL idn_with_percent("https://%Da%aa.example");
+  EXPECT_FALSE(idn_with_percent.is_valid());
 
   GURL with_bracket("http://[wwww].example");
   EXPECT_FALSE(with_bracket.is_valid());
 }
+
+} // namespace
