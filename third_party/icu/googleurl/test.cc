b320b8e9d11f5fd6641fd1ffa03a947c8fedaf37
Dhi Aurrahman <dio@tetrate.io>
2020-11-03 19:51:25 +0000

    More tests
    Signed-off-by: Dhi Aurrahman <dio@tetrate.io>

--- third_party/icu/googleurl/test.cc
+++ third_party/icu/googleurl/test.cc
@@ -13,7 +13,19 @@ TEST(GoogleUrl, SmokeTest) {
   EXPECT_EQ(url.query(), "foo=bar");
   EXPECT_EQ(url.ref(), "section");
 
+  GURL punycode("https://xn--c1yn36f.example");
+  EXPECT_TRUE(punycode.is_valid());
+
   // Ensure ICU shim is functioning correctly, i.e. not crashing and resulting invalid parsed URL.
   GURL idn_url("https://\xe5\x85\x89.example/");
   EXPECT_FALSE(idn_url.is_valid());
+
+  GURL percent_encoded("https://%Da%aa.example");
+  EXPECT_FALSE(percent_encoded.is_valid());
+
+  GURL percent_encoded_more("https://%20.example");
+  EXPECT_FALSE(percent_encoded.is_valid());
+
+  GURL with_bracket("http://[wwww].example");
+  EXPECT_FALSE(with_bracket.is_valid());
 }
