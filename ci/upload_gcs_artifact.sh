def78dd5f7d183a74447317f6569a31837522e4d
Ryan Northey <ryan@synca.io>
2020-11-04 10:49:30 +0000

    add redirect
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ci/upload_gcs_artifact.sh
+++ ci/upload_gcs_artifact.sh
@@ -18,8 +18,9 @@ if [ ! -d "${SOURCE_DIRECTORY}" ]; then
   exit 1
 fi
 
-if [ -n "${SHORT_COMMIT_SHA}" ]; then
+if [[ -n "${SHORT_COMMIT_SHA}" ]]; then
     UPLOAD_PATH="${SHORT_COMMIT_SHA}"
+    LINKED_PATH="${SYSTEM_PULLREQUEST_PULLREQUESTNUMBER:-${BUILD_SOURCEBRANCHNAME}}"
 else
     UPLOAD_PATH="${SYSTEM_PULLREQUEST_PULLREQUESTNUMBER:-${BUILD_SOURCEBRANCHNAME}}"
 fi
@@ -28,4 +29,15 @@ GCS_LOCATION="${GCS_ARTIFACT_BUCKET}/${UPLOAD_PATH}/${TARGET_SUFFIX}"
 
 echo "Uploading to gs://${GCS_LOCATION} ..."
 gsutil -mq rsync -dr "${SOURCE_DIRECTORY}" "gs://${GCS_LOCATION}"
+
+if [[ -n "$LINKED_PATH" ]]; then
+    TMPLINK="/tmp/docredirect/${LINKED_PATH}/docs"
+    mkdir -p "$TMPLINK"
+    echo "<meta http-equiv=\"refresh\" content=\"0; URL='https://storage.googleapis.com/${GCS_LOCATION}/index.html'\" />" \
+	 >  "${TMPLINK}/index.html"
+    GCS_LOCATION="${GCS_ARTIFACT_BUCKET}/${LINKED_PATH}"
+    echo "Uploading redirect to gs://${GCS_LOCATION} ..."
+    gsutil -mq rsync -dr "/tmp/docredirect/${LINKED_PATH}" "gs://${GCS_LOCATION}"
+fi
+
 echo "Artifacts uploaded to: https://storage.googleapis.com/${GCS_LOCATION}/index.html"
