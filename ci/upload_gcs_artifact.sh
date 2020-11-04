6bfb669bf9742efde61a2e2cd09702675bdfd821
Ryan Northey <ryan@synca.io>
2020-11-04 12:21:33 +0000

    ci
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ci/upload_gcs_artifact.sh
+++ ci/upload_gcs_artifact.sh
@@ -39,7 +39,7 @@ if [[ "$TARGET_SUFFIX" == "docs" ]]; then
 	 >  "${TMP_REDIRECT}/index.html"
     GCS_REDIRECT="${GCS_ARTIFACT_BUCKET}/${REDIRECT_PATH}"
     echo "Uploading redirect to gs://${GCS_REDIRECT} ..."
-    gsutil -mq rsync -dr "/tmp/docredirect/${REDIRECT_PATH}" "gs://${GCS_REDIRECT}"
+    gsutil -h "Cache-Control:no-cache,max-age=0" -mq rsync -dr "/tmp/docredirect/${REDIRECT_PATH}" "gs://${GCS_REDIRECT}"
 fi
 
 echo "Artifacts uploaded to: https://storage.googleapis.com/${GCS_LOCATION}/index.html"
