28aefc439bf8248d3e67204494f32c377cf7a5ce
mathetake <takeshi@tetrate.io>
2020-11-08 01:16:00 +0900

    fix format
    Signed-off-by: mathetake <takeshi@tetrate.io>

--- test/extensions/common/wasm/wasm_test.cc
+++ test/extensions/common/wasm/wasm_test.cc
@@ -21,7 +21,6 @@
 #include "openssl/sha.h"
 #include "zlib.h"
 
-
 using Envoy::Server::ServerLifecycleNotifier;
 using StageCallbackWithCompletion =
     Envoy::Server::ServerLifecycleNotifier::StageCallbackWithCompletion;
