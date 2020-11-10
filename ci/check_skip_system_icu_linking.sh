99e5f2b733220136ab75582fa719c5fdb15bb2a4
Dhi Aurrahman <dio@tetrate.io>
2020-11-10 16:19:22 +0000

    Activate check
    Signed-off-by: Dhi Aurrahman <dio@tetrate.io>

--- ci/check_skip_system_icu_linking.sh
+++ ci/check_skip_system_icu_linking.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-# function check {
-#   NEEDS_ICU=$(readelf -d "${1}" | grep 'NEEDED' | grep 'icu')
-#   if [[ "${NEEDS_ICU}" != "" ]]; then
-#      echo "${1} has dependecy to ICU ${NEEDS_ICU}"
-#      exit 1
-#   fi
-# }
+function check {
+  NEEDS_ICU=$(readelf -d "${1}" | grep 'NEEDED' | grep 'icu')
+  if [[ "${NEEDS_ICU}" != "" ]]; then
+     echo "${1} has dependecy to ICU ${NEEDS_ICU}"
+     exit 1
+  fi
+}
 
-ls -la bazel-bin/third_party/icu/googleurl/googleurl_test.runfiles/envoy/third_party/icu
-ls -la bazel-bin/third_party/icu/googleurl/googleurl_test.runfiles/envoy/third_party/icu/googleurl
-ls -la bazel-bin/third_party/icu/googleurl/googleurl_test.runfiles/envoy/third_party/icu/googleurl/googleurl_test
+# Validate the test file.
+TEST_FILE=$(realpath bazel-bin/third_party/icu/googleurl/googleurl_test.runfiles/envoy/third_party/icu/googleurl/googleurl_test)
+check ${TEST_FILE}
