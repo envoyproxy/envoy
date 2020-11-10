c005164652b43f4aec9ae57ad6b778aec7e1db6a
Dhi Aurrahman <dio@tetrate.io>
2020-11-10 12:02:45 +0000

    Debug again
    Signed-off-by: Dhi Aurrahman <dio@tetrate.io>

--- ci/check_skip_system_icu_linking.sh
+++ ci/check_skip_system_icu_linking.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-ls -la
+ls -R bazel-bin/third_party/icu
 
-ls -la bazel-bin/third_party
-
-BINARY_NEEDS_ICU=$(readelf -d bazel-bin/third_party/icu/googleurl/googleurl_test | grep 'NEEDED' | grep 'icu')
-if [[ "${BINARY_NEEDS_ICU}" != "" ]]; then
-  echo "Binary has dependecy to ICU ${BINARY_NEEDS_ICU}"
-  exit 1
-fi
+# BINARY_NEEDS_ICU=$(readelf -d bazel-bin/third_party/icu/googleurl/googleurl_test | grep 'NEEDED' | grep 'icu')
+# if [[ "${BINARY_NEEDS_ICU}" != "" ]]; then
+#   echo "Binary has dependecy to ICU ${BINARY_NEEDS_ICU}"
+#   exit 1
+# fi
