3158e46c1181f86eb3ebb75196455f90163f1a54
Sotiris Nanopoulos <sonanopo@microsoft.com>
2020-11-10 21:27:51 -0800

    fix with runfiles
    Signed-off-by: Sotiris Nanopoulos <sonanopo@microsoft.com>

--- test/exe/outofproc_main_test.py
+++ test/exe/outofproc_main_test.py
@@ -8,44 +8,35 @@ import tempfile
 import signal
 import subprocess
 import unittest
-
+from rules_python.python.runfiles import runfiles
 
 class OutOfProcEnvoyTests(unittest.TestCase):
   """
   We test that Envoy process behaves correctly.
   """
 
-  @staticmethod
-  def find_envoy():
-    """
-    This method locates envoy binary.
-    It's present at ./source/exe/envoy-static (at least for mac/bazel-asan/bazel-tsan),
-    or at ./external/envoy/source/exe/envoy-static (for bazel-compile_time_options).
-    """
-    candidate = os.path.join('.', 'source', 'exe', 'envoy-static')
-    if os.path.isfile(candidate):
-      return candidate
-    candidate = os.path.join('.', 'external', 'envoy', 'source', 'exe', 'envoy-static')
-    if os.path.isfile(candidate):
-      return candidate
-    raise Exception("Could not find Envoy")
-
   @classmethod
   def setUp(self):
-    envoy_path = OutOfProcEnvoyTests.find_envoy()
-    envoy_config = os.path.join('.', 'test/config/integration/google_com_proxy_port_0.yaml')
+    r = runfiles.Create()
+    envoy_path = None
+    if sys.platform == 'win32':
+      envoy_path = r.Rlocation("envoy/source/exe/envoy-static.exe")
+    else:
+      envoy_path = r.Rlocation("envoy/source/exe/envoy-static")
+    if not os.path.isfile(envoy_path):
+      raise Exception("Could not find Envoy")
+
+    envoy_config = r.Rlocation('envoy/test/config/integration/google_com_proxy_port_0.yaml')
     with open(envoy_config, 'r') as f:
       content = f.read()
 
-    content = re.sub("{{ upstream_\d }}", "0", content)
     content = content.replace("{{ ip_any_address }}", "127.0.0.1")
     content = content.replace("{{ dns_lookup_family }}", "V4_ONLY")
-    if os.name == 'nt':
+    if os.name != 'nt':
       content = content.replace("{{ null_device_path }}", "/dev/null")
     else:
       content = content.replace("{{ null_device_path }}", "NUL")
 
-    print(content)
     envoy_config_new = os.path.join(os.getenv('TEST_TMPDIR'), 'google_com_proxy_port_0.yaml')
     with open(envoy_config_new, "w") as f:
       f.write(content)
@@ -63,14 +54,16 @@ class OutOfProcEnvoyTests(unittest.TestCase):
   def tearDown(self):
     self.envoy_proc.kill()
 
-  def test_envoy_closes_on_ctrl_c_event(self):
+  def test_envoy_closes_on_ctrl_break_event(self):
     self.assertTrue(self.envoy_proc.poll() == None)
-    ctr_c_signal = None
+    ctrl_break_signal = None
     if sys.platform == 'win32':
-      ctr_c_signal = signal.CTRL_C_EVENT
+      # On Windows sending a ctrl + c signal is quite tricky
+      # so we test that Envoy stops on ctrl + break.
+      ctrl_break_signal = signal.CTRL_BREAK_EVENT
     else:
-      ctr_c_signal = signal.SIGINT
-    self.envoy_proc.send_signal(ctr_c_signal)
+      ctrl_break_signal = signal.SIGTERM
+    self.envoy_proc.send_signal(ctrl_break_signal)
     time.sleep(2)
     poll = self.envoy_proc.poll()
     self.assertTrue(poll != None)
