5381f744833fc1f0a652f692adc1faa4e6dbce0b
andreyprezotto <andreypp@gmail.com>
2020-11-13 14:03:05 -0300

    Revert "Auth_scope default value if config wasn't set"
    This reverts commit 73d3ce1f343f36690b4a995c885fe0035f176b9e.
    
    Signed-off-by: andreyprezotto <andreypp@gmail.com>

--- source/extensions/filters/http/oauth2/config.cc
+++ source/extensions/filters/http/oauth2/config.cc
@@ -36,8 +36,6 @@ secretsProvider(const envoy::extensions::transport_sockets::tls::v3::SdsSecretCo
     return secret_manager.findStaticGenericSecretProvider(config.name());
   }
 }
-
-const std::string DEFAULT_AUTH_SCOPE = "user";
 } // namespace
 
 Http::FilterFactoryCb OAuth2Config::createFilterFactoryFromProtoTyped(
@@ -69,10 +67,6 @@ Http::FilterFactoryCb OAuth2Config::createFilterFactoryFromProtoTyped(
 
   auto secret_reader = std::make_shared<SDSSecretReader>(
       secret_provider_token_secret, secret_provider_hmac_secret, context.api());
-
-  auto auth_scopes = 
-    (!proto_config.auth_scopes().empty()) ? proto_config.auth_scopes() : DEFAULT_AUTH_SCOPE;
-
   auto config = std::make_shared<FilterConfig>(proto_config, cluster_manager, secret_reader,
                                                context.scope(), stats_prefix);
 
