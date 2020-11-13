5e126b9397d91c957301f42a0158581931f82ae8
andreyprezotto <andreypp@gmail.com>
2020-11-13 14:12:31 -0300

    revert implementation
    Signed-off-by: andreyprezotto <andreypp@gmail.com>

--- source/extensions/filters/http/oauth2/config.cc
+++ source/extensions/filters/http/oauth2/config.cc
@@ -37,7 +37,6 @@ secretsProvider(const envoy::extensions::transport_sockets::tls::v3::SdsSecretCo
   }
 }
 
-const std::string DEFAULT_AUTH_SCOPE = "user";
 } // namespace
 
 Http::FilterFactoryCb OAuth2Config::createFilterFactoryFromProtoTyped(
@@ -69,10 +68,6 @@ Http::FilterFactoryCb OAuth2Config::createFilterFactoryFromProtoTyped(
 
   auto secret_reader = std::make_shared<SDSSecretReader>(
       secret_provider_token_secret, secret_provider_hmac_secret, context.api());
-
-  auto auth_scopes = 
-    (!proto_config.auth_scopes().empty()) ? proto_config.auth_scopes() : DEFAULT_AUTH_SCOPE;
-
   auto config = std::make_shared<FilterConfig>(proto_config, cluster_manager, secret_reader,
                                                context.scope(), stats_prefix);
 
