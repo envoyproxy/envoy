84bb88250406c7a1c493c3a2943a20a218e036ca
Snow Pettersen <snowp@lyft.com>
2020-11-14 00:31:27 +0000

    wkt
    Signed-off-by: Snow Pettersen <snowp@lyft.com>

--- test/common/matcher/matcher_test.cc
+++ test/common/matcher/matcher_test.cc
@@ -18,7 +18,7 @@ struct TestInput : public DataInput {
 struct TestData : public MatchingData {};
 
 TEST(Matcher, MultimapMatcher) {
-  google::protobuf::StringValue string_value;
+  ProtobufWkt::StringValue string_value;
   string_value.set_value("no_match");
 
   envoy::config::core::v3::TypedExtensionConfig no_match;
@@ -33,7 +33,7 @@ TEST(Matcher, MultimapMatcher) {
   EXPECT_EQ(nullptr, result.second->second);
   EXPECT_TRUE(result.second->first.has_value());
 
-  google::protobuf::StringValue result_value;
+  ProtobufWkt::StringValue result_value;
   MessageUtil::unpackTo(result.second->first->typed_config(), result_value);
   EXPECT_TRUE(TestUtility::protoEqual(result_value, string_value));
 }
