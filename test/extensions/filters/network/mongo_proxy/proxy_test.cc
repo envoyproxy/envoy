000f20803f42a32e2dd2484ea049423e24e73ef5
Bill Chung <bill@mercari.com>
2020-11-14 20:46:11 +0900

    rerun test
    Signed-off-by: Bill Chung <bill@mercari.com>

--- test/extensions/filters/network/mongo_proxy/proxy_test.cc
+++ test/extensions/filters/network/mongo_proxy/proxy_test.cc
@@ -650,8 +650,8 @@ TEST_F(MongoProxyFilterTest, ConnectionDestroyRemote) {
 
   EXPECT_CALL(*delay_timer, disableTimer());
   read_filter_callbacks_.connection_.raiseEvent(Network::ConnectionEvent::LocalClose);
-  EXPECT_EQ(1U, store_.counter("test.cx_destroy_local_with_active_rq").value());
   EXPECT_EQ(0U, store_.counter("test.cx_destroy_remote_with_active_rq").value());
+  EXPECT_EQ(1U, store_.counter("test.cx_destroy_local_with_active_rq").value());
 }
 
 } // namespace MongoProxy
