b0f2f880fecfb1e83bf3f4622fa6c3c59a4de5fa
Ryan Northey <ryan@synca.io>
2020-11-14 11:06:38 +0000

    examples/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- examples/tls-sni/verify.sh
+++ examples/tls-sni/verify.sh
@@ -25,15 +25,18 @@ create_self_signed_certs domain3
 bring_up_example
 
 run_log "Query domain1 with curl and tls/sni"
-curl -sk --resolve domain1.example.com:10000:127.0.0.1 https://domain1.example.com:10000 \
+curl -sk --resolve domain1.example.com:10000:127.0.0.1 \
+     https://domain1.example.com:10000 \
     | jq '.os.hostname' | grep http-upstream1
 
 run_log "Query domain2 with curl and tls/sni"
-curl -sk --resolve domain2.example.com:10000:127.0.0.1 https://domain2.example.com:10000 \
+curl -sk --resolve domain2.example.com:10000:127.0.0.1 \
+     https://domain2.example.com:10000 \
     | jq '.os.hostname' | grep http-upstream2
 
 run_log "Query domain3 with curl and tls/sni"
-curl -sk --resolve domain3.example.com:10000:127.0.0.1 https://domain3.example.com:10000 \
+curl -sk --resolve domain3.example.com:10000:127.0.0.1 \
+     https://domain3.example.com:10000 \
     | jq '.os.hostname' | grep http-upstream3
 
 run_log "Query domain1 via Envoy sni client"
