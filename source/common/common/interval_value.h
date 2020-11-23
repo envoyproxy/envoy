f95e89b235fff2145d018eaa8d2bdd0a27f42a23
Alex Konradi <akonradi@google.com>
2020-11-23 15:43:58 -0500

    Mark static functions constexpr
    Signed-off-by: Alex Konradi <akonradi@google.com>

--- source/common/common/interval_value.h
+++ source/common/common/interval_value.h
@@ -14,8 +14,8 @@ template <typename T, T MinValue, T MaxValue> struct Interval {
 // Utility type that represents a value of type T in the given interval.
 template <typename T, typename Interval> class ClosedIntervalValue {
 public:
-  static ClosedIntervalValue min() { return ClosedIntervalValue(Interval::min_value); }
-  static ClosedIntervalValue max() { return ClosedIntervalValue(Interval::max_value); }
+  static constexpr ClosedIntervalValue min() { return ClosedIntervalValue(Interval::min_value); }
+  static constexpr ClosedIntervalValue max() { return ClosedIntervalValue(Interval::max_value); }
 
   constexpr explicit ClosedIntervalValue(T value)
       : value_(std::max<T>(Interval::min_value, std::min<T>(Interval::max_value, value))) {}
