d9beab44a7c2cba32335db773c5fb21a188536e0
Snow Pettersen <snowp@lyft.com>
2020-11-24 13:02:43 +0000

    add custom match coverage
    Signed-off-by: Snow Pettersen <snowp@lyft.com>

--- test/common/matcher/matcher_test.cc
+++ test/common/matcher/matcher_test.cc
@@ -1,12 +1,14 @@
+#include <exception>
 #include <memory>
 
 #include "envoy/config/common/matcher/v3/matcher.pb.validate.h"
 #include "envoy/config/core/v3/extension.pb.h"
+#include "envoy/matcher/matcher.h"
+#include "envoy/registry/registry.h"
 
 #include "common/matcher/matcher.h"
 #include "common/protobuf/utility.h"
 
-#include "envoy/registry/registry.h"
 #include "test/test_common/registry.h"
 #include "test/test_common/utility.h"
 
@@ -66,6 +68,28 @@ public:
   std::string name() const override { return "test_action"; }
 };
 
+class NeverMatch : public InputMatcher {
+public:
+  bool match(absl::optional<absl::string_view>) override { return false; }
+};
+
+class NeverMatchFactory : public InputMatcherFactory {
+public:
+  NeverMatchFactory() : inject_factory_(*this) {}
+
+  InputMatcherPtr createInputMatcher(const Protobuf::Message&) override {
+    return std::make_unique<NeverMatch>();
+  }
+
+  ProtobufTypes::MessagePtr createEmptyConfigProto() override {
+    return std::make_unique<ProtobufWkt::StringValue>();
+  }
+
+  std::string name() const override { return "never_match"; }
+
+  Registry::InjectFactory<InputMatcherFactory> inject_factory_;
+};
+
 class MatcherTest : public ::testing::Test {
 public:
   MatcherTest() : inject_action_(action_factory_) {}
@@ -119,6 +143,44 @@ matcher_tree:
   EXPECT_TRUE(result.match_completed_);
   EXPECT_TRUE(result.on_match_.has_value());
   EXPECT_NE(result.on_match_->action_cb_, nullptr);
+}
+
+TEST_F(MatcherTest, CustomMatcher) {
+  const std::string yaml = R"EOF(
+matcher_list:
+  matchers:
+  - on_match:
+      action:
+        name: test_action
+        typed_config:
+          "@type": type.googleapis.com/google.protobuf.StringValue
+          value: match!!
+    predicate:
+      single_predicate:
+        input:
+          name: inner_input
+          typed_config:
+            "@type": type.googleapis.com/google.protobuf.StringValue
+        custom_match:
+          name: never_match
+          typed_config: {}
+  )EOF";
+
+  envoy::config::common::matcher::v3::Matcher matcher;
+  MessageUtil::loadFromYaml(yaml, matcher, ProtobufMessage::getStrictValidationVisitor());
+
+  TestUtility::validate(matcher);
+
+  MatchTreeFactory<TestData> factory(ProtobufMessage::getStrictValidationVisitor());
+
+  auto inner_factory = TestDataInputFactory("inner_input", "foo");
+  NeverMatchFactory match_factory;
+
+  auto match_tree = factory.create(matcher);
+
+  const auto result = match_tree->match(TestData());
+  EXPECT_TRUE(result.match_completed_);
+  EXPECT_FALSE(result.on_match_.has_value());
 }
 
 TEST_F(MatcherTest, TestAndMatcher) {
