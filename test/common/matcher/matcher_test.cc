3d31a47e7f4fd96b4de0bc94cf62081dfaa76af8
Snow Pettersen <snowp@lyft.com>
2020-11-24 12:50:27 +0000

    improve coverage for proto factory
    Signed-off-by: Snow Pettersen <snowp@lyft.com>

--- test/common/matcher/matcher_test.cc
+++ test/common/matcher/matcher_test.cc
@@ -6,6 +6,7 @@
 #include "common/matcher/matcher.h"
 #include "common/protobuf/utility.h"
 
+#include "envoy/registry/registry.h"
 #include "test/test_common/registry.h"
 #include "test/test_common/utility.h"
 
@@ -20,7 +21,7 @@ class TestDataInput : public DataInput<TestData> {
 public:
   explicit TestDataInput(const std::string& value) : value_(value) {}
 
-  virtual DataInputGetResult get(const TestData&) override {
+  DataInputGetResult get(const TestData&) override {
     return {DataInputGetResult::DataAvailability::AllDataAvailable, value_};
   }
 
@@ -31,7 +32,7 @@ private:
 class TestDataInputFactory : public DataInputFactory<TestData> {
 public:
   TestDataInputFactory(absl::string_view factory_name, absl::string_view data)
-      : factory_name_(std::string(factory_name)), value_(std::string(data)) {}
+      : factory_name_(std::string(factory_name)), value_(std::string(data)), injection_(*this) {}
 
   DataInputPtr<TestData> createDataInput(const Protobuf::Message&) override {
     return std::make_unique<TestDataInput>(value_);
@@ -45,6 +46,7 @@ public:
 private:
   const std::string factory_name_;
   const std::string value_;
+  Registry::InjectFactory<DataInputFactory<TestData>> injection_;
 };
 
 class TestAction : public Action {
@@ -64,7 +66,15 @@ public:
   std::string name() const override { return "test_action"; }
 };
 
-TEST(Matcher, TestMatcher) {
+class MatcherTest : public ::testing::Test {
+public:
+  MatcherTest() : inject_action_(action_factory_) {}
+
+  TestActionFactory action_factory_;
+  Registry::InjectFactory<ActionFactory> inject_action_;
+};
+
+TEST_F(MatcherTest, TestMatcher) {
   const std::string yaml = R"EOF(
 matcher_tree:
   input:
@@ -100,14 +110,9 @@ matcher_tree:
 
   MatchTreeFactory<TestData> factory(ProtobufMessage::getStrictValidationVisitor());
 
-  TestActionFactory action_factory;
-  Registry::InjectFactory<ActionFactory> inject_action(action_factory);
-
   auto outer_factory = TestDataInputFactory("outer_input", "value");
-  Registry::InjectFactory<DataInputFactory<TestData>> inject_outer(outer_factory);
-
   auto inner_factory = TestDataInputFactory("inner_input", "foo");
-  Registry::InjectFactory<DataInputFactory<TestData>> inject_inner(inner_factory);
+
   auto match_tree = factory.create(matcher);
 
   const auto result = match_tree->match(TestData());
@@ -116,7 +121,119 @@ matcher_tree:
   EXPECT_NE(result.on_match_->action_cb_, nullptr);
 }
 
-TEST(Matcher, TestRecursiveMatcher) {
+TEST_F(MatcherTest, TestAndMatcher) {
+  const std::string yaml = R"EOF(
+matcher_tree:
+  input:
+    name: outer_input
+    typed_config:
+      "@type": type.googleapis.com/google.protobuf.StringValue
+  exact_match_map:
+    map:
+      value:
+        matcher:
+          matcher_list:
+            matchers:
+            - on_match:
+                action:
+                  name: test_action
+                  typed_config:
+                    "@type": type.googleapis.com/google.protobuf.StringValue
+                    value: match!!
+              predicate:
+                and_matcher:
+                  predicate:
+                  - single_predicate:
+                      input:
+                        name: inner_input
+                        typed_config:
+                          "@type": type.googleapis.com/google.protobuf.StringValue
+                      value_match:
+                        exact: foo
+                  - single_predicate:
+                      input:
+                        name: inner_input
+                        typed_config:
+                          "@type": type.googleapis.com/google.protobuf.StringValue
+                      value_match:
+                        exact: foo
+  )EOF";
+
+  envoy::config::common::matcher::v3::Matcher matcher;
+  MessageUtil::loadFromYaml(yaml, matcher, ProtobufMessage::getStrictValidationVisitor());
+
+  TestUtility::validate(matcher);
+
+  MatchTreeFactory<TestData> factory(ProtobufMessage::getStrictValidationVisitor());
+
+  auto outer_factory = TestDataInputFactory("outer_input", "value");
+  auto inner_factory = TestDataInputFactory("inner_input", "foo");
+
+  auto match_tree = factory.create(matcher);
+
+  const auto result = match_tree->match(TestData());
+  EXPECT_TRUE(result.match_completed_);
+  EXPECT_TRUE(result.on_match_.has_value());
+  EXPECT_NE(result.on_match_->action_cb_, nullptr);
+}
+
+TEST_F(MatcherTest, TestOrMatcher) {
+  const std::string yaml = R"EOF(
+matcher_tree:
+  input:
+    name: outer_input
+    typed_config:
+      "@type": type.googleapis.com/google.protobuf.StringValue
+  exact_match_map:
+    map:
+      value:
+        matcher:
+          matcher_list:
+            matchers:
+            - on_match:
+                action:
+                  name: test_action
+                  typed_config:
+                    "@type": type.googleapis.com/google.protobuf.StringValue
+                    value: match!!
+              predicate:
+                or_matcher:
+                  predicate:
+                  - single_predicate:
+                      input:
+                        name: inner_input
+                        typed_config:
+                          "@type": type.googleapis.com/google.protobuf.StringValue
+                      value_match:
+                        exact: foo
+                  - single_predicate:
+                      input:
+                        name: inner_input
+                        typed_config:
+                          "@type": type.googleapis.com/google.protobuf.StringValue
+                      value_match:
+                        exact: foo
+  )EOF";
+
+  envoy::config::common::matcher::v3::Matcher matcher;
+  MessageUtil::loadFromYaml(yaml, matcher, ProtobufMessage::getStrictValidationVisitor());
+
+  TestUtility::validate(matcher);
+
+  MatchTreeFactory<TestData> factory(ProtobufMessage::getStrictValidationVisitor());
+
+  auto outer_factory = TestDataInputFactory("outer_input", "value");
+  auto inner_factory = TestDataInputFactory("inner_input", "foo");
+
+  auto match_tree = factory.create(matcher);
+
+  const auto result = match_tree->match(TestData());
+  EXPECT_TRUE(result.match_completed_);
+  EXPECT_TRUE(result.on_match_.has_value());
+  EXPECT_NE(result.on_match_->action_cb_, nullptr);
+}
+
+TEST_F(MatcherTest, TestRecursiveMatcher) {
   const std::string yaml = R"EOF(
 matcher_list:
   matchers:
@@ -155,14 +272,9 @@ matcher_list:
 
   MatchTreeFactory<TestData> factory(ProtobufMessage::getStrictValidationVisitor());
 
-  TestActionFactory action_factory;
-  Registry::InjectFactory<ActionFactory> inject_action(action_factory);
-
   auto outer_factory = TestDataInputFactory("outer_input", "value");
-  Registry::InjectFactory<DataInputFactory<TestData>> inject_outer(outer_factory);
-
   auto inner_factory = TestDataInputFactory("inner_input", "foo");
-  Registry::InjectFactory<DataInputFactory<TestData>> inject_inner(inner_factory);
+
   auto match_tree = factory.create(matcher);
 
   const auto result = match_tree->match(TestData());
