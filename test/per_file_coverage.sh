dd219055896db9780c02a29c6d796176a762ec15
Snow Pettersen <snowp@lyft.com>
2020-12-02 17:35:07 +0000

    allow reduced coverage
    Signed-off-by: Snow Pettersen <snowp@lyft.com>

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -21,6 +21,7 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/common/thread_local:95.7"
 "source/common/crypto:0.0"
 "source/common/common:96.1"
+"source/common/matcher:92.8"
 "source/common/common/posix:94.1"
 "source/common/signal:83.1" # Death tests don't report LCOV
 "source/common/watchdog:42.9" # Death tests don't report LCOV
