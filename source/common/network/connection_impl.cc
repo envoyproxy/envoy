90f346344fcaee036d8e5b1542a7450ab7f034da
Antonio Vicente <avd@google.com>
2020-12-04 13:56:55 -0500

    fix clang-tidy
    Signed-off-by: Antonio Vicente <avd@google.com>

--- source/common/network/connection_impl.cc
+++ source/common/network/connection_impl.cc
@@ -72,7 +72,7 @@ ConnectionImpl::ConnectionImpl(Event::Dispatcher& dispatcher, ConnectionSocketPt
   // We never ask for both early close and read at the same time. If we are reading, we want to
   // consume all available data.
   file_event_ = dispatcher_.createFileEvent(
-      ioHandle().fd(), [this](uint32_t events) -> void { onFileEvent(events); }, trigger,
+      socket_->ioHandle().fd(), [this](uint32_t events) -> void { onFileEvent(events); }, trigger,
       Event::FileReadyType::Read | Event::FileReadyType::Write);
 
   transport_socket_->setTransportSocketCallbacks(*this);
