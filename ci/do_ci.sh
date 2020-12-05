c7851731a2ce74d0c83bd129b9938c3f9c9c64ca
Lizan Zhou <lizan@tetrate.io>
2020-12-05 02:19:18 -0800

    fix
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -285,7 +285,7 @@ elif [[ "$CI_TARGET" == "bazel.compile_time_options" ]]; then
     "--define" "use_new_codecs_in_integration_tests=false"
     "--define" "tcmalloc=gperftools"
     "--define" "zlib=ng"
-    "--//source/extensions/filters/http/kill_request:enabled"
+    "--@envoy//source/extensions/filters/http/kill_request:enabled"
     "--test_env=ENVOY_HAS_EXTRA_EXTENSIONS=true")
 
   ENVOY_STDLIB="${ENVOY_STDLIB:-libstdc++}"
