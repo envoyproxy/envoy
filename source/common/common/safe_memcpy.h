89e53c2f714107c0955a250204fcd20be87e0854
grial1 <grial@google.com>
2020-12-06 10:26:45 +0000

    format fixed
    Signed-off-by: grial1 <grial@google.com>

--- source/common/common/safe_memcpy.h
+++ source/common/common/safe_memcpy.h
@@ -14,13 +14,15 @@ template <typename T1, typename T2> inline void safeMemcpy(T1* dst, T2* src) {
 
 /**
  * @brief Copies src to dst based on the size of dst
- * @note  Sizes are not compared, so ensure the src is of size sizeof(*(dst)) before proceeding to call safeMemcpyUnsafeSrc
+ * @note  Sizes are not compared, so ensure the src is of size sizeof(*(dst)) before proceeding to
+ * call safeMemcpyUnsafeSrc
  */
 #define safeMemcpyUnsafeSrc(dst, src) memcpy(dst, src, sizeof(*(dst)));
 
 /**
  * @brief Copies src to dst based on the size of src
- * @note  Sizes are not compared, so ensure the dst is of size sizeof(*(src)) before proceeding to call safeMemcpyUnsafeDst
+ * @note  Sizes are not compared, so ensure the dst is of size sizeof(*(src)) before proceeding to
+ * call safeMemcpyUnsafeDst
  */
 #define safeMemcpyUnsafeDst(dst, src) memcpy(dst, src, sizeof(*(src)));
 
