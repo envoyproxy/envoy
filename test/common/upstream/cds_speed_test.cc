db19c30a2c68c1dbe314c1702a1bad0cb0cdc084
Phil Genera <pgenera@google.com>
2020-12-07 17:13:12 +0000

    respond to review comments; split out v2 tests
    Signed-off-by: Phil Genera <pgenera@google.com>

--- test/common/upstream/cds_speed_test.cc
+++ test/common/upstream/cds_speed_test.cc
@@ -2,6 +2,7 @@
 // quiescent system with disabled cstate power management.
 
 #include "envoy/config/cluster/v3/cluster.pb.h"
+#include "envoy/config/cluster/v3/cluster.pb.validate.h"
 #include "envoy/config/core/v3/health_check.pb.h"
 #include "envoy/config/endpoint/v3/endpoint.pb.h"
 #include "envoy/config/endpoint/v3/endpoint_components.pb.h"
@@ -13,11 +14,13 @@
 #include "common/config/utility.h"
 #include "common/singleton/manager_impl.h"
 #include "common/upstream/eds.h"
+#include "common/upstream/static_cluster.h"
 
 #include "server/transport_socket_config_impl.h"
 
 #include "test/benchmark/main.h"
 #include "test/common/upstream/utility.h"
+#include "test/config/utility.h"
 #include "test/mocks/local_info/mocks.h"
 #include "test/mocks/protobuf/mocks.h"
 #include "test/mocks/runtime/mocks.h"
@@ -40,82 +43,60 @@ class CdsSpeedTest {
 public:
   CdsSpeedTest(State& state, bool v2_config)
       : state_(state), v2_config_(v2_config),
-        type_url_(v2_config_
-                      ? "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment"
-                      : "type.googleapis.com/envoy.config.endpoint.v3.ClusterLoadAssignment"),
+        type_url_(v2_config_ ? "type.googleapis.com/envoy.api.v2.Cluster"
+                             : "type.googleapis.com/envoy.config.cluster.v3.Cluster"),
         subscription_stats_(Config::Utility::generateStats(stats_)),
         api_(Api::createApiForTest(stats_)), async_client_(new Grpc::MockAsyncClient()),
         grpc_mux_(new Config::GrpcMuxImpl(
             local_info_, std::unique_ptr<Grpc::MockAsyncClient>(async_client_), dispatcher_,
             *Protobuf::DescriptorPool::generated_pool()->FindMethodByName(
-                "envoy.service.endpoint.v3.EndpointDiscoveryService.StreamEndpoints"),
+                "envoy.service.cluster.v3.ClusterDiscoveryService.StreamClusters"),
             envoy::config::core::v3::ApiVersion::AUTO, random_, stats_, {}, true)) {
 
-    resetCluster(R"EOF(
-      name: name
-      connect_timeout: 0.25s
-      type: EDS
-      eds_cluster_config:
-        service_name: fare
-        eds_config:
-          api_config_source:
-            cluster_names:
-            - eds
-            refresh_delay: 1s
-    )EOF",
-                 Envoy::Upstream::Cluster::InitializePhase::Secondary);
+    resetCluster();
 
-    EXPECT_CALL(*cm_.subscription_factory_.subscription_, start(_, _));
     cluster_->initialize([this] { initialized_ = true; });
     EXPECT_CALL(*async_client_, startRaw(_, _, _, _)).WillOnce(testing::Return(&async_stream_));
     subscription_->start({"fare"});
   }
 
-  void resetCluster(const std::string& yaml_config, Cluster::InitializePhase initialize_phase) {
+  void resetCluster() {
     local_info_.node_.mutable_locality()->set_zone("us-east-1a");
-    eds_cluster_ = parseClusterFromV3Yaml(yaml_config);
+    static_cluster_ = ConfigHelper::buildStaticCluster("cluster", 1024, "127.0.0.1");
     Envoy::Stats::ScopePtr scope = stats_.createScope(fmt::format(
-        "cluster.{}.",
-        eds_cluster_.alt_stat_name().empty() ? eds_cluster_.name() : eds_cluster_.alt_stat_name()));
+        "cluster.{}.", static_cluster_.alt_stat_name().empty() ? static_cluster_.name()
+                                                               : static_cluster_.alt_stat_name()));
     Envoy::Server::Configuration::TransportSocketFactoryContextImpl factory_context(
         admin_, ssl_context_manager_, *scope, cm_, local_info_, dispatcher_, stats_,
         singleton_manager_, tls_, validation_visitor_, *api_);
-    cluster_ = std::make_shared<EdsClusterImpl>(eds_cluster_, runtime_, factory_context,
-                                                std::move(scope), false);
-    EXPECT_EQ(initialize_phase, cluster_->initializePhase());
-    eds_callbacks_ = cm_.subscription_factory_.callbacks_;
+    cluster_ = std::make_shared<StaticClusterImpl>(static_cluster_, runtime_, factory_context,
+                                                   std::move(scope), false);
+    EXPECT_EQ(Envoy::Upstream::Cluster::InitializePhase::Primary, cluster_->initializePhase());
+    callbacks_ = cm_.subscription_factory_.callbacks_;
     subscription_ = std::make_unique<Config::GrpcSubscriptionImpl>(
-        grpc_mux_, *eds_callbacks_, resource_decoder_, subscription_stats_, type_url_, dispatcher_,
+        grpc_mux_, *callbacks_, resource_decoder_, subscription_stats_, type_url_, dispatcher_,
         std::chrono::milliseconds(), false);
   }
 
   void clusterHelper(bool ignore_unknown_dynamic_fields, size_t num_clusters) {
-
     auto response = std::make_unique<envoy::service::discovery::v3::DiscoveryResponse>();
     response->set_type_url(type_url_);
     response->set_version_info(fmt::format("version-{}", version_++));
 
-    // make a pile of dynamic clusters and add them to the response
+    // make a pile of static clusters and add them to the response
     for (size_t i = 0; i < num_clusters; ++i) {
-      envoy::config::endpoint::v3::ClusterLoadAssignment cluster_load_assignment;
-      cluster_load_assignment.set_cluster_name("fare_" + std::to_string(i));
-
-      auto* endpoints = cluster_load_assignment.add_endpoints();
-      endpoints->set_priority(1);
-      auto* locality = endpoints->mutable_locality();
-      locality->set_region("region");
-      locality->set_zone("zone");
-      locality->set_sub_zone("sub_zone");
+      envoy::config::cluster::v3::Cluster cluster = ConfigHelper::buildStaticCluster(
+          "cluster_" + std::to_string(i), i % 60000, "10.0.1." + std::to_string(i / 60000));
 
       auto* resource = response->mutable_resources()->Add();
-      resource->PackFrom(cluster_load_assignment);
+      resource->PackFrom(cluster);
       if (v2_config_) {
-        RELEASE_ASSERT(resource->type_url() ==
-                           "type.googleapis.com/envoy.config.endpoint.v3.ClusterLoadAssignment",
-                       "");
-        resource->set_type_url("type.googleapis.com/envoy.api.v2.ClusterLoadAssignment");
+        RELEASE_ASSERT(
+            resource->type_url() == "type.googleapis.com/envoy.config.cluster.v3.Cluster", "");
+        resource->set_type_url("type.googleapis.com/envoy.api.v2.Cluster");
       }
     }
+
     validation_visitor_.setSkipValidation(ignore_unknown_dynamic_fields);
 
     state_.SetComplexityN(num_clusters);
@@ -133,13 +114,13 @@ public:
   Stats::IsolatedStoreImpl stats_;
   Config::SubscriptionStats subscription_stats_;
   Ssl::MockContextManager ssl_context_manager_;
-  envoy::config::cluster::v3::Cluster eds_cluster_;
+  envoy::config::cluster::v3::Cluster static_cluster_;
   NiceMock<MockClusterManager> cm_;
   NiceMock<Event::MockDispatcher> dispatcher_;
-  EdsClusterImplSharedPtr cluster_;
-  Config::SubscriptionCallbacks* eds_callbacks_{};
-  Config::OpaqueResourceDecoderImpl<envoy::config::endpoint::v3::ClusterLoadAssignment>
-      resource_decoder_{validation_visitor_, "cluster_name"};
+  ClusterImplBaseSharedPtr cluster_;
+  Config::SubscriptionCallbacks* callbacks_{};
+  Config::OpaqueResourceDecoderImpl<envoy::config::cluster::v3::Cluster> resource_decoder_{
+      validation_visitor_, "name"};
   NiceMock<Random::MockRandomGenerator> random_;
   NiceMock<Runtime::MockLoader> runtime_;
   NiceMock<LocalInfo::MockLocalInfo> local_info_;
@@ -157,20 +138,36 @@ public:
 } // namespace Upstream
 } // namespace Envoy
 
-static void addClusters(State& state) {
-  Envoy::Upstream::CdsSpeedTest speed_test(state, state.range(0));
+static void addV3Clusters(State& state) {
+  Envoy::Upstream::CdsSpeedTest speed_test(state, false);
   // if we've been instructed to skip tests, only run once no matter the argument:
-  const uint32_t num_clusters = skipExpensiveBenchmarks() ? 1 : state.range(2);
+  const uint32_t num_clusters = skipExpensiveBenchmarks() ? 1 : state.range(1);
   for (auto _ : state) { // NOLINT(clang-analyzer-deadcode.DeadStores)
     // timing is resumed within the helper:
     state.PauseTiming();
-    speed_test.clusterHelper(state.range(1), num_clusters);
+    speed_test.clusterHelper(state.range(0), num_clusters);
     state.ResumeTiming();
   }
 }
 
-BENCHMARK(addClusters)
-    ->Ranges({{false, true}, {false, true}, {64, 100000}})
+BENCHMARK(addV3Clusters)
+    ->Ranges({{false, true}, {64, 100000}})
+    ->Unit(benchmark::kMillisecond)
+    ->Complexity();
+
+static void addV2Clusters(State& state) {
+  // this true is the only difference from addV3clusters above
+  Envoy::Upstream::CdsSpeedTest speed_test(state, true);
+  const uint32_t num_clusters = skipExpensiveBenchmarks() ? 1 : state.range(1);
+  for (auto _ : state) { // NOLINT(clang-analyzer-deadcode.DeadStores)
+    state.PauseTiming();
+    speed_test.clusterHelper(state.range(0), num_clusters);
+    state.ResumeTiming();
+  }
+}
+
+BENCHMARK(addV2Clusters)
+    ->Ranges({{false, true}, {64, 100000}})
     ->Unit(benchmark::kMillisecond)
     ->Complexity();
 
