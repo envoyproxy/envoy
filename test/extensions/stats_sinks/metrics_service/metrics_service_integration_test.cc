b61ae9328c7ad2f1b8f5114a037720b7950f8269
Jose Nino <jnino@lyft.com>
2020-12-08 12:22:34 -0800

    add back
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- test/extensions/stats_sinks/metrics_service/metrics_service_integration_test.cc
+++ test/extensions/stats_sinks/metrics_service/metrics_service_integration_test.cc
@@ -66,6 +66,7 @@ public:
 
   ABSL_MUST_USE_RESULT
   AssertionResult waitForMetricsRequest() {
+    bool known_summary_exists = false;
     bool known_histogram_exists = false;
     bool known_counter_exists = false;
     bool known_gauge_exists = false;
@@ -74,7 +75,8 @@ public:
     // required stats are flushed.
     // TODO(ramaraochavali): Figure out a more robust way to find out all required stats have been
     // flushed.
-    while (!(known_counter_exists && known_gauge_exists && known_histogram_exists)) {
+    while (!(known_counter_exists && known_gauge_exists && known_summary_exists &&
+             known_histogram_exists)) {
       envoy::service::metrics::v3::StreamMetricsMessage request_msg;
       VERIFY_ASSERTION(metrics_service_request_->waitForGrpcMessage(*dispatcher_, request_msg));
       EXPECT_EQ("POST", metrics_service_request_->headers().getMethodValue());
@@ -100,6 +102,7 @@ public:
         }
         if (metrics_family.name() == "cluster.cluster_0.upstream_rq_time" &&
             metrics_family.type() == ::io::prometheus::client::MetricType::SUMMARY) {
+          known_summary_exists = true;
           Stats::HistogramStatisticsImpl empty_statistics;
           EXPECT_EQ(metrics_family.metric(0).summary().quantile_size(),
                     empty_statistics.supportedQuantiles().size());
@@ -116,13 +119,15 @@ public:
           EXPECT_EQ(previous_time_stamp, metrics_family.metric(0).timestamp_ms());
         }
         previous_time_stamp = metrics_family.metric(0).timestamp_ms();
-        if (known_counter_exists && known_gauge_exists && known_histogram_exists) {
+        if (known_counter_exists && known_gauge_exists && known_summary_exists &&
+            known_histogram_exists) {
           break;
         }
       }
     }
     EXPECT_TRUE(known_counter_exists);
     EXPECT_TRUE(known_gauge_exists);
+    EXPECT_TRUE(known_summary_exists);
     EXPECT_TRUE(known_histogram_exists);
 
     return AssertionSuccess();
