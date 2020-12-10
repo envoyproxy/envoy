bf9691402f23f61caf8be7b7abb0429b079f3da5
alyssawilk <alyssar@chromium.org>
2020-12-10 17:03:14 -0500

    tcp_proxy: logs consistently (#14368)
    moving the lone ENVOY_LOG in tcp proxy session to ENVOY_CONN_LOG
    
    Risk Level: low
    Testing: tests pass
    Docs Changes: n/a
    Release Notes: n/a
    
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- source/common/tcp_proxy/tcp_proxy.cc
+++ source/common/tcp_proxy/tcp_proxy.cc
@@ -632,8 +632,8 @@ void Filter::onUpstreamConnection() {
       Upstream::Outlier::Result::LocalOriginConnectSuccessFinal);
 
   getStreamInfo().setRequestedServerName(read_callbacks_->connection().requestedServerName());
-  ENVOY_LOG(debug, "TCP:onUpstreamEvent(), requestedServerName: {}",
-            getStreamInfo().requestedServerName());
+  ENVOY_CONN_LOG(debug, "TCP:onUpstreamEvent(), requestedServerName: {}",
+                 read_callbacks_->connection(), getStreamInfo().requestedServerName());
 
   if (config_->idleTimeout()) {
     // The idle_timer_ can be moved to a Drainer, so related callbacks call into
