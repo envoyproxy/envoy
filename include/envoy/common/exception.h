d77d349b983273ab58848d469c9c3f94b69e20c3
chaoqin-li1123 <chaoqin@uchicago.edu>
2020-12-12 20:21:27 -0600

    macros and helper function
    Signed-off-by: chaoqin-li1123 <chaoqin@uchicago.edu>

--- include/envoy/common/exception.h
+++ include/envoy/common/exception.h
@@ -2,13 +2,26 @@
 
 #include <stdexcept>
 #include <string>
+#include <thread>
 
 namespace Envoy {
 /**
  * Base class for all envoy exceptions.
  */
+#define envoy_try \
+assert(isMainThread()); try 
+
 class EnvoyException : public std::runtime_error {
 public:
   EnvoyException(const std::string& message) : std::runtime_error(message) {}
 };
+
+
+static bool isMainThread() {
+    static std::thread::id main_thread_id = std::this_thread::get_id();
+    return main_thread_id == std::this_thread::get_id(); 
+}
+
+
+
 } // namespace Envoy
