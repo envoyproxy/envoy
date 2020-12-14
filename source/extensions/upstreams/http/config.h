73c874e519b13e446234081854dbbae78aecf2f6
Alyssa Wilk <alyssar@chromium.org>
2020-12-14 10:28:54 -0500

    comments
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- source/extensions/upstreams/http/config.h
+++ source/extensions/upstreams/http/config.h
@@ -12,6 +12,7 @@
 #include "envoy/extensions/upstreams/http/v3/http_protocol_options.pb.validate.h"
 #include "envoy/http/filter.h"
 #include "envoy/server/filter_config.h"
+#include "envoy/server/transport_socket_config.h"
 
 #include "common/common/logger.h"
 #include "common/protobuf/message_validator_impl.h"
@@ -45,12 +46,12 @@ public:
 
 class ProtocolOptionsConfigFactory : public Server::Configuration::ProtocolOptionsFactory {
 public:
-  Upstream::ProtocolOptionsConfigConstSharedPtr
-  createProtocolOptionsConfig(const Protobuf::Message& config,
-                              Server::Configuration::ProtocolOptionsFactoryContext&) override {
-    const envoy::extensions::upstreams::http::v3::HttpProtocolOptions& typed_config =
-        *dynamic_cast<const envoy::extensions::upstreams::http::v3::HttpProtocolOptions*>(&config);
-    MessageUtil::validate(typed_config, ProtobufMessage::getStrictValidationVisitor());
+  Upstream::ProtocolOptionsConfigConstSharedPtr createProtocolOptionsConfig(
+      const Protobuf::Message& config,
+      Server::Configuration::ProtocolOptionsFactoryContext& context) override {
+    const auto& typed_config = MessageUtil::downcastAndValidate<
+        const envoy::extensions::upstreams::http::v3::HttpProtocolOptions&>(
+        config, context.messageValidationVisitor());
     return std::make_shared<ProtocolOptionsConfigImpl>(typed_config);
   }
   std::string category() const override { return "envoy.upstream_options"; }
