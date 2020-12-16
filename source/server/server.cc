bacbc44572a7fc30e24b1c241028433ea0c2e2de
chaoqin-li1123 <chaoqin@uchicago.edu>
2020-12-15 22:42:26 -0600

    move main thread utility
    Signed-off-by: chaoqin-li1123 <chaoqin@uchicago.edu>

--- source/server/server.cc
+++ source/server/server.cc
@@ -809,7 +809,7 @@ InstanceImpl::registerCallback(Stage stage, StageCallbackWithCompletion callback
 }
 
 void InstanceImpl::notifyCallbacksForStage(Stage stage, Event::PostCb completion_cb) {
-  ASSERT(MainThreadSingleton::get().isMainThread());
+  ASSERT(ThreadLocal::MainThreadSingleton::get().isMainThread());
   const auto it = stage_callbacks_.find(stage);
   if (it != stage_callbacks_.end()) {
     for (const StageCallback& callback : it->second) {
