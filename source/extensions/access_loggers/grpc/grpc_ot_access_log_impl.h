090a926ccc47b7e2462c145c236a1dce6e4834ad
Itamar Kaminski <itamark@google.com>
2020-12-21 19:21:35 +0200

    OTLP gRPC access logger (skeleton)
    Signed-off-by: Itamar Kaminski <itamark@google.com>

---
+++ source/extensions/access_loggers/grpc/grpc_ot_access_log_impl.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <memory>
+
+#include "opentelemetry/proto/collector/logs/v1/logs_service.pb.h"
+#include "opentelemetry/proto/common/v1/common.pb.h"
+#include "opentelemetry/proto/logs/v1/logs.pb.h"
+#include "opentelemetry/proto/resource/v1/resource.pb.h"
+
+#include "envoy/event/dispatcher.h"
+#include "envoy/extensions/access_loggers/grpc/v3/als.pb.h"
+#include "envoy/grpc/async_client_manager.h"
+#include "envoy/thread_local/thread_local.h"
+
+#include "extensions/access_loggers/common/grpc_access_logger.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace AccessLoggers {
+namespace GrpcCommon {
+
+// Note: OpenTelemetry protos are extra flexible and used also in the OT collector for batching and
+// so forth. As a result, some fields are repeated, but for our use case we assume the following
+// structure:
+// ExportLogsServiceRequest -> (single) ResourceLogs -> (single) InstrumentationLibrary ->
+// (repeated) LogRecord.
+class GrpcOpenTelemetryAccessLoggerImpl
+    : public Common::GrpcAccessLogger<
+          opentelemetry::proto::logs::v1::LogRecord,
+          opentelemetry::proto::logs::v1::ResourceLogs /*TCP*/,
+          opentelemetry::proto::collector::logs::v1::ExportLogsServiceRequest,
+          opentelemetry::proto::collector::logs::v1::ExportLogsServiceResponse> {
+public:
+  GrpcOpenTelemetryAccessLoggerImpl(Grpc::RawAsyncClientPtr&& client,
+                                    std::chrono::milliseconds buffer_flush_interval_msec,
+                                    uint64_t max_buffer_size_bytes, Event::Dispatcher& dispatcher,
+                                    Stats::Scope& scope,
+                                    envoy::config::core::v3::ApiVersion transport_api_version);
+
+private:
+  // Explanation.
+  static InstrumentationLibraryLogs initRoot() const;
+  // Extensions::AccessLoggers::GrpcCommon::GrpcAccessLogger
+  void addEntry(opentelemetry::proto::logs::v1::LogRecord&& entry) override;
+  void addEntry(opentelemetry::proto::logs::v1::ResourceLogs&& entry) override;
+  bool isEmpty() override;
+  void initMessage() override;
+
+  // Root for adding new log entries (see comment above about structure).
+  const opentelemetry::proto::logs::v1::InstrumentationLibraryLogs root_;
+};
+
+class GrpcOpenTelemetryAccessLoggerCacheImpl
+    : public Common::GrpcAccessLoggerCache<
+          GrpcOpenTelemetryAccessLoggerImpl,
+          envoy::extensions::access_loggers::grpc::v3::CommonGrpcAccessLogConfig> {
+public:
+  // Inherited constructor.
+  using Common::GrpcAccessLoggerCache<GrpcOpenTelemetryAccessLoggerImpl,
+                                      envoy::extensions::access_loggers::grpc::v3::
+                                          CommonGrpcAccessLogConfig>::GrpcAccessLoggerCache;
+
+private:
+  // Common::GrpcAccessLoggerCache
+  GrpcOpenTelemetryAccessLoggerImpl::SharedPtr
+  createLogger(const envoy::extensions::access_loggers::grpc::v3::CommonGrpcAccessLogConfig& config,
+               Grpc::RawAsyncClientPtr&& client,
+               std::chrono::milliseconds buffer_flush_interval_msec, uint64_t max_buffer_size_bytes,
+               Event::Dispatcher& dispatcher, Stats::Scope& scope) override;
+};
+
+/**
+ * Aliases for class interfaces for mock definitions.
+ */
+using GrpcAccessLogger = GrpcOpenTelemetryAccessLoggerImpl::Interface;
+using GrpcAccessLoggerSharedPtr = GrpcAccessLogger::SharedPtr;
+
+using GrpcAccessLoggerCache = GrpcOpenTelemetryAccessLoggerCacheImpl::Interface;
+using GrpcAccessLoggerCacheSharedPtr = GrpcAccessLoggerCache::SharedPtr;
+
+} // namespace GrpcCommon
+} // namespace AccessLoggers
+} // namespace Extensions
+} // namespace Envoy
