87a7b37b452b2411135727229dd9b3f27fe2d956
Antonio Vicente <avd@google.com>
2020-12-29 01:46:47 -0500

    move guarddog timer enable to dispatcher thread
    Signed-off-by: Antonio Vicente <avd@google.com>

--- source/server/guarddog_impl.cc
+++ source/server/guarddog_impl.cc
@@ -218,10 +218,13 @@ void GuardDogImpl::stopWatching(WatchDogSharedPtr wd) {
 void GuardDogImpl::start(Api::Api& api) {
   Thread::LockGuard guard(mutex_);
   // See comments in WorkerImpl::start for the naming convention.
-  loop_timer_->enableTimer(std::chrono::milliseconds(0));
   Thread::Options options{absl::StrCat("dog:", dispatcher_->name())};
   thread_ = api.threadFactory().createThread(
-      [this]() -> void { dispatcher_->run(Event::Dispatcher::RunType::RunUntilExit); }, options);
+      [this]() -> void {
+        loop_timer_->enableTimer(std::chrono::milliseconds(0));
+        dispatcher_->run(Event::Dispatcher::RunType::RunUntilExit);
+      },
+      options);
 }
 
 void GuardDogImpl::stop() {
