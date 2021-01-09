d1fc7ddacfb38484f28a81b6f230718f29052e33
Antonio Vicente <avd@google.com>
2021-01-08 19:51:14 -0500

    Allow ENVOY_BUILD_TARGET to contain multiple targets
    Signed-off-by: Antonio Vicente <avd@google.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -117,7 +117,7 @@ function bazel_binary_build() {
   # This is a workaround for https://github.com/bazelbuild/bazel/issues/11834
   [[ -n "${ENVOY_RBE}" ]] && rm -rf bazel-bin/"${ENVOY_BIN}"*
 
-  bazel build "${BAZEL_BUILD_OPTIONS[@]}" -c "${COMPILE_TYPE}" "${ENVOY_BUILD_TARGET}" ${CONFIG_ARGS}
+  bazel build "${BAZEL_BUILD_OPTIONS[@]}" -c "${COMPILE_TYPE}" ${ENVOY_BUILD_TARGET} ${CONFIG_ARGS}
   collect_build_profile "${BINARY_TYPE}"_build
 
   # Copy the built envoy binary somewhere that we can access outside of the
