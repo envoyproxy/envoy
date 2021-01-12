dffb8e3b93e9c0ce004dbf28f98634c5b7a86869
Martin Matusiak <numerodix@gmail.com>
2021-01-12 13:06:39 +1100

    router: Remove envoy.reloadable_features.consume_all_retry_headers
    This patch removes the
    envoy.reloadable_features.consume_all_retry_headers runtime flag.
    
    Signed-off-by: Martin Matusiak <numerodix@gmail.com>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -61,7 +61,6 @@ constexpr const char* runtime_features[] = {
     "envoy.reloadable_features.allow_500_after_100",
     "envoy.reloadable_features.allow_preconnect",
     "envoy.reloadable_features.allow_response_for_timeout",
-    "envoy.reloadable_features.consume_all_retry_headers",
     "envoy.reloadable_features.check_ocsp_policy",
     "envoy.reloadable_features.disable_tls_inspector_injection",
     "envoy.reloadable_features.disallow_unbounded_access_logs",
