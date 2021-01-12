f006801152f7b6cb89f3e70b7735454f36e0b7d4
Asra Ali <asraa@google.com>
2021-01-12 14:18:39 -0500

    update link
    Signed-off-by: Asra Ali <asraa@google.com>

--- source/extensions/transport_sockets/tls/context_impl.cc
+++ source/extensions/transport_sockets/tls/context_impl.cc
@@ -471,7 +471,7 @@ ContextImpl::ContextImpl(Stats::Scope& scope, const Envoy::Ssl::ContextConfig& c
   }
 
   // Add hardcoded cipher suites from the TLS 1.3 spec:
-  // https://tools.ietf.org/html/rfc8446
+  // https://tools.ietf.org/html/rfc8446#appendix-B.4
   stat_name_set_->rememberBuiltins({"TLS_AES_128_GCM_SHA256", "TLS_AES_256_GCM_SHA384",
                                     "TLS_AES_128_CCM_SHA256", "TLS_AES_256_CCM_8_SHA256",
                                     "TLS_CHACHA20_POLY1305_SHA256"});
