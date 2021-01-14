09d112c68423eb2989641bf33efc9102d91e9794
htuch <htuch@users.noreply.github.com>
2021-01-14 13:34:41 -0500

    xdstp: LDS glob collection support. (#14311)
    This patch introduces support for LDS xdstp:// collection URLs for glob collections over ADS. Context
    parameters are currently computed from node and resource URLs. Followup PRs will add support for
    other collection types (CDS, SRDS), non-ADS, provide dynamic context parameter update, extend support to
    singleton resources and then other xdstp:// features (list collections, redirects, alternatives,
    etc.)
    
    Part of #11264.
    
    Risk level: Low (opt-in)
    Testing: ADS integration test added. Various unit tests following implementation.
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- source/common/secret/sds_api.cc
+++ source/common/secret/sds_api.cc
@@ -33,7 +33,7 @@ SdsApi::SdsApi(envoy::config::core::v3::ConfigSource sds_config, absl::string_vi
   const auto resource_name = getResourceName();
   // This has to happen here (rather than in initialize()) as it can throw exceptions.
   subscription_ = subscription_factory_.subscriptionFromConfigSource(
-      sds_config_, Grpc::Common::typeUrl(resource_name), *scope_, *this, resource_decoder_);
+      sds_config_, Grpc::Common::typeUrl(resource_name), *scope_, *this, resource_decoder_, false);
 }
 
 void SdsApi::resolveDataSource(const FileContentMap& files,
