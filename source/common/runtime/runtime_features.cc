7653adfd88379586cc55518b0548ba88fbea6dae
alyssawilk <alyssar@chromium.org>
2021-01-14 10:30:39 -0500

    hcm: removing envoy.reloadable_features.early_errors_via_hcm #14641 (#14684)
    Risk Level: Low (removal of deprecated disabled code)
    Testing: n/a
    Docs Changes: n/a
    Release Notes: inline
    Fixes #14641
    
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -64,7 +64,6 @@ constexpr const char* runtime_features[] = {
     "envoy.reloadable_features.consume_all_retry_headers",
     "envoy.reloadable_features.check_ocsp_policy",
     "envoy.reloadable_features.disable_tls_inspector_injection",
-    "envoy.reloadable_features.early_errors_via_hcm",
     "envoy.reloadable_features.enable_dns_cache_circuit_breakers",
     "envoy.reloadable_features.fix_upgrade_response",
     "envoy.reloadable_features.fix_wildcard_matching",
