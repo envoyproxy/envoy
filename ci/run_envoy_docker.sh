057e387780737ca6358c77465d77a58bab4fcef7
Asra Ali <asraa@google.com>
2021-01-14 13:35:43 -0500

    add env var
    Signed-off-by: Asra Ali <asraa@google.com>

--- ci/run_envoy_docker.sh
+++ ci/run_envoy_docker.sh
@@ -80,7 +80,7 @@ docker run --rm \
        -e GCP_SERVICE_ACCOUNT_KEY \
        -e NUM_CPUS \
        -e ENVOY_RBE \
-       -e FUZZIT_API_KEY \
+       -e COVERAGE_FAILED \
        -e ENVOY_BUILD_IMAGE \
        -e ENVOY_SRCDIR \
        -e ENVOY_BUILD_TARGET \
