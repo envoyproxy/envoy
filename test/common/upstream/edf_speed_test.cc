dd688b8cead673fdf166214998cf69c44cf709f0
Yuchen Dai <silentdai@gmail.com>
2021-01-14 16:44:54 -0800

    fix benchmark
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- test/common/upstream/edf_speed_test.cc
+++ test/common/upstream/edf_speed_test.cc
@@ -11,18 +11,12 @@ using Envoy::benchmark::skipExpensiveBenchmarks;
 namespace Envoy {
 namespace Upstream {
 
-class EdfSpeedTest {
-public:
-  EdfSpeedTest(State& state) : state_(state) {}
-  State& state_;
-};
-
-static void heap(State& state) {
+template <class Edf> static void BM_pick_and_add(State& state) {
   for (auto _ : state) {
+    UNREFERENCED_PARAMETER(_);
     // if we've been instructed to skip tests, only run once no matter the argument:
     uint32_t num_entries = skipExpensiveBenchmarks() ? 1 : state.range(0);
-
-    EdfScheduler<uint32_t> sched;
+    Edf sched;
     std::vector<std::shared_ptr<uint32_t>> entries;
     entries.reserve(num_entries);
     for (uint32_t i = 0; i < num_entries; ++i) {
@@ -35,26 +29,12 @@ static void heap(State& state) {
   }
 }
 
-static void wheel(State& state) {
-  for (auto _ : state) {
-    // if we've been instructed to skip tests, only run once no matter the argument:
-    uint32_t num_entries = skipExpensiveBenchmarks() ? 1 : state.range(0);
-
-    TimeWheel<uint32_t> sched;
-    std::vector<std::shared_ptr<uint32_t>> entries;
-    entries.reserve(num_entries);
-    for (uint32_t i = 0; i < num_entries; ++i) {
-      entries.push_back(std::make_shared<uint32_t>(i));
-      sched.add((i % 2) + 1, entries[i]);
-    }
-    for (uint32_t rounds = 0; rounds < 1000000; ++rounds) {
-      auto _pick = sched.pickAndAdd([](const double&) { return 1; });
-    }
-  }
-}
-
-BENCHMARK(heap)->Ranges({{1, 10000}})->Unit(::benchmark::kMillisecond);
-BENCHMARK(wheel)->Ranges({{1, 10000}})->Unit(::benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(BM_pick_and_add, EdfScheduler<uint32_t>)
+    ->Ranges({{1, 10000}})
+    ->Unit(::benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(BM_pick_and_add, TimeWheel<uint32_t>)
+    ->Ranges({{1, 10000}})
+    ->Unit(::benchmark::kMillisecond);
 
 } // namespace Upstream
 } // namespace Envoy
