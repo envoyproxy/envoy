3d2b843a9309a9328aa73525aa76c89bc600e82e
Asra Ali <asraa@google.com>
2021-01-15 12:24:04 -0500

    revert manual test removal
    Signed-off-by: Asra Ali <asraa@google.com>

--- ci/run_envoy_docker.sh
+++ ci/run_envoy_docker.sh
@@ -80,7 +80,6 @@ docker run --rm \
        -e GCP_SERVICE_ACCOUNT_KEY \
        -e NUM_CPUS \
        -e ENVOY_RBE \
-       -e COVERAGE_FAILED \
        -e ENVOY_BUILD_IMAGE \
        -e ENVOY_SRCDIR \
        -e ENVOY_BUILD_TARGET \
