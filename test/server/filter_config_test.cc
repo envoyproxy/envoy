43eaae7dddb6c9aa528e4dc427ba31845bf09655
Auni Ahsan <auni@google.com>
2021-01-18 17:01:36 +0000

    Add test/server:filter_config_test
    This is in preparation for complicating the top-level FilterConfig interface as part of filterchain dependency work.
    
    Signed-off-by: Auni Ahsan <auni@google.com>

--- test/server/filter_config_test.cc
+++ test/server/filter_config_test.cc
@@ -1,20 +1,58 @@
-#include <memory>
-
 #include "envoy/server/filter_config.h"
-#include "testing/base/public/gmock.h"
-#include "testing/base/public/gunit.h"
-#include "third_party/absl/memory/memory.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "test/mocks/server/factory_context.h"
 
 namespace Envoy {
 namespace {
 
-class FilterConfigTest : public ::testing::Test {
+class TestHttpFilter : public Http::StreamDecoderFilter {
  public:
-  FilterConfigTest() {}
+  TestHttpFilter() {}
 
- protected:
-  std::unique_ptr<FilterConfig> filter_config_;
+  Http::FilterHeadersStatus decodeHeaders(Http::RequestHeaderMap&, bool) override {
+    return Envoy::Http::FilterHeadersStatus::Continue;
+  }
+  Http::FilterDataStatus decodeData(Buffer::Instance&, bool) override {
+    return Envoy::Http::FilterDataStatus::Continue;
+  }
+  Http::FilterTrailersStatus decodeTrailers(Http::RequestTrailerMap&) override {
+    return Envoy::Http::FilterTrailersStatus::Continue;
+  }
+
+  void setDecoderFilterCallbacks(Http::StreamDecoderFilterCallbacks&) override {}
+  void onDestroy() override {};
 };
 
-}  // namespace
+class TestHttpFilterConfigFactory : public Server::Configuration::NamedHttpFilterConfigFactory {
+public:
+  TestHttpFilterConfigFactory() {}
+
+  Http::FilterFactoryCb
+  createFilterFactoryFromProto(const Protobuf::Message&, const std::string&,
+                               Server::Configuration::FactoryContext&) override {
+    return [](Http::FilterChainFactoryCallbacks& callbacks) -> void {
+      callbacks.addStreamDecoderFilter(std::make_shared<TestHttpFilter>());
+    };
+  }
+
+  ProtobufTypes::MessagePtr createEmptyConfigProto() override { return nullptr; }
+  ProtobufTypes::MessagePtr createEmptyProtocolOptionsProto() override {
+    return nullptr;
+  }
+
+  std::string name() const override { return "envoy.test.http_filter"; }
+  std::string configType() override { return ""; };
+};
+
+TEST(NamedHttpFilterConfigFactoryTest, CreateFilterFactory) {
+  TestHttpFilterConfigFactory factory;
+  const std::string stats_prefix = "foo";
+  Server::Configuration::MockFactoryContext context;
+  ProtobufTypes::MessagePtr message{new Envoy::ProtobufWkt::Struct()};
+
+  factory.createFilterFactoryFromProto(*message, stats_prefix, context);
+}
+
+} // namespace
 } // namespace Envoy
