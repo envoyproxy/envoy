f67e218ab0366a13f70129550307d9f8081dcbee
Pengyuan Bian <bianpengyuan@google.com>
2021-01-23 18:45:34 +0000

    fix test build
    Signed-off-by: Pengyuan Bian <bianpengyuan@google.com>

--- test/extensions/filters/network/http_connection_manager/config_test.cc
+++ test/extensions/filters/network/http_connection_manager/config_test.cc
@@ -57,7 +57,7 @@ public:
   NiceMock<Router::MockRouteConfigProviderManager> route_config_provider_manager_;
   NiceMock<Config::MockConfigProviderManager> scoped_routes_config_provider_manager_;
   NiceMock<Tracing::MockHttpTracerManager> http_tracer_manager_;
-  Filter::Http::FilterConfigProviderManagerImpl filter_config_provider_manager_;
+  Filter::Http::FilterConfigProviderManagerImpl filter_config_provider_manager_{context_.admin()};
   std::shared_ptr<NiceMock<Tracing::MockHttpTracer>> http_tracer_{
       std::make_shared<NiceMock<Tracing::MockHttpTracer>>()};
   void createHttpConnectionManagerConfig(const std::string& yaml) {
