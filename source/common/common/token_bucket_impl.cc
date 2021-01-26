9e3e285ccef414b5f4584d17921ca5fdf1615bb7
Nitin <nigoyal@microsoft.com>
2021-01-26 04:05:05 -0800

    add synch test
    Signed-off-by: Nitin <nigoyal@microsoft.com>

--- source/common/common/token_bucket_impl.cc
+++ source/common/common/token_bucket_impl.cc
@@ -1,17 +1,22 @@
 #include "common/common/token_bucket_impl.h"
+#include "common/common/lock_guard.h"
 
 #include <chrono>
 
 namespace Envoy {
 
+const char TokenBucketImpl::MutexLockedSyncPoint[] = "post_lock";
+
 TokenBucketImpl::TokenBucketImpl(uint64_t max_tokens, TimeSource& time_source, double fill_rate,
-                                 absl::Mutex* mutex)
+                                 Thread::MutexBasicLockable* mutex)
     : max_tokens_(max_tokens), fill_rate_(std::abs(fill_rate)), tokens_(max_tokens),
       last_fill_(time_source.monotonicTime()), time_source_(time_source), mutex_(mutex),
       reset_once_(false) {}
 
 uint64_t TokenBucketImpl::consume(uint64_t tokens, bool allow_partial) {
-  absl::MutexLockMaybe lock(mutex_);
+  Thread::OptionalLockGuard lock(mutex_);
+  // absl::MutexLockMaybe lock(mutex_);
+  synchronizer_.syncPoint(MutexLockedSyncPoint);
   if (tokens_ < max_tokens_) {
     const auto time_now = time_source_.monotonicTime();
     tokens_ = std::min((std::chrono::duration<double>(time_now - last_fill_).count() * fill_rate_) +
@@ -34,7 +39,8 @@ uint64_t TokenBucketImpl::consume(uint64_t tokens, bool allow_partial) {
 
 std::chrono::milliseconds TokenBucketImpl::nextTokenAvailable() {
   // If there are tokens available, return immediately.
-  absl::MutexLockMaybe lock(mutex_);
+  Thread::OptionalLockGuard lock(mutex_);
+  synchronizer_.syncPoint(MutexLockedSyncPoint);
   if (tokens_ >= 1) {
     return std::chrono::milliseconds(0);
   }
@@ -44,7 +50,8 @@ std::chrono::milliseconds TokenBucketImpl::nextTokenAvailable() {
 
 void TokenBucketImpl::reset(uint64_t num_tokens) {
   ASSERT(num_tokens <= max_tokens_);
-  absl::MutexLockMaybe lock(mutex_);
+  Thread::OptionalLockGuard lock(mutex_);
+  synchronizer_.syncPoint(MutexLockedSyncPoint);
   // Don't reset if thread-safe i.e. shared and reset once before.
   if (mutex_ && reset_once_) {
     return;
--- source/common/common/token_bucket_impl.h
+++ source/common/common/token_bucket_impl.h
@@ -3,9 +3,9 @@
 #include "envoy/common/time.h"
 #include "envoy/common/token_bucket.h"
 
+#include "common/common/thread.h"
+#include "common/common/thread_synchronizer.h"
 #include "common/common/utility.h"
-
-#include "absl/synchronization/mutex.h"
 
 namespace Envoy {
 
@@ -14,6 +14,7 @@ namespace Envoy {
  */
 class TokenBucketImpl : public TokenBucket {
 public:
+  static const char MutexLockedSyncPoint[];
   /**
    * @param max_tokens supplies the maximum number of tokens in the bucket.
    * @param time_source supplies the time source.
@@ -23,21 +24,25 @@ public:
    * is shared. By default the class will be thread-unsafe.
    */
   explicit TokenBucketImpl(uint64_t max_tokens, TimeSource& time_source, double fill_rate = 1,
-                           absl::Mutex* mutex = nullptr);
+                           Thread::MutexBasicLockable* mutex = nullptr);
 
   TokenBucketImpl(const TokenBucketImpl&) = delete;
   TokenBucketImpl(TokenBucketImpl&&) = delete;
 
   // TokenBucket
-  uint64_t consume(uint64_t tokens, bool allow_partial) override;
-  std::chrono::milliseconds nextTokenAvailable() override;
+  uint64_t consume(uint64_t tokens, bool allow_partial)
+      ABSL_EXCLUSIVE_LOCKS_REQUIRED(mutex_) override;
+  std::chrono::milliseconds nextTokenAvailable() ABSL_EXCLUSIVE_LOCKS_REQUIRED(mutex_) override;
 
   /**
    * Resets the bucket to contain tokens equal to @param num_tokens
    * When the token bucket is shared, only the first reset call will work. Subsequent calls to reset
    * method will be ignored.
    */
-  void reset(uint64_t num_tokens) override;
+  void reset(uint64_t num_tokens) ABSL_EXCLUSIVE_LOCKS_REQUIRED(mutex_) override;
+
+  // Used only for testing.
+  Thread::ThreadSynchronizer& synchronizer() { return synchronizer_; };
 
 private:
   const double max_tokens_;
@@ -45,8 +50,9 @@ private:
   double tokens_ ABSL_GUARDED_BY(mutex_);
   MonotonicTime last_fill_ ABSL_GUARDED_BY(mutex_);
   TimeSource& time_source_;
-  absl::Mutex* const mutex_;
+  Thread::MutexBasicLockable* mutex_;
   bool reset_once_;
+  mutable Thread::ThreadSynchronizer synchronizer_; // Used only for testing.
 };
 
 } // namespace Envoy
