b82462f30e87404a65b6bfc36bddfc57dedd639e
Christoph Pakulski <christoph@tetrate.io>
2021-01-29 23:36:22 +0000

    Fixed config compile error.
    Signed-off-by: Christoph Pakulski <christoph@tetrate.io>

--- source/extensions/filters/network/postgres_proxy/config.cc
+++ source/extensions/filters/network/postgres_proxy/config.cc
@@ -15,13 +15,13 @@ NetworkFilters::PostgresProxy::PostgresConfigFactory::createFilterFactoryFromPro
   ASSERT(!proto_config.stat_prefix().empty());
 
   PostgresFilterConfig::PostgresFilterConfigOptions config_options;
-  config_options.stat_prefix_ = fmt::format("postgres.{}", proto_config.stat_prefix());
-  config_options_.enable_sql_parsing_ =
+  config_options.stats_prefix_ = fmt::format("postgres.{}", proto_config.stat_prefix());
+  config_options.enable_sql_parsing_ =
       PROTOBUF_GET_WRAPPED_OR_DEFAULT(proto_config, enable_sql_parsing, true);
-  config_options_.terminate_ssl_ = proto_config.terminate_ssl();
+  config_options.terminate_ssl_ = proto_config.terminate_ssl();
 
-  PostgresFilterConfigSharedPtr filter_config(std::make_shared<PostgresFilterConfig>(
-      stat_prefix, enable_sql, terminate_ssl, context.scope()));
+  PostgresFilterConfigSharedPtr filter_config(
+      std::make_shared<PostgresFilterConfig>(config_options, context.scope()));
   return [filter_config](Network::FilterManager& filter_manager) -> void {
     filter_manager.addFilter(std::make_shared<PostgresFilter>(filter_config));
   };
