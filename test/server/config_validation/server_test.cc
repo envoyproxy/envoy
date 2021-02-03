b70ce2072c55f6776355384c44fdf3bf7460c033
davinci26 <sotirisnan@gmail.com>
2021-02-03 12:24:46 -0800

    fix ifdef
    Signed-off-by: davinci26 <sotirisnan@gmail.com>

--- test/server/config_validation/server_test.cc
+++ test/server/config_validation/server_test.cc
@@ -135,7 +135,7 @@ TEST_P(ValidationServerTest, NoopLifecycleNotifier) {
 // exists.)
 
 auto testing_values = ::testing::Values("front-proxy_front-envoy.yaml", "envoyproxy_io_proxy.yaml",
-#if defined(WIN32) && !defined(SO_ORIGINAL_DST)
+#if defined(WIN32) && defined(SO_ORIGINAL_DST)
                                         "configs_original-dst-cluster_proxy_config.yaml"
 #endif
 #ifndef WIN32
