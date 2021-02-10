9e4153bff0694d6d5216fda1c6b79bd120d0eff8
Yuchen Dai <silentdai@gmail.com>
2021-02-10 01:32:43 -0800

    fix typo and incr coverage
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- test/extensions/io_socket/user_space/io_handle_impl_test.cc
+++ test/extensions/io_socket/user_space/io_handle_impl_test.cc
@@ -974,7 +974,7 @@ TEST_F(IoHandleImplTest, NotifyWritableAfterShutdownWrite) {
 }
 
 TEST_F(IoHandleImplTest, ReturnValidInternalAddress) {
-  const auto& local_address = io_handle_->peerAddress();
+  const auto& local_address = io_handle_->localAddress();
   ASSERT_NE(nullptr, local_address);
   ASSERT_EQ(nullptr, local_address->ip());
   ASSERT_EQ(nullptr, local_address->pipe());
