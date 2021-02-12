39a0cb618695b5b97f31dfdbe8a81873f3a55dd1
Teju Nareddy <nareddyt@google.com>
2021-02-12 12:44:10 -0500

    grpc_json_transcoder: Fine-tune configurability of strict http request validation (#15019)
    Follow-up on #14715 to allow finer configuration of the strict http request validation behavior. As mentioned in #14972, there are some cases where rejecting unknown paths is undesirable. So we provide more configurability to fit all use cases.
    
    Risk Level:
    Low.
    
    This is a breaking API change, but it changes the API introduces < 14 days ago in #14715. This is compliant.
    All behavior change only occurs when the option is enabled.
    Testing:
    Integration tests
    
    Docs Changes:
    Proto config changes
    
    Release Notes:
    None. #14715 includes release notes that still apply
    
    Signed-off-by: Teju Nareddy <nareddyt@google.com>
    Co-authored-by: Yan Tang <tangyan@gmail.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- source/extensions/filters/http/grpc_json_transcoder/json_transcoder_filter.cc
+++ source/extensions/filters/http/grpc_json_transcoder/json_transcoder_filter.cc
@@ -223,7 +223,7 @@ JsonTranscoderConfig::JsonTranscoderConfig(
 
   match_incoming_request_route_ = proto_config.match_incoming_request_route();
   ignore_unknown_query_parameters_ = proto_config.ignore_unknown_query_parameters();
-  strict_http_request_validation_ = proto_config.strict_http_request_validation();
+  request_validation_options_ = proto_config.request_validation_options();
 }
 
 void JsonTranscoderConfig::addFileDescriptor(const Protobuf::FileDescriptorProto& file) {
@@ -444,18 +444,33 @@ Http::FilterHeadersStatus JsonTranscoderFilter::decodeHeaders(Http::RequestHeade
   if (!status.ok()) {
     ENVOY_LOG(debug, "Failed to transcode request headers: {}", status.error_message());
 
-    if (config_.strict_http_request_validation_) {
-      ENVOY_LOG(debug, "Request is rejected due to strict rejection policy.");
-      error_ = true;
-      decoder_callbacks_->sendLocalReply(
-          Http::Code::BadRequest, absl::StrCat("Bad request: ", status.error_message().ToString()),
-          nullptr, absl::nullopt,
-          absl::StrCat(RcDetails::get().GrpcTranscodeFailedEarly, "{BAD_REQUEST}"));
-      return Http::FilterHeadersStatus::StopIteration;
-    } else {
-      ENVOY_LOG(debug, "Request is passed through without transcoding.");
+    if (status.code() == Code::NOT_FOUND &&
+        !config_.request_validation_options_.reject_unknown_method()) {
+      ENVOY_LOG(debug, "Request is passed through without transcoding because it cannot be mapped "
+                       "to a gRPC method.");
       return Http::FilterHeadersStatus::Continue;
     }
+
+    if (status.code() == Code::INVALID_ARGUMENT &&
+        !config_.request_validation_options_.reject_unknown_query_parameters()) {
+      ENVOY_LOG(debug, "Request is passed through without transcoding because it contains unknown "
+                       "query parameters.");
+      return Http::FilterHeadersStatus::Continue;
+    }
+
+    // protobuf::util::Status.error_code is the same as Envoy GrpcStatus
+    // This cast is safe.
+    auto http_code = Envoy::Grpc::Utility::grpcToHttpStatus(
+        static_cast<Envoy::Grpc::Status::GrpcStatus>(status.code()));
+
+    ENVOY_LOG(debug, "Request is rejected due to strict rejection policy.");
+    error_ = true;
+    decoder_callbacks_->sendLocalReply(static_cast<Http::Code>(http_code),
+                                       status.error_message().ToString(), nullptr, absl::nullopt,
+                                       absl::StrCat(RcDetails::get().GrpcTranscodeFailedEarly, "{",
+                                                    MessageUtil::CodeEnumToString(status.code()),
+                                                    "}"));
+    return Http::FilterHeadersStatus::StopIteration;
   }
 
   if (method_->request_type_is_http_body_) {
