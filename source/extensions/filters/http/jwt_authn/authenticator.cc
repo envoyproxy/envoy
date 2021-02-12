42496720233063e99345ae096b99b1d5a240855d
wozz <wozz@users.noreply.github.com>
2021-02-11 23:39:45 -0800

    use timeSource for jwt_authn
    Signed-off-by: Michael Wozniak <wozz@koh.ms>
    
    Signed-off-by: wozz <wozz@users.noreply.github.com>

--- source/extensions/filters/http/jwt_authn/authenticator.cc
+++ source/extensions/filters/http/jwt_authn/authenticator.cc
@@ -175,7 +175,10 @@ void AuthenticatorImpl::startVerify() {
   if (jwks_data_->getJwtProvider().clock_skew_seconds() > 0) {
     clock_skew_seconds = jwks_data_->getJwtProvider().clock_skew_seconds();
   }
-  status = jwt_->verifyTimeConstraint(absl::ToUnixSeconds(absl::Now()), clock_skew_seconds);
+  const uint64_t unix_timestamp =
+      std::chrono::duration_cast<std::chrono::seconds>(timeSource().systemTime().time_since_epoch())
+          .count();
+  status = jwt_->verifyTimeConstraint(unix_timestamp, clock_skew_seconds);
   if (status != Status::Ok) {
     doneWithStatus(status);
     return;
