f9c674e1a32ec2b527d7bfc41dcf67c60f510990
Tommy Wang <xiaobinwang@google.com>
2021-02-17 19:16:10 +0000

    Fix format.
    Signed-off-by: Tommy Wang <xiaobinwang@google.com>

--- source/extensions/filters/http/kill_request/kill_request_filter.cc
+++ source/extensions/filters/http/kill_request/kill_request_filter.cc
@@ -46,8 +46,7 @@ Http::FilterHeadersStatus KillRequestFilter::decodeHeaders(Http::RequestHeaderMa
         route_entry->mostSpecificPerFilterConfigTyped<KillSettings>(name);
 
     if (per_route_kill_settings) {
-      envoy::type::v3::FractionalPercent probability =
-          per_route_kill_settings->getProbability();
+      envoy::type::v3::FractionalPercent probability = per_route_kill_settings->getProbability();
       kill_request_.set_allocated_probability(&probability);
     }
   }
