27b3a45adf75a752d4e57b59951f3b9a34a69099
Raul Gutierrez Segales <rgs@pinterest.com>
2021-02-18 11:09:06 -0500

    Test failure case
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>

--- test/server/hot_restarting_base_test.cc
+++ test/server/hot_restarting_base_test.cc
@@ -30,7 +30,28 @@ public:
   MockHotRestartingBase base_;
 };
 
-TEST_F(HotRestartingBaseTest, SendMsgRetry) {
+TEST_F(HotRestartingBaseTest, SendMsgRetryFailsAfterRetries) {
+  Api::MockOsSysCalls os_sys_calls;
+  TestThreadsafeSingletonInjector<Api::OsSysCallsImpl> os_calls(&os_sys_calls);
+
+  EXPECT_CALL(os_sys_calls, sendmsg(_, _, _))
+      .WillRepeatedly(Invoke([&](os_fd_t, const msghdr*, int) {
+        return Api::SysCallSizeResult{0, ECONNREFUSED};
+      }));
+
+  std::string dst_path = "/tmp/dst";
+  sockaddr_un sun;
+  sun.sun_family = AF_UNIX;
+  StringUtil::strlcpy(&sun.sun_path[1], dst_path.data(), dst_path.size());
+  sun.sun_path[0] = '\0';
+
+  HotRestartMessage message;
+  message.mutable_request()->mutable_pass_listen_socket()->set_address("tcp://0.0.0.0:80");
+
+  ASSERT_DEATH(base_.sendMessage(sun, message), "");
+}
+
+TEST_F(HotRestartingBaseTest, SendMsgRetrySucceedsEventually) {
   Api::MockOsSysCalls os_sys_calls;
   TestThreadsafeSingletonInjector<Api::OsSysCallsImpl> os_calls(&os_sys_calls);
 
