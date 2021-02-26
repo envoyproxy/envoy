566a52ed161bdb445910590d7b257cdb51f58891
James Buckland <jbuckland@google.com>
2021-02-26 16:32:20 -0500

    Add default sslctx_cb() behaviors to quiche mock clientcontextconfig.
    Signed-off-by: James Buckland <jbuckland@google.com>

--- test/extensions/quic_listeners/quiche/envoy_quic_proof_source_test.cc
+++ test/extensions/quic_listeners/quiche/envoy_quic_proof_source_test.cc
@@ -43,6 +43,7 @@ public:
         .WillByDefault(ReturnRef(empty_string));
     ON_CALL(client_context_config_, certificateValidationContext())
         .WillByDefault(Return(&cert_validation_ctx_config_));
+    ON_CALL(client_context_config_, sslctx_cb()).WillByDefault(Return([](SSL_CTX*){}));
 
     // Getting the last cert in the chain as the root CA cert.
     std::string cert_chain(quic::test::kTestCertificateChainPem);
