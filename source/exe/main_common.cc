d120e3e9c453d745437e6c107e00ae845ee00bee
Raul Gutierrez Segales <rgs@pinterest.com>
2021-03-02 14:36:39 -0500

    Info log when prctl succeeds
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>

--- source/exe/main_common.cc
+++ source/exe/main_common.cc
@@ -54,7 +54,9 @@ MainCommonBase::MainCommonBase(const Server::Options& options, Event::TimeSystem
 
   // Enable core dumps as early as possible.
   if (options_.coreDumpEnabled()) {
-    if (!platform_impl_.enableCoreDump()) {
+    if (platform_impl_.enableCoreDump()) {
+      ENVOY_LOG_MISC(info, "core dump enabled");
+    } else {
       ENVOY_LOG_MISC(error, "failed to enable core dump");
     }
   }
