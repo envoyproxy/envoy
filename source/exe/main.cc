f8e77ac6e66ef8776fcd7057890a0211967c9ac5
Sotiris Nanopoulos <sotirisnan@gmail.com>
2021-03-04 13:33:33 -0800

    [Windows] Onboard Envoy to SCM (#14352)
    Signed-off-by: davinci26 <sotirisnan@gmail.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- source/exe/main.cc
+++ source/exe/main.cc
@@ -1,5 +1,9 @@
 #include "exe/main_common.h"
 
+#ifdef WIN32
+#include "exe/service_base.h"
+#endif
+
 // NOLINT(namespace-envoy)
 
 /**
@@ -9,4 +13,13 @@
  * deployment such as initializing signal handling. It calls main_common
  * after setting up command line options.
  */
-int main(int argc, char** argv) { return Envoy::MainCommon::main(argc, argv); }
+int main(int argc, char** argv) {
+#ifdef WIN32
+  Envoy::ServiceBase service;
+  if (!Envoy::ServiceBase::TryRunAsService(service)) {
+    return Envoy::MainCommon::main(argc, argv);
+  }
+  return EXIT_SUCCESS;
+#endif
+  return Envoy::MainCommon::main(argc, argv);
+}
---
+++ source/exe/win32/service_base.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <processenv.h>
+#include <shellapi.h>
+#include <winsvc.h>
+
+#include <functional>
+#include <string>
+
+#include "envoy/event/signal.h"
+
+#include "exe/service_status.h"
+
+namespace Envoy {
+class ServiceBase {
+public:
+  virtual ~ServiceBase() = default;
+  /**
+   * Try to register Envoy as a service. For this we register `ServiceMain` and `Handler`
+   * @param ServiceBase instance of the service.
+   * @return false if Envoy can not be registered to run as service.
+   */
+  static bool TryRunAsService(ServiceBase& service);
+
+  ServiceBase(DWORD controlsAccepted = SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_PRESHUTDOWN);
+  /**
+   * Start the service.
+   * @return exit code of Envoy.
+   */
+  DWORD Start(std::vector<std::string> args);
+
+  /**
+   * Stop the service.
+   * @param control the control code Service Control Manager requested.
+   */
+  void Stop(DWORD control);
+
+  /**
+   * Update the state of the service.
+   * @param state New service state.
+   * @param errorCode Last error code encountered by Envoy.
+   * @param serviceError If the error is coming from the service or a Win32 interaction.
+   */
+  void UpdateState(DWORD state, HRESULT errorCode = S_OK, bool serviceError = true);
+
+private:
+  void SetServiceStatus();
+
+  static void WINAPI ServiceMain(DWORD argc, LPSTR* argv);
+
+  static void WINAPI Handler(DWORD control);
+
+  static ServiceBase* service_static;
+  SERVICE_STATUS_HANDLE handle_;
+  ServiceStatus status_;
+  Event::SignalEventPtr sigterm_;
+};
+} // namespace Envoy
