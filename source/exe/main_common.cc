129d19c97449c2d4b5431bc6cf937312e2af95e1
Raul Gutierrez Segales <rgs@pinterest.com>
2021-03-04 14:33:53 -0500

    Fix logging
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>

--- source/exe/main_common.cc
+++ source/exe/main_common.cc
@@ -54,11 +54,11 @@ MainCommonBase::MainCommonBase(const Server::Options& options, Event::TimeSystem
 
   // Enable core dumps as early as possible.
   if (options_.coreDumpEnabled()) {
-    auto ret = platform_impl_.enableCoreDump();
+    const auto ret = platform_impl_.enableCoreDump();
     if (ret) {
-      ENVOY_LOG_MISC(info, "core dump enabled: {}", ret);
+      ENVOY_LOG_MISC(info, "core dump enabled");
     } else {
-      ENVOY_LOG_MISC(warn, "core dump enabled: {}", ret);
+      ENVOY_LOG_MISC(warn, "failed to enable core dump");
     }
   }
 
