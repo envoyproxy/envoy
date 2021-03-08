c66fd5c1922dc1a687f95d3d951be7f31dedd68e
Ryan Northey <ryan@synca.io>
2021-03-08 10:37:40 +0000

    configs/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- configs/example_configs_validation.py
+++ configs/example_configs_validation.py
@@ -14,36 +14,22 @@ from tools.config_validation.validate_fragment import ValidateFragment
 
 
 def main():
-  parse_errors = []
-  other_errors = []
+  errors = []
   for arg in sys.argv[1:]:
     if not arg.endswith(".yaml"):
       continue
     try:
       ValidateFragment("envoy.config.bootstrap.v3.Bootstrap",
                        yaml.safe_load(pathlib.Path(arg).read_text()))
-    except ParseError as e:
-      parse_errors.append(arg)
+    except (ParseError, KeyError) as e:
+      errors.append(arg)
       print()
-      print(f"ERROR (parse failed): {arg}")
+      print(f"ERROR (validation failed): {arg}")
       print(e)
       print()
-    except Exception as e:
-      other_errors.append(arg)
-      print()
-      print(f"ERROR ({e.__class__.__name__}): {arg}")
-      print(e)
-      traceback.print_exc()
-      print()
 
-  if parse_errors:
-    print(f"ERROR: some configuration files ({len(parse_errors)}) failed to parse")
-
-  if other_errors:
-    print(f"ERROR: some configuration files ({len(other_errors)}) contained other errors")
-
-  if parse_errors or other_errors:
-    raise SystemExit("ERROR: some configuration files are invalid")
+  if errors:
+    raise SystemExit(f"ERROR: some configuration files ({len(errors)}) failed to parse")
 
 
 if __name__ == "__main__":
