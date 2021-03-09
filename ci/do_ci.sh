6de526e5f38f2048fbd0cee021daa71271aab24e
phlax <phlax@users.noreply.github.com>
2021-03-09 17:06:34 +0000

    configs: Add protobuf validation for example configs (#15284)
    This adds python protobuf validation for all config files in the repo
    
    This is in addition to the existing "validation mode" tests which are not validating to the spec, but rather testing the config works to bootstrap the server
    
    Signed-off-by: Ryan Northey <ryan@synca.io>
    Co-authored-by: Dmitri Dolguikh <ddolguik@redhat.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -418,6 +418,8 @@ elif [[ "$CI_TARGET" == "check_format" ]]; then
   ./tools/code_format/check_format.py check
   ./tools/code_format/format_python_tools.sh check
   BAZEL_BUILD_OPTIONS="${BAZEL_BUILD_OPTIONS[*]}" ./tools/proto_format/proto_format.sh check --test
+  bazel run "${BAZEL_BUILD_OPTIONS[@]}" //configs:example_configs_validation
+
   exit 0
 elif [[ "$CI_TARGET" == "check_repositories" ]]; then
   echo "check_repositories..."
