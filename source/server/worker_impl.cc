653c396e2cd36be67be2d78fbae87db6d9c01ce0
Yuchen Dai <silentdai@gmail.com>
2021-03-08 19:17:00 -0800

    revert ud
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- source/server/worker_impl.cc
+++ source/server/worker_impl.cc
@@ -18,8 +18,8 @@ WorkerPtr ProdWorkerFactory::createWorker(uint32_t index, OverloadManager& overl
                                           const std::string& worker_name) {
   Event::DispatcherPtr dispatcher(
       api_.allocateDispatcher(worker_name, overload_manager.scaledTimerFactory()));
-  auto conn_handler = std::make_unique<ConnectionHandlerImpl>(*dispatcher, index);
-  return std::make_unique<WorkerImpl>(tls_, hooks_, std::move(dispatcher), std::move(conn_handler),
+  return std::make_unique<WorkerImpl>(tls_, hooks_, std::move(dispatcher),
+                                      std::make_unique<ConnectionHandlerImpl>(*dispatcher, index),
                                       overload_manager, api_);
 }
 
