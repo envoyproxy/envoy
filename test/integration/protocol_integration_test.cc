eb8628e33fa9d5ef996c0200b4f558a02af7c9e9
Alyssa Wilk <alyssar@chromium.org>
2021-03-11 11:31:46 -0500

    quic: moving helpers to be shared by connection pool
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/integration/protocol_integration_test.cc
+++ test/integration/protocol_integration_test.cc
@@ -1913,7 +1913,6 @@ TEST_P(ProtocolIntegrationTest, MultipleSetCookies) {
 
 // Resets the downstream stream immediately and verifies that we clean up everything.
 TEST_P(ProtocolIntegrationTest, TestDownstreamResetIdleTimeout) {
-  EXCLUDE_UPSTREAM_HTTP3;
   useAccessLog("%RESPONSE_FLAGS% %RESPONSE_CODE_DETAILS%");
   config_helper_.setDownstreamHttpIdleTimeout(std::chrono::milliseconds(100));
 
