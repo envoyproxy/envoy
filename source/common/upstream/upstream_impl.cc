ad8cf8a437c2d5c61a3a23a265eb37e643341900
Alyssa Wilk <alyssar@chromium.org>
2021-03-15 14:39:43 -0400

    todo
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- source/common/upstream/upstream_impl.cc
+++ source/common/upstream/upstream_impl.cc
@@ -946,6 +946,7 @@ ClusterImplBase::ClusterImplBase(
         fmt::format("ALPN configured for cluster {} which has a non-ALPN transport socket: {}",
                     cluster.name(), cluster.DebugString()));
   }
+  // TODO(#12829) clean up (e.g. move tests out of extensions) once QUIC is no longer an extension.
   if ((info_->features() & ClusterInfoImpl::Features::HTTP3) &&
       (cluster.transport_socket().name() != "envoy.transport_sockets.quic")) {
     throw EnvoyException(fmt::format("HTTP3 requires a QuicUpstreamTransport tranport socket: {}",
