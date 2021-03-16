b9452f759d59ac314379e5f3d6eb8680bd52c040
Teju Nareddy <nareddyt@google.com>
2021-03-16 13:46:29 -0700

    Review comments
    Signed-off-by: Teju Nareddy <nareddyt@google.com>

--- source/common/http/filter_manager.cc
+++ source/common/http/filter_manager.cc
@@ -183,12 +183,6 @@ bool ActiveStreamFilterBase::commonHandleAfterDataCallback(FilterDataStatus stat
       buffer_was_streaming = status == FilterDataStatus::StopIterationAndWatermark;
       commonHandleBufferData(provided_data);
     } else if (complete() && !hasTrailers() && !bufferedData()) {
-      if (parent_.state_.destroyed_) {
-        // If the stream is destroyed, no need to handle the data buffer or trailers.
-        // This can occur if the filter calls sendLocalReply.
-        return false;
-      }
-
       // If this filter is doing StopIterationNoBuffer and this stream is terminated with a zero
       // byte data frame, we need to create an empty buffer to make sure that when commonContinue
       // is called, the pipeline resumes with an empty data frame with end_stream = true
