bd7659b304e566c55aa1b1e3ff41312455cc3e1d
Adi (Suissa) Peleg <adip@google.com>
2021-03-16 13:22:19 -0400

    api: Adding API major.minor.patch version (#15186)
    This PR adds the API_VERSION to Envoy, and the api version handling class to fetch the latest and oldest supported API versions by Envoy.
    This is the first step in implementing the proposal in: #8416.
    
    Risk Level: Low (no usage in the code).
    Testing: Unit tests.
    Docs Changes: None.
    Release Notes: None.
    Platform Specific Features: None.
    
    Signed-off-by: Adi Suissa-Peleg <adip@google.com>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

---
+++ tools/api_versioning/generate_api_version_header.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+"""Parses a file containing the API version (X.Y.Z format), and outputs (to
+stdout) a C++ header file with the ApiVersion value.
+"""
+from collections import namedtuple
+import pathlib
+import string
+import sys
+
+ApiVersion = namedtuple('ApiVersion', ['major', 'minor', 'patch'])
+
+FILE_TEMPLATE = string.Template("""#pragma once
+#include "common/version/api_version_struct.h"
+
+namespace Envoy {
+
+constexpr ApiVersion api_version = {$major, $minor, $patch};
+constexpr ApiVersion oldest_api_version = {$oldest_major, $oldest_minor, $oldest_patch};
+
+} // namespace Envoy""")
+
+
+def GenerateHeaderFile(input_path):
+  """Generates a c++ header file containing the api_version variable with the
+  correct value.
+
+  Args:
+    input_path: the file containing the API version (API_VERSION).
+
+  Returns:
+    the header file contents.
+  """
+  lines = pathlib.Path(input_path).read_text().splitlines()
+  assert (len(lines) == 1)
+
+  # Mapping each field to int verifies it is a valid version
+  version = ApiVersion(*map(int, lines[0].split('.')))
+  oldest_version = ComputeOldestApiVersion(version)
+
+  header_file_contents = FILE_TEMPLATE.substitute({
+      'major': version.major,
+      'minor': version.minor,
+      'patch': version.patch,
+      'oldest_major': oldest_version.major,
+      'oldest_minor': oldest_version.minor,
+      'oldest_patch': oldest_version.patch
+  })
+  return header_file_contents
+
+
+def ComputeOldestApiVersion(current_version: ApiVersion):
+  """Computest the oldest API version the client supports. According to the
+  specification (see: api/API_VERSIONING.md), Envoy supports up to 2 most
+  recent minor versions. Therefore if the latest API version "X.Y.Z", Envoy's
+  oldest API version is "X.Y-1.0". Note that the major number is always the
+  same as the latest version, and the patch number is always 0. In addition,
+  the minor number is at least 0, and the oldest api version cannot be set
+  to a previous major number.
+
+  Args:
+    current_version: the current API version.
+
+  Returns:
+    the oldest supported API version.
+  """
+  return ApiVersion(current_version.major, max(current_version.minor - 1, 0), 0)
+
+
+if __name__ == '__main__':
+  input_path = sys.argv[1]
+  output = GenerateHeaderFile(input_path)
+  # Print output to stdout
+  print(output)
---
+++ tools/api_versioning/generate_api_version_header_test.py
@@ -0,0 +1,91 @@
+"""Tests the api version header file generation.
+"""
+import generate_api_version_header
+from generate_api_version_header import ApiVersion
+import os
+import pathlib
+import string
+import tempfile
+import unittest
+
+
+class GenerateApiVersionHeaderTest(unittest.TestCase):
+  EXPECTED_TEMPLATE = string.Template("""#pragma once
+#include "common/version/api_version_struct.h"
+
+namespace Envoy {
+
+constexpr ApiVersion api_version = {$major, $minor, $patch};
+constexpr ApiVersion oldest_api_version = {$oldest_major, $oldest_minor, $oldest_patch};
+
+} // namespace Envoy""")
+
+  def setUp(self):
+    # Using mkstemp instead of NamedTemporaryFile because in windows NT or later
+    # the created NamedTemporaryFile cannot be reopened again (see comment in:
+    # https://docs.python.org/3.9/library/tempfile.html#tempfile.NamedTemporaryFile)
+    self._temp_fd, self._temp_fname = tempfile.mkstemp(text=True)
+
+  def tearDown(self):
+    # Close and delete the temp file.
+    os.close(self._temp_fd)
+    pathlib.Path(self._temp_fname).unlink()
+
+  # General success pattern when valid file contents is detected.
+  def SuccessfulTestTemplate(self, output_string, current_version: ApiVersion,
+                             oldest_version: ApiVersion):
+    pathlib.Path(self._temp_fname).write_text(output_string)
+
+    # Read the string from the file, and parse the version.
+    output = generate_api_version_header.GenerateHeaderFile(self._temp_fname)
+    expected_output = GenerateApiVersionHeaderTest.EXPECTED_TEMPLATE.substitute({
+        'major': current_version.major,
+        'minor': current_version.minor,
+        'patch': current_version.patch,
+        'oldest_major': oldest_version.major,
+        'oldest_minor': oldest_version.minor,
+        'oldest_patch': oldest_version.patch
+    })
+    self.assertEqual(expected_output, output)
+
+  # General failure pattern when invalid file contents is detected.
+  def FailedTestTemplate(self, output_string, assertion_error_type):
+    pathlib.Path(self._temp_fname).write_text(output_string)
+
+    # Read the string from the file, and expect version parsing to fail.
+    with self.assertRaises(assertion_error_type,
+                           msg='The call to GenerateHeaderFile should have thrown an exception'):
+      generate_api_version_header.GenerateHeaderFile(self._temp_fname)
+
+  def test_valid_version(self):
+    self.SuccessfulTestTemplate('1.2.3', ApiVersion(1, 2, 3), ApiVersion(1, 1, 0))
+
+  def test_valid_version_newline(self):
+    self.SuccessfulTestTemplate('3.2.1\n', ApiVersion(3, 2, 1), ApiVersion(3, 1, 0))
+
+  def test_invalid_version_string(self):
+    self.FailedTestTemplate('1.2.abc3', ValueError)
+
+  def test_invalid_version_partial(self):
+    self.FailedTestTemplate('1.2.', ValueError)
+
+  def test_empty_file(self):
+    # Not writing anything to the file
+    self.FailedTestTemplate('', AssertionError)
+
+  def test_invalid_multiple_lines(self):
+    self.FailedTestTemplate('1.2.3\n1.2.3', AssertionError)
+
+  def test_valid_oldest_api_version(self):
+    expected_latest_oldest_pairs = [(ApiVersion(3, 2, 2), ApiVersion(3, 1, 0)),
+                                    (ApiVersion(4, 5, 30), ApiVersion(4, 4, 0)),
+                                    (ApiVersion(1, 1, 5), ApiVersion(1, 0, 0)),
+                                    (ApiVersion(2, 0, 3), ApiVersion(2, 0, 0))]
+
+    for latest_version, expected_oldest_version in expected_latest_oldest_pairs:
+      self.assertEqual(expected_oldest_version,
+                       generate_api_version_header.ComputeOldestApiVersion(latest_version))
+
+
+if __name__ == '__main__':
+  unittest.main()
