d9a3cde5f7eacb81a275e33ffc7b99f5de4c7934
Rei Shimizu <rei@tetrate.io>
2021-03-18 05:22:46 +0900

    grpc: avoid grpc-status overwrite on sendLocalReply() (#15278)
    Signed-off-by: Shikugawa <rei@tetrate.io>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/common/http/utility_test.cc
+++ test/common/http/utility_test.cc
@@ -633,6 +633,55 @@ TEST(HttpUtility, SendLocalGrpcReply) {
       Utility::LocalReplyData{true, Http::Code::PayloadTooLarge, "large", absl::nullopt, false});
 }
 
+TEST(HttpUtility, SendLocalGrpcReplyGrpcStatusAlreadyExists) {
+  MockStreamDecoderFilterCallbacks callbacks;
+  bool is_reset = false;
+
+  EXPECT_CALL(callbacks, streamInfo());
+  EXPECT_CALL(callbacks, encodeHeaders_(_, true))
+      .WillOnce(Invoke([&](const ResponseHeaderMap& headers, bool) -> void {
+        EXPECT_EQ(headers.getStatusValue(), "200");
+        EXPECT_NE(headers.GrpcStatus(), nullptr);
+        EXPECT_EQ(headers.getGrpcStatusValue(),
+                  std::to_string(enumToInt(Grpc::Status::WellKnownGrpcStatus::InvalidArgument)));
+        EXPECT_NE(headers.GrpcMessage(), nullptr);
+        EXPECT_EQ(headers.getGrpcMessageValue(), "large");
+      }));
+  Utility::sendLocalReply(
+      is_reset, callbacks,
+      Utility::LocalReplyData{true, Http::Code::PayloadTooLarge, "large",
+                              Grpc::Status::WellKnownGrpcStatus::InvalidArgument, false});
+}
+
+TEST(HttpUtility, SendLocalGrpcReplyGrpcStatusPreserved) {
+  MockStreamDecoderFilterCallbacks callbacks;
+  bool is_reset = false;
+
+  auto encode_functions =
+      Utility::EncodeFunctions{[&](ResponseHeaderMap& headers) -> void {
+                                 headers.setGrpcStatus(std::to_string(
+                                     enumToInt(Grpc::Status::WellKnownGrpcStatus::NotFound)));
+                               },
+                               nullptr,
+                               [&](ResponseHeaderMapPtr&& headers, bool end_stream) -> void {
+                                 callbacks.encodeHeaders(std::move(headers), end_stream, "");
+                               },
+                               nullptr};
+  EXPECT_CALL(callbacks, encodeHeaders_(_, true))
+      .WillOnce(Invoke([&](const ResponseHeaderMap& headers, bool) -> void {
+        EXPECT_EQ(headers.getStatusValue(), "200");
+        EXPECT_NE(headers.GrpcStatus(), nullptr);
+        EXPECT_EQ(headers.getGrpcStatusValue(),
+                  std::to_string(enumToInt(Grpc::Status::WellKnownGrpcStatus::NotFound)));
+        EXPECT_NE(headers.GrpcMessage(), nullptr);
+        EXPECT_EQ(headers.getGrpcMessageValue(), "large");
+      }));
+  Utility::sendLocalReply(
+      is_reset, encode_functions,
+      Utility::LocalReplyData{true, Http::Code::PayloadTooLarge, "large",
+                              Grpc::Status::WellKnownGrpcStatus::InvalidArgument, false});
+}
+
 TEST(HttpUtility, SendLocalGrpcReplyWithUpstreamJsonPayload) {
   MockStreamDecoderFilterCallbacks callbacks;
   bool is_reset = false;
