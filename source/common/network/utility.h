684c8af253e6bb9e0c55c09710939ba0dc8e44ae
Raul Gutierrez Segales <rgs@pinterest.com>
2021-03-17 18:50:35 -0400

    Fix merge
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>

--- source/common/network/utility.h
+++ source/common/network/utility.h
@@ -135,7 +135,7 @@ public:
 
   /**
    * Parse an internet host address (IPv4 or IPv6) and create an Instance from it. The address must
-   * not include a port number.
+   * not include a port number. Throws EnvoyException if unable to parse the address.
    * @param ip_address string to be parsed as an internet address.
    * @param port optional port to include in Instance created from ip_address, 0 by default.
    * @param v6only disable IPv4-IPv6 mapping for IPv6 addresses?
@@ -147,7 +147,7 @@ public:
 
   /**
    * Parse an internet host address (IPv4 or IPv6) and create an Instance from it. The address must
-   * not include a port number.
+   * not include a port number. Throws EnvoyException if unable to parse the address.
    * @param ip_address string to be parsed as an internet address.
    * @param port optional port to include in Instance created from ip_address, 0 by default.
    * @param v6only disable IPv4-IPv6 mapping for IPv6 addresses?
