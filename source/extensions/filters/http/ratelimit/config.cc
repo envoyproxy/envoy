84852a29ac3324407fffa949278fd0ff0efa667c
Harvey Tuch <htuch@google.com>
2021-03-17 23:25:55 +0000

    config: ensure Config::Utility::getAndCheckTransportVersion is called on main thread.
    During the v2 -> v3 migration, some uses of this were added inside filter factory lambdas and could
    occur on worker threads, rejecting config on the data plane.
    
    Relevant to #10943
    
    Risk level: Low
    Testing: New assertion in method caused existing tests to fail, fix addresses these. Also manually
      audited via grep.
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- source/extensions/filters/http/ratelimit/config.cc
+++ source/extensions/filters/http/ratelimit/config.cc
@@ -28,13 +28,14 @@ Http::FilterFactoryCb RateLimitFilterConfig::createFilterFactoryFromProtoTyped(
   const std::chrono::milliseconds timeout =
       std::chrono::milliseconds(PROTOBUF_GET_MS_OR_DEFAULT(proto_config, timeout, 20));
 
-  return [proto_config, &context, timeout,
-          filter_config](Http::FilterChainFactoryCallbacks& callbacks) -> void {
+  return [proto_config, &context, timeout, filter_config,
+          transport_version =
+              Config::Utility::getAndCheckTransportVersion(proto_config.rate_limit_service())](
+             Http::FilterChainFactoryCallbacks& callbacks) -> void {
     callbacks.addStreamFilter(std::make_shared<Filter>(
-        filter_config,
-        Filters::Common::RateLimit::rateLimitClient(
-            context, proto_config.rate_limit_service().grpc_service(), timeout,
-            Config::Utility::getAndCheckTransportVersion(proto_config.rate_limit_service()))));
+        filter_config, Filters::Common::RateLimit::rateLimitClient(
+                           context, proto_config.rate_limit_service().grpc_service(), timeout,
+                           transport_version)));
   };
 }
 
