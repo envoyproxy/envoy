ba72cd861ad36aa9fa132465d2bc623e03c3ccd6
Yuchen Dai <silentdai@gmail.com>
2021-03-19 18:52:17 -0700

    update per file coverage
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -18,6 +18,7 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/common/protobuf:94.6"
 "source/common/signal:84.5" # Death tests don't report LCOV
 "source/common/singleton:95.1"
+"source/common/stream_info:96.4"
 "source/common/thread:0.0" # Death tests don't report LCOV
 "source/common/matcher:93.3"
 "source/common/tracing:94.9"
