26c9e137522c34c48215321e8024633e5d0e5e8d
John Murray <murray@stripe.com>
2021-03-23 10:05:29 -0600

    export SRCDIR + ENVOY_SRCDIR
    Signed-off-by: John Murray <murray@stripe.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -13,8 +13,8 @@ if [[ "$1" == "format_pre" || "$1" == "fix_format" || "$1" == "check_format" || 
 fi
 
 # TODO(phlax): Clarify and/or integrate SRCDIR and ENVOY_SRCDIR
-SRCDIR="${SRCDIR:-$PWD}"
-ENVOY_SRCDIR="${ENVOY_SRCDIR:-$PWD}" 
+export SRCDIR="${SRCDIR:-$PWD}"
+export ENVOY_SRCDIR="${ENVOY_SRCDIR:-$PWD}" 
 NO_BUILD_SETUP="${NO_BUILD_SETUP:-}"
 
 if [[ -z "$NO_BUILD_SETUP" ]]; then
