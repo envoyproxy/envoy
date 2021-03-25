ada478458a989d67ee452dc79ba279a99c01f23a
Rei Shimizu <Shikugawa@gmail.com>
2021-03-26 01:58:11 +0900

    ssl: fix crash when peer sends an SSL Alert with an unknown code (#259)
    Fixes CVE-2021-28683 (crash when peer sends an SSL Alert with an unknown code)
    
    Signed-off-by: Shikugawa <Shikugawa@gmail.com>
    Co-authored-by: Christoph Pakulski <christoph@tetrate.io>
    Signed-off-by: Tony Allen <tony@allen.gg>

--- source/extensions/transport_sockets/tls/ssl_socket.cc
+++ source/extensions/transport_sockets/tls/ssl_socket.cc
@@ -216,9 +216,10 @@ void SslSocket::drainErrorQueue() {
     if (failure_reason_.empty()) {
       failure_reason_ = "TLS error:";
     }
-    failure_reason_.append(absl::StrCat(" ", err, ":", ERR_lib_error_string(err), ":",
-                                        ERR_func_error_string(err), ":",
-                                        ERR_reason_error_string(err)));
+    failure_reason_.append(absl::StrCat(" ", err, ":",
+                                        absl::NullSafeStringView(ERR_lib_error_string(err)), ":",
+                                        absl::NullSafeStringView(ERR_func_error_string(err)), ":",
+                                        absl::NullSafeStringView(ERR_reason_error_string(err))));
   }
   ENVOY_CONN_LOG(debug, "{}", callbacks_->connection(), failure_reason_);
   if (saw_error && !saw_counted_error) {
