0908f56a053ed6b69af397b756cb8fac3a647d61
chaoqin-li1123 <chaoqinli@google.com>
2021-03-30 16:05:13 +0000

    initial
    Signed-off-by: chaoqin-li1123 <chaoqinli@google.com>

--- source/common/http/http1/codec_impl.cc
+++ source/common/http/http1/codec_impl.cc
@@ -538,13 +538,7 @@ bool ConnectionImpl::maybeDirectDispatch(Buffer::Instance& data) {
   return true;
 }
 
-Http::Status ConnectionImpl::dispatch(Buffer::Instance& data) {
-  // TODO(#10878): Remove this wrapper when exception removal is complete. innerDispatch may either
-  // throw an exception or return an error status. The utility wrapper catches exceptions and
-  // converts them to error statuses.
-  return Utility::exceptionToStatus(
-      [&](Buffer::Instance& data) -> Http::Status { return innerDispatch(data); }, data);
-}
+Http::Status ConnectionImpl::dispatch(Buffer::Instance& data) { return innerDispatch(data); }
 
 Http::Status ClientConnectionImpl::dispatch(Buffer::Instance& data) {
   Http::Status status = ConnectionImpl::dispatch(data);
