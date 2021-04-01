2a74aafc5c35b7f1cbd793a42b6d488cb227e524
Mike Schore <mike.schore@gmail.com>
2021-04-02 07:13:05 +0800

    engine: set concurrency to 0 (#1338)
    Description: Until now, we've relied upon the implicit behavior that Envoy will start at most one worker without a connection-based listener. This explicitly sets concurrency to 0 to prevent multiple workers from starting.
    Risk Level: Moderate
    Testing: Local and CI
    
    Signed-off-by: Mike Schore <mike.schore@gmail.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/common/engine.cc
+++ mobile/library/common/engine.cc
@@ -32,8 +32,16 @@ envoy_status_t Engine::run(const std::string config, const std::string log_level
       const std::string name = "envoy";
       const std::string config_flag = "--config-yaml";
       const std::string log_flag = "-l";
-      const char* envoy_argv[] = {name.c_str(),     config_flag.c_str(), config.c_str(),
-                                  log_flag.c_str(), log_level.c_str(),   nullptr};
+      const std::string concurrency_option = "--concurrency";
+      const std::string concurrency_arg = "0";
+      const char* envoy_argv[] = {name.c_str(),
+                                  config_flag.c_str(),
+                                  config.c_str(),
+                                  concurrency_option.c_str(),
+                                  concurrency_arg.c_str(),
+                                  log_flag.c_str(),
+                                  log_level.c_str(),
+                                  nullptr};
 
       main_common_ = std::make_unique<MobileMainCommon>(5, envoy_argv);
       event_dispatcher_ = &main_common_->server()->dispatcher();
