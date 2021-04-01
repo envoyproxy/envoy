67d819cbb258ff0d955762e6cecbca1a19e80b22
Charlie Getzen <charliegetzenlc@gmail.com>
2021-04-01 09:30:12 -0500

    test -- cluster_info
    Signed-off-by: Charlie Getzen <charliegetzenlc@gmail.com>

--- test/mocks/upstream/cluster_info.h
+++ test/mocks/upstream/cluster_info.h
@@ -190,7 +190,7 @@ public:
   absl::optional<envoy::config::cluster::v3::Cluster::OriginalDstLbConfig> lb_original_dst_config_;
   absl::optional<envoy::config::core::v3::TypedExtensionConfig> upstream_config_;
   Network::ConnectionSocket::OptionsSharedPtr cluster_socket_options_;
-  envoy::config::cluster::v3::LoadBalancingPolicy load_balancing_policy;
+  envoy::config::cluster::v3::LoadBalancingPolicy load_balancing_policy_;
   envoy::config::cluster::v3::Cluster::CommonLbConfig lb_config_;
   envoy::config::core::v3::Metadata metadata_;
   std::unique_ptr<Envoy::Config::TypedMetadata> typed_metadata_;
