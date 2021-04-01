08d430561a3743d031fa0e3574ca834b58c8fe95
Cerek Hillen <crockeo@users.noreply.github.com>
2021-04-01 11:24:05 -0700

    add in a headers iterator (#1318)
    Description: Adds an iterator to the `Headers` classes to iterate through headers. Note that this violates C++ design by iterating through the header key, not the header key/value pair. I chose this design because `pybind11` was giving me invalid memory access errors while trying to iterate through pairs.
    Risk Level: Low
    Testing: Pending
    
    Signed-off-by: Cerek Hillen <chillen@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/cc/headers.cc
+++ mobile/library/cc/headers.cc
@@ -3,6 +3,14 @@
 namespace Envoy {
 namespace Platform {
 
+Headers::const_iterator Headers::begin() const {
+  return Headers::const_iterator(this->all_headers().begin());
+}
+
+Headers::const_iterator Headers::end() const {
+  return Headers::const_iterator(this->all_headers().end());
+}
+
 const std::vector<std::string>& Headers::operator[](const std::string& key) const {
   return this->headers_.at(key);
 }
