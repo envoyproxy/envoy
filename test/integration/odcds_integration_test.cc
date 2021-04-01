e791d2be3c16a965998ec4b120dff0cf2ba3d712
Krzesimir Nowak <krzesimir@kinvolk.io>
2021-04-01 13:03:06 +0200

    test: Add test checking disabling ODCDS
    Signed-off-by: Krzesimir Nowak <krzesimir@kinvolk.io>

--- test/integration/odcds_integration_test.cc
+++ test/integration/odcds_integration_test.cc
@@ -87,15 +87,19 @@ static_resources:
                                                   Platform::null_device_path));
 }
 
-class OdCdsIntegrationTest : public HttpIntegrationTest,
-                             public Grpc::GrpcClientIntegrationParamTest {
+class OdCdsIntegrationTestBase : public HttpIntegrationTest,
+                                 public Grpc::GrpcClientIntegrationParamTest {
 public:
-  OdCdsIntegrationTest()
-      : HttpIntegrationTest(Http::CodecClient::Type::HTTP2, ipVersion(), config()) {
+  OdCdsIntegrationTestBase(const std::string& config)
+      : HttpIntegrationTest(Http::CodecClient::Type::HTTP2, ipVersion(), config) {
     use_lds_ = false;
   }
 
-  void TearDown() override { cleanUpXdsConnection(); }
+  void TearDown() override {
+    if (doCleanUpXdsConnection_) {
+      cleanUpXdsConnection();
+    }
+  }
 
   void initialize() override {
     // Controls how many addFakeUpstream() will happen in
@@ -126,6 +130,12 @@ public:
 
   FakeStreamPtr odcds_stream_;
   envoy::config::cluster::v3::Cluster new_cluster_;
+  bool doCleanUpXdsConnection_ = true;
+};
+
+class OdCdsIntegrationTest : public OdCdsIntegrationTestBase {
+public:
+  OdCdsIntegrationTest() : OdCdsIntegrationTestBase(config()) {}
 };
 
 INSTANTIATE_TEST_SUITE_P(IpVersionsClientType, OdCdsIntegrationTest,
@@ -165,6 +175,114 @@ TEST_P(OdCdsIntegrationTest, OnDemandClusterDiscoveryWorksWithClusterHeader) {
 
   response->waitForHeaders();
   EXPECT_EQ("200", response->headers().getStatusValue());
+
+  cleanupUpstreamAndDownstream();
+}
+
+const std::string& configODCDSVhostEnabledRouteDisabled() {
+  CONSTRUCT_ON_FIRST_USE(std::string, fmt::format(R"EOF(
+admin:
+  access_log:
+  - name: envoy.access_loggers.file
+    typed_config:
+      "@type": type.googleapis.com/envoy.extensions.access_loggers.file.v3.FileAccessLog
+      path: "{}"
+  address:
+    socket_address:
+      address: 127.0.0.1
+      port_value: 0
+static_resources:
+  clusters:
+  - name: xds_cluster
+    type: STATIC
+    typed_extension_protocol_options:
+      envoy.extensions.upstreams.http.v3.HttpProtocolOptions:
+        "@type": type.googleapis.com/envoy.extensions.upstreams.http.v3.HttpProtocolOptions
+        explicit_http_config:
+          http2_protocol_options: {{}}
+    load_assignment:
+      cluster_name: xds_cluster
+      endpoints:
+      - lb_endpoints:
+        - endpoint:
+            address:
+              socket_address:
+                address: 127.0.0.1
+                port_value: 0
+  listeners:
+  - name: http
+    address:
+      socket_address:
+        address: 127.0.0.1
+        port_value: 0
+    filter_chains:
+    - filters:
+      - name: http
+        typed_config:
+          "@type": type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager
+          stat_prefix: config_test
+          http_filters:
+          - name: envoy.filters.http.on_demand
+          - name: envoy.filters.http.router
+          codec_type: HTTP2
+          route_config:
+            name: local_route
+            virtual_hosts:
+            - name: local_service
+              domains: ["*"]
+              typed_per_filter_config:
+                envoy.filters.http.on_demand:
+                  "@type": type.googleapis.com/envoy.extensions.filters.http.on_demand.v3.PerRouteConfig
+                  odcds_config:
+                    resource_api_version: V3
+                    api_config_source:
+                      api_type: DELTA_GRPC
+                      transport_api_version: V3
+                      grpc_services:
+                        envoy_grpc:
+                          cluster_name: xds_cluster
+              routes:
+              - match: {{ prefix: "/" }}
+                route:
+                  cluster_header: "Pick-This-Cluster"
+                typed_per_filter_config:
+                  envoy.filters.http.on_demand:
+                    "@type": type.googleapis.com/envoy.extensions.filters.http.on_demand.v3.PerRouteConfig
+)EOF",
+                                                  Platform::null_device_path));
+}
+
+class OdCdsIntegrationTestDisabled : public OdCdsIntegrationTestBase {
+public:
+  OdCdsIntegrationTestDisabled()
+      : OdCdsIntegrationTestBase(configODCDSVhostEnabledRouteDisabled()) {
+    doCleanUpXdsConnection_ = false;
+  }
+};
+
+INSTANTIATE_TEST_SUITE_P(IpVersionsClientType, OdCdsIntegrationTestDisabled,
+                         GRPC_CLIENT_INTEGRATION_PARAMS);
+
+// tests a scenario when:
+//  - ODCDS is enabled at a virtual host level
+//  - ODCDS is disabled at a route level
+//  - making a request to an unknown cluster
+//  - request fails
+TEST_P(OdCdsIntegrationTestDisabled, DisablingODCDSAtRouteLevelWorks) {
+  initialize();
+  codec_client_ = makeHttpConnection(makeClientConnection(lookupPort("http")));
+  Http::TestRequestHeaderMapImpl request_headers{{":method", "GET"},
+                                                 {":path", "/"},
+                                                 {":scheme", "http"},
+                                                 {":authority", "vhost.first"},
+                                                 {"Pick-This-Cluster", "new_cluster"}};
+  IntegrationStreamDecoderPtr response = codec_client_->makeHeaderOnlyRequest(request_headers);
+
+  EXPECT_FALSE(fake_upstreams_[0]->waitForHttpConnection(*dispatcher_, xds_connection_,
+                                                         std::chrono::milliseconds(1000)));
+
+  response->waitForHeaders();
+  EXPECT_EQ("503", response->headers().getStatusValue());
 
   cleanupUpstreamAndDownstream();
 }
