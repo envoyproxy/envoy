328f644cb2a20686fe7e72f510c0c053f23fd8fb
Krzesimir Nowak <krzesimir@kinvolk.io>
2021-04-01 16:17:10 +0200

    CDS: Document the helper class
    Signed-off-by: Krzesimir Nowak <krzesimir@kinvolk.io>

--- source/common/upstream/cds_api_helper.h
+++ source/common/upstream/cds_api_helper.h
@@ -12,9 +12,23 @@
 namespace Envoy {
 namespace Upstream {
 
+/**
+ * A named helper class for handling a successful cluster configuration update from Subscription. A
+ * name is used mostly for logging to differentiate between different users of the helper class.
+ */
 class CdsApiHelper : Logger::Loggable<Logger::Id::upstream> {
 public:
   CdsApiHelper(ClusterManager& cm, std::string name) : cm_(cm), name_(std::move(name)) {}
+  /**
+   * onConfigUpdate handles the addition and removal of clusters by notifying the ClusterManager
+   * about the cluster changes. It closely follows the onConfigUpdate API from
+   * Config::SubscriptionCallbacks, with the exception of the return value documented below.
+   *
+   * @param added_resources clusters newly added since the previous fetch.
+   * @param removed_resources names of clusters that this fetch instructed to be removed.
+   * @param system_version_info aggregate response data "version", for debugging.
+   * @return std::vector<std::string> a list of errors that occurred while updating the clusters.
+   */
   std::vector<std::string>
   onConfigUpdate(const std::vector<Config::DecodedResourceRef>& added_resources,
                  const Protobuf::RepeatedPtrField<std::string>& removed_resources,
