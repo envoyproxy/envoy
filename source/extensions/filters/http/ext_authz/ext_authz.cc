0314715ee7cfaa72b57aa592b13d89f4b90cb9c1
Rama Chavali <rama.rao@salesforce.com>
2021-04-02 18:52:34 +0530

    unnecessary removal
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- source/extensions/filters/http/ext_authz/ext_authz.cc
+++ source/extensions/filters/http/ext_authz/ext_authz.cc
@@ -93,8 +93,6 @@ Http::FilterHeadersStatus Filter::decodeHeaders(Http::RequestHeaderMap& headers,
                        *decoder_callbacks_);
       decoder_callbacks_->streamInfo().setResponseFlag(
           StreamInfo::ResponseFlag::UnauthorizedExternalService);
-      std::cout << "calling send local reply"
-                << "\n";
       decoder_callbacks_->sendLocalReply(config_->statusOnError(), EMPTY_STRING, nullptr,
                                          absl::nullopt, RcDetails::get().AuthzError);
       return Http::FilterHeadersStatus::StopIteration;
@@ -303,8 +301,6 @@ void Filter::onComplete(Filters::Common::ExtAuthz::ResponsePtr&& response) {
                                              false};
       config_->httpContext().codeStats().chargeResponseStat(info);
     }
-    std::cout << "calling send local reply"
-              << "\n";
 
     decoder_callbacks_->sendLocalReply(
         response->status_code, response->body,
