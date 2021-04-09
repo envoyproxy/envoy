129b0bfe890c4b0e4969c1715a4810af49fc7dfb
Raul Gutierrez Segales <rgs@pinterest.com>
2021-04-09 09:16:46 -0400

    Expand integration test
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>

--- test/integration/original_ip_detection_integration_test.cc
+++ test/integration/original_ip_detection_integration_test.cc
@@ -11,35 +11,45 @@ using testing::HasSubstr;
 namespace Envoy {
 namespace Formatter {
 
-class OriginalIPDetectionIntegrationTest : public testing::Test, public HttpIntegrationTest {
+class OriginalIPDetectionIntegrationTest
+    : public testing::TestWithParam<Network::Address::IpVersion>,
+      public HttpIntegrationTest {
 public:
   OriginalIPDetectionIntegrationTest()
-      : HttpIntegrationTest(Http::CodecClient::Type::HTTP1, Network::Address::IpVersion::v4) {}
+      : HttpIntegrationTest(Http::CodecClient::Type::HTTP1, GetParam()) {}
+
+  void runTest(const std::string& ip) {
+    useAccessLog("%DOWNSTREAM_REMOTE_ADDRESS_WITHOUT_PORT%");
+    config_helper_.addConfigModifier(
+        [&](envoy::extensions::filters::network::http_connection_manager::v3::HttpConnectionManager&
+                hcm) -> void {
+          envoy::extensions::http::original_ip_detection::custom_header::v3::CustomHeaderConfig
+              config;
+          config.set_header_name("x-cdn-detected-ip");
+
+          auto* extension = hcm.add_original_ip_detection_extensions();
+          extension->set_name("envoy.http.original_ip_detection.custom_header");
+          extension->mutable_typed_config()->PackFrom(config);
+
+          hcm.mutable_use_remote_address()->set_value(false);
+        });
+    initialize();
+    auto raw_http =
+        fmt::format("GET / HTTP/1.1\r\nHost: host\r\nx-cdn-detected-ip: {}\r\n\r\n", ip);
+    std::string response;
+    sendRawHttpAndWaitForResponse(lookupPort("http"), raw_http.c_str(), &response, true);
+    std::string log = waitForAccessLog(access_log_name_);
+    EXPECT_THAT(log, HasSubstr(ip));
+  }
 };
 
-TEST_F(OriginalIPDetectionIntegrationTest, HeaderBasedDetection) {
-  useAccessLog("%DOWNSTREAM_REMOTE_ADDRESS_WITHOUT_PORT%");
-  config_helper_.addConfigModifier(
-      [&](envoy::extensions::filters::network::http_connection_manager::v3::HttpConnectionManager&
-              hcm) -> void {
-        envoy::extensions::http::original_ip_detection::custom_header::v3::CustomHeaderConfig
-            config;
-        config.set_header_name("x-cdn-detected-ip");
+INSTANTIATE_TEST_SUITE_P(IpVersions, OriginalIPDetectionIntegrationTest,
+                         testing::ValuesIn(TestEnvironment::getIpVersionsForTest()),
+                         TestUtility::ipTestParamsToString);
 
-        auto* extension = hcm.add_original_ip_detection_extensions();
-        extension->set_name("envoy.http.original_ip_detection.custom_header");
-        extension->mutable_typed_config()->PackFrom(config);
+TEST_P(OriginalIPDetectionIntegrationTest, HeaderBasedDetectionIPv4) { runTest("9.9.9.9"); }
 
-        hcm.mutable_use_remote_address()->set_value(false);
-      });
-  initialize();
-  std::string response;
-  sendRawHttpAndWaitForResponse(
-      lookupPort("http"), "GET / HTTP/1.1\r\nHost: host\r\nx-cdn-detected-ip: 9.9.9.9\r\n\r\n",
-      &response, true);
-  std::string log = waitForAccessLog(access_log_name_);
-  EXPECT_THAT(log, HasSubstr("9.9.9.9"));
-}
+TEST_P(OriginalIPDetectionIntegrationTest, HeaderBasedDetectionIPv6) { runTest("fc00::1"); }
 
 } // namespace Formatter
 } // namespace Envoy
