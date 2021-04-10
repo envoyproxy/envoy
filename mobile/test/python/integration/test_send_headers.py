98758f1811bca667633ce82959bccdebc4d64833
Cerek Hillen <crockeo@users.noreply.github.com>
2021-04-09 18:32:12 -0700

    python: allow Engine to take a custom config template (#1339)
    Description: Pipes through a `config_template` argument that can be used to override the default config template provided OOTB. This will be useful for
    Risk Level: Low
    Testing: Migrated `test_send_headers.py` to use config template + be more like Swift test.
    Docs Changes: N/A
    Release Notes: N/A
    
    Signed-off-by: Cerek Hillen <chillen@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/test/python/integration/test_send_headers.py
+++ mobile/test/python/integration/test_send_headers.py
@@ -4,11 +4,53 @@ import unittest
 from library.python import envoy_engine
 
 
+API_LISTENER_TYPE = "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager"
+ASSERTION_FILTER_TYPE = "type.googleapis.com/envoymobile.extensions.filters.http.assertion.Assertion"
+
+CONFIG_TEMPLATE = f"""\
+static_resources:
+  listeners:
+  - name: base_api_listener
+    address:
+      socket_address:
+        protocol: TCP
+        address: 0.0.0.0
+        port_value: 10000
+    api_listener:
+      api_listener:
+        "@type": {API_LISTENER_TYPE}
+        stat_prefix: hcm
+        route_config:
+          name: api_router
+          virtual_hosts:
+            - name: api
+              domains:
+                - "*"
+              routes:
+                - match:
+                    prefix: "/"
+                  direct_response:
+                    status: 200
+        http_filters:
+          - name: envoy.filters.http.assertion
+            typed_config:
+              "@type": {ASSERTION_FILTER_TYPE}
+              match_config:
+                http_request_headers_match:
+                  headers:
+                    - name: ":authority"
+                      exact_match: example.com
+          - name: envoy.router
+            typed_config:
+              "@type": type.googleapis.com/envoy.extensions.filters.http.router.v3.Router
+"""
+
+
 class TestSendHeaders(unittest.TestCase):
     def test_send_headers(self):
         engine_running = threading.Event()
         engine = (
-            envoy_engine.EngineBuilder()
+            envoy_engine.EngineBuilder(CONFIG_TEMPLATE)
             .add_log_level(envoy_engine.LogLevel.Error)
             .set_on_engine_running(lambda: engine_running.set())
             .build()
@@ -16,15 +58,12 @@ class TestSendHeaders(unittest.TestCase):
         engine_running.wait()
 
         response_lock = threading.Lock()
-        response = {"status": None, "data": bytearray()}
+        response = {"status": None, "end_stream": None}
 
-        def _on_headers(response_headers: envoy_engine.ResponseHeaders, _: bool):
+        def _on_headers(response_headers: envoy_engine.ResponseHeaders, end_stream: bool):
             with response_lock:
                 response["status"] = response_headers.http_status()
-
-        def _on_data(response_data: bytes, _: bool):
-            with response_lock:
-                response["data"].extend(response_data)
+                response["end_stream"] = end_stream
 
         stream_complete = threading.Event()
         stream = (
@@ -32,7 +71,6 @@ class TestSendHeaders(unittest.TestCase):
             .stream_client()
             .new_stream_prototype()
             .set_on_headers(_on_headers)
-            .set_on_data(_on_data)
             # unused:
             # .set_on_metadata(on_metadata)
             # .set_on_trailers(on_trailers)
@@ -45,16 +83,19 @@ class TestSendHeaders(unittest.TestCase):
             envoy_engine.RequestHeadersBuilder(
                 envoy_engine.RequestMethod.GET,
                 "https",
-                "api.lyft.com",
-                "/ping",
+                "example.com",
+                "/test",
             )
+            .add_upstream_http_protocol(envoy_engine.UpstreamHttpProtocol.HTTP2)
             .build(),
             True,
         )
         stream_complete.wait()
 
-        assert response["status"] == 200
-        assert bytes(response["data"]) == b"{}"
+        assert response == {
+            "status": 200,
+            "end_stream": True,
+        }
 
 if __name__ == "__main__":
     unittest.main()
