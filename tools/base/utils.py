257c50712bc61a8732eda72d07d14f68e8d7fa06
Ryan Northey <ryan@synca.io>
2021-04-12 13:03:41 +0100

    python: Integrate linting tools
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tools/base/utils.py
+++ tools/base/utils.py
@@ -2,10 +2,11 @@
 # Provides shared utils used by other python modules
 #
 
+import io
 import os
 import tempfile
 from configparser import ConfigParser
-from contextlib import contextmanager
+from contextlib import ExitStack, contextmanager, redirect_stderr, redirect_stdout
 from typing import Iterator
 
 
@@ -26,3 +27,42 @@ def coverage_with_data_file(data_file: str) -> Iterator[str]:
         with open(tmprc, "w") as f:
             parser.write(f)
         yield tmprc
+
+
+class BufferUtilError(Exception):
+    pass
+
+
+@contextmanager
+def nested(*contexts):
+    with ExitStack() as stack:
+        yield [stack.enter_context(context) for context in contexts]
+
+
+@contextmanager
+def buffered(stdout: list = None, stderr: list = None, mangle=None) -> None:
+    """Captures stdout and stderr and feeds lines to supplied lists"""
+
+    mangle = mangle or (lambda lines: lines)
+
+    if stdout is None and stderr is None:
+        raise BufferUtilError("You must specify stdout and/or stderr")
+
+    contexts = []
+
+    if stdout is not None:
+        _stdout = io.StringIO()
+        contexts.append(redirect_stdout(_stdout))
+    if stderr is not None:
+        _stderr = io.StringIO()
+        contexts.append(redirect_stderr(_stderr))
+
+    with nested(*contexts):
+        yield
+
+    if stdout is not None:
+        _stdout.seek(0)
+        stdout.extend(mangle(_stdout.read().strip().split("\n")))
+    if stderr is not None:
+        _stderr.seek(0)
+        stderr.extend(mangle(_stderr.read().strip().split("\n")))
