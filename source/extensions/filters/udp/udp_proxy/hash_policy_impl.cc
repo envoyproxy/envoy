78f4b2c39240257609cea60a1db3fcf4a2bd81f7
davidkornel <konel1020@gmail.com>
2021-04-14 13:11:11 +0200

    udp: Extend hash policies with key based hashing
    Co-authored-by: DongRyeol Cha <dr83.cha@samsung.com>
    Signed-off-by: Kornel David <konel1020@gmail.com>

--- source/extensions/filters/udp/udp_proxy/hash_policy_impl.cc
+++ source/extensions/filters/udp/udp_proxy/hash_policy_impl.cc
@@ -20,12 +20,33 @@ public:
   }
 };
 
+class KeyHashMethod : public HashPolicyImpl::HashMethod {
+public:
+  KeyHashMethod(const std::string& key) : Key(key) {}
+  const std::string& Key;
+
+  absl::optional<uint64_t>
+  evaluate(const Network::Address::Instance& downstream_addr) const override {
+    (void)downstream_addr;
+    // ASSERT(!Key.empty());
+    if (!Key.empty()) {
+      uint64_t hash = HashUtil::xxHash64(Key);
+      return hash;
+    }
+
+    return absl::nullopt;
+  }
+};
+
 HashPolicyImpl::HashPolicyImpl(
     const absl::Span<const UdpProxyConfig::HashPolicy* const>& hash_policies) {
   ASSERT(hash_policies.size() == 1);
   switch (hash_policies[0]->policy_specifier_case()) {
   case UdpProxyConfig::HashPolicy::PolicySpecifierCase::kSourceIp:
     hash_impl_ = std::make_unique<SourceIpHashMethod>();
+    break;
+  case UdpProxyConfig::HashPolicy::PolicySpecifierCase::kKey:
+    hash_impl_ = std::make_unique<KeyHashMethod>(hash_policies[0]->key());
     break;
   default:
     NOT_REACHED_GCOVR_EXCL_LINE;
