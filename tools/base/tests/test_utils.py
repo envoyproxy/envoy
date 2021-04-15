6be06863959416ba3aa5ae2dfbe67a63b69c07dd
phlax <phlax@users.noreply.github.com>
2021-04-15 19:47:47 +0100

    python: Integrate linting tools (#15922)
    Signed-off-by: Ryan Northey <ryan@synca.io>
    Signed-off-by: Gokul Nair <gnair@twitter.com>

--- tools/base/tests/test_utils.py
+++ tools/base/tests/test_utils.py
@@ -1,4 +1,8 @@
 import importlib
+import sys
+from contextlib import contextmanager
+
+import pytest
 
 from tools.base import utils
 
@@ -6,6 +10,76 @@ from tools.base import utils
 # this is necessary to fix coverage as these libs are imported before pytest
 # is invoked
 importlib.reload(utils)
+
+
+def test_util_buffered_stdout():
+    stdout = []
+
+    with utils.buffered(stdout=stdout):
+        print("test1")
+        print("test2")
+        sys.stdout.write("test3\n")
+        sys.stderr.write("error0\n")
+
+    assert stdout == ["test1", "test2", "test3"]
+
+
+def test_util_buffered_stderr():
+    stderr = []
+
+    with utils.buffered(stderr=stderr):
+        print("test1")
+        print("test2")
+        sys.stdout.write("test3\n")
+        sys.stderr.write("error0\n")
+        sys.stderr.write("error1\n")
+
+    assert stderr == ["error0", "error1"]
+
+
+def test_util_buffered_stdout_stderr():
+    stdout = []
+    stderr = []
+
+    with utils.buffered(stdout=stdout, stderr=stderr):
+        print("test1")
+        print("test2")
+        sys.stdout.write("test3\n")
+        sys.stderr.write("error0\n")
+        sys.stderr.write("error1\n")
+
+    assert stdout == ["test1", "test2", "test3"]
+    assert stderr == ["error0", "error1"]
+
+
+def test_util_buffered_no_stdout_stderr():
+
+    with pytest.raises(utils.BufferUtilError):
+        with utils.buffered():
+            pass
+
+
+def test_util_nested():
+
+    fun1_args = []
+    fun2_args = []
+
+    @contextmanager
+    def fun1(arg):
+        fun1_args.append(arg)
+        yield "FUN1"
+
+    @contextmanager
+    def fun2(arg):
+        fun2_args.append(arg)
+        yield "FUN2"
+
+    with utils.nested(fun1("A"), fun2("B")) as (fun1_yield, fun2_yield):
+        assert fun1_yield == "FUN1"
+        assert fun2_yield == "FUN2"
+
+    assert fun1_args == ["A"]
+    assert fun2_args == ["B"]
 
 
 def test_util_coverage_with_data_file(patches):
