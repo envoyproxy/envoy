7931b55c85d7992a4a438bca3c980505eb367edd
Ryan Northey <ryan@synca.io>
2021-04-19 10:10:11 +0100

    tools/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tools/code_format/rst_check.py
+++ tools/code_format/rst_check.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import argparse
+import configparser
 import multiprocessing
 import re
 import os
@@ -7,8 +9,10 @@ import shutil
 import subprocess
 import sys
 import tempfile
-from contextlib import contextmanager
+from contextlib import closing, contextmanager
+from functools import cached_property
 
+import docutils
 import rstcheck
 import sphinx
 
@@ -40,94 +44,113 @@ def sphinx_enabled():
         shutil.rmtree(srcdir)
 
 
-def _check_file(parameters):
-    """Return list of errors."""
-    (filename, args) = parameters
+def _check_rst_file(params: tuple) -> list:
+    """Check an rst file and return list of errors.
 
-    if filename == '-':
-        contents = sys.stdin.read()
-    else:
-        with contextlib.closing(
-            docutils.io.FileInput(source_path=filename)
-        ) as input_file:
-            contents = input_file.read()
+    This function *must* be module level in order to be pickled
+    for multiprocessing
+    """
+    filename, args = params
 
-    args = load_configuration_from_file(
-        os.path.dirname(os.path.realpath(filename)), args)
+    with closing(docutils.io.FileInput(source_path=filename)) as input_file:
+        contents = input_file.read()
 
-    ignore_directives_and_roles(args.ignore_directives, args.ignore_roles)
+    rstcheck.ignore_directives_and_roles(
+        args.ignore_directives, args.ignore_roles)
 
     for substitution in args.ignore_substitutions:
-        contents = contents.replace('|{}|'.format(substitution), 'None')
+        contents = contents.replace(f"|{substitution}|", "None")
 
     ignore = {
-        'languages': args.ignore_language,
-        'messages': args.ignore_messages,
-    }
-    all_errors = []
-    for error in check(contents,
-                       filename=filename,
-                       report_level=args.report,
-                       ignore=ignore,
-                       debug=args.debug):
-        all_errors.append(error)
-    return (filename, all_errors)
+        "languages": args.ignore_language,
+        "messages": args.ignore_messages}
+    return filename, list(
+        rstcheck.check(
+            contents,
+            filename=filename,
+            report_level=args.report,
+            ignore=ignore))
 
 
 class RstChecker(checker.ForkingChecker):
     checks = ("greps", "rstcheck")
 
-    def check_greps(self):
-        for check in RSTCHECK_GREP_FAIL:
-            self.grep_check(check)
+    @property
+    def failing_regexes(self) -> tuple:
+        return RSTCHECK_GREP_FAIL
 
-    def check_rstcheck(self):
-        self.run_rstcheck(list(RSTCHECK_COMMAND) + [self.path])
+    @cached_property
+    def rstcheck_args(self) -> argparse.Namespace:
+        """Return new ``args`` with configuration loaded from file."""
+        args = argparse.Namespace()
+        args.report = self.rstcheck_report_level
+        for ignore in ["language", "directives", "substitutions", "roles"]:
+            setattr(
+                args,
+                f"ignore_{ignore}",
+                [o.strip()
+                 for o in self.rstcheck_config.get(f"ignore_{ignore}", "").split(",")
+                 if o.strip()])
+        args.ignore_messages = self.rstcheck_config.get("ignore_messages", "")
+        return args
 
-    def grep_check(self, check):
+    @cached_property
+    def rstcheck_config(self) -> dict:
+        parser = configparser.ConfigParser()
+        parser.read(rstcheck.find_config(self.path))
+        try:
+            return dict(parser.items("rstcheck"))
+        except configparser.NoSectionError:
+            return {}
+
+    @cached_property
+    def rstcheck_report_level(self) -> int:
+        report = self.rstcheck_config.get("report", "info")
+        threshold_dictionary = docutils.frontend.OptionParser.thresholds
+        return int(threshold_dictionary.get(report, report))
+
+    @cached_property
+    def rst_files(self) -> list:
+        return list(
+            rstcheck.find_files(
+                filenames=self.paths,
+                recursive=True))
+
+    def check_greps(self) -> None:
+        for check in self.failing_regexes:
+            self.grep_unwanted(check)
+
+    def check_rstcheck(self) -> None:
+        with sphinx_enabled():
+            try:
+                self.parallel(
+                    _check_rst_file,
+                    [(path, self.rstcheck_args) for path in self.rst_files],
+                    self._rstcheck_handle)
+            except (IOError, UnicodeError) as e:
+                self.error("rstcheck", [f"Rstcheck failed: {e}"])
+
+    def grep_unwanted(self, check: str) -> None:
         response = self.fork(["grep", "-nr", "--include", "\\*.rst"] + [check])
         if not response.returncode:
             self.error("grep", [f"grepping found bad '{check}': {path}" for path in resp.stdout.split("\n")])
         else:
             self.succeed("grep", [f"grepping found no errors for '{check}'"])
 
-    def run_rstcheck(self, command):
-        """Return 0 on success.
-
-        this is copied from rstcheck to capture stderr
-
-        """
-        paths = list(
-            rstcheck.find_files(
-                filenames=self.paths,
-                recursive=True))
-        breakpoint()
-
-        with sphinx_enabled():
-            try:
-                self.parallel(
-                    rstcheck._check_file,
-                    [(path, []) for path in paths],
-                    self._handle)
-            except (IOError, UnicodeError) as e:
-                self.error("rstcheck", [f"Rstcheck failed: {e}"])
-
-    def _handle(self, results):
+    def _rstcheck_handle(self, results: list) -> None:
+        """Handle multiprocessed error results of rstchecks"""
         for (filename, _errors) in results:
-            for error in _errors:
-                line_number = error[0]
-                message = error[1]
-
+            for (line_number, message) in _errors:
                 if not re.match(r'\([A-Z]+/[0-9]+\)', message):
                     message = '(ERROR/3) ' + message
+                self.error(
+                    "rstcheck",
+                    [f"{self._strip_line(filename)}:{line_number}: {message}"])
+            else:
+                self.succeed("rstcheck", [f"No errors for: {filename}"])
 
-                self.error("rstcheck", [f"{self._strip_line(filename)}:{line_number}: {message}"])
-
-    def _strip_line(self, line) -> str:
+    def _strip_line(self, line: str) -> str:
         return line[len(self.path) + 1:] if line.startswith(f"{self.path}/") else line
-
-    def _strip_lines(self, lines) -> list:
-        return [self._strip_line(line) for line in lines if line]
 
 
 def main(*args: list) -> None:
