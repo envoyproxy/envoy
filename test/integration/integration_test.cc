d58d0b39c93c32682df791235e4507d26f0a1b9d
Alex Konradi <akonradi@google.com>
2021-04-19 11:27:18 -0400

    Fix formatting
    Signed-off-by: Alex Konradi <akonradi@google.com>

--- test/integration/integration_test.cc
+++ test/integration/integration_test.cc
@@ -964,9 +964,9 @@ TEST_P(IntegrationTest, PipelineWithTrailers) {
 // an inline sendLocalReply to make sure the "kick" works under the call stack
 // of dispatch as well as when a response is proxied from upstream.
 TEST_P(IntegrationTest, PipelineInline) {
-  config_helper_.addConfigModifier([]( envoy::extensions::filters::network::http_connection_manager::v3::HttpConnectionManager& hcm) {
-    hcm.mutable_stream_error_on_invalid_http_message()->set_value(true);
-  });
+  config_helper_.addConfigModifier(
+      [](envoy::extensions::filters::network::http_connection_manager::v3::HttpConnectionManager&
+             hcm) { hcm.mutable_stream_error_on_invalid_http_message()->set_value(true); });
 
   autonomous_upstream_ = true;
   initialize();
