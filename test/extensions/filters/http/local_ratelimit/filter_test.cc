273f0e1e81905a645ef32fabde860b8d2198a64d
Gokul Nair <gnair@twitter.com>
2021-04-20 13:58:19 -0700

    add tests
    Signed-off-by: Gokul Nair <gnair@twitter.com>

--- test/extensions/filters/http/local_ratelimit/filter_test.cc
+++ test/extensions/filters/http/local_ratelimit/filter_test.cc
@@ -34,11 +34,15 @@ response_headers_to_add:
     header:
       key: x-test-rate-limit
       value: 'true'
+<<<<<<< HEAD
 request_headers_to_add_when_not_enforced:
   - append: false
     header:
       key: x-local-ratelimited
       value: 'true'
+=======
+per_connection: true
+>>>>>>> 4183f6c3f... add tests
   )";
 
 class FilterTest : public testing::Test {
@@ -64,6 +68,12 @@ public:
                                              per_route);
     filter_ = std::make_shared<Filter>(config_);
     filter_->setDecoderFilterCallbacks(decoder_callbacks_);
+
+    filter_2_ = std::make_shared<Filter>(config_);
+    filter_2_->setDecoderFilterCallbacks(decoder_callbacks_2_);
+
+    filter_3_ = std::make_shared<Filter>(config_);
+    filter_3_->setDecoderFilterCallbacks(decoder_callbacks_);
   }
   void setup(const std::string& yaml, const bool enabled = true, const bool enforced = true) {
     setupPerRoute(yaml, enabled, enforced);
@@ -78,11 +88,14 @@ public:
 
   Stats::IsolatedStoreImpl stats_;
   testing::NiceMock<Http::MockStreamDecoderFilterCallbacks> decoder_callbacks_;
+  testing::NiceMock<Http::MockStreamDecoderFilterCallbacks> decoder_callbacks_2_;
   NiceMock<Event::MockDispatcher> dispatcher_;
   NiceMock<Runtime::MockLoader> runtime_;
   NiceMock<LocalInfo::MockLocalInfo> local_info_;
   std::shared_ptr<FilterConfig> config_;
   std::shared_ptr<Filter> filter_;
+  std::shared_ptr<Filter> filter_2_;
+  std::shared_ptr<Filter> filter_3_;
 };
 
 TEST_F(FilterTest, Runtime) {
@@ -107,9 +120,11 @@ TEST_F(FilterTest, RequestOk) {
   setup(fmt::format(config_yaml, "1"));
   auto headers = Http::TestRequestHeaderMapImpl();
   EXPECT_EQ(Http::FilterHeadersStatus::Continue, filter_->decodeHeaders(headers, false));
-  EXPECT_EQ(1U, findCounter("test.http_local_rate_limit.enabled"));
+  EXPECT_EQ(Http::FilterHeadersStatus::Continue, filter_2_->decodeHeaders(headers, false));
+  EXPECT_EQ(2U, findCounter("test.http_local_rate_limit.enabled"));
   EXPECT_EQ(0U, findCounter("test.http_local_rate_limit.enforced"));
-  EXPECT_EQ(1U, findCounter("test.http_local_rate_limit.ok"));
+  EXPECT_EQ(2U, findCounter("test.http_local_rate_limit.ok"));
+  EXPECT_EQ(0U, findCounter("test.http_local_rate_limit.rate_limited"));
 }
 
 TEST_F(FilterTest, RequestRateLimited) {
@@ -181,6 +196,7 @@ response_headers_to_add:
     header:
       key: x-test-rate-limit
       value: 'true'
+per_connection: true
 descriptors:
 - entries:
    - key: hello
