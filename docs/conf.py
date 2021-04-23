63bd0e78dbf033d6da1d67a34399f71aa7d0a86f
Long Dai <long0dai@foxmail.com>
2021-04-23 15:36:04 +0800

    enable extensions
    Signed-off-by: Long Dai <long0dai@foxmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,8 @@ sys.path.append(os.path.abspath("./_ext"))
 
 extensions = [
     'sphinxcontrib.httpdomain', 'sphinx.ext.extlinks', 'sphinx.ext.ifconfig', 'sphinx_tabs.tabs',
-    'sphinx_copybutton', 'validating_code_block', 'sphinxext.rediraffe', 'powershell_lexer'
+    'sphinx_copybutton', 'validating_code_block', 'sphinxext.rediraffe', 'powershell_lexer',
+    'sphinx_search.extension'
 ]
 extlinks = {
     'repo': ('https://github.com/envoyproxy/envoy/blob/{}/%s'.format(blob_sha), ''),
