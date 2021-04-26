9e43177c7c645e5e8192d737e77f8aa035ed1f2d
alyssawilk <alyssar@chromium.org>
2021-04-26 14:22:01 -0400

    http: removing envoy.reloadable_features.unify_grpc_handling (#16173)
    Risk Level: Low (removing deprecated flag)
    Testing: n/a
    Docs Changes: n/a
    Release Notes: inline
    Fixes #16014
    
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>
    Signed-off-by: Gokul Nair <gnair@twitter.com>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -91,7 +91,6 @@ constexpr const char* runtime_features[] = {
     "envoy.reloadable_features.use_observable_cluster_name",
     "envoy.reloadable_features.vhds_heartbeats",
     "envoy.reloadable_features.wasm_cluster_name_envoy_grpc",
-    "envoy.reloadable_features.unify_grpc_handling",
     "envoy.reloadable_features.upstream_http2_flood_checks",
     "envoy.restart_features.use_apple_api_for_dns_lookups",
     "envoy.reloadable_features.header_map_correctly_coalesce_cookies",
