c8afa374b825abbca52b71a4842db8e2f2c460d5
alyssawilk <alyssar@chromium.org>
2021-04-28 13:28:01 -0400

    http: removing envoy.reloadable_features.http_set_copy_replace_all_headers (#16209)
    Risk Level: low
    Testing: n/a
    Docs Changes: n/a
    Release Notes: inline
    Fixes #16018
    
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/common/http/header_map_impl_test.cc
+++ test/common/http/header_map_impl_test.cc
@@ -794,55 +794,7 @@ TEST_P(HeaderMapImplTest, SetReferenceKey) {
   EXPECT_EQ("monde", headers.get(foo)[0]->value().getStringView());
 }
 
-TEST_P(HeaderMapImplTest, SetCopyOldBehavior) {
-  Runtime::LoaderSingleton::getExisting()->mergeValues(
-      {{"envoy.reloadable_features.http_set_copy_replace_all_headers", "false"}});
-
-  TestRequestHeaderMapImpl headers;
-  LowerCaseString foo("hello");
-  headers.setCopy(foo, "world");
-  EXPECT_EQ("world", headers.get(foo)[0]->value().getStringView());
-
-  // Overwrite value.
-  headers.setCopy(foo, "monde");
-  EXPECT_EQ("monde", headers.get(foo)[0]->value().getStringView());
-
-  // Add another foo header.
-  headers.addCopy(foo, "monde2");
-  EXPECT_EQ(headers.size(), 2);
-
-  // Only the first foo header is overridden.
-  headers.setCopy(foo, "override-monde");
-  EXPECT_EQ(headers.size(), 2);
-
-  HeaderAndValueCb cb;
-
-  InSequence seq;
-  EXPECT_CALL(cb, Call("hello", "override-monde"));
-  EXPECT_CALL(cb, Call("hello", "monde2"));
-  headers.iterate(cb.asIterateCb());
-
-  // Test setting an empty string and then overriding.
-  EXPECT_EQ(2UL, headers.remove(foo));
-  EXPECT_EQ(headers.size(), 0);
-  const std::string empty;
-  headers.setCopy(foo, empty);
-  EXPECT_EQ(headers.size(), 1);
-  headers.setCopy(foo, "not-empty");
-  EXPECT_EQ(headers.get(foo)[0]->value().getStringView(), "not-empty");
-
-  // Use setCopy with inline headers both indirectly and directly.
-  headers.clear();
-  EXPECT_EQ(headers.size(), 0);
-  headers.setCopy(Headers::get().Path, "/");
-  EXPECT_EQ(headers.size(), 1);
-  EXPECT_EQ(headers.getPathValue(), "/");
-  headers.setPath("/foo");
-  EXPECT_EQ(headers.size(), 1);
-  EXPECT_EQ(headers.getPathValue(), "/foo");
-}
-
-TEST_P(HeaderMapImplTest, SetCopyNewBehavior) {
+TEST_P(HeaderMapImplTest, SetCopy) {
   TestRequestHeaderMapImpl headers;
   LowerCaseString foo("hello");
   headers.setCopy(foo, "world");
