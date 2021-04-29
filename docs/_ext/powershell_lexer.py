eeae84a039b7684eaf39a8a17438add774eb1a54
Sotiris Nanopoulos <sonanopo@microsoft.com>
2021-04-29 16:20:19 -0700

    fix warning in the docs
    Signed-off-by: Sotiris Nanopoulos <sonanopo@microsoft.com>

--- docs/_ext/powershell_lexer.py
+++ docs/_ext/powershell_lexer.py
@@ -1,5 +1,4 @@
-from pygments.lexers import get_lexer_by_name
-
+from pygments.lexers import get_lexer_by_name, PowerShellLexer
 
 def setup(app):
-    app.add_lexer('powershell', get_lexer_by_name('powershell'))
+    app.add_lexer('powershell', PowerShellLexer)
