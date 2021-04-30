935d48c6e86ceebc1ba359d2d063bf8242ef2e98
Adam Kotwasinski <adam.kotwasinski@gmail.com>
2021-04-29 17:35:56 -0700

    Add generated constants for request api keys and max version
    Signed-off-by: Adam Kotwasinski <adam.kotwasinski@gmail.com>

--- source/extensions/filters/network/kafka/mesh/command_handlers/api_versions.cc
+++ source/extensions/filters/network/kafka/mesh/command_handlers/api_versions.cc
@@ -1,5 +1,6 @@
 #include "source/extensions/filters/network/kafka/mesh/command_handlers/api_versions.h"
 
+#include "source/extensions/filters/network/kafka/external/requests.h"
 #include "source/extensions/filters/network/kafka/external/responses.h"
 
 namespace Envoy {
@@ -8,14 +9,18 @@ namespace NetworkFilters {
 namespace Kafka {
 namespace Mesh {
 
-constexpr int16_t PRODUCE_API_KEY = 0;
-// Introduced in Kafka 1.0, together with record format 2.
-constexpr int16_t MIN_PRODUCE_SUPPORTED = 5;
-constexpr int16_t MAX_PRODUCE_SUPPORTED = 8;
+// These constants define which versions of requests this "Kafka server" will understand.
 
-constexpr int16_t METADATA_KEY = 3;
+// As we can process only record format 2 (which itself is pretty old coming from Kafka 1.0), we are
+// going to handle only produce requests with versions higher than 5.
+constexpr int16_t MIN_PRODUCE_SUPPORTED = 5;
+constexpr int16_t MAX_PRODUCE_SUPPORTED = PRODUCE_REQUEST_MAX_VERSION; /* Generated value. */
+// Right now we do not want to handle old version 0 request, as it expects us to enumerate all
+// topics if list of requested topics is empty.
+// Impl note: if filter gains knowledge of upstream cluster topics (e.g. thru admin clients), we
+// could decrease this value.
 constexpr int16_t MIN_METADATA_SUPPORTED = 1;
-constexpr int16_t MAX_METADATA_SUPPORTED = 1;
+constexpr int16_t MAX_METADATA_SUPPORTED = METADATA_REQUEST_MAX_VERSION; /* Generated value. */
 
 ApiVersionsRequestHolder::ApiVersionsRequestHolder(
     AbstractRequestListener& filter, const std::shared_ptr<Request<ApiVersionsRequest>> request)
@@ -24,6 +29,7 @@ ApiVersionsRequestHolder::ApiVersionsRequestHolder(
 // Api Versions requests are immediately ready for answer (as they do not need to reach upstream).
 void ApiVersionsRequestHolder::startProcessing() { notifyFilter(); }
 
+// Because these requests can be trivially handled, they are okay to be sent downstream at any time.
 bool ApiVersionsRequestHolder::finished() const { return true; }
 
 AbstractResponseSharedPtr ApiVersionsRequestHolder::computeAnswer() const {
@@ -31,11 +37,11 @@ AbstractResponseSharedPtr ApiVersionsRequestHolder::computeAnswer() const {
   const ResponseMetadata metadata = {header.api_key_, header.api_version_, header.correlation_id_};
 
   const int16_t error_code = 0;
-  const ApiVersionsResponseKey produce_key = {PRODUCE_API_KEY, MIN_PRODUCE_SUPPORTED,
-                                              MAX_PRODUCE_SUPPORTED};
-  const ApiVersionsResponseKey metadata_key = {METADATA_KEY, MIN_METADATA_SUPPORTED,
-                                               MAX_METADATA_SUPPORTED};
-  const ApiVersionsResponse real_response = {error_code, {produce_key, metadata_key}};
+  const ApiVersionsResponseKey produce_entry = {PRODUCE_REQUEST_API_KEY, MIN_PRODUCE_SUPPORTED,
+                                                MAX_PRODUCE_SUPPORTED};
+  const ApiVersionsResponseKey metadata_entry = {METADATA_REQUEST_API_KEY, MIN_METADATA_SUPPORTED,
+                                                 MAX_METADATA_SUPPORTED};
+  const ApiVersionsResponse real_response = {error_code, {produce_entry, metadata_entry}};
 
   return std::make_shared<Response<ApiVersionsResponse>>(metadata, real_response);
 }
