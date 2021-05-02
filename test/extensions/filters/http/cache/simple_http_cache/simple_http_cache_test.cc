0cb5c4019861ca2f23e9b82c507eb5b72feab365
phlax <phlax@users.noreply.github.com>
2021-05-02 15:44:48 +0100

    protos: Move simple_http_cache config proto to api (#16230)
    Signed-off-by: Ryan Northey <ryan@synca.io>
    Signed-off-by: Gokul Nair <gnair@twitter.com>

--- test/extensions/filters/http/cache/simple_http_cache/simple_http_cache_test.cc
+++ test/extensions/filters/http/cache/simple_http_cache/simple_http_cache_test.cc
@@ -220,7 +220,7 @@ TEST_F(SimpleHttpCacheTest, StreamingPut) {
 
 TEST(Registration, GetFactory) {
   HttpCacheFactory* factory = Registry::FactoryRegistry<HttpCacheFactory>::getFactoryByType(
-      "envoy.source.extensions.filters.http.cache.SimpleHttpCacheConfig");
+      "envoy.extensions.cache.simple_http_cache.v3alpha.SimpleHttpCacheConfig");
   ASSERT_NE(factory, nullptr);
   envoy::extensions::filters::http::cache::v3alpha::CacheConfig config;
   config.mutable_typed_config()->PackFrom(*factory->createEmptyConfigProto());
