6b0235c8396d550bbfb4e0d036e7b7fa7b6f9176
Gokul Nair <gnair@twitter.com>
2021-05-03 16:43:54 -0700

    add docs and fix format
    Signed-off-by: Gokul Nair <gnair@twitter.com>

--- test/extensions/filters/http/local_ratelimit/filter_test.cc
+++ test/extensions/filters/http/local_ratelimit/filter_test.cc
@@ -194,9 +194,11 @@ TEST_F(FilterTest, RequestRateLimitedPerConnection) {
 
   EXPECT_EQ(request_headers, expected_headers);
   EXPECT_EQ(Http::FilterHeadersStatus::Continue, filter_->decodeHeaders(request_headers, false));
-  EXPECT_EQ(Http::FilterHeadersStatus::StopIteration, filter_->decodeHeaders(request_headers, false));
+  EXPECT_EQ(Http::FilterHeadersStatus::StopIteration,
+            filter_->decodeHeaders(request_headers, false));
   EXPECT_EQ(Http::FilterHeadersStatus::Continue, filter_2_->decodeHeaders(request_headers, false));
-  EXPECT_EQ(Http::FilterHeadersStatus::StopIteration, filter_2_->decodeHeaders(request_headers, false));
+  EXPECT_EQ(Http::FilterHeadersStatus::StopIteration,
+            filter_2_->decodeHeaders(request_headers, false));
   EXPECT_EQ(4U, findCounter("test.http_local_rate_limit.enabled"));
   EXPECT_EQ(2U, findCounter("test.http_local_rate_limit.enforced"));
   EXPECT_EQ(2U, findCounter("test.http_local_rate_limit.ok"));
