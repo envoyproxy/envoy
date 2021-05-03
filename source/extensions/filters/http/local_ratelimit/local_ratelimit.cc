f712bc8b65319f24208d9473a1da88f8db8f10e6
Gokul Nair <gnair@twitter.com>
2021-05-03 14:45:24 -0700

    eliminate full proto copy and store individual fields under FilterConfig as needed
    Signed-off-by: Gokul Nair <gnair@twitter.com>

--- source/extensions/filters/http/local_ratelimit/local_ratelimit.cc
+++ source/extensions/filters/http/local_ratelimit/local_ratelimit.cc
@@ -47,7 +47,7 @@ FilterConfig::FilterConfig(
       request_headers_parser_(Envoy::Router::HeaderParser::configure(
           config.request_headers_to_add_when_not_enforced())),
       stage_(static_cast<uint64_t>(config.stage())),
-      has_descriptors_(!config.descriptors().empty()), proto_config_(config) {
+      has_descriptors_(!config.descriptors().empty()) {
   // Note: no token bucket is fine for the global config, which would be the case for enabling
   //       the filter globally but disabled and then applying limits at the virtual host or
   //       route level. At the virtual or route level, it makes no sense to have an no token
