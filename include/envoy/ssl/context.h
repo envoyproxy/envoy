d6b1bdf465f0a6255c7c48fb3b6a3b7f5a1516c6
Alyssa Wilk <alyssar@chromium.org>
2021-05-04 10:22:30 -0400

    typo
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- include/envoy/ssl/context.h
+++ include/envoy/ssl/context.h
@@ -56,7 +56,7 @@ public:
                                std::string& /*error_details*/) {
     // Unlike the other functions here, this can not easily be pure virtual. Members
     // are forward-declared structs from ssl libraries, and the test mock context then
-    // doesn't have definisions for x509_st and stack_st_X509.
+    // doesn't have definitions for x509_st and stack_st_X509.
     return false;
   }
 };
