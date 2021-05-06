782c926690a305cb21d56f59da5ccee8f76f4de2
Sunil Narasimhamurthy <sunnrs@amazon.com>
2021-05-06 18:59:22 +0000

    fix clang tidy issue
    Signed-off-by: Sunil Narasimhamurthy <sunnrs@amazon.com>

--- source/extensions/filters/udp/dns_filter/dns_filter_resolver.h
+++ source/extensions/filters/udp/dns_filter/dns_filter_resolver.h
@@ -21,9 +21,9 @@ public:
   DnsFilterResolver(DnsFilterResolverCallback& callback, AddressConstPtrVec resolvers,
                     std::chrono::milliseconds timeout, Event::Dispatcher& dispatcher,
                     uint64_t max_pending_lookups,
-                    const envoy::config::core::v3::DnsResolverOptions& dns_resolver_options_)
+                    const envoy::config::core::v3::DnsResolverOptions& dns_resolver_options)
       : timeout_(timeout), dispatcher_(dispatcher),
-        resolver_(dispatcher.createDnsResolver(resolvers, dns_resolver_options_)),
+        resolver_(dispatcher.createDnsResolver(resolvers, dns_resolver_options)),
         callback_(callback), max_pending_lookups_(max_pending_lookups) {}
   /**
    * @brief entry point to resolve the name in a DnsQueryRecord
