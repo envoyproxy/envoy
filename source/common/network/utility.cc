fb6bb16dd12268144c8c557a3ea59a3247b7cc1a
Dan Zhang <danzh@google.com>
2021-05-06 16:39:17 -0400

    fix sign compare
    Signed-off-by: Dan Zhang <danzh@google.com>

--- source/common/network/utility.cc
+++ source/common/network/utility.cc
@@ -698,15 +698,15 @@ Api::IoErrorPtr Utility::readPacketsFromSocket(IoHandle& handle,
                                                uint32_t& packets_dropped) {
   // Make sure at not too many packets will be read in following loop, but at least it will read
   // once.
-  size_t num_packets_to_read = std::min(MAX_NUM_PACKETS_PER_EVENT_LOOP,
-                                        udp_packet_processor.numPacketsExpectedPerEventLoop());
+  size_t num_packets_to_read = std::min<size_t>(
+      MAX_NUM_PACKETS_PER_EVENT_LOOP, udp_packet_processor.numPacketsExpectedPerEventLoop());
   const bool use_gro = prefer_gro && handle.supportsUdpGro();
   size_t num_reads =
       use_gro ? (num_packets_to_read / NUM_DATAGRAMS_PER_GRO_RECEIVE)
               : (handle.supportsMmsg() ? (num_packets_to_read / NUM_DATAGRAMS_PER_MMSG_RECEIVE)
                                        : num_packets_to_read);
   // Make sure at least read once.
-  num_reads = std::max(1ul, num_reads);
+  num_reads = std::max<size_t>(1, num_reads);
   do {
     const uint32_t old_packets_dropped = packets_dropped;
     const MonotonicTime receive_time = time_source.monotonicTime();
