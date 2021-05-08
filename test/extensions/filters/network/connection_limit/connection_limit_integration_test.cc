d9f316e26329d8515cb568356d6b61b4eb05ad00
delli <delli@microsoft.com>
2021-05-08 17:21:10 +0800

    Update integration test
    Signed-off-by: delli <delli@microsoft.com>

--- test/extensions/filters/network/connection_limit/connection_limit_integration_test.cc
+++ test/extensions/filters/network/connection_limit/connection_limit_integration_test.cc
@@ -47,7 +47,7 @@ typed_config:
   ASSERT_TRUE(fake_upstream_connection->waitForDisconnect());
 
   test_server_->waitForGaugeEq("connection_limit.connection_limit_stats.active_connections", 0,
-                               std::chrono::milliseconds(200));
+                               std::chrono::milliseconds(100));
 
   EXPECT_EQ(0, test_server_->counter("connection_limit.connection_limit_stats.limited_connections")
                    ->value());
@@ -72,21 +72,17 @@ typed_config:
   ASSERT_TRUE(fake_upstreams_[0]->waitForRawConnection(fake_upstream_connection1) ||
               fake_upstreams_[0]->waitForRawConnection(fake_upstream_connection2));
 
-  EXPECT_EQ(1, test_server_->counter("connection_limit.connection_limit_stats.limited_connections")
-                   ->value());
-  EXPECT_EQ(
-      2,
-      test_server_->gauge("connection_limit.connection_limit_stats.active_connections")->value());
   test_server_->waitForGaugeEq("connection_limit.connection_limit_stats.active_connections", 1,
                                std::chrono::milliseconds(200));
 
   tcp_client1->close();
   tcp_client2->close();
 
+  test_server_->waitForGaugeEq("connection_limit.connection_limit_stats.active_connections", 0,
+                               std::chrono::milliseconds(100));
+
   EXPECT_EQ(1, test_server_->counter("connection_limit.connection_limit_stats.limited_connections")
                    ->value());
-  test_server_->waitForGaugeEq("connection_limit.connection_limit_stats.active_connections", 0,
-                               std::chrono::milliseconds(200));
 }
 
 } // namespace
