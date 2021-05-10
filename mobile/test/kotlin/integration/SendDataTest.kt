3cf4374872914efe28c9c434fc5247c4e190cc0c
Alan Chiu <achiu@lyft.com>
2021-05-10 11:38:23 -0700

    kotlin: add send data test (#1454)
    Signed-off-by: Alan Chiu <achiu@lyft.com>
    
    For an explanation of how to fill out the fields, please see the relevant section
    in [PULL_REQUESTS.md](https://github.com/envoyproxy/envoy/blob/master/PULL_REQUESTS.md)
    
    Description: kotlin: add send data test
    Risk Level: low
    Testing: unit
    Docs Changes: n/a
    Release Notes: n/a
    [Optional Fixes #Issue]
    [Optional Deprecated:]
    
    Signed-off-by: JP Simard <jp@jpsim.com>

---
+++ mobile/test/kotlin/integration/SendDataTest.kt
@@ -0,0 +1,110 @@
+package test.kotlin.integration
+
+import io.envoyproxy.envoymobile.Custom
+import io.envoyproxy.envoymobile.EngineBuilder
+import io.envoyproxy.envoymobile.RequestHeadersBuilder
+import io.envoyproxy.envoymobile.RequestMethod
+import io.envoyproxy.envoymobile.UpstreamHttpProtocol
+import io.envoyproxy.envoymobile.engine.JniLibrary
+import java.nio.ByteBuffer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.fail
+import org.junit.Test
+
+private const val apiListenerType =
+  "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager"
+private const val assertionFilterType = "type.googleapis.com/envoymobile.extensions.filters.http.assertion.Assertion"
+private const val requestStringMatch = "match_me"
+private const val config =
+  """
+    static_resources:
+      listeners:
+      - name: base_api_listener
+        address:
+          socket_address:
+            protocol: TCP
+            address: 0.0.0.0
+            port_value: 10000
+        api_listener:
+          api_listener:
+            "@type": $apiListenerType
+            stat_prefix: hcm
+            route_config:
+              name: api_router
+              virtual_hosts:
+                - name: api
+                  domains:
+                    - "*"
+                  routes:
+                    - match:
+                        prefix: "/"
+                      direct_response:
+                        status: 200
+            http_filters:
+              - name: envoy.filters.http.assertion
+                typed_config:
+                  "@type": $assertionFilterType
+                  match_config:
+                    http_request_generic_body_match:
+                      patterns:
+                        - string_match: $requestStringMatch
+              - name: envoy.filters.http.buffer
+                typed_config:
+                  "@type": type.googleapis.com/envoy.extensions.filters.http.buffer.v3.Buffer
+                  max_request_bytes: 65000
+              - name: envoy.router
+                typed_config:
+                  "@type": type.googleapis.com/envoy.extensions.filters.http.router.v3.Router
+    """
+
+class SendDataTest {
+  init {
+    JniLibrary.loadTestLibrary()
+  }
+
+  @Test
+  fun `successful sending data`() {
+    val expectation = CountDownLatch(1)
+    val engine = EngineBuilder(Custom(config))
+      .setOnEngineRunning { }
+      .build()
+
+    val client = engine.streamClient()
+
+    val requestHeaders = RequestHeadersBuilder(
+      method = RequestMethod.GET,
+      scheme = "https",
+      authority = "example.com",
+      path = "/test"
+    )
+      .addUpstreamHttpProtocol(UpstreamHttpProtocol.HTTP2)
+      .build()
+
+    val body = ByteBuffer.wrap(requestStringMatch.toByteArray(Charsets.UTF_8))
+
+    var responseStatus: Int? = null
+    var responseHeadersEndStream = false
+    client.newStreamPrototype()
+      .setOnResponseHeaders { headers, endStream ->
+        responseStatus = headers.httpStatus
+        responseHeadersEndStream = endStream
+        expectation.countDown()
+      }
+      .setOnError {
+        fail("Unexpected error")
+      }
+      .start()
+      .sendHeaders(requestHeaders, false)
+      .close(body)
+
+    expectation.await(10, TimeUnit.SECONDS)
+
+    engine.terminate()
+
+    assertThat(expectation.count).isEqualTo(0)
+    assertThat(responseStatus).isEqualTo(200)
+    assertThat(responseHeadersEndStream).isTrue()
+  }
+}
