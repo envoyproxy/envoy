1ff414fbba6a855dce220cf809b78d78987ac071
Ryan Hamilton <rch@google.com>
2021-05-10 19:57:37 +0000

    Comments from Alyssa
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- source/common/http/alternate_protocols_cache_impl.h
+++ source/common/http/alternate_protocols_cache_impl.h
@@ -5,7 +5,7 @@
 #include <tuple>
 #include <vector>
 
-#include "envoy/common/http/alternate_protocols_cache.h"
+#include "envoy/http/alternate_protocols_cache.h"
 #include "envoy/common/optref.h"
 #include "envoy/common/time.h"
 
@@ -14,27 +14,16 @@
 namespace Envoy {
 namespace Http {
 
-// Tracks alternate protocols that can be used to make an HTTP connection to an origin server.
-// See https://tools.ietf.org/html/rfc7838 for HTTP Alternate Services and
-// https://datatracker.ietf.org/doc/html/draft-ietf-dnsop-svcb-https-04 for the
-// "HTTPS" DNS resource record.
+// An implementation of AlternateProtocolsCache.
 class AlternateProtocolsCacheImpl : public AlternateProtocolsCache {
 public:
   explicit AlternateProtocolsCacheImpl(TimeSource& time_source);
   ~AlternateProtocolsCacheImpl() override;
 
-  // Sets the possible alternative protocols which can be used to connect to the
-  // specified origin. Expires after the specified expiration time.
+  // AlternateProtocolsCache
   void setAlternatives(const Origin& origin, const std::vector<AlternateProtocol>& protocols,
                        const MonotonicTime& expiration) override;
-
-  // Returns the possible alternative protocols which can be used to connect to the
-  // specified origin, or nullptr if not alternatives are found. The returned pointer
-  // is owned by the AlternateProtocolsCacheImpl and is valid until the next operation on
-  // AlternateProtocolsCacheImpl.
   OptRef<const std::vector<AlternateProtocol>> findAlternatives(const Origin& origin) override;
-
-  // Returns the number of entries in the map.
   size_t size() const override;
 
 private:
