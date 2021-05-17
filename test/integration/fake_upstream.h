4d2e0187d2dd4136c57c0f47cf5a209bcc13bda4
alyssawilk <alyssar@chromium.org>
2021-05-17 17:02:50 -0400

    http: cleaning up obsolete grpc args (#16525)
    Risk Level: low (removing unused args)
    Testing: n/a
    Docs Changes: n/a
    Release Notes: n/a
    
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/integration/fake_upstream.h
+++ test/integration/fake_upstream.h
@@ -99,7 +99,7 @@ public:
     return encoder_.http1StreamEncoderOptions();
   }
   void
-  sendLocalReply(bool is_grpc_request, Http::Code code, absl::string_view body,
+  sendLocalReply(Http::Code code, absl::string_view body,
                  const std::function<void(Http::ResponseHeaderMap& headers)>& /*modify_headers*/,
                  const absl::optional<Grpc::Status::GrpcStatus> grpc_status,
                  absl::string_view /*details*/) override {
@@ -119,7 +119,7 @@ public:
              [&](Buffer::Instance& data, bool end_stream) -> void {
                encoder_.encodeData(data, end_stream);
              }}),
-        Http::Utility::LocalReplyData({is_grpc_request, code, body, grpc_status, is_head_request}));
+        Http::Utility::LocalReplyData({false, code, body, grpc_status, is_head_request}));
   }
 
   ABSL_MUST_USE_RESULT
