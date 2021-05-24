4c87bf0a381f6fb7b3724c2bb3428f5aa5650f51
Mike Schore <mike.schore@gmail.com>
2021-05-25 07:11:34 +0800

    one more format fix
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- configs/example_configs_validation.py
+++ configs/example_configs_validation.py
@@ -12,9 +12,7 @@ def main():
     errors = []
     for arg in sys.argv[1:]:
         try:
-            validate_yaml(
-                "envoy.config.bootstrap.v3.Bootstrap",
-                pathlib.Path(arg).read_text())
+            validate_yaml("envoy.config.bootstrap.v3.Bootstrap", pathlib.Path(arg).read_text())
         except (ParseError, KeyError) as e:
             errors.append(arg)
             print(f"\nERROR (validation failed): {arg}\n{e}\n\n")
