e9d8948a0b420d1eb2de21c6e7f9961ed467c60b
Jose Nino <jnino@lyft.com>
2021-05-27 15:37:28 -0500

    mocks: fix spelling of MockScopeTrackedObject
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- test/mocks/http/mocks.h
+++ test/mocks/http/mocks.h
@@ -275,7 +275,7 @@ public:
   std::list<DownstreamWatermarkCallbacks*> callbacks_{};
   testing::NiceMock<Tracing::MockSpan> active_span_;
   testing::NiceMock<Tracing::MockConfig> tracing_config_;
-  testing::NiceMock<MockScopedTrackedObject> scope_;
+  testing::NiceMock<MockScopeTrackedObject> scope_;
   bool is_grpc_request_{};
   bool is_head_request_{false};
   bool stream_destroyed_{};
@@ -327,7 +327,7 @@ public:
   Buffer::InstancePtr buffer_;
   testing::NiceMock<Tracing::MockSpan> active_span_;
   testing::NiceMock<Tracing::MockConfig> tracing_config_;
-  testing::NiceMock<MockScopedTrackedObject> scope_;
+  testing::NiceMock<MockScopeTrackedObject> scope_;
 };
 
 class MockStreamDecoderFilter : public StreamDecoderFilter {
