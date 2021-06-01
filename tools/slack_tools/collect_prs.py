71e0d7b6868ae2048dd318b4420edb1655618daa
Alyssa Wilk <alyssar@chromium.org>
2021-06-01 12:35:16 -0400

    comments v1
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- tools/slack_tools/collect_prs.py
+++ tools/slack_tools/collect_prs.py
@@ -8,13 +8,10 @@ import os
 import sys
 
 import github
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
-try:
-    input = raw_input  # Python 2
-except NameError:
-    pass  # Python 3
-
-maintainers = {
+Maintainers = {
     'alyssawilk': 'U78RP48V9',
     'mattklein123': 'U5CALEVSL',
     'lizan': 'U79E51EQ6',
@@ -67,7 +64,7 @@ def track_prs():
                 waiting_prs.append("PR %s' is tagged as waiting" % (pr_title))
                 waiting = True
                 continue
-        if (waiting):
+        if waiting:
             continue
 
         # If the PR hasn't been updated recently, don't nag.
@@ -78,14 +75,14 @@ def track_prs():
         delta = now - pr_age
         delta_days = delta.days
         delta_hours = delta.seconds // 3600
-        if (delta < datetime.timedelta(hours=8)):
+        if delta < datetime.timedelta(hours=8):
             recent_prs.append("PR %s' was updated %s hours ago" % (pr_title, delta_hours))
             continue
 
         # If we get to this point, the review may be in SLO - nudge if it's in
         # SLO, nudge in bold if not.
         message = ""
-        if (delta < datetime.timedelta(hours=get_slo_hours())):
+        if delta < datetime.timedelta(hours=get_slo_hours()):
             message = "<%s|%s> has been waiting %s days %s hours\n" % (
                 pr_url, pr_title, delta_days, delta_hours)
         else:
@@ -93,7 +90,7 @@ def track_prs():
                 pr_url, pr_title, delta_days, delta_hours)
 
         # If the PR has been out-SLO for over a day, inform on-call
-        if (delta > datetime.timedelta(hours=get_slo_hours() + 36)):
+        if delta > datetime.timedelta(hours=get_slo_hours() + 36):
             message = "<%s|%s> has been waiting *%s days %s hours*\n" % (
                 pr_url, pr_title, delta_days, delta_hours)
             stalled_prs = stalled_prs + message
@@ -102,12 +99,12 @@ def track_prs():
         has_maintainer_assignee = False
         for assignee_info in pr_info.assignees:
             assignee = assignee_info.login
-            if assignee not in maintainers:
+            if assignee not in Maintainers:
                 continue
             has_maintainer_assignee = True
             if assignee not in maintainers_and_prs.keys():
                 maintainers_and_prs[
-                    assignee] = "Good morning, %s, here are your PR reminders for the day \n" % assignee
+                    assignee] = "Hello, %s, here are your PR reminders for the day \n" % assignee
             maintainers_and_prs[assignee] = maintainers_and_prs[assignee] + message
 
         # If there was no maintainer, track it as unassigned.
@@ -135,10 +132,6 @@ def track_prs():
         print('\n\n\n')
 
 
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
-
-
 def post_to_maintainers(client, maintainers_and_messages):
     # Post updates to individual maintainers
     for key in maintainers_and_messages:
@@ -146,11 +139,11 @@ def post_to_maintainers(client, maintainers_and_messages):
 
         # Only send messages if we have the maintainer UID
         # if key not in ['alyssawilk']:  # Use this line for debugging.
-        if key not in maintainers:
+        if key not in Maintainers:
             # Right now we skip "unassigned" but eventually that should go to #maintainers
             print("Skipping key %s " % key)
             continue
-        uid = maintainers[key]
+        uid = Maintainers[key]
 
         # Ship messages off to slack.
         try:
