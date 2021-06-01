f4a6f65afbcfd0995602c3eb2675845bd9e21b8a
Kateryna Nezdolii <nezdolik@spotify.com>
2021-06-01 13:39:04 +0000

    Fix tests
    Signed-off-by: Kateryna Nezdolii <nezdolik@spotify.com>

--- test/extensions/resource_monitors/injected_resource/injected_resource_monitor_test.cc
+++ test/extensions/resource_monitors/injected_resource/injected_resource_monitor_test.cc
@@ -40,7 +40,7 @@ private:
   Event::Dispatcher& dispatcher_;
 };
 
-class MockedCallbacks : public Server::ResourceMonitor::Callbacks {
+class MockedCallbacks : public Server::ResourceUpdateCallbacks {
 public:
   MOCK_METHOD(void, onSuccess, (const Server::ResourceUsage&));
   MOCK_METHOD(void, onFailure, (const EnvoyException&));
