336f1333b3192b043abde8afddaa884a502a9c35
Dan Zhang <danzh@google.com>
2021-06-03 17:26:59 -0400

    fix param name
    Signed-off-by: Dan Zhang <danzh@google.com>

--- test/config/utility.h
+++ test/config/utility.h
@@ -243,7 +243,7 @@ public:
   void addSslConfig() { addSslConfig({}); }
 
   // Add the default SSL configuration for QUIC downstream.
-  void addQuicDownstreamTransportSocketConfig(bool resuse_port);
+  void addQuicDownstreamTransportSocketConfig(bool reuse_port);
 
   // Set the HTTP access log for the first HCM (if present) to a given file. The default is
   // the platform's null device.
