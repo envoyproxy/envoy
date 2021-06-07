b76035fdb63e2a7e3d35beaba92c8bf58a42a3ee
Pavel Fomin <mail@vaccarium.ru>
2021-06-07 14:21:29 +0300

    CONSTRUCT_ON_FIRST_USE for TagFormat structs
    Signed-off-by: Pavel Fomin <mail@vaccarium.ru>

---
+++ source/extensions/stat_sinks/common/statsd/tag_formats.cc
@@ -0,0 +1,33 @@
+#include "extensions/stat_sinks/common/statsd/tag_formats.h"
+
+#include "common/common/macros.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace StatSinks {
+namespace Common {
+namespace Statsd {
+
+const TagFormat& getDefaultTagFormat() {
+  CONSTRUCT_ON_FIRST_USE(TagFormat, TagFormat{
+                                        "|#",                       // start
+                                        ":",                        // assign
+                                        ",",                        // separator
+                                        TagPosition::TagAfterValue, // tag_position
+                                    });
+}
+
+const TagFormat& getGraphiteTagFormat() {
+  CONSTRUCT_ON_FIRST_USE(TagFormat, TagFormat{
+                                        ";",                       // start
+                                        "=",                       // assign
+                                        ";",                       // separator
+                                        TagPosition::TagAfterName, // tag_position
+                                    });
+}
+
+} // namespace Statsd
+} // namespace Common
+} // namespace StatSinks
+} // namespace Extensions
+} // namespace Envoy
