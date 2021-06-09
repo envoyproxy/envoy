d32b9ae1bf55b82435ea84166215632f383bed33
John Murray <murray@stripe.com>
2021-06-09 13:03:11 -0600

    [test fix] allow for independent child draining
    Signed-off-by: John Murray <murray@stripe.com>

--- source/server/drain_manager_impl.cc
+++ source/server/drain_manager_impl.cc
@@ -31,7 +31,12 @@ DrainManagerImpl::createChildManager(Event::Dispatcher& dispatcher,
   auto child = std::make_shared<DrainManagerImpl>(server_, drain_type, dispatcher);
 
   // Wire up the child so that when the parent starts draining, the child also sees the state-change
-  auto child_cb = children_.add(dispatcher, [child] { child->startDrainSequence([] {}); });
+  auto child_cb = children_.add(dispatcher, [child_ = std::weak_ptr(child)] {
+    auto child = child_.lock();
+    if (child && !child->draining_) {
+      child->startDrainSequence([] {});
+    }
+  });
   child->parent_callback_handle_ = child_cb;
   return child;
 }
