6f5436ad06509a460146607e890f0fbff0299dd4
Jacob Delgado <jacob.delgado@volunteers.acasi.info>
2021-06-10 14:58:31 -0600

    Code review comments
    Signed-off-by: Jacob Delgado <jacob.delgado@volunteers.acasi.info>

--- source/server/connection_handler_impl.cc
+++ source/server/connection_handler_impl.cc
@@ -194,18 +194,18 @@ ConnectionHandlerImpl::getBalancedHandlerByAddress(const Network::Address::Insta
   // TODO(wattli): consolidate with previous search for more efficiency.
   if (Runtime::runtimeFeatureEnabled(
           "envoy.reloadable_features.listener_wildcard_match_ip_family")) {
-    listener_it = std::find_if(
-        listeners_.begin(), listeners_.end(),
-        [&address](const std::pair<Network::Address::InstanceConstSharedPtr,
-                                   ConnectionHandlerImpl::ActiveListenerDetails>& p) {
-          return absl::holds_alternative<std::reference_wrapper<ActiveTcpListener>>(
-                     p.second.typed_listener_) &&
-                 p.second.listener_->listener() != nullptr &&
-                 p.first->type() == Network::Address::Type::Ip &&
-                 p.first->ip()->port() == address.ip()->port() && p.first->ip()->isAnyAddress() &&
-                 ((p.first->ip()->ipv4() != nullptr && address.ip()->ipv4() != nullptr) ||
-                  (p.first->ip()->ipv6() != nullptr && address.ip()->ipv6() != nullptr));
-        });
+    listener_it =
+        std::find_if(listeners_.begin(), listeners_.end(),
+                     [&address](const std::pair<Network::Address::InstanceConstSharedPtr,
+                                                ConnectionHandlerImpl::ActiveListenerDetails>& p) {
+                       return absl::holds_alternative<std::reference_wrapper<ActiveTcpListener>>(
+                                  p.second.typed_listener_) &&
+                              p.second.listener_->listener() != nullptr &&
+                              p.first->type() == Network::Address::Type::Ip &&
+                              p.first->ip()->port() == address.ip()->port() &&
+                              p.first->ip()->isAnyAddress() &&
+                              p.first->ip()->version() == address.ip()->version();
+                     });
   } else {
     listener_it =
         std::find_if(listeners_.begin(), listeners_.end(),
