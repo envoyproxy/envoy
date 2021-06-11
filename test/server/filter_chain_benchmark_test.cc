8bccf23c0c604d9f10e3c52f835b2066a3819811
John Murray <murray@stripe.com>
2021-06-11 11:52:30 -0600

    [fix] unused parameter warning fix
    Signed-off-by: John Murray <murray@stripe.com>

--- test/server/filter_chain_benchmark_test.cc
+++ test/server/filter_chain_benchmark_test.cc
@@ -218,6 +218,7 @@ BENCHMARK_DEFINE_F(FilterChainBenchmarkFixture, FilterChainManagerBuildTest)
   initialize(state);
   NiceMock<Server::Configuration::MockDrainableFactoryContext> factory_context;
   for (auto _ : state) {
+    UNREFERENCED_PARAMETER(_);
     FilterChainManagerImpl filter_chain_manager{
         std::make_shared<Network::Address::Ipv4Instance>("127.0.0.1", 1234), factory_context,
         init_manager_};
