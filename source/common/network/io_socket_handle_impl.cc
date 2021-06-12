fe02e366b3d3b7eb2dced9941f32a39031872b5c
He Jie Xu <hejie.xu@intel.com>
2021-06-12 13:18:58 +0000

    Reset the file event before initialize new one
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- source/common/network/io_socket_handle_impl.cc
+++ source/common/network/io_socket_handle_impl.cc
@@ -635,8 +635,11 @@ Address::InstanceConstSharedPtr IoSocketHandleImpl::peerAddress() {
 
 void IoSocketHandleImpl::initializeFileEvent(Event::Dispatcher& dispatcher, Event::FileReadyCb cb,
                                              Event::FileTriggerType trigger, uint32_t events) {
-  ASSERT(file_event_ == nullptr, "Attempting to initialize two `file_event_` for the same "
-                                 "file descriptor. This is not allowed.");
+  // Reset the file_event_ for the case of initialize two `file_event_` for
+  // the same file descriptor. This is not allowed.
+  if (file_event_ != nullptr) {
+    file_event_.reset();
+  }
   file_event_ = dispatcher.createFileEvent(fd_, cb, trigger, events);
 }
 
