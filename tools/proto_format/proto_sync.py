8fbfd2b5680aa314e3ad5bb6aa40fd1351c698eb
Ryan Northey <ryan@synca.io>
2021-06-15 23:08:41 +0100

    thrift.rate_limit
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tools/proto_format/proto_sync.py
+++ tools/proto_format/proto_sync.py
@@ -96,6 +96,7 @@ IGNORED_V2_PROTOS = [
     "envoy/config/filter/network/rbac/v2",
     "envoy/config/filter/network/sni_cluster/v2",
     "envoy/config/filter/network/zookeeper_proxy/v1alpha1",
+    "envoy/config/filter/thrift/rate_limit/v2alpha1",
     "envoy/config/filter/udp/udp_proxy/v2alpha",
 ]
 
