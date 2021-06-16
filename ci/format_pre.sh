d038b1e4b417637e0e763a3e5a599a2be9d69f9d
Ryan Northey <ryan@synca.io>
2021-06-16 15:53:55 +0100

    hash
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ci/format_pre.sh
+++ ci/format_pre.sh
@@ -59,7 +59,7 @@ bazel run "${BAZEL_BUILD_OPTIONS[@]}" //tools/extensions:extensions_check
 if [[ "${#FAILED[@]}" -ne "0" ]]; then
     echo "#[error]: TESTS FAILED:" >&2
     for failed in "${FAILED[@]}"; do
-        echo "#[error]:  $failed" >&2
+        echo "##[error]:  $failed" >&2
     done
     exit 1
 fi
