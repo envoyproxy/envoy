1efba1fc6518525a8875f829b811f06395cb4ee4
He Jie Xu <hejie.xu@intel.com>
2021-06-20 07:19:09 +0000

    revert
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- test/mocks/network/io_handle.h
+++ test/mocks/network/io_handle.h
@@ -63,7 +63,6 @@ public:
   MOCK_METHOD(absl::optional<std::chrono::milliseconds>, lastRoundTripTime, ());
   MOCK_METHOD(Api::SysCallIntResult, ioctl,
               (unsigned long, void*, unsigned long, void*, unsigned long, unsigned long*));
-  MOCK_METHOD(bool, isFileEventInitialized, ());
 };
 
 } // namespace Network
