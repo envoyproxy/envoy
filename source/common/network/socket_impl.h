e1a583b891713a84632eedcaa63cb68383d6de69
He Jie Xu <hejie.xu@intel.com>
2021-07-04 08:52:13 +0000

    fix format
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- source/common/network/socket_impl.h
+++ source/common/network/socket_impl.h
@@ -49,12 +49,9 @@ public:
   void setRequestedServerName(const absl::string_view requested_server_name) override {
     server_name_ = std::string(requested_server_name);
   }
-  absl::optional<uint64_t> connectionID() const override {
-    return connection_id_;
-  }
-  void setConnectionID(uint64_t id) override {
-    connection_id_ = id;
-  }
+  absl::optional<uint64_t> connectionID() const override { return connection_id_; }
+  void setConnectionID(uint64_t id) override { connection_id_ = id; }
+
 private:
   Address::InstanceConstSharedPtr local_address_;
   bool local_address_restored_{false};
