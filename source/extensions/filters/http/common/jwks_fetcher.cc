6a8cba56ff6a91df0ac90563ce4a80dcaf7350a6
Anthony Lichnewsky <alichnewsky@users.noreply.github.com>
2021-07-05 10:50:26 +0000

    WIP: RemoteJwks uses envoy.config.core.v3.RetryPolicy and adapts it to encoy.config.route.v3.RetryPolicy to pass it to async client
    Signed-off-by: Anthony Lichnewsky <alichnewsky@users.noreply.github.com>

--- source/extensions/filters/http/common/jwks_fetcher.cc
+++ source/extensions/filters/http/common/jwks_fetcher.cc
@@ -5,6 +5,7 @@
 #include "source/common/common/enum_to_int.h"
 #include "source/common/http/headers.h"
 #include "source/common/http/utility.h"
+#include "source/common/protobuf/utility.h"
 
 #include "jwt_verify_lib/status.h"
 
@@ -23,6 +24,10 @@ public:
   JwksFetcherImpl(Upstream::ClusterManager& cm, const RemoteJwks& remote_jwks)
       : cm_(cm), remote_jwks_(remote_jwks) {
     ENVOY_LOG(trace, "{}", __func__);
+
+    if (remote_jwks_.has_retry_policy()) {
+      route_retry_policy_ = adaptRetryPolicy(remote_jwks_.retry_policy());
+    }
   }
 
   ~JwksFetcherImpl() override { cancel(); }
@@ -63,11 +68,7 @@ public:
                        .setChildSpanName("JWT Remote PubKey Fetch");
 
     if (remote_jwks_.has_retry_policy()) {
-      // interaction with
-      // upstream.use_retry ?
-      // interaction with timeout from the request and per_try_timeout ? see RouteImpl
-      // lifecycle ?
-      options.setRetryPolicy(remote_jwks_.retry_policy());
+      options.setRetryPolicy(route_retry_policy_.value());
       options.setBufferBodyForRetry(true);
     }
 
@@ -123,9 +124,67 @@ private:
   const RemoteJwks& remote_jwks_;
   Http::AsyncClient::Request* request_{};
 
+  // http async client uses richer semantics than the ones allowed in RemoteJwks
+  // envoy.config.route.v3.RetryPolicy vs envoy.config.core.v3.RetryPolicy
+  // mapping is done in constructor and reused.
+  absl::optional<envoy::config::route::v3::RetryPolicy> route_retry_policy_{absl::nullopt};
+
   void reset() {
     request_ = nullptr;
     receiver_ = nullptr;
+  }
+
+  /**
+   * @details construct an envoy.config.route.v3.RetryPolicy protobuf message
+   *          from a less feature rich envoy.config.core.v3.RetryPolicy one.
+   *
+   *          this is about limiting the user's possibilities.
+   *          just doing truncated exponential backoff
+   *
+   *          the upstream.use_retry feature flag will need to be turned on (default)
+   *          for this to work.
+   *
+   * @param retry policy from the RemoteJwks proto
+   * @return a retry policy useable by the http async client.
+   */
+  static envoy::config::route::v3::RetryPolicy
+  adaptRetryPolicy(const envoy::config::core::v3::RetryPolicy& core_retry_policy) {
+    envoy::config::route::v3::RetryPolicy route_retry_policy;
+
+    static constexpr uint32_t RetryInitialDelayMilliseconds = 1000;
+    static constexpr uint32_t RetryMaxDelayMilliseconds = 10 * 1000;
+    uint64_t base_interval_ms = RetryInitialDelayMilliseconds;
+    uint64_t max_interval_ms = RetryMaxDelayMilliseconds;
+
+    if (core_retry_policy.has_retry_back_off()) {
+      const auto& core_back_off = core_retry_policy.retry_back_off();
+
+      base_interval_ms = PROTOBUF_GET_MS_REQUIRED(core_back_off, base_interval);
+
+      max_interval_ms =
+          PROTOBUF_GET_MS_OR_DEFAULT(core_back_off, max_interval, base_interval_ms * 10);
+
+      if (max_interval_ms < base_interval_ms) {
+        throw EnvoyException("max_interval must be greater than or equal to the base_interval");
+      }
+    }
+
+    route_retry_policy.mutable_num_retries()->set_value(
+        PROTOBUF_GET_WRAPPED_OR_DEFAULT(core_retry_policy, num_retries, 1));
+
+    auto* route_mutable_back_off = route_retry_policy.mutable_retry_back_off();
+
+    route_mutable_back_off->mutable_base_interval()->CopyFrom(
+        Protobuf::util::TimeUtil::MillisecondsToDuration(base_interval_ms));
+    route_mutable_back_off->mutable_max_interval()->CopyFrom(
+        Protobuf::util::TimeUtil::MillisecondsToDuration(max_interval_ms));
+
+    // set all the other fields with appropriate values.
+    route_retry_policy.set_retry_on("5xx,gateway-error,connect-failure,reset");
+    route_retry_policy.mutable_per_try_timeout()->CopyFrom(
+        route_retry_policy.retry_back_off().max_interval());
+
+    return route_retry_policy;
   }
 };
 } // namespace
