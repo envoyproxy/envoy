93eff0c0109881500ca8512951981c3d9cc984f0
Anthony Lichnewsky <alichnewsky@users.noreply.github.com>
2021-07-05 12:51:53 +0000

    add test for various RemoteJwks configs with RetryPolicy
    Signed-off-by: Anthony Lichnewsky <alichnewsky@users.noreply.github.com>

--- test/extensions/filters/http/common/jwks_fetcher_test.cc
+++ test/extensions/filters/http/common/jwks_fetcher_test.cc
@@ -166,6 +166,116 @@ TEST_F(JwksFetcherTest, TestSpanPassedDown) {
   // Act
   fetcher_->fetch(parent_span_, receiver);
 }
+struct RetryingParameters {
+  RetryingParameters(const std::string& config, uint32_t n, int64_t base_ms, int64_t max_ms)
+      : config_(config), expected_num_retries_(n), expected_backoff_base_interval_ms_(base_ms)
+      , expected_backoff_max_interval_ms_( max_ms)
+  {}
+
+  std::string config_;
+
+  uint32_t expected_num_retries_;
+  int64_t expected_backoff_base_interval_ms_;
+  int64_t expected_backoff_max_interval_ms_;
+};
+
+class JwksFetcherRetryingTest : public testing::TestWithParam<RetryingParameters> {
+public:
+  void setupFetcher(const std::string& config_str) {
+    TestUtility::loadFromYaml(config_str, remote_jwks_);
+    mock_factory_ctx_.cluster_manager_.initializeThreadLocalClusters({"pubkey_cluster"});
+    fetcher_ = JwksFetcher::create(mock_factory_ctx_.cluster_manager_, remote_jwks_);
+    EXPECT_TRUE(fetcher_ != nullptr);
+  }
+
+  RemoteJwks remote_jwks_;
+  testing::NiceMock<Server::Configuration::MockFactoryContext> mock_factory_ctx_;
+  std::unique_ptr<JwksFetcher> fetcher_;
+  NiceMock<Tracing::MockSpan> parent_span_;
+};
+
+INSTANTIATE_TEST_SUITE_P(Retrying, JwksFetcherRetryingTest,
+                        testing::Values( RetryingParameters{ R"(
+http_uri:
+  uri: https://pubkey_server/pubkey_path
+  cluster: pubkey_cluster
+  timeout:
+    seconds: 5
+retry_policy:
+  retry_back_off:
+    base_interval: 0.1s
+    max_interval: 32s
+  num_retries: 10
+)", 10, 100, 32000},
+                                         RetryingParameters{ R"(
+http_uri:
+  uri: https://pubkey_server/pubkey_path
+  cluster: pubkey_cluster
+  timeout:
+    seconds: 5
+retry_policy: {}
+)", 1, 1000, 10000},
+                                         RetryingParameters{ R"(
+http_uri:
+  uri: https://pubkey_server/pubkey_path
+  cluster: pubkey_cluster
+  timeout:
+    seconds: 5
+retry_policy:
+  num_retries: 2
+)", 2, 1000, 10000}
+));
+
+TEST_P(JwksFetcherRetryingTest, TestCompleteRetryPolicy) {
+
+  // Setup
+  setupFetcher(GetParam().config_);
+  MockUpstream mock_pubkey(mock_factory_ctx_.cluster_manager_, "200", publicKey);
+  NiceMock<MockJwksReceiver> receiver;
+
+  // Expectations for envoy.config.core.v3.RetryPolicy to envoy.config.route.v3.RetryPolicy
+  // used by async client.
+  // execution deep down in async_client_'s route entry implementation
+  // is not exercised here, just the configuration adaptation.
+  EXPECT_CALL(mock_factory_ctx_.cluster_manager_.thread_local_cluster_.async_client_,
+              send_(_, _, _))
+      .WillOnce(Invoke(
+          [](Http::RequestMessagePtr&, Http::AsyncClient::Callbacks&,
+             const Http::AsyncClient::RequestOptions& options) -> Http::AsyncClient::Request* {
+
+            RetryingParameters const & rp = GetParam();
+
+            EXPECT_TRUE(options.retry_policy.has_value());
+            EXPECT_TRUE(options.buffer_body_for_retry);
+            EXPECT_TRUE(options.retry_policy.value().has_num_retries());
+            EXPECT_EQ(PROTOBUF_GET_WRAPPED_REQUIRED(options.retry_policy.value(), num_retries), rp.expected_num_retries_);
+
+            EXPECT_TRUE(options.retry_policy.value().has_retry_back_off());
+            EXPECT_TRUE(options.retry_policy.value().retry_back_off().has_base_interval());
+            EXPECT_EQ(PROTOBUF_GET_MS_REQUIRED(options.retry_policy.value().retry_back_off(), base_interval),
+                      rp.expected_backoff_base_interval_ms_);
+            EXPECT_TRUE(options.retry_policy.value().retry_back_off().has_max_interval());
+            EXPECT_EQ(PROTOBUF_GET_MS_REQUIRED(options.retry_policy.value().retry_back_off(), max_interval),
+                      rp.expected_backoff_max_interval_ms_);
+
+            EXPECT_TRUE(options.retry_policy.value().has_per_try_timeout());
+            EXPECT_LE(PROTOBUF_GET_MS_REQUIRED(options.retry_policy.value().retry_back_off(), max_interval),
+                      PROTOBUF_GET_MS_REQUIRED(options.retry_policy.value(), per_try_timeout));
+
+            const std::string & retry_on = options.retry_policy.value().retry_on();
+            std::set<std::string> retry_on_modes = absl::StrSplit(retry_on, ',');
+
+            EXPECT_EQ(retry_on_modes.count("5xx"), 1);
+            EXPECT_EQ(retry_on_modes.count("gateway-error"), 1);
+            EXPECT_EQ(retry_on_modes.count("connect-failure"), 1);
+            EXPECT_EQ(retry_on_modes.count("reset"), 1);
+
+            return nullptr;
+          }));
+
+  // Act
+  fetcher_->fetch(parent_span_, receiver);
+}
 
 } // namespace
 } // namespace Common
