15995fc42fb260cc54375fbf3d2313f5a7024e13
Takeshi Yoneda <takeshi@tetrate.io>
2021-07-14 04:08:54 +0900

    wasm,test: remove unused no_mangle from extern "c" (#17305)
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- test/extensions/filters/http/wasm/test_data/headers_rust.rs
+++ test/extensions/filters/http/wasm/test_data/headers_rust.rs
@@ -2,7 +2,6 @@ use log::{debug, error, info, trace, warn};
 use proxy_wasm::traits::{Context, HttpContext};
 use proxy_wasm::types::*;
 
-#[no_mangle]
 extern "C" {
     fn __wasilibc_initialize_environ();
 }
