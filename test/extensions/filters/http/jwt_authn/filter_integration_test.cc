95ae8bca9e259058830bb3daaaf1568b728eb73f
Wayne Zhang <qiwzhang@google.com>
2021-07-13 14:20:53 -0700

    update comment, fixed spelling
    Signed-off-by: Wayne Zhang <qiwzhang@google.com>

--- test/extensions/filters/http/jwt_authn/filter_integration_test.cc
+++ test/extensions/filters/http/jwt_authn/filter_integration_test.cc
@@ -523,12 +523,13 @@ TEST_P(RemoteJwksIntegrationTest, WithGoodTokenAsyncFetchFast) {
   on_server_init_function_ = [this]() { waitForJwksResponse("200", PublicKey); };
   initializeAsyncFetchFilter(true);
 
-  // There is a race condition in this test:
-  // In fast_fatch mode, the listener is activated without waiting for jwks fetch to be
-  // done. When the first request comes, if jwks is not fetched, it will fetch again.
-  // The second fetch will fail in this test since its fake_upstream is not waiting.
-  // To avoid such race condition, before sending out the first request,
-  // wait for the first fetch stats to be updated.
+  // This test is only expecting one jwks fetch, but there is a race condition in the test:
+  // In fast fetch mode, the listener is activated without waiting for jwks fetch to be
+  // completed. When the first request comes at the worker thread, jwks fetch could be at
+  // any state at the main thread. If its result is not saved into jwks thread local slot,
+  // the first request will trigger a second jwks fetch, this is not expected, test will fail.
+  // To avoid such race condition, before making the first request, wait for the first
+  // fetch stats to be updated.
   test_server_->waitForCounterGe("http.config_test.jwt_authn.jwks_fetch_success", 1);
 
   codec_client_ = makeHttpConnection(lookupPort("http"));
