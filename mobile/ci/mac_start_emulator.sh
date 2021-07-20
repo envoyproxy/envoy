2df12c70e300ee2324aa8fd36723c6925500e322
Jingwei <jingwei.hao@gmail.com>
2021-07-19 18:29:24 -0700

    ci: update path to android sdk and use build-tools 30.0.3 for android build (#1599)
    Description:
    
    1) github made a [change](https://github.com/actions/virtual-environments/issues/3638) on android sdkmanager, this pr follows the migration step from the announced change.
    2) android build-tools 31.0.0 (the version installed on the ci machines for now) is missing dx.jar, which causes android build to fail, this pr uninstalls 31.0.0 and installs 30.0.3 for the android build to complete.
    
    Risk Level: Low
    Testing: CI
    
    Signed-off-by: Jingwei Hao <jingwei.hao@gmail.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/ci/mac_start_emulator.sh
+++ mobile/ci/mac_start_emulator.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-29;google_apis;x86'
+echo "y" | $ANDROID_SDK_ROOT/cmdline-tools/latest/bin/sdkmanager --install 'system-images;android-29;google_apis;x86'
 echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n test_android_emulator -k 'system-images;android-29;google_apis;x86' --force
+ls $ANDROID_HOME/tools/bin/
 nohup $ANDROID_HOME/emulator/emulator -partition-size 1024 -avd test_android_emulator -no-snapshot > /dev/null 2>&1 & $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
