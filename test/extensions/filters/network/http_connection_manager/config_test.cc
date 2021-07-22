4dda813200f1068da0cb15dc1bdea4d9347b99f3
Tianyu Xia <tyxia@google.com>
2021-07-22 16:10:38 +0000

    Remove the deprecated test that is covered by `CommonHttpProtocolIdleTimeout`
    Signed-off-by: Tianyu Xia <tyxia@google.com>

--- test/extensions/filters/network/http_connection_manager/config_test.cc
+++ test/extensions/filters/network/http_connection_manager/config_test.cc
@@ -776,25 +776,6 @@ TEST_F(HttpConnectionManagerConfigTest, DisabledStreamIdleTimeout) {
   EXPECT_EQ(0, config.streamIdleTimeout().count());
 }
 
-// Validate that deprecated idle_timeout is still ingested.
-TEST_F(HttpConnectionManagerConfigTest, DEPRECATED_FEATURE_TEST(IdleTimeout)) {
-  TestDeprecatedV2Api _deprecated_v2_api;
-  const std::string yaml_string = R"EOF(
-  stat_prefix: ingress_http
-  idle_timeout: 1s
-  route_config:
-    name: local_route
-  http_filters:
-  - name: envoy.filters.http.router
-  )EOF";
-
-  HttpConnectionManagerConfig config(parseHttpConnectionManagerFromYaml(yaml_string, false),
-                                     context_, date_provider_, route_config_provider_manager_,
-                                     scoped_routes_config_provider_manager_, http_tracer_manager_,
-                                     filter_config_provider_manager_);
-  EXPECT_EQ(1000, config.idleTimeout().value().count());
-}
-
 // Validate that idle_timeout set in common_http_protocol_options is used.
 TEST_F(HttpConnectionManagerConfigTest, CommonHttpProtocolIdleTimeout) {
   const std::string yaml_string = R"EOF(
