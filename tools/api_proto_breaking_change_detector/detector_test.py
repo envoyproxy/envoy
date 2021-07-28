29e7dd2a431b12ed7d6b25c55733e5caa867443a
Yaseen Alkhafaji <yaseena@google.com>
2021-07-28 19:16:36 +0000

    small test cleanup
    Signed-off-by: Yaseen Alkhafaji <yaseena@google.com>

--- tools/api_proto_breaking_change_detector/detector_test.py
+++ tools/api_proto_breaking_change_detector/detector_test.py
@@ -23,68 +23,62 @@ class BufTests(unittest.TestCase):
 
 class TestBreakingChanges(BufTests):
 
+    def run_breaking_test(self, testname):
+        self.run_buf_test(testname, is_breaking=True, expects_changes=False)
+
     def test_change_field_id(self):
-        self.run_buf_test(
-            self.test_change_field_id.__name__, is_breaking=True, expects_changes=False)
+        self.run_breaking_test(self.test_change_field_id.__name__)
 
     def test_change_field_type(self):
-        self.run_buf_test(
-            self.test_change_field_type.__name__, is_breaking=True, expects_changes=False)
+        self.run_breaking_test(self.test_change_field_type.__name__)
 
     def test_change_field_plurality(self):
-        self.run_buf_test(
-            self.test_change_field_plurality.__name__, is_breaking=True, expects_changes=False)
+        self.run_breaking_test(self.test_change_field_plurality.__name__)
 
     def test_change_field_name(self):
-        self.run_buf_test(
-            self.test_change_field_name.__name__, is_breaking=True, expects_changes=False)
+        self.run_breaking_test(self.test_change_field_name.__name__)
 
     def test_change_package_name(self):
-        self.run_buf_test(
-            self.test_change_package_name.__name__, is_breaking=True, expects_changes=False)
+        self.run_breaking_test(self.test_change_package_name.__name__)
 
     def test_change_field_from_oneof(self):
-        self.run_buf_test(
-            self.test_change_field_from_oneof.__name__, is_breaking=True, expects_changes=False)
+        self.run_breaking_test(self.test_change_field_from_oneof.__name__)
 
     def test_change_field_to_oneof(self):
-        self.run_buf_test(
-            self.test_change_field_to_oneof.__name__, is_breaking=True, expects_changes=False)
+        self.run_breaking_test(self.test_change_field_to_oneof.__name__)
 
     @unittest.skip("PGV field support not yet added to buf")
     def test_change_pgv_field(self):
-        self.run_buf_test(
-            self.test_change_pgv_field.__name__, is_breaking=True, expects_changes=False)
+        self.run_breaking_test(self.test_change_pgv_field.__name__)
 
     @unittest.skip("PGV message option support not yet added to buf")
     def test_change_pgv_message(self):
-        self.run_buf_test(
-            self.test_change_pgv_message.__name__, is_breaking=True, expects_changes=False)
+        self.run_breaking_test(self.test_change_pgv_message.__name__)
 
     @unittest.skip("PGV oneof option support not yet added to buf")
     def test_change_pgv_oneof(self):
-        self.run_buf_test(
-            self.test_change_pgv_oneof.__name__, is_breaking=True, expects_changes=False)
+        self.run_breaking_test(self.test_change_pgv_oneof.__name__)
 
 
 class TestAllowedChanges(BufTests):
 
+    def run_allowed_test(self, testname, additional_args=None):
+        self.run_buf_test(testname, is_breaking=False, expects_changes=True)
+
     def test_add_comment(self):
-        self.run_buf_test(self.test_add_comment.__name__, is_breaking=False, expects_changes=True)
+        self.run_allowed_test(self.test_add_comment.__name__)
 
     def test_add_field(self):
-        self.run_buf_test(self.test_add_field.__name__, is_breaking=False, expects_changes=True)
+        self.run_allowed_test(self.test_add_field.__name__)
 
     def test_add_option(self):
-        self.run_buf_test(self.test_add_option.__name__, is_breaking=False, expects_changes=True)
+        self.run_allowed_test(self.test_add_option.__name__)
 
     def test_add_enum_value(self):
-        self.run_buf_test(
-            self.test_add_enum_value.__name__, is_breaking=False, expects_changes=True)
+        self.run_allowed_test(self.test_add_enum_value.__name__)
 
     def test_remove_and_reserve_field(self):
-        self.run_buf_test(
-            self.test_remove_and_reserve_field.__name__, is_breaking=False, expects_changes=True)
+        self.run_allowed_test(self.test_remove_and_reserve_field.__name__)
 
     # copied from protolock tests but might remove
     # It doesn't make sense to evaluate 'forcing' a breaking change in buf because by default,
@@ -92,11 +86,7 @@ class TestAllowedChanges(BufTests):
     # Buf does not require forcing breaking changes into the lock file like protolock does
     @unittest.skip("'forcing' a breaking change does not make sense for buf")
     def test_force_breaking_change(self):
-        self.run_buf_test(
-            self.test_force_breaking_change.__name__,
-            is_breaking=False,
-            expects_changes=True,
-            additional_args=["--force"])
+        self.run_allowed_test(self.test_force_breaking_change.__name__, additional_args=["--force"])
 
 
 if __name__ == '__main__':
