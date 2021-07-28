707a4087dcb260554d40b484a3781c70b5546f9a
Mike Schore <mike.schore@gmail.com>
2021-07-28 13:11:11 -0700

    tests: add filter reset idle integration test (#1591)
    Signed-off-by: Mike Schore <mike.schore@gmail.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

---
+++ mobile/test/swift/integration/FilterResetIdleTest.swift
@@ -0,0 +1,195 @@
+import Envoy
+import EnvoyEngine
+import Foundation
+import XCTest
+
+final class FilterResetIdleTests: XCTestCase {
+  func testFilterResetIdle() {
+    let idleTimeout = "0.5s"
+    // swiftlint:disable:next line_length
+    let hcmType = "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager"
+    // swiftlint:disable:next line_length
+    let pbfType = "type.googleapis.com/envoymobile.extensions.filters.http.platform_bridge.PlatformBridge"
+    // swiftlint:disable:next line_length
+    let localErrorFilterType = "type.googleapis.com/envoymobile.extensions.filters.http.local_error.LocalError"
+    let filterName = "reset_idle_test_filter"
+    let config =
+"""
+static_resources:
+  listeners:
+  - name: fake_remote_listener
+    address:
+      socket_address: { protocol: TCP, address: 127.0.0.1, port_value: 10101 }
+    filter_chains:
+    - filters:
+      - name: envoy.filters.network.http_connection_manager
+        typed_config:
+          "@type": \(hcmType)
+          stat_prefix: remote_hcm
+          route_config:
+            name: remote_route
+            virtual_hosts:
+            - name: remote_service
+              domains: ["*"]
+              routes:
+              - match: { prefix: "/" }
+                direct_response: { status: 200 }
+          http_filters:
+          - name: envoy.router
+            typed_config:
+              "@type": type.googleapis.com/envoy.extensions.filters.http.router.v3.Router
+  - name: base_api_listener
+    address:
+      socket_address: { protocol: TCP, address: 0.0.0.0, port_value: 10000 }
+    api_listener:
+      api_listener:
+        "@type": \(hcmType)
+        stat_prefix: api_hcm
+        stream_idle_timeout: \(idleTimeout)
+        route_config:
+          name: api_router
+          virtual_hosts:
+          - name: api
+            domains: ["*"]
+            routes:
+            - match: { prefix: "/" }
+              route: { cluster: fake_remote }
+        http_filters:
+        - name: envoy.filters.http.platform_bridge
+          typed_config:
+            "@type": \(pbfType)
+            platform_filter_name: \(filterName)
+        - name: envoy.filters.http.local_error
+          typed_config:
+            "@type": \(localErrorFilterType)
+        - name: envoy.router
+          typed_config:
+            "@type": type.googleapis.com/envoy.extensions.filters.http.router.v3.Router
+  clusters:
+  - name: fake_remote
+    connect_timeout: 0.25s
+    type: STATIC
+    lb_policy: ROUND_ROBIN
+    load_assignment:
+      cluster_name: fake_remote
+      endpoints:
+      - lb_endpoints:
+        - endpoint:
+            address:
+              socket_address: { address: 127.0.0.1, port_value: 10101 }
+"""
+
+    class ResetIdleTestFilter: AsyncRequestFilter, ResponseFilter {
+      let queue = DispatchQueue(label: "io.envoyproxy.async")
+      let resetExpectation: XCTestExpectation
+      var callbacks: RequestFilterCallbacks!
+      var resetCount = 0
+
+      // Trigger 3 idle timer exception, then allow timeout
+      private func signalActivity() {
+        if resetCount < 3 {
+          self.resetCount += 1
+          self.queue.asyncAfter(deadline: .now() + 0.25) { [weak self] in
+            guard let strongSelf = self else {
+              return
+            }
+            strongSelf.callbacks.resetIdleTimer()
+            strongSelf.signalActivity()
+          }
+        } else {
+          resetExpectation.fulfill()
+        }
+      }
+
+      init(resetExpectation: XCTestExpectation) {
+        self.resetExpectation = resetExpectation
+      }
+
+      func setRequestFilterCallbacks(_ callbacks: RequestFilterCallbacks) {
+        self.callbacks = callbacks
+      }
+
+      func onResumeRequest(
+        headers: RequestHeaders?,
+        data: Data?,
+        trailers: RequestTrailers?,
+        endStream: Bool
+      ) -> FilterResumeStatus<RequestHeaders, RequestTrailers> {
+        XCTFail("Unexpected call to onResumeRequest")
+        return .resumeIteration(headers: nil, data: nil, trailers: nil)
+      }
+
+      func onRequestHeaders(_ headers: RequestHeaders, endStream: Bool)
+        -> FilterHeadersStatus<RequestHeaders>
+      {
+        self.signalActivity()
+        return .stopIteration
+      }
+
+      func onRequestData(_ body: Data, endStream: Bool) -> FilterDataStatus<RequestHeaders> {
+        XCTFail("Unexpected call to onRequestData filter callback")
+        return .stopIterationNoBuffer
+      }
+
+      func onRequestTrailers(_ trailers: RequestTrailers)
+          -> FilterTrailersStatus<RequestHeaders, RequestTrailers> {
+        XCTFail("Unexpected call to onRequestTrailers filter callback")
+        return .stopIteration
+      }
+
+      func onResponseHeaders(_ headers: ResponseHeaders, endStream: Bool)
+        -> FilterHeadersStatus<ResponseHeaders>
+      {
+        self.signalActivity()
+        return .stopIteration
+      }
+
+      func onResponseData(_ body: Data, endStream: Bool) -> FilterDataStatus<ResponseHeaders> {
+        XCTFail("Unexpected call to onResponseData filter callback")
+        return .stopIterationNoBuffer
+      }
+
+      func onResponseTrailers(_ trailers: ResponseTrailers)
+          -> FilterTrailersStatus<ResponseHeaders, ResponseTrailers> {
+        XCTFail("Unexpected call to onResponseTrailers filter callback")
+        return .stopIteration
+      }
+
+      func onError(_ error: EnvoyError) {}
+
+      func onCancel() {
+        XCTFail("Unexpected call to onCancel filter callback")
+      }
+    }
+
+    let resetExpectation = self.expectation(description: "Stream idle timer reset 3 times")
+    let timeoutExpectation = self.expectation(description: "Stream idle timeout triggered")
+
+    let client = EngineBuilder(yaml: config)
+      .addLogLevel(.trace)
+      .addPlatformFilter(
+        name: filterName,
+        factory: { ResetIdleTestFilter(resetExpectation: resetExpectation) }
+      )
+      .build()
+      .streamClient()
+
+    let requestHeaders = RequestHeadersBuilder(method: .get, scheme: "https",
+                                               authority: "example.com", path: "/test")
+      .addUpstreamHttpProtocol(.http2)
+      .build()
+
+    client
+      .newStreamPrototype()
+      .setOnError { _ in
+        timeoutExpectation.fulfill()
+      }
+      .start()
+      .sendHeaders(requestHeaders, endStream: true)
+
+    XCTAssertEqual(
+      XCTWaiter.wait(for: [resetExpectation, timeoutExpectation], timeout: 10),
+      .completed
+    )
+  }
+}
