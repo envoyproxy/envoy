e98f4fdcea478a52885177224457442b17c4d993
Alyssa Wilk <alyssar@chromium.org>
2021-07-28 17:02:57 -0400

    runtime: removing require_ocsp_response_for_must_staple_certs
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/extensions/transport_sockets/tls/ssl_socket_test.cc
+++ test/extensions/transport_sockets/tls/ssl_socket_test.cc
@@ -5846,34 +5846,6 @@ TEST_P(SslSocketTest, TestConnectionSucceedsForMustStapleCertExpirationValidatio
                .setExpectedOcspResponse(expected_response));
 }
 
-TEST_P(SslSocketTest, TestConnectionSucceedsForMustStapleCertNoValidationNoResponse) {
-  const std::string server_ctx_yaml = R"EOF(
-  common_tls_context:
-    tls_certificates:
-    - certificate_chain:
-        filename: "{{ test_rundir }}/test/extensions/transport_sockets/tls/ocsp/test_data/revoked_cert.pem"
-      private_key:
-        filename: "{{ test_rundir }}/test/extensions/transport_sockets/tls/ocsp/test_data/revoked_key.pem"
-  ocsp_staple_policy: lenient_stapling
-  )EOF";
-
-  const std::string client_ctx_yaml = R"EOF(
-  common_tls_context:
-    tls_params:
-      cipher_suites:
-      - TLS_RSA_WITH_AES_128_GCM_SHA256
-)EOF";
-
-  TestScopedRuntime scoped_runtime;
-  Runtime::LoaderSingleton::getExisting()->mergeValues(
-      {{"envoy.reloadable_features.require_ocsp_response_for_must_staple_certs", "false"},
-       {"envoy.reloadable_features.check_ocsp_policy", "false"}});
-  TestUtilOptions test_options(client_ctx_yaml, server_ctx_yaml, true, GetParam());
-  testUtil(test_options.setExpectedServerStats("ssl.ocsp_staple_omitted")
-               .enableOcspStapling()
-               .setExpectedOcspResponse(""));
-}
-
 TEST_P(SslSocketTest, TestFilterMultipleCertsFilterByOcspPolicyFallbackOnFirst) {
   const std::string server_ctx_yaml = R"EOF(
   common_tls_context:
