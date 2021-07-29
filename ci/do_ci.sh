f70f3dde7a25e714257c8963381efc275486cba8
John Murray <murray@stripe.com>
2021-07-29 10:43:54 -0600

    PR Feedback
    Signed-off-by: John Murray <murray@stripe.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -144,8 +144,7 @@ function bazel_binary_build() {
 }
 
 function run_process_test_result() {
-  [[ -n "$CI_SKIP_PROCESS_TEST_RESULTS" ]] && return 
-  if [[ $(find "$TEST_TMPDIR" -name "*_attempt.xml" 2> /dev/null) ]]; then
+  if [[ -z "$CI_SKIP_PROCESS_TEST_RESULTS" ]] && [[ $(find "$TEST_TMPDIR" -name "*_attempt.xml" 2> /dev/null) ]]; then
       echo "running flaky test reporting script"
       "${ENVOY_SRCDIR}"/ci/flaky_test/run_process_xml.sh "$CI_TARGET"
   else
