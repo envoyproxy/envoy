938ada158660a08368c7d38271950c4152c301a9
Yan Avlasov <yavlasov@google.com>
2021-08-02 20:04:15 -0400

    Reject or strip fragment from request URI
    Fixes CVE-2021-32779
    
    Signed-off-by: Yan Avlasov <yavlasov@google.com>

--- test/extensions/filters/http/rbac/rbac_filter_integration_test.cc
+++ test/extensions/filters/http/rbac/rbac_filter_integration_test.cc
@@ -64,6 +64,21 @@ typed_config:
           - any: true
 )EOF";
 
+const std::string RBAC_CONFIG_DENY_WITH_PATH_EXACT_MATCH = R"EOF(
+name: rbac
+typed_config:
+  "@type": type.googleapis.com/envoy.extensions.filters.http.rbac.v3.RBAC
+  rules:
+    action: DENY
+    policies:
+      foo:
+        permissions:
+          - url_path:
+              path: { exact: "/deny" }
+        principals:
+          - any: true
+)EOF";
+
 const std::string RBAC_CONFIG_WITH_PATH_IGNORE_CASE_MATCH = R"EOF(
 name: rbac
 typed_config:
@@ -307,8 +322,10 @@ TEST_P(RBACIntegrationTest, RouteOverride) {
   EXPECT_EQ("200", response->headers().getStatusValue());
 }
 
-TEST_P(RBACIntegrationTest, PathWithQueryAndFragment) {
+TEST_P(RBACIntegrationTest, PathWithQueryAndFragmentWithOverride) {
   config_helper_.addFilter(RBAC_CONFIG_WITH_PATH_EXACT_MATCH);
+  config_helper_.addRuntimeOverride("envoy.reloadable_features.http_reject_path_with_fragment",
+                                    "false");
   initialize();
 
   codec_client_ = makeHttpConnection(lookupPort("http"));
@@ -331,6 +348,62 @@ TEST_P(RBACIntegrationTest, PathWithQueryAndFragment) {
     response->waitForEndStream();
     ASSERT_TRUE(response->complete());
     EXPECT_EQ("200", response->headers().getStatusValue());
+  }
+}
+
+TEST_P(RBACIntegrationTest, PathWithFragmentRejectedByDefault) {
+  config_helper_.addFilter(RBAC_CONFIG_WITH_PATH_EXACT_MATCH);
+  initialize();
+
+  codec_client_ = makeHttpConnection(lookupPort("http"));
+
+  auto response = codec_client_->makeRequestWithBody(
+      Http::TestRequestHeaderMapImpl{
+          {":method", "POST"},
+          {":path", "/allow?p1=v1#seg"},
+          {":scheme", "http"},
+          {":authority", "host"},
+          {"x-forwarded-for", "10.0.0.1"},
+      },
+      1024);
+  // Request should not hit the upstream
+  response->waitForEndStream();
+  ASSERT_TRUE(response->complete());
+  EXPECT_EQ("400", response->headers().getStatusValue());
+}
+
+// This test ensures that the exact match deny rule is not affected by fragment and query
+// when Envoy is configured to strip both fragment and query.
+TEST_P(RBACIntegrationTest, DenyExactMatchIgnoresQueryAndFragment) {
+  config_helper_.addFilter(RBAC_CONFIG_DENY_WITH_PATH_EXACT_MATCH);
+  config_helper_.addRuntimeOverride("envoy.reloadable_features.http_reject_path_with_fragment",
+                                    "false");
+  initialize();
+
+  codec_client_ = makeHttpConnection(lookupPort("http"));
+
+  const std::vector<std::string> paths{"/deny#", "/deny#fragment", "/deny?p1=v1&p2=v2",
+                                       "/deny?p1=v1#seg"};
+
+  for (const auto& path : paths) {
+    printf("Testing: %s\n", path.c_str());
+    auto response = codec_client_->makeRequestWithBody(
+        Http::TestRequestHeaderMapImpl{
+            {":method", "POST"},
+            {":path", path},
+            {":scheme", "http"},
+            {":authority", "host"},
+            {"x-forwarded-for", "10.0.0.1"},
+        },
+        1024);
+
+    response->waitForEndStream();
+    ASSERT_TRUE(response->complete());
+    EXPECT_EQ("403", response->headers().getStatusValue());
+    if (downstreamProtocol() == Http::CodecClient::Type::HTTP1) {
+      ASSERT_TRUE(codec_client_->waitForDisconnect());
+      codec_client_ = makeHttpConnection(lookupPort("http"));
+    }
   }
 }
 
