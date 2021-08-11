3e660b14fa3e328eab1be29b8aa00a29696e68af
Greg Greenway <ggreenway@apple.com>
2021-08-11 11:29:58 -0700

    Support upstream network filter
    Signed-off-by: Greg Greenway <ggreenway@apple.com>

--- source/extensions/filters/network/linux_network_stats/config.cc
+++ source/extensions/filters/network/linux_network_stats/config.cc
@@ -11,6 +11,25 @@ namespace Extensions {
 namespace NetworkFilters {
 namespace LinuxNetworkStats {
 
+namespace {
+const std::string FilterName = "envoy.filters.network.linux_network_stats";
+Network::FilterFactoryCb createFilterFactory(
+    const envoy::extensions::filters::network::linux_network_stats::v3::Config& config_proto,
+    Stats::Scope& scope) {
+#if defined(__linux__)
+  ConfigConstSharedPtr config = std::make_shared<Config>(config_proto, scope);
+  return [config](Network::FilterManager& filter_manager) -> void {
+    filter_manager.addReadFilter(std::make_shared<Filter>(config));
+  };
+#else
+  UNREFERENCED_PARAMETER(config_proto);
+  UNREFERENCED_PARAMETER(factory_context);
+  throw EnvoyException("linux_network_stats filter is not supported on this platform.");
+#endif
+}
+} // namespace
+
+namespace Downstream {
 /**
  * Config registration for the filter. @see NamedNetworkFilterConfigFactory.
  */
@@ -18,22 +37,13 @@ class LinuxNetworkStatsConfigFactory
     : public Common::FactoryBase<
           envoy::extensions::filters::network::linux_network_stats::v3::Config> {
 public:
-  LinuxNetworkStatsConfigFactory() : FactoryBase("envoy.filters.network.linux_network_stats") {}
+  LinuxNetworkStatsConfigFactory() : FactoryBase(FilterName) {}
 
 private:
   Network::FilterFactoryCb createFilterFactoryFromProtoTyped(
       const envoy::extensions::filters::network::linux_network_stats::v3::Config& config_proto,
       Server::Configuration::FactoryContext& factory_context) override {
-#if defined(__linux__)
-    ConfigConstSharedPtr config = std::make_shared<Config>(config_proto, factory_context.scope());
-    return [config](Network::FilterManager& filter_manager) -> void {
-      filter_manager.addReadFilter(std::make_shared<Filter>(config));
-    };
-#else
-    UNREFERENCED_PARAMETER(config_proto);
-    UNREFERENCED_PARAMETER(factory_context);
-    throw EnvoyException("linux_network_stats filter is not supported on this platform.");
-#endif
+    return createFilterFactory(config_proto, factory_context.scope());
   }
 };
 
@@ -42,6 +52,29 @@ private:
  */
 REGISTER_FACTORY(LinuxNetworkStatsConfigFactory,
                  Server::Configuration::NamedNetworkFilterConfigFactory);
+} // namespace Downstream
+
+namespace Upstream {
+class LinuxNetworkStatsConfigFactory
+    : public Common::UpstreamFactoryBase<
+          envoy::extensions::filters::network::linux_network_stats::v3::Config> {
+public:
+  LinuxNetworkStatsConfigFactory() : UpstreamFactoryBase(FilterName) {}
+
+private:
+  Network::FilterFactoryCb createFilterFactoryFromProtoTyped(
+      const envoy::extensions::filters::network::linux_network_stats::v3::Config& config_proto,
+      Server::Configuration::CommonFactoryContext& factory_context) override {
+    return createFilterFactory(config_proto, factory_context.scope());
+  }
+};
+
+/**
+ * Static registration for the filter. @see RegisterFactory.
+ */
+REGISTER_FACTORY(LinuxNetworkStatsConfigFactory,
+                 Server::Configuration::NamedUpstreamNetworkFilterConfigFactory);
+} // namespace Upstream
 
 } // namespace LinuxNetworkStats
 } // namespace NetworkFilters
