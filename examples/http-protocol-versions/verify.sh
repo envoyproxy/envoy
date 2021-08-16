6a5eadf32978eb23f6920b37f508fe4c84db7490
su225 <suchithjn22@gmail.com>
2021-08-16 03:31:43 +0000

    refactor example - part 1
    Signed-off-by: su225 <suchithjn22@gmail.com>

--- examples/http-protocol-versions/verify.sh
+++ examples/http-protocol-versions/verify.sh
@@ -1,36 +1,41 @@
 #!/bin/bash -e
 
 export NAME=http-protocol-options
+export MANUAL=true
 
 # shellcheck source=examples/verify-common.sh
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
 set -e
 
+run_log "Build simple HTTP/3 client container"
+docker build -t h3client:test -f Dockerfile-http3-client .
+
+bring_up_example
+
 path_list=( http11 http2 http3 )
 
 run_log "Test HTTP1.1 -> HTTP1.1/HTTP2/HTTP3"
 for path in "${path_list[@]}"; do
-    run_log "HTTP1.1 -> $path"
-    curl -svk -o /dev/null --http1.1 "https://localhost:10000/${path}" 2>&1 | grep "HTTP/1.1 200"
+    run_log "Check HTTP1.1 -> $path"
+    responds_with_header "HTTP/1.1 200" \
+        "https://localhost:10000/${path}" \
+        -k --http1.1
 done
 
 run_log "Test HTTP2 -> HTTP1.1/HTTP2/HTTP3"
 for path in "${path_list[@]}"; do
-    run_log "HTTP2 -> $path"
-    curl -svk -o /dev/null --http2 --http2-prior-knowledge "https://localhost:10001/${path}" 2>&1 | grep "HTTP/2 200"
+    run_log "Check HTTP2 -> $path"
+    responds_with_header "HTTP/2 200" \
+        "https://localhost:10001/${path}" \
+        -k --http2 --http2-prior-knowledge
 done
-
-run_log "Build simple HTTP/3 client container"
-pushd ./http3-client
-docker build -t h3client:test .
-popd
 
 run_log "Test HTTP3 -> HTTP1.1/HTTP2/HTTP3"
 for path in "${path_list[@]}"; do
-    run_log "HTTP3 -> $path"
+    run_log "Check HTTP3 -> $path"
     # SNI parameter needs to be set explicitly because when hostname is localhost
     # crypto handshake fails because it could not figure out the value for SNI parameter
     docker run -it --name h3client --rm --network=host h3client:test /root/h3client \
-        --sni "test.proxy" --address "https://localhost:10002/${path}" | grep "SUCCESS"
+        --sni "test.proxy" --address "https://localhost:10002/${path}" 2>&1
 done
