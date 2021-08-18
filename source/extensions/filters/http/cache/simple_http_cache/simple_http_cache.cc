c3151a8208fa5abba0d09c244661e14c857a50b1
Saidi Tang <tangsaidi@icloud.com>
2021-08-18 14:30:04 -0400

    Omit requests with vary header

--- source/extensions/filters/http/cache/simple_http_cache/simple_http_cache.cc
+++ source/extensions/filters/http/cache/simple_http_cache/simple_http_cache.cc
@@ -112,13 +112,24 @@ LookupContextPtr SimpleHttpCache::makeLookupContext(LookupRequest&& request) {
 void SimpleHttpCache::updateHeaders(const LookupContext& lookup_context,
                                     const Http::ResponseHeaderMap& response_headers,
                                     const ResponseMetadata& metadata) {
-    // TODO(tangsaidi) handle Vary updates properly
     absl::WriterMutexLock lock(&mutex_);
     const auto& simple_lookup_context = static_cast<const SimpleLookupContext&>(lookup_context);
     const Key& key = simple_lookup_context.request().key();
-    map_[key].response_headers_ =
+
+    auto iter = map_.find(key);
+    if (iter == map_.end() || !iter->second.response_headers_) {
+      return;
+    }
+    auto& entry = iter->second;
+
+    // TODO(tangsaidi) handle Vary header updates properly
+    if (VaryHeader::hasVary(*(entry.response_headers_))) {
+      return;
+    }
+
+    entry.response_headers_ =
         Http::createHeaderMap<Http::ResponseHeaderMapImpl>(response_headers);
-    map_[key].metadata_ = metadata;
+    entry.metadata_ = metadata;
 }
 
 SimpleHttpCache::Entry SimpleHttpCache::lookup(const LookupRequest& request) {
