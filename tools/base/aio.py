809d61478851a715ab7364225f8994a2929917a2
Ryan Northey <ryan@synca.io>
2021-08-20 13:12:20 +0100

    tooling: Add github release_manager util
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tools/base/aio.py
+++ tools/base/aio.py
@@ -27,6 +27,21 @@ class ConcurrentExecutionError(ConcurrentError):
     pass
 
 
+class ConcurrentError(Exception):
+    """Raised when given inputs/awaitables are incorrect"""
+    pass
+
+
+class ConcurrentIteratorError(ConcurrentError):
+    """Raised when iteration of provided awaitables fails"""
+    pass
+
+
+class ConcurrentExecutionError(ConcurrentError):
+    """Raised when execution of a provided awaitable fails"""
+    pass
+
+
 class async_subprocess:  # noqa: N801
 
     @classmethod
