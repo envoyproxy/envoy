1dff01561db55b27689f7904a8f6715b62689f97
Yaseen Alkhafaji <yaseena@google.com>
2021-08-22 21:13:28 +0000

    cd during all buf ops to fix ci
    Signed-off-by: Yaseen Alkhafaji <yaseena@google.com>

--- tools/api_proto_breaking_change_detector/buf_utils.py
+++ tools/api_proto_breaking_change_detector/buf_utils.py
@@ -13,7 +13,7 @@ def _generate_buf_args(target_path, config_file_loc, additional_args):
     target_relative = Path(target_path).absolute().relative_to(Path.cwd().absolute())
 
     # buf does not accept . as a root; if we are already in the target dir, no need for a --path arg
-    if target_relative != Path.cwd():
+    if str(target_relative) != ".":
         buf_args.extend(["--path", str(target_relative)])
 
     if config_file_loc:
@@ -24,6 +24,18 @@ def _generate_buf_args(target_path, config_file_loc, additional_args):
     return buf_args
 
 
+def _cd_into_config_parent(func):
+
+    def cd_wrapper(*args, **kwargs):
+        config_file_loc = kwargs.get("config_file_loc")
+        config_parent = Path(config_file_loc).parent if config_file_loc else Path.cwd()
+        with cd_and_return(config_parent):
+            return func(*args, **kwargs)
+
+    return cd_wrapper
+
+
+@_cd_into_config_parent
 def pull_buf_deps(
         buf_path: Union[str, Path],
         target_path: Union[str, Path],
@@ -44,25 +56,27 @@ def pull_buf_deps(
     Raises:
         ChangeDetectorInitializeError: if buf encounters an error while attempting to update the buf.lock file or build afterward
     """
-    config_parent = Path(config_file_loc).parent if config_file_loc else Path.cwd()
-    with cd_and_return(config_parent):
-        buf_args = _generate_buf_args(target_path, config_file_loc, additional_args)
+    buf_args = _generate_buf_args(target_path, config_file_loc, additional_args)
 
-        update_code, _, update_err = run_command(f'{buf_path} mod update')
-        # for some reason buf prints out the "downloading..." lines on stderr
-        if update_code != 0:
-            raise ChangeDetectorInitializeError(
-                f'Error running `buf mod update`: exit status code {update_code} | stderr: {update_err}'
-            )
+    update_code, _, update_err = run_command(f'{buf_path} mod update')
+    # for some reason buf prints out the "downloading..." lines on stderr
+    if update_code != 0:
+        raise ChangeDetectorInitializeError(
+            f'Error running `buf mod update`: exit status code {update_code} | stderr: {update_err}'
+        )
+    if not Path(Path.cwd(), "buf.lock").exists():
+        raise ChangeDetectorInitializeError(
+            "buf mod update did not generate a buf.lock file (silent error... incorrect config?)")
 
-        build_code, build_out, build_err = run_command(' '.join([f'{buf_path} build', *buf_args]))
-        build_out, build_err = ''.join(build_out), ''.join(build_err)
-        if build_code != 0:
-            raise ChangeDetectorInitializeError(
-                f'Error running `buf build` after updating deps: exit status code {build_code} | stdout: {build_out} | stderr: {build_err}'
-            )
+    build_code, build_out, build_err = run_command(' '.join([f'{buf_path} build', *buf_args]))
+    build_out, build_err = ''.join(build_out), ''.join(build_err)
+    if build_code != 0:
+        raise ChangeDetectorInitializeError(
+            f'Error running `buf build` after updating deps: exit status code {build_code} | stdout: {build_out} | stderr: {build_err}'
+        )
 
 
+@_cd_into_config_parent
 def make_lock(
         buf_path: Union[str, Path],
         target_path: Union[str, Path],
@@ -100,6 +114,7 @@ def make_lock(
     return initial_code, initial_out, initial_err
 
 
+@_cd_into_config_parent
 def check_breaking(
         buf_path: Union[str, Path],
         target_path: Union[str, Path],
