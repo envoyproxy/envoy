07bef73f1ec36fb90d85683811a00f3cc77cd309
su225 <suchithjn22@gmail.com>
2021-08-23 20:05:11 +0530

    refactor HTTP/3 client image to use docker-compose
    Signed-off-by: su225 <suchithjn22@gmail.com>

--- examples/http-protocol-versions/verify.sh
+++ examples/http-protocol-versions/verify.sh
@@ -8,9 +8,13 @@ export MANUAL=true
 
 set -e
 
-run_log "Build simple HTTP/3 client container"
-docker build -t h3client:test -f Dockerfile-http3-client .
+run_log "Generate front envoy certificates"
+openssl req -x509 -newkey rsa:2048 \
+    -keyout serverkey.pem \
+    -out  servercert.pem \
+    -days 3650 -nodes -subj '/CN=front-envoy'
 
+run_log "Bring up HTTP protocol stack"
 bring_up_example
 
 path_list=( http11 http2 http3 )
@@ -36,6 +40,6 @@ for path in "${path_list[@]}"; do
     run_log "Check HTTP3 -> $path"
     # SNI parameter needs to be set explicitly because when hostname is localhost
     # crypto handshake fails because it could not figure out the value for SNI parameter
-    docker run --name h3client --rm --network=host h3client:test /root/h3client \
-        --sni "test.proxy" --address "https://localhost:10002/${path}" 2>&1 | grep "SUCCESS"
+    docker-compose -f docker-compose-h3-client.yaml run \
+        h3client /root/h3client --sni "test.proxy" --address "https://localhost:10002/${path}" 2>&1 | grep "SUCCESS"
 done
