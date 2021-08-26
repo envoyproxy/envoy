2da772f9320b9cba83e00902bb3c4d6c33ecb3bd
Snow Pettersen <snowp@lyft.com>
2021-08-26 18:10:58 -0400

    common: use RELEASE_ASSERT to verify API registration (#1681)
    Description: use RELEASE_ASSERT to verify API registration
    Risk Level: Medium
    Testing: Existing tests
    Docs Changes: n/a
    Release Notes: n/a
    
    Signed-off-by: Snow Pettersen <snowp@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/common/api/external.cc
+++ mobile/library/common/api/external.cc
@@ -15,13 +15,16 @@ static absl::flat_hash_map<std::string, void*> registry_{};
 // TODO(goaway): To expose this for general usage, it will need to be made thread-safe. For now it
 // relies on the assumption that usage will occur only as part of Engine configuration, and thus be
 // limited to a single thread.
-void registerApi(std::string name, void* api) { registry_[name] = api; }
+void registerApi(std::string name, void* api) {
+  RELEASE_ASSERT(api != nullptr, "cannot register null API");
+  registry_[name] = api;
+}
 
 // TODO(goaway): This is not thread-safe, but the assumption here is that all writes will complete
 // before any reads occur.
 void* retrieveApi(std::string name) {
   void* api = registry_[name];
-  ASSERT(api, fmt::format("{} not registered", name));
+  RELEASE_ASSERT(api != nullptr, fmt::format("{} not registered", name));
   return api;
 }
 
