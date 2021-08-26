072fe7c7f50e4ce6982859e7804e053b7e30bd06
John Esmet <john.esmet@gmail.com>
2021-08-26 21:15:03 +0000

    Fix format
    Signed-off-by: John Esmet <john.esmet@gmail.com>

--- source/extensions/filters/common/ext_authz/ext_authz_http_impl.cc
+++ source/extensions/filters/common/ext_authz/ext_authz_http_impl.cc
@@ -355,7 +355,8 @@ ResponsePtr RawHttpClientImpl::toResponse(Http::ResponseMessagePtr message) {
   }
 
   // Create a Denied authorization response.
-  SuccessResponse denied{message->headers(), config_->clientHeaderMatchers(),
+  SuccessResponse denied{message->headers(),
+                         config_->clientHeaderMatchers(),
                          config_->upstreamHeaderToAppendMatchers(),
                          config_->clientHeaderOnSuccessMatchers(),
                          config_->dynamicMetadataMatchers(),
