a78ca39b28e6054b82f7e9baec57c09e6afca90e
He Jie Xu <hejie.xu@intel.com>
2021-08-31 10:35:27 +0000

    fix
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- source/server/active_tcp_socket.h
+++ source/server/active_tcp_socket.h
@@ -75,6 +75,9 @@ struct ActiveTcpSocket : public Network::ListenerFilterManager,
   // Network::ListenerFilterManager
   void addAcceptFilter(const Network::ListenerFilterMatcherSharedPtr& listener_filter_matcher,
                        Network::ListenerFilterPtr&& filter) override {
+    if (filter->inspectSize() > inspect_buffer_size_) {
+      inspect_buffer_size_ = filter->inspectSize();
+    }
     accept_filters_.emplace_back(
         std::make_unique<GenericListenerFilter>(listener_filter_matcher, std::move(filter)));
   }
