59cc9e31573f28316c8ac7931517ca1737ab2231
Takeshi Yoneda <takeshi@tetrate.io>
2021-08-31 12:02:05 +0900

    Fix test
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- test/extensions/access_loggers/grpc/http_config_test.cc
+++ test/extensions/access_loggers/grpc/http_config_test.cc
@@ -30,17 +30,41 @@ public:
 
     message_ = factory_->createEmptyConfigProto();
     ASSERT_NE(nullptr, message_);
+  }
 
-    EXPECT_CALL(context_.cluster_manager_.async_client_manager_, factoryForGrpcService(_, _, _))
-        .WillOnce(Invoke([](const envoy::config::core::v3::GrpcService&, Stats::Scope&, bool) {
-          return std::make_unique<NiceMock<Grpc::MockAsyncClientFactory>>();
-        }));
+  void run(const std::string cluster_name) {
+    const auto fake_static = "fake_static";
+    const auto fake_added_via_api = "fake_added_via_api";
+    context_.cluster_manager_.initializeClusters({fake_static, fake_added_via_api}, {});
+    ON_CALL(*context_.cluster_manager_.active_clusters_[fake_static]->info_, addedViaApi())
+        .WillByDefault(Return(false));
+    ON_CALL(*context_.cluster_manager_.active_clusters_[fake_added_via_api]->info_, addedViaApi())
+        .WillByDefault(Return(true));
 
     auto* common_config = http_grpc_access_log_.mutable_common_config();
     common_config->set_log_name("foo");
-    common_config->mutable_grpc_service()->mutable_envoy_grpc()->set_cluster_name("bar");
+    common_config->mutable_grpc_service()->mutable_envoy_grpc()->set_cluster_name(cluster_name);
     common_config->set_transport_api_version(envoy::config::core::v3::ApiVersion::V3);
     TestUtility::jsonConvert(http_grpc_access_log_, *message_);
+
+    if (cluster_name == fake_static) {
+      EXPECT_CALL(context_.cluster_manager_.async_client_manager_, factoryForGrpcService(_, _, _))
+          .WillOnce(Invoke([](const envoy::config::core::v3::GrpcService&, Stats::Scope&, bool) {
+            return std::make_unique<NiceMock<Grpc::MockAsyncClientFactory>>();
+          }));
+      AccessLog::InstanceSharedPtr instance =
+          factory_->createAccessLogInstance(*message_, std::move(filter_), context_);
+      EXPECT_NE(nullptr, instance);
+      EXPECT_NE(nullptr, dynamic_cast<HttpGrpcAccessLog*>(instance.get()));
+    } else if (cluster_name == fake_added_via_api) {
+      EXPECT_THROW_WITH_MESSAGE(
+          factory_->createAccessLogInstance(*message_, std::move(filter_), context_),
+          EnvoyException, fmt::format("gRPC client cluster '{}' is not static", cluster_name));
+    } else {
+      EXPECT_THROW_WITH_MESSAGE(
+          factory_->createAccessLogInstance(*message_, std::move(filter_), context_),
+          EnvoyException, fmt::format("Unknown gRPC client cluster '{}'", cluster_name));
+    }
   }
 
   AccessLog::FilterPtr filter_;
@@ -51,11 +75,12 @@ public:
 };
 
 // Normal OK configuration.
-TEST_F(HttpGrpcAccessLogConfigTest, Ok) {
-  AccessLog::InstanceSharedPtr instance =
-      factory_->createAccessLogInstance(*message_, std::move(filter_), context_);
-  EXPECT_NE(nullptr, instance);
-  EXPECT_NE(nullptr, dynamic_cast<HttpGrpcAccessLog*>(instance.get()));
+TEST_F(HttpGrpcAccessLogConfigTest, Ok) { run("fake_static"); }
+
+// Wrong configuration with invalid clusters.
+TEST_F(HttpGrpcAccessLogConfigTest, InvalidCluster) {
+  run("fake_added_via_api");
+  run("non_exist");
 }
 
 } // namespace
