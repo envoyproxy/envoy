d9f08038064f05b101628413c8a1487caaa3c6e8
Krzesimir Nowak <knowak@microsoft.com>
2021-09-01 17:57:15 +0200

    Document the resource state machine
    Signed-off-by: Krzesimir Nowak <knowak@microsoft.com>

--- source/common/config/delta_subscription_state.h
+++ source/common/config/delta_subscription_state.h
@@ -22,6 +22,57 @@ namespace Config {
 // There can be multiple DeltaSubscriptionStates active. They will always all be
 // blissfully unaware of each other's existence, even when their messages are
 // being multiplexed together by ADS.
+//
+// There are two scenarios which affect how DeltaSubscriptionState manages the resources. First
+// scenario is when we are subscribed to a wildcard resource, and other scenario is when we are not.
+//
+// Delta subscription state also divides the resources it cached into three categories: requested,
+// wildcard and ambiguous.
+//
+// The "requested" category is for resources that we have explicitly asked for (either through the
+// initial set of resources or through the on-demand mechanism). Resources in this category are in
+// one of two states: "complete" and "waiting for server".
+//
+// "Complete" resources are resources about which the server sent us the information we need (for
+// now - just resource version).
+//
+// The "waiting for server" state is either for resources that we have just requested, but we still
+// didn't receive any version information from the server, or for the "complete" resources that,
+// according to the server, are gone, but we are still interested in them - in such case we strip
+// the information from the resource.
+//
+// The "wildcard" category is for resources that we are not explicitly interested in, but we are
+// indirectly interested through the subscription to the wildcard resource.
+//
+// The "ambiguous" category is for resources that we stopped being interested in, but we may still
+// be interested indirectly through the wildcard subscription - resources in these category are
+// "waiting" for the config server to confirm their status.
+//
+// Please refer to drawings (non-wildcard-resource-state-machine.png and
+// (wildcard-resource-state-machine.png) for visual depictions of the resource state machine.
+//
+// In the "no wildcard subscription" scenario all the cached resources should be in the "requested"
+// category. Resources are added to the category upon the explicit request and dropped when we
+// explicitly unsubscribe from it. Transitions between "complete" and "waiting for server" happen
+// when we receive messages from the server - if a resource in the message is in "added resources"
+// list (thus contains version information), the resource becomes "complete". If the resource in the
+// message is in "removed resources" list, it changes into the "waiting for server" state. If a
+// server sends us a resource that we didn't request, it's going to end up in the "wildcard"
+// category. Such resources are dropped when the server send us a message with the resource in
+// "removed resources" list. But this normally should not happen.
+//
+// In the "wildcard subscription" scenario, "requested" category is the same as in "no wildcard
+// subscription" scenario, with one exception - the unsubscribed "complete" resource is not removed
+// from the cache, but it's moved to the "ambiguous" resources instead. At this point we are waiting
+// for the server to tell us that this resource should be either moved to the "wildcard" resources,
+// or dropped. Resources in "wildcard" category are only added there or dropped from there by the
+// server. Resources from both "wildcard" and "ambiguous" categories can become "requested"
+// "complete" resources if we subscribe to them again.
+//
+// The delta subscription state transitions between the two scenarios depending on whether we are
+// subscribed to wildcard resource or not. Nothing special happens when we transition from "no
+// wildcard subscription" to "wildcard subscription" scenario, but when transitioning in the other
+// direction, we drop all the resources in "wildcard" and "ambiguous" categories.
 class DeltaSubscriptionState : public Logger::Loggable<Logger::Id::config> {
 public:
   DeltaSubscriptionState(std::string type_url, UntypedConfigUpdateCallbacks& watch_map,
