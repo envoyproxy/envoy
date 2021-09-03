9d339752df37c5e1121efd22e372eaa510896ded
Jojy George Varghese <jojy_varghese@apple.com>
2021-09-03 15:11:03 -0700

    Updated extensions_check.py.
    Signed-off-by: Jojy George Varghese <jojy_varghese@apple.com>

--- tools/extensions/extensions_check.py
+++ tools/extensions/extensions_check.py
@@ -54,7 +54,7 @@ EXTENSION_CATEGORIES = (
     "envoy.retry_host_predicates", "envoy.retry_priorities", "envoy.stats_sinks",
     "envoy.thrift_proxy.filters", "envoy.tracers", "envoy.transport_sockets.downstream",
     "envoy.transport_sockets.upstream", "envoy.tls.cert_validator", "envoy.upstreams",
-    "envoy.wasm.runtime", "envoy.common.key_value", "envoy.rbac.matchers")
+    "envoy.wasm.runtime", "envoy.common.key_value", "envoy.filters.common.rbac.matcher")
 
 EXTENSION_STATUS_VALUES = (
     # This extension is stable and is expected to be production usable.
