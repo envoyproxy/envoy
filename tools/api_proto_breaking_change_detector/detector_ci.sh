939c6864c1611ab1d08eb56bee4be8909ce9f01b
Ryan Northey <ryan@synca.io>
2021-09-03 17:34:21 +0100

    tooling: Shift `api_proto_breaking_change_detector` to bazel
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tools/api_proto_breaking_change_detector/detector_ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-tools="$(dirname "$(dirname "$(realpath "$0")")")"
-root=$(realpath "$tools/..")
-
-cd "$root" || exit 1
-# to satisfy dependency on run_command (as done in tools/code_format/check_format_test_helper.sh)
-export PYTHONPATH="$root"
-./tools/api_proto_breaking_change_detector/detector_ci.py "$@"
