1de658c3eac1b0254e1ecba4ab8888421c40ae68
Raul Gutierrez Segales <rgs@pinterest.com>
2021-09-13 15:09:01 -0400

    Bump envoy-filter-example version.
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>

--- ci/filter_example_setup.sh
+++ ci/filter_example_setup.sh
@@ -5,7 +5,7 @@
 set -e
 
 # This is the hash on https://github.com/envoyproxy/envoy-filter-example.git we pin to.
-ENVOY_FILTER_EXAMPLE_GITSHA="ac6a66a4a1c08138ccc03c23aafc9637b2df55a1"
+ENVOY_FILTER_EXAMPLE_GITSHA="85b7e0ed452f0987b480feb9cfe46abc914aca67"
 ENVOY_FILTER_EXAMPLE_SRCDIR="${BUILD_DIR}/envoy-filter-example"
 
 # shellcheck disable=SC2034
