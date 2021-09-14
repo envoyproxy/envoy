1dcd77932a4f8e5694d43438ff3b783b00605469
Takeshi Yoneda <takeshi@tetrate.io>
2021-09-14 10:08:54 +0900

    access_logger: correctly throw exception for grpc logger (#17908)
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>
    Signed-off-by: gayang <gayang@microsoft.com>

--- test/extensions/access_loggers/grpc/tcp_config_test.cc
+++ test/extensions/access_loggers/grpc/tcp_config_test.cc
@@ -4,7 +4,7 @@
 #include "envoy/server/access_log_config.h"
 #include "envoy/stats/scope.h"
 
-#include "source/extensions/access_loggers/grpc/http_grpc_access_log_impl.h"
+#include "source/extensions/access_loggers/grpc/tcp_grpc_access_log_impl.h"
 
 #include "test/mocks/server/factory_context.h"
 
@@ -17,15 +17,15 @@ using testing::Invoke;
 namespace Envoy {
 namespace Extensions {
 namespace AccessLoggers {
-namespace HttpGrpc {
+namespace TcpGrpc {
 namespace {
 
-class HttpGrpcAccessLogConfigTest : public testing::Test {
+class TcpGrpcAccessLogConfigTest : public testing::Test {
 public:
   void SetUp() override {
     factory_ =
         Registry::FactoryRegistry<Server::Configuration::AccessLogInstanceFactory>::getFactory(
-            "envoy.access_loggers.http_grpc");
+            "envoy.access_loggers.tcp_grpc");
     ASSERT_NE(nullptr, factory_);
 
     message_ = factory_->createEmptyConfigProto();
@@ -41,11 +41,11 @@ public:
           }
         }));
 
-    auto* common_config = http_grpc_access_log_.mutable_common_config();
+    auto* common_config = tcp_grpc_access_log_.mutable_common_config();
     common_config->set_log_name("foo");
     common_config->mutable_grpc_service()->mutable_envoy_grpc()->set_cluster_name(cluster_name);
     common_config->set_transport_api_version(envoy::config::core::v3::ApiVersion::V3);
-    TestUtility::jsonConvert(http_grpc_access_log_, *message_);
+    TestUtility::jsonConvert(tcp_grpc_access_log_, *message_);
 
     if (cluster_name == good_cluster) {
       EXPECT_CALL(context_.cluster_manager_.async_client_manager_, factoryForGrpcService(_, _, _))
@@ -55,7 +55,7 @@ public:
       AccessLog::InstanceSharedPtr instance =
           factory_->createAccessLogInstance(*message_, std::move(filter_), context_);
       EXPECT_NE(nullptr, instance);
-      EXPECT_NE(nullptr, dynamic_cast<HttpGrpcAccessLog*>(instance.get()));
+      EXPECT_NE(nullptr, dynamic_cast<TcpGrpcAccessLog*>(instance.get()));
     } else {
       EXPECT_THROW_WITH_MESSAGE(
           factory_->createAccessLogInstance(*message_, std::move(filter_), context_),
@@ -65,19 +65,19 @@ public:
 
   AccessLog::FilterPtr filter_;
   NiceMock<Server::Configuration::MockServerFactoryContext> context_;
-  envoy::extensions::access_loggers::grpc::v3::HttpGrpcAccessLogConfig http_grpc_access_log_;
+  envoy::extensions::access_loggers::grpc::v3::TcpGrpcAccessLogConfig tcp_grpc_access_log_;
   ProtobufTypes::MessagePtr message_;
   Server::Configuration::AccessLogInstanceFactory* factory_{};
 };
 
 // Normal OK configuration.
-TEST_F(HttpGrpcAccessLogConfigTest, Ok) { run("good_cluster"); }
+TEST_F(TcpGrpcAccessLogConfigTest, Ok) { run("good_cluster"); }
 
 // Wrong configuration with invalid clusters.
-TEST_F(HttpGrpcAccessLogConfigTest, InvalidCluster) { run("invalid"); }
+TEST_F(TcpGrpcAccessLogConfigTest, InvalidCluster) { run("invalid"); }
 
 } // namespace
-} // namespace HttpGrpc
+} // namespace TcpGrpc
 } // namespace AccessLoggers
 } // namespace Extensions
 } // namespace Envoy
