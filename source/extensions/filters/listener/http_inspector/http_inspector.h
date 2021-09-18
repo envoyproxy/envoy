efa5857463cbaf9bbf4ae8fe7c3bf9860bdc1778
He Jie Xu <hejie.xu@intel.com>
2021-09-18 03:51:41 +0000

    Convert the http inspect filter to new interface
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- source/extensions/filters/listener/http_inspector/http_inspector.h
+++ source/extensions/filters/listener/http_inspector/http_inspector.h
@@ -69,16 +69,15 @@ public:
 
   // Network::ListenerFilter
   Network::FilterStatus onAccept(Network::ListenerFilterCallbacks& cb) override;
+  Network::FilterStatus onData(Network::ListenerFilterBuffer& buffer) override;
 
-  // TODO (soulxu) implement this instead of filter peek data by itself.
-  Network::FilterStatus onData(Network::ListenerFilterBuffer&) override {
-    return Network::FilterStatus::Continue;
-  };
+  size_t maxReadBytes() const override {
+    return Config::MAX_INSPECT_SIZE;
+  }
 
 private:
   static const absl::string_view HTTP2_CONNECTION_PREFACE;
 
-  ParseState onRead();
   void done(bool success);
   ParseState parseHttpHeader(absl::string_view data);
 
@@ -90,9 +89,6 @@ private:
   absl::string_view protocol_;
   http_parser parser_;
   static http_parser_settings settings_;
-
-  // Use static thread_local to avoid allocating buffer over and over again.
-  static thread_local uint8_t buf_[Config::MAX_INSPECT_SIZE];
 };
 
 } // namespace HttpInspector
