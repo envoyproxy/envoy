e67a46e726ef81dfcfcc56bd3185f53c4e43888d
John Esmet <john.esmet@gmail.com>
2021-09-21 15:14:54 +0000

    Format
    Signed-off-by: John Esmet <john.esmet@gmail.com>

--- source/extensions/filters/common/ext_authz/ext_authz_grpc_impl.cc
+++ source/extensions/filters/common/ext_authz/ext_authz_grpc_impl.cc
@@ -62,7 +62,8 @@ void GrpcClientImpl::onSuccess(std::unique_ptr<envoy::service::auth::v3::CheckRe
           if (query_parameter.remove()) {
             authz_response->query_parameters_to_remove.push_back(query_parameter.key());
           } else {
-            authz_response->query_parameters_to_set.push_back(std::pair(query_parameter.key(), query_parameter.value()));
+            authz_response->query_parameters_to_set.push_back(
+                std::pair(query_parameter.key(), query_parameter.value()));
           }
         }
       }
