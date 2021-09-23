e47e5571730f7b8676ba5eb3d0a1d73380199d6d
danzh <danzh2010@users.noreply.github.com>
2021-09-23 16:59:43 -0400

    quiche: support inplace filter chain update (#17988)
    Commit Message: updateListenerConfig() and onFilterChainDraining() to ActiveListener interface and implement them in ActiveQuicListener. Unblock listener update for UDP listener.
    
    Risk Level: high, touches LDS.
    Testing: new integration tests
    Fixes #13115
    Signed-off-by: Dan Zhang <danzh@google.com>
    Signed-off-by: gayang <gayang@microsoft.com>

--- source/server/active_tcp_listener.h
+++ source/server/active_tcp_listener.h
@@ -74,7 +74,7 @@ public:
    * Update the listener config. The follow up connections will see the new config. The existing
    * connections are not impacted.
    */
-  void updateListenerConfig(Network::ListenerConfig& config);
+  void updateListenerConfig(Network::ListenerConfig& config) override;
 
   Network::TcpConnectionHandler& tcp_conn_handler_;
   // The number of connections currently active on this listener. This is typically used for
