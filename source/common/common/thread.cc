ab113de3f0c744d0372ad45c64bd6bd468160b48
Joshua Marantz <jmarantz@google.com>
2021-09-23 16:29:19 -0400

    use InjectableSingleton rather than rolling our own, and use mutexes rather than atomics.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/common/common/thread.cc
+++ source/common/common/thread.cc
@@ -3,41 +3,48 @@
 #include <thread>
 
 #include "source/common/common/assert.h"
+#include "source/common/singleton/threadsafe_singleton.h"
 
 namespace Envoy {
 namespace Thread {
 
 struct ThreadIds {
-  bool inMainThread(std::thread::id id) const {
-    return main_thread_id_.has_value() && (main_thread_id_.value() == id);
+  using Singleton = InjectableSingleton<ThreadIds>;
+
+  bool inMainOrTestThread() const {
+    std::thread::id id = std::this_thread::get_id();
+    absl::MutexLock lock(&mutex_);
+    return ((main_thread_id_.has_value() && (main_thread_id_.value() == id)) ||
+            (test_thread_id_.has_value() && (test_thread_id_.value() == id)));
   }
 
-  bool inTestThread(std::thread::id id) const {
-    return test_thread_id_.has_value() && (test_thread_id_.value() == id);
+  static ThreadIds& get() {
+    ThreadIds* singleton = Singleton::getExisting();
+    ASSERT(singleton != nullptr, "Thread::MainThread or Thread::TestTHread must be instantiated");
+    return *singleton;
   }
 
-  static ThreadIds* get() {
-    ASSERT(singleton_ != nullptr, "Thread::MainThread or Thread::TestTHread must be instantiated");
-    return singleton_;
-  }
-
-  static ThreadIds* acquire() {
-    if (singleton_ == nullptr) {
-      singleton_ = new ThreadIds;
+  static ThreadIds& acquire() {
+    ThreadIds* singleton = Singleton::getExisting();
+    if (singleton == nullptr) {
+      singleton = new ThreadIds;
+      Singleton::initialize(singleton);
     } else {
-      ++singleton_->ref_count_;
+      absl::MutexLock lock(&singleton->mutex_);
+      ++singleton->ref_count_;
     }
-    return singleton_;
+    return *singleton;
   }
 
-  void release() {
+  void release() EXCLUSIVE_LOCKS_REQUIRED(mutex_) {
     if (--ref_count_ == 0) {
-      delete singleton_;
-      singleton_ = nullptr;
+      Singleton::clear();
+      delete this;
     }
   }
 
   void releaseMainThread() {
+    absl::MutexLock lock(&mutex_);
     ASSERT(std::this_thread::get_id() == main_thread_id_.value());
     if (--main_thread_use_count_ == 0) {
       main_thread_id_ = absl::nullopt;
@@ -46,6 +53,7 @@ struct ThreadIds {
   }
 
   void releaseTestThread() {
+    absl::MutexLock lock(&mutex_);
     ASSERT(std::this_thread::get_id() == test_thread_id_.value());
     if (--test_thread_use_count_ == 0) {
       test_thread_id_ = absl::nullopt;
@@ -54,6 +62,7 @@ struct ThreadIds {
   }
 
   void registerMainThread() {
+    absl::MutexLock lock(&mutex_);
     ++main_thread_use_count_;
     if (main_thread_id_.has_value()) {
       ASSERT(std::this_thread::get_id() == main_thread_id_.value());
@@ -63,6 +72,7 @@ struct ThreadIds {
   }
 
   void registerTestThread() {
+    absl::MutexLock lock(&mutex_);
     ++test_thread_use_count_;
     if (test_thread_id_.has_value()) {
       ASSERT(std::this_thread::get_id() == test_thread_id_.value());
@@ -72,29 +82,23 @@ struct ThreadIds {
   }
 
 private:
-  absl::optional<std::thread::id> main_thread_id_;
-  absl::optional<std::thread::id> test_thread_id_;
-  std::atomic<int32_t> ref_count_{1};
-  std::atomic<int32_t> main_thread_use_count_{0};
-  std::atomic<int32_t> test_thread_use_count_{0};
-  static ThreadIds* singleton_;
+  absl::optional<std::thread::id> main_thread_id_ GUARDED_BY(mutex_);
+  absl::optional<std::thread::id> test_thread_id_ GUARDED_BY(mutex_);
+  int32_t main_thread_use_count_ GUARDED_BY(mutex_) = 0;
+  int32_t test_thread_use_count_ GUARDED_BY(mutex_) = 0;
+  int32_t ref_count_ GUARDED_BY(mutex_) = 1;
+  mutable absl::Mutex mutex_;
 };
 
-ThreadIds* ThreadIds::singleton_ = nullptr;
+bool MainThread::isMainThread() { return ThreadIds::get().inMainOrTestThread(); }
 
-bool MainThread::isMainThread() {
-  ThreadIds* ids = ThreadIds::get();
-  std::thread::id id = std::this_thread::get_id();
-  return ids->inMainThread(id) || ids->inTestThread(id);
-}
+TestThread::TestThread() { ThreadIds::acquire().registerTestThread(); }
 
-TestThread::TestThread() { ThreadIds::acquire()->registerTestThread(); }
+TestThread::~TestThread() { ThreadIds::get().releaseTestThread(); }
 
-TestThread::~TestThread() { ThreadIds::get()->releaseTestThread(); }
+MainThread::MainThread() { ThreadIds::acquire().registerMainThread(); }
 
-MainThread::MainThread() { ThreadIds::acquire()->registerMainThread(); }
-
-MainThread::~MainThread() { ThreadIds::get()->releaseMainThread(); }
+MainThread::~MainThread() { ThreadIds::get().releaseMainThread(); }
 
 } // namespace Thread
 } // namespace Envoy
