6267882c4016d2da40a3bdd47134d72ce4628756
chaoqin-li1123 <chaoqinli@google.com>
2021-09-24 14:18:03 +0000

    add comment
    Signed-off-by: chaoqin-li1123 <chaoqinli@google.com>

--- test/mocks/grpc/mocks.h
+++ test/mocks/grpc/mocks.h
@@ -90,6 +90,7 @@ public:
                const Http::AsyncClient::StreamOptions& options));
 
   std::unique_ptr<testing::NiceMock<Grpc::MockAsyncRequest>> async_request_;
+  // Keep track of the number of requests to detect potential race condition.
   int send_cnt_{};
 };
 
