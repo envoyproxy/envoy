dc9165ebde83ae2a8bd6b9c0471654130db34d0c
Douglas Reid <dougreid@google.com>
2021-09-28 16:51:16 +0000

    accesslogs: add CEL-based extension filter
    This PR establishes the ability to filter access log production via
    CEL expressions over the set of Envoy attributes. This can simply
    the creation of Envoy access log filters, allowing complex tailoring.
    
    Signed-off-by: Douglas Reid <dougreid@google.com>

---
+++ source/extensions/access_loggers/filters/cel/config.cc
@@ -0,0 +1,55 @@
+#include "source/extensions/access_loggers/filters/cel/config.h"
+
+#include "envoy/extensions/access_loggers/filters/cel/v3/cel.pb.h"
+
+#include "source/extensions/access_loggers/filters/cel/cel.h"
+
+#include "parser/parser.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace AccessLoggers {
+namespace Filters {
+namespace CEL {
+
+Envoy::AccessLog::FilterPtr CELAccessLogExtensionFilterFactory::createFilter(
+    const envoy::config::accesslog::v3::ExtensionFilter& config, Runtime::Loader&,
+    Random::RandomGenerator&) {
+  auto factory_config = Config::Utility::translateToFactoryConfig(
+      config, Envoy::ProtobufMessage::getNullValidationVisitor(), *this);
+
+  envoy::extensions::access_loggers::filters::cel::v3::ExpressionFilter cel_config =
+      *dynamic_cast<const envoy::extensions::access_loggers::filters::cel::v3::ExpressionFilter*>(
+          factory_config.get());
+
+  auto parse_status = google::api::expr::parser::Parse(cel_config.expression());
+  if (!parse_status.ok()) {
+    throw EnvoyException("Not able to parse filter expression: " +
+                         parse_status.status().ToString());
+  }
+  return std::make_unique<CELAccessLogExtensionFilter>(getOrCreateBuilder(),
+                                                       parse_status.value().expr());
+}
+
+ProtobufTypes::MessagePtr CELAccessLogExtensionFilterFactory::createEmptyConfigProto() {
+  return std::make_unique<envoy::extensions::access_loggers::filters::cel::v3::ExpressionFilter>();
+}
+
+Extensions::Filters::Common::Expr::Builder&
+CELAccessLogExtensionFilterFactory::getOrCreateBuilder() {
+  if (expr_builder_ == nullptr) {
+    expr_builder_ = Extensions::Filters::Common::Expr::createBuilder(nullptr);
+  }
+  return *expr_builder_;
+}
+
+/**
+ * Static registration for the CELAccessLogExtensionFilter. @see RegisterFactory.
+ */
+REGISTER_FACTORY(CELAccessLogExtensionFilterFactory, Envoy::AccessLog::ExtensionFilterFactory);
+
+} // namespace CEL
+} // namespace Filters
+} // namespace AccessLoggers
+} // namespace Extensions
+} // namespace Envoy
