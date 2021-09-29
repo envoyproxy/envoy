faf39f1e4f23fc4e92d811b24e71fe6ebddc395b
Shikugawa <rei@tetrate.io>
2021-09-29 08:41:33 +0000

    fix
    Signed-off-by: Shikugawa <rei@tetrate.io>

--- test/mocks/ssl/mocks.h
+++ test/mocks/ssl/mocks.h
@@ -167,6 +167,7 @@ public:
   MOCK_METHOD(envoy::extensions::transport_sockets::tls::v3::CertificateValidationContext::
                   TrustChainVerification,
               trustChainVerification, (), (const));
+  MOCK_METHOD(bool, onlyVerifyFinalCertificateCrl, (), (const));
 };
 
 class MockPrivateKeyMethodManager : public PrivateKeyMethodManager {
