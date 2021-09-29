24845d1516a81843e97121b5fa9e1a20e3d94653
Adam Kotwasinski <adam.kotwasinski@gmail.com>
2021-09-29 09:52:51 -0700

    kafka: add header support to mesh-filter (#18248)
    Signed-off-by: Adam Kotwasinski <adam.kotwasinski@gmail.com>

--- contrib/kafka/filters/network/source/mesh/outbound_record.h
+++ contrib/kafka/filters/network/source/mesh/outbound_record.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <utility>
+#include <vector>
 
 #include "absl/strings/string_view.h"
 
@@ -10,6 +12,9 @@ namespace NetworkFilters {
 namespace Kafka {
 namespace Mesh {
 
+// Kafka header.
+using Header = std::pair<absl::string_view, absl::string_view>;
+
 // Binds a single inbound record from Kafka client with its delivery information.
 struct OutboundRecord {
 
@@ -18,15 +23,16 @@ struct OutboundRecord {
   const int32_t partition_;
   const absl::string_view key_;
   const absl::string_view value_;
+  const std::vector<Header> headers_;
 
   // These fields will get updated when delivery to upstream Kafka cluster finishes.
   int16_t error_code_;
   uint32_t saved_offset_;
 
   OutboundRecord(const std::string& topic, const int32_t partition, const absl::string_view key,
-                 const absl::string_view value)
-      : topic_{topic}, partition_{partition}, key_{key}, value_{value}, error_code_{0},
-        saved_offset_{0} {};
+                 const absl::string_view value, const std::vector<Header>& headers)
+      : topic_{topic}, partition_{partition}, key_{key}, value_{value}, headers_{headers},
+        error_code_{0}, saved_offset_{0} {};
 };
 
 } // namespace Mesh
