8411c1c317787b51570576182c2593cbc0fcf2f8
Jose Nino <jnino@lyft.com>
2021-10-09 12:11:24 -0700

    rejigger for coverage
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- source/common/network/dns_impl.cc
+++ source/common/network/dns_impl.cc
@@ -97,6 +97,15 @@ void DnsResolverImpl::initializeChannel(ares_options* options, int optmask) {
 
 void DnsResolverImpl::PendingResolution::onAresGetAddrInfoCallback(int status, int timeouts,
                                                                    ares_addrinfo* addrinfo) {
+  // Purely for checking for invalid state. This will change is Auto and V4Preferred are changed to
+  // have concurrent lookups.
+  if (status != ARES_SUCCESS && !pending_response_.address_list_.empty()) {
+    // The only way the resolver would have addresses here is if it is configured to resolve All
+    // families, and the first resolution was successful. In that case, we might as well return
+    // the addresses that resolved.
+    ASSERT(dns_lookup_family_ == DnsLookupFamily::All && !dual_resolution_);
+  }
+
   // We receive ARES_EDESTRUCTION when destructing with pending queries.
   if (status == ARES_EDESTRUCTION) {
     ASSERT(owned_);
@@ -107,12 +116,7 @@ void DnsResolverImpl::PendingResolution::onAresGetAddrInfoCallback(int status, i
     ENVOY_LOG_EVENT(debug, "cares_dns_resolution_destroyed", "dns resolution for {} destroyed",
                     dns_name_);
 
-    if (!pending_response_.address_list_.empty()) {
-      ASSERT(dns_lookup_family_ == DnsLookupFamily::All && !dual_resolution_);
-      // The only way the resolver would have addresses here is if it is configured to resolve All
-      // families, and the first resolution was successful. In that case, we might as well return
-      // the addresses that resolved as a success.
-    } else {
+    if (pending_response_.address_list_.empty()) {
       pending_response_.status_ = ResolutionStatus::Failure;
     }
     // Nothing can follow a call to finishResolve due to the deletion of this object upon
@@ -174,12 +178,7 @@ void DnsResolverImpl::PendingResolution::onAresGetAddrInfoCallback(int status, i
     ASSERT(addrinfo != nullptr);
     ares_freeaddrinfo(addrinfo);
   } else {
-    if (!pending_response_.address_list_.empty()) {
-      ASSERT(dns_lookup_family_ == DnsLookupFamily::All && !dual_resolution_);
-      // The only way the resolver would have addresses here is if it is configured to resolve All
-      // families, and the first resolution was successful. In that case, we might as well return
-      // the addresses that resolved.
-    } else {
+    if (pending_response_.address_list_.empty()) {
       pending_response_.status_ = ResolutionStatus::Failure;
     }
   }
