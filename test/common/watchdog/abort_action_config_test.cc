e33d27a2b256c72aa1cfa3c8cab1ccdfb43a5175
Mark D. Roth <roth@google.com>
2021-10-12 21:24:19 +0000

    Revert "fix code coverage"
    This reverts commit 3c1eec65bb718e8694cf8f6f08e7b57971422fc5.
    
    Signed-off-by: Mark D. Roth <roth@google.com>

--- test/common/watchdog/abort_action_config_test.cc
+++ test/common/watchdog/abort_action_config_test.cc
@@ -8,19 +8,13 @@
 #include "test/mocks/event/mocks.h"
 #include "test/test_common/utility.h"
 
-#include "absl/strings/str_format.h"
 #include "gtest/gtest.h"
 
 namespace Envoy {
 namespace Watchdog {
 namespace {
 
-class AbortActionFactoryTest : public ::testing::TestWithParam<const char*> {};
-
-INSTANTIATE_TEST_SUITE_P(TypedStruct, AbortActionFactoryTest,
-                         ::testing::Values("xds.type.v3.TypedStruct", "udpa.type.v1.TypedStruct"));
-
-TEST_P(AbortActionFactoryTest, CanCreateAction) {
+TEST(AbortActionFactoryTest, CanCreateAction) {
   auto factory =
       Registry::FactoryRegistry<Server::Configuration::GuardDogActionFactory>::getFactory(
           "envoy.watchdog.abort_action");
@@ -28,13 +22,13 @@ TEST_P(AbortActionFactoryTest, CanCreateAction) {
 
   // Create config and mock context
   envoy::config::bootstrap::v3::Watchdog::WatchdogAction config;
-  TestUtility::loadFromJson(absl::StrFormat(
-                                R"EOF(
+  TestUtility::loadFromJson(
+      R"EOF(
         {
           "config": {
             "name": "envoy.watchdog.abort_action",
             "typed_config": {
-              "@type": "type.googleapis.com/%s",
+              "@type": "type.googleapis.com/xds.type.v3.TypedStruct",
               "type_url": "type.googleapis.com/envoy.watchdog.abort_action.v3.AbortActionConfig",
               "value": {
                 "wait_duration": "2s",
@@ -43,8 +37,7 @@ TEST_P(AbortActionFactoryTest, CanCreateAction) {
           },
         }
       )EOF",
-                                GetParam()),
-                            config);
+      config);
 
   Stats::TestUtil::TestStore stats_;
   Event::MockDispatcher dispatcher;
