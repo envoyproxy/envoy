a8a60f67dbd40473aa1326b70637debfee1958d9
Alan Chiu <achiu@lyft.com>
2021-10-14 12:36:06 -0700

    android: stub out jni logging by default (#1879)
    Description: These logs are quite spammy in practice without any meaningful information. The proposal here is to avoid spamming Android logs and rely on this for unit test debugging or local development debugging.
    Risk Level: low
    Testing: local
    
    Signed-off-by: Alan Chiu <achiu@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/common/jni/ndk_jni_support.cc
+++ mobile/library/common/jni/ndk_jni_support.cc
@@ -5,11 +5,13 @@
 // NOLINT(namespace-envoy)
 
 int jni_log_fmt(const char* tag, const char* fmt, void* value) {
-  return __android_log_print(ANDROID_LOG_VERBOSE, tag, fmt, value);
+  // For debug logging, use __android_log_print(ANDROID_LOG_VERBOSE, tag, fmt, value);
+  return 0;
 }
 
 int jni_log(const char* tag, const char* str) {
-  return __android_log_write(ANDROID_LOG_VERBOSE, tag, str);
+  // For debug logging, use __android_log_write(ANDROID_LOG_VERBOSE, tag, str);
+  return 0;
 }
 
 jint attach_jvm(JavaVM* vm, JNIEnv** p_env, void* thr_args) {
