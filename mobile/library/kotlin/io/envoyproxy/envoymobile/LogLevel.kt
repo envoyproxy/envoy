202aa1587d3353487e4cd1dd02dfc2faddf46abe
Jingwei <jingwei.hao@gmail.com>
2021-10-14 15:42:51 -0700

    Assign an int to each log level (#1885)
    Description: Assign an int to each log level for consumption.
    Risk Level: low
    Testing: ci
    Docs Changes: n/a
    Release Notes: n/a
    [Optional Fixes #Issue] n/a
    [Optional Deprecated:] n/a
    
    Signed-off-by: Jingwei Hao <jingwei.hao@gmail.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/kotlin/io/envoyproxy/envoymobile/LogLevel.kt
+++ mobile/library/kotlin/io/envoyproxy/envoymobile/LogLevel.kt
@@ -3,12 +3,12 @@ package io.envoyproxy.envoymobile
 /**
  * Available logging levels for an Envoy instance. Note some levels may be compiled out.
  */
-enum class LogLevel(internal val level: String) {
-  TRACE("trace"),
-  DEBUG("debug"),
-  INFO("info"),
-  WARN("warn"),
-  ERROR("error"),
-  CRITICAL("critical"),
-  OFF("off");
+enum class LogLevel(internal val level: String, val levelInt: Int) {
+  TRACE("trace", 0),
+  DEBUG("debug", 1),
+  INFO("info", 2),
+  WARN("warn", 3),
+  ERROR("error", 4),
+  CRITICAL("critical", 5),
+  OFF("off", -1);
 }
--- mobile/library/swift/LogLevel.swift
+++ mobile/library/swift/LogLevel.swift
@@ -4,13 +4,13 @@ import Foundation
 /// Note that some levels may be compiled out.
 @objc
 public enum LogLevel: Int {
-  case trace
-  case debug
-  case info
-  case warn
-  case error
-  case critical
-  case off
+  case trace = 0
+  case debug = 1
+  case info = 2
+  case warn = 3
+  case error = 4
+  case critical = 5
+  case off = -1
 
   /// String representation of the log level.
   var stringValue: String {
