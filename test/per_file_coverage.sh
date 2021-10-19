07c3483e53d1c5a62d848932aab0d23ed78088c5
Takeshi Yoneda <takeshi@tetrate.io>
2021-10-20 00:14:41 +0900

    tls: decouple dnsNameMatch from default cert validator. (#18285)
    This PR decouple static DefaultCertValidator::dnsNameMatch method from cert validator and move it to Tls::Utility namespace for clarity since it is also used in QUIC and there's no certificate verification specific logic there.
    
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -72,7 +72,7 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/extensions/tracers/zipkin:96.1"
 "source/extensions/transport_sockets:95.3"
 "source/extensions/transport_sockets/tls:94.5"
-"source/extensions/transport_sockets/tls/cert_validator:96.0"
+"source/extensions/transport_sockets/tls/cert_validator:95.8"
 "source/extensions/transport_sockets/tls/ocsp:96.5"
 "source/extensions/transport_sockets/tls/private_key:77.8"
 "source/extensions/wasm_runtime/wamr:0.0" # Not enabled in coverage build
