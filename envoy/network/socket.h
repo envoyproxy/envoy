a0624a30b08a532dff6564b79972debc41a28e72
Mike Schore <mike.schore@gmail.com>
2021-10-26 20:42:38 +0800

    socket option: add support for directly creating ipv4/ipv6 pairs
    Signed-off-by: Mike Schore <mike.schore@gmail.com>

--- envoy/network/socket.h
+++ envoy/network/socket.h
@@ -299,8 +299,16 @@ public:
     virtual absl::optional<Details>
     getOptionDetails(const Socket& socket,
                      envoy::config::core::v3::SocketOption::SocketState state) const PURE;
+
+    /**
+     * Implementations should typically return true. Unsupported or placeholder implementations
+     * may indicate such by returning false.
+     * @return Whether this is an actual socket option.
+     */
+    virtual bool isSupported() const PURE;
   };
 
+  using OptionConstPtr = std::unique_ptr<const Option>;
   using OptionConstSharedPtr = std::shared_ptr<const Option>;
   using Options = std::vector<OptionConstSharedPtr>;
   using OptionsSharedPtr = std::shared_ptr<Options>;
