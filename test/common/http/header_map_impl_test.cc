bba4473d86574fc8ac8e40198e33c7d2d08a161f
gauravb <gauravb@uber.com>
2021-10-29 00:42:44 +0000

    adding more tests
    Signed-off-by: gauravb <gauravb@uber.com>

--- test/common/http/header_map_impl_test.cc
+++ test/common/http/header_map_impl_test.cc
@@ -130,36 +130,43 @@ TEST(HeaderStringTest, All) {
     EXPECT_EQ(data_with_leading_lws, string.getStringView());
   }
 
-  // Inline trim removes leading and trailing whitespace.
+    // Inline trim, only whitespace
   {
     const std::string only_lws = " \t\f\v";
-    HeaderString string1;
-    string1.append(only_lws.data(), only_lws.size());
-    EXPECT_EQ(only_lws, string1.getStringView());
-    string1.trim();
-    EXPECT_NE(only_lws, string1.getStringView());
-    EXPECT_TRUE(string1.getStringView().empty());
+    HeaderString string;
+    string.append(only_lws.data(), only_lws.size());
+    EXPECT_EQ(only_lws, string.getStringView());
+    string.trim();
+    EXPECT_NE(only_lws, string.getStringView());
+    EXPECT_TRUE(string.getStringView().empty());
+  }
 
+  // Inline trim, no leading whitespace
+  {
     const std::string data_without_leading_and_trailing_lws = "data";
     const std::string data_with_only_trailing_lws =
         data_without_leading_and_trailing_lws + " \t\f\v";
-    HeaderString string2;
-    string2.append(data_with_only_trailing_lws.data(),
-                   data_with_only_trailing_lws.size());
-    EXPECT_EQ(data_with_only_trailing_lws, string2.getStringView());
-    string2.trim();
-    EXPECT_NE(data_with_only_trailing_lws, string2.getStringView());
-    EXPECT_EQ(data_without_leading_and_trailing_lws, string2.getStringView());
+    HeaderString string;
+    string.append(data_with_only_trailing_lws.data(),
+                  data_with_only_trailing_lws.size());
+    EXPECT_EQ(data_with_only_trailing_lws, string.getStringView());
+    string.trim();
+    EXPECT_NE(data_with_only_trailing_lws, string.getStringView());
+    EXPECT_EQ(data_without_leading_and_trailing_lws, string.getStringView());
+  }
 
+  // Inline trim, both leading and trailing whitespace
+  {
+    const std::string data_without_leading_and_trailing_lws = "data";
     const std::string data_with_leading_and_trailing_lws =
         " \t\f\v " + data_without_leading_and_trailing_lws + " \t\f\v";
-    HeaderString string3;
-    string3.append(data_with_leading_and_trailing_lws.data(),
-                   data_with_leading_and_trailing_lws.size());
-    EXPECT_EQ(data_with_leading_and_trailing_lws, string3.getStringView());
-    string3.trim();
-    EXPECT_NE(data_with_leading_and_trailing_lws, string3.getStringView());
-    EXPECT_EQ(data_without_leading_and_trailing_lws, string3.getStringView());
+    HeaderString string;
+    string.append(data_with_leading_and_trailing_lws.data(),
+                  data_with_leading_and_trailing_lws.size());
+    EXPECT_EQ(data_with_leading_and_trailing_lws, string.getStringView());
+    string.trim();
+    EXPECT_NE(data_with_leading_and_trailing_lws, string.getStringView());
+    EXPECT_EQ(data_without_leading_and_trailing_lws, string.getStringView());
   }
 
   // Static clear() does nothing.
