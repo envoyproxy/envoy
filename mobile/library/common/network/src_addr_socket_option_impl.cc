0a282eed23a192f59fd7b2471fe9cee15d040056
Mike Schore <mike.schore@gmail.com>
2021-11-02 10:40:44 -0700

    network: make SrcAddrSocketOptionImpl safely handle null addresses (#1905)
    Description: Adds defensive handling since returned addresses may be null (especially after filtering: #1901).
    Risk Level: Low
    Testing: Local & On Device
    
    Signed-off-by: Mike Schore <mike.schore@gmail.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/common/network/src_addr_socket_option_impl.cc
+++ mobile/library/common/network/src_addr_socket_option_impl.cc
@@ -10,13 +10,12 @@ namespace Network {
 SrcAddrSocketOptionImpl::SrcAddrSocketOptionImpl(
     Network::Address::InstanceConstSharedPtr source_address)
     : source_address_(std::move(source_address)) {
-  ASSERT(source_address_->type() == Network::Address::Type::Ip);
+  ASSERT(!source_address_ || source_address_->type() == Network::Address::Type::Ip);
 }
 
 bool SrcAddrSocketOptionImpl::setOption(
     Network::Socket& socket, envoy::config::core::v3::SocketOption::SocketState state) const {
-
-  if (state == envoy::config::core::v3::SocketOption::STATE_PREBIND) {
+  if (source_address_ && state == envoy::config::core::v3::SocketOption::STATE_PREBIND) {
     socket.connectionInfoProvider().setLocalAddress(source_address_);
   }
 
@@ -39,11 +38,11 @@ void SrcAddrSocketOptionImpl::hashKey(std::vector<uint8_t>& key) const {
   // the same.
   if (source_address_->ip()->version() == Network::Address::IpVersion::v4) {
     // note raw_address is already in network order
-    uint32_t raw_address = source_address_->ip()->ipv4()->address();
+    uint32_t raw_address = source_address_ ? source_address_->ip()->ipv4()->address() : 0;
     addressIntoVector(key, raw_address);
   } else if (source_address_->ip()->version() == Network::Address::IpVersion::v6) {
     // note raw_address is already in network order
-    absl::uint128 raw_address = source_address_->ip()->ipv6()->address();
+    absl::uint128 raw_address = source_address_ ? source_address_->ip()->ipv6()->address() : 0;
     addressIntoVector(key, raw_address);
   }
 }
