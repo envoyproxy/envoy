a78bdc0d5902e91cad63a199a76e5c6302f2b7db
Jingwei <jingwei.hao@gmail.com>
2021-11-04 12:05:50 -0700

    make functions as c functions (#1921)
    Description: declare functions as c functions (as opposed to c++ functions)
    Risk Level: low
    Testing: ci
    Docs Changes: n/a
    Release Notes: n/a
    
    Signed-off-by: Jingwei Hao <jingwei.hao@gmail.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/common/jni/jni_support.h
+++ mobile/library/common/jni/jni_support.h
@@ -2,8 +2,8 @@
 
 // NOLINT(namespace-envoy)
 
-int jni_log_fmt(const char* tag, const char* fmt, void* value);
+extern "C" int jni_log_fmt(const char* tag, const char* fmt, void* value);
 
-int jni_log(const char* tag, const char* str);
+extern "C" int jni_log(const char* tag, const char* str);
 
-jint attach_jvm(JavaVM* vm, JNIEnv** p_env, void* thr_args);
+extern "C" jint attach_jvm(JavaVM* vm, JNIEnv** p_env, void* thr_args);
