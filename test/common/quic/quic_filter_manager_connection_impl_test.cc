60919fc7500e3a2f9913d038bab2bdac25b0c643
Ryan Hamilton <rch@google.com>
2021-11-15 19:24:45 +0000

    format
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- test/common/quic/quic_filter_manager_connection_impl_test.cc
+++ test/common/quic/quic_filter_manager_connection_impl_test.cc
@@ -1,24 +1,21 @@
 #include "source/common/quic/quic_filter_manager_connection_impl.h"
 
-#include "quiche/quic/core/quic_connection.h"
-
 #include "test/mocks/event/mocks.h"
 #include "test/mocks/network/mocks.h"
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-
+#include "quiche/quic/core/quic_connection.h"
 
 namespace Envoy {
 namespace Quic {
 
 class TestQuicFilterManagerConnectionImpl : public QuicFilterManagerConnectionImpl {
 public:
-  TestQuicFilterManagerConnectionImpl(
-      QuicNetworkConnection& connection, const quic::QuicConnectionId& connection_id,
-      Event::Dispatcher& dispatcher, uint32_t send_buffer_limit)
+  TestQuicFilterManagerConnectionImpl(QuicNetworkConnection& connection,
+                                      const quic::QuicConnectionId& connection_id,
+                                      Event::Dispatcher& dispatcher, uint32_t send_buffer_limit)
       : QuicFilterManagerConnectionImpl(connection, connection_id, dispatcher, send_buffer_limit) {}
-
 
   void dumpState(std::ostream& /*os*/, int /*indent_level = 0*/) const override {}
   absl::string_view requestedServerName() const override { return {}; }
@@ -29,7 +26,7 @@ public:
   void closeNow() {
     const quic::QuicConnectionCloseFrame frame;
     quic::ConnectionCloseSource source = quic::ConnectionCloseSource::FROM_SELF;
-    const quic::ParsedQuicVersion version =  quic::AllSupportedVersions().front();
+    const quic::ParsedQuicVersion version = quic::AllSupportedVersions().front();
     onConnectionCloseEvent(frame, source, version);
   }
 };
@@ -42,11 +39,12 @@ TEST(QuicFilterManagerConnectionImplTest, ConnectionInfoProviderSharedPtr) {
   Event::MockDispatcher dispatcher;
   uint32_t send_buffer_limit = 0;
 
-  TestQuicFilterManagerConnectionImpl impl_(connection, connection_id, dispatcher, send_buffer_limit);
+  TestQuicFilterManagerConnectionImpl impl_(connection, connection_id, dispatcher,
+                                            send_buffer_limit);
   EXPECT_TRUE(impl_.connectionInfoProviderSharedPtr() != nullptr);
   impl_.closeNow();
   EXPECT_TRUE(impl_.connectionInfoProviderSharedPtr() == nullptr);
 }
 
-}  // namespace Quic
-}  // namespace Envoy
+} // namespace Quic
+} // namespace Envoy
