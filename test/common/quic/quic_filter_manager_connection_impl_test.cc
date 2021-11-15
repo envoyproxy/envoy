74d4a6d936ba7412b426ace544a18f4655fa4870
Ryan Hamilton <rch@google.com>
2021-11-15 21:48:01 +0000

    Fix merge issues
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- test/common/quic/quic_filter_manager_connection_impl_test.cc
+++ test/common/quic/quic_filter_manager_connection_impl_test.cc
@@ -3,9 +3,11 @@
 #include "test/mocks/event/mocks.h"
 #include "test/mocks/network/mocks.h"
 
+#include "quiche/quic/core/quic_connection.h"
+#include "quiche/quic/test_tools/quic_test_utils.h"
+
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "quiche/quic/core/quic_connection.h"
 
 namespace Envoy {
 namespace Quic {
@@ -14,8 +16,9 @@ class TestQuicFilterManagerConnectionImpl : public QuicFilterManagerConnectionIm
 public:
   TestQuicFilterManagerConnectionImpl(QuicNetworkConnection& connection,
                                       const quic::QuicConnectionId& connection_id,
-                                      Event::Dispatcher& dispatcher, uint32_t send_buffer_limit)
-      : QuicFilterManagerConnectionImpl(connection, connection_id, dispatcher, send_buffer_limit) {}
+                                      Event::Dispatcher& dispatcher, uint32_t send_buffer_limit,
+                                      std::shared_ptr<QuicSslConnectionInfo>&& ssl_info)
+      : QuicFilterManagerConnectionImpl(connection, connection_id, dispatcher, send_buffer_limit, std::move(ssl_info)) {}
 
   void dumpState(std::ostream& /*os*/, int /*indent_level = 0*/) const override {}
   absl::string_view requestedServerName() const override { return {}; }
@@ -38,9 +41,13 @@ TEST(QuicFilterManagerConnectionImplTest, ConnectionInfoProviderSharedPtr) {
   const quic::QuicConnectionId connection_id;
   Event::MockDispatcher dispatcher;
   uint32_t send_buffer_limit = 0;
+  quic::test::MockQuicConnectionHelper helper;
+  quic::test::MockAlarmFactory alarm_factory;
+  quic::test::MockQuicSession quic_session(new quic::test::MockQuicConnection(&helper, &alarm_factory, quic::Perspective::IS_SERVER));
+  auto ssl_info = std::make_shared<QuicSslConnectionInfo>(quic_session);
 
   TestQuicFilterManagerConnectionImpl impl_(connection, connection_id, dispatcher,
-                                            send_buffer_limit);
+                                            send_buffer_limit, std::move(ssl_info));
   EXPECT_TRUE(impl_.connectionInfoProviderSharedPtr() != nullptr);
   impl_.closeNow();
   EXPECT_TRUE(impl_.connectionInfoProviderSharedPtr() == nullptr);
