18b19f0a3fe9c9d20ac0ad9aadb3ea1838f36b91
Rohit Agrawal <rohit.agrawal@databricks.com>
2021-11-17 17:47:35 -0500

    Remove Unwanted Fields
    Signed-off-by: Rohit Agrawal <rohit.agrawal@databricks.com>

--- test/common/stream_info/test_util.h
+++ test/common/stream_info/test_util.h
@@ -34,61 +34,7 @@ public:
     return *downstream_connection_info_provider_;
   }
 
-  void setUpstreamSslConnection(const Ssl::ConnectionInfoConstSharedPtr& connection_info) override {
-    upstream_connection_info_ = connection_info;
-  }
-
-  Ssl::ConnectionInfoConstSharedPtr upstreamSslConnection() const override {
-    return upstream_connection_info_;
-  }
-  void setRouteName(absl::string_view route_name) override {
-    route_name_ = std::string(route_name);
-  }
-  const std::string& getRouteName() const override { return route_name_; }
-
-  void setVirtualClusterName(const std::string& virtual_cluster_name) override {
-    virtual_cluster_name_ = virtual_cluster_name;
-  }
   const std::string& getVirtualClusterName() const override { return virtual_cluster_name_; }
-
-  Router::RouteConstSharedPtr route() const override { return route_; }
-
-  absl::optional<std::chrono::nanoseconds>
-  duration(const absl::optional<MonotonicTime>& time) const {
-    if (!time) {
-      return {};
-    }
-
-    return std::chrono::duration_cast<std::chrono::nanoseconds>(time.value() -
-                                                                start_time_monotonic_);
-  }
-
-  absl::optional<std::chrono::nanoseconds> lastDownstreamRxByteReceived() const override {
-    return duration(last_rx_byte_received_);
-  }
-
-  absl::optional<std::chrono::nanoseconds> firstUpstreamTxByteSent() const override {
-    return duration(upstream_timing_.first_upstream_tx_byte_sent_);
-  }
-
-  absl::optional<std::chrono::nanoseconds> lastUpstreamTxByteSent() const override {
-    return duration(upstream_timing_.last_upstream_tx_byte_sent_);
-  }
-  absl::optional<std::chrono::nanoseconds> firstUpstreamRxByteReceived() const override {
-    return duration(upstream_timing_.first_upstream_rx_byte_received_);
-  }
-
-  absl::optional<std::chrono::nanoseconds> lastUpstreamRxByteReceived() const override {
-    return duration(upstream_timing_.last_upstream_rx_byte_received_);
-  }
-
-  absl::optional<std::chrono::nanoseconds> firstDownstreamTxByteSent() const override {
-    return duration(downstream_timing_.firstDownstreamTxByteSent());
-  }
-
-  absl::optional<std::chrono::nanoseconds> lastDownstreamTxByteSent() const override {
-    return duration(downstream_timing_.lastDownstreamTxByteSent());
-  }
 
   void onRequestComplete() override { end_time_ = timeSystem().monotonicTime(); }
 
@@ -110,16 +56,7 @@ public:
   SystemTime start_time_;
   MonotonicTime start_time_monotonic_;
   absl::optional<MonotonicTime> end_time_;
-  absl::optional<Http::Protocol> protocol_{Http::Protocol::Http11};
-  absl::optional<uint32_t> response_code_;
-  absl::optional<std::string> response_code_details_;
-  absl::optional<std::string> connection_termination_details_;
-  uint64_t response_flags_{};
-  Upstream::HostDescriptionConstSharedPtr upstream_host_{};
-  bool health_check_request_{};
-  std::string route_name_;
   std::string virtual_cluster_name_;
-  Network::Address::InstanceConstSharedPtr upstream_local_address_;
   Network::ConnectionInfoSetterSharedPtr downstream_connection_info_provider_{
       std::make_shared<Network::ConnectionInfoSetterImpl>(nullptr, nullptr)};
   Envoy::Event::SimulatedTimeSystem test_time_;
