8598c3f3b3ed9ff0090dbcb1c83464551300bd18
Yuchen Dai <silentdai@gmail.com>
2021-11-17 09:03:33 -0800

    listener: create internal listener (#18104)
    This PR allows creating server connection but the abilitity to connect to such a listener is located in #18105.
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- source/server/listener_impl.h
+++ source/server/listener_impl.h
@@ -326,6 +326,10 @@ public:
     return udp_listener_config_ != nullptr ? *udp_listener_config_
                                            : Network::UdpListenerConfigOptRef();
   }
+  Network::InternalListenerConfigOptRef internalListenerConfig() override {
+    return internal_listener_config_ != nullptr ? *internal_listener_config_
+                                                : Network::InternalListenerConfigOptRef();
+  }
   Network::ConnectionBalancer& connectionBalancer() override { return *connection_balancer_; }
   ResourceLimit& openConnections() override { return *open_connections_; }
   const std::vector<AccessLog::InstanceSharedPtr>& accessLogs() const override {
@@ -372,6 +376,13 @@ private:
     Network::UdpListenerWorkerRouterPtr listener_worker_router_;
   };
 
+  struct InternalListenerConfigImpl : public Network::InternalListenerConfig {
+    InternalListenerConfigImpl(
+        const envoy::config::listener::v3::Listener_InternalListenerConfig config)
+        : config_(config) {}
+    const envoy::config::listener::v3::Listener_InternalListenerConfig config_;
+  };
+
   /**
    * Create a new listener from an existing listener and the new config message if the in place
    * filter chain update is decided. Should be called only by newListenerWithFilterChain().
@@ -381,6 +392,7 @@ private:
                const std::string& name, bool added_via_api, bool workers_started, uint64_t hash);
   // Helpers for constructor.
   void buildAccessLog();
+  void buildInternalListener();
   void validateConfig(Network::Socket::Type socket_type);
   void buildUdpListenerFactory(Network::Socket::Type socket_type, uint32_t concurrency);
   void buildListenSocketOptions(Network::Socket::Type socket_type);
@@ -428,6 +440,7 @@ private:
   const std::chrono::milliseconds listener_filters_timeout_;
   const bool continue_on_listener_filters_timeout_;
   std::shared_ptr<UdpListenerConfigImpl> udp_listener_config_;
+  std::unique_ptr<Network::InternalListenerConfig> internal_listener_config_;
   Network::ConnectionBalancerSharedPtr connection_balancer_;
   std::shared_ptr<PerListenerFactoryContextImpl> listener_factory_context_;
   FilterChainManagerImpl filter_chain_manager_;
