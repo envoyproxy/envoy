a42efa6dc5c6be836ad9705af785994457df5835
Yuchen Dai <silentdai@gmail.com>
2021-11-30 00:38:14 +0000

    renaming
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- envoy/network/client_connection_factory.h
+++ envoy/network/client_connection_factory.h
@@ -12,7 +12,7 @@ namespace Network {
 class ClientConnectionFactory : public Config::UntypedFactory {
 public:
   ClientConnectionFactory() = default;
-  virtual ~ClientConnectionFactory() = default;
+  virtual ~ClientConnectionFactory() override = default;
 
   // Config::UntypedFactory
   std::string category() const override { return "network.connection"; }
