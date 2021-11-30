22dd1c62e4aa3488b7142d41c3a77f018f39d7ee
zhaonan <zhaonan06@corp.netease.com>
2021-11-30 01:29:28 +0000

    fix format
    Signed-off-by: zhaonan <zhaonan06@corp.netease.com>

--- source/extensions/filters/network/dubbo_proxy/router/router_impl.h
+++ source/extensions/filters/network/dubbo_proxy/router/router_impl.h
@@ -7,9 +7,9 @@
 #include "envoy/tcp/conn_pool.h"
 
 #include "source/common/common/logger.h"
-#include "source/common/upstream/load_balancer_impl.h"
 #include "source/common/config/well_known_names.h"
 #include "source/common/router/metadatamatchcriteria_impl.h"
+#include "source/common/upstream/load_balancer_impl.h"
 #include "source/extensions/filters/network/dubbo_proxy/filters/filter.h"
 #include "source/extensions/filters/network/dubbo_proxy/router/router.h"
 
