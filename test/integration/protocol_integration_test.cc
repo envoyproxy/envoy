6e1ed4b81f319509c608db970267dbdc7df48c27
Dan Zhang <danzh@google.com>
2021-12-02 22:32:47 -0500

    socket fail test
    Signed-off-by: Dan Zhang <danzh@google.com>

--- test/integration/protocol_integration_test.cc
+++ test/integration/protocol_integration_test.cc
@@ -3557,7 +3557,7 @@ TEST_P(DownstreamProtocolIntegrationTest, HandleSocketFail) {
   socket_swap.write_matcher_->setSourcePort(lookupPort("http"));
   socket_swap.write_matcher_->setWriteOverride(ebadf);
   // TODO(danzh) set to true.
-  upstream_request_->encodeHeaders(Http::TestResponseHeaderMapImpl{{":status", "200"}}, false);
+  upstream_request_->encodeHeaders(Http::TestResponseHeaderMapImpl{{":status", "200"}}, true);
 
   if (downstreamProtocol() == Http::CodecType::HTTP3) {
     // For HTTP/3 since the packets are black holed, there is no client side
