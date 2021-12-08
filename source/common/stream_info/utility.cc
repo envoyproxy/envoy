a4b2862622c79b49b5370f55987b5d169badb4fd
James Buckland <jbuckland@google.com>
2021-12-08 13:04:13 -0500

    Add utility/stream_info proxy-status string handling utilities.
    Signed-off-by: James Buckland <jbuckland@google.com>

--- source/common/stream_info/utility.cc
+++ source/common/stream_info/utility.cc
@@ -2,6 +2,12 @@
 
 #include <string>
 
+#include "envoy/extensions/filters/network/http_connection_manager/v3/http_connection_manager.pb.h"
+
+#include "source/common/http/default_server_string.h"
+
+#include "absl/strings/str_format.h"
+
 namespace Envoy {
 namespace StreamInfo {
 
@@ -132,6 +138,240 @@ Utility::formatDownstreamAddressJustPort(const Network::Address::Instance& addre
     port = std::to_string(address.ip()->port());
   }
   return port;
+}
+
+const absl::optional<uint32_t>
+ProxyStatusUtils::recommendedHttpStatusCode(const ProxyStatusError proxy_status) {
+  // This switch statement was derived from the mapping from proxy error type to
+  // recommended HTTP status code in
+  // https://datatracker.ietf.org/doc/html/draft-ietf-httpbis-proxy-status-05#section-2.3 and below.
+  //
+  // TODO(ambuc): Replace this with the non-draft URL when finalized.
+  switch (proxy_status) {
+  case ProxyStatusError::DnsTimeout:
+    return 504;
+  case ProxyStatusError::DnsError:
+    return 502;
+  case ProxyStatusError::DestinationNotFound:
+    return 500;
+  case ProxyStatusError::DestinationUnavailable:
+    return 503;
+  case ProxyStatusError::DestinationIpProhibited:
+    return 502;
+  case ProxyStatusError::DestinationIpUnroutable:
+    return 502;
+  case ProxyStatusError::ConnectionRefused:
+    return 502;
+  case ProxyStatusError::ConnectionTerminated:
+    return 502;
+  case ProxyStatusError::ConnectionTimeout:
+    return 504;
+  case ProxyStatusError::ConnectionReadTimeout:
+    return 504;
+  case ProxyStatusError::ConnectionWriteTimeout:
+    return 504;
+  case ProxyStatusError::ConnectionLimitReached:
+    return 503;
+  case ProxyStatusError::TlsProtocolError:
+    return 502;
+  case ProxyStatusError::TlsCertificateError:
+    return 502;
+  case ProxyStatusError::TlsAlertReceived:
+    return 502;
+  case ProxyStatusError::HttpRequestDenied:
+    return 403;
+  case ProxyStatusError::HttpResponseIncomplete:
+    return 502;
+  case ProxyStatusError::HttpResponseHeaderSectionSize:
+    return 502;
+  case ProxyStatusError::HttpResponseHeaderSize:
+    return 502;
+  case ProxyStatusError::HttpResponseBodySize:
+    return 502;
+  case ProxyStatusError::HttpResponseTrailerSectionSize:
+    return 502;
+  case ProxyStatusError::HttpResponseTrailerSize:
+    return 502;
+  case ProxyStatusError::HttpResponseTransferCoding:
+    return 502;
+  case ProxyStatusError::HttpResponseContentCoding:
+    return 502;
+  case ProxyStatusError::HttpResponseTimeout:
+    return 504;
+  case ProxyStatusError::HttpUpgradeFailed:
+    return 502;
+  case ProxyStatusError::HttpProtocolError:
+    return 502;
+  case ProxyStatusError::ProxyInternalError:
+    return 500;
+  case ProxyStatusError::ProxyConfigurationError:
+    return 500;
+  case ProxyStatusError::ProxyLoopDetected:
+    return 502;
+  case ProxyStatusError::ProxyInternalResponse:
+  case ProxyStatusError::HttpRequestError:
+  default:
+    return absl::nullopt;
+  }
+}
+
+const std::string
+ProxyStatusUtils::toString(const StreamInfo& stream_info, const ProxyStatusError error,
+                           absl::string_view node_id,
+                           const envoy::extensions::filters::network::http_connection_manager::v3::
+                               HttpConnectionManager::ProxyStatusConfig& proxy_status_config) {
+  std::vector<std::string> retval = {};
+
+  switch (proxy_status_config.proxy_name()) {
+  case envoy::extensions::filters::network::http_connection_manager::v3::HttpConnectionManager::
+      ProxyStatusConfig::NODE_ID: {
+    retval.push_back(std::string(node_id));
+    break;
+  }
+  case envoy::extensions::filters::network::http_connection_manager::v3::HttpConnectionManager::
+      ProxyStatusConfig::ENVOY_LITERAL:
+  default: {
+    retval.push_back(Envoy::Http::DefaultServerString::get());
+    break;
+  }
+  }
+
+  retval.push_back(absl::StrFormat("error=%s", proxyStatusErrorToString(error)));
+
+  if (!proxy_status_config.remove_details() && stream_info.responseCodeDetails().has_value()) {
+    std::vector<std::string> details = {};
+    details.push_back(stream_info.responseCodeDetails().value());
+    if (!proxy_status_config.remove_connection_termination_details() &&
+        stream_info.connectionTerminationDetails().has_value()) {
+      details.push_back(stream_info.connectionTerminationDetails().value());
+    }
+    if (!proxy_status_config.remove_response_flags() && stream_info.hasAnyResponseFlag()) {
+      details.push_back(ResponseFlagUtils::toShortString(stream_info));
+    }
+    retval.push_back(absl::StrFormat("details='%s'", absl::StrJoin(details, "; ")));
+  }
+
+  return absl::StrJoin(retval, "; ");
+}
+
+const absl::string_view
+ProxyStatusUtils::proxyStatusErrorToString(const ProxyStatusError proxy_status) {
+  switch (proxy_status) {
+  case ProxyStatusError::DnsTimeout:
+    return DNS_TIMEOUT;
+  case ProxyStatusError::DnsError:
+    return DNS_ERROR;
+  case ProxyStatusError::DestinationNotFound:
+    return DESTINATION_NOT_FOUND;
+  case ProxyStatusError::DestinationUnavailable:
+    return DESTINATION_UNAVAILABLE;
+  case ProxyStatusError::DestinationIpProhibited:
+    return DESTINATION_IP_PROHIBITED;
+  case ProxyStatusError::DestinationIpUnroutable:
+    return DESTINATION_IP_UNROUTABLE;
+  case ProxyStatusError::ConnectionRefused:
+    return CONNECTION_REFUSED;
+  case ProxyStatusError::ConnectionTerminated:
+    return CONNECTION_TERMINATED;
+  case ProxyStatusError::ConnectionTimeout:
+    return CONNECTION_TIMEOUT;
+  case ProxyStatusError::ConnectionReadTimeout:
+    return CONNECTION_READ_TIMEOUT;
+  case ProxyStatusError::ConnectionWriteTimeout:
+    return CONNECTION_WRITE_TIMEOUT;
+  case ProxyStatusError::ConnectionLimitReached:
+    return CONNECTION_LIMIT_REACHED;
+  case ProxyStatusError::TlsProtocolError:
+    return TLS_PROTOCOL_ERORR;
+  case ProxyStatusError::TlsCertificateError:
+    return TLS_CERTIFICATE_ERORR;
+  case ProxyStatusError::TlsAlertReceived:
+    return TLS_ALERT_RECEIVED;
+  case ProxyStatusError::HttpRequestError:
+    return HTTP_REQUEST_ERROR;
+  case ProxyStatusError::HttpRequestDenied:
+    return HTTP_REQUEST_DENIED;
+  case ProxyStatusError::HttpResponseIncomplete:
+    return HTTP_RESPONSE_INCOMPLETE;
+  case ProxyStatusError::HttpResponseHeaderSectionSize:
+    return HTTP_RESPONSE_HEADER_SECTION_SIZE;
+  case ProxyStatusError::HttpResponseHeaderSize:
+    return HTTP_RESPONSE_HEADER_SIZE;
+  case ProxyStatusError::HttpResponseBodySize:
+    return HTTP_RESPONSE_BODY_SIZE;
+  case ProxyStatusError::HttpResponseTrailerSectionSize:
+    return HTTP_RESPONSE_TRAILER_SECTION_SIZE;
+  case ProxyStatusError::HttpResponseTrailerSize:
+    return HTTP_RESPONSE_TRAILER_SIZE;
+  case ProxyStatusError::HttpResponseTransferCoding:
+    return HTTP_RESPONSE_TRANSFER_CODING;
+  case ProxyStatusError::HttpResponseContentCoding:
+    return HTTP_RESPONSE_CONTENT_CODING;
+  case ProxyStatusError::HttpResponseTimeout:
+    return HTTP_RESPONSE_TIMEOUT;
+  case ProxyStatusError::HttpUpgradeFailed:
+    return HTTP_UPGRADE_FAILED;
+  case ProxyStatusError::HttpProtocolError:
+    return HTTP_PROTOCOL_ERROR;
+  case ProxyStatusError::ProxyInternalResponse:
+    return PROXY_INTERNAL_RESPONSE;
+  case ProxyStatusError::ProxyInternalError:
+    return PROXY_INTERNAL_ERROR;
+  case ProxyStatusError::ProxyConfigurationError:
+    return PROXY_CONFIGURATION_ERROR;
+  case ProxyStatusError::ProxyLoopDetected:
+    return PROXY_LOOP_DETECTED;
+  }
+  return "-";
+}
+
+const absl::optional<ProxyStatusError>
+ProxyStatusUtils::fromStreamInfo(const StreamInfo& stream_info) {
+  // NB: This mapping from Envoy-specific ResponseFlag enum to Proxy-Status
+  // error enum is lossy, since ResponseFlag is really a bitset of many
+  // ResponseFlag enums. Here, we search the list of all known ResponseFlag values in
+  // enum order, returning the first matching ProxyStatusError.
+  if (stream_info.hasResponseFlag(ResponseFlag::FailedLocalHealthCheck)) {
+    return ProxyStatusError::DestinationUnavailable;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::NoHealthyUpstream)) {
+    return ProxyStatusError::DestinationUnavailable;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::UpstreamRequestTimeout)) {
+    return ProxyStatusError::ConnectionTimeout;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::LocalReset)) {
+    return ProxyStatusError::ConnectionTimeout;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::UpstreamRemoteReset)) {
+    return ProxyStatusError::ConnectionTerminated;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::UpstreamConnectionFailure)) {
+    return ProxyStatusError::ConnectionRefused;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::UpstreamConnectionTermination)) {
+    return ProxyStatusError::ConnectionTerminated;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::UpstreamOverflow)) {
+    return ProxyStatusError::ConnectionLimitReached;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::NoRouteFound)) {
+    return ProxyStatusError::DestinationNotFound;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::RateLimited)) {
+    return ProxyStatusError::ConnectionLimitReached;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::RateLimitServiceError)) {
+    return ProxyStatusError::ConnectionLimitReached;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::UpstreamRetryLimitExceeded)) {
+    return ProxyStatusError::DestinationUnavailable;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::StreamIdleTimeout)) {
+    return ProxyStatusError::HttpResponseTimeout;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::InvalidEnvoyRequestHeaders)) {
+    return ProxyStatusError::HttpRequestError;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::DownstreamProtocolError)) {
+    return ProxyStatusError::HttpRequestError;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::UpstreamMaxStreamDurationReached)) {
+    return ProxyStatusError::HttpResponseTimeout;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::NoFilterConfigFound)) {
+    return ProxyStatusError::ProxyConfigurationError;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::UpstreamProtocolError)) {
+    return ProxyStatusError::HttpProtocolError;
+  } else if (stream_info.hasResponseFlag(ResponseFlag::NoClusterFound)) {
+    return ProxyStatusError::DestinationUnavailable;
+  } else {
+    return absl::nullopt;
+  }
 }
 
 } // namespace StreamInfo
--- test/common/stream_info/utility_test.cc
+++ test/common/stream_info/utility_test.cc
@@ -1,3 +1,5 @@
+#include "envoy/extensions/filters/network/http_connection_manager/v3/http_connection_manager.pb.h"
+
 #include "source/common/network/address_impl.h"
 #include "source/common/stream_info/utility.h"
 
@@ -7,12 +9,17 @@
 #include "gtest/gtest.h"
 
 using testing::_;
+using testing::AllOf;
+using testing::HasSubstr;
 using testing::NiceMock;
+using testing::Not;
 using testing::Return;
 
 namespace Envoy {
 namespace StreamInfo {
 namespace {
+
+using envoy::extensions::filters::network::http_connection_manager::v3::HttpConnectionManager;
 
 TEST(ResponseFlagUtilsTest, toShortStringConversion) {
   for (const auto& [flag_string, flag_enum] : ResponseFlagUtils::ALL_RESPONSE_STRING_FLAGS) {
@@ -55,6 +62,121 @@ TEST(UtilityTest, formatDownstreamAddressNoPort) {
             Utility::formatDownstreamAddressNoPort(Network::Address::Ipv4Instance("1.2.3.4")));
   EXPECT_EQ("/hello",
             Utility::formatDownstreamAddressNoPort(Network::Address::PipeInstance("/hello")));
+}
+
+class ProxyStatusTest : public ::testing::Test {
+protected:
+  void SetUp() override {
+    proxy_status_config_.set_remove_details(false);
+    proxy_status_config_.set_proxy_name(HttpConnectionManager::ProxyStatusConfig::ENVOY_LITERAL);
+
+    ON_CALL(stream_info_, hasAnyResponseFlag()).WillByDefault(Return(true));
+    ON_CALL(stream_info_, hasResponseFlag(ResponseFlag::DelayInjected)).WillByDefault(Return(true));
+  }
+
+  HttpConnectionManager::ProxyStatusConfig proxy_status_config_;
+  NiceMock<MockStreamInfo> stream_info_;
+};
+
+TEST_F(ProxyStatusTest, ToStringAbsentDetails) {
+  proxy_status_config_.set_remove_details(false);
+  proxy_status_config_.set_remove_connection_termination_details(false);
+  proxy_status_config_.set_remove_response_flags(false);
+  stream_info_.response_code_details_ = absl::nullopt;
+  stream_info_.connection_termination_details_ = absl::nullopt;
+  EXPECT_THAT(ProxyStatusUtils::toString(stream_info_, ProxyStatusError::ProxyConfigurationError,
+                                         /*node_id=*/"UNUSED", proxy_status_config_),
+              Not(HasSubstr("details=")));
+}
+
+TEST_F(ProxyStatusTest, ToStringNoDetails) {
+  proxy_status_config_.set_remove_details(true);
+  proxy_status_config_.set_remove_connection_termination_details(false);
+  proxy_status_config_.set_remove_response_flags(false);
+  stream_info_.response_code_details_ = "some_response_code_details";
+  stream_info_.connection_termination_details_ = "some_connection_termination_details";
+  EXPECT_THAT(ProxyStatusUtils::toString(stream_info_, ProxyStatusError::ProxyConfigurationError,
+                                         /*node_id=*/"UNUSED", proxy_status_config_),
+              AllOf(Not(HasSubstr("details=")), Not(HasSubstr("some_response_code_details")),
+                    Not(HasSubstr("some_connection_termination_details"))));
+}
+
+TEST_F(ProxyStatusTest, ToStringWithAllFields) {
+  proxy_status_config_.set_remove_details(false);
+  proxy_status_config_.set_remove_connection_termination_details(false);
+  proxy_status_config_.set_remove_response_flags(false);
+  stream_info_.response_code_details_ = "some_response_code_details";
+  stream_info_.connection_termination_details_ = "some_connection_termination_details";
+  EXPECT_THAT(
+      ProxyStatusUtils::toString(stream_info_, ProxyStatusError::ProxyConfigurationError,
+                                 /*node_id=*/"UNUSED", proxy_status_config_),
+      HasSubstr("details='some_response_code_details; some_connection_termination_details; DI'"));
+}
+
+TEST_F(ProxyStatusTest, ToStringNoConnectionTerminationDetails) {
+  proxy_status_config_.set_remove_details(false);
+  proxy_status_config_.set_remove_connection_termination_details(true);
+  proxy_status_config_.set_remove_response_flags(false);
+  stream_info_.response_code_details_ = "some_response_code_details";
+  stream_info_.connection_termination_details_ = "some_connection_termination_details";
+  EXPECT_THAT(ProxyStatusUtils::toString(stream_info_, ProxyStatusError::ProxyConfigurationError,
+                                         /*node_id=*/"UNUSED", proxy_status_config_),
+              AllOf(HasSubstr("details='some_response_code_details; DI'"),
+                    Not(HasSubstr("some_connection_termination_details"))));
+}
+
+TEST_F(ProxyStatusTest, ToStringAbsentConnectionTerminationDetails) {
+  proxy_status_config_.set_remove_details(false);
+  proxy_status_config_.set_remove_connection_termination_details(false); // Don't remove them,
+  proxy_status_config_.set_remove_response_flags(false);
+  stream_info_.response_code_details_ = "some_response_code_details";
+  stream_info_.connection_termination_details_ = absl::nullopt; // But they're absent,
+  EXPECT_THAT(
+      ProxyStatusUtils::toString(stream_info_, ProxyStatusError::ProxyConfigurationError,
+                                 /*node_id=*/"UNUSED", proxy_status_config_),
+      HasSubstr("details='some_response_code_details; DI'")); // So they shouldn't be printed.
+}
+
+TEST_F(ProxyStatusTest, ToStringNoResponseFlags) {
+  proxy_status_config_.set_remove_details(false);
+  proxy_status_config_.set_remove_connection_termination_details(false);
+  proxy_status_config_.set_remove_response_flags(true);
+  stream_info_.response_code_details_ = "some_response_code_details";
+  stream_info_.connection_termination_details_ = "some_connection_termination_details";
+  EXPECT_THAT(
+      ProxyStatusUtils::toString(stream_info_, ProxyStatusError::ProxyConfigurationError,
+                                 /*node_id=*/"UNUSED", proxy_status_config_),
+      AllOf(HasSubstr("details='some_response_code_details; some_connection_termination_details'"),
+            Not(HasSubstr("DI"))));
+}
+
+TEST_F(ProxyStatusTest, ToStringAbsentResponseFlags) {
+  proxy_status_config_.set_remove_details(false);
+  proxy_status_config_.set_remove_connection_termination_details(false);
+  proxy_status_config_.set_remove_response_flags(false);
+  stream_info_.response_code_details_ = "some_response_code_details";
+  stream_info_.connection_termination_details_ = "some_connection_termination_details";
+  ON_CALL(stream_info_, hasAnyResponseFlag()).WillByDefault(Return(false));
+  ON_CALL(stream_info_, hasResponseFlag(_)).WillByDefault(Return(false));
+  EXPECT_THAT(
+      ProxyStatusUtils::toString(stream_info_, ProxyStatusError::ProxyConfigurationError,
+                                 /*node_id=*/"UNUSED", proxy_status_config_),
+      AllOf(HasSubstr("details='some_response_code_details; some_connection_termination_details'"),
+            Not(HasSubstr("DI"))));
+}
+
+TEST_F(ProxyStatusTest, ToStringNoServerName) {
+  proxy_status_config_.set_proxy_name(HttpConnectionManager::ProxyStatusConfig::ENVOY_LITERAL);
+  EXPECT_THAT(ProxyStatusUtils::toString(stream_info_, ProxyStatusError::ProxyConfigurationError,
+                                         /*node_id=*/"UNUSED", proxy_status_config_),
+              AllOf(HasSubstr("envoy"), Not(HasSubstr("UNUSED"))));
+}
+
+TEST_F(ProxyStatusTest, ToStringServerName) {
+  proxy_status_config_.set_proxy_name(HttpConnectionManager::ProxyStatusConfig::NODE_ID);
+  EXPECT_THAT(ProxyStatusUtils::toString(stream_info_, ProxyStatusError::ProxyConfigurationError,
+                                         /*node_id=*/"foo", proxy_status_config_),
+              AllOf(HasSubstr("foo"), Not(HasSubstr("envoy"))));
 }
 
 } // namespace
