fed67803aa71be6eab67f4870b000816aed30ec3
Alyssa Wilk <alyssar@chromium.org>
2022-01-10 14:28:56 -0500

    test: disabling a flaky test
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -42,7 +42,7 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/extensions/filters/http/cache/simple_http_cache:96.0"
 "source/extensions/filters/http/grpc_json_transcoder:94.7"
 "source/extensions/filters/http/ip_tagging:89.1"
-"source/extensions/filters/http/kill_request:95.3" # Death tests don't report LCOV
+"source/extensions/filters/http/kill_request:91.7" # Death tests don't report LCOV
 "source/extensions/filters/http/lua:96.4"
 "source/extensions/filters/http/wasm:95.8"
 "source/extensions/filters/listener:95.9"
