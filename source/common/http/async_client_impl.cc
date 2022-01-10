b9d429a63651124a809774772be5f83c73eb647e
Caroline Stere <47414156+carolinestere@users.noreply.github.com>
2022-01-10 13:09:16 +0200

    Update comment
    Co-authored-by: Dhi Aurrahman <dio@tetrate.io>
    Signed-off-by: Caroline Stere <stere@adobe.com>
    
    Signed-off-by: Caroline Stere <47414156+carolinestere@users.noreply.github.com>

--- source/common/http/async_client_impl.cc
+++ source/common/http/async_client_impl.cc
@@ -257,7 +257,7 @@ AsyncRequestImpl::AsyncRequestImpl(RequestMessagePtr&& request, AsyncClientImpl&
     child_span_ = std::make_unique<Tracing::NullSpan>();
   }
   if (options.sampled_.has_value()) {
-    // Change span's sampling status; otherwise keep sampling status of the parent
+    // Change span's sampling status; otherwise keep sampling status of the parent.
     child_span_->setSampled(options.sampled_.value());
   }
 }
