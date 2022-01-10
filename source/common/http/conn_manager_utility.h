d9ac81b3353c34b1db7184f7706fbe22f7c5b9b8
James Buckland <jbuckland@google.com>
2022-01-10 09:18:41 -0500

    Change spelling for format_pre check.
    Signed-off-by: James Buckland <jbuckland@google.com>

--- source/common/http/conn_manager_utility.h
+++ source/common/http/conn_manager_utility.h
@@ -83,7 +83,7 @@ public:
    * @param request_headers the request headers.
    * @param the configuration for the HCM, which affects request ID headers.
    * @param via the via header to append, if any.
-   * @param stream_info a reference to the filter manager streaminfo.
+   * @param stream_info a reference to the filter manager stream info.
    * @param node_id the node ID.
    * @param clear_hop_by_hop_headers true if hop by hop headers should be
    *        cleared. This should only ever be false for envoy-mobile.
@@ -103,7 +103,7 @@ public:
    * fromStreamInfo().
    * @param response_headers the headers to mutate.
    * @param the configuration for the HCM, which affects request ID headers.
-   * @param stream_info a reference to the filter manager streaminfo.
+   * @param stream_info a reference to the filter manager stream info.
    * @param node_id the node ID.
    */
   static void setProxyStatusHeader(ResponseHeaderMap& response_headers,
