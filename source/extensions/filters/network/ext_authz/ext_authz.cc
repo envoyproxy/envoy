7a4fb90a3ee82179945414b4e0781be31a4c0eda
Haitao Chen <haidaocht@gmail.com>
2022-01-11 14:50:55 -0800

    fix build error
    Signed-off-by: Haitao Chen <haidaocht@gmail.com>

--- source/extensions/filters/network/ext_authz/ext_authz.cc
+++ source/extensions/filters/network/ext_authz/ext_authz.cc
@@ -25,10 +25,10 @@ void Filter::callCheck() {
                                                                config_->includePeerCertificate(),
                                                                config_->destinationLabels());
   // TODO: test this
-  const std::string server_name(read_callbacks_->connection().requestedServerName());
+  const std::string server_name(filter_callbacks_->connection().requestedServerName());
   if (!server_name.empty()) {
     auto attrs = check_request_.mutable_attributes();
-    (*attrs->mutable_destination()->mutable_service()) = server_name
+    (*attrs->mutable_destination()->mutable_service()) = server_name;
   }
 
   status_ = Status::Calling;
