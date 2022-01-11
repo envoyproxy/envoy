8a6e8730bbfd074350b7d4b85ff2741bc3643e66
Alyssa Wilk <alyssar@chromium.org>
2022-01-11 13:29:05 -0500

    maintainers: additions and subtractions
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- tools/code_format/check_format.py
+++ tools/code_format/check_format.py
@@ -1172,8 +1172,10 @@ if __name__ == "__main__":
     def owned_directories(error_messages):
         owned = []
         maintainers = [
-            '@mattklein123', '@htuch', '@alyssawilk', '@zuercher', '@lizan', '@snowp', '@asraa',
-            '@yanavlasov', '@junr03', '@dio', '@jmarantz', '@antoniovicente', '@ggreenway'
+            '@mattklein123', '@htuch', '@alyssawilk', '@zuercher', '@lizan', '@snowp',
+            '@yanavlasov', '@junr03', '@dio', '@jmarantz', '@antoniovicente', '@ggreenway',
+            '@phlax', '@wrowe', '@davinci26', '@rojkov', '@RyanTheOptimist', '@adisuissa',
+            '@KBaichoo',
         ]
 
         try:
