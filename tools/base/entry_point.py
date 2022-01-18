5a4861f1da7a6e32a030857d2e32764b81794f3d
Ryan Northey <ryan@synca.io>
2022-01-18 06:27:43 +0000

    add-comment
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tools/base/entry_point.py
+++ tools/base/entry_point.py
@@ -6,6 +6,11 @@ ENTRY_POINT_ALIAS = "_ENTRY_POINT_ALIAS_"
 
 
 def find_tool_path():
+    # In order to work in both build/run scenarios, and to work
+    # when the rule is invoked directly, or by another rule, the
+    # path to the entry_point binary has to be found from the
+    # `ENTRY_POINT_ALIAS` that is injected by the `genrule`
+
     entry_point_alias = f"external/{ENTRY_POINT_ALIAS.split('/external/')[1]}"
     if pathlib.Path(entry_point_alias).exists():
         return entry_point_alias
