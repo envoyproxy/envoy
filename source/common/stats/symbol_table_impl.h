a7074e25263a33d8920a055bb437b058c0ca2ed2
Joshua Marantz <jmarantz@google.com>
2022-01-22 09:43:23 -0500

    fix doc and restore API comments
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/common/stats/symbol_table_impl.h
+++ source/common/stats/symbol_table_impl.h
@@ -74,7 +74,7 @@ using SymbolVec = std::vector<Symbol>;
  * same string is re-encoded, it may or may not encode to the same underlying
  * symbol.
  */
-class SymbolTable {
+class SymbolTable final {
 public:
   /**
    * Efficient byte-encoded storage of an array of tokens. The most common
@@ -205,33 +205,163 @@ public:
   SymbolTable();
   ~SymbolTable();
 
-  // SymbolTable
+  /**
+   * Decodes a vector of symbols back into its period-delimited stat name. If
+   * decoding fails on any part of the symbol_vec, we release_assert and crash,
+   * since this should never happen, and we don't want to continue running
+   * with a corrupt stats set.
+   *
+   * @param stat_name the stat name.
+   * @return std::string stringified stat_name.
+   */
   std::string toString(const StatName& stat_name) const;
+
+  /**
+   * @return uint64_t the number of symbols in the symbol table.
+   */
   uint64_t numSymbols() const;
+
+  /**
+   * Determines whether one StatName lexically precedes another. Note that
+   * the lexical order may not exactly match the lexical order of the
+   * elaborated strings. For example, stat-name of "-.-" would lexically
+   * sort after "---" but when encoded as a StatName would come lexically
+   * earlier. In practice this is unlikely to matter as those are not
+   * reasonable names for Envoy stats.
+   *
+   * Note that this operation has to be performed with the context of the
+   * SymbolTable so that the individual Symbol objects can be converted
+   * into strings for lexical comparison.
+   *
+   * @param a the first stat name
+   * @param b the second stat name
+   * @return bool true if a lexically precedes b.
+   */
   bool lessThan(const StatName& a, const StatName& b) const;
-  void free(const StatName& stat_name);
-  void incRefCount(const StatName& stat_name);
+
+  /**
+   * Joins two or more StatNames. For example if we have StatNames for {"a.b",
+   * "c.d", "e.f"} then the joined stat-name matches "a.b.c.d.e.f". The
+   * advantage of using this representation is that it avoids having to
+   * decode/encode into the elaborated form, and does not require locking the
+   * SymbolTable.
+   *
+   * Note that this method does not bump reference counts on the referenced
+   * Symbols in the SymbolTable, so it's only valid as long for the lifetime of
+   * the joined StatNames.
+   *
+   * This is intended for use doing cached name lookups of scoped stats, where
+   * the scope prefix and the names to combine it with are already in StatName
+   * form. Using this class, they can be combined without accessing the
+   * SymbolTable or, in particular, taking its lock.
+   *
+   * @param stat_names the names to join.
+   * @return Storage allocated for the joined name.
+   */
   StoragePtr join(const StatNameVec& stat_names) const;
+
+  /**
+   * Populates a StatNameList from a list of encodings. This is not done at
+   * construction time to enable StatNameList to be instantiated directly in
+   * a class that doesn't have a live SymbolTable when it is constructed.
+   *
+   * @param names A pointer to the first name in an array, allocated by the caller.
+   * @param num_names The number of names.
+   * @param list The StatNameList representing the stat names.
+   */
   void populateList(const StatName* names, uint32_t num_names, StatNameList& list);
-  StoragePtr encode(absl::string_view name);
-  StoragePtr makeDynamicStorage(absl::string_view name);
 
 #ifndef ENVOY_CONFIG_COVERAGE
   void debugPrint() const;
 #endif
 
+  /**
+   * Creates a StatNameSet.
+   *
+   * @param name the name of the set.
+   * @return the set.
+   */
   StatNameSetPtr makeSet(absl::string_view name);
+
   using RecentLookupsFn = std::function<void(absl::string_view, uint64_t)>;
+
+  /**
+   * Calls the provided function with the name of the most recently looked-up
+   * symbols, including lookups on any StatNameSets, and with a count of
+   * the recent lookups on that symbol.
+   *
+   * @param iter the function to call for every recent item.
+   */
   uint64_t getRecentLookups(const RecentLookupsFn&) const;
+
+  /**
+   * Clears the recent-lookups structures.
+   */
   void clearRecentLookups();
+
+  /**
+   * Sets the recent-lookup capacity.
+   */
   void setRecentLookupCapacity(uint64_t capacity);
+
+  /**
+   * @return The configured recent-lookup tracking capacity.
+   */
   uint64_t recentLookupCapacity() const;
+
+  /**
+   * Identifies the dynamic components of a stat_name into an array of integer
+   * pairs, indicating the begin/end of spans of tokens in the stat-name that
+   * are created from StatNameDynamicStore or StatNameDynamicPool.
+   *
+   * This can be used to reconstruct the same exact StatNames in
+   * StatNames::mergeStats(), to enable stat continuity across hot-restart.
+   *
+   * @param stat_name the input stat name.
+   * @return the array of pairs indicating the bounds.
+   */
   DynamicSpans getDynamicSpans(StatName stat_name) const;
 
 private:
   friend class StatName;
   friend class StatNameTest;
   friend class StatNameDeathTest;
+  friend class StatNameDynamicStorage;
+  friend class StatNameList;
+  friend class StatNameStorage;
+
+  /**
+   * Encodes 'name' into the symbol table. Bumps reference counts for referenced
+   * symbols. The caller must manage the storage, and is responsible for calling
+   * SymbolTable::free() to release the reference counts.
+   *
+   * @param name The name to encode.
+   * @return The encoded name, transferring ownership to the caller.
+   *
+   */
+  StoragePtr encode(absl::string_view name);
+  StoragePtr makeDynamicStorage(absl::string_view name);
+
+  /**
+   * Since SymbolTable does manual reference counting, a client of SymbolTable
+   * must manually call free(symbol_vec) when it is freeing the backing store
+   * for a StatName. This way, the symbol table will grow and shrink
+   * dynamically, instead of being write-only.
+   *
+   * @param stat_name the stat name.
+   */
+  void free(const StatName& stat_name);
+
+  /**
+   * StatName backing-store can be managed by callers in a variety of ways
+   * to minimize overhead. But any persistent reference to a StatName needs
+   * to hold onto its own reference-counts for all symbols. This method
+   * helps callers ensure the symbol-storage is maintained for the lifetime
+   * of a reference.
+   *
+   * @param stat_name the stat name.
+   */
+  void incRefCount(const StatName& stat_name);
 
   struct SharedSymbol {
     SharedSymbol(Symbol symbol) : symbol_(symbol), ref_count_(1) {}
@@ -553,9 +683,9 @@ private:
  */
 class StatNameDynamicStorage : public StatNameStorageBase {
 public:
-  // Basic constructor based on a name. Note the table is used for
-  // a virtual-function call to encode the name, but no locks are taken
-  // in either implementation of the SymbolTable api.
+  // Basic constructor based on a name. Note the table is used for a call to
+  // encode the name, but no locks are taken in either implementation of the
+  // SymbolTable api.
   StatNameDynamicStorage(absl::string_view name, SymbolTable& table)
       : StatNameStorageBase(table.makeDynamicStorage(name)) {}
   // Move constructor.
