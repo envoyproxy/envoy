fabb13ed4ae31472e78d5facff4ddc1d4677453c
Kuat Yessenov <kuat@google.com>
2022-01-27 15:32:03 -0800

    fix test
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- test/extensions/io_socket/user_space/file_event_impl_test.cc
+++ test/extensions/io_socket/user_space/file_event_impl_test.cc
@@ -51,6 +51,9 @@ public:
   MOCK_METHOD(bool, isPeerWritable, (), (const));
   MOCK_METHOD(void, onPeerBufferLowWatermark, ());
   MOCK_METHOD(bool, isReadable, (), (const));
+  MOCK_METHOD(const envoy::config::core::v3::Metadata&, metadata, (), (const));
+  MOCK_METHOD(const envoy::config::core::v3::Metadata*, peerMetadata, (), (const));
+  MOCK_METHOD(void, setMetadata, (const envoy::config::core::v3::Metadata&));
 };
 
 class FileEventImplTest : public testing::Test {
