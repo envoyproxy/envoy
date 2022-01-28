a51ecfa4046235a6de90136ffdc99e28cb284093
Yanjun Xiang <yanjunxiang@google.com>
2022-01-28 00:19:32 +0000

    Decouple unit tests with the existing Protobuf DebugString format
    Signed-off-by: Yanjun Xiang <yanjunxiang@google.com>

--- test/common/network/resolver_impl_test.cc
+++ test/common/network/resolver_impl_test.cc
@@ -74,8 +74,10 @@ TEST(ResolverTest, InternalListenerNameFromProtoAddress) {
 TEST(ResolverTest, UninitializedInternalAddressFromProtoAddress) {
   envoy::config::core::v3::Address internal_address;
   internal_address.mutable_envoy_internal_address();
-  EXPECT_THROW_WITH_MESSAGE(resolveProtoAddress(internal_address), EnvoyException,
-                            "Failed to resolve address:envoy_internal_address {\n}\n");
+  EXPECT_THROW_WITH_MESSAGE(resolveProtoAddress(internal_address),
+                            EnvoyException,
+                            fmt::format("Failed to resolve address:{}",
+                                        internal_address.DebugString()));
 }
 
 // Validate correct handling of ipv4_compat field.
--- test/common/tracing/http_tracer_manager_impl_test.cc
+++ test/common/tracing/http_tracer_manager_impl_test.cc
@@ -130,12 +130,13 @@ TEST_F(HttpTracerManagerImplTest, ShouldFailIfProviderSpecificConfigIsNotValid) 
   tracing_config.set_name("envoy.tracers.sample");
   tracing_config.mutable_typed_config()->PackFrom(ValueUtil::stringValue("value"));
 
+  ProtobufWkt::Any expected_any_proto;
+  expected_any_proto.PackFrom(ValueUtil::stringValue("value"));
   EXPECT_THROW_WITH_MESSAGE(
-      http_tracer_manager_.getOrCreateHttpTracer(&tracing_config), EnvoyException,
-      R"(Unable to unpack as google.protobuf.Struct: [type.googleapis.com/google.protobuf.Value] {
-  string_value: "value"
-}
-)");
+      http_tracer_manager_.getOrCreateHttpTracer(&tracing_config),
+      EnvoyException,
+      fmt::format("Unable to unpack as google.protobuf.Struct: {}",
+                  expected_any_proto.DebugString()));
 }
 
 class HttpTracerManagerImplCacheTest : public testing::Test {
