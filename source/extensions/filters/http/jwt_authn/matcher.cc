11e461a0fa1d3aa46ccfe9c5f20d3a8a5ef70c78
Yan Avlasov <yavlasov@google.com>
2022-02-01 19:04:30 +0000

    [1.18] CVE-2021-43824
    jwt_atuhn: fixed the crash when a CONNECT request is sent to JWT filter
    configured with regex match.
    
    Signed-off-by: Yan Avlasov <yavlasov@google.com>

--- source/extensions/filters/http/jwt_authn/matcher.cc
+++ source/extensions/filters/http/jwt_authn/matcher.cc
@@ -125,6 +125,9 @@ public:
 
   bool matches(const Http::RequestHeaderMap& headers) const override {
     if (BaseMatcherImpl::matchRoute(headers)) {
+      if (headers.Path() == nullptr) {
+        return false;
+      }
       const Http::HeaderString& path = headers.Path()->value();
       const absl::string_view query_string = Http::Utility::findQueryStringStart(path);
       absl::string_view path_view = path.getStringView();
