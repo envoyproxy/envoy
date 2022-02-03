1ced5116e5b16ce92cb0c38357d0494b282f419a
Michael Behr <mkbehr@google.com>
2022-02-03 14:28:46 -0500

    cache filter: Create an abstract test for HttpCache implementations.
    Move SimpleHttpCacheTest into a new HttpCacheImplementationTest filter. Other
    HttpCache implementations can reuse the tests in HttpCacheImplementationTest by
    defining an HttpCacheTestDelegate, which wraps the cache.
    
    HttpCacheTestDelegate allows caches to specify whether or not they support
    validating stale cache entries and updating their headers. The relevant tests
    from SimpleHttpCacheTest are split into different versions to run depending on
    whether or not the cache supports validation.
    
    This also moves operator definitions out of
    test/extensions/filters/http/cache/common.h to avoid linker errors.
    
    Signed-off-by: Michael Behr <mkbehr@google.com>

---
+++ test/extensions/filters/http/cache/common.cc
@@ -0,0 +1,64 @@
+#include "test/extensions/filters/http/cache/common.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace HttpFilters {
+namespace Cache {
+
+std::ostream& operator<<(std::ostream& os, const RequestCacheControl& request_cache_control) {
+  std::string s = "{";
+  s += request_cache_control.must_validate_ ? "must_validate, " : "";
+  s += request_cache_control.no_store_ ? "no_store, " : "";
+  s += request_cache_control.no_transform_ ? "no_transform, " : "";
+  s += request_cache_control.only_if_cached_ ? "only_if_cached, " : "";
+
+  s += request_cache_control.max_age_.has_value()
+           ? "max-age=" + std::to_string(request_cache_control.max_age_.value().count()) + ", "
+           : "";
+  s += request_cache_control.min_fresh_.has_value()
+           ? "min-fresh=" + std::to_string(request_cache_control.min_fresh_.value().count()) + ", "
+           : "";
+  s += request_cache_control.max_stale_.has_value()
+           ? "max-stale=" + std::to_string(request_cache_control.max_stale_.value().count()) + ", "
+           : "";
+
+  // Remove any extra ", " at the end
+  if (s.size() > 1) {
+    s.pop_back();
+    s.pop_back();
+  }
+
+  s += "}";
+  return os << s;
+}
+
+std::ostream& operator<<(std::ostream& os, const ResponseCacheControl& response_cache_control) {
+  std::string s = "{";
+  s += response_cache_control.must_validate_ ? "must_validate, " : "";
+  s += response_cache_control.no_store_ ? "no_store, " : "";
+  s += response_cache_control.no_transform_ ? "no_transform, " : "";
+  s += response_cache_control.no_stale_ ? "no_stale, " : "";
+  s += response_cache_control.is_public_ ? "public, " : "";
+
+  s += response_cache_control.max_age_.has_value()
+           ? "max-age=" + std::to_string(response_cache_control.max_age_.value().count()) + ", "
+           : "";
+
+  // Remove any extra ", " at the end
+  if (s.size() > 1) {
+    s.pop_back();
+    s.pop_back();
+  }
+
+  s += "}";
+  return os << s;
+}
+
+std::ostream& operator<<(std::ostream& os, const AdjustedByteRange& range) {
+  return os << "[" << range.begin() << "," << range.end() << ")";
+}
+
+} // namespace Cache
+} // namespace HttpFilters
+} // namespace Extensions
+} // namespace Envoy
