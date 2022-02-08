25f84dbbc1bade7a66202018228b132046239964
Piotr Sikora <piotrsikora@google.com>
2022-02-07 21:17:05 -0800

    review: purge temurin-*-jdk.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- .azure-pipelines/cleanup.sh
+++ .azure-pipelines/cleanup.sh
@@ -5,6 +5,6 @@ set -e
 # Temporary script to remove tools from Azure pipelines agent to create more disk space room.
 sudo apt-get update -y || true
 sudo apt-get purge -y --no-upgrade 'ghc-*' 'zulu-*-azure-jdk' 'libllvm*' 'mysql-*' 'dotnet-*' 'libgl1' \
-  'adoptopenjdk-*' 'azure-cli' 'google-chrome-stable' 'firefox' 'hhvm'
+  'adoptopenjdk-*' 'azure-cli' 'google-chrome-stable' 'firefox' 'hhvm' 'temurin-*-jdk'
 
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -rn
