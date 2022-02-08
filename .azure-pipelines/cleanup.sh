85d5d580281de3c45ff10c022d3cb93d64435225
Ryan Northey <ryan@synca.io>
2022-02-08 11:48:58 +0000

    feedback-and-cleanups
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- .azure-pipelines/cleanup.sh
+++ .azure-pipelines/cleanup.sh
@@ -3,8 +3,23 @@
 set -e
 
 # Temporary script to remove tools from Azure pipelines agent to create more disk space room.
+
+PURGE_PACKAGES=(
+     'adoptopenjdk-*'
+     azure-cli
+     'ghc-*'
+     'libllvm*'
+     'mysql-*'
+     'dotnet-*'
+     firefox
+     google-chrome-stable
+     hhvm
+     libgl1
+     mongodb-mongosh
+     'temurin-*-jdk'
+     'zulu-*-azure-jdk')
+
 sudo apt-get update -y || true
-sudo apt-get purge -y --no-upgrade 'ghc-*' 'zulu-*-azure-jdk' 'libllvm*' 'mysql-*' 'dotnet-*' 'libgl1' \
-  'adoptopenjdk-*' 'azure-cli' 'google-chrome-stable' 'firefox' 'hhvm'
+sudo apt-get purge -y --no-upgrade "${PURGE_PACKAGES[@]}"
 
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -rn
