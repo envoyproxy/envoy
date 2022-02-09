42bc4a670374f30b8009c4956d7cf29b0f9b7559
asheryer <asheryer@amazon.com>
2022-02-09 20:15:58 +0000

    Fix more formatting issues
    Signed-off-by: asheryer <asheryer@amazon.com>

--- source/common/upstream/outlier_detection_impl.cc
+++ source/common/upstream/outlier_detection_impl.cc
@@ -256,9 +256,8 @@ DetectorConfig::DetectorConfig(const envoy::config::cluster::v3::OutlierDetectio
       max_ejection_time_ms_(static_cast<uint64_t>(PROTOBUF_GET_MS_OR_DEFAULT(
           config, max_ejection_time,
           std::max(DEFAULT_MAX_EJECTION_TIME_MS, base_ejection_time_ms_)))),
-      max_ejection_time_jitter_ms_(static_cast<uint64_t>(
-          PROTOBUF_GET_MS_OR_DEFAULT(config, max_ejection_time_jitter,
-                                     DEFAULT_MAX_EJECTION_TIME_JITTER_MS))) {}
+      max_ejection_time_jitter_ms_(static_cast<uint64_t>(PROTOBUF_GET_MS_OR_DEFAULT(
+          config, max_ejection_time_jitter, DEFAULT_MAX_EJECTION_TIME_JITTER_MS))) {}
 
 DetectorImpl::DetectorImpl(const Cluster& cluster,
                            const envoy::config::cluster::v3::OutlierDetection& config,
@@ -283,12 +282,10 @@ DetectorImpl::~DetectorImpl() {
   }
 }
 
-std::shared_ptr<DetectorImpl>
-DetectorImpl::create(const Cluster& cluster,
-                     const envoy::config::cluster::v3::OutlierDetection& config,
-                     Event::Dispatcher& dispatcher, Runtime::Loader& runtime,
-                     TimeSource& time_source, EventLoggerSharedPtr event_logger,
-                     Random::RandomGenerator& random) {
+std::shared_ptr<DetectorImpl> DetectorImpl::create(
+    const Cluster& cluster, const envoy::config::cluster::v3::OutlierDetection& config,
+    Event::Dispatcher& dispatcher, Runtime::Loader& runtime, TimeSource& time_source,
+    EventLoggerSharedPtr event_logger, Random::RandomGenerator& random) {
   std::shared_ptr<DetectorImpl> detector(
       new DetectorImpl(cluster, config, dispatcher, runtime, time_source, event_logger, random));
 
@@ -478,23 +475,25 @@ void DetectorImpl::ejectHost(HostSharedPtr host,
           runtime_.snapshot().getInteger(BaseEjectionTimeMsRuntime, config_.baseEjectionTimeMs()));
       const std::chrono::milliseconds max_eject_time = std::chrono::milliseconds(
           runtime_.snapshot().getInteger(MaxEjectionTimeMsRuntime, config_.maxEjectionTimeMs()));
-      const std::chrono::milliseconds max_eject_time_jitter = std::chrono::milliseconds(
-          runtime_.snapshot().getInteger(MaxEjectionTimeJitterMsRuntime, config_.maxEjectionTimeJitterMs()));
+      const std::chrono::milliseconds max_eject_time_jitter =
+          std::chrono::milliseconds(runtime_.snapshot().getInteger(
+              MaxEjectionTimeJitterMsRuntime, config_.maxEjectionTimeJitterMs()));
 
       // Generate random jitter so that not all hosts uneject at the same time,
       // which could possibly generate a connection storm
 
       // std::chrono::milliseconds receives a signed integer as input, so we ensure that our random
       // random value is in the range [0, std::numeric_limits<long>::max())
-      long random_long = static_cast<long>(random_generator_() % (std::numeric_limits<long>::max()));
+      long random_long =
+          static_cast<long>(random_generator_() % (std::numeric_limits<long>::max()));
       // cap the jitter at max_eject_time_jitter
       // if max_eject_time_jitter is 0, the jitter will also equal 0 because we apply the
       // modulo operator with 1.
       const std::chrono::milliseconds jitter =
-          std::chrono::milliseconds(random_long) % (max_eject_time_jitter + std::chrono::milliseconds(1));
+          std::chrono::milliseconds(random_long) %
+          (max_eject_time_jitter + std::chrono::milliseconds(1));
       // save the jitter on the current host_monitor
       host_monitors_[host]->setJitter(jitter);
-
 
       if ((host_monitors_[host]->ejectTimeBackoff() * base_eject_time + jitter) <
           (max_eject_time + base_eject_time + jitter)) {
