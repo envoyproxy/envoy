f1306a7bcea14d03572cdd45826f90e278e4b762
JP Simard <jp@jpsim.com>
2022-02-16 11:38:03 -0500

    [Project] Establish a Lyft support rotation (#2051)
    This change includes:
    
    * Documentation stating the purpose, function and reasoning behind the
      Lyft support rotation.
    * A YAML file capturing the rotation list of GitHub user names and who's
      currently on rotation.
    * A script to bump who's on rotation to the next person.
    * A GitHub Action that runs weekly to automatically update who's on
      rotation.
    
    There's a lot of room for growth in this system, such as posting who's
    on rotation to Slack, generating a calendar viewable in Google Calendar
    or other calendar clients, automatically assigning PRs, extending to
    have rotations for different companies or project areas etc.
    
    Signed-off-by: JP Simard <jp@jpsim.com>

---
+++ mobile/tools/bump_lyft_support_rotation.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+#######################################################
+# bump_lyft_support_rotation.sh
+#
+# Assigns the next person in the Lyft maintainers list.
+#######################################################
+
+# TODO(jpsim): Use a yaml parsing library if the format ever expands
+# beyond its current very simple form.
+
+function next_maintainer() {
+  current="$1"
+  file="$2"
+
+  while IFS= read -r line; do
+    maintainers+=("${line#"  - "}")
+  done <<< "$(sed -n '/^  - /p' "$file")"
+
+  for i in "${!maintainers[@]}"; do
+    if [[ "${maintainers[$i]}" = "${current}" ]]; then
+      last_index=$((${#maintainers[@]}-1))
+      if [[ $i == "$last_index" ]]; then
+        echo "${maintainers[0]}"
+        break
+      else
+        echo "${maintainers[$((i + 1))]}"
+        break
+      fi
+    fi
+  done
+}
+
+function set_maintainer() {
+  maintainer="$1"
+  file="$2"
+
+  echo "current: $maintainer" > "$file.tmp"
+  tail -n +2 "$file" >> "$file.tmp"
+  mv "$file.tmp" "$file"
+}
+
+maintainers_file=".github/lyft_maintainers.yml"
+first_line="$(head -n 1 "$maintainers_file")"
+current=${first_line#"current: "}
+next="$(next_maintainer "$current" "$maintainers_file")"
+set_maintainer "$next" "$maintainers_file"
+echo "Lyft support maintainer changing from $current to $next"
