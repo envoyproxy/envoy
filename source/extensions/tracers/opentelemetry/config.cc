db054cef7a6589d8941861adfcb6bddbf696dd57
Alex Ellis <alexellis@google.com>
2022-02-20 13:20:36 -0500

    Add config for otlp tracer
    Signed-off-by: Alex Ellis <alexellis@google.com>

---
+++ source/extensions/tracers/opentelemetry/config.cc
@@ -0,0 +1,44 @@
+#include "source/extensions/tracers/opentelemetry/config.h"
+
+#include "envoy/config/trace/v3/opentelemetry.pb.h"
+#include "envoy/config/trace/v3/opentelemetry.pb.validate.h"
+#include "envoy/registry/registry.h"
+#include "source/common/common/logger.h"
+
+#include "source/extensions/tracers/opentelemetry/opentelemetry_tracer_impl.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Tracers {
+namespace OpenTelemetry {
+
+OpenTelemetryTracerFactory::OpenTelemetryTracerFactory()
+    : FactoryBase("envoy.tracers.opentelemetry") {}
+
+Tracing::DriverSharedPtr OpenTelemetryTracerFactory::createTracerDriverTyped(
+    const envoy::config::trace::v3::OpenTelemetryConfig& proto_config,
+    Server::Configuration::TracerFactoryContext& context) {
+  // Since OpenTelemetry can only support a single tracing configuration per entire process,
+  // we need to make sure that it is configured at most once.
+  if (driver_) {
+    if (Envoy::Protobuf::util::MessageDifferencer::Equals(config_, proto_config)) {
+      return driver_;
+    } else {
+      throw EnvoyException("OpenTelemetry has already been configured with a different config.");
+    }
+  }
+
+  driver_ = std::make_shared<Driver>(proto_config, context);
+  config_ = proto_config;
+  return driver_;
+}
+
+/**
+ * Static registration for the OpenTelemetry tracer. @see RegisterFactory.
+ */
+REGISTER_FACTORY(OpenTelemetryTracerFactory, Server::Configuration::TracerFactory);
+
+} // namespace OpenTelemetry
+} // namespace Tracers
+} // namespace Extensions
+} // namespace Envoy
---
+++ source/extensions/tracers/opentelemetry/config.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+
+#include "envoy/config/trace/v3/opentelemetry.pb.h"
+#include "envoy/config/trace/v3/opentelemetry.pb.validate.h"
+
+#include "source/extensions/tracers/common/factory_base.h"
+#include "envoy/config/trace/v3/opentelemetry.pb.validate.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Tracers {
+namespace OpenTelemetry {
+
+/**
+ * Config registration for the OpenTelemetry tracer. @see TracerFactory.
+ */
+class OpenTelemetryTracerFactory
+    : Logger::Loggable<Logger::Id::tracing>, public Common::FactoryBase<envoy::config::trace::v3::OpenTelemetryConfig> {
+public:
+  OpenTelemetryTracerFactory();
+
+private:
+  // FactoryBase
+  Tracing::DriverSharedPtr
+  createTracerDriverTyped(const envoy::config::trace::v3::OpenTelemetryConfig& proto_config,
+                          Server::Configuration::TracerFactoryContext& context) override;
+
+  Tracing::DriverSharedPtr driver_;
+  envoy::config::trace::v3::OpenTelemetryConfig config_;
+};
+
+} // namespace OpenTelemetry
+} // namespace Tracers
+} // namespace Extensions
+} // namespace Envoy
