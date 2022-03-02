0a30621883b02c21d9e40bd63c0e68af393260a0
Alyssa Wilk <alyssar@chromium.org>
2022-03-02 11:27:57 -0500

    fixes
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/common/http/header_map_impl_fuzz_test.cc
+++ test/common/http/header_map_impl_fuzz_test.cc
@@ -20,10 +20,9 @@ DEFINE_PROTO_FUZZER(const test::common::http::HeaderMapImplFuzzTestCase& input) 
   TestScopedRuntime scoped_runtime;
   // Set the lazy header-map threshold if found.
   if (input.has_config()) {
-    scoped_runtime.mergeValues(
-        {{"envoy.reloadable_features.deprecate_global_ints", "false"},
-         {"envoy.http.headermap.lazy_map_min_size",
-          absl::StrCat(input.config().lazy_map_min_size())}});
+    scoped_runtime.mergeValues({{"envoy.reloadable_features.deprecate_global_ints", "false"},
+                                {"envoy.http.headermap.lazy_map_min_size",
+                                 absl::StrCat(input.config().lazy_map_min_size())}});
   }
 
   auto header_map = Http::RequestHeaderMapImpl::create();
