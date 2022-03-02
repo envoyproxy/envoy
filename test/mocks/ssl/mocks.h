17916d788727c2c81b07105a91aaf599e328dce2
bozhang <bozhang@ebay.com>
2022-03-01 21:35:20 -0700

    format fix
    Signed-off-by: bozhang <bozhang@ebay.com>

--- test/mocks/ssl/mocks.h
+++ test/mocks/ssl/mocks.h
@@ -104,7 +104,7 @@ public:
   MOCK_METHOD(const std::string&, signingAlgorithmsForTest, (), (const));
   MOCK_METHOD(const Network::Address::IpList&, tlsKeyLogLocal, (), (const));
   MOCK_METHOD(const Network::Address::IpList&, tlsKeyLogRemote, (), (const));
-  MOCK_METHOD(const std::string&, tlsKeyLogPath(), (), (const));
+  MOCK_METHOD(const std::string&, tlsKeyLogPath, (), (const));
   Ssl::HandshakerCapabilities capabilities_;
   std::string sni_{"default_sni.example.com"};
   std::string ciphers_{"RSA"};
