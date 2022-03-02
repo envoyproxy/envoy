b9a0078d6a424775e3764db4344315c40a497f96
Jose Ulises Nino Rivera <junr03@users.noreply.github.com>
2022-03-02 13:12:09 -0800

    envoy: 3267361cedf936 (#2076)
    Signed-off-by: Jose Nino <jnino@lyft.com>
    
    Co-authored-by: Alan Chiu <achiu@lyft.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/common/extensions/retry/options/network_configuration/predicate.cc
+++ mobile/library/common/extensions/retry/options/network_configuration/predicate.cc
@@ -32,12 +32,19 @@ NetworkConfigurationRetryOptionsPredicate::updateOptions(
     return Upstream::RetryOptionsPredicate::UpdateOptionsReturn{absl::nullopt};
   }
 
-  auto& extra_stream_info =
+  StreamInfo::ExtraStreamInfo* extra_stream_info =
       filter_state->getDataMutable<StreamInfo::ExtraStreamInfo>(StreamInfo::ExtraStreamInfo::key());
+
+  if (extra_stream_info == nullptr) {
+    ENVOY_LOG(warn, "extra stream info is missing");
+
+    // Returning nullopt results in existing socket options being preserved.
+    return Upstream::RetryOptionsPredicate::UpdateOptionsReturn{absl::nullopt};
+  }
 
   // This check is also defensive. The NetworkConfigurationFilter should always set this when
   // ExtraStreaminfo is created.
-  if (!extra_stream_info.configuration_key_.has_value()) {
+  if (!extra_stream_info->configuration_key_.has_value()) {
     ENVOY_LOG(warn, "network configuration key is missing");
 
     // Returning nullopt results in existing socket options being preserved.
@@ -54,11 +61,11 @@ NetworkConfigurationRetryOptionsPredicate::updateOptions(
 
   // Report request status to network configurator, so that socket configuration may be adapted
   // to current network conditions.
-  network_configurator_->reportNetworkUsage(extra_stream_info.configuration_key_.value(),
+  network_configurator_->reportNetworkUsage(extra_stream_info->configuration_key_.value(),
                                             network_fault);
 
   // Update socket configuration for next retry attempt.
-  extra_stream_info.configuration_key_ = network_configurator_->addUpstreamSocketOptions(options);
+  extra_stream_info->configuration_key_ = network_configurator_->addUpstreamSocketOptions(options);
 
   // The options returned here replace any existing socket options used for a prior attempt. At
   // present, all socket options set in Envoy Mobile are provided by the NetworkConfigurator, so
