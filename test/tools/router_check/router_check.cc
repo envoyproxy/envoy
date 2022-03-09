6bec8f5c6f24c9ecdc6c246bfd4897c5930376a0
Zurab Svianadze <zurab@google.com>
2022-03-09 11:15:08 +0000

    Add an option to write Envoy router check tool test results to a file.
    Signed-off-by: Zurab Svianadze <zurab@google.com>

--- test/tools/router_check/router_check.cc
+++ test/tools/router_check/router_check.cc
@@ -1,12 +1,46 @@
 // NOLINT(namespace-envoy)
 #include <iostream>
+#include <memory>
 #include <string>
+#include <vector>
 
+#include "source/common/common/fmt.h"
 #include "source/common/common/thread.h"
+#include "source/common/protobuf/utility.h"
 #include "source/exe/platform_impl.h"
 
 #include "test/test_common/test_runtime.h"
 #include "test/tools/router_check/router.h"
+#include "test/tools/router_check/validation.pb.h"
+
+bool hasFailures(
+    const std::vector<envoy::RouterCheckToolSchema::ValidationItemResult>& test_results) {
+  for (const auto& test_result : test_results) {
+    if (!test_result.test_passed()) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void writeOutput(const envoy::RouterCheckToolSchema::ValidationResult& result,
+                 const std::string& filepath) {
+  auto stats = std::make_unique<Envoy::Stats::IsolatedStoreImpl>();
+  auto api = Envoy::Api::createApiForTest(*stats);
+  static constexpr Envoy::Filesystem::FlagSet DefaultFlags{
+      1 << Envoy::Filesystem::File::Operation::Write |
+      1 << Envoy::Filesystem::File::Operation::Create};
+  Envoy::Filesystem::FilePathAndType file_info{Envoy::Filesystem::DestinationType::File, filepath};
+  auto file = api->fileSystem().createFile(file_info);
+  if (!file || !file->open(DefaultFlags).return_value_) {
+    throw Envoy::EnvoyException(fmt::format("Failed to open file for write {}", filepath));
+  }
+  const auto& write_result = file->write(result.SerializeAsString());
+  if (!write_result.ok()) {
+    throw Envoy::EnvoyException(fmt::format("Failed to write output to file {}, error: {}",
+                                            filepath, write_result.err_->getErrorDetails()));
+  }
+}
 
 int main(int argc, char* argv[]) {
   Envoy::Options options(argc, argv);
@@ -29,9 +63,15 @@ int main(int argc, char* argv[]) {
       checktool.setOnlyShowFailures();
     }
 
-    bool is_equal = checktool.compareEntries(options.testPath());
+    const std::vector<envoy::RouterCheckToolSchema::ValidationItemResult> test_results =
+        checktool.compareEntries(options.testPath());
+    if (!options.outputPath().empty()) {
+      envoy::RouterCheckToolSchema::ValidationResult result;
+      *result.mutable_test_results() = {test_results.begin(), test_results.end()};
+      writeOutput(result, options.outputPath());
+    }
     // Test fails if routes do not match what is expected
-    if (!is_equal) {
+    if (hasFailures(test_results)) {
       return EXIT_FAILURE;
     }
 
