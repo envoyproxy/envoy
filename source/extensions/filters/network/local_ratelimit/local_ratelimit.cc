1e615b7dee191cff360396cbf06a425e664bf3ee
Greg Greenway <ggreenway@apple.com>
2022-03-11 17:45:20 -0800

    local_ratelimit: allow sharing token bucket between filter chains
    Signed-off-by: Greg Greenway <ggreenway@apple.com>

--- source/extensions/filters/network/local_ratelimit/local_ratelimit.cc
+++ source/extensions/filters/network/local_ratelimit/local_ratelimit.cc
@@ -10,26 +10,60 @@ namespace Extensions {
 namespace NetworkFilters {
 namespace LocalRateLimitFilter {
 
+SINGLETON_MANAGER_REGISTRATION(shared_local_ratelimit);
+
+LocalRateLimiterImplSharedPtr SharedRateLimitSingleton::get(
+    const envoy::extensions::filters::network::local_ratelimit::v3::LocalRateLimit& proto_config,
+    std::function<LocalRateLimiterImplSharedPtr()> create_fn) {
+  if (proto_config.share_key().empty()) {
+    // If there is no key, never share.
+    return create_fn();
+  }
+
+  const Key key{proto_config.share_key(), proto_config.token_bucket()};
+  auto it = limiters_.find(key);
+  if (it == limiters_.end()) {
+    ENVOY_LOG(debug, fmt::format("LocalRateLimit for share_key '{}' is creating a new token bucket "
+                                 "due to no match found",
+                                 proto_config.share_key()));
+    it = limiters_.emplace(key, create_fn()).first;
+  } else {
+    ENVOY_LOG(
+        debug,
+        fmt::format("LocalRateLimit for share_key '{}' is using an existing matching token bucket",
+                    proto_config.share_key()));
+  }
+  return it->second;
+}
+
 Config::Config(
     const envoy::extensions::filters::network::local_ratelimit::v3::LocalRateLimit& proto_config,
-    Event::Dispatcher& dispatcher, Stats::Scope& scope, Runtime::Loader& runtime)
-    : rate_limiter_(Filters::Common::LocalRateLimit::LocalRateLimiterImpl(
-          std::chrono::milliseconds(
-              PROTOBUF_GET_MS_REQUIRED(proto_config.token_bucket(), fill_interval)),
-          proto_config.token_bucket().max_tokens(),
-          PROTOBUF_GET_WRAPPED_OR_DEFAULT(proto_config.token_bucket(), tokens_per_fill, 1),
-          dispatcher,
-          Protobuf::RepeatedPtrField<
-              envoy::extensions::common::ratelimit::v3::LocalRateLimitDescriptor>())),
-      enabled_(proto_config.runtime_enabled(), runtime),
-      stats_(generateStats(proto_config.stat_prefix(), scope)) {}
+    Event::Dispatcher& dispatcher, Stats::Scope& scope, Runtime::Loader& runtime,
+    Singleton::Manager& singleton_manager)
+    : enabled_(proto_config.runtime_enabled(), runtime),
+      stats_(generateStats(proto_config.stat_prefix(), scope)),
+      shared_bucket_registry_(singleton_manager.getTyped<SharedRateLimitSingleton>(
+          SINGLETON_MANAGER_REGISTERED_NAME(shared_local_ratelimit),
+          []() { return std::make_shared<SharedRateLimitSingleton>(); })) {
+
+  rate_limiter_ = shared_bucket_registry_->get(proto_config, [&]() {
+    return std::make_shared<Filters::Common::LocalRateLimit::LocalRateLimiterImpl>(
+        std::chrono::milliseconds(
+            PROTOBUF_GET_MS_REQUIRED(proto_config.token_bucket(), fill_interval)),
+        proto_config.token_bucket().max_tokens(),
+        PROTOBUF_GET_WRAPPED_OR_DEFAULT(proto_config.token_bucket(), tokens_per_fill, 1),
+        dispatcher,
+        Protobuf::RepeatedPtrField<
+            envoy::extensions::common::ratelimit::v3::LocalRateLimitDescriptor>());
+  });
+}
 
 LocalRateLimitStats Config::generateStats(const std::string& prefix, Stats::Scope& scope) {
   const std::string final_prefix = "local_rate_limit." + prefix;
   return {ALL_LOCAL_RATE_LIMIT_STATS(POOL_COUNTER_PREFIX(scope, final_prefix))};
 }
 
-bool Config::canCreateConnection() { return rate_limiter_.requestAllowed({}); }
+bool Config::canCreateConnection() { return rate_limiter_->requestAllowed({}); }
 
 Network::FilterStatus Filter::onNewConnection() {
   if (!config_->enabled()) {
--- source/extensions/filters/network/local_ratelimit/local_ratelimit.h
+++ source/extensions/filters/network/local_ratelimit/local_ratelimit.h
@@ -4,6 +4,7 @@
 #include "envoy/extensions/filters/network/local_ratelimit/v3/local_rate_limit.pb.h"
 #include "envoy/network/filter.h"
 #include "envoy/runtime/runtime.h"
+#include "envoy/singleton/manager.h"
 #include "envoy/stats/stats_macros.h"
 
 #include "source/common/common/thread_synchronizer.h"
@@ -27,6 +28,34 @@ struct LocalRateLimitStats {
   ALL_LOCAL_RATE_LIMIT_STATS(GENERATE_COUNTER_STRUCT)
 };
 
+using LocalRateLimiterImplSharedPtr =
+    std::shared_ptr<Filters::Common::LocalRateLimit::LocalRateLimiterImpl>;
+
+class SharedRateLimitSingleton : public Singleton::Instance, Logger::Loggable<Logger::Id::filter> {
+public:
+  LocalRateLimiterImplSharedPtr
+  get(const envoy::extensions::filters::network::local_ratelimit::v3::LocalRateLimit& proto_config,
+      std::function<LocalRateLimiterImplSharedPtr()> create_fn);
+
+  class Key : public std::pair<std::string, envoy::type::v3::TokenBucket> {
+  public:
+    using std::pair<std::string, envoy::type::v3::TokenBucket>::pair;
+
+    // Add absl::Hash support.
+    template <typename H> friend H AbslHashValue(H h, const Key& key) {
+      return H::combine(std::move(h), key.first, MessageUtil::hash(key.second));
+    }
+
+    bool operator==(const Key& that) const {
+      return (this->first == that.first) &&
+             Protobuf::util::MessageDifferencer::Equivalent(this->second, that.second);
+    }
+  };
+
+private:
+  absl::flat_hash_map<Key, LocalRateLimiterImplSharedPtr> limiters_;
+};
+
 /**
  * Configuration shared across all connections. Must be thread safe.
  */
@@ -34,7 +63,8 @@ class Config : Logger::Loggable<Logger::Id::filter> {
 public:
   Config(
       const envoy::extensions::filters::network::local_ratelimit::v3::LocalRateLimit& proto_config,
-      Event::Dispatcher& dispatcher, Stats::Scope& scope, Runtime::Loader& runtime);
+      Event::Dispatcher& dispatcher, Stats::Scope& scope, Runtime::Loader& runtime,
+      Singleton::Manager& singleton_manager);
 
   bool canCreateConnection();
   bool enabled() { return enabled_.enabled(); }
@@ -42,11 +72,13 @@ public:
 
 private:
   static LocalRateLimitStats generateStats(const std::string& prefix, Stats::Scope& scope);
-  void onFillTimer();
 
-  Filters::Common::LocalRateLimit::LocalRateLimiterImpl rate_limiter_;
+  LocalRateLimiterImplSharedPtr rate_limiter_;
   Runtime::FeatureFlag enabled_;
   LocalRateLimitStats stats_;
+
+  // Nothing else holds a reference to the singleton, so hold one here to ensure it isn't deleted.
+  std::shared_ptr<SharedRateLimitSingleton> shared_bucket_registry_;
 
   friend class LocalRateLimitTestBase;
 };
