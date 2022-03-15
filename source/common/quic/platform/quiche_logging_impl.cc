d181022ca67cd6422b5c5a70f15615bb1d5729a1
Ryan Hamilton <rch@google.com>
2022-03-15 23:25:41 +0000

    Move quic_logging_impl.{h,cc} to quiche_logging_impl.{h,cc} to match the name expected by the QUICHE platform API
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- source/common/quic/platform/quiche_logging_impl.cc
+++ source/common/quic/platform/quiche_logging_impl.cc
@@ -4,7 +4,7 @@
 // consumed or referenced directly by other Envoy code. It serves purely as a
 // porting layer for QUICHE.
 
-#include "source/common/quic/platform/quic_logging_impl.h"
+#include "source/common/quic/platform/quiche_logging_impl.h"
 
 #include <atomic>
 #include <cstdlib>
