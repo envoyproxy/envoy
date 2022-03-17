76ea83a588fc3f000ed77d04e61507f24274e051
Ryan Hamilton <rch@google.com>
2022-03-17 06:32:31 -0700

    Move quic_logging_impl.{h,cc} to quiche_logging_impl.{h,cc} (#20361)
    Move quic_logging_impl.{h,cc} to quiche_logging_impl.{h,cc} to match the name expected by the QUICHE platform API. Initially quic_logging_impl was the name reference from API, but in the past it changed to quiche_logging_impl. When that happened, we created quiche_logging_impl.h which simply #included quic_logging_impl.h and quic_logging_impl.h was modified to define QUICHE_foo instead of QUIC_foo. But this adds an unnecessary layer of indirection.
    
    Risk Level: N/A
    Testing: N/A
    Docs Changes: N/A
    Release Notes: N/A
    Platform Specific Features: N/A
    
    Signed-off-by: Ryan Hamilton rch@google.com

--- test/common/quic/platform/quic_mock_log_impl.h
+++ test/common/quic/platform/quic_mock_log_impl.h
@@ -17,7 +17,7 @@ namespace quic {
 
 // A QuicEnvoyMockLog object captures QUIC_LOG() messages emitted between StartCapturingLogs() and
 // destruction(or StopCapturingLogs()).
-class QuicEnvoyMockLog : public QuicLogSink {
+class QuicEnvoyMockLog : public quiche::QuicheLogSink {
 public:
   QuicEnvoyMockLog() = default;
 
@@ -27,14 +27,16 @@ public:
     }
   }
 
-  MOCK_METHOD(void, Log, (QuicLogLevel level, const std::string& message));
+  MOCK_METHOD(void, Log, (quiche::QuicheLogLevel level, const std::string& message));
 
+  // NOLINTNEXTLINE(readability-identifier-naming)
   void StartCapturingLogs() {
     ASSERT(!is_capturing_);
     is_capturing_ = true;
     original_sink_ = SetLogSink(this);
   }
 
+  // NOLINTNEXTLINE(readability-identifier-naming)
   void StopCapturingLogs() {
     ASSERT(is_capturing_);
     is_capturing_ = false;
@@ -42,7 +44,7 @@ public:
   }
 
 private:
-  QuicLogSink* original_sink_;
+  quiche::QuicheLogSink* original_sink_;
   bool is_capturing_{false};
 };
 
@@ -51,14 +53,14 @@ private:
 // behavior is restored.
 class ScopedDisableExitOnDFatal {
 public:
-  ScopedDisableExitOnDFatal() : previous_value_(isDFatalExitDisabled()) {
-    setDFatalExitDisabled(true);
+  ScopedDisableExitOnDFatal() : previous_value_(quiche::isDFatalExitDisabled()) {
+    quiche::setDFatalExitDisabled(true);
   }
 
   ScopedDisableExitOnDFatal(const ScopedDisableExitOnDFatal&) = delete;
   ScopedDisableExitOnDFatal& operator=(const ScopedDisableExitOnDFatal&) = delete;
 
-  ~ScopedDisableExitOnDFatal() { setDFatalExitDisabled(previous_value_); }
+  ~ScopedDisableExitOnDFatal() { quiche::setDFatalExitDisabled(previous_value_); }
 
 private:
   const bool previous_value_;
@@ -73,7 +75,7 @@ using QuicMockLogImpl = quic::QuicEnvoyMockLog;
 #define EXPECT_QUIC_LOG_CALL_IMPL(log) EXPECT_CALL(log, Log(testing::_, testing::_))
 
 #define EXPECT_QUIC_LOG_CALL_CONTAINS_IMPL(log, level, content)                                    \
-  EXPECT_CALL(log, Log(static_cast<quic::QuicLogLevel>(quic::LogLevel##level),                     \
+  EXPECT_CALL(log, Log(static_cast<quiche::QuicheLogLevel>(quiche::LogLevel##level),               \
                        testing::HasSubstr(content)))
 
 // Not part of the api exposed by quic_mock_log.h. This is used by
@@ -81,7 +83,8 @@ using QuicMockLogImpl = quic::QuicEnvoyMockLog;
 #define EXPECT_QUIC_LOG_IMPL(statement, level, matcher)                                            \
   do {                                                                                             \
     quic::QuicEnvoyMockLog mock_log;                                                               \
-    EXPECT_CALL(mock_log, Log(static_cast<quic::QuicLogLevel>(quic::LogLevel##level), matcher))    \
+    EXPECT_CALL(mock_log,                                                                          \
+                Log(static_cast<quiche::QuicheLogLevel>(quiche::LogLevel##level), matcher))        \
         .Times(testing::AtLeast(1));                                                               \
     mock_log.StartCapturingLogs();                                                                 \
     { statement; }                                                                                 \
