b479a0eed8880d2f0ad51e78f4574575d899d713
Scott Ganyo <theganyo@google.com>
2022-03-18 15:28:58 +0000

    fix via_upstream population of RESPONSE_CODE_DETAILS for response headers
    added test (thanks @alyssawilk)
    
    Signed-off-by: Scott Ganyo <theganyo@google.com>
    
    fix via_upstream population of RESPONSE_CODE_DETAILS for response headers
    
    Signed-off-by: Scott Ganyo <theganyo@google.com>

--- source/common/router/router.cc
+++ source/common/router/router.cc
@@ -1470,6 +1470,8 @@ void Filter::onUpstreamHeaders(uint64_t response_code, Http::ResponseHeaderMapPt
   for (const auto& header_value : downstream_set_cookies_) {
     headers->addReferenceKey(Http::Headers::get().SetCookie, header_value);
   }
+  
+  callbacks_->streamInfo().setResponseCodeDetails(StreamInfo::ResponseCodeDetails::get().ViaUpstream);
 
   // TODO(zuercher): If access to response_headers_to_add (at any level) is ever needed outside
   // Router::Filter we'll need to find a better location for this work. One possibility is to
