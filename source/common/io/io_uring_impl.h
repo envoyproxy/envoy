aa2ddaec49ae48c8b94ee639da000281fcc10a97
Dmitry Rozhkov <dmitry.rozhkov@intel.com>
2022-03-23 13:54:06 +0200

    use ThreadLocal::TypedSlot
    Signed-off-by: Dmitry Rozhkov <dmitry.rozhkov@intel.com>

--- source/common/io/io_uring_impl.h
+++ source/common/io/io_uring_impl.h
@@ -9,21 +9,6 @@
 namespace Envoy {
 namespace Io {
 
-class IoUringFactoryImpl : public IoUringFactory {
-public:
-  IoUringFactoryImpl(uint32_t io_uring_size, bool use_submission_queue_polling,
-                     ThreadLocal::SlotAllocator& tls);
-
-  // IoUringFactory
-  IoUring& getOrCreate() const override;
-  void onServerInitialized() override;
-
-private:
-  const uint32_t io_uring_size_{};
-  const bool use_submission_queue_polling_{};
-  ThreadLocal::SlotPtr tls_;
-};
-
 class IoUringImpl : public IoUring, public ThreadLocal::ThreadLocalObject {
 public:
   IoUringImpl(uint32_t io_uring_size, bool use_submission_queue_polling);
@@ -49,6 +34,21 @@ private:
   struct io_uring ring_;
   std::vector<struct io_uring_cqe*> cqes_;
   os_fd_t event_fd_{INVALID_SOCKET};
+};
+
+class IoUringFactoryImpl : public IoUringFactory {
+public:
+  IoUringFactoryImpl(uint32_t io_uring_size, bool use_submission_queue_polling,
+                     ThreadLocal::SlotAllocator& tls);
+
+  // IoUringFactory
+  IoUring& getOrCreate() const override;
+  void onServerInitialized() override;
+
+private:
+  const uint32_t io_uring_size_{};
+  const bool use_submission_queue_polling_{};
+  ThreadLocal::TypedSlot<IoUringImpl> tls_;
 };
 
 } // namespace Io
