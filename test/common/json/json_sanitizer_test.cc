918d9d1fa43780d7df392b80b08da2475121b94e
Joshua Marantz <jmarantz@google.com>
2022-03-25 15:40:30 -0400

    added one more test surveying all 8-bit inputs.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/common/json/json_sanitizer_test.cc
+++ test/common/json/json_sanitizer_test.cc
@@ -145,6 +145,73 @@ TEST_F(JsonSanitizerTest, MultiByteUtf8) {
   EXPECT_EQ(UnicodeSizePair(0x1d11e, 4), decode(TrebleClefUtf8));
 }
 
+TEST_F(JsonSanitizerTest, Low8Bit) {
+  // The characters from 0 to 0xBF (191) inclusive are all rendered identically
+  // to the protobuf json encoder.
+  std::string x0_7f;
+  for (uint32_t i = 0; i <= 0x7f; ++i) {
+    char ch = i;
+    x0_7f.push_back(ch);
+  }
+  EXPECT_EQ(
+      // Control-characters 0-31
+      "\\u0000\\u0001\\u0002\\u0003\\u0004\\u0005\\u0006\\u0007\\b\\t\\n"
+      "\\u000b\\f\\r\\u000e\\u000f\\u0010\\u0011\\u0012\\u0013\\u0014\\u0015"
+      "\\u0016\\u0017\\u0018\\u0019\\u001a\\u001b\\u001c\\u001d\\u001e\\u001f"
+
+      // Printable characters starting with space. Double-quote is back-slashed.
+      " !\\\"#$%&'()*+,-./0123456789:;"
+
+      // < and > are serialized by json as unicode.
+      "\\u003c=\\u003e?@ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+      // Remaining 7-bit codes ending with 127, which is rendered as a unicode escape.
+      "[\\\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\\u007f",
+
+      sanitizeAndCheckAgainstProtobufJson(x0_7f));
+}
+
+TEST_F(JsonSanitizerTest, High8Bit) {
+  std::string x80_ff;
+  for (uint32_t i = 0x80; i <= 0xff; ++i) {
+    char ch = i;
+    x80_ff.push_back(ch);
+  }
+  // The characters from 0x80 (192) to 255 all start out like they are
+  // multi-byte utf-8 sequences, but in this context are not followed by the
+  // right continuation pattern. The protobuf json serializer generates
+  // lots of error messages for these and yields empty strings, but we
+  // just escape them as single bytes.
+  EXPECT_EQ(
+      // The codes from 128-159 (0x9f) are rendered as several ways: unicode
+      // escapes or literal 8-bit characters.
+      "\\u0080\\u0081\\u0082\\u0083\\u0084\\u0085\\u0086\\u0087\\u0088\\u0089"
+      "\\u008a\\u008b\\u008c\\u008d\\u008e\\u008f\\u0090\\u0091\\u0092\\u0093"
+      "\\u0094\\u0095\\u0096\\u0097\\u0098\\u0099\\u009a\\u009b\\u009c\\u009d"
+      "\\u009e\\u009f"
+
+      // Then a sequence of literal 8-bit characters.
+      "\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC"
+
+      // Weird special-case behavior to match json sanitizer
+      "\\u00ad"
+
+      // More literal 8-bit characters.
+      "\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC"
+      "\xBD\xBE\xBF"
+
+      // Codes with a utf8 introductory byte pattern that lack the correct
+      // pattern for the remaining codes. These get hex-escaped by the json
+      // sanitizer, whereas the protobuf serializer generates an error message
+      // and returns an empty string.
+      "\\xc0\\xc1\\xc2\\xc3\\xc4\\xc5\\xc6\\xc7\\xc8\\xc9\\xca"
+      "\\xcb\\xcc\\xcd\\xce\\xcf\\xd0\\xd1\\xd2\\xd3\\xd4\\xd5\\xd6\\xd7\\xd8"
+      "\\xd9\\xda\\xdb\\xdc\\xdd\\xde\\xdf\\xe0\\xe1\\xe2\\xe3\\xe4\\xe5\\xe6"
+      "\\xe7\\xe8\\xe9\\xea\\xeb\\xec\\xed\\xee\\xef\\xf0\\xf1\\xf2\\xf3\\xf4"
+      "\\xf5\\xf6\\xf7\\xf8\\xf9\\xfa\\xfb\\xfc\\xfd\\xfe\\xff",
+      sanitizer_.sanitize(buffer_, x80_ff));
+}
+
 TEST_F(JsonSanitizerTest, InvalidUtf8) {
   // 2 byte
   EXPECT_EQ("\\xce", sanitizeInvalid(truncate(LambdaUtf8)));
