02d435b23c78a4f40bd3bf08229681edb15e1122
Raven Black <ravenblack@dropbox.com>
2022-03-30 21:10:20 +0000

    Add const to listener_filter_fakes
    Signed-off-by: Raven Black <ravenblack@dropbox.com>

--- test/extensions/filters/listener/common/fuzz/listener_filter_fakes.h
+++ test/extensions/filters/listener/common/fuzz/listener_filter_fakes.h
@@ -59,9 +59,10 @@ private:
 // TODO: Move over to Fake (name is confusing)
 class FakeOsSysCalls : public Api::OsSysCallsImpl {
 public:
-  MOCK_METHOD(Api::SysCallSizeResult, recv, (os_fd_t, void*, size_t, int));
+  MOCK_METHOD(Api::SysCallSizeResult, recv, (os_fd_t, void*, size_t, int), (const));
   MOCK_METHOD(Api::SysCallIntResult, ioctl,
-              (os_fd_t, unsigned long, void*, unsigned long, void*, unsigned long, unsigned long*));
+              (os_fd_t, unsigned long, void*, unsigned long, void*, unsigned long, unsigned long*),
+              (const));
 };
 
 } // namespace ListenerFilters
