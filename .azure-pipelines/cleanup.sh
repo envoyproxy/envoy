a345a2cb667c12e3eadfc215144629b20cbfccc5
Ryan Northey <ryan@synca.io>
2022-04-01 19:29:51 +0100

    [TESTING/WIP] repo: Remove more packages to free diskspace in Docker job
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- .azure-pipelines/cleanup.sh
+++ .azure-pipelines/cleanup.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+echo "Disk space before cleanup"
+df -h
+
 # Temporary script to remove tools from Azure pipelines agent to create more disk space room.
 
 PURGE_PACKAGES=(
@@ -19,7 +22,38 @@ PURGE_PACKAGES=(
      'temurin-*-jdk'
      'zulu-*-azure-jdk')
 
+PURGE_EXTRA_PACKAGES=(
+    "nginx*"
+    "mongodb*"
+    "mono*"
+    "mssql*"
+    "podman"
+    "powershell"
+    "php*")
+
 sudo apt-get update -y || true
 sudo apt-get purge -y --no-upgrade "${PURGE_PACKAGES[@]}"
+echo "Disk space after package removal current"
+df -h
+
+sudo apt-get autoremove --purge -y
+echo "Disk space after autoremove current"
+df -h
+
+sudo apt-get clean
+echo "Disk space after apt clean current"
+df -h
+
+sudo apt-get purge -y --no-upgrade "${PURGE_EXTRA_PACKAGES[@]}"
+echo "Disk space after package removal extra"
+df -h
+
+sudo apt-get autoremove --purge -y
+echo "Disk space after autoremove extra"
+df -h
+
+sudo apt-get clean
+echo "Disk space after apt clean extra"
+df -h
 
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -rn
