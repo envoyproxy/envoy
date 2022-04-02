6d2982ba5835d5616071f6ed5e82325fe7cf474f
Joshua Marantz <jmarantz@google.com>
2022-04-01 23:10:36 -0400

    fuzz-test compile fix.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/common/json/json_sanitizer_fuzz_test.cc
+++ test/common/json/json_sanitizer_fuzz_test.cc
@@ -11,39 +11,22 @@
 namespace Envoy {
 namespace Fuzz {
 
-const Envoy::Json::JsonSanitizer& staticSanitizer() {
-  CONSTRUCT_ON_FIRST_USE(Envoy::Json::JsonSanitizer);
-}
-
 DEFINE_FUZZER(const uint8_t* buf, size_t len) {
-  const Envoy::Json::JsonSanitizer& sanitizer = staticSanitizer();
   FuzzedDataProvider provider(buf, len);
-  std::string buffer1, buffer2;
+  std::string buffer1, buffer2, errmsg;
   while (provider.remaining_bytes() != 0) {
     std::string input = provider.ConsumeRandomLengthString(provider.remaining_bytes());
-    absl::string_view hand_sanitized = sanitizer.sanitize(buffer1, input);
+    absl::string_view sanitized = Envoy::Json::sanitize(buffer1, input);
 
     // If the input is valid UTF-8 we can do a differential test against the
     // Protobuf JSON sanitizer. Otherwise we are simply ensuring that the
     // sanitizer does not crash.
-    if (Envoy::Json::isProtoSerializableUtf8(input)) {
+    if (Envoy::Json::TestUtil::isProtoSerializableUtf8(input)) {
       buffer2 =
           MessageUtil::getJsonStringFromMessageOrDie(ValueUtil::stringValue(input), false, true);
-      absl::string_view proto_sanitized = Envoy::Json::stripDoubleQuotes(buffer2);
-      if (hand_sanitized != proto_sanitized) {
-        std::cerr << "ERROR on input = ";
-        for (char c : input) {
-          if (c == '\\' || c == '"') {
-            std::cerr << "\\" << c;
-          } else if (c < ' ' || c > 126) {
-            std::cerr << "\\" << absl::StrFormat("%03o", static_cast<uint8_t>(c));
-          } else {
-            std::cerr << c;
-          }
-        }
-        std::cerr << std::endl;
-      }
-      FUZZ_ASSERT_EQ(hand_sanitized, proto_sanitized, input);
+      absl::string_view proto_sanitized = Envoy::Json::TestUtil::stripDoubleQuotes(buffer2);
+      RELEASE_ASSERT(Envoy::Json::TestUtil::utf8Equivalent(sanitized, proto_sanitized, errmsg),
+                     errmsg);
     }
   }
 }
