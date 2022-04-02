41ac656dc1aeec474c7c196627780471d9432dcb
Joshua Marantz <jmarantz@google.com>
2022-04-02 14:32:36 -0400

    Use uint23_t for the array of booleans as it's slightly faster.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/common/json/json_sanitizer.cc
+++ source/common/json/json_sanitizer.cc
@@ -11,35 +11,38 @@ namespace Json {
 
 // clang-format off
 // SPELLCHECKER(off)
-static constexpr bool needs_slow_sanitizer[256] = {
+//
+// Performance benchmarks show this is slightly faster as an array of uint32_t
+// rather than an array of bool.
+static constexpr uint32_t needs_slow_sanitizer[256] = {
   // Control-characters 0-31 all require escapes.
-  true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
-  true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 
   // Pass through printable characters starting with space. Double-quote and
   // backslash require an escape.
-  false, false, true /* " */, false, false, false, false, false,         //  !"#$%&'
-  false, false, false, false, false, false, false, false,                // ()*+,-.7
-  false, false, false, false, false, false, false, false,                // 01234567
-  false, false, false, false, false, false, false, false,                // 89:;<=>?
-  false, false, false, false, false, false, false, false,                // @ABCDEFG
-  false, false, false, false, false, false, false, false,                // HIJKLMNO
-  false, false, false, false, false, false, false, false,                // PQRSTUVW
-  false, false, false, false, true /* backslash */, false, false, false, // XYZ[\]^_
-  false, false, false, false, false, false, false, false,                // `abcdefg
-  false, false, false, false, false, false, false, false,                // hijklmno
-  false, false, false, false, false, false, false, false,                // pqrstuvw
-  false, false, false, false, false, false, false, true,                 // xyz{|}~\177
+  0, 0, 1 /* " */, 0, 0, 0, 0, 0,         //  !"#$%&'
+  0, 0, 0, 0, 0, 0, 0, 0,                 // ()*+,-.7
+  0, 0, 0, 0, 0, 0, 0, 0,                 // 01234567
+  0, 0, 0, 0, 0, 0, 0, 0,                 // 89:;<=>?
+  0, 0, 0, 0, 0, 0, 0, 0,                 // @ABCDEFG
+  0, 0, 0, 0, 0, 0, 0, 0,                 // HIJKLMNO
+  0, 0, 0, 0, 0, 0, 0, 0,                 // PQRSTUVW
+  0, 0, 0, 0, 1 /* backslash */, 0, 0, 0, // XYZ[\]^_
+  0, 0, 0, 0, 0, 0, 0, 0,                 // `abcdefg
+  0, 0, 0, 0, 0, 0, 0, 0,                 // hijklmno
+  0, 0, 0, 0, 0, 0, 0, 0,                 // pqrstuvw
+  0, 0, 0, 0, 0, 0, 0, 1,                 // xyz{|}~\177
 
   // 0x80-0xff, all of which require calling the slow sanitizer.
-  true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
-  true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
-  true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
-  true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
-  true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
-  true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
-  true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
-  true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 };
 // SPELLCHECKER(on)
 // clang-format on
@@ -53,13 +56,13 @@ absl::string_view sanitize(std::string& buffer, absl::string_view str) {
   // loop. This avoids branches and allows simpler loop unrolling by the
   // compiler.
   ASSERT(ARRAY_SIZE(needs_slow_sanitizer) == 256);
-  uint32_t need_slow = false;
+  uint32_t need_slow = 0;
   for (char c : str) {
     // We need to escape control characters, characters >= 127, and double-quote
     // and backslash.
     need_slow |= needs_slow_sanitizer[static_cast<uint8_t>(c)];
   }
-  if (!need_slow) {
+  if (need_slow == 0) {
     return str; // Fast path, should be executed most of the time.
   }
   TRY_ASSERT_MAIN_THREAD {
--- source/common/json/json_sanitizer.h
+++ source/common/json/json_sanitizer.h
@@ -10,7 +10,16 @@ namespace Json {
 
 /**
  * Sanitizes a string so it is suitable for JSON. The buffer is
- * used if any of the characters in str need to be escaped.
+ * used if any of the characters in str need to be escaped. Performance
+ * is good if there are no characters requiring escaping or utf-8 decode.
+ *
+ * --------------------------------------------------------------------
+ * Benchmark                          Time             CPU   Iterations
+ * --------------------------------------------------------------------
+ * BM_ProtoEncoderNoEscape         1445 ns         1444 ns       455727
+ * BM_NlohmannNoEscape             9.79 ns         9.79 ns     71449511
+ * BM_ProtoEncoderWithEscape       1521 ns         1521 ns       462697
+ * BM_NlohmannWithEscape            215 ns          215 ns      3264218
  *
  * @param buffer a string in which an escaped string can be written, if needed.
  *   It is not necessary for callers to clear the buffer first; it be cleared
