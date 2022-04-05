4527f1c65b715beab45f958cce81da5c41240e56
Yan Avlasov <yavlasov@google.com>
2022-04-05 15:32:05 -0400

    fix format check
    Signed-off-by: Yan Avlasov <yavlasov@google.com>

--- tools/code_format/check_format.py
+++ tools/code_format/check_format.py
@@ -51,7 +51,8 @@ REAL_TIME_ALLOWLIST = (
 # perform temporary registrations.
 REGISTER_FACTORY_TEST_ALLOWLIST = (
     "./test/common/config/registry_test.cc", "./test/integration/clusters/",
-    "./test/integration/filters/", "./test/integration/load_balancers/")
+    "./test/integration/filters/", "./test/integration/load_balancers/",
+    "./test/extensions/transport_sockets/tls/integration/")
 
 # Files in these paths can use MessageLite::SerializeAsString
 SERIALIZE_AS_STRING_ALLOWLIST = (
