3b7724cc961a3f714804296d1ccbfb5551119555
giantcroc <changran.wang@intel.com>
2022-04-07 14:55:38 +0800

    add verify
    Signed-off-by: giantcroc <changran.wang@intel.com>

--- examples/gzip/service.py
+++ examples/gzip/service.py
@@ -13,11 +13,13 @@ def get_plain_file():
 def get_json_file():
     return send_from_directory("data", "file.json")
 
+
 @app.route("/upload", methods=['POST'])
 def test_decompressor():
     resp = Response("OK")
     resp.headers["decompressed-size"] = len(next(iter(request.form)))
     return resp
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
