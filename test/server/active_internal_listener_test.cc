a1debd474b6f252d82b2ffadb18e766222a0d2c4
He Jie Xu <hejie.xu@intel.com>
2022-04-10 11:06:01 +0000

    fix internal listener test
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- test/server/active_internal_listener_test.cc
+++ test/server/active_internal_listener_test.cc
@@ -117,10 +117,6 @@ TEST_F(ActiveInternalListenerTest, DestroyListenerClosesActiveSocket) {
   Network::MockConnectionSocket* accepted_socket = new NiceMock<Network::MockConnectionSocket>();
   NiceMock<Network::MockIoHandle> io_handle;
   EXPECT_CALL(*accepted_socket, ioHandle()).WillRepeatedly(ReturnRef(io_handle));
-  EXPECT_CALL(io_handle, recv)
-      .WillOnce(Return(ByMove(Api::IoCallUint64Result(
-          0, Api::IoErrorPtr(Network::IoSocketError::getIoSocketEagainInstance(),
-                             Network::IoSocketError::deleteIoError)))));
   EXPECT_CALL(io_handle, isOpen()).WillOnce(Return(true));
 
   EXPECT_CALL(filter_chain_factory_, createListenerFilterChain(_))
