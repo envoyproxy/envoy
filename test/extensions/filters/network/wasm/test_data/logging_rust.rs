9a46a2b835b893b7b752afe498e229b3e5252fed
Piotr Sikora <piotrsikora@google.com>
2022-04-15 11:18:32 -0500

    wasm: update Proxy-Wasm Rust SDK to v0.2.0. (#20743)
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- test/extensions/filters/network/wasm/test_data/logging_rust.rs
+++ test/extensions/filters/network/wasm/test_data/logging_rust.rs
@@ -2,13 +2,12 @@ use log::trace;
 use proxy_wasm::traits::{Context, StreamContext};
 use proxy_wasm::types::*;
 
-#[no_mangle]
-pub fn _start() {
+proxy_wasm::main! {{
     proxy_wasm::set_log_level(LogLevel::Trace);
     proxy_wasm::set_stream_context(|context_id, _| -> Box<dyn StreamContext> {
         Box::new(TestStream { context_id })
     });
-}
+}}
 
 struct TestStream {
     context_id: u32,
