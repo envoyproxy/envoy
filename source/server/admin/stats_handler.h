465678b07fcb7363665f1e8706e0b79400742a1d
Joshua Marantz <jmarantz@google.com>
2022-04-15 11:41:06 -0400

    admin: roll back stats_handler.cc et al back to previous state while we sort out some sporadic crashes (#20835)
    Signed-off-by: Joshua Marantz <jmarantz@google.com>
    Signed-off-by: Andre Vehreschild <vehre@x41-dsec.de>

--- source/server/admin/stats_handler.h
+++ source/server/admin/stats_handler.h
@@ -9,6 +9,7 @@
 #include "envoy/server/admin.h"
 #include "envoy/server/instance.h"
 
+#include "source/common/stats/histogram_impl.h"
 #include "source/server/admin/handler_ctx.h"
 #include "source/server/admin/utils.h"
 
@@ -37,18 +38,63 @@ public:
   Http::Code handlerStatsRecentLookupsEnable(absl::string_view path_and_query,
                                              Http::ResponseHeaderMap& response_headers,
                                              Buffer::Instance& response, AdminStream&);
+  Http::Code handlerStats(absl::string_view path_and_query,
+                          Http::ResponseHeaderMap& response_headers, Buffer::Instance& response,
+                          AdminStream&);
+  static Http::Code handlerStats(Stats::Store& stats, bool used_only, bool json,
+                                 const absl::optional<std::regex>& filter,
+                                 Utility::HistogramBucketsMode histogram_buckets_mode,
+                                 Http::ResponseHeaderMap& response_headers,
+                                 Buffer::Instance& response);
+
   Http::Code handlerPrometheusStats(absl::string_view path_and_query,
                                     Http::ResponseHeaderMap& response_headers,
                                     Buffer::Instance& response, AdminStream&);
-  Http::Code prometheusStats(absl::string_view path_and_query, Buffer::Instance& response);
   Http::Code handlerContention(absl::string_view path_and_query,
                                Http::ResponseHeaderMap& response_headers,
                                Buffer::Instance& response, AdminStream&);
 
-  Admin::RequestPtr makeRequest(absl::string_view path, AdminStream& admin_stream);
-  static Admin::RequestPtr makeRequest(Stats::Store& stats, bool used_only, bool json,
-                                       Utility::HistogramBucketsMode histogram_buckets_mode,
-                                       const absl::optional<std::regex>& regex);
+private:
+  template <class StatType>
+  static bool shouldShowMetric(const StatType& metric, const bool used_only,
+                               const absl::optional<std::regex>& regex) {
+    return ((!used_only || metric.used()) &&
+            (!regex.has_value() || std::regex_search(metric.name(), regex.value())));
+  }
+
+  friend class StatsHandlerTest;
+
+  static std::string statsAsJson(const std::map<std::string, uint64_t>& all_stats,
+                                 const std::map<std::string, std::string>& text_readouts,
+                                 const std::vector<Stats::ParentHistogramSharedPtr>& all_histograms,
+                                 bool used_only, const absl::optional<std::regex>& regex,
+                                 Utility::HistogramBucketsMode histogram_buckets_mode,
+                                 bool pretty_print = false);
+
+  static void statsAsText(const std::map<std::string, uint64_t>& all_stats,
+                          const std::map<std::string, std::string>& text_readouts,
+                          const std::vector<Stats::ParentHistogramSharedPtr>& all_histograms,
+                          bool used_only, const absl::optional<std::regex>& regex,
+                          Utility::HistogramBucketsMode histogram_buckets_mode,
+                          Buffer::Instance& response);
+
+  static std::string computeDisjointBucketSummary(const Stats::ParentHistogramSharedPtr& histogram);
+
+  static void statsAsJsonQuantileSummaryHelper(
+      Protobuf::Map<std::string, ProtobufWkt::Value>& histograms_obj_container_fields,
+      const std::vector<Stats::ParentHistogramSharedPtr>& all_histograms, bool used_only,
+      const absl::optional<std::regex>& regex);
+
+  static void statsAsJsonHistogramBucketsHelper(
+      Protobuf::Map<std::string, ProtobufWkt::Value>& histograms_obj_container_fields,
+      const std::vector<Stats::ParentHistogramSharedPtr>& all_histograms, bool used_only,
+      const absl::optional<std::regex>& regex,
+      std::function<std::vector<uint64_t>(const Stats::HistogramStatistics&)> computed_buckets);
+
+  static ProtobufWkt::Value statsAsJsonHistogramBucketsCreateHistogramElementHelper(
+      Stats::ConstSupportedBuckets& supported_buckets,
+      const std::vector<uint64_t>& interval_buckets,
+      const std::vector<uint64_t>& cumulative_buckets, const std::string& name);
 };
 
 } // namespace Server
