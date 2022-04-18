e8696dec2922990c96d61e2d9a19613a67b2d1e9
wangkai19 <wangkai19@corp.netease.com>
2022-04-18 20:03:47 +0800

    add descriptor example verify
    Signed-off-by: wangkai19 <wangkai19@corp.netease.com>

--- examples/local_ratelimit/verify.sh
+++ examples/local_ratelimit/verify.sh
@@ -5,8 +5,8 @@ export NAME=ratelimit
 # shellcheck source=examples/verify-common.sh
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
-run_log "Test upstream: localhost:10000 without rate limit header two times"
-for i in {1..2}; do
+run_log "Test upstream: localhost:10000 without rate limit header three times"
+for i in {1..3}; do
     output=$(curl -s -X GET --head http://localhost:10000)
     echo "${output}" | grep "429 Too Many Requests" && exit 1
     echo "${output}" | grep "x-local-rate-limit: true" && exit 1
@@ -22,8 +22,8 @@ done
 run_log "Sleep 5s to wait rate limiting refresh"
 sleep 5
 
-run_log "Test upstream: localhost:10000 without rate limit response two times"
-for i in {1..2}; do
+run_log "Test upstream: localhost:10000 without rate limit response three times"
+for i in {1..3}; do
     responds_without \
         "local_rate_limited" \
         http://localhost:10000
