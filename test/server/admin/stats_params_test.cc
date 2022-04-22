9747a02af7a1134418ef53ff976a14bbfee7fa69
Joshua Marantz <jmarantz@google.com>
2022-04-22 18:16:54 -0400

    run the params test.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/server/admin/stats_params_test.cc
+++ test/server/admin/stats_params_test.cc
@@ -6,22 +6,12 @@
 namespace Envoy {
 namespace Server {
 
-TEST(StatsParamsTest, TypeToString) {
-  EXPECT_EQ("TextReadouts", StatsParams::typeToString(StatsType::TextReadouts));
-  EXPECT_EQ("Gauges", StatsParams::typeToString(StatsType::Gauges));
-  EXPECT_EQ("Counters", StatsParams::typeToString(StatsType::Counters));
-  EXPECT_EQ("Histograms", StatsParams::typeToString(StatsType::Histograms));
-  EXPECT_EQ("All", StatsParams::typeToString(StatsType::All));
-}
-
 TEST(StatsParamsTest, ParseParamsFormat) {
   Buffer::OwnedImpl response;
   StatsParams params;
 
   ASSERT_EQ(Http::Code::OK, params.parse("?format=text", response));
   EXPECT_EQ(StatsFormat::Text, params.format_);
-  ASSERT_EQ(Http::Code::OK, params.parse("?format=html", response));
-  EXPECT_EQ(StatsFormat::Html, params.format_);
   ASSERT_EQ(Http::Code::OK, params.parse("?format=json", response));
   EXPECT_EQ(StatsFormat::Json, params.format_);
   ASSERT_EQ(Http::Code::OK, params.parse("?format=prometheus", response));
