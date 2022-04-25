4f8dd622f235df3e3b090a4c085902a0623ce454
Charles Le Borgne <80125532+carloseltuerto@users.noreply.github.com>
2022-04-25 18:00:20 +0100

    rollback #2195 (#2196)
    Signed-off-by: Charles Le Borgne <cleborgne@google.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/common/http/client.cc
+++ mobile/library/common/http/client.cc
@@ -269,6 +269,9 @@ void Client::DirectStreamCallbacks::onCancel() {
 
   ENVOY_LOG(debug, "[S{}] dispatching to platform cancel stream", direct_stream_.stream_handle_);
   http_client_.stats().stream_cancel_.inc();
+  // Attempt to latch the latest stream info. This will be a no-op if the stream
+  // is already complete.
+  direct_stream_.saveFinalStreamIntel();
   bridge_callbacks_.on_cancel(streamIntel(), finalStreamIntel(), bridge_callbacks_.context);
 }
 
@@ -512,9 +515,6 @@ void Client::cancelStream(envoy_stream_t stream) {
     bool stream_was_open =
         getStream(stream, GetStreamFilters::ALLOW_ONLY_FOR_OPEN_STREAMS) != nullptr;
     ScopeTrackerScopeState scope(direct_stream.get(), scopeTracker());
-    // Attempt to latch the latest stream info. This will be a no-op if the stream
-    // is already complete.
-    direct_stream->saveFinalStreamIntel();
     removeStream(direct_stream->stream_handle_);
 
     ENVOY_LOG(debug, "[S{}] application cancelled stream", stream);
