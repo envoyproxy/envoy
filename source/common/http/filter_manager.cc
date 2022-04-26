e8bc18fda8ab9dc81fb48c9d1ef91f81d60dde87
GiantCroc <changran.wang@intel.com>
2022-04-26 21:22:12 +0800

    remove fix-added-trailers (#21001)
    fixes #20979
    
    Signed-off-by: giantcroc <changran.wang@intel.com>

--- source/common/http/filter_manager.cc
+++ source/common/http/filter_manager.cc
@@ -619,9 +619,6 @@ void FilterManager::decodeData(ActiveStreamDecoderFilter* filter, Buffer::Instan
   ScopeTrackerScopeState scope(&*this, dispatcher_);
   filter_manager_callbacks_.resetIdleTimer();
 
-  const bool fix_added_trailers =
-      Runtime::runtimeFeatureEnabled("envoy.reloadable_features.fix_added_trailers");
-
   // If a response is complete or a reset has been sent, filters do not care about further body
   // data. Just drop it.
   if (state_.local_complete_) {
@@ -708,9 +705,7 @@ void FilterManager::decodeData(ActiveStreamDecoderFilter* filter, Buffer::Instan
 
     if (!trailers_exists_at_start && filter_manager_callbacks_.requestTrailers() &&
         trailers_added_entry == decoder_filters_.end()) {
-      if (fix_added_trailers) {
-        end_stream = false;
-      }
+      end_stream = false;
       trailers_added_entry = entry;
     }
 
@@ -719,11 +714,7 @@ void FilterManager::decodeData(ActiveStreamDecoderFilter* filter, Buffer::Instan
       // Stop iteration IFF this is not the last filter. If it is the last filter, continue with
       // processing since we need to handle the case where a terminal filter wants to buffer, but
       // a previous filter has added trailers.
-      if (fix_added_trailers) {
-        break;
-      } else {
-        return;
-      }
+      break;
     }
   }
 
