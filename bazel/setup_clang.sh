76a923eb0981b4c27e2475782dd0f51094c54b6c
Lizan Zhou <lizan@tetrate.io>
2022-05-04 20:36:26 +0000

    image fix
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- bazel/setup_clang.sh
+++ bazel/setup_clang.sh
@@ -13,13 +13,11 @@ PATH="$("${LLVM_PREFIX}"/bin/llvm-config --bindir):${PATH}"
 export PATH
 
 RT_LIBRARY_PATH="$(llvm-config --libdir)/clang/$(llvm-config --version)/lib/$(llvm-config --host-target)"
-LD_LIBRARY_PATH="$(llvm-config --libdir)/$(llvm-config --host-target)"
 
 echo "# Generated file, do not edit. If you want to disable clang, just delete this file.
 build:clang --action_env='PATH=${PATH}'
 build:clang --action_env=CC=clang
 build:clang --action_env=CXX=clang++
-build:clang --action_env=LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
 build:clang --action_env='LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config'
 build:clang --repo_env='LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config'
 build:clang --linkopt='-L$(llvm-config --libdir)'
