4afdf50dfccb3af029a8477a322341e5d42b039c
Kuo-Chung Hsu <CuveeHsu@gmail.com>
2022-05-05 12:11:16 -0700

    thrift: EncoderFilter and BidirectionalFilter support  (#20816)
    EncoderFilter and BidirectionalFilter support in thrift_proxy filter.
    
    In this diff, the EncoderFilter and BidirectionalFilter support peek at the metadata
    and content for encoding and modify the metadata and content for encoding.
    Does not support:
    * pass through data separately for encode and decode, e.g.,  passThroughData in encoding but not passThroughData in decoding.
    * StopIteration in encoding (would intentionally crash)
    
    Risk Level: low
    Testing: Unit test
    Docs Changes: n/a
    Release Notes: n/a
    Fixes #20497
    
    Signed-off-by: kuochunghsu <kuochunghsu@pinterest.com>

--- source/extensions/filters/network/thrift_proxy/protocol_converter.h
+++ source/extensions/filters/network/thrift_proxy/protocol_converter.h
@@ -11,7 +11,7 @@ namespace NetworkFilters {
 namespace ThriftProxy {
 
 /**
- * ProtocolConverter is an abstract class that implements protocol-related methods on
+ * ProtocolConverter is an class that implements protocol-related methods on
  * DecoderEventHandler in terms of converting the decoded messages into a different protocol.
  */
 class ProtocolConverter : public virtual DecoderEventHandler {
@@ -29,6 +29,10 @@ public:
     buffer_->move(data);
     return FilterStatus::Continue;
   }
+
+  FilterStatus transportBegin(MessageMetadataSharedPtr) override { return FilterStatus::Continue; }
+
+  FilterStatus transportEnd() override { return FilterStatus::Continue; }
 
   FilterStatus messageBegin(MessageMetadataSharedPtr metadata) override {
     proto_->writeMessageBegin(*buffer_, *metadata);
@@ -132,6 +136,7 @@ private:
   Buffer::Instance* buffer_{};
 };
 
+using ProtocolConverterSharedPtr = std::shared_ptr<ProtocolConverter>;
 } // namespace ThriftProxy
 } // namespace NetworkFilters
 } // namespace Extensions
