b465561ca0c9ebbaa47cadb8565b4eb56a4b8ab7
Yanjun Xiang <yanjunxiang@google.com>
2022-05-16 23:49:33 +0000

    cancel unrelated change
    Signed-off-by: Yanjun Xiang <yanjunxiang@google.com>

--- source/common/filter/config_discovery_impl.cc
+++ source/common/filter/config_discovery_impl.cc
@@ -232,7 +232,7 @@ void FilterConfigProviderManagerImplBase::applyLastOrDefaultConfig(
 
 void FilterConfigProviderManagerImplBase::validateProtoConfigDefaultFactory(
     const bool null_default_factory, const std::string& filter_config_name,
-    absl::string_view type_url) const {
+    const std::string& type_url) const {
   if (null_default_factory) {
     throw EnvoyException(fmt::format("Error: cannot find filter factory {} for default filter "
                                      "configuration with type URL {}.",
