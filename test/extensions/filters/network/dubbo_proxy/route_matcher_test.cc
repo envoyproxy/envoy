e72ee4e984995509a1c8785c87405bc6c0c23bd7
zhaonan <zhaonan06@corp.netease.com>
2022-05-17 11:48:56 +0000

    fix test format
    Signed-off-by: zhaonan <zhaonan06@corp.netease.com>

--- test/extensions/filters/network/dubbo_proxy/route_matcher_test.cc
+++ test/extensions/filters/network/dubbo_proxy/route_matcher_test.cc
@@ -1046,7 +1046,7 @@ routes:
   v3.set_string_value("v3");
   HashedValue hv1(v1), hv2(v2), hv3(v3);
 
-  // match with mutiple set of weighted cluster metadta criteria defined
+  // match with multiple set of weighted cluster metadta criteria defined
   {
 
     RouteConstSharedPtr route = matcher.route(metadata, 0);
@@ -1075,7 +1075,7 @@ routes:
     EXPECT_EQ(nullptr, route->routeEntry()->metadataMatchCriteria());
   }
 
-  // weight cluster match with single metadta entry
+  // weight cluster match with single metadata entry
   {
     RouteConstSharedPtr route = matcher.route(metadata, 5001);
     EXPECT_NE(nullptr, route);
@@ -1143,7 +1143,7 @@ routes:
   v3.set_string_value("v3");
   HashedValue hv1(v1), hv2(v2), hv3(v3);
 
-  // match with weighted cluster metadta and route action metadata
+  // match with weighted cluster metadata and route action metadata
   {
 
     RouteConstSharedPtr route = matcher.route(metadata, 0);
