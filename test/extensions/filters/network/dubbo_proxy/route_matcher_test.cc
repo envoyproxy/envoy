1150383488db826d637682583192cecc21e8a955
zhaonan <zhaonan06@corp.netease.com>
2022-05-19 12:42:29 +0000

    fix test commit
    Signed-off-by: zhaonan <zhaonan06@corp.netease.com>

--- test/extensions/filters/network/dubbo_proxy/route_matcher_test.cc
+++ test/extensions/filters/network/dubbo_proxy/route_matcher_test.cc
@@ -942,7 +942,7 @@ routes:
       parseRouteConfigurationFromV2Yaml(yaml);
   auto invo = std::make_shared<RpcInvocationImpl>();
 
-  // Match with metadata
+  // Match with metadata.
   {
     MessageMetadata metadata;
     metadata.setInvocationInfo(invo);
@@ -976,7 +976,7 @@ routes:
     EXPECT_EQ(hv2, mmc[1]->value());
   }
 
-  // Match with no metadata
+  // Match with no metadata.
   {
     MessageMetadata metadata;
     metadata.setInvocationInfo(invo);
@@ -1046,7 +1046,7 @@ routes:
   v3.set_string_value("v3");
   HashedValue hv1(v1), hv2(v2), hv3(v3);
 
-  // Match with multiple set of weighted cluster metadata criteria defined
+  // Weight cluster match with Multiple metadata entries.
   {
 
     RouteConstSharedPtr route = matcher.route(metadata, 0);
@@ -1067,7 +1067,7 @@ routes:
     EXPECT_EQ(hv2, mmc[1]->value());
   }
 
-  // Match with weighted cluster with different metadata key
+  // Weighted cluster match with metadata key with "not.envoy.lb".
   {
     RouteConstSharedPtr route = matcher.route(metadata, 2001);
     EXPECT_NE(nullptr, route);
@@ -1075,7 +1075,7 @@ routes:
     EXPECT_EQ(nullptr, route->routeEntry()->metadataMatchCriteria());
   }
 
-  // Weight cluster match with single metadata entry
+  // Weight cluster match with single metadata entry.
   {
     RouteConstSharedPtr route = matcher.route(metadata, 5001);
     EXPECT_NE(nullptr, route);
@@ -1143,7 +1143,7 @@ routes:
   v3.set_string_value("v3");
   HashedValue hv1(v1), hv2(v2), hv3(v3);
 
-  // Match with weighted cluster metadata and route action metadata
+  // Match with weighted cluster metadata and route action metadata.
   {
 
     RouteConstSharedPtr route = matcher.route(metadata, 0);
@@ -1167,7 +1167,7 @@ routes:
     EXPECT_EQ(hv3, mmc[2]->value());
   }
 
-  // Match with just route action metadata
+  // Match with just route action metadata.
   {
     RouteConstSharedPtr route = matcher.route(metadata, 2001);
     EXPECT_NE(nullptr, route);
@@ -1186,7 +1186,7 @@ routes:
     EXPECT_EQ(hv2, mmc[1]->value());
   }
 
-  // Match with weighted cluster metadata and route action metadata merged
+  // Match with weighted cluster metadata and route action metadata (merged).
   {
     RouteConstSharedPtr route = matcher.route(metadata, 5001);
     EXPECT_NE(nullptr, route);
