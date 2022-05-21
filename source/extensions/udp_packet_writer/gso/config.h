61bad783c1a57b4f66a28c8ac3b4c91fed8c9e0e
Ryan Hamilton <rch@google.com>
2022-05-21 23:57:41 +0000

    compile time options
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- source/extensions/udp_packet_writer/gso/config.h
+++ source/extensions/udp_packet_writer/gso/config.h
@@ -4,7 +4,9 @@
 #include "envoy/network/udp_packet_writer_handler.h"
 #include "envoy/registry/registry.h"
 
+#ifdef ENVOY_ENABLE_QUIC
 #include "source/common/quic/udp_gso_batch_writer.h"
+#endif
 
 #if UDP_GSO_BATCH_WRITER_COMPILETIME_SUPPORT
 
@@ -16,7 +18,11 @@ public:
   std::string name() const override { return "envoy.udp_packet_writer.gso"; }
   Network::UdpPacketWriterFactoryPtr
   createUdpPacketWriterFactory(const envoy::config::core::v3::TypedExtensionConfig&) override {
+#ifdef ENVOY_ENABLE_QUIC
     return std::make_unique<UdpGsoBatchWriterFactory>();
+#else
+    return {};
+#endif
   }
   ProtobufTypes::MessagePtr createEmptyConfigProto() override {
     return std::make_unique<envoy::extensions::udp_packet_writer::v3::UdpGsoBatchWriterFactory>();
