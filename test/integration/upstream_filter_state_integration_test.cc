7d2c1860d26ee15492405cf48629560293a5a8cf
Jojy George Varghese <jojy_varghese@apple.com>
2022-05-22 08:54:58 -0700

    CI error fix
    Signed-off-by: Jojy George Varghese <jojy_varghese@apple.com>

--- test/integration/upstream_filter_state_integration_test.cc
+++ test/integration/upstream_filter_state_integration_test.cc
@@ -62,7 +62,7 @@ public:
       : PassthroughFactory(std::move(inner_factory)) {}
 
   Network::TransportSocketPtr
-  createTransportSocket(Network::TransportSocketOptionsConstSharedPtr options) const {
+  createTransportSocket(Network::TransportSocketOptionsConstSharedPtr options) const override {
     auto inner_socket = transport_socket_factory_->createTransportSocket(options);
     if (inner_socket == nullptr) {
       return nullptr;
