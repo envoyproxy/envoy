d90032d3b51e3137467a83a1c76bd6cbc0fede27
Sergii Tkachenko <sergiitk@google.com>
2022-05-27 17:30:52 -0700

    move common methods to py_library
    Signed-off-by: Sergii Tkachenko <sergiitk@google.com>

---
+++ tools/config_validation/common.py
@@ -0,0 +1,57 @@
+# Common tools for building and validating Envoy doc fragments.
+
+import pathlib
+
+import yaml
+
+from google.protobuf import descriptor_pb2
+from google.protobuf import descriptor_pool
+from google.protobuf import message_factory
+from google.protobuf import text_format
+
+from bazel_tools.tools.python.runfiles import runfiles
+
+
+class IgnoredKey(yaml.YAMLObject):
+    """Python support type for Envoy's config !ignore tag."""
+    yaml_tag = '!ignore'
+
+    def __init__(self, strval):
+        self.strval = strval
+
+    def __repr__(self):
+        return f'IgnoredKey({str})'
+
+    def __eq__(self, other):
+        return isinstance(other, IgnoredKey) and self.strval == other.strval
+
+    def __hash__(self):
+        return hash((self.yaml_tag, self.strval))
+
+    @classmethod
+    def from_yaml(cls, loader, node):
+        return IgnoredKey(node.value)
+
+    @classmethod
+    def to_yaml(cls, dumper, data):
+        return dumper.represent_scalar(cls.yaml_tag, data.strval)
+
+
+def load_pool(type_name, descriptor_path=None):
+    if not descriptor_path:
+        r = runfiles.Create()
+        descriptor_path = r.Rlocation(
+            'envoy/tools/type_whisperer/all_protos_with_ext_pb_text.pb_text')
+
+    file_desc_set = descriptor_pb2.FileDescriptorSet()
+    text_format.Parse(
+        pathlib.Path(descriptor_path).read_text(), file_desc_set,
+        allow_unknown_extension=True)
+
+    pool = descriptor_pool.DescriptorPool()
+    for f in file_desc_set.file:
+        pool.Add(f)
+    desc = pool.FindMessageTypeByName(type_name)
+    msg = message_factory.MessageFactory(pool=pool).GetPrototype(desc)()
+
+    return msg, pool
--- tools/config_validation/textproto2yaml.py
+++ tools/config_validation/textproto2yaml.py
@@ -3,118 +3,79 @@
 # Example usage:
 #
 # bazel run //tools/config_validation:textproto2yaml -- \
-#   envoy.config.bootstrap.v3.Bootstrap $PWD/configs/envoyproxy_io_proxy.textproto
+#   --as_fragment envoy.config.bootstrap.v3.Bootstrap path/to/message.textproto
 
+import argparse
 import json
 import pathlib
+import sys
 
 import yaml
 
-from google.protobuf import descriptor_pb2
-from google.protobuf import descriptor_pool
 from google.protobuf import json_format
-from google.protobuf import message_factory
 from google.protobuf import text_format
 
-from bazel_tools.tools.python.runfiles import runfiles
 
-import argparse
-
-
-class IgnoredKey(yaml.YAMLObject):
-    """Python support type for Envoy's config !ignore tag."""
-    yaml_tag = '!ignore'
-
-    def __init__(self, strval):
-        self.strval = strval
-
-    def __repr__(self):
-        return f'IgnoredKey({str})'
-
-    def __eq__(self, other):
-        return isinstance(other, IgnoredKey) and self.strval == other.strval
-
-    def __hash__(self):
-        return hash((self.yaml_tag, self.strval))
-
-    @classmethod
-    def from_yaml(cls, loader, node):
-        return IgnoredKey(node.value)
-
-    @classmethod
-    def to_yaml(cls, dumper, data):
-        return dumper.represent_scalar(cls.yaml_tag, data.strval)
+from common import load_pool
+from common import IgnoredKey
 
 
-def load_pool(type_name, descriptor_path=None):
-    if not descriptor_path:
-        r = runfiles.Create()
-        descriptor_path = r.Rlocation(
-            'envoy/tools/type_whisperer/all_protos_with_ext_pb_text.pb_text')
-
-    file_desc_set = descriptor_pb2.FileDescriptorSet()
-    text_format.Parse(
-        pathlib.Path(descriptor_path).read_text(), file_desc_set, allow_unknown_extension=True)
-
-    pool = descriptor_pool.DescriptorPool()
-    for f in file_desc_set.file:
-        pool.Add(f)
-    desc = pool.FindMessageTypeByName(type_name)
-    msg = message_factory.MessageFactory(pool=pool).GetPrototype(desc)()
-
-    return msg, pool
-
-
-def textproto_to_yaml(in_textproto, pool):
+def proto_message_to_yaml(proto_message, pool):
     yaml.SafeLoader.add_constructor('!ignore', IgnoredKey.from_yaml)
     yaml.SafeDumper.add_multi_representer(IgnoredKey, IgnoredKey.to_yaml)
     json_msg = json_format.MessageToJson(
-      in_textproto,
+      proto_message,
       descriptor_pool=pool,
       preserving_proto_field_name=True)
-    # print(in_textproto)
-    # print("----")
-    # print(json_msg)
-    # print("----")
     return yaml.dump(json.loads(json_msg), sort_keys=False)
 
 
-def parse_args():
+def textproto_file_to_yaml(textproto_path, message_type, *,
+                           descriptor_path=None) -> str:
+    msg, pool = load_pool(message_type, descriptor_path=descriptor_path)
+    content = pathlib.Path(textproto_path).read_text()
+    proto_message = text_format.Parse(content, msg, descriptor_pool=pool)
+    return proto_message_to_yaml(proto_message, pool)
+
+
+def yaml_to_fragment(yaml_message, message_type) -> str:
+    fragment = [
+        '// .. validated-code-block:: yaml',
+        f'//   :type-name: {message_type}',
+        '//',
+    ]
+    for line in yaml_message.splitlines():
+        fragment.append(f'//   {line}')
+    fragment.append('//')
+    return '\n'.join(fragment) + '\n'
+
+
+def parse_args(args):
     parser = argparse.ArgumentParser(description='Print textproto as YAML.')
     parser.add_argument(
         'message_type',
-        help='a string providing the type name, e.g. envoy.config.bootstrap.v3.Bootstrap.')
-    parser.add_argument('textproto_path', nargs='?', help='Path to a .textproto configuration fragment.')
-    parser.add_argument('--descriptor_path', nargs='?', help='Path to a protobuf descriptor file.')
-    parser.add_argument('--as_fragment', action=argparse.BooleanOptionalAction, help='Print as a fragment.')
-    return parser.parse_args()
+        help=('a string providing the type name, '
+              'e.g. envoy.config.bootstrap.v3.Bootstrap.'))
+    parser.add_argument('textproto_path', nargs='?',
+                        help='Path to a .textproto configuration fragment.')
+    parser.add_argument('--descriptor_path', nargs='?',
+                        help='Path to a protobuf descriptor file.')
+    parser.add_argument('--as_fragment', action=argparse.BooleanOptionalAction,
+                        help='Print as a fragment.')
+    return parser.parse_args(args)
 
 
-def main(message_type, descriptor_path, content, *, as_fragment=False):
-  msg, pool = load_pool(message_type, descriptor_path=descriptor_path)
-  in_textproto = text_format.Parse(content, msg, descriptor_pool=pool)
-  out_yaml: str = textproto_to_yaml(in_textproto, pool)
-
-  if not as_fragment:
-    return out_yaml.rstrip('\n')
-
-  fragment = [
-    '// .. validated-code-block:: yaml',
-    f'//   :type-name: {message_type}',
-    '//',
-  ]
-  for line in out_yaml.splitlines():
-    fragment.append(f'//   {line}')
-
-  fragment.append('//')
-  return '\n'.join(fragment)
+def main(*args) -> int:
+    parsed_args = parse_args(args)
+    yaml_message = textproto_file_to_yaml(
+        parsed_args.textproto_path,
+        parsed_args.message_type,
+        descriptor_path=parsed_args.descriptor_path)
+    out = yaml_message if not parsed_args.as_fragment else yaml_to_fragment(
+        yaml_message, parsed_args.message_type)
+    sys.stdout.write(out)
+    return 0
 
 
-if __name__ == '__main__':
-    parsed_args = parse_args()
-    out = main(parsed_args.message_type,
-         parsed_args.descriptor_path,
-         pathlib.Path(parsed_args.textproto_path).read_text(),
-         as_fragment=parsed_args.as_fragment)
-
-    print(out,)
+if __name__ == "__main__":
+    sys.exit(main(*sys.argv[1:]))
