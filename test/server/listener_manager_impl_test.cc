c9f993478f40b8c4c1b1a0d6cd03f1ec7acddbf4
Loong Dai <loong.dai@intel.com>
2022-06-16 01:11:06 +0000

    add a test
    Signed-off-by: Loong Dai <loong.dai@intel.com>

--- test/server/listener_manager_impl_test.cc
+++ test/server/listener_manager_impl_test.cc
@@ -6683,6 +6683,20 @@ TEST(ListenerEnableReusePortTest, All) {
   }
 }
 
+TEST_P(ListenerManagerImplWithRealFiltersTest, InvalidExtendConnectionBalanceConfig) {
+  auto listener = createIPv4Listener("TCPListener");
+  auto* connection_balance_config = listener.mutable_connection_balance_config();
+  auto* extend_balance_config = connection_balance_config->mutable_extend_balance();
+  extend_balance_config->set_name("envoy.network.connection_balance.test");
+  extend_balance_config->mutable_typed_config()->set_type_url(
+      "type.googleapis.com/google.protobuf.Struct");
+
+  EXPECT_THROW_WITH_MESSAGE(
+      new ListenerImpl(listener, "version", *manager_, "foo", true, false,
+                       /*hash=*/static_cast<uint64_t>(0), 1),
+      EnvoyException, "Didn't find a registered implementation for type: 'google.protobuf.Struct'");
+}
+
 INSTANTIATE_TEST_SUITE_P(Matcher, ListenerManagerImplTest, ::testing::Values(false));
 INSTANTIATE_TEST_SUITE_P(Matcher, ListenerManagerImplWithRealFiltersTest,
                          ::testing::Values(false, true));
