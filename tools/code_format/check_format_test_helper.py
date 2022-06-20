b7069d3316eb35258268df29415b8cfcf3cab5da
Ryan Northey <ryan@synca.io>
2022-06-20 10:33:26 +0100

    ci: Fix CLANG_FORMAT version
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tools/code_format/check_format_test_helper.py
+++ tools/code_format/check_format_test_helper.py
@@ -121,7 +121,7 @@ def check_tool_not_found_error():
     # Temporarily change PATH to test the error about lack of external tools.
     oldPath = os.environ["PATH"]
     os.environ["PATH"] = "/sbin:/usr/sbin"
-    clang_format = os.getenv("CLANG_FORMAT", "clang-format-11")
+    clang_format = os.getenv("CLANG_FORMAT", "clang-format")
     # If CLANG_FORMAT points directly to the binary, skip this test.
     if os.path.isfile(clang_format) and os.access(clang_format, os.X_OK):
         os.environ["PATH"] = oldPath
