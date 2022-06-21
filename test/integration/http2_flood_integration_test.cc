f0c3e983a47773420b748620a2e03cfea7d53eef
Alyssa Wilk <alyssar@chromium.org>
2022-06-21 16:14:35 -0400

    test: fixing waitforaccesslogs
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/integration/http2_flood_integration_test.cc
+++ test/integration/http2_flood_integration_test.cc
@@ -1158,8 +1158,8 @@ TEST_P(Http2FloodMitigationTest, ZerolenHeaderAllowed) {
   EXPECT_EQ(1, test_server_->counter("http2.rx_messaging_error")->value());
   EXPECT_EQ(0,
             test_server_->counter("http.config_test.downstream_cx_delayed_close_timeout")->value());
-  EXPECT_THAT(waitForAccessLog(access_log_name_), HasSubstr("http2.invalid.header.field"));
   // expect Downstream Protocol Error
+  EXPECT_THAT(waitForAccessLog(access_log_name_), HasSubstr("http2.invalid.header.field"));
   EXPECT_THAT(waitForAccessLog(access_log_name_), HasSubstr("DPE"));
 }
 
