9f15dc1bdc196a78f03d1343d57f40a47c72551d
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-07-06 16:01:15 +0700

    Improved verify.sh logic
    Signed-off-by: Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>

--- examples/kafka-broker/verify.sh
+++ examples/kafka-broker/verify.sh
@@ -5,13 +5,49 @@ export NAME=kafka-broker
 # shellcheck source=examples/verify-common.sh
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
-run_log "Waiting for Zookeeper and Kafka started."
-sleep 40
+run_log "Waiting for Kafka on 9092."
+while true; do if docker-compose logs -f kafka | grep -q 'started (kafka.server.KafkaServer)'; then 
+    break; 
+else 
+    echo waiting ...; sleep 1; 
+fi; done
+run_log "Kafka launched."
 
+# Topic
 TOPIC="envoy.kafka.broker"
 run_log "Creating topic."
-docker exec kafka kafka-topics --bootstrap-server localhost:19092 --create --topic $TOPIC quickstart-events
+docker-compose exec kafka kafka-topics --bootstrap-server localhost:19092 --create --topic $TOPIC quickstart-events
 sleep 5
 
-run_log "Producer is sending messages. Please type..."
-docker exec -it kafka kafka-console-producer --request-required-acks 1 --broker-list localhost:19092 --topic $TOPIC
+run_log "Checking topic."
+topic_created=$(docker-compose exec kafka kafka-topics --bootstrap-server localhost:19092 --list)
+if [[ "$topic_created" == "$TOPIC" ]]; then
+    run_log "Checked topic $topic_created succesfully."
+else
+    run_log "Checked topic $topic_created failed."
+    exit 1
+fi
+
+# Initialize message for producer
+MESSAGE="Welcome to Envoy and Kafka Broker filter!"
+
+# Producer
+run_log "Create Producer and send message."
+docker-compose exec kafka /bin/bash -c "echo $MESSAGE >> message.txt & kafka-console-producer --request-required-acks 1 --broker-list localhost:19092 --topic envoy.kafka.broker < message.txt"
+run_log "Sent messages succesfully."
+
+# Consumer
+run_log "Create Consumer."
+# `--timeout-ms 10000` lets the consumer exit after receiving the message. And
+# ```
+#   ERROR Error processing message, terminating consumer process:  (kafka.tools.ConsoleConsumer$)
+#   org.apache.kafka.common.errors.TimeoutException
+#   Processed a total of 1 messages
+# ```
+# is the expected error
+message_received=$(docker-compose exec kafka kafka-console-consumer --bootstrap-server localhost:19092 --topic $TOPIC --from-beginning --timeout-ms 10000)
+if [[ "$message_received" == "$MESSAGE" ]]; then
+    run_log "Received message succesfully."
+else
+    run_log "Received message failed."
+fi
