77ba1e0eb8fe358428f3719b6ecbd0fcd92e4356
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-07-08 09:59:46 +0700

    Handle inheriting the STDIO streams and has not yet exited
    Signed-off-by: Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>

--- examples/kafka-broker/verify.sh
+++ examples/kafka-broker/verify.sh
@@ -5,21 +5,21 @@ export NAME=kafka-broker
 # shellcheck source=examples/verify-common.sh
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
-run_log "Waiting for Kafka on port 9092."
+run_log "Waiting for Kafka on port 9092"
 wait_for 120 sh -c "docker-compose logs -f kafka | grep -q 'started (kafka.server.KafkaServer)'"
-run_log "Kafka launched."
+run_log "Kafka launched"
 
 # Topic
 TOPIC="envoy.kafka.broker"
-run_log "Creating topic."
+run_log "Creating topic"
 docker-compose exec kafka kafka-topics --bootstrap-server localhost:19092 --create --topic $TOPIC quickstart-events
 
-run_log "Checking topic."
+run_log "Checking topic"
 topic_created=$(docker-compose exec kafka kafka-topics --bootstrap-server localhost:19092 --list)
 if [[ "$topic_created" == "$TOPIC" ]]; then
-    run_log "Checked topic $topic_created succesfully."
+    run_log "Checked topic $topic_created succesfully"
 else
-    run_log "Checked topic $topic_created failed."
+    run_log "Checked topic $topic_created failed"
     exit 1
 fi
 
@@ -27,27 +27,29 @@ fi
 MESSAGE="Welcome to Envoy and Kafka Broker filter!"
 
 # Producer
-run_log "Create Producer and send message."
+run_log "Create Producer and send message"
 docker-compose exec kafka /bin/bash -c "echo $MESSAGE >> message.txt & kafka-console-producer --request-required-acks 1 --broker-list localhost:19092 --topic envoy.kafka.broker < message.txt"
-run_log "Sent messages succesfully."
+run_log "Sent messages succesfully"
 
 # Consumer
-run_log "Create Consumer."
+run_log "Create Consumer"
 
 read_message() {
-    run_log "Reading message."
+    run_log "Reading message"
     docker-compose exec kafka kafka-console-consumer --bootstrap-server localhost:19092 --topic $TOPIC --from-beginning >> consumer.txt
 }
 
 check_message() {
     while true; do
     if [[ $(< consumer.txt) == "$MESSAGE" ]]; then
-        run_log "Received message succesfully."
+        run_log "Received message succesfully"
         rm consumer.txt # clean up consumer.txt
-        cleanup # try to stop kafka-console-consumer & docker services
-        exit 1
+        run_log "Bring down the kafka"
+        docker-compose stop kafka
+        wait_for 10 sh -c "docker-compose ps kafka | grep -v unhealthy"
+        break
     else
-        run_log "Checking message."
+        run_log "Checking message"
         sleep 1
     fi;
     done
