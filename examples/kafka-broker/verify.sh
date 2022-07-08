fba88ceb07f3ee896ec3ed0973cfb6d477a5b948
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-07-08 11:20:28 +0700

    Add -T flag for docker-compose exec
    Signed-off-by: Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>

--- examples/kafka-broker/verify.sh
+++ examples/kafka-broker/verify.sh
@@ -12,10 +12,10 @@ run_log "Kafka launched"
 # Topic
 TOPIC="envoy.kafka.broker"
 run_log "Creating topic"
-docker-compose exec kafka kafka-topics --bootstrap-server localhost:19092 --create --topic $TOPIC quickstart-events
+docker-compose exec -T kafka kafka-topics --bootstrap-server localhost:19092 --create --topic $TOPIC quickstart-events
 
 run_log "Checking topic"
-topic_created=$(docker-compose exec kafka kafka-topics --bootstrap-server localhost:19092 --list)
+topic_created=$(docker-compose exec -T kafka kafka-topics --bootstrap-server localhost:19092 --list)
 if [[ "$topic_created" == "$TOPIC" ]]; then
     run_log "Checked topic $topic_created succesfully"
 else
@@ -28,7 +28,7 @@ MESSAGE="Welcome to Envoy and Kafka Broker filter!"
 
 # Producer
 run_log "Create Producer and send message"
-docker-compose exec kafka /bin/bash -c "echo $MESSAGE >> message.txt & kafka-console-producer --request-required-acks 1 --broker-list localhost:19092 --topic envoy.kafka.broker < message.txt"
+docker-compose exec -T kafka /bin/bash -c "echo $MESSAGE >> message.txt & kafka-console-producer --request-required-acks 1 --broker-list localhost:19092 --topic envoy.kafka.broker < message.txt"
 run_log "Sent messages succesfully"
 
 # Consumer
@@ -36,7 +36,7 @@ run_log "Create Consumer"
 
 read_message() {
     run_log "Reading message"
-    docker-compose exec kafka kafka-console-consumer --bootstrap-server localhost:19092 --topic $TOPIC --from-beginning >> consumer.txt
+    docker-compose exec -T kafka kafka-console-consumer --bootstrap-server localhost:19092 --topic $TOPIC --from-beginning >> consumer.txt
 }
 
 check_message() {
@@ -47,6 +47,7 @@ check_message() {
         run_log "Bring down the kafka"
         docker-compose stop kafka
         wait_for 10 sh -c "docker-compose ps kafka | grep -v unhealthy"
+        run_log "Kafka was down"
         break
     else
         run_log "Checking message"
