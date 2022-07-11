716b2cd9ca6256debd08cec2f2d6a39510a449ca
He Jie Xu <hejie.xu@intel.com>
2022-07-11 06:01:39 +0000

    fix format
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- tools/socket_passing.py
+++ tools/socket_passing.py
@@ -42,7 +42,8 @@ def generate_new_config(original_yaml, admin_address, updated_json):
             index = 0
             for discovered in discovered_listeners['listener_statuses']:
                 replaced = False
-                addresses = discovered['additional_local_addresses'] + [discovered['local_address']] if discovered.get('additional_local_addresses') else [discovered['local_address']]
+                addresses = (discovered['additional_local_addresses'] + [discovered['local_address']]
+                    if discovered.get('additional_local_addresses') else [discovered['local_address']])
                 for local_address in  addresses:
                     if 'pipe' in local_address:
                         path = local_address['pipe']['path']
