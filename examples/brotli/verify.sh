ac0e6960314d58614883186492682f8e803d43db
Ryan Northey <ryan@synca.io>
2022-07-12 06:17:47 +0100

    examples: Improve waits/fix flakes
    Fix #22113
    
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- examples/brotli/verify.sh
+++ examples/brotli/verify.sh
@@ -8,7 +8,10 @@ export PORT_STATS1="${BROTLI_PORT_PROXY:-10202}"
 # shellcheck source=examples/verify-common.sh
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
-sleep 5
+wait_for 10 bash -c "\
+         responds_with_header \
+         'content-encoding: br' \
+         https://localhost:${PORT_PROXY}/file.json -ki -H 'Accept-Encoding: br'"
 
 run_log "Test service: localhost:${PORT_PROXY}/file.json with compression"
 responds_with_header \
