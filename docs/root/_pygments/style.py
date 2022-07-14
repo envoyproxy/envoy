59c7097ff3693845c105d3dd1458d9e704c0e23f
Ryan Northey <ryan@synca.io>
2022-07-14 15:48:23 +0100

    darken-more
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- docs/root/_pygments/style.py
+++ docs/root/_pygments/style.py
@@ -2,7 +2,7 @@ from pygments.styles import get_style_by_name
 from pygments.token import Generic
 
 ENVOY_STYLES = {
-    Generic.Output: "#888888",
+    Generic.Output: "#777777",
 }
 
 TangoStyle = get_style_by_name("tango")
