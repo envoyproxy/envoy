f0582d4b9d7e94015c9bd72fc404a8cae49f7f31
Xie Zhihao <zhihao.xie@intel.com>
2022-07-18 10:22:30 +0800

    test: replace EXPECT_EQ with EXPECT_LT
    Signed-off-by: Xie Zhihao <zhihao.xie@intel.com>

--- contrib/hyperscan/matching/input_matchers/test/matcher_test.cc
+++ contrib/hyperscan/matching/input_matchers/test/matcher_test.cc
@@ -50,9 +50,9 @@ TEST(ThreadLocalTest, RaceScratchCreation) {
 
 // Verify that comparing works correctly for bounds.
 TEST(BoundTest, Compare) {
-  EXPECT_TRUE(Bound(1, 1) < Bound(2, 1));
-  EXPECT_TRUE(Bound(1, 2) < Bound(2, 1));
-  EXPECT_TRUE(Bound(2, 2) < Bound(2, 1));
+  EXPECT_LT(Bound(1, 1), Bound(2, 1));
+  EXPECT_LT(Bound(1, 2), Bound(2, 1));
+  EXPECT_LT(Bound(2, 2), Bound(2, 1));
 }
 
 class MatcherTest : public ::testing::Test {
