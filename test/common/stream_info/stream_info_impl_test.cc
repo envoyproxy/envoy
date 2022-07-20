522fe0f8a29e3aaedbb8418ce3fd4828c9fa1525
Xuyang Tao <taoxuy@google.com>
2022-07-19 20:42:25 -0700

    fix test
    Signed-off-by: Xuyang Tao <taoxuy@google.com>

--- test/common/stream_info/stream_info_impl_test.cc
+++ test/common/stream_info/stream_info_impl_test.cc
@@ -228,7 +228,7 @@ TEST_F(StreamInfoImplTest, SetFrom) {
 #ifdef __clang__
 #if defined(__linux__)
 #if defined(__has_feature) && !(__has_feature(thread_sanitizer))
-  ASSERT_TRUE(sizeof(s1) == 760 || sizeof(s1) == 776 || sizeof(s1) == 800)
+  ASSERT_TRUE(sizeof(s1) == 760 || sizeof(s1) == 776 || sizeof(s1) == 791 || sizeof(s1) == 800)
       << "If adding fields to StreamInfoImpl, please check to see if you "
          "need to add them to setFromForRecreateStream! Current size "
       << sizeof(s1);
