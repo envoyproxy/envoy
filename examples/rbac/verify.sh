5b03c9aa29d571a2bf0051349e84653021a3dcf0
Xie Zhihao <zhihao.xie@intel.com>
2022-07-29 14:58:32 +0800

    examples: fix verifing script
    Signed-off-by: Xie Zhihao <zhihao.xie@intel.com>

--- examples/rbac/verify.sh
+++ examples/rbac/verify.sh
@@ -8,10 +8,10 @@ export PORT_ADMIN="${RBAC_PORT_ADMIN:-11811}"
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
 run_log "Test upstream with access denied response"
-responds_with "RBAC: access denied" "http://localhost${PORT_PROXY}"
+responds_with "RBAC: access denied" "http://localhost:${PORT_PROXY}"
 
 run_log "Test authorized upstream response"
-responds_without "RBAC: access denied" "http://localhost${PORT_PROXY}" -H "Referer: https://www.envoyproxy.io/docs/envoy"
+responds_without "RBAC: access denied" "http://localhost:${PORT_PROXY}" -H "Referer: https://www.envoyproxy.io/docs/envoy"
 
 run_log "Check admin stats"
-responds_with rbac "http://localhost:${PORT_PROXY}/stats?fitler=rbac"
+responds_with rbac "http://localhost:${PORT_ADMIN}/stats?fitler=rbac"
