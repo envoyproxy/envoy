6430d28b520291e829d048ba2d550befd3dc5854
Xuyang Tao <taoxuy@google.com>
2022-07-28 22:03:10 -0700

    doc
    Signed-off-by: Xuyang Tao <taoxuy@google.com>

--- envoy/http/filter.h
+++ envoy/http/filter.h
@@ -685,6 +685,7 @@ public:
   struct LocalReplyData {
     // The error code which (barring reset) will be sent to the client.
     Http::Code code_;
+    // The gRPC status set in local reply.
     absl::optional<Grpc::Status::GrpcStatus> grpc_status_;
     // The details of why a local reply is being sent.
     absl::string_view details_;
