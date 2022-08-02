e6c81a0975dd31f9eedcc34d4c91a34be53050b3
Joshua Marantz <jmarantz@google.com>
2022-08-02 15:50:32 -0400

    indicate which endpoints require POST in the help text.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/server/admin/admin.cc
+++ source/server/admin/admin.cc
@@ -394,7 +394,8 @@ void AdminImpl::getHelp(Buffer::Instance& response) {
 
   // Prefix order is used during searching, but for printing do them in alpha order.
   for (const UrlHandler* handler : sortedHandlers()) {
-    response.add(fmt::format("  {}: {}\n", handler->prefix_, handler->help_text_));
+    const absl::string_view method = handler->mutates_server_state_ ? " (POST)" : "";
+    response.add(fmt::format("  {}{}: {}\n", handler->prefix_, method, handler->help_text_));
     for (const ParamDescriptor& param : handler->params_) {
       response.add(fmt::format("      {}: {}", param.id_, param.help_));
       if (param.type_ == ParamDescriptor::Type::Enum) {
