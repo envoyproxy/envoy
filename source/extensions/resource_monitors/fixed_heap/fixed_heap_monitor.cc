bbcc663eeefc4332a6ed147c04ee7ea193469853
Alan Diaz <diazalan@google.com>
2022-08-03 20:04:57 +0000

    Added detection for free pages in TCMalloc
    Signed-off-by: Alan Diaz <diazalan@google.com>

--- source/extensions/resource_monitors/fixed_heap/fixed_heap_monitor.cc
+++ source/extensions/resource_monitors/fixed_heap/fixed_heap_monitor.cc
@@ -4,6 +4,7 @@
 
 #include "source/common/common/assert.h"
 #include "source/common/memory/stats.h"
+#include "source/common/runtime/runtime_features.h"
 
 namespace Envoy {
 namespace Extensions {
@@ -14,6 +15,8 @@ uint64_t MemoryStatsReader::reservedHeapBytes() { return Memory::Stats::totalCur
 
 uint64_t MemoryStatsReader::unmappedHeapBytes() { return Memory::Stats::totalPageHeapUnmapped(); }
 
+uint64_t MemoryStatsReader::freeMappedHeapBytes() { return Memory::Stats::totalPageHeapFree(); }
+
 FixedHeapMonitor::FixedHeapMonitor(
     const envoy::extensions::resource_monitors::fixed_heap::v3::FixedHeapConfig& config,
     std::unique_ptr<MemoryStatsReader> stats)
@@ -22,10 +25,25 @@ FixedHeapMonitor::FixedHeapMonitor(
 }
 
 void FixedHeapMonitor::updateResourceUsage(Server::ResourceUpdateCallbacks& callbacks) {
-  const size_t physical = stats_->reservedHeapBytes();
-  const size_t unmapped = stats_->unmappedHeapBytes();
-  ASSERT(physical >= unmapped);
-  const size_t used = physical - unmapped;
+
+  size_t used;
+
+  if(!Runtime::runtimeFeatureEnabled("envoy.reloadable_features.do_not_count_mapped_pages_as_free"))
+  {
+    const size_t physical = stats_->reservedHeapBytes();
+    const size_t unmapped = stats_->unmappedHeapBytes();
+    const size_t freeMapped = stats_->freeMappedHeapBytes();
+    ASSERT(physical >= (unmapped + freeMapped));
+    used = physical - unmapped - freeMapped;
+  }
+  else
+  {
+    const size_t physical = stats_->reservedHeapBytes();
+    const size_t unmapped = stats_->unmappedHeapBytes();
+    ASSERT(physical >= unmapped);
+    used = physical - unmapped;
+  }
+
 
   Server::ResourceUsage usage;
   usage.resource_pressure_ = used / static_cast<double>(max_heap_);
