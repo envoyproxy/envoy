b7014b3725f942e9992dfe6e9d5d8f907c545a81
Alan Diaz <diazalan@google.com>
2022-08-04 13:00:08 +0000

    Added new tests for changes in fixed_heap_monitor
    Signed-off-by: Alan Diaz <diazalan@google.com>

--- test/extensions/resource_monitors/fixed_heap/fixed_heap_monitor_test.cc
+++ test/extensions/resource_monitors/fixed_heap/fixed_heap_monitor_test.cc
@@ -5,6 +5,7 @@
 #include "absl/types/optional.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "test/test_common/test_runtime.h"
 
 namespace Envoy {
 namespace Extensions {
@@ -18,6 +19,7 @@ public:
 
   MOCK_METHOD(uint64_t, reservedHeapBytes, ());
   MOCK_METHOD(uint64_t, unmappedHeapBytes, ());
+  MOCK_METHOD(uint64_t, freeMappedHeapBytes, ());
 };
 
 class ResourcePressure : public Server::ResourceUpdateCallbacks {
@@ -39,6 +41,30 @@ private:
 };
 
 TEST(FixedHeapMonitorTest, ComputesCorrectUsage) {
+
+  TestScopedRuntime scoped_runtime;
+  scoped_runtime.mergeValues({{"envoy.reloadable_features.do_not_count_mapped_pages_as_free", "false"}});
+
+  envoy::extensions::resource_monitors::fixed_heap::v3::FixedHeapConfig config;
+  config.set_max_heap_size_bytes(1000);
+  auto stats_reader = std::make_unique<MockMemoryStatsReader>();
+  EXPECT_CALL(*stats_reader, reservedHeapBytes()).WillOnce(testing::Return(800));
+  EXPECT_CALL(*stats_reader, unmappedHeapBytes()).WillOnce(testing::Return(100));
+  EXPECT_CALL(*stats_reader, freeMappedHeapBytes()).WillOnce(testing::Return(200));
+  std::unique_ptr<FixedHeapMonitor> monitor(new FixedHeapMonitor(config, std::move(stats_reader)));
+
+  ResourcePressure resource;
+  monitor->updateResourceUsage(resource);
+  EXPECT_TRUE(resource.hasPressure());
+  EXPECT_FALSE(resource.hasError());
+  EXPECT_EQ(resource.pressure(), 0.5);
+}
+
+TEST(FixedHeapMonitorTest, LegacyComputesCorrectUsage) {
+
+  TestScopedRuntime scoped_runtime;
+  scoped_runtime.mergeValues({{"envoy.reloadable_features.do_not_count_mapped_pages_as_free", "true"}});
+
   envoy::extensions::resource_monitors::fixed_heap::v3::FixedHeapConfig config;
   config.set_max_heap_size_bytes(1000);
   auto stats_reader = std::make_unique<MockMemoryStatsReader>();
@@ -54,6 +80,30 @@ TEST(FixedHeapMonitorTest, ComputesCorrectUsage) {
 }
 
 TEST(FixedHeapMonitorTest, ComputeUsageWithRealMemoryStats) {
+
+  TestScopedRuntime scoped_runtime;
+  scoped_runtime.mergeValues({{"envoy.reloadable_features.do_not_count_mapped_pages_as_free", "false"}});
+
+  envoy::extensions::resource_monitors::fixed_heap::v3::FixedHeapConfig config;
+  uint64_t max_heap = 1024 * 1024 * 1024;
+  config.set_max_heap_size_bytes(max_heap);
+  auto stats_reader = std::make_unique<MemoryStatsReader>();
+  const double expected_usage =
+      (stats_reader->reservedHeapBytes() -
+       stats_reader->unmappedHeapBytes() - stats_reader->freeMappedHeapBytes()) /
+      static_cast<double>(max_heap);
+  std::unique_ptr<FixedHeapMonitor> monitor(new FixedHeapMonitor(config, std::move(stats_reader)));
+
+  ResourcePressure resource;
+  monitor->updateResourceUsage(resource);
+  EXPECT_NEAR(resource.pressure(), expected_usage, 0.0005);
+}
+
+TEST(FixedHeapMonitorTest, LegacyComputeUsageWithRealMemoryStats) {
+
+  TestScopedRuntime scoped_runtime;
+  scoped_runtime.mergeValues({{"envoy.reloadable_features.do_not_count_mapped_pages_as_free", "true"}});
+
   envoy::extensions::resource_monitors::fixed_heap::v3::FixedHeapConfig config;
   uint64_t max_heap = 1024 * 1024 * 1024;
   config.set_max_heap_size_bytes(max_heap);
