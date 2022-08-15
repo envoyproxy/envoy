06cb8bedb22e68adc977758ee7308a21aea885d0
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-08-15 21:35:26 +0700

    Add flag --max-messages
    Signed-off-by: Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>

--- examples/kafka-broker/verify.sh
+++ examples/kafka-broker/verify.sh
@@ -20,37 +20,11 @@ run_log "Check the Kafka topic"
 docker-compose exec -T kafka kafka-topics --bootstrap-server localhost:19092 --list | grep $TOPIC
 
 # Producer
-run_log "Create a producer and send the message"
+run_log "Send a message using the Kafka producer"
 docker-compose exec -T kafka /bin/bash -c " \
     echo $MESSAGE >> message.txt \
     && kafka-console-producer --request-required-acks 1 --broker-list localhost:19092 --topic $TOPIC < message.txt"
-run_log "Sent messages succesfully"
 
 # Consumer
-run_log "Create a consumer"
-
-read_message() {
-    run_log "Reading the message"
-    docker-compose exec -T kafka kafka-console-consumer --bootstrap-server localhost:19092 --topic $TOPIC --from-beginning >> consumer.txt
-}
-
-check_message() {
-    while true; do
-    if [[ $(< consumer.txt) == "$MESSAGE" ]]; then
-        run_log "Received the message succesfully"
-        rm consumer.txt # clean up consumer.txt
-        run_log "Bring down the kafka"
-        docker-compose stop kafka
-        wait_for 10 sh -c "docker-compose ps kafka | grep -v unhealthy"
-        run_log "Kafka was down"
-        break
-    else
-        run_log "Checking the message"
-        sleep 1
-    fi;
-    done
-}
-
-read_message &
-check_message &
-wait
+run_log "Receive a message using the Kafka consumer"
+docker-compose exec -T kafka kafka-console-consumer --bootstrap-server localhost:19092 --topic $TOPIC --from-beginning --max-messages 1
