c6d689a8a326bc95447692039f8e3f4508253cb8
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-08-15 22:07:35 +0700

    Add kafka-client
    Signed-off-by: Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>

--- examples/kafka-broker/verify.sh
+++ examples/kafka-broker/verify.sh
@@ -14,17 +14,17 @@ TOPIC="envoy-kafka-broker"
 MESSAGE="Welcome to Envoy and Kafka Broker filter!"
 
 run_log "Create a Kafka topic"
-docker-compose exec -T kafka kafka-topics --bootstrap-server localhost:19092 --create --topic $TOPIC quickstart-events
+docker-compose exec -T kafka-server kafka-topics --bootstrap-server localhost:19092 --create --topic $TOPIC quickstart-events
 
 run_log "Check the Kafka topic"
-docker-compose exec -T kafka kafka-topics --bootstrap-server localhost:19092 --list | grep $TOPIC
+docker-compose exec -T kafka-server kafka-topics --bootstrap-server localhost:19092 --list | grep $TOPIC
 
 # Producer
 run_log "Send a message using the Kafka producer"
-docker-compose exec -T kafka /bin/bash -c " \
+docker-compose exec -T kafka-server /bin/bash -c " \
     echo $MESSAGE >> message.txt \
     && kafka-console-producer --request-required-acks 1 --broker-list localhost:19092 --topic $TOPIC < message.txt"
 
 # Consumer
 run_log "Receive a message using the Kafka consumer"
-docker-compose exec -T kafka kafka-console-consumer --bootstrap-server localhost:19092 --topic $TOPIC --from-beginning --max-messages 1
+docker-compose exec -T kafka-server kafka-console-consumer --bootstrap-server localhost:19092 --topic $TOPIC --from-beginning --max-messages 1
