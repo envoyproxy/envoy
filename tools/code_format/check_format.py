516a086b0e03f5a0d32fad7cd13419070aba7e9b
silverstar195 <seanmaloney@google.com>
2022-08-16 16:53:19 +0000

    Unformat
    Signed-off-by: silverstar195 <seanmaloney@google.com>

--- tools/code_format/check_format.py
+++ tools/code_format/check_format.py
@@ -7,21 +7,17 @@ import multiprocessing
 import os
 import pathlib
 import re
-import shutil
-import stat
 import subprocess
+import stat
 import sys
 import traceback
+import shutil
+from functools import cached_property
+from typing import Callable, Dict, List, Pattern, Tuple, Union
+
 # The way this script is currently used (ie no bazel) it relies on system deps.
 # As `pyyaml` is present in `envoy-build-ubuntu` it should be safe to use here.
 import yaml
-from functools import cached_property
-from typing import Callable
-from typing import Dict
-from typing import List
-from typing import Pattern
-from typing import Tuple
-from typing import Union
 
 import paths
 
