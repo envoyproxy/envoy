57045b1b96bb878448d617fe6bbd551f932cadef
Ryan Northey <ryan@synca.io>
2022-08-16 08:56:29 +0100

    [TEST] cython deps (on arm/mac)
    Signed-off-by: Ryan Northey <ryan@synca.io>

---
+++ tools/dependency/cython_test.py
@@ -0,0 +1,12 @@
+
+try:
+    import uvloop
+    print("uvloop imported correctly")
+except:
+    print("Unable to import uvloop")
+
+try:
+    import orjson
+    print("orjson imported correctly")
+except:
+    print("Unable to import orjson")
