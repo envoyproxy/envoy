5a36eb3abee7fdeae03fdcecde067ccf405b454c
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-08-17 16:18:47 +0700

    Optimize producer's command line
    Signed-off-by: Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>

--- examples/kafka-broker/verify.sh
+++ examples/kafka-broker/verify.sh
@@ -16,15 +16,15 @@ TOPIC="envoy-kafka-broker"
 MESSAGE="Welcome to Envoy and Kafka Broker filter!"
 
 run_log "Create a Kafka topic"
-kafka_client kafka-topics --bootstrap-server proxy:10000 --create --topic $TOPIC quickstart-events
+kafka_client kafka-topics --bootstrap-server proxy:10000 --create --topic $TOPIC
 
 run_log "Check the Kafka topic"
 kafka_client kafka-topics --bootstrap-server proxy:10000 --list | grep $TOPIC
 
 run_log "Send a message using the Kafka producer"
 kafka_client /bin/bash -c " \
-    echo $MESSAGE >> message.txt \
-    && kafka-console-producer --request-required-acks 1 --broker-list proxy:10000 --topic $TOPIC < message.txt"
+    echo $MESSAGE \
+    | kafka-console-producer --request-required-acks 1 --broker-list proxy:10000 --topic $TOPIC"
 
 run_log "Receive a message using the Kafka consumer"
-kafka_client kafka-console-consumer --bootstrap-server proxy:10000 --topic $TOPIC --from-beginning --max-messages 1
+kafka_client kafka-console-consumer --bootstrap-server proxy:10000 --topic $TOPIC --from-beginning --max-messages 1 | grep "$MESSAGE"
