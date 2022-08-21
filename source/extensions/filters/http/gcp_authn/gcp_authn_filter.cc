37ccc28c4417f0a67c23aaf471d52ce05b1012f0
Tianyu Xia <tyxia@google.com>
2022-08-21 19:24:44 +0000

    update with new utility function
    Signed-off-by: Tianyu Xia <tyxia@google.com>

--- source/extensions/filters/http/gcp_authn/gcp_authn_filter.cc
+++ source/extensions/filters/http/gcp_authn/gcp_authn_filter.cc
@@ -43,10 +43,11 @@ Http::FilterHeadersStatus GcpAuthnFilter::decodeHeaders(Http::RequestHeaderMap& 
     const auto filter_it = filter_metadata.find(std::string(FilterName));
     if (filter_it != filter_metadata.end()) {
       envoy::extensions::filters::http::gcp_authn::v3::Audience audience;
-      if (filter_it->second.UnpackTo(&audience)) {
+      auto status = MessageUtil::unpackToNoThrow(filter_it->second, audience);
+      if (status.ok()) {
         audience_str_ = audience.url();
       } else {
-        ENVOY_LOG(error, "Failed to parse the audience message: {}", audience.DebugString());
+        ENVOY_LOG(error, "Failed to parse the audience message: {}", status.message());
       }
     }
   }
