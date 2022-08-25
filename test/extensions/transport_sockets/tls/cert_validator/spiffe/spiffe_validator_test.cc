ca8e8f6b5cfbee4872bedbe3f09e19de1dfd9bb9
Jarno Rajahalme <jarno@isovalent.com>
2022-08-25 17:19:50 +0300

    test: Fix test
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- test/extensions/transport_sockets/tls/cert_validator/spiffe/spiffe_validator_test.cc
+++ test/extensions/transport_sockets/tls/cert_validator/spiffe/spiffe_validator_test.cc
@@ -557,9 +557,9 @@ typed_config:
         filename: "{{ test_rundir }}/test/extensions/transport_sockets/tls/test_data/intermediate_ca_cert.pem"
   )EOF"),
              time_system);
-  EXPECT_EQ(19231, validator().daysUntilFirstCertExpires());
+  EXPECT_EQ(19959, validator().daysUntilFirstCertExpires());
   time_system.setSystemTime(std::chrono::milliseconds(864000000));
-  EXPECT_EQ(19221, validator().daysUntilFirstCertExpires());
+  EXPECT_EQ(19949, validator().daysUntilFirstCertExpires());
 }
 
 TEST_F(TestSPIFFEValidator, TestAddClientValidationContext) {
