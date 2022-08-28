944e12975c98091d814ad0a2439899ad547fc8a9
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-08-28 14:35:55 +0700

    Add kafka stats checking
    Signed-off-by: Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>

--- examples/kafka-broker/verify.sh
+++ examples/kafka-broker/verify.sh
@@ -8,7 +8,7 @@ export PORT_ADMIN="${KAFKA_PORT_ADMIN:-11101}"
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
 kafka_client () {
-    docker-compose run kafka-client "$@"
+    docker-compose run --rm kafka-client "$@"
 }
 
 TOPIC="envoy-kafka-broker"
@@ -28,3 +28,37 @@ kafka_client /bin/bash -c " \
 
 run_log "Receive a message using the Kafka consumer"
 kafka_client kafka-console-consumer --bootstrap-server proxy:10000 --topic $TOPIC --from-beginning --max-messages 1 | grep "$MESSAGE"
+
+run_log "Check admin kafka_broker stats"
+responds_with \
+    "kafka.kafka_broker.request.create_topics_request: 1" \
+    "http://localhost:${PORT_ADMIN}/stats?filter=kafka.kafka_broker.request.create_topics_request"
+responds_with \
+    "kafka.kafka_broker.request.api_versions_request: 4" \
+    "http://localhost:${PORT_ADMIN}/stats?filter=kafka.kafka_broker.request.api_versions_request"
+responds_with \
+    "kafka.kafka_broker.request.find_coordinator_request: 1" \
+    "http://localhost:${PORT_ADMIN}/stats?filter=kafka.kafka_broker.request.find_coordinator_request"
+responds_with \
+    "kafka.kafka_broker.request.metadata_request: 4" \
+    "http://localhost:${PORT_ADMIN}/stats?filter=kafka.kafka_broker.request.metadata_request"
+responds_with \
+    "kafka.kafka_broker.response.api_versions_response: 4" \
+    "http://localhost:${PORT_ADMIN}/stats?filter=kafka.kafka_broker.response.api_versions_response"
+responds_with \
+    "kafka.kafka_broker.response.find_coordinator_response: 1" \
+    "http://localhost:${PORT_ADMIN}/stats?filter=kafka.kafka_broker.response.find_coordinator_response"
+responds_with \
+    "kafka.kafka_broker.response.metadata_response: 4" \
+    "http://localhost:${PORT_ADMIN}/stats?filter=kafka.kafka_broker.response.metadata_response"
+
+run_log "Check admin kafka_service stats"
+responds_with \
+    "cluster.kafka_service.max_host_weight: 1" \
+    "http://localhost:${PORT_ADMIN}/stats?filter=cluster.kafka_service.max_host_weight"
+responds_with \
+    "cluster.kafka_service.membership_healthy: 1" \
+    "http://localhost:${PORT_ADMIN}/stats?filter=cluster.kafka_service.membership_healthy"
+responds_with \
+    "cluster.kafka_service.membership_total: 1" \
+    "http://localhost:${PORT_ADMIN}/stats?filter=cluster.kafka_service.membership_total"
