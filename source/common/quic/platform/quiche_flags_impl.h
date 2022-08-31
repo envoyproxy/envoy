64535db2d96238a88cb29f71400f1fc7dd408038
Ryan Hamilton <rch@google.com>
2022-08-31 21:36:17 +0000

    format
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- source/common/quic/platform/quiche_flags_impl.h
+++ source/common/quic/platform/quiche_flags_impl.h
@@ -47,14 +47,14 @@ private:
 // Flag declarations
 #define QUIC_FLAG(flag, ...) ABSL_DECLARE_FLAG(bool, flag);
 #include "quiche/quic/core/quic_flags_list.h"
-QUIC_FLAG(quic_reloadable_flag_spdy_testonly_default_false, false) // NOLINT
-QUIC_FLAG(quic_reloadable_flag_spdy_testonly_default_true, true) // NOLINT
-QUIC_FLAG(quic_restart_flag_spdy_testonly_default_false, false) // NOLINT
-QUIC_FLAG(quic_restart_flag_spdy_testonly_default_true, true) // NOLINT
+QUIC_FLAG(quic_reloadable_flag_spdy_testonly_default_false, false)  // NOLINT
+QUIC_FLAG(quic_reloadable_flag_spdy_testonly_default_true, true)    // NOLINT
+QUIC_FLAG(quic_restart_flag_spdy_testonly_default_false, false)     // NOLINT
+QUIC_FLAG(quic_restart_flag_spdy_testonly_default_true, true)       // NOLINT
 QUIC_FLAG(quic_reloadable_flag_http2_testonly_default_false, false) // NOLINT
-QUIC_FLAG(quic_reloadable_flag_http2_testonly_default_true, true) // NOLINT
-QUIC_FLAG(quic_restart_flag_http2_testonly_default_false, false) // NOLINT
-QUIC_FLAG(quic_restart_flag_http2_testonly_default_true, true) // NOLINT
+QUIC_FLAG(quic_reloadable_flag_http2_testonly_default_true, true)   // NOLINT
+QUIC_FLAG(quic_restart_flag_http2_testonly_default_false, false)    // NOLINT
+QUIC_FLAG(quic_restart_flag_http2_testonly_default_true, true)      // NOLINT
 #undef QUIC_FLAG
 
 #define QUIC_PROTOCOL_FLAG(type, flag, ...) ABSL_DECLARE_FLAG(type, flag);
