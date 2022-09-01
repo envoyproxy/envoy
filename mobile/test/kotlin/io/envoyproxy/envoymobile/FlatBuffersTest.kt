7073e1d6c1d834cbae33b4d2fa44b387d64ac4dd
Mike Schore <mike.schore@gmail.com>
2022-09-01 14:33:16 -0700

    test: fix Kotlin FlatBuffersTest warnings (#2508)
    Signed-off-by: Mike Schore <mike.schore@gmail.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/test/kotlin/io/envoyproxy/envoymobile/FlatBuffersTest.kt
+++ mobile/test/kotlin/io/envoyproxy/envoymobile/FlatBuffersTest.kt
@@ -2,15 +2,17 @@ package io.envoyproxy.envoymobile
 
 import Test.Nested.SomeType
 import com.google.flatbuffers.FlatBufferBuilder
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class FlatBuffersTest {
 
   @Test
+  @OptIn(ExperimentalUnsignedTypes::class)
   fun `flatbuffers construction`() {
     // This test doesn't really test any functionality, just demonstrates that we are able to utilize flatbuffers pending
     // real usage within the code base.
-    val s = SomeType()
-    val f = FlatBufferBuilder()
+    assertThat(SomeType()).isNotNull()
+    assertThat(FlatBufferBuilder()).isNotNull()
   }
 }
