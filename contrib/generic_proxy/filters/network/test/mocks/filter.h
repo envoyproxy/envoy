51c0d6f47c98087c7e3288205cbf8edf50ae0196
code <wangbaiping@corp.netease.com>
2022-09-13 08:16:52 +0800

    extension: added generic proxy network filter (#22826)
    * initial draft for the meta protocol proxy
    
    Signed-off-by: wbpcode <comems@msn.com>
    
    * minor update
    
    Signed-off-by: wbpcode <comems@msn.com>
    
    * add match implemented
    
    Signed-off-by: wbpcode <comems@msn.com>
    
    * add some simple test
    
    Signed-off-by: wbpcode <wbphub@live.com>
    
    * add more test for route matcher
    
    Signed-off-by: wbpcode <wbphub@live.com>
    
    * partial commit
    
    Signed-off-by: wbpcode <wbphub@live.com>
    
    * complete basic unit test
    
    Signed-off-by: wbpcode <comems@msn.com>
    
    * fix format
    
    Signed-off-by: wbpcode <comems@msn.com>
    
    * fix error after merge
    
    Signed-off-by: wbpcode <wbphub@live.com>
    
    * add some more test
    
    Signed-off-by: wbpcode <wbphub@live.com>
    
    * minor fix
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * fix test
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * just make it run
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * just make it run
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * first integration test
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * complete almost all the tests and fix docs and format
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * fix proto format
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * minor update
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * add cleanup
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * update for clang tidy and type error
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * lower coverage threshould
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * fix unexpected include
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * fix window build
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * rename to generic proxy to avoid name conflict
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * fix docs
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * remove unnecessary readme
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * update comments and name of matcher
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * fix format
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * add name method
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * move everything to contrib
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * simple release note
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * fix format
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    Signed-off-by: wbpcode <comems@msn.com>
    Signed-off-by: wbpcode <wbphub@live.com>
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>

---
+++ contrib/generic_proxy/filters/network/test/mocks/filter.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include "contrib/generic_proxy/filters/network/source/interface/config.h"
+#include "contrib/generic_proxy/filters/network/source/interface/filter.h"
+#include "gmock/gmock.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace NetworkFilters {
+namespace GenericProxy {
+
+class MockDecoderFilter : public DecoderFilter {
+public:
+  MockDecoderFilter();
+
+  MOCK_METHOD(void, onDestroy, ());
+
+  MOCK_METHOD(void, setDecoderFilterCallbacks, (DecoderFilterCallback & callbacks));
+  MOCK_METHOD(FilterStatus, onStreamDecoded, (Request & request));
+};
+
+class MockEncoderFilter : public EncoderFilter {
+public:
+  MockEncoderFilter();
+
+  MOCK_METHOD(void, onDestroy, ());
+
+  MOCK_METHOD(void, setEncoderFilterCallbacks, (EncoderFilterCallback & callbacks));
+  MOCK_METHOD(FilterStatus, onStreamEncoded, (Response & response));
+};
+
+class MockStreamFilter : public StreamFilter {
+public:
+  MockStreamFilter();
+
+  MOCK_METHOD(void, onDestroy, ());
+
+  MOCK_METHOD(void, setEncoderFilterCallbacks, (EncoderFilterCallback & callbacks));
+  MOCK_METHOD(FilterStatus, onStreamEncoded, (Response & response));
+
+  MOCK_METHOD(void, setDecoderFilterCallbacks, (DecoderFilterCallback & callbacks));
+  MOCK_METHOD(FilterStatus, onStreamDecoded, (Request & request));
+};
+
+class MockStreamFilterConfig : public NamedFilterConfigFactory {
+public:
+  MockStreamFilterConfig();
+
+  MOCK_METHOD(FilterFactoryCb, createFilterFactoryFromProto,
+              (const Protobuf::Message& config, const std::string& stat_prefix,
+               Server::Configuration::FactoryContext& context));
+  MOCK_METHOD(ProtobufTypes::MessagePtr, createEmptyConfigProto, ());
+  MOCK_METHOD(ProtobufTypes::MessagePtr, createEmptyRouteConfigProto, ());
+  MOCK_METHOD(RouteSpecificFilterConfigConstSharedPtr, createRouteSpecificFilterConfig,
+              (const Protobuf::Message&, Server::Configuration::ServerFactoryContext&,
+               ProtobufMessage::ValidationVisitor&));
+  MOCK_METHOD(std::string, name, (), (const));
+  MOCK_METHOD(std::set<std::string>, configTypes, ());
+  MOCK_METHOD(bool, isTerminalFilter, ());
+};
+
+class MockFilterChainFactoryCallbacks : public FilterChainFactoryCallbacks {
+public:
+  MockFilterChainFactoryCallbacks() = default;
+
+  MOCK_METHOD(void, addDecoderFilter, (DecoderFilterSharedPtr filter));
+  MOCK_METHOD(void, addEncoderFilter, (EncoderFilterSharedPtr filter));
+  MOCK_METHOD(void, addFilter, (StreamFilterSharedPtr filter));
+};
+
+template <class Base> class MockStreamFilterCallbacks : public Base {
+public:
+  MOCK_METHOD(Envoy::Event::Dispatcher&, dispatcher, ());
+  MOCK_METHOD(const CodecFactory&, downstreamCodec, ());
+  MOCK_METHOD(void, resetStream, ());
+  MOCK_METHOD(const RouteEntry*, routeEntry, (), (const));
+};
+
+class MockDecoderFilterCallback : public MockStreamFilterCallbacks<DecoderFilterCallback> {
+public:
+  MOCK_METHOD(void, sendLocalReply, (Status, ResponseUpdateFunction&&));
+  MOCK_METHOD(void, continueDecoding, ());
+  MOCK_METHOD(void, upstreamResponse, (ResponsePtr response));
+  MOCK_METHOD(void, completeDirectly, ());
+};
+
+class MockEncoderFilterCallback : public MockStreamFilterCallbacks<EncoderFilterCallback> {
+public:
+  MOCK_METHOD(void, continueEncoding, ());
+};
+
+} // namespace GenericProxy
+} // namespace NetworkFilters
+} // namespace Extensions
+} // namespace Envoy
