7ce3f4ced4f2e40def8b2799b9e83138795d37fd
Greg Greenway <ggreenway@users.noreply.github.com>
2022-09-14 08:05:25 -0700

    stats: fix tag extraction for `worker_id` to avoid double counting (#23081)
    Default tag extraction rules were changed for `worker_id` extraction. Previously, `worker_` was removed from the original name during the extraction. This led to the same base stat name for both the per-worker and overall stat. For instance, in prometheus stats, the following stats were produced:
    
          envoy_listener_downstream_cx_total{} 2
          envoy_listener_downstream_cx_total{envoy_worker_id="0"} 1
          envoy_listener_downstream_cx_total{envoy_worker_id="1"} 1
    
    This resulted in ``sum(envoy_listener_downstream_cx_total)`` producing 4, even though there are only 2 connections.
    The new behavior results in stats such as this:
    
          envoy_listener_downstream_cx_total{} 2
          envoy_listener_worker_downstream_cx_total{envoy_worker_id="0"} 1
          envoy_listener_worker_downstream_cx_total{envoy_worker_id="1"} 1
    
    Fixes #22705
    
    Signed-off-by: Greg Greenway <ggreenway@apple.com>

--- test/common/stats/tag_extractor_impl_test.cc
+++ test/common/stats/tag_extractor_impl_test.cc
@@ -392,19 +392,20 @@ TEST(TagExtractorTest, DefaultTagExtractors) {
   worker_id.value_ = "123";
 
   regex_tester.testRegex("listener_manager.worker_123.dispatcher.loop_duration_us",
-                         "listener_manager.dispatcher.loop_duration_us", {worker_id});
+                         "listener_manager.worker_dispatcher.loop_duration_us", {worker_id});
 
   // Listener worker id
   listener_address.value_ = "127.0.0.1_3012";
   regex_tester.testRegex("listener.127.0.0.1_3012.worker_123.downstream_cx_active",
-                         "listener.downstream_cx_active", {listener_address, worker_id});
+                         "listener.worker_downstream_cx_active", {listener_address, worker_id});
 
   listener_address.value_ = "myprefix";
   regex_tester.testRegex("listener.myprefix.worker_123.downstream_cx_active",
-                         "listener.downstream_cx_active", {listener_address, worker_id});
+                         "listener.worker_downstream_cx_active", {listener_address, worker_id});
 
   // Server worker id
-  regex_tester.testRegex("server.worker_123.watchdog_miss", "server.watchdog_miss", {worker_id});
+  regex_tester.testRegex("server.worker_123.watchdog_miss", "server.worker_watchdog_miss",
+                         {worker_id});
 
   // Thrift Proxy Prefix
   Tag thrift_prefix;
