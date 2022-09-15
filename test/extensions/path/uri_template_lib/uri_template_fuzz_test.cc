df35c908d5f47dd6a11166c8a9cdb2237f51e2ee
silverstar195 <seanmaloney@google.com>
2022-09-15 15:40:55 +0000

    Add fuzzing
    Signed-off-by: silverstar195 <seanmaloney@google.com>

---
+++ test/extensions/path/uri_template_lib/uri_template_fuzz_test.cc
@@ -0,0 +1,23 @@
+#include "source/common/common/statusor.h"
+#include "source/extensions/path/uri_template_lib/uri_template.h"
+
+#include "test/fuzz/fuzz_runner.h"
+#include "test/fuzz/utility.h"
+
+#include "absl/strings/string_view.h"
+
+namespace Envoy {
+namespace Fuzz {
+
+DEFINE_FUZZER(const uint8_t* buf, size_t len) {
+  absl::string_view input(reinterpret_cast<const char*>(buf), len);
+
+  // test rewriter parser
+  absl::StatusOr<std::vector<Envoy::Extensions::UriTemplate::ParsedSegment>> rewrite = Envoy::Extensions::UriTemplate::parseRewritePattern(input);
+
+  // test matcher parser
+  absl::StatusOr<std::string_view> match = Envoy::Extensions::UriTemplate::convertPathPatternSyntaxToRegex(input);
+}
+
+} // namespace Fuzz
+} // namespace Envoy
