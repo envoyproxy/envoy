7369ab57ff665700cd02dfa8c02e4396d6fba1a1
Ali Beyad <abeyad@google.com>
2022-09-16 07:12:20 -0700

    key_value: Use known registered name to create global PlatformInterfaceImpl (#2551)
    The PlatformKeyValueStoreFactory used to take a PlatformInterfaceImpl where the registered KV store was found in the API registry and used to bridge the PlatformKeyValueStore with the native underlying store (e.g. SharedPrefs on Android).
    
    However, registered factory classes only have their default constructor invoked, so the current PlatformKeyValueStoreFactory constructor taking in the PlatformInterfaceImpl would not have worked.
    
    Instead, we use a globally known registered API name to fetch the registered KV store, and use it to create a static global PlatformInstanceImpl.
    
    Signed-off-by: Ali Beyad <abeyad@google.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/common/extensions/key_value/platform/config.cc
+++ mobile/library/common/extensions/key_value/platform/config.cc
@@ -12,10 +12,16 @@ namespace Envoy {
 namespace Extensions {
 namespace KeyValue {
 
-class PlatformInterfaceImpl : PlatformInterface, public Logger::Loggable<Logger::Id::filter> {
+namespace {
+
+constexpr absl::string_view PlatformStoreName{"reserved.platform_store"};
+
+class PlatformInterfaceImpl : public PlatformInterface,
+                              public Logger::Loggable<Logger::Id::filter> {
 public:
-  PlatformInterfaceImpl(const std::string& name)
-      : bridged_store_(*static_cast<envoy_kv_store*>(Api::External::retrieveApi(name))) {}
+  PlatformInterfaceImpl()
+      : bridged_store_(*static_cast<envoy_kv_store*>(
+            Api::External::retrieveApi(std::string(PlatformStoreName)))) {}
 
   ~PlatformInterfaceImpl() override {}
 
@@ -32,8 +38,15 @@ public:
   }
 
 private:
-  envoy_kv_store bridged_store_;
+  envoy_kv_store& bridged_store_;
 };
+
+PlatformInterfaceImpl& getPlatformInterfaceImplSingleton() {
+  static PlatformInterfaceImpl impl = PlatformInterfaceImpl();
+  return impl;
+}
+
+} // namespace
 
 PlatformKeyValueStore::PlatformKeyValueStore(Event::Dispatcher& dispatcher,
                                              std::chrono::milliseconds save_interval,
@@ -41,7 +54,7 @@ PlatformKeyValueStore::PlatformKeyValueStore(Event::Dispatcher& dispatcher,
                                              uint64_t max_entries, const std::string& key)
     : KeyValueStoreBase(dispatcher, save_interval, max_entries),
       platform_interface_(platform_interface), key_(key) {
-  const std::string contents = platform_interface.read(key);
+  const std::string contents = platform_interface_.read(key);
   if (!parseContents(contents)) {
     ENVOY_LOG(warn, "Failed to parse key value store contents {}", key);
   }
@@ -63,14 +76,14 @@ PlatformKeyValueStoreFactory::createStore(const Protobuf::Message& config,
   const auto& typed_config = MessageUtil::downcastAndValidate<
       const ::envoy::config::common::key_value::v3::KeyValueStoreConfig&>(config,
                                                                           validation_visitor);
-  const auto file_config = MessageUtil::anyConvertAndValidate<
+  const auto platform_kv_store_config = MessageUtil::anyConvertAndValidate<
       envoymobile::extensions::key_value::platform::PlatformKeyValueStoreConfig>(
       typed_config.config().typed_config(), validation_visitor);
-  auto milliseconds =
-      std::chrono::milliseconds(DurationUtil::durationToMilliseconds(file_config.save_interval()));
-  return std::make_unique<PlatformKeyValueStore>(dispatcher, milliseconds,
-                                                 platform_interface_.value().get(),
-                                                 file_config.max_entries(), file_config.key());
+  auto milliseconds = std::chrono::milliseconds(
+      DurationUtil::durationToMilliseconds(platform_kv_store_config.save_interval()));
+  return std::make_unique<PlatformKeyValueStore>(
+      dispatcher, milliseconds, getPlatformInterfaceImplSingleton(),
+      platform_kv_store_config.max_entries(), platform_kv_store_config.key());
 }
 
 REGISTER_FACTORY(PlatformKeyValueStoreFactory, KeyValueStoreFactory);
--- mobile/library/common/extensions/key_value/platform/config.h
+++ mobile/library/common/extensions/key_value/platform/config.h
@@ -38,9 +38,7 @@ private:
 
 class PlatformKeyValueStoreFactory : public KeyValueStoreFactory {
 public:
-  PlatformKeyValueStoreFactory() {}
-  PlatformKeyValueStoreFactory(PlatformInterface& platform_interface)
-      : platform_interface_(platform_interface) {}
+  PlatformKeyValueStoreFactory() = default;
 
   // KeyValueStoreFactory
   KeyValueStorePtr createStore(const Protobuf::Message& config,
@@ -50,14 +48,11 @@ public:
 
   // TypedFactory
   ProtobufTypes::MessagePtr createEmptyConfigProto() override {
-    return ProtobufTypes::MessagePtr{
-        new envoymobile::extensions::key_value::platform::PlatformKeyValueStoreConfig()};
+    return std::make_unique<
+        envoymobile::extensions::key_value::platform::PlatformKeyValueStoreConfig>();
   }
 
   std::string name() const override { return "envoy.key_value.platform"; }
-  // TODO(alyssawilk, goaway) the default PlatformInterface should do up calls through Java and this
-  // can be moved to a non-optional reference.
-  OptRef<PlatformInterface> platform_interface_;
 };
 
 } // namespace KeyValue
