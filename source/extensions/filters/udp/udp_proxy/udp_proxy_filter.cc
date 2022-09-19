91b89537a2815424b8d6313d4d2e6ebc17dd935d
Xie Zhihao <zhihao.xie@intel.com>
2022-09-19 10:47:41 +0000

    udp: add runtime guard
    Signed-off-by: Xie Zhihao <zhihao.xie@intel.com>

--- source/extensions/filters/udp/udp_proxy/udp_proxy_filter.cc
+++ source/extensions/filters/udp/udp_proxy/udp_proxy_filter.cc
@@ -351,14 +351,17 @@ void UdpProxyFilter::ActiveSession::write(const Buffer::Instance& buffer) {
   //       set. We allow the OS to select the right IP based on outbound routing rules if
   //       use_original_src_ip_ is not set, else use downstream peer IP as local IP.
   const Network::Address::Ip* local_ip = use_original_src_ip_ ? addresses_.peer_->ip() : nullptr;
-  if (!use_original_src_ip_ && !connected_) {
-    Api::SysCallIntResult rc = socket_->ioHandle().connect(host_->address());
-    if (SOCKET_FAILURE(rc.return_value_)) {
-      ENVOY_LOG(error, "cannot connect: ({}) {}", rc.errno_, errorDetails(rc.errno_));
-      cluster_.cluster_stats_.sess_tx_errors_.inc();
-      return;
+  if (!use_original_src_ip_ && !skip_connecting_) {
+    if (Runtime::runtimeFeatureEnabled("envoy.reloadable_features.udp_proxy_connect")) {
+      Api::SysCallIntResult rc = socket_->ioHandle().connect(host_->address());
+      if (SOCKET_FAILURE(rc.return_value_)) {
+        ENVOY_LOG(error, "cannot connect: ({}) {}", rc.errno_, errorDetails(rc.errno_));
+        cluster_.cluster_stats_.sess_tx_errors_.inc();
+        return;
+      }
     }
-    connected_ = true;
+
+    skip_connecting_ = true;
   }
   Api::IoCallUint64Result rc =
       Network::Utility::writeToSocket(socket_->ioHandle(), buffer, local_ip, *host_->address());
