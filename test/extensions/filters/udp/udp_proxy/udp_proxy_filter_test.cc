ed3554d4df58277df729bac2d2cbea1c055e7c96
Xie Zhihao <zhihao.xie@intel.com>
2022-09-26 10:33:08 +0000

    udp: test connect error handling
    Signed-off-by: Xie Zhihao <zhihao.xie@intel.com>

--- test/extensions/filters/udp/udp_proxy/udp_proxy_filter_test.cc
+++ test/extensions/filters/udp/udp_proxy/udp_proxy_filter_test.cc
@@ -76,35 +76,44 @@ public:
 
     void expectWriteToUpstream(const std::string& data, int sys_errno = 0,
                                const Network::Address::Ip* local_ip = nullptr,
-                               bool expect_connect = false) {
+                               bool expect_connect = false, int connect_sys_errno = 0) {
       EXPECT_CALL(*idle_timer_, enableTimer(parent_.config_->sessionTimeout(), nullptr));
       if (expect_connect) {
-        EXPECT_CALL(*socket_->io_handle_, connect(_)).WillOnce(Return(Api::SysCallIntResult{0, 0}));
+        EXPECT_CALL(*socket_->io_handle_, connect(_))
+            .WillOnce(Invoke([connect_sys_errno]() -> Api::SysCallIntResult {
+              if (!connect_sys_errno) {
+                return Api::SysCallIntResult{0, 0};
+              } else {
+                return Api::SysCallIntResult{-1, connect_sys_errno};
+              }
+            }));
       } else {
         EXPECT_CALL(*socket_->io_handle_, connect(_)).Times(0);
       }
-      EXPECT_CALL(*socket_->io_handle_, sendmsg(_, 1, 0, _, _))
-          .WillOnce(Invoke(
-              [this, data, local_ip, sys_errno](
-                  const Buffer::RawSlice* slices, uint64_t, int,
-                  const Network::Address::Ip* self_ip,
-                  const Network::Address::Instance& peer_address) -> Api::IoCallUint64Result {
-                EXPECT_EQ(data, absl::string_view(static_cast<const char*>(slices[0].mem_),
-                                                  slices[0].len_));
-                EXPECT_EQ(peer_address, *upstream_address_);
-                if (local_ip == nullptr) {
-                  EXPECT_EQ(nullptr, self_ip);
-                } else {
-                  EXPECT_EQ(self_ip->addressAsString(), local_ip->addressAsString());
-                }
-                // For suppression of clang-tidy NewDeleteLeaks rule, don't use the ternary
-                // operator.
-                if (sys_errno == 0) {
-                  return makeNoError(data.size());
-                } else {
-                  return makeError(sys_errno);
-                }
-              }));
+      if (!connect_sys_errno) {
+        EXPECT_CALL(*socket_->io_handle_, sendmsg(_, 1, 0, _, _))
+            .WillOnce(Invoke(
+                [this, data, local_ip, sys_errno](
+                    const Buffer::RawSlice* slices, uint64_t, int,
+                    const Network::Address::Ip* self_ip,
+                    const Network::Address::Instance& peer_address) -> Api::IoCallUint64Result {
+                  EXPECT_EQ(data, absl::string_view(static_cast<const char*>(slices[0].mem_),
+                                                    slices[0].len_));
+                  EXPECT_EQ(peer_address, *upstream_address_);
+                  if (local_ip == nullptr) {
+                    EXPECT_EQ(nullptr, self_ip);
+                  } else {
+                    EXPECT_EQ(self_ip->addressAsString(), local_ip->addressAsString());
+                  }
+                  // For suppression of clang-tidy NewDeleteLeaks rule, don't use the ternary
+                  // operator.
+                  if (sys_errno == 0) {
+                    return makeNoError(data.size());
+                  } else {
+                    return makeError(sys_errno);
+                  }
+                }));
+      }
     }
 
     void recvDataFromUpstream(const std::string& data, int recv_sys_errno = 0,
@@ -557,6 +566,45 @@ matcher:
 
   filter_.reset();
   EXPECT_EQ(access_log_data_.value(), "fake_cluster 0 10 1 1 0 2");
+}
+
+// Verify upstream connect error handling.
+TEST_F(UdpProxyFilterTest, ConnectErrorHandling) {
+  InSequence s;
+
+  const std::string access_log_format = "%DYNAMIC_METADATA(udp.proxy:cluster_name)% "
+                                        "%DYNAMIC_METADATA(udp.proxy:bytes_sent)% "
+                                        "%DYNAMIC_METADATA(udp.proxy:bytes_received)% "
+                                        "%DYNAMIC_METADATA(udp.proxy:errors_sent)% "
+                                        "%DYNAMIC_METADATA(udp.proxy:errors_received)% "
+                                        "%DYNAMIC_METADATA(udp.proxy:datagrams_sent)% "
+                                        "%DYNAMIC_METADATA(udp.proxy:datagrams_received)%";
+
+  setup(accessLogConfig(R"EOF(
+stat_prefix: foo
+matcher:
+  on_no_match:
+    action:
+      name: route
+      typed_config:
+        '@type': type.googleapis.com/envoy.extensions.filters.udp.udp_proxy.v3.Route
+        cluster: fake_cluster
+  )EOF",
+                        access_log_format));
+
+  expectSessionCreate(upstream_address_);
+  test_sessions_[0].expectWriteToUpstream("hello", 0, nullptr, true, 1);
+  EXPECT_LOG_CONTAINS("debug", "cannot connect",
+                      recvDataFromDownstream("10.0.0.1:1000", "10.0.0.2:80", "hello"));
+  checkTransferStats(5 /*rx_bytes*/, 1 /*rx_datagrams*/, 0 /*tx_bytes*/, 0 /*tx_datagrams*/);
+  EXPECT_EQ(
+      1, TestUtility::findCounter(
+             factory_context_.cluster_manager_.thread_local_cluster_.cluster_.info_->stats_store_,
+             "udp.sess_tx_errors")
+             ->value());
+
+  filter_.reset();
+  EXPECT_EQ(access_log_data_.value(), "fake_cluster 0 5 0 0 0 1");
 }
 
 // No upstream host handling.
