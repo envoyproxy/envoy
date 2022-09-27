25c2d8acec6b40afaab8470375228aff62632768
Jarno Rajahalme <jarno@isovalent.com>
2022-09-27 15:51:46 +0300

    tcp_proxy: Add option to read before connect
    Add filter state object bool proxy_receive_before_connect that tcp_proxy
    filter checks for and when set to 'true' will not set readDisable on the
    downstream connection before upstream connection is established.
    
    This change allows advanced cases where a network read filter needs to
    receive (and possibly send) downstream data that may change the
    destination of the upstream TCP connection, e.g. via metadata set based
    on the received data. If this is the case, then the read filter preceding
    the tcp_proxy filter must return StopIteration from its onNewConnection()
    call, causing tcp_proxy filter to postpone its upstream connection
    establishment until onData() returns Continue.
    
    Any data reaching the tcp_proxy filter before the upstream connection is
    established is buffered so that the downstream filters do not see the
    same data again which would be the case if it would remaining in the buffer
    and more data is received. This also allows downstream filters inject
    data before the upstream connection is established; such injected data
    would be lost if tcp_proxy would not buffer it while connection
    establishment is still ongoing.
    
    An existing dynamic metadata integration test is modified to use
    proxy_receive_before_connect=true. This makes the use case less
    reliant on the tcp_proxy internal detail, such as balancing and timing of
    the readDisable() calls on the downstream connection.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- source/common/tcp_proxy/tcp_proxy.h
+++ source/common/tcp_proxy/tcp_proxy.h
@@ -22,6 +22,7 @@
 #include "envoy/upstream/cluster_manager.h"
 #include "envoy/upstream/upstream.h"
 
+#include "source/common/buffer/buffer_impl.h"
 #include "source/common/common/logger.h"
 #include "source/common/formatter/substitution_format_string.h"
 #include "source/common/http/header_map_impl.h"
@@ -540,6 +541,9 @@ protected:
   uint32_t connect_attempts_{};
   bool connecting_{};
   bool downstream_closed_{};
+  bool receive_before_connect_{false};
+  bool early_data_end_stream_{false};
+  Buffer::OwnedImpl early_data_buffer_{};
 };
 
 // This class deals with an upstream connection that needs to finish flushing, when the downstream
