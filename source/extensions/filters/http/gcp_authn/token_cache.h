71c03f24ffacae607cf0f00bcaa930e9f3bccbe3
Tianyu Xia <tyxia@google.com>
2022-10-04 19:29:40 +0000

    tweak comments
    Signed-off-by: Tianyu Xia <tyxia@google.com>

--- source/extensions/filters/http/gcp_authn/token_cache.h
+++ source/extensions/filters/http/gcp_authn/token_cache.h
@@ -86,10 +86,11 @@ TokenType* TokenCacheImpl<TokenType>::validateTokenAndReturn(const std::string& 
                                                              TokenType* const found_token) {
   if constexpr (std::is_same<TokenType, JwtToken>::value) {
     ASSERT(found_token != nullptr);
-    // Verify the validness of the token by checking its expiration time field.
-    // Note: verifyTimeConstraint() interface is correct to be used by token consumer. However, for
-    // the token producer here, we should instead include the clock skew as the part of the current
-    // time up front to account for the clock skew on the consumer side,
+    // Verify the validness of the token by checking its expiration time field. Default clock skew
+    // is one minute and it could be configurable via config if it is needed in the future.
+    // Note: verifyTimeConstraint() interface is correct fo the token consumer. However, as the
+    // token producer here, we should instead include the clock skew as the part of the `now` time
+    // up front to account for the clock skew on the consumer side where the token will be consumed.
     if (found_token->verifyTimeConstraint(
             DateUtil::nowToSeconds(time_source_) + ::google::jwt_verify::kClockSkewInSecond,
             /*clock_skew=*/0) == ::google::jwt_verify::Status::JwtExpired) {
