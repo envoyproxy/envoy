ee1026942ad80ca95183dbce3e563b8da350cff4
Asher Yermiyahu <asheryer@amazon.com>
2022-10-04 18:00:43 +0300

    redis: added support for transactions (#22162) (#22729)
    Signed-off-by: asheryer <asheryer@amazon.com>

--- source/extensions/clusters/redis/redis_cluster.cc
+++ source/extensions/clusters/redis/redis_cluster.cc
@@ -301,7 +301,7 @@ void RedisCluster::RedisDiscoverySession::startResolveRedis() {
     client->host_ = current_host_address_;
     client->client_ = client_factory_.create(host, dispatcher_, *this, redis_command_stats_,
                                              parent_.info()->statsScope(), parent_.auth_username_,
-                                             parent_.auth_password_);
+                                             parent_.auth_password_, false);
     client->client_->addConnectionCallbacks(*client);
   }
   ENVOY_LOG(debug, "executing redis cluster slot request for '{}'", parent_.info_->name());
