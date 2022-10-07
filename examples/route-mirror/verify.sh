8097243d07e4c60d094f1dbddf59b07d11b3feec
Amit Saha <asaha@atlassian.com>
2022-10-07 12:22:05 +1100

    update verification script
    Signed-off-by: Amit Saha <asaha@atlassian.com>

--- examples/route-mirror/verify.sh
+++ examples/route-mirror/verify.sh
@@ -7,14 +7,18 @@ export PORT_PROXY="${FRONT_PROXY_PORT_PROXY:-11820}"
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
 
-run_log "Test service: localhost:${PORT_PROXY}/service/1"
+run_log "Static mirror cluster: localhost:${PORT_PROXY}/service/1"
 wait_for 20 bash -c "responds_with \"Hello from behind Envoy (service 1)!\" http://localhost:${PORT_PROXY}/service/1"
+
+docker-compose logs service1 | grep --quiet "Host: localhost:${PORT_PROXY}"
 docker-compose logs service1-mirror | grep --quiet "Host: localhost-shadow:${PORT_PROXY}"
 
 
-run_log "Test service: localhost:${PORT_PROXY}/service/2"
+run_log "Mirror cluster via header: x-mirror-cluster localhost:${PORT_PROXY}/service/2"
 responds_with \
     "Hello from behind Envoy (service 2)!" \
     "http://localhost:${PORT_PROXY}/service/2" \
     --header 'x-mirror-cluster: service2-mirror'
+
+docker-compose logs service2 | grep --quiet "Host: localhost:${PORT_PROXY}"
 docker-compose logs service2-mirror | grep --quiet "Host: localhost-shadow:${PORT_PROXY}"
