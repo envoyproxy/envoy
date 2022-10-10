754b4af3f6009060215953121db8a081dcf52b7b
Amit Saha <asaha@atlassian.com>
2022-10-10 22:03:33 +1100

    Trial commit removing wait_for
    Signed-off-by: Amit Saha <asaha@atlassian.com>

--- examples/route-mirror/verify.sh
+++ examples/route-mirror/verify.sh
@@ -6,9 +6,8 @@ export PORT_PROXY="${FRONT_PROXY_PORT_PROXY:-11820}"
 # shellcheck source=examples/verify-common.sh
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
-
 run_log "Make a request to the statically mirrored route"
-wait_for 10 bash -c "responds_with \"Hello from behind Envoy (service 1)!\" http://localhost:${PORT_PROXY}/service/1"
+responds_with "Hello from behind Envoy (service 1)!" "http://localhost:${PORT_PROXY}/service/1"
 
 run_log "View logs for the request mirrored by request header"
 docker-compose logs service1 | grep --quiet "Host: localhost:${PORT_PROXY}"
