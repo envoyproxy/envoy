3e6ef82ea66b26a43b04fb8964a4be853de311b1
Amit Saha <asaha@atlassian.com>
2022-10-10 17:08:02 +1100

    update verification script
    Signed-off-by: Amit Saha <asaha@atlassian.com>

--- examples/route-mirror/verify.sh
+++ examples/route-mirror/verify.sh
@@ -7,18 +7,20 @@ export PORT_PROXY="${FRONT_PROXY_PORT_PROXY:-11820}"
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
 
-run_log "Static mirror cluster: localhost:${PORT_PROXY}/service/1"
-wait_for 20 bash -c "responds_with \"Hello from behind Envoy (service 1)!\" http://localhost:${PORT_PROXY}/service/1"
+run_log "Make a request to the statically mirrored route"
+wait_for 10 bash -c "responds_with \"Hello from behind Envoy (service 1)!\" http://localhost:${PORT_PROXY}/service/1"
 
+run_log "View logs for the request mirrored by request header"
 docker-compose logs service1 | grep --quiet "Host: localhost:${PORT_PROXY}"
 docker-compose logs service1-mirror | grep --quiet "Host: localhost-shadow:${PORT_PROXY}"
 
 
-run_log "Mirror cluster via header: x-mirror-cluster localhost:${PORT_PROXY}/service/2"
+run_log "Make a request to the route mirrored by request header"
 responds_with \
     "Hello from behind Envoy (service 2)!" \
     "http://localhost:${PORT_PROXY}/service/2" \
     --header 'x-mirror-cluster: service2-mirror'
 
+run_log "View logs for the request mirrored by request header"
 docker-compose logs service2 | grep --quiet "Host: localhost:${PORT_PROXY}"
 docker-compose logs service2-mirror | grep --quiet "Host: localhost-shadow:${PORT_PROXY}"
