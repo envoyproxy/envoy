a0da675166e6b02664f43e6cfa95cdb2890a3c3e
asheryer <asheryer@amazon.com>
2022-10-29 10:39:53 +0000

    Fix formatting
    Signed-off-by: asheryer <asheryer@amazon.com>

--- test/extensions/filters/network/redis_proxy/redis_proxy_integration_test.cc
+++ test/extensions/filters/network/redis_proxy/redis_proxy_integration_test.cc
@@ -1322,15 +1322,15 @@ TEST_P(RedisProxyWithCommandStatsIntegrationTest, PipelinedTransactionTest) {
   initialize();
 
   std::array<FakeRawConnectionPtr, 1> fake_upstream_connection;
-  std::string transaction_commands = makeBulkStringArray({"MULTI"}) +
-                                     makeBulkStringArray({"set", "foo", "bar"}) +
-                                     makeBulkStringArray({"get", "foo"}) +
-                                     makeBulkStringArray({"exec"});
+  std::string transaction_commands =
+      makeBulkStringArray({"MULTI"}) + makeBulkStringArray({"set", "foo", "bar"}) +
+      makeBulkStringArray({"get", "foo"}) + makeBulkStringArray({"exec"});
   const std::string& response = "";
   IntegrationTcpClientPtr redis_client = makeTcpConnection(lookupPort("redis_proxy"));
   ASSERT_TRUE(redis_client->write(transaction_commands));
 
-  expectUpstreamRequestResponse(fake_upstreams_[0], transaction_commands, response, fake_upstream_connection[0]);
+  expectUpstreamRequestResponse(fake_upstreams_[0], transaction_commands, response,
+                                fake_upstream_connection[0]);
 
   redis_client->waitForData(response);
   EXPECT_EQ(response, redis_client->data());
