3ea63d73407f5af8992e20e1bf0fb4b481b71d13
Dhi Aurrahman <dio@rockybars.com>
2022-11-02 00:44:00 +0700

    tools, github: Add error handling and update README (#23772)
    * tools, github: Add error handling and update README
    
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- tools/github/write_current_source_version.py
+++ tools/github/write_current_source_version.py
@@ -9,10 +9,12 @@
 # tarball.
 
 import argparse
+import http
 import json
 import os
 import pathlib
 import sys
+import urllib.error
 import urllib.request
 
 if __name__ == "__main__":
@@ -74,8 +76,18 @@ if __name__ == "__main__":
         # To avoid rate-limited API calls.
         commit_info_request.add_header(
             "Authorization", f"{authorization_header_prefix} {github_token}")
-    with urllib.request.urlopen(commit_info_request) as response:
-        commit_info = json.loads(response.read())
-        source_version_file = project_root_dir.joinpath("SOURCE_VERSION")
-        # Write the extracted current version commit hash "sha" to SOURCE_VERSION.
-        source_version_file.write_text(commit_info["sha"])
+    try:
+        with urllib.request.urlopen(commit_info_request) as response:
+            commit_info = json.loads(response.read())
+            source_version_file = project_root_dir.joinpath("SOURCE_VERSION")
+            # Write the extracted current version commit hash "sha" to SOURCE_VERSION.
+            source_version_file.write_text(commit_info["sha"])
+    except urllib.error.HTTPError as e:
+        status_code = e.code
+        if e.code in (http.HTTPStatus.UNAUTHORIZED, http.HTTPStatus.FORBIDDEN):
+            print(
+                f"Please check the GitHub token provided in {args.github_api_token_env_name} environment variable. {e.reason}."
+            )
+            sys.exit(1)
+        else:
+            raise Exception(f"Failed since {e.reason} ({status_code}).")
