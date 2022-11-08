f19005ed6d15da351a500a444d37539ed60b8a4c
Boteng Yao <boteng@google.com>
2022-11-08 14:57:30 +0000

    fix format
    Signed-off-by: Boteng Yao <boteng@google.com>

--- source/common/config/decoded_resource_impl.h
+++ source/common/config/decoded_resource_impl.h
@@ -76,7 +76,9 @@ public:
   const Protobuf::Message& resource() const override { return *resource_; };
   bool hasResource() const override { return has_resource_; }
   absl::optional<std::chrono::milliseconds> ttl() const override { return ttl_; }
-  const OptRef<const envoy::config::core::v3::Metadata> metadata() const override { return metadata_; }
+  const OptRef<const envoy::config::core::v3::Metadata> metadata() const override {
+    return metadata_;
+  }
 
 private:
   DecodedResourceImpl(OpaqueResourceDecoder& resource_decoder, absl::optional<std::string> name,
