0104aad40c944e2c186708c9956bfe07fb8bd70f
Xie Zhihao <zhihao.xie@intel.com>
2022-11-09 09:27:42 +0000

    http: move forwarded port to req header only
    Signed-off-by: Xie Zhihao <zhihao.xie@intel.com>

--- envoy/http/header_map.h
+++ envoy/http/header_map.h
@@ -205,7 +205,8 @@ private:
   HEADER_FUNC(EnvoyMaxRetries)                                                                     \
   HEADER_FUNC(EnvoyUpstreamRequestTimeoutMs)                                                       \
   HEADER_FUNC(EnvoyUpstreamRequestPerTryTimeoutMs)                                                 \
-  HEADER_FUNC(EnvoyUpstreamStreamDurationMs)
+  HEADER_FUNC(EnvoyUpstreamStreamDurationMs)                                                       \
+  HEADER_FUNC(ForwardedPort)
 
 #define INLINE_REQ_HEADERS(HEADER_FUNC)                                                            \
   INLINE_REQ_STRING_HEADERS(HEADER_FUNC)                                                           \
@@ -249,8 +250,7 @@ private:
 
 #define INLINE_REQ_RESP_NUMERIC_HEADERS(HEADER_FUNC)                                               \
   HEADER_FUNC(ContentLength)                                                                       \
-  HEADER_FUNC(EnvoyAttemptCount)                                                                   \
-  HEADER_FUNC(ForwardedPort)
+  HEADER_FUNC(EnvoyAttemptCount)
 
 #define INLINE_REQ_RESP_HEADERS(HEADER_FUNC)                                                       \
   INLINE_REQ_RESP_STRING_HEADERS(HEADER_FUNC)                                                      \
