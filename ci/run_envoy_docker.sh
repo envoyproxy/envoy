30db197979ec1320741ae5f46cb32f9c860bfa21
Ryan Northey <ryan@synca.io>
2022-11-10 15:48:18 +0000

    ci/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ci/run_envoy_docker.sh
+++ ci/run_envoy_docker.sh
@@ -46,6 +46,8 @@ else
     && useradd -o --uid $(id -u) --gid ${DOCKER_GID} --no-create-home --home-dir /build envoybuild \
     && usermod -a -G pcap envoybuild \
     && chown envoybuild:envoygroup /build \
+    && ls -lh /dev/stderr \
+    && chown envoybuild /dev/stderr \
     && sudo -EHs -u envoybuild bash -c 'cd /source && $*'")
 fi
 
