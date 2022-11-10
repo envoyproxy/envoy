250a7c67a4573621c08bc144cb1031d749a4df28
Alyssa Wilk <alyssar@chromium.org>
2022-11-10 15:15:15 -0500

    ci
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

---
+++ ci/envoy_mobile_mirror.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+#ENVOY_SRCDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/../" && pwd)
+CHECKOUT_DIR=envoy-mobile
+#MAIN_BRANCH="refs/heads/main"
+ENVOY_MOBILE_MAIN_BRANCH="main"
+
+echo "Cloning..."
+git clone git@github.com:envoyproxy/envoy-mobile "$CHECKOUT_DIR" -b "${ENVOY_MOBILE_MAIN_BRANCH}"
+
+git -C "$CHECKOUT_DIR" config user.name "envoy-filter-example(Azure Pipelines)"
+git -C "$CHECKOUT_DIR" config user.email envoy-filter-example@users.noreply.github.com
+
+echo "Updating Submodule..."
+# Update submodule to latest Envoy SHA
+ENVOY_SHA=$(git rev-parse HEAD)
+git -C "$CHECKOUT_DIR" submodule update --init
+git -C "$CHECKOUT_DIR/envoy" checkout "$ENVOY_SHA"
+
+#echo "Updating Workspace file."
+#sed -e "s|{ENVOY_SRCDIR}|envoy|" "${ENVOY_SRCDIR}"/ci/WORKSPACE.filter.example > "${CHECKOUT_DIR}"/WORKSPACE
+
+echo "Committing, and Pushing..."
+git -C "$CHECKOUT_DIR" commit -a -m "Update Envoy submodule to $ENVOY_SHA"
+git -C "$CHECKOUT_DIR" push origin "${ENVOY_MOBILE_MAIN_BRANCH}"
+echo "Done"
