20a8f8d3b37427242e857ad855a85383bb20829b
Ryan Hamilton <rch@google.com>
2022-11-16 11:35:33 -0800

    Use std::string instead of const std::string& in C++ APIs where the string is being copied. (#2667)
    Use std::string instead of const std::string& in C++ APIs where the string is being copied.
    
    This offers the potential for a perf win because the caller can std::move() their string and avoid a copy.
    
    Risk Level: Low
    Testing: Existing
    Docs Changes: N/A
    Release Notes: N/A
    
    Signed-off-by: Ryan Hamilton <rch@google.com>
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/library/cc/headers_builder.h
+++ mobile/library/cc/headers_builder.h
@@ -9,17 +9,17 @@ class HeadersBuilder {
 public:
   virtual ~HeadersBuilder() {}
 
-  HeadersBuilder& add(const std::string& name, const std::string& value);
-  HeadersBuilder& set(const std::string& name, const std::vector<std::string>& values);
+  HeadersBuilder& add(std::string name, std::string value);
+  HeadersBuilder& set(std::string name, std::vector<std::string> values);
   HeadersBuilder& remove(const std::string& name);
 
 protected:
   HeadersBuilder();
-  HeadersBuilder& internalSet(const std::string& name, const std::vector<std::string>& values);
+  HeadersBuilder& internalSet(std::string name, std::vector<std::string> values);
   const RawHeaderMap& allHeaders() const;
 
 private:
-  bool isRestrictedHeader(const std::string& name) const;
+  bool isRestrictedHeader(absl::string_view name) const;
 
   RawHeaderMap headers_;
 };
