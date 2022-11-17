fcf0eb8838dbf758c4f8c1d4ec7aaffefab57806
wbpcode <wangbaiping@corp.netease.com>
2022-11-17 00:49:07 +0000

    fix format
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>

--- source/common/upstream/load_balancer_impl.cc
+++ source/common/upstream/load_balancer_impl.cc
@@ -638,10 +638,10 @@ ZoneAwareLoadBalancerBase::hostSourceToUse(LoadBalancerContext* context, uint64_
   // If we're doing locality weighted balancing, pick locality.
   //
   // The chooseDegradedLocality or chooseHealthyLocality may return valid locality index
-  // when the locality_weighted_lb_config is set or load balancing policy extensioin is used.
+  // when the locality_weighted_lb_config is set or load balancing policy extension is used.
   // This if statement is to make sure we only do locality weighted balancing when the
   // locality_weighted_lb_config is set explicitly even the hostSourceToUse is called in the
-  // load balancing policy extensioins.
+  // load balancing policy extensions.
   if (locality_weighted_balancing_) {
     absl::optional<uint32_t> locality;
     if (host_availability == HostAvailability::Degraded) {
