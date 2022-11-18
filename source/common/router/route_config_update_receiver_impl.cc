07ffcc370891e2efde9f11d9d8d95174ab006838
yurykats <yurykats@yahoo.com>
2022-11-17 19:41:53 -0500

    Improve RouteConfigUpdateReceiverImpl::onRdsUpdate performance when there is no VHDS
    Avoid unnecessary proto merging during RDS update if there is no VHDS.  Currently, if VHDS is not being used, RDS update clears are re-populates vhosts with the same set of hosts. 
    
    
    
    Signed-off-by: yurykats <yurykats@yahoo.com>

--- source/common/router/route_config_update_receiver_impl.cc
+++ source/common/router/route_config_update_receiver_impl.cc
@@ -63,7 +63,10 @@ bool RouteConfigUpdateReceiverImpl::onRdsUpdate(const Protobuf::Message& rc,
   for (const auto& vhost : new_route_config->virtual_hosts()) {
     rds_virtual_hosts.emplace(vhost.name(), vhost);
   }
-  rebuildRouteConfigVirtualHosts(rds_virtual_hosts, *vhds_virtual_hosts_, *new_route_config);
+  if(vhds_virtual_hosts_->size() > 0) {
+    // If there are vhosts that were provided by VHDS, merge them with RDS vhosts.
+    rebuildRouteConfigVirtualHosts(rds_virtual_hosts, *vhds_virtual_hosts_, *new_route_config);
+  }
   base_.updateConfig(std::move(new_route_config));
   base_.updateHash(new_hash);
   rds_virtual_hosts_ = std::move(rds_virtual_hosts);
