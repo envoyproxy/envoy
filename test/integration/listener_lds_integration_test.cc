2a92f9c2b59f945b1792b0d0643af0b32edc2c23
alyssawilk <alyssar@chromium.org>
2022-11-21 14:56:05 -0500

    test: fixing lds flake (#24071)
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- test/integration/listener_lds_integration_test.cc
+++ test/integration/listener_lds_integration_test.cc
@@ -566,6 +566,12 @@ TEST_P(ListenerIntegrationTest, MultipleLdsUpdatesSharingListenSocketFactory) {
   test_server_->waitForCounterGe("listener_manager.listener_create_success", 1);
   // Make a connection to the listener from version 1.
   codec_client_ = makeHttpConnection(lookupPort(listener_name_));
+  // Ensure Envoy has accepted the connection before starting reloads.
+  if (version_ == Network::Address::IpVersion::v4) {
+    test_server_->waitForCounterGe("listener.127.0.0.1_0.downstream_cx_total", 1);
+  } else {
+    test_server_->waitForCounterGe("listener.[__1]_0.downstream_cx_total", 1);
+  }
 
   for (int version = 2; version <= 10; version++) {
     // Touch the metadata to get a different hash.
