d46c1639aafb92ced3c32170bf547fb224626b68
Greg Brail <gbrail@users.noreply.github.com>
2022-11-22 16:03:23 -0800

    Clean up partial buffering code a bit more.
    Remove some duplicated code, and simplify logic.
    
    Signed-off-by: Greg Brail <gbrail@users.noreply.github.com>

--- source/extensions/filters/http/ext_proc/ext_proc.cc
+++ source/extensions/filters/http/ext_proc/ext_proc.cc
@@ -339,6 +339,9 @@ FilterDataStatus Filter::onData(ProcessorState& state, Buffer::Instance& data, b
     if (state.partialBodyProcessed()) {
       // We already sent and received the buffer, so everything else just falls through.
       ENVOY_LOG(trace, "Partial buffer limit reached");
+      // Make sure that we do not accidentally try to modify the headers before
+      // we continue, which will result in them possibly being sent.
+      state.setHeaders(nullptr);
       result = FilterDataStatus::Continue;
     } else if (state.callbackState() ==
                ProcessorState::CallbackState::BufferedPartialBodyCallback) {
@@ -348,28 +351,14 @@ FilterDataStatus Filter::onData(ProcessorState& state, Buffer::Instance& data, b
       ENVOY_LOG(trace, "Call in progress for partial mode");
       state.setPaused(true);
       result = FilterDataStatus::StopIterationNoBuffer;
-    } else if (end_stream || state.queueOverHighLimit()) {
-      bool terminate;
-      FilterDataStatus chunk_result;
-      std::tie(terminate, chunk_result) = sendStreamChunk(state, data, end_stream);
-
-      if (terminate) {
-        return chunk_result;
-      }
-      result = FilterDataStatus::StopIterationNoBuffer;
     } else {
-      // Keep on running and buffering
       state.enqueueStreamingChunk(data, false, false);
-
-      if (state.queueOverHighLimit()) {
-        // The filter will send the queued data if it was invoked again, or if
-        // we explicitly kick it off. It might not happen right away, so we opt for the latter,
-        // explicitly kicking it off.
+      if (end_stream || state.queueOverHighLimit()) {
+        // At either end of stream or when the buffer is full, it's time to send what we have
+        // to the processor.
         bool terminate;
         FilterDataStatus chunk_result;
-        Buffer::OwnedImpl empty_buffer{};
-        std::tie(terminate, chunk_result) = sendStreamChunk(state, empty_buffer, false);
-
+        std::tie(terminate, chunk_result) = sendStreamChunk(state, end_stream);
         if (terminate) {
           return chunk_result;
         }
@@ -400,8 +389,8 @@ FilterDataStatus Filter::onData(ProcessorState& state, Buffer::Instance& data, b
   return result;
 }
 
-std::pair<bool, Http::FilterDataStatus>
-Filter::sendStreamChunk(ProcessorState& state, Buffer::Instance& data, bool end_stream) {
+std::pair<bool, Http::FilterDataStatus> Filter::sendStreamChunk(ProcessorState& state,
+                                                                bool end_stream) {
   switch (openStream()) {
   case StreamOpenState::Error:
     return {true, FilterDataStatus::StopIterationNoBuffer};
@@ -411,7 +400,7 @@ Filter::sendStreamChunk(ProcessorState& state, Buffer::Instance& data, bool end_
     // Fall through
     break;
   }
-  state.enqueueStreamingChunk(data, false, false);
+
   // Put all buffered data so far into one big buffer
   const auto& all_data = state.consolidateStreamedChunks(true);
   ENVOY_LOG(debug, "Sending {} bytes of data in buffered partial mode. end_stream = {}",
