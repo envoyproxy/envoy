eb9a6fd54dfb0c5cf92635da2641f08856c5bd76
JP Simard <jp@jpsim.com>
2022-11-28 13:49:19 -0500

    Re-apply changes applied to previous import
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/tools/check_format.sh
+++ mobile/tools/check_format.sh
@@ -22,29 +22,9 @@ TARGET_PATH="$2"
 
 # TODO(mattklein123): WORKSPACE is excluded due to warning about @bazel_tools reference. Fix here
 #                     or in the upstream checker.
-
-FORMAT_ARGS=(
-    --config_path envoy/tools/code_format/config.yaml
-    --add-excluded-prefixes
-    ./envoy/ ./envoy_build_config/extensions_build_config.bzl ./WORKSPACE
-    ./Envoy.xcodeproj/ ./dist/ ./library/common/config_template.cc
-    ./bazel/envoy_mobile_swift_bazel_support.bzl
-    ./bazel/envoy_mobile_repositories.bzl
-    ./examples/swift/swiftpm/Packages/Envoy.xcframework
-    --skip_envoy_build_rule_check
-    "$ENVOY_FORMAT_ACTION")
-if [[ -n "$TARGET_PATH" ]]; then
-    FORMAT_ARGS+=("$TARGET_PATH")
-fi
-FORMAT_ARGS+=(
-    --namespace_check_excluded_paths
-    ./envoy ./examples/ ./library/java/ ./library/kotlin
-    ./library/objective-c ./test/java ./test/java
-    ./test/objective-c ./test/swift ./experimental/swift
-    --build_fixer_check_excluded_paths
-    ./envoy ./BUILD ./dist ./examples ./library/java
-    ./library/kotlin ./library/objective-c ./library/swift
-    ./library/common/extensions ./test/java ./test/kotlin ./test/objective-c
-    ./test/swift ./experimental/swift)
-
-ENVOY_BAZEL_PREFIX=@envoy envoy/tools/code_format/check_format.py "${FORMAT_ARGS[@]}"
+ENVOY_BAZEL_PREFIX=@envoy ../tools/code_format/check_format.py \
+    --config_path ../tools/code_format/config.yaml \
+    --add-excluded-prefixes ./envoy/ ./envoy_build_config/extensions_build_config.bzl ./WORKSPACE ./Envoy.xcodeproj/ ./dist/ ./library/common/config_template.cc ./bazel/envoy_mobile_swift_bazel_support.bzl ./bazel/envoy_mobile_repositories.bzl ./examples/swift/swiftpm/Packages/Envoy.xcframework \
+    --skip_envoy_build_rule_check "$ENVOY_FORMAT_ACTION" "$TARGET_PATH" \
+    --namespace_check_excluded_paths ./envoy ./examples/ ./library/java/ ./library/kotlin ./library/objective-c ./test/java ./test/java ./test/objective-c ./test/swift ./experimental/swift \
+    --build_fixer_check_excluded_paths ./envoy ./BUILD ./dist ./examples ./library/java ./library/kotlin ./library/objective-c ./library/swift ./library/common/extensions ./test/java ./test/kotlin ./test/objective-c ./test/swift ./experimental/swift
