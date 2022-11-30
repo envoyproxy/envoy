a2f78efdf32feb169568f448acbbe05caf96dd3a
wangfakang <fakangwang@gmail.com>
2022-11-30 23:39:08 +0800

    add integration tests for passthrough go filter.
    Signed-off-by: wangfakang <fakangwang@gmail.com>

--- contrib/golang/filters/http/test/golang_integration_test.cc
+++ contrib/golang/filters/http/test/golang_integration_test.cc
@@ -48,6 +48,7 @@ typed_config:
   }
 
   const std::string ECHO{"echo"};
+  const std::string PASSTHROUGH{"passthrough"};
 };
 
 INSTANTIATE_TEST_SUITE_P(IpVersions, GolangIntegrationTest,
@@ -71,11 +72,55 @@ TEST_P(GolangIntegrationTest, Echo) {
 
   ASSERT_TRUE(response->waitForEndStream());
 
-  // check resp status for echo
+  // check status for echo
   EXPECT_EQ("403", response->headers().getStatusValue());
 
   // check body for echo
   auto body = StringUtil::toUpper(absl::StrFormat("forbidden from go, path: %s\r\n", path));
+  EXPECT_EQ(body, StringUtil::toUpper(response->body()));
+
+  codec_client_->close();
+
+  if (fake_upstream_connection_ != nullptr) {
+    AssertionResult result = fake_upstream_connection_->close();
+    RELEASE_ASSERT(result, result.message());
+    result = fake_upstream_connection_->waitForDisconnect();
+    RELEASE_ASSERT(result, result.message());
+  }
+}
+
+TEST_P(GolangIntegrationTest, Passthrough) {
+  initializeConfig(PASSTHROUGH, genSoPath(PASSTHROUGH), PASSTHROUGH);
+  initialize();
+  registerTestServerPorts({"http"});
+
+  auto path = "/";
+  auto good = "good";
+  auto bye = "bye";
+  codec_client_ = makeHttpConnection(makeClientConnection(lookupPort("http")));
+  Http::TestRequestHeaderMapImpl request_headers{
+      {":method", "POST"}, {":path", path}, {":scheme", "http"}, {":authority", "test.com"}};
+
+  auto encoder_decoder = codec_client_->startRequest(request_headers);
+  Http::RequestEncoder& request_encoder = encoder_decoder.first;
+  auto response = std::move(encoder_decoder.second);
+  codec_client_->sendData(request_encoder, "helloworld", true);
+
+  waitForNextUpstreamRequest();
+  Http::TestResponseHeaderMapImpl response_headers{{":status", "200"}};
+  upstream_request_->encodeHeaders(response_headers, false);
+  Buffer::OwnedImpl response_data1(good);
+  upstream_request_->encodeData(response_data1, false);
+  Buffer::OwnedImpl response_data2(bye);
+  upstream_request_->encodeData(response_data2, true);
+
+  ASSERT_TRUE(response->waitForEndStream());
+
+  // check status for pasthrough
+  EXPECT_EQ("200", response->headers().getStatusValue());
+
+  // check body for pasthrough
+  auto body = StringUtil::toUpper(absl::StrFormat("%s%s", good, bye));
   EXPECT_EQ(body, StringUtil::toUpper(response->body()));
 
   codec_client_->close();
