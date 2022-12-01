9f1283f9d991aaee6056f692d1ed19927ec342b8
tyxia <72890320+tyxia@users.noreply.github.com>
2022-12-01 15:10:06 -0500

    rate_limit_quota: Quota based rate limiting, part 1 (#22135)
    Signed-off-by: Tianyu Xia <tyxia@google.com>

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -53,6 +53,7 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/extensions/filters/http/lua:96.4"
 "source/extensions/filters/http/oauth2:96.0"
 "source/extensions/filters/http/wasm:1.9"
+"source/extensions/filters/http/rate_limit_quota:70" # TODO(tyxia) WIP. Improve coverage as development continues
 "source/extensions/filters/listener:95.9"
 "source/extensions/filters/listener/http_inspector:95.8"
 "source/extensions/filters/listener/original_dst:82.4"
