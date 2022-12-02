9e0c47a37d306cdb31c671cce29ccad8c6cb1f9c
Felix Du <felix.du@nokia-sbell.com>
2022-12-02 02:42:05 +0000

    fix ut
    Signed-off-by: Felix Du <felix.du@nokia-sbell.com>

--- contrib/sip_proxy/filters/network/test/cache_manager_test.cc
+++ contrib/sip_proxy/filters/network/test/cache_manager_test.cc
@@ -10,7 +10,7 @@ namespace SipProxy {
 TEST(CacheManagerTest, FindTypeNotExist) {
   auto cache_manager = CacheManager<std::string, std::string, std::string>();
   auto result = cache_manager.at("empty", "empty");
-  EXPECT_EQ("", result);
+  EXPECT_EQ(absl::nullopt, result);
   EXPECT_EQ(false, cache_manager.contains("empty", "empty"));
 }
 
@@ -19,7 +19,7 @@ TEST(CacheManagerTest, FindKeyNotExist) {
   cache_manager.initCache("lskpmc", 12);
   cache_manager.insertCache("lskpmc", "S3F2", "192.168.0.1");
   auto result = cache_manager.at("lskpmc", "fake");
-  EXPECT_EQ("", result);
+  EXPECT_EQ(absl::nullopt, result);
   EXPECT_EQ(true, cache_manager.contains("lskpmc", "S3F2"));
   EXPECT_EQ(false, cache_manager.contains("lskpmc", "fake"));
 }
@@ -28,7 +28,7 @@ TEST(CacheManagerTest, FindKeyExist) {
   auto cache_manager = CacheManager<std::string, std::string, std::string>();
   cache_manager.insertCache("lskpmc", "S3F2", "192.168.0.1");
   auto result = cache_manager.at("lskpmc", "S3F2");
-  EXPECT_EQ("192.168.0.1", result);
+  EXPECT_EQ("192.168.0.1", result.value().get());
   EXPECT_EQ(true, cache_manager.contains("lskpmc", "S3F2"));
 }
 
@@ -51,7 +51,7 @@ TEST(CacheManagerTest, CacheReplacement) {
 
   EXPECT_EQ(2, cache_manager["lskpmc"].size());
   auto result = cache_manager.at("lskpmc", "S3F2");
-  EXPECT_EQ("192.168.0.2", result);
+  EXPECT_EQ("192.168.0.2", result.value().get());
 }
 
 } // namespace SipProxy
