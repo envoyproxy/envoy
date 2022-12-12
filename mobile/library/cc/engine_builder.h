1adb2575d1868e24b335e857ebd7be4e113581f9
caschoener <schoener@google.com>
2022-12-12 18:16:48 +0000

    move to private
    Signed-off-by: caschoener <schoener@google.com>

--- mobile/library/cc/engine_builder.h
+++ mobile/library/cc/engine_builder.h
@@ -16,8 +16,6 @@ namespace Envoy {
 namespace Platform {
 
 class EngineBuilder {
-  friend Envoy::BaseClientIntegrationTest;
-
 public:
   EngineBuilder(std::string config_template);
   EngineBuilder();
@@ -74,6 +72,7 @@ protected:
   void setAdminAddressPathForTests(std::string admin) { admin_address_path_for_tests_ = admin; }
 
 private:
+  friend BaseClientIntegrationTest;
   struct NativeFilterConfig {
     NativeFilterConfig(std::string name, std::string typed_config)
         : name_(std::move(name)), typed_config_(std::move(typed_config)) {}
