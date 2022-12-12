9d4340c04fc3dacc3e304efddf52290673160ece
Rafal Augustyniak <raugustyniak@lyft.com>
2022-12-12 12:54:06 -0500

    lint fix
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>

--- mobile/envoy_build_config/extension_registry.cc
+++ mobile/envoy_build_config/extension_registry.cc
@@ -16,6 +16,7 @@
 #include "source/extensions/filters/network/http_connection_manager/config.h"
 #include "source/extensions/http/header_formatters/preserve_case/config.h"
 #include "source/extensions/http/original_ip_detection/xff/config.h"
+#include "source/extensions/listener_managers/listener_manager/listener_manager_impl.h"
 #include "source/extensions/network/dns_resolver/getaddrinfo/getaddrinfo.h"
 #include "source/extensions/request_id/uuid/config.h"
 #include "source/extensions/stat_sinks/metrics_service/config.h"
@@ -24,7 +25,6 @@
 #include "source/extensions/transport_sockets/tls/cert_validator/default_validator.h"
 #include "source/extensions/transport_sockets/tls/config.h"
 #include "source/extensions/upstreams/http/generic/config.h"
-#include "source/extensions/listener_managers/listener_manager/listener_manager_impl.h"
 
 #include "extension_registry_platform_additions.h"
 #include "library/common/extensions/cert_validator/platform_bridge/config.h"
