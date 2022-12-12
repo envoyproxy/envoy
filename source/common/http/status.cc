1bede3f817c5f56c6b57d45cb13713646b5d17a7
JP Simard <jp@jpsim.com>
2022-12-12 11:50:11 -0500

    fixup! Apply suggestions from code review
    Signed-off-by: JP Simard <jp@jpsim.com>

--- source/common/http/status.cc
+++ source/common/http/status.cc
@@ -42,7 +42,8 @@ struct PrematureResponsePayload : public EnvoyStatusPayload {
 
 template <typename T> void storePayload(absl::Status& status, const T& payload) {
   const T* allocated = new T(payload);
-  const absl::string_view sv = absl::string_view(reinterpret_cast<const char*>(allocated), sizeof(allocated));
+  const absl::string_view sv =
+      absl::string_view(reinterpret_cast<const char*>(allocated), sizeof(allocated));
   absl::Cord cord = absl::MakeCordFromExternal(sv, [allocated]() { delete allocated; });
   cord.Flatten(); // Flatten ahead of time for easier access later.
   status.SetPayload(EnvoyPayloadUrl, std::move(cord));
