433fdb3676b237a9c24718395e2e0e880654f045
Rafal Augustyniak <raugustyniak@lyft.com>
2022-12-12 10:27:53 -0500

    fixes
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>

--- mobile/envoy_build_config/extension_registry.cc
+++ mobile/envoy_build_config/extension_registry.cc
@@ -24,7 +24,7 @@
 #include "source/extensions/transport_sockets/tls/cert_validator/default_validator.h"
 #include "source/extensions/transport_sockets/tls/config.h"
 #include "source/extensions/upstreams/http/generic/config.h"
-#include "source/listener_manager/listener_manager_impl.h"
+#include "source/extensions/listener_managers/listener_manager/listener_manager_impl.h"
 
 #include "extension_registry_platform_additions.h"
 #include "library/common/extensions/cert_validator/platform_bridge/config.h"
