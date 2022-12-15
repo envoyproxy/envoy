81c5c7e41ad7152164ebde7846a709634471879e
kuochunghsu <kuochunghsu@pinterest.com>
2022-12-14 19:54:07 -0800

    fix conflict with #24406
    Signed-off-by: kuochunghsu <kuochunghsu@pinterest.com>

--- source/common/tcp/conn_pool.cc
+++ source/common/tcp/conn_pool.cc
@@ -100,7 +100,7 @@ void ActiveTcpClient::onEvent(Network::ConnectionEvent event) {
 
 void ActiveTcpClient::onIdleTimeout() {
   ENVOY_CONN_LOG(debug, "per client idle timeout", *connection_);
-  parent_.host()->cluster().trafficStats().upstream_cx_idle_timeout_.inc();
+  parent_.host()->cluster().trafficStats()->upstream_cx_idle_timeout_.inc();
   close();
 }
 
