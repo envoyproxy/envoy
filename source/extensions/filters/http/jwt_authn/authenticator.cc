4f172d223fbdff7d67a9ca68a809e49c3e11054f
danieldradware <117576776+danieldradware@users.noreply.github.com>
2022-12-15 12:23:29 +0200

    Author: danieldradware danield@radware.com Update authenticator.cc
    add writing to dynamic metadata to AuthenticatorImpl::doneWithStatus(const Status& status) while status != Status::Ok
    
    Signed-off-by: danieldradware <117576776+danieldradware@users.noreply.github.com>
    Signed-off-by: danield <danield@radware.com>

--- source/extensions/filters/http/jwt_authn/authenticator.cc
+++ source/extensions/filters/http/jwt_authn/authenticator.cc
@@ -373,6 +373,19 @@ void AuthenticatorImpl::doneWithStatus(const Status& status) {
   ENVOY_LOG(debug, "{}: JWT token verification completed with: {}", name(),
             ::google::jwt_verify::getStatusString(status));
 
+  if(Status::Ok != status) {
+    //Forward the failed status to dynamic metadata
+
+    if ((jwks_data_ != nullptr) &&
+        !jwks_data_->getJwtProvider().failed_status_in_metadata().empty()) {
+      ProtobufWkt::Struct failed_status;
+      auto& failed_status_fields = *failed_status.mutable_fields();
+      failed_status_fields["status"].set_string_value(std::to_string(status));
+      ENVOY_LOG(debug, "Writing to metada failure reason: {}",google::jwt_verify::getStatusString(enumToInt(status)));
+      set_extracted_jwt_data_cb_(jwks_data_->getJwtProvider().failed_status_in_metadata(), failed_status);
+    }
+  }
+  
   // If a request has multiple tokens, all of them must be valid. Otherwise it may have
   // following security hole: a request has a good token and a bad one, it will pass
   // verification, forwarded to the backend, and the backend may mistakenly use the bad
