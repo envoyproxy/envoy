3d48a498e07d0582f95bd46733d6e3079a97a013
Raven Black <ravenblack@dropbox.com>
2022-12-20 19:52:32 +0000

    Encapsulate the comment a bit better
    Signed-off-by: Raven Black <ravenblack@dropbox.com>

--- source/common/grpc/codec.h
+++ source/common/grpc/codec.h
@@ -103,10 +103,10 @@ protected:
   virtual void frameDataEnd() {}
 
   State state_{State::FhFlag};
-  // Note that this union does not rely on bytes being positioned accordingly for a
-  // uint32_t, it merely shares the storage. absl::big_endian is used to deserialize
-  // the bytes correctly once they are populated.
   union {
+    // Note that this union does not rely on bytes being arranged accurately for a
+    // uint32_t, it merely shares the storage. absl::big_endian is used to deserialize
+    // the bytes correctly once they are populated.
     uint32_t length_{0};
     uint8_t length_as_bytes_[4];
   };
