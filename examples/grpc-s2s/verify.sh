16f2687354182690b04147901f7ca7293c2dd90d
Amit Saha <asaha@atlassian.com>
2022-12-28 14:03:47 +1100

    Simplify the verification script
    Signed-off-by: Amit Saha <asaha@atlassian.com>

--- examples/grpc-s2s/verify.sh
+++ examples/grpc-s2s/verify.sh
@@ -7,31 +7,26 @@ export PORT_PROXY="${FRONT_PROXY_PORT_PROXY:-12000}"
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
 # TODO network=host
-run_log "Make an example request"
+run_log "Make an example request to Hello which will call World as well"
 docker run --network=host fullstorydev/grpcurl -plaintext "localhost:${PORT_PROXY}" Hello/Greet
+
+run_log "Query healthy instances for envoy: Hello"
+curl -s "http://localhost:9090/stats" | grep -q "cluster.hello.health_check.healthy: 2"
+
+run_log "Query healthy instances for envoy: World"
+curl -s "http://localhost:9091/stats" | grep -q "cluster.world.health_check.healthy: 2"
 
 run_log "Render an instance of Hello unhealthy"
-docker-compose exec --index 1 hello date
-docker-compose exec --index 1 hello ip addr
 docker-compose exec --index 1 hello kill -SIGUSR1 1
 
-# To allow the host to be evicted
-sleep 1
-curl "http://localhost:9090/clusters" | grep failed_active_hc
-
-docker run --network=host fullstorydev/grpcurl -plaintext "localhost:${PORT_PROXY}" Hello/Greet
-
 run_log "Render an instance of World unhealthy"
-docker-compose exec --index 1 world date
-docker-compose exec --index 1 world ip addr
 docker-compose exec --index 1 world kill -SIGUSR1 1
 
-# To allow the host to be evicted
-sleep 1
-curl "http://localhost:9091/clusters" | grep failed_active_hc
+run_log "Ensure that we now only have one healthy instance of Hello"
+curl -s "http://localhost:9090/stats" | grep -q "cluster.hello.health_check.healthy: 1"
 
+run_log "Ensure that we now only have one healthy instance of World"
+curl -s "http://localhost:9091/stats" | grep -q "cluster.world.health_check.healthy: 1"
+
+run_log "Make a request to Hello which will call World as well"
 docker run --network=host fullstorydev/grpcurl -plaintext "localhost:${PORT_PROXY}" Hello/Greet
-
-run_log "Query healthcheck metrics"
-curl "http://localhost:9090/stats" | grep cluster.hello.health_check
-curl "http://localhost:9091/stats" | grep cluster.world.health_check
