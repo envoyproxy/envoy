4403305beb1b64487dad362971e8704d0958bb15
Amit Saha <asaha@atlassian.com>
2022-12-28 14:27:57 +1100

    Update verification script to remove usage of hardcoded ports
    Signed-off-by: Amit Saha <asaha@atlassian.com>

--- examples/grpc-s2s/verify.sh
+++ examples/grpc-s2s/verify.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
 export NAME=grpc-s2s
-export PORT_PROXY="${FRONT_PROXY_PORT_PROXY:-12000}"
+export PORT_PROXY="${GRPC_S2S_PORT_PROXY:-12000}"
+export PORT_ADMIN_HELLO="${GRPC_S2S_HELLO_PORT_ADMIN:-12800}"
+export PORT_ADMIN_WORLD="${GRPC_S2S_WORLD_PORT_ADMIN:-12801}"
 
 # shellcheck source=examples/verify-common.sh
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
@@ -11,10 +13,10 @@ run_log "Make an example request to Hello which will call World as well"
 docker run --network=host fullstorydev/grpcurl -plaintext "localhost:${PORT_PROXY}" Hello/Greet
 
 run_log "Query healthy instances for envoy: Hello"
-curl -s "http://localhost:9090/stats" | grep -q "cluster.hello.health_check.healthy: 2"
+curl -s "http://localhost:${PORT_ADMIN_HELLO}/stats" | grep -q "cluster.hello.health_check.healthy: 2"
 
 run_log "Query healthy instances for envoy: World"
-curl -s "http://localhost:9091/stats" | grep -q "cluster.world.health_check.healthy: 2"
+curl -s "http://localhost:${PORT_ADMIN_WORLD}/stats" | grep -q "cluster.world.health_check.healthy: 2"
 
 run_log "Render an instance of Hello unhealthy"
 docker-compose exec --index 1 hello kill -SIGUSR1 1
@@ -23,10 +25,10 @@ run_log "Render an instance of World unhealthy"
 docker-compose exec --index 1 world kill -SIGUSR1 1
 
 run_log "Ensure that we now only have one healthy instance of Hello"
-curl -s "http://localhost:9090/stats" | grep -q "cluster.hello.health_check.healthy: 1"
+curl -s "http://localhost:${PORT_ADMIN_HELLO}/stats" | grep -q "cluster.hello.health_check.healthy: 1"
 
 run_log "Ensure that we now only have one healthy instance of World"
-curl -s "http://localhost:9091/stats" | grep -q "cluster.world.health_check.healthy: 1"
+curl -s "http://localhost:${PORT_ADMIN_WORLD}/stats" | grep -q "cluster.world.health_check.healthy: 1"
 
 run_log "Make a request to Hello which will call World as well"
 docker run --network=host fullstorydev/grpcurl -plaintext "localhost:${PORT_PROXY}" Hello/Greet
