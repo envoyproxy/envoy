1da31c0790e6622452e51720b518b19e79745609
Ryan Hamilton <rch@google.com>
2023-01-11 21:34:49 +0000

    Add // IWYU: export to log_level.h
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- mobile/library/cc/log_level.h
+++ mobile/library/cc/log_level.h
@@ -7,7 +7,7 @@
 namespace Envoy {
 namespace Platform {
 
-using LogLevel = Envoy::Logger::Logger::Levels;
+using LogLevel = Envoy::Logger::Logger::Levels;  // IWYU: export
 
 std::string logLevelToString(LogLevel method);
 LogLevel logLevelFromString(const std::string& str);
