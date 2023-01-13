ccf46ae9e921f1071d305883ef0e4fb6c5a29e0a
JP Simard <jp@jpsim.com>
2023-01-13 13:28:52 -0500

    Call `register_test_extensions()` in all Swift integration tests
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/test/swift/integration/IdleTimeoutTest.swift
+++ mobile/test/swift/integration/IdleTimeoutTest.swift
@@ -1,9 +1,15 @@
 import Envoy
 import EnvoyEngine
 import Foundation
+import TestExtensions
 import XCTest
 
 final class IdleTimeoutTests: XCTestCase {
+  override static func setUp() {
+    super.setUp()
+    register_test_extensions()
+  }
+
   func testIdleTimeout() {
     let idleTimeout = "0.5s"
     let remotePort = Int.random(in: 10001...11000)
