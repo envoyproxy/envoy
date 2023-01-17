3adde441e140cbfecda9a58dee0e853f6eddb2ec
Ryan Hamilton <rch@google.com>
2023-01-17 06:38:30 -0800

    Remove redundant "this->" in Envoy Mobile code (#24954)
    Remove redundant "this->" in Envoy Mobile code
    
    Risk Level: Low
    Testing: Existing
    Docs Changes: N/A
    Release Notes: N/A
    
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- mobile/library/cc/stream.cc
+++ mobile/library/cc/stream.cc
@@ -12,25 +12,23 @@ Stream::Stream(envoy_engine_t engine_handle, envoy_stream_t handle)
 
 Stream& Stream::sendHeaders(RequestHeadersSharedPtr headers, bool end_stream) {
   envoy_headers raw_headers = rawHeaderMapAsEnvoyHeaders(headers->allHeaders());
-  ::send_headers(this->engine_handle_, this->handle_, raw_headers, end_stream);
+  ::send_headers(engine_handle_, handle_, raw_headers, end_stream);
   return *this;
 }
 
 Stream& Stream::sendData(envoy_data data) {
-  ::send_data(this->engine_handle_, this->handle_, data, false);
+  ::send_data(engine_handle_, handle_, data, false);
   return *this;
 }
 
 void Stream::close(RequestTrailersSharedPtr trailers) {
   envoy_headers raw_headers = rawHeaderMapAsEnvoyHeaders(trailers->allHeaders());
-  ::send_trailers(this->engine_handle_, this->handle_, raw_headers);
+  ::send_trailers(engine_handle_, handle_, raw_headers);
 }
 
-void Stream::close(envoy_data data) {
-  ::send_data(this->engine_handle_, this->handle_, data, true);
-}
+void Stream::close(envoy_data data) { ::send_data(engine_handle_, handle_, data, true); }
 
-void Stream::cancel() { reset_stream(this->engine_handle_, this->handle_); }
+void Stream::cancel() { reset_stream(engine_handle_, handle_); }
 
 } // namespace Platform
 } // namespace Envoy
