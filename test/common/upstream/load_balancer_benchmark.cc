f07e8102c5b437bfbd96a8bc8a9156d5e3bcff55
DiazAlan <109677874+DiazAlan@users.noreply.github.com>
2023-01-19 11:26:29 -0500

    upstream: lb policy configuration memory optimization (#24905)
    Changed how some of these configs are stored to be unique_ptr instead of optionals. This will help save memory in cases where some of these policies are not set or used.
    
    Signed-off-by: AlanDiaz <diazalan@google.com>

--- test/common/upstream/load_balancer_benchmark.cc
+++ test/common/upstream/load_balancer_benchmark.cc
@@ -16,6 +16,7 @@
 #include "test/mocks/upstream/cluster_info.h"
 #include "test/test_common/simulated_time_system.h"
 
+#include "absl/types/optional.h"
 #include "benchmark/benchmark.h"
 
 namespace Envoy {
@@ -154,9 +155,13 @@ public:
   RingHashTester(uint64_t num_hosts, uint64_t min_ring_size) : BaseTester(num_hosts) {
     config_ = envoy::config::cluster::v3::Cluster::RingHashLbConfig();
     config_.value().mutable_minimum_ring_size()->set_value(min_ring_size);
-    ring_hash_lb_ =
-        std::make_unique<RingHashLoadBalancer>(priority_set_, stats_, *stats_store_.rootScope(),
-                                               runtime_, random_, config_, common_config_);
+    ring_hash_lb_ = std::make_unique<RingHashLoadBalancer>(
+        priority_set_, stats_, stats_store_, runtime_, random_,
+        config_.has_value()
+            ? makeOptRef<const envoy::config::cluster::v3::Cluster::RingHashLbConfig>(
+                  config_.value())
+            : absl::nullopt,
+        common_config_);
   }
 
   absl::optional<envoy::config::cluster::v3::Cluster::RingHashLbConfig> config_;
@@ -167,9 +172,12 @@ class MaglevTester : public BaseTester {
 public:
   MaglevTester(uint64_t num_hosts, uint32_t weighted_subset_percent = 0, uint32_t weight = 0)
       : BaseTester(num_hosts, weighted_subset_percent, weight) {
-    maglev_lb_ =
-        std::make_unique<MaglevLoadBalancer>(priority_set_, stats_, *stats_store_.rootScope(),
-                                             runtime_, random_, config_, common_config_);
+    maglev_lb_ = std::make_unique<MaglevLoadBalancer>(
+        priority_set_, stats_, stats_store_, runtime_, random_,
+        config_.has_value()
+            ? makeOptRef<const envoy::config::cluster::v3::Cluster::MaglevLbConfig>(config_.value())
+            : absl::nullopt,
+        common_config_);
   }
 
   absl::optional<envoy::config::cluster::v3::Cluster::MaglevLbConfig> config_;
