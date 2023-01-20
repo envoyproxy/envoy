07a8cd8c3bca658534b5b133c9ba5ce8a3b03129
AlanDiaz <diazalan@google.com>
2023-01-20 15:39:35 -0500

    Address comment and make change for coverage
    Signed-off-by: AlanDiaz <diazalan@google.com>

--- source/common/upstream/load_balancer_impl.h
+++ source/common/upstream/load_balancer_impl.h
@@ -686,48 +686,7 @@ protected:
 
 private:
   void refreshHostSource(const HostsSource&) override {}
-  double hostWeight(const Host& host) override {
-    // This method is called to calculate the dynamic weight as following when all load balancing
-    // weights are not equal:
-    //
-    // `weight = load_balancing_weight / (active_requests + 1)^active_request_bias`
-    //
-    // `active_request_bias` can be configured via runtime and its value is cached in
-    // `active_request_bias_` to avoid having to do a runtime lookup each time a host weight is
-    // calculated.
-    //
-    // When `active_request_bias == 0.0` we behave like `RoundRobinLoadBalancer` and return the
-    // host weight without considering the number of active requests at the time we do the pick.
-    //
-    // When `active_request_bias > 0.0` we scale the host weight by the number of active
-    // requests at the time we do the pick. We always add 1 to avoid division by 0.
-    //
-    // It might be possible to do better by picking two hosts off of the schedule, and selecting the
-    // one with fewer active requests at the time of selection.
-
-    double host_weight = static_cast<double>(host.weight());
-
-    // If the value of active requests is the max value, adding +1 will overflow
-    // it and cause a divide by zero. This won't happen in normal cases but stops
-    // failing fuzz tests
-    auto active_request_value =
-        host.stats().rq_active_.value() != std::numeric_limits<uint64_t>::max()
-            ? host.stats().rq_active_.value() + 1
-            : host.stats().rq_active_.value();
-
-    if (active_request_bias_ == 1.0) {
-      host_weight = static_cast<double>(host.weight()) / active_request_value;
-    } else if (active_request_bias_ != 0.0) {
-      host_weight =
-          static_cast<double>(host.weight()) / std::pow(active_request_value, active_request_bias_);
-    }
-
-    if (!noHostsAreInSlowStart()) {
-      return applySlowStartFactor(host_weight, host);
-    } else {
-      return host_weight;
-    }
-  }
+  double hostWeight(const Host& host) override;
   HostConstSharedPtr unweightedHostPeek(const HostVector& hosts_to_use,
                                         const HostsSource& source) override;
   HostConstSharedPtr unweightedHostPick(const HostVector& hosts_to_use,
