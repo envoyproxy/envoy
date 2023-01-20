2b3304395653997187d4205a562fbcca4fea4ed2
sky <fakangwang@gmail.com>
2023-01-20 13:30:37 +0800

    Update examples/golang/verify.sh
    Co-authored-by: phlax <phlax@users.noreply.github.com>
    Signed-off-by: sky <fakangwang@gmail.com>

--- examples/golang/verify.sh
+++ examples/golang/verify.sh
@@ -20,7 +20,7 @@ responds_with \
     "forbidden from go, path: /localreply" \
     "http://localhost:8080/localreply"
 
-run_log "Test golang plugin for status"
+run_log "Test golang plugin for response status"
 responds_with_header \
     "HTTP/1.1 403 Forbidden" \
     "http://localhost:8080/localreply"
