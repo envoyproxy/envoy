e271c15c12f170a0bd619ce446704c812ac22682
doujiang24 <doujiang24@gmail.com>
2023-01-20 22:50:28 +0800

    invoke the gofmt once.
    Signed-off-by: doujiang24 <doujiang24@gmail.com>

--- tools/code_format/check_format_go_code.sh
+++ tools/code_format/check_format_go_code.sh
@@ -2,18 +2,21 @@
 
 set -e
 
+GOFMT_BIN="bazel run @go_sdk//:bin/gofmt -- "
+
 tools="$(dirname "$(dirname "$(realpath "$0")")")"
 root=$(realpath "$tools/..")
 
 cd "$root" || exit 1
 
-gofmt="@go_sdk//:bin/gofmt"
+# all go files
+go_files=$(find . -name "*.go")
 
 # only get the filenames which not satisfy gofmt
-files=$(find . -name "*.go" -exec bazel run $gofmt -- -l {} \;)
+files=$($GOFMT_BIN -l $go_files)
 if [[ $files != "" ]]; then
   # write changes to original files, so that we can get the changes by git diff.
-  find . -name "*.go" -exec bazel run $gofmt -- -w {} \;
+  $GOFMT_BIN -w $go_files
   echo "ERROR: files not satisfy gofmt:"
   echo "$files"
   exit 1
