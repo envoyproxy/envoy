865717f025b4ba391cfd7113820fdbc2a8eecee1
Ryan Northey <ryan@synca.io>
2023-01-25 19:41:50 +0000

    test/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- test/config_test/example_configs_test_setup.sh
+++ test/config_test/example_configs_test_setup.sh
@@ -5,9 +5,8 @@ set -e
 DIR="$TEST_TMPDIR"/test/config_test
 mkdir -p "$DIR"
 
-echo "UNTAR $TEST_SRCDIR/$EXAMPLE_CONFIGS_TAR_PATH -> $DIR"
-
-tar -xvf "$TEST_SRCDIR"/"$EXAMPLE_CONFIGS_TAR_PATH" -C "$DIR"
+# Windows struggles with its own paths, so we have to help it out with `--force-local`
+tar --force-local -xvf "$TEST_SRCDIR"/"$EXAMPLE_CONFIGS_TAR_PATH" -C "$DIR"
 
 # find uses full path to prevent using Windows find on Windows.
 /usr/bin/find "$DIR" -type f | grep -c .yaml > "$TEST_TMPDIR"/config-file-count.txt
