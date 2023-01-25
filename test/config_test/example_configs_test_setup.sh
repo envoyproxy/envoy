1f4ce502ee19b1bd6cbd94148f4eb03a06fa91b9
Ryan Northey <ryan@synca.io>
2023-01-25 17:59:49 +0000

    test/
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- test/config_test/example_configs_test_setup.sh
+++ test/config_test/example_configs_test_setup.sh
@@ -4,7 +4,12 @@ set -e
 
 DIR="$TEST_TMPDIR"/test/config_test
 mkdir -p "$DIR"
+
+echo "UNTAR $TEST_SRCDIR/$EXAMPLE_CONFIGS_TAR_PATH -> $DIR"
+
 tar -xvf "$TEST_SRCDIR"/"$EXAMPLE_CONFIGS_TAR_PATH" -C "$DIR"
 
 # find uses full path to prevent using Windows find on Windows.
 /usr/bin/find "$DIR" -type f | grep -c .yaml > "$TEST_TMPDIR"/config-file-count.txt
+
+cat "$TEST_TMPDIR"/config-file-count.txt
