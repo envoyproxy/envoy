33ecb0b250e8eb918c9535683b73ae51d7cca69f
Alyssa Wilk <alyssar@chromium.org>
2023-01-26 09:04:37 -0500

    WIP
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

---
+++ mobile/library/common/extensions/filters/http/test_remote_response/filter.cc
@@ -0,0 +1,42 @@
+#include "library/common/extensions/filters/http/test_remote_response/filter.h"
+
+#include "envoy/server/filter_config.h"
+#include "envoy/http/header_map.h"
+#include "source/common/http/header_map_impl.h"
+
+#include "library/common/http/header_utility.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace HttpFilters {
+namespace TestRemoteResponse {
+
+Http::FilterHeadersStatus TestRemoteResponseFilter::decodeHeaders(Http::RequestHeaderMap&,
+                                                                  bool end_stream) {
+  if (end_stream) {
+    sendResponse();
+  }
+  return Http::FilterHeadersStatus::StopIteration;
+}
+Http::FilterDataStatus TestRemoteResponseFilter::decodeData(Buffer::Instance&, bool end_stream) {
+  if (end_stream) {
+    sendResponse();
+  }
+  return Http::FilterDataStatus::StopIterationAndBuffer;
+}
+Http::FilterTrailersStatus TestRemoteResponseFilter::decodeTrailers(Http::RequestTrailerMap&) {
+  sendResponse();
+  return Http::FilterTrailersStatus::StopIteration;
+}
+
+void TestRemoteResponseFilter::sendResponse() {
+  Http::ResponseHeaderMapPtr headers{Http::createHeaderMap<Http::ResponseHeaderMapImpl>(
+      {{Http::Headers::get().Status, "200"}})};
+  decoder_callbacks_->encodeHeaders(std::move(headers), true,
+                            StreamInfo::ResponseCodeDetails::get().ViaUpstream);
+}
+
+} // namespace TestRemoteResponse
+} // namespace HttpFilters
+} // namespace Extensions
+} // namespace Envoy
