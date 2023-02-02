8dcdf48b91287ae6edfc55838a0d40e3df5cddc6
wangfakang <fakangwang@gmail.com>
2023-02-02 20:29:21 +0800

    fixd build-entrypoint.sh.
    Signed-off-by: wangfakang <fakangwang@gmail.com>

--- examples/shared/build/build-entrypoint.sh
+++ examples/shared/build/build-entrypoint.sh
@@ -5,7 +5,8 @@ set -e
 if [[ $(id -u envoybuild) != "${BUILD_UID}" ]]; then
     usermod -u "${BUILD_UID}" envoybuild
     chown envoybuild /home/envoybuild
-    chown envoybuild /output
 fi
+
+chown envoybuild /output
 
 exec gosu envoybuild "$@"
