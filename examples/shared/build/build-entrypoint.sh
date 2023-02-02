8dc0d37dd22a2402cbef9c0f4434de724bda09f0
wangfakang <fakangwang@gmail.com>
2023-02-02 20:01:04 +0800

    chown owner for output dir.
    Signed-off-by: wangfakang <fakangwang@gmail.com>

--- examples/shared/build/build-entrypoint.sh
+++ examples/shared/build/build-entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 if [[ $(id -u envoybuild) != "${BUILD_UID}" ]]; then
     usermod -u "${BUILD_UID}" envoybuild
     chown envoybuild /home/envoybuild
+    chown envoybuild /output
 fi
 
 exec gosu envoybuild "$@"
