23362587e7477906ecd0537794e01f81ce31d527
Alyssa Wilk <alyssar@chromium.org>
2023-02-06 18:12:17 -0500

    fix up tests
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/common/quic/envoy_quic_client_session_test.cc
+++ test/common/quic/envoy_quic_client_session_test.cc
@@ -47,7 +47,7 @@ public:
                                   supported_versions, dispatcher, std::move(connection_socket),
                                   generator) {
     SetEncrypter(quic::ENCRYPTION_FORWARD_SECURE,
-                 std::make_unique<quic::NullEncrypter>(quic::Perspective::IS_CLIENT));
+                 std::make_unique<quic::test::TaggingEncrypter>(quic::ENCRYPTION_FORWARD_SECURE));
     SetDefaultEncryptionLevel(quic::ENCRYPTION_FORWARD_SECURE);
   }
 
