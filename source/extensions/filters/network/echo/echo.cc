e1b7100aaca4752dd71a4fc0ec9cad14ac23a8bd
Snow Pettersen <snowp@lyft.com>
2023-02-07 18:06:27 +0000

    remove dead code
    Signed-off-by: Snow Pettersen <snowp@lyft.com>

--- source/extensions/filters/network/echo/echo.cc
+++ source/extensions/filters/network/echo/echo.cc
@@ -8,16 +8,7 @@
 namespace Envoy {
 namespace Extensions {
 namespace NetworkFilters {
-namespace Echo {
-
-/* Network::FilterStatus EchoFilter::onData(Buffer::Instance& data, bool end_stream) { */
-/*   ENVOY_CONN_LOG(trace, "echo: got {} bytes", read_callbacks_->connection(), data.length()); */
-/*   read_callbacks_->connection().write(data, end_stream); */
-/*   ASSERT(0 == data.length()); */
-/*   return Network::FilterStatus::StopIteration; */
-/* } */
-
-} // namespace Echo
+namespace Echo {} // namespace Echo
 } // namespace NetworkFilters
 } // namespace Extensions
 } // namespace Envoy
