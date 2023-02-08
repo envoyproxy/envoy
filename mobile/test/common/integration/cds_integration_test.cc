f1dbb031b7fe5758831a6ef3dc0409c7406e50d9
caschoener <schoener@google.com>
2023-02-08 17:29:27 +0000

    remove flakiness
    Signed-off-by: caschoener <schoener@google.com>

--- mobile/test/common/integration/cds_integration_test.cc
+++ mobile/test/common/integration/cds_integration_test.cc
@@ -41,14 +41,12 @@ TEST_P(CdsIntegrationTest, Basic) {
       ClusterName, fake_upstreams_[0]->localAddress()->ip()->port(),
       Network::Test::getLoopbackAddressString(ipVersion()), "ROUND_ROBIN");
   initializeXdsStream();
-  int cluster_count = getGaugeValue("cluster_manager.active_clusters");
   // Do the initial compareDiscoveryRequest / sendDiscoveryResponse for cluster_1.
   EXPECT_TRUE(compareDiscoveryRequest(Config::TypeUrl::get().Cluster, "", {}, {}, {}, true));
   sendDiscoveryResponse<envoy::config::cluster::v3::Cluster>(Config::TypeUrl::get().Cluster,
                                                              {cluster1}, {cluster1}, {}, "55");
   // Wait for cluster to be added
   ASSERT_TRUE(waitForCounterGe("cluster_manager.cluster_added", 1));
-  ASSERT_EQ(cluster_count + 1, getGaugeValue("cluster_manager.active_clusters"));
 }
 
 } // namespace
