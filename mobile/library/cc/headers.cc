407b64bf3e1072c2bf028eb2dc986bd31c3eccff
Alyssa Wilk <alyssar@chromium.org>
2023-02-09 11:36:26 -0500

    fix
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- mobile/library/cc/headers.cc
+++ mobile/library/cc/headers.cc
@@ -9,7 +9,7 @@ Headers::const_iterator Headers::begin() const {
 
 Headers::const_iterator Headers::end() const { return Headers::const_iterator(allHeaders().end()); }
 
-const std::vector<std::string>& Headers::operator[](absl::string_view key) const {
+const std::vector<std::string>& Headers::operator[](std::string key) const {
   return headers_.at(key);
 }
 
