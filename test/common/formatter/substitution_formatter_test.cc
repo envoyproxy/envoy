9a9a244ddeb02db54d2810cb7328798d80c51c13
Boteng Yao <boteng@google.com>
2023-02-09 20:57:04 +0000

    fix mock
    Signed-off-by: Boteng Yao <boteng@google.com>

--- test/common/formatter/substitution_formatter_test.cc
+++ test/common/formatter/substitution_formatter_test.cc
@@ -888,22 +888,22 @@ TEST(SubstitutionFormatterTest, streamInfoFormatter) {
   }
 
   {
-    StreamInfoFormatter upstream_format("DOWNSTREAM_TRANSPORT_FAILURE_REASON");
+    StreamInfoFormatter listener_format("DOWNSTREAM_TRANSPORT_FAILURE_REASON");
     std::string downstream_transport_failure_reason = "TLS error";
     stream_info.setDownstreamTransportFailureReason(downstream_transport_failure_reason);
-    EXPECT_EQ("TLS_error", upstream_format.format(request_headers, response_headers,
+    EXPECT_EQ("TLS_error", listener_format.format(request_headers, response_headers,
                                                   response_trailers, stream_info, body));
-    EXPECT_THAT(upstream_format.formatValue(request_headers, response_headers, response_trailers,
+    EXPECT_THAT(listener_format.formatValue(request_headers, response_headers, response_trailers,
                                             stream_info, body),
                 ProtoEq(ValueUtil::stringValue("TLS_error")));
   }
   {
-    StreamInfoFormatter upstream_format("DOWNSTREAM_TRANSPORT_FAILURE_REASON");
+    StreamInfoFormatter listener_format("DOWNSTREAM_TRANSPORT_FAILURE_REASON");
     std::string downstream_transport_failure_reason;
     stream_info.setDownstreamTransportFailureReason(downstream_transport_failure_reason);
-    EXPECT_EQ(absl::nullopt, upstream_format.format(request_headers, response_headers,
+    EXPECT_EQ(absl::nullopt, listener_format.format(request_headers, response_headers,
                                                     response_trailers, stream_info, body));
-    EXPECT_THAT(upstream_format.formatValue(request_headers, response_headers, response_trailers,
+    EXPECT_THAT(listener_format.formatValue(request_headers, response_headers, response_trailers,
                                             stream_info, body),
                 ProtoEq(ValueUtil::nullValue()));
   }
