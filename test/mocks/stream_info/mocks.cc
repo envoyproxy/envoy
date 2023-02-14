0131d28c1e3014fdef9ad1793f9839f38a4da474
Boteng Yao <boteng@google.com>
2023-02-14 21:40:52 +0000

    back to my knowledge
    Signed-off-by: Boteng Yao <boteng@google.com>

--- test/mocks/stream_info/mocks.cc
+++ test/mocks/stream_info/mocks.cc
@@ -188,9 +188,8 @@ MockStreamInfo::MockStreamInfo()
       .WillByDefault(Invoke([this](absl::string_view failure_reason) {
         downstream_transport_failure_reason_ = std::string(failure_reason);
       }));
-  ON_CALL(*this, downstreamTransportFailureReason()).WillByDefault(Invoke([this]() {
-    return absl::string_view(downstream_transport_failure_reason_);
-  }));
+  ON_CALL(*this, downstreamTransportFailureReason())
+      .WillByDefault(ReturnPointee(&downstream_transport_failure_reason_));
 }
 
 MockStreamInfo::~MockStreamInfo() = default;
