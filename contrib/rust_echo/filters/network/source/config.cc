b1ebdd06fd2eab991ffd1627a34c292327932e64
Snow Pettersen <snowp@lyft.com>
2023-02-17 13:44:38 -0500

    fix
    Signed-off-by: Snow Pettersen <snowp@lyft.com>

--- contrib/rust_echo/filters/network/source/config.cc
+++ contrib/rust_echo/filters/network/source/config.cc
@@ -4,8 +4,8 @@
 #include "source/extensions/filters/network/common/factory_base.h"
 #include "source/extensions/filters/network/well_known_names.h"
 
-#include "contrib/envoy/extensions/filters/network/rust_echo/v3/echo.pb.h"
-#include "contrib/envoy/extensions/filters/network/rust_echo/v3/echo.pb.validate.h"
+#include "contrib/envoy/extensions/filters/network/rust_echo/v3alpha/echo.pb.h"
+#include "contrib/envoy/extensions/filters/network/rust_echo/v3alpha/echo.pb.validate.h"
 #include "contrib/rust_echo/filters/network/source/echo.h"
 
 namespace Envoy {
@@ -16,23 +16,23 @@ namespace Echo {
 /**
  * Config registration for the echo filter. @see NamedNetworkFilterConfigFactory.
  */
-class EchoFactory
-    : public Common::FactoryBase<envoy::extensions::filters::network::rust_echo::v3::RustEcho> {
+class EchoFactory : public Common::FactoryBase<
+                        envoy::extensions::filters::network::rust_echo::v3alpha::RustEcho> {
 public:
   EchoFactory() : FactoryBase("envoy.filters.network.rust_echo") {}
 
 private:
   Network::FilterFactoryCb createFilterFactoryFromProtoTyped(
-      const envoy::extensions::filters::network::rust_echo::v3::RustEcho&,
+      const envoy::extensions::filters::network::rust_echo::v3alpha::RustEcho&,
       Server::Configuration::FactoryContext&) override {
     return [](Network::FilterManager& filter_manager) -> void {
       filter_manager.addReadFilter(std::make_shared<Filter>());
     };
   }
 
-  bool
-  isTerminalFilterByProtoTyped(const envoy::extensions::filters::network::rust_echo::v3::RustEcho&,
-                               Server::Configuration::ServerFactoryContext&) override {
+  bool isTerminalFilterByProtoTyped(
+      const envoy::extensions::filters::network::rust_echo::v3alpha::RustEcho&,
+      Server::Configuration::ServerFactoryContext&) override {
     return true;
   }
 };
