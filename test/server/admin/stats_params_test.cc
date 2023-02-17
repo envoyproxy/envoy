599fd9c434cc7991462d352d1be0d83142d5f298
Shyam Sundar Govindaraj <shyirtt@gmail.com>
2023-02-17 20:45:38 +0530

    envoy_reloadable_features_admin_stats_filter_use_re2 deprecation (#25083)
    * envoy_reloadable_features_admin_stats_filter_use_re2 deprecation
    
    This change is to deprecate the runtime guard,
    envoy_reloadable_features_admin_stats_filter_use_re2 introduced when
    the default regex engine is set to Re2 version from the previous Ecmascript regexes.
    
    Signed-off-by: Shyam Govindaraj <shyam.govindaraj@shyamgo-ltma4mf.internal.salesforce.com>

--- test/server/admin/stats_params_test.cc
+++ test/server/admin/stats_params_test.cc
@@ -1,8 +1,6 @@
 #include "source/common/buffer/buffer_impl.h"
 #include "source/server/admin/stats_params.h"
 
-#include "test/test_common/test_runtime.h"
-
 #include "gtest/gtest.h"
 
 namespace Envoy {
@@ -56,19 +54,8 @@ TEST(StatsParamsTest, ParseParamsFilter) {
   Buffer::OwnedImpl response;
 
   {
-    TestScopedRuntime scoped_runtime;
-    scoped_runtime.mergeValues({{"envoy.reloadable_features.admin_stats_filter_use_re2", "false"}});
-
     StatsParams params;
     ASSERT_EQ(Http::Code::OK, params.parse("?filter=foo", response));
-    EXPECT_NE(nullptr, params.filter_);
-    EXPECT_EQ(nullptr, params.re2_filter_);
-  }
-
-  {
-    StatsParams params;
-    ASSERT_EQ(Http::Code::OK, params.parse("?filter=foo", response));
-    EXPECT_EQ(nullptr, params.filter_);
     EXPECT_NE(nullptr, params.re2_filter_);
   }
 }
