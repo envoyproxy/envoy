0398bfeba82c56e3c4ee91c955f8b929dd26cfc1
Joshua Marantz <jmarantz@google.com>
2023-02-20 11:53:46 -0500

    separate the dynamic test from the test infrastructure.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/server/admin/admin_test.js
+++ test/server/admin/admin_test.js
@@ -5,14 +5,72 @@
 
 
 /**
- * Renders a URL, and after 3 seconds delay, runs the 'tester' function.
+ * Master list of tests. Each test in a struct with a url, name, and test Function.
  */
-function testPage(url, name, tester) {
-  document.getElementById('test-results').textContent += name + ' ...';
+const test_list = [];
+
+
+/**
+ * Adds a new test to the test-list. These will be run after all test scripts
+ * load, from admin.html's call to runAllTests().
+ */
+function addTest(url, name, test_function) {
+  test_list.push({url: url, name: name, test_function: test_function});
+}
+
+
+/**
+ * Renders a URL, and after 3 seconds delay, runs the 'tester' function.
+ * Log whether that function failed (threw exception) or passed. Either
+ * way, when that completes resolve a returned promise so the next test can
+ * run.
+ */
+function runTest(url, name, tester) {
+  const results = document.getElementById('test-results');
+  results.textContent += name + ' ...';
   iframe = document.createElement("iframe");
   iframe.width = 800;
   iframe.height = 1000;
   iframe.src = url;
   document.body.appendChild(iframe);
-  window.setTimeout(() => tester(iframe), 3000);
+  return new Promise((resolve, reject) => {
+    window.setTimeout(() => {
+      try {
+        tester(iframe);
+        results.textContent += 'passed\n';
+      } catch (err) {
+        results.textContent += 'FAILED: ' + err + '\n';
+      }
+      iframe.parentElement.removeChild(iframe);
+      resolve(null);
+    },
+                      3000);
+  });
+}
+
+
+/**
+ * Checks for a condition, throwing an exception with a comment if it fails.
+ */
+function assertTrue(cond, comment) {
+  if (!cond) {
+    throw new Error(comment);
+  }
+}
+
+/**
+ * Runs all tests added via addTest() above.
+ */
+function runAllTests() {
+  addEventListener("DOMContentLoaded", () => {
+    const next_test = (test_index) => {
+      if (test_index < test_list.length) {
+        test = test_list[test_index];
+        runTest(test.url, test.name, test.test_function).then(() => {
+          next_test(test_index + 1);
+        });
+      }
+    };
+    next_test(0);
+  });
 }
