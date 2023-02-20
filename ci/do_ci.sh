ff591cae0815ef08e5583c66e86d9164062ccd10
phlax <phlax@users.noreply.github.com>
2023-02-20 09:35:13 +0000

    ci: Fix flaky test reporting (#25624)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -163,7 +163,7 @@ function bazel_contrib_binary_build() {
 }
 
 function run_process_test_result() {
-  if [[ -z "$CI_SKIP_PROCESS_TEST_RESULTS" ]] && [[ $(find "$TEST_TMPDIR" -name "*_attempt.xml" 2> /dev/null) ]]; then
+  if [[ -z "$CI_SKIP_PROCESS_TEST_RESULTS" ]] && [[ $(find "$TEST_TMPDIR" -name "*_attempt_*.xml" 2> /dev/null) ]]; then
       echo "running flaky test reporting script"
       bazel run "${BAZEL_BUILD_OPTIONS[@]}" //ci/flaky_test:process_xml "$CI_TARGET"
   else
