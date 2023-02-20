362de924d215bdf11b4e21a4818222c4af3ce3d3
doujiang24 <doujiang24@gmail.com>
2023-02-20 08:51:36 +0800

    cluster specifier test.
    Signed-off-by: doujiang24 <doujiang24@gmail.com>

--- contrib/golang/http/cluster_specifier/test/test_data/simple/config.go
+++ contrib/golang/http/cluster_specifier/test/test_data/simple/config.go
@@ -2,32 +2,40 @@ package main
 
 import (
 	xds "github.com/cncf/xds/go/xds/type/v3"
-	"github.com/envoyproxy/envoy/contrib/golang/filters/http/source/go/pkg/api"
-	"github.com/envoyproxy/envoy/contrib/golang/filters/http/source/go/pkg/http"
+	"github.com/envoyproxy/envoy/contrib/golang/common/go/registry"
+	"github.com/envoyproxy/envoy/contrib/golang/http/cluster_specifier/source/go/pkg/api"
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
-const Name = "echo"
-
 func init() {
-	http.RegisterHttpFilterConfigFactory(Name, ConfigFactory)
+	registry.RegisterClusterSpecifierFactory(configFactory)
+	registry.RegisterClusterSpecifierConfigParser(&parser{})
 }
 
-func ConfigFactory(config interface{}) api.StreamFilterFactory {
-	any, ok := config.(*anypb.Any)
-	if !ok {
-		return nil
-	}
+type parser struct {
+}
 
+func (p *parser) Parse(config *anypb.Any) interface{} {
 	configStruct := &xds.TypedStruct{}
-	if err := any.UnmarshalTo(configStruct); err != nil {
-		return nil
+	if err := config.UnmarshalTo(configStruct); err != nil {
+		panic(err)
 	}
-	return func(callbacks api.FilterCallbackHandler) api.StreamFilter {
-		return &filter{
-			callbacks: callbacks,
-			config:    configStruct.Value,
+	var conf pluginConfig
+	if cluster, ok := configStruct.Value.AsMap()["cluster"]; ok {
+		if clusterStr, ok := cluster.(string); ok {
+			conf.cluster = clusterStr
 		}
+	}
+	return &conf
+}
+
+func configFactory(config interface{}) api.ClusterSpecifier {
+	conf, ok := config.(*pluginConfig)
+	if !ok {
+		panic("invalid config type")
+	}
+	return &clusterSpecifier{
+		config: conf,
 	}
 }
 
