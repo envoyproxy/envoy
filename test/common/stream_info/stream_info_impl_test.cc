70a4c8674718757f88c3ae84ee889b62bac0e322
silverstar195 <seanmaloney@google.com>
2023-02-21 17:43:35 +0000

    Format
    Signed-off-by: silverstar195 <seanmaloney@google.com>

--- test/common/stream_info/stream_info_impl_test.cc
+++ test/common/stream_info/stream_info_impl_test.cc
@@ -37,7 +37,8 @@ std::chrono::nanoseconds checkDuration(std::chrono::nanoseconds last,
 class StreamInfoImplTest : public testing::Test {
 protected:
   void assertStreamInfoSize(StreamInfoImpl stream_info) {
-    ASSERT_TRUE(sizeof(stream_info) == 800 || sizeof(stream_info) == 816 || sizeof(stream_info) == 824 || sizeof(stream_info) == 840 ||
+    ASSERT_TRUE(sizeof(stream_info) == 800 || sizeof(stream_info) == 816 ||
+                sizeof(stream_info) == 824 || sizeof(stream_info) == 840 ||
                 sizeof(stream_info) == 872 || sizeof(stream_info) == 920)
         << "If adding fields to StreamInfoImpl, please check to see if you "
            "need to add them to setFromForRecreateStream or setFrom! Current size "
