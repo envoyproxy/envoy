07f78c92039028319c0240b582ba7c9233c29508
botengyao <boteng@google.com>
2023-02-21 03:18:20 -0500

    access log: add downstream transport failure reason (#25322)
    Signed-off-by: Boteng Yao <boteng@google.com>

--- source/extensions/listener_managers/listener_manager/active_stream_listener_base.cc
+++ source/extensions/listener_managers/listener_manager/active_stream_listener_base.cc
@@ -112,6 +112,7 @@ void ActiveTcpConnection::onEvent(Network::ConnectionEvent event) {
   // Any event leads to destruction of the connection.
   if (event == Network::ConnectionEvent::LocalClose ||
       event == Network::ConnectionEvent::RemoteClose) {
+    stream_info_->setDownstreamTransportFailureReason(connection_->transportFailureReason());
     active_connections_.listener_.removeConnection(*this);
   }
 }
