ec6469ef79e74adb2f41c5da2815c5bacd47e5c1
AlanDiaz <diazalan@google.com>
2023-02-21 17:38:34 -0500

    Add comment to seperate smaller members of the class
    Signed-off-by: AlanDiaz <diazalan@google.com>

--- source/common/upstream/cluster_manager_impl.h
+++ source/common/upstream/cluster_manager_impl.h
@@ -377,6 +377,7 @@ protected:
       const HostVector hosts_removed_;
       PrioritySet::UpdateHostsParams update_hosts_params_;
       LocalityWeightsConstSharedPtr locality_weights_;
+      // Keep small members (bools and enums) at the end of class, to reduce alignment overhead.
       const uint32_t priority_;
       uint32_t overprovisioning_factor_;
     };
