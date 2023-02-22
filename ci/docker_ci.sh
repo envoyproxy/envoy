4186ab7e72432905c0b6b70d283dbf1c0cd32100
phlax <phlax@users.noreply.github.com>
2023-02-22 20:25:20 +0000

    docker: Unify image names (part2) (#25726)
    Include `tools` and `google-vrp` images as variants.
    
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ci/docker_ci.sh
+++ ci/docker_ci.sh
@@ -194,7 +194,7 @@ fi
 for BUILD_TYPE in "${BUILD_TYPES[@]}"; do
   build_images "${BUILD_TYPE}" "$(image_tag_name "${BUILD_TYPE}")"
 
-  if ! is_windows && [[ "${BUILD_TYPE}" != *-google-vrp  && "${BUILD_TYPE}" != *-tools ]]; then
+  if ! is_windows; then
       build_images "${BUILD_TYPE}" "$(new_image_tag_name "${BUILD_TYPE}")"
   fi
 done
@@ -213,7 +213,7 @@ fi
 
 for BUILD_TYPE in "${BUILD_TYPES[@]}"; do
   push_images "${BUILD_TYPE}" "$(image_tag_name "${BUILD_TYPE}")"
-  if ! is_windows && [[ "${BUILD_TYPE}" != *-google-vrp  && "${BUILD_TYPE}" != *-tools ]]; then
+  if ! is_windows; then
       push_images "${BUILD_TYPE}" "$(new_image_tag_name "${BUILD_TYPE}")"
   fi
 
@@ -221,7 +221,7 @@ for BUILD_TYPE in "${BUILD_TYPES[@]}"; do
   if [[ "${AZP_BRANCH}" == "${MAIN_BRANCH}" ]]; then
     is_windows && docker tag "${DOCKER_IMAGE_PREFIX}${BUILD_TYPE}${IMAGE_POSTFIX}:${IMAGE_NAME}" "${DOCKER_IMAGE_PREFIX}${BUILD_TYPE}${IMAGE_POSTFIX}:latest"
     push_images "${BUILD_TYPE}" "$(image_tag_name "${BUILD_TYPE}" latest)"
-    if ! is_windows && [[ "${BUILD_TYPE}" != *-google-vrp  && "${BUILD_TYPE}" != *-tools ]]; then
+    if ! is_windows; then
         push_images "${BUILD_TYPE}" "$(new_image_tag_name "${BUILD_TYPE}" latest)"
     fi
   fi
@@ -231,7 +231,7 @@ for BUILD_TYPE in "${BUILD_TYPES[@]}"; do
     RELEASE_LINE=$(echo "$IMAGE_NAME" | sed -E 's/(v[0-9]+\.[0-9]+)\.[0-9]+/\1-latest/')
     is_windows && docker tag "${DOCKER_IMAGE_PREFIX}${BUILD_TYPE}${IMAGE_POSTFIX}:${IMAGE_NAME}" "${DOCKER_IMAGE_PREFIX}${BUILD_TYPE}${IMAGE_POSTFIX}:${RELEASE_LINE}"
     push_images "${BUILD_TYPE}" "$(image_tag_name "${BUILD_TYPE}" "${RELEASE_LINE}")"
-    if ! is_windows && [[ "${BUILD_TYPE}" != *-google-vrp  && "${BUILD_TYPE}" != *-tools ]]; then
+    if ! is_windows; then
         push_images "${BUILD_TYPE}" "$(new_image_tag_name "${BUILD_TYPE}" "${RELEASE_LINE}")"
     fi
   fi
