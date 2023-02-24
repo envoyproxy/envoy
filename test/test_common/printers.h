7074908d0783c14520b5988d067ff0d9cccceffd
Kevin Baichoo <kbaichoo@google.com>
2023-02-24 19:34:34 +0000

    Restore printer, add no lint to match with GoogleTest.
    Signed-off-by: Kevin Baichoo <kbaichoo@google.com>

--- test/test_common/printers.h
+++ test/test_common/printers.h
@@ -11,15 +11,18 @@ namespace Http {
  * Pretty print const HeaderMapImpl&
  */
 class HeaderMapImpl;
-void printTo(const HeaderMapImpl& headers, std::ostream* os);
+// NOLINTNEXTLINE(readability-identifier-naming)
+void PrintTo(const HeaderMapImpl& headers, std::ostream* os);
 
 /**
  * Pretty print const HeaderMapPtr&
  */
 class HeaderMap;
 using HeaderMapPtr = std::unique_ptr<HeaderMap>;
-void printTo(const HeaderMap& headers, std::ostream* os);
-void printTo(const HeaderMapPtr& headers, std::ostream* os);
+// NOLINTNEXTLINE(readability-identifier-naming)
+void PrintTo(const HeaderMap& headers, std::ostream* os);
+// NOLINTNEXTLINE(readability-identifier-naming)
+void PrintTo(const HeaderMapPtr& headers, std::ostream* os);
 } // namespace Http
 
 namespace Buffer {
@@ -27,18 +30,21 @@ namespace Buffer {
  * Pretty print const Instance&
  */
 class Instance;
-void printTo(const Instance& buffer, std::ostream* os);
+// NOLINTNEXTLINE(readability-identifier-naming)
+void PrintTo(const Instance& buffer, std::ostream* os);
 
 /**
  * Pretty print const Buffer::OwnedImpl&
  */
 class OwnedImpl;
-void printTo(const OwnedImpl& buffer, std::ostream* os);
+// NOLINTNEXTLINE(readability-identifier-naming)
+void PrintTo(const OwnedImpl& buffer, std::ostream* os);
 } // namespace Buffer
 
 namespace Network {
 namespace Address {
-void printTo(const Instance& address, std::ostream* os);
-}
+// NOLINTNEXTLINE(readability-identifier-naming)
+void PrintTo(const Instance& address, std::ostream* os);
+} // namespace Address
 } // namespace Network
 } // namespace Envoy
