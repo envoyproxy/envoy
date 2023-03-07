433c9fde2cd33700cd87167f17b6070c0b803a4d
Joshua Marantz <jmarantz@google.com>
2023-03-07 21:48:46 +0000

    admin: Add admin format that sorts stats by activity rate while the page is open (#25586)
    Adds a new admin stats format "active-html", which opens a JavaScript assisted UI to show
    stats ordered by frequency of change, over the duration the page is open.
    
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/server/admin/html/generate_admin_html.sh
+++ source/server/admin/html/generate_admin_html.sh
@@ -10,3 +10,9 @@ cat "$2"
 echo '</style>'
 echo '</head>'
 echo ')EOF";'
+echo 'constexpr absl::string_view AdminActiveStatsJs = R"EOF('
+cat "$3"
+echo ')EOF";'
+echo 'constexpr absl::string_view AdminActiveParamsHtml = R"EOF('
+cat "$4"
+echo ')EOF";'
