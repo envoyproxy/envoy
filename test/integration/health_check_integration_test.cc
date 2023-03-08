a7f38e6522f0b9b9d1d224b9d08bb6fbea28bfc1
bozhang <bozhang@ebay.com>
2023-03-08 05:54:19 -0700

    add gtest_repeat to reproduce the flake issue
    Signed-off-by: bozhang <bozhang@ebay.com>

--- test/integration/health_check_integration_test.cc
+++ test/integration/health_check_integration_test.cc
@@ -687,8 +687,9 @@ TEST_P(TcpHealthCheckIntegrationTest, DisableHCForActiveTraffic) {
   std::cout << "failure: "
             << test_server_->counter("cluster.cluster_1.health_check.failure")->value()
             << std::endl;
-  test_server_->waitForCounterEq("cluster.cluster_1.health_check.success", 2,
-                                 std::chrono::milliseconds(1));
+  // test_server_->waitForCounterEq("cluster.cluster_1.health_check.success", 2,
+  //                              std::chrono::milliseconds(1));
+  test_server_->waitForCounterEq("cluster.cluster_1.health_check.success", 2);
 
   std::cout << "current time:" << timeSystem().monotonicTime().time_since_epoch().count()
             << std::endl;
@@ -705,7 +706,7 @@ TEST_P(TcpHealthCheckIntegrationTest, DisableHCForActiveTraffic) {
   std::cout << "failure: "
             << test_server_->counter("cluster.cluster_1.health_check.failure")->value()
             << std::endl;
-  // EXPECT_EQ(0, test_server_->counter("cluster.cluster_1.health_check.failure")->value());
+  EXPECT_EQ(0, test_server_->counter("cluster.cluster_1.health_check.failure")->value());
 }
 
 // Tests that an invalid response fails the health check.
