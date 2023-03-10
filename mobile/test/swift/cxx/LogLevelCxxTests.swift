5106aff73f5cce8e4f5cbcfada417003ac828ce9
JP Simard <jp@jpsim.com>
2023-03-10 13:15:50 -0500

    Update LogLevelCxxTests
    Signed-off-by: JP Simard <jp@jpsim.com>

--- mobile/test/swift/cxx/LogLevelCxxTests.swift
+++ mobile/test/swift/cxx/LogLevelCxxTests.swift
@@ -4,12 +4,12 @@ import XCTest
 
 final class LogLevelCxxTests: XCTestCase {
   func testToCxxDescription() {
-    XCTAssertEqual(LogLevel.trace.toCXXDescription(), "T")
-    XCTAssertEqual(LogLevel.debug.toCXXDescription(), "D")
-    XCTAssertEqual(LogLevel.info.toCXXDescription(), "I")
-    XCTAssertEqual(LogLevel.warn.toCXXDescription(), "W")
-    XCTAssertEqual(LogLevel.error.toCXXDescription(), "E")
-    XCTAssertEqual(LogLevel.critical.toCXXDescription(), "C")
-    XCTAssertEqual(LogLevel.off.toCXXDescription(), "O")
+    XCTAssertEqual(LogLevel.trace.toCXXDescription(), "trace")
+    XCTAssertEqual(LogLevel.debug.toCXXDescription(), "debug")
+    XCTAssertEqual(LogLevel.info.toCXXDescription(), "info")
+    XCTAssertEqual(LogLevel.warn.toCXXDescription(), "warn")
+    XCTAssertEqual(LogLevel.error.toCXXDescription(), "error")
+    XCTAssertEqual(LogLevel.critical.toCXXDescription(), "critical")
+    XCTAssertEqual(LogLevel.off.toCXXDescription(), "off")
   }
 }
