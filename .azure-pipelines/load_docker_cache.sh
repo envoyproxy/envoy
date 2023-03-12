d9efed69b60bdae750a1bd9af6f6880c81098481
Ryan Northey <ryan@synca.io>
2023-03-12 16:51:30 +0000

    ci: Cache build image
    Signed-off-by: Ryan Northey <ryan@synca.io>

---
+++ .azure-pipelines/load_docker_cache.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+DOCKER_CACHE_PATH="$1"
+DOCKER_BIND_PATH="$2"
+
+if [[ -z "$DOCKER_CACHE_PATH" ]]; then
+    echo "load_docker_cache called without path arg" >&2
+    exit 1
+fi
+
+
+DOCKER_CACHE_TARBALL="${DOCKER_CACHE_PATH}/docker.tar.zst"
+
+echo "Stopping Docker daemon ..."
+systemctl stop docker
+mv /var/lib/docker/ /var/lib/docker.old
+mkdir -p /var/lib/docker
+
+if id -u vsts &> /dev/null && [[ -n "$DOCKER_BIND_PATH" ]]; then
+    # use separate disk on windows hosted
+    mkdir -p "$DOCKER_BIND_PATH"
+    mount -o bind "$DOCKER_BIND_PATH" /var/lib/docker
+fi
+
+echo "Extracting docker cache ${DOCKER_CACHE_TARBALL} ..."
+tar -I "zstd -d -T0 " -axf "$DOCKER_CACHE_TARBALL" -C /var/lib/docker
+echo "Starting Docker daemon ..."
+systemctl start docker
+umount "${DOCKER_CACHE_PATH}"
+docker images
+df -h
+
+# background this
+rm -rf /var/lib/docker.old &
