93aff371ca1d82c440c04e84dd5e3dcb7e67d738
caschoener <schoener@google.com>
2023-03-16 15:30:58 +0000

    2
    Signed-off-by: caschoener <schoener@google.com>

--- mobile/library/swift/EngineBuilder.swift
+++ mobile/library/swift/EngineBuilder.swift
@@ -618,7 +618,7 @@ open class EngineBuilder: NSObject {
 
   /// Adds a CDS layer to the configuration.
   ///
-  /// - parameter cdsResourcesLocator: The url for the CDS resources.
+  /// - parameter cdsResourcesLocator: The xdstp resource URI for fetching clusters.
   /// - parameter timeoutSeconds:      The timeout in seconds.
   ///
   /// - returns: This builder.
