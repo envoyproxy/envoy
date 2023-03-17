1dd666392aa34458caa932c00be629889c349788
asingh-g <abhisinghx@google.com>
2023-03-17 19:40:07 +0000

    Fix comment
    Signed-off-by: asingh-g <abhisinghx@google.com>

--- source/common/quic/envoy_quic_client_stream.h
+++ source/common/quic/envoy_quic_client_stream.h
@@ -79,8 +79,8 @@ private:
 
   bool decoded_1xx_{false};
 
-  // When an http upgrade is requested, this contains the protocol upgrade type, e.g. "websocket".
-  // it will be empty, when no such request is active
+  // When an Http Upgrade is requested, this contains the protocol upgrade type, e.g. "websocket".
+  // It will be empty, when no such request is active.
   std::string upgrade_type_;
 };
 
