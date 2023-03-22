28f950f581e5db925233c2df3f3ee981501b21ad
Alyssa Wilk <alyssar@chromium.org>
2023-03-22 11:10:26 -0400

    more improvements
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/test_common/utility.h
+++ test/test_common/utility.h
@@ -572,7 +572,8 @@ public:
   static std::string convertTime(const std::string& input, const std::string& input_format,
                                  const std::string& output_format);
 
-  static constexpr std::chrono::milliseconds DefaultTimeout = std::chrono::milliseconds(10000);
+  static constexpr std::chrono::milliseconds DefaultTimeout =
+      std::chrono::milliseconds(10000) * TSAN_TIMEOUT_FACTOR;
 
   /**
    * Return a prefix string matcher.
