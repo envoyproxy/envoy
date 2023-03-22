9785d96f7dcf47abe224024cbb80730e34dee15d
Ohad Vano <ohadvano@gmail.com>
2023-03-22 15:58:50 +0200

    Update integration tests
    Signed-off-by: Ohad Vano <ohadvano@gmail.com>

--- test/common/tcp_proxy/tcp_proxy_test.cc
+++ test/common/tcp_proxy/tcp_proxy_test.cc
@@ -980,19 +980,26 @@ TEST_F(TcpProxyTest, IntermediateLogEntry) {
 }
 
 TEST_F(TcpProxyTest, TestAccessLogOnUpstreamConnected) {
-  auto config = accessLogConfig("%UPSTREAM_HOST%");
+  auto config = accessLogConfig("%STREAM_STATE%");
   config.set_flush_access_log_on_connected(true);
 
   setup(1, config);
+
+  EXPECT_CALL(filter_callbacks_.connection_.stream_info_, streamState())
+      .WillOnce(Return(StreamInfo::StreamState::Started))
+      .WillOnce(Return(StreamInfo::StreamState::Ended));
+
   raiseEventUpstreamConnected(0);
 
   // Default access log will only be flushed after the stream is closed.
   // Passing the following check makes sure that the access log was flushed
   // before the stream was closed.
-  EXPECT_EQ(access_log_data_, "127.0.0.1:80");
+  EXPECT_EQ(access_log_data_.value(), StreamInfo::StreamStateStrings::get().StreamStarted);
 
   filter_callbacks_.connection_.raiseEvent(Network::ConnectionEvent::RemoteClose);
   filter_.reset();
+
+  EXPECT_EQ(access_log_data_.value(), StreamInfo::StreamStateStrings::get().StreamEnded);
 }
 
 TEST_F(TcpProxyTest, AccessLogUpstreamSSLConnection) {
