b61f65c0e050e9fcd295238bb36a41c736a94a21
Rafal Augustyniak <raugustyniak@lyft.com>
2023-03-24 14:59:49 -0400

    add docs to system proxy settings
    Signed-off-by: Rafal Augustyniak <raugustyniak@lyft.com>

--- mobile/library/common/network/system_proxy_settings.h
+++ mobile/library/common/network/system_proxy_settings.h
@@ -3,9 +3,23 @@
 namespace Envoy {
 namespace Network {
 
+/**
+ * System proxy settings. There are 2 ways to specify desired proxy settings. Either provide proxy's
+ * hostname or provide a URL to a Proxy Auto-Configuration (PAC) file.
+ */
 class SystemProxySettings {
 public:
+  /**
+   * Creates proxy settings using provided hostname and port.
+   * @param hostname A domain name or an IP address.
+   * @param port     A valid internet port from within [0-65535] range.
+   */
   SystemProxySettings(const std::string& hostname, int port) : hostname_(hostname), port_(port){};
+
+  /**
+   * Creates proxy settings using provided URL to a Proxy Auto-Configuration (PAC) file.
+   * @param pac_file_url A URL to a Proxy Auto-Configuration (PAC) file.
+   */
   SystemProxySettings(const std::string& pac_file_url) : port_(-1), pac_file_url_(pac_file_url){};
 
   std::string hostname() const { return hostname_; }
