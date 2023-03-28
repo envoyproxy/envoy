envoyproxy/envoy

commit 42e5b432033b392d6cfef74a7eab4c2cd2e1ec2f

android: log cleared pending exceptions to platform layer (#26133)
Commit Message: The main purpose of this PR is to add logging of the information about pending exceptions found by JNI layer to the platform layer. That should help us to understand better cases where the library crashes/misbehaves due to Java exception being thrown and not handled correctly by our JNI layer. 
Additional Description: I made a little bit of a refactor as I moved methods related to `JNIEnv` to their own c++ `Envoy::JNI::Env` class and methods related to `JavaVM` to their own `JavaVirtualMachine` c++ class. That helped me to introduce an `Envoy::JNI::String` class that makes working with `jstring` type easier and less error-prone (hopefully).
Risk Level: Low
Testing: Integration
Docs Changes:
Release Notes:
Platform Specific Features:
[Optional Runtime guard:]
[Optional Fixes #Issue]
[Optional Fixes commit #PR or SHA]
[Optional Deprecated:]
[Optional [API Considerations](https://github.com/envoyproxy/envoy/blob/main/api/review_checklist.md):]

mobile/library/common/jni/BUILD
@@ -30,15 +30,14 @@ cc_library(
     name = "jni_utility_lib",
     srcs = [
         "jni_utility.cc",
-        "jni_version.cc",
     ],
     hdrs = [
         "jni_utility.h",
-        "jni_version.h",
     ],
     deps = [
         ":jni_support_lib",
         "//library/common/jni/import:jni_import_lib",
+        "//library/common/jni/types:jni_env_lib",
         "//library/common/types:c_types_lib",
         "//library/common/types:managed_types_lib",
         "@envoy//source/common/common:assert_lib",
@@ -64,6 +63,8 @@ cc_library(
         "//library/common/api:c_types",
         "//library/common/extensions/cert_validator/platform_bridge:c_types_lib",
         "//library/common/extensions/key_value/platform:config",
+        "//library/common/jni/types:jni_exception_lib",
+        "//library/common/jni/types:jni_javavm_lib",
         "//library/common/types:managed_types_lib",
     ],
     # We need this to ensure that we link this into the .so even though there are no code references.
@@ -85,6 +86,7 @@ cc_library(
         "//library/common/data:utility_lib",
         "//library/common/extensions/cert_validator/platform_bridge:c_types_lib",
         "//library/common/jni/import:jni_import_lib",
+        "//library/common/jni/types:jni_javavm_lib",
         "//library/common/types:c_types_lib",
         "@envoy//bazel:boringssl",
     ],

mobile/library/common/jni/android_network_utility.cc
@@ -3,6 +3,7 @@
 #include "library/common/data/utility.h"
 #include "library/common/jni/jni_support.h"
 #include "library/common/jni/jni_utility.h"
+#include "library/common/jni/types/java_virtual_machine.h"
 #include "openssl/ssl.h"
 
 // NOLINT(namespace-envoy)
@@ -144,6 +145,8 @@ static envoy_cert_validation_result verify_x509_cert_chain(const envoy_data* cer
             "AndroidNetworkLibrary_verifyServerCertificates failed: not yet valid."};
   }
 }
+
+void jvm_detach_thread() { Envoy::JNI::JavaVirtualMachine::detachCurrentThread(); }
 
 envoy_cert_validator* get_android_cert_validator_api() {
   envoy_cert_validator* api = (envoy_cert_validator*)safe_malloc(sizeof(envoy_cert_validator));

mobile/library/common/jni/jni_utility.cc
@@ -6,17 +6,11 @@
 #include "source/common/common/assert.h"
 
 #include "library/common/jni/jni_support.h"
-#include "library/common/jni/jni_version.h"
+#include "library/common/jni/types/env.h"
 
 // NOLINT(namespace-envoy)
 
-static JavaVM* static_jvm = nullptr;
 static jobject static_class_loader = nullptr;
-static thread_local JNIEnv* local_env = nullptr;
-
-void set_vm(JavaVM* vm) { static_jvm = vm; }
-
-JavaVM* get_vm() { return static_jvm; }
 
 void set_class_loader(jobject class_loader) { static_class_loader = class_loader; }
 
@@ -38,23 +32,7 @@ jclass find_class(const char* class_name) {
   return clazz;
 }
 
-JNIEnv* get_env() {
-  if (local_env) {
-    return local_env;
-  }
-
-  jint result = static_jvm->GetEnv(reinterpret_cast<void**>(&local_env), JNI_VERSION);
-  if (result == JNI_EDETACHED) {
-    // Note: the only thread that should need to be attached is Envoy's engine std::thread.
-    static const char* thread_name = "EnvoyMain";
-    JavaVMAttachArgs args = {JNI_VERSION, const_cast<char*>(thread_name), nullptr};
-    result = attach_jvm(static_jvm, &local_env, &args);
-  }
-  RELEASE_ASSERT(result == JNI_OK, "Unable to get a JVM env for the current thread");
-  return local_env;
-}
-
-void jvm_detach_thread() { static_jvm->DetachCurrentThread(); }
+JNIEnv* get_env() { return Envoy::JNI::Env::get(); }
 
 void jni_delete_global_ref(void* context) {
   JNIEnv* env = get_env();
@@ -64,16 +42,6 @@ void jni_delete_global_ref(void* context) {
 
 void jni_delete_const_global_ref(const void* context) {
   jni_delete_global_ref(const_cast<void*>(context));
-}
-
-bool clear_pending_exceptions(JNIEnv* env) {
-  if (env->ExceptionCheck() == JNI_TRUE) {
-    env->ExceptionClear();
-    // TODO(Augustyniak): Log exception details.
-    return true;
-  } else {
-    return false;
-  }
 }
 
 int unbox_integer(JNIEnv* env, jobject boxedInteger) {

mobile/library/common/jni/types/BUILD
@@ -1,0 +1,74 @@
+load("@envoy//bazel:envoy_build_system.bzl", "envoy_mobile_defines", "envoy_package")
+
+licenses(["notice"])  # Apache 2
+
+envoy_package()
+
+cc_library(
+    name = "jni_exception_lib",
+    srcs = [
+        "exception.cc",
+    ],
+    hdrs = [
+        "exception.h",
+    ],
+    defines = envoy_mobile_defines("@envoy"),
+    deps = [
+        "//library/common/jni/import:jni_import_lib",
+        "//library/common/jni/types:jni_string_lib",
+        "@envoy//source/common/common:assert_lib",
+    ],
+    # We need this to ensure that we link this into the .so even though there are no code references.
+    alwayslink = True,
+)
+
+cc_library(
+    name = "jni_string_lib",
+    hdrs = [
+        "string.h",
+    ],
+    defines = envoy_mobile_defines("@envoy"),
+    deps = [
+        "//library/common/jni/import:jni_import_lib",
+        "//library/common/jni/types:jni_env_lib",
+    ],
+    # We need this to ensure that we link this into the .so even though there are no code references.
+    alwayslink = True,
+)
+
+cc_library(
+    name = "jni_env_lib",
+    srcs = [
+        "env.cc",
+    ],
+    hdrs = [
+        "env.h",
+    ],
+    defines = envoy_mobile_defines("@envoy"),
+    deps = [
+        "//library/common/jni:jni_support_lib",
+        "//library/common/jni/import:jni_import_lib",
+        "//library/common/jni/types:jni_javavm_lib",
+        "@envoy//source/common/common:assert_lib",
+    ],
+    # We need this to ensure that we link this into the .so even though there are no code references.
+    alwayslink = True,
+)
+
+cc_library(
+    name = "jni_javavm_lib",
+    srcs = [
+        "java_virtual_machine.cc",
+    ],
+    hdrs = [
+        "java_virtual_machine.h",
+    ],
+    defines = envoy_mobile_defines("@envoy"),
+    deps = [
+        "//library/common/jni:jni_support_lib",
+        "//library/common/jni/import:jni_import_lib",
+        "@envoy//source/common/common:assert_lib",
+    ],
+    # We need this to ensure that we link this into the .so even though there are no code references.
+    alwayslink = True,
+)

mobile/library/common/jni/android_jni_utility.cc
@@ -9,7 +9,6 @@
 #include "library/common/data/utility.h"
 #include "library/common/jni/import/jni_import.h"
 #include "library/common/jni/jni_support.h"
-#include "library/common/jni/jni_version.h"
 #include "library/common/jni/jni_utility.h"
 #endif
 

mobile/library/common/jni/jni_interface.cc
@@ -10,7 +10,8 @@
 #include "library/common/jni/import/jni_import.h"
 #include "library/common/jni/jni_support.h"
 #include "library/common/jni/jni_utility.h"
-#include "library/common/jni/jni_version.h"
+#include "library/common/jni/types/exception.h"
+#include "library/common/jni/types/java_virtual_machine.h"
 #include "library/common/main_interface.h"
 #include "library/common/types/managed_envoy_headers.h"
 
@@ -19,13 +20,12 @@ using Envoy::Platform::EngineBuilder;
 // NOLINT(namespace-envoy)
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
-  JNIEnv* env = nullptr;
-  if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION) != JNI_OK) {
-    return -1;
+  const auto result = Envoy::JNI::JavaVirtualMachine::initialize(vm);
+  if (result != JNI_OK) {
+    return result;
   }
 
-  set_vm(vm);
-  return JNI_VERSION;
+  return Envoy::JNI::JavaVirtualMachine::getJNIVersion();
 }
 
 // JniLibrary
@@ -73,7 +73,7 @@ static void jvm_on_exit(void*) {
   // needs to be detached is the engine thread.
   // This function is called from the context of the engine's
   // thread due to it being posted to the engine's event dispatcher.
-  jvm_detach_thread();
+  Envoy::JNI::JavaVirtualMachine::detachCurrentThread();
 }
 
 static void jvm_on_track(envoy_map events, const void* context) {
@@ -242,7 +242,7 @@ static void* jvm_on_headers(const char* method, const Envoy::Types::ManagedEnvoy
                                          end_stream ? JNI_TRUE : JNI_FALSE, j_stream_intel);
   // TODO(Augustyniak): Pass the name of the filter in here so that we can instrument the origin of
   // the JNI exception better.
-  bool exception_cleared = clear_pending_exceptions(env);
+  bool exception_cleared = Envoy::JNI::Exception::checkAndClear(method);
 
   env->DeleteLocalRef(j_stream_intel);
   env->DeleteLocalRef(jcls_JvmCallbackContext);
@@ -697,7 +697,7 @@ static void* call_jvm_on_complete(envoy_stream_intel stream_intel,
   jobject result =
       env->CallObjectMethod(j_context, jmid_onComplete, j_stream_intel, j_final_stream_intel);
 
-  clear_pending_exceptions(env);
+  Envoy::JNI::Exception::checkAndClear();
 
   env->DeleteLocalRef(j_stream_intel);
   env->DeleteLocalRef(j_final_stream_intel);
@@ -722,7 +722,7 @@ static void* call_jvm_on_error(envoy_error error, envoy_stream_intel stream_inte
   jobject result = env->CallObjectMethod(j_context, jmid_onError, error.error_code, j_error_message,
                                          error.attempt_count, j_stream_intel, j_final_stream_intel);
 
-  clear_pending_exceptions(env);
+  Envoy::JNI::Exception::checkAndClear();
 
   env->DeleteLocalRef(j_stream_intel);
   env->DeleteLocalRef(j_final_stream_intel);
@@ -756,7 +756,7 @@ static void* call_jvm_on_cancel(envoy_stream_intel stream_intel,
   jobject result =
       env->CallObjectMethod(j_context, jmid_onCancel, j_stream_intel, j_final_stream_intel);
 
-  clear_pending_exceptions(env);
+  Envoy::JNI::Exception::checkAndClear();
 
   env->DeleteLocalRef(j_stream_intel);
   env->DeleteLocalRef(j_final_stream_intel);

mobile/library/common/jni/jni_utility.h
@@ -9,10 +9,7 @@
 
 // NOLINT(namespace-envoy)
 
-void set_vm(JavaVM* vm);
-
-JavaVM* get_vm();
-
+// TODO(Augustyniak): Replace the usages of this global method with Envoy::JNI::Env::get()
 JNIEnv* get_env();
 
 void set_class_loader(jobject class_loader);
@@ -38,8 +35,6 @@ void set_class_loader(jobject class_loader);
  *         it couldn't be found.
  */
 jclass find_class(const char* class_name);
-
-void jvm_detach_thread();
 
 void jni_delete_global_ref(void* context);
 

mobile/library/common/jni/jni_version.cc
@@ -1,5 +1,0 @@
-#include "library/common/jni/jni_version.h"
-
-// NOLINT(namespace-envoy)
-
-const jint JNI_VERSION = JNI_VERSION_1_6;

commit cfd7d5695612f6be1ba3ac94e467550a0a973e6f

mobile: add more JNI exception checks (#26382)
Commit Message: Add more checks for pending exceptions while in the JNI layer. Log pending exceptions using the logging interfaces introduced in https://github.com/envoyproxy/envoy/pull/26133. This changes should allow us to identify the cause for Android crashes for when JNI platform cert validation is enabled.
Additional Description:
Risk Level: Low
Testing:
Docs Changes:
Release Notes:
Platform Specific Features:
[Optional Runtime guard:]
[Optional Fixes #Issue]
[Optional Fixes commit #PR or SHA]
[Optional Deprecated:]
[Optional [API Considerations](https://github.com/envoyproxy/envoy/blob/main/api/review_checklist.md):]

mobile/library/common/jni/BUILD
@@ -38,6 +38,7 @@ cc_library(
         ":jni_support_lib",
         "//library/common/jni/import:jni_import_lib",
         "//library/common/jni/types:jni_env_lib",
+        "//library/common/jni/types:jni_exception_lib",
         "//library/common/types:c_types_lib",
         "//library/common/types:managed_types_lib",
         "//library/common/types:matcher_data_lib",
@@ -87,6 +88,7 @@ cc_library(
         "//library/common/data:utility_lib",
         "//library/common/extensions/cert_validator/platform_bridge:c_types_lib",
         "//library/common/jni/import:jni_import_lib",
+        "//library/common/jni/types:jni_exception_lib",
         "//library/common/jni/types:jni_javavm_lib",
         "//library/common/types:c_types_lib",
         "@envoy//bazel:boringssl",

mobile/library/common/jni/android_network_utility.cc
@@ -3,6 +3,7 @@
 #include "library/common/data/utility.h"
 #include "library/common/jni/jni_support.h"
 #include "library/common/jni/jni_utility.h"
+#include "library/common/jni/types/exception.h"
 #include "library/common/jni/types/java_virtual_machine.h"
 #include "openssl/ssl.h"
 
@@ -16,8 +17,10 @@ bool jvm_cert_is_issued_by_known_root(JNIEnv* env, jobject result) {
       find_class("io.envoyproxy.envoymobile.utilities.AndroidCertVerifyResult");
   jmethodID jmid_isIssuedByKnownRoot =
       env->GetMethodID(jcls_AndroidCertVerifyResult, "isIssuedByKnownRoot", "()Z");
+  Envoy::JNI::Exception::checkAndClear("jvm_cert_is_issued_by_known_root:GetMethodID");
   ASSERT(jmid_isIssuedByKnownRoot);
   bool is_issued_by_known_root = env->CallBooleanMethod(result, jmid_isIssuedByKnownRoot);
+  Envoy::JNI::Exception::checkAndClear("jvm_cert_is_issued_by_known_root:CallBooleanMethod");
   env->DeleteLocalRef(jcls_AndroidCertVerifyResult);
   return is_issued_by_known_root;
 }
@@ -26,9 +29,11 @@ envoy_cert_verify_status_t jvm_cert_get_status(JNIEnv* env, jobject j_result) {
   jclass jcls_AndroidCertVerifyResult =
       find_class("io.envoyproxy.envoymobile.utilities.AndroidCertVerifyResult");
   jmethodID jmid_getStatus = env->GetMethodID(jcls_AndroidCertVerifyResult, "getStatus", "()I");
+  Envoy::JNI::Exception::checkAndClear("jvm_cert_get_status:GetMethodID");
   ASSERT(jmid_getStatus);
   envoy_cert_verify_status_t result = CERT_VERIFY_STATUS_FAILED;
   result = static_cast<envoy_cert_verify_status_t>(env->CallIntMethod(j_result, jmid_getStatus));
+  Envoy::JNI::Exception::checkAndClear("jvm_cert_get_status:CallIntMethod");
 
   env->DeleteLocalRef(jcls_AndroidCertVerifyResult);
   return result;
@@ -39,8 +44,10 @@ jobjectArray jvm_cert_get_certificate_chain_encoded(JNIEnv* env, jobject result)
       find_class("io.envoyproxy.envoymobile.utilities.AndroidCertVerifyResult");
   jmethodID jmid_getCertificateChainEncoded =
       env->GetMethodID(jcls_AndroidCertVerifyResult, "getCertificateChainEncoded", "()[[B");
+  Envoy::JNI::Exception::checkAndClear("jvm_cert_get_certificate_chain_encoded:GetMethodID");
   jobjectArray certificate_chain =
       static_cast<jobjectArray>(env->CallObjectMethod(result, jmid_getCertificateChainEncoded));
+  Envoy::JNI::Exception::checkAndClear("jvm_cert_get_certificate_chain_encoded:CallObjectMethod");
   env->DeleteLocalRef(jcls_AndroidCertVerifyResult);
   return certificate_chain;
 }
@@ -67,12 +74,14 @@ jobject call_jvm_verify_x509_cert_chain(JNIEnv* env, const std::vector<std::stri
   jmethodID jmid_verifyServerCertificates = env->GetStaticMethodID(
       jcls_AndroidNetworkLibrary, "verifyServerCertificates",
       "([[B[B[B)Lio/envoyproxy/envoymobile/utilities/AndroidCertVerifyResult;");
+  Envoy::JNI::Exception::checkAndClear("call_jvm_verify_x509_cert_chain:GetStaticMethodID");
   jobjectArray chain_byte_array = ToJavaArrayOfByteArray(env, cert_chain);
   jbyteArray auth_string = ToJavaByteArray(env, auth_type);
   jbyteArray host_string = ToJavaByteArray(env, host);
   jobject result =
       env->CallStaticObjectMethod(jcls_AndroidNetworkLibrary, jmid_verifyServerCertificates,
                                   chain_byte_array, auth_string, host_string);
+  Envoy::JNI::Exception::checkAndClear("call_jvm_verify_x509_cert_chain:CallStaticObjectMethod");
   env->DeleteLocalRef(chain_byte_array);
   env->DeleteLocalRef(auth_string);
   env->DeleteLocalRef(host_string);
@@ -88,17 +97,13 @@ static void jvm_verify_x509_cert_chain(const std::vector<std::string>& cert_chai
                                        std::vector<std::string>* verified_chain) {
   JNIEnv* env = get_env();
   jobject result = call_jvm_verify_x509_cert_chain(env, cert_chain, auth_type, host);
-  if (env->ExceptionCheck() == JNI_TRUE) {
-    env->ExceptionDescribe();
-    env->ExceptionClear();
+  if (Envoy::JNI::Exception::checkAndClear()) {
     *status = CERT_VERIFY_STATUS_NOT_YET_VALID;
   } else {
     ExtractCertVerifyResult(get_env(), result, status, is_issued_by_known_root, verified_chain);
-    if (env->ExceptionCheck() == JNI_TRUE) {
-      env->ExceptionDescribe();
-      env->ExceptionClear();
+    if (Envoy::JNI::Exception::checkAndClear()) {
       *status = CERT_VERIFY_STATUS_FAILED;
-    };
+    }
   }
   env->DeleteLocalRef(result);
 }

mobile/library/common/jni/jni_utility.cc
@@ -7,6 +7,7 @@
 
 #include "library/common/jni/jni_support.h"
 #include "library/common/jni/types/env.h"
+#include "library/common/jni/types/exception.h"
 
 // NOLINT(namespace-envoy)
 
@@ -23,11 +24,15 @@ jobject get_class_loader() {
 jclass find_class(const char* class_name) {
   JNIEnv* env = get_env();
   jclass class_loader = env->FindClass("java/lang/ClassLoader");
+  Envoy::JNI::Exception::checkAndClear("find_class:FindClass");
   jmethodID find_class_method =
       env->GetMethodID(class_loader, "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;");
+  Envoy::JNI::Exception::checkAndClear("find_class:GetMethodID");
   jstring str_class_name = env->NewStringUTF(class_name);
+  Envoy::JNI::Exception::checkAndClear("find_class:NewStringUTF");
   jclass clazz =
       (jclass)(env->CallObjectMethod(get_class_loader(), find_class_method, str_class_name));
+  Envoy::JNI::Exception::checkAndClear("find_class:CallObjectMethod");
   env->DeleteLocalRef(str_class_name);
   return clazz;
 }

mobile/test/kotlin/apps/experimental/MainActivity.kt
@@ -65,6 +65,7 @@ class MainActivity : Activity() {
       .enableSocketTagging(true)
       .enableProxying(true)
       .enableSkipDNSLookupForProxiedRequests(true)
+      .enablePlatformCertificatesValidation(true)
       .addNativeFilter("envoy.filters.http.buffer", "{\"@type\":\"type.googleapis.com/envoy.extensions.filters.http.buffer.v3.Buffer\",\"max_request_bytes\":5242880}")
       .addStringAccessor("demo-accessor", { "PlatformString" })
       .setOnEngineRunning { Log.d("MainActivity", "Envoy async internal setup completed") }