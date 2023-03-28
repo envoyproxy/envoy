f63c88af9af38e677ad76bb6653f3e3a62d06462
ohadvano <49730675+ohadvano@users.noreply.github.com>
2023-03-28 22:59:30 +0200

    Update tcp_proxy.cc
    Signed-off-by: ohadvano <49730675+ohadvano@users.noreply.github.com>

--- source/common/tcp_proxy/tcp_proxy.cc
+++ source/common/tcp_proxy/tcp_proxy.cc
@@ -822,16 +822,18 @@ void Filter::onMaxDownstreamConnectionDuration() {
 }
 
 void Filter::onAccessLogFlushInterval() {
-  if (getStreamInfo().streamState()) {
-    if (getStreamInfo().streamState() == StreamInfo::StreamState::InProgress) {
-      for (const auto& access_log : config_->accessLogs()) {
-        access_log->log(nullptr, nullptr, nullptr, getStreamInfo());
-      }
-    }
+  if (!getStreamInfo().streamState()) {
+    return;
+  }
 
-    if (getStreamInfo().streamState() != StreamInfo::StreamState::Ended) {
-      resetAccessLogFlushTimer();
+  if (getStreamInfo().streamState() == StreamInfo::StreamState::InProgress) {
+    for (const auto& access_log : config_->accessLogs()) {
+      access_log->log(nullptr, nullptr, nullptr, getStreamInfo());
     }
+  }
+
+  if (getStreamInfo().streamState() != StreamInfo::StreamState::Ended) {
+    resetAccessLogFlushTimer();
   }
 }
 
