05189fc29107d0af0f07edadb0db755fdb47aa20
Ryan Hamilton <rch@google.com>
2023-03-31 02:34:00 +0000

    Fix
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- mobile/library/common/network/apple_platform_cert_verifier.cc
+++ mobile/library/common/network/apple_platform_cert_verifier.cc
@@ -109,7 +109,8 @@ extern "C" {
 #endif
 
 void register_apple_platform_cert_verifier() {
-  envoy_cert_validator* api = static_cast<envoy_cert_validator*>(safe_malloc(sizeof(envoy_cert_validator));
+  envoy_cert_validator* api =
+      static_cast<envoy_cert_validator*>(safe_malloc(sizeof(envoy_cert_validator)));
   api->validate_cert = verify_cert;
   api->validation_cleanup = NULL;
   register_platform_api("platform_cert_validator", api);
