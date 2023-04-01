4cc34cdbdea28a7ba4b33e79421388fc5f86f5fd
wbpcode <wangbaiping@corp.netease.com>
2023-04-01 12:13:40 +0000

    initial interface update
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>

--- contrib/generic_proxy/filters/network/source/interface/codec_callbacks.h
+++ contrib/generic_proxy/filters/network/source/interface/codec_callbacks.h
@@ -12,6 +12,81 @@ namespace NetworkFilters {
 namespace GenericProxy {
 
 /**
+ * Extended options from request or response to control the behavior of the
+ * generic proxy filter.
+ * All these options are optional for the simple ping-pong use case.
+ */
+class ExtendedOptions {
+public:
+  /**
+   * @return the stream id of the request or response. This is used to match the
+   * downstream request with the upstream response.
+
+   * NOTE: In most cases, the stream id is not needed and will be ignored completely.
+   * The stream id is only used when we can't match the downstream request
+   * with the upstream response by the active stream instance self directly.
+   * For example, when the multiple downstream requests are multiplexed into one
+   * upstream connection.
+   */
+  uint64_t streamId() const { return stream_id_; }
+  /**
+   * Set the stream id of the request or response.
+   * @param stream_id the stream id of the request or response.
+   */
+  void setStreamId(uint64_t stream_id) {
+    stream_id_ = stream_id;
+    has_stream_id_ = true;
+  }
+  /**
+   * @return whether the stream id is set.
+   */
+  bool hasStreamId() const { return has_stream_id_; }
+
+  /**
+   * @return whether the current request requires an upstream response.
+   */
+  bool waitResponse() const { return wait_response_; }
+
+  /**
+   * Set whether the current request requires an upstream response.
+   * @param wait_response whether the current request requires an upstream response.
+   */
+  void setWaitResponse(bool wait_response) { wait_response_ = wait_response; }
+
+  /**
+   * @return whether the downstream/upstream connection should be drained after
+   * current active requests are finished.
+   */
+  bool drainClose() const { return drain_close_; }
+  /**
+   * Set whether the downstream/upstream connection should be drained after
+   * current active requests are finished.
+   * @param drain_close whether the downstream/upstream connection should be drained
+   * after current active requests are finished.
+   */
+  void setDrainClose(bool drain_close) { drain_close_ = drain_close; }
+
+  /**
+   * @return whether the current request/response is a heartbeat request/response.
+   */
+  bool isHeartbeat() const { return is_heartbeat_; }
+  /**
+   * Set whether the current request/response is a heartbeat request/response.
+   * @param is_heartbeat whether the current request/response is a heartbeat
+   * request/response.
+   */
+  void setHeartbeat(bool is_heartbeat) { is_heartbeat_ = is_heartbeat; }
+
+private:
+  uint64_t stream_id_{};
+  bool has_stream_id_{};
+
+  bool wait_response_{true};
+  bool drain_close_{};
+  bool is_heartbeat_{};
+};
+
+/**
  * Decoder callback of request.
  */
 class RequestDecoderCallback {
@@ -21,8 +96,9 @@ public:
   /**
    * If request decoding success then this method will be called.
    * @param request request from decoding.
+   * @param options extended options from request.
    */
-  virtual void onDecodingSuccess(RequestPtr request) PURE;
+  virtual void onDecodingSuccess(RequestPtr request, ExtendedOptions options) PURE;
 
   /**
    * If request decoding failure then this method will be called.
@@ -40,8 +116,9 @@ public:
   /**
    * If response decoding success then this method will be called.
    * @param response response from decoding.
+   * @param options extended options from response.
    */
-  virtual void onDecodingSuccess(ResponsePtr response) PURE;
+  virtual void onDecodingSuccess(ResponsePtr response, ExtendedOptions options) PURE;
 
   /**
    * If response decoding failure then this method will be called.
@@ -59,9 +136,8 @@ public:
   /**
    * If request encoding success then this method will be called.
    * @param buffer encoding result buffer.
-   * @param expect_response whether the current request requires an upstream response.
    */
-  virtual void onEncodingSuccess(Buffer::Instance& buffer, bool expect_response) PURE;
+  virtual void onEncodingSuccess(Buffer::Instance& buffer) PURE;
 };
 
 /**
@@ -74,9 +150,8 @@ public:
   /**
    * If response encoding success then this method will be called.
    * @param buffer encoding result buffer.
-   * @param close_connection whether the downstream connection should be closed.
    */
-  virtual void onEncodingSuccess(Buffer::Instance& buffer, bool close_connection) PURE;
+  virtual void onEncodingSuccess(Buffer::Instance& buffer) PURE;
 };
 
 } // namespace GenericProxy
