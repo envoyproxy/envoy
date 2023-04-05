a68bfa9e30e6535180b0b1e964aea62e5cd15881
Yanjun Xiang <yanjunxiang@google.com>
2023-04-05 20:46:17 +0000

    addressing comments
    Signed-off-by: Yanjun Xiang <yanjunxiang@google.com>

--- test/extensions/filters/http/ext_proc/ext_proc_grpc_fuzz_persistent.cc
+++ test/extensions/filters/http/ext_proc/ext_proc_grpc_fuzz_persistent.cc
@@ -5,6 +5,14 @@ namespace Extensions {
 namespace HttpFilters {
 namespace ExternalProcessing {
 
+namespace {
+// Reset and recreate Envoy and test server after every 2000 fuzzer execs.
+bool fuzzCreateEnvoy(const uint32_t exec) {
+  const uint32_t exec_before_reset_envoy = 2000;
+  return (exec % exec_before_reset_envoy == 0);
+}
+} // namespace
+
 using envoy::extensions::filters::http::ext_proc::v3::ProcessingMode;
 using envoy::service::ext_proc::v3::ProcessingRequest;
 using envoy::service::ext_proc::v3::ProcessingResponse;
@@ -15,10 +23,6 @@ static std::unique_ptr<ExtProcFuzzHelper> fuzz_helper = nullptr;
 // Protects fuzz_helper which will be accessed by multiple threads.
 static Thread::MutexBasicLockable fuzz_helper_lock_;
 
-static uint32_t fuzz_exec_count = 0;
-// Reset and recreate Envoy and test server after every 5000 fuzzer execs.
-#define FUZZ_CREATE_ENVOY(x) ((x) % 5000 == 0)
-
 DEFINE_PROTO_FUZZER(const test::extensions::filters::http::ext_proc::ExtProcGrpcTestCase& input) {
   try {
     TestUtility::validate(input);
@@ -26,7 +30,6 @@ DEFINE_PROTO_FUZZER(const test::extensions::filters::http::ext_proc::ExtProcGrpc
     ENVOY_LOG_MISC(debug, "ProtoValidationException: {}", e.what());
     return;
   }
-
   // have separate data providers.
   FuzzedDataProvider downstream_provider(
       reinterpret_cast<const uint8_t*>(input.downstream_data().data()),
@@ -34,8 +37,9 @@ DEFINE_PROTO_FUZZER(const test::extensions::filters::http::ext_proc::ExtProcGrpc
   FuzzedDataProvider ext_proc_provider(
       reinterpret_cast<const uint8_t*>(input.ext_proc_data().data()), input.ext_proc_data().size());
 
+  static uint32_t fuzz_exec_count = 0;
   // Initialize fuzzer once with IP and gRPC version from environment
-  if (FUZZ_CREATE_ENVOY(fuzz_exec_count)) {
+  if (fuzzCreateEnvoy(fuzz_exec_count)) {
     fuzzer = std::make_unique<ExtProcIntegrationFuzz>(
         TestEnvironment::getIpVersionsForTest()[0], TestEnvironment::getsGrpcVersionsForTest()[0]);
   }
@@ -44,7 +48,7 @@ DEFINE_PROTO_FUZZER(const test::extensions::filters::http::ext_proc::ExtProcGrpc
   fuzz_helper = std::make_unique<ExtProcFuzzHelper>(&ext_proc_provider);
 
   // Initialize test server.
-  if (FUZZ_CREATE_ENVOY(fuzz_exec_count)) {
+  if (fuzzCreateEnvoy(fuzz_exec_count)) {
     // This starts an external processor in a separate thread. This allows for the
     // external process to consume messages in a loop without blocking the fuzz
     // target from receiving the response.
@@ -95,7 +99,7 @@ DEFINE_PROTO_FUZZER(const test::extensions::filters::http::ext_proc::ExtProcGrpc
         });
   }
   // Initialize Envoy
-  if (FUZZ_CREATE_ENVOY(fuzz_exec_count)) {
+  if (fuzzCreateEnvoy(fuzz_exec_count)) {
     fuzzer->initializeFuzzer(true);
     ENVOY_LOG_MISC(trace, "Fuzzer initialized");
   }
@@ -113,9 +117,8 @@ DEFINE_PROTO_FUZZER(const test::extensions::filters::http::ext_proc::ExtProcGrpc
     ENVOY_LOG_MISC(trace, "Response timed out.");
   }
 
-  // Reset Envoy and test server after every 5000 fuzzer execs.
   fuzz_exec_count++;
-  if (FUZZ_CREATE_ENVOY(fuzz_exec_count)) {
+  if (fuzzCreateEnvoy(fuzz_exec_count)) {
     fuzzer->tearDown();
     fuzzer.reset();
     fuzzer = nullptr;
