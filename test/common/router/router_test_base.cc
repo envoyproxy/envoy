5d47906149d581a93c0258c75ea4cd3a2ea96ca7
Ohad Vano <ohadvano@gmail.com>
2023-04-06 23:43:54 +0300

    fix format
    Signed-off-by: Ohad Vano <ohadvano@gmail.com>

--- test/common/router/router_test_base.cc
+++ test/common/router/router_test_base.cc
@@ -20,8 +20,7 @@ RouterTestBase::RouterTestBase(bool start_child_span, bool suppress_envoy_header
               ShadowWriterPtr{shadow_writer_}, true, start_child_span, suppress_envoy_headers,
               false, suppress_grpc_request_failure_code_stats,
               flush_upstream_log_on_upstream_stream, std::move(strict_headers_to_check),
-               test_time_.timeSystem(), http_context_,
-              router_context_),
+              test_time_.timeSystem(), http_context_, router_context_),
       router_(std::make_unique<RouterTestFilter>(config_, config_.default_stats_)) {
   router_->setDecoderFilterCallbacks(callbacks_);
   upstream_locality_.set_zone("to_az");
