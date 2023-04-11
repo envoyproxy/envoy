4c7da6a49081460268bd142c59fd2c8510b391ae
Ohad Vano <ohadvano@gmail.com>
2023-04-11 16:56:20 +0300

    unit test class
    Signed-off-by: Ohad Vano <ohadvano@gmail.com>

--- test/extensions/stats_sinks/open_telemetry/open_telemetry_impl_test.cc
+++ test/extensions/stats_sinks/open_telemetry/open_telemetry_impl_test.cc
@@ -4,6 +4,7 @@
 
 #include "test/mocks/common.h"
 #include "test/mocks/grpc/mocks.h"
+#include "test/mocks/stats/mocks.h"
 
 using namespace std::chrono_literals;
 using testing::_;
@@ -20,14 +21,49 @@ namespace {
 class OpenTelemetryStatsSinkTests : public testing::Test {
 public:
   OpenTelemetryStatsSinkTests()
-    : options_(std::make_shared<OtlpOptions>(
+    : default_options_(std::make_shared<OtlpOptions>(
           envoy::extensions::stat_sinks::open_telemetry::v3::SinkConfig())) {}
 
   const OtlpOptionsSharedPtr& defaultOptions() {
-    return options_;
+    return default_options_;
   }
 
-  const OtlpOptionsSharedPtr options_;
+  void addCounterToSnapshot(
+      NiceMock<Stats::MockMetricSnapshot>& snapshot,
+      std::vector<std::unique_ptr<NiceMock<Stats::MockCounter>>>& counter_storage,
+      const std::string& name, uint64_t delta, uint64_t value, bool used = true) {
+    counter_storage.emplace_back(std::make_unique<NiceMock<Stats::MockCounter>>());
+    counter_storage.back()->name_ = name;
+    counter_storage.back()->value_ = value;
+    counter_storage.back()->used_ = used;
+
+    snapshot.counters_.push_back({delta, *counter_storage.back()});
+  }
+
+  void addGaugeToSnapshot(
+        NiceMock<Stats::MockMetricSnapshot>& snapshot,
+        std::vector<std::unique_ptr<NiceMock<Stats::MockGauge>>>& gauge_storage,
+        const std::string& name, uint64_t value, bool used = true) {
+    gauge_storage.emplace_back(std::make_unique<NiceMock<Stats::MockGauge>>());
+    gauge_storage.back()->name_ = name;
+    gauge_storage.back()->value_ = value;
+    gauge_storage.back()->used_ = used;
+
+    snapshot.gauges_.push_back(*gauge_storage.back());
+  }
+
+  void addHistogramToSnapshot(
+        NiceMock<Stats::MockMetricSnapshot>& snapshot,
+        std::vector<std::unique_ptr<NiceMock<Stats::MockParentHistogram>>>& histogram_storage,
+        const std::string& name, bool used = true) {
+    histogram_storage.emplace_back(std::make_unique<NiceMock<Stats::MockParentHistogram>>());
+    histogram_storage.back()->name_ = name;
+    histogram_storage.back()->used_ = used;
+
+    snapshot.histograms_.push_back(*histogram_storage.back());
+  }
+
+  const OtlpOptionsSharedPtr default_options_;
 };
 
 class OpenTelemetryGrpcMetricsExporterImplTest : public OpenTelemetryStatsSinkTests {
@@ -37,7 +73,6 @@ public:
         defaultOptions(), Grpc::RawAsyncClientSharedPtr{&async_client_});
   }
 
-  //Grpc::MockAsyncClient* async_client_{new NiceMock<Grpc::MockAsyncClient>};
   NiceMock<Grpc::MockAsyncClient> async_client_;
   OpenTelemetryGrpcMetricsExporterImplPtr exporter_;
 };
@@ -60,6 +95,14 @@ public:
 
   // OpenTelemetryGrpcMetricsExporter
   MOCK_METHOD(void, send, (MetricsExportRequestPtr&&));
+};
+
+class OpenTelemetryGrpcSinkTest : public OpenTelemetryStatsSinkTests {
+public:
+  NiceMock<Stats::MockMetricSnapshot> snapshot_;
+  std::vector<std::unique_ptr<NiceMock<Stats::MockCounter>>> counter_storage_;
+  std::vector<std::unique_ptr<NiceMock<Stats::MockGauge>>> gauge_storage_;
+  std::vector<std::unique_ptr<NiceMock<Stats::MockParentHistogram>>> histogram_storage_;
 };
 
 } // namespace
