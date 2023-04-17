90d90c17df9d81c1564db127e38374e1df8cd6a5
ohadvano <ohadvano@gmail.com>
2023-04-17 21:55:40 +0300

    use default case for NotSet
    Signed-off-by: ohadvano <ohadvano@gmail.com>

--- source/common/access_log/utility.cc
+++ source/common/access_log/utility.cc
@@ -11,8 +11,6 @@ namespace AccessLog {
 
 const std::string& Utility::getAccessLogTypeString(const AccessLogType access_log_type) {
   switch (access_log_type) {
-  case AccessLogType::NotSet:
-    return AccessLogTypeStrings::get().NotSet;
   case AccessLogType::TcpUpstreamConnected:
     return AccessLogTypeStrings::get().TcpUpstreamConnected;
   case AccessLogType::TcpPeriodic:
@@ -31,14 +29,14 @@ const std::string& Utility::getAccessLogTypeString(const AccessLogType access_lo
     return AccessLogTypeStrings::get().UpstreamPeriodic;
   case AccessLogType::UpstreamEnd:
     return AccessLogTypeStrings::get().UpstreamEnd;
+  default:
+    return AccessLogTypeStrings::get().NotSet;
   }
 }
 
 AccessLogTypeProto::AccessLogType
 Utility::getAccessLogTypeProto(const AccessLogType access_log_type) {
   switch (access_log_type) {
-  case AccessLogType::NotSet:
-    return AccessLogTypeProto::NotSet;
   case AccessLogType::TcpUpstreamConnected:
     return AccessLogTypeProto::TcpUpstreamConnected;
   case AccessLogType::TcpPeriodic:
@@ -57,6 +55,8 @@ Utility::getAccessLogTypeProto(const AccessLogType access_log_type) {
     return AccessLogTypeProto::UpstreamPeriodic;
   case AccessLogType::UpstreamEnd:
     return AccessLogTypeProto::UpstreamEnd;
+  default:
+    return AccessLogTypeProto::NotSet;
   }
 }
 
