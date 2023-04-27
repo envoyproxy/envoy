5929f9f04439bc904421472c1e2022d7aa9e9003
Keith Smiley <keithbsmiley@gmail.com>
2023-04-27 17:06:01 +0000

    Replace with pragma
    Signed-off-by: Keith Smiley <keithbsmiley@gmail.com>

--- envoy/common/union_string.h
+++ envoy/common/union_string.h
@@ -58,7 +58,18 @@ public:
   /**
    * Default constructor. Sets up for inline storage.
    */
-  UnionStringBase();
+
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wuninitialized"
+#endif
+  UnionStringBase() : buffer_(InlinedStringVector()) {
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
+    ASSERT((getInVec(buffer_).capacity()) >= MaxIntegerLength);
+    ASSERT(valid());
+  }
 
   /**
    * Constructor for a string reference.
