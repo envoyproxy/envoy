6c3904581177797d9a8cb9e15a2c92f089be4e1f
Renjie Tang <renjietang@google.com>
2023-04-27 21:32:40 -0700

    fix a build failure
    Signed-off-by: Renjie Tang <renjietang@google.com>

--- mobile/test/java/org/chromium/net/testing/CronetTestRuleTest.java
+++ mobile/test/java/org/chromium/net/testing/CronetTestRuleTest.java
@@ -65,7 +65,6 @@ public class CronetTestRuleTest {
   @SmallTest
   @Feature({"Cronet"})
   public void testRunOnlyNativeMustRun() {
-    assertFalse(mTestRule.testingJavaImpl());
     assertFalse(mTestWasRun);
     mTestWasRun = true;
     assertEquals(mTestFramework.mCronetEngine.getClass(), CronetUrlRequestContext.class);
