903047ea1a6b5694c1290b210d8f80270d4f14a6
phlax <phlax@users.noreply.github.com>
2023-05-01 21:09:07 +0100

    ci/mobile: Hide CI progress in .bazelrc (#27045)
    Signed-off-by: Ryan Northey <ryan@synca.io>
    Signed-off-by: Ryan Eskin <ryan.eskin89@protonmail.com>

--- ci/build_setup.sh
+++ ci/build_setup.sh
@@ -131,8 +131,6 @@ BAZEL_BUILD_OPTIONS=(
   "--verbose_failures"
   "--experimental_generate_json_trace_profile"
   "--test_output=errors"
-  "--noshow_progress"
-  "--noshow_loading_progress"
   "--action_env=CLANG_FORMAT"
   "${BAZEL_BUILD_EXTRA_OPTIONS[@]}"
   "${BAZEL_EXTRA_TEST_OPTIONS[@]}")
