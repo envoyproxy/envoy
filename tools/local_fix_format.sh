5deafc7905c135cb9f8ab6ec7b69b740b8165350
ohadvano <49730675+ohadvano@users.noreply.github.com>
2023-05-01 16:03:03 +0300

    Update local_fix_format.sh
    Signed-off-by: ohadvano <49730675+ohadvano@users.noreply.github.com>

--- tools/local_fix_format.sh
+++ tools/local_fix_format.sh
@@ -23,14 +23,15 @@
 unset DISPLAY
 
 # The following optional argument is added to be able to run this script using Docker,
-# due to a problem to locate clang using WSL on Windows. Call with -docker as the first arument.
+# due to a problem to locate clang using WSL on Windows. https://learn.microsoft.com/en-us/windows/wsl/about
+# Call with -docker as the first arument.
 if [[ $# -gt 0 && "$1" == "-docker" ]]; then
   shift
   exec ./ci/run_envoy_docker.sh "$0" -run-build-setup "$@"
 fi
 if [[ $# -gt 0 && "$1" == "-run-build-setup" ]]; then
-  . ci/build_setup.sh
   shift
+  . ci/build_setup.sh
 fi
 
 if [[ $# -gt 0 && "$1" == "-verbose" ]]; then
