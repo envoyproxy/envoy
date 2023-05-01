8f02c8008abc04e18270a1bc91f8adfb5b73122c
yanavlasov <yavlasov@google.com>
2023-05-01 13:34:36 -0400

    Remove unused member from GrpcStream (#27055)
    Remove unused `random_` member from GrpcMux
    
    Signed-off-by: Yan Avlasov <yavlasov@google.com>

--- test/common/config/delta_subscription_impl_test.cc
+++ test/common/config/delta_subscription_impl_test.cc
@@ -157,13 +157,13 @@ TEST_P(DeltaSubscriptionNoGrpcStreamTest, NoGrpcStream) {
   if (GetParam() == LegacyOrUnified::Unified) {
     xds_context = std::make_shared<Config::XdsMux::GrpcMuxDelta>(
         std::unique_ptr<Grpc::MockAsyncClient>(async_client), dispatcher, *method_descriptor,
-        random, *stats_store.rootScope(), rate_limit_settings, local_info, false,
+        *stats_store.rootScope(), rate_limit_settings, local_info, false,
         std::make_unique<NiceMock<MockCustomConfigValidators>>(), std::move(backoff_strategy),
         /*xds_config_tracker=*/XdsConfigTrackerOptRef());
   } else {
     xds_context = std::make_shared<NewGrpcMuxImpl>(
         std::unique_ptr<Grpc::MockAsyncClient>(async_client), dispatcher, *method_descriptor,
-        random, *stats_store.rootScope(), rate_limit_settings, local_info,
+        *stats_store.rootScope(), rate_limit_settings, local_info,
         std::make_unique<NiceMock<MockCustomConfigValidators>>(), std::move(backoff_strategy),
         /*xds_config_tracker=*/XdsConfigTrackerOptRef());
   }
