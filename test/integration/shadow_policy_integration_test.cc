216bf04f888d845915d71204d6e49e7c84d1e0c3
Paul Gallagher <pgal@google.com>
2023-05-02 14:13:18 -0700

    test: fix shadow test flake (#27126)
    Commit Message: test: fix shadow test flakes
    Additional Description: The second time we call sendRequestAndValidateResponse(), we have to wait for the counter to be g.e. 2.
    Risk Level: none
    
    Signed-off-by: Paul Gallagher <pgal@google.com>

--- test/integration/shadow_policy_integration_test.cc
+++ test/integration/shadow_policy_integration_test.cc
@@ -69,7 +69,7 @@ public:
         });
   }
 
-  void sendRequestAndValidateResponse() {
+  void sendRequestAndValidateResponse(int times_called = 1) {
     codec_client_ = makeHttpConnection(lookupPort("http"));
 
     IntegrationStreamDecoderPtr response =
@@ -80,7 +80,8 @@ public:
     if (filter_name_ != "add-body-filter") {
       EXPECT_EQ(10U, response->body().size());
     }
-    test_server_->waitForCounterEq("cluster.cluster_1.internal.upstream_rq_completed", 1);
+    test_server_->waitForCounterGe("cluster.cluster_1.internal.upstream_rq_completed",
+                                   times_called);
 
     upstream_headers_ =
         reinterpret_cast<AutonomousUpstream*>(fake_upstreams_[0].get())->lastRequestHeaders();
