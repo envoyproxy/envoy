b1a25929dd8b99957a81f3880c95611bc6d2c549
yanavlasov <yavlasov@google.com>
2023-05-03 03:35:00 -0400

    Redirect trace log output to /dev/null for coverage tests (#27118)
    Signed-off-by: Yan Avlasov <yavlasov@google.com>
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- test/common/common/logger_test.cc
+++ test/common/common/logger_test.cc
@@ -120,6 +120,7 @@ public:
 
   void expectLogMessage(const std::string& pattern, const std::string& message,
                         const std::string& expected) {
+    StderrSinkDelegate stacked(Envoy::Logger::Registry::getSink());
     auto formatter = std::make_unique<spdlog::pattern_formatter>();
     formatter
         ->add_flag<CustomFlagFormatter::EscapeMessageNewLine>(
