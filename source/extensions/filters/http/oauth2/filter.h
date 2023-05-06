f88e29647931c7c1f1d2ff181e4d95602b0928fa
Gustavo Gabriel Moyano <gustavo.g.moyano@gmail.com>
2023-05-06 11:14:41 -0300

    oauth2: fix Max-Age attribute of Set-Cookie response header (#26764)
    Signed-off-by: Gustavo Gabriel Moyano <gustavo.g.moyano@gmail.com>

--- source/extensions/filters/http/oauth2/filter.h
+++ source/extensions/filters/http/oauth2/filter.h
@@ -261,6 +261,7 @@ private:
   std::string access_token_; // TODO - see if we can avoid this being a member variable
   std::string id_token_;
   std::string refresh_token_;
+  std::string expires_in_;
   std::string new_expires_;
   absl::string_view host_;
   std::string state_;
