ac459284f045babb6d02912960ac7839e8b6ceaa
Ryan Hamilton <rch@google.com>
2023-05-06 19:03:50 +0000

    Format
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- mobile/test/common/mocks/common/mocks.h
+++ mobile/test/common/mocks/common/mocks.h
@@ -1,13 +1,12 @@
-#include "library/common/common/system_helper.h"
-
 #include "gmock/gmock.h"
+#include "library/common/common/system_helper.h"
 
 namespace Envoy {
 namespace test {
 
 // Mock implementation of SystemHelper.
 class MockSystemHelper : public SystemHelper {
- public:
+public:
   MockSystemHelper();
 
   // SystemHelper:
@@ -17,39 +16,34 @@ class MockSystemHelper : public SystemHelper {
 // SystemHelperPeer allows the replacement of the SystemHelper singleton
 // with a MockSystemHelper.
 class SystemHelperPeer {
- public:
+public:
   class Handle;
 
   // Replaces the SystemHelper singleton with a new MockSystemHelper which is
   // wrapped in a Handle. The MockSystemHelper can be accessed via the
   // Handle's `mock_helper()` accessor.
-  static std::unique_ptr<Handle> replaceSystemHelper() {
-    return std::make_unique<Handle>();
-  }
+  static std::unique_ptr<Handle> replaceSystemHelper() { return std::make_unique<Handle>(); }
 
   // RAII type for replacing the SystemHelper singleton with a the MockSystemHelper.
   // When this object is destroyed, it resets the SystemHelper singleton back
   // to the previous state.
   class Handle {
-   public:
+  public:
     Handle() {
       previous_ = std::make_unique<test::MockSystemHelper>();
       SystemHelper::instance_.swap(previous_);
     }
 
-    ~Handle() {
-      SystemHelper::instance_ = std::move(previous_);
-    }
+    ~Handle() { SystemHelper::instance_ = std::move(previous_); }
 
     test::MockSystemHelper& mock_helper() {
       return *static_cast<test::MockSystemHelper*>(SystemHelper::instance_.get());
     }
 
-   private:
+  private:
     std::unique_ptr<SystemHelper> previous_;
   };
 };
 
-
-}  // namespace test
-}  // namespace Envoy
+} // namespace test
+} // namespace Envoy
