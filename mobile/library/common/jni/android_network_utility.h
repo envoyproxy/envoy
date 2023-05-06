f06dd93546fa7661335fce1766ab50b3fe5f5a77
Ryan Hamilton <rch@google.com>
2023-05-06 20:54:03 +0000

    Add android impl
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- mobile/library/common/jni/android_network_utility.h
+++ mobile/library/common/jni/android_network_utility.h
@@ -14,6 +14,12 @@
 jobject call_jvm_verify_x509_cert_chain(JNIEnv* env, const std::vector<std::string>& cert_chain,
                                         std::string auth_type, std::string host);
 
+
+envoy_cert_validation_result verify_x509_cert_chain(const envoy_data* certs, uint8_t size,
+                                                    const char* host_name);
+
+void jvm_detach_thread();
+
 /* Returns a group of C functions to do certificates validation using AndroidNetworkLibrary.
  */
 envoy_cert_validator* get_android_cert_validator_api();
