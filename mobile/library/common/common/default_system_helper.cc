e246338756e99bf4f25c4e66ad7572d660dbe910
Ryan Hamilton <rch@google.com>
2023-05-08 16:25:04 +0000

    Add define for android system helper
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- mobile/library/common/common/default_system_helper.cc
+++ mobile/library/common/common/default_system_helper.cc
@@ -8,12 +8,9 @@ envoy_cert_validation_result
 DefaultSystemHelper::validateCertificateChain(const std::vector<std::string>& /*certs*/,
                                               absl::string_view /*hostname*/) {
   envoy_cert_validation_result result;
-  // result.result = ENVOY_FAILURE;
-  // result.tls_alert = 80; // internal error
-  // result.error_details = "Certifcate verification not implemented ";
-  result.result = ENVOY_SUCCESS;
-  result.tls_alert = 0;
-  result.error_details = "";
+  result.result = ENVOY_FAILURE;
+  result.tls_alert = 80; // internal error
+  result.error_details = "Certifcate verification not implemented ";
   return result;
 }
 
