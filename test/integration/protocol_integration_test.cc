d7c4c94d5cf536d102908e5118966dc1737836bb
Yan Avlasov <yavlasov@google.com>
2023-05-08 14:16:32 -0400

    Deflake ManyRequestHeadersTimeout under TSAN
    Signed-off-by: Yan Avlasov <yavlasov@google.com>

--- test/integration/protocol_integration_test.cc
+++ test/integration/protocol_integration_test.cc
@@ -2332,7 +2332,7 @@ TEST_P(DownstreamProtocolIntegrationTest, ManyRequestTrailersAccepted) {
 // time-consuming byte size validations that will cause this test to timeout.
 TEST_P(DownstreamProtocolIntegrationTest, ManyRequestHeadersTimeout) {
   // Set timeout for 5 seconds, and ensure that a request with 10k+ headers can be sent.
-  testManyRequestHeaders(std::chrono::milliseconds(5000));
+  testManyRequestHeaders(TestUtility::DefaultTimeout * 5);
 }
 
 TEST_P(DownstreamProtocolIntegrationTest, LargeRequestTrailersAccepted) {
