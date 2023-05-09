49f9ef2d235f0797278ab4abf9e1f9a5673d423d
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2023-05-08 17:22:24 -0700

    Log the type of exception (if any) and its reason in terminate handler
    Log uncaught exceptions when possible
    
    Signed-off-by: Dmitri Dolguikh <dmitri@appliedlogic.ca>

--- source/exe/terminate_handler.cc
+++ source/exe/terminate_handler.cc
@@ -2,14 +2,28 @@
 
 #include <cstdlib>
 
+#include "envoy/common/exception.h"
 #include "source/common/common/logger.h"
 #include "source/server/backtrace.h"
+
+#include "absl/strings/str_format.h"
 
 namespace Envoy {
 
 std::terminate_handler TerminateHandler::logOnTerminate() const {
   return std::set_terminate([]() {
-    ENVOY_LOG(critical, "std::terminate called! (possible uncaught exception, see trace)");
+    auto currentException = std::current_exception();
+    if (currentException) {
+      try {
+        std::rethrow_exception(currentException);
+      } catch (const EnvoyException& e) {
+        ENVOY_LOG(critical, absl::StrFormat("std::terminate called! Uncaught EnvoyException '%s', see trace.", e.what()));
+      } catch (...) {
+        ENVOY_LOG(critical, "std::terminate called! See trace. Uncaught unknown exception, see trace.");
+      }
+    } else {
+      ENVOY_LOG(critical, "std::terminate called! See trace.");
+    }
     BACKTRACE_LOG();
     std::abort();
   });
