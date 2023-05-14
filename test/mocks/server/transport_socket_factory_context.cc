069bd643ce4a623db9a9c778035ecd6c925263c1
wbpcode <wangbaiping@corp.netease.com>
2023-05-14 04:08:24 +0000

    use state less mock factory context
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>

--- test/mocks/server/transport_socket_factory_context.cc
+++ test/mocks/server/transport_socket_factory_context.cc
@@ -12,13 +12,20 @@ namespace Configuration {
 using ::testing::ReturnRef;
 
 MockTransportSocketFactoryContext::MockTransportSocketFactoryContext()
-    : secret_manager_(std::make_unique<Secret::SecretManagerImpl>(config_tracker_)) {
+    : secret_manager_(std::make_unique<Secret::SecretManagerImpl>(config_tracker_)),
+      singleton_manager_(Thread::threadFactoryForTest()) {
   ON_CALL(*this, serverFactoryContext()).WillByDefault(ReturnRef(server_context_));
   ON_CALL(*this, clusterManager()).WillByDefault(ReturnRef(cluster_manager_));
   ON_CALL(*this, messageValidationVisitor())
       .WillByDefault(ReturnRef(ProtobufMessage::getStrictValidationVisitor()));
   ON_CALL(*this, sslContextManager()).WillByDefault(ReturnRef(context_manager_));
   ON_CALL(*this, statsScope()).WillByDefault(ReturnRef(*store_.rootScope()));
+
+  ON_CALL(server_context_, serverScope()).WillByDefault(ReturnRef(*store_.rootScope()));
+  ON_CALL(server_context_, api()).WillByDefault(ReturnRef(api_));
+  ON_CALL(server_context_, options()).WillByDefault(ReturnRef(options_));
+  ON_CALL(server_context_, accessLogManager()).WillByDefault(ReturnRef(access_log_manager_));
+  ON_CALL(server_context_, singletonManager()).WillByDefault(ReturnRef(singleton_manager_));
 }
 
 MockTransportSocketFactoryContext::~MockTransportSocketFactoryContext() = default;
