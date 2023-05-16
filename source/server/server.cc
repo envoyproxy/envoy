39c3470895499eda8637f535658cc5247b980ca6
He Jie Xu <hejie.xu@intel.com>
2023-05-16 09:47:41 +0000

    server: initilize the bootstrap extension after worker registered
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- source/server/server.cc
+++ source/server/server.cc
@@ -712,6 +712,11 @@ void InstanceImpl::initialize(Network::Address::InstanceConstSharedPtr local_add
       messageValidationContext(), *api_, http_context_, grpc_context_, router_context_,
       access_log_manager_, *singleton_manager_, options_, quic_stat_names_, *this);
 
+  // Now that we are initialized, notify the bootstrap extensions.
+  for (auto&& bootstrap_extension : bootstrap_extensions_) {
+    bootstrap_extension->onServerInitialized();
+  }
+
   // Now the configuration gets parsed. The configuration may start setting
   // thread local data per above. See MainImpl::initialize() for why ConfigImpl
   // is constructed as part of the InstanceImpl and then populated once
@@ -750,10 +755,7 @@ void InstanceImpl::initialize(Network::Address::InstanceConstSharedPtr local_add
     stat_flush_timer_->enableTimer(stats_config.flushInterval());
   }
 
-  // Now that we are initialized, notify the bootstrap extensions.
-  for (auto&& bootstrap_extension : bootstrap_extensions_) {
-    bootstrap_extension->onServerInitialized();
-  }
+  
 
   // GuardDog (deadlock detection) object and thread setup before workers are
   // started and before our own run() loop runs.
