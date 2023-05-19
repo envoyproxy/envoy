def5309b0af65369f87c2e3b2654f7815958301b
Chris Schoener <schoener@google.com>
2023-05-19 10:10:07 -0400

    2
    Signed-off-by: Chris Schoener <schoener@google.com>

--- mobile/test/objective-c/EnvoyTestServer.h
+++ mobile/test/objective-c/EnvoyTestServer.h
@@ -3,11 +3,17 @@
 // Interface for starting and managing a test server. Calls into to test_server.cc
 @interface EnvoyTestServer : NSObject
 
+// Starts a server with HTTP3 and TLS.
 + (void)startQuicTestServer;
+// Get the port of the upstream server.
 + (NSInteger)getEnvoyPort;
+// Starts a server with HTTP2 and TLS.
 + (void)startTestServer;
+// Starts a server with HTTP1 and no TLS.
 + (void)startHttpTestServer;
+// Shut down and clean up server.
 + (void)shutdownTestServer;
+// Add response data to the upstream.
 + (void)setHeadersAndData:(NSString *)header_key
              header_value:(NSString *)header_value
                      response_body:(NSString *)response_body;
