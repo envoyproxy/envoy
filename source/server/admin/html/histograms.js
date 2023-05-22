f242645049ca651120a6992ec4f1d6fdd95cbe4a
Joshua Marantz <jmarantz@google.com>
2023-05-22 00:17:32 -0400

    tighten up html generation js
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/server/admin/html/histograms.js
+++ source/server/admin/html/histograms.js
@@ -40,23 +40,26 @@ function renderHistogramDetail(histogramDiv, supported_percentiles, details) {
 let pendingLeave;
 let pendingTimeout;
 
+function makeElement(parent, type, className) {
+  const element = document.createElement(type);
+  if (className) {
+    element.className = className;
+  }
+  parent.appendChild(element);
+  return element;
+}
+
 function renderHistogram(histogramDiv, supported_percentiles, histogram, changeCount) {
-  const div = document.createElement('div');
-  const label = document.createElement('span');
-  label.className = 'histogram-name';
+  const div = makeElement(histogramDiv, 'div');
+  const label = makeElement(div, 'span', 'histogram-name');
   label.textContent = histogram.name + (changeCount == null ? "" : " (" + changeCount + ")");
-  div.appendChild(label);
-  histogramDiv.appendChild(div);
 
   let timeout;
   let highlightedBucket;
 
   const numBuckets = histogram.totals.length;
   if (numBuckets == 0) {
-    const no_data = document.createElement('span');
-    no_data.className = 'histogram-no-data';
-    no_data.textContent = 'No recorded values';
-    div.appendChild(no_data);
+    makeElement(div, 'span', 'histogram-no-data').textContent = 'No recorded values';
     return;
   }
 
@@ -79,15 +82,10 @@ function renderHistogram(histogramDiv, supported_percentiles, histogram, changeC
     const cumulative = percentile_values[i].cumulative;
     updateMinMaxValue(cumulative, cumulative);
   }
-  const graphics = document.createElement('div');
-  const labels = document.createElement('div');
-  div.appendChild(label);
-  div.appendChild(graphics);
-  div.appendChild(labels);
+  const graphics = makeElement(div, 'div', 'histogram-graphics');
+  const labels = makeElement(div, 'div', 'histogram-labels');
 
-  const detailPopup = document.createElement('div');
-  detailPopup.className = 'histogram-popup';
-  graphics.appendChild(detailPopup);
+  const detailPopup = makeElement(graphics, 'div', 'histogram-popup');
   detailPopup.addEventListener('mouseover', (event) => {
     // If the mouse enters the popup then cancel the timer that would
     // erase it. This should make it easier to cut&paste the contents
@@ -102,9 +100,6 @@ function renderHistogram(histogramDiv, supported_percentiles, histogram, changeC
   detailPopup.addEventListener('mouseout', (event) => {
     pendingTimeout = window.setTimeout(pendingLeave, 2000);
   });
-
-  graphics.className = 'histogram-graphics';
-  labels.className = 'histogram-labels';
 
   // First we can over the detailed bucket value and count info, and determine
   // some limits so we can scale the histogram data to (for now) consume
@@ -155,9 +150,7 @@ function renderHistogram(histogramDiv, supported_percentiles, histogram, changeC
       if (bucket.annotations.length > 0) {
         bucket.annotations.sort((a, b) => a[0] < b[0]);
         if (!annotationsDiv) {
-          annotationsDiv = document.createElement('div');
-          div.appendChild(annotationsDiv);
-          annotationsDiv.className = 'histogram-percentiles';
+          annotationsDiv = makeElement(div, 'div', 'histogram-percentiles');
         }
       }
     }
@@ -220,9 +213,9 @@ function renderHistogram(histogramDiv, supported_percentiles, histogram, changeC
 
         // We always put the marker proportionally between this bucket and
         // the next one.
-        const span = document.createElement('span');
-        span.className = (annotation[1] == PERCENTILE) ? 'histogram-percentile' :
-            'histogram-interval';
+        const span = makeElement(annotationsDiv, 'span',
+                                 (annotation[1] == PERCENTILE) ? 'histogram-percentile' :
+                                 'histogram-interval');
         let percentilePercent = toPercentPosition(percentileVpx);
         span.style.left = percentilePercent;
 
@@ -233,8 +226,7 @@ function renderHistogram(histogramDiv, supported_percentiles, histogram, changeC
         }
         prevPercentileLabelVpx = percentileVpx + percentileWidthAndMarginVpx;
 
-        const percentilePLabel = document.createElement('span');
-        percentilePLabel.className = 'percentile-label';
+        const percentilePLabel = makeElement(annotationsDiv, 'span', 'percentile-label');
         percentilePLabel.style.bottom = 0;
         if (annotation[1] == PERCENTILE) {
           percentilePLabel.textContent = 'P' + annotation[2];
@@ -243,21 +235,15 @@ function renderHistogram(histogramDiv, supported_percentiles, histogram, changeC
         }
         percentilePLabel.style.left = percentilePercent;
 
-        const percentileVLabel = document.createElement('span');
-        percentileVLabel.className = 'percentile-label';
+        const percentileVLabel = makeElement(annotationsDiv, 'span', 'percentile-label');
         percentileVLabel.style.bottom = '30%';
         percentileVLabel.textContent = format(annotation[0]);
         percentileVLabel.style.left = percentilePercent;
-
-        annotationsDiv.appendChild(span);
-        annotationsDiv.appendChild(percentilePLabel);
-        annotationsDiv.appendChild(percentileVLabel);
       }
     }
 
     // Now draw the bucket.
-    const bucketSpan = document.createElement('span');
-    bucketSpan.className = 'histogram-bucket';
+    const bucketSpan = makeElement(graphics, 'span', 'histogram-bucket');
     const heightPercent = maxCount == 0 ? 0 : formatPercent(bucket.count / maxCount);
     bucketSpan.style.height = heightPercent;
     const upper_bound = bucket.lower_bound + bucket.width;
@@ -270,19 +256,16 @@ function renderHistogram(histogramDiv, supported_percentiles, histogram, changeC
     if (++textIntervalIndex == textInterval) {
       showingCount = true;
       textIntervalIndex = 0;
-      const lower_label = document.createElement('span');
+      const lower_label = makeElement(labels, 'span');
       lower_label.textContent = format(bucket.lower_bound);
       lower_label.style.left = toPercentPosition(leftVpx);
       lower_label.style.width = toPercentWidth(bucketWidthVpx);
-      labels.appendChild(lower_label);
 
-      const bucketLabel = document.createElement('span');
-      bucketLabel.className = 'bucket-label';
+      const bucketLabel = makeElement(graphics, 'span', 'bucket-label');
       bucketLabel.textContent = format(bucket.count);
       bucketLabel.style.left = toPercentPosition(leftVpx);
       bucketLabel.style.width = toPercentWidth(bucketWidthVpx);
       bucketLabel.style.bottom = heightPercent;
-      graphics.appendChild(bucketLabel);
     }
 
     const bucketOnLeftSide = i <= numBuckets / 2;
@@ -315,25 +298,21 @@ function renderHistogram(histogramDiv, supported_percentiles, histogram, changeC
       highlightedBucket = bucketSpan;
 
       detailPopup.replaceChildren();
-      const bounds = document.createElement('div');
-      bounds.textContent = '[' + format(bucket.lower_bound) + ', ' +
+      makeElement(detailPopup, 'div').textContent =
+          '[' + format(bucket.lower_bound) + ', ' +
           format(bucket.lower_bound + bucket.width) + ')';
-      detailPopup.appendChild(bounds);
       if (!showingCount) {
-        const count = document.createElement('div');
-        count.textContent = 'count=' + bucket.count;
-        detailPopup.appendChild(count);
+        makeElement(detailPopup, 'div').textContent = 'count=' + bucket.count;
       }
       if (bucket.annotations) {
         for (annotation of bucket.annotations) {
           if (annotation[1] == PERCENTILE) {
-            const span = document.createElement('div');
+            const span = makeElement(detailPopup, 'div');
             if (annotation[1] == PERCENTILE) {
               span.textContent = 'P' + annotation[2] + ': ' + format(annotation[0]);
             } else {
               span.textContent = 'I' + annotation[3] + '[' + annotation[2] + ']: ' + format(annotation[0]);
             }
-            detailPopup.appendChild(span);
           }
         }
       }
@@ -343,8 +322,6 @@ function renderHistogram(histogramDiv, supported_percentiles, histogram, changeC
       pendingTimeout = window.setTimeout(leaveHandler, 2000);
       pendingLeave = leaveHandler;
     });
-
-    graphics.appendChild(bucketSpan);
 
     prevVpx = leftVpx;
     leftVpx = nextVpx;
