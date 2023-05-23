8a51a93be8bd13c6cdbab84fce799b0dc6c34b9b
alyssawilk <alyssar@chromium.org>
2023-05-23 16:58:21 -0400

    test: moving ssl utility to use parsed protos (#27585)
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>
    Signed-off-by: Ryan Eskin <ryan.eskin89@protonmail.com>

--- test/integration/ssl_utility.cc
+++ test/integration/ssl_utility.cc
@@ -25,47 +25,30 @@ namespace Ssl {
 void initializeUpstreamTlsContextConfig(
     const ClientSslTransportOptions& options,
     envoy::extensions::transport_sockets::tls::v3::UpstreamTlsContext& tls_context) {
-  std::string yaml_plain = R"EOF(
-  common_tls_context:
-    validation_context:
-      trusted_ca:
-        filename: "{{ test_rundir }}/test/config/integration/certs/cacert.pem"
-)EOF";
+  std::string rundir = TestEnvironment::runfilesDirectory();
+  tls_context.mutable_common_tls_context()
+      ->mutable_validation_context()
+      ->mutable_trusted_ca()
+      ->set_filename(rundir + "/test/config/integration/certs/cacert.pem");
+  auto* certs = tls_context.mutable_common_tls_context()->add_tls_certificates();
+  std::string chain;
+  std::string key;
   if (options.client_ecdsa_cert_) {
-    yaml_plain += R"EOF(
-    tls_certificates:
-      certificate_chain:
-        filename: "{{ test_rundir }}/test/config/integration/certs/client_ecdsacert.pem"
-      private_key:
-        filename: "{{ test_rundir }}/test/config/integration/certs/client_ecdsakey.pem"
-)EOF";
+    chain = rundir + "/test/config/integration/certs/client_ecdsacert.pem";
+    key = rundir + "/test/config/integration/certs/client_ecdsakey.pem";
   } else if (options.use_expired_spiffe_cert_) {
-    yaml_plain += R"EOF(
-    tls_certificates:
-      certificate_chain:
-        filename: "{{ test_rundir }}/test/extensions/transport_sockets/tls/test_data/expired_spiffe_san_cert.pem"
-      private_key:
-        filename: "{{ test_rundir }}/test/extensions/transport_sockets/tls/test_data/expired_spiffe_san_key.pem"
-)EOF";
+    chain = rundir + "/test/extensions/transport_sockets/tls/test_data/expired_spiffe_san_cert.pem";
+    key = rundir + "/test/extensions/transport_sockets/tls/test_data/expired_spiffe_san_key.pem";
   } else if (options.client_with_intermediate_cert_) {
-    yaml_plain += R"EOF(
-    tls_certificates:
-      certificate_chain:
-        filename: "{{ test_rundir }}/test/config/integration/certs/client2_chain.pem"
-      private_key:
-        filename: "{{ test_rundir }}/test/config/integration/certs/client2key.pem"
-)EOF";
+    chain = rundir + "/test/config/integration/certs/client2_chain.pem";
+    key = rundir + "/test/config/integration/certs/client2key.pem";
   } else {
-    yaml_plain += R"EOF(
-    tls_certificates:
-      certificate_chain:
-        filename: "{{ test_rundir }}/test/config/integration/certs/clientcert.pem"
-      private_key:
-        filename: "{{ test_rundir }}/test/config/integration/certs/clientkey.pem"
-)EOF";
+    chain = rundir + "/test/config/integration/certs/clientcert.pem";
+    key = rundir + "/test/config/integration/certs/clientkey.pem";
   }
+  certs->mutable_certificate_chain()->set_filename(chain);
+  certs->mutable_private_key()->set_filename(key);
 
-  TestUtility::loadFromYaml(TestEnvironment::substitute(yaml_plain), tls_context);
   auto* common_context = tls_context.mutable_common_tls_context();
 
   if (options.alpn_) {
