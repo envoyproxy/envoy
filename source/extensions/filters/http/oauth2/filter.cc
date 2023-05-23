6895c824b36c3e8e42cac19b2463ac61b0580690
Ketankumar Rana <ketankumarr@nvidia.com>
2023-05-23 12:27:04 -0700

    Fixing the format
    Signed-off-by: Ketankumar Rana <ketankumarr@nvidia.com>

--- source/extensions/filters/http/oauth2/filter.cc
+++ source/extensions/filters/http/oauth2/filter.cc
@@ -532,7 +532,7 @@ void OAuth2Filter::addResponseCookies(Http::ResponseHeaderMap& headers,
   // If opted-in, we also create a new Bearer cookie for the authorization token provided by the
   // auth server.
   if (config_->forwardBearerToken()) {
-    std::string cookie_attribute_httponly = 
+    std::string cookie_attribute_httponly =
         Runtime::runtimeFeatureEnabled("envoy.reloadable_features.oauth_make_token_cookie_httponly")
             ? cookie_tail_http_only
             : cookie_tail;
