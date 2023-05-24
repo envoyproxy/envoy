c04df875d4f512fc212d5786c397f3d9dbe50535
Yanjun Xiang <yanjunxiang@google.com>
2023-05-24 22:18:56 +0000

    ext_proc filter allow_mode_override API implemenation
    Signed-off-by: Yanjun Xiang <yanjunxiang@google.com>

--- source/extensions/filters/http/ext_proc/ext_proc.h
+++ source/extensions/filters/http/ext_proc/ext_proc.h
@@ -110,7 +110,8 @@ public:
         message_timeout_(message_timeout), max_message_timeout_ms_(max_message_timeout_ms),
         stats_(generateStats(stats_prefix, config.stat_prefix(), scope)),
         processing_mode_(config.processing_mode()), mutation_checker_(config.mutation_rules()),
-        filter_metadata_(config.filter_metadata()) {}
+        filter_metadata_(config.filter_metadata()),
+        allow_mode_override_(config.allow_mode_override()) {}
 
   bool failureModeAllow() const { return failure_mode_allow_; }
 
@@ -123,6 +124,8 @@ public:
   const envoy::extensions::filters::http::ext_proc::v3::ProcessingMode& processingMode() const {
     return processing_mode_;
   }
+
+  bool allowModeOverride() const { return allow_mode_override_; }
 
   const Filters::Common::MutationRules::Checker& mutationChecker() const {
     return mutation_checker_;
@@ -148,6 +151,8 @@ private:
   const envoy::extensions::filters::http::ext_proc::v3::ProcessingMode processing_mode_;
   const Filters::Common::MutationRules::Checker mutation_checker_;
   const Envoy::ProtobufWkt::Struct filter_metadata_;
+  // If set to true, allow the processing mode to be modified by the ext_proc response.
+  const bool allow_mode_override_;
 };
 
 using FilterConfigSharedPtr = std::shared_ptr<FilterConfig>;
