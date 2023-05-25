38b3f65c91eb8638d5b4784a1f2ee19c5e2cb85e
Yanjun Xiang <yanjunxiang@google.com>
2023-05-25 18:14:56 +0000

    revert openstream change
    Signed-off-by: Yanjun Xiang <yanjunxiang@google.com>

--- source/extensions/filters/http/ext_proc/ext_proc.cc
+++ source/extensions/filters/http/ext_proc/ext_proc.cc
@@ -118,14 +118,15 @@ void Filter::setEncoderFilterCallbacks(Http::StreamEncoderFilterCallbacks& callb
 }
 
 Filter::StreamOpenState Filter::openStream() {
-  if (processing_complete_) {
-    // Stream failed while starting and either onGrpcError or onGrpcClose was already called
-    return sent_immediate_response_ ? StreamOpenState::Error : StreamOpenState::IgnoreError;
-  }
+  ENVOY_BUG(!processing_complete_, "openStream should not have been called");
   if (!stream_) {
     ENVOY_LOG(debug, "Opening gRPC stream to external processor");
     stream_ = client_->start(*this, grpc_service_, decoder_callbacks_->streamInfo());
     stats_.streams_started_.inc();
+    if (processing_complete_) {
+      // Stream failed while starting and either onGrpcError or onGrpcClose was already called
+      return sent_immediate_response_ ? StreamOpenState::Error : StreamOpenState::IgnoreError;
+    }
   }
   return StreamOpenState::Ok;
 }
