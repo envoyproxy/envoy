36ca2b9887d6125185fddf4833006d2bf439352f
Vikas Choudhary <choudharyvikas16@gmail.com>
2023-05-29 13:02:28 +0530

    asan
    Signed-off-by: Vikas Choudhary <choudharyvikas16@gmail.com>

--- source/common/tcp_proxy/upstream.h
+++ source/common/tcp_proxy/upstream.h
@@ -310,11 +310,12 @@ public:
               Ssl::ConnectionInfoConstSharedPtr ssl_info)
         : conn_pool_(&conn_pool), host_(host), ssl_info_(ssl_info) {}
     virtual ~Callbacks() = default;
-    virtual void onSuccess(Http::RequestEncoder& request_encoder) {
+    virtual void onSuccess(Http::RequestEncoder* request_encoder) {
       ASSERT(conn_pool_ != nullptr);
       if (!Runtime::runtimeFeatureEnabled(
               "envoy.reloadable_features.upstream_http_filters_with_tcp_proxy")) {
-        conn_pool_->onGenericPoolReady(host_, request_encoder.getStream().connectionInfoProvider(),
+        ASSERT(request_encoder != nullptr);
+        conn_pool_->onGenericPoolReady(host_, request_encoder->getStream().connectionInfoProvider(),
                                        ssl_info_);
         return;
       }
@@ -494,7 +495,7 @@ private:
       if (!is_valid_response || end_stream) {
         parent_.resetEncoder(Network::ConnectionEvent::LocalClose);
       } else if (parent_.conn_pool_callbacks_ != nullptr) {
-        parent_.conn_pool_callbacks_->onSuccess(*parent_.request_encoder_);
+        parent_.conn_pool_callbacks_->onSuccess(parent_.request_encoder_);
         parent_.conn_pool_callbacks_.reset();
       }
     }
