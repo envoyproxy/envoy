8a2cf5b3e217806c2767193ffc9b9fc6733383e8
Yanjun Xiang <yanjunxiang@google.com>
2023-06-02 17:31:57 +0000

    removing logs
    Signed-off-by: Yanjun Xiang <yanjunxiang@google.com>

--- source/extensions/filters/http/ext_proc/processor_state.cc
+++ source/extensions/filters/http/ext_proc/processor_state.cc
@@ -365,17 +365,14 @@ void ProcessorState::clearRouteCache(const CommonResponse& common_response) {
   }
   // No need to clear route cache for response traffic to the downstream client.
   if (trafficDirection() == envoy::config::core::v3::TrafficDirection::OUTBOUND) {
-    ENVOY_LOG(debug, "NOT clearing route cache for response traffic to downstream client");
     return;
   }
   // Only clear the route cache if there is a mutation to the header and clearing is allowed.
   if (filter_.config().disableClearRouteCache()) {
     filter_.stats().clear_route_cache_disabled_.inc();
-    ENVOY_LOG(debug, "NOT clearing route cache, it is disabled in the config");
     return;
   }
   if (common_response.has_header_mutation()) {
-    ENVOY_LOG(debug, "clearing route cache");
     filter_callbacks_->downstreamCallbacks()->clearRouteCache();
     return;
   }
