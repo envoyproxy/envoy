91a95cf1651bbac8c629b0903313e924e6ec6b03
code <wangbaiping@corp.netease.com>
2023-06-07 09:39:54 +0800

    upstream: the subset load balancing policy could be configured as extension (#27593)
    * upstream: update interface of TypedLoadBalancerFactory to support hierarchical lb
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * subset lb: split child lb creation out to separated creator
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * subset lb: complete the development of lb
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * complete test of subset
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * fix docs
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * resolve conflict after merge main
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * fix type url in extension file
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * remove unnecessary comment
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    * add more test for coverage
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>
    
    ---------
    
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>

--- source/extensions/load_balancing_policies/maglev/config.cc
+++ source/extensions/load_balancing_policies/maglev/config.cc
@@ -9,17 +9,20 @@ namespace Extensions {
 namespace LoadBalancingPolices {
 namespace Maglev {
 
-Upstream::ThreadAwareLoadBalancerPtr Factory::create(const Upstream::ClusterInfo& cluster_info,
-                                                     const Upstream::PrioritySet& priority_set,
-                                                     Runtime::Loader& runtime,
-                                                     Random::RandomGenerator& random, TimeSource&) {
+Upstream::ThreadAwareLoadBalancerPtr
+Factory::create(OptRef<const Upstream::LoadBalancerConfig> lb_config,
+                const Upstream::ClusterInfo& cluster_info,
+                const Upstream::PrioritySet& priority_set, Runtime::Loader& runtime,
+                Random::RandomGenerator& random, TimeSource&) {
 
-  const auto* typed_config =
-      dynamic_cast<const envoy::extensions::load_balancing_policies::maglev::v3::Maglev*>(
-          cluster_info.loadBalancingPolicy().get());
+  const auto* typed_lb_config =
+      dynamic_cast<const Upstream::LoadBalancerConfigWrapper*>(lb_config.ptr());
+  auto typed_proto_config = typed_lb_config == nullptr
+                                ? OptRef<const MaglevLbProto>{}
+                                : typed_lb_config->typedProtoConfig<MaglevLbProto>();
 
   // Assume legacy config.
-  if (!typed_config) {
+  if (!typed_proto_config.has_value()) {
     return std::make_unique<Upstream::MaglevLoadBalancer>(
         priority_set, cluster_info.lbStats(), cluster_info.statsScope(), runtime, random,
         cluster_info.lbMaglevConfig(), cluster_info.lbConfig());
@@ -29,7 +32,7 @@ Upstream::ThreadAwareLoadBalancerPtr Factory::create(const Upstream::ClusterInfo
       priority_set, cluster_info.lbStats(), cluster_info.statsScope(), runtime, random,
       static_cast<uint32_t>(PROTOBUF_PERCENT_TO_ROUNDED_INTEGER_OR_DEFAULT(
           cluster_info.lbConfig(), healthy_panic_threshold, 100, 50)),
-      *typed_config);
+      typed_proto_config.value());
 }
 
 /**
