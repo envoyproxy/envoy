039ee0fae0fb3af624d76befb85b72320e411e84
Pawan Kumar <pawanbishnoi@outlook.com>
2023-06-10 10:36:29 +0000

    add test
    Signed-off-by: Pawan Kumar <pawanbishnoi@outlook.com>

--- source/common/upstream/outlier_detection_impl.cc
+++ source/common/upstream/outlier_detection_impl.cc
@@ -480,7 +480,7 @@ void DetectorImpl::ejectHost(HostSharedPtr host,
   double ejected_percent = 100.0 * (ejections_active_helper_.value() + 1) / host_monitors_.size();
   // Note this is not currently checked per-priority level, so it is possible
   // for outlier detection to eject all hosts at any given priority level.
-  // Note: at-least one host is ejected, we ignore max ejection percentage when ejecting first host
+  // Note: at-least one host is ejected, we ignore max ejection percentage when ejecting first host.
   if ((ejections_active_helper_.value() == 0) || (ejected_percent <= max_ejection_percent)) {
     if (type == envoy::data::cluster::v3::CONSECUTIVE_5XX ||
         type == envoy::data::cluster::v3::SUCCESS_RATE) {
