911e85e3a7b8453fc6c3fd9983822bb4c46a3f89
He Jie Xu <hejie.xu@intel.com>
2023-06-12 14:44:08 +0000

    Fix io_uring_worker_impl_test.cc
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- test/common/io/io_uring_worker_impl_test.cc
+++ test/common/io/io_uring_worker_impl_test.cc
@@ -408,10 +408,9 @@ TEST(IoUringWorkerImplTest, CloseDetected) {
   EXPECT_CALL(mock_io_uring, prepareReadv(_, _, _, _, _))
       .WillOnce(DoAll(SaveArg<4>(&read_req), Return<IoUringResult>(IoUringResult::Ok)));
   EXPECT_CALL(mock_io_uring, submit()).Times(1).RetiresOnSaturation();
-  bool is_read = false;
   IoUringServerSocket socket(
       0, worker,
-      [this, &is_read](uint32_t events) { EXPECT_EQ(events, Event::FileReadyType::Closed); }, 0,
+      [](uint32_t events) { EXPECT_EQ(events, Event::FileReadyType::Closed); }, 0,
       true);
   socket.disable();
 
