8e9dfe53757fbaf07cf04fdd27b9b30de665679f
Xin <stevenzzz@google.com>
2023-06-13 15:55:33 -0400

    Update source/common/stats/deferred_creation.h
    Co-authored-by: Greg Greenway <github@greenwayfamily.com>
    Signed-off-by: Xin <stevenzzz@google.com>

--- source/common/stats/deferred_creation.h
+++ source/common/stats/deferred_creation.h
@@ -30,10 +30,9 @@ public:
   DeferredStats(const typename StatsStructType::StatNameType& stat_names,
                 Stats::ScopeSharedPtr scope)
       : initialized_(
-            /* A lambda is used as we need to register the name into the symbol table.
-            Note: there is no issue to capture a reference of the scope here as this lambda is only
-            used to initialize the 'initialized_' Gauge.
-            */
+            // A lambda is used as we need to register the name into the symbol table.
+            // Note: there is no issue to capture a reference of the scope here as this lambda is only
+            // used to initialize the 'initialized_' Gauge.
             [&scope]() -> Gauge& {
               Stats::StatNamePool pool(scope->symbolTable());
               return Stats::Utility::gaugeFromElements(
