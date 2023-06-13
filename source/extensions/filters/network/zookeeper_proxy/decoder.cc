d2c27b45743f0f6588ea6cec901cd66d5b6c89be
Zhewei Hu <zhewei.hu33@gmail.com>
2023-06-13 07:37:12 -0700

    [ZK filter] Add 'addWatch' opcode support (#27791)
    * [ZK filter] Add 'addWatch' opcode support
    
    Signed-off-by: Zhewei Hu <zhu@pinterest.com>
    Signed-off-by: Ryan Eskin <ryan.eskin89@protonmail.com>

--- source/extensions/filters/network/zookeeper_proxy/decoder.cc
+++ source/extensions/filters/network/zookeeper_proxy/decoder.cc
@@ -149,6 +149,9 @@ void DecoderImpl::decodeOnData(Buffer::Instance& data, uint64_t& offset) {
   case OpCodes::SetWatches2:
     parseSetWatches2Request(data, offset, len);
     break;
+  case OpCodes::AddWatch:
+    parseAddWatchRequest(data, offset, len);
+    break;
   case OpCodes::CheckWatches:
     parseXWatchesRequest(data, offset, len, OpCodes::CheckWatches);
     break;
@@ -465,6 +468,15 @@ void DecoderImpl::parseSetWatches2Request(Buffer::Instance& data, uint64_t& offs
   skipStrings(data, offset);
 
   callbacks_.onSetWatches2Request();
+}
+
+void DecoderImpl::parseAddWatchRequest(Buffer::Instance& data, uint64_t& offset, uint32_t len) {
+  ensureMinLength(len, XID_LENGTH + OPCODE_LENGTH + (2 * INT_LENGTH));
+
+  const std::string path = helper_.peekString(data, offset);
+  const int32_t mode = helper_.peekInt32(data, offset);
+
+  callbacks_.onAddWatchRequest(path, mode);
 }
 
 void DecoderImpl::parseXWatchesRequest(Buffer::Instance& data, uint64_t& offset, uint32_t len,
