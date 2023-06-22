963c444d2f0e72560722a5df3b873f6b5010ac01
tyxia <tyxia@google.com>
2023-06-22 00:52:06 +0000

    move impl to cc
    Signed-off-by: tyxia <tyxia@google.com>

--- source/extensions/filters/http/composite/action.cc
+++ source/extensions/filters/http/composite/action.cc
@@ -7,6 +7,44 @@ namespace Composite {
 void ExecuteFilterAction::createFilters(Http::FilterChainFactoryCallbacks& callbacks) const {
   cb_(callbacks);
 }
+
+Matcher::ActionFactoryCb ExecuteFilterActionFactory::createActionFactoryCb(
+    const Protobuf::Message& config, Http::Matching::HttpFilterActionContext& context,
+    ProtobufMessage::ValidationVisitor& validation_visitor) {
+  const auto& composite_action = MessageUtil::downcastAndValidate<
+      const envoy::extensions::filters::http::composite::v3::ExecuteFilterAction&>(
+      config, validation_visitor);
+
+  auto& factory =
+      Config::Utility::getAndCheckFactory<Server::Configuration::NamedHttpFilterConfigFactory>(
+          composite_action.typed_config());
+  ProtobufTypes::MessagePtr message = Config::Utility::translateAnyToFactoryConfig(
+      composite_action.typed_config().typed_config(), validation_visitor, factory);
+
+  Envoy::Http::FilterFactoryCb callback = nullptr;
+
+  // First, try to create the filter factory creation function from factory context (if exists).
+  if (context.factory_context_.has_value()) {
+    callback = factory.createFilterFactoryFromProto(*message, context.stat_prefix_,
+                                                    context.factory_context_.value());
+  }
+
+  // If above failed, try to create the filter factory creation function from server factory
+  // context (if exists).
+  if (callback == nullptr && context.server_factory_context_.has_value()) {
+    callback = factory.createFilterFactoryFromProtoWithServerContext(
+        *message, context.stat_prefix_, context.server_factory_context_.value());
+  }
+
+  if (callback == nullptr) {
+    throw EnvoyException("Failed to get filter factory creation function");
+  }
+
+  return [cb = std::move(callback)]() -> Matcher::ActionPtr {
+    return std::make_unique<ExecuteFilterAction>(cb);
+  };
+}
+
 REGISTER_FACTORY(ExecuteFilterActionFactory,
                  Matcher::ActionFactory<Http::Matching::HttpFilterActionContext>);
 } // namespace Composite
