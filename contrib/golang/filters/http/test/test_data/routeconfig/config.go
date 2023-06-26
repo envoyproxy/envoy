915fa384a85795d88b0de3e874c15499ce73ba42
antJack <52443884+antJack@users.noreply.github.com>
2023-06-26 11:54:42 +0800

    golang: L4 network extension (#26173)
    Signed-off-by: yongjie.yyj <yeyeyongjie@gmail.com>
    Signed-off-by: asheryer <asheryer@amazon.com>

--- contrib/golang/filters/http/test/test_data/routeconfig/config.go
+++ contrib/golang/filters/http/test/test_data/routeconfig/config.go
@@ -4,9 +4,10 @@ import (
 	"errors"
 
 	xds "github.com/cncf/xds/go/xds/type/v3"
-	"github.com/envoyproxy/envoy/contrib/golang/filters/http/source/go/pkg/api"
-	"github.com/envoyproxy/envoy/contrib/golang/filters/http/source/go/pkg/http"
 	"google.golang.org/protobuf/types/known/anypb"
+
+	"github.com/envoyproxy/envoy/contrib/golang/common/go/api"
+	"github.com/envoyproxy/envoy/contrib/golang/filters/http/source/go/pkg/http"
 )
 
 const Name = "routeconfig"
