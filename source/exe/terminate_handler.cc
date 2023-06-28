e42d14977004f57e643af2e35990e16910a81b60
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2023-06-28 05:36:12 -0700

    Added tests
    Signed-off-by: Dmitri Dolguikh <dmitri@appliedlogic.ca>

--- source/exe/terminate_handler.cc
+++ source/exe/terminate_handler.cc
@@ -14,24 +14,28 @@ namespace Envoy {
 std::terminate_handler TerminateHandler::logOnTerminate() const {
   return std::set_terminate([]() {
     std::exception_ptr current_exception = std::current_exception();
-    if (current_exception != nullptr) {
-      try {
-        std::rethrow_exception(current_exception);
-      } catch (const EnvoyException& e) {
-        ENVOY_LOG(critical, "std::terminate called! Uncaught EnvoyException '{}', see trace.",
-                  e.what());
-      } catch (const std::exception& e) {
-        ENVOY_LOG(critical, "std::terminate called! Uncaught exception '{}', see trace.", e.what());
-      } catch (...) {
-        ENVOY_LOG(critical,
-                  "std::terminate called! See trace. Uncaught unknown exception, see trace.");
-      }
-    } else {
-      ENVOY_LOG(critical, "std::terminate called! See trace.");
-    }
+    logException(std::current_exception());
     BACKTRACE_LOG();
     std::abort();
   });
+}
+
+void TerminateHandler::logException(const std::exception_ptr current_exception) {
+  if (current_exception != nullptr) {
+    try {
+      std::rethrow_exception(current_exception);
+    } catch (const EnvoyException& e) {
+      ENVOY_LOG(critical, "std::terminate called! Uncaught EnvoyException '{}', see trace.",
+                e.what());
+    } catch (const std::exception& e) {
+      ENVOY_LOG(critical, "std::terminate called! Uncaught exception '{}', see trace.", e.what());
+    } catch (...) {
+      ENVOY_LOG(critical,
+                "std::terminate called! Uncaught unknown exception, see trace.");
+    }
+  } else {
+    ENVOY_LOG(critical, "std::terminate called! See trace.");
+  }
 }
 
 } // namespace Envoy
