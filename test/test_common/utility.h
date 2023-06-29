8bd12cec755a3538d0280180035eb20cb5800096
Joshua Marantz <jmarantz@google.com>
2023-06-29 09:06:20 -0400

    buffer: Disable flaky test on windows. (#28176)
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- test/test_common/utility.h
+++ test/test_common/utility.h
@@ -1321,4 +1321,12 @@ MATCHER_P(JsonStringEq, expected, "") {
 }
 #endif
 
+#ifdef WIN32
+#define DISABLE_UNDER_WINDOWS return
+#else
+#define DISABLE_UNDER_WINDOWS                                                                      \
+  do {                                                                                             \
+  } while (0)
+#endif
+
 } // namespace Envoy
