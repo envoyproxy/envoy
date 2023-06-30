6d45cae5e14a3956a71fa69bd0fb959ff35911c0
Joshua Marantz <jmarantz@google.com>
2023-06-30 09:40:03 -0400

    Don't display HTML detailed/graphical view by default; use the existing buckets-mode param to control detail.
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/server/admin/stats_html_render.cc
+++ source/server/admin/stats_html_render.cc
@@ -23,7 +23,7 @@ StatsHtmlRender::StatsHtmlRender(Http::ResponseHeaderMap& response_headers,
                                  Buffer::Instance& response, const StatsParams& params)
     : StatsTextRender(params), active_(params.format_ == StatsFormat::ActiveHtml) {
   AdminHtmlUtil::renderHead(response_headers, response);
-  if (!active_) {
+  if (!active_ && params.histogram_buckets_mode_ == Utility::HistogramBucketsMode::Detailed) {
     StatsParams json_params(params);
     json_params.histogram_buckets_mode_ = Utility::HistogramBucketsMode::Detailed;
     json_response_headers_ = Http::ResponseHeaderMapImpl::create();
@@ -35,7 +35,7 @@ StatsHtmlRender::StatsHtmlRender(Http::ResponseHeaderMap& response_headers,
 void StatsHtmlRender::finalize(Buffer::Instance& response) {
   // Render all the histograms here using the JSON data we've accumulated
   // for them.
-  if (!active_) {
+  if (histogram_json_render_ != nullptr) {
     histogram_json_render_->finalize(json_data_);
     response.add("</pre>\n<div id='histograms'></div>\n<script>\nconst json = \n");
     response.add(json_data_);
@@ -75,6 +75,21 @@ void StatsHtmlRender::noStats(Buffer::Instance& response, absl::string_view type
     response.addFragments({"</pre>\n<br/><i>No ", types, " found</i><br/>\n<pre>\n"});
   }
 }
+
+// We override the generate method for HTML to trigger some JS that will
+// render the histogram graphically. We will render that from JavaScript
+// and convey the histogram data to the JS via JSON, so we can delegate
+// to an instantiated JSON `sub-renderer` that will write into json_data_.
+void StatsHtmlRender::generate(Buffer::Instance& response, const std::string& name,
+                               const Stats::ParentHistogram& histogram) {
+  if (histogram_json_render_ != nullptr) {
+    histogram_json_render_->generate(json_data_, name, histogram);
+  } else {
+    StatsTextRender::generate(response, name, histogram);
+  }
+}
+
+
 
 } // namespace Server
 } // namespace Envoy
