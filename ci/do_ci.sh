372c2c657cd531903376c458a6f2d3c90c6a89d8
Vikas Choudhary <choudharyvikas16@gmail.com>
2023-07-01 08:09:15 +0530

    fix formatting
    Signed-off-by: Vikas Choudhary <choudharyvikas16@gmail.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -721,8 +721,8 @@ case $CI_TARGET in
              --config=rbe-toolchain-tsan \
              -c dbg \
              --build_tests_only \
+             --test_timeout=390,750,1500,5700 \
              --remote_download_minimal \
-	     --test_timeout=390,750,1500,5700 \
              "${TEST_TARGETS[@]}"
         if [ "${ENVOY_BUILD_FILTER_EXAMPLE}" == "1" ]; then
             echo "Building and testing envoy-filter-example tests..."
