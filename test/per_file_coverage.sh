89d65a934aefe7fc043d4fb612d2718a531e880f
Yan Avlasov <yavlasov@google.com>
2023-07-05 12:18:00 +0000

    Fix format
    Signed-off-by: Yan Avlasov <yavlasov@google.com>

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -35,7 +35,7 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/extensions/filters/common/fault:94.5"
 "source/extensions/filters/common/rbac:90.5"
 "source/extensions/filters/http/cache:93.4"
-"source/extensions/filters/http/grpc_json_transcoder:93.7" # TODO(#28232): 
+"source/extensions/filters/http/grpc_json_transcoder:93.7" # TODO(#28232)
 "source/extensions/filters/http/ip_tagging:88.0"
 "source/extensions/filters/http/kill_request:91.7" # Death tests don't report LCOV
 "source/extensions/filters/http/wasm:1.9"
