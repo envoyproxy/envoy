c6fe0ac50dee481834ff97d569d90876b020f45b
Jacob Neil Taylor <me@jacobtaylor.id.au>
2023-07-08 21:35:42 +1000

    Fix crash from AWS NLB healthchecks when proxy protocol is enabled
    Fix: [CVE-2024-23327](https://github.com/envoyproxy/envoy/security/advisories/GHSA-4h5x-x9vh-m29j)
    
    Signed-off-by: Jacob Neil Taylor <me@jacobtaylor.id.au>
    Signed-off-by: Greg Greenway <ggreenway@apple.com>
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -29,6 +29,7 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/extensions/access_loggers/wasm:93.5"
 "source/extensions/clusters/common:91.5" # This can be increased again once `#24903` lands
 "source/extensions/common:93.0" #flaky: be careful adjusting
+"source/extensions/common/proxy_protocol:93.8" # Adjusted for security patch
 "source/extensions/common/tap:94.2"
 "source/extensions/common/wasm:87.5" # flaky: be careful adjusting
 "source/extensions/common/wasm/ext:92.0"
