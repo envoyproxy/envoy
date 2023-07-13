1f00e1626c4a3ff4d675805b7ea970682bf39f3a
Xuyang Tao <taoxuy@google.com>
2023-07-13 11:06:34 -0700

    add TODO
    Signed-off-by: Xuyang Tao <taoxuy@google.com>

--- source/extensions/filters/http/grpc_field_extraction/message_converter/message_converter.cc
+++ source/extensions/filters/http/grpc_field_extraction/message_converter/message_converter.cc
@@ -125,6 +125,8 @@ MessageConverter::convertBackToBuffer(StreamMessagePtr message) {
 
   // Data owned in StreamMessage is appended to Envoy's Buffer via external
   // data reference (no copy).
+  // TODO(#28388): refactor grpc_field_extraction/messag_converter reading ZeroCopyInputStream to
+  // EnvoyBuffer.
   const void* data = nullptr;
   int size = 0;
   bool is_empty = true;
