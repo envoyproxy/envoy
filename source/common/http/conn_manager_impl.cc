27d59031444851d51427f8f33dc21c2c059ecc0a
Yan Avlasov <yavlasov@google.com>
2023-07-22 00:20:16 +0000

    Update comment
    Signed-off-by: Yan Avlasov <yavlasov@google.com>

--- source/common/http/conn_manager_impl.cc
+++ source/common/http/conn_manager_impl.cc
@@ -1008,6 +1008,7 @@ bool ConnectionManagerImpl::ActiveStream::validateHeaders() {
         sendLocalReply(response_code, "", modify_headers, grpc_status, failure_details);
         // Pre UHV HCM did not respect stream_error_on_invalid_http_message
         // and only sent 400 when :path contained fragment
+        // TODO(#28555): make this error respect the stream_error_on_invalid_http_message
         if (!response_encoder_->streamErrorOnInvalidHttpMessage() &&
             failure_details != UhvResponseCodeDetail::get().FragmentInUrlPath) {
           connection_manager_.handleCodecError(failure_details);
