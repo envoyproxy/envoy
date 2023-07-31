5a895e1202561176f829f78f89d0355bc2be3716
Raven Black <ravenblack@dropbox.com>
2023-07-31 14:42:53 +0000

    Revert superfluous whitespace change
    Signed-off-by: Raven Black <ravenblack@dropbox.com>

--- source/extensions/http/cache/simple_http_cache/simple_http_cache.cc
+++ source/extensions/http/cache/simple_http_cache/simple_http_cache.cc
@@ -94,6 +94,7 @@ public:
                   bool end_stream) override {
     ASSERT(!committed_);
     ASSERT(ready_for_next_chunk || end_stream);
+
     body_.add(chunk);
     if (end_stream) {
       ready_for_next_chunk(commit());
