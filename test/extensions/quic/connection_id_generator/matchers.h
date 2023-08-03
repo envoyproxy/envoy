6222a5e24ced5d2a621c293a52c8397b15c3c652
Raven Black <ravenblack@dropbox.com>
2023-08-03 20:07:40 +0000

    Exclude all bpf-related code from matcher for now
    Signed-off-by: Raven Black <ravenblack@dropbox.com>

--- test/extensions/quic/connection_id_generator/matchers.h
+++ test/extensions/quic/connection_id_generator/matchers.h
@@ -17,6 +17,11 @@ namespace Envoy {
 namespace Quic {
 namespace Matcher {
 
+#if defined(SO_ATTACH_REUSEPORT_CBPF) && defined(__linux__) &&                                     \
+    defined(TODO_TEST_FOR_BPF_PROG_RUN_SUPPORT)
+#define SUPPORTS_TESTING_BPF_PROG
+#endif
+
 // A matcher for connection id factories to test both the bpf filter and worker selector
 // functions. Recommended usage is like:
 // EXPECT_THAT(FactoryFunctions(factory, concurrency), GivenPacket(buffer).ReturnsWorkerId(7))
@@ -30,14 +35,8 @@ namespace Matcher {
 class FactoryFunctions {
 public:
   FactoryFunctions(EnvoyQuicConnectionIdGeneratorFactory& factory, uint32_t concurrency)
-      : worker_selector_(factory.getCompatibleConnectionIdWorkerSelector(concurrency)),
-        bpf_prog_(captureBpfProg(factory, concurrency)) {}
-  const QuicConnectionIdWorkerSelector worker_selector_;
-  const sock_fprog* bpf_prog_;
-
-private:
-  static const sock_fprog* captureBpfProg(EnvoyQuicConnectionIdGeneratorFactory& factory,
-                                          uint32_t concurrency) {
+      : worker_selector_(factory.getCompatibleConnectionIdWorkerSelector(concurrency)) {
+#ifdef SUPPORTS_TESTING_BPF_PROG
     Network::Socket::OptionConstSharedPtr sockopt =
         factory.createCompatibleLinuxBpfSocketOption(concurrency);
     // Using a mock socket to capture the socket option which otherwise cannot be
@@ -45,21 +44,24 @@ private:
     Network::MockListenSocket mock_socket;
     const sock_fprog* bpf_prog;
     EXPECT_CALL(mock_socket, setSocketOption(testing::_, testing::_, testing::_, testing::_))
-        .WillOnce([&](int, int, const void* optval, socklen_t) {
-          bpf_prog = static_cast<const sock_fprog*>(optval);
+        .WillOnce([this](int, int, const void* optval, socklen_t) {
+          bpf_prog_ = static_cast<const sock_fprog*>(optval);
           return Api::SysCallIntResult{0, 0};
         });
     sockopt->setOption(mock_socket, envoy::config::core::v3::SocketOption::STATE_BOUND);
-    return bpf_prog;
+#endif
   }
+  const QuicConnectionIdWorkerSelector worker_selector_;
+#ifdef SUPPORTS_TESTING_BPF_PROG
+  const sock_fprog* bpf_prog_;
+#endif
 };
 
 MATCHER_P2(FactoryFunctionsReturnWorkerId, packet, expected_id, "") {
   uint32_t id = arg.worker_selector_(*packet, 0xffffffff);
   // TODO: run the bpf program, something like the below. The headers we have in the docker
   //       container don't seem to provide `bpf()` or `bpf_prog_run()`.
-#if defined(SO_ATTACH_REUSEPORT_CBPF) && defined(__linux__) &&                                     \
-    defined(TODO_TEST_FOR_BPF_PROG_RUN_SUPPORT)
+#ifdef SUPPORTS_TESTING_BPF_PROG
   std::string bytes = packet->toString();
   uint32_t bpf_id = bpf_prog_run(arg.bpf_prog_, bytes.c_str());
 #else
@@ -89,6 +91,8 @@ public:
 private:
   const Buffer::Instance& packet_;
 };
+
+#undef SUPPORTS_TESTING_BPF_PROG
 
 } // namespace Matcher
 } // namespace Quic
