e91b8785607296bcbc823254f564b77addf78cd7
Ryan Hamilton <rch@google.com>
2023-08-08 17:48:19 +0000

    Add a unit test for the mobile fetch client
    Signed-off-by: Ryan Hamilton <rch@google.com>

---
+++ mobile/test/cc/unit/fetch_client_test.cc
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+#include "gtest/gtest.h"
+#include "examples/cc/fetch_client/fetch_client.h"
+
+namespace Envoy {
+namespace Platform {
+namespace {
+
+// This test verifies that the fetch client is able to successfully
+// build and start the Envoy engine. It will panic if it is unable
+// to do so.
+TEST(FetchClientTest, Foo) {
+  Envoy::Fetch client;
+  client.fetch({"https://wwwfff.google.com/"});
+}
+
+} // namespace
+} // namespace Platform
+} // namespace Envoy
