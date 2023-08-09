417d09fb4daca3c0e0ecf5352c2454ad7d549869
pcrao <pcrao@google.com>
2023-08-09 20:32:14 +0000

    Address Feedback.
    Signed-off-by: pcrao <pcrao@google.com>

--- source/common/upstream/default_local_address_selector.cc
+++ source/common/upstream/default_local_address_selector.cc
@@ -8,6 +8,8 @@ namespace Upstream {
 DefaultUpstreamLocalAddressSelector::DefaultUpstreamLocalAddressSelector(
     std::vector<::Envoy::Upstream::UpstreamLocalAddress> upstream_local_addresses)
     : upstream_local_addresses_(std::move(upstream_local_addresses)) {
+  // If bind config is not provided, we insert at least one
+  // ``UpstreamLocalAddress`` with null address.
   ASSERT(upstream_local_addresses_.size() > 0);
 }
 
@@ -18,7 +20,8 @@ UpstreamLocalAddress DefaultUpstreamLocalAddressSelector::getUpstreamLocalAddres
       continue;
     }
 
-    // Invalid addresses should have been rejected in constructor
+    // Invalid addresses should have been rejected while parsing the bind
+    // config.
     ASSERT(local_address.address_->ip() != nullptr);
     if (endpoint_address->ip() != nullptr &&
         local_address.address_->ip()->version() == endpoint_address->ip()->version()) {
