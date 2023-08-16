f39ffca0c1f358ca99e4ab59d8afd5a326d96ce5
wbpcode <wangbaiping@corp.netease.com>
2023-08-16 11:15:16 +0000

    fix test 3
    Signed-off-by: wbpcode <wangbaiping@corp.netease.com>

--- source/extensions/formatter/metadata/metadata.cc
+++ source/extensions/formatter/metadata/metadata.cc
@@ -74,7 +74,8 @@ MetadataFormatterCommandParser::parse(const std::string& command, const std::str
     }
 
     // Return a pointer to formatter provider.
-    return std::make_unique<Envoy::Formatter::StreamInfoFormatter>(
+    return std::make_unique<
+        Envoy::Formatter::StreamInfoFormatter<Envoy::Formatter::HttpFormatterContext>>(
         provider->second(filter_namespace, path, max_length));
   }
   return nullptr;
