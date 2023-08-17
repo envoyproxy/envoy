18a8eb9b30af93883abb62394b3558dad9e02370
Alyssa Wilk <alyssar@chromium.org>
2023-08-17 10:44:37 -0400

    cluster: removing exceptions from cluster creation
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- source/extensions/clusters/logical_dns/logical_dns_cluster.cc
+++ source/extensions/clusters/logical_dns/logical_dns_cluster.cc
@@ -60,22 +60,11 @@ LogicalDnsCluster::LogicalDnsCluster(const envoy::config::cluster::v3::Cluster& 
           cluster, dns_refresh_rate_ms_.count(),
           context.serverFactoryContext().api().randomGenerator());
 
-  const auto& locality_lb_endpoints = load_assignment_.endpoints();
-  if (locality_lb_endpoints.size() != 1 || locality_lb_endpoints[0].lb_endpoints().size() != 1) {
-    if (cluster.has_load_assignment()) {
-      throw EnvoyException(
-          "LOGICAL_DNS clusters must have a single locality_lb_endpoint and a single lb_endpoint");
-    } else {
-      throw EnvoyException("LOGICAL_DNS clusters must have a single host");
-    }
-  }
-
   const envoy::config::core::v3::SocketAddress& socket_address =
       lbEndpoint().endpoint().address().socket_address();
 
-  if (!socket_address.resolver_name().empty()) {
-    throw EnvoyException("LOGICAL_DNS clusters must NOT have a custom resolver name set");
-  }
+  // Checked by factory;
+  ASSERT(socket_address.resolver_name().empty());
   dns_address_ = socket_address.address();
   dns_port_ = socket_address.port_value();
 
@@ -174,13 +163,32 @@ void LogicalDnsCluster::startResolve() {
       });
 }
 
-std::pair<ClusterImplBaseSharedPtr, ThreadAwareLoadBalancerPtr>
+absl::StatusOr<std::pair<ClusterImplBaseSharedPtr, ThreadAwareLoadBalancerPtr>>
 LogicalDnsClusterFactory::createClusterImpl(const envoy::config::cluster::v3::Cluster& cluster,
                                             ClusterFactoryContext& context) {
   auto selected_dns_resolver = selectDnsResolver(cluster, context);
 
-  return std::make_pair(
-      std::make_shared<LogicalDnsCluster>(cluster, context, selected_dns_resolver), nullptr);
+  const auto& load_assignment = cluster.load_assignment();
+  const auto& locality_lb_endpoints = load_assignment.endpoints();
+  if (locality_lb_endpoints.size() != 1 || locality_lb_endpoints[0].lb_endpoints().size() != 1) {
+    if (cluster.has_load_assignment()) {
+      return absl::InvalidArgumentError(
+          "LOGICAL_DNS clusters must have a single locality_lb_endpoint and a single lb_endpoint");
+    } else {
+      return absl::InvalidArgumentError("LOGICAL_DNS clusters must have a single host");
+    }
+  }
+
+  const envoy::config::core::v3::SocketAddress& socket_address =
+      locality_lb_endpoints[0].lb_endpoints()[0].endpoint().address().socket_address();
+  if (!socket_address.resolver_name().empty()) {
+    return absl::InvalidArgumentError(
+        "LOGICAL_DNS clusters must NOT have a custom resolver name set");
+  }
+
+  return std::make_pair(std::shared_ptr<LogicalDnsCluster>(
+                            new LogicalDnsCluster(cluster, context, selected_dns_resolver)),
+                        nullptr);
 }
 
 /**
