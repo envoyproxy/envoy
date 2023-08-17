2cf39713b2fbc832a51c297fedee1b5a6a466779
Ryan Northey <ryan@synca.io>
2023-08-17 11:15:48 +0100

    Revert "tracing: Deprecate OpenTracing (#28987)"
    This reverts commit 494c716cefcf98bc30773f0bd850d9a3788a1615.
    
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- test/extensions/tracers/dynamic_ot/config_test.cc
+++ test/extensions/tracers/dynamic_ot/config_test.cc
@@ -22,7 +22,7 @@ namespace Tracers {
 namespace DynamicOt {
 namespace {
 
-TEST(DynamicOtTracerConfigTest, DEPRECATED_FEATURE_TEST(DynamicOpentracingHttpTracer)) {
+TEST(DynamicOtTracerConfigTest, DynamicOpentracingHttpTracer) {
   NiceMock<Server::Configuration::MockTracerFactoryContext> context;
   EXPECT_CALL(context.server_factory_context_.cluster_manager_,
               getThreadLocalCluster(Eq("fake_cluster")))
