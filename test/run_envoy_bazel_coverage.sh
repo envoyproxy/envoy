eda4fc509bcd4b8a7f35dfdda8c37b4b6d85df26
Alyssa Wilk <alyssar@chromium.org>
2023-08-17 14:46:31 -0400

    Revert "coverage: Use upstream coverage collect script (#28802)"
    This reverts commit 8004b60fc9902f65b58c4a9153eb065d1a4152dc.

--- test/run_envoy_bazel_coverage.sh
+++ test/run_envoy_bazel_coverage.sh
@@ -72,7 +72,6 @@ BAZEL_OUTPUT_BASE="$(bazel "${BAZEL_STARTUP_OPTIONS[@]}" info "${BAZEL_BUILD_OPT
 echo "Running bazel coverage with:"
 echo "  Options: ${BAZEL_BUILD_OPTIONS[*]} ${BAZEL_COVERAGE_OPTIONS[*]}"
 echo "  Targets: ${COVERAGE_TARGETS[*]}"
-
 bazel "${BAZEL_STARTUP_OPTIONS[@]}" coverage "${BAZEL_BUILD_OPTIONS[@]}" "${BAZEL_COVERAGE_OPTIONS[@]}" "${COVERAGE_TARGETS[@]}"
 
 echo "Collecting profile and testlogs"
@@ -81,9 +80,6 @@ if [[ -n "${ENVOY_BUILD_PROFILE}" ]]; then
 fi
 
 if [[ -n "${ENVOY_BUILD_DIR}" ]]; then
-    if [[ -e "${ENVOY_BUILD_DIR}/testlogs.tar.zst" ]]; then
-        rm -f "${ENVOY_BUILD_DIR}/testlogs.tar.zst"
-    fi
     find bazel-testlogs/ -name test.log \
         | tar cf - -T - \
         | bazel "${BAZEL_STARTUP_OPTIONS[@]}" run "${BAZEL_BUILD_OPTIONS[@]}" //tools/zstd -- \
@@ -121,10 +117,6 @@ if [[ "${FUZZ_COVERAGE}" == "true" ]]; then
                     - -T0 -o "${ENVOY_FUZZ_COVERAGE_ARTIFACT}"
     fi
 elif [[ -n "${ENVOY_COVERAGE_ARTIFACT}" ]]; then
-    if [[ -e "${ENVOY_COVERAGE_ARTIFACT}" ]]; then
-        rm "${ENVOY_COVERAGE_ARTIFACT}"
-    fi
-
      tar cf - -C "${COVERAGE_DIR}" --transform 's/^\./coverage/' . \
          | bazel "${BAZEL_STARTUP_OPTIONS[@]}" run "${BAZEL_BUILD_OPTIONS[@]}" //tools/zstd -- \
                  - -T0 -o "${ENVOY_COVERAGE_ARTIFACT}"
