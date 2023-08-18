7b9d8031d80fa5ae18c6665357e88cdaee82b114
Kevin Baichoo <kbaichoo@google.com>
2023-08-18 14:42:51 +0000

    Clarify the stream state enums as strings and provide directionality so fuzz logs are more readable.
    Signed-off-by: Kevin Baichoo <kbaichoo@google.com>

--- test/common/http/codec_impl_fuzz_test.cc
+++ test/common/http/codec_impl_fuzz_test.cc
@@ -110,6 +110,19 @@ public:
   // explore these violations via MutateAction and SwapAction at the connection
   // buffer level.
   enum class StreamState : int { PendingHeaders, PendingDataOrTrailers, Closed };
+  static absl::string_view streamStateToString(StreamState state) {
+    switch (state) {
+    case StreamState::PendingHeaders:
+      static std::string pending_headers = "PendingHeaders";
+      return pending_headers;
+    case StreamState::PendingDataOrTrailers:
+      static std::string pending_data_or_trailers = "PendingDataOrTrailers";
+      return pending_data_or_trailers;
+    case StreamState::Closed:
+      static std::string closed = "Closed";
+      return closed;
+    }
+  }
 
   struct DirectionalState {
     // TODO(mattklein123): Split this more clearly into request and response directional state.
@@ -390,16 +403,16 @@ public:
   void streamAction(const test::common::http::StreamAction& stream_action) {
     switch (stream_action.stream_action_selector_case()) {
     case test::common::http::StreamAction::kRequest: {
-      ENVOY_LOG_MISC(debug, "Request stream action on {} in state {} {}", stream_index_,
-                     static_cast<int>(request_.stream_state_),
-                     static_cast<int>(response_.stream_state_));
+      ENVOY_LOG_MISC(debug, "Request stream action on {} in state request({}) response({})",
+                     stream_index_, streamStateToString(request_.stream_state_),
+                     streamStateToString(response_.stream_state_));
       stream_action_active_ = true;
       if (stream_action.has_dispatching_action()) {
         // Simulate some response action while dispatching request headers, data, or trailers. This
         // may happen as a result of a filter sending a direct response.
-        ENVOY_LOG_MISC(debug, "Setting dispatching action  on {} in state {} {}", stream_index_,
-                       static_cast<int>(request_.stream_state_),
-                       static_cast<int>(response_.stream_state_));
+        ENVOY_LOG_MISC(debug, "Setting dispatching action  on {} in state request({}) response({})",
+                       stream_index_, streamStateToString(request_.stream_state_),
+                       streamStateToString(response_.stream_state_));
         auto request_action = stream_action.dispatching_action().directional_action_selector_case();
         if (request_action == test::common::http::DirectionalAction::kHeaders) {
           EXPECT_CALL(request_.request_decoder_, decodeHeaders_(_, _))
@@ -438,9 +451,9 @@ public:
       break;
     }
     case test::common::http::StreamAction::kResponse: {
-      ENVOY_LOG_MISC(debug, "Response stream action on {} in state {} {}", stream_index_,
-                     static_cast<int>(request_.stream_state_),
-                     static_cast<int>(response_.stream_state_));
+      ENVOY_LOG_MISC(debug, "Response stream action on {} in state request({}) response({})",
+                     stream_index_, streamStateToString(request_.stream_state_),
+                     streamStateToString(response_.stream_state_));
       directionalAction(response_, stream_action.response());
       break;
     }
