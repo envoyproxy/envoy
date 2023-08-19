515c7ab98313d20adee142e0734c18ed51f0ab3a
Vikas Choudhary <choudharyvikas16@gmail.com>
2023-08-19 06:01:10 +0530

    cleanup
    Signed-off-by: Vikas Choudhary <choudharyvikas16@gmail.com>

--- test/extensions/upstreams/tcp/generic/config_test.cc
+++ test/extensions/upstreams/tcp/generic/config_test.cc
@@ -1,10 +1,7 @@
-#include "envoy/extensions/filters/network/tcp_proxy/v3/tcp_proxy.pb.h"
-
 #include "source/common/stream_info/bool_accessor_impl.h"
 #include "source/common/tcp_proxy/tcp_proxy.h"
 #include "source/extensions/upstreams/tcp/generic/config.h"
 
-#include "test/mocks/http/mocks.h"
 #include "test/mocks/server/factory_context.h"
 #include "test/mocks/tcp/mocks.h"
 #include "test/mocks/upstream/cluster_manager.h"
@@ -36,9 +33,6 @@ public:
   NiceMock<StreamInfo::MockStreamInfo> downstream_stream_info_;
   NiceMock<Network::MockConnection> connection_;
   Upstream::MockLoadBalancerContext lb_context_;
-  envoy::extensions::filters::network::tcp_proxy::v3::TcpProxy tcp_proxy_;
-  NiceMock<Stats::MockStore> store_;
-  NiceMock<Envoy::Http::MockStreamDecoderFilterCallbacks> decoder_callbacks_;
   NiceMock<Server::Configuration::MockFactoryContext> context_;
 };
 
@@ -51,8 +45,8 @@ TEST_F(TcpConnPoolTest, TestNoTunnelingConfig) {
 
 TEST_F(TcpConnPoolTest, TestTunnelingDisabledByFilterState) {
   envoy::extensions::filters::network::tcp_proxy::v3::TcpProxy_TunnelingConfig config_proto;
-  tcp_proxy_.mutable_tunneling_config()->set_hostname("host");
-  const TcpProxy::TunnelingConfigHelperImpl config(tcp_proxy_, context_);
+  config_proto.set_hostname("host");
+  const TcpProxy::TunnelingConfigHelperImpl config(config_proto, context_);
 
   downstream_stream_info_.filterState()->setData(
       TcpProxy::DisableTunnelingFilterStateKey,
@@ -67,8 +61,8 @@ TEST_F(TcpConnPoolTest, TestTunnelingDisabledByFilterState) {
 
 TEST_F(TcpConnPoolTest, TestTunnelingNotDisabledIfFilterStateHasFalseValue) {
   envoy::extensions::filters::network::tcp_proxy::v3::TcpProxy_TunnelingConfig config_proto;
-  tcp_proxy_.mutable_tunneling_config()->set_hostname("host");
-  const TcpProxy::TunnelingConfigHelperImpl config(tcp_proxy_, context_);
+  config_proto.set_hostname("host");
+  const TcpProxy::TunnelingConfigHelperImpl config(config_proto, context_);
 
   downstream_stream_info_.filterState()->setData(
       TcpProxy::DisableTunnelingFilterStateKey,
@@ -83,8 +77,8 @@ TEST_F(TcpConnPoolTest, TestTunnelingNotDisabledIfFilterStateHasFalseValue) {
 
 TEST_F(TcpConnPoolTest, TestNoConnPool) {
   envoy::extensions::filters::network::tcp_proxy::v3::TcpProxy_TunnelingConfig config_proto;
-  tcp_proxy_.mutable_tunneling_config()->set_hostname("host");
-  const TcpProxy::TunnelingConfigHelperImpl config(tcp_proxy_, context_);
+  config_proto.set_hostname("host");
+  const TcpProxy::TunnelingConfigHelperImpl config(config_proto, context_);
   EXPECT_CALL(thread_local_cluster_, httpConnPool(_, _, _)).WillOnce(Return(absl::nullopt));
   EXPECT_EQ(nullptr, factory_.createGenericConnPool(
                          thread_local_cluster_, TcpProxy::TunnelingConfigHelperOptConstRef(config),
@@ -92,15 +86,14 @@ TEST_F(TcpConnPoolTest, TestNoConnPool) {
 }
 
 TEST_F(TcpConnPoolTest, Http2Config) {
-  auto info = std::make_shared<Upstream::MockClusterInfo>();
   const std::string fake_cluster_name = "fake_cluster";
-
+  auto info = std::make_shared<Upstream::MockClusterInfo>();
   EXPECT_CALL(*info, features()).WillOnce(Return(Upstream::ClusterInfo::Features::HTTP2));
+  EXPECT_CALL(thread_local_cluster_, info).Times(2).WillRepeatedly((Return(info)));
   EXPECT_CALL(*info, name()).WillOnce(ReturnRef(fake_cluster_name));
-  EXPECT_CALL(thread_local_cluster_, info).Times(2).WillRepeatedly(Return(info));
   envoy::extensions::filters::network::tcp_proxy::v3::TcpProxy_TunnelingConfig config_proto;
-  tcp_proxy_.mutable_tunneling_config()->set_hostname("host");
-  const TcpProxy::TunnelingConfigHelperImpl config(tcp_proxy_, context_);
+  config_proto.set_hostname("host");
+  const TcpProxy::TunnelingConfigHelperImpl config(config_proto, context_);
 
   EXPECT_CALL(thread_local_cluster_, httpConnPool(_, _, _)).WillOnce(Return(absl::nullopt));
   EXPECT_EQ(nullptr, factory_.createGenericConnPool(
@@ -109,16 +102,16 @@ TEST_F(TcpConnPoolTest, Http2Config) {
 }
 
 TEST_F(TcpConnPoolTest, Http3Config) {
-  auto info = std::make_shared<Upstream::MockClusterInfo>();
   const std::string fake_cluster_name = "fake_cluster";
+  auto info = std::make_shared<Upstream::MockClusterInfo>();
   EXPECT_CALL(*info, features())
       .Times(AnyNumber())
       .WillRepeatedly(Return(Upstream::ClusterInfo::Features::HTTP3));
-  EXPECT_CALL(*info, name()).WillOnce(ReturnRef(fake_cluster_name));
   EXPECT_CALL(thread_local_cluster_, info).Times(AnyNumber()).WillRepeatedly(Return(info));
   envoy::extensions::filters::network::tcp_proxy::v3::TcpProxy_TunnelingConfig config_proto;
-  tcp_proxy_.mutable_tunneling_config()->set_hostname("host");
-  const TcpProxy::TunnelingConfigHelperImpl config(tcp_proxy_, context_);
+  EXPECT_CALL(*info, name()).WillOnce(ReturnRef(fake_cluster_name));
+  config_proto.set_hostname("host");
+  const TcpProxy::TunnelingConfigHelperImpl config(config_proto, context_);
   EXPECT_CALL(thread_local_cluster_, httpConnPool(_, _, _)).WillOnce(Return(absl::nullopt));
   EXPECT_EQ(nullptr, factory_.createGenericConnPool(
                          thread_local_cluster_, TcpProxy::TunnelingConfigHelperOptConstRef(config),
