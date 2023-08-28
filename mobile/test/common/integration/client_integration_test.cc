0a7cad3aff388da98fa0f7bf77c7299729193ee1
Alyssa Wilk <alyssar@chromium.org>
2023-08-28 13:01:46 -0400

    H2 test
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- mobile/test/common/integration/client_integration_test.cc
+++ mobile/test/common/integration/client_integration_test.cc
@@ -24,7 +24,6 @@ public:
   void SetUp() override {
     setUpstreamCount(config_helper_.bootstrap().static_resources().clusters_size());
     // TODO(abeyad): Add paramaterized tests for HTTP1, HTTP2, and HTTP3.
-    setUpstreamProtocol(Http::CodecType::HTTP1);
     helper_handle_ = test::SystemHelperPeer::replaceSystemHelper();
     EXPECT_CALL(helper_handle_->mock_helper(), isCleartextPermitted(_))
         .WillRepeatedly(Return(true));
@@ -44,7 +43,6 @@ INSTANTIATE_TEST_SUITE_P(IpVersions, ClientIntegrationTest,
                          TestUtility::ipTestParamsToString);
 
 void ClientIntegrationTest::basicTest() {
-
   Buffer::OwnedImpl request_data = Buffer::OwnedImpl("request body");
   default_request_headers_.addCopy(AutonomousStream::EXPECT_REQUEST_SIZE_BYTES,
                                    std::to_string(request_data.length()));
@@ -73,13 +71,17 @@ void ClientIntegrationTest::basicTest() {
   ASSERT_EQ(cc_.status, "200");
   ASSERT_EQ(cc_.on_data_calls, 2);
   ASSERT_EQ(cc_.on_complete_calls, 1);
-  ASSERT_EQ(cc_.on_header_consumed_bytes_from_response, 27);
+  if (upstreamProtocol() == Http::CodecType::HTTP1) {
+    ASSERT_EQ(cc_.on_header_consumed_bytes_from_response, 27);
+  }
 }
 
 TEST_P(ClientIntegrationTest, Basic) {
   initialize();
   basicTest();
   ASSERT_EQ(cc_.on_complete_received_byte_count, 67);
+  // HTTP/1
+  ASSERT_EQ(1, last_stream_final_intel_.upstream_protocol);
 }
 
 TEST_P(ClientIntegrationTest, LargeResponse) {
@@ -181,6 +183,25 @@ TEST_P(ClientIntegrationTest, ClearTextNotPermitted) {
   ASSERT_EQ(cc_.status, "400");
   ASSERT_EQ(cc_.on_data_calls, 1);
   ASSERT_EQ(cc_.on_complete_calls, 1);
+}
+
+TEST_P(ClientIntegrationTest, BasicHttp2) {
+  EXPECT_CALL(helper_handle_->mock_helper(), isCleartextPermitted(_)).Times(0);
+  EXPECT_CALL(helper_handle_->mock_helper(), validateCertificateChain(_, _));
+  EXPECT_CALL(helper_handle_->mock_helper(), cleanupAfterCertificateValidation());
+
+  setUpstreamProtocol(Http::CodecType::HTTP2);
+  builder_.enablePlatformCertificatesValidation(true);
+
+  upstream_tls_ = true;
+
+  initialize();
+
+  default_request_headers_.setScheme("https");
+
+  basicTest();
+  // HTTP/2
+  ASSERT_EQ(2, last_stream_final_intel_.upstream_protocol);
 }
 
 TEST_P(ClientIntegrationTest, BasicHttps) {
