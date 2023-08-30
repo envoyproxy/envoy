c1ca2a455324fb7c366511cc0ff11076347cbaca
Ryan Northey <ryan@synca.io>
2023-08-30 17:06:04 +0100

    Revert "golang filter: add GetProperty (#28595)"
    This reverts commit 1987e387c3d60801cb8a6e4c417342ae8a12ed13.
    
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- contrib/golang/filters/http/source/go/pkg/http/filter.go
+++ contrib/golang/filters/http/source/go/pkg/http/filter.go
@@ -130,10 +130,6 @@ func (r *httpRequest) LogLevel() api.LogType {
 	return cAPI.HttpLogLevel()
 }
 
-func (r *httpRequest) GetProperty(key string) (string, error) {
-	return cAPI.HttpGetStringProperty(unsafe.Pointer(r), key)
-}
-
 func (r *httpRequest) StreamInfo() api.StreamInfo {
 	return &streamInfo{
 		request: r,
