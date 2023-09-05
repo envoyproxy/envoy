75ea4571b6e8fe12d51863daf99e0c8670299c3d
Alyssa Wilk <alyssar@chromium.org>
2023-09-05 14:10:52 -0400

    tests
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

---
+++ mobile/library/java/io/envoyproxy/envoymobile/utilities/StatsUtils.java
@@ -0,0 +1,30 @@
+package io.envoyproxy.envoymobile.utilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Class for utilities around Envoy stats.
+ */
+public class StatsUtils {
+
+  /**
+   * Takes in a stats string from engine.dumpStats() and parses it into individual map entries.
+   * @param statsString, the string from dumpStats
+   * @return a map of stats entries, e.g. "runtime.load_success", "1"
+   */
+  public static Map<String, String> statsToList(String statsString) {
+    Map<String, String> stats = new HashMap<>();
+
+    String[] lines = statsString.split("\n");
+    for (int i = 0; i < lines.length; i++) {
+      String[] keyValue = lines[i].split(": ");
+      if (keyValue.length != 2) {
+        System.out.println("Unexpected stats token");
+        continue;
+      }
+      stats.put(keyValue[0], keyValue[1]);
+    }
+    return stats;
+  }
+}
