bca9a8eac56ed414bc8eb5da1d288e7ec4f81d98
Raven Black <ravenblack@dropbox.com>
2023-09-06 16:25:30 +0000

    WIP adding test
    Signed-off-by: Raven Black <ravenblack@dropbox.com>

--- test/server/hot_restarting_child_test.cc
+++ test/server/hot_restarting_child_test.cc
@@ -28,9 +28,14 @@ public:
     EXPECT_CALL(os_sys_calls_, bind(_, _, _)).Times(2);
     EXPECT_CALL(os_sys_calls_, close(_)).Times(2);
     hot_restarting_child_ = std::make_unique<HotRestartingChild>(0, 1, "@envoy_domain_socket", 0);
+    EXPECT_CALL(dispatcher_, createFileEvent(_, _, _, Event::FileReadyType::Read))
+        .WillOnce(SaveArg<1>(file_ready_callback_));
+    hot_restarting_child_->initialize(dispatcher_);
   }
   void TearDown() override { hot_restarting_child_.reset(); }
   Api::MockOsSysCalls os_sys_calls_;
+  Event::MockDispatcher dispatcher_;
+  Event::FileReadyCb file_ready_callback_;
   TestThreadsafeSingletonInjector<Api::OsSysCallsImpl> os_calls{&os_sys_calls_};
   std::unique_ptr<HotRestartingChild> hot_restarting_child_;
 };
