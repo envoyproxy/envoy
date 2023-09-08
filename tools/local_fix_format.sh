c8fce61769b2a8d8fafcc8b191805b291572d176
Joshua Marantz <jmarantz@google.com>
2023-09-08 17:27:14 -0400

    tools: Add option to skip bazel for format fix (#29528)
    tools: Add option to skip bazel for format fix
    
    Signed-off-by: Joshua Marantz <jmarantz@google.com>
    
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tools/local_fix_format.sh
+++ tools/local_fix_format.sh
@@ -22,6 +22,27 @@
 # If DISPLAY is set, then tkdiff pops up for some BUILD changes.
 unset DISPLAY
 
+# The following optional argument is added to be able to run this script using Docker,
+# due to a problem to locate clang using WSL on Windows. https://learn.microsoft.com/en-us/windows/wsl/about
+# Call with -docker as the first arument.
+if [[ $# -gt 0 && "$1" == "-docker" ]]; then
+  shift
+  exec ./ci/run_envoy_docker.sh "$0" -run-build-setup "$@"
+fi
+
+if [[ $# -gt 0 && "$1" == "-run-build-setup" ]]; then
+  shift
+  . ci/build_setup.sh
+fi
+
+use_bazel=1
+if [[ $# -gt 0 && "$1" == "-skip-bazel" ]]; then
+  echo -n "WARNING: not using bazel to invoke this script may result in "
+  echo "mismatched versions and incorrect formatting"
+  shift
+  use_bazel=0
+fi
+
 if [[ $# -gt 0 && "$1" == "-verbose" ]]; then
   verbose=1
   shift
@@ -36,8 +57,13 @@ function format_some() {
     if [[ "$verbose" == "1" ]]; then
       set -x
     fi
-    bazel run //tools/code_format:check_format -- fix "$@"
-    ./tools/spelling/check_spelling_pedantic.py fix "$@"
+    if [[ "$use_bazel" == "1" ]]; then
+      bazel run //tools/code_format:check_format -- fix "$@"
+    else
+      for arg in "$@"; do
+        ./tools/spelling/check_spelling_pedantic.py fix "$arg"
+      done
+    fi
   )
 }
 
