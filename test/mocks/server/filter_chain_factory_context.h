ee23a672d057dcfa595e6b7f2046323b4bf1427f
Rama Chavali <rama.rao@salesforce.com>
2023-09-11 19:52:35 +0530

    mock fixes
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- test/mocks/server/filter_chain_factory_context.h
+++ test/mocks/server/filter_chain_factory_context.h
@@ -11,6 +11,20 @@ class MockFilterChainFactoryContext : public MockFactoryContext, public FilterCh
 public:
   MockFilterChainFactoryContext();
   ~MockFilterChainFactoryContext() override;
+
+  void createDynamicFilterConfigProvider(
+      const envoy::config::core::v3::ExtensionConfigSource& config_source,
+      const std::string& filter_config_name,
+      Server::Configuration::ServerFactoryContext& server_context,
+      Server::Configuration::FactoryContext& factory_context,
+      Upstream::ClusterManager& cluster_manager, bool last_filter_in_filter_chain,
+      const std::string& filter_chain_type,
+      const Network::ListenerFilterMatcherSharedPtr& listener_filter_matcher) override {}
+
+  OptRef<Http::FilterFactoryCb>
+  dynamicProviderConfig(const std::string& filter_config_name) override {
+    return absl::nullopt;
+  }
 };
 } // namespace Configuration
 } // namespace Server
