ecf01ee5277a4be5aefb9fe3037ee2a1a6d27068
Ryan Northey <ryan@synca.io>
2023-09-16 12:43:24 +0100

    clang/tidy: Fixes (`/source`)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- source/common/network/io_socket_handle_impl.cc
+++ source/common/network/io_socket_handle_impl.cc
@@ -62,7 +62,7 @@ Api::IoCallUint64Result IoSocketHandleImpl::close() {
   ASSERT(SOCKET_VALID(fd_));
   const int rc = Api::OsSysCallsSingleton::get().close(fd_).return_value_;
   SET_SOCKET_INVALID(fd_);
-  return Api::IoCallUint64Result(rc, Api::IoErrorPtr(nullptr, IoSocketError::deleteIoError));
+  return {static_cast<unsigned long>(rc), Api::IoErrorPtr(nullptr, IoSocketError::deleteIoError)};
 }
 
 bool IoSocketHandleImpl::isOpen() const { return SOCKET_VALID(fd_); }
