1a0dc1112d4a520d912c9d2568cb35c6f8611f79
Ryan Northey <ryan@synca.io>
2023-09-18 09:42:00 +0100

    mobile/ci: Further fix for docs publishing
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- mobile/docs/publish.sh
+++ mobile/docs/publish.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # This is run on every commit that GitHub Actions picks up. It assumes that docs have already been
 # built via docs/build.sh. The push behavior differs depending on the nature of the commit:
@@ -7,32 +7,29 @@
 #                `main` because using `main` as the default branch currently results in 404s.
 # * Otherwise: noop.
 
-set -e
+set -o pipefail
 
 DOCS_DIR=generated/docs
-CHECKOUT_DIR=../envoy-mobile-docs
 BUILD_SHA="$(git rev-parse HEAD)"
 
-if [ "$GITHUB_REF_TYPE" == "tag" ]
-then
-  PUBLISH_DIR="$CHECKOUT_DIR"/docs/envoy-mobile/"$GITHUB_REF_NAME"
-elif [ "$GITHUB_REF_NAME" == "main" ]
-then
-  PUBLISH_DIR="$CHECKOUT_DIR"/docs/envoy-mobile/latest
-else
-  echo "Ignoring docs push"
-  exit 0
+if [[ -z "$MOBILE_DOCS_CHECKOUT_DIR" ]]; then
+    echo "MOBILE_DOCS_CHECKOUT_DIR is not set, exiting" >&2
+    exit 1
 fi
 
-echo 'cloning'
-git clone git@github.com:envoy-mobile/envoy-mobile.github.io "$CHECKOUT_DIR"
+if [[ "$GITHUB_REF_TYPE" == "tag" ]]; then
+    PUBLISH_DIR="$MOBILE_DOCS_CHECKOUT_DIR"/docs/envoy-mobile/"$GITHUB_REF_NAME"
+else
+    PUBLISH_DIR="$MOBILE_DOCS_CHECKOUT_DIR"/docs/envoy-mobile/latest
+fi
 
-git -C "$CHECKOUT_DIR" fetch
-git -C "$CHECKOUT_DIR" checkout -B master origin/master
+echo "Publishing docs in ${PUBLISH_DIR}"
+
+git -C "$MOBILE_DOCS_CHECKOUT_DIR" checkout -B master origin/master
 rm -fr "$PUBLISH_DIR"
 mkdir -p "$PUBLISH_DIR"
 cp -r "$DOCS_DIR"/* "$PUBLISH_DIR"
-cd "$CHECKOUT_DIR"
+cd "$MOBILE_DOCS_CHECKOUT_DIR" || exit 1
 
 git config user.name "envoy-mobile-docs(ci)"
 git config user.email envoy-mobile-docs@users.noreply.github.com
@@ -40,5 +37,11 @@ echo 'add'
 git add .
 echo 'commit'
 git commit -m "docs envoy-mobile@$BUILD_SHA"
-echo 'push'
-git push origin master
+
+if [[ "$MOBILE_PUSH_CHANGES" == "true" ]]; then
+    echo 'push'
+    git push origin master
+else
+    git diff
+    echo "Not pushing for pull request"
+fi
