6887f3417695cc998c33eb3c6d7b9cc7b2d4dc50
thomas.ebner <thomas.ebner@dynatrace.com>
2023-09-20 14:23:57 +0200

    add expolation factor as span attribute; NOTE: parsing is probably wrong, attribute name has to be defined

--- source/extensions/tracers/opentelemetry/samplers/dynatrace/dynatrace_sampler.cc
+++ source/extensions/tracers/opentelemetry/samplers/dynatrace/dynatrace_sampler.cc
@@ -13,27 +13,44 @@ namespace Extensions {
 namespace Tracers {
 namespace OpenTelemetry {
 
+static const char* SAMPLING_EXTRAPOLATION_SPAN_ATTRIBUTE_NAME =
+    "sampling_extrapolation_set_in_sampler";
 SamplingResult
 DynatraceSampler::shouldSample(absl::StatusOr<SpanContext>& parent_context,
                                const std::string& /*trace_id*/, const std::string& /*name*/,
                                ::opentelemetry::proto::trace::v1::Span::SpanKind /*spankind*/,
                                const std::map<std::string, std::string>& /*attributes*/,
                                const std::set<SpanContext> /*links*/) {
+
   SamplingResult result;
+  std::map<std::string, std::string> att;
   uint32_t current_counter = counter_++;
-  if (current_counter % 2 == 0) {
+
+  if (parent_context.ok()) { // there is already a trace,
     result.decision = Decision::RECORD_AND_SAMPLE;
-    result.trace_state = "tracestate_set_in_dynatrace_samplerX";
-    std::map<std::string, std::string> att;
-    att["key_set_in_dynatrace_sampler"] = "value_set_in_dynatrace_sampler";
-    result.attributes = std::make_unique<const std::map<std::string, std::string>>(std::move(att));
-  } else {
-    result.decision = Decision::DROP;
-    if (parent_context.ok()) {
-      result.trace_state = parent_context.value().tracestate();
+    //Expects a tracestate like "FW4;129;12;-2023406815;4539717;5555;17;66;c511;2h02;3h12345678;4h676767"
+    std::vector<absl::string_view> tracestate_components =
+        absl::StrSplit(parent_context->tracestate(), ';', absl::AllowEmpty());
+    if (tracestate_components.size() > 6) {
+      att[SAMPLING_EXTRAPOLATION_SPAN_ATTRIBUTE_NAME] = tracestate_components[5];
+    }
+    result.trace_state = parent_context.value().tracestate();
+  } else { // start new trace
+
+    if (current_counter % 2 == 0) {
+      result.decision = Decision::RECORD_AND_SAMPLE;
+      result.trace_state = "FW4;129;12;-2023406815;4539717;1000;17;66;c511;2h02;3h12345678;4h676767";
+      att[SAMPLING_EXTRAPOLATION_SPAN_ATTRIBUTE_NAME] = "1000";
+    } else {
+      result.decision = Decision::RECORD_ONLY;
+      if (parent_context.ok()) {
+        result.trace_state = parent_context.value().tracestate();
+      }
     }
   }
-
+  if (att.size()) {
+    result.attributes = std::make_unique<const std::map<std::string, std::string>>(std::move(att));
+  }
   return result;
 }
 
