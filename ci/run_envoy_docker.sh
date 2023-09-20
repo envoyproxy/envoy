90c9dc969d52360233cf4cb43cbd305e6b895336
Ryan Northey <ryan@synca.io>
2023-09-20 10:10:52 +0100

    build/image: Bump to include skopeo
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ci/run_envoy_docker.sh
+++ ci/run_envoy_docker.sh
@@ -56,16 +56,6 @@ else
   BUILD_DIR_MOUNT_DEST=/build
   SOURCE_DIR="${PWD}"
   SOURCE_DIR_MOUNT_DEST=/source
-  if [[ -n "$DOCKER_IN_DOCKER" ]]; then
-      DOCKER_START_EXTRA=(
-          "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/ /' | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-          "&& curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/devel_kubic_libcontainers_stable.gpg > /dev/null"
-          "&& apt-get -qq update -y"
-          "&& apt-get -qq install -y --no-install-recommends skopeo")
-  else
-      DOCKER_START_EXTRA=(":")
-  fi
-
   START_COMMAND=(
       "/bin/bash"
       "-lc"
@@ -74,7 +64,6 @@ else
           && usermod -a -G pcap envoybuild \
           && chown envoybuild:envoygroup /build \
           && chown envoybuild /proc/self/fd/2 \
-          && ${DOCKER_START_EXTRA[*]} \
           && sudo -EHs -u envoybuild bash -c 'cd /source && $*'")
 fi
 
