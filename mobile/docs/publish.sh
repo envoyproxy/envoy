59650623ce4b9e929b2beabd9f5c123fe6defc85
phlax <phlax@users.noreply.github.com>
2023-09-20 15:29:42 +0100

    mobile/docs: Publish outside of build container
    Signed-off-by: Ryan Northey <ryan@synca.io>
    
    Signed-off-by: phlax <phlax@users.noreply.github.com>

--- mobile/docs/publish.sh
+++ mobile/docs/publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
 # This is run on every commit that GitHub Actions picks up. It assumes that docs have already been
-# built via docs/build.sh. The push behavior differs depending on the nature of the commit:
-# * Tag commit (e.g. v1.6.0): pushes docs to versioned location.
-# * Main commit: pushes docs to latest. Note that envoy-mobile.github.io uses `master` rather than
+# built via docs/build.sh. The commit behavior differs depending on the nature of the commit:
+# * Tag commit (e.g. v1.6.0): commits docs to versioned location.
+# * Main commit: commits docs to latest. Note that envoy-mobile.github.io uses `master` rather than
 #                `main` because using `main` as the default branch currently results in 404s.
 # * Otherwise: noop.
 
@@ -29,19 +29,10 @@ git -C "$MOBILE_DOCS_CHECKOUT_DIR" checkout -B master origin/master
 rm -fr "$PUBLISH_DIR"
 mkdir -p "$PUBLISH_DIR"
 cp -r "$DOCS_DIR"/* "$PUBLISH_DIR"
-cd "$MOBILE_DOCS_CHECKOUT_DIR" || exit 1
 
-git config user.name "envoy-mobile-docs(ci)"
-git config user.email envoy-mobile-docs@users.noreply.github.com
+git -C "${MOBILE_DOCS_CHECKOUT_DIR}" config user.name "envoy-mobile-docs(ci)"
+git -C "${MOBILE_DOCS_CHECKOUT_DIR}" config user.email envoy-mobile-docs@users.noreply.github.com
 echo 'add'
-git add .
+git -C "${MOBILE_DOCS_CHECKOUT_DIR}" add .
 echo 'commit'
-git commit -m "docs envoy-mobile@$BUILD_SHA"
-
-if [[ "$MOBILE_PUSH_CHANGES" == "true" ]]; then
-    echo 'push'
-    git push origin master
-else
-    git diff
-    echo "Not pushing for pull request"
-fi
+git -C "${MOBILE_DOCS_CHECKOUT_DIR}" commit -m "docs envoy-mobile@$BUILD_SHA"
