67dfc715ca747ff239ff894e678b8fd253c8287b
Raven Black <ravenblack@dropbox.com>
2023-09-21 14:03:13 +0000

    Trailing whitespace
    Signed-off-by: Raven Black <ravenblack@dropbox.com>

--- test/integration/null_test.py
+++ test/integration/null_test.py
@@ -1,4 +1,4 @@
-# py_test targets with empty srcs provoke a build error, so this file is 
+# py_test targets with empty srcs provoke a build error, so this file is
 # necessary for hotrestart_handoff_test to be possible to disable based on config.
 
 if __name__ == '__main__':
