53a91e2a2888b0515f8dd7c27ea981b5a9fd640f
thomas.ebner <thomas.ebner@dynatrace.com>
2023-09-26 14:32:25 +0200

    extend sampler_test

--- test/extensions/tracers/opentelemetry/samplers/sampler_test.cc
+++ test/extensions/tracers/opentelemetry/samplers/sampler_test.cc
@@ -1,7 +1,18 @@
+
+#include "envoy/config/trace/v3/opentelemetry.pb.h"
+#include "envoy/registry/registry.h"
+
+#include "source/common/tracing/http_tracer_impl.h"
+#include "source/extensions/tracers/opentelemetry/opentelemetry_tracer_impl.h"
+#include "source/extensions/tracers/opentelemetry/samplers/sampler.h"
+#include "source/extensions/tracers/opentelemetry/span_context.h"
+
+#include "test/mocks/server/tracer_factory_context.h"
+#include "test/test_common/registry.h"
+#include "test/test_common/utility.h"
+
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-
-#include "source/extensions/tracers/opentelemetry/samplers/sampler.h"
 
 namespace Envoy {
 namespace Extensions {
@@ -9,30 +20,59 @@ namespace Tracers {
 namespace OpenTelemetry {
 namespace {
 
-/*
-class ResourceProviderTest : public testing::Test {
+class TestSampler : public Sampler {
 public:
-  ResourceProviderTest() {
-    resource_a_.attributes_.insert(std::pair<std::string, std::string>("key1", "val1"));
-    resource_b_.attributes_.insert(std::pair<std::string, std::string>("key2", "val2"));
+  MOCK_METHOD(SamplingResult, shouldSample,
+              ((absl::StatusOr<SpanContext>&), (const std::string&), (const std::string&),
+               (::opentelemetry::proto::trace::v1::Span::SpanKind),
+               (const std::map<std::string, std::string>&), (const std::set<SpanContext>)),
+              (override));
+  MOCK_METHOD(std::string, getDescription, (), (const, override));
+  MOCK_METHOD(std::string, modifyTracestate,
+              (const std::string& span_id, const std::string& current_tracestate), (const));
+};
+
+class TestSamplerFactory : public SamplerFactory {
+public:
+  MOCK_METHOD(SamplerPtr, createSampler,
+              (const Protobuf::Message& message,
+               Server::Configuration::TracerFactoryContext& context));
+
+  ProtobufTypes::MessagePtr createEmptyConfigProto() override {
+    return std::make_unique<ProtobufWkt::Struct>();
   }
-  NiceMock<Server::Configuration::MockTracerFactoryContext> context_;
-  Resource resource_a_;
-  Resource resource_b_;
+
+  std::string name() const override { return "envoy.tracers.opentelemetry.samplers.testsampler"; }
 };
 
-*/
+TEST(SamplerFactoryTest, test) {
+  NiceMock<Server::Configuration::MockTracerFactoryContext> context;
 
-class TestSampler: public Sampler {
+  auto test_sampler = std::make_shared<NiceMock<TestSampler>>();
+  TestSamplerFactory sampler_factory;
+  Registry::InjectFactory<SamplerFactory> sampler_factory_registration(sampler_factory);
 
-};
+  EXPECT_CALL(sampler_factory, createSampler(_, _)).WillOnce(Return(test_sampler));
 
-TEST(SamplerFactoryTest, SamplerFactoryTest) {
-	EXPECT_TRUE(true);
+  const std::string yaml_string = R"EOF(
+    grpc_service:
+      envoy_grpc:
+        cluster_name: fake-cluster
+      timeout: 0.250s
+    service_name: my-service
+    sampler:
+      name: envoy.tracers.opentelemetry.samplers.testsampler
+      typed_config:
+        "@type": type.googleapis.com/google.protobuf.Struct
+    )EOF";
+
+  envoy::config::trace::v3::OpenTelemetryConfig opentelemetry_config;
+  TestUtility::loadFromYaml(yaml_string, opentelemetry_config);
+
+  auto driver = std::make_unique<Driver>(opentelemetry_config, context);
 }
 
-
-}
+} // namespace
 } // namespace OpenTelemetry
 } // namespace Tracers
 } // namespace Extensions
