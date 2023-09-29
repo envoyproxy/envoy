c0a5dc140ec5972d8ff9fc5f926494256d809930
Rama Chavali <rama.rao@salesforce.com>
2023-09-29 19:17:51 +0530

    fix test compile
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- test/mocks/server/factory_context.h
+++ test/mocks/server/factory_context.h
@@ -64,7 +64,7 @@ public:
     return nullptr;
   }
   Configuration::DownstreamFilterConfigProviderManagerPtr
-  FactoryContextImpl::downstreamFilterConfigProviderManager() override {
+  downstreamFilterConfigProviderManager() override {
     return nullptr;
   }
 
