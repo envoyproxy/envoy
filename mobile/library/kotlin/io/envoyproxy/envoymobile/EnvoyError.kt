00ba9b9e35ea34c03db370db9e04d4ef4ac09601
Fredy Wijaya <fredyw@google.com>
2023-10-03 20:40:01 +0000

    Run the formatter
    Signed-off-by: Fredy Wijaya <fredyw@google.com>

--- mobile/library/kotlin/io/envoyproxy/envoymobile/EnvoyError.kt
+++ mobile/library/kotlin/io/envoyproxy/envoymobile/EnvoyError.kt
@@ -5,11 +5,12 @@ package io.envoyproxy.envoymobile
  *
  * @param errorCode internal error code associated with the exception that occurred.
  * @param message a description of what exception that occurred.
- * @param attemptCount an optional number of times an operation was attempted before firing
- *                     this error.
+ * @param attemptCount an optional number of times an operation was attempted before firing this
+ *   error.
  * @param cause an optional cause for the exception.
  */
-class EnvoyError constructor(
+class EnvoyError
+constructor(
   val errorCode: Int,
   val message: String,
   val attemptCount: Int? = null,
