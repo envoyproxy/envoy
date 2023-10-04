3dc084271b230565dc7b99838717e475f3e37226
Kuat Yessenov <kuat@google.com>
2023-10-04 20:54:08 +0000

    add coverage
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- test/extensions/filters/network/set_filter_state/integration_test.cc
+++ test/extensions/filters/network/set_filter_state/integration_test.cc
@@ -1,22 +1,8 @@
-#include <memory>
-
-#include "envoy/extensions/filters/network/set_filter_state/v3/set_filter_state.pb.h"
-#include "envoy/extensions/filters/network/set_filter_state/v3/set_filter_state.pb.validate.h"
-
-#include "source/common/protobuf/protobuf.h"
 #include "source/common/router/string_accessor_impl.h"
-#include "source/extensions/filters/network/set_filter_state/config.h"
 
-#include "test/mocks/network/mocks.h"
-#include "test/mocks/server/factory_context.h"
-#include "test/mocks/stream_info/mocks.h"
+#include "test/integration/integration.h"
+#include "test/integration/utility.h"
 #include "test/test_common/utility.h"
-
-#include "gmock/gmock.h"
-#include "gtest/gtest.h"
-
-using testing::NiceMock;
-using testing::ReturnRef;
 
 namespace Envoy {
 namespace Extensions {
@@ -34,38 +20,46 @@ public:
 
 REGISTER_FACTORY(ObjectFooFactory, StreamInfo::FilterState::ObjectFactory);
 
-class SetMetadataIntegrationTest : public testing::Test {
-public:
-  SetMetadataIntegrationTest() = default;
+class SetFilterStateIntegrationTest : public testing::TestWithParam<Network::Address::IpVersion>,
 
-  void runFilter(const std::string& yaml_config) {
-    envoy::extensions::filters::network::set_filter_state::v3::Config proto_config;
-    TestUtility::loadFromYaml(yaml_config, proto_config);
-    auto config = std::make_shared<Filters::Common::SetFilterState::Config>(
-        proto_config.on_new_connection(), StreamInfo::FilterState::LifeSpan::Connection, context_);
-    auto filter = std::make_shared<SetFilterState>(config);
-    NiceMock<Network::MockReadFilterCallbacks> callbacks;
-    filter->initializeReadFilterCallbacks(callbacks);
-    EXPECT_CALL(callbacks.connection_, streamInfo()).WillRepeatedly(ReturnRef(info_));
-    EXPECT_EQ(Network::FilterStatus::Continue, filter->onNewConnection());
+                                      public BaseIntegrationTest {
+public:
+  SetFilterStateIntegrationTest() : BaseIntegrationTest(GetParam(), config()) {}
+
+  static std::string config() {
+    return absl::StrCat(ConfigHelper::baseConfig(), R"EOF(
+    filter_chains:
+      filters:
+      - name: direct_response
+        typed_config:
+          "@type": type.googleapis.com/envoy.extensions.filters.network.set_filter_state.v3.Config
+          on_new_connection:
+          - object_key: foo
+            format_string:
+              text_format_source:
+                inline_string: "bar"
+      - name: envoy.filters.network.echo
+        typed_config:
+            "@type": type.googleapis.com/envoy.extensions.filters.network.echo.v3.Echo
+      )EOF");
   }
 
-  NiceMock<Server::Configuration::MockFactoryContext> context_;
-  NiceMock<StreamInfo::MockStreamInfo> info_;
+  void SetUp() override {
+    useListenerAccessLog("%FILTER_STATE(foo)%");
+    BaseIntegrationTest::initialize();
+  }
 };
 
-TEST_F(SetMetadataIntegrationTest, FromHeader) {
-  const std::string yaml_config = R"EOF(
-  on_new_connection:
-  - object_key: foo
-    format_string:
-      text_format_source:
-        inline_string: "bar"
-  )EOF";
-  runFilter(yaml_config);
-  const auto* foo = info_.filterState()->getDataReadOnly<Router::StringAccessor>("foo");
-  ASSERT_NE(nullptr, foo);
-  EXPECT_EQ(foo->serializeAsString(), "bar");
+INSTANTIATE_TEST_SUITE_P(IpVersions, SetFilterStateIntegrationTest,
+                         testing::ValuesIn(TestEnvironment::getIpVersionsForTest()),
+                         TestUtility::ipTestParamsToString);
+
+TEST_P(SetFilterStateIntegrationTest, OnConnection) {
+  IntegrationTcpClientPtr tcp_client = makeTcpConnection(lookupPort("listener_0"));
+  ASSERT_TRUE(tcp_client->write("hello"));
+  ASSERT_TRUE(tcp_client->connected());
+  tcp_client->close();
+  EXPECT_THAT(waitForAccessLog(listener_access_log_name_), testing::HasSubstr("bar"));
 }
 
 } // namespace SetFilterState
