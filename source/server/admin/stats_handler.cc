7ce1506aa661a578d91fa7b93c6c7e3c858de630
Greg Greenway <ggreenway@apple.com>
2023-10-12 10:14:41 -0700

    fix !ENVOY_ADMIN_HTML
    Signed-off-by: Greg Greenway <ggreenway@apple.com>

--- source/server/admin/stats_handler.cc
+++ source/server/admin/stats_handler.cc
@@ -98,13 +98,12 @@ Admin::RequestPtr StatsHandler::makeRequest(AdminStream& admin_stream) {
 
 #ifdef ENVOY_ADMIN_HTML
   const bool active_mode = params.format_ == StatsFormat::ActiveHtml;
+#else
+  constexpr bool active_mode = false;
+#endif
   return makeRequest(
       server_.stats(), params, server_.clusterManager(),
       [this, active_mode]() -> Admin::UrlHandler { return statsHandler(active_mode); });
-#else
-  return makeRequest(server_.stats(), params,
-                     [this]() -> Admin::UrlHandler { return statsHandler(false); });
-#endif
 }
 
 Admin::RequestPtr StatsHandler::makeRequest(Stats::Store& stats, const StatsParams& params,
