bcc6d59204e2326750c3e63710c35818843d7ab1
Renjie Tang <renjietang@google.com>
2023-10-17 13:23:56 -0700

    complete logging
    Signed-off-by: Renjie Tang <renjietang@google.com>

--- source/common/quic/platform/mobile_impl/quiche_bug_tracker_impl.h
+++ source/common/quic/platform/mobile_impl/quiche_bug_tracker_impl.h
@@ -10,5 +10,5 @@
 
 #define QUICHE_BUG_IMPL(b) QUICHE_DLOG(DFATAL) << #b ": "
 #define QUICHE_BUG_IF_IMPL(b, condition) QUICHE_DLOG_IF(DFATAL, condition) << #b ": "
-#define QUICHE_PEER_BUG_IMPL(b) QUICHE_DLOG(DFATAL)
-#define QUICHE_PEER_BUG_IF_IMPL(b, condition) QUICHE_DLOG_IF(DFATAL, condition)
+#define QUICHE_PEER_BUG_IMPL(b) QUICHE_DLOG(DFATAL) << #b ": "
+#define QUICHE_PEER_BUG_IF_IMPL(b, condition) QUICHE_DLOG_IF(DFATAL, condition) << #b ": "
