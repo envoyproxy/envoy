dd7fcbaf12403e729c873f2cc297f71b920e1af6
Fredy Wijaya <fredyw@google.com>
2023-10-19 18:58:48 +0000

    mobile/ci: Increase the Start emulator timeout to 20 minutes
    This is an attempt to fix multiple timeout failures when starting the
    emulator. This also adds some logging when starting the emulator instead
    of redirecting everything to `/dev/null`.
    
    Signed-off-by: Fredy Wijaya <fredyw@google.com>

--- mobile/ci/start_android_emulator.sh
+++ mobile/ci/start_android_emulator.sh
@@ -6,7 +6,8 @@ echo "y" | "${ANDROID_HOME}/cmdline-tools/latest/bin/sdkmanager" --install 'syst
 echo "no" | "${ANDROID_HOME}/cmdline-tools/latest/bin/avdmanager" create avd -n test_android_emulator -k 'system-images;android-29;google_apis;x86_64' --force
 ls "${ANDROID_HOME}/cmdline-tools/latest/bin/"
 
-nohup "${ANDROID_HOME}/emulator/emulator" -partition-size 1024 -avd test_android_emulator -no-snapshot > /dev/null 2>&1 & {
+# shellcheck disable=SC2094
+nohup "${ANDROID_HOME}/emulator/emulator" -partition-size 1024 -avd test_android_emulator -no-snapshot  > nohup.out 2>&1 | tail -f nohup.out & {
     # shellcheck disable=SC2016
     "${ANDROID_HOME}/platform-tools/adb" wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\''\r'\'') ]]; do sleep 1; done; input keyevent 82'
 }
