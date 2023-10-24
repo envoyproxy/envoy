39917290ba786623cf120d6e8439de8d7445c7ff
Fredy Wijaya <fredyw@google.com>
2023-10-24 22:51:20 +0000

    mobile: Fix a bug where ASSERT is elided at on a release build
    This fixes a bug in the JNI code where a call to `ASSERT(some_func())`
    gets elided causing the code to do nothing.
    
    Signed-off-by: Fredy Wijaya <fredyw@google.com>

--- mobile/library/cc/engine_builder.cc
+++ mobile/library/cc/engine_builder.cc
@@ -877,10 +877,10 @@ std::unique_ptr<envoy::config::bootstrap::v3::Bootstrap> EngineBuilder::generate
     node->mutable_locality()->set_zone(node_locality_->zone);
     node->mutable_locality()->set_sub_zone(node_locality_->sub_zone);
   }
-  ProtobufWkt::Struct& metadata = *node->mutable_metadata();
   if (node_metadata_.has_value()) {
     *node->mutable_metadata() = *node_metadata_;
   }
+  ProtobufWkt::Struct& metadata = *node->mutable_metadata();
   (*metadata.mutable_fields())["app_id"].set_string_value(app_id_);
   (*metadata.mutable_fields())["app_version"].set_string_value(app_version_);
   (*metadata.mutable_fields())["device_os"].set_string_value(device_os_);
