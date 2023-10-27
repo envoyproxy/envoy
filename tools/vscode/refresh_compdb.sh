4d94cd4d50661c4a69e9da2e9982d1f54df8b80a
phlax <phlax@users.noreply.github.com>
2023-10-27 16:20:11 +0100

    deps: Bump `com_github_aignas_rules_shellcheck` -> 0.2.4 (#30543)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- tools/vscode/refresh_compdb.sh
+++ tools/vscode/refresh_compdb.sh
@@ -8,7 +8,7 @@ command -v bazelisk &> /dev/null || bazel_or_isk=bazel
 [[ -z "${EXCLUDE_CONTRIB}" ]] || opts="--exclude_contrib"
 
 # Setting TEST_TMPDIR here so the compdb headers won't be overwritten by another bazel run
-TEST_TMPDIR=${BUILD_DIR:-/tmp}/envoy-compdb tools/gen_compilation_database.py --vscode --bazel=$bazel_or_isk ${opts}
+TEST_TMPDIR=${BUILD_DIR:-/tmp}/envoy-compdb tools/gen_compilation_database.py --vscode --bazel=$bazel_or_isk "${opts}"
 
 # Kill clangd to reload the compilation database
 pkill clangd || :
