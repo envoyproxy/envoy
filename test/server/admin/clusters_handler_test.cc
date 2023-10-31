40e61777f9a32c32b1183010967cf2c121ec9eca
Networking team <networking@lyft.com>
2023-10-31 13:07:03 -0700

    fix cluster_handler_test
    Signed-off-by: Networking team <networking@lyft.com>

--- test/server/admin/clusters_handler_test.cc
+++ test/server/admin/clusters_handler_test.cc
@@ -80,6 +80,8 @@ TEST_P(AdminInstanceTest, ClustersJsonAndText) {
       .WillByDefault(Return(true));
   ON_CALL(*host, healthFlagGet(Upstream::Host::HealthFlag::FAILED_EDS_HEALTH))
       .WillByDefault(Return(false));
+  ON_CALL(*host, healthFlagGet(Upstream::Host::HealthFlag::DRAINING_EDS_HEALTH))
+      .WillByDefault(Return(true));
   ON_CALL(*host, healthFlagGet(Upstream::Host::HealthFlag::DEGRADED_ACTIVE_HC))
       .WillByDefault(Return(true));
   ON_CALL(*host, healthFlagGet(Upstream::Host::HealthFlag::DEGRADED_EDS_HEALTH))
@@ -172,6 +174,7 @@ TEST_P(AdminInstanceTest, ClustersJsonAndText) {
      ],
      "health_status": {
       "eds_health_status": "DEGRADED",
+      "excluded_via_eds_draining": true,
       "failed_active_health_check": true,
       "failed_outlier_check": true,
       "failed_active_degraded_check": true,
@@ -228,7 +231,7 @@ fake_cluster::1.2.3.4:80::rest_counter::10
 fake_cluster::1.2.3.4:80::test_counter::10
 fake_cluster::1.2.3.4:80::test_gauge::11
 fake_cluster::1.2.3.4:80::hostname::foo.com
-fake_cluster::1.2.3.4:80::health_flags::/failed_active_hc/failed_outlier_check/degraded_active_hc/degraded_eds_health/pending_dynamic_removal
+fake_cluster::1.2.3.4:80::health_flags::/failed_active_hc/failed_outlier_check/degraded_active_hc/degraded_eds_health/pending_dynamic_removal/draining_eds_health
 fake_cluster::1.2.3.4:80::weight::5
 fake_cluster::1.2.3.4:80::region::test_region
 fake_cluster::1.2.3.4:80::zone::test_zone
