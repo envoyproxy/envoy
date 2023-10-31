46504c1603dcbf4ff72f18480a79e12d26a9c7d9
Dan Zhang <danzh@google.com>
2023-10-31 16:51:29 +0000

    format
    Signed-off-by: Dan Zhang <danzh@google.com>

--- source/common/quic/envoy_quic_dispatcher.cc
+++ source/common/quic/envoy_quic_dispatcher.cc
@@ -183,9 +183,12 @@ void EnvoyQuicDispatcher::closeConnectionsWithFilterChain(
       // This will remove the connection from the list. And the last removal will remove connections
       // from the map as well.
       connection.close(Network::ConnectionCloseType::NoFlush);
-      if (!delete_sessions_immediately && dynamic_cast<QuicFilterManagerConnectionImpl&>(connection)
-                                              .fix_quic_lifetime_issues()) {
-        // If `envoy.reloadable_features.quic_fix_filter_manager_uaf` is true, the closed sessions need to be deleted right away to consistently handle quic lifetimes. Because upon returning the filter chain configs will be destroyed, and no longer safe to be accessed. If any filters access those configs during destruction, it'll be use-after-free
+      if (!delete_sessions_immediately &&
+          dynamic_cast<QuicFilterManagerConnectionImpl&>(connection).fix_quic_lifetime_issues()) {
+        // If `envoy.reloadable_features.quic_fix_filter_manager_uaf` is true, the closed sessions
+        // need to be deleted right away to consistently handle quic lifetimes. Because upon
+        // returning the filter chain configs will be destroyed, and no longer safe to be accessed.
+        // If any filters access those configs during destruction, it'll be use-after-free
         delete_sessions_immediately = true;
       }
     }
