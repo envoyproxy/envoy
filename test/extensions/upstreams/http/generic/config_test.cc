8e658715e7f9b1dbbabc4d3d6124142af2f6595a
Ryan Hamilton <rch@google.com>
2023-11-02 17:23:36 +0000

    Test
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- test/extensions/upstreams/http/generic/config_test.cc
+++ test/extensions/upstreams/http/generic/config_test.cc
@@ -21,7 +21,7 @@ public:
 
 protected:
   NiceMock<Upstream::MockThreadLocalCluster> thread_local_cluster_;
-  NiceMock<Router::MockRouteEntry> route_entry_;
+  Upstream::ResourcePriority priority_ = Upstream::ResourcePriority::Default;
   Upstream::HostConstSharedPtr host_;
   GenericGenericConnPoolFactory factory_;
 };
@@ -29,26 +29,26 @@ protected:
 TEST_F(GenericGenericConnPoolFactoryTest, CreateValidHttpConnPool) {
   EXPECT_TRUE(factory_.createGenericConnPool(thread_local_cluster_,
                                              Router::GenericConnPoolFactory::UpstreamProtocol::HTTP,
-                                             route_entry_, Envoy::Http::Protocol::Http2, nullptr));
+                                             priority_, Envoy::Http::Protocol::Http2, nullptr));
 }
 
 TEST_F(GenericGenericConnPoolFactoryTest, CreateValidTcpConnPool) {
   EXPECT_TRUE(factory_.createGenericConnPool(thread_local_cluster_,
                                              Router::GenericConnPoolFactory::UpstreamProtocol::TCP,
-                                             route_entry_, Envoy::Http::Protocol::Http2, nullptr));
+                                             priority_, Envoy::Http::Protocol::Http2, nullptr));
 }
 
 TEST_F(GenericGenericConnPoolFactoryTest, CreateValidUdpConnPool) {
   EXPECT_TRUE(factory_.createGenericConnPool(thread_local_cluster_,
                                              Router::GenericConnPoolFactory::UpstreamProtocol::UDP,
-                                             route_entry_, Envoy::Http::Protocol::Http2, nullptr));
+                                             priority_, Envoy::Http::Protocol::Http2, nullptr));
 }
 
 TEST_F(GenericGenericConnPoolFactoryTest, InvalidConnPool) {
   // Passes an invalid UpstreamProtocol and check a nullptr is returned.
   EXPECT_FALSE(factory_.createGenericConnPool(
       thread_local_cluster_, static_cast<Router::GenericConnPoolFactory::UpstreamProtocol>(0xff),
-      route_entry_, Envoy::Http::Protocol::Http2, nullptr));
+      priority_, Envoy::Http::Protocol::Http2, nullptr));
 }
 
 } // namespace Generic
