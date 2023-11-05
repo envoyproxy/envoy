fe7d5767567eb75f294d2c654e788804bd10a45b
Yiqi Ou <ouyiqi.work@gmail.com>
2023-11-04 21:14:26 -0700

    stat properly extract tag for dns filter
    Signed-off-by: Yiqi Ou <ouyiqi.work@gmail.com>

--- test/extensions/filters/udp/dns_filter/dns_filter_integration_test.cc
+++ test/extensions/filters/udp/dns_filter/dns_filter_integration_test.cc
@@ -22,11 +22,6 @@ public:
       : BaseIntegrationTest(GetParam(), configToUse()), api_(Api::createApiForTest()),
         counters_(mock_query_buffer_underflow_, mock_record_name_overflow_, query_parsing_failure_,
                   queries_with_additional_rrs_, queries_with_ans_or_authority_rrs_) {
-    // TODO(ggreenway): add tag extraction rules.
-    // Missing stat tag-extraction rule for stat 'dns_filter.my_prefix.local_a_record_answers' and
-    // stat_prefix 'my_prefix'.
-    skip_tag_extraction_rule_check_ = true;
-
     setupResponseParser();
   }
 
