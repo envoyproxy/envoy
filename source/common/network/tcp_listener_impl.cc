89957339b42b5db1468a98ccfc3af58359e768fa
Kateryna Nezdolii <nezdolik@spotify.com>
2023-11-06 17:14:47 +0100

    Undeprecate global_downstream_max_connections key (#30735)
    Signed-off-by: Kateryna Nezdolii <nezdolik@spotify.com>
    Signed-off-by: Sean Killeen <SeanKilleen@gmail.com>

--- source/common/network/tcp_listener_impl.cc
+++ source/common/network/tcp_listener_impl.cc
@@ -25,14 +25,15 @@ bool TcpListenerImpl::rejectCxOverGlobalLimit() const {
   if (ignore_global_conn_limit_) {
     return false;
   }
-
+  // TODO(nezdolik): deprecate `overload.global_downstream_max_connections` key once
+  // downstream connections monitor extension is stable.
   if (track_global_cx_limit_in_overload_manager_) {
-    // Check if deprecated runtime flag `overload.global_downstream_max_connections` is configured
+    // Check if runtime flag `overload.global_downstream_max_connections` is configured
     // simultaneously with downstream connections monitor in overload manager.
     if (runtime_.threadsafeSnapshot()->get(GlobalMaxCxRuntimeKey)) {
       ENVOY_LOG_ONCE_MISC(
           warn,
-          "Global downstream connections limits is configured via deprecated runtime key {} and in "
+          "Global downstream connections limits is configured via runtime key {} and in "
           "{}. Using overload manager config.",
           GlobalMaxCxRuntimeKey,
           Server::OverloadProactiveResources::get().GlobalDownstreamMaxConnections);
