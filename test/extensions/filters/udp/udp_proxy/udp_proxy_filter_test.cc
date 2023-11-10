d7728e9f86391fa465f7c38ee9e43cd07f224d44
Issa Abu Kalbein <iabukalbein@microsoft.com>
2023-11-10 15:03:57 +0200

    fix format
    Signed-off-by: Issa Abu Kalbein <iabukalbein@microsoft.com>

--- test/extensions/filters/udp/udp_proxy/udp_proxy_filter_test.cc
+++ test/extensions/filters/udp/udp_proxy/udp_proxy_filter_test.cc
@@ -482,7 +482,7 @@ upstream_socket_config:
   EXPECT_EQ(output_.front(), "17 3 17 3 0 1 0");
   EXPECT_THAT(output_.back(),
               testing::ContainsRegex("17 3 17 3 10.0.0.1:1000 10.0.0.2:80 20.0.0.1:443 "
-                                    "[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}"));
+                                     "[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}"));
 }
 
 // Route with source IP.
