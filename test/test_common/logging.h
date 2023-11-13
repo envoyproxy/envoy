270513f3e5a5db47f6cf64539febb36d20a0fbda
Raven Black <ravenblack@dropbox.com>
2023-11-13 16:48:22 +0000

    Strict namespace
    Signed-off-by: Raven Black <ravenblack@dropbox.com>

--- test/test_common/logging.h
+++ test/test_common/logging.h
@@ -98,7 +98,7 @@ using ExpectedLogMessages = std::vector<StringPair>;
 #define EXPECT_LOG_CONTAINS_ALL_OF_HELPER(expected_messages, stmt, escaped)                        \
   do {                                                                                             \
     ASSERT_FALSE(expected_messages.empty()) << "Expected messages cannot be empty.";               \
-    resetEnvoyBugCountersForTest();                                                                \
+    ::Envoy::Assert::resetEnvoyBugCountersForTest();                                               \
     Envoy::LogLevelSetter save_levels(spdlog::level::trace);                                       \
     Envoy::Logger::DelegatingLogSinkSharedPtr sink_ptr = Envoy::Logger::Registry::getSink();       \
     sink_ptr->setShouldEscape(escaped);                                                            \
