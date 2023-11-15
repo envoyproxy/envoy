043f443b9efe183cb22de9c1f5ab82ac2749c05f
wbpcode <wbphub@live.com>
2023-11-15 13:14:50 +0000

    diminish throshold of wasm filter
    Signed-off-by: wbpcode <wbphub@live.com>

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -35,7 +35,7 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/extensions/filters/http/grpc_json_transcoder:93.8" # TODO(#28232)
 "source/extensions/filters/http/ip_tagging:88.0"
 "source/extensions/filters/http/kill_request:91.7" # Death tests don't report LCOV
-"source/extensions/filters/http/wasm:1.9"
+"source/extensions/filters/http/wasm:1.8"
 "source/extensions/filters/listener/original_src:92.1"
 "source/extensions/filters/network/common:96.4"
 "source/extensions/filters/network/mongo_proxy:96.0"
