2b7aaf0d80e4cfa5f02e3efbb450d6ec93f617ac
code <wangbaiping@corp.netease.com>
2023-11-16 09:46:18 +0800

    factory context simplification: update code in contrib to access server-wide resource only by server context (#30867)
    * factory context simplification: update code in contrib to access server-wide resource only by server context
    
    Signed-off-by: wbpcode <wbphub@live.com>
    
    * fix test
    
    Signed-off-by: wbpcode <wbphub@live.com>
    
    ---------
    
    Signed-off-by: wbpcode <wbphub@live.com>

--- contrib/squash/filters/http/test/config_test.cc
+++ contrib/squash/filters/http/test/config_test.cc
@@ -28,7 +28,7 @@ TEST(SquashFilterConfigFactoryTest, SquashFilterCorrectYaml) {
   envoy::extensions::filters::http::squash::v3::Squash proto_config;
   TestUtility::loadFromYaml(yaml_string, proto_config);
   NiceMock<Server::Configuration::MockFactoryContext> context;
-  context.cluster_manager_.initializeClusters({"fake_cluster"}, {});
+  context.server_factory_context_.cluster_manager_.initializeClusters({"fake_cluster"}, {});
   SquashFilterConfigFactory factory;
   Http::FilterFactoryCb cb =
       factory.createFilterFactoryFromProto(proto_config, "stats", context).value();
--- contrib/sxg/filters/http/test/config_test.cc
+++ contrib/sxg/filters/http/test/config_test.cc
@@ -26,11 +26,12 @@ void expectCreateFilter(std::string yaml, bool is_sds_config) {
   FilterFactory factory;
   ProtobufTypes::MessagePtr proto_config = factory.createEmptyConfigProto();
   TestUtility::loadFromYaml(yaml, *proto_config);
-  Server::Configuration::MockFactoryContext context;
-  context.cluster_manager_.initializeClusters({"foo"}, {});
+  testing::NiceMock<Server::Configuration::MockFactoryContext> context;
+  context.server_factory_context_.cluster_manager_.initializeClusters({"foo"}, {});
 
   // This returns non-nullptr for certificate and private_key.
-  auto& secret_manager = context.cluster_manager_.cluster_manager_factory_.secretManager();
+  auto& secret_manager =
+      context.server_factory_context_.cluster_manager_.cluster_manager_factory_.secretManager();
   if (is_sds_config) {
     ON_CALL(secret_manager, findOrCreateGenericSecretProvider(_, _, _, _))
         .WillByDefault(Return(std::make_shared<Secret::GenericSecretConfigProviderImpl>(
@@ -41,10 +42,10 @@ void expectCreateFilter(std::string yaml, bool is_sds_config) {
             envoy::extensions::transport_sockets::tls::v3::GenericSecret())));
   }
   EXPECT_CALL(context, messageValidationVisitor());
-  EXPECT_CALL(context, clusterManager());
+  EXPECT_CALL(context.server_factory_context_, clusterManager());
   EXPECT_CALL(context, scope());
-  EXPECT_CALL(context, timeSource());
-  EXPECT_CALL(context, api());
+  EXPECT_CALL(context.server_factory_context_, timeSource());
+  EXPECT_CALL(context.server_factory_context_, api());
   EXPECT_CALL(context, initManager()).Times(2);
   EXPECT_CALL(context, getTransportSocketFactoryContext());
   Http::FilterFactoryCb cb =
@@ -71,7 +72,8 @@ validity_url: "/.sxg/validity.msg"
   TestUtility::loadFromYaml(yaml, *proto_config);
   NiceMock<Server::Configuration::MockFactoryContext> context;
 
-  auto& secret_manager = context.cluster_manager_.cluster_manager_factory_.secretManager();
+  auto& secret_manager =
+      context.server_factory_context_.cluster_manager_.cluster_manager_factory_.secretManager();
   ON_CALL(secret_manager, findStaticGenericSecretProvider(
                               failed_secret_name == "private_key" ? "certificate" : "private_key"))
       .WillByDefault(Return(std::make_shared<Secret::GenericSecretConfigProviderImpl>(
