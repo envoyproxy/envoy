3b701c52f5d6a157237b26bc320954bbc0d06de4
He Jie Xu <hejie.xu@intel.com>
2023-11-17 06:26:36 +0000

    Fix multiplexed_integration_test with timeout
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- test/integration/multiplexed_integration_test.cc
+++ test/integration/multiplexed_integration_test.cc
@@ -2553,23 +2553,9 @@ TEST_P(Http2FrameIntegrationTest, CloseConnectionWithDeferredStreams) {
   ASSERT_TRUE(tcp_client_->connected());
   // Drop the downstream connection
   tcp_client_->close();
-  // NOTE (soulxu): the connection may closed before all the streams are parsed when
-  // using the io-uring. So we need to check the number of reset streams equal to
-  // the number of streams. Before check the number of reset stream, we need ensure
-  // all the streams are reset by checking the active connection.
-  test_server_->waitForGaugeEq("http.config_test.downstream_cx_active", 0,
-                               TestUtility::DefaultTimeout * 3);
-  auto downstream_rq_total_counter = Envoy::TestUtility::findCounter(
-      test_server_->statStore(), "http.config_test.downstream_rq_total");
-  auto downstream_rq_completed = Envoy::TestUtility::findCounter(
-      test_server_->statStore(), "http.config_test.downstream_rq_completed");
-  test_server_->waitForGaugeEq("http.config_test.downstream_rq_active", 0,
-                               TestUtility::DefaultTimeout * 3);
   // Test that Envoy can clean-up deferred streams
   // Make the timeout longer to accommodate non optimized builds
-  test_server_->waitForCounterEq("http.config_test.downstream_rq_rx_reset",
-                                 downstream_rq_total_counter->value() -
-                                     downstream_rq_completed->value(),
+  test_server_->waitForCounterEq("http.config_test.downstream_rq_rx_reset", kRequestsSentPerIOCycle,
                                  TestUtility::DefaultTimeout * 3);
 }
 
