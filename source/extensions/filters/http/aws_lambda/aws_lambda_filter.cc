5447de25870d4ca24d43d85f783453b02f63df74
Neal Turett <neal.turett@datadoghq.com>
2023-11-20 14:51:27 -0500

    clean up iteration by auto&
    Signed-off-by: Neal Turett <neal.turett@datadoghq.com>

--- source/extensions/filters/http/aws_lambda/aws_lambda_filter.cc
+++ source/extensions/filters/http/aws_lambda/aws_lambda_filter.cc
@@ -286,8 +286,8 @@ void Filter::jsonizeRequest(Http::RequestHeaderMap const& headers, const Buffer:
 
   // Wrap the Query String
   if (headers.Path()) {
-    for (auto&& kv_pair :
-         Http::Utility::QueryParamsMulti::parseQueryString(headers.getPathValue()).data()) {
+    auto queryParams = Http::Utility::QueryParamsMulti::parseQueryString(headers.getPathValue());
+    for (const auto& kv_pair : queryParams.data()) {
       json_req.mutable_query_string_parameters()->insert({kv_pair.first, kv_pair.second[0]});
     }
   }
