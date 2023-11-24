52fab2e5ad72555fd62d473238b00a453b66aa77
Sunil Narasimhamurthy <sunnrs@amazon.com>
2023-11-24 17:15:43 +0000

    ASSERT receiver
    Signed-off-by: Sunil Narasimhamurthy <sunnrs@amazon.com>
    
    Signed-off-by: Nigel Brittain <nbaws@amazon.com>

--- source/extensions/common/aws/metadata_fetcher.cc
+++ source/extensions/common/aws/metadata_fetcher.cc
@@ -49,7 +49,6 @@ public:
   void fetch(Http::RequestMessage& message, Tracing::Span& parent_span,
              MetadataFetcher::MetadataReceiver& receiver) override {
     ASSERT(!request_);
-    ASSERT(!receiver_);
     complete_ = false;
     receiver_ = makeOptRef(receiver);
     const auto thread_local_cluster = cm_.getThreadLocalCluster(cluster_name_);
@@ -116,6 +115,7 @@ public:
 
   // HTTP async receive method on success.
   void onSuccess(const Http::AsyncClient::Request&, Http::ResponseMessagePtr&& response) override {
+    ASSERT(receiver_);
     complete_ = true;
     const uint64_t status_code = Http::Utility::getResponseStatus(response->headers());
     if (status_code == enumToInt(Http::Code::OK)) {
@@ -145,6 +145,7 @@ public:
   // HTTP async receive method on failure.
   void onFailure(const Http::AsyncClient::Request&,
                  Http::AsyncClient::FailureReason reason) override {
+    ASSERT(receiver_);
     ENVOY_LOG(debug, "{}: fetch AWS Metadata [cluster = {}]: network error {}", __func__,
               cluster_name_, enumToInt(reason));
     complete_ = true;
@@ -162,10 +163,7 @@ private:
   OptRef<MetadataFetcher::MetadataReceiver> receiver_;
   OptRef<Http::AsyncClient::Request> request_;
 
-  void reset() {
-    request_.reset();
-    receiver_.reset();
-  }
+  void reset() { request_.reset(); }
 };
 } // namespace
 
