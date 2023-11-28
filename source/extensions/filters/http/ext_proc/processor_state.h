7730f8bb5e0aeaa807a88d88d5c39a517b735f89
yanjunxiang-google <78807980+yanjunxiang-google@users.noreply.github.com>
2023-11-27 20:53:00 -0500

    Revert ext_proc: send attributes #30781 (#31017)
    Signed-off-by: Yanjun Xiang <yanjunxiang@google.com>

--- source/extensions/filters/http/ext_proc/processor_state.h
+++ source/extensions/filters/http/ext_proc/processor_state.h
@@ -172,8 +172,6 @@ public:
   virtual envoy::service::ext_proc::v3::HttpTrailers*
   mutableTrailers(envoy::service::ext_proc::v3::ProcessingRequest& request) const PURE;
 
-  virtual StreamInfo::StreamInfo& streamInfo() PURE;
-
 protected:
   void setBodyMode(
       envoy::extensions::filters::http::ext_proc::v3::ProcessingMode_BodySendMode body_mode);
@@ -285,8 +283,6 @@ public:
   void requestWatermark() override;
   void clearWatermark() override;
 
-  StreamInfo::StreamInfo& streamInfo() override { return decoder_callbacks_->streamInfo(); }
-
 private:
   void setProcessingModeInternal(
       const envoy::extensions::filters::http::ext_proc::v3::ProcessingMode& mode);
@@ -359,8 +355,6 @@ public:
 
   void requestWatermark() override;
   void clearWatermark() override;
-
-  StreamInfo::StreamInfo& streamInfo() override { return encoder_callbacks_->streamInfo(); }
 
 private:
   void setProcessingModeInternal(
