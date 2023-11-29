8964ad965022dcf239bbd7e47769efb75b49a68b
wbpcode <wbphub@live.com>
2023-11-29 14:38:33 +0000

    fix...
    Signed-off-by: wbpcode <wbphub@live.com>

--- mobile/library/common/engine.cc
+++ mobile/library/common/engine.cc
@@ -91,7 +91,8 @@ envoy_status_t Engine::main(std::unique_ptr<Envoy::OptionsImplBase>&& options) {
           ASSERT(Thread::MainThread::isMainOrTestThread());
 
           Envoy::Server::GenericFactoryContextImpl generic_context(
-              server_->serverFactoryContext(), server_->messageValidationVisitor());
+              server_->serverFactoryContext(),
+              server_->serverFactoryContext().messageValidationVisitor());
           connectivity_manager_ = Network::ConnectivityManagerFactory{generic_context}.get();
           auto v4_interfaces = connectivity_manager_->enumerateV4Interfaces();
           auto v6_interfaces = connectivity_manager_->enumerateV6Interfaces();
