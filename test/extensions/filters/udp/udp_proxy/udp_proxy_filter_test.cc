1d4981bacacf33fdc5f60cfd56bede2802770a79
IssaAbuKalbein <86603440+IssaAbuKalbein@users.noreply.github.com>
2023-11-30 19:47:41 +0200

    UDP Proxy: Add access log options to UDP session access log (#30881)
    Add access log options for UDP session access log to support flushing session access log periodically, and support flushing session access log on upstream tunnel connected when using UDP tunneling.
    
    Additional Description:
    Risk Level: low
    Testing: unit tests, integration tests
    Docs Changes:
    Release Notes:
    Platform Specific Features: None
    
    Signed-off-by: Issa Abu Kalbein <iabukalbein@microsoft.com>

--- test/extensions/filters/udp/udp_proxy/udp_proxy_filter_test.cc
+++ test/extensions/filters/udp/udp_proxy/udp_proxy_filter_test.cc
@@ -438,7 +438,8 @@ TEST_F(UdpProxyFilterTest, BasicFlow) {
       "%DOWNSTREAM_REMOTE_ADDRESS% "
       "%DOWNSTREAM_LOCAL_ADDRESS% "
       "%UPSTREAM_HOST% "
-      "%STREAM_ID%";
+      "%STREAM_ID% "
+      "%ACCESS_LOG_TYPE%";
 
   const std::string proxy_access_log_format =
       "%DYNAMIC_METADATA(udp.proxy.proxy:bytes_received)% "
@@ -488,9 +489,12 @@ upstream_socket_config:
   filter_.reset();
   EXPECT_EQ(output_.size(), 2);
   EXPECT_EQ(output_.front(), "17 3 17 3 0 1 0");
-  EXPECT_TRUE(std::regex_match(output_.back(),
-                               std::regex("17 3 17 3 10.0.0.1:1000 10.0.0.2:80 20.0.0.1:443 "
-                                          "[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}")));
+  const std::string session_access_log_regex =
+      "17 3 17 3 10.0.0.1:1000 10.0.0.2:80 20.0.0.1:443 "
+      "[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12} " +
+      AccessLogType_Name(AccessLog::AccessLogType::UdpSessionEnd);
+
+  EXPECT_TRUE(std::regex_match(output_.back(), std::regex(session_access_log_regex)));
 }
 
 // Route with source IP.
@@ -1839,8 +1843,9 @@ public:
     header_evaluator_ = Envoy::Router::HeaderParser::configure(headers_to_add);
     config_ = std::make_unique<NiceMock<MockUdpTunnelingConfig>>(*header_evaluator_);
     stream_info_.downstream_connection_info_provider_->setConnectionID(0);
-    pool_ = std::make_unique<TunnelingConnectionPoolImpl>(cluster_, &context_, *config_, callbacks_,
-                                                          stream_info_);
+    session_access_logs_ = std::make_unique<std::vector<AccessLog::InstanceSharedPtr>>();
+    pool_ = std::make_unique<TunnelingConnectionPoolImpl>(
+        cluster_, &context_, *config_, callbacks_, stream_info_, false, *session_access_logs_);
   }
 
   void createNewStream() { pool_->newStream(stream_callbacks_); }
@@ -1851,6 +1856,7 @@ public:
   std::unique_ptr<NiceMock<MockUdpTunnelingConfig>> config_;
   NiceMock<MockUpstreamTunnelCallbacks> callbacks_;
   NiceMock<StreamInfo::MockStreamInfo> stream_info_;
+  std::unique_ptr<std::vector<AccessLog::InstanceSharedPtr>> session_access_logs_;
   NiceMock<MockHttpStreamCallbacks> stream_callbacks_;
   NiceMock<Http::MockRequestEncoder> request_encoder_;
   std::shared_ptr<NiceMock<Upstream::MockHostDescription>> upstream_host_{
@@ -1917,12 +1923,13 @@ TEST_F(TunnelingConnectionPoolImplTest, FactoryTest) {
   setup();
 
   TunnelingConnectionPoolFactory factory;
-  auto valid_pool = factory.createConnPool(cluster_, &context_, *config_, callbacks_, stream_info_);
+  auto valid_pool = factory.createConnPool(cluster_, &context_, *config_, callbacks_, stream_info_,
+                                           false, *session_access_logs_);
   EXPECT_FALSE(valid_pool == nullptr);
 
   EXPECT_CALL(cluster_, httpConnPool(_, _, _)).WillOnce(Return(absl::nullopt));
-  auto invalid_pool =
-      factory.createConnPool(cluster_, &context_, *config_, callbacks_, stream_info_);
+  auto invalid_pool = factory.createConnPool(cluster_, &context_, *config_, callbacks_,
+                                             stream_info_, false, *session_access_logs_);
   EXPECT_TRUE(invalid_pool == nullptr);
 }
 
