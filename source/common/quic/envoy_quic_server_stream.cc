4d471642f3b1029d794da26403168b26e84da446
Steve Wang <wangsteve@google.com>
2023-12-04 12:24:37 -0500

    Remove names for unused parameters.
    Signed-off-by: Steve Wang <wangsteve@google.com>

--- source/common/quic/envoy_quic_server_stream.cc
+++ source/common/quic/envoy_quic_server_stream.cc
@@ -125,7 +125,7 @@ void EnvoyQuicServerStream::encodeData(Buffer::Instance& data, bool end_stream) 
       single_slice_buffer->move(data, slice.len_);
       quic_slices.emplace_back(
           reinterpret_cast<char*>(single_slice_buffer->frontSlice().mem_), slice.len_,
-          [single_slice_buffer = std::move(single_slice_buffer)](const char* buf) {});
+          [single_slice_buffer = std::move(single_slice_buffer)](const char*) {});
     }
     quic::QuicConsumedData result{0, false};
     absl::Span<quiche::QuicheMemSlice> span(quic_slices);
