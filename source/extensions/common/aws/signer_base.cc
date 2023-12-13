72d2cf554d91f16f80381f4eb4cd7592a2ca5962
Nigel Brittain <nbaws@amazon.com>
2023-12-13 11:54:50 +0000

    refactor signer classes
    Signed-off-by: Nigel Brittain <nbaws@amazon.com>

---
+++ source/extensions/common/aws/signer_base.cc
@@ -0,0 +1,118 @@
+#include <openssl/ssl.h>
+
+#include <cstddef>
+
+#include "envoy/common/exception.h"
+
+#include "source/common/buffer/buffer_impl.h"
+#include "source/common/common/fmt.h"
+#include "source/common/common/hex.h"
+#include "source/common/crypto/utility.h"
+#include "source/common/http/headers.h"
+#include "source/extensions/common/aws/utility.h"
+#include "source/extensions/common/aws/signer_base.h"
+
+#include "absl/strings/str_join.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Common {
+namespace Aws {
+
+void SignerBase::sign(Http::RequestMessage& message, bool sign_body,
+                           const absl::string_view override_region) {
+  const auto content_hash = createContentHash(message, sign_body);
+  auto& headers = message.headers();
+  sign(headers, content_hash, override_region);
+}
+
+void SignerBase::signEmptyPayload(Http::RequestHeaderMap& headers,
+                                       const absl::string_view override_region) {
+  headers.setReference(SignatureHeaders::get().ContentSha256,
+                       SignatureConstants::get().HashedEmptyString);
+  sign(headers, SignatureConstants::get().HashedEmptyString, override_region);
+}
+
+void SignerBase::signUnsignedPayload(Http::RequestHeaderMap& headers,
+                                          const absl::string_view override_region) {
+  headers.setReference(SignatureHeaders::get().ContentSha256,
+                       SignatureConstants::get().UnsignedPayload);
+  sign(headers, SignatureConstants::get().UnsignedPayload, override_region);
+}
+
+// Required only for sigv4a
+void SignerBase::addRegionHeader(ABSL_ATTRIBUTE_UNUSED Http::RequestHeaderMap &headers, ABSL_ATTRIBUTE_UNUSED const absl::string_view override_region) const
+{
+}
+
+std::string SignerBase::getRegion() const
+{
+  return region_;
+}
+
+void SignerBase::sign(Http::RequestHeaderMap& headers, const std::string& content_hash,
+                           const absl::string_view override_region) {
+  headers.setReferenceKey(SignatureHeaders::get().ContentSha256, content_hash);
+  const auto& credentials = credentials_provider_->getCredentials();
+  if (!credentials.accessKeyId() || !credentials.secretAccessKey()) {
+    // Empty or "anonymous" credentials are a valid use-case for non-production environments.
+    // This behavior matches what the AWS SDK would do.
+    return;
+  }
+  const auto* method_header = headers.Method();
+  if (method_header == nullptr) {
+    throw EnvoyException("Message is missing :method header");
+  }
+  const auto* path_header = headers.Path();
+  if (path_header == nullptr) {
+    throw EnvoyException("Message is missing :path header");
+  }
+
+  if (credentials.sessionToken()) {
+    headers.addCopy(SignatureHeaders::get().SecurityToken, credentials.sessionToken().value());
+  }
+  const auto long_date = long_date_formatter_.now(time_source_);
+  const auto short_date = short_date_formatter_.now(time_source_);
+  headers.addCopy(SignatureHeaders::get().Date, long_date);
+
+  addRegionHeader(headers, override_region);
+
+  // Phase 1: Create a canonical request
+  const auto canonical_headers = Utility::canonicalizeHeaders(headers, excluded_header_matchers_);
+  const auto canonical_request = Utility::createCanonicalRequest(
+      service_name_, method_header->value().getStringView(), path_header->value().getStringView(),
+      canonical_headers, content_hash);
+  ENVOY_LOG(debug, "Canonical request:\n{}", canonical_request);
+
+  // Phase 2: Create a string to sign
+  const auto credential_scope = createCredentialScope(short_date, override_region);
+  const auto string_to_sign = createStringToSign(canonical_request, long_date, credential_scope);
+  ENVOY_LOG(debug, "String to sign:\n{}", string_to_sign);
+
+  // Phase 3: Create a signature
+  const auto signature = createSignature(credentials.accessKeyId().value(), credentials.secretAccessKey().value(), short_date,
+                                         string_to_sign, override_region);
+  // Phase 4: Sign request
+  const auto authorization_header = createAuthorizationHeader(
+      credentials.accessKeyId().value(), credential_scope, canonical_headers, signature);
+  ENVOY_LOG(debug, "Signing request with: {}", authorization_header);
+  headers.addCopy(Http::CustomHeaders::get().Authorization, authorization_header);
+}
+
+std::string SignerBase::createContentHash(Http::RequestMessage& message,
+                                                const bool sign_body) const {
+  if (!sign_body) {
+    return SignatureConstants::get().HashedEmptyString;
+  }
+  auto& crypto_util = Envoy::Common::Crypto::UtilitySingleton::get();
+  const auto content_hash = message.body().length() > 0
+                                ? Hex::encode(crypto_util.getSha256Digest(message.body()))
+                                : SignatureConstants::get().HashedEmptyString;
+  return content_hash;
+}
+
+
+} // namespace Aws
+} // namespace Common
+} // namespace Extensions
+} // namespace Envoy
--- source/extensions/common/aws/sigv4a_signer_impl.h
+++ source/extensions/common/aws/sigv4a_signer_impl.h
@@ -9,25 +9,22 @@
 #include "source/common/singleton/const_singleton.h"
 #include "source/extensions/common/aws/credentials_provider.h"
 #include "source/extensions/common/aws/signer.h"
+#include "source/extensions/common/aws/signer_base.h"
 
 namespace Envoy {
 namespace Extensions {
 namespace Common {
 namespace Aws {
 
-class SigV4ASignatureHeaderValues {
+class SigV4ASignatureHeaderValues: public SignatureHeaderValues {
 public:
-  const Http::LowerCaseString ContentSha256{"x-amz-content-sha256"};
-  const Http::LowerCaseString Date{"x-amz-date"};
-  const Http::LowerCaseString SecurityToken{"x-amz-security-token"};
   const Http::LowerCaseString RegionSet{"x-amz-region-set"};
 };
 
 using SigV4ASignatureHeaders = ConstSingleton<SigV4ASignatureHeaderValues>;
 
-class SigV4ASignatureConstantValues {
+class SigV4ASignatureConstantValues: public SignatureConstantValues {
 public:
-  const std::string Aws4Request{"aws4_request"};
   const std::string SigV4AAuthorizationHeaderFormat{
       "AWS4-ECDSA-P256-SHA256 Credential={}/{}, SignedHeaders={}, Signature={}"};
   const std::string SigV4ACredentialScopeFormat{"{}/{}/aws4_request"};
@@ -36,10 +33,6 @@ public:
   const std::string SigV4ASignatureVersion{"AWS4A"};
   const std::string SigV4AStringToSignFormat{"AWS4-ECDSA-P256-SHA256\n{}\n{}\n{}"};
   const std::string SigV4ALabel = "AWS4-ECDSA-P256-SHA256";
-
-  const std::string LongDateFormat{"%Y%m%dT%H%M00Z"};
-  const std::string ShortDateFormat{"%Y%m%d"};
-  const std::string UnsignedPayload{"UNSIGNED-PAYLOAD"};
 };
 
 enum SigV4AKeyDerivationResult {
@@ -57,69 +50,35 @@ using AwsSigningHeaderExclusionVector = std::vector<envoy::type::matcher::v3::St
  * See https://docs.aws.amazon.com/general/latest/gr/signature-version-4.html
  */
 
-class SigV4ASignerImpl : public Signer, public Logger::Loggable<Logger::Id::aws> {
-public:
-  SigV4ASignerImpl(absl::string_view service_name, absl::string_view region,
-                   const CredentialsProviderSharedPtr& credentials_provider,
-                   TimeSource& time_source, const AwsSigningHeaderExclusionVector& matcher_config)
-      : service_name_(service_name), region_(region), credentials_provider_(credentials_provider),
-        time_source_(time_source),
-        long_date_formatter_(SigV4ASignatureConstants::get().LongDateFormat),
-        short_date_formatter_(SigV4ASignatureConstants::get().ShortDateFormat) {
-    for (const auto& matcher : matcher_config) {
-      excluded_header_matchers_.emplace_back(
-          std::make_unique<Matchers::StringMatcherImpl<envoy::type::matcher::v3::StringMatcher>>(
-              matcher));
-    }
-  }
-
-  void sign(Http::RequestMessage& message, bool sign_body = false,
-            const absl::string_view override_region = "") override;
-  void sign(Http::RequestHeaderMap& headers, const std::string& content_hash,
-            const absl::string_view override_region = "") override;
-  void signEmptyPayload(Http::RequestHeaderMap& headers,
-                        const absl::string_view override_region = "") override;
-  void signUnsignedPayload(Http::RequestHeaderMap& headers,
-                           const absl::string_view override_region = "") override;
+class SigV4ASignerImpl : public SignerBase {
+  public:
+      SigV4ASignerImpl(absl::string_view service_name, absl::string_view region,
+                  const CredentialsProviderSharedPtr& credentials_provider, TimeSource& time_source,
+                  const AwsSigningHeaderExclusionVector& matcher_config) : SignerBase(service_name, region,
+                  credentials_provider, time_source,
+                  matcher_config) {}
 
 private:
-  std::string createContentHash(Http::RequestMessage& message, bool sign_body) const;
 
-  std::string createCredentialScope(absl::string_view short_date) const;
+  void addRegionHeader(Http::RequestHeaderMap &headers, const absl::string_view override_region) const override;
 
-  std::string createStringToSign(absl::string_view canonical_request, absl::string_view long_date,
-                                 absl::string_view credential_scope) const;
+  std::string createCredentialScope(const absl::string_view short_date, const absl::string_view override_region) const override;
 
-  std::string createSignature(absl::string_view access_key_id, absl::string_view secret_access_key,
-                              absl::string_view string_to_sign) const;
+  std::string createStringToSign(const absl::string_view canonical_request, const absl::string_view long_date,
+                                 const absl::string_view credential_scope) const override;
 
-  std::string createAuthorizationHeader(absl::string_view access_key_id,
-                                        absl::string_view credential_scope,
+   std::string createSignature(
+                              const absl::string_view access_key_id,
+                              const absl::string_view secret_access_key, 
+                              ABSL_ATTRIBUTE_UNUSED const absl::string_view short_date,
+                              const absl::string_view string_to_sign,
+                              const absl::string_view override_region) const override;
+
+  std::string createAuthorizationHeader(const absl::string_view access_key_id,
+                                        const absl::string_view credential_scope,
                                         const std::map<std::string, std::string>& canonical_headers,
-                                        absl::string_view signature) const;
+                                        const absl::string_view signature) const override;
 
-  std::vector<Matchers::StringMatcherPtr> defaultMatchers() const {
-    std::vector<Matchers::StringMatcherPtr> matcher_ptrs{};
-    for (const auto& header : default_excluded_headers_) {
-      envoy::type::matcher::v3::StringMatcher m;
-      m.set_exact(header);
-      matcher_ptrs.emplace_back(
-          std::make_unique<Matchers::StringMatcherImpl<envoy::type::matcher::v3::StringMatcher>>(
-              m));
-    }
-    return matcher_ptrs;
-  }
-
-  const std::string service_name_;
-  const std::string region_;
-  const std::vector<std::string> default_excluded_headers_ = {
-      Http::Headers::get().ForwardedFor.get(), Http::Headers::get().ForwardedProto.get(),
-      "x-amzn-trace-id"};
-  std::vector<Matchers::StringMatcherPtr> excluded_header_matchers_ = defaultMatchers();
-  CredentialsProviderSharedPtr credentials_provider_;
-  TimeSource& time_source_;
-  DateFormatter long_date_formatter_;
-  DateFormatter short_date_formatter_;
 };
 
 } // namespace Aws
