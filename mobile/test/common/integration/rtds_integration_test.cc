34d9a5220074daa2752206399cf42732b665c84c
Ali Beyad <abeyad@google.com>
2023-12-13 22:32:16 +0000

    format
    Signed-off-by: Ali Beyad <abeyad@google.com>

--- mobile/test/common/integration/rtds_integration_test.cc
+++ mobile/test/common/integration/rtds_integration_test.cc
@@ -57,13 +57,12 @@ public:
     // Send a RTDS request and get back the RTDS response.
     EXPECT_TRUE(compareDiscoveryRequest(Config::TypeUrl::get().Runtime, "", {"some_rtds_resource"},
                                         {"some_rtds_resource"}, {}, true));
-                                         envoy::service::runtime::v3::Runtime some_rtds_resource;
 
     envoy::service::runtime::v3::Runtime some_rtds_resource;
     some_rtds_resource.set_name("some_rtds_resource");
     auto* static_layer = some_rtds_resource.mutable_layer();
-    (*static_layer->mutable_fields())["envoy.reloadable_features.test_feature_false"].set_bool_value(
-        true);
+    (*static_layer->mutable_fields())["envoy.reloadable_features.test_feature_false"]
+        .set_bool_value(true);
 
     sendDiscoveryResponse<envoy::service::runtime::v3::Runtime>(
         Config::TypeUrl::get().Runtime, {some_rtds_resource}, {some_rtds_resource}, {}, "1");
@@ -76,8 +75,8 @@ public:
     load_success_value = getCounterValue(load_success_counter);
     EXPECT_TRUE(compareDiscoveryRequest(Config::TypeUrl::get().Runtime, "", {"some_rtds_resource"},
                                         {"some_rtds_resource"}, {}));
-    (*static_layer->mutable_fields())["envoy.reloadable_features.test_feature_false"].set_bool_value(
-        false);
+    (*static_layer->mutable_fields())["envoy.reloadable_features.test_feature_false"]
+        .set_bool_value(false);
 
     // Send another response with Resource wrapper.
     sendDiscoveryResponse<envoy::service::runtime::v3::Runtime>(
