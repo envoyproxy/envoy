95ba0711acf7e9cc2f4a05b1fde3444b897960fb
wbpcode <wbphub@live.com>
2023-12-15 11:16:22 +0000

    initial commit
    Signed-off-by: wbpcode <wbphub@live.com>

---
+++ contrib/generic_proxy/filters/network/test/codecs/kafka/config_test.cc
@@ -0,0 +1,31 @@
+#include <cstdint>
+#include <memory>
+
+#include "test/mocks/server/factory_context.h"
+
+#include "contrib/generic_proxy/filters/network/source/codecs/kafka/config.h"
+#include "contrib/generic_proxy/filters/network/test/mocks/codec.h"
+#include "gtest/gtest.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace NetworkFilters {
+namespace GenericProxy {
+namespace Codec {
+namespace Kafka {
+namespace {
+
+TEST(KafkaCodecTest, SimpleFrameTest) {
+    {
+        auto request = std::make_shared<NetworkFilters::Kafka::
+    }
+}
+
+
+}
+} // namespace Dubbo
+} // namespace Codec
+} // namespace GenericProxy
+} // namespace NetworkFilters
+} // namespace Extensions
+} // namespace Envoy
