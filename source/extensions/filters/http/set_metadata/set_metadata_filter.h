483ecb2d4055b42a806f0a1003243ba62c86cf3a
Jacob Bohanon <jacob.bohanon@solo.io>
2023-12-19 18:29:59 -0500

    set_metadata: allow setting typed metadata (#31244)
    Signed-off-by: Jacob Bohanon <jacob.bohanon@solo.io>

--- source/extensions/filters/http/set_metadata/set_metadata_filter.h
+++ source/extensions/filters/http/set_metadata/set_metadata_filter.h
@@ -5,28 +5,51 @@
 #include <vector>
 
 #include "envoy/extensions/filters/http/set_metadata/v3/set_metadata.pb.h"
+#include "envoy/stats/stats_macros.h"
 
 #include "source/common/common/logger.h"
 #include "source/extensions/filters/http/common/pass_through_filter.h"
 
 #include "absl/strings/string_view.h"
+#include "absl/types/variant.h"
 
 namespace Envoy {
 namespace Extensions {
 namespace HttpFilters {
 namespace SetMetadataFilter {
 
+#define ALL_SET_METADATA_FILTER_STATS(COUNTER) COUNTER(overwrite_denied)
+
+struct FilterStats {
+  ALL_SET_METADATA_FILTER_STATS(GENERATE_COUNTER_STRUCT)
+};
+
+struct UntypedMetadataEntry {
+  bool allow_overwrite{};
+  std::string metadata_namespace;
+  ProtobufWkt::Struct value;
+};
+struct TypedMetadataEntry {
+  bool allow_overwrite{};
+  std::string metadata_namespace;
+  ProtobufWkt::Any value;
+};
 class Config : public ::Envoy::Router::RouteSpecificFilterConfig,
                public Logger::Loggable<Logger::Id::config> {
 public:
-  Config(const envoy::extensions::filters::http::set_metadata::v3::Config& config);
+  Config(const envoy::extensions::filters::http::set_metadata::v3::Config& config,
+         Stats::Scope& scope, const std::string& stats_prefix);
 
-  absl::string_view metadataNamespace() const { return namespace_; }
-  const ProtobufWkt::Struct& value() { return value_; }
+  const std::vector<UntypedMetadataEntry>& untyped() { return untyped_; }
+  const std::vector<TypedMetadataEntry>& typed() { return typed_; }
+  const FilterStats& stats() const { return stats_; }
 
 private:
-  std::string namespace_;
-  ProtobufWkt::Struct value_;
+  static FilterStats generateStats(const std::string& prefix, Stats::Scope& scope);
+
+  std::vector<UntypedMetadataEntry> untyped_;
+  std::vector<TypedMetadataEntry> typed_;
+  FilterStats stats_;
 };
 
 using ConfigSharedPtr = std::shared_ptr<Config>;
