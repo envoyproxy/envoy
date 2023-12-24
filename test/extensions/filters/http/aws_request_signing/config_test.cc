040264be939f40ac23d8b4998c19ae10baa66cf6
Nigel Brittain <nbaws@amazon.com>
2023-12-24 09:49:34 +0000

    add additional test
    Signed-off-by: Nigel Brittain <nbaws@amazon.com>

--- test/extensions/filters/http/aws_request_signing/config_test.cc
+++ test/extensions/filters/http/aws_request_signing/config_test.cc
@@ -52,6 +52,46 @@ match_excluded_headers:
   cb(filter_callbacks);
 }
 
+TEST(AwsRequestSigningFilterConfigTest, SimpleConfigExplicitSigningAlgorithm) {
+  const std::string yaml = R"EOF(
+service_name: s3
+signing_algorithm: aws_sigv4
+region: us-west-2
+host_rewrite: new-host
+match_excluded_headers:
+  - prefix: x-envoy
+  - exact: foo
+  - exact: bar
+  )EOF";
+
+  AwsRequestSigningProtoConfig proto_config;
+  TestUtility::loadFromYamlAndValidate(yaml, proto_config);
+
+  AwsRequestSigningProtoConfig expected_config;
+  expected_config.set_service_name("s3");
+  expected_config.set_region("us-west-2");
+  expected_config.set_host_rewrite("new-host");
+  expected_config.set_signing_algorithm(envoy::extensions::filters::http::aws_request_signing::v3::
+                                            AwsRequestSigning_SigningAlgorithm_AWS_SIGV4);
+  expected_config.add_match_excluded_headers()->set_prefix("x-envoy");
+  expected_config.add_match_excluded_headers()->set_exact("foo");
+  expected_config.add_match_excluded_headers()->set_exact("bar");
+
+  Protobuf::util::MessageDifferencer differencer;
+  differencer.set_message_field_comparison(Protobuf::util::MessageDifferencer::EQUAL);
+  differencer.set_repeated_field_comparison(Protobuf::util::MessageDifferencer::AS_SET);
+  EXPECT_TRUE(differencer.Compare(expected_config, proto_config));
+
+  testing::NiceMock<Server::Configuration::MockFactoryContext> context;
+  AwsRequestSigningFilterFactory factory;
+
+  Http::FilterFactoryCb cb =
+      factory.createFilterFactoryFromProto(proto_config, "stats", context).value();
+  Http::MockFilterChainFactoryCallbacks filter_callbacks;
+  EXPECT_CALL(filter_callbacks, addStreamDecoderFilter(_));
+  cb(filter_callbacks);
+}
+
 TEST(AwsRequestSigningFilterConfigTest, SimpleConfigSigV4A) {
   const std::string yaml = R"EOF(
 service_name: s3
@@ -148,10 +188,11 @@ match_excluded_headers:
       EnvoyException);
 }
 
-TEST(AwsRequestSigningFilterConfigTest, RouteSpecificFilterConfig) {
+TEST(AwsRequestSigningFilterConfigTest, RouteSpecificFilterConfigSigV4) {
   const std::string yaml = R"EOF(
 aws_request_signing:
   service_name: s3
+  signing_algorithm: aws_sigv4
   region: us-west-2
   host_rewrite: new-host
   match_excluded_headers:
@@ -172,6 +213,30 @@ stat_prefix: foo_prefix
   ASSERT_NE(route_config, nullptr);
 }
 
+TEST(AwsRequestSigningFilterConfigTest, RouteSpecificFilterConfigSigV4A) {
+  const std::string yaml = R"EOF(
+aws_request_signing:
+  service_name: s3
+  signing_algorithm: aws_sigv4a
+  region_set: '*'
+  host_rewrite: new-host
+  match_excluded_headers:
+    - prefix: x-envoy
+    - exact: foo
+    - exact: bar
+stat_prefix: foo_prefix
+  )EOF";
+
+  AwsRequestSigningProtoPerRouteConfig proto_config;
+  TestUtility::loadFromYamlAndValidate(yaml, proto_config);
+
+  testing::NiceMock<Server::Configuration::MockServerFactoryContext> context;
+  AwsRequestSigningFilterFactory factory;
+
+  const auto route_config = factory.createRouteSpecificFilterConfig(
+      proto_config, context, ProtobufMessage::getNullValidationVisitor());
+  ASSERT_NE(route_config, nullptr);
+}
 } // namespace AwsRequestSigningFilter
 } // namespace HttpFilters
 } // namespace Extensions
