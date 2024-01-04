ed0fa84280f8c9b423622d7823c4c94a6f48b0d0
Kuat Yessenov <kuat@google.com>
2024-01-04 05:57:46 +0000

    patches
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- test/config_test/config_test.cc
+++ test/config_test/config_test.cc
@@ -46,8 +46,11 @@ OptionsImpl asConfigYaml(const OptionsImpl& src, Api::Api& api) {
 }
 
 static std::vector<absl::string_view> unsuported_win32_configs = {
+#if defined(WIN32)
+    "rbac_envoy.yaml",
+#endif
 #if defined(WIN32) && !defined(SO_ORIGINAL_DST)
-    "configs_original-dst-cluster_proxy_config.yaml", "rbac_envoy.yaml"
+    "configs_original-dst-cluster_proxy_config.yaml"
 #endif
 };
 
