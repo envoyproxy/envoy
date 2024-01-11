fcc8b0c7b1262721ff8095ce0d1c99965cb6fde1
Fredy Wijaya <fredyw@google.com>
2024-01-11 18:09:39 +0000

    Install Android SDK 30
    Signed-off-by: Fredy Wijaya <fredyw@google.com>

--- mobile/ci/mac_ci_setup.sh
+++ mobile/ci/mac_ci_setup.sh
@@ -65,6 +65,7 @@ if [[ "${1:-}" == "--android" ]]; then
   "${SDKMANAGER}" --install "ndk;21.4.7075529"
   # Download and set up build-tools 30.0.3, 31.0.0 is missing dx.jar.
   "${SDKMANAGER}" --install "build-tools;30.0.3"
+  "${SDKMANAGER}" --install "platforms;android-30"
   ANDROID_NDK_HOME="${ANDROID_HOME}/ndk/21.4.7075529"
   export ANDROID_NDK_HOME
 fi
