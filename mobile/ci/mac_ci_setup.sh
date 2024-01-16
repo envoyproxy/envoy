8360849cace5f75f76d83f6de4c19a779b627055
Fredy Wijaya <fredyw@google.com>
2024-01-16 19:09:21 +0000

    mobile/ci: Install Android build-tools:30.0.2
    Signed-off-by: Fredy Wijaya <fredyw@google.com>

--- mobile/ci/mac_ci_setup.sh
+++ mobile/ci/mac_ci_setup.sh
@@ -64,8 +64,7 @@ if [[ "${1:-}" == "--android" ]]; then
   "${SDKMANAGER}" --install "platforms;android-30"
   "${SDKMANAGER}" --uninstall "ndk-bundle"
   "${SDKMANAGER}" --install "ndk;21.4.7075529"
-  # Download and set up build-tools 30.0.3, 31.0.0 is missing dx.jar.
-  "${SDKMANAGER}" --install "build-tools;30.0.3"
+  "${SDKMANAGER}" --install "build-tools;30.0.2"
   ANDROID_NDK_HOME="${ANDROID_HOME}/ndk/21.4.7075529"
   export ANDROID_NDK_HOME
 fi
