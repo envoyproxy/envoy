22300de47cb781b3b070c02a103a398d3466ecc6
Bin Wu <wub@google.com>
2024-01-23 20:35:54 +0000

    Add comments.
    Signed-off-by: Bin Wu <wub@google.com>

--- envoy/common/execution_context.h
+++ envoy/common/execution_context.h
@@ -7,6 +7,11 @@
 namespace Envoy {
 
 class ScopedExecutionContext;
+
+// ExecutionContext can be inherited by subclasses to represent arbitrary information associated
+// with the execution of a piece of code. activate/deactivate are called when the said execution
+// starts/ends. For an example usage, please see
+// https://github.com/envoyproxy/envoy/pull/31937#issuecomment-1905143264.
 class ExecutionContext {
 public:
   ExecutionContext() = default;
@@ -19,12 +24,37 @@ public:
   ExecutionContext(ExecutionContext&&) = delete;
 
 protected:
+  // Called when the current thread starts to run code on behalf of the owner of this object.
+  // protected because it should only be called by ScopedExecutionContext.
   virtual void activate() PURE;
+  // Called when the current thread stops running code on behalf of the owner of this object.
+  // protected because it should only be called by ScopedExecutionContext.
   virtual void deactivate() PURE;
 
   friend class ScopedExecutionContext;
 };
 
+// ScopedExecutionContext is a stack-only RAII object to call ExecutionContext::activate on
+// construction and ExecutionContext::deactivate on destruction.
+//
+// ScopedExecutionContext is intened to be used in a simple c++ scope:
+//   {
+//     ExecutionContext context;
+//     // context.activate() called here. context.deactivate() called when scoped_execution_context
+//     destructs. ScopedExecutionContext scoped_execution_context(&context);
+//   }
+//
+// Or, you can 'extend' its scope from a function to its caller via return-by-value:
+//   ScopedExecutionContext InnerFunction() {
+//     // context_.activate() called here.
+//     ScopedExecutionContext scoped_execution_context(&context_);
+//     return scoped_execution_context;  // Does not call context_.deactivate().
+//   }
+//
+//   void OuterFunction() {
+//     ScopedExecutionContext scoped_execution_context = InnerFunction();
+//     // context_.deactivate() called when scoped_execution_context destructs.
+//   }
 class ScopedExecutionContext {
 public:
   ScopedExecutionContext() : ScopedExecutionContext(nullptr) {}
--- source/common/network/common_connection_filter_states.h
+++ source/common/network/common_connection_filter_states.h
@@ -9,6 +9,10 @@ namespace Envoy {
 static constexpr absl::string_view kConnectionExecutionContextFilterStateName =
     "connection-execution-context";
 
+// ConnectionExecutionContextFilterState is an optional connection-level filter state goes by the
+// name kConnectionExecutionContextFilterStateName. It owns a ExecutionContext, whose
+// activate/deactivate methods will be called when a thread starts/finishes running code on behalf
+// of the corresponding connection.
 class ConnectionExecutionContextFilterState : public Envoy::StreamInfo::FilterState::Object {
 public:
   explicit ConnectionExecutionContextFilterState(
@@ -21,9 +25,11 @@ private:
   std::unique_ptr<ExecutionContext> execution_context_;
 };
 
+// Returns the ExecutionContext of a connection, if any. Or nullptr if not found.
 const ExecutionContext*
 GetConnectionExecutionContextReadOnly(const Network::Connection& connection);
 
+// Returns the mutable ExecutionContext of a connection. Or nullptr if not found.
 ExecutionContext* GetConnectionExecutionContextMutable(const Network::Connection& connection);
 
 } // namespace Envoy
