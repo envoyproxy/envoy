e6ff9a9a4330686a851559a231df9afc754bbdef
jst <jst@meitu.com>
2024-01-23 18:32:30 +0800

    fix: fix member reference error
    Signed-off-by: jst <jst@meitu.com>

--- source/extensions/filters/http/compressor/compressor_filter.cc
+++ source/extensions/filters/http/compressor/compressor_filter.cc
@@ -286,7 +286,7 @@ Http::FilterHeadersStatus CompressorFilter::encodeHeaders(Http::ResponseHeaderMa
       isEnabledAndContentLengthBigEnough && !Http::Utility::isUpgrade(headers) &&
       config.isContentTypeAllowed(headers) && !hasCacheControlNoTransform(headers) &&
       isEtagAllowed(headers) && !headers.getInline(response_content_encoding_handle.handle()) &&
-      headers->get(Http::Headers::get().ContentRange).empty();
+      headers.get(Http::Headers::get().ContentRange).empty();
   if (!end_stream && isAcceptEncodingAllowed(isEnabledAndContentLengthBigEnough, headers) &&
       isCompressible && isTransferEncodingAllowed(headers)) {
     sanitizeEtagHeader(headers);
