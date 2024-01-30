8f95f9ec501febe91e3f7688a3f85e33a2052d7a
Jacob Bohanon <jacob.bohanon@solo.io>
2024-01-30 13:18:57 -0500

    ext_proc: send and receive dynamic metadata (#30747)
    Introduce the ability to send dynamic metadata in the External Processing Request. Also implements the API for returning dynamic metadata as part of the External Processing Response.
    
    ---------
    
    Signed-off-by: Jacob Bohanon <jacob.bohanon@solo.io>

--- test/extensions/filters/http/ext_proc/config_test.cc
+++ test/extensions/filters/http/ext_proc/config_test.cc
@@ -36,6 +36,15 @@ TEST(HttpExtProcConfigTest, CorrectConfig) {
     response_trailer_mode: send
   filter_metadata:
     hello: "world"
+  metadata_options:
+    forwarding_namespaces:
+      typed:
+      - ns1
+      untyped:
+      - ns2
+    receiving_namespaces:
+      untyped:
+      - ns2
   )EOF";
 
   ExternalProcessingFilterConfig factory;
@@ -73,6 +82,15 @@ TEST(HttpExtProcConfigTest, CorrectConfigServerContext) {
     response_trailer_mode: send
   filter_metadata:
     hello: "world"
+  metadata_options:
+    forwarding_namespaces:
+      typed:
+      - ns1
+      untyped:
+      - ns2
+    receiving_namespaces:
+      untyped:
+      - ns2
   )EOF";
 
   ExternalProcessingFilterConfig factory;
