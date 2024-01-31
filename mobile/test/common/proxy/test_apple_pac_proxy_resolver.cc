6ad9c9ceb87112c93dc811ac786577f52a662cea
Ali Beyad <abeyad@google.com>
2024-01-30 20:43:55 -0500

    fix pac url test
    Signed-off-by: Ali Beyad <abeyad@google.com>

--- mobile/test/common/proxy/test_apple_pac_proxy_resolver.cc
+++ mobile/test/common/proxy/test_apple_pac_proxy_resolver.cc
@@ -79,7 +79,11 @@ CFRunLoopSourceRef TestApplePacProxyResolver::createPacUrlResolverSource(
                              /*cancel=*/nullptr,
                              /*perform=*/runLoopCallback});
 
-  return CFRunLoopSourceCreate(kCFAllocatorDefault, /*order=*/0, run_loop_context_.get());
+  auto run_loop_source = CFRunLoopSourceCreate(kCFAllocatorDefault, /*order=*/0, run_loop_context_.get());
+  // There are no network events that get triggered to notify this test run loop source that
+  // it should process a result, so we have to signal it manually.
+  CFRunLoopSourceSignal(run_loop_source);
+  return run_loop_source;
 }
 
 } // namespace test
