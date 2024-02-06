0850fdc92da77fd4958748caa2b77123306e87b2
Yan Avlasov <yavlasov@google.com>
2024-02-06 17:01:53 +0000

    Remove fallback from accept4 to accept
    Signed-off-by: Yan Avlasov <yavlasov@google.com>

--- source/common/api/posix/os_sys_calls_impl.cc
+++ source/common/api/posix/os_sys_calls_impl.cc
@@ -325,18 +325,15 @@ SysCallSocketResult OsSysCallsImpl::accept(os_fd_t sockfd, sockaddr* addr, sockl
 
 #if defined(__linux__)
   rc = ::accept4(sockfd, addr, addrlen, SOCK_NONBLOCK);
-  // If failed with EINVAL try without flags
-  if (rc >= 0 || errno != EINVAL) {
-    return {rc, rc != -1 ? 0 : errno};
-  }
-#endif
-
+  return {rc, rc != -1 ? 0 : errno};
+#else
   rc = ::accept(sockfd, addr, addrlen);
   if (rc >= 0) {
     setsocketblocking(rc, false);
   }
 
   return {rc, rc != -1 ? 0 : errno};
+#endif
 }
 
 SysCallBoolResult OsSysCallsImpl::socketTcpInfo([[maybe_unused]] os_fd_t sockfd,
