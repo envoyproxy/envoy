1995d9291835e3292895a34bf009c683f578e75a
Jared Kirschner <85913323+jkirschner-hashicorp@users.noreply.github.com>
2024-02-09 01:00:05 -0500

    Add FULL_SCAN selection mode to least request LB (#31507)
    Add FULL_SCAN mode to least request load balancer.
    
    By default, the least request load balancer returns the host with the fewest
    active requests from a set of N randomly selected hosts.
    
    This introduces a new "full scan" selection method that returns the host with
    the fewest number of active requests from all hosts. If multiple hosts are
    tied for "least", one of the tied hosts is randomly chosen.
    
    Added selection_method option to the least request load balancer. If set to 
    FULL_SCAN, Envoy will select the host with the fewest active requests from
    the entire host set rather than choice_count random choices.
    
    Risk Level: low, existing code path unchanged
    Testing: unit tests add
    Docs Changes: protobuf docs
    Release Notes: added
    Signed-off-by: Jared Kirschner <jkirschner@hashicorp.com>
    Signed-off-by: Leonardo da Mata <ldamata@spotify.com>
    Co-authored-by: Leonardo da Mata <barroca@gmail.com>

--- source/common/upstream/load_balancer_impl.cc
+++ source/common/upstream/load_balancer_impl.cc
@@ -1298,12 +1298,72 @@ HostConstSharedPtr LeastRequestLoadBalancer::unweightedHostPick(const HostVector
                                                                 const HostsSource&) {
   HostSharedPtr candidate_host = nullptr;
 
+  switch (selection_method_) {
+  case envoy::extensions::load_balancing_policies::least_request::v3::LeastRequest::FULL_SCAN:
+    candidate_host = unweightedHostPickFullScan(hosts_to_use);
+    break;
+  case envoy::extensions::load_balancing_policies::least_request::v3::LeastRequest::N_CHOICES:
+    candidate_host = unweightedHostPickNChoices(hosts_to_use);
+    break;
+  default:
+    IS_ENVOY_BUG("unknown selection method specified for least request load balancer");
+  }
+
+  return candidate_host;
+}
+
+HostSharedPtr LeastRequestLoadBalancer::unweightedHostPickFullScan(const HostVector& hosts_to_use) {
+  HostSharedPtr candidate_host = nullptr;
+
+  size_t num_hosts_known_tied_for_least = 0;
+
+  const size_t num_hosts = hosts_to_use.size();
+
+  for (size_t i = 0; i < num_hosts; ++i) {
+    const HostSharedPtr& sampled_host = hosts_to_use[i];
+
+    if (candidate_host == nullptr) {
+      // Make a first choice to start the comparisons.
+      num_hosts_known_tied_for_least = 1;
+      candidate_host = sampled_host;
+      continue;
+    }
+
+    const auto candidate_active_rq = candidate_host->stats().rq_active_.value();
+    const auto sampled_active_rq = sampled_host->stats().rq_active_.value();
+
+    if (sampled_active_rq < candidate_active_rq) {
+      // Reset the count of known tied hosts.
+      num_hosts_known_tied_for_least = 1;
+      candidate_host = sampled_host;
+    } else if (sampled_active_rq == candidate_active_rq) {
+      ++num_hosts_known_tied_for_least;
+
+      // Use reservoir sampling to select 1 unique sample from the total number of hosts N
+      // that will tie for least requests after processing the full hosts array.
+      //
+      // Upon each new tie encountered, replace candidate_host with sampled_host
+      // with probability (1 / num_hosts_known_tied_for_least percent).
+      // The end result is that each tied host has an equal 1 / N chance of being the
+      // candidate_host returned by this function.
+      const size_t random_tied_host_index = random_.random() % num_hosts_known_tied_for_least;
+      if (random_tied_host_index == 0) {
+        candidate_host = sampled_host;
+      }
+    }
+  }
+
+  return candidate_host;
+}
+
+HostSharedPtr LeastRequestLoadBalancer::unweightedHostPickNChoices(const HostVector& hosts_to_use) {
+  HostSharedPtr candidate_host = nullptr;
+
   for (uint32_t choice_idx = 0; choice_idx < choice_count_; ++choice_idx) {
     const int rand_idx = random_.random() % hosts_to_use.size();
     const HostSharedPtr& sampled_host = hosts_to_use[rand_idx];
 
     if (candidate_host == nullptr) {
-
       // Make a first choice to start the comparisons.
       candidate_host = sampled_host;
       continue;
@@ -1311,6 +1371,7 @@ HostConstSharedPtr LeastRequestLoadBalancer::unweightedHostPick(const HostVector
 
     const auto candidate_active_rq = candidate_host->stats().rq_active_.value();
     const auto sampled_active_rq = sampled_host->stats().rq_active_.value();
+
     if (sampled_active_rq < candidate_active_rq) {
       candidate_host = sampled_host;
     }
