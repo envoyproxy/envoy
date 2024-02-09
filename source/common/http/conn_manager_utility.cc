ad06416d1d7f976bed2c30d007e30d4cd929c9d3
Nathanael DEMACON <ndemacon@scaleway.com>
2024-02-09 22:10:58 +0100

    explicit typing
    Signed-off-by: Nathanael DEMACON <ndemacon@scaleway.com>

--- source/common/http/conn_manager_utility.cc
+++ source/common/http/conn_manager_utility.cc
@@ -303,7 +303,7 @@ void ConnectionManagerUtility::sanitizeTEHeader(RequestHeaderMap& request_header
 
   // If the TE header contains the "trailers" value, set the TE header to "trailers" only.
   std::vector<std::string> te_values = absl::StrSplit(te_header, ',');
-  for (const auto& te_value : te_values) {
+  for (const std::string& te_value : te_values) {
     bool has_trailers_te = absl::StripAsciiWhitespace(te_value) == Http::Headers::get().TEValues.Trailers;
 
     if (has_trailers_te) {
