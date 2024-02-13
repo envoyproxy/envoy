db7a52d2407094c79d907c51da363aacc6eb7904
Xin Zhuang <stevenzzz@google.com>
2024-02-13 16:54:08 +0000

    add failed to save changes on integration test
    Signed-off-by: Xin Zhuang <stevenzzz@google.com>

--- test/integration/multiplexed_integration_test.cc
+++ test/integration/multiplexed_integration_test.cc
@@ -27,6 +27,7 @@
 #include "test/test_common/network_utility.h"
 #include "test/test_common/printers.h"
 #include "test/test_common/simulated_time_system.h"
+#include "test/test_common/status_utility.h"
 #include "test/test_common/utility.h"
 
 #include "gtest/gtest.h"
