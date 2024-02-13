99760e7a462b0d6612b8d2d1d7d54f426a38464d
Alyssa Wilk <alyssar@chromium.org>
2024-02-13 12:49:51 -0500

    spelling
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/mocks/grpc/mocks.cc
+++ test/mocks/grpc/mocks.cc
@@ -39,7 +39,6 @@ MockAsyncClientFactory::MockAsyncClientFactory() {
 MockAsyncClientFactory::~MockAsyncClientFactory() = default;
 
 MockAsyncClientManager::MockAsyncClientManager() {
-  // Preferve behavior prior to statusor refactor.
   ON_CALL(*this, getOrCreateRawAsyncClient(_, _, _)).WillByDefault(Return(nullptr));
   ON_CALL(*this, factoryForGrpcService(_, _, _))
       .WillByDefault(Invoke([](const envoy::config::core::v3::GrpcService&, Stats::Scope&, bool) {
