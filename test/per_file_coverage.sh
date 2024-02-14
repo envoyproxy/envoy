4c095a199ad3a3d8c5b06e99a68aa98a9224a2ee
Greg Greenway <ggreenway@apple.com>
2024-02-14 08:51:28 -0800

    slightly lower coverage for quic
    Some paths can't be hit anymore because they're caught earlier when
    constructing the ContextImpl, but they should still remain in the code
    for sanity
    
    Signed-off-by: Greg Greenway <ggreenway@apple.com>

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -16,7 +16,7 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/common/matcher:94.6"
 "source/common/network:94.4" # Flaky, `activateFileEvents`, `startSecureTransport` and `ioctl`, listener_socket do not always report LCOV
 "source/common/network/dns_resolver:91.4"  # A few lines of MacOS code not tested in linux scripts. Tested in MacOS scripts
-"source/common/quic:93.4"
+"source/common/quic:93.3"
 "source/common/secret:95.1"
 "source/common/signal:87.2" # Death tests don't report LCOV
 "source/common/thread:0.0" # Death tests don't report LCOV
