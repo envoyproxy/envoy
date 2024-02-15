62fedb6e0e8976d03ce1cfc27e209e00bcfd783b
Kirtimaan <krajshiva@google.com>
2024-02-15 22:09:30 +0000

    Handling undefined 1xx responses from upstream
    Signed-off-by: Kirtimaan <krajshiva@google.com>

--- source/common/http/header_utility.cc
+++ source/common/http/header_utility.cc
@@ -234,8 +234,9 @@ bool HeaderUtility::authorityIsValid(const absl::string_view header_value) {
 }
 
 bool HeaderUtility::isSpecial1xx(const ResponseHeaderMap& response_headers) {
-  return response_headers.Status()->value() == "100" ||
-         response_headers.Status()->value() == "102" || response_headers.Status()->value() == "103";
+    return (Utility::getResponseStatus(response_headers) >= 100 &&
+          Utility::getResponseStatus(response_headers) < 200 &&
+          Utility::getResponseStatus(response_headers) != 101);
 }
 
 bool HeaderUtility::isConnect(const RequestHeaderMap& headers) {
