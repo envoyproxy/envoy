ab2bf64324e825a3f915fd13d71c3a6cb29130bc
Adi (Suissa) Peleg <adip@google.com>
2024-02-16 08:57:49 -0500

    xDS rate-limit: rejecting inf and nan fill rate values (#32203)
    Rejecting a config with invalid Inf or NaN values for the fill-rate of the xDS streams.
    
    Risk Level: low
    Testing: Added unit tests and fuzz corpus
    Docs Changes: N/A
    Release Notes: Added
    Platform Specific Features: N/A
    Fixes fuzz bug 66327
    
    Signed-off-by: Adi Suissa-Peleg <adip@google.com>

--- source/extensions/config_subscription/grpc/grpc_subscription_factory.cc
+++ source/extensions/config_subscription/grpc/grpc_subscription_factory.cc
@@ -29,12 +29,15 @@ GrpcConfigSubscriptionFactory::create(ConfigSubscriptionFactory::SubscriptionDat
   auto factory_or_error = Utility::factoryForGrpcApiConfigSource(
       data.cm_.grpcAsyncClientManager(), api_config_source, data.scope_, true);
   THROW_IF_STATUS_NOT_OK(factory_or_error, throw);
+  absl::StatusOr<RateLimitSettings> rate_limit_settings_or_error =
+      Utility::parseRateLimitSettings(api_config_source);
+  THROW_IF_STATUS_NOT_OK(rate_limit_settings_or_error, throw);
   GrpcMuxContext grpc_mux_context{
       /*async_client_=*/factory_or_error.value()->createUncachedRawAsyncClient(),
       /*dispatcher_=*/data.dispatcher_,
       /*service_method_=*/sotwGrpcMethod(data.type_url_),
       /*local_info_=*/data.local_info_,
-      /*rate_limit_settings_=*/Utility::parseRateLimitSettings(api_config_source),
+      /*rate_limit_settings_=*/rate_limit_settings_or_error.value(),
       /*scope_=*/data.scope_,
       /*config_validators_=*/std::move(custom_config_validators),
       /*xds_resources_delegate_=*/data.xds_resources_delegate_,
@@ -74,12 +77,15 @@ DeltaGrpcConfigSubscriptionFactory::create(ConfigSubscriptionFactory::Subscripti
   auto factory_or_error = Utility::factoryForGrpcApiConfigSource(
       data.cm_.grpcAsyncClientManager(), api_config_source, data.scope_, true);
   THROW_IF_STATUS_NOT_OK(factory_or_error, throw);
+  absl::StatusOr<RateLimitSettings> rate_limit_settings_or_error =
+      Utility::parseRateLimitSettings(api_config_source);
+  THROW_IF_STATUS_NOT_OK(rate_limit_settings_or_error, throw);
   GrpcMuxContext grpc_mux_context{
       /*async_client_=*/factory_or_error.value()->createUncachedRawAsyncClient(),
       /*dispatcher_=*/data.dispatcher_,
       /*service_method_=*/deltaGrpcMethod(data.type_url_),
       /*local_info_=*/data.local_info_,
-      /*rate_limit_settings_=*/Utility::parseRateLimitSettings(api_config_source),
+      /*rate_limit_settings_=*/rate_limit_settings_or_error.value(),
       /*scope_=*/data.scope_,
       /*config_validators_=*/std::move(custom_config_validators),
       /*xds_resources_delegate_=*/{},
