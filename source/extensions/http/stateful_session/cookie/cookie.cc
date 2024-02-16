60b6a956ec202aa1083dec986719fb4c20752b58
Kuat <kyessenov@users.noreply.github.com>
2024-02-16 05:27:15 -0800

    cookie session: support 0 ttl (#32093)
    * cookie session: support 0 ttl
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * add note
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * update changelog
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * review
    
    Change-Id: I24b84f52cbe3617e5e298d89b4cafd3c76a62ec8
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * clarify doc
    
    Change-Id: Ib88c98412a6fe696365a01e3f69a9198bf1b90e7
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    * review
    
    Change-Id: I81e461948b75eaee9ef909d8d13bf8fea7c66dc9
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    ---------
    
    Signed-off-by: Yuanlin Xu <yuanlin.xu@redhat.com>
    Co-authored-by: Kuat Yessenov <kuat@google.com>

--- source/extensions/http/stateful_session/cookie/cookie.cc
+++ source/extensions/http/stateful_session/cookie/cookie.cc
@@ -16,12 +16,15 @@ void CookieBasedSessionStateFactory::SessionStateImpl::onUpdate(
   if (!upstream_address_.has_value() || host_address != upstream_address_.value()) {
     if (Runtime::runtimeFeatureEnabled(
             "envoy.reloadable_features.stateful_session_encode_ttl_in_cookie")) {
-      auto expiry_time = std::chrono::duration_cast<std::chrono::seconds>(
-          (time_source_.monotonicTime() + std::chrono::seconds(factory_.ttl_)).time_since_epoch());
       // Build proto message
       envoy::Cookie cookie;
       cookie.set_address(std::string(host_address));
-      cookie.set_expires(expiry_time.count());
+      if (factory_.ttl_ != std::chrono::seconds::zero()) {
+        const auto expiry_time = std::chrono::duration_cast<std::chrono::seconds>(
+            (time_source_.monotonicTime() + std::chrono::seconds(factory_.ttl_))
+                .time_since_epoch());
+        cookie.set_expires(expiry_time.count());
+      }
       std::string proto_string;
       cookie.SerializeToString(&proto_string);
 
