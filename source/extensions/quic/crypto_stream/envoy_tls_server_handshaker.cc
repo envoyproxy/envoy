1551e5ddce69294c672d85da9c84125feb7b1b72
He Jie Xu <hejie.xu@intel.com>
2024-02-25 03:19:01 +0000

    add more doc and comment
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- source/extensions/quic/crypto_stream/envoy_tls_server_handshaker.cc
+++ source/extensions/quic/crypto_stream/envoy_tls_server_handshaker.cc
@@ -23,6 +23,9 @@ ssl_private_key_result_t EnvoyTlsServerHandshaker::PrivateKeySign(uint8_t* out, 
                                                                   size_t max_out, uint16_t sig_alg,
                                                                   absl::string_view in) {
   if (transport_socket_factory_->getTlsCertificates().size() > 0) {
+    // TODO(soulxu): Currently the QUIC transport socket only support one certficate. After
+    // QUIC transport socket with multiple certificates, we will figure out how to support
+    // multiple certificates for private key provider also.
     auto private_key_method =
         transport_socket_factory_->getTlsCertificates()[0].get().privateKeyMethod();
     if (private_key_method != nullptr) {
