af32112d116500b9d5c036917533d923ba7f2b1b
Keith Smiley <keithbsmiley@gmail.com>
2024-02-29 11:46:31 -0800

    Discard changes to mobile/library/jni/android_jni_utility.cc

--- mobile/library/jni/android_jni_utility.cc
+++ mobile/library/jni/android_jni_utility.cc
@@ -2,7 +2,7 @@
 
 #include "source/common/common/assert.h"
 
-#if defined(__ANDROID__)
+#if defined(__ANDROID_API__)
 #include "library/common/data/utility.h"
 #include "library/jni/import/jni_import.h"
 #include "library/jni/jni_support.h"
@@ -13,7 +13,7 @@ namespace Envoy {
 namespace JNI {
 
 bool isCleartextPermitted(absl::string_view hostname) {
-#if defined(__ANDROID__)
+#if defined(__ANDROID_API__)
   envoy_data host = Envoy::Data::Utility::copyToBridgeData(hostname);
   JniHelper jni_helper(getEnv());
   LocalRefUniquePtr<jstring> java_host = envoyDataToJavaString(jni_helper, host);
@@ -32,7 +32,7 @@ bool isCleartextPermitted(absl::string_view hostname) {
 }
 
 void tagSocket(int ifd, int uid, int tag) {
-#if defined(__ANDROID__)
+#if defined(__ANDROID_API__)
   JniHelper jni_helper(getEnv());
   LocalRefUniquePtr<jclass> jcls_AndroidNetworkLibrary =
       findClass("io.envoyproxy.envoymobile.utilities.AndroidNetworkLibrary");
