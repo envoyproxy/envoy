f799b84f25e42561d8c45befd510478f4811156b
Joshua Marantz <jmarantz@google.com>
2024-02-29 11:36:49 -0500

    grammar
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/exe/admin_response.h
+++ source/exe/admin_response.h
@@ -32,7 +32,7 @@ class AdminResponse;
 // Requests can also be cancelled explicitly by calling cancel(). After
 // cancel() is called, no further callbacks will be called by the response.
 //
-// The lifecycle of an AdminResponse is rendered as an finite state machine
+// The lifecycle of an AdminResponse is rendered as a finite state machine
 // bubble diagram:
 // https://docs.google.com/drawings/d/1njUl1twApEMoxmjaG4b7optTh5fcb_YNcfSnkHbdfq0/view
 class AdminResponse : public std::enable_shared_from_this<AdminResponse> {
