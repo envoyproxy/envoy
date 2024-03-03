cdaaa885e60ce9412ef5b9569742e4b266137045
Ali Beyad <abeyad@google.com>
2024-03-03 17:00:02 +0000

    mobile: Reenable the FilterIntegrationTest.AltSvcCachedH2Slow test
    After some upstream changes, the test no longer seems to flake.
    
    Tested with:
    bazelisk test --runs_per_test=500 --test_output=streamed
     --cache_test_results=no
     --test_arg="--gtest_filter=*FilterIntegrationTest*AltSvcCachedH2Slow*"
     test/extensions/filters/http/alternate_protocols_cache:filter_integration_test
    
    Fixes #32151
    
    Signed-off-by: Ali Beyad <abeyad@google.com>

--- test/extensions/filters/http/alternate_protocols_cache/filter_integration_test.cc
+++ test/extensions/filters/http/alternate_protocols_cache/filter_integration_test.cc
@@ -262,7 +262,7 @@ TEST_P(FilterIntegrationTest, AltSvcCachedH3Slow) {
 }
 
 // TODO(32151): Figure out why it's flaky and re-enable.
-TEST_P(FilterIntegrationTest, DISABLED_AltSvcCachedH2Slow) {
+TEST_P(FilterIntegrationTest, AltSvcCachedH2Slow) {
 #ifdef WIN32
   // TODO: sort out what race only happens on windows and GCC.
   GTEST_SKIP() << "Skipping on Windows";
