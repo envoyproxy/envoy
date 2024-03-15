34c0502fd8c4dd8ba236990c897310995f582ca0
Joshua Marantz <jmarantz@google.com>
2024-03-14 20:13:19 -0400

    format
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/extensions/clusters/common/logical_host.h
+++ source/extensions/clusters/common/logical_host.h
@@ -124,7 +124,9 @@ public:
   }
   const std::string& hostname() const override { return logical_host_->hostname(); }
   Network::Address::InstanceConstSharedPtr address() const override { return address_; }
-  const AddressVectorSharedPtr& addressList() const override { return logical_host_->addressList(); }
+  const AddressVectorSharedPtr& addressList() const override {
+    return logical_host_->addressList();
+  }
   const envoy::config::core::v3::Locality& locality() const override {
     return logical_host_->locality();
   }
