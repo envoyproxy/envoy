694f6e28742afcbba48728cf5647885bb2bbb5a8
Fredy Wijaya <fredyw@google.com>
2024-03-19 05:55:22 +0000

    Add null check
    Signed-off-by: Fredy Wijaya <fredyw@google.com>

--- mobile/library/objective-c/EnvoyEngineImpl.mm
+++ mobile/library/objective-c/EnvoyEngineImpl.mm
@@ -401,7 +401,9 @@ static void ios_track_event(envoy_map map, const void *context) {
     // This code block runs inside the Envoy event loop. Therefore, an explicit autoreleasepool
     // block is necessary to act as a breaker for any Objective-C allocation that happens.
     @autoreleasepool {
-      self.onEngineRunning();
+      if (self.onEngineRunning) {
+        self.onEngineRunning();
+      }
     }
   };
   native_callbacks->on_exit = [] {
