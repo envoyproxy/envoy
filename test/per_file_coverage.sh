52739b487e4a1be25011b5c9b0282c2c88d1b2ab
Greg Greenway <ggreenway@apple.com>
2024-03-22 07:55:40 -0700

    adjust coverage
    Signed-off-by: Greg Greenway <ggreenway@apple.com>

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -28,8 +28,9 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/extensions/common/tap:94.5"
 "source/extensions/common/wasm:88.0" # flaky: be careful adjusting
 "source/extensions/common/wasm/ext:92.0"
+"source/extensions/filters/common:96.5"
 "source/extensions/filters/common/fault:94.5"
-"source/extensions/filters/common/rbac:90.5"
+"source/extensions/filters/common/rbac:90.2"
 "source/extensions/filters/http/cache:95.0"
 "source/extensions/filters/http/grpc_json_transcoder:93.8" # TODO(#28232)
 "source/extensions/filters/http/ip_tagging:88.0"
