c838106d8f9da0707838c738fd24ba3266c46bcd
Kuat Yessenov <kuat@google.com>
2024-03-26 20:14:33 +0000

    fix tests
    Change-Id: I835d26ec21a843fb4905413768a457bef3aca3a6
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- test/server/hot_restarting_child_test.cc
+++ test/server/hot_restarting_child_test.cc
@@ -228,9 +228,11 @@ TEST_F(HotRestartingChildTest, ForwardsPacketToRegisteredListenerOnMatch) {
   packet->set_payload(udp_contents);
   packet->set_receive_time_epoch_microseconds(packet_timestamp);
   Network::MockUdpListenerWorkerRouter mock_worker_router;
-  EXPECT_CALL(*mock_udp_listener_config,
-              listenerWorkerRouter(WhenDynamicCastTo<const Network::Address::Ipv4Instance&>(
-                  Eq(dynamic_cast<const Network::Address::Ipv4Instance&>(*test_listener_addr)))))
+  EXPECT_CALL(*mock_udp_listener_config, listenerWorkerRouter(testing::ResultOf(
+                                             [](const Network::Address::Instance& instance) {
+                                               return instance.asStringView();
+                                             },
+                                             Eq(test_listener_addr->asStringView()))))
       .WillOnce(ReturnRef(mock_worker_router));
   EXPECT_CALL(mock_worker_router,
               deliver(worker_index, IsUdpWith(test_listener_addr, test_remote_addr, udp_contents,
