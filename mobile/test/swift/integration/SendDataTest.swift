5ff8dced6cc8253899988eaab0a803bbc1a73e90
Alyssa Wilk <alyssar@chromium.org>
2024-03-26 15:51:51 -0400

    swift: using the builder
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- mobile/test/swift/integration/SendDataTest.swift
+++ mobile/test/swift/integration/SendDataTest.swift
@@ -1,5 +1,6 @@
 import Envoy
 import EnvoyEngine
+import EnvoyTestServer
 import Foundation
 import TestExtensions
 import XCTest
@@ -11,66 +12,27 @@ final class SendDataTests: XCTestCase {
   }
 
   func testSendData() throws {
-    // swiftlint:disable:next line_length
-    let emhcmType = "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.EnvoyMobileHttpConnectionManager"
-    // swiftlint:disable:next line_length
-    let assertionFilterType = "type.googleapis.com/envoymobile.extensions.filters.http.assertion.Assertion"
     let requestStringMatch = "match_me"
-    let config =
-"""
-listener_manager:
-    name: envoy.listener_manager_impl.api
-    typed_config:
-      "@type": type.googleapis.com/envoy.config.listener.v3.ApiListenerManager
-static_resources:
-  listeners:
-  - name: base_api_listener
-    address:
-      socket_address:
-        protocol: TCP
-        address: 0.0.0.0
-        port_value: 10000
-    api_listener:
-      api_listener:
-        "@type": \(emhcmType)
-        config:
-          stat_prefix: hcm
-          route_config:
-            name: api_router
-            virtual_hosts:
-              - name: api
-                domains:
-                  - "*"
-                routes:
-                  - match:
-                      prefix: "/"
-                    direct_response:
-                      status: 200
-          http_filters:
-            - name: envoy.filters.http.assertion
-              typed_config:
-                "@type": \(assertionFilterType)
-                match_config:
-                  http_request_generic_body_match:
-                    patterns:
-                      - string_match: \(requestStringMatch)
-            - name: envoy.filters.http.buffer
-              typed_config:
-                "@type": type.googleapis.com/envoy.extensions.filters.http.buffer.v3.Buffer
-                max_request_bytes: 65000
-            - name: envoy.router
-              typed_config:
-                "@type": type.googleapis.com/envoy.extensions.filters.http.router.v3.Router
-"""
     let expectation = self.expectation(description: "Run called with expected http status")
-    let engine = EngineBuilder(yaml: config)
+    EnvoyTestServer.startHttp1PlaintextServer()
+    let port = String(EnvoyTestServer.getEnvoyPort())
+
+    let engine = EngineBuilder()
       .addLogLevel(.debug)
+      .addNativeFilter(
+        name: "envoy.filters.http.buffer",
+        // swiftlint:disable:next line_length
+        typedConfig: "{\"@type\":\"type.googleapis.com/envoy.extensions.filters.http.buffer.v3.Buffer\",\"\"max_request_bytes\":65000}")
+      .addNativeFilter(
+        name: "envoy.filters.http.assertion",
+        // swiftlint:disable:next line_length
+        typedConfig: "{\"@type\":\"type.googleapis.com/envoymobile.extensions.filters.http.assertion.Assertion\",match_config: {http_request_generic_body_match: {patterns: [{string_match: \"match_me\"}]}}}")
       .build()
 
     let client = engine.streamClient()
 
-    let requestHeaders = RequestHeadersBuilder(method: .get, scheme: "https",
-                                               authority: "example.com", path: "/test")
+    let requestHeaders = RequestHeadersBuilder(method: .get, scheme: "http",
+                                               authority: "localhost:" + port, path: "/test")
       .build()
     let body = try XCTUnwrap(requestStringMatch.data(using: .utf8))
 
@@ -91,5 +53,6 @@ static_resources:
     XCTAssertEqual(XCTWaiter.wait(for: [expectation], timeout: 10), .completed)
 
     engine.terminate()
+    EnvoyTestServer.shutdownTestServer()
   }
 }
