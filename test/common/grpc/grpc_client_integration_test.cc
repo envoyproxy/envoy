2cfc399c743a8e391bc1c01381b81998e74b2f8b
tyxia <tyxia@google.com>
2024-03-29 15:32:26 +0000

    update
    Signed-off-by: tyxia <tyxia@google.com>

--- test/common/grpc/grpc_client_integration_test.cc
+++ test/common/grpc/grpc_client_integration_test.cc
@@ -241,8 +241,7 @@ TEST_P(GrpcClientIntegrationTest, BadReplyOverGrpcFrameLimit) {
   stream->sendRequest();
   stream->sendServerInitialMetadata(empty_metadata_);
   stream->expectTrailingMetadata(empty_metadata_);
-  // TODO(tyxia) Change the error status to RESOURCE_EXHAUSTED.
-  stream->expectGrpcStatus(Status::WellKnownGrpcStatus::Internal);
+  stream->expectGrpcStatus(Status::WellKnownGrpcStatus::RESOURCE_EXHAUSTED);
   auto serialized_response = Grpc::Common::serializeToGrpcFrame(reply);
   stream->fake_stream_->encodeData(*serialized_response, true);
   dispatcher_helper_.runDispatcher();
