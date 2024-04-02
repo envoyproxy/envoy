5dfefa5d2c80ffa784913438c69943db5e3e6b86
Alyssa Wilk <alyssar@chromium.org>
2024-04-02 10:44:26 -0400

    format
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- tools/repo/notify.py
+++ tools/repo/notify.py
@@ -163,7 +163,8 @@ class RepoNotifier(runner.Runner):
         priorweek = now - datetime.timedelta(14)
 
         # Handle the event being created before today.
-        date = ("%s%s%s" % (priorweek.year, str(priorweek.month).zfill(2), str(priorweek.day).zfill(2)))
+        date = (
+            "%s%s%s" % (priorweek.year, str(priorweek.month).zfill(2), str(priorweek.day).zfill(2)))
         response = await self.session.get(CALENDAR + "?getdate=" + date)
         content = await response.read()
         parsed_calendar = icalendar.Calendar.from_ical(content)
@@ -176,7 +177,7 @@ class RepoNotifier(runner.Runner):
                     return component.get("summary")
                 if (monday.date() == component.decoded("dtstart").date()):
                     return component.get("summary")
-        print( "unable to find this week's oncall")
+        print("unable to find this week's oncall")
         return "unable to find this week's oncall"
 
     @async_property
