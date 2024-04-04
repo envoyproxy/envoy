267dde1a4e78eb9ff4de616aabde1924cbf3b786
Fredy Wijaya <fredyw@google.com>
2024-04-04 13:47:36 +0000

    Add @VisibleForTesting
    Signed-off-by: Fredy Wijaya <fredyw@google.com>

--- mobile/library/java/io/envoyproxy/envoymobile/engine/AndroidNetworkMonitor.java
+++ mobile/library/java/io/envoyproxy/envoymobile/engine/AndroidNetworkMonitor.java
@@ -59,6 +59,7 @@ public class AndroidNetworkMonitor extends BroadcastReceiver {
    * Sets the {@link AndroidNetworkMonitor} singleton instance to null, so that it can be recreated
    * when a new EnvoyEngine is created.
    */
+  @VisibleForTesting
   public static void shutdown() { instance = null; }
 
   private AndroidNetworkMonitor(Context context, EnvoyEngine envoyEngine) {
