45ecd790d9f0fd1e3635ea9794e2205ea4811ef3
Boteng Yao <boteng@google.com>
2024-04-17 01:58:14 +0000

    fix format
    Signed-off-by: Boteng Yao <boteng@google.com>

--- source/common/router/upstream_request.cc
+++ source/common/router/upstream_request.cc
@@ -134,7 +134,7 @@ UpstreamRequest::UpstreamRequest(RouterFilterInterface& parent,
   stream_info_.healthCheck(parent_.callbacks()->streamInfo().healthCheck());
   stream_info_.setIsShadow(parent_.callbacks()->streamInfo().isShadow());
   absl::optional<Upstream::ClusterInfoConstSharedPtr> cluster_info =
-    parent_.callbacks()->streamInfo().upstreamClusterInfo();
+      parent_.callbacks()->streamInfo().upstreamClusterInfo();
   if (cluster_info.has_value()) {
     stream_info_.setUpstreamClusterInfo(*cluster_info);
   }
