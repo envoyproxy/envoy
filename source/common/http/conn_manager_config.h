05dba2c8a4fa7917c6fe8138a57b1e8fead5bc7d
zty98751 <zty98751@alibaba-inc.com>
2024-04-18 19:00:38 +0800

    HCM supports configuring keepalive header timeout
    Signed-off-by: zty98751 <zty98751@alibaba-inc.com>

--- source/common/http/conn_manager_config.h
+++ source/common/http/conn_manager_config.h
@@ -544,6 +544,12 @@ public:
    *         Connection Lifetime.
    */
   virtual bool addProxyProtocolConnectionState() const PURE;
+
+  /**
+   * @return the timeout seconds will be set in the "Keep-Alive" response header.
+   * Zero indicates this behavior is disabled.
+   */
+  virtual std::chrono::seconds keepaliveHeaderTimeout() const PURE;
 };
 } // namespace Http
 } // namespace Envoy
