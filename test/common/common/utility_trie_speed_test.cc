02a6c2c2392096bdb9c15c8aec24825fb8b92fca
Raven Black <ravenblack@dropbox.com>
2024-04-25 21:49:50 +0000

    Benchmarks
    Signed-off-by: Raven Black <ravenblack@dropbox.com>

---
+++ test/common/common/utility_trie_speed_test.cc
@@ -0,0 +1,62 @@
+// Note: this should be run with --compilation_mode=opt, and would benefit from a
+// quiescent system with disabled cstate power management.
+
+#include <random>
+
+#include "source/common/common/assert.h"
+#include "source/common/common/utility.h"
+
+#include "absl/strings/string_view.h"
+#include "benchmark/benchmark.h"
+
+namespace Envoy {
+
+// NOLINT(namespace-envoy)
+
+using KeyLengthRange = std::pair<size_t, size_t>;
+
+// Args are:
+// 0 - size_t num_keys
+// 1 - std::pair<size_t, size_t> keylength_range
+template <typename... Args> static void bmTrieLookups(benchmark::State& state, Args&&... args) {
+  auto args_t = std::make_tuple(std::move(args)...);
+  const size_t num_keys = std::get<0>(args_t);
+  KeyLengthRange keylength_range = std::get<1>(args_t);
+  std::mt19937 prng(1); // PRNG with a fixed seed, for repeatability
+  std::uniform_int_distribution<char> char_distribution('a', 'z');
+  std::uniform_int_distribution<size_t> keylength_distribution(keylength_range.first,
+                                                               keylength_range.second);
+  std::uniform_int_distribution<size_t> keyindex_distribution(0, num_keys - 1);
+  auto make_key = [&](size_t len) {
+    std::string ret;
+    for (size_t i = 0; i < len; i++) {
+      ret.push_back(char_distribution(prng));
+    }
+    return ret;
+  };
+  TrieLookupTable<const void*> trie;
+  std::vector<std::string> keys;
+  for (size_t i = 0; i < num_keys; i++) {
+    std::string key = make_key(keylength_distribution(prng));
+    trie.add(key, reinterpret_cast<const void*>(&num_keys));
+    keys.push_back(std::move(key));
+  }
+
+  std::vector<size_t> key_selections;
+  for (size_t i = 0; i < 1024; i++) {
+    key_selections.push_back(keyindex_distribution(prng));
+  }
+  size_t key_index = 0;
+  for (auto _ : state) {
+    UNREFERENCED_PARAMETER(_);
+    auto v = trie.find(keys[key_selections[key_index++]]);
+    key_index &= 1023;
+    benchmark::DoNotOptimize(v);
+  }
+}
+BENCHMARK_CAPTURE(bmTrieLookups, 100_short_keys, 100, KeyLengthRange(8, 8));
+BENCHMARK_CAPTURE(bmTrieLookups, 100_long_keys, 100, KeyLengthRange(128, 128));
+BENCHMARK_CAPTURE(bmTrieLookups, 100_mixed_keys, 100, KeyLengthRange(8, 128));
+BENCHMARK_CAPTURE(bmTrieLookups, 10000_mixed_keys, 10000, KeyLengthRange(8, 128));
+
+} // namespace Envoy
