d8d2470ef9687b82ffb5cdee98a372f5f17d0ced
Vikas Choudhary <choudharyvikas16@gmail.com>
2024-04-27 07:44:07 +0530

    test
    Signed-off-by: Vikas Choudhary <choudharyvikas16@gmail.com>

--- test/extensions/filters/http/credential_injector/credential_injector_oauth_integration_test.cc
+++ test/extensions/filters/http/credential_injector/credential_injector_oauth_integration_test.cc
@@ -129,6 +129,7 @@ typed_config:
         client_secret:
           name: client-secret
           sds_config:
+            resource_api_version: V3
             path_config_source:
               path: "{{ test_tmpdir }}/client_secret.yaml"
 )EOF";
@@ -179,6 +180,7 @@ typed_config:
         client_secret:
           name: client-secret
           sds_config:
+            resource_api_version: V3
             path_config_source:
               path: "{{ test_tmpdir }}/client_secret.yaml"
 )EOF";
@@ -233,6 +235,7 @@ typed_config:
         client_secret:
           name: client-secret
           sds_config:
+            resource_api_version: V3
             path_config_source:
               path: "{{ test_tmpdir }}/client_secret.yaml"
 )EOF";
@@ -286,6 +289,7 @@ typed_config:
         client_secret:
           name: client-secret
           sds_config:
+            resource_api_version: V3
             path_config_source:
               path: "{{ test_tmpdir }}/initial_secret.yaml"
 )EOF";
@@ -415,6 +419,7 @@ typed_config:
         client_secret:
           name: client-secret
           sds_config:
+            resource_api_version: V3
             path_config_source:
               path: "{{ test_tmpdir }}/client_secret.yaml"
 )EOF";
