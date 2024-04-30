54eea712cda023c08582eaa56b8af38b887de4df
Vikas Choudhary <choudharyvikas16@gmail.com>
2024-04-30 09:16:22 +0530

    remove debugging changes
    Signed-off-by: Vikas Choudhary <choudharyvikas16@gmail.com>

--- test/config_test/example_configs_test_setup.sh
+++ test/config_test/example_configs_test_setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 DIR="$TEST_TMPDIR"/test/config_test
 mkdir -p "$DIR"
 
 # Windows struggles with its own paths, so we have to help it out with `--force-local`
-# tar --force-local -xvf "$TEST_SRCDIR"/"$EXAMPLE_CONFIGS_TAR_PATH" -C "$DIR"
-tar --force-local -xvf /home/vikas/cpp/envoy2/bazel-bin/configs/example_configs.tar -C "$DIR"
+tar --force-local -xvf "$TEST_SRCDIR"/"$EXAMPLE_CONFIGS_TAR_PATH" -C "$DIR"
 
 # find uses full path to prevent using Windows find on Windows.
 # grep -v is used to exclude the secrets directory because secrets are yamls but are not configs to be tested.
