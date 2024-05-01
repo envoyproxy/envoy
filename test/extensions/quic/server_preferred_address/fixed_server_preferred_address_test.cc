59ded87c5f16fe14f22573cfb7e1b83e5b34748a
Greg Greenway <ggreenway@apple.com>
2024-05-01 08:41:22 -0700

    more tests
    Signed-off-by: Greg Greenway <ggreenway@apple.com>

--- test/extensions/quic/server_preferred_address/fixed_server_preferred_address_test.cc
+++ test/extensions/quic/server_preferred_address/fixed_server_preferred_address_test.cc
@@ -53,6 +53,16 @@ TEST_F(FixedServerPreferredAddressConfigTest, Validation) {
         ".*'dnat_address' but not 'address' is set in server preferred address for v4.*");
   }
   {
+    // Cannot set port on address if dnat address isn't set.
+    envoy::extensions::quic::server_preferred_address::v3::FixedServerPreferredAddressConfig cfg;
+    cfg.mutable_ipv4_config()->mutable_address()->set_address("127.0.0.1");
+    cfg.mutable_ipv4_config()->mutable_address()->set_port_value(1);
+    EXPECT_THROW_WITH_REGEX(factory_.createServerPreferredAddressConfig(cfg, visitor_, {}),
+                            EnvoyException,
+                            ".*'address' port must be zero unless 'dnat_address' is set in address "
+                            "127.0.0.1:1 for address family v4.*");
+  }
+  {
     // v6 address in v4 field.
     envoy::extensions::quic::server_preferred_address::v3::FixedServerPreferredAddressConfig cfg;
     cfg.mutable_ipv4_config()->mutable_address()->set_address("::1");
@@ -85,6 +95,8 @@ TEST_F(FixedServerPreferredAddressConfigTest, AddressAndPortIgnoresListenerPort)
   envoy::extensions::quic::server_preferred_address::v3::FixedServerPreferredAddressConfig cfg;
   cfg.mutable_ipv4_config()->mutable_address()->set_address("1.2.3.4");
   cfg.mutable_ipv4_config()->mutable_address()->set_port_value(5);
+  cfg.mutable_ipv4_config()->mutable_dnat_address()->set_address("127.0.0.1");
+  cfg.mutable_ipv4_config()->mutable_dnat_address()->set_port_value(0);
   auto obj = factory_.createServerPreferredAddressConfig(cfg, visitor_, {});
   auto addresses = obj->getServerPreferredAddresses(
       Network::Utility::parseInternetAddressNoThrow("127.0.0.1", 1234));
@@ -99,6 +111,40 @@ TEST_F(FixedServerPreferredAddressConfigTest, AddressAndZeroPortUsesListenerPort
   auto addresses = obj->getServerPreferredAddresses(
       Network::Utility::parseInternetAddressNoThrow("127.0.0.1", 1234));
   EXPECT_EQ(addresses.ipv4_.ToString(), "1.2.3.4:1234");
+}
+
+TEST_F(FixedServerPreferredAddressConfigTest, DnatAddressAndZeroPortUsesListenerPort) {
+  envoy::extensions::quic::server_preferred_address::v3::FixedServerPreferredAddressConfig cfg;
+  cfg.mutable_ipv4_config()->mutable_address()->set_address("1.2.3.4");
+  cfg.mutable_ipv4_config()->mutable_address()->set_port_value(0);
+  cfg.mutable_ipv4_config()->mutable_dnat_address()->set_address("127.0.0.1");
+  cfg.mutable_ipv4_config()->mutable_dnat_address()->set_port_value(0);
+  auto obj = factory_.createServerPreferredAddressConfig(cfg, visitor_, {});
+  auto addresses = obj->getServerPreferredAddresses(
+      Network::Utility::parseInternetAddressNoThrow("127.0.0.1", 1234));
+  EXPECT_EQ(addresses.dnat_ipv4_.ToString(), "127.0.0.1:1234");
+}
+
+// `ipv4_config` is preferred over `ipv4_address` if both are set.
+TEST_F(FixedServerPreferredAddressConfigTest, FieldPrecedence) {
+  envoy::extensions::quic::server_preferred_address::v3::FixedServerPreferredAddressConfig cfg;
+  cfg.set_ipv4_address("2.2.2.2");
+  cfg.mutable_ipv4_config()->mutable_address()->set_address("1.2.3.4");
+  cfg.mutable_ipv4_config()->mutable_address()->set_port_value(0);
+  auto obj = factory_.createServerPreferredAddressConfig(cfg, visitor_, {});
+  auto addresses = obj->getServerPreferredAddresses(
+      Network::Utility::parseInternetAddressNoThrow("127.0.0.1", 1234));
+  EXPECT_EQ(addresses.ipv4_.ToString(), "1.2.3.4:1234");
+}
+
+// If only `ipv4_address` is set, it is used.
+TEST_F(FixedServerPreferredAddressConfigTest, LegacyField) {
+  envoy::extensions::quic::server_preferred_address::v3::FixedServerPreferredAddressConfig cfg;
+  cfg.set_ipv4_address("2.2.2.2");
+  auto obj = factory_.createServerPreferredAddressConfig(cfg, visitor_, {});
+  auto addresses = obj->getServerPreferredAddresses(
+      Network::Utility::parseInternetAddressNoThrow("127.0.0.1", 1234));
+  EXPECT_EQ(addresses.ipv4_.ToString(), "2.2.2.2:1234");
 }
 
 } // namespace Quic
