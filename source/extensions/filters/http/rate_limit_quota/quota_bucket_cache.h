5db1b00c22b0fbd5f69aa3b943aedc44a8430110
Brian Surber <bsurber@google.com>
2024-05-03 23:49:49 +0000

    Create a new bucket cache type, that can be safely used across multiple filter worker threads, and the client interface that the worker threads can call to for unsafe operations.
    Signed-off-by: Brian Surber <bsurber@google.com>

--- source/extensions/filters/http/rate_limit_quota/quota_bucket_cache.h
+++ source/extensions/filters/http/rate_limit_quota/quota_bucket_cache.h
@@ -1,18 +1,20 @@
 #pragma once
 #include <algorithm>
+#include <atomic>
+#include <chrono>
+#include <cstddef>
 #include <memory>
 
 #include "envoy/extensions/filters/http/rate_limit_quota/v3/rate_limit_quota.pb.h"
 #include "envoy/extensions/filters/http/rate_limit_quota/v3/rate_limit_quota.pb.validate.h"
 #include "envoy/service/rate_limit_quota/v3/rlqs.pb.h"
 #include "envoy/service/rate_limit_quota/v3/rlqs.pb.validate.h"
-
+#include "envoy/common/token_bucket.h"
+#include "envoy/event/dispatcher.h"
+#include "envoy/thread_local/thread_local_object.h"
 #include "source/common/common/token_bucket_impl.h"
 #include "source/common/protobuf/utility.h"
 #include "source/extensions/filters/http/common/factory_base.h"
-#include "source/extensions/filters/http/rate_limit_quota/client.h"
-
-#include "absl/time/time.h"
 
 namespace Envoy {
 namespace Extensions {
@@ -22,102 +24,84 @@ namespace RateLimitQuota {
 using ::envoy::service::rate_limit_quota::v3::BucketId;
 using ::envoy::service::rate_limit_quota::v3::RateLimitQuotaUsageReports;
 
-using BucketAction = ::envoy::service::rate_limit_quota::v3::RateLimitQuotaResponse::BucketAction;
-using BucketQuotaUsage =
-    ::envoy::service::rate_limit_quota::v3::RateLimitQuotaUsageReports::BucketQuotaUsage;
+using BucketAction = ::envoy::service::rate_limit_quota::v3::
+    RateLimitQuotaResponse::BucketAction;
+using BucketQuotaUsage = ::envoy::service::rate_limit_quota::v3::
+    RateLimitQuotaUsageReports::BucketQuotaUsage;
 
 struct QuotaUsage {
+  QuotaUsage(uint64_t num_requests_allowed, uint64_t num_requests_denied,
+             std::chrono::nanoseconds last_report)
+      : num_requests_allowed(num_requests_allowed),
+        num_requests_denied(num_requests_denied),
+        last_report(last_report) {}
+
   // Requests allowed.
-  uint64_t num_requests_allowed = {};
+  std::atomic<uint64_t> num_requests_allowed;
   // Requests throttled.
-  uint64_t num_requests_denied = {};
-  // Last report time.
-  std::chrono::nanoseconds last_report = {};
+  std::atomic<uint64_t> num_requests_denied;
+  // Last report time. Should be visible to worker threads so they can check if
+  // reporting timing is going correctly. Should only be updated by main thread.
+  std::atomic<std::chrono::nanoseconds> last_report;
 };
 
-// This object stores the data for single bucket entry.
-struct Bucket {
-  // BucketId object that is associated with this bucket. It is part of the report that is sent to
-  // RLQS server.
+// This object stores the data for single bucket entry. The usage cache & action
+// cache are in separate pointers to enable separate pointer-swapping.
+struct CachedBucket {
+  CachedBucket(
+      const BucketId& bucket_id, std::shared_ptr<QuotaUsage> quota_usage,
+      const BucketAction& bucket_action,
+      std::shared_ptr<::Envoy::TokenBucket> token_bucket_limiter)
+      : bucket_id(bucket_id),
+        quota_usage(quota_usage),
+        bucket_action(bucket_action),
+        token_bucket_limiter(token_bucket_limiter) {}
+
+  // BucketId object that is associated with this bucket. It is part of the
+  // report that is sent to RLQS server.
   BucketId bucket_id;
-  // Cached action from the response that was received from the RLQS server.
+  // Aggregated usage of the ID'd bucket for the next report cycle.
+  std::shared_ptr<QuotaUsage> quota_usage;
+  // Cached action from the RLQS server's last response that gave an updated
+  // assignment for this ID'd bucket.
   BucketAction bucket_action;
-  // Cache quota usage.
-  QuotaUsage quota_usage;
-  // Rate limiter based on token bucket algorithm.
-  TokenBucketPtr token_bucket_limiter;
+  // Rate limiter based on token bucket algorithm. Shared to make a single
+  // TokenBucket reusable when copying & pointer-swapping the overall cache.
+  std::shared_ptr<::Envoy::TokenBucket> token_bucket_limiter;
 };
 
-using BucketsCache = absl::flat_hash_map<size_t, std::unique_ptr<Bucket>>;
+/**
+ * An interface for a client used in the RateLimitQuotaService (RLQS) filter
+ * worker threads to safely access & modify global resources.
+ */
+class RateLimitClient {
+ public:
+  virtual ~RateLimitClient() = default;
 
-struct ThreadLocalClient : public Logger::Loggable<Logger::Id::rate_limit_quota> {
-  ThreadLocalClient(Envoy::Event::Dispatcher& dispatcher) {
-    send_reports_timer = dispatcher.createTimer([this] { sendPeriodicalReports(); });
-  }
-
-  // Disable copy constructor and assignment.
-  ThreadLocalClient(const ThreadLocalClient& client) = delete;
-  ThreadLocalClient& operator=(const ThreadLocalClient& client) = delete;
-  // Default move constructor and assignment.
-  ThreadLocalClient(ThreadLocalClient&& client) = default;
-  ThreadLocalClient& operator=(ThreadLocalClient&& client) = default;
-
-  ~ThreadLocalClient() {
-    if (rate_limit_client != nullptr) {
-      rate_limit_client->closeStream();
-    }
-  }
-
-  // Helper function to send the reports periodically on timer.
-  void sendPeriodicalReports() {
-    if (rate_limit_client != nullptr) {
-      rate_limit_client->sendUsageReport(absl::nullopt);
-    } else {
-      ENVOY_LOG(error, "Rate limit client has been destroyed; no periodical report send");
-    }
-
-    if (send_reports_timer != nullptr) {
-      send_reports_timer->enableTimer(report_interval_ms);
-    } else {
-      ENVOY_LOG(error, "Reports timer has been destroyed; no periodical report send");
-    }
-  }
-
-  // Rate limit client. It is owned here (in TLS) and is used by all the buckets.
-  std::unique_ptr<RateLimitClient> rate_limit_client;
-  // The timer for sending the reports periodically.
-  Event::TimerPtr send_reports_timer;
-  // Periodical reporting interval(in milliseconds).
-  std::chrono::milliseconds report_interval_ms = std::chrono::milliseconds::zero();
+  // Safe creation & getting of global buckets.
+  virtual void createBucket(const BucketId& bucket_id, size_t id,
+                            const BucketAction& initial_bucket_action,
+                            bool initial_request_allowed) PURE;
+  virtual std::shared_ptr<CachedBucket> getBucket(size_t id) PURE;
 };
 
-class ThreadLocalBucket : public Envoy::ThreadLocal::ThreadLocalObject {
-public:
-  ThreadLocalBucket(Envoy::Event::Dispatcher& dispatcher) : client_(dispatcher) {}
+// shared_ptr<CachedBucket> must be passed around to guarantee the
+// CachedBucket's existence for the duration of its use in a local thread after
+// getting it via getBucket.
+using BucketsCache = absl::flat_hash_map<size_t, std::shared_ptr<CachedBucket>>;
 
-  // Return the buckets; returned by reference so it can be modified.
-  BucketsCache& quotaBuckets() { return quota_buckets_; }
+class ThreadLocalBucketsCache : public Envoy::ThreadLocal::ThreadLocalObject,
+                                Logger::Loggable<Logger::Id::rate_limit_quota> {
+ public:
+  ThreadLocalBucketsCache(std::shared_ptr<BucketsCache> quota_buckets)
+      : quota_buckets_(quota_buckets) {}
 
-  // Return the rate limit client; returned by reference so no ownership transferred.
-  ThreadLocalClient& rateLimitClient() { return client_; }
-
-private:
-  // Thread local storage for bucket container and client.
-  BucketsCache quota_buckets_;
-  ThreadLocalClient client_;
+  // Thread-unsafe operations like index creation should only be done by the
+  // global ThreadLocalClient.
+  std::shared_ptr<BucketsCache> quota_buckets_;
 };
 
-struct QuotaBucket {
-  QuotaBucket(Envoy::Server::Configuration::FactoryContext& context)
-      : tls(context.serverFactoryContext().threadLocal()) {
-    tls.set([](Envoy::Event::Dispatcher& dispatcher) {
-      return std::make_shared<ThreadLocalBucket>(dispatcher);
-    });
-  }
-  Envoy::ThreadLocal::TypedSlot<ThreadLocalBucket> tls;
-};
-
-} // namespace RateLimitQuota
-} // namespace HttpFilters
-} // namespace Extensions
-} // namespace Envoy
+}  // namespace RateLimitQuota
+}  // namespace HttpFilters
+}  // namespace Extensions
+}  // namespace Envoy
