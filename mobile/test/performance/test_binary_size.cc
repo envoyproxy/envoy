aff8c1ccab26432da6ad874f33eaf0c687bdb294
Ali Beyad <abeyad@google.com>
2024-05-06 23:53:13 +0000

    try
    Signed-off-by: Ali Beyad <abeyad@google.com>

--- mobile/test/performance/test_binary_size.cc
+++ mobile/test/performance/test_binary_size.cc
@@ -3,6 +3,7 @@
 // NOLINT(namespace-envoy)
 
 // This binary is used to perform stripped down binary size investigations of the Envoy codebase.
+//
 // Please refer to the development docs for more information:
 // https://envoymobile.io/docs/envoy-mobile/latest/development/performance/binary_size.html
 int main() { return reinterpret_cast<Envoy::InternalEngine*>(0)->run(nullptr); }
