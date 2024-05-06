6f2c2773ab1da6847e15871feeb0f853f9336752
Ali Beyad <abeyad@google.com>
2024-05-06 19:59:32 +0000

    fix
    Signed-off-by: Ali Beyad <abeyad@google.com>

--- mobile/test/performance/test_binary_size.cc
+++ mobile/test/performance/test_binary_size.cc
@@ -5,7 +5,4 @@
 // This binary is used to perform stripped down binary size investigations of the Envoy codebase.
 // Please refer to the development docs for more information:
 // https://envoymobile.io/docs/envoy-mobile/latest/development/performance/binary_size.html
-int main() {
-  auto options = std::make_shared<Envoy::OptionsImplBase>();
-  return reinterpret_cast<Envoy::InternalEngine*>(0)->run(std::move(options));
-}
+int main() { return reinterpret_cast<Envoy::InternalEngine*>(0)->run(nullptr); }
