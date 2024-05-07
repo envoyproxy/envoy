bc971f3b224d2958f0d5fae8a27e95ecbfb2cdf4
Adi Suissa-Peleg <adip@google.com>
2024-05-07 21:10:22 +0000

    trying to solve Envoy Mobile issue
    Signed-off-by: Adi Suissa-Peleg <adip@google.com>

--- source/common/protobuf/utility.cc
+++ source/common/protobuf/utility.cc
@@ -401,7 +401,7 @@ public:
     const Protobuf::ReflectableMessage reflectable_message = createReflectableMessage(message);
     if (reflectable_message->GetDescriptor()->full_name() == "google.protobuf.Duration") {
       ProtobufWkt::Duration duration_message;
-      duration_message.CheckTypeAndMergeFrom(message);
+      duration_message.MergeFrom(message);
       // Validate the value of the duration.
       absl::Status status = validateDurationUnifiedNoThrow(duration_message);
       if (!status.ok()) {
