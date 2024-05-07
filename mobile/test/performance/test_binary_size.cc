23d600ac4c366475a3729951097254c654ca723a
Ali Beyad <abeyad@google.com>
2024-05-07 14:59:25 -0400

    mobile: Fix the test_binary_size program to use the InternalEngine (#34010)
    The previous test_binary_size code was:
    reinterpret_cast<Envoy::InternalEngine*>(0)->run(nullptr, nullptr);
    
    This code has a couple problems: (1) we don't actually create an InternalEngine; we just cast an arbitrary
    integer value (in this case, 0) to a InternalEngine pointer and (2) the run() function takes const references,
    not pointers. The way this code was structured, it's believed that the binary size expectations were incorrect,
    because the compiler could strip/elide code that this particular program setup would render unused code.
    
    After changing the program structure, we do indeed see a size difference. Instead of 4.5MB, as is the current
    expectation, we are seeing the program size actually as 5.41MB.
    
    Signed-off-by: Ali Beyad <abeyad@google.com>

--- mobile/test/performance/test_binary_size.cc
+++ mobile/test/performance/test_binary_size.cc
@@ -5,4 +5,9 @@
 // This binary is used to perform stripped down binary size investigations of the Envoy codebase.
 // Please refer to the development docs for more information:
 // https://envoymobile.io/docs/envoy-mobile/latest/development/performance/binary_size.html
-int main() { return reinterpret_cast<Envoy::InternalEngine*>(0)->run(nullptr, nullptr); }
+int main() {
+  Envoy::InternalEngine engine(nullptr, nullptr, nullptr);
+  const std::string config = "";
+  const std::string log_level = "";
+  engine.run(config, log_level);
+}
