d809b86bd6bf10f06f5648dd8bdd6a22acaf8b38
alyssawilk <alyssar@chromium.org>
2024-05-07 16:03:52 -0400

    mobile: more DNS testing (#33974)
    mobile: adding an e2e test with fake dns
    
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- source/extensions/network/dns_resolver/getaddrinfo/getaddrinfo.cc
+++ source/extensions/network/dns_resolver/getaddrinfo/getaddrinfo.cc
@@ -186,24 +186,6 @@ void GetAddrInfoDnsResolver::resolveThreadRoutine() {
   ENVOY_LOG(debug, "getaddrinfo resolver thread exiting");
 }
 
-// getaddrinfo DNS resolver factory
-class GetAddrInfoDnsResolverFactory : public DnsResolverFactory,
-                                      public Logger::Loggable<Logger::Id::dns> {
-public:
-  std::string name() const override { return {"envoy.network.dns_resolver.getaddrinfo"}; }
-
-  ProtobufTypes::MessagePtr createEmptyConfigProto() override {
-    return ProtobufTypes::MessagePtr{new envoy::extensions::network::dns_resolver::getaddrinfo::v3::
-                                         GetAddrInfoDnsResolverConfig()};
-  }
-
-  DnsResolverSharedPtr
-  createDnsResolver(Event::Dispatcher& dispatcher, Api::Api& api,
-                    const envoy::config::core::v3::TypedExtensionConfig&) const override {
-    return std::make_shared<GetAddrInfoDnsResolver>(dispatcher, api);
-  }
-};
-
 // Register the CaresDnsResolverFactory
 REGISTER_FACTORY(GetAddrInfoDnsResolverFactory, DnsResolverFactory);
 
--- source/extensions/network/dns_resolver/getaddrinfo/getaddrinfo.h
+++ source/extensions/network/dns_resolver/getaddrinfo/getaddrinfo.h
@@ -73,5 +73,23 @@ protected:
   const Thread::ThreadPtr resolver_thread_;
 };
 
+// getaddrinfo DNS resolver factory
+class GetAddrInfoDnsResolverFactory : public DnsResolverFactory,
+                                      public Logger::Loggable<Logger::Id::dns> {
+public:
+  std::string name() const override { return {"envoy.network.dns_resolver.getaddrinfo"}; }
+
+  ProtobufTypes::MessagePtr createEmptyConfigProto() override {
+    return ProtobufTypes::MessagePtr{new envoy::extensions::network::dns_resolver::getaddrinfo::v3::
+                                         GetAddrInfoDnsResolverConfig()};
+  }
+
+  DnsResolverSharedPtr
+  createDnsResolver(Event::Dispatcher& dispatcher, Api::Api& api,
+                    const envoy::config::core::v3::TypedExtensionConfig&) const override {
+    return std::make_shared<GetAddrInfoDnsResolver>(dispatcher, api);
+  }
+};
+
 } // namespace Network
 } // namespace Envoy
