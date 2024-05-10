68a7492dc7300a1c77cc615220bbf6da0f5f6c28
Tianyu Xia <tyxia@google.com>
2024-05-10 02:11:22 -0400

    add test
    Signed-off-by: Tianyu Xia <tyxia@google.com>

--- test/extensions/filters/http/rate_limit_quota/integration_test.cc
+++ test/extensions/filters/http/rate_limit_quota/integration_test.cc
@@ -722,6 +722,60 @@ TEST_P(RateLimitQuotaIntegrationTest, DISABLED_MultiRequestWithTokenBucketThrott
   }
 }
 
+TEST_P(RateLimitQuotaIntegrationTest, MultiRequestWithThrottling) {
+  initializeConfig();
+  HttpIntegrationTest::initialize();
+  absl::flat_hash_map<std::string, std::string> custom_headers = {{"environment", "staging"},
+                                                                  {"group", "envoy"}};
+  int max_token = 1;
+  int tokens_per_fill = 30;
+  int fill_interval_sec = 60;
+  int fill_one_token_in_ms = fill_interval_sec / tokens_per_fill * 1000;
+  for (int i = 0; i < 8; ++i) {
+    // We advance time by 2s for 3rd and 5th requests so that token bucket can
+    // be refilled.
+    if (i == 2 || i == 4) {
+      simTime().advanceTimeAndRun(std::chrono::milliseconds(fill_one_token_in_ms), *dispatcher_,
+                                  Envoy::Event::Dispatcher::RunType::NonBlock);
+    }
+    // Send downstream client request to upstream.
+    sendClientRequest(&custom_headers);
+
+    // Only first downstream client request will trigger the reports to RLQS
+    // server as the subsequent requests will find the entry in the cache.
+    if (i == 0) {
+      // Start the gRPC stream to RLQS server.
+      ASSERT_TRUE(grpc_upstreams_[0]->waitForHttpConnection(*dispatcher_, rlqs_connection_));
+      ASSERT_TRUE(rlqs_connection_->waitForNewStream(*dispatcher_, rlqs_stream_));
+
+      envoy::service::rate_limit_quota::v3::RateLimitQuotaUsageReports reports;
+      ASSERT_TRUE(rlqs_stream_->waitForGrpcMessage(*dispatcher_, reports));
+      rlqs_stream_->startGrpcStream();
+
+      // Build the response.
+      envoy::service::rate_limit_quota::v3::RateLimitQuotaResponse rlqs_response;
+      absl::flat_hash_map<std::string, std::string> custom_headers_cpy = custom_headers;
+      custom_headers_cpy.insert({"name", "prod"});
+      auto* bucket_action = rlqs_response.add_bucket_action();
+      for (const auto& [key, value] : custom_headers_cpy) {
+        (*bucket_action->mutable_bucket_id()->mutable_bucket()).insert({key, value});
+        auto* quota_assignment = bucket_action->mutable_quota_assignment_action();
+        quota_assignment->mutable_assignment_time_to_live()->set_seconds(120);
+        auto* strategy = quota_assignment->mutable_rate_limit_strategy();
+        auto* token_bucket = strategy->mutable_token_bucket();
+        token_bucket->set_max_tokens(max_token);
+        token_bucket->mutable_tokens_per_fill()->set_value(30);
+        token_bucket->mutable_fill_interval()->set_seconds(60);
+      }
+
+      // Send the response from RLQS server.
+      rlqs_stream_->sendGrpcMessage(rlqs_response);
+    }
+
+    cleanUp();
+  }
+}
+
 } // namespace
 } // namespace RateLimitQuota
 } // namespace HttpFilters
