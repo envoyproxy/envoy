d1a8ed85d2912d1577c806909efa67781976abf8
Raven Black <ravenblack@dropbox.com>
2024-05-10 20:14:31 +0000

    More comments
    Signed-off-by: Raven Black <ravenblack@dropbox.com>

--- source/common/common/compiled_string_map.h
+++ source/common/common/compiled_string_map.h
@@ -17,6 +17,12 @@ namespace Envoy {
  * target key are considered.
  * 2. a trie that branches on the "most divisions" position of the key.
  *
+ * Using this structure is not recommended unless the table is
+ * initialize-once, use-many, as the "compile" operation is expensive.
+ *
+ * Unlike a regular trie, this structure cannot be used for prefix-based
+ * matching.
+ *
  * For example, if we consider the case where the set of headers is
  * `x-prefix-banana`
  * `x-prefix-babana`
@@ -59,12 +65,15 @@ public:
     return table_[key.size()](key);
   };
   /**
-   * Construct the lookup table. This is a somewhat slow multi-pass
-   * operation - using this structure is not recommended unless the
-   * table is initialize-once, use-many.
+   * Construct the lookup table. This can be a somewhat slow multi-pass
+   * operation if the input table is large.
    * @param initial a vector of key->value pairs. This is taken by value because
    *                we're going to modify it. If the caller still wants the original
    *                then it can be copied in, if not it can be moved in.
+   *                Note that the keys are string_views - the base string data must
+   *                exist for the duration of compile(). The leaf nodes take copies
+   *                of the key strings, so the string_views can be invalidated once
+   *                compile has completed.
    */
   void compile(std::vector<KV> initial) {
     if (initial.empty()) {
@@ -94,7 +103,8 @@ public:
 
 private:
   /*
-   * @param node_contents the set of key-value pairs that will
+   * @param node_contents the set of key-value pairs that will be children of
+   *                      this node.
    */
   static FindFn createEqualLengthNode(std::vector<KV> node_contents) {
     if (node_contents.size() == 1) {
