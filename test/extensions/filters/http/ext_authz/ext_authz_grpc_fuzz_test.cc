9b2725d0e47a2aeaf2253aaa5d027eb0f8ea4067
Antonio V. Leonti <53806445+antoniovleonti@users.noreply.github.com>
2024-05-13 15:19:58 -0400

    Add CheckResponse to ext_authz grpc fuzzer input (#34045)
    Signed-off-by: antoniovleonti <leonti@google.com>

--- test/extensions/filters/http/ext_authz/ext_authz_grpc_fuzz_test.cc
+++ test/extensions/filters/http/ext_authz/ext_authz_grpc_fuzz_test.cc
@@ -1,12 +1,17 @@
+#include <memory>
+
+#include "envoy/extensions/filters/http/ext_authz/v3/ext_authz.pb.validate.h"
 #include "envoy/grpc/status.h"
 #include "envoy/service/auth/v3/external_auth.pb.h"
 
 #include "source/common/common/assert.h"
-#include "source/extensions/filters/common/ext_authz/ext_authz_http_impl.h"
+#include "source/extensions/filters/common/ext_authz/ext_authz_grpc_impl.h"
+#include "source/extensions/filters/http/ext_authz/ext_authz.h"
 
 #include "test/extensions/filters/common/ext_authz/test_common.h"
 #include "test/extensions/filters/http/common/fuzz/http_filter_fuzzer.h"
 #include "test/extensions/filters/http/ext_authz/ext_authz_fuzz.pb.h"
+#include "test/extensions/filters/http/ext_authz/ext_authz_fuzz.pb.validate.h"
 #include "test/extensions/filters/http/ext_authz/ext_authz_fuzz_lib.h"
 #include "test/fuzz/fuzz_runner.h"
 #include "test/mocks/grpc/mocks.h"
@@ -14,37 +19,13 @@
 #include "gmock/gmock.h"
 
 using Envoy::Extensions::Filters::Common::ExtAuthz::TestCommon;
-using envoy::extensions::filters::http::ext_authz::ExtAuthzTestCase;
-using testing::Return;
+using envoy::extensions::filters::http::ext_authz::ExtAuthzTestCaseGrpc;
 
 namespace Envoy {
 namespace Extensions {
 namespace HttpFilters {
 namespace ExtAuthz {
 namespace {
-
-std::unique_ptr<envoy::service::auth::v3::CheckResponse>
-makeGrpcCheckResponse(const Grpc::Status::WellKnownGrpcStatus status) {
-  auto response = std::make_unique<envoy::service::auth::v3::CheckResponse>();
-  response->mutable_status()->set_code(status);
-  // TODO: We only add the response status.
-  // Add fuzzed inputs for headers_to_(set/append/add), body, status_code to the Response.
-  return response;
-}
-
-Grpc::Status::WellKnownGrpcStatus
-resultCaseToGrpcStatus(const ExtAuthzTestCase::AuthResult result) {
-  switch (result) {
-    PANIC_ON_PROTO_ENUM_SENTINEL_VALUES;
-  case ExtAuthzTestCase::OK:
-    return Grpc::Status::WellKnownGrpcStatus::Ok;
-  case ExtAuthzTestCase::ERROR:
-    return Grpc::Status::WellKnownGrpcStatus::Internal;
-  case ExtAuthzTestCase::DENIED:
-    return Grpc::Status::WellKnownGrpcStatus::PermissionDenied;
-  }
-  PANIC_DUE_TO_CORRUPT_ENUM;
-}
 
 class ReusableGrpcClientFactory {
 public:
@@ -59,25 +40,31 @@ public:
               EXPECT_TRUE(check_request.ParseFromString(serialized_req->toString()))
                   << "Could not parse serialized check request";
 
-              // TODO: Query the request header map in HttpFilterFuzzer to test
-              // headers_to_(add/remove/append).
-              // TODO: Test check request attributes against config
-              // and filter metadata.
-              ENVOY_LOG_MISC(trace, "Check Request attributes {}",
+              ENVOY_LOG_MISC(trace, "Check Request attributes:\n{}",
                              check_request.attributes().DebugString());
 
-              if (status_ == Grpc::Status::WellKnownGrpcStatus::Ok) {
-                grpc_client_->onSuccess(makeGrpcCheckResponse(status_), mock_span_);
+              if (failure_reason_) {
+                grpc_client_->onFailure(Envoy::Grpc::Status::WellKnownGrpcStatus::Unknown,
+                                        *failure_reason_, mock_span_);
               } else {
-                grpc_client_->onFailure(status_, "Fuzz input status was not ok!", mock_span_);
+                grpc_client_->onSuccess(std::move(response_), mock_span_);
               }
               return &grpc_async_request_;
             }));
   }
 
   std::unique_ptr<Filters::Common::ExtAuthz::GrpcClientImpl>
-  newGrpcClientImpl(const ExtAuthzTestCase::AuthResult result) {
-    status_ = resultCaseToGrpcStatus(result);
+  newGrpcClientImpl(const ExtAuthzTestCaseGrpc& input) {
+    if (input.has_failure_reason()) {
+      failure_reason_ = input.failure_reason();
+      response_ = nullptr;
+      ENVOY_LOG_MISC(trace, "Failure reason: {}", *failure_reason_);
+    } else {
+      failure_reason_ = std::nullopt;
+      response_ = std::make_unique<envoy::service::auth::v3::CheckResponse>(input.response());
+      ENVOY_LOG_MISC(trace, "Check Response:\n{}", response_->DebugString());
+    }
+
     grpc_client_ = new Filters::Common::ExtAuthz::GrpcClientImpl(internal_grpc_mock_client_,
                                                                  std::chrono::milliseconds(1000));
     return std::unique_ptr<Filters::Common::ExtAuthz::GrpcClientImpl>{grpc_client_};
@@ -88,24 +75,35 @@ private:
   Envoy::Tracing::MockSpan mock_span_;
   NiceMock<Grpc::MockAsyncRequest> grpc_async_request_;
 
-  // Set by calling newGrpcClientImpl
-  Grpc::Status::WellKnownGrpcStatus status_;
+  // Set by calling newGrpcClientImpl. Only one of response_ or failure_reason_ will be set.
+  std::unique_ptr<envoy::service::auth::v3::CheckResponse> response_;
+  absl::optional<std::string> failure_reason_;
   Filters::Common::ExtAuthz::GrpcClientImpl* grpc_client_;
 };
 
-DEFINE_PROTO_FUZZER(const ExtAuthzTestCase& input) {
+DEFINE_PROTO_FUZZER(ExtAuthzTestCaseGrpc& input) {
   static ReusableFuzzerUtil fuzzer_util;
   static ReusableGrpcClientFactory grpc_client_factory;
-  auto grpc_client = grpc_client_factory.newGrpcClientImpl(input.result());
-  absl::StatusOr<std::unique_ptr<Filter>> filter = fuzzer_util.setup(input, std::move(grpc_client));
+
+  try {
+    TestUtility::validate(input);
+  } catch (const EnvoyException& e) {
+    ENVOY_LOG_MISC(debug, "EnvoyException during validation: {}", e.what());
+    return;
+  }
+
+  auto grpc_client = grpc_client_factory.newGrpcClientImpl(input);
+  // Force this to be true so that the filter can handle invalid mutations gracefully.
+  input.mutable_base()->mutable_config()->set_validate_mutations(true);
+  absl::StatusOr<std::unique_ptr<Filter>> filter =
+      fuzzer_util.setup(input.base(), std::move(grpc_client));
   if (!filter.ok()) {
     return;
   }
 
-  // TODO: Add response headers.
   static Envoy::Extensions::HttpFilters::HttpFilterFuzzer fuzzer;
   fuzzer.runData(static_cast<Envoy::Http::StreamDecoderFilter*>(filter->get()),
-                 input.request_data());
+                 input.base().request_data());
 }
 
 } // namespace
