74d8fe740625468b3d3c5e5eb5a8b6a1d59c43d7
Willem Veerman <6502426+willemveerman@users.noreply.github.com>
2024-05-14 09:10:29 +0100

    fix test bug
    Signed-off-by: Willem Veerman <6502426+willemveerman@users.noreply.github.com>

--- contrib/golang/filters/http/test/test_data/basic/filter.go
+++ contrib/golang/filters/http/test/test_data/basic/filter.go
@@ -186,11 +186,12 @@ func (f *filter) decodeHeaders(header api.RequestHeaderMap, endStream bool) api.
 	f.all_headers = make(map[string][]string)
 
 	header.RangeWithCopy(func(key, value string) bool {
-		f.all_headers[key] = append(f.all_headers[key], val)
+		f.all_headers[key] = append(f.all_headers[key], value)
 		return true
 	})
 
 	header_map := header.GetAllHeaders()
+
 	if !reflect.DeepEqual(f.all_headers, header_map) {
 		return f.fail("GetAllHeaders returned incorrect data, expected:\n%v\n got:\n%v", f.all_headers, header_map)
 	}
