bf8a629606a7453457b07dc314e5f64078bcc3ea
danieldradware <117576776+danieldradware@users.noreply.github.com>
2024-05-21 12:27:42 +0300

    --sign-of danield removed runtime guard
    Signed-off-by: Daniel Danan <danield@radware.com>

--- source/extensions/filters/http/jwt_authn/extractor.cc
+++ source/extensions/filters/http/jwt_authn/extractor.cc
@@ -329,11 +329,7 @@ absl::string_view ExtractorImpl::extractJWT(absl::string_view value_str,
   }
 
   // There should be two dots (periods; 0x2e) inside the string, but we don't verify that here
-  auto ending = value_str.find_first_not_of(ConstantBase64UrlEncodingCharsPlusDot, starting);
-  if (ending == value_str.npos) { // Base64Url-encoded string occupies the rest of the line
-    return value_str.substr(starting);
-  }
-  return value_str.substr(starting, ending - starting);
+  return value_str.substr(starting);
 }
 
 void ExtractorImpl::sanitizeHeaders(Http::HeaderMap& headers) const {
