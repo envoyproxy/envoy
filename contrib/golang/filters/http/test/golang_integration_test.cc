7a5912fa87ffce2795c7c4bd65089a3e9bb9bb09
doujiang24 <doujiang24@gmail.com>
2024-05-26 15:33:22 +0800

    fix format
    Signed-off-by: doujiang24 <doujiang24@gmail.com>

--- contrib/golang/filters/http/test/golang_integration_test.cc
+++ contrib/golang/filters/http/test/golang_integration_test.cc
@@ -55,7 +55,7 @@ public:
     decoder_callbacks_ = &callbacks;
   }
 
-  void onDestroy() override {};
+  void onDestroy() override{};
   Http::StreamEncoderFilterCallbacks* decoder_callbacks_;
 };
 
