d19462ed2cddbaf23bc04648d2ac9c271b52c57e
zirain <zirain2009@gmail.com>
2024-05-27 10:32:27 +0800

    rename getTraceIdAsHex to getTraceId (#34314)
    * rename getTraceId
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    
    * implement getTraceId
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    
    * fix test
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    
    * fix test
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    
    ---------
    
    Signed-off-by: zirain <zirain2009@gmail.com>

--- source/extensions/tracers/opentelemetry/tracer.h
+++ source/extensions/tracers/opentelemetry/tracer.h
@@ -117,7 +117,7 @@ public:
     span_.set_trace_id(absl::HexStringToBytes(trace_id_hex));
   }
 
-  std::string getTraceIdAsHex() const override { return absl::BytesToHexString(span_.trace_id()); };
+  std::string getTraceId() const override { return absl::BytesToHexString(span_.trace_id()); };
 
   OTelSpanKind spankind() const { return span_.kind(); }
 
