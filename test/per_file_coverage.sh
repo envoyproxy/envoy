66cfd8aa66b67eb33a740624a785db625733c680
alyssawilk <alyssar@chromium.org>
2024-05-29 14:39:45 -0400

    coverage: adding details for passing coverage (#34389)
    * coverage: adding details for passing coverage
    * coverage: bumping and enhancing tooling
    
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- test/per_file_coverage.sh
+++ test/per_file_coverage.sh
@@ -6,39 +6,37 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/common:96.2"
 "source/common/api:84.5" # flaky due to posix: be careful adjusting
 "source/common/api/posix:83.8" # flaky (accept failover non-deterministic): be careful adjusting
-"source/common/common/posix:88.8" # No easy way to test pthread_create failure.
+"source/common/common/posix:89.4" # No easy way to test pthread_create failure.
 "source/common/config:95.6"
 "source/common/crypto:95.5"
 "source/common/event:95.1" # Emulated edge events guards don't report LCOV
 "source/common/filesystem/posix:96.2" # FileReadToEndNotReadable fails in some env; createPath can't test all failure branches.
-"source/common/http/http2:95.6"
+"source/common/http/http2:96.0"
 "source/common/json:94.6"
 "source/common/matcher:94.4"
 "source/common/memory:73.6" # tcmalloc code path is not enabled in coverage build, only gperf tcmalloc, see PR#32589
 "source/common/network:94.4" # Flaky, `activateFileEvents`, `startSecureTransport` and `ioctl`, listener_socket do not always report LCOV
 "source/common/network/dns_resolver:91.4"  # A few lines of MacOS code not tested in linux scripts. Tested in MacOS scripts
-"source/common/quic:93.5"
-"source/common/secret:95.1"
+"source/common/quic:93.6"
+"source/common/secret:95.4"
 "source/common/signal:87.2" # Death tests don't report LCOV
 "source/common/thread:0.0" # Death tests don't report LCOV
 "source/common/watchdog:58.6" # Death tests don't report LCOV
 "source/exe:94.0" # increased by #32346, need coverage for terminate_handler and hot restart failures
-"source/extensions/clusters/common:91.5" # This can be increased again once `#24903` lands
+"source/extensions/clusters/common:95.6"
 "source/extensions/common:93.0" #flaky: be careful adjusting
 "source/extensions/common/proxy_protocol:93.8" # Adjusted for security patch
-"source/extensions/common/tap:94.5"
+"source/extensions/common/tap:94.6"
 "source/extensions/common/wasm:88.0" # flaky: be careful adjusting
 "source/extensions/common/wasm/ext:92.0"
-"source/extensions/filters/common:96.5"
 "source/extensions/filters/common/fault:94.5"
-"source/extensions/filters/common/rbac:90.2"
-"source/extensions/filters/http/cache:95.0"
+"source/extensions/filters/common/rbac:90.7"
+"source/extensions/filters/http/cache:95.1"
 "source/extensions/filters/http/grpc_json_transcoder:93.8" # TODO(#28232)
 "source/extensions/filters/http/ip_tagging:88.0"
 "source/extensions/filters/http/kill_request:91.7" # Death tests don't report LCOV
 "source/extensions/filters/http/wasm:1.8"
 "source/extensions/filters/listener/original_src:92.1"
-"source/extensions/filters/network/common:96.4"
 "source/extensions/filters/network/mongo_proxy:96.1"
 "source/extensions/filters/network/sni_cluster:88.9"
 "source/extensions/filters/network/wasm:76.9"
@@ -47,7 +45,7 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/extensions/rate_limit_descriptors/expr:95.0"
 "source/extensions/stat_sinks/graphite_statsd:82.8" # Death tests don't report LCOV
 "source/extensions/stat_sinks/statsd:85.2" # Death tests don't report LCOV
-"source/extensions/tracers:96.4"
+"source/extensions/tracers:96.5"
 "source/extensions/tracers/common:74.8"
 "source/extensions/tracers/common/ot:72.9"
 "source/extensions/tracers/opencensus:93.9"
@@ -63,10 +61,10 @@ declare -a KNOWN_LOW_COVERAGE=(
 "source/extensions/listener_managers/validation_listener_manager:70.5"
 "source/extensions/watchdog/profile_action:83.3"
 "source/server:91.0" # flaky: be careful adjusting. See https://github.com/envoyproxy/envoy/issues/15239
-"source/server/config_validation:88.9"
-"source/extensions/health_checkers:96.0"
+"source/server/config_validation:91.3"
+"source/extensions/health_checkers:96.1"
 "source/extensions/health_checkers/http:93.9"
-"source/extensions/health_checkers/grpc:92.0"
+"source/extensions/health_checkers/grpc:92.1"
 "source/extensions/config_subscription/rest:94.7"
 "source/extensions/matching/input_matchers/cel_matcher:91.3" #Death tests don't report LCOV
 )
@@ -95,6 +93,7 @@ get_coverage_target() {
 # Make sure that for each directory with code, coverage doesn't dip
 # below the default coverage threshold.
 SOURCES=$(find source/* -type d)
+HIGH_COVERAGE_STRING=""
 while read -r DIRECTORY
 do
   get_coverage_target "$DIRECTORY"
@@ -115,6 +114,10 @@ do
     echo "Code coverage for ${DIRECTORY} is lower than limit of ${DIRECTORY_THRESHOLD} (${COVERAGE_VALUE})"
     FAILED=1
   fi
+  COVERAGE_HIGH=$(echo "${COVERAGE_VALUE}>${DIRECTORY_THRESHOLD}" | bc)
+  if [[ (${DIRECTORY_THRESHOLD} != "${DEFAULT_COVERAGE_THRESHOLD}") && "${COVERAGE_HIGH}" -eq 1 ]]; then
+    HIGH_COVERAGE_STRING+="\"${DIRECTORY}:${COVERAGE_VALUE} (${DIRECTORY_THRESHOLD})\"\n"
+  fi
   if [[ -n ${VERBOSE} && ${COVERAGE_VALUE} > ${DIRECTORY_THRESHOLD} ]]; then
     if [[ ${DIRECTORY_THRESHOLD} < $DEFAULT_COVERAGE_THRESHOLD ]]; then
       echo "Code coverage for ${DIRECTORY} is now ${COVERAGE_VALUE} (previously ${DIRECTORY_THRESHOLD})"
@@ -122,5 +125,9 @@ do
   fi
 
 done <<< "$SOURCES"
+
+if [[ ${FAILED} != 1 ]]; then
+  echo -e "Coverage in the following directories may be adjusted up:\n ${HIGH_COVERAGE_STRING}"
+fi
 
 exit $FAILED
