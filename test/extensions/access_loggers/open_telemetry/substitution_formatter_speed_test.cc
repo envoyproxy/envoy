da9414a3bb44974a29095b811154615205e3f7d9
zirain <zirain2009@gmail.com>
2024-06-02 03:09:30 +0800

    implement
    Signed-off-by: zirain <zirain2009@gmail.com>

--- test/extensions/access_loggers/open_telemetry/substitution_formatter_speed_test.cc
+++ test/extensions/access_loggers/open_telemetry/substitution_formatter_speed_test.cc
@@ -53,7 +53,7 @@ std::unique_ptr<OpenTelemetryFormatter> makeOpenTelemetryFormatter() {
           string_value: '%REQ(USER-AGENT)%'
   )EOF";
   TestUtility::loadFromYaml(format_yaml, otel_log_format);
-  return std::make_unique<OpenTelemetryFormatter>(otel_log_format);
+  return std::make_unique<OpenTelemetryFormatter>(otel_log_format, {});
 }
 
 std::unique_ptr<Envoy::TestStreamInfo> makeStreamInfo() {
