1b06924b4364bc691efcd3db45d795182643ff74
Loong <loong.dai@intel.com>
2024-06-11 14:10:04 +0800

    skip dlb test
    Signed-off-by: Loong <loong.dai@intel.com>

--- ci/verify_examples.sh
+++ ci/verify_examples.sh
@@ -11,6 +11,8 @@ WARNINGS=()
 # Sandboxes listed here should be regarded as broken(/untested) until whatever
 # is causing them to flake is resolved!!!
 FLAKY_SANDBOXES=(
+    # no device available in CI
+    dlb
     # https://github.com/envoyproxy/envoy/issues/28542
     double-proxy
     # https://github.com/envoyproxy/envoy/issues/31347
