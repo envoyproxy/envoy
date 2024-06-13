7e2746fa16a60e8a6ec28820cbf4d5188de9fd34
Renjie Tang <renjietang@google.com>
2024-06-13 13:18:50 -0700

    Shorten Envoy Mobile error details
    Signed-off-by: Renjie Tang <renjietang@google.com>

--- mobile/test/common/http/client_test.cc
+++ mobile/test/common/http/client_test.cc
@@ -477,8 +477,8 @@ TEST_P(ClientTest, EnvoyLocalError) {
   stream_callbacks.on_error_ = [&](const EnvoyError& error, envoy_stream_intel,
                                    envoy_final_stream_intel) -> void {
     EXPECT_EQ(error.error_code_, ENVOY_CONNECTION_FAILURE);
-    EXPECT_THAT(error.message_, Eq("RESPONSE_CODE: 503|ERROR_CODE: 2|RESPONSE_FLAGS: "
-                                   "4,26|PROTOCOL: 3|DETAILS: failed miserably"));
+    EXPECT_THAT(error.message_, Eq("envoy_rc: 503|envoy_ec: 2|rsp_flags: "
+                                   "4,26|protocol: 3|det: failed miserably"));
     EXPECT_EQ(error.attempt_count_, 123);
     callbacks_called.on_error_calls_++;
   };
--- mobile/test/java/org/chromium/net/CronetUrlRequestTest.java
+++ mobile/test/java/org/chromium/net/CronetUrlRequestTest.java
@@ -2058,26 +2058,26 @@ public class CronetUrlRequestTest {
     checkSpecificErrorCode(
         EnvoyMobileError.DNS_RESOLUTION_FAILED, NetError.ERR_NAME_NOT_RESOLVED,
         NetworkException.ERROR_HOSTNAME_NOT_RESOLVED, "NAME_NOT_RESOLVED", false,
-        /*error_details=*/"RESPONSE_CODE: 400|ERROR_CODE: 0|RESPONSE_FLAGS: 26|PROTOCOL: 1");
+        /*error_details=*/"envoy_rc: 400|envoy_ec: 0|resp_flags: 26|protocol: 1");
     checkSpecificErrorCode(
         EnvoyMobileError.UPSTREAM_CONNECTION_TERMINATION, NetError.ERR_CONNECTION_CLOSED,
         NetworkException.ERROR_CONNECTION_CLOSED, "CONNECTION_CLOSED", true,
-        /*error_details=*/"RESPONSE_CODE: 400|ERROR_CODE: 0|RESPONSE_FLAGS: 6|PROTOCOL: 1");
+        /*error_details=*/"envoy_rc: 400|envoy_ec: 0|resp_flags: 6|protocol: 1");
     checkSpecificErrorCode(
         EnvoyMobileError.UPSTREAM_CONNECTION_FAILURE, NetError.ERR_CONNECTION_REFUSED,
         NetworkException.ERROR_CONNECTION_REFUSED, "CONNECTION_REFUSED", false,
-        /*error_details=*/"RESPONSE_CODE: 400|ERROR_CODE: 0|RESPONSE_FLAGS: 5|PROTOCOL: 1");
+        /*error_details=*/"envoy_rc: 400|envoy_ec: 0|resp_flags: 5|protocol: 1");
     checkSpecificErrorCode(
         EnvoyMobileError.UPSTREAM_REMOTE_RESET, NetError.ERR_CONNECTION_RESET,
         NetworkException.ERROR_CONNECTION_RESET, "CONNECTION_RESET", true,
-        /*error_details=*/"RESPONSE_CODE: 400|ERROR_CODE: 0|RESPONSE_FLAGS: 4|PROTOCOL: 1");
+        /*error_details=*/"envoy_rc: 400|envoy_ec: 0|resp_flags: 4|protocol: 1");
     checkSpecificErrorCode(
         EnvoyMobileError.STREAM_IDLE_TIMEOUT, NetError.ERR_TIMED_OUT,
         NetworkException.ERROR_TIMED_OUT, "TIMED_OUT", true,
-        /*error_details=*/"RESPONSE_CODE: 400|ERROR_CODE: 0|RESPONSE_FLAGS: 16|PROTOCOL: 1");
+        /*error_details=*/"envoy_rc: 400|envoy_ec: 0|resp_flags: 16|protocol: 1");
     checkSpecificErrorCode(
         0x2000, NetError.ERR_OTHER, NetworkException.ERROR_OTHER, "OTHER", false,
-        /*error_details=*/"RESPONSE_CODE: 400|ERROR_CODE: 0|RESPONSE_FLAGS: 13|PROTOCOL: 1");
+        /*error_details=*/"envoy_rc: 400|envoy_ec: 0|resp_flags: 13|protocol: 1");
   }
 
   /*
@@ -2102,7 +2102,7 @@ public class CronetUrlRequestTest {
     checkSpecificErrorCode(
         EnvoyMobileError.DNS_RESOLUTION_FAILED, NetError.ERR_INTERNET_DISCONNECTED,
         NetworkException.ERROR_INTERNET_DISCONNECTED, "INTERNET_DISCONNECTED", false,
-        /*error_details=*/"RESPONSE_CODE: 400|ERROR_CODE: 0|RESPONSE_FLAGS: 26|PROTOCOL: 1");
+        /*error_details=*/"envoy_rc: 400|envoy_rc: 0|resp_flags: 26|protocol: 1");
 
     // bring back online since the AndroidNetworkMonitor class is a singleton
     connectivityManager.setActiveNetworkInfo(networkInfo);
@@ -2323,7 +2323,7 @@ public class CronetUrlRequestTest {
     checkSpecificErrorCode(
         EnvoyMobileError.UPSTREAM_CONNECTION_FAILURE, NetError.ERR_CONNECTION_REFUSED,
         NetworkException.ERROR_CONNECTION_REFUSED, "CONNECTION_REFUSED", false,
-        /*error_details=*/"RESPONSE_CODE: 400|ERROR_CODE: 0|RESPONSE_FLAGS: 5|PROTOCOL: 1");
+        /*error_details=*/"envoy_rc: 400|envoy_ec: 0|resp_flags: 5|protocol: 1");
   }
 
   @Test
