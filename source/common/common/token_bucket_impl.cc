d47a183b4758ad0e310890ea7835e572f0535703
wbpcode <wbphub@live.com>
2024-06-18 06:24:02 +0000

    token bucket: new atomic token bucket implementation
    Signed-off-by: wbpcode <wbphub@live.com>

--- source/common/common/token_bucket_impl.cc
+++ source/common/common/token_bucket_impl.cc
@@ -56,4 +56,95 @@ void TokenBucketImpl::maybeReset(uint64_t num_tokens) {
   last_fill_ = time_source_.monotonicTime();
 }
 
+AtomicTokenBucketImpl::AtomicTokenBucketImpl(uint64_t max_tokens, TimeSource& time_source,
+                                             double fill_rate)
+    : max_tokens_(max_tokens), fill_rate_(std::max(std::abs(fill_rate), kMinFillRate)),
+      total_tokens_(max_tokens), last_fill_(time_source.monotonicTime()),
+      time_source_(time_source) {}
+
+uint64_t AtomicTokenBucketImpl::consume(uint64_t tokens, bool allow_partial) {
+  bool expect_could_fill = true;
+  // Only one thread could fill the bucket at same time. No loop is needed here because one
+  // thread fills the bucket is enough.
+  if (could_fill_.compare_exchange_weak(expect_could_fill, false)) {
+    const MonotonicTime time_now = time_source_.monotonicTime();
+
+    // Fill the bucket by the CAS loop. Note double type number is used to store the total tokens
+    // because the fill_tokens could be a fraction.
+    const double fill_tokens =
+        (std::chrono::duration<double>(time_now - last_fill_).count() * fill_rate_);
+    double expected_tokens = total_tokens_.load(std::memory_order_relaxed);
+    do {
+    } while (!total_tokens_.compare_exchange_weak(
+        expected_tokens, std::min(max_tokens_, expected_tokens + fill_tokens)));
+
+    // Update the last fill time and set could_fill_ to true to allow other threads to fill the
+    // bucket if needed.
+    last_fill_ = time_now;
+    could_fill_ = true;
+  }
+
+  // Try to consume tokens.
+  double expected_tokens = total_tokens_.load(std::memory_order_relaxed);
+  uint64_t consume_tokens = tokens;
+  do {
+    // Convert expected total tokens to uint64_t because we can consume only integer tokens.
+    const uint64_t available_tokens = static_cast<uint64_t>(std::floor(expected_tokens));
+
+    // No available tokens to consume means the total tokens is less than 1. Return 0.
+    if (available_tokens == 0) {
+      return 0;
+    }
+
+    // No enough available tokens to consume. Return 0 if allow_partial is false.
+    // Update consume_tokens to available_tokens if allow_partial is true.
+    if (available_tokens < consume_tokens) {
+      if (allow_partial) {
+        consume_tokens = available_tokens;
+      } else {
+        return 0;
+      }
+    }
+
+    // consume_tokens <= available_tokens <= expected_tokens. So the subtraction will not
+    // overflow.
+    // Loop while the weak CAS fails trying to subtract consume_tokens from expected.
+  } while (!total_tokens_.compare_exchange_weak(
+      expected_tokens, static_cast<double>(expected_tokens - consume_tokens)));
+
+  return consume_tokens;
+}
+
+uint64_t AtomicTokenBucketImpl::consume(uint64_t tokens, bool allow_partial,
+                                        std::chrono::milliseconds& time_to_next_token) {
+  auto tokens_consumed = consume(tokens, allow_partial);
+  time_to_next_token = nextTokenAvailable();
+  return tokens_consumed;
+}
+
+std::chrono::milliseconds AtomicTokenBucketImpl::nextTokenAvailable() {
+  const double expected_tokens = total_tokens_.load(std::memory_order_relaxed);
+  if (expected_tokens >= 1) {
+    return std::chrono::milliseconds(0);
+  }
+  return std::chrono::milliseconds(static_cast<uint64_t>(std::ceil((1 / fill_rate_) * 1000)));
+}
+
+void AtomicTokenBucketImpl::maybeReset(uint64_t num_tokens) {
+  const double new_total_tokens = std::min(static_cast<double>(num_tokens), max_tokens_);
+
+  bool expect_could_fill = true;
+  while (true) {
+    if (could_fill_.compare_exchange_weak(expect_could_fill, false)) {
+      // The CAS is used in other cases because we need to make sure related operations are applied
+      // on the latest value or we need to check and change the value atomically.
+      // Bu we can set new value directly here because we don't care about the previous value.
+      total_tokens_ = new_total_tokens;
+      last_fill_ = time_source_.monotonicTime();
+      could_fill_ = true;
+      break;
+    }
+  }
+}
+
 } // namespace Envoy
--- source/common/common/token_bucket_impl.h
+++ source/common/common/token_bucket_impl.h
@@ -35,4 +35,40 @@ private:
   TimeSource& time_source_;
 };
 
+/**
+ * Atomic token bucket. This class is thread-safe.
+ */
+class AtomicTokenBucketImpl : public TokenBucket {
+public:
+  /**
+   * @param max_tokens supplies the maximum number of tokens in the bucket.
+   * @param time_source supplies the time source.
+   * @param fill_rate supplies the number of tokens that will return to the bucket on each second.
+   * The default is 1.
+   */
+  explicit AtomicTokenBucketImpl(uint64_t max_tokens, TimeSource& time_source,
+                                 double fill_rate = 1);
+
+  uint64_t consume(uint64_t tokens, bool allow_partial) override;
+  uint64_t consume(uint64_t tokens, bool allow_partial,
+                   std::chrono::milliseconds& time_to_next_token) override;
+  // The next token available time is calculated based on the fill rate and the number of tokens
+  // available in the bucket. But in the multi-threaded environment, the number of tokens available
+  // can change between the calculation and the actual consumption. So, the next token available
+  // time may not be accurate.
+  std::chrono::milliseconds nextTokenAvailable() override;
+  // Reset the number of tokens in the bucket to the specified number. But in the multi-threaded
+  // environment, the number of tokens available in the bucket can change by other mayReset calls.
+  // So, the final number of tokens in the bucket may not be the same as the specified number.
+  void maybeReset(uint64_t num_tokens) override;
+
+private:
+  const double max_tokens_{};
+  const double fill_rate_{};
+  std::atomic<double> total_tokens_{};
+  std::atomic<bool> could_fill_{true};
+  MonotonicTime last_fill_;
+  TimeSource& time_source_;
+};
+
 } // namespace Envoy
