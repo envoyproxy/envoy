c2c37bf7727e28589466deadc5cdcad8fc0624eb
tyxia <tyxia@google.com>
2024-06-18 04:05:05 +0000

    test
    Signed-off-by: tyxia <tyxia@google.com>

--- source/common/grpc/google_async_client_impl.h
+++ source/common/grpc/google_async_client_impl.h
@@ -238,10 +238,8 @@ public:
   const StreamInfo::StreamInfo& streamInfo() const override { return unused_stream_info_; }
 
   // Google-gRPC code doesn't use Envoy watermark buffers, so the functions below are not used.
-  void setWatermarkCallbacks(Http::DecoderFilterWatermarkCallbacks&) override {
-    PANIC("not implemented");
-  }
-  void removeWatermarkCallbacks() override { PANIC("not implemented"); }
+  void setWatermarkCallbacks(Http::DecoderFilterWatermarkCallbacks&) override {}
+  void removeWatermarkCallbacks() override {}
 
 protected:
   bool callFailed() const { return call_failed_; }
