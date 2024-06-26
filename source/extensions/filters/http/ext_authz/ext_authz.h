a96c4661a728aa5aa2dd36548c5bae6d5a7a3cf0
antoniovleonti <leonti@google.com>
2024-06-26 20:59:34 +0000

    ext_authz: filter state access logging stats PREP
    Signed-off-by: antoniovleonti <leonti@google.com>

--- source/extensions/filters/http/ext_authz/ext_authz.h
+++ source/extensions/filters/http/ext_authz/ext_authz.h
@@ -50,6 +50,35 @@ struct ExtAuthzFilterStats {
   ALL_EXT_AUTHZ_FILTER_STATS(GENERATE_COUNTER_STRUCT)
 };
 
+class ExtAuthzLoggingInfo : public Envoy::StreamInfo::FilterState::Object {
+public:
+  explicit ExtAuthzLoggingInfo(const Envoy::ProtobufWkt::Struct& filter_metadata,
+                               const std::chrono::microseconds latency,
+                               absl::optional<uint64_t> bytes_sent,
+                               absl::optional<uint64_t> bytes_received,
+                               Upstream::ClusterInfoConstSharedPtr cluster_info,
+                               Upstream::HostDescriptionConstSharedPtr upstream_host)
+      : filter_metadata_(filter_metadata), latency_(latency), bytes_sent_(bytes_sent),
+        bytes_received_(bytes_received), cluster_info_(std::move(cluster_info)),
+        upstream_host_(std::move(upstream_host)) {}
+
+  const ProtobufWkt::Struct& filterMetadata() const { return filter_metadata_; }
+  std::chrono::microseconds latency() const { return latency_; };
+  absl::optional<uint64_t> bytesSent() const { return bytes_sent_; }
+  absl::optional<uint64_t> bytesReceived() const { return bytes_received_; }
+  Upstream::ClusterInfoConstSharedPtr clusterInfo() const { return cluster_info_; }
+  Upstream::HostDescriptionConstSharedPtr upstreamHost() const { return upstream_host_; }
+
+private:
+  const Envoy::ProtobufWkt::Struct filter_metadata_;
+  const std::chrono::microseconds latency_;
+  // The following stats are populated for ext_authz filters using Envoy gRPC only.
+  absl::optional<uint64_t> bytes_sent_;
+  absl::optional<uint64_t> bytes_received_;
+  Upstream::ClusterInfoConstSharedPtr cluster_info_;
+  Upstream::HostDescriptionConstSharedPtr upstream_host_;
+};
+
 /**
  * Configuration for the External Authorization (ext_authz) filter.
  */
