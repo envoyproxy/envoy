5ecb4ce4d536535d621bddba4ecac5e3cade371d
Juan Manuel Ollé <jolle@mulesoft.com>
2024-06-26 22:04:12 -0300

    adding test case
    Signed-off-by: Juan Manuel Ollé <jolle@mulesoft.com>

--- test/extensions/tracers/opentelemetry/resource_detectors/static/static_config_resource_detector_test.cc
+++ test/extensions/tracers/opentelemetry/resource_detectors/static/static_config_resource_detector_test.cc
@@ -20,20 +20,6 @@ namespace Extensions {
 namespace Tracers {
 namespace OpenTelemetry {
 
-// Test detector when env variable is not present
-TEST(StaticConfigResourceDetectorTest, EnvVariableNotPresent) {
-  NiceMock<Server::Configuration::MockTracerFactoryContext> context;
-
-  envoy::extensions::tracers::opentelemetry::resource_detectors::v3::
-      StaticConfigResourceDetectorConfig config;
-
-  auto detector = std::make_unique<StaticConfigResourceDetector>(config, context);
-  Resource resource = detector->detect();
-
-  EXPECT_EQ(resource.schema_url_, "");
-  EXPECT_TRUE(resource.attributes_.empty());
-}
-
 // Test detector when when attributes is empty
 TEST(StaticConfigResourceDetectorTest, EmptyAttributesMap) {
   NiceMock<Server::Configuration::MockTracerFactoryContext> context;
@@ -46,6 +32,38 @@ TEST(StaticConfigResourceDetectorTest, EmptyAttributesMap) {
 
   EXPECT_EQ(resource.schema_url_, "");
   EXPECT_TRUE(resource.attributes_.empty());
+}
+
+// Test detector with valid values in attributes config
+TEST(StaticConfigResourceDetectorTest, EmptyAttributesAreIgnored) {
+  NiceMock<Server::Configuration::MockTracerFactoryContext> context;
+
+  ResourceAttributes expected_attributes = {{"key1", "val1"}};
+
+  Api::ApiPtr api = Api::createApiForTest();
+  EXPECT_CALL(context.server_factory_context_, api()).WillRepeatedly(ReturnRef(*api));
+
+  const std::string yaml = R"EOF(
+      attributes:
+        key1: val1
+        key2: ""
+  )EOF";
+  envoy::extensions::tracers::opentelemetry::resource_detectors::v3::
+      StaticConfigResourceDetectorConfig proto_config;
+  TestUtility::loadFromYamlAndValidate(yaml, proto_config);
+
+  auto detector = std::make_unique<StaticConfigResourceDetector>(proto_config, context);
+  Resource resource = detector->detect();
+
+  EXPECT_EQ(resource.schema_url_, "");
+  EXPECT_EQ(1, resource.attributes_.size());
+
+  for (auto& actual : resource.attributes_) {
+    auto expected = expected_attributes.find(actual.first);
+
+    EXPECT_TRUE(expected != expected_attributes.end());
+    EXPECT_EQ(expected->second, actual.second);
+  }
 }
 
 // Test detector with valid values in attributes config
