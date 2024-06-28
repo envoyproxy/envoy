b86a01ac44caec1c414492a5e236b254ff57c490
Neal Soni <Nealsoni00@gmail.com>
2024-06-28 09:19:16 -0400

    [HF] ensure 104 informational responses
    Signed-off-by: Neal Soni <Nealsoni00@gmail.com>

--- test/common/http/http1/codec_impl_test.cc
+++ test/common/http/http1/codec_impl_test.cc
@@ -2922,14 +2922,22 @@ TEST_P(Http1ClientConnectionImplTest, UploadResumptionSupportedHeaders) {
   TestRequestHeaderMapImpl headers{{":method", "GET"}, {":path", "/"}, {":authority", "host"}};
   EXPECT_TRUE(request_encoder.encodeHeaders(headers, true).ok());
 
-  EXPECT_CALL(response_decoder, decode1xxHeaders_(_));
+  // As per Resumable Uploads for HTTP draft, the upload-draft-interop-version header
+  // must be passed through with 104 responses.
+  EXPECT_CALL(response_decoder, decode1xxHeaders_(_))
+      .WillOnce(Invoke([&](ResponseHeaderMapPtr& headers) {
+        EXPECT_EQ(headers->get(Http::LowerCaseString("upload-draft-interop-version")).size(), 1);
+        EXPECT_EQ(headers->get(Http::LowerCaseString("upload-draft-interop-version"))[0]->value().getStringView(), "4");
+      }));
   EXPECT_CALL(response_decoder, decodeData(_, _)).Times(0);
-  Buffer::OwnedImpl initial_response("HTTP/1.1 104 Upload Resumption Supported\r\n\r\n");
+
+  Buffer::OwnedImpl initial_response("HTTP/1.1 104 Upload Resumption Supported\r\nUpload-Draft-Interop-Version: 4\r\n\r\n");
   auto status = codec_->dispatch(initial_response);
   EXPECT_TRUE(status.ok());
 
   EXPECT_CALL(response_decoder, decodeHeaders_(_, false));
   EXPECT_CALL(response_decoder, decodeData(_, _)).Times(0);
+
   Buffer::OwnedImpl response("HTTP/1.1 200 OK\r\n\r\n");
   status = codec_->dispatch(response);
   EXPECT_TRUE(status.ok());
