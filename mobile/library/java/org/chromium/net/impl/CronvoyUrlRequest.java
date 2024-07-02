28b6d58e40af0fe34058ffbd4bc6a65e3d2dc2bb
Renjie Tang <renjietang@google.com>
2024-07-02 12:54:48 -0700

    fix build
    Signed-off-by: Renjie Tang <renjietang@google.com>

--- mobile/library/java/org/chromium/net/impl/CronvoyUrlRequest.java
+++ mobile/library/java/org/chromium/net/impl/CronvoyUrlRequest.java
@@ -778,7 +778,7 @@ public final class CronvoyUrlRequest extends CronvoyUrlRequestBase {
       List<String> statuses = headers.get(":status");
       final int responseCode =
           statuses != null && !statuses.isEmpty() ? Integer.valueOf(statuses.get(0)) : -1;
-      final String locationField = null;
+      String locationField = null;
       if (responseCode >= 300 && responseCode < 400) {
         setUrlResponseInfo(headers, responseCode);
         List<String> locationFields = mUrlResponseInfo.getAllHeaders().get("location");
