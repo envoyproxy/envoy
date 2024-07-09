f778183400a0701997a2aae2ddd7b72c808e36a8
Alyssa Wilk <alyssar@chromium.org>
2024-07-09 15:00:59 +0000

    ci: test
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- source/common/http/conn_pool_grid.cc
+++ source/common/http/conn_pool_grid.cc
@@ -79,13 +79,14 @@ void ConnectivityGrid::WrapperCallbacks::ConnectionAttemptCallbacks::onPoolFailu
   parent_.onConnectionAttemptFailed(this, reason, transport_failure_reason, host);
 }
 
-void ConnectivityGrid::WrapperCallbacks::attemptSecondHttp3Connection() {
+ConnectivityGrid::StreamCreationResult
+ConnectivityGrid::WrapperCallbacks::attemptSecondHttp3Connection() {
   has_tried_http3_alternate_address_ = true;
   auto attempt =
       std::make_unique<ConnectionAttemptCallbacks>(*this, *grid_.getOrCreateHttp3AlternativePool());
   LinkedList::moveIntoList(std::move(attempt), connection_attempts_);
   // Kick off a new stream attempt.
-  connection_attempts_.front()->newStream();
+  return connection_attempts_.front()->newStream();
 }
 
 bool ConnectivityGrid::WrapperCallbacks::shouldAttemptSecondHttp3Connection() {
@@ -100,6 +101,11 @@ bool ConnectivityGrid::WrapperCallbacks::shouldAttemptSecondHttp3Connection() {
   }
   // Branch on reloadable flags.
   if (!Runtime::runtimeFeatureEnabled("envoy.reloadable_features.http3_happy_eyeballs")) {
+    return false;
+  }
+  // Make sure the original caller hasn't been called. This shouldn't be
+  // possible but is a helpful sanity check.
+  if (!inner_callbacks_) {
     return false;
   }
   // QUIC "happy eyeballs" currently only handles one v4 and one v6 address. If
@@ -391,10 +397,18 @@ ConnectionPool::Cancellable* ConnectivityGrid::newStream(Http::ResponseDecoder& 
   ConnectionPool::Instance* pool = getOrCreateHttp3Pool();
   Instance::StreamOptions overriding_options = options;
   bool delay_tcp_attempt = true;
+  bool delay_alternate_http3_attempt = true;
   if (shouldAttemptHttp3() && options.can_use_http3_) {
     if (getHttp3StatusTracker().hasHttp3FailedRecently()) {
       overriding_options.can_send_early_data_ = false;
       delay_tcp_attempt = false;
+    }
+    if (http3_pool_ && http3_alternate_pool_ && !http3_pool_->hasActiveConnections() &&
+        http3_alternate_pool_->hasActiveConnections()) {
+      // If it looks like the main HTTP/3 pool is not functional and the
+      // alternate works, don't wait 300ms before attempting to use the
+      // alternate pool.
+      delay_alternate_http3_attempt = false;
     }
   } else {
     pool = getOrCreateHttp2Pool();
@@ -408,6 +422,11 @@ ConnectionPool::Cancellable* ConnectivityGrid::newStream(Http::ResponseDecoder& 
     // callback and does not need a cancellable handle. At this point the
     // WrappedCallbacks object is queued to be deleted.
     return nullptr;
+  }
+  if (!delay_alternate_http3_attempt && ret->shouldAttemptSecondHttp3Connection()) {
+    if (ret->attemptSecondHttp3Connection() == StreamCreationResult::ImmediateResult) {
+      return nullptr;
+    }
   }
   if (!delay_tcp_attempt) {
     // Immediately start TCP attempt if HTTP/3 failed recently.
