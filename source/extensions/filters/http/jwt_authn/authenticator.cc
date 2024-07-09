6905f040673665564d559a433c10dd31fd3cbe5b
Kuat Yessenov <kuat@google.com>
2024-07-09 18:37:25 +0000

    jwt: fix clear route cache with remote JWKs
    Change-Id: If54c7143ecb1bf936e88fbb3371d9c47f6d8f671
    Signed-off-by: Kuat Yessenov <kuat@google.com>
    
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- source/extensions/filters/http/jwt_authn/authenticator.cc
+++ source/extensions/filters/http/jwt_authn/authenticator.cc
@@ -460,6 +460,11 @@ void AuthenticatorImpl::doneWithStatus(const Status& status) {
   // Unless allowing failed or missing, all tokens must be verified successfully.
   if ((Status::Ok != status && !is_allow_failed_ && !is_allow_missing_) || tokens_.empty()) {
     tokens_.clear();
+    if (clear_route_cache_ && clear_route_cb_) {
+      clear_route_cb_();
+    }
+    clear_route_cb_ = nullptr;
+
     if (is_allow_failed_) {
       callback_(Status::Ok);
     } else if (is_allow_missing_ && status == Status::JwtMissed) {
@@ -468,11 +473,6 @@ void AuthenticatorImpl::doneWithStatus(const Status& status) {
       callback_(status);
     }
     callback_ = nullptr;
-
-    if (clear_route_cache_ && clear_route_cb_) {
-      clear_route_cb_();
-    }
-    clear_route_cb_ = nullptr;
 
     return;
   }
