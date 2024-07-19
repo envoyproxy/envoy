ec18c8477cafa72e0fb115e411f27178e894c157
antoniovleonti <leonti@google.com>
2024-07-19 11:44:53 +0000

    inline false in admin.h
    Signed-off-by: antoniovleonti <leonti@google.com>

--- source/server/admin/admin.h
+++ source/server/admin/admin.h
@@ -148,7 +148,7 @@ public:
     return max_connection_duration_;
   }
   bool http1SafeMaxConnectionDuration() const override {
-    return http1_safe_max_connection_duration_;
+    return false;
   }
   uint32_t maxRequestHeadersKb() const override { return max_request_headers_kb_; }
   uint32_t maxRequestHeadersCount() const override { return max_request_headers_count_; }
@@ -480,7 +480,6 @@ private:
   const uint32_t max_request_headers_count_{Http::DEFAULT_MAX_HEADERS_COUNT};
   absl::optional<std::chrono::milliseconds> idle_timeout_;
   absl::optional<std::chrono::milliseconds> max_connection_duration_;
-  bool http1_safe_max_connection_duration_{false};
   absl::optional<std::chrono::milliseconds> max_stream_duration_;
   absl::optional<std::string> user_agent_;
   Http::SlowDateProviderImpl date_provider_;
