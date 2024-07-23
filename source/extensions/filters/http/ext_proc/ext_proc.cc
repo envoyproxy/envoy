850bac4d1d74d599598c41d55ec74b7c24110e49
Yanjun Xiang <yanjunxiang@google.com>
2024-07-23 21:20:51 +0000

    address comments
    Signed-off-by: Yanjun Xiang <yanjunxiang@google.com>

--- source/extensions/filters/http/ext_proc/ext_proc.cc
+++ source/extensions/filters/http/ext_proc/ext_proc.cc
@@ -1248,10 +1248,9 @@ void Filter::sendImmediateResponse(const ImmediateResponse& response) {
           : absl::nullopt;
   const auto mutate_headers = [this, &response](Http::ResponseHeaderMap& headers) {
     if (response.has_headers()) {
-      absl::Status mut_status;
-      mut_status = MutationUtils::applyHeaderMutations(response.headers(), headers, false,
-                                                       config().mutationChecker(),
-                                                       stats_.rejected_header_mutations_);
+      const absl::Status mut_status = MutationUtils::applyHeaderMutations(
+          response.headers(), headers, false, config().mutationChecker(),
+          stats_.rejected_header_mutations_);
       if (!mut_status.ok()) {
         ENVOY_LOG_EVERY_POW_2(error, "Immediate response mutations failed with {}",
                               mut_status.message());
