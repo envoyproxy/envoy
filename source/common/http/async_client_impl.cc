5cb29189a4342075acec0944d08807ea6b89d14d
Boteng Yao <boteng@google.com>
2024-07-24 21:48:19 +0000

    fix local reply in async client and destroy order
    Signed-off-by: Boteng Yao <boteng@google.com>
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- source/common/http/async_client_impl.cc
+++ source/common/http/async_client_impl.cc
@@ -9,6 +9,7 @@
 #include "source/common/grpc/common.h"
 #include "source/common/http/null_route_impl.h"
 #include "source/common/http/utility.h"
+#include "source/common/local_reply/local_reply.h"
 #include "source/common/protobuf/message_validator_impl.h"
 #include "source/common/stream_info/filter_state_impl.h"
 #include "source/common/tracing/http_tracer_impl.h"
@@ -32,7 +33,8 @@ AsyncClientImpl::AsyncClientImpl(Upstream::ClusterInfoConstSharedPtr cluster,
           factory_context.api().randomGenerator(), std::move(shadow_writer), true, false, false,
           false, false, false, Protobuf::RepeatedPtrField<std::string>{}, dispatcher.timeSource(),
           http_context, router_context)),
-      dispatcher_(dispatcher), runtime_(factory_context.runtime()) {}
+      dispatcher_(dispatcher), runtime_(factory_context.runtime()),
+      local_reply_(LocalReply::Factory::createDefault()) {}
 
 AsyncClientImpl::~AsyncClientImpl() {
   while (!active_streams_.empty()) {
@@ -120,7 +122,7 @@ AsyncStreamImpl::AsyncStreamImpl(AsyncClientImpl& parent, AsyncClient::StreamCal
                        ? options.filter_state
                        : std::make_shared<StreamInfo::FilterStateImpl>(
                              StreamInfo::FilterState::LifeSpan::FilterChain)),
-      tracing_config_(Tracing::EgressConfig::get()),
+      tracing_config_(Tracing::EgressConfig::get()), local_reply_(*parent.local_reply_),
       retry_policy_(createRetryPolicy(parent, options, parent_.factory_context_, creation_status)),
       route_(std::make_shared<NullRouteImpl>(
           parent_.cluster_->name(),
@@ -141,6 +143,35 @@ AsyncStreamImpl::AsyncStreamImpl(AsyncClientImpl& parent, AsyncClient::StreamCal
   // TODO(mattklein123): Correctly set protocol in stream info when we support access logging.
 }
 
+void AsyncStreamImpl::sendLocalReply(Code code, absl::string_view body,
+                                     std::function<void(ResponseHeaderMap& headers)> modify_headers,
+                                     const absl::optional<Grpc::Status::GrpcStatus> grpc_status,
+                                     absl::string_view details) {
+  if (encoded_response_headers_) {
+    resetStream();
+    return;
+  }
+  Utility::sendLocalReply(
+      remote_closed_,
+      Utility::EncodeFunctions{
+          [modify_headers](ResponseHeaderMap& headers) -> void {
+            if (modify_headers != nullptr) {
+              modify_headers(headers);
+            }
+          },
+          [this](ResponseHeaderMap& response_headers, Code& code, std::string& body,
+                 absl::string_view& content_type) -> void {
+            local_reply_.rewrite(request_headers_, response_headers, stream_info_, code, body,
+                                 content_type);
+          },
+          [this, &details](ResponseHeaderMapPtr&& headers, bool end_stream) -> void {
+            encodeHeaders(std::move(headers), end_stream, details);
+          },
+          [this](Buffer::Instance& data, bool end_stream) -> void {
+            encodeData(data, end_stream);
+          }},
+      Utility::LocalReplyData{is_grpc_request_, code, body, grpc_status, is_head_request_});
+}
 void AsyncStreamImpl::encodeHeaders(ResponseHeaderMapPtr&& headers, bool end_stream,
                                     absl::string_view) {
   ENVOY_LOG(debug, "async http request response headers (end_stream={}):\n{}", end_stream,
@@ -283,8 +314,15 @@ void AsyncStreamImpl::closeRemote(bool end_stream) {
 }
 
 void AsyncStreamImpl::reset() {
-  router_.onDestroy();
+  routerDestroy();
   resetStream();
+}
+
+void AsyncStreamImpl::routerDestroy() {
+  if (!router_destroyed_) {
+    router_destroyed_ = true;
+    router_.onDestroy();
+  }
 }
 
 void AsyncStreamImpl::cleanup() {
@@ -293,6 +331,7 @@ void AsyncStreamImpl::cleanup() {
   // This will destroy us, but only do so if we are actually in a list. This does not happen in
   // the immediate failure case.
   if (inserted()) {
+    routerDestroy();
     dispatcher().deferredDelete(removeFromList(parent_.active_streams_));
   }
 }
