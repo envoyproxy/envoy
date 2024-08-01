e1b839e170be78e159a1613f032c4e3798d8b1eb
Alyssa Wilk <alyssar@chromium.org>
2024-08-01 18:43:25 +0000

    comments
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- mobile/test/java/org/chromium/net/CronetHttp3Test.java
+++ mobile/test/java/org/chromium/net/CronetHttp3Test.java
@@ -28,9 +28,6 @@ import io.envoyproxy.envoymobile.engine.testing.HttpTestServerFactory;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Collections;
-import java.io.File;
-import java.nio.file.Paths;
-import java.nio.file.Files;
 
 /**
  * Test CronetEngine with production HTTP/3 logic
@@ -45,17 +42,15 @@ public class CronetHttp3Test {
 
   // If true, dump envoy logs on test completion.
   // Ideally we could override this from the command line but that's TBD.
-  boolean mPrintEnvoyLogs = false;
+  private boolean printEnvoyLogs = false;
   // The HTTP/2 server, set up to alt-svc to the HTTP/3 server
-  private HttpTestServerFactory.HttpTestServer mHttp2TestServer;
+  private HttpTestServerFactory.HttpTestServer http2TestServer;
   // The HTTP/3 server
-  private HttpTestServerFactory.HttpTestServer mHttp3TestServer;
-  // An optional CronvoyLogger, set up if mPrintEnvoyLogs is true.
-  private CronvoyLogger mLogger;
-  // The log file name,  set up if mPrintEnvoyLogs is true.
-  private String mLogFilename;
+  private HttpTestServerFactory.HttpTestServer http3TestServer;
+  // An optional CronvoyLogger, set up if printEnvoyLogs is true.
+  private CronvoyLogger logger;
   // The engine for this test.
-  CronvoyUrlRequestContext mCronvoyEngine;
+  private CronvoyUrlRequestContext cronvoyEngine;
 
   @BeforeClass
   public static void loadJniLibrary() {
@@ -65,44 +60,37 @@ public class CronetHttp3Test {
   public void setUp(boolean setUpLogging) throws Exception {
     // Set up the HTTP/3 server
     Map<String, String> headers = new HashMap<>();
-    mHttp3TestServer = HttpTestServerFactory.start(HttpTestServerFactory.Type.HTTP3, 0, headers,
-                                                   "This is a simple text file served by QUIC.\n",
-                                                   Collections.emptyMap());
+    http3TestServer = HttpTestServerFactory.start(HttpTestServerFactory.Type.HTTP3, 0, headers,
+                                                  "This is a simple text file served by QUIC.\n",
+                                                  Collections.emptyMap());
     // Next set up the HTTP/2 server, advertising HTTP/3 support for the HTTP/3 server
-    String alt_svc = "h3=\":" + mHttp3TestServer.getPort() + "\"; ma=86400";
-    headers.put("alt-svc", alt_svc);
+    String altSvc = "h3=\":" + http3TestServer.getPort() + "\"; ma=86400";
+    headers.put("alt-svc", altSvc);
     // Note that the HTTP/2 server must start on the same port as Envoy currently does not accept
     // alt-svc with differing ports. This may cause problems if this UDP port is in use at which
     // point listening on 127.0.0.N where N!=1 may improve flakiness.
-    mHttp2TestServer = HttpTestServerFactory.start(
-        HttpTestServerFactory.Type.HTTP2_WITH_TLS, mHttp3TestServer.getPort(), headers,
+    http2TestServer = HttpTestServerFactory.start(
+        HttpTestServerFactory.Type.HTTP2_WITH_TLS, http3TestServer.getPort(), headers,
         "This is a simple text file served by QUIC.\n", Collections.emptyMap());
 
     // Optionally, set up logging. This will slow down the tests a bit but make debugging much
     // easier.
     if (setUpLogging) {
-      File file = File.createTempFile("some-prefix", "file-ext");
-      file.deleteOnExit();
-      mLogFilename = file.getAbsolutePath() + "foo"; // Pick a path that doesn't exist.
-      mLogger = new CronvoyLogger();
-      mLogger.setNetLogToFile(mLogFilename);
+      logger = new CronvoyLogger() {
+        @Override
+        public void log(int logLevel, String message) {
+          System.out.print(message);
+        }
+      };
     }
   }
 
   @After
   public void tearDown() throws Exception {
-    // If logging is on, dump Envoy logs to System.out
-    if (mPrintEnvoyLogs) {
-      mLogger.stopLogging();
-      byte[] bytes = Files.readAllBytes(Paths.get(mLogFilename));
-      String fileContent = new String(bytes);
-      System.out.println(fileContent);
-    }
-
     // Shut down Envoy and the test servers.
-    mCronvoyEngine.shutdown();
-    mHttp2TestServer.shutdown();
-    mHttp3TestServer.shutdown();
+    cronvoyEngine.shutdown();
+    http2TestServer.shutdown();
+    http3TestServer.shutdown();
   }
 
   @Test
@@ -110,37 +98,37 @@ public class CronetHttp3Test {
   @Feature({"Cronet"})
   public void testInitEngineAndStartRequest() throws Exception {
     // Ideally we could override this from the command line but that's TBD.
-    setUp(mPrintEnvoyLogs);
+    setUp(printEnvoyLogs);
 
     // Set up the Envoy engine.
     NativeCronvoyEngineBuilderImpl nativeCronetEngineBuilder =
         new NativeCronvoyEngineBuilderImpl(ApplicationProvider.getApplicationContext());
-    if (mPrintEnvoyLogs) {
-      nativeCronetEngineBuilder.setLogger(mLogger);
+    if (printEnvoyLogs) {
+      nativeCronetEngineBuilder.setLogger(logger);
       nativeCronetEngineBuilder.setLogLevel(EnvoyEngine.LogLevel.TRACE);
     }
     // Make sure the handshake will work despite lack of real certs.
     nativeCronetEngineBuilder.setMockCertVerifierForTesting();
-    mCronvoyEngine = new CronvoyUrlRequestContext(nativeCronetEngineBuilder);
+    cronvoyEngine = new CronvoyUrlRequestContext(nativeCronetEngineBuilder);
 
     // Do a request to https://127.0.0.1:test_server_port/
-    TestUrlRequestCallback callback = new TestUrlRequestCallback();
-    String mNewUrl = "https://" + mHttp2TestServer.getAddress() + "/";
+    TestUrlRequestCallback callback1 = new TestUrlRequestCallback();
+    String newUrl = "https://" + http2TestServer.getAddress() + "/";
     UrlRequest.Builder urlRequestBuilder =
-        mCronvoyEngine.newUrlRequestBuilder(mNewUrl, callback, callback.getExecutor());
+        cronvoyEngine.newUrlRequestBuilder(newUrl, callback1, callback1.getExecutor());
     urlRequestBuilder.build().start();
-    callback.blockForDone();
+    callback1.blockForDone();
 
     // Make sure the request succeeded. It should go out over HTTP/2 as it's the first
     // request and HTTP/3 support is not established.
-    assertEquals(200, callback.mResponseInfo.getHttpStatusCode());
-    assertEquals("h2", callback.mResponseInfo.getNegotiatedProtocol());
+    assertEquals(200, callback1.mResponseInfo.getHttpStatusCode());
+    assertEquals("h2", callback1.mResponseInfo.getNegotiatedProtocol());
 
     // Set up a second request, which will hopefully go out over HTTP/3 due to alt-svc
     // advertisement.
     TestUrlRequestCallback callback2 = new TestUrlRequestCallback();
     UrlRequest.Builder urlRequestBuilder2 =
-        mCronvoyEngine.newUrlRequestBuilder(mNewUrl, callback2, callback2.getExecutor());
+        cronvoyEngine.newUrlRequestBuilder(newUrl, callback2, callback2.getExecutor());
     urlRequestBuilder2.build().start();
     callback2.blockForDone();
 
