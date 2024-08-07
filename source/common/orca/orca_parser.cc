6c06e24d94f6dfd9dfbb917325556420779e15e4
blake-snyder <blakesnyder@google.com>
2024-08-07 20:59:54 +0000

    Address nits.
    Signed-off-by: blake-snyder <blakesnyder@google.com>

--- source/common/orca/orca_parser.cc
+++ source/common/orca/orca_parser.cc
@@ -10,18 +10,24 @@
 #include "absl/strings/string_view.h"
 
 using ::Envoy::Http::HeaderMap;
-using ::Envoy::Http::LowerCaseString;
 using xds::data::orca::v3::OrcaLoadReport;
 
 namespace Envoy {
 namespace Orca {
 
+namespace {
+
+const Http::LowerCaseString& endpointLoadMetricsHeaderBin() {
+  CONSTRUCT_ON_FIRST_USE(Http::LowerCaseString, kEndpointLoadMetricsHeaderBin);
+}
+
+} // namespace
+
 absl::StatusOr<OrcaLoadReport> parseOrcaLoadReportHeaders(const HeaderMap& headers) {
   OrcaLoadReport load_report;
 
   // Binary protobuf format.
-  if (const auto header_bin = headers.get(LowerCaseString(kEndpointLoadMetricsHeaderBin));
-      !header_bin.empty()) {
+  if (const auto header_bin = headers.get(endpointLoadMetricsHeaderBin()); !header_bin.empty()) {
     const auto header_value = header_bin[0]->value().getStringView();
     const std::string decoded_value = Envoy::Base64::decode(header_value);
     if (!load_report.ParseFromString(decoded_value)) {
