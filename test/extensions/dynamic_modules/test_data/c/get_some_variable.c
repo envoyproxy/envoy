03cc448597f4552d2190f7c996c8a7998552fda5
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-08-07 20:35:08 +0000

    unnecessary
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- test/extensions/dynamic_modules/test_data/c/get_some_variable.c
+++ test/extensions/dynamic_modules/test_data/c/get_some_variable.c
@@ -1,7 +1,3 @@
-#include "source/extensions/dynamic_modules/abi.h"
-
-envoy_dynamic_module_type_program_init_result envoy_dynamic_module_on_program_init() { return 0; }
-
 int getSomeVariable() {
   static int some_variable = 0;
   some_variable++;
