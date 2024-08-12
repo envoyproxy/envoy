5e738812c5ac325d40a8576a38f112e783e28874
tyxia <tyxia@google.com>
2024-08-12 22:20:48 +0000

    add log
    Signed-off-by: tyxia <tyxia@google.com>

--- source/extensions/filters/http/rate_limit_quota/client_impl.cc
+++ source/extensions/filters/http/rate_limit_quota/client_impl.cc
@@ -66,12 +66,16 @@ RateLimitQuotaUsageReports RateLimitClientImpl::buildReport(absl::optional<size_
 void RateLimitClientImpl::sendUsageReport(absl::optional<size_t> bucket_id) {
   // Don't send any reports if stream has already been closed.
   if (stream_closed_) {
+    ENVOY_LOG(debug, "The stream has already been closed; no reports will be sent.");
     return;
   }
+
   ASSERT(stream_ != nullptr);
-  // Build the report and then send the report to RLQS server.
-  // `end_stream` should always be set to false as we don't want to close the stream locally.
-  stream_->sendMessage(buildReport(bucket_id), /*end_stream=*/false);
+  if (stream_ != nullptr) {
+    // Build the report and then send the report to RLQS server.
+    // `end_stream` should always be set to false as we don't want to close the stream locally.
+    stream_->sendMessage(buildReport(bucket_id), /*end_stream=*/false);
+  }
 }
 
 void RateLimitClientImpl::onReceiveMessage(RateLimitQuotaResponsePtr&& response) {
