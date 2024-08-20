8280dfd2b6ba91b4fc1dc2ce78037d0000131ffb
Ting Pan <panting@google.com>
2024-08-20 10:23:14 -0400

    cluster: flip envoy_reloadable_features_use_config_in_happy_eyeballs to true (#35697)
    Commit Message: flip
    envoy_reloadable_features_use_config_in_happy_eyeballs to true
    Additional Description: This flag has been flipped to true internally
    since March
    Risk Level: low
    Testing: tested internally for 4 month
    Docs Changes:
    Release Notes: Make flag
    envoy_reloadable_features_use_config_in_happy_eyeballs default to true
    
    ---------
    
    Signed-off-by: Ting Pan <panting@google.com>
    Signed-off-by: sunaydagli <sunaydagli@google.com>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -90,6 +90,7 @@ RUNTIME_GUARD(envoy_reloadable_features_test_feature_true);
 RUNTIME_GUARD(envoy_reloadable_features_udp_socket_apply_aggregated_read_limit);
 RUNTIME_GUARD(envoy_reloadable_features_uhv_allow_malformed_url_encoding);
 RUNTIME_GUARD(envoy_reloadable_features_upstream_remote_address_use_connection);
+RUNTIME_GUARD(envoy_reloadable_features_use_config_in_happy_eyeballs);
 RUNTIME_GUARD(envoy_reloadable_features_use_http3_header_normalisation);
 RUNTIME_GUARD(envoy_reloadable_features_use_route_host_mutation_for_auto_sni_san);
 RUNTIME_GUARD(envoy_reloadable_features_use_typed_metadata_in_proxy_protocol_listener);
@@ -136,8 +137,6 @@ FALSE_RUNTIME_GUARD(envoy_reloadable_features_use_http_client_to_fetch_aws_crede
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_enable_universal_header_validator);
 // TODO(pksohn): enable after canarying fix for https://github.com/envoyproxy/envoy/issues/29930
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_quic_defer_logging_to_ack_listener);
-// TODO(panting): flip this to true after some test time.
-FALSE_RUNTIME_GUARD(envoy_reloadable_features_use_config_in_happy_eyeballs);
 // TODO(#33474) removed it once GRO packet dropping is fixed.
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_prefer_quic_client_udp_gro);
 // TODO(alyssar) evaluate and either make this a config knob or remove.
