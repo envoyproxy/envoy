91faaadffc88d76918cd62d8081d09c6130c9fb8
wangbaiping <wbphub@gmail.com>
2024-08-22 21:10:48 +0800

    minor update
    Signed-off-by: wangbaiping <wbphub@gmail.com>

--- mobile/tools/check_format.sh
+++ mobile/tools/check_format.sh
@@ -45,7 +45,7 @@ FORMAT_ARGS+=(
 
 export ENVOY_BAZEL_PREFIX="@envoy" && ./bazelw run @envoy//tools/code_format:check_format -- "${ENVOY_FORMAT_ACTION}" --path "$PWD" "${FORMAT_ARGS[@]}"
 
-KTFMT="$(dirname "${BASH_SOURCE[0]}")"/ktfmt.sh
+KTFMT="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"/ktfmt.sh
 KOTLIN_DIRS=(
   "library/kotlin"
   "test/kotlin"
