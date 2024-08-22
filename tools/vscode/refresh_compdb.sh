d7852ebf0d746d83f7665e40a0556db9b74b31a9
wangbaiping <wbphub@gmail.com>
2024-08-22 23:06:49 +0800

    tools: update refresh compdb to use build setup
    Signed-off-by: wangbaiping <wbphub@gmail.com>

--- tools/vscode/refresh_compdb.sh
+++ tools/vscode/refresh_compdb.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+CURRENT_SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+
+# shellcheck source=ci/build_setup.sh
+. "${CURRENT_SCRIPT_DIR}"/../../ci/build_setup.sh
+
 [[ -z "${SKIP_PROTO_FORMAT}" ]] && tools/proto_format/proto_format.sh fix
 
 bazel_or_isk=bazelisk
