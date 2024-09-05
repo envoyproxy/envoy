c85c065660e76c768aa318906aff622dd3e87761
phlax <phlax@users.noreply.github.com>
2024-09-05 11:53:25 +0100

    bazel/ci: Dont break caches with `PATH` (#35978)
    setting `PATH` breaks caches by making otherwise hermetically equivalent
    builds build differently in different environments
    
    to make things worse `PATH` can be set in multiple places, so we break
    our own caches dep on how things are built
    
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- bazel/setup_clang.sh
+++ bazel/setup_clang.sh
@@ -11,9 +11,6 @@ if [[ ! -e "${LLVM_PREFIX}/bin/llvm-config" ]]; then
   exit 1
 fi
 
-PATH="$("${LLVM_PREFIX}"/bin/llvm-config --bindir):${PATH}"
-export PATH
-
 LLVM_VERSION="$(llvm-config --version)"
 LLVM_LIBDIR="$(llvm-config --libdir)"
 LLVM_TARGET="$(llvm-config --host-target)"
@@ -21,7 +18,6 @@ LLVM_TARGET="$(llvm-config --host-target)"
 RT_LIBRARY_PATH="${LLVM_LIBDIR}/clang/${LLVM_VERSION}/lib/${LLVM_TARGET}"
 
 echo "# Generated file, do not edit. If you want to disable clang, just delete this file.
-build:clang --action_env='PATH=${PATH}' --host_action_env='PATH=${PATH}'
 build:clang --action_env='LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config' --host_action_env='LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config'
 build:clang --repo_env='LLVM_CONFIG=${LLVM_PREFIX}/bin/llvm-config'
 build:clang --linkopt='-L$(llvm-config --libdir)'
