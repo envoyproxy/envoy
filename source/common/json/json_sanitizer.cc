9386d3441d3e92ab41477c4d704ca316805e5bf2
Joshua Marantz <jmarantz@google.com>
2024-09-05 21:07:33 -0400

    json: generate hex escapes when utf8 is not valid, and check that those are correct in tests. (#35972)
    Commit Message: Currently the JSON serializer, when sanitizing a string
    that is not valid UTF-8, generates correct JavaScript string syntax.
    However, the syntax is not valid JSON; in JSON you must use 4-digit hex
    escapes, and 3-digit octal escapes are not in spec.
    
    This was working for the Envoy admin detailed histograms, which would
    load the histogram detail via JSON, but parse it with JavaScript.
    However it does not pass validation.
    
    Note that this would only show up if the strings (e.g. stat names,
    prefixes) are not valid utf-8.
    
    This PR changes the escape syntax to hex, so it will pass JSON
    validation. It also adds a test that checks to see the escaped JSON
    strings match the input.
    
    Additional Description: These extra checks were added in anticipation of
    helping to test https://github.com/envoyproxy/envoy/pull/35936
    Risk Level: low
    Testing: //test/common/json/...
    Docs Changes: n/a
    Release Notes: n/a
    Platform Specific Features: n/a
    
    ---------
    
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/common/json/json_sanitizer.cc
+++ source/common/json/json_sanitizer.cc
@@ -77,16 +77,26 @@ absl::string_view sanitize(std::string& buffer, absl::string_view str) {
   }
   END_TRY
   catch (std::exception&) {
-    // If Nlohmann throws an error, emit an octal escape for any character
+    // If Nlohmann throws an error, emit a hex escape for any character
     // requiring it. This can occur for invalid utf-8 sequences, and we don't
     // want to crash the server if such a sequence makes its way into a string
     // we need to serialize. For example, if admin endpoint /stats?format=json
     // is called, and a stat name was synthesized from dynamic content such as a
     // gRPC method.
+    //
+    // Note that JSON string escapes are always 4 digit hex. 3 digit octal would
+    // be more compact, and is legal JavaScript, but not legal JSON. See
+    // https://www.json.org/json-en.html for details.
+    //
+    // TODO(jmarantz): It would better to use the compact JSON escapes for
+    // quotes, slashes, backspace, form-feed, linefeed, CR, and tab, in which
+    // case we'd also need to modify jsonEquivalentStrings in
+    // test/common/json/json_sanitizer_test_util.h. We don't expect to hit this
+    // often, so it isn't a priority to use these more compact encodings.
     buffer.clear();
     for (char c : str) {
       if (needs_slow_sanitizer[static_cast<uint8_t>(c)]) {
-        buffer.append(absl::StrFormat("\\%03o", c));
+        buffer.append(absl::StrFormat("\\u%04x", c));
       } else {
         buffer.append(1, c);
       }
