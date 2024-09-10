523af40a8e22cb230459bc1cb3a65fc08c5c1fac
Ali Beyad <abeyad@google.com>
2024-09-10 13:51:28 +0000

    comment
    Signed-off-by: Ali Beyad <abeyad@google.com>

--- envoy/thread/thread.h
+++ envoy/thread/thread.h
@@ -64,7 +64,7 @@ struct Options {
   //
   // If no value is set, the thread will be created with the default thread priority for the
   // platform.
-  absl::optional<int> priority_{absl::nullopt};
+  absl::optional<int> priority_;
 };
 
 using OptionsOptConstRef = const absl::optional<Options>&;
