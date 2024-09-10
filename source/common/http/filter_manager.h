1fdb8a9bd6e2ab79204493e8ebb5d844d09c1337
sunaydagli <sunaydagli@google.com>
2024-09-10 23:29:51 +0000

    remove errant list Signed-off-by: sunaydagli <sunaydagli@google.com>

--- source/common/http/filter_manager.h
+++ source/common/http/filter_manager.h
@@ -1066,7 +1066,6 @@ private:
   std::list<ActiveStreamEncoderFilterPtr> encoder_filters_;
   std::list<StreamFilterBase*> filters_;
   std::list<AccessLog::InstanceSharedPtr> access_log_handlers_;
-  std::list<AccessLog::InstanceSharedPtr> config_log_handlers_;
 
   // Stores metadata added in the decoding filter that is being processed. Will be cleared before
   // processing the next filter. The storage is created on demand. We need to store metadata
