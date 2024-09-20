6cfa17f9fedb84161f0b0bc9d86bd7f266ff6878
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-09-20 02:18:18 +0000

    try
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- source/extensions/dynamic_modules/sdk/rust/build.rs
+++ source/extensions/dynamic_modules/sdk/rust/build.rs
@@ -1,7 +1,17 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    // TODO: this is just to ensure that bindgen dependency is available in build.rs.
-    let _ = bindgen::Builder::default()
+    println!("cargo:rerun-if-changed=abi.h");
+    let bindings = bindgen::Builder::default()
         .header("abi.h")
+        .header("abi_version.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate();
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
