c3a4eb11063ffe6f0fd00da4dacbfd45d51aa6c5
sunaydagli <sunaydagli@google.com>
2024-09-20 00:14:34 +0000

    og sol Signed-off-by: sunaydagli <sunaydagli@google.com>

--- source/common/http/conn_manager_impl.cc
+++ source/common/http/conn_manager_impl.cc
@@ -357,7 +357,7 @@ void ConnectionManagerImpl::doDeferredStreamDestroy(ActiveStream& stream) {
     stream.deferHeadersAndTrailers();
   } else {
     // For HTTP/1 and HTTP/2, log here as usual.
-    stream.log(AccessLog::AccessLogType::DownstreamEnd);
+    stream.filter_manager_.log(AccessLog::AccessLogType::DownstreamEnd);
   }
 
   stream.filter_manager_.destroyFilters();
@@ -828,6 +828,11 @@ ConnectionManagerImpl::ActiveStream::ActiveStream(ConnectionManagerImpl& connect
          "set in "
          "ConnectionManagerImpl.");
 
+  for (const AccessLog::InstanceSharedPtr& access_log : connection_manager_.config_->accessLogs()) {
+    filter_manager_.addAccessLogHandler(access_log);
+    filter_manager_.addConfigAccessLogHandler(access_log);
+  }
+
   filter_manager_.streamInfo().setStreamIdProvider(
       std::make_shared<HttpStreamIdProviderImpl>(*this));
 
@@ -897,7 +902,7 @@ ConnectionManagerImpl::ActiveStream::ActiveStream(ConnectionManagerImpl& connect
       // If the request is complete, we've already done the stream-end access-log, and shouldn't
       // do the periodic log.
       if (!streamInfo().requestComplete().has_value()) {
-        log(AccessLog::AccessLogType::DownstreamPeriodic);
+        filter_manager_.log(AccessLog::AccessLogType::DownstreamPeriodic);
         refreshAccessLogFlushTimer();
       }
       const SystemTime now = connection_manager_.timeSource().systemTime();
@@ -912,29 +917,6 @@ ConnectionManagerImpl::ActiveStream::ActiveStream(ConnectionManagerImpl& connect
       }
     });
     refreshAccessLogFlushTimer();
-  }
-}
-
-void ConnectionManagerImpl::ActiveStream::log(AccessLog::AccessLogType type) {
-  const Formatter::HttpFormatterContext log_context{
-      request_headers_.get(), response_headers_.get(), response_trailers_.get(), {}, type,
-      active_span_.get()};
-
-  const bool filter_access_loggers_first =
-      Runtime::runtimeFeatureEnabled("envoy.reloadable_features.filter_access_loggers_first");
-
-  if (!filter_access_loggers_first) {
-    for (const auto& access_logger : connection_manager_.config_->accessLogs()) {
-      access_logger->log(log_context, filter_manager_.streamInfo());
-    }
-  }
-
-  filter_manager_.log(log_context);
-
-  if (filter_access_loggers_first) {
-    for (const auto& access_logger : connection_manager_.config_->accessLogs()) {
-      access_logger->log(log_context, filter_manager_.streamInfo());
-    }
   }
 }
 
@@ -1390,7 +1372,7 @@ void ConnectionManagerImpl::ActiveStream::decodeHeaders(RequestHeaderMapSharedPt
   const bool upgrade_rejected = filter_manager_.createFilterChain() == false;
 
   if (connection_manager_.config_->flushAccessLogOnNewRequest()) {
-    log(AccessLog::AccessLogType::DownstreamStart);
+    filter_manager_.log(AccessLog::AccessLogType::DownstreamStart);
   }
 
   // TODO if there are no filters when starting a filter iteration, the connection manager
@@ -1866,7 +1848,7 @@ void ConnectionManagerImpl::ActiveStream::encodeHeaders(ResponseHeaderMap& heade
 
   if (state_.is_tunneling_ &&
       connection_manager_.config_->flushAccessLogOnTunnelSuccessfullyEstablished()) {
-    log(AccessLog::AccessLogType::DownstreamTunnelSuccessfullyEstablished);
+    filter_manager_.log(AccessLog::AccessLogType::DownstreamTunnelSuccessfullyEstablished);
   }
   ENVOY_STREAM_LOG(debug, "encoding headers via codec (end_stream={}):\n{}", *this, end_stream,
                    headers);
--- source/common/http/filter_manager.h
+++ source/common/http/filter_manager.h
@@ -669,6 +669,9 @@ public:
   void addAccessLogHandler(AccessLog::InstanceSharedPtr handler) {
     access_log_handlers_.push_back(std::move(handler));
   }
+  void addConfigAccessLogHandler(AccessLog::InstanceSharedPtr handler) {
+    config_log_handlers_.push_back(std::move(handler));
+  }
   void addStreamDecoderFilter(ActiveStreamDecoderFilterPtr filter) {
     // Note: configured decoder filters are appended to decoder_filters_.
     // This means that if filters are configured in the following order (assume all three filters
@@ -696,13 +699,45 @@ public:
   // FilterChainManager
   void applyFilterFactoryCb(FilterContext context, FilterFactoryCb& factory) override;
 
-  void log(const Formatter::HttpFormatterContext log_context) {
+  void log(AccessLog::AccessLogType access_log_type) {
+    const Formatter::HttpFormatterContext log_context{
+        filter_manager_callbacks_.requestHeaders().ptr(),
+        filter_manager_callbacks_.responseHeaders().ptr(),
+        filter_manager_callbacks_.responseTrailers().ptr(),
+        {},
+        access_log_type,
+        &filter_manager_callbacks_.activeSpan()};
+    const bool filter_access_loggers_first =
+        Runtime::runtimeFeatureEnabled("envoy.reloadable_features.filter_access_loggers_first");
+
+    if (!filter_access_loggers_first) {
+      for (const auto& log_handler : config_log_handlers_) {
+        log_handler->log(log_context, streamInfo());
+      }
+    }
+
     for (const auto& log_handler : access_log_handlers_) {
       log_handler->log(log_context, streamInfo());
     }
+
+    if (filter_access_loggers_first) {
+      for (const auto& log_handler : config_log_handlers_) {
+        log_handler->log(log_context, streamInfo());
+      }
+    }
   }
 
-  std::list<AccessLog::InstanceSharedPtr> accessLogHandlers() { return access_log_handlers_; }
+  std::list<AccessLog::InstanceSharedPtr> accessLogHandlers() {
+    std::list<AccessLog::InstanceSharedPtr> combined_log_handlers(access_log_handlers_);
+    if (!Runtime::runtimeFeatureEnabled("envoy.reloadable_features.filter_access_loggers_first")) {
+      combined_log_handlers.insert(combined_log_handlers.begin(), config_log_handlers_.begin(),
+                                   config_log_handlers_.end());
+    } else {
+      combined_log_handlers.insert(combined_log_handlers.end(), config_log_handlers_.begin(),
+                                   config_log_handlers_.end());
+    }
+    return combined_log_handlers;
+  }
 
   void onStreamComplete() {
     for (auto filter : filters_) {
@@ -1036,6 +1071,7 @@ private:
   std::list<ActiveStreamEncoderFilterPtr> encoder_filters_;
   std::list<StreamFilterBase*> filters_;
   std::list<AccessLog::InstanceSharedPtr> access_log_handlers_;
+  std::list<AccessLog::InstanceSharedPtr> config_log_handlers_;
 
   // Stores metadata added in the decoding filter that is being processed. Will be cleared before
   // processing the next filter. The storage is created on demand. We need to store metadata
