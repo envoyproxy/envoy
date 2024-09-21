83bd9c9dd591bc955a2eb6db0e2e1c0804304053
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-09-21 02:46:57 +0000

    update
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- source/extensions/dynamic_modules/sdk/rust/build.rs
+++ source/extensions/dynamic_modules/sdk/rust/build.rs
@@ -6,7 +6,7 @@ fn main() {
     let bindings = bindgen::Builder::default()
         .header("../../abi.h")
         .header("../../abi_version.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
 
