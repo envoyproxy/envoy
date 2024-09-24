144f08177d4602aec9126cccc9c4b73c9b525ef6
phlax <phlax@users.noreply.github.com>
2024-09-24 13:52:13 +0100

    bazel/ci: Use workspace rather than action envs for fake scm revision (#36298)
    Signed-off-by: Ryan Northey <ryan@synca.io>
    
    Signed-off-by: phlax <phlax@users.noreply.github.com>
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- ci/run_envoy_docker.sh
+++ ci/run_envoy_docker.sh
@@ -129,7 +129,6 @@ docker run --rm \
        -e BAZEL_STARTUP_OPTIONS \
        -e BAZEL_BUILD_EXTRA_OPTIONS \
        -e BAZEL_EXTRA_TEST_OPTIONS \
-       -e BAZEL_FAKE_SCM_REVISION \
        -e BAZEL_REMOTE_CACHE \
        -e BAZEL_STARTUP_EXTRA_OPTIONS \
        -e CI_BRANCH \
