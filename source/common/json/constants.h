b7cad3987eb953224da2ae51820e25da3162d01f
code <wbphub@gmail.com>
2024-09-27 07:43:48 +0800

    access log: new 20x faster json formatter implementation (#35545)
    Commit Message: access log: new 20x faster json formatter implementation
    Additional Description:
    
    ### The core idea of the new `JsonFormatter`.
    
    The core idea of the new `JsonFormatter` is to do as much as possible
    work when loading configuration. Given a specific `json_format`:
    
    ```
       json_format:
         text: "text"
         template: "%START_TIME%"
         number: 2
         bool: true
         list:
           - "list_raw_value"
           - false
           - "%EMIT_TIME%"
         nested:
           text: "nested_text"
    ```
    
    When loading configuration, the keys, raw values, delimiters (all parts
    don't contains the substitution format commands) will be sanitized and
    serialized as JSON string pieces (raw JSON strings) directly.
    The strings that contain substitution commands will be extracted as
    template string and parsed as substitution formatters (objects that
    could extract dynamic values from request/response/stream info, etc.).
    Finally, the whole proto struct configuration we be parsed to an array
    of raw JSON piece strings and formatters.
    
    Basically, above configuration finally will be parsed and serialized to
    following pieces and formatters:
    
    ```
    - '{"text":"text","tmpl":'                                       # Element 0, raw JSON piece.
    - '%START_TIME% %FLAG%'                                          # Element 1, formatter string, must be independent element.
    - ',"number":2,"bool":true,"list":["list_raw_value",false,'      # Element 2, raw JSON piece.
    - '%EMIT_TIME%'                                                  # Element 3, formatter string, must be independent element.
    - '],"nested":{"text":"nested_text"}}'                           # Element 4, raw JSON piece.
    ```
    
    Then, at runtime, when a request is complete and the caller want to
    construct a log line. The above element array will be iterated in order.
    The raw JSON piece string will be put into output buffer directly. And
    the formatters will be evaluated. The outputs of formatters will be
    sanitized/serialized to legal JSON values and then be put into output
    buffer.
    
    At the runtime (core data paths), the logic of new JsonFormatter is
    almost same with the text formatter. By this way, the JsonFormatter
    could get comparable/same performance with the text formatter.
    
    ### The performance result of the formatter.
    
    The previous json formatter is too too slow and the new one perform much
    better (16x-26x faster).
    
    ```
    -------------------------------------------------------------------------------
    Benchmark                                     Time             CPU   Iterations
    -------------------------------------------------------------------------------
    BM_AccessLogFormatterSetup                 7020 ns         7020 ns        96714
    BM_AccessLogFormatter                       349 ns          349 ns      2008804
    BM_StructAccessLogFormatter                5837 ns         5837 ns       117163
    BM_TypedStructAccessLogFormatter           4883 ns         4883 ns       148206
    BM_LegacyJsonAccessLogFormatter           10460 ns        10460 ns        68180
    BM_LegacyTypedJsonAccessLogFormatter       9541 ns         9541 ns        73877
    BM_JsonAccessLogFormatter                   460 ns          460 ns      1521227.  <-- here
    BM_FormatterCommandParsing                 35.6 ns         35.6 ns     18557843
    
    
    ```
    
    
    Risk Level: low. guard by the runtime flag.
    Testing: unit.
    Docs Changes: n/a.
    Release Notes: added.
    Platform Specific Features: n/a.
    
    ---------
    
    Signed-off-by: wangbaiping <wangbaiping@bytedance.com>

--- source/common/json/constants.h
+++ source/common/json/constants.h
@@ -11,6 +11,15 @@ public:
   static constexpr absl::string_view True = "true";
   static constexpr absl::string_view False = "false";
   static constexpr absl::string_view Null = "null";
+
+  // Constants for common JSON delimiters.
+  static constexpr absl::string_view DoubleQuote = "\"";
+  static constexpr absl::string_view Comma = ",";
+  static constexpr absl::string_view Colon = ":";
+  static constexpr absl::string_view ArrayBegin = "[";
+  static constexpr absl::string_view ArrayEnd = "]";
+  static constexpr absl::string_view MapBegin = "{";
+  static constexpr absl::string_view MapEnd = "}";
 };
 
 } // namespace Json
