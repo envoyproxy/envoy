0ffb420e1530c6efd4ba1564ab0ce4e9f06799da
alyssawilk <alyssar@chromium.org>
2024-09-26 22:32:39 -0400

    original_ip: removing exceptions (#36311)
    Risk Level: low
    Testing: updated tests
    Docs Changes: n/a
    Release Notes: n/a
    https://github.com/envoyproxy/envoy-mobile/issues/176
    
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- source/extensions/filters/network/http_connection_manager/config.cc
+++ source/extensions/filters/network/http_connection_manager/config.cc
@@ -517,12 +517,13 @@ HttpConnectionManagerConfig::HttpConnectionManagerConfig(
     }
 
     auto extension = factory->createExtension(extension_config.typed_config(), context_);
-    if (!extension) {
+    SET_AND_RETURN_IF_NOT_OK(extension.status(), creation_status);
+    if (!*extension) {
       creation_status = absl::InvalidArgumentError(fmt::format(
           "Original IP detection extension could not be created: '{}'", extension_config.name()));
       return;
     }
-    original_ip_detection_extensions_.push_back(extension);
+    original_ip_detection_extensions_.push_back(*extension);
   }
 
   const auto& header_mutation_extensions = config.early_header_mutation_extensions();
