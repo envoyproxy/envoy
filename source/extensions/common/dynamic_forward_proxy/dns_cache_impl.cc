5c195b4507d41936209587a91efe05697d8c1ea3
Fredy Wijaya <fredyw@google.com>
2024-09-27 15:48:38 +0000

    Remove debug
    Signed-off-by: Fredy Wijaya <fredyw@google.com>

--- source/extensions/common/dynamic_forward_proxy/dns_cache_impl.cc
+++ source/extensions/common/dynamic_forward_proxy/dns_cache_impl.cc
@@ -423,7 +423,6 @@ void DnsCacheImpl::finishResolve(const std::string& host,
     if (primary_host_info != nullptr && primary_host_info->active_query_ != nullptr) {
       details_with_maybe_trace = absl::StrCat(
           details, ":", absl::StrJoin(primary_host_info->active_query_->getTraces(), ","));
-      std::cerr << "Clearing traces\n";
       primary_host_info->active_query_->clearTraces();
       if (primary_host_info->active_query_->shouldDeleteThis()) {
         delete primary_host_info->active_query_;
