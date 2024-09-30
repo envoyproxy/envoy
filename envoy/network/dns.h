95d252eec44aea5fb0cbff97f95a1005a0e1aafc
Fredy Wijaya <fredyw@google.com>
2024-09-30 14:06:45 +0000

    Debug
    Signed-off-by: Fredy Wijaya <fredyw@google.com>

--- envoy/network/dns.h
+++ envoy/network/dns.h
@@ -36,8 +36,6 @@ public:
    */
   virtual void cancel(CancelReason reason) PURE;
 
-  absl::Mutex& lock() { return mutex_; }
-
   /** Add a trace for the DNS query. */
   void addTrace(uint8_t trace) {
     absl::MutexLock lock(&mutex_);
@@ -46,13 +44,13 @@ public:
 
   /** Return the DNS query traces. */
   const std::vector<uint8_t>& getTraces() {
-    // absl::MutexLock lock(&mutex_);
+    absl::MutexLock lock(&mutex_);
     return traces_;
   }
 
   /** Clears the DNS query traces. */
   void clearTraces() {
-    // absl::MutexLock lock(&mutex_);
+    absl::MutexLock lock(&mutex_);
     traces_.clear();
   }
 
