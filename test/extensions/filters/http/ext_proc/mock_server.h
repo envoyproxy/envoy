a3e32c92c5ae699a4daad094c6a87b58e1e84ec2
yanjunxiang-google <78807980+yanjunxiang-google@users.noreply.github.com>
2024-09-30 19:58:32 -0400

    Ext proc http functionality support (#35740)
    Risk Level: low
    Testing: n/a
    Docs Changes: n/a
    Release Notes: inline
    Fixes:
    
    Description:
    This is to address the issue:
    https://github.com/envoyproxy/envoy/issues/35488, i.e, integrate the
    ext_proc HTTP client to ext_proc filter. With this PR, the basic
    functionalities to have Envoy ext_proc filter talk to a HTTP server
    using HTTP messages are accomplished.
    
    This is the follow up of PR:
    https://github.com/envoyproxy/envoy/pull/35676
    
    ---------
    
    Signed-off-by: Yanjun Xiang <yanjunxiang@google.com>

--- test/extensions/filters/http/ext_proc/mock_server.h
+++ test/extensions/filters/http/ext_proc/mock_server.h
@@ -13,14 +13,15 @@ class MockClient : public ExternalProcessorClient {
 public:
   MockClient();
   ~MockClient() override;
+
   MOCK_METHOD(ExternalProcessorStreamPtr, start,
               (ExternalProcessorCallbacks&, const Grpc::GrpcServiceConfigWithHashKey&,
                const Envoy::Http::AsyncClient::StreamOptions&,
                Envoy::Http::StreamFilterSidestreamWatermarkCallbacks&));
-  MOCK_METHOD(ExternalProcessorStream*, stream, ());
-  MOCK_METHOD(void, setStream, (ExternalProcessorStream * stream));
-
-  ExternalProcessorStream* stream_ = nullptr;
+  MOCK_METHOD(void, sendRequest,
+              (envoy::service::ext_proc::v3::ProcessingRequest&&, bool, const uint64_t,
+               RequestCallbacks*, StreamBase*));
+  MOCK_METHOD(void, cancel, ());
 };
 
 class MockStream : public ExternalProcessorStream {
