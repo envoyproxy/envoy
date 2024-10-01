36c477a50c5cc11b8f333b47d7beb94c984360be
wangbaiping <wangbaiping@bytedance.com>
2024-10-01 21:58:46 +0800

    clean up unnecessary changes
    Signed-off-by: wangbaiping <wangbaiping@bytedance.com>

--- source/common/protobuf/utility.cc
+++ source/common/protobuf/utility.cc
@@ -10,7 +10,6 @@
 #include "source/common/common/assert.h"
 #include "source/common/common/documentation_url.h"
 #include "source/common/common/fmt.h"
-#include "source/common/json/json_streamer.h"
 #include "source/common/protobuf/deterministic_hash.h"
 #include "source/common/protobuf/message_validator_impl.h"
 #include "source/common/protobuf/protobuf.h"
