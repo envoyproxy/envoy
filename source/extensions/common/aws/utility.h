957cdde1c2133fe540e22fdebe0deaa662bf22a2
Nigel Brittain <108375408+nbaws@users.noreply.github.com>
2024-10-15 01:37:59 +1100

    aws: add sigv4/a test corpuses (#36463)
    This patch adds around 160 test cases based on the aws signer test
    corpus from
    https://github.com/awslabs/aws-c-auth/tree/main/tests/aws-signing-test-suite
    
    Adding these test cases uncovered the following bugs, which have also
    been fixed in this patch:
    - Incorrect handling of UTF8 encoded parameters
    - Incorrect path canonicalisation of paths containing /./
    - Incorrect date calculation (missing seconds from the date formatter)
    - Invalid handling of tilde within query parameters (also reported by
    Animal Logic leading to this patch)
    - Invalid handling of other query parameter encodings
    
    Commit Message: aws: add sigv4/a test corpuses and test cases
    Additional Description:
    Risk Level: Low
    Testing: Unit
    Docs Changes:
    Release Notes:
    Platform Specific Features:
    [Optional Runtime guard:]
    [Optional Fixes #Issue]
    [Optional Fixes commit #PR or SHA]
    [Optional Deprecated:]
    [Optional [API
    Considerations](https://github.com/envoyproxy/envoy/blob/main/api/review_checklist.md):]
    
    ---------
    
    Signed-off-by: Nigel Brittain <nbaws@amazon.com>
    Signed-off-by: Gustavo <grnmeira@gmail.com>

--- source/extensions/common/aws/utility.h
+++ source/extensions/common/aws/utility.h
@@ -31,35 +31,33 @@ public:
   /**
    * Creates an AWS Signature V4 canonical request string.
    * See https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
-   * @param service_name the AWS service name.
    * @param method the HTTP request method.
    * @param path the request path.
    * @param canonical_headers the pre-canonicalized request headers.
    * @param content_hash the hashed request body.
+   * @param should_normalize_uri_path whether we should normalize the path string
+   * @param use_double_uri_uncode whether we should perform an additional uri encode
    * @return the canonicalized request string.
    */
-  static std::string createCanonicalRequest(
-      absl::string_view service_name, absl::string_view method, absl::string_view path,
-      const std::map<std::string, std::string>& canonical_headers, absl::string_view content_hash);
+  static std::string
+  createCanonicalRequest(absl::string_view method, absl::string_view path,
+                         const std::map<std::string, std::string>& canonical_headers,
+                         absl::string_view content_hash, bool should_normalize_uri_path,
+                         bool use_double_uri_uncode);
 
   /**
    * Normalizes the path string based on AWS requirements.
    * See step 2 in https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
-   * @param query_string the query string from the HTTP request.
-   * @param service_name the AWS service name.
-   * @return the canonicalized query string.
+   * @param headers header map
    */
-  static std::string canonicalizePathString(absl::string_view path_string,
-                                            absl::string_view service_name);
+  static std::string normalizePath(absl::string_view original_path);
 
   /**
-   * URI encodes the given string based on AWS requirements.
+   * URI encodes the path string based on AWS requirements.
    * See step 2 in https://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
-   * @param decoded the decoded string.
-   * @param service_name the AWS service name.
-   * @return the URI encoded string.
+   * @param headers header map
    */
-  static std::string encodePathSegment(absl::string_view decoded, absl::string_view service_name);
+  static std::string uriEncodePath(absl::string_view original_path);
 
   /**
    * Normalizes the query string based on AWS requirements.
@@ -75,7 +73,7 @@ public:
    * @param decoded the decoded string.
    * @return the URI encoded string.
    */
-  static std::string encodeQueryParam(absl::string_view decoded);
+  static std::string encodeQueryComponent(absl::string_view decoded);
 
   /**
    * Get the semicolon-delimited string of canonical header names.
@@ -196,6 +194,23 @@ public:
   static int64_t getIntegerFromJsonOrDefault(Json::ObjectSharedPtr json_object,
                                              const std::string& integer_value,
                                              const int64_t integer_default);
+
+  /**
+   * @brief Given a service name, should we uri encode the path
+   * Logic for this is based on inspection of smithy definitions and currently only includes s3
+   * services
+   * @return boolean
+   */
+
+  static bool useDoubleUriEncode(const std::string service_name);
+
+  /**
+   * @brief Given a service name, should we normalize the path
+   * Logic for this is based on inspection of smithy definitions and currently only includes s3
+   * services
+   * @return boolean
+   */
+  static bool shouldNormalizeUriPath(const std::string service_name);
 };
 
 } // namespace Aws
