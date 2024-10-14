09004c3dffac16eeb60e0c40d382fd81686a5479
alyssawilk <alyssar@chromium.org>
2024-10-14 09:22:10 -0400

    map matcher: removing exceptions (#36514)
    Risk Level: low
    Testing: updated tests
    Docs Changes: n/a
    Release Notes: n/a
    https://github.com/envoyproxy/envoy-mobile/issues/176
    
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>
    Signed-off-by: Gustavo <grnmeira@gmail.com>

--- test/common/matcher/exact_map_matcher_test.cc
+++ test/common/matcher/exact_map_matcher_test.cc
@@ -12,86 +12,87 @@ namespace Envoy {
 namespace Matcher {
 
 TEST(ExactMapMatcherTest, NoMatch) {
-  ExactMapMatcher<TestData> matcher(
+  std::unique_ptr<ExactMapMatcher<TestData>> matcher = *ExactMapMatcher<TestData>::create(
       std::make_unique<TestInput>(
           DataInputGetResult{DataInputGetResult::DataAvailability::AllDataAvailable, "blah"}),
       absl::nullopt);
 
   TestData data;
-  const auto result = matcher.match(data);
+  const auto result = matcher->match(data);
   verifyNoMatch(result);
 }
 
 TEST(ExactMapMatcherTest, NoMatchDueToNoData) {
-  ExactMapMatcher<TestData> matcher(
+  std::unique_ptr<ExactMapMatcher<TestData>> matcher = *ExactMapMatcher<TestData>::create(
       std::make_unique<TestInput>(DataInputGetResult{
           DataInputGetResult::DataAvailability::AllDataAvailable, absl::monostate()}),
       absl::nullopt);
 
   TestData data;
-  const auto result = matcher.match(data);
+  const auto result = matcher->match(data);
   verifyNoMatch(result);
 }
 
 TEST(ExactMapMatcherTest, NoMatchWithFallback) {
-  ExactMapMatcher<TestData> matcher(
+  std::unique_ptr<ExactMapMatcher<TestData>> matcher = *ExactMapMatcher<TestData>::create(
       std::make_unique<TestInput>(
           DataInputGetResult{DataInputGetResult::DataAvailability::AllDataAvailable, "blah"}),
       stringOnMatch<TestData>("no_match"));
 
   TestData data;
-  const auto result = matcher.match(data);
+  const auto result = matcher->match(data);
   verifyImmediateMatch(result, "no_match");
 }
 
 TEST(ExactMapMatcherTest, Match) {
-  ExactMapMatcher<TestData> matcher(
+  std::unique_ptr<ExactMapMatcher<TestData>> matcher = *ExactMapMatcher<TestData>::create(
       std::make_unique<TestInput>(
           DataInputGetResult{DataInputGetResult::DataAvailability::AllDataAvailable, "match"}),
       stringOnMatch<TestData>("no_match"));
 
-  matcher.addChild("match", stringOnMatch<TestData>("match"));
+  matcher->addChild("match", stringOnMatch<TestData>("match"));
 
   TestData data;
-  const auto result = matcher.match(data);
+  const auto result = matcher->match(data);
   verifyImmediateMatch(result, "match");
 }
 
 TEST(ExactMapMatcherTest, DataNotAvailable) {
-  ExactMapMatcher<TestData> matcher(std::make_unique<TestInput>(DataInputGetResult{
-                                        DataInputGetResult::DataAvailability::NotAvailable, {}}),
-                                    stringOnMatch<TestData>("no_match"));
+  std::unique_ptr<ExactMapMatcher<TestData>> matcher = *ExactMapMatcher<TestData>::create(
+      std::make_unique<TestInput>(
+          DataInputGetResult{DataInputGetResult::DataAvailability::NotAvailable, {}}),
+      stringOnMatch<TestData>("no_match"));
 
-  matcher.addChild("match", stringOnMatch<TestData>("match"));
+  matcher->addChild("match", stringOnMatch<TestData>("match"));
 
   TestData data;
-  const auto result = matcher.match(data);
+  const auto result = matcher->match(data);
   verifyNotEnoughDataForMatch(result);
 }
 
 TEST(ExactMapMatcherTest, MoreDataMightBeAvailableNoMatch) {
-  ExactMapMatcher<TestData> matcher(
+  std::unique_ptr<ExactMapMatcher<TestData>> matcher = *ExactMapMatcher<TestData>::create(
       std::make_unique<TestInput>(DataInputGetResult{
           DataInputGetResult::DataAvailability::MoreDataMightBeAvailable, "no match"}),
       stringOnMatch<TestData>("no_match"));
 
-  matcher.addChild("match", stringOnMatch<TestData>("match"));
+  matcher->addChild("match", stringOnMatch<TestData>("match"));
 
   TestData data;
-  const auto result = matcher.match(data);
+  const auto result = matcher->match(data);
   verifyNotEnoughDataForMatch(result);
 }
 
 TEST(ExactMapMatcherTest, MoreDataMightBeAvailableMatch) {
-  ExactMapMatcher<TestData> matcher(
+  std::unique_ptr<ExactMapMatcher<TestData>> matcher = *ExactMapMatcher<TestData>::create(
       std::make_unique<TestInput>(DataInputGetResult{
           DataInputGetResult::DataAvailability::MoreDataMightBeAvailable, "match"}),
       stringOnMatch<TestData>("no_match"));
 
-  matcher.addChild("match", stringOnMatch<TestData>("match"));
+  matcher->addChild("match", stringOnMatch<TestData>("match"));
 
   TestData data;
-  const auto result = matcher.match(data);
+  const auto result = matcher->match(data);
   verifyImmediateMatch(result, "match");
 }
 } // namespace Matcher
