cc4fea50c982dbbbaea0d987faa2cb0a7c526acb
Ryan Northey <ryan@synca.io>
2024-10-16 11:30:54 +0100

    bazel/release: Fixes for versions hashing
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- bazel/generate_release_hash.sh
+++ bazel/generate_release_hash.sh
@@ -2,16 +2,9 @@
 
 set -e -o pipefail
 
-ENVOY_SRCDIR="${1}"
 
-if [[ ! -e "$ENVOY_SRCDIR" ]]; then
-    echo "Unable to find Envoy src dir: ${ENVOY_SRCDIR}" >&2
-    exit 1
-fi
-
-git -C "$ENVOY_SRCDIR" fetch --tags
-
-git -C "$ENVOY_SRCDIR" tag --list 'v[0-9]*.[0-9]*.[0-9]*' \
-    | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+$' \
+git ls-remote --tags https://github.com/envoyproxy/envoy \
+    | grep -E 'refs/tags/v[0-9]+\.[0-9]+\.[0-9]+$' \
     | sort -u \
-    | sha256sum
+    | sha256sum \
+    | cut -d ' ' -f 1
