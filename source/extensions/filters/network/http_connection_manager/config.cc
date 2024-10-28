90361193bc4c8aa70ecf5b59a0a6d1c8a0f05687
Alyssa Wilk <alyssar@chromium.org>
2024-10-28 16:14:52 +0000

    format
    Signed-off-by: Alyssa Wilk <alyssar@chromium.org>

--- source/extensions/filters/network/http_connection_manager/config.cc
+++ source/extensions/filters/network/http_connection_manager/config.cc
@@ -80,11 +80,11 @@ std::unique_ptr<Http::InternalAddressConfig> createInternalAddressConfig(
 
   if (!Runtime::runtimeFeatureEnabled(
           "envoy.reloadable_features.explicit_internal_address_config")) {
-    ENVOY_LOG_ONCE_MISC(
-        warn, "internal_address_config is not configured. The prior default behaviour "
-              "trusted RFC1918 IP addresses, but this was changed in the 1.32 release. "
-              "Please explictily config internal address config if you need it before "
-              "envoy.reloadable_features.explicit_internal_address_config is removed.");
+    ENVOY_LOG_ONCE_MISC(warn,
+                        "internal_address_config is not configured. The prior default behaviour "
+                        "trusted RFC1918 IP addresses, but this was changed in the 1.32 release. "
+                        "Please explictily config internal address config if you need it before "
+                        "envoy.reloadable_features.explicit_internal_address_config is removed.");
   }
 
   return std::make_unique<Http::DefaultInternalAddressConfig>();
