0e9e14d0dd70ad3492a28ba33876c8e57cc36fe9
Boteng Yao <boteng@google.com>
2024-10-29 16:19:23 +0000

    clean up
    Signed-off-by: Boteng Yao <boteng@google.com>

--- source/extensions/common/wasm/wasm.h
+++ source/extensions/common/wasm/wasm.h
@@ -171,7 +171,7 @@ using CreateWasmCallback = std::function<void(WasmHandleSharedPtr)>;
 bool createWasm(const PluginSharedPtr& plugin, const Stats::ScopeSharedPtr& scope,
                 Upstream::ClusterManager& cluster_manager, Init::Manager& init_manager,
                 Event::Dispatcher& dispatcher, Api::Api& api,
-                Envoy::Server::ServerLifecycleNotifier&,
+                Server::ServerLifecycleNotifier& lifecycle_notifier,
                 RemoteAsyncDataProviderPtr& remote_data_provider, CreateWasmCallback&& callback,
                 CreateContextFn create_root_context_for_testing = nullptr);
 
