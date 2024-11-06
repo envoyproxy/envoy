ca72347b176686ddeb2f4233c5ca3154d4ecaf48
Brian Sonnenberg <bsonnenberg@google.com>
2024-11-06 00:25:49 +0000

    http_inspector dynamic buffer

--- source/extensions/filters/listener/http_inspector/http_inspector.cc
+++ source/extensions/filters/listener/http_inspector/http_inspector.cc
@@ -18,12 +18,20 @@ namespace Extensions {
 namespace ListenerFilters {
 namespace HttpInspector {
 
-Config::Config(Stats::Scope& scope)
-    : stats_{ALL_HTTP_INSPECTOR_STATS(POOL_COUNTER_PREFIX(scope, "http_inspector."))} {}
+Config::Config(Stats::Scope& scope,
+               const envoy::extensions::filters::listener::http_inspector::v3::HttpInspector& proto_config)
+    : initial_read_buffer_size_(PROTOBUF_GET_WRAPPED_OR_DEFAULT(proto_config,
+                                                                initial_read_buffer_size,
+                                                                DEFAULT_INITIAL_BUFFER_SIZE)),
+      stats_{ALL_HTTP_INSPECTOR_STATS(POOL_COUNTER_PREFIX(scope, "http_inspector."))}
+{}
 
 const absl::string_view Filter::HTTP2_CONNECTION_PREFACE = "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n";
 
-Filter::Filter(const ConfigSharedPtr config) : config_(config), no_op_callbacks_() {
+Filter::Filter(const ConfigSharedPtr config)
+    : config_(config),
+      no_op_callbacks_(),
+      requested_read_bytes_(config->initialReadBufferSize()) {
   // Filter for only Request Message types with NoOp Parser callbacks.
   if (Runtime::runtimeFeatureEnabled("envoy.reloadable_features.http_inspector_use_balsa_parser")) {
     // Set both allow_custom_methods and enable_trailers to true with BalsaParser.
@@ -49,6 +57,21 @@ Network::FilterStatus Filter::onData(Network::ListenerFilterBuffer& buffer) {
     done(true);
     return Network::FilterStatus::Continue;
   case ParseState::Continue:
+    ENVOY_LOG(trace, "http inspector: need more bytes");
+
+    // If we have requested the maximum amount of data the filter is configured
+    // to allow, then move on; the request is too large to identify the HTTP
+    // version
+    if (static_cast<size_t>(nread_) == maxConfigReadBytes()) {
+      done(false);
+      return Network::FilterStatus::Continue;
+    }
+
+    // Otherwise, double the buffer size and try again
+    if (static_cast<size_t>(nread_) >= requested_read_bytes_) {
+      requested_read_bytes_ = std::min<uint32_t>(2 * requested_read_bytes_, maxConfigReadBytes());
+    }
+
     return Network::FilterStatus::StopIteration;
   }
   PANIC_DUE_TO_CORRUPT_ENUM
