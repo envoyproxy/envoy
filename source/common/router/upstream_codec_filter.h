f8d72012238445ead54f213f3f693da7d3520eb8
Yury Kats <ykats@google.com>
2024-11-17 14:32:23 -0500

    Optimize class layout for UpstreamCodecFilter
    Signed-off-by: Yury Kats <ykats@google.com>

--- source/common/router/upstream_codec_filter.h
+++ source/common/router/upstream_codec_filter.h
@@ -87,10 +87,10 @@ public:
   Http::StreamDecoderFilterCallbacks* callbacks_;
   CodecBridge bridge_;
   OptRef<Http::RequestHeaderMap> latched_headers_;
-  // Keep small members (bools and enums) at the end of class, to reduce alignment overhead.
-  bool calling_encode_headers_ : 1;
   absl::Status deferred_reset_status_;
   absl::optional<bool> latched_end_stream_;
+  // Keep small members (bools and enums) at the end of class, to reduce alignment overhead.
+  bool calling_encode_headers_ : 1;
 
 private:
   StreamInfo::UpstreamTiming& upstreamTiming() {
