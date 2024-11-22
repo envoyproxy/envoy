21db5f20e6caeb05265c7d168ccdd522af4a7b22
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-11-22 16:08:07 +0000

    dynamic_modules: exclude ABI C header from '-modernize-use-using'
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "164a60ff214ca3cd62526ddb7c3fe21cf943e8721a115c87feca81a58510072c";
+const char* kAbiVersion = "152372902a15b54edce8a868b15931579b79d979dbc004689761b47b8622d4a3";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
