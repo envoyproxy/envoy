95f7b7117d65c9ab02a4950cf81e4f8ee8b360e8
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-11-25 19:40:09 +0000

    Removes BAZEL_BUILD_EXTRA_OPTIONS
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,10 +2,6 @@
 
 BAZELRC_FILE=~/.bazelrc bazel/setup_clang.sh /opt/llvm
 
-if [[ -n "${BAZEL_BUILD_EXTRA_OPTIONS}" ]]; then
-    echo "build ${BAZEL_BUILD_EXTRA_OPTIONS}" | tee -a ~/.bazelrc
-fi
-
 # Ideally we want this line so bazel doesn't pollute things outside of the devcontainer, but some of
 # API tooling (proto_sync) depends on symlink like bazel-bin.
 # TODO(lizan): Fix API tooling and enable this again
