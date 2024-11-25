f5fd15895ed83c381d483a1ee073a398b2cda097
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-11-25 21:58:28 +0000

    wip - dynamic_modules: try using stddef
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- source/extensions/dynamic_modules/sdk/rust/build.rs
+++ source/extensions/dynamic_modules/sdk/rust/build.rs
@@ -2,16 +2,32 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rerun-if-changed=abi.h");
-    let bindings = bindgen::Builder::default()
-        .header("../../abi.h")
-        .header("../../abi_version.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .generate()
-        .expect("Unable to generate bindings");
+  // Print all environment variables for debugging!
+  for (key, value) in env::vars() {
+    eprintln!("{}: {}", key, value);
+  }
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings");
+  // Check if it has CC environment variable, and if that matches clang,
+  // then set the CLANG_PATH to the value of CC to instruct bindgen to use
+  // clang as the compiler.
+  // https://github.com/KyleMayes/clang-sys?tab=readme-ov-file#environment-variables
+  if let Some(cc) = env::var("CC").ok() {
+    if cc.contains("clang") {
+      env::set_var("CLANG_PATH", cc);
+    }
+  }
+
+  println!("cargo:rerun-if-changed=abi.h");
+  let bindings = bindgen::Builder::default()
+    .header("../../abi.h")
+    .header("../../abi_version.h")
+    .clang_arg("-v")
+    .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+    .generate()
+    .expect("Unable to generate bindings");
+
+  let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+  bindings
+    .write_to_file(out_path.join("bindings.rs"))
+    .expect("Couldn't write bindings");
 }
