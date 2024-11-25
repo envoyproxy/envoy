be6916f568cfd58629e4cf18aedddf6dbb9bec02
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-11-25 19:31:14 +0000

    wip; dynamic_modules: try using stdlib headers in bindgen
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "152372902a15b54edce8a868b15931579b79d979dbc004689761b47b8622d4a3";
+const char* kAbiVersion = "d7b9d1bb09e96f6b96caafec7624721a958d49ee57b63c13251ce5c7a0439d68";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
