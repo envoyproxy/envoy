b1a2af6a29b6a6d95449d407c9e3e5ae64c74788
botengyao <boteng@google.com>
2024-11-26 11:14:43 -0500

    http: allow trigger GOAWAY from l7 filters (#36306)
    Risk Level: low
    Testing: integration test
    
    ---------
    
    Signed-off-by: Boteng Yao <boteng@google.com>

--- envoy/http/filter.h
+++ envoy/http/filter.h
@@ -645,6 +645,11 @@ public:
                               absl::string_view details) PURE;
 
   /**
+   * Attempt to send GOAWAY and close the connection, and no filter chain will move forward.
+   */
+  virtual void sendGoAwayAndClose() PURE;
+
+  /**
    * Adds decoded metadata. This function can only be called in
    * StreamDecoderFilter::decodeHeaders/Data/Trailers(). Do not call in
    * StreamDecoderFilter::decodeMetadata().
