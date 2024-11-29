934c9e0d305049059738070d146a506183d475ff
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-11-29 10:36:30 -0800

    Update ci/do_ci.sh
    Co-authored-by: phlax <phlax@users.noreply.github.com>
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -337,6 +337,16 @@ case $CI_TARGET in
         export FIX_YAML="${ENVOY_TEST_TMPDIR}/lint-fixes/clang-tidy-fixes.yaml"
         export CLANG_TIDY_APPLY_FIXES=1
         mkdir -p "${ENVOY_TEST_TMPDIR}/lint-fixes"
+      if [[ -n "$CLANG_TIDY_TARGETS" ]]; then
+          read -ra CLANG_TIDY_TARGETS <<< "${CLANG_TIDY_TARGETS-}"
+      else
+          CLANG_TIDY_TARGETS=(
+            //contrib/...
+            //source/...
+            //test/...
+            @envoy_api//...)
+      fi
+
         TARGETS="${CLANG_TIDY_TARGETS:-//contrib/... //source/... //test/... @envoy_api//...}"
         CLANG_TIDY_TARGETS=(${TARGETS[@]})
         echo "Running clang-tidy on ${CLANG_TIDY_TARGETS[*]}"
