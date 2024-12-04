a306623bad67fba582a0cf5e1f84868bcdb06ea9
Christoph Pakulski <paker8848@gmail.com>
2024-12-04 20:01:00 +0000

    Refactored addDynamicProvider as template.
    Signed-off-by: Christoph Pakulski <paker8848@gmail.com>

--- source/common/rds/route_config_provider_manager.cc
+++ source/common/rds/route_config_provider_manager.cc
@@ -1,7 +1,6 @@
 #include "source/common/rds/route_config_provider_manager.h"
 
 #include "source/common/rds/util.h"
-#include "source/common/runtime/runtime_features.h"
 
 namespace Envoy {
 namespace Rds {
@@ -74,56 +73,6 @@ RouteConfigProviderPtr RouteConfigProviderManager::addStaticProvider(
   auto provider = create_static_provider();
   static_route_config_providers_.insert(provider.get());
   return provider;
-}
-
-RouteConfigProviderSharedPtr RouteConfigProviderManager::addDynamicProvider(
-    const Protobuf::Message& rds, const std::string& route_config_name, Init::Manager& init_manager,
-    std::function<
-        std::pair<RouteConfigProviderSharedPtr, const Init::Target*>(uint64_t manager_identifier)>
-        create_dynamic_provider) {
-
-  // RdsRouteConfigSubscriptions are unique based on their serialized RDS config.
-  uint64_t manager_identifier;
-  if (Runtime::runtimeFeatureEnabled("envoy.reloadable_features.normalize_rds_provider_config")) {
-    Protobuf::ReflectableMessage reflectable_message = createReflectableMessage(rds);
-
-    const Protobuf::FieldDescriptor* config_source_field =
-        reflectable_message->GetDescriptor()->FindFieldByName("config_source");
-
-    // If this assertion fails it means that dynamic provider config passed as 'rds' parameter does
-    // not contain a proto field called 'config_source'. That field must have
-    // envoy::config::core::v3::ConfigSource type.
-    ASSERT(config_source_field != nullptr);
-
-    Protobuf::Message* mutable_config_source = reflectable_message->GetReflection()->MutableMessage(
-        &(*reflectable_message), config_source_field);
-
-    ASSERT(mutable_config_source != nullptr);
-
-    envoy::config::core::v3::ConfigSource& config_source =
-        dynamic_cast<envoy::config::core::v3::ConfigSource&>(*mutable_config_source);
-
-    // Save the value of initial_fetch_timeout. Even though config 'rds' was passed as constant,
-    // `reflectable_message` allows to modify values inside the 'rds'.
-    // initial_fetch_timeout will be normalized (zeroed) before hash is calculated and must be
-    // restored to the original value after hash has been calculated.
-    auto* orig_initial_timeout = config_source.release_initial_fetch_timeout();
-    manager_identifier = MessageUtil::hash(rds);
-    config_source.set_allocated_initial_fetch_timeout(orig_initial_timeout);
-  } else {
-    manager_identifier = MessageUtil::hash(rds);
-  }
-
-  auto existing_provider =
-      reuseDynamicProvider(manager_identifier, init_manager, route_config_name);
-
-  if (existing_provider) {
-    return existing_provider;
-  }
-  auto new_provider = create_dynamic_provider(manager_identifier);
-  init_manager.add(*new_provider.second);
-  dynamic_route_config_providers_.insert({manager_identifier, new_provider});
-  return new_provider.first;
 }
 
 RouteConfigProviderSharedPtr
