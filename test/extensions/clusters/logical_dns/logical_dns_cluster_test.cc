b12f2947bd221dbba01b2a858482480b8901ef09
Steven Jin Xuan <sjinxuan@microsoft.com>
2024-12-04 21:20:16 -0500

    Fix tests
    Signed-off-by: Steven Jin Xuan <sjinxuan@microsoft.com>

--- test/extensions/clusters/logical_dns/logical_dns_cluster_test.cc
+++ test/extensions/clusters/logical_dns/logical_dns_cluster_test.cc
@@ -667,7 +667,7 @@ TEST_F(LogicalDnsClusterTest, NegativeDnsJitter) {
                     port_value: 443
   )EOF";
   EXPECT_THROW_WITH_MESSAGE(setupFromV3Yaml(yaml, false), EnvoyException,
-                            "Invalid duration: Expected positive duration: seconds: -1\n");
+                            "Expected positive duration: seconds: -1\n");
 }
 
 TEST_F(LogicalDnsClusterTest, ExtremeJitter) {
