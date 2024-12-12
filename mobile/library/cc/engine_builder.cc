2f8bb82231f6668c65b0d7d27cf2c64ef093f879
Ali Beyad <abeyad@google.com>
2024-12-12 17:21:54 +0000

    mobile: Don't turn on GRO for Apple
    Signed-off-by: Ali Beyad <abeyad@google.com>

--- mobile/library/cc/engine_builder.cc
+++ mobile/library/cc/engine_builder.cc
@@ -856,7 +856,10 @@ std::unique_ptr<envoy::config::bootstrap::v3::Bootstrap> EngineBuilder::generate
     (*reloadable_features.mutable_fields())[guard_and_value.first].set_bool_value(
         guard_and_value.second);
   }
+  // GRO is not available on Apple platforms.
+#if !defined(__APPLE__)
   (*reloadable_features.mutable_fields())["prefer_quic_client_udp_gro"].set_bool_value(true);
+#endif
   ProtobufWkt::Struct& restart_features =
       *(*runtime_values.mutable_fields())["restart_features"].mutable_struct_value();
   for (auto& guard_and_value : restart_runtime_guards_) {
