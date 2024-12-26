ba8d75d87a6b5c33a0489962df2c877322c3c5c4
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-12-26 07:05:24 +0000

    buffer doesn't need to be mocked
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- source/extensions/dynamic_modules/sdk/rust/src/buffer.rs
+++ source/extensions/dynamic_modules/sdk/rust/src/buffer.rs
@@ -1,39 +1,26 @@
-#[cfg(test)]
-#[path = "./buffer_test.rs"]
-mod buffer_test;
-
-mod envoy_buffer {
-  #[cfg(any(test, feature = "testing"))]
-  use mockall::automock;
-
-  /// A buffer struct that represents a contiguous memory region owned by Envoy.
-  /// This has a mock implementation for testing.
-  ///
-  /// The life time (not in Rust sense) of the buffer is managed by Envoy, and it depends on how
-  /// this [`EnvoyBuffer`] is created, for example, via [`crate::EnvoyHttpFilter`]'s methods.
-  #[repr(C)]
-  pub struct EnvoyBuffer {
-    raw_ptr: *const u8,
-    length: usize,
-  }
-
-  #[cfg_attr(any(test, feature = "testing"), automock)]
-  impl EnvoyBuffer {
-    pub fn new(raw_ptr: *const u8, length: usize) -> Self {
-      Self { raw_ptr, length }
-    }
-
-    pub fn as_slice(&self) -> &[u8] {
-      unsafe { std::slice::from_raw_parts(self.raw_ptr, self.length) }
-    }
-
-    pub fn as_mut_slice(&mut self) -> &mut [u8] {
-      unsafe { std::slice::from_raw_parts_mut(self.raw_ptr as *mut u8, self.length) }
-    }
-  }
+/// A buffer struct that represents a contiguous memory region owned by Envoy.
+///
+/// The life time (not in Rust sense) of the buffer is managed by Envoy, and it depends on how
+/// this [`EnvoyBuffer`] is created, for example, via [`crate::EnvoyHttpFilter`]'s methods.
+///
+/// This is cloneable and copyable, but the underlying memory is not copied.
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+pub struct EnvoyBuffer {
+  raw_ptr: *const u8,
+  length: usize,
 }
 
-#[cfg(not(any(test, feature = "testing")))]
-pub type EnvoyBuffer = envoy_buffer::EnvoyBuffer;
-#[cfg(any(test, feature = "testing"))]
-pub type EnvoyBuffer = envoy_buffer::MockEnvoyBuffer;
+impl EnvoyBuffer {
+  pub fn new(raw_ptr: *const u8, length: usize) -> Self {
+    Self { raw_ptr, length }
+  }
+
+  pub fn as_slice(&self) -> &[u8] {
+    unsafe { std::slice::from_raw_parts(self.raw_ptr, self.length) }
+  }
+
+  pub fn as_mut_slice(&mut self) -> &mut [u8] {
+    unsafe { std::slice::from_raw_parts_mut(self.raw_ptr as *mut u8, self.length) }
+  }
+}
