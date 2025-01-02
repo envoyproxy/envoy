86fe96a55fbac0112afc72942fa6e98b3cdd96de
Ben Plotnick <bplotnick@netflix.com>
2025-01-02 16:53:39 -0500

    Fix abi version sha
    Signed-off-by: Ben Plotnick <bplotnick@netflix.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "379116de880d168273ff430819d28960188fc0b11fe1ce55db0df7849d820926";
+const char* kAbiVersion = "79e2d76037150b0baac9cbe5eb374601eb8f693690e8bda8f4ef961f53128670";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
