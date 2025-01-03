398d53ef949eb0196e1ce881a138b83163374b7b
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-01-03 03:28:01 +0000

    dynamic_modules: adds dynamic metadata callbakcs
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "27dbe7918b67b12bfcf43e050ce142a300feb73fb3bd5779c754d1d1d3c8645f";
+const char* kAbiVersion = "fd363586ca79ce0a40c4a2ad365746aed80a331cb042c08f276f4cf9a6dfb2a9";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
