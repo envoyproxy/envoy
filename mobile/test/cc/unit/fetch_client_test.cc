9f76c2c038f20474a03ff78bd3c90cd22ea454a4
Fredy Wijaya <fredyw@google.com>
2025-01-15 22:36:25 +0000

    Force it to fail
    Signed-off-by: Fredy Wijaya <fredyw@google.com>

--- mobile/test/cc/unit/fetch_client_test.cc
+++ mobile/test/cc/unit/fetch_client_test.cc
@@ -22,7 +22,7 @@ TEST(FetchClientTest, Http3) {
             ENVOY_SUCCESS);
   // The first request could either be HTTP/2 or HTTP/3 because we no longer give HTTP/3 a head
   // start.
-  ASSERT_GE(protocols.at(0), Http::Protocol::Http2);
+  ASSERT_GE(protocols.at(0), Http::Protocol::Http3);
   // // TODO(fredyw): In EngFlow CI, HTTP/3 does not work and will use HTTP/2 instead.
   // ASSERT_GE(protocols.at(1), Http::Protocol::Http2);
 }
