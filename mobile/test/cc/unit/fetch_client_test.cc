1aa202951a1e11f9c98df4b7aa41182135521d5e
Fredy Wijaya <fredyw@google.com>
2025-01-15 21:15:14 +0000

    Test
    Signed-off-by: Fredy Wijaya <fredyw@google.com>

--- mobile/test/cc/unit/fetch_client_test.cc
+++ mobile/test/cc/unit/fetch_client_test.cc
@@ -18,14 +18,13 @@ TEST(FetchClientTest, Http2) {
 TEST(FetchClientTest, Http3) {
   Envoy::Fetch client;
   std::vector<Http::Protocol> protocols;
-  ASSERT_EQ(client.fetch({"https://www.google.com/", "https://www.google.com/"}, {"www.google.com"},
-                         protocols),
+  ASSERT_EQ(client.fetch({"https://www.google.com/"}, {"www.google.com"}, protocols),
             ENVOY_SUCCESS);
   // The first request could either be HTTP/2 or HTTP/3 because we no longer give HTTP/3 a head
   // start.
   ASSERT_GE(protocols.at(0), Http::Protocol::Http2);
-  // TODO(fredyw): In EngFlow CI, HTTP/3 does not work and will use HTTP/2 instead.
-  ASSERT_GE(protocols.at(1), Http::Protocol::Http2);
+  // // TODO(fredyw): In EngFlow CI, HTTP/3 does not work and will use HTTP/2 instead.
+  // ASSERT_GE(protocols.at(1), Http::Protocol::Http2);
 }
 
 } // namespace
