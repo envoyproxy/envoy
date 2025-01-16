3f3624f4ddcc1d44e2c21a4825b4a6ce6b2b3680
birenroy <birenroy@google.com>
2025-01-16 16:28:23 -0500

    http2: enables `envoy.reloadable_features.http2_use_oghttp2` by default (#38026)
    The remaining differences with nghttp2 were resolved in
    https://github.com/envoyproxy/envoy/pull/38003.
    
    Commit Message: http2: enables
    `envoy.reloadable_features.http2_use_oghttp2` by default
    Additional Description:
    Risk Level:
    Testing:
    Docs Changes:
    Release Notes:
    Platform Specific Features:
    Runtime guard: `envoy.reloadable_features.http2_use_oghttp2`
    
    ---------
    
    Signed-off-by: Biren Roy <birenroy@google.com>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -55,6 +55,7 @@ RUNTIME_GUARD(envoy_reloadable_features_http1_use_balsa_parser);
 RUNTIME_GUARD(envoy_reloadable_features_http2_discard_host_header);
 RUNTIME_GUARD(envoy_reloadable_features_http2_no_protocol_error_upon_clean_close);
 RUNTIME_GUARD(envoy_reloadable_features_http2_propagate_reset_events);
+RUNTIME_GUARD(envoy_reloadable_features_http2_use_oghttp2);
 RUNTIME_GUARD(envoy_reloadable_features_http3_happy_eyeballs);
 RUNTIME_GUARD(envoy_reloadable_features_http3_remove_empty_trailers);
 // Delay deprecation and decommission until UHV is enabled.
@@ -119,8 +120,6 @@ RUNTIME_GUARD(envoy_restart_features_use_eds_cache_for_ads);
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_test_feature_false);
 // TODO(adisuissa) reset to true to enable unified mux by default
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_unified_mux);
-// TODO(birenroy) flip after the security issue is addressed.
-FALSE_RUNTIME_GUARD(envoy_reloadable_features_http2_use_oghttp2);
 // Used to track if runtime is initialized.
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_runtime_initialized);
 // TODO(alyssawilk, renjietang) figure out what to do with this for optimal defaults
