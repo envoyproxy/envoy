c49fac50918e41cfbebad872ff2774afc720a546
Yanjun Xiang <yanjunxiang@google.com>
2025-01-21 17:14:09 +0000

    Fix a bug where local replies were incorrectly sent to the ext_proc server
    Signed-off-by: Yanjun Xiang <yanjunxiang@google.com>
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- source/extensions/filters/http/ext_proc/ext_proc.h
+++ source/extensions/filters/http/ext_proc/ext_proc.h
@@ -433,6 +433,18 @@ public:
   const ProcessorState& encodingState() { return encoding_state_; }
   const ProcessorState& decodingState() { return decoding_state_; }
 
+  Envoy::Http::LocalErrorStatus
+  onLocalReply(const Envoy::Http::StreamFilterBase::LocalReplyData&) override {
+    if (Runtime::runtimeFeatureEnabled("envoy.reloadable_features.skip_ext_proc_on_local_reply")) {
+      ENVOY_STREAM_LOG(debug,
+                       "When onLocalReply() is called, set processing_complete_ to true to skip "
+                       "external processing",
+                       *decoder_callbacks_);
+      processing_complete_ = true;
+    }
+    return ::Envoy::Http::LocalErrorStatus::Continue;
+  }
+
 private:
   void mergePerRouteConfig();
   StreamOpenState openStream();
