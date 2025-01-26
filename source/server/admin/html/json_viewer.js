edc43ca22e833b42fda31e3f8b1ae84856536891
Joshua Marantz <jmarantz@google.com>
2025-01-26 10:41:07 -0500

    skip spelling on generated JS file
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

--- source/server/admin/html/json_viewer.js
+++ source/server/admin/html/json_viewer.js
@@ -1,19 +1,21 @@
+// SPELLCHECKER(skip-file)
+
 // Hierarchical Json viewer from https://www.npmjs.com/package/@andypf/json-viewer and
 // https://github.com/andypf/json-viewer.
 //
-// This optimized version was downloaded on Jan 24, 2025 from
+// This minified version was downloaded on Jan 24, 2025 from
 // https://pfau-software.de/json-viewer/dist/iife/index.js.
 
 /*
 
 MIT License
 
-Copyright (c) 2023 `Andreas Pfau`
+Copyright (c) 2023 Andreas Pfau
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
 the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sub-license, and/or sell copies of
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
 the Software, and to permit persons to whom the Software is furnished to do so,
 subject to the following conditions:
 
