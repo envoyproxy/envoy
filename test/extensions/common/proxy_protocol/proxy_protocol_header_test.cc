e799d6c46912f9330796a8c926c93dc7abbf06ff
Ronak Jain <ronakjainc@gmail.com>
2025-02-02 21:06:14 +0000

    move proxy protocol TLV parser into core
    Signed-off-by: Ronak Jain <ronakjainc@gmail.com>

--- test/extensions/common/proxy_protocol/proxy_protocol_header_test.cc
+++ test/extensions/common/proxy_protocol/proxy_protocol_header_test.cc
@@ -271,23 +271,6 @@ TEST(ProxyProtocolHeaderTest, GeneratesV2WithCustomTLVsNoPassthrough) {
   EXPECT_TRUE(TestUtility::buffersEqual(expectedBuff, buff));
 }
 
-TEST(ProxyProtocolHeaderTest, ParseTLVs) {
-  Protobuf::RepeatedPtrField<envoy::config::core::v3::ProxyProtocolTLV> tlvs;
-  auto* tlv = tlvs.Add();
-  tlv->set_type(0x1);
-  tlv->set_value("tlv1");
-  tlv = tlvs.Add();
-  tlv->set_type(0xE1);
-  tlv->set_value("tlv2");
-  const auto tlv_vector = parseTLVs(tlvs);
-
-  EXPECT_EQ(2, tlv_vector.size());
-  EXPECT_EQ(0x1, tlv_vector[0].type);
-  EXPECT_EQ(std::vector<unsigned char>({'t', 'l', 'v', '1'}), tlv_vector[0].value);
-  EXPECT_EQ(0xE1, tlv_vector[1].type);
-  EXPECT_EQ(std::vector<unsigned char>({'t', 'l', 'v', '2'}), tlv_vector[1].value);
-}
-
 } // namespace
 } // namespace ProxyProtocol
 } // namespace Common
