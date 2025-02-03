acca7128b79cf82a41c786adef707d992b73490a
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-02-03 17:00:36 +0000

    fix
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- tools/code_format/check_format.py
+++ tools/code_format/check_format.py
@@ -429,8 +429,7 @@ class FormatChecker:
         return False
 
     def is_external_build_file(self, file_path):
-        return self.is_build_file(file_path) and (
-            file_path.startswith("./bazel/external/"))
+        return self.is_build_file(file_path) and (file_path.startswith("./bazel/external/"))
 
     def is_starlark_file(self, file_path):
         return file_path.endswith(".bzl")
