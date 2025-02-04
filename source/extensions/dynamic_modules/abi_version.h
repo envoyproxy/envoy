dcbbe770bf037244686676824bcd05846c791487
Mikhail Krinkin <mkrinkin@microsoft.com>
2025-02-04 17:44:51 +0000

    Update ABI version sha256 hash given that abi.h was changed
    Signed-off-by: Mikhail Krinkin <mkrinkin@microsoft.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "869dcc448533527a69d9b25dc6e367fb7787e992f9291e71442165b783f19bd1";
+const char* kAbiVersion = "05d8d5fac54ffcfc596312d93fff5d5082cbe37d4aca21fd4270ffa253980f34";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
