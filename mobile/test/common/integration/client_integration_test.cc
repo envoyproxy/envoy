6c3797c691fb5a9a3e11a8d47992856f4208379f
Fredy Wijaya <fredyw@google.com>
2025-02-05 17:58:49 +0000

    Fix failing tests
    Signed-off-by: Fredy Wijaya <fredyw@google.com>

--- mobile/test/common/integration/client_integration_test.cc
+++ mobile/test/common/integration/client_integration_test.cc
@@ -309,6 +309,7 @@ TEST_P(ClientIntegrationTest, DisableDnsRefreshOnNetworkChange) {
   initialize();
 
   internalEngine()->onDefaultNetworkChanged(1);
+  basicTest();
 
   EXPECT_FALSE(found_force_dns_refresh);
 }
