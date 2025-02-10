5ce5a2d006d3c141310096e1f3676e6975348cb5
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-02-10 23:02:07 +0000

    dynamic_modules: adds ClearRouteCache callback
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "05d8d5fac54ffcfc596312d93fff5d5082cbe37d4aca21fd4270ffa253980f34";
+const char* kAbiVersion = "c49e84f9df73b57feac0ab03d8390609e9019ce50d9fb418e45b17b477ce26da";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
