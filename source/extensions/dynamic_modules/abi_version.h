dbae237a72016c6ae19679ae4d4dc41aeb99702e
Takeshi Yoneda <t.y.mathetake@gmail.com>
2025-02-10 23:22:48 +0000

    fix
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "c49e84f9df73b57feac0ab03d8390609e9019ce50d9fb418e45b17b477ce26da";
+const char* kAbiVersion = "9e0126c182a3b4b5cae6f6e4a24246bdafa024e2831e0754df49ec3ccc909b38";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
