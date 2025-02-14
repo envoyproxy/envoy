e7512f19512cdf915fdb1da7ebafc8b18be2657f
zirain <zirain2009@gmail.com>
2025-02-14 11:58:37 +0800

    more test
    Signed-off-by: zirain <zirain2009@gmail.com>

--- test/extensions/tracers/opentelemetry/samplers/cel/config_test.cc
+++ test/extensions/tracers/opentelemetry/samplers/cel/config_test.cc
@@ -13,7 +13,7 @@ namespace Tracers {
 namespace OpenTelemetry {
 
 // Test create sampler via factory
-TEST(CELSamplerFactoryTest, Test) {
+TEST(CELSamplerFactoryTest, TestParsedExpr) {
   auto* factory = Registry::FactoryRegistry<SamplerFactory>::getFactory(
       "envoy.tracers.opentelemetry.samplers.cel");
   ASSERT_NE(factory, nullptr);
@@ -27,6 +27,42 @@ typed_config:
   "@type": type.googleapis.com/envoy.extensions.tracers.opentelemetry.samplers.v3.CELSamplerConfig
   expression:
     parsed_expr:
+      expr:
+        id: 3
+        call_expr:
+          function: _==_
+          args:
+          - id: 2
+            select_expr:
+              operand:
+                id: 1
+                ident_expr:
+                  name: request
+              field: path
+          - id: 4
+            const_expr:
+              string_value: "/test-1234-deny"
+)EOF";
+  TestUtility::loadFromYaml(yaml, typed_config);
+  NiceMock<Server::Configuration::MockTracerFactoryContext> context;
+  EXPECT_NE(factory->createSampler(typed_config.typed_config(), context), nullptr);
+  EXPECT_STREQ(factory->name().c_str(), "envoy.tracers.opentelemetry.samplers.cel");
+}
+
+TEST(CELSamplerFactoryTest, TestCheckedExpr) {
+  auto* factory = Registry::FactoryRegistry<SamplerFactory>::getFactory(
+      "envoy.tracers.opentelemetry.samplers.cel");
+  ASSERT_NE(factory, nullptr);
+  EXPECT_STREQ(factory->name().c_str(), "envoy.tracers.opentelemetry.samplers.cel");
+  EXPECT_NE(factory->createEmptyConfigProto(), nullptr);
+
+  envoy::config::core::v3::TypedExtensionConfig typed_config;
+  const std::string yaml = R"EOF(
+name: envoy.tracers.opentelemetry.samplers.cel
+typed_config:
+  "@type": type.googleapis.com/envoy.extensions.tracers.opentelemetry.samplers.v3.CELSamplerConfig
+  expression:
+    checked_expr:
       expr:
         id: 3
         call_expr:
