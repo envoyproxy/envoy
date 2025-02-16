209c5bedf0c8b7894baad01202616a061bb90e42
Ronak Jain <ronakjainc@gmail.com>
2025-02-16 19:10:26 +0000

    address comments
    Signed-off-by: Ronak Jain <ronakjainc@gmail.com>

--- source/common/proxy_protocol/proxy_protocol.cc
+++ source/common/proxy_protocol/proxy_protocol.cc
@@ -8,8 +8,8 @@ Network::ProxyProtocolTLVVector
 parseTLVs(absl::Span<const envoy::config::core::v3::TlvEntry* const> tlvs) {
   Network::ProxyProtocolTLVVector tlv_vector;
   for (const auto& tlv : tlvs) {
-    const std::vector<unsigned char> value(tlv->value().begin(), tlv->value().end());
-    tlv_vector.push_back({static_cast<uint8_t>(tlv->type()), value});
+    tlv_vector.push_back({static_cast<uint8_t>(tlv->type()),
+                          std::vector<unsigned char>(tlv->value().begin(), tlv->value().end())});
   }
   return tlv_vector;
 }
