5c369a73d624b3dbac2507b269c1c191ee4a8af2
William <wtzhang23@gmail.com>
2025-02-24 09:30:19 -0800

    add bytes test
    Signed-off-by: William <wtzhang23@gmail.com>

--- test/extensions/dynamic_modules/http/factory_test.cc
+++ test/extensions/dynamic_modules/http/factory_test.cc
@@ -49,6 +49,69 @@ filter_config:
   factory_cb(callbacks);
 }
 
+TEST(DynamicModuleConfigFactory, LoadEmpty) {
+  TestEnvironment::setEnvVar(
+      "ENVOY_DYNAMIC_MODULES_SEARCH_PATH",
+      TestEnvironment::substitute("{{ test_rundir }}/test/extensions/dynamic_modules/test_data/c"),
+      1);
+
+  envoy::extensions::filters::http::dynamic_modules::v3::DynamicModuleFilter config;
+  const std::string yaml = R"EOF(
+dynamic_module_config:
+    name: no_op
+    do_not_close: true
+filter_name: foo
+)EOF";
+
+  envoy::extensions::filters::http::dynamic_modules::v3::DynamicModuleFilter proto_config;
+  TestUtility::loadFromYamlAndValidate(yaml, proto_config);
+
+  NiceMock<Server::Configuration::MockFactoryContext> context;
+
+  Envoy::Server::Configuration::DynamicModuleConfigFactory factory;
+  auto result = factory.createFilterFactoryFromProto(proto_config, "", context);
+  EXPECT_TRUE(result.ok());
+  auto factory_cb = result.value();
+  Http::MockFilterChainFactoryCallbacks callbacks;
+
+  EXPECT_CALL(callbacks, addStreamDecoderFilter(testing::_));
+  EXPECT_CALL(callbacks, addStreamEncoderFilter(testing::_));
+  factory_cb(callbacks);
+}
+
+TEST(DynamicModuleConfigFactory, LoadBytes) {
+  TestEnvironment::setEnvVar(
+      "ENVOY_DYNAMIC_MODULES_SEARCH_PATH",
+      TestEnvironment::substitute("{{ test_rundir }}/test/extensions/dynamic_modules/test_data/c"),
+      1);
+
+  envoy::extensions::filters::http::dynamic_modules::v3::DynamicModuleFilter config;
+  const std::string yaml = R"EOF(
+dynamic_module_config:
+    name: no_op
+    do_not_close: true
+filter_name: foo
+filter_config:
+    "@type": "type.googleapis.com/google.protobuf.BytesValue"
+    value: "YmFyCg==" # echo "bar" | base64
+)EOF";
+
+  envoy::extensions::filters::http::dynamic_modules::v3::DynamicModuleFilter proto_config;
+  TestUtility::loadFromYamlAndValidate(yaml, proto_config);
+
+  NiceMock<Server::Configuration::MockFactoryContext> context;
+
+  Envoy::Server::Configuration::DynamicModuleConfigFactory factory;
+  auto result = factory.createFilterFactoryFromProto(proto_config, "", context);
+  EXPECT_TRUE(result.ok());
+  auto factory_cb = result.value();
+  Http::MockFilterChainFactoryCallbacks callbacks;
+
+  EXPECT_CALL(callbacks, addStreamDecoderFilter(testing::_));
+  EXPECT_CALL(callbacks, addStreamEncoderFilter(testing::_));
+  factory_cb(callbacks);
+}
+
 TEST(DynamicModuleConfigFactory, LoadError) {
   TestEnvironment::setEnvVar(
       "ENVOY_DYNAMIC_MODULES_SEARCH_PATH",
