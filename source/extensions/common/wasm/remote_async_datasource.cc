581e818749520d77316863016eb9fda756550586
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2025-02-25 23:18:04 +0100

    Refactor RemoteAsyncDataProvider constructor
    Signed-off-by: Jacek Ewertowski <jacek.ewertowski1@gmail.com>

--- source/extensions/common/wasm/remote_async_datasource.cc
+++ source/extensions/common/wasm/remote_async_datasource.cc
@@ -17,22 +17,12 @@ RemoteAsyncDataProvider::RemoteAsyncDataProvider(
     Upstream::ClusterManager& cm, Init::Manager& manager,
     const envoy::config::core::v3::RemoteDataSource& source, Event::Dispatcher& dispatcher,
     Random::RandomGenerator& random, bool allow_empty, AsyncDataSourceCb&& callback)
-    : allow_empty_(allow_empty), callback_(std::move(callback)),
-      fetcher_(std::make_unique<Config::DataFetcher::RemoteDataFetcher>(cm, source.http_uri(),
-                                                                        source.sha256(), *this)),
-      init_target_("RemoteAsyncDataProvider", [this]() { fetcher_->fetch(); }),
-      retries_remaining_(
-          PROTOBUF_GET_WRAPPED_OR_DEFAULT(source.retry_policy(), num_retries, RetryCount)) {
-
-  auto strategy_or_error = Config::Utility::prepareJitteredExponentialBackOffStrategy(
-      source, random, RetryInitialDelayMilliseconds, RetryMaxDelayMilliseconds);
-  THROW_IF_NOT_OK_REF(strategy_or_error.status());
-  backoff_strategy_ = std::move(strategy_or_error.value());
-
-  retry_timer_ = dispatcher.createTimer([this]() -> void { fetcher_->fetch(); });
-
-  manager.add(init_target_);
-}
+    : RemoteAsyncDataProvider(
+          [this, &cm, &source]() {
+            return std::make_unique<Config::DataFetcher::RemoteDataFetcher>(cm, source.http_uri(),
+                                                                            source.sha256(), *this);
+          },
+          source, manager, dispatcher, random, allow_empty, std::move(callback)){};
 
 RemoteAsyncDataProvider::RemoteAsyncDataProvider(
     CreateFetcherFn&& create_fetcher_fn, const envoy::config::core::v3::RemoteDataSource& source,
