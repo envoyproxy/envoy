931823d0ed092e21953e99312a84c4471e77a324
Francois JACQUES <fjacques@murex.com>
2025-02-28 11:53:40 +0100

    Fix include
    Signed-off-by: Francois JACQUES <fjacques@murex.com>

--- contrib/golang/filters/http/test/test_data/destroyconfig/destroyconfig.h
+++ contrib/golang/filters/http/test/test_data/destroyconfig/destroyconfig.h
@@ -1,12 +1,14 @@
 #pragma once
 #pragma GCC diagnostic ignored "-Wold-style-cast"
-#include "contrib/golang/common/dso/api.h"
+
 #ifdef __cplusplus
+#include "contrib/golang/common/dso/api.h"
 struct httpDestroyableConfig : httpConfig {
     int destroyed;
 } ;
 extern "C" {
 #else
+#include "contrib/golang/common/go/api/api.h"
 typedef struct {
     httpConfig c;
     int destroyed;
