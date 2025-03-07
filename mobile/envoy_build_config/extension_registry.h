05d0599275a457885f1c69b4ba81e5f2c14f467d
Xie Zhihao <xzh1206@gmail.com>
2025-03-07 09:16:53 +0800

    ci: trigger mobile CI
    Signed-off-by: Xie Zhihao <xzh1206@gmail.com>

--- mobile/envoy_build_config/extension_registry.h
+++ mobile/envoy_build_config/extension_registry.h
@@ -3,7 +3,7 @@
 namespace Envoy {
 class ExtensionRegistry {
 public:
-  // As a server, Envoy's static factory registration happens when main is run. However, when
+  // As  a server, Envoy's static factory registration happens when main is run. However, when
   // compiled as a library, there is no guarantee that such registration will happen before the
   // names are needed. The following calls ensure that registration happens before the entities are
   // needed. Note that as more registrations are needed, explicit initialization calls will need to
