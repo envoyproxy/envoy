21838d9c081d5bb9511d7ae7c82162edd0641f45
Zhaoyang Li <zhaoyli311@gmail.com>
2025-03-06 22:07:01 -0800

    update abi version
    Signed-off-by: Zhaoyang Li <zhaoyli311@gmail.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "93073eeb2b5ff44359eed4f5acab49f9c51b7691706b04bfd976b1873c8e4fd9";
+const char* kAbiVersion = "cf448e788b7b565ef583167d94489c93320c234224a50fa4a92f096f2467038d";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
