66f08e5abcaf1ea689bf3db7333dec3ad1f08e5d
Vikas Choudhary (vikasc) <choudharyvikas16@gmail.com>
2025-03-09 17:16:32 +0530

    dynamic_modules: make dynamic module filter a dual filter
    Signed-off-by: Vikas Choudhary (vikasc) <choudharyvikas16@gmail.com>

--- test/extensions/dynamic_modules/http/integration_test.cc
+++ test/extensions/dynamic_modules/http/integration_test.cc
@@ -3,14 +3,38 @@
 #include "test/integration/http_integration.h"
 
 namespace Envoy {
+using HttpFilterProto =
+    envoy::extensions::filters::network::http_connection_manager::v3::HttpFilter;
 class DynamicModulesIntegrationTest : public testing::TestWithParam<Network::Address::IpVersion>,
                                       public HttpIntegrationTest {
 public:
   DynamicModulesIntegrationTest() : HttpIntegrationTest(Http::CodecType::HTTP2, GetParam()){};
 
-  void initializeFilter(
-      const std::string& filter_name, const std::string& config = "",
-      const std::string& type_url = "type.googleapis.com/google.protobuf.StringValue") {
+  const HttpFilterProto getCodecFilterConfig() {
+    HttpFilterProto filter_config;
+    filter_config.set_name("envoy.filters.http.upstream_codec");
+    auto configuration = envoy::extensions::filters::http::upstream_codec::v3::UpstreamCodec();
+    filter_config.mutable_typed_config()->PackFrom(configuration);
+    return filter_config;
+  }
+
+  void addHttpUpstreamFilterToCluster(const HttpFilterProto& config) {
+    config_helper_.addConfigModifier([config](envoy::config::bootstrap::v3::Bootstrap& bootstrap) {
+      auto* cluster = bootstrap.mutable_static_resources()->mutable_clusters(0);
+      ConfigHelper::HttpProtocolOptions protocol_options =
+          MessageUtil::anyConvert<ConfigHelper::HttpProtocolOptions>(
+              (*cluster->mutable_typed_extension_protocol_options())
+                  ["envoy.extensions.upstreams.http.v3.HttpProtocolOptions"]);
+      *protocol_options.add_http_filters() = config;
+      (*cluster->mutable_typed_extension_protocol_options())
+          ["envoy.extensions.upstreams.http.v3.HttpProtocolOptions"]
+              .PackFrom(protocol_options);
+    });
+  }
+  void
+  initializeFilter(const std::string& filter_name, const std::string& config = "",
+                   const std::string& type_url = "type.googleapis.com/google.protobuf.StringValue",
+                   bool upstream_filter = false) {
     TestEnvironment::setEnvVar(
         "ENVOY_DYNAMIC_MODULES_SEARCH_PATH",
         TestEnvironment::substitute(
@@ -31,8 +55,58 @@ typed_config:
 
     config_helper_.addConfigModifier(setEnableDownstreamTrailersHttp1());
     config_helper_.addConfigModifier(setEnableUpstreamTrailersHttp1());
-    config_helper_.prependFilter(fmt::format(filter_config, filter_name, type_url, config));
+    config_helper_.prependFilter(fmt::format(filter_config, filter_name, type_url, config),
+                                 !upstream_filter);
     initialize();
+  }
+  void runHeaderCallbacksTest(bool upstream_filter) {
+    initializeFilter("header_callbacks", "dog:cat",
+                     "type.googleapis.com/google.protobuf.StringValue", upstream_filter);
+    codec_client_ = makeHttpConnection(makeClientConnection((lookupPort("http"))));
+
+    Http::TestRequestHeaderMapImpl request_headers{{"foo", "bar"},
+                                                   {":method", "POST"},
+                                                   {":path", "/test/long/url"},
+                                                   {":scheme", "http"},
+                                                   {":authority", "host"}};
+    Http::TestRequestTrailerMapImpl request_trailers{{"foo", "bar"}};
+    Http::TestResponseHeaderMapImpl response_headers{{":status", "200"}, {"foo", "bar"}};
+    Http::TestResponseTrailerMapImpl response_trailers{{"foo", "bar"}};
+
+    auto encoder_decoder = codec_client_->startRequest(request_headers);
+    auto response = std::move(encoder_decoder.second);
+    codec_client_->sendData(encoder_decoder.first, 10, false);
+    codec_client_->sendTrailers(encoder_decoder.first, request_trailers);
+
+    waitForNextUpstreamRequest();
+    upstream_request_->encodeHeaders(response_headers, false);
+    upstream_request_->encodeData(10, false);
+    upstream_request_->encodeTrailers(response_trailers);
+
+    ASSERT_TRUE(response->waitForEndStream());
+
+    // Verify the proxied request was received upstream, as expected.
+    EXPECT_TRUE(upstream_request_->complete());
+    EXPECT_EQ(10U, upstream_request_->bodyLength());
+    // Verify that the headers/trailers are added as expected.
+    EXPECT_EQ(
+        "cat",
+        upstream_request_->headers().get(Http::LowerCaseString("dog"))[0]->value().getStringView());
+    EXPECT_EQ("cat", upstream_request_->trailers()
+                         .get()
+                         ->get(Http::LowerCaseString("dog"))[0]
+                         ->value()
+                         .getStringView());
+    // Verify the proxied response was received downstream, as expected.
+    EXPECT_TRUE(response->complete());
+    EXPECT_EQ("200", response->headers().Status()->value().getStringView());
+    EXPECT_EQ(10U, response->body().size());
+    // Verify that the headers/trailers are added as expected.
+    EXPECT_EQ("cat",
+              response->headers().get(Http::LowerCaseString("dog"))[0]->value().getStringView());
+    EXPECT_EQ(
+        "cat",
+        response->trailers().get()->get(Http::LowerCaseString("dog"))[0]->value().getStringView());
   }
 };
 
@@ -64,7 +138,13 @@ TEST_P(DynamicModulesIntegrationTest, PassThrough) {
   EXPECT_EQ(10U, response->body().size());
 }
 
-TEST_P(DynamicModulesIntegrationTest, HeaderCallbacks) {
+TEST_P(DynamicModulesIntegrationTest, HeaderCallbacks) { runHeaderCallbacksTest(false); }
+
+TEST_P(DynamicModulesIntegrationTest, HeaderCallbacksWithUpstreamFilter) {
+  runHeaderCallbacksTest(true);
+}
+
+TEST_P(DynamicModulesIntegrationTest, UpstreamFilter) {
   initializeFilter("header_callbacks", "dog:cat");
   codec_client_ = makeHttpConnection(makeClientConnection((lookupPort("http"))));
 
