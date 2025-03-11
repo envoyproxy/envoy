2568d6c1c6c0f0f1ae80d101bacc4ae584d54f2d
Nigel Brittain <nbaws@amazon.com>
2025-03-11 02:25:40 +0000

    stub tests
    Signed-off-by: Nigel Brittain <nbaws@amazon.com>

--- source/extensions/common/aws/signers/sigv4a_common.h
+++ source/extensions/common/aws/signers/sigv4a_common.h
@@ -6,34 +6,33 @@
 using AwsSigningHeaderExclusionVector = std::vector<envoy::type::matcher::v3::StringMatcher>;
 
 namespace Envoy {
-    namespace Extensions {
-    namespace Common {
-    namespace Aws {
-    
+namespace Extensions {
+namespace Common {
+namespace Aws {
 
 class SigV4ASignatureHeaderValues : public SignatureHeaderValues {
-    public:
-      const Http::LowerCaseString RegionSet{"x-amz-region-set"};
-    };
-    
-    using SigV4ASignatureHeaders = ConstSingleton<SigV4ASignatureHeaderValues>;
-    
-    class SigV4ASignatureConstants : public SignatureConstants {
-    public:
-      static constexpr absl::string_view SigV4AAuthorizationHeaderFormat =
-          "AWS4-ECDSA-P256-SHA256 Credential={}, SignedHeaders={}, Signature={}";
-      static constexpr absl::string_view SigV4ACredentialScopeFormat = "{}/{}/aws4_request";
-      static constexpr absl::string_view SigV4ASignatureVersion = "AWS4A";
-      static constexpr absl::string_view SigV4AStringToSignFormat = "{}\n{}\n{}\n{}";
-      static constexpr absl::string_view SigV4AAlgorithm = "AWS4-ECDSA-P256-SHA256";
-    };
-    
-    enum SigV4AKeyDerivationResult {
-      AkdrSuccess,
-      AkdrNextCounter,
-      AkdrFailure,
-    };
-    
+public:
+  const Http::LowerCaseString RegionSet{"x-amz-region-set"};
+};
+
+using SigV4ASignatureHeaders = ConstSingleton<SigV4ASignatureHeaderValues>;
+
+class SigV4ASignatureConstants : public SignatureConstants {
+public:
+  static constexpr absl::string_view SigV4AAuthorizationHeaderFormat =
+      "AWS4-ECDSA-P256-SHA256 Credential={}, SignedHeaders={}, Signature={}";
+  static constexpr absl::string_view SigV4ACredentialScopeFormat = "{}/{}/aws4_request";
+  static constexpr absl::string_view SigV4ASignatureVersion = "AWS4A";
+  static constexpr absl::string_view SigV4AStringToSignFormat = "{}\n{}\n{}\n{}";
+  static constexpr absl::string_view SigV4AAlgorithm = "AWS4-ECDSA-P256-SHA256";
+};
+
+enum SigV4AKeyDerivationResult {
+  AkdrSuccess,
+  AkdrNextCounter,
+  AkdrFailure,
+};
+
 } // namespace Aws
 } // namespace Common
 } // namespace Extensions
