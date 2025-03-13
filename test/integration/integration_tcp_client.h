e151f1f1e3925f3fe38f8b1e2f9ba9be96961a38
Raven Black <ravenblack@dropbox.com>
2025-03-13 20:53:54 +0000

    Words
    Signed-off-by: Raven Black <ravenblack@dropbox.com>

--- test/integration/integration_tcp_client.h
+++ test/integration/integration_tcp_client.h
@@ -82,7 +82,7 @@ using IntegrationTcpClientPtr = std::unique_ptr<IntegrationTcpClient>;
 // between the received data and the matcher.
 // This can't be implemented as a matcher because it has side-effects, and
 // implementing it as a function results in the test line number being lost
-// appearing on failure, so this is an appropriate case for a macro.
+// from the output on failure, so this is an appropriate case for a macro.
 #define EXPECT_TCP_RESPONSE(tcp_client, matcher)                                                   \
   {                                                                                                \
     testing::Matcher<absl::string_view> m = matcher;                                               \
