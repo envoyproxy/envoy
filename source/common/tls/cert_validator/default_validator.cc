e451caf9609b11418ec67f9d29b109e7a966a22c
Kuat <kyessenov@users.noreply.github.com>
2025-04-08 13:57:33 -0700

    fips: update boringssl to use the main branch (#39017)
    Change-Id: I23c1fcf05b4713d848a2f46852525d6c2506986a
    
    Commit Message: Update the BoringSSL FIPS build to use the same commit
    as a regular non-FIPS build (9/13/2024) per the revised FedRAMP policy.
    
    Additional Description: BoringSSL considers the main branch to be the
    "update stream" in the policy. The new recommendation is to use the
    latest stable versions of the compilers, so we also bump Clang version
    to be the same as the regular build (from 14 to 18). Please note that
    this change **does not** include setting the compliance policy, as that
    would be a breaking change.
    
    Risk Level: low (as we match the thoroughly tested non-FIPS build)
    Testing: regression tests passed
    Issue: https://github.com/envoyproxy/envoy/issues/38353
    Docs Changes: none
    Release Notes: yes (it is quite plausible a default might have changed
    in the crypto settings due to the large version bump from 2022 to 2024,
    so this is a notable change).
    
    ---------
    
    Signed-off-by: Kuat Yessenov <kuat@google.com>

--- source/common/tls/cert_validator/default_validator.cc
+++ source/common/tls/cert_validator/default_validator.cc
@@ -562,13 +562,11 @@ absl::Status DefaultCertValidator::addClientValidationContext(SSL_CTX* ctx,
   // Set the verify_depth
   if (config_->maxVerifyDepth().has_value()) {
     uint32_t max_verify_depth = std::min(config_->maxVerifyDepth().value(), uint32_t{INT_MAX});
-#if BORINGSSL_API_VERSION >= 29
     // Older BoringSSLs behave like OpenSSL 1.0.x and exclude the leaf from the
     // depth but include the trust anchor. Newer BoringSSLs match OpenSSL 1.1.x
     // and later in excluding both the leaf and trust anchor. `maxVerifyDepth`
     // documents the older behavior, so adjust the value to match.
     max_verify_depth = max_verify_depth > 0 ? max_verify_depth - 1 : 0;
-#endif
     SSL_CTX_set_verify_depth(ctx, static_cast<int>(max_verify_depth));
   }
   return absl::OkStatus();
