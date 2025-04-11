eb8fad4a02f26a31d3e3e8d1682d9b89811f44a5
Kyle O'Connor <oconnorkyle@google.com>
2025-04-11 17:05:29 -0400

    Implement GetAllHeaders in the golang cluster_specifier plugin.
    Signed-off-by: Kyle O'Connor <oconnorkyle@google.com>

--- contrib/golang/router/cluster_specifier/source/go/pkg/cluster_specifier/api.h
+++ contrib/golang/router/cluster_specifier/source/go/pkg/cluster_specifier/api.h
@@ -6,8 +6,13 @@
 extern "C" {
 #endif
 
+#include <stdint.h> // NOLINT(modernize-deprecated-headers)
+
+uint64_t envoyGoClusterSpecifierGetNumHeaders(unsigned long long header_ptr);
+uint64_t envoyGoClusterSpecifierGetHeadersByteSize(unsigned long long header_ptr);
 int envoyGoClusterSpecifierGetHeader(unsigned long long header_ptr, void* key, void* value);
 void envoyGoClusterSpecifierLogError(unsigned long long plugin_ptr, void* msg);
+void envoyGoClusterSpecifierGetAllHeaders(unsigned long long header_ptr, void* strs, void* buf);
 
 #ifdef __cplusplus
 } // extern "C"
