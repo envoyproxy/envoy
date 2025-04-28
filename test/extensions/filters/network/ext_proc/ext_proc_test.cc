87394699c30bbcc82a0c35b2828ca0ed522f8a29
Adi Suissa-Peleg <adip@google.com>
2025-04-28 18:22:25 +0000

    increase coverage
    Signed-off-by: Adi Suissa-Peleg <adip@google.com>

--- test/extensions/filters/network/ext_proc/ext_proc_test.cc
+++ test/extensions/filters/network/ext_proc/ext_proc_test.cc
@@ -450,6 +450,20 @@ TEST_F(NetworkExtProcFilterTest, ProcessingModeConfigurations) {
   EXPECT_EQ(Network::FilterStatus::Continue, filter_->onWrite(data, false));
 }
 
+// Creates a config without a GrpcService so the default will be used.
+TEST_F(NetworkExtProcFilterTest, DefaultGrpcServiceConfig) {
+  // Set up basic config with failure_mode_allow = false, and remove the
+  // grpc_service field.
+  auto internal_config = createConfig(false);
+  internal_config.mutable_grpc_service()->Clear();
+  auto filter_config = std::make_shared<Config>(internal_config);
+  auto client = std::make_unique<NiceMock<MockExternalProcessorClient>>();
+  client_ = client.get();
+  filter_ = std::make_unique<NetworkExtProcFilter>(filter_config, std::move(client));
+  filter_->initializeReadFilterCallbacks(read_callbacks_);
+  filter_->initializeWriteFilterCallbacks(write_callbacks_);
+}
+
 } // namespace
 } // namespace ExtProc
 } // namespace NetworkFilters
