96021e546d5df3e0d9eaf0eee53bb516c5b43bb4
Misha Efimov <mef@google.com>
2025-04-29 15:07:34 -0400

    Add a fuzz test for orca parser.
    Signed-off-by: Misha Efimov <mef@google.com>

---
+++ test/common/orca/orca_parser_fuzz_test.cc
@@ -0,0 +1,32 @@
+#include <limits>
+
+#include "source/common/common/base64.h"
+#include "source/common/orca/orca_parser.h"
+
+#include "test/common/orca/orca_parser_fuzz.pb.validate.h"
+#include "test/fuzz/fuzz_runner.h"
+#include "test/test_common/status_utility.h"
+#include "test/test_common/utility.h"
+
+#include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
+#include "xds/data/orca/v3/orca_load_report.pb.h"
+
+namespace Envoy {
+namespace Upstream {
+
+DEFINE_PROTO_FUZZER(const test::common::upstream::OrcaParserTestCase& input) {
+  try {
+    TestUtility::validate(input);
+  } catch (const ProtoValidationException& e) {
+    ENVOY_LOG_MISC(debug, "ProtoValidationException: {}", e.what());
+    return;
+  }
+
+  // Try to parse the load report, expect no crash.
+  Http::TestResponseHeaderMapImpl headers{{input.header_name(), input.header_value()}};
+  auto ignored_load_report = Orca::parseOrcaLoadReportHeaders(headers);
+}
+
+} // namespace Upstream
+} // namespace Envoy
