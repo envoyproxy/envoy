fe3e9892a3173688f5b85c111d11af38e8f055e7
Leonor Figueira <leonor.figueira@tecnico.ulisboa.pt>
2025-04-29 12:12:13 +0100

    feat: avoid duplicate local replies by tracking local_reply_sent_
    Signed-off-by: Leonor Figueira <leonor.figueira@tecnico.ulisboa.pt>

--- source/common/http/filter_manager.cc
+++ source/common/http/filter_manager.cc
@@ -1236,12 +1236,13 @@ void FilterManager::encodeHeaders(ActiveStreamEncoderFilter* filter, ResponseHea
   // filter chains.
   auto validate_required_headers = absl::MakeCleanup([&]() {
     const auto status = HeaderUtility::checkRequiredResponseHeaders(headers);
-    if (!status.ok()) {
+    if (!status.ok() && !state.local_reply_sent_) {
       // If the check failed, then we reply with BadGateway, and stop the further processing.
       sendLocalReply(
           Http::Code::BadGateway, status.message(), nullptr, absl::nullopt,
           absl::StrCat(StreamInfo::ResponseCodeDetails::get().FilterRemovedRequiredResponseHeaders,
                        "{", StringUtil::replaceAllEmptySpace(status.message()), "}"));
+      state_.local_reply_sent_ = true;
       should_exit = true;
     }
   });
