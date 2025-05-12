85feab811ad280924c48526d9ae5b30ac7bca85d
Jacob Lisi <jtlisi@google.com>
2025-05-12 11:16:35 -0400

    fix typo
    Signed-off-by: Jacob Lisi <jtlisi@google.com>

--- test/common/http/filter_manager_test.cc
+++ test/common/http/filter_manager_test.cc
@@ -767,7 +767,7 @@ TEST_F(FilterManagerTest, SetSocketOptionTest) {
 
   auto option = std::make_shared<MockSocketOption>();
 
-  EXPECT_CALL(filter_manager_callbacks_, setSocketOption(_)).WillOnce(Return(true));
+  EXPECT_CALL(filter_manager_callbacks_, setDownstreamSocketOption(_)).WillOnce(Return(true));
 
   EXPECT_TRUE(filter_manager_->setDownstreamSocketOption(option));
   filter_manager_->destroyFilters();
