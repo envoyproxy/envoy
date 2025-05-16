7274359ae33b0dd2649f8b54f56e50bec47f01ef
Raven Black <ravenblack@dropbox.com>
2025-05-16 19:38:55 +0000

    Explicit absl::StrCat inclusion
    Signed-off-by: Raven Black <ravenblack@dropbox.com>

--- envoy/common/union_string.h
+++ envoy/common/union_string.h
@@ -6,6 +6,7 @@
 #include "source/common/common/utility.h"
 
 #include "absl/container/inlined_vector.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/variant.h"
 
