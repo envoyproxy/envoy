e63094fd5a3208a70a123650534aed1d8a49a812
wangbaiping(wbpcode) <wbphub@gmail.com>
2025-05-19 13:57:27 +0000

    override host: support multiple addresses parsing and cache parsing result
    Signed-off-by: wangbaiping(wbpcode) <wbphub@gmail.com>

--- source/extensions/load_balancing_policies/override_host/override_host_filter_state.h
+++ source/extensions/load_balancing_policies/override_host/override_host_filter_state.h
@@ -2,6 +2,7 @@
 
 #include "envoy/stream_info/filter_state.h"
 
+#include "absl/strings/str_split.h"
 #include "absl/strings/string_view.h"
 
 namespace Envoy {
@@ -16,15 +17,22 @@ class OverrideHostFilterState : public StreamInfo::FilterState::Object {
 public:
   static constexpr absl::string_view kFilterStateKey =
       "envoy.extensions.load_balancing_policies.override_host.filter_state";
-  OverrideHostFilterState() = default;
 
-  uint64_t fallbackHostIndex() const { return fallback_host_index_; }
-  void setFallbackHostIndex(uint64_t fallback_host_index) {
-    fallback_host_index_ = fallback_host_index;
+  OverrideHostFilterState(std::string&& host_list) : host_list_(std::move(host_list)) {
+    for (absl::string_view host : absl::StrSplit(host_list_, ',', absl::SkipWhitespace())) {
+      selected_hosts_.push_back(absl::StripAsciiWhitespace(host));
+    }
   }
 
+  uint64_t hostIndex() const { return host_index_; }
+  void setHostIndex(uint64_t host_index) { host_index_ = host_index; }
+
+  absl::Span<const absl::string_view> selectedHosts() const { return selected_hosts_; }
+
 private:
-  uint64_t fallback_host_index_ = 1;
+  const std::string host_list_;
+  absl::InlinedVector<const absl::string_view, 8> selected_hosts_;
+  uint64_t host_index_ = 0;
 };
 
 } // namespace OverrideHost
