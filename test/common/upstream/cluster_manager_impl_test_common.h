f55955067ca0491e2578ca81ab0ae66bc0d3592c
yanavlasov <yavlasov@google.com>
2025-05-19 12:04:52 -0400

    Split cluster manager test (#39478)
    In preparation for gcc upgrade split cluster_manager_impl_test into
    smaller suites. Otherwise gcc runs out of memory during compilation.
    
    Risk Level: None
    Testing: Unit tests
    Docs Changes: N/A
    Release Notes: N/A
    Platform Specific Features: N/A
    
    ---------
    
    Signed-off-by: Yan Avlasov <yavlasov@google.com>
    Signed-off-by: Ting Pan <panting@google.com>

---
+++ test/common/upstream/cluster_manager_impl_test_common.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "envoy/common/matchers.h"
+#include "envoy/config/bootstrap/v3/bootstrap.pb.h"
+#include "envoy/network/dns_resolver.h"
+#include "envoy/upstream/host_description.h"
+
+#include "source/common/grpc/context_impl.h"
+#include "source/common/http/context_impl.h"
+#include "source/common/router/context_impl.h"
+
+#include "test/common/upstream/test_cluster_manager.h"
+#include "test/mocks/access_log/mocks.h"
+#include "test/mocks/config/xds_manager.h"
+#include "test/mocks/network/mocks.h"
+#include "test/mocks/server/admin.h"
+#include "test/mocks/server/instance.h"
+#include "test/test_common/registry.h"
+#include "test/test_common/simulated_time_system.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace Envoy {
+namespace Upstream {
+
+using ::envoy::config::bootstrap::v3::Bootstrap;
+using ::testing::NiceMock;
+
+Bootstrap parseBootstrapFromV3Yaml(const std::string& yaml);
+Bootstrap defaultConfig();
+std::string clustersJson(const std::vector<std::string>& clusters);
+
+class HttpPoolDataPeer {
+public:
+  static Http::ConnectionPool::MockInstance* getPool(absl::optional<HttpPoolData> data) {
+    ASSERT(data.has_value());
+    return dynamic_cast<Http::ConnectionPool::MockInstance*>(data.value().pool_);
+  }
+};
+
+class TcpPoolDataPeer {
+public:
+  static Tcp::ConnectionPool::MockInstance* getPool(absl::optional<TcpPoolData> data) {
+    ASSERT(data.has_value());
+    return dynamic_cast<Tcp::ConnectionPool::MockInstance*>(data.value().pool_);
+  }
+};
+
+// Helper to intercept calls to postThreadLocalClusterUpdate.
+class MockLocalClusterUpdate {
+public:
+  MOCK_METHOD(void, post,
+              (uint32_t priority, const HostVector& hosts_added, const HostVector& hosts_removed));
+};
+
+class MockLocalHostsRemoved {
+public:
+  MOCK_METHOD(void, post, (const HostVector&));
+};
+
+class ClusterManagerImplTest : public testing::Test {
+public:
+  ClusterManagerImplTest();
+
+  virtual void create(const Bootstrap& bootstrap);
+
+  void createWithBasicStaticCluster();
+
+  void createWithLocalClusterUpdate(const bool enable_merge_window = true);
+
+  void checkStats(uint64_t added, uint64_t modified, uint64_t removed, uint64_t active,
+                  uint64_t warming);
+
+  void
+  checkConfigDump(const std::string& expected_dump_yaml,
+                  const Matchers::StringMatcher& name_matcher = Matchers::UniversalStringMatcher());
+
+  MetadataConstSharedPtr buildMetadata(const std::string& version) const;
+
+  Event::SimulatedTimeSystem time_system_;
+  NiceMock<TestClusterManagerFactory> factory_;
+  NiceMock<Config::MockXdsManager> xds_manager_;
+  std::unique_ptr<TestClusterManagerImpl> cluster_manager_;
+  AccessLog::MockAccessLogManager log_manager_;
+  NiceMock<Server::MockAdmin> admin_;
+  MockLocalClusterUpdate local_cluster_update_;
+  MockLocalHostsRemoved local_hosts_removed_;
+  Http::ContextImpl http_context_;
+  Grpc::ContextImpl grpc_context_;
+  Router::ContextImpl router_context_;
+  NiceMock<Server::MockInstance> server_;
+  NiceMock<Network::MockDnsResolverFactory> dns_resolver_factory_;
+  Registry::InjectFactory<Network::DnsResolverFactory> registered_dns_factory_;
+};
+
+} // namespace Upstream
+} // namespace Envoy
