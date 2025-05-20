d74af8abd1739d4a167c2f3ee388d1e7eb87ebce
code <wbphub@gmail.com>
2025-05-20 22:13:39 +0800

    clean up router: remove part of DebugConfig and simlify the header mutation (#39534)
    Commit Message: clean up router: remove part of DebugConfig and simlify
    the header mutation
    Additional Description:
    
    In the previous implementation, the respnose header mutation is pretty
    complex. We chained multiple modifications by wrapping multiple layers
    lambda. **This way make the code is hard to follow.**
    
    And there are some limitation and unexpected behavior in the previous
    implmentation:
    1. `response_headers_to_add`/`response_headers_to_remove` only works for
    upstream response and couldn't works for local reply.
    2. No unified header mutation behavior: parts of local response may
    contains debug headers (upstream cluster, upstream host, etc) and parts
    of local response may don't.
    
    This PR make following changes:
    1. Initialize the `modify_headers_` at the start of decodeHeaders and
    make `response_headers_to_add`/`response_headers_to_remove` be part of
    the `modify_headers_` lambda. This ensure the
    `response_headers_to_add`/`response_headers_to_remove` could works for
    local response.
    2. The `modify_headers_` will be used directly in most cases. If
    additional header modification is necessary, another lambda will be used
    to wrap `modify_headers_`. This ensure all response will have unified
    header modification behavior and at most create two layers lambda.
    3. Removed most features of `DebugConfig` because it's feature could be
    replaced by the `response_headers_to_add` and substitution formatter.
    This simplify our core code path.
    4. `do_not_forward` is still kept and a new filter state factory is
    added. Now we can use set_filter_state filter to use the feature
    directly. But note, we won't allowed to configure the header name of the
    do not forward flag.
    
    
    The PR bring big change to the DebugConfig. But DebugConfig is not
    exposed as public API. So, it should be fine.
    
    Risk Level: mid.
    Testing: n/a.
    Docs Changes: n/a.
    Release Notes: n/a.
    Platform Specific Features: n/a.
    
    ---------
    
    Signed-off-by: wangbaiping(wbpcode) <wbphub@gmail.com>

--- source/common/router/debug_config.cc
+++ source/common/router/debug_config.cc
@@ -1,24 +1,22 @@
 #include "source/common/router/debug_config.h"
 
-#include "source/common/common/macros.h"
+#include "envoy/registry/registry.h"
 
 namespace Envoy {
 namespace Router {
 
-DebugConfig::DebugConfig(bool append_cluster, absl::optional<Http::LowerCaseString> cluster_header,
-                         bool append_upstream_host,
-                         absl::optional<Http::LowerCaseString> hostname_header,
-                         absl::optional<Http::LowerCaseString> host_address_header,
-                         bool do_not_forward,
-                         absl::optional<Http::LowerCaseString> not_forwarded_header)
-    : append_cluster_(append_cluster), cluster_header_(std::move(cluster_header)),
-      append_upstream_host_(append_upstream_host), hostname_header_(std::move(hostname_header)),
-      host_address_header_(std::move(host_address_header)), do_not_forward_(do_not_forward),
-      not_forwarded_header_(std::move(not_forwarded_header)) {}
+constexpr absl::string_view DebugConfigKey = "envoy.router.debug_config";
 
-const std::string& DebugConfig::key() {
-  CONSTRUCT_ON_FIRST_USE(std::string, "envoy.router.debug_config");
+absl::string_view DebugConfig::key() { return DebugConfigKey; }
+
+std::string DebugConfigFactory::name() const { return std::string(DebugConfigKey); }
+
+std::unique_ptr<StreamInfo::FilterState::Object>
+DebugConfigFactory::createFromBytes(absl::string_view data) const {
+  return std::make_unique<DebugConfig>(data == "true");
 }
+
+REGISTER_FACTORY(DebugConfigFactory, StreamInfo::FilterState::ObjectFactory);
 
 } // namespace Router
 } // namespace Envoy
