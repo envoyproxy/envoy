cf4e900ee2610bce10ca8863bf46afbed5b33de7
Jacob Lisi <jtlisi@google.com>
2025-05-20 10:59:04 -0400

    update socket option from BOUND to LISTENING
    Signed-off-by: Jacob Lisi <jtlisi@google.com>

--- test/common/network/connection_impl_test.cc
+++ test/common/network/connection_impl_test.cc
@@ -2582,7 +2582,7 @@ TEST_P(ConnectionImplTest, SetSocketOptionTest) {
   setUpBasicConnection();
 
   auto option = std::make_shared<MockSocketOption>();
-  EXPECT_CALL(*option, setOption(_, envoy::config::core::v3::SocketOption::STATE_BOUND))
+  EXPECT_CALL(*option, setOption(_, envoy::config::core::v3::SocketOption::STATE_LISTENING))
       .WillOnce(Return(true));
 
   EXPECT_TRUE(client_connection_->setSocketOption(option));
