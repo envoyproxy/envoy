e17a126c3e723671fd6a463677ced785dbbbc646
Raven Black <ravenblack@dropbox.com>
2025-05-29 11:25:05 -0700

    Make route prefix map matchers able to retry (#38986)
    Commit Message: Make route prefix map matchers able to retry
    Additional Description: The concept of this change is that the current
    behavior is quite surprising; I believe what happens after this change
    is what a user would typically expect the prefix matcher behavior to be,
    but unfortunately changing it in-place would be a behavior change now
    that the existing behavior is established, so this needs a runtime
    guard.
    
    The difference with this version of the prefix match map is that the
    `on_no_match` field still runs if there was a subtree matcher that
    didn't match, whereas the old one requires each subtree to have its own
    `on_no_match`, resulting in a huge amount of duplicate config code for
    our use-case.
    
    Also, before falling back to the on_no_match, this variant tries any
    also-matching shorter prefixes, e.g. a prefix match map with retries
    like
    ```
    "/banana": subtree_a,
    "/ban": subtree_b
    "/": subtree_c
    on_no_match
    ```
    
    With a request path `/banana/is/a/fruit` would first try subtree_a, and
    if conditions in there don't return a match, would try subtree_b, then
    again to subtree_c, then finally the on_no_match action.
    
    Risk Level: A little; runtime guard allows reverting.
    Testing: Increased testing for existing config to ensure current [maybe
    unwanted] behavior, added tests for new behavior.
    Docs Changes: relnotes only.
    Release Notes: Yes.
    Platform Specific Features: n/a
    
    ---------
    
    Signed-off-by: Raven Black <ravenblack@dropbox.com>

--- source/common/matcher/prefix_map_matcher.h
+++ source/common/matcher/prefix_map_matcher.h
@@ -2,6 +2,7 @@
 
 #include "source/common/common/trie_lookup_table.h"
 #include "source/common/matcher/map_matcher.h"
+#include "source/common/runtime/runtime_features.h"
 
 namespace Envoy {
 namespace Matcher {
@@ -30,15 +31,26 @@ protected:
                    absl::optional<OnMatch<DataType>> on_no_match, absl::Status& creation_status)
       : MapMatcher<DataType>(std::move(data_input), std::move(on_no_match), creation_status) {}
 
-  // TODO(ravenblackx): Implement a Reentrant. Re-entry gives access to the
-  // shorter, matching prefixes for callers.
-  absl::optional<OnMatch<DataType>> doMatch(const std::string& data) override {
-    const auto result = children_.findLongestPrefix(data);
-    if (result) {
-      return *result;
+  typename MatchTree<DataType>::MatchResult
+  doMatch(const DataType& data, absl::string_view key,
+          SkippedMatchCb<DataType> skipped_match_cb) override {
+    const absl::InlinedVector<std::shared_ptr<OnMatch<DataType>>, 4> results =
+        children_.findMatchingPrefixes(key);
+    bool retry_shorter = Runtime::runtimeFeatureEnabled(
+        "envoy.reloadable_features.prefix_map_matcher_resume_after_subtree_miss");
+    for (auto it = results.rbegin(); it != results.rend(); ++it) {
+      const std::shared_ptr<OnMatch<DataType>>& on_match = *it;
+      typename MatchTree<DataType>::MatchResult result =
+          MatchTree<DataType>::handleRecursionAndSkips(*on_match, data, skipped_match_cb);
+      if (result.match_state_ != MatchState::MatchComplete || result.on_match_.has_value() ||
+          !retry_shorter) {
+        // If the match failed to complete, or if it matched, or
+        // if we're doing the legacy "don't try additional matchers"
+        // behavior, return whatever the first match's result was.
+        return result;
+      }
     }
-
-    return absl::nullopt;
+    return {MatchState::MatchComplete, absl::nullopt};
   }
 
 private:
