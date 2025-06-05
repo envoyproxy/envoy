f250702397edec6b4636facd243a444cfca6055f
Adi Suissa-Peleg <adip@google.com>
2025-06-05 13:56:48 +0000

    match_delegate: remove redundant bool data member
    Signed-off-by: Adi Suissa-Peleg <adip@google.com>

--- source/extensions/filters/http/match_delegate/config.cc
+++ source/extensions/filters/http/match_delegate/config.cc
@@ -105,7 +105,7 @@ void DelegatingStreamFilter::FilterMatchState::evaluateMatchTree(
   }
 
   // If no match tree is set, interpret as a skip.
-  if (!has_match_tree_) {
+  if (match_tree_ == nullptr) {
     skip_filter_ = true;
     match_tree_evaluated_ = true;
     return;
