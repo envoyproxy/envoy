70c07feda15e68ce08edfaa7a61fbb94b51e00de
Yuval Kohavi <yuval.kohavi@gmail.com>
2025-06-10 06:22:43 -0400

    fix test
    Signed-off-by: Yuval Kohavi <yuval.kohavi@gmail.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "6880e51e622847350697658aa1390e04f8adde82adcab3d5ea6cfa1a5e604695";
+const char* kAbiVersion = "b6ac25de6a80b56593bcec2fc20d0de9685d5ca948e97d8b9c544e00ebd52f8e";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
