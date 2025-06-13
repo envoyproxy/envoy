af73032f6f41a8edab5e24f96bd02b13b1cd3613
wangbaiping(wbpcode) <wbphub@gmail.com>
2025-06-13 15:19:04 +0000

    fix test
    Signed-off-by: wangbaiping(wbpcode) <wbphub@gmail.com>

--- test/common/router/delegating_route_impl_test.cc
+++ test/common/router/delegating_route_impl_test.cc
@@ -39,8 +39,9 @@ TEST(DelegatingRoute, DelegatingRouteTest) {
 
 // Verify that DelegatingRouteEntry class forwards all calls to internal base route.
 TEST(DelegatingRouteEntry, DelegatingRouteEntryTest) {
-  const std::shared_ptr<MockRoute> base_route_ptr = std::make_shared<MockRoute>();
-  const std::shared_ptr<MockRouteEntry> inner_object_ptr = std::make_shared<MockRouteEntry>();
+  const std::shared_ptr<MockRoute> base_route_ptr = std::make_shared<NiceMock<MockRoute>>();
+  const std::shared_ptr<MockRouteEntry> inner_object_ptr =
+      std::make_shared<NiceMock<MockRouteEntry>>();
   ON_CALL(*base_route_ptr, directResponseEntry()).WillByDefault(Return(nullptr));
   EXPECT_CALL(*base_route_ptr, routeEntry).WillOnce(Return(inner_object_ptr.get()));
 
