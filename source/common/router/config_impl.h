e56265521a1d513dcfa2314c56b03d699c9a8f83
wangbaiping(wbpcode) <wbphub@gmail.com>
2025-06-17 08:14:49 +0000

    revert unnecessary chagne
    Signed-off-by: wangbaiping(wbpcode) <wbphub@gmail.com>

--- source/common/router/config_impl.h
+++ source/common/router/config_impl.h
@@ -964,7 +964,7 @@ public:
     };
     const std::string& routeName() const override { return parent_->routeName(); }
 
-  protected:
+  private:
     const RouteEntryAndRoute* parent_;
 
     // If a DynamicRouteEntry instance is created and returned to the caller directly, then keep an
@@ -975,9 +975,7 @@ public:
     // avoid possible circular reference. For example, the WeightedClusterEntry (derived from
     // DynamicRouteEntry) will be member of the RouteEntryImplBase, so the owner_ should be nullptr.
     const RouteConstSharedPtr owner_;
-    // The dynamic route entry will not be accessed cross thread and it is safe to mark this be
-    // mutable.
-    mutable std::string cluster_name_;
+    const std::string cluster_name_;
   };
 
   /**
