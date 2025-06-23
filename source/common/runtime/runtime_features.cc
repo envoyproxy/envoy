c01fecb5f19cc21bd39284847915dcae7716ea7a
Ryan Hamilton <rch@google.com>
2025-06-23 06:59:51 -0700

    http3: Change Envoy's HTTP/3 implementation to validate pseudo headers (#39615)
    Can be disabled by setting
    ``envoy.restart_features.do_not_validate_http3_pseudo_headers`` to
    false.
    
    ---------
    
    Signed-off-by: Ryan Hamilton <rch@google.com>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -114,6 +114,7 @@ RUNTIME_GUARD(envoy_restart_features_allow_slot_destroy_on_worker_threads);
 RUNTIME_GUARD(envoy_restart_features_quic_handle_certs_with_shared_tls_code);
 RUNTIME_GUARD(envoy_restart_features_udp_read_normalize_addresses);
 RUNTIME_GUARD(envoy_restart_features_use_fast_protobuf_hash);
+RUNTIME_GUARD(envoy_restart_features_validate_http3_pseudo_headers);
 
 // Begin false flags. Most of them should come with a TODO to flip true.
 
