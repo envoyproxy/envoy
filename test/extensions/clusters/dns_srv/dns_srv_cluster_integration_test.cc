9d0fa1e15d0dc5c021a9b0cd27093e51e72188f9
Mikhail Galanin <mikhail@perplexity.ai>
2025-07-05 16:17:27 +0100

    Removed redundand comment

--- test/extensions/clusters/dns_srv/dns_srv_cluster_integration_test.cc
+++ test/extensions/clusters/dns_srv/dns_srv_cluster_integration_test.cc
@@ -23,46 +23,6 @@ public:
       : HttpIntegrationTest(Envoy::Http::CodecType::HTTP1, GetParam()),
         registered_dns_factory_(dns_resolver_factory_) {}
 
-  // void initialize() override {
-  // setUpstreamCount(num_upstreams_);
-  // setDeterministicValue();
-  // config_helper_.renameListener("redis_proxy");
-
-  // Change the port for each of the discovery host in cluster_0.
-  // config_helper_.addConfigModifier([this](envoy::config::bootstrap::v3::Bootstrap& bootstrap) {
-  //   uint32_t upstream_idx = 0;
-  //   auto* cluster_0 = bootstrap.mutable_static_resources()->mutable_clusters(0);
-  //   if (version_ == Network::Address::IpVersion::v4) {
-  //     cluster_0->set_dns_lookup_family(envoy::config::cluster::v3::Cluster::V4_ONLY);
-  //   }
-  //   for (int j = 0; j < cluster_0->load_assignment().endpoints_size(); ++j) {
-  //     auto locality_lb = cluster_0->mutable_load_assignment()->mutable_endpoints(j);
-  //     for (int k = 0; k < locality_lb->lb_endpoints_size(); ++k) {
-  //       auto lb_endpoint = locality_lb->mutable_lb_endpoints(k);
-  //       if (lb_endpoint->endpoint().address().has_socket_address()) {
-  //         auto* host_socket_addr =
-  //             lb_endpoint->mutable_endpoint()->mutable_address()->mutable_socket_address();
-  //         RELEASE_ASSERT(fake_upstreams_.size() > upstream_idx, "");
-  //         host_socket_addr->set_address(
-  //             fake_upstreams_[upstream_idx]->localAddress()->ip()->addressAsString());
-  //         host_socket_addr->set_port_value(
-  //             fake_upstreams_[upstream_idx++]->localAddress()->ip()->port());
-  //       }
-  //     }
-  //   }
-  // });
-
-  // on_server_ready_function_ = [this](Envoy::IntegrationTestServer& test_server) {
-  //   mock_rng_ = dynamic_cast<Random::MockRandomGenerator*>(
-  //       &(test_server.server().api().randomGenerator()));
-  //   // Abort now if we cannot downcast the server's random number generator pointer.
-  //   ASSERT_TRUE(mock_rng_ != nullptr);
-  //   // Ensure that fake_upstreams_[0] is the load balancer's host of choice by default.
-  //   ON_CALL(*mock_rng_, random()).WillByDefault(Return(random_index_));
-  // };
-
-  // HttpIntegrationTest::initialize();
-  // }
   void createUpstreams() override { HttpIntegrationTest::createUpstreams(); }
 
   NiceMock<Envoy::Network::MockDnsResolverFactory> dns_resolver_factory_;
