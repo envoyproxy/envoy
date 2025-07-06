8b7012f4c502bdc36b347a261da9c6e9ad9ed471
Ashesh Vidyut <asheshvidyut@google.com>
2025-07-06 16:33:23 +0530

    trie hybrid tests similar to trie lookup tests

--- test/common/common/trie_hybrid_test.cc
+++ test/common/common/trie_hybrid_test.cc
@@ -3,69 +3,108 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
+using testing::ElementsAre;
+
 namespace Envoy {
 namespace Common {
 
-TEST(TrieHybridTest, BasicOperations) {
-    TrieHybrid<std::string, const void*> hybrid;
-    
-    // Insert and retrieve a short key
-    auto [hybrid1, oldVal1, didUpdate1] = hybrid.insert("short", "value1");
-    EXPECT_FALSE(didUpdate1);
-    EXPECT_EQ(hybrid1.len(), 0); // Only long keys contribute to len()
-    
-    // Insert and retrieve a long key
-    auto [hybrid2, oldVal2, didUpdate2] = hybrid1.insert("very_long_key_that_exceeds_eight_characters", "value2");
-    EXPECT_FALSE(didUpdate2);
-    EXPECT_EQ(hybrid2.len(), 1);
-    
-    // Get operations
-    auto result1 = hybrid2.Get("short");
-    EXPECT_TRUE(result1.has_value());
-    EXPECT_EQ(result1.value(), static_cast<const void*>("value1"));
-    
-    auto result2 = hybrid2.Get("very_long_key_that_exceeds_eight_characters");
-    EXPECT_TRUE(result2.has_value());
-    EXPECT_EQ(result2.value(), static_cast<const void*>("value2"));
-    
-    // LongestPrefix only works for long keys
-    auto longest1 = hybrid2.LongestPrefix("very_long_key_that_exceeds_eight_characters");
-    EXPECT_TRUE(longest1.found);
-    EXPECT_EQ(longest1.key, "very_long_key_that_exceeds_eight_characters");
-    EXPECT_EQ(longest1.val, static_cast<const void*>("value2"));
-    
-    // findMatchingPrefixes returns exact match for short key
-    auto matches = hybrid2.findMatchingPrefixes("short");
-    EXPECT_EQ(matches.size(), 1);
-    EXPECT_EQ(matches[0].first, "short");
-    EXPECT_EQ(matches[0].second, static_cast<const void*>("value1"));
+TEST(TrieHybrid, AddItems) {
+  TrieHybrid<std::string, const char*> trie;
+  const char* cstr_a = "a";
+  const char* cstr_b = "b";
+  const char* cstr_c = "c";
+
+  auto [trie1, _, didUpdate1] = trie.insert("foo", cstr_a);
+  EXPECT_TRUE(didUpdate1);
+  auto [trie2, _, didUpdate2] = trie1.insert("bar", cstr_b);
+  EXPECT_TRUE(didUpdate2);
+  EXPECT_EQ(cstr_a, trie2.Get("foo").value());
+  EXPECT_EQ(cstr_b, trie2.Get("bar").value());
+
+  // overwrite_existing = false (not supported in TrieHybrid, so we test overwrite)
+  auto [trie3, _, didUpdate3] = trie2.insert("foo", cstr_c);
+  EXPECT_TRUE(didUpdate3);
+  EXPECT_EQ(cstr_c, trie3.Get("foo").value());
 }
 
-TEST(TrieHybridTest, CrossTreeOperations) {
-    TrieHybrid<std::string, const void*> hybrid;
-    
-    // Insert keys of different lengths
-    auto [hybrid1, _, __] = hybrid.insert("short", "trie_value");
-    auto [hybrid2, ___, ____] = hybrid1.insert("very_long_key_for_radix_tree", "radix_value");
-    
-    // Get operations for both keys
-    auto trie_result = hybrid2.Get("short");
-    EXPECT_TRUE(trie_result.has_value());
-    EXPECT_EQ(trie_result.value(), static_cast<const void*>("trie_value"));
-    
-    auto radix_result = hybrid2.Get("very_long_key_for_radix_tree");
-    EXPECT_TRUE(radix_result.has_value());
-    EXPECT_EQ(radix_result.value(), static_cast<const void*>("radix_value"));
-    
-    // LongestPrefix only works for long keys
-    auto longest = hybrid2.LongestPrefix("very_long_key_for_radix_tree");
-    EXPECT_TRUE(longest.found);
-    EXPECT_EQ(longest.key, "very_long_key_for_radix_tree");
-    EXPECT_EQ(longest.val, static_cast<const void*>("radix_value"));
-    
-    // LongestPrefix for short key returns not found
-    auto longest2 = hybrid2.LongestPrefix("short");
-    EXPECT_FALSE(longest2.found);
+TEST(TrieHybrid, LongestPrefix) {
+  TrieHybrid<std::string, const char*> trie;
+  const char* cstr_a = "a";
+  const char* cstr_b = "b";
+  const char* cstr_c = "c";
+  const char* cstr_d = "d";
+  const char* cstr_e = "e";
+  const char* cstr_f = "f";
+
+  auto [trie1, _, didUpdate1] = trie.insert("foo", cstr_a);
+  EXPECT_TRUE(didUpdate1);
+  auto [trie2, _, didUpdate2] = trie1.insert("bar", cstr_b);
+  EXPECT_TRUE(didUpdate2);
+  auto [trie3, _, didUpdate3] = trie2.insert("baro", cstr_c);
+  EXPECT_TRUE(didUpdate3);
+  auto [trie4, _, didUpdate4] = trie3.insert("foo/bar", cstr_d);
+  EXPECT_TRUE(didUpdate4);
+  auto [trie5, _, didUpdate5] = trie4.insert("barn", cstr_e);
+  EXPECT_TRUE(didUpdate5);
+  auto [trie6, _, didUpdate6] = trie5.insert("barp", cstr_f);
+  EXPECT_TRUE(didUpdate6);
+
+  EXPECT_EQ(cstr_a, trie6.Get("foo").value());
+  EXPECT_EQ(cstr_a, trie6.LongestPrefix("foo").val);
+  auto matches1 = trie6.findMatchingPrefixes("foo");
+  EXPECT_THAT(matches1 | testing::Each(testing::Field(&std::pair<std::string, const char*>::second, testing::_)), ElementsAre(cstr_a));
+  EXPECT_EQ(cstr_a, trie6.LongestPrefix("foosball").val);
+  auto matches2 = trie6.findMatchingPrefixes("foosball");
+  EXPECT_THAT(matches2 | testing::Each(testing::Field(&std::pair<std::string, const char*>::second, testing::_)), ElementsAre(cstr_a));
+  EXPECT_EQ(cstr_a, trie6.LongestPrefix("foo/").val);
+  auto matches3 = trie6.findMatchingPrefixes("foo/");
+  EXPECT_THAT(matches3 | testing::Each(testing::Field(&std::pair<std::string, const char*>::second, testing::_)), ElementsAre(cstr_a));
+  EXPECT_EQ(cstr_d, trie6.LongestPrefix("foo/bar").val);
+  auto matches4 = trie6.findMatchingPrefixes("foo/bar");
+  EXPECT_THAT(matches4 | testing::Each(testing::Field(&std::pair<std::string, const char*>::second, testing::_)), ElementsAre(cstr_a, cstr_d));
+  EXPECT_EQ(cstr_d, trie6.LongestPrefix("foo/bar/zzz").val);
+  auto matches5 = trie6.findMatchingPrefixes("foo/bar/zzz");
+  EXPECT_THAT(matches5 | testing::Each(testing::Field(&std::pair<std::string, const char*>::second, testing::_)), ElementsAre(cstr_a, cstr_d));
+
+  EXPECT_EQ(cstr_b, trie6.Get("bar").value());
+  EXPECT_EQ(cstr_b, trie6.LongestPrefix("bar").val);
+  auto matches6 = trie6.findMatchingPrefixes("bar");
+  EXPECT_THAT(matches6 | testing::Each(testing::Field(&std::pair<std::string, const char*>::second, testing::_)), ElementsAre(cstr_b));
+  EXPECT_EQ(cstr_b, trie6.LongestPrefix("baritone").val);
+  auto matches7 = trie6.findMatchingPrefixes("baritone");
+  EXPECT_THAT(matches7 | testing::Each(testing::Field(&std::pair<std::string, const char*>::second, testing::_)), ElementsAre(cstr_b));
+  EXPECT_EQ(cstr_c, trie6.LongestPrefix("barometer").val);
+  auto matches8 = trie6.findMatchingPrefixes("barometer");
+  EXPECT_THAT(matches8 | testing::Each(testing::Field(&std::pair<std::string, const char*>::second, testing::_)), ElementsAre(cstr_b, cstr_c));
+
+  EXPECT_EQ(cstr_e, trie6.Get("barn").value());
+  EXPECT_EQ(cstr_e, trie6.LongestPrefix("barnacle").val);
+  auto matches9 = trie6.findMatchingPrefixes("barnacle");
+  EXPECT_THAT(matches9 | testing::Each(testing::Field(&std::pair<std::string, const char*>::second, testing::_)), ElementsAre(cstr_b, cstr_e));
+
+  EXPECT_EQ(cstr_f, trie6.Get("barp").value());
+  EXPECT_EQ(cstr_f, trie6.LongestPrefix("barpomus").val);
+  auto matches10 = trie6.findMatchingPrefixes("barpomus");
+  EXPECT_THAT(matches10 | testing::Each(testing::Field(&std::pair<std::string, const char*>::second, testing::_)), ElementsAre(cstr_b, cstr_f));
+
+  EXPECT_FALSE(trie6.Get("toto").has_value());
+  EXPECT_FALSE(trie6.LongestPrefix("toto").found);
+  auto matches11 = trie6.findMatchingPrefixes("toto");
+  EXPECT_THAT(matches11 | testing::Each(testing::Field(&std::pair<std::string, const char*>::second, testing::_)), ElementsAre());
+  EXPECT_FALSE(trie6.Get(" ").has_value());
+  EXPECT_FALSE(trie6.LongestPrefix(" ").found);
+  auto matches12 = trie6.findMatchingPrefixes(" ");
+  EXPECT_THAT(matches12 | testing::Each(testing::Field(&std::pair<std::string, const char*>::second, testing::_)), ElementsAre());
+}
+
+TEST(TrieHybrid, VeryDeepTrieDoesNotStackOverflowOnDestructor) {
+  TrieHybrid<std::string, const char*> trie;
+  const char* cstr_a = "a";
+
+  std::string key_a(20960, 'a');
+  auto [trie1, _, didUpdate1] = trie.insert(key_a, cstr_a);
+  EXPECT_TRUE(didUpdate1);
+  EXPECT_EQ(cstr_a, trie1.Get(key_a).value());
 }
 
 } // namespace Common
