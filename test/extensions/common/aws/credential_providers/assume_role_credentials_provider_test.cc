c73a84e8c532ba58f8ae242261999884715643fa
Nigel Brittain <nbaws@amazon.com>
2025-07-09 07:36:34 +0000

    test case coverage
    Signed-off-by: Nigel Brittain <nbaws@amazon.com>

--- test/extensions/common/aws/credential_providers/assume_role_credentials_provider_test.cc
+++ test/extensions/common/aws/credential_providers/assume_role_credentials_provider_test.cc
@@ -629,6 +629,210 @@ TEST_F(AssumeRoleCredentialsProviderTest, Coverage) {
   timer_->invokeCallback();
 }
 
+
+TEST_F(AssumeRoleCredentialsProviderTest, WithSessionDuration) {
+  timer_ = new NiceMock<Event::MockTimer>(&context_.dispatcher_);
+  
+  expectDocument(200, std::move(R"EOF(
+{
+  "AssumeRoleResponse": {
+    "AssumeRoleResult": {
+      "Credentials": {
+        "AccessKeyId": "test-access-key",
+        "SecretAccessKey": "test-secret-key",
+        "SessionToken": "test-session-token"
+      }
+    }
+  }
+}
+)EOF"));
+
+  // Setup provider with session duration
+  ON_CALL(context_, clusterManager()).WillByDefault(ReturnRef(cluster_manager_));
+  envoy::extensions::common::aws::v3::AssumeRoleCredentialProvider cred_provider = {};
+  cred_provider.set_role_arn("aws:iam::123456789012:role/arn");
+  cred_provider.set_role_session_name("role-session-name");
+  cred_provider.mutable_session_duration()->set_seconds(3600); 
+
+  mock_manager_ = std::make_shared<MockAwsClusterManager>();
+  EXPECT_CALL(*mock_manager_, getUriFromClusterName(_))
+      .WillRepeatedly(Return("sts.region.amazonaws.com:443"));
+
+  auto cluster_name = "credentials_provider_cluster";
+  envoy::extensions::common::aws::v3::AwsCredentialProvider defaults;
+  envoy::extensions::common::aws::v3::EnvironmentCredentialProvider env_provider;
+  TestEnvironment::setEnvVar("AWS_ACCESS_KEY_ID", "akid", 1);
+  TestEnvironment::setEnvVar("AWS_SECRET_ACCESS_KEY", "secret", 1);
+  TestEnvironment::setEnvVar("AWS_SESSION_TOKEN", "token", 1);
+
+  defaults.mutable_environment_credential_provider()->CopyFrom(env_provider);
+  auto credentials_provider_chain =
+      std::make_shared<Extensions::Common::Aws::CommonCredentialsProviderChain>(
+          context_, "region", defaults);
+  auto signer = std::make_unique<SigV4SignerImpl>(
+      STS_SERVICE_NAME, "region", credentials_provider_chain, context_,
+      Extensions::Common::Aws::AwsSigningHeaderExclusionVector{});
+
+  provider_ = std::make_shared<AssumeRoleCredentialsProvider>(
+      context_, mock_manager_, cluster_name,
+      [this](Upstream::ClusterManager&, absl::string_view) {
+        metadata_fetcher_.reset(raw_metadata_fetcher_);
+        return std::move(metadata_fetcher_);
+      },
+      "region", MetadataFetcher::MetadataReceiver::RefreshState::Ready,
+      std::chrono::seconds(2), std::move(signer), cred_provider);
+
+  timer_->enableTimer(std::chrono::milliseconds(1), nullptr);
+  EXPECT_CALL(*timer_, enableTimer(_, nullptr));
+
+  auto provider_friend = MetadataCredentialsProviderBaseFriend(provider_);
+  provider_friend.onClusterAddOrUpdate();
+  timer_->invokeCallback();
+
+  const auto credentials = provider_->getCredentials();
+  EXPECT_TRUE(credentials.accessKeyId().has_value());
+  EXPECT_EQ("test-access-key", credentials.accessKeyId().value());
+}
+
+TEST_F(AssumeRoleCredentialsProviderTest, SigningFailure) {
+  timer_ = new NiceMock<Event::MockTimer>(&context_.dispatcher_);
+  
+  ON_CALL(context_, clusterManager()).WillByDefault(ReturnRef(cluster_manager_));
+  envoy::extensions::common::aws::v3::AssumeRoleCredentialProvider cred_provider = {};
+  cred_provider.set_role_arn("aws:iam::123456789012:role/arn");
+  cred_provider.set_role_session_name("role-session-name");
+
+  mock_manager_ = std::make_shared<MockAwsClusterManager>();
+  EXPECT_CALL(*mock_manager_, getUriFromClusterName(_))
+      .WillRepeatedly(Return("sts.region.amazonaws.com:443"));
+
+  auto cluster_name = "credentials_provider_cluster";
+  
+  // Create a signer with invalid credentials to trigger signing failure
+  envoy::extensions::common::aws::v3::AwsCredentialProvider invalid_defaults;
+  envoy::extensions::common::aws::v3::EnvironmentCredentialProvider invalid_env_provider;
+  TestEnvironment::setEnvVar("AWS_ACCESS_KEY_ID", "", 1);
+  TestEnvironment::setEnvVar("AWS_SECRET_ACCESS_KEY", "", 1);
+  TestEnvironment::setEnvVar("AWS_SESSION_TOKEN", "", 1);
+  
+  invalid_defaults.mutable_environment_credential_provider()->CopyFrom(invalid_env_provider);
+  auto invalid_credentials_provider_chain =
+      std::make_shared<Extensions::Common::Aws::CommonCredentialsProviderChain>(
+          context_, "region", invalid_defaults);
+  auto mock_signer = std::make_unique<SigV4SignerImpl>(
+      STS_SERVICE_NAME, "region", invalid_credentials_provider_chain, context_,
+      Extensions::Common::Aws::AwsSigningHeaderExclusionVector{});
+
+  provider_ = std::make_shared<AssumeRoleCredentialsProvider>(
+      context_, mock_manager_, cluster_name,
+      [this](Upstream::ClusterManager&, absl::string_view) {
+        metadata_fetcher_.reset(raw_metadata_fetcher_);
+        return std::move(metadata_fetcher_);
+      },
+      "region", MetadataFetcher::MetadataReceiver::RefreshState::Ready,
+      std::chrono::seconds(2), std::move(mock_signer), cred_provider);
+
+  timer_->enableTimer(std::chrono::milliseconds(1), nullptr);
+
+  auto provider_friend = MetadataCredentialsProviderBaseFriend(provider_);
+  provider_friend.onClusterAddOrUpdate();
+  timer_->invokeCallback();
+
+  const auto credentials = provider_->getCredentials();
+  EXPECT_FALSE(credentials.accessKeyId().has_value());
+}
+
+
+TEST_F(AssumeRoleCredentialsProviderTest, TimerDisableAndFetcherCancel) {
+  timer_ = new NiceMock<Event::MockTimer>(&context_.dispatcher_);
+  
+  expectDocument(200, std::move(R"EOF(
+{
+  "AssumeRoleResponse": {
+    "AssumeRoleResult": {
+      "Credentials": {
+        "AccessKeyId": "test-access-key",
+        "SecretAccessKey": "test-secret-key",
+        "SessionToken": "test-session-token"
+      }
+    }
+  }
+}
+)EOF"));
+
+  setupProvider();
+  
+  timer_->enableTimer(std::chrono::milliseconds(1), nullptr);
+  EXPECT_CALL(*timer_, enableTimer(_, nullptr));
+
+  auto provider_friend = MetadataCredentialsProviderBaseFriend(provider_);
+  provider_friend.onClusterAddOrUpdate();
+  timer_->invokeCallback();
+
+  const auto credentials = provider_->getCredentials();
+  EXPECT_TRUE(credentials.accessKeyId().has_value());
+}
+
+
+TEST_F(AssumeRoleCredentialsProviderTest, AsyncCallbackSetup) {
+  timer_ = new NiceMock<Event::MockTimer>(&context_.dispatcher_);
+  
+  expectDocument(200, std::move(R"EOF(
+{
+  "AssumeRoleResponse": {
+    "AssumeRoleResult": {
+      "Credentials": {
+        "AccessKeyId": "test-access-key",
+        "SecretAccessKey": "test-secret-key",
+        "SessionToken": "test-session-token"
+      }
+    }
+  }
+}
+)EOF"));
+
+  setupProvider();
+  timer_->enableTimer(std::chrono::milliseconds(1), nullptr);
+  EXPECT_CALL(*timer_, enableTimer(_, nullptr));
+
+  auto provider_friend = MetadataCredentialsProviderBaseFriend(provider_);
+  provider_friend.onClusterAddOrUpdate();
+  timer_->invokeCallback(); 
+
+  const auto credentials = provider_->getCredentials();
+  EXPECT_TRUE(credentials.accessKeyId().has_value());
+}
+
+
+TEST_F(AssumeRoleCredentialsProviderTest, CredentialsPendingFlag) {
+  timer_ = new NiceMock<Event::MockTimer>(&context_.dispatcher_);
+  
+  expectDocument(200, std::move(R"EOF(
+{
+  "AssumeRoleResponse": {
+    "AssumeRoleResult": {
+      "Credentials": {
+        "AccessKeyId": "test-access-key",
+        "SecretAccessKey": "test-secret-key",
+        "SessionToken": "test-session-token"
+      }
+    }
+  }
+}
+)EOF"));
+
+  setupProvider();
+  timer_->enableTimer(std::chrono::milliseconds(1), nullptr);
+  EXPECT_CALL(*timer_, enableTimer(_, nullptr));
+
+  auto provider_friend = MetadataCredentialsProviderBaseFriend(provider_);
+  provider_friend.onClusterAddOrUpdate();
+  timer_->invokeCallback(); 
+
+  const auto credentials = provider_->getCredentials();
+  EXPECT_TRUE(credentials.accessKeyId().has_value());
+}
+
 } // namespace Aws
 } // namespace Common
 } // namespace Extensions
