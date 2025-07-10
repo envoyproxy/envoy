3927f24855c4c59207be03ce0738c4d549b8721b
Rohit Agrawal <rohit.agrawal@databricks.com>
2025-07-10 09:17:59 -0700

    Update source/extensions/filters/http/dynamic_forward_proxy/proxy_filter.cc
    Co-authored-by: yanavlasov <yavlasov@google.com>
    Signed-off-by: Rohit Agrawal <rohit.agrawal@databricks.com>

--- source/extensions/filters/http/dynamic_forward_proxy/proxy_filter.cc
+++ source/extensions/filters/http/dynamic_forward_proxy/proxy_filter.cc
@@ -199,7 +199,7 @@ bool ProxyFilter::isProxying() {
 Http::FilterHeadersStatus ProxyFilter::decodeHeaders(Http::RequestHeaderMap& headers, bool) {
   Router::RouteConstSharedPtr route = decoder_callbacks_->route();
   const Router::RouteEntry* route_entry = route ? route->routeEntry() : nullptr;
-  if (!route || !((route_entry = route->routeEntry()))) {
+  if (!route_entry) {
     return Http::FilterHeadersStatus::Continue;
   }
 
