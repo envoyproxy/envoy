8875b82cfa8d7e6ea365b4e4679c5ed33d7c96cd
antoniovleonti <leonti@google.com>
2025-07-14 13:21:54 +0000

    Fix: Avoid adding empty trusted_ca in CertificateValidationContextSdsApi
    Signed-off-by: antoniovleonti <leonti@google.com>

--- test/common/secret/sds_api_test.cc
+++ test/common/secret/sds_api_test.cc
@@ -649,6 +649,37 @@ TEST_F(SdsApiTest, DynamicCertificateValidationContextUpdateSuccess) {
             cvc_config->caCert());
 }
 
+// Validate that CertificateValidationContextSdsApi does not add an empty trusted_ca
+// if it was not present in the original config.
+TEST_F(SdsApiTest, CertificateValidationContextNoTrustedCa) {
+  envoy::config::core::v3::ConfigSource config_source;
+  setupMocks();
+  CertificateValidationContextSdsApi sds_api(
+      config_source, "abc.com", subscription_factory_, time_system_, validation_visitor_, stats_,
+      []() {}, *dispatcher_, *api_);
+  init_manager_.add(*sds_api.initTarget());
+
+  NiceMock<Secret::MockSecretCallbacks> secret_callback;
+  auto handle = sds_api.addUpdateCallback(
+      [&secret_callback]() { return secret_callback.onAddOrUpdateSecret(); });
+
+  std::string yaml =
+      R"EOF(
+  name: "abc.com"
+  validation_context:
+    allow_expired_certificate: true
+  )EOF";
+
+  envoy::extensions::transport_sockets::tls::v3::Secret typed_secret;
+  TestUtility::loadFromYaml(yaml, typed_secret);
+  const auto decoded_resources = TestUtility::decodeResources({typed_secret});
+  EXPECT_CALL(secret_callback, onAddOrUpdateSecret());
+  initialize();
+  EXPECT_TRUE(subscription_factory_.callbacks_->onConfigUpdate(decoded_resources.refvec_, "").ok());
+
+  EXPECT_FALSE(sds_api.secret()->has_trusted_ca());
+}
+
 class CvcValidationCallback {
 public:
   virtual ~CvcValidationCallback() = default;
