0e61f277aa108de468c308f417d6ceea84f94c38
Luwei Ge <lwge@google.com>
2025-07-14 15:17:30 +0000

    use char[] and add change log
    Signed-off-by: Luwei Ge <lwge@google.com>

--- source/extensions/transport_sockets/alts/alts_channel_pool.cc
+++ source/extensions/transport_sockets/alts/alts_channel_pool.cc
@@ -21,7 +21,7 @@ namespace Alts {
 
 // TODO(matthewstevenson88): Extend this to be configurable through API.
 constexpr std::size_t ChannelPoolSize = 10;
-constexpr absl::string_view UseGrpcExperimentalAltsHandshakerKeepaliveParams =
+constexpr char UseGrpcExperimentalAltsHandshakerKeepaliveParams[] =
     "GRPC_EXPERIMENTAL_ALTS_HANDSHAKER_KEEPALIVE_PARAMS";
 
 // 10 seconds
@@ -35,8 +35,7 @@ AltsChannelPool::create(absl::string_view handshaker_service_address) {
   channel_pool.reserve(ChannelPoolSize);
   grpc::ChannelArguments channel_args;
   channel_args.SetInt(GRPC_ARG_USE_LOCAL_SUBCHANNEL_POOL, 1);
-  const char* keep_alive =
-      std::getenv(std::string(UseGrpcExperimentalAltsHandshakerKeepaliveParams).c_str());
+  const char* keep_alive = std::getenv(UseGrpcExperimentalAltsHandshakerKeepaliveParams);
   if (keep_alive != nullptr && std::strcmp(keep_alive, "true") == 0) {
     channel_args.SetInt(GRPC_ARG_KEEPALIVE_TIMEOUT_MS, ExperimentalKeepAliveTimeoutMs);
     channel_args.SetInt(GRPC_ARG_KEEPALIVE_TIME_MS, ExperimentalKeepAliveTimeMs);
