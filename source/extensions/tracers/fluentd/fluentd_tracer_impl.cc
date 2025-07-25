fb9507eec22650c7b0e557a3ffa6b55451328885
code <wbphub@gmail.com>
2025-07-26 04:15:38 +0800

    tracing: support get all to avoid full interation (#40423)
    Added `getAll()` method to the `TraceContextHandler`. Then the
    opentelemtry/fluentd needn't to iterate the whole header map to find all
    `tracestate` headers/values.
    
    Risk Level: low.
    Testing: n/a.
    Docs Changes: n/a.
    Release Notes: n/a.
    
    Signed-off-by: wangbaiping(wbpcode) <wbphub@gmail.com>

--- source/extensions/tracers/fluentd/fluentd_tracer_impl.cc
+++ source/extensions/tracers/fluentd/fluentd_tracer_impl.cc
@@ -89,21 +89,11 @@ absl::StatusOr<SpanContext> SpanContextExtractor::extractSpanContext() {
   // it is invalid and MUST be discarded. Because we're already checking for the
   // traceparent header above, we don't need to check here.
   // See https://www.w3.org/TR/trace-context/#processing-model-for-working-with-trace-context
-  absl::string_view tracestate_key = FluentdConstants::get().TRACE_STATE.key();
-  std::vector<std::string> tracestate_values;
-  // Multiple tracestate header fields MUST be handled as specified by RFC7230 Section 3.2.2 Field
-  // Order.
-  trace_context_.forEach(
-      [&tracestate_key, &tracestate_values](absl::string_view key, absl::string_view value) {
-        if (key == tracestate_key) {
-          tracestate_values.push_back(std::string{value});
-        }
-        return true;
-      });
-  std::string tracestate = absl::StrJoin(tracestate_values, ",");
+  const auto tracestate_values = FluentdConstants::get().TRACE_STATE.getAll(trace_context_);
 
-  SpanContext span_context(version, trace_id, parent_id, sampled, tracestate);
-  return span_context;
+  SpanContext parent_context(version, trace_id, parent_id, sampled,
+                             absl::StrJoin(tracestate_values, ","));
+  return parent_context;
 }
 
 // Define default version and trace context construction// Define default version and trace context
