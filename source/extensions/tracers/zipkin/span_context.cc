50751bdcae997b6606826d7714ff8020064411f8
WangBaiping <wbphub@gmail.com>
2025-07-30 16:23:45 +0000

    remove unncessary file
    Signed-off-by: WangBaiping <wbphub@gmail.com>

--- source/extensions/tracers/zipkin/span_context.cc
+++
@@ -1,9 +0,0 @@
-#include "source/extensions/tracers/zipkin/span_context.h"
-
-namespace Envoy {
-namespace Extensions {
-namespace Tracers {
-namespace Zipkin {} // namespace Zipkin
-} // namespace Tracers
-} // namespace Extensions
-} // namespace Envoy
