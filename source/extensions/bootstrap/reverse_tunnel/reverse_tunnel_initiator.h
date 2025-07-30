89c3489fbaffe308e7b1d0c13b33ffe106698990
Basundhara Chakrabarty <basundhara.c@nutanix.com>
2025-07-30 01:00:24 +0000

    temp commit: remove external deps
    Signed-off-by: Basundhara Chakrabarty <basundhara.c@nutanix.com>

--- source/extensions/bootstrap/reverse_tunnel/reverse_tunnel_initiator.h
+++ source/extensions/bootstrap/reverse_tunnel/reverse_tunnel_initiator.h
@@ -24,7 +24,6 @@
 #include "source/common/network/io_socket_handle_impl.h"
 #include "source/common/network/socket_interface.h"
 #include "source/common/upstream/load_balancer_context_base.h"
-#include "source/extensions/bootstrap/reverse_tunnel/factory_base.h"
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
