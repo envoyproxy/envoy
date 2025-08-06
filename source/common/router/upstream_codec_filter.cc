3e49e15c2b1c03e275ef7fa995831589e28b8a98
Rohit Agrawal <rohit.agrawal@databricks.com>
2025-08-06 23:26:50 +0200

    quic: deprecate flag report_stream_reset_error_code and remove legacy code paths (#40592)
    This PR removes the deprecated reloadable flag
    `report_stream_reset_error_code` and cleans up the legacy paths.
    
    Fix https://github.com/envoyproxy/envoy/issues/38051
    
    ---
    
    **Commit Message:** router: deprecate flag
    report_stream_reset_error_code and remove legacy code paths
    **Additional Description:** Remove the deprecated reloadable flag
    `report_stream_reset_error_code` and cleans up the legacy paths.
    **Risk Level:** Low
    **Testing:** CI
    **Docs Changes:** N/A
    **Release Notes:** Added
    
    Signed-off-by: Rohit Agrawal <rohit.agrawal@databricks.com>

--- source/common/router/upstream_codec_filter.cc
+++ source/common/router/upstream_codec_filter.cc
@@ -233,14 +233,7 @@ void UpstreamCodecFilter::CodecBridge::onResetStream(Http::StreamResetReason rea
 
   std::string failure_reason(transport_failure_reason);
   if (reason == Http::StreamResetReason::LocalReset) {
-    if (!Runtime::runtimeFeatureEnabled(
-            "envoy.reloadable_features.report_stream_reset_error_code")) {
-      ASSERT(transport_failure_reason.empty());
-      // Use this to communicate to the upstream request to not force-terminate.
-      failure_reason = "codec_error";
-    } else {
-      failure_reason = absl::StrCat(transport_failure_reason, "|codec_error");
-    }
+    failure_reason = absl::StrCat(transport_failure_reason, "|codec_error");
   }
   filter_.callbacks_->resetStream(reason, failure_reason);
 }
