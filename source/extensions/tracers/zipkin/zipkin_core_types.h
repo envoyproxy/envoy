9e39b179238bb23e41ad1492332a54cfc2bc627b
WangBaiping <wbphub@gmail.com>
2025-08-07 09:20:01 +0000

    resolve conflict
    Signed-off-by: WangBaiping <wbphub@gmail.com>

--- source/extensions/tracers/zipkin/zipkin_core_types.h
+++ source/extensions/tracers/zipkin/zipkin_core_types.h
@@ -543,6 +543,10 @@ public:
    */
   SpanContext spanContext() const;
 
+  void setIgnoreDecision(bool ignore_decision) {
+    ignore_decision_ = ignore_decision;
+  }
+
   // Tracing::Span
 
   /**
@@ -557,6 +561,11 @@ public:
   void setTag(absl::string_view name, absl::string_view value) override;
   void log(SystemTime timestamp, const std::string& event) override;
   void setSampled(bool val) override { sampled_ = val; }
+  void setDecision(bool decision) override {
+    if(!ignore_decision_) {
+      setSampled(decision);
+    }
+  }
   void setOperation(absl::string_view operation) override { setName(std::string(operation)); }
   void injectContext(Tracing::TraceContext& trace_context,
                      const Tracing::UpstreamContext&) override;
@@ -584,6 +593,7 @@ private:
   int64_t monotonic_start_time_{0};
   TimeSource& time_source_;
   TracerInterface& tracer_;
+  bool ignore_decision_{false};
 };
 
 using SpanPtr = std::unique_ptr<Span>;
