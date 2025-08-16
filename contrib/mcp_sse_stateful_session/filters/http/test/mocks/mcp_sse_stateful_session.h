074a4c300f1fea672debd098a0ca3f9e575714dd
jue-yin <jueyin.hsl@alibaba-inc.com>
2025-08-16 05:22:55 +0000

    fix format
    Signed-off-by: jue-yin <jueyin.hsl@alibaba-inc.com>

--- contrib/mcp_sse_stateful_session/filters/http/test/mocks/mcp_sse_stateful_session.h
+++ contrib/mcp_sse_stateful_session/filters/http/test/mocks/mcp_sse_stateful_session.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "envoy/http/mcp_sse_stateful_session.h"
+
+#include "source/common/protobuf/protobuf.h"
+
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "source/common/protobuf/protobuf.h"
 
 namespace Envoy {
 namespace Http {
