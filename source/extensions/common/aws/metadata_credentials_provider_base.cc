836ad0fe5d92333c345acf0cca33e5a97f8b31a7
Nigel Brittain <nbaws@amazon.com>
2025-08-20 23:49:57 +0000

    update test case
    Signed-off-by: Nigel Brittain <nbaws@amazon.com>

--- source/extensions/common/aws/metadata_credentials_provider_base.cc
+++ source/extensions/common/aws/metadata_credentials_provider_base.cc
@@ -34,10 +34,13 @@ MetadataCredentialsProviderBase::MetadataCredentialsProviderBase(
 void MetadataCredentialsProviderBase::onClusterAddOrUpdate() {
   ENVOY_LOG(debug, "Received callback from aws cluster manager for cluster {}", cluster_name_);
   if (!cache_duration_timer_) {
+    std::weak_ptr<MetadataCredentialsProviderStats> weak_stats = stats_;
     std::weak_ptr<MetadataCredentialsProviderBase> weak_self = shared_from_this();
-    cache_duration_timer_ = context_.mainThreadDispatcher().createTimer([weak_self]() -> void {
+    cache_duration_timer_ = context_.mainThreadDispatcher().createTimer([weak_stats, weak_self]() -> void {
+      if (auto stats = weak_stats.lock()) {
+        stats->credential_refreshes_performed_.inc();
+      }
       if (auto self = weak_self.lock()) {
-        self->stats_->credential_refreshes_performed_.inc();
         self->refresh();
       }
     });
