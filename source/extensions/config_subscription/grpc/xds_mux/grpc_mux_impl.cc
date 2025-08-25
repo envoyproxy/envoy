032e1257d19a8e6387f7fc9771fb42bd7ccdfab2
Rama Chavali <rama.rao@salesforce.com>
2025-08-25 14:07:31 +0530

    fix ConnectedStateValue enum to have proper enum case
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>

--- source/extensions/config_subscription/grpc/xds_mux/grpc_mux_impl.cc
+++ source/extensions/config_subscription/grpc/xds_mux/grpc_mux_impl.cc
@@ -77,7 +77,7 @@ std::unique_ptr<GrpcStreamInterface<RQ, RS>> GrpcMuxImpl<S, F, RQ, RS>::createGr
           return std::make_unique<GrpcStream<RQ, RS>>(
               callbacks, std::move(async_client), service_method, dispatcher, scope,
               std::move(backoff_strategy), rate_limit_settings,
-              GrpcStream<RQ, RS>::ConnectedStateValue::FIRST_ENTRY);
+              GrpcStream<RQ, RS>::ConnectedStateValue::FirstEntry);
         },
         /*failover_stream_creator=*/
         failover_async_client
@@ -92,7 +92,7 @@ std::unique_ptr<GrpcStreamInterface<RQ, RS>> GrpcMuxImpl<S, F, RQ, RS>::createGr
                         // be the same as the primary source.
                         std::make_unique<FixedBackOffStrategy>(
                             GrpcMuxFailover<RQ, RS>::DefaultFailoverBackoffMilliseconds),
-                        rate_limit_settings, GrpcStream<RQ, RS>::ConnectedStateValue::SECOND_ENTRY);
+                        rate_limit_settings, GrpcStream<RQ, RS>::ConnectedStateValue::SecondEntry);
                   })
             : absl::nullopt,
         /*grpc_mux_callbacks=*/*this,
@@ -101,7 +101,7 @@ std::unique_ptr<GrpcStreamInterface<RQ, RS>> GrpcMuxImpl<S, F, RQ, RS>::createGr
   return std::make_unique<GrpcStream<RQ, RS>>(this, std::move(async_client), service_method,
                                               dispatcher_, scope, std::move(backoff_strategy),
                                               rate_limit_settings,
-                                              GrpcStream<RQ, RS>::ConnectedStateValue::FIRST_ENTRY);
+                                              GrpcStream<RQ, RS>::ConnectedStateValue::FirstEntry);
 }
 
 template <class S, class F, class RQ, class RS> GrpcMuxImpl<S, F, RQ, RS>::~GrpcMuxImpl() {
