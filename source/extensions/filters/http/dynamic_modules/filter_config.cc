c2ea7c6ecd39c1cbc024dc1c7014db5dad8ca715
Ian Kerins <git@isk.haus>
2025-09-04 20:19:42 -0400

    dynamic_modules: add support for user-defined metrics (#40661)
    Commit Message: dynamic_modules: add support for user-defined metrics
    Additional Description: Similar to how it is done in wasm:
    - DynamicModuleHttpFilterConfig now holds a Stats::Scope. All metrics
    declared in this scope are placed in a custom stat namespace so as to
    not collide with envoy-defined metrics. Using
    Stats::CustomStatNamespaces, this namespace prefix is stripped from all
    stat names before they are emitted, e.g. to Prometheus format.
    - Add new ABI "method"s on
    envoy_dynamic_module_http_filter_envoy_config_ptr that allow metrics to
    be declared in the scope of the filter config. These methods return
    opaque pointers to Stats::{Counter,Gauge,Histogram} that allow said
    declared metrics to be manipulated. Right now, only the basics of each
    metric type are exposed in the ABI. For example, you can't read metric
    values, or reset metrics to 0, or...
    - Add corresponding wrappers to the Rust SDK.
    - Add tests, both against the C++ ABI implementation, and against the
    Rust SDK.
    
    Risk Level: Low, in that this is a new feature in the experimental
    dynamic modules system.
    Testing: Tests for both the C++ implementation in isolation, and in the
    rust "integration" tests.
    Docs Changes: There are not any docs covering specific dynamic modules
    features (yet?), so I've added nothing beyond doc comments.
    Release Notes: 
    Platform Specific Features: N/A
    
    ---------
    
    Signed-off-by: Ian Kerins <git@isk.haus>

--- source/extensions/filters/http/dynamic_modules/filter_config.cc
+++ source/extensions/filters/http/dynamic_modules/filter_config.cc
@@ -7,9 +7,11 @@ namespace HttpFilters {
 
 DynamicModuleHttpFilterConfig::DynamicModuleHttpFilterConfig(
     const absl::string_view filter_name, const absl::string_view filter_config,
-    Extensions::DynamicModules::DynamicModulePtr dynamic_module,
+    Extensions::DynamicModules::DynamicModulePtr dynamic_module, Stats::Scope& stats_scope,
     Server::Configuration::ServerFactoryContext& context)
-    : cluster_manager_(context.clusterManager()), filter_name_(filter_name),
+    : cluster_manager_(context.clusterManager()), stats_scope_(stats_scope.createScope("")),
+      stat_name_pool_(stats_scope_->symbolTable()),
+      custom_stat_namespace_(stat_name_pool_.add(CustomStatNamespace)), filter_name_(filter_name),
       filter_config_(filter_config), dynamic_module_(std::move(dynamic_module)) {};
 
 DynamicModuleHttpFilterConfig::~DynamicModuleHttpFilterConfig() {
@@ -34,7 +36,7 @@ newDynamicModuleHttpPerRouteConfig(const absl::string_view per_route_config_name
               "envoy_dynamic_module_on_http_filter_per_route_config_new");
   RETURN_IF_NOT_OK_REF(constructor.status());
 
-  auto destroy = dynamic_module->getFunctionPointer<OnHttpPerRouteConfigDestoryType>(
+  auto destroy = dynamic_module->getFunctionPointer<OnHttpPerRouteConfigDestroyType>(
       "envoy_dynamic_module_on_http_filter_per_route_config_destroy");
   RETURN_IF_NOT_OK_REF(destroy.status());
 
@@ -49,17 +51,16 @@ newDynamicModuleHttpPerRouteConfig(const absl::string_view per_route_config_name
                                                                        destroy.value());
 }
 
-absl::StatusOr<DynamicModuleHttpFilterConfigSharedPtr>
-newDynamicModuleHttpFilterConfig(const absl::string_view filter_name,
-                                 const absl::string_view filter_config,
-                                 Extensions::DynamicModules::DynamicModulePtr dynamic_module,
-                                 Server::Configuration::ServerFactoryContext& context) {
+absl::StatusOr<DynamicModuleHttpFilterConfigSharedPtr> newDynamicModuleHttpFilterConfig(
+    const absl::string_view filter_name, const absl::string_view filter_config,
+    Extensions::DynamicModules::DynamicModulePtr dynamic_module, Stats::Scope& stats_scope,
+    Server::Configuration::ServerFactoryContext& context) {
   auto constructor =
       dynamic_module->getFunctionPointer<decltype(&envoy_dynamic_module_on_http_filter_config_new)>(
           "envoy_dynamic_module_on_http_filter_config_new");
   RETURN_IF_NOT_OK_REF(constructor.status());
 
-  auto on_config_destroy = dynamic_module->getFunctionPointer<OnHttpConfigDestoryType>(
+  auto on_config_destroy = dynamic_module->getFunctionPointer<OnHttpConfigDestroyType>(
       "envoy_dynamic_module_on_http_filter_config_destroy");
   RETURN_IF_NOT_OK_REF(on_config_destroy.status());
 
@@ -108,8 +109,8 @@ newDynamicModuleHttpFilterConfig(const absl::string_view filter_name,
       "envoy_dynamic_module_on_http_filter_scheduled");
   RETURN_IF_NOT_OK_REF(on_scheduled.status());
 
-  auto config = std::make_shared<DynamicModuleHttpFilterConfig>(filter_name, filter_config,
-                                                                std::move(dynamic_module), context);
+  auto config = std::make_shared<DynamicModuleHttpFilterConfig>(
+      filter_name, filter_config, std::move(dynamic_module), stats_scope, context);
 
   const void* filter_config_envoy_ptr =
       (*constructor.value())(static_cast<void*>(config.get()), filter_name.data(),
@@ -117,6 +118,8 @@ newDynamicModuleHttpFilterConfig(const absl::string_view filter_name,
   if (filter_config_envoy_ptr == nullptr) {
     return absl::InvalidArgumentError("Failed to initialize dynamic module");
   }
+
+  config->stat_creation_frozen_ = true;
 
   config->in_module_config_ = filter_config_envoy_ptr;
   config->on_http_filter_config_destroy_ = on_config_destroy.value();
