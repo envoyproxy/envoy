84290a07854dd1acae2e0815fb04c98f85a8ff6e
Cliff Chen <cliff.chen@nokia-sbell.com>
2025-09-05 09:56:33 +0800

    Ext/Filter/http/Tap: Record the upstream connection information in the HTTP buffer trace output (#40875)
    Signed-off-by: fchen7 <cliff.chen@nokia-sbell.com>

--- source/extensions/filters/http/tap/tap_filter.h
+++ source/extensions/filters/http/tap/tap_filter.h
@@ -100,9 +100,7 @@ public:
   void setDecoderFilterCallbacks(Http::StreamDecoderFilterCallbacks& callbacks) override {
     HttpTapConfigSharedPtr config = config_->currentConfig();
     if (config != nullptr) {
-      auto streamId = callbacks.streamId();
-      auto connection = callbacks.connection();
-      tapper_ = config->createPerRequestTapper(config_->getTapConfig(), streamId, connection);
+      tapper_ = config->createPerRequestTapper(config_->getTapConfig(), callbacks);
     } else {
       tapper_ = nullptr;
     }
