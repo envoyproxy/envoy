7070ea257729e1e6099197f3c64878a64d2b0c57
William Zhang <wtzhang23@gmail.com>
2025-09-11 16:53:07 -0700

    bump abi version
    Signed-off-by: William Zhang <wtzhang23@gmail.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "56f837023ff81a080b9c04a828b00132a4e9cdd0b0e7b908432eb3edbffec044";
+const char* kAbiVersion = "269e796e3b26c0978bf03c452550b488648c36d49b2ce43ea228efbbdd2d7ec1";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
