d453d844b3ebb4c7a5be8dd5c4853ffa991b214a
antoniovleonti <leonti@google.com>
2025-09-24 19:15:08 +0000

    use rejectResponse instead of duping that code
    Signed-off-by: antoniovleonti <leonti@google.com>

--- source/extensions/filters/http/ext_authz/ext_authz.cc
+++ source/extensions/filters/http/ext_authz/ext_authz.cc
@@ -875,16 +875,7 @@ void Filter::onComplete(Filters::Common::ExtAuthz::ResponsePtr&& response) {
     }
 
     if (request_headers_->size() > request_headers_->maxHeadersCount()) {
-      ENVOY_STREAM_LOG(
-          debug,
-          "ext_authz filter rejected the request because the header count limit would be exceeded",
-          *decoder_callbacks_);
-      decoder_callbacks_->streamInfo().setResponseFlag(
-          StreamInfo::CoreResponseFlag::UnauthorizedExternalService);
-      decoder_callbacks_->sendLocalReply(
-          Http::Code::InternalServerError,
-          "request headers too large after ext_authz modifications", nullptr, absl::nullopt,
-          Filters::Common::ExtAuthz::ResponseCodeDetails::get().AuthzError);
+      rejectResponse();
       return;
     }
 
