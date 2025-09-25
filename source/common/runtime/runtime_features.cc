413d3153d5bb2ffcc48194f96b77dccf970301af
Pradeep Rao <84025829+pradeepcrao@users.noreply.github.com>
2025-09-25 15:32:46 -0400

    xDS/LRS: Allow sharing of xDS client connections for ADS and LRS (#40884)
    Signed-off-by: pcrao <pcrao@google.com>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -179,7 +179,9 @@ FALSE_RUNTIME_GUARD(envoy_reloadable_features_remove_legacy_route_formatter);
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_enable_new_dns_implementation);
 // Force a local reply from upstream envoy for reverse connections.
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_reverse_conn_force_local_reply);
-
+// TODO(pradeepcrao): Create a config option to enable this instead after
+// testing.
+FALSE_RUNTIME_GUARD(envoy_restart_features_use_cached_grpc_client_for_xds);
 // Block of non-boolean flags. Use of int flags is deprecated. Do not add more.
 ABSL_FLAG(uint64_t, re2_max_program_size_error_level, 100, ""); // NOLINT
 ABSL_FLAG(uint64_t, re2_max_program_size_warn_level,            // NOLINT
