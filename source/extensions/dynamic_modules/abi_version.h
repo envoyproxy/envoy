67f6717bd52d9308c7f6163704ad90dcc0a3866a
Anuraag Agrawal <anuraaga@gmail.com>
2025-09-27 14:13:01 +0900

    Fix abi_version
    Signed-off-by: Anuraag Agrawal <anuraaga@gmail.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "85b0d501c5b354bb077cfbe61e210aacc5b21c0574813152aeb147f0abea77fd";
+const char* kAbiVersion = "d819ca29c55aef116efacb718671b370e3a5aed9bbc3056ea334c2c57e77fa0b";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
