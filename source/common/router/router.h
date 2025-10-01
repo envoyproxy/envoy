197d7912288d6c8e0ef25f0b676362147019de7e
Ben Plotnick <bplotnick@netflix.com>
2025-10-01 21:40:16 +0000

    Handle null downstream connection
    Signed-off-by: Ben Plotnick <bplotnick@netflix.com>

--- source/common/router/router.h
+++ source/common/router/router.h
@@ -373,11 +373,14 @@ public:
     }
 
     absl::optional<Protobuf::Struct> connection_metadata;
-    const auto& connection_fm =
-        downstreamConnection()->streamInfo().dynamicMetadata().filter_metadata();
-    if (const auto it = connection_fm.find(Envoy::Config::MetadataFilters::get().ENVOY_LB);
-        it != connection_fm.end()) {
-      connection_metadata = it->second;
+    const auto* downstream_conn = downstreamConnection();
+    if (downstream_conn != nullptr) {
+      const auto& connection_fm =
+          downstream_conn->streamInfo().dynamicMetadata().filter_metadata();
+      if (const auto it = connection_fm.find(Envoy::Config::MetadataFilters::get().ENVOY_LB);
+          it != connection_fm.end()) {
+        connection_metadata = it->second;
+      }
     }
 
     absl::optional<Protobuf::Struct> request_metadata;
