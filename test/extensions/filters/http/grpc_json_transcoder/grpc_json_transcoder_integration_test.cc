envoyproxy/envoy

commit 46227d85aaa71fc0a183f279d6869498e28c6651

[json_transcoder_filter] Cap grpc stream frame size (#41115)
Commit Message: [json_transcoder_filter] Cap grpc stream frame size
Additional Description: If the downstream sends data quickly enough, a
`decodeData` buffer can contain over 4MB. The upstream grpc service by
default will respond with an error if it receives such a frame, that
error is surprisingly mapped to a 429 by envoy, and the upstream service
won't see it because the grpc library never delivered it, which makes it
very difficult to figure out what happened.
This change makes it so if the `decodeData` buffer is larger than 1MB
while streaming, the buffer is broken into 1MB pieces, thereby avoiding
that error (and completing an ancient TODO).
This is a behavior change, but should be largely indistinguishable from
previous behavior as there are no guarantees about how many buffers the
`decodeData` stream is delivered in - it could already have been doing
1MB frames for 5MB of data, or 32KB frames, or really any size. As such
I don't think the change merits a runtime guard.
Risk Level: Very small, only a [beneficial] behavior change when
receiving large buffers.
Testing: Added test.
Docs Changes: n/a
Release Notes: Yes
Platform Specific Features: n/a

---------

Signed-off-by: Raven Black <ravenblack@dropbox.com>

source/extensions/filters/http/grpc_json_transcoder/json_transcoder_filter.cc
@@ -594,12 +594,23 @@ Http::FilterDataStatus JsonTranscoderFilter::decodeData(Buffer::Instance& data, 
   if (method_->request_type_is_http_body_) {
     stats_->transcoder_request_buffer_bytes_.add(data.length());
     request_data_.move(data);
-    if (decoderBufferLimitReached(request_data_.length())) {
+    if (!method_->descriptor_->client_streaming() &&
+        decoderBufferLimitReached(request_data_.length())) {
       return Http::FilterDataStatus::StopIterationNoBuffer;
     }
 
-    // TODO(euroelessar): Upper bound message size for streaming case.
-    if (end_stream || method_->descriptor_->client_streaming()) {
+    if (method_->descriptor_->client_streaming()) {
+      // To avoid sending a grpc frame larger than 4MB (which grpc will by default reject),
+      // split the input buffer into 1MB pieces until the buffer is smaller than 1MB.
+      Buffer::OwnedImpl remaining_request_data;
+      remaining_request_data.move(request_data_);
+      while (remaining_request_data.length() > 0) {
+        uint64_t piece_size =
+            std::min(remaining_request_data.length(), JsonTranscoderConfig::MaxStreamedPieceSize);
+        request_data_.move(remaining_request_data, piece_size);
+        maybeSendHttpBodyRequestMessage(&data);
+      }
+    } else if (end_stream) {
       maybeSendHttpBodyRequestMessage(&data);
     } else {
       // TODO(euroelessar): Avoid buffering if content length is already known.

source/extensions/filters/http/grpc_json_transcoder/json_transcoder_filter.h
@@ -50,6 +50,11 @@ public:
           proto_config,
       Api::Api& api);
 
+  // grpc by default doesn't like a frame larger than 4MB. Splitting streamed data
+  // into 1MB pieces should keep that threshold from being exceeded when data comes
+  // in as a large buffer.
+  static constexpr size_t MaxStreamedPieceSize = 1024 * 1024;
+
   /**
    * Create an instance of Transcoder interface based on incoming request.
    * @param headers headers received from decoder.

test/extensions/filters/http/grpc_json_transcoder/json_transcoder_filter_test.cc
@@ -1297,6 +1297,68 @@ TEST_F(GrpcJsonTranscoderFilterTest, TranscodingStreamPostWithHttpBody) {
   }
 }
 
+class GrpcJsonTranscoderFilterTestWithLargerBuffer : public GrpcJsonTranscoderFilterTest {
+public:
+  GrpcJsonTranscoderFilterTestWithLargerBuffer()
+      : GrpcJsonTranscoderFilterTest(modifiedBookstoreProtoConfig()) {}
+  envoy::extensions::filters::http::grpc_json_transcoder::v3::GrpcJsonTranscoder
+  modifiedBookstoreProtoConfig() {
+    auto proto_config = bookstoreProtoConfig();
+    proto_config.mutable_max_request_body_size()->set_value(1024 * 1024 * 2);
+    return proto_config;
+  }
+};
+
+TEST_F(GrpcJsonTranscoderFilterTestWithLargerBuffer, TranscodingStreamPostWithLargeBufferHttpBody) {
+  Http::TestRequestHeaderMapImpl request_headers{
+      {":method", "POST"}, {":path", "/streamBody?arg=hi"}, {"content-type", "text/plain"}};
+
+  EXPECT_CALL(decoder_callbacks_.downstream_callbacks_, clearRouteCache());
+
+  EXPECT_EQ(Http::FilterHeadersStatus::Continue, filter_.decodeHeaders(request_headers, false));
+  EXPECT_EQ("application/grpc", request_headers.get_("content-type"));
+  EXPECT_EQ("/streamBody?arg=hi", request_headers.get_("x-envoy-original-path"));
+  EXPECT_EQ("POST", request_headers.get_("x-envoy-original-method"));
+  EXPECT_EQ("/bookstore.Bookstore/StreamBody", request_headers.get_(":path"));
+  EXPECT_EQ("trailers", request_headers.get_("te"));
+
+  // For client_streaming, a large buffer should be packaged into multiple grpc frames.
+  // Test this with a buffer of 2MB plus 512 bytes.
+  std::string text(JsonTranscoderConfig::MaxStreamedPieceSize * 2 + 512, 'X');
+  Buffer::OwnedImpl buffer;
+  buffer.add(text);
+  EXPECT_CALL(decoder_callbacks_, sendLocalReply).Times(0);
+  EXPECT_EQ(Http::FilterDataStatus::Continue, filter_.decodeData(buffer, true));
+
+  Grpc::Decoder decoder;
+  std::vector<Grpc::Frame> frames;
+  std::ignore = decoder.decode(buffer, frames);
+  ASSERT_EQ(frames.size(), 3);
+
+  // First frame should include non-streamed content, plus 1MB of streamed content.
+  bookstore::EchoBodyRequest expected_first_request;
+  expected_first_request.set_arg("hi");
+  expected_first_request.mutable_nested()->mutable_content()->set_content_type("text/plain");
+  expected_first_request.mutable_nested()->mutable_content()->set_data(
+      std::string(JsonTranscoderConfig::MaxStreamedPieceSize, 'X'));
+  bookstore::EchoBodyRequest request;
+  request.ParseFromString(frames[0].data_->toString());
+  EXPECT_THAT(request, ProtoEq(expected_first_request));
+
+  // Second frame should have only 1MB of streamed content.
+  bookstore::EchoBodyRequest expected_second_request;
+  expected_second_request.mutable_nested()->mutable_content()->set_data(
+      std::string(JsonTranscoderConfig::MaxStreamedPieceSize, 'X'));
+  request.ParseFromString(frames[1].data_->toString());
+  EXPECT_THAT(request, ProtoEq(expected_second_request));
+
+  // Third frame should have the remaining 512 bytes of streamed content.
+  bookstore::EchoBodyRequest expected_third_request;
+  expected_third_request.mutable_nested()->mutable_content()->set_data(std::string(512, 'X'));
+  request.ParseFromString(frames[2].data_->toString());
+  EXPECT_THAT(request, ProtoEq(expected_third_request));
+}
+
 TEST_F(GrpcJsonTranscoderFilterTest, TranscodingStreamSSE) {
   envoy::extensions::filters::http::grpc_json_transcoder::v3::GrpcJsonTranscoder proto_config =
       bookstoreProtoConfig();
@@ -1337,7 +1399,7 @@ TEST_F(GrpcJsonTranscoderFilterTest, TranscodingStreamSSE) {
 // The configured buffer limits will not apply.
 TEST_F(GrpcJsonTranscoderFilterTest, TranscodingStreamPostWithHttpBodyNoBuffer) {
   EXPECT_CALL(decoder_callbacks_, decoderBufferLimit())
-      .Times(testing::AtLeast(3))
+      .Times(testing::AtLeast(1))
       .WillRepeatedly(Return(8));
 
   Http::TestRequestHeaderMapImpl request_headers{

commit 055347e7ed8561a4996e410904afaea71ce17d29

Make grpc_json_transcoder_filter correctly handle empty decodeData (#41323)
Commit Message: Make grpc_json_transcoder_filter correctly handle empty
decodeData
Additional Description: The symptom without this fix is upstreams start
flakily declaring that the client cancelled the stream, for us this was
about one request in twenty, of `HttpBody` styled streaming grpcs. I
think the amount it flakes depends to some extent on the behavior of the
downstream, as whether a request of this form comes in as
headers-with-end-stream or
headers-without-end-stream-then-empty-body-with-end-stream is determined
by the outcome of a race. This bug was introduced by me in #41115.
Risk Level: Negative.
Testing:
New test before fix:
```
[ RUN      ] IpVersions/GrpcJsonTranscoderIntegrationTest.EmptyMessagePost/IPv4
test/extensions/filters/http/grpc_json_transcoder/grpc_json_transcoder_integration_test.cc:302: Failure
Expected equality of these values:
  1
  frames.size()
    Which is: 0
```

Docs Changes: n/a
Release Notes: n/a (because it's a fix to a bug that hasn't been in a
release yet)
Platform Specific Features: n/a

Signed-off-by: Raven Black <ravenblack@dropbox.com>

source/extensions/filters/http/grpc_json_transcoder/json_transcoder_filter.cc
@@ -604,7 +604,7 @@ Http::FilterDataStatus JsonTranscoderFilter::decodeData(Buffer::Instance& data, 
       // split the input buffer into 1MB pieces until the buffer is smaller than 1MB.
       Buffer::OwnedImpl remaining_request_data;
       remaining_request_data.move(request_data_);
-      while (remaining_request_data.length() > 0) {
+      while (!first_request_sent_ || remaining_request_data.length() > 0) {
         uint64_t piece_size = std::min<uint64_t>(remaining_request_data.length(),
                                                  JsonTranscoderConfig::MaxStreamedPieceSize);
         request_data_.move(remaining_request_data, piece_size);

test/extensions/filters/http/grpc_json_transcoder/grpc_json_transcoder_integration_test.cc
@@ -272,6 +272,72 @@ INSTANTIATE_TEST_SUITE_P(IpVersions, GrpcJsonTranscoderIntegrationTestWithSizeLi
                          testing::ValuesIn(TestEnvironment::getIpVersionsForTest()),
                          TestUtility::ipTestParamsToString);
 
+TEST_P(GrpcJsonTranscoderIntegrationTest, EmptyMessageStreamedHttpBodyPost) {
+  HttpIntegrationTest::initialize();
+  // Can't use testTranscoding for this case because we want to send an empty data,
+  // as distinct from not sending data. The difference being
+  // decodeHeaders(end_stream=false), decodeData(emptyBuffer, end_stream=true)
+  // vs. decodeHeaders(end_stream=true)
+  Http::TestRequestHeaderMapImpl request_headers{{":method", "POST"},
+                                                 {":path", "/streamBody"},
+                                                 {":authority", "host"},
+                                                 {"content-type", "application/json"}};
+  codec_client_ = makeHttpConnection(lookupPort("http"));
+  IntegrationStreamDecoderPtr response;
+  auto encoder_decoder = codec_client_->startRequest(request_headers);
+  request_encoder_ = &encoder_decoder.first;
+  response = std::move(encoder_decoder.second);
+  Buffer::OwnedImpl body; // Empty body.
+  codec_client_->sendData(*request_encoder_, body, true);
+  ASSERT_TRUE(fake_upstreams_[0]->waitForHttpConnection(*dispatcher_, fake_upstream_connection_));
+  ASSERT_TRUE(fake_upstream_connection_->waitForNewStream(*dispatcher_, upstream_request_));
+  ASSERT_TRUE(upstream_request_->waitForEndStream(*dispatcher_));
+  std::string dump;
+  Buffer::OwnedImpl request_body = upstream_request_->body();
+  for (char ch : request_body.toString()) {
+    dump += std::to_string(int(ch));
+    dump += " ";
+  }
+  Grpc::Decoder grpc_decoder;
+  std::vector<Grpc::Frame> frames;
+  ASSERT_TRUE(grpc_decoder.decode(request_body, frames).ok()) << dump;
+  ASSERT_EQ(1, frames.size());
+  bookstore::EchoBodyRequest actual_message;
+  ASSERT_TRUE(actual_message.ParseFromString(frames[0].data_->toString()));
+  bookstore::EchoBodyRequest expected_message;
+  expected_message.mutable_nested()->mutable_content()->set_content_type("application/json");
+  EXPECT_THAT(actual_message, ProtoEq(expected_message));
+  EXPECT_EQ("", request_body.toString());
+
+  Http::TestResponseHeaderMapImpl response_headers;
+  response_headers.setStatus(200);
+  response_headers.setContentType("application/grpc");
+  response_headers.addCopy(Http::LowerCaseString("trailer"), "Grpc-Status");
+  response_headers.addCopy(Http::LowerCaseString("trailer"), "Grpc-Message");
+  upstream_request_->encodeHeaders(response_headers, false);
+  {
+    Protobuf::Empty response_message;
+    auto buffer = Grpc::Common::serializeToGrpcFrame(response_message);
+    upstream_request_->encodeData(*buffer, false);
+  }
+  Http::TestResponseTrailerMapImpl response_trailers;
+  absl::Status grpc_status;
+  response_trailers.setGrpcStatus(static_cast<uint64_t>(grpc_status.code()));
+  response_trailers.setGrpcMessage(grpc_status.message());
+  upstream_request_->encodeTrailers(response_trailers);
+  EXPECT_TRUE(upstream_request_->complete());
+  ASSERT_TRUE(response->waitForEndStream());
+  EXPECT_TRUE(response->complete());
+  // No need to validate the response details in this test, as the purpose
+  // of the test is to validate that an empty-bodied request is delivered
+  // to the upstream as a grpc frame.
+  codec_client_->close();
+  if (fake_upstream_connection_) {
+    ASSERT_TRUE(fake_upstream_connection_->close());
+    ASSERT_TRUE(fake_upstream_connection_->waitForDisconnect());
+  }
+}
+
 TEST_P(GrpcJsonTranscoderIntegrationTest, UnaryPost) {
   HttpIntegrationTest::initialize();
   testTranscoding<bookstore::CreateShelfRequest, bookstore::Shelf>(