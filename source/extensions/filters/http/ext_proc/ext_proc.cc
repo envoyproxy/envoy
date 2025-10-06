5600dd04fad4c94558898af6d94ab72696c93f22
Melissa Ginaldi <mginaldi@google.com>
2025-10-06 18:08:06 +0000

    Change the default logging status to OK instead of Aborted for onGrpcClose()
    Signed-off-by: Melissa Ginaldi <mginaldi@google.com>

--- source/extensions/filters/http/ext_proc/ext_proc.cc
+++ source/extensions/filters/http/ext_proc/ext_proc.cc
@@ -1685,7 +1685,7 @@ void Filter::onGrpcError(Grpc::Status::GrpcStatus status, const std::string& mes
   }
 }
 
-void Filter::onGrpcClose() { onGrpcCloseWithStatus(Grpc::Status::Aborted); }
+void Filter::onGrpcClose() { onGrpcCloseWithStatus(Grpc::Status::Ok); }
 
 void Filter::onGrpcCloseWithStatus(Grpc::Status::GrpcStatus status) {
   ENVOY_STREAM_LOG(debug, "Received gRPC stream close", *decoder_callbacks_);
