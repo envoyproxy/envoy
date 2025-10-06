a8499410768add3901fedfe476a2e8eca66db6bd
botengyao <boteng@google.com>
2025-10-06 11:50:26 -0400

    mcp: add the simple json_rpc parse to dynamic metadata (#41351)
    Simple basic passthrough but store the JSON_RPC to metadata.
    #39174
    
    Commit Message:
    Additional Description:
    Risk Level: low
    Testing:
    
    ---------
    
    Signed-off-by: Boteng Yao <boteng@google.com>

--- source/extensions/filters/http/mcp/mcp_filter.h
+++ source/extensions/filters/http/mcp/mcp_filter.h
@@ -7,12 +7,18 @@
 #include "envoy/server/filter_config.h"
 
 #include "source/common/common/logger.h"
+#include "source/common/protobuf/protobuf.h"
 #include "source/extensions/filters/http/common/pass_through_filter.h"
 
 namespace Envoy {
 namespace Extensions {
 namespace HttpFilters {
 namespace Mcp {
+
+namespace MetadataKeys {
+// Core MCP fields
+constexpr absl::string_view FilterName = "mcp_proxy";
+} // namespace MetadataKeys
 
 /**
  * Configuration for the MCP filter.
@@ -25,9 +31,9 @@ public:
 using McpFilterConfigSharedPtr = std::shared_ptr<McpFilterConfig>;
 
 /**
- * HTTP MCP filter implementation.
+ * MCP proxy implementation.
  */
-class McpFilter : public Http::PassThroughFilter, public Logger::Loggable<Logger::Id::filter> {
+class McpFilter : public Http::PassThroughFilter, public Logger::Loggable<Logger::Id::mcp> {
 public:
   explicit McpFilter(McpFilterConfigSharedPtr config) : config_(config) {}
 
@@ -37,12 +43,23 @@ public:
   Http::FilterDataStatus decodeData(Buffer::Instance& data, bool end_stream) override;
 
   // Http::StreamEncoderFilter
-  Http::FilterHeadersStatus encodeHeaders(Http::ResponseHeaderMap& headers,
-                                          bool end_stream) override;
-  Http::FilterDataStatus encodeData(Buffer::Instance& data, bool end_stream) override;
+  Http::FilterHeadersStatus encodeHeaders(Http::ResponseHeaderMap&, bool) override {
+    return Http::FilterHeadersStatus::Continue;
+  };
+  Http::FilterDataStatus encodeData(Buffer::Instance&, bool) override {
+    return Http::FilterDataStatus::Continue;
+  };
+
+  void setDecoderFilterCallbacks(Http::StreamDecoderFilterCallbacks& callbacks) override {
+    decoder_callbacks_ = &callbacks;
+  }
 
 private:
+  void finalizeDynamicMetadata();
   McpFilterConfigSharedPtr config_;
+  Http::StreamDecoderFilterCallbacks* decoder_callbacks_{};
+  bool is_json_post_request_{false};
+  std::unique_ptr<Protobuf::Struct> metadata_;
 };
 
 } // namespace Mcp
