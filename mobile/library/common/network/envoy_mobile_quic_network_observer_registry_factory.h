1c144952c6c8d0e2160e8022034ca1d8fc7e62e2
Dan Zhang <danzh@google.com>
2025-10-09 18:01:52 +0000

    Revert "revert mobile changes"
    This reverts commit 6ab8b05bad57162ee4f7ef808bf4edc583c82be6.
    
    Signed-off-by: Dan Zhang <danzh@google.com>

---
+++ mobile/library/common/network/envoy_mobile_quic_network_observer_registry_factory.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "source/common/quic/envoy_quic_network_observer_registry_factory.h"
+#include "source/common/quic/quic_network_connectivity_observer.h"
+
+namespace Envoy {
+namespace Quic {
+
+// A mobile implementation that also handles network change events.
+class EnvoyMobileQuicNetworkObserverRegistry : public EnvoyQuicNetworkObserverRegistry {
+public:
+  explicit EnvoyMobileQuicNetworkObserverRegistry(Event::Dispatcher& dispatcher)
+      : dispatcher_(dispatcher) {}
+
+  // Called when the default network has changed to notify each registered observer.
+  void onNetworkMadeDefault(NetworkHandle network);
+
+  // Called when a new network is connected to notify each registered observer.
+  void onNetworkConnected(NetworkHandle network);
+
+private:
+  Event::Dispatcher& dispatcher_;
+};
+
+class EnvoyMobileQuicNetworkObserverRegistryFactory
+    : public EnvoyQuicNetworkObserverRegistryFactory {
+public:
+  EnvoyMobileQuicNetworkObserverRegistryFactory() { thread_local_observer_registries_.reserve(1); }
+
+  EnvoyQuicNetworkObserverRegistryPtr
+  createQuicNetworkObserverRegistry(Event::Dispatcher& dispatcher) override;
+
+  std::vector<std::reference_wrapper<EnvoyMobileQuicNetworkObserverRegistry>>&
+  getCreatedObserverRegistries() {
+    return thread_local_observer_registries_;
+  }
+
+private:
+  std::vector<std::reference_wrapper<EnvoyMobileQuicNetworkObserverRegistry>>
+      thread_local_observer_registries_;
+};
+
+using EnvoyMobileQuicNetworkObserverRegistryPtr =
+    std::unique_ptr<EnvoyMobileQuicNetworkObserverRegistry>;
+
+} // namespace Quic
+} // namespace Envoy
