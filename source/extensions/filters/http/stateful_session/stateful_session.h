21b70d126c0a5c16d844658ee2db68f94deb7548
Rohit Agrawal <rohit.agrawal@databricks.com>
2025-10-09 00:39:13 -0700

    http: add stats for stateful session filter (#41236)
    ## Description
    
    Today when the stateful session filter is running in the `strict=false`
    mode it's hard to understand the cases where it falls back on the
    default load balancing after not getting a routable destination.
    
    In this PR we are trying to add stats on how many requests were failed
    open and failed closed to improve the observability.
    
    ---
    
    **Commit Message:** http: add stats for stateful session filter
    **Additional Description:** Added stats in stateful session filter on
    how many requests were failed open and failed closed to improve the
    observability.
    **Risk Level:** Low
    **Testing:** Added Unit + Integration Tests
    **Docs Changes:** Added
    **Release Notes:** Added
    
    ---------
    
    Signed-off-by: Rohit Agrawal <rohit.agrawal@databricks.com>

--- source/extensions/filters/http/stateful_session/stateful_session.h
+++ source/extensions/filters/http/stateful_session/stateful_session.h
@@ -7,9 +7,11 @@
 
 #include "envoy/extensions/filters/http/stateful_session/v3/stateful_session.pb.h"
 #include "envoy/http/stateful_session.h"
+#include "envoy/stats/stats_macros.h"
 #include "envoy/upstream/load_balancer.h"
 
 #include "source/common/common/logger.h"
+#include "source/common/common/utility.h"
 #include "source/extensions/filters/http/common/pass_through_filter.h"
 
 #include "absl/strings/string_view.h"
@@ -23,10 +25,26 @@ using ProtoConfig = envoy::extensions::filters::http::stateful_session::v3::Stat
 using PerRouteProtoConfig =
     envoy::extensions::filters::http::stateful_session::v3::StatefulSessionPerRoute;
 
+/**
+ * All stats for the Stateful Session filter. @see stats_macros.h
+ */
+#define ALL_STATEFUL_SESSION_FILTER_STATS(COUNTER)                                                 \
+  COUNTER(routed)                                                                                  \
+  COUNTER(failed_open)                                                                             \
+  COUNTER(failed_closed)
+
+/**
+ * Wrapper struct for Stateful Session filter stats. @see stats_macros.h
+ */
+struct StatefulSessionFilterStats {
+  ALL_STATEFUL_SESSION_FILTER_STATS(GENERATE_COUNTER_STRUCT)
+};
+
 class StatefulSessionConfig {
 public:
   StatefulSessionConfig(const ProtoConfig& config,
-                        Server::Configuration::GenericFactoryContext& context);
+                        Server::Configuration::GenericFactoryContext& context,
+                        const std::string& stats_prefix, Stats::Scope& scope);
 
   Http::SessionStatePtr createSessionState(Http::RequestHeaderMap& headers) const {
     ASSERT(factory_ != nullptr);
@@ -35,9 +53,12 @@ public:
 
   bool isStrict() const { return strict_; }
 
+  OptRef<StatefulSessionFilterStats> stats() { return makeOptRefFromPtr(stats_.get()); }
+
 private:
   Http::SessionStateFactorySharedPtr factory_;
   bool strict_{false};
+  std::shared_ptr<StatefulSessionFilterStats> stats_;
 };
 using StatefulSessionConfigSharedPtr = std::shared_ptr<StatefulSessionConfig>;
 
@@ -69,8 +90,26 @@ public:
   Http::SessionStatePtr& sessionStateForTest() { return session_state_; }
 
 private:
+  void markRouted() {
+    if (auto stats = effective_config_->stats(); stats.has_value()) {
+      stats->routed_.inc();
+    }
+  }
+  void markFailedClosed() {
+    if (auto stats = effective_config_->stats(); stats.has_value()) {
+      stats->failed_closed_.inc();
+    }
+  }
+  void markFailedOpen() {
+    if (auto stats = effective_config_->stats(); stats.has_value()) {
+      stats->failed_open_.inc();
+    }
+  }
+
   Http::SessionStatePtr session_state_;
   StatefulSessionConfigSharedPtr config_;
+  // Cached effective config resolved from route or base config.
+  StatefulSessionConfig* effective_config_{nullptr};
 };
 
 } // namespace StatefulSession
