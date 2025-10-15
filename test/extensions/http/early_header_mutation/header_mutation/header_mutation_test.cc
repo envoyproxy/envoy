869e42c22edd15d0328ead52cdec62bb23e73d20
Ryan Anderson <ryan@michonline.com>
2025-10-15 00:00:04 +0000

    revert the early_header_mutation changes
    Signed-off-by: Ryan Anderson <ryan@michonline.com>

--- test/extensions/http/early_header_mutation/header_mutation/header_mutation_test.cc
+++ test/extensions/http/early_header_mutation/header_mutation/header_mutation_test.cc
@@ -41,14 +41,6 @@ TEST(HeaderMutationTest, TestAll) {
         key: "flag-header-4"
         value: "flag-header-4-value"
       append_action: "OVERWRITE_IF_EXISTS_OR_ADD"
-  - regex_copy:
-      source_header: "flag-header-5"
-      target_header: "flag-header-5-target"
-      expression:
-        pattern:
-          regex: "^(.*)-old$"
-        substitution: \1-new
-
   )EOF";
 
   Server::Configuration::MockServerFactoryContext context;
@@ -65,7 +57,6 @@ TEST(HeaderMutationTest, TestAll) {
       {"flag-header-2", "flag-header-2-value-old"},
       {"flag-header-3", "flag-header-3-value-old"},
       {"flag-header-4", "flag-header-4-value-old"},
-      {"flag-header-5", "flag-header-5-value-old"},
       {":method", "GET"},
   };
 
@@ -81,10 +72,6 @@ TEST(HeaderMutationTest, TestAll) {
   // 'flag-header-4' is overwritten.
   EXPECT_EQ(1, headers.get(Envoy::Http::LowerCaseString("flag-header-4")).size());
   EXPECT_EQ("flag-header-4-value", headers.get_("flag-header-4"));
-  // flag-header-5 is copied to flag-header-5-target, and -old is changed to -new:
-  EXPECT_EQ(1, headers.get(Envoy::Http::LowerCaseString("flag-header-5")).size());
-  EXPECT_EQ(1, headers.get(Envoy::Http::LowerCaseString("flag-header-5-target")).size());
-  EXPECT_EQ("flag-header-5-value-new", headers.get_("flag-header-5-target"));
 }
 
 } // namespace
