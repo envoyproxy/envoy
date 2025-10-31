54f8142dc3e4a3b1e6d2755d49a63b0053ca2310
WangBaiping <wbphub@gmail.com>
2025-10-31 06:47:48 +0000

    fix abi version
    Signed-off-by: WangBaiping <wbphub@gmail.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "dcd3808436396000e681f7014dda187ca87708995367ea1374e4b3191281cb7c";
+const char* kAbiVersion = "88c582df7667206f8f412bc65e2e2fef32ce66fc19e8d681d135b69400e989c9";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
