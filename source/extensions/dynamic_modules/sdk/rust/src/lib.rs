61e115f4d994a895b02f753b937953a509e0028a
code <wbphub@gmail.com>
2025-11-04 00:50:24 +0800

    Update source/extensions/dynamic_modules/sdk/rust/src/lib.rs
    Co-authored-by: Takeshi Yoneda <t.y.mathetake@gmail.com>
    Signed-off-by: code <wbphub@gmail.com>

--- source/extensions/dynamic_modules/sdk/rust/src/lib.rs
+++ source/extensions/dynamic_modules/sdk/rust/src/lib.rs
@@ -947,10 +947,9 @@ pub trait EnvoyHttpFilter {
 
   /// Similar to [`append_received_response_body`], but appends to the buffered response body.
   ///
-  /// This should only be called by the latest data processing filter in the filter chain.
-  /// That is say for a given filter X, if and only if the filters following filter X in the filter
-  /// chain have not yet accessed the body, filter X can safely modify the buffered body with this
-  /// method.
+  /// This method should only be used by the final data-processing filter in the chain.
+  /// In other words, a filter may safely modify the buffered body only if no later filters
+  /// in the chain have accessed it yet.
   ///
   /// Returns false if the response body is not available.
   /// Note that after changing the response body, it is caller's responsibility to modify the
