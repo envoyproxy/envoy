5c65e0a3dfb261d7eaf610b3d98c2f8409c94b2f
wbpcode <wbphub@gmail.com>
2025-11-14 16:52:30 +0000

    dynamic module: new abi to support custom flag and response code details
    Signed-off-by: wbpcode <wbphub@gmail.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "cb4b1a2acfcb8b68e74f754d8a9197e64eb3b926056d4054971b86eee6f19d95";
+const char* kAbiVersion = "213ee4f06f529a8a21afb77944fb32946e38b2f0b5a0ea252c43019a8b143273";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
