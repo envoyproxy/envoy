ca6881388b3b8bb5a489838a38efe72f69184ac8
wbpcode <wbphub@gmail.com>
2025-11-15 07:09:41 +0000

    correct the abi version
    Signed-off-by: wbpcode <wbphub@gmail.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "213ee4f06f529a8a21afb77944fb32946e38b2f0b5a0ea252c43019a8b143273";
+const char* kAbiVersion = "e7c1b3b48b6ef759ad0766916b5124ea01ca7117db79bc5b13d8cdd294deb9fc";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
