5df3f916fcd0f1a3572e9182425ec357a5d4e3b4
Ryan Northey <ryan@synca.io>
2025-11-16 13:41:15 +0000

    Revert "Simple implementation of `WrrLocality` LB policy (#40577)"
    This reverts commit 99a5f49c077c5e78fc6033c1c004c7cf14d11cca.
    
    This test has been failing constantly since the day it landed - its
    causing a lot of failure on main and lot of developer friction
    
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- source/extensions/load_balancing_policies/client_side_weighted_round_robin/client_side_weighted_round_robin_lb.h
+++ source/extensions/load_balancing_policies/client_side_weighted_round_robin/client_side_weighted_round_robin_lb.h
@@ -17,7 +17,6 @@ namespace Upstream {
 using ClientSideWeightedRoundRobinLbProto = envoy::extensions::load_balancing_policies::
     client_side_weighted_round_robin::v3::ClientSideWeightedRoundRobin;
 using OrcaLoadReportProto = xds::data::orca::v3::OrcaLoadReport;
-using CommonLbConfig = envoy::config::cluster::v3::Cluster::CommonLbConfig;
 
 /**
  * Load balancer config used to wrap the config proto.
@@ -172,9 +171,6 @@ public:
     Upstream::LoadBalancerPtr create(Upstream::LoadBalancerParams params) override;
 
     bool recreateOnHostChange() const override { return false; }
-
-    Upstream::LoadBalancerPtr createWithCommonLbConfig(const CommonLbConfig& common_lb_config,
-                                                       Upstream::LoadBalancerParams params);
 
     void applyWeightsToAllWorkers();
 
