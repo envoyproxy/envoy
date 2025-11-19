cb5b0f0007c3799af6729e0e3a68f76372ca3a67
code <wbphub@gmail.com>
2025-11-20 01:25:15 +0800

    dynamic module: make the loading flag configurable (#42058)

--- source/extensions/filters/http/dynamic_modules/factory.cc
+++ source/extensions/filters/http/dynamic_modules/factory.cc
@@ -13,7 +13,7 @@ absl::StatusOr<Http::FilterFactoryCb> DynamicModuleConfigFactory::createFilterFa
 
   const auto& module_config = proto_config.dynamic_module_config();
   auto dynamic_module = Extensions::DynamicModules::newDynamicModuleByName(
-      module_config.name(), module_config.do_not_close());
+      module_config.name(), module_config.do_not_close(), module_config.load_globally());
   if (!dynamic_module.ok()) {
     return absl::InvalidArgumentError("Failed to load dynamic module: " +
                                       std::string(dynamic_module.status().message()));
@@ -56,7 +56,7 @@ DynamicModuleConfigFactory::createRouteSpecificFilterConfigTyped(
 
   const auto& module_config = proto_config.dynamic_module_config();
   auto dynamic_module = Extensions::DynamicModules::newDynamicModuleByName(
-      module_config.name(), module_config.do_not_close());
+      module_config.name(), module_config.do_not_close(), module_config.load_globally());
   if (!dynamic_module.ok()) {
     return absl::InvalidArgumentError("Failed to load dynamic module: " +
                                       std::string(dynamic_module.status().message()));
