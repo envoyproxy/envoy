6a4dc5cfec93b64690afef55aa8d16516f6b5fdc
Dan Zhang <danzh@google.com>
2025-11-24 05:06:57 +0000

    add extension impl
    Signed-off-by: Dan Zhang <danzh@google.com>

---
+++ source/extensions/quic/client_packet_writer/default_quic_client_packet_writer_factory_config.cc
@@ -0,0 +1,10 @@
+#include "source/extensions/quic/client_packet_writer/default_quic_client_packet_writer_factory_config.h"
+
+namespace Envoy {
+namespace Quic {
+
+REGISTER_FACTORY(DefaultQuicClientPacketWriterFactoryConfig,
+                 QuicClientPacketWriterConfigFactory);
+
+} // namespace Quic
+} // namespace Envoy
