644d30da600a795d857d7fb9052180d2c11624c5
Tero Saarni <tero.saarni@est.tech>
2025-12-06 12:28:47 +0200

    devcontainer: use toolchain from compdb base
    Suggested by @wbpcode
    
    Signed-off-by: Tero Saarni <tero.saarni@est.tech>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -9,6 +9,8 @@ echo "build --config=clang" >> user.bazelrc
 
 [[ -n "${BUILD_DIR}" ]] && sudo chown -R "$(id -u):$(id -g)" "${BUILD_DIR}"
 
-echo "Downloading tools and creating symlinks..."
-sudo chown "$(id -u):$(id -g)" /usr/local/bin
-bazel run //tools/devcontainer:create_tool_links
+# Ensure that toolchain is downloaded.
+./ci/do_ci.sh pre_refresh_compdb
+
+sudo ln -sf /build/bazel_root/base-envoy-compdb/external/llvm_toolchain_llvm/bin/{clang-format,clangd} /usr/local/bin/
+sudo ln -sf /build/bazel_root/base-envoy-compdb/external/llvm_toolchain/bin/cc_wrapper.sh /usr/local/bin/
