24d73333d1074ed3b0bdeb97f28ee018624f5c01
Tero Saarni <tero.saarni@est.tech>
2025-12-07 04:33:59 +0200

    Fix devcontainer for new base image (#42415)
    <!--
    !!!ATTENTION!!!
    
    If you are fixing *any* crash or *any* potential security issue, *do
    not*
    open a pull request in this repo. Please report the issue via emailing
    envoy-security@googlegroups.com where the issue will be triaged
    appropriately.
    Thank you in advance for helping to keep Envoy secure.
    
    !!!ATTENTION!!!
    
    For an explanation of how to fill out the fields, please see the
    relevant section
    in
    [PULL_REQUESTS.md](https://github.com/envoyproxy/envoy/blob/main/PULL_REQUESTS.md)
    -->
    
    Commit Message: Fix devcontainer for new base image
    Additional Description:
    
    * Removed user creation since base container now already has a non-root
    user `envoybuild` and updated the devcontainer scripts for this user.
    * Removed unnecessary install of `libpython3.13` and added `python3`
    (temporarily, until it will be in devtools image again
    https://github.com/envoyproxy/envoy/pull/42405#discussion_r2590047684)
    * Symlink `cc_wrapper.sh`, `clangd` and `clang-format` from the
    toolchain to `/usr/local/bin/` so that vscode can access them.
    * Removed obsolete `CLANG_FORMAT` environment variable, in order to let
    the script discover the tool through the PATH.
    * Rewrite `external/llvm_toolchain/bin/cc_wrapper.sh` to `cc_wrapper.sh`
    in `compile_commands.json` and allow it as clangd compile command.
    * Installed back some common tools that were present earlier: `procps`
    for `ps` command and `iputils-ping` for `ping`
    * Removed `pcap` group since it is not relevant anymore (see
    https://github.com/envoyproxy/envoy/pull/42405#discussion_r2589985675)
    * ~Removed obsolete `/opt/llvm/bin` from PATH as it does not exist
    anymore after introduction of hermetic toolchain and new image.~ Merged
    in #42427
     
    Risk Level:
    Testing:
    Docs Changes:
    Release Notes:
    Platform Specific Features:
    [Optional Runtime guard:]
    [Optional Fixes #Issue]
    [Optional Fixes commit #PR or SHA]
    [Optional Deprecated:]
    [Optional [API
    Considerations](https://github.com/envoyproxy/envoy/blob/main/api/review_checklist.md):]
    
    Fixes #42414
    
    ---------
    
    Signed-off-by: Tero Saarni <tero.saarni@est.tech>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,3 +8,9 @@ echo "build --config=clang" >> user.bazelrc
 #echo "build --symlink_prefix=/" >> ~/.bazelrc
 
 [[ -n "${BUILD_DIR}" ]] && sudo chown -R "$(id -u):$(id -g)" "${BUILD_DIR}"
+
+# Ensure that toolchain is downloaded.
+./ci/do_ci.sh pre_refresh_compdb
+
+sudo ln -sf /build/bazel_root/base-envoy-compdb/external/llvm_toolchain_llvm/bin/{clang-format,clangd} /usr/local/bin/
+sudo ln -sf /build/bazel_root/base-envoy-compdb/external/llvm_toolchain/bin/cc_wrapper.sh /usr/local/bin/
