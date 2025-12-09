8e947ec407a2b990bfbfa74a97f00415eadb1b55
Raj Shetty <rajeshetty87@gmail.com>
2025-12-08 16:41:55 -0800

    fix code format checks
    Signed-off-by: Raj Shetty <rajeshetty87@gmail.com>

--- source/extensions/resource_monitors/cpu_utilization/linux_cpu_stats_reader.cc
+++ source/extensions/resource_monitors/cpu_utilization/linux_cpu_stats_reader.cc
@@ -128,7 +128,6 @@ CpuTimes CgroupV1CpuStatsReader::getCpuTimes() {
   }
 }
 
-
 CgroupV2CpuStatsReader::CgroupV2CpuStatsReader(Filesystem::Instance& fs, TimeSource& time_source)
     : LinuxContainerCpuStatsReader(fs, time_source), stat_path_(CpuPaths::V2::getStatPath()),
       max_path_(CpuPaths::V2::getMaxPath()), effective_path_(CpuPaths::V2::getEffectiveCpusPath()) {
