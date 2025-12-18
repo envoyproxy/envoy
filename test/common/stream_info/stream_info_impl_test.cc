22013f97076380ce6eb52b39a8ef2f2da5addf1e
Xuyang Tao <taoxuy@google.com>
2025-12-18 19:39:11 +0000

    fix size test failure
    Signed-off-by: Xuyang Tao <taoxuy@google.com>

--- test/common/stream_info/stream_info_impl_test.cc
+++ test/common/stream_info/stream_info_impl_test.cc
@@ -42,11 +42,11 @@ protected:
   void assertStreamInfoSize(StreamInfoImpl stream_info) {
     ASSERT_TRUE(
         // with --config=docker-msan
-        sizeof(stream_info) == 712 ||
+        sizeof(stream_info) == 720 ||
         // with --config=docker-clang
         sizeof(stream_info) == 736 ||
         // with --config=docker-clang-libc++
-        sizeof(stream_info) == 688)
+        sizeof(stream_info) == 696)
         << "If adding fields to StreamInfoImpl, please check to see if you "
            "need to add them to setFromForRecreateStream or setFrom! Current size "
         << sizeof(stream_info);
