envoyproxy/envoy

commit 39f8327556897dd35db3ef16838128411e73a63c

ext_authz: add error_response for gRPC ExtAuthZ to return custom error response (#42099)
## Description

Similar to
[denied_response](https://www.envoyproxy.io/docs/envoy/latest/api-v3/service/auth/v3/external_auth.proto#service-auth-v3-checkresponse),
this PR adds a new `error_response` on the gRPC ExtAuthZ service to
provide a way to send back an `error_response` with a custom status
code, headers and/or body from gRPC ExtAuthZ servers back to Envoy.

It's useful in cases where we are not explicitly "denying" the request
but just communicating that the server could be overloaded or the
processing has failed for some reason and suggesting the user to retry
after some time if needed.

Fix https://github.com/envoyproxy/envoy/issues/42098

---

**Commit Message:** ext_authz: add error_response for gRPC ExtAuthZ to
return custom error response
**Additional Description:** Adds a new `error_response` on the gRPC
ExtAuthZ service to provide a way to send back an `error_response` with
a custom status code, headers and/or body from gRPC ExtAuthZ servers
back to Envoy.
**Risk Level:** Low
**Testing:** Added Unit + Integration Tests
**Docs Changes:** Added
**Release Notes:** Added

---------

Signed-off-by: Rohit Agrawal <rohit.agrawal@databricks.com>

source/extensions/filters/common/ext_authz/ext_authz_grpc_impl.cc
@@ -117,6 +117,21 @@ void GrpcClientImpl::onSuccess(std::unique_ptr<envoy::service::auth::v3::CheckRe
       const auto& ok_response = response->ok_response();
       copyOkResponseMutations(authz_response, ok_response);
     }
+  } else if (response->has_error_response()) {
+    // If error_response is present, treat it as an error and not denial.
+    span.setTag(TracingConstants::get().TraceStatus, TracingConstants::get().TraceError);
+    authz_response->status = CheckStatus::Error;
+
+    // For error responses, don't set a default status_code.
+    // Let the filter use status_on_error configuration.
+    const auto& error_response = response->error_response();
+    copyHeaderFieldIntoResponse(authz_response, error_response.headers());
+
+    const uint32_t status_code = error_response.status().code();
+    if (status_code > 0) {
+      authz_response->status_code = static_cast<Http::Code>(status_code);
+    }
+    authz_response->body = error_response.body();
   } else {
     span.setTag(TracingConstants::get().TraceStatus, TracingConstants::get().TraceUnauthz);
     authz_response->status = CheckStatus::Denied;

test/extensions/filters/common/ext_authz/ext_authz_grpc_impl_test.cc
@@ -337,6 +337,90 @@ TEST_F(ExtAuthzGrpcClientTest, AuthorizationRequestTimeout) {
   client_->onFailure(grpc_status, "", span_);
 }
 
+// Test the client when an error response with custom attributes is received.
+TEST_F(ExtAuthzGrpcClientTest, AuthorizationErrorWithAllAttributes) {
+  initialize();
+
+  const std::string expected_body{"Internal Server Error"};
+  const auto expected_headers =
+      TestCommon::makeHeaderValueOption({{"x-error-code", "AUTH_SERVICE_ERROR", false}});
+  const auto grpc_status = Grpc::Status::WellKnownGrpcStatus::Internal;
+  auto check_response = TestCommon::makeErrorCheckResponse(
+      grpc_status, envoy::type::v3::InternalServerError, expected_body, expected_headers);
+  auto authz_response = TestCommon::makeAuthzResponse(
+      CheckStatus::Error, Http::Code::InternalServerError, expected_body, expected_headers,
+      HeaderValueOptionVector{}, grpc_status);
+
+  envoy::service::auth::v3::CheckRequest request;
+  expectCallSend(request);
+  client_->check(request_callbacks_, request, Tracing::NullSpan::instance(), stream_info_);
+
+  Http::TestRequestHeaderMapImpl headers;
+  client_->onCreateInitialMetadata(headers);
+  EXPECT_EQ(nullptr, headers.RequestId());
+  EXPECT_CALL(span_, setTag(Eq("ext_authz_status"), Eq("ext_authz_error")));
+  EXPECT_CALL(request_callbacks_, onComplete_(WhenDynamicCastTo<ResponsePtr&>(
+                                      AuthzErrorResponseWithAttributes(authz_response))));
+
+  client_->onSuccess(std::move(check_response), span_);
+}
+
+// Test the client when an error response with empty status code is received.
+// The response sent to client should use the status_on_error configuration.
+TEST_F(ExtAuthzGrpcClientTest, AuthorizationErrorWithEmptyErrorResponseStatus) {
+  initialize();
+
+  const std::string expected_body{"Service Unavailable"};
+  const auto expected_headers =
+      TestCommon::makeHeaderValueOption({{"x-error-message", "auth backend down", false}});
+  const auto grpc_status = Grpc::Status::WellKnownGrpcStatus::Unavailable;
+  auto check_response = TestCommon::makeErrorCheckResponse(grpc_status, envoy::type::v3::Empty,
+                                                           expected_body, expected_headers);
+  // When the error_response has no HTTP status code, the gRPC client doesn't set a default.
+  // The filter will use status_on_error configuration instead.
+  auto authz_response =
+      TestCommon::makeAuthzResponse(CheckStatus::Error, static_cast<Http::Code>(0), expected_body,
+                                    expected_headers, HeaderValueOptionVector{}, grpc_status);
+
+  envoy::service::auth::v3::CheckRequest request;
+  expectCallSend(request);
+  client_->check(request_callbacks_, request, Tracing::NullSpan::instance(), stream_info_);
+
+  Http::TestRequestHeaderMapImpl headers;
+  client_->onCreateInitialMetadata(headers);
+  EXPECT_EQ(nullptr, headers.RequestId());
+  EXPECT_CALL(span_, setTag(Eq("ext_authz_status"), Eq("ext_authz_error")));
+  EXPECT_CALL(request_callbacks_, onComplete_(WhenDynamicCastTo<ResponsePtr&>(
+                                      AuthzErrorResponseWithAttributes(authz_response))));
+
+  client_->onSuccess(std::move(check_response), span_);
+}
+
+// Test the client when an error response with no attributes is received.
+TEST_F(ExtAuthzGrpcClientTest, AuthorizationErrorNoAttributes) {
+  initialize();
+
+  const auto grpc_status = Grpc::Status::WellKnownGrpcStatus::Internal;
+  auto check_response = TestCommon::makeErrorCheckResponse(
+      grpc_status, envoy::type::v3::InternalServerError, "", HeaderValueOptionVector{});
+  auto authz_response = TestCommon::makeAuthzResponse(
+      CheckStatus::Error, Http::Code::InternalServerError, "", HeaderValueOptionVector{},
+      HeaderValueOptionVector{}, grpc_status);
+
+  envoy::service::auth::v3::CheckRequest request;
+  expectCallSend(request);
+  client_->check(request_callbacks_, request, Tracing::NullSpan::instance(), stream_info_);
+
+  Http::TestRequestHeaderMapImpl headers;
+  client_->onCreateInitialMetadata(headers);
+  EXPECT_EQ(nullptr, headers.RequestId());
+  EXPECT_CALL(span_, setTag(Eq("ext_authz_status"), Eq("ext_authz_error")));
+  EXPECT_CALL(request_callbacks_, onComplete_(WhenDynamicCastTo<ResponsePtr&>(
+                                      AuthzErrorResponseWithAttributes(authz_response))));
+
+  client_->onSuccess(std::move(check_response), span_);
+}
+
 // Test the client when an OK response is received with dynamic metadata in that OK response.
 TEST_F(ExtAuthzGrpcClientTest, AuthorizationOkWithDynamicMetadata) {
   initialize();

test/extensions/filters/http/ext_authz/ext_authz_integration_test.cc
@@ -2396,6 +2396,211 @@ TEST_P(ExtAuthzGrpcIntegrationTest, EncodeHeadersToAppendIfAbsentExceedsLimit) {
   cleanup();
 }
 
+// Test that an error response with custom status, headers, and body is correctly sent to the
+// client.
+TEST_P(ExtAuthzGrpcIntegrationTest, ErrorResponseWithCustomAttributes) {
+  ext_authz_grpc_status_ = LoggingTestFilterConfig::INTERNAL;
+  initializeConfig();
+
+  setDownstreamProtocol(Http::CodecType::HTTP1);
+  HttpIntegrationTest::initialize();
+
+  initiateClientConnection(0);
+
+  waitForExtAuthzRequest(expectedCheckRequest(Http::CodecType::HTTP1));
+
+  // Send error_response with custom attributes.
+  ext_authz_request_->startGrpcStream();
+  envoy::service::auth::v3::CheckResponse check_response;
+  check_response.mutable_status()->set_code(Grpc::Status::WellKnownGrpcStatus::Internal);
+  auto* error_response = check_response.mutable_error_response();
+  error_response->mutable_status()->set_code(envoy::type::v3::ServiceUnavailable);
+  error_response->set_body("{\"error\": \"auth service unavailable\"}");
+
+  auto* header1 = error_response->mutable_headers()->Add();
+  header1->mutable_header()->set_key("x-error-code");
+  header1->mutable_header()->set_value("AUTH_SERVICE_ERROR");
+
+  auto* header2 = error_response->mutable_headers()->Add();
+  header2->mutable_header()->set_key("x-error-message");
+  header2->mutable_header()->set_value("Service temporarily unavailable");
+
+  ext_authz_request_->sendGrpcMessage(check_response);
+  ext_authz_request_->finishGrpcStream(Grpc::Status::Ok);
+
+  ASSERT_TRUE(response_->waitForEndStream());
+  EXPECT_TRUE(response_->complete());
+  EXPECT_EQ("503", response_->headers().getStatusValue());
+  EXPECT_EQ(
+      "AUTH_SERVICE_ERROR",
+      response_->headers().get(Http::LowerCaseString("x-error-code"))[0]->value().getStringView());
+  EXPECT_EQ("Service temporarily unavailable", response_->headers()
+                                                   .get(Http::LowerCaseString("x-error-message"))[0]
+                                                   ->value()
+                                                   .getStringView());
+  EXPECT_EQ("{\"error\": \"auth service unavailable\"}", response_->body());
+
+  cleanup();
+}
+
+// Test that an error response respects failure_mode_allow configuration.
+TEST_P(ExtAuthzGrpcIntegrationTest, ErrorResponseWithFailureModeAllow) {
+  GrpcInitializeConfigOpts opts;
+  opts.failure_mode_allow = true;
+  ext_authz_grpc_status_ = LoggingTestFilterConfig::INTERNAL;
+  initializeConfig(opts);
+
+  setDownstreamProtocol(Http::CodecType::HTTP1);
+  HttpIntegrationTest::initialize();
+
+  initiateClientConnection(0);
+
+  waitForExtAuthzRequest(expectedCheckRequest(Http::CodecType::HTTP1));
+
+  // Send error_response - should be ignored due to failure_mode_allow.
+  ext_authz_request_->startGrpcStream();
+  envoy::service::auth::v3::CheckResponse check_response;
+  check_response.mutable_status()->set_code(Grpc::Status::WellKnownGrpcStatus::Internal);
+  auto* error_response = check_response.mutable_error_response();
+  error_response->mutable_status()->set_code(envoy::type::v3::InternalServerError);
+  error_response->set_body("This should be ignored");
+
+  auto* header = error_response->mutable_headers()->Add();
+  header->mutable_header()->set_key("x-should-not-appear");
+  header->mutable_header()->set_value("ignored");
+
+  ext_authz_request_->sendGrpcMessage(check_response);
+  ext_authz_request_->finishGrpcStream(Grpc::Status::Ok);
+
+  // Request should be allowed and forwarded to upstream.
+  waitForSuccessfulUpstreamResponse("200");
+
+  // Verify error headers are not present in the response.
+  EXPECT_TRUE(response_->headers().get(Http::LowerCaseString("x-should-not-appear")).empty());
+
+  cleanup();
+}
+
+// Test that an error response body is truncated if it exceeds max_denied_response_body_bytes.
+TEST_P(ExtAuthzGrpcIntegrationTest, ErrorResponseWithBodyTruncation) {
+  GrpcInitializeConfigOpts opts;
+  opts.max_denied_response_body_bytes = 15;
+  ext_authz_grpc_status_ = LoggingTestFilterConfig::INTERNAL;
+  initializeConfig(opts);
+
+  setDownstreamProtocol(Http::CodecType::HTTP1);
+  HttpIntegrationTest::initialize();
+
+  initiateClientConnection(0);
+
+  waitForExtAuthzRequest(expectedCheckRequest(Http::CodecType::HTTP1));
+
+  ext_authz_request_->startGrpcStream();
+  envoy::service::auth::v3::CheckResponse check_response;
+  check_response.mutable_status()->set_code(Grpc::Status::WellKnownGrpcStatus::Internal);
+  auto* error_response = check_response.mutable_error_response();
+  error_response->mutable_status()->set_code(envoy::type::v3::InternalServerError);
+  error_response->set_body("this-is-a-very-long-error-body-that-should-be-truncated");
+
+  ext_authz_request_->sendGrpcMessage(check_response);
+  ext_authz_request_->finishGrpcStream(Grpc::Status::Ok);
+
+  ASSERT_TRUE(response_->waitForEndStream());
+  EXPECT_TRUE(response_->complete());
+  EXPECT_EQ("500", response_->headers().getStatusValue());
+  EXPECT_EQ("this-is-a-very-", response_->body()); // Truncated to 15 bytes
+
+  cleanup();
+}
+
+// Test that error response with both headers_to_set and headers_to_append works correctly.
+TEST_P(ExtAuthzGrpcIntegrationTest, ErrorResponseWithSetAndAppendHeaders) {
+  ext_authz_grpc_status_ = LoggingTestFilterConfig::UNAVAILABLE;
+  initializeConfig();
+
+  setDownstreamProtocol(Http::CodecType::HTTP1);
+  HttpIntegrationTest::initialize();
+
+  initiateClientConnection(0);
+
+  waitForExtAuthzRequest(expectedCheckRequest(Http::CodecType::HTTP1));
+
+  ext_authz_request_->startGrpcStream();
+  envoy::service::auth::v3::CheckResponse check_response;
+  check_response.mutable_status()->set_code(Grpc::Status::WellKnownGrpcStatus::Unavailable);
+  auto* error_response = check_response.mutable_error_response();
+  error_response->mutable_status()->set_code(envoy::type::v3::ServiceUnavailable);
+  error_response->set_body("Service error");
+
+  // Add header with append = false (headers_to_set).
+  auto* header_set = error_response->mutable_headers()->Add();
+  header_set->mutable_append()->set_value(false);
+  header_set->mutable_header()->set_key("x-error-set");
+  header_set->mutable_header()->set_value("set-value");
+
+  // Add header with append = true (headers_to_append).
+  auto* header_append = error_response->mutable_headers()->Add();
+  header_append->mutable_append()->set_value(true);
+  header_append->mutable_header()->set_key("x-error-append");
+  header_append->mutable_header()->set_value("append-value");
+
+  ext_authz_request_->sendGrpcMessage(check_response);
+  ext_authz_request_->finishGrpcStream(Grpc::Status::Ok);
+
+  ASSERT_TRUE(response_->waitForEndStream());
+  EXPECT_TRUE(response_->complete());
+  EXPECT_EQ("503", response_->headers().getStatusValue());
+  EXPECT_EQ(
+      "set-value",
+      response_->headers().get(Http::LowerCaseString("x-error-set"))[0]->value().getStringView());
+  EXPECT_EQ("append-value", response_->headers()
+                                .get(Http::LowerCaseString("x-error-append"))[0]
+                                ->value()
+                                .getStringView());
+  EXPECT_EQ("Service error", response_->body());
+
+  cleanup();
+}
+
+// Test that error response with invalid headers is rejected when validate_mutations is enabled.
+TEST_P(ExtAuthzGrpcIntegrationTest, ErrorResponseWithInvalidHeaders) {
+  GrpcInitializeConfigOpts opts;
+  opts.validate_mutations = true;
+  ext_authz_grpc_status_ = LoggingTestFilterConfig::INTERNAL;
+  initializeConfig(opts);
+
+  setDownstreamProtocol(Http::CodecType::HTTP1);
+  HttpIntegrationTest::initialize();
+
+  initiateClientConnection(0);
+
+  waitForExtAuthzRequest(expectedCheckRequest(Http::CodecType::HTTP1));
+
+  ext_authz_request_->startGrpcStream();
+  envoy::service::auth::v3::CheckResponse check_response;
+  check_response.mutable_status()->set_code(Grpc::Status::WellKnownGrpcStatus::Internal);
+  auto* error_response = check_response.mutable_error_response();
+  error_response->mutable_status()->set_code(envoy::type::v3::InternalServerError);
+  error_response->set_body("This body should be cleared due to invalid headers");
+
+  // Add invalid header with newlines.
+  auto* header = error_response->mutable_headers()->Add();
+  header->mutable_header()->set_key("invalid\nheader");
+  header->mutable_header()->set_value("value");
+
+  ext_authz_request_->sendGrpcMessage(check_response);
+  ext_authz_request_->finishGrpcStream(Grpc::Status::Ok);
+
+  ASSERT_TRUE(response_->waitForEndStream());
+  EXPECT_TRUE(response_->complete());
+  // Should fall back to default 403 status.
+  EXPECT_EQ("403", response_->headers().getStatusValue());
+  // Body should be empty due to validation failure.
+  EXPECT_TRUE(response_->body().empty());
+
+  cleanup();
+}
+
 // Regression test for https://github.com/envoyproxy/envoy/issues/17344
 TEST(ExtConfigValidateTest, Validate) {
   Server::TestComponentFactory component_factory;

api/envoy/service/auth/v3/external_auth.proto
@@ -114,6 +114,7 @@ message OkHttpResponse {
 }
 
 // Intended for gRPC and Network Authorization servers ``only``.
+// [#next-free-field: 6]
 message CheckResponse {
   option (udpa.annotations.versioning).previous_message_type =
       "envoy.service.auth.v2.CheckResponse";
@@ -132,6 +133,18 @@ message CheckResponse {
 
     // Supplies http attributes for an ok response.
     OkHttpResponse ok_response = 3;
+
+    // Supplies http attributes for an error response. This is used when the authorization
+    // service encounters an internal error and wants to return custom headers and body to the
+    // downstream client. When ``error_response`` is set, the ext_authz filter increments the
+    // ``ext_authz_error`` stat and respects the :ref:`failure_mode_allow
+    // <envoy_v3_api_field_extensions.filters.http.ext_authz.v3.ExtAuthz.failure_mode_allow>`
+    // configuration. The HTTP status code, headers, and body are taken from the
+    // :ref:`DeniedHttpResponse <envoy_v3_api_msg_service.auth.v3.DeniedHttpResponse>` message.
+    // If the status field is not set, Envoy sends the status code configured via
+    // :ref:`status_on_error <envoy_v3_api_field_extensions.filters.http.ext_authz.v3.ExtAuthz.status_on_error>`,
+    // which defaults to ``403 Forbidden``.
+    DeniedHttpResponse error_response = 5;
   }
 
   // Optional response metadata that will be emitted as dynamic metadata to be consumed by the next

source/extensions/filters/common/ext_authz/ext_authz.h
@@ -28,6 +28,7 @@ struct TracingConstantValues {
   const std::string TraceStatus = "ext_authz_status";
   const std::string TraceUnauthz = "ext_authz_unauthorized";
   const std::string TraceOk = "ext_authz_ok";
+  const std::string TraceError = "ext_authz_error";
   const std::string HttpStatus = "ext_authz_http_status";
 };
 

source/extensions/filters/http/ext_authz/ext_authz.cc
@@ -1019,6 +1019,19 @@ void Filter::onComplete(Filters::Common::ExtAuthz::ResponsePtr&& response) {
       config_->incCounter(cluster_->statsScope(), config_->ext_authz_error_);
     }
     stats_.error_.inc();
+
+    // Validate error response headers and clear custom attributes if invalid.
+    validateAndClearInvalidErrorResponseAttributes(response);
+
+    // Apply max_denied_response_body_bytes limit to error response body as well.
+    if (config_->maxDeniedResponseBodyBytes() > 0 &&
+        response->body.length() > config_->maxDeniedResponseBodyBytes()) {
+      ENVOY_STREAM_LOG(
+          trace, "ext_authz filter is truncating the error response body from {} to {} bytes.",
+          *decoder_callbacks_, response->body.length(), config_->maxDeniedResponseBodyBytes());
+      response->body.resize(config_->maxDeniedResponseBodyBytes());
+    }
+
     if (config_->failureModeAllow()) {
       ENVOY_STREAM_LOG(trace, "ext_authz filter allowed the request with error",
                        *decoder_callbacks_);
@@ -1032,14 +1045,25 @@ void Filter::onComplete(Filters::Common::ExtAuthz::ResponsePtr&& response) {
       }
       continueDecoding();
     } else {
+      // Use custom status code from error_response if provided, otherwise use status_on_error.
+      // Status code 0 means not set.
+      const Http::Code status_code = response->status_code != static_cast<Http::Code>(0)
+                                         ? response->status_code
+                                         : config_->statusOnError();
       ENVOY_STREAM_LOG(
           trace, "ext_authz filter rejected the request with an error. Response status code: {}",
-          *decoder_callbacks_, enumToInt(config_->statusOnError()));
+          *decoder_callbacks_, enumToInt(status_code));
       decoder_callbacks_->streamInfo().setResponseFlag(
           StreamInfo::CoreResponseFlag::UnauthorizedExternalService);
+
       decoder_callbacks_->sendLocalReply(
-          config_->statusOnError(), EMPTY_STRING, nullptr, absl::nullopt,
-          Filters::Common::ExtAuthz::ResponseCodeDetails::get().AuthzError);
+          status_code, response->body,
+          [&headers_to_set = response->headers_to_set,
+           &headers_to_append = response->headers_to_append,
+           this](Http::HeaderMap& response_headers) -> void {
+            addErrorResponseHeaders(response_headers, headers_to_set, headers_to_append);
+          },
+          absl::nullopt, Filters::Common::ExtAuthz::ResponseCodeDetails::get().AuthzError);
     }
     break;
   }
@@ -1112,6 +1136,93 @@ Filter::PerRouteFlags Filter::getPerRouteFlags(const Router::RouteConstSharedPtr
   }
 
   return PerRouteFlags{false /*skip_check_*/, defaultCheckSettings()};
+}
+
+bool Filter::validateAndClearInvalidErrorResponseAttributes(
+    Filters::Common::ExtAuthz::ResponsePtr& response) {
+  if (!config_->validateMutations()) {
+    return true;
+  }
+
+  // Validate headers_to_set.
+  for (const auto& [key, value] : response->headers_to_set) {
+    if (!Http::HeaderUtility::headerNameIsValid(key) ||
+        !Http::HeaderUtility::headerValueIsValid(value)) {
+      ENVOY_STREAM_LOG(trace, "Rejected invalid error header '{}':'{}'.", *decoder_callbacks_, key,
+                       value);
+      ENVOY_STREAM_LOG(info,
+                       "Custom error response from ext_authz will be ignored due to invalid "
+                       "header. Falling back to generic error response.",
+                       *decoder_callbacks_);
+      // Fall back to generic error by clearing all custom attributes.
+      response->headers_to_set.clear();
+      response->headers_to_append.clear();
+      response->body.clear();
+      response->status_code = static_cast<Http::Code>(0); // Clear custom status.
+      return false;
+    }
+  }
+
+  // Validate headers_to_append.
+  for (const auto& [key, value] : response->headers_to_append) {
+    if (!Http::HeaderUtility::headerNameIsValid(key) ||
+        !Http::HeaderUtility::headerValueIsValid(value)) {
+      ENVOY_STREAM_LOG(trace, "Rejected invalid error header '{}':'{}'.", *decoder_callbacks_, key,
+                       value);
+      ENVOY_STREAM_LOG(info,
+                       "Custom error response from ext_authz will be ignored due to invalid "
+                       "header. Falling back to generic error response.",
+                       *decoder_callbacks_);
+      // Fall back to generic error by clearing all custom attributes.
+      response->headers_to_set.clear();
+      response->headers_to_append.clear();
+      response->body.clear();
+      response->status_code = static_cast<Http::Code>(0); // Clear custom status.
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool Filter::canAddResponseHeader(Http::HeaderMap& response_headers) {
+  if (config_->enforceResponseHeaderLimits() &&
+      response_headers.size() >= response_headers.maxHeadersCount()) {
+    stats_.omitted_response_headers_.inc();
+    ENVOY_LOG_EVERY_POW_2(warn, "Some ext_authz error response headers weren't added because the "
+                                "header map was full.");
+    return false;
+  }
+  return true;
+}
+
+void Filter::addErrorResponseHeaders(
+    Http::HeaderMap& response_headers,
+    const std::vector<std::pair<std::string, std::string>>& headers_to_set,
+    const std::vector<std::pair<std::string, std::string>>& headers_to_append) {
+  ENVOY_STREAM_LOG(trace,
+                   "ext_authz filter added header(s) to the error response:", *decoder_callbacks_);
+
+  // First, handle headers_to_set which should override existing headers.
+  for (const auto& [key, _] : headers_to_set) {
+    response_headers.remove(Http::LowerCaseString(key));
+  }
+  for (const auto& [key, value] : headers_to_set) {
+    if (!canAddResponseHeader(response_headers)) {
+      break;
+    }
+    ENVOY_STREAM_LOG(trace, " '{}':'{}'", *decoder_callbacks_, key, value);
+    response_headers.addCopy(Http::LowerCaseString(key), value);
+  }
+
+  // Then, handle headers_to_append which should append to existing headers.
+  for (const auto& [key, value] : headers_to_append) {
+    if (!canAddResponseHeader(response_headers)) {
+      break;
+    }
+    ENVOY_STREAM_LOG(trace, " '{}':'{}'", *decoder_callbacks_, key, value);
+    response_headers.addCopy(Http::LowerCaseString(key), value);
+  }
 }
 
 } // namespace ExtAuthz

commit 451c12e08f8361d5a2d6110c89da10536a9e52fe

ext_authz: fix status_on_error ignored on gRPC failure (#42764)
GrpcClientImpl::onFailure() hardcoded status 403, preventing
`status_on_error` config from working when gRPC calls fail since it
starts checking the 0 status. Now leaves status unset so the filter uses
the configured value.

fix a regression introduced from
https://github.com/envoyproxy/envoy/pull/42099

Commit Message:
Additional Description:
Risk Level: low
Testing: both 
Docs Changes:
Release Notes:

Signed-off-by: Boteng Yao <boteng@google.com>

source/extensions/filters/common/ext_authz/ext_authz_grpc_impl.cc
@@ -168,7 +168,6 @@ void GrpcClientImpl::onFailure(Grpc::Status::GrpcStatus status, const std::strin
   ASSERT(status != Grpc::Status::WellKnownGrpcStatus::Ok);
   Response response{};
   response.status = CheckStatus::Error;
-  response.status_code = Http::Code::Forbidden;
   response.grpc_status = status;
   callbacks_->onComplete(std::make_unique<Response>(response));
   callbacks_ = nullptr;

test/extensions/filters/common/ext_authz/ext_authz_grpc_impl_test.cc
@@ -307,6 +307,27 @@ TEST_F(ExtAuthzGrpcClientTest, UnknownError) {
   client_->onFailure(grpc_status, "", span_);
 }
 
+// Test that gRPC call failure (onFailure) leaves status_code unset (0).
+// This allows the filter to use status_on_error config instead of a hardcoded value.
+TEST_F(ExtAuthzGrpcClientTest, GrpcCallFailureDoesNotSetStatusCode) {
+  initialize();
+
+  const auto grpc_status = Grpc::Status::WellKnownGrpcStatus::Unavailable;
+  // Expected: status_code should be unset (0), not Forbidden.
+  auto expected_response = Response{};
+  expected_response.status = CheckStatus::Error;
+  expected_response.status_code = static_cast<Http::Code>(0); // Unset
+  expected_response.grpc_status = grpc_status;
+
+  envoy::service::auth::v3::CheckRequest request;
+  expectCallSend(request);
+  client_->check(request_callbacks_, request, Tracing::NullSpan::instance(), stream_info_);
+
+  EXPECT_CALL(request_callbacks_, onComplete_(WhenDynamicCastTo<ResponsePtr&>(
+                                      AuthzErrorResponseWithAttributes(expected_response))));
+  client_->onFailure(grpc_status, "", span_);
+}
+
 // Test the client when the request is canceled.
 TEST_F(ExtAuthzGrpcClientTest, CancelledAuthorizationRequest) {
   initialize();

test/extensions/filters/http/ext_authz/ext_authz_integration_test.cc
@@ -52,6 +52,7 @@ struct GrpcInitializeConfigOpts {
   // In timeout tests we expect zero response bytes.
   bool stats_expect_response_bytes = true;
   bool enforce_response_header_limits = false;
+  uint32_t status_on_error_code = 0;
 };
 
 struct WaitForSuccessfulUpstreamResponseOpts {
@@ -151,6 +152,11 @@ public:
 
       if (opts.enforce_response_header_limits) {
         proto_config_.set_enforce_response_header_limits(true);
+      }
+
+      if (opts.status_on_error_code > 0) {
+        proto_config_.mutable_status_on_error()->set_code(
+            static_cast<envoy::type::v3::StatusCode>(opts.status_on_error_code));
       }
 
       // Add labels and verify they are passed.
@@ -1263,6 +1269,30 @@ TEST_P(ExtAuthzGrpcIntegrationTest, TimeoutFailClosed) {
   ASSERT_TRUE(response_->waitForEndStream());
   EXPECT_TRUE(response_->complete());
   EXPECT_EQ("403", response_->headers().getStatusValue()); // Unauthorized status.
+
+  cleanup();
+}
+
+// Test that gRPC call failure respects status_on_error configuration.
+// When the gRPC call fails (e.g., timeout), the filter should use the configured
+// status_on_error (503) instead of the default (403).
+TEST_P(ExtAuthzGrpcIntegrationTest, GrpcCallFailureUsesStatusOnError) {
+  GrpcInitializeConfigOpts opts;
+  opts.stats_expect_response_bytes = false;
+  opts.failure_mode_allow = false;
+  opts.timeout_ms = 1;
+  opts.status_on_error_code = 503;
+  ext_authz_grpc_status_ = LoggingTestFilterConfig::UNAVAILABLE;
+  initializeConfig(opts);
+
+  setDownstreamProtocol(Http::CodecType::HTTP1);
+  HttpIntegrationTest::initialize();
+
+  initiateClientConnection(0);
+
+  ASSERT_TRUE(response_->waitForEndStream());
+  EXPECT_TRUE(response_->complete());
+  EXPECT_EQ("503", response_->headers().getStatusValue());
 
   cleanup();
 }