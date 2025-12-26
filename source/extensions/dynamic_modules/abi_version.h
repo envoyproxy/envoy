89eae9dcda9e097aa787458f28d4b5f2f5e02dab
code <wbphub@gmail.com>
2025-12-26 08:35:27 +0800

    dynamic module: merged header/body ABI and use enum for different types (#42773)

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "e63b2d100f473018f01b6d91f830318dadd686489cb6ad0cc4628b582248b301";
+const char* kAbiVersion = "9df469f17c3456233a68afdc5463472fb09d7d6ac6462b8eff5f8186a10279d6";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
