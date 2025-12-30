604c230e53a5217d9e37b5d11447d638fbedfe80
VivekSubr <tzar123@gmail.com>
2025-12-30 17:21:02 +0000

    updated abi version
    Signed-off-by: VivekSubr <tzar123@gmail.com>

--- source/extensions/dynamic_modules/abi_version.h
+++ source/extensions/dynamic_modules/abi_version.h
@@ -6,7 +6,7 @@ namespace DynamicModules {
 #endif
 // This is the ABI version calculated as a sha256 hash of the ABI header files. When the ABI
 // changes, this value must change, and the correctness of this value is checked by the test.
-const char* kAbiVersion = "3fe64075e0a96694bcfc55111b047d3c9a62c46482f9e7c91159e12b9211ba96";
+const char* kAbiVersion = "a063377e5c6ef4034b148220388e528f5af3980149ab01dd33ac4d0d4ae0a7db";
 
 #ifdef __cplusplus
 } // namespace DynamicModules
