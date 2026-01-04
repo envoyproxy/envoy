aecf9a03b41539fd20ce0b8d33e8a363c83756dd
Ali Beyad <abeyad@google.com>
2026-01-04 15:50:30 -0600

    link quality check
    Signed-off-by: Ali Beyad <abeyad@google.com>

--- mobile/library/cc/apple/apple_network_change_monitor_impl.mm
+++ mobile/library/cc/apple/apple_network_change_monitor_impl.mm
@@ -76,6 +76,43 @@ static NSString *RadioAccessTechnologyNR() {
 
 #pragma mark Private Methods
 
+// Helper method to determine the cellular network type using CoreTelephony APIs.
+// Returns the network type flags for the cellular network (WWAN plus any sub-type like
+// 2G/3G/4G/5G).
+- (int)getCellularNetworkType {
+  int networkType = static_cast<int>(Envoy::NetworkType::WWAN);
+#if !TARGET_OS_VISION && !TARGET_OS_WATCH && !TARGET_OS_OSX
+  // Check the sub-type of the cellular network.
+  NSSet<NSString *> *technologies2g =
+      [NSSet setWithObjects:CTRadioAccessTechnologyGPRS, CTRadioAccessTechnologyEdge,
+                            CTRadioAccessTechnologyCDMA1x, nil];
+  NSSet<NSString *> *technologies3g =
+      [NSSet setWithObjects:CTRadioAccessTechnologyWCDMA, CTRadioAccessTechnologyHSDPA,
+                            CTRadioAccessTechnologyHSUPA, CTRadioAccessTechnologyCDMAEVDORev0,
+                            CTRadioAccessTechnologyCDMAEVDORevA,
+                            CTRadioAccessTechnologyCDMAEVDORevB, CTRadioAccessTechnologyeHRPD, nil];
+  NSSet<NSString *> *technologies4g = [NSSet setWithObjects:CTRadioAccessTechnologyLTE, nil];
+  NSSet<NSString *> *technologies5g =
+      [NSSet setWithObjects:RadioAccessTechnologyNR(), RadioAccessTechnologyNRNSA(), nil];
+  NSString *serviceIdentifier = _telephonyInfo.dataServiceIdentifier;
+  if (serviceIdentifier != nil) {
+    NSString *technology = _telephonyInfo.serviceCurrentRadioAccessTechnology[serviceIdentifier];
+    if (technology != nil) {
+      if ([technologies2g containsObject:technology]) {
+        networkType |= static_cast<int>(Envoy::NetworkType::WWAN_2G);
+      } else if ([technologies3g containsObject:technology]) {
+        networkType |= static_cast<int>(Envoy::NetworkType::WWAN_3G);
+      } else if ([technologies4g containsObject:technology]) {
+        networkType |= static_cast<int>(Envoy::NetworkType::WWAN_4G);
+      } else if ([technologies5g containsObject:technology]) {
+        networkType |= static_cast<int>(Envoy::NetworkType::WWAN_5G);
+      }
+    }
+  }
+#endif
+  return networkType;
+}
+
 - (void)checkReachabilityAndNotifyEnvoy:(nw_path_t)path {
   nw_path_status_t pathStatus = nw_path_get_status(path);
   if (pathStatus == nw_path_status_satisfied || pathStatus == nw_path_status_satisfiable) {
@@ -84,44 +121,43 @@ static NSString *RadioAccessTechnologyNR() {
       _networkChangeListener->onDefaultNetworkAvailable();
     }
     int networkType = 0;
-    if (nw_path_uses_interface_type(path, nw_interface_type_wifi) ||
-        nw_path_uses_interface_type(path, nw_interface_type_wired)) {
-      networkType |= static_cast<int>(Envoy::NetworkType::WLAN);
-    } else if (nw_path_uses_interface_type(path, nw_interface_type_cellular)) {
-      networkType |= static_cast<int>(Envoy::NetworkType::WWAN);
-#if !TARGET_OS_VISION && !TARGET_OS_WATCH && !TARGET_OS_OSX
-      // Check the sub-type of the cellular network.
-      NSSet<NSString *> *technologies2g =
-          [NSSet setWithObjects:CTRadioAccessTechnologyGPRS, CTRadioAccessTechnologyEdge,
-                                CTRadioAccessTechnologyCDMA1x, nil];
-      NSSet<NSString *> *technologies3g = [NSSet
-          setWithObjects:CTRadioAccessTechnologyWCDMA, CTRadioAccessTechnologyHSDPA,
-                         CTRadioAccessTechnologyHSUPA, CTRadioAccessTechnologyCDMAEVDORev0,
-                         CTRadioAccessTechnologyCDMAEVDORevA, CTRadioAccessTechnologyCDMAEVDORevB,
-                         CTRadioAccessTechnologyeHRPD, nil];
-      NSSet<NSString *> *technologies4g = [NSSet setWithObjects:CTRadioAccessTechnologyLTE, nil];
-      NSSet<NSString *> *technologies5g =
-          [NSSet setWithObjects:RadioAccessTechnologyNR(), RadioAccessTechnologyNRNSA(), nil];
-      NSString *serviceIdentifier = _telephonyInfo.dataServiceIdentifier;
-      if (serviceIdentifier != nil) {
-        NSString *technology =
-            _telephonyInfo.serviceCurrentRadioAccessTechnology[serviceIdentifier];
-        if (technology != nil) {
-          if ([technologies2g containsObject:technology]) {
-            networkType |= static_cast<int>(Envoy::NetworkType::WWAN_2G);
-          } else if ([technologies3g containsObject:technology]) {
-            networkType |= static_cast<int>(Envoy::NetworkType::WWAN_3G);
-          } else if ([technologies4g containsObject:technology]) {
-            networkType |= static_cast<int>(Envoy::NetworkType::WWAN_4G);
-          } else if ([technologies5g containsObject:technology]) {
-            networkType |= static_cast<int>(Envoy::NetworkType::WWAN_5G);
-          }
+
+    // Check which interface types are available.
+    BOOL hasWifiOrWired = nw_path_uses_interface_type(path, nw_interface_type_wifi) ||
+                          nw_path_uses_interface_type(path, nw_interface_type_wired);
+    BOOL hasCellular = nw_path_uses_interface_type(path, nw_interface_type_cellular);
+
+    if (hasWifiOrWired && hasCellular) {
+      // Both WiFi/wired and cellular are available. Use link quality to determine
+      // if the WiFi/wired connection is good enough to prefer it over cellular.
+#if (defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 260000) ||     \
+    (defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 260000)
+      if (@available(iOS 26.0, macOS 26.0, *)) {
+        nw_path_link_quality_t linkQuality = nw_path_get_link_quality(path);
+        // Link quality is for the "best" interface. If quality is poor or unknown, prefer cellular.
+        if (linkQuality <= nw_path_link_quality_poor) {
+          // Poor WiFi quality, prefer cellular.
+          networkType = [self getCellularNetworkType];
+        } else {
+          // WiFi/wired quality is acceptable, prefer it.
+          networkType = static_cast<int>(Envoy::NetworkType::WLAN);
         }
+      } else {
+        // Fallback for older OS versions: prefer WiFi/wired over cellular.
+        networkType = static_cast<int>(Envoy::NetworkType::WLAN);
       }
+#else
+      // SDK doesn't have link quality APIs, prefer WiFi/wired over cellular.
+      networkType = static_cast<int>(Envoy::NetworkType::WLAN);
 #endif
+    } else if (hasWifiOrWired) {
+      networkType = static_cast<int>(Envoy::NetworkType::WLAN);
+    } else if (hasCellular) {
+      networkType = [self getCellularNetworkType];
     } else {
-      networkType |= static_cast<int>(Envoy::NetworkType::Generic);
+      networkType = static_cast<int>(Envoy::NetworkType::Generic);
     }
+
     // A network can be both VPN and another type, so we need to check for VPN separately.
     if (nw_path_uses_interface_type(path, nw_interface_type_other)) {
       networkType |= static_cast<int>(Envoy::NetworkType::Generic);
