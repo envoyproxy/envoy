b67d1ca0528f81418240d7dca5480f21193c58cc
Xuyang Tao <taoxuy@google.com>
2026-01-09 23:41:03 +0000

    fix
    Signed-off-by: Xuyang Tao <taoxuy@google.com>

--- source/common/network/connection_impl.h
+++ source/common/network/connection_impl.h
@@ -318,7 +318,7 @@ protected:
   void setDetectedCloseType(StreamInfo::DetectedCloseType close_type) override {
     ConnectionImpl::setDetectedCloseType(close_type);
     if (stream_info_.upstreamInfo() != nullptr) {
-      stream_info_.upstreamInfo()->setDownstreamDetectedCloseType(close_type);
+      stream_info_.upstreamInfo()->setUpstreamDetectedCloseType(close_type);
     }
   }
 
