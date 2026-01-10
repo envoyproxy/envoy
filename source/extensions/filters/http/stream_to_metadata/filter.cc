8469c1abfa2ec129c47fb5f7677bd8298bb655d6
Peter Leng <yleng@pinterest.com>
2026-01-10 13:32:58 -0800

    move sse parser to common/http and add fuzz test
    Signed-off-by: Peter Leng <yleng@pinterest.com>

--- source/extensions/filters/http/stream_to_metadata/filter.cc
+++ source/extensions/filters/http/stream_to_metadata/filter.cc
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "source/common/common/utility.h"
+#include "source/common/http/sse/sse_parser.h"
 #include "source/common/http/utility.h"
 #include "source/common/json/json_loader.h"
 #include "source/common/protobuf/utility.h"
@@ -26,117 +27,6 @@ constexpr absl::string_view DefaultSseContentType{"text/event-stream"};
 absl::string_view normalizeContentType(absl::string_view content_type) {
   return StringUtil::trim(StringUtil::cropRight(content_type, ";"));
 }
-
-// Utility class for parsing Server-Sent Events (SSE) format.
-// See: https://html.spec.whatwg.org/multipage/server-sent-events.html
-class SseParser {
-public:
-  // Extracts and concatenates all 'data' field values from an SSE event.
-  // Per SSE spec, multiple data fields are joined with newlines.
-  static std::string extractDataField(absl::string_view event) {
-    std::vector<absl::string_view> data_fields;
-    absl::string_view remaining = event;
-
-    while (!remaining.empty()) {
-      auto [line_end, next_line] = findLineEnd(remaining, true);
-      absl::string_view line = remaining.substr(0, line_end);
-      remaining = remaining.substr(next_line);
-
-      auto [field_name, field_value] = parseFieldLine(line);
-      if (field_name == "data") {
-        data_fields.push_back(field_value);
-      }
-    }
-
-    // Per SSE spec, multiple data fields are concatenated with newline.
-    return absl::StrJoin(data_fields, "\n");
-  }
-
-  // Finds the end of the next SSE event in the buffer.
-  // Returns {event_end, next_event_start} positions, or {npos, npos} if no complete event.
-  // An event ends with a blank line (two consecutive line breaks).
-  static std::pair<size_t, size_t> findEventEnd(absl::string_view str, bool end_stream) {
-    size_t consumed = 0;
-    absl::string_view remaining = str;
-
-    while (!remaining.empty()) {
-      auto [line_end, next_line] = findLineEnd(remaining, end_stream);
-
-      if (line_end == absl::string_view::npos) {
-        return {absl::string_view::npos, absl::string_view::npos};
-      }
-
-      if (line_end == 0) {
-        return {consumed, consumed + next_line};
-      }
-
-      consumed += next_line;
-      remaining = remaining.substr(next_line);
-    }
-
-    return {absl::string_view::npos, absl::string_view::npos};
-  }
-
-private:
-  // Parses an SSE field line into {field_name, field_value}.
-  // Handles comments (lines starting with ':') and strips leading space from value.
-  static std::pair<absl::string_view, absl::string_view> parseFieldLine(absl::string_view line) {
-    if (line.empty()) {
-      return {"", ""};
-    }
-
-    // Per SSE spec, lines starting with ':' are comments and should be ignored.
-    if (line[0] == ':') {
-      return {"", ""};
-    }
-
-    const auto colon_pos = line.find(':');
-    if (colon_pos == absl::string_view::npos) {
-      return {line, ""};
-    }
-
-    absl::string_view field_name = line.substr(0, colon_pos);
-    absl::string_view field_value = line.substr(colon_pos + 1);
-
-    // Per SSE spec, remove leading space from value if present.
-    if (!field_value.empty() && field_value[0] == ' ') {
-      field_value = field_value.substr(1);
-    }
-
-    return {field_name, field_value};
-  }
-
-  // Finds the end of the current line, handling CR, LF, and CRLF line endings.
-  // Returns {line_end, next_line_start} positions.
-  static std::pair<size_t, size_t> findLineEnd(absl::string_view str, bool end_stream) {
-    const auto pos = str.find_first_of("\r\n");
-
-    if (pos == absl::string_view::npos) {
-      if (end_stream) {
-        return {str.size(), str.size()};
-      }
-      return {absl::string_view::npos, absl::string_view::npos};
-    }
-
-    if (str[pos] == '\n') {
-      return {pos, pos + 1};
-    }
-
-    // Per SSE spec, handle CR (\r) and CRLF (\r\n) line endings.
-    if (pos + 1 < str.size()) {
-      if (str[pos + 1] == '\n') {
-        return {pos, pos + 2};
-      }
-      return {pos, pos + 1};
-    }
-
-    // If '\r' is at the end and more data may come, wait to see if it's CRLF.
-    if (end_stream) {
-      return {pos, pos + 1};
-    }
-    return {absl::string_view::npos, absl::string_view::npos};
-  }
-};
 
 } // namespace
 
@@ -233,7 +123,7 @@ void Filter::processBuffer(bool end_stream) {
   absl::string_view buffer_view(buffer_);
 
   while (!buffer_view.empty() && !processing_complete_) {
-    auto [event_end, next_event] = SseParser::findEventEnd(buffer_view, end_stream);
+    auto [event_end, next_event] = Http::Sse::SseParser::findEventEnd(buffer_view, end_stream);
 
     if (event_end == absl::string_view::npos) {
       // No complete event found. Check if buffer exceeds max size.
@@ -265,7 +155,7 @@ void Filter::processBuffer(bool end_stream) {
 }
 
 bool Filter::processSseEvent(absl::string_view event) {
-  const std::string json_string = SseParser::extractDataField(event);
+  const std::string json_string = Http::Sse::SseParser::extractDataField(event);
 
   if (json_string.empty()) {
     ENVOY_LOG(debug, "Event does not contain 'data' field");
