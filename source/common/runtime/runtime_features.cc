envoyproxy/envoy

commit 90e99327ff399e21d9320d73c47bee5bec577ebc

upstream_proxy_protocol: Introduce custom TLV support (#37591)
<!--
!!!ATTENTION!!!

If you are fixing *any* crash or *any* potential security issue, *do
not*
open a pull request in this repo. Please report the issue via emailing
envoy-security@googlegroups.com where the issue will be triaged
appropriately.
Thank you in advance for helping to keep Envoy secure.

!!!ATTENTION!!!

For an explanation of how to fill out the fields, please see the
relevant section
in
[PULL_REQUESTS.md](https://github.com/envoyproxy/envoy/blob/main/PULL_REQUESTS.md)
-->

Commit Message:

This commit introduces support for injecting custom TLVs into the Proxy
Protocol v2 (PP2) header for upstream transport sockets. This enables
xDS control planes to build upstream PP2 headers with greater
flexibility. Previously, upstream PP2 headers only passed through TLVs
from downstream connections when using the Proxy Protocol listener,
limiting customization.

With this change, users can define custom TLVs by configuring the
`custom_tlvs` field in the upstream_proxy_protocol transport socket
config, or specifying host metadata in a well-known namespace in order
to provide dynamic and more granular control over PP2 header content.
For example:

```yaml
      transport_socket:
        name: envoy.transport_sockets.upstream_proxy_protocol
        typed_config:
          "@type": type.googleapis.com/envoy.extensions.transport_sockets.proxy_protocol.v3.ProxyProtocolUpstreamTransport
          config:
            version: V2
            added_tlvs:
              - type: 150
                value: Zm9v
              - type: 151
                value: YmFy
```

And

```yaml
clusters:
      - name: httpbin
        load_assignment:
          ...
          endpoints:
          - lbEndpoints:
            - metadata:
                filter_metadata:
                  envoy.transport_socket_match:
                    outbound-proxy: true

api/envoy/config/core/v3/proxy_protocol.proto
@@ -32,6 +32,15 @@ message ProxyProtocolPassThroughTLVs {
   repeated uint32 tlv_type = 2 [(validate.rules).repeated = {items {uint32 {lt: 256}}}];
 }
 
+// Represents a single Type-Length-Value (TLV) entry.
+message TlvEntry {
+  // The type of the TLV. Must be a uint8 (0-255) as per the Proxy Protocol v2 specification.
+  uint32 type = 1 [(validate.rules).uint32 = {lt: 256}];
+
+  // The value of the TLV. Must be at least one byte long.
+  bytes value = 2 [(validate.rules).bytes = {min_len: 1}];
+}
+
 message ProxyProtocolConfig {
   enum Version {
     // PROXY protocol version 1. Human readable format.
@@ -47,4 +56,35 @@ message ProxyProtocolConfig {
   // This config controls which TLVs can be passed to upstream if it is Proxy Protocol
   // V2 header. If there is no setting for this field, no TLVs will be passed through.
   ProxyProtocolPassThroughTLVs pass_through_tlvs = 2;
+
+  // This config allows additional TLVs to be included in the upstream PROXY protocol
+  // V2 header. Unlike ``pass_through_tlvs``, which passes TLVs from the downstream request,
+  // ``added_tlvs`` provides an extension mechanism for defining new TLVs that are included
+  // with the upstream request. These TLVs may not be present in the downstream request and
+  // can be defined at either the transport socket level or the host level to provide more
+  // granular control over the TLVs that are included in the upstream request.
+  //
+  // Host-level TLVs are specified in the ``metadata.typed_filter_metadata`` field under the
+  // ``envoy.transport_sockets.proxy_protocol`` namespace.
+  //
+  // .. literalinclude:: /_configs/repo/proxy_protocol.yaml
+  //    :language: yaml
+  //    :lines: 49-57
+  //    :linenos:
+  //    :lineno-start: 49
+  //    :caption: :download:`proxy_protocol.yaml </_configs/repo/proxy_protocol.yaml>`
+  //
+  // **Precedence behavior**:
+  //
+  // - When a TLV is defined at both the host level and the transport socket level, the value
+  //   from the host level configuration takes precedence. This allows users to define default TLVs
+  //   at the transport socket level and override them at the host level.
+  // - Any TLV defined in the ``pass_through_tlvs`` field will be overridden by either the host-level
+  //   or transport socket-level TLV.
+  repeated TlvEntry added_tlvs = 3;
+}
+
+message PerHostConfig {
+  // Enables per-host configuration for Proxy Protocol.
+  repeated TlvEntry added_tlvs = 1;
 }

source/extensions/common/proxy_protocol/proxy_protocol_header.cc
@@ -111,19 +111,47 @@ void generateV2Header(const Network::Address::Ip& source_address,
 }
 
 bool generateV2Header(const Network::ProxyProtocolData& proxy_proto_data, Buffer::Instance& out,
-                      bool pass_all_tlvs, const absl::flat_hash_set<uint8_t>& pass_through_tlvs) {
-  uint64_t extension_length = 0;
-  for (auto&& tlv : proxy_proto_data.tlv_vector_) {
+                      bool pass_all_tlvs, const absl::flat_hash_set<uint8_t>& pass_through_tlvs,
+                      const std::vector<Envoy::Network::ProxyProtocolTLV>& custom_tlvs) {
+  std::vector<Envoy::Network::ProxyProtocolTLV> combined_tlv_vector;
+  std::vector<Envoy::Network::ProxyProtocolTLV> final_tlvs;
+  combined_tlv_vector.reserve(custom_tlvs.size() + proxy_proto_data.tlv_vector_.size());
+
+  absl::flat_hash_set<uint8_t> seen_types;
+  for (const auto& tlv : custom_tlvs) {
+    ASSERT(!seen_types.contains(tlv.type));
+    combined_tlv_vector.emplace_back(tlv);
+    seen_types.insert(tlv.type);
+  }
+
+  // Combine TLVs from the proxy_proto_data with the custom TLVs.
+  for (const auto& tlv : proxy_proto_data.tlv_vector_) {
     if (!pass_all_tlvs && !pass_through_tlvs.contains(tlv.type)) {
+      // Skip any TLV that is not in the set of passthrough TLVs.
       continue;
     }
-    extension_length += PROXY_PROTO_V2_TLV_TYPE_LENGTH_LEN + tlv.value.size();
-    if (extension_length > std::numeric_limits<uint16_t>::max()) {
-      ENVOY_LOG_MISC(
-          warn, "Generating Proxy Protocol V2 header: TLVs exceed length limit {}, already got {}",
-          std::numeric_limits<uint16_t>::max(), extension_length);
-      return false;
+    if (seen_types.contains(tlv.type)) {
+      // Skip any duplicate TLVs from being added to the combined TLV vector.
+      ENVOY_LOG_EVERY_POW_2_MISC(info, "Skipping duplicate TLV type {}", tlv.type);
+      continue;
     }
+    seen_types.insert(tlv.type);
+    combined_tlv_vector.emplace_back(tlv);
+  }
+
+  // Filter out TLVs that would exceed the 65535 limit.
+  uint64_t extension_length = 0;
+  bool skipped_tlvs = false;
+  for (auto&& tlv : combined_tlv_vector) {
+    uint64_t new_size = extension_length + PROXY_PROTO_V2_TLV_TYPE_LENGTH_LEN + tlv.value.size();
+    if (new_size > std::numeric_limits<uint16_t>::max()) {
+      ENVOY_LOG_MISC(warn, "Skipping TLV type {} because adding it would exceed the 65535 limit.",
+                     tlv.type);
+      skipped_tlvs = true;
+      continue;
+    }
+    extension_length = new_size;
+    final_tlvs.push_back(tlv);
   }
 
   ASSERT(extension_length <= std::numeric_limits<uint16_t>::max());
@@ -141,17 +169,16 @@ bool generateV2Header(const Network::ProxyProtocolData& proxy_proto_data, Buffer
   generateV2Header(src.addressAsString(), dst.addressAsString(), src.port(), dst.port(),
                    src.version(), static_cast<uint16_t>(extension_length), out);
 
-  // Generate the TLV vector.
-  for (auto&& tlv : proxy_proto_data.tlv_vector_) {
-    if (!pass_all_tlvs && !pass_through_tlvs.contains(tlv.type)) {
-      continue;
-    }
+  for (auto&& tlv : combined_tlv_vector) {
     out.add(&tlv.type, 1);
     uint16_t size = htons(static_cast<uint16_t>(tlv.value.size()));
     out.add(&size, sizeof(uint16_t));
     out.add(&tlv.value.front(), tlv.value.size());
   }
-  return true;
+
+  // return true if no TLVs were skipped, otherwise false to increment the counter
+  // in the upstream proxy protocol transport socket stats.
+  return !skipped_tlvs;
 }
 
 void generateProxyProtoHeader(const envoy::config::core::v3::ProxyProtocolConfig& config,

commit 99e99dd4075c33ec7e435b16c247808994c97809

proxy_protocol: fix bug that disallowed duplicate TLVs (#42894)
A bug was introduced in
https://github.com/envoyproxy/envoy/pull/37591 which removed TLVs if
there were multiple TLVs with the same key. This is not correct behavior
according to the PROXY protocol specification.

This change allows duplicates to both be passed through from a
downstream, or configured for addition at the transport socket or host
level.

Runtime guard:
`envoy.reloadable_features.proxy_protocol_allow_duplicate_tlvs`
Fixes #42075

Signed-off-by: Greg Greenway <ggreenway@apple.com>

api/envoy/config/core/v3/proxy_protocol.proto
@@ -102,6 +102,9 @@ message ProxyProtocolConfig {
   //   at the transport socket level and override them at the host level.
   // - Any TLV defined in the ``pass_through_tlvs`` field will be overridden by either the host-level
   //   or transport socket-level TLV.
+  //
+  // If there are multiple TLVs with the same type, only the TLVs from the highest precedence level
+  // will be used.
   repeated TlvEntry added_tlvs = 3;
 }
 

source/common/runtime/runtime_features.cc
@@ -61,6 +61,7 @@ RUNTIME_GUARD(envoy_reloadable_features_odcds_over_ads_fix);
 RUNTIME_GUARD(envoy_reloadable_features_on_demand_track_end_stream);
 RUNTIME_GUARD(envoy_reloadable_features_original_dst_rely_on_idle_timeout);
 RUNTIME_GUARD(envoy_reloadable_features_prefix_map_matcher_resume_after_subtree_miss);
+RUNTIME_GUARD(envoy_reloadable_features_proxy_protocol_allow_duplicate_tlvs);
 RUNTIME_GUARD(envoy_reloadable_features_quic_defer_logging_to_ack_listener);
 RUNTIME_GUARD(envoy_reloadable_features_quic_fix_defer_logging_miss_for_half_closed_stream);
 // Ignore the automated "remove this flag" issue: we should keep this for 1 year. Confirm with

source/extensions/common/proxy_protocol/proxy_protocol_header.cc
@@ -6,6 +6,7 @@
 #include "envoy/network/address.h"
 
 #include "source/common/network/address_impl.h"
+#include "source/common/runtime/runtime_features.h"
 
 namespace Envoy {
 namespace Extensions {
@@ -117,26 +118,46 @@ bool generateV2Header(const Network::ProxyProtocolData& proxy_proto_data, Buffer
   std::vector<Envoy::Network::ProxyProtocolTLV> final_tlvs;
   combined_tlv_vector.reserve(custom_tlvs.size() + proxy_proto_data.tlv_vector_.size());
 
-  absl::flat_hash_set<uint8_t> seen_types;
-  for (const auto& tlv : custom_tlvs) {
-    ASSERT(!seen_types.contains(tlv.type));
-    combined_tlv_vector.emplace_back(tlv);
-    seen_types.insert(tlv.type);
-  }
+  if (Runtime::runtimeFeatureEnabled(
+          "envoy.reloadable_features.proxy_protocol_allow_duplicate_tlvs")) {
+    absl::flat_hash_set<uint8_t> config_specified_types;
+    for (const auto& tlv : custom_tlvs) {
+      combined_tlv_vector.emplace_back(tlv);
+      config_specified_types.insert(tlv.type);
+    }
 
-  // Combine TLVs from the proxy_proto_data with the custom TLVs.
-  for (const auto& tlv : proxy_proto_data.tlv_vector_) {
-    if (!pass_all_tlvs && !pass_through_tlvs.contains(tlv.type)) {
-      // Skip any TLV that is not in the set of passthrough TLVs.
-      continue;
+    // Combine TLVs from the proxy_proto_data with the custom TLVs.
+    for (const auto& tlv : proxy_proto_data.tlv_vector_) {
+      if (!pass_all_tlvs && !pass_through_tlvs.contains(tlv.type)) {
+        // Skip any TLV that is not in the set of passthrough TLVs.
+        continue;
+      }
+      if (!config_specified_types.contains(tlv.type)) {
+        combined_tlv_vector.emplace_back(tlv);
+      }
     }
-    if (seen_types.contains(tlv.type)) {
-      // Skip any duplicate TLVs from being added to the combined TLV vector.
-      ENVOY_LOG_EVERY_POW_2_MISC(info, "Skipping duplicate TLV type {}", tlv.type);
-      continue;
+  } else {
+    absl::flat_hash_set<uint8_t> seen_types;
+    for (const auto& tlv : custom_tlvs) {
+      ASSERT(!seen_types.contains(tlv.type));
+      combined_tlv_vector.emplace_back(tlv);
+      seen_types.insert(tlv.type);
     }
-    seen_types.insert(tlv.type);
-    combined_tlv_vector.emplace_back(tlv);
+
+    // Combine TLVs from the proxy_proto_data with the custom TLVs.
+    for (const auto& tlv : proxy_proto_data.tlv_vector_) {
+      if (!pass_all_tlvs && !pass_through_tlvs.contains(tlv.type)) {
+        // Skip any TLV that is not in the set of passthrough TLVs.
+        continue;
+      }
+      if (seen_types.contains(tlv.type)) {
+        // Skip any duplicate TLVs from being added to the combined TLV vector.
+        ENVOY_LOG_EVERY_POW_2_MISC(info, "Skipping duplicate TLV type {}", tlv.type);
+        continue;
+      }
+      seen_types.insert(tlv.type);
+      combined_tlv_vector.emplace_back(tlv);
+    }
   }
 
   // Filter out TLVs that would exceed the 65535 limit.

source/extensions/transport_sockets/proxy_protocol/proxy_protocol.cc
@@ -179,7 +179,10 @@ void UpstreamProxyProtocolSocketFactory::hashKey(
 
 std::vector<Envoy::Network::ProxyProtocolTLV> UpstreamProxyProtocolSocket::buildCustomTLVs() const {
   std::vector<Envoy::Network::ProxyProtocolTLV> custom_tlvs;
-  absl::flat_hash_set<uint8_t> processed_tlv_types;
+  absl::flat_hash_set<uint8_t> host_level_tlv_types;
+
+  const bool runtime_allow_duplicate_tlvs = Runtime::runtimeFeatureEnabled(
+      "envoy.reloadable_features.proxy_protocol_allow_duplicate_tlvs");
 
   // Attempt to parse host-level TLVs first.
   const auto& upstream_info = callbacks_->connection().streamInfo().upstreamInfo();
@@ -198,16 +201,25 @@ std::vector<Envoy::Network::ProxyProtocolTLV> UpstreamProxyProtocolSocket::build
                     upstream_info->upstreamHost()->address()->asString(), status.message());
         } else {
           // Insert host-level TLVs.
-          for (const auto& entry : host_tlv_metadata.added_tlvs()) {
-            if (processed_tlv_types.contains(entry.type())) {
-              ENVOY_LOG_EVERY_POW_2_MISC(info, "Skipping duplicate TLV type from host metadata {}",
-                                         entry.type());
-              continue;
+          if (runtime_allow_duplicate_tlvs) {
+            for (const auto& entry : host_tlv_metadata.added_tlvs()) {
+              custom_tlvs.push_back(Network::ProxyProtocolTLV{
+                  static_cast<uint8_t>(entry.type()),
+                  std::vector<unsigned char>(entry.value().begin(), entry.value().end())});
+              host_level_tlv_types.insert(entry.type());
             }
-            custom_tlvs.push_back(Network::ProxyProtocolTLV{
-                static_cast<uint8_t>(entry.type()),
-                std::vector<unsigned char>(entry.value().begin(), entry.value().end())});
-            processed_tlv_types.insert(entry.type());
+          } else {
+            for (const auto& entry : host_tlv_metadata.added_tlvs()) {
+              if (host_level_tlv_types.contains(entry.type())) {
+                ENVOY_LOG_EVERY_POW_2_MISC(
+                    info, "Skipping duplicate TLV type from host metadata {}", entry.type());
+                continue;
+              }
+              custom_tlvs.push_back(Network::ProxyProtocolTLV{
+                  static_cast<uint8_t>(entry.type()),
+                  std::vector<unsigned char>(entry.value().begin(), entry.value().end())});
+              host_level_tlv_types.insert(entry.type());
+            }
           }
         }
       }
@@ -215,13 +227,22 @@ std::vector<Envoy::Network::ProxyProtocolTLV> UpstreamProxyProtocolSocket::build
   }
 
   // If host-level parse failed or was not present, we still read config-level TLVs.
-  for (const auto& tlv : added_tlvs_) {
-    if (processed_tlv_types.contains(tlv.type)) {
-      ENVOY_LOG_EVERY_POW_2_MISC(info, "Skipping duplicate TLV type from added_tlvs {}", tlv.type);
-      continue;
+  if (runtime_allow_duplicate_tlvs) {
+    for (const auto& tlv : added_tlvs_) {
+      if (!host_level_tlv_types.contains(tlv.type)) {
+        custom_tlvs.push_back(tlv);
+      }
     }
-    custom_tlvs.push_back(tlv);
-    processed_tlv_types.insert(tlv.type);
+  } else {
+    for (const auto& tlv : added_tlvs_) {
+      if (host_level_tlv_types.contains(tlv.type)) {
+        ENVOY_LOG_EVERY_POW_2_MISC(info, "Skipping duplicate TLV type from added_tlvs {}",
+                                   tlv.type);
+        continue;
+      }
+      custom_tlvs.push_back(tlv);
+      host_level_tlv_types.insert(tlv.type);
+    }
   }
 
   return custom_tlvs;

test/extensions/transport_sockets/proxy_protocol/BUILD
@@ -24,6 +24,7 @@ envoy_extension_cc_test(
         "//test/mocks/network:io_handle_mocks",
         "//test/mocks/network:network_mocks",
         "//test/mocks/network:transport_socket_mocks",
+        "//test/test_common:test_runtime_lib",
         "@envoy_api//envoy/config/core/v3:pkg_cc_proto",
         "@envoy_api//envoy/extensions/transport_sockets/proxy_protocol/v3:pkg_cc_proto",
     ],

test/extensions/transport_sockets/proxy_protocol/proxy_protocol_test.cc
@@ -13,6 +13,7 @@
 #include "test/mocks/network/io_handle.h"
 #include "test/mocks/network/mocks.h"
 #include "test/mocks/network/transport_socket.h"
+#include "test/test_common/test_runtime.h"
 #include "test/test_common/utility.h"
 
 #include "gmock/gmock.h"
@@ -880,12 +881,212 @@ TEST_F(ProxyProtocolTest, V2CombinedPrecedenceHostConfigPassthrough) {
 }
 
 // Test verifies that duplicate TLVs within the config and host metadata are properly handled.
+// Each level (host, config, pass-through) overrides TLVs with the same key from a lower level, but
+// duplicates within a level are allowed.
 TEST_F(ProxyProtocolTest, V2DuplicateTLVsInConfigAndMetadataHandledProperly) {
+  TestScopedRuntime scoped_runtime;
+  scoped_runtime.mergeValues(
+      {{"envoy.reloadable_features.proxy_protocol_allow_duplicate_tlvs", "true"}});
+
   auto src_addr =
       Network::Address::InstanceConstSharedPtr(new Network::Address::Ipv6Instance("1:2:3::4", 8));
   auto dst_addr = Network::Address::InstanceConstSharedPtr(
       new Network::Address::Ipv6Instance("1:100:200:3::", 2));
-  Network::ProxyProtocolTLVVector tlv_vector{Network::ProxyProtocolTLV{0x5, {'a', 'b', 'c'}}};
+
+  constexpr uint8_t pass_through_tlv = 0x5;
+  constexpr uint8_t transport_socket_config_tlv_1 = 0x96;
+  constexpr uint8_t transport_socket_config_tlv_2 = 0x97;
+  constexpr uint8_t host_config_tlv = 0x98;
+
+  // These are the downstream over-the-wire TLVs.
+  // This contains all the TLVs in the configuration (0x96-0x98) as well as one that isn't (0x5).
+  // Only 0x5 is passed through as the others are overridden.
+  Network::ProxyProtocolTLVVector tlv_vector{
+      {pass_through_tlv, {'a', 'b', 'c'}},
+
+      // Two values for this key to ensure both are removed and overridden.
+      {transport_socket_config_tlv_1, {0}},
+      {transport_socket_config_tlv_1, {1}},
+
+      {transport_socket_config_tlv_2, {0}},
+      {host_config_tlv, {0}},
+  };
+  Network::ProxyProtocolData proxy_proto_data{src_addr, dst_addr, tlv_vector};
+
+  Network::TransportSocketOptionsConstSharedPtr socket_options =
+      std::make_shared<Network::TransportSocketOptionsImpl>(
+          "", std::vector<std::string>{}, std::vector<std::string>{}, std::vector<std::string>{},
+          absl::optional<Network::ProxyProtocolData>(proxy_proto_data));
+  transport_callbacks_.connection_.stream_info_.downstream_connection_info_provider_
+      ->setLocalAddress(*Network::Utility::resolveUrl("tcp://[1:100:200:3::]:50000"));
+  transport_callbacks_.connection_.stream_info_.downstream_connection_info_provider_
+      ->setRemoteAddress(*Network::Utility::resolveUrl("tcp://[e:b:c:f::]:8080"));
+
+  auto host = std::make_shared<NiceMock<Upstream::MockHostDescription>>();
+  auto metadata = std::make_shared<envoy::config::core::v3::Metadata>();
+  const std::string metadata_key =
+      Config::MetadataFilters::get().ENVOY_TRANSPORT_SOCKETS_PROXY_PROTOCOL;
+
+  PerHostConfig host_metadata_config;
+  auto host_added_tlvs = host_metadata_config.add_added_tlvs();
+  host_added_tlvs->set_type(host_config_tlv);
+  host_added_tlvs->set_value("d1");
+  auto duplicate_host_entry = host_metadata_config.add_added_tlvs();
+  duplicate_host_entry->set_type(host_config_tlv);
+  duplicate_host_entry->set_value("d2");
+  Protobuf::Any typed_metadata;
+  typed_metadata.PackFrom(host_metadata_config);
+  metadata->mutable_typed_filter_metadata()->emplace(std::make_pair(metadata_key, typed_metadata));
+  EXPECT_CALL(*host, metadata()).WillRepeatedly(Return(metadata));
+  transport_callbacks_.connection_.streamInfo().upstreamInfo()->setUpstreamHost(host);
+
+  // The output buffer will include the host TLVs before the config TLVs.
+  const std::vector<uint8_t> expected{
+      0x0d,
+      0x0a,
+      0x0d,
+      0x0a,
+      0x00,
+      0x0d,
+      0x0a,
+      0x51,
+      0x55,
+      0x49,
+      0x54,
+      0x0a,
+      0x21,
+      0x21,
+      0x00,
+      0x47,
+      0x00,
+      0x01,
+      0x00,
+      0x02,
+      0x00,
+      0x03,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x04,
+      0x00,
+      0x01,
+      0x01,
+      0x00,
+      0x02,
+      0x00,
+      0x00,
+      0x03,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x08,
+      0x00,
+      0x02,
+
+      host_config_tlv,
+      0x00,
+      0x02,
+      'd',
+      '1',
+
+      host_config_tlv,
+      0x00,
+      0x02,
+      'd',
+      '2',
+
+      transport_socket_config_tlv_1,
+      0x00,
+      0x03,
+      'b',
+      'a',
+      'r',
+
+      transport_socket_config_tlv_1,
+      0x00,
+      0x04,
+      'b',
+      'a',
+      'r',
+      '2',
+
+      transport_socket_config_tlv_2,
+      0x00,
+      0x03,
+      'b',
+      'a',
+      'z',
+
+      pass_through_tlv,
+      0x00,
+      0x03,
+      'a',
+      'b',
+      'c',
+  };
+
+  // Configure duplicate TLVs in the configuration.
+  ProxyProtocolConfig config;
+  config.set_version(ProxyProtocolConfig_Version::ProxyProtocolConfig_Version_V2);
+  config.mutable_pass_through_tlvs()->set_match_type(ProxyProtocolPassThroughTLVs::INCLUDE_ALL);
+  auto tlv = config.add_added_tlvs();
+  tlv->set_type(transport_socket_config_tlv_1);
+  tlv->set_value("bar");
+  auto duplicate_tlv_entry = config.add_added_tlvs();
+  duplicate_tlv_entry->set_type(transport_socket_config_tlv_1);
+  duplicate_tlv_entry->set_value("bar2");
+  auto unique_tlv_entry = config.add_added_tlvs();
+  unique_tlv_entry->set_type(transport_socket_config_tlv_2);
+  unique_tlv_entry->set_value("baz");
+  initialize(config, socket_options);
+
+  EXPECT_CALL(io_handle_, write(_))
+      .WillOnce(Invoke([&](Buffer::Instance& buffer) -> Api::IoCallUint64Result {
+        const auto length = buffer.length();
+
+        // Compare as hex encoded to make errors easier to read.
+        EXPECT_EQ(
+            Hex::encode(reinterpret_cast<uint8_t*>(buffer.linearize(length)), buffer.length()),
+            Hex::encode(expected));
+
+        buffer.drain(length);
+        return {length, Api::IoError::none()};
+      }));
+  auto msg = Buffer::OwnedImpl("some data");
+  EXPECT_CALL(*inner_socket_, doWrite(BufferEqual(&msg), false));
+
+  auto resp = proxy_protocol_socket_->doWrite(msg, false);
+  EXPECT_EQ(resp.bytes_processed_, expected.size());
+}
+
+// Test verifies that duplicate TLVs within the config and host metadata are properly handled. No
+// duplicate TLVs are allowed.
+TEST_F(ProxyProtocolTest, V2DuplicateTLVsInConfigAndMetadataHandledProperlyNoDuplicatesAllowed) {
+  TestScopedRuntime scoped_runtime;
+  scoped_runtime.mergeValues(
+      {{"envoy.reloadable_features.proxy_protocol_allow_duplicate_tlvs", "false"}});
+
+  auto src_addr =
+      Network::Address::InstanceConstSharedPtr(new Network::Address::Ipv6Instance("1:2:3::4", 8));
+  auto dst_addr = Network::Address::InstanceConstSharedPtr(
+      new Network::Address::Ipv6Instance("1:100:200:3::", 2));
+  Network::ProxyProtocolTLVVector tlv_vector{
+      Network::ProxyProtocolTLV{0x5, {'a', 'b', 'c'}},
+      Network::ProxyProtocolTLV{0x5, {'d'}}, // This is a duplicate and will be removed.
+      Network::ProxyProtocolTLV{0x6, {'a'}}, // This is not passed through.
+  };
   Network::ProxyProtocolData proxy_proto_data{src_addr, dst_addr, tlv_vector};
   Network::TransportSocketOptionsConstSharedPtr socket_options =
       std::make_shared<Network::TransportSocketOptionsImpl>(
@@ -914,7 +1115,7 @@ TEST_F(ProxyProtocolTest, V2DuplicateTLVsInConfigAndMetadataHandledProperly) {
   EXPECT_CALL(*host, metadata()).WillRepeatedly(Return(metadata));
   transport_callbacks_.connection_.streamInfo().upstreamInfo()->setUpstreamHost(host);
 
-  absl::flat_hash_set<uint8_t> pass_through_tlvs{};
+  absl::flat_hash_set<uint8_t> pass_through_tlvs{0x5};
   // The output buffer will include the host TLVs before the config TLVs.
   std::vector<Envoy::Network::ProxyProtocolTLV> custom_tlvs = {
       {0x98, {'d', '1'}},
@@ -927,6 +1128,8 @@ TEST_F(ProxyProtocolTest, V2DuplicateTLVsInConfigAndMetadataHandledProperly) {
 
   // Configure duplicate TLVs in the configuration.
   ProxyProtocolConfig config;
+  config.mutable_pass_through_tlvs()->set_match_type(ProxyProtocolPassThroughTLVs::INCLUDE);
+  config.mutable_pass_through_tlvs()->add_tlv_type(0x5);
   config.set_version(ProxyProtocolConfig_Version::ProxyProtocolConfig_Version_V2);
   auto tlv = config.add_added_tlvs();
   tlv->set_type(0x96);