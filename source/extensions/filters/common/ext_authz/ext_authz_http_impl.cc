f2e433f7c9874b501cfe1c235d9dd447973a1808
Rohit Agrawal <rohit.agrawal@databricks.com>
2026-01-16 21:34:24 -0800

    ext_authz: fix header propagation to client for denied authorization responses
    Signed-off-by: Rohit Agrawal <rohit.agrawal@databricks.com>

--- source/extensions/filters/common/ext_authz/ext_authz_http_impl.cc
+++ source/extensions/filters/common/ext_authz/ext_authz_http_impl.cc
@@ -434,26 +434,30 @@ ResponsePtr RawHttpClientImpl::toResponse(Http::ResponseMessagePtr message) {
   }
 
   // Create a Denied authorization response.
-  SuccessResponse denied{message->headers(),
-                         config_->upstreamHeaderMatchers(),
-                         config_->upstreamHeaderToAppendMatchers(),
-                         config_->clientHeaderMatchers(),
-                         config_->dynamicMetadataMatchers(),
-                         Response{CheckStatus::Denied,
-                                  UnsafeHeaderVector{},
-                                  UnsafeHeaderVector{},
-                                  UnsafeHeaderVector{},
-                                  UnsafeHeaderVector{},
-                                  UnsafeHeaderVector{},
-                                  UnsafeHeaderVector{},
-                                  UnsafeHeaderVector{},
-                                  false,
-                                  {{}},
-                                  Http::Utility::QueryParamsVector{},
-                                  {},
-                                  message->bodyAsString(),
-                                  static_cast<Http::Code>(status_code),
-                                  Protobuf::Struct{}}};
+  // For denied responses, we will use clientHeaderMatchers i.e., allowed_client_headers in
+  // the first matcher position so headers are placed in headers_to_set, which is what the
+  // ext_authz filter uses when sending the local reply to the client.
+  SuccessResponse denied{
+      message->headers(),
+      config_->clientHeaderMatchers(),           // allowed_client_headers
+      config_->upstreamHeaderToAppendMatchers(), // allowed_upstream_headers_to_append
+      config_->clientHeaderMatchers(),           // allowed_client_headers_on_success
+      config_->dynamicMetadataMatchers(),        // dynamic_metadata_from_headers
+      Response{CheckStatus::Denied,
+               UnsafeHeaderVector{},
+               UnsafeHeaderVector{},
+               UnsafeHeaderVector{},
+               UnsafeHeaderVector{},
+               UnsafeHeaderVector{},
+               UnsafeHeaderVector{},
+               UnsafeHeaderVector{},
+               false,
+               {{}},
+               Http::Utility::QueryParamsVector{},
+               {},
+               message->bodyAsString(),
+               static_cast<Http::Code>(status_code),
+               Protobuf::Struct{}}};
   return std::move(denied.response_);
 }
 
