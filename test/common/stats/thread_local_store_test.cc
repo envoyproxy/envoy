88a7488ba9e7235dfd93c972010e3a7ce26f5441
Xuyang Tao <taoxuy@google.com>
2026-02-07 00:14:54 +0000

    Revert "fix"
    This reverts commit f889b9980a1aebec54c7987e66a909153f6430e3.
    
    Signed-off-by: Xuyang Tao <taoxuy@google.com>

--- test/common/stats/thread_local_store_test.cc
+++ test/common/stats/thread_local_store_test.cc
@@ -504,7 +504,11 @@ TEST_F(StatsThreadLocalStoreTest, StatsNumLimitsWithEviction) {
   InSequence s;
   store_->initializeThreading(main_thread_dispatcher_, tls_);
 
-  ScopeSharedPtr scope = store_->createScope("scope.", true, {1, 1, 1});
+  EvictionSettings settings;
+  settings.evict_counters = true;
+  settings.evict_gauges = true;
+  settings.evict_histograms = true;
+  ScopeSharedPtr scope = store_->createScope("scope.", settings, {1, 1, 1});
   EXPECT_EQ(0, TestUtility::findCounter(*store_, "server.stats_overflow.counter")->value());
   EXPECT_EQ(0, TestUtility::findCounter(*store_, "server.stats_overflow.gauge")->value());
   EXPECT_EQ(0, TestUtility::findCounter(*store_, "server.stats_overflow.histogram")->value());
