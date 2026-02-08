b8050f928bfd8ebdf2d78cbf79cc04cb9cd0df40
Ryan Northey <ryan@synca.io>
2026-02-08 10:20:53 +0000

    release/binary: Add contrib to version string
    Signed-off-by: Ryan Northey <ryan@synca.io>

---
+++ contrib/exe/version_suffix.cc
@@ -0,0 +1,4 @@
+// This file provides the version suffix for the contrib Envoy binary.
+// The contrib suffix distinguishes it from the standard binary.
+
+__attribute__((used)) const char build_version_suffix[] = "-contrib";
---
+++ source/exe/version_suffix.cc
@@ -0,0 +1,4 @@
+// This file provides the version suffix for the standard Envoy binary.
+// The contrib binary provides its own version with a "-contrib" suffix.
+
+__attribute__((used)) const char build_version_suffix[] = "";
