77542c5d2d690c6efe27c8152ad2fbd5f4c268ba
Rohit Agrawal <rohit.agrawal@databricks.com>
2026-02-12 09:10:26 -0800

    dynamic_modules: adds timer API to the bootstrap extension (#43389)
    ## Description
    
    This PR adds timer API to the bootstrap extension dynamic modules ABI.
    It'll enable periodic tasks such as ETCD refresh, certificate rotation,
    and service discovery heartbeats without requiring manual OS thread
    management.
    
    ---
    
    **Commit Message:** dynamic_modules: adds timer API to the bootstrap
    extension
    **Additional Description:** Added timer API to the bootstrap extension
    dynamic modules ABI.
    **Risk Level:** Low
    **Testing:** Added Tests
    **Docs Changes:** Added
    **Release Notes:** Added
    
    ---------
    
    Signed-off-by: Rohit Agrawal <rohit.agrawal@databricks.com>

--- test/extensions/dynamic_modules/test_data/c/bootstrap_no_timer_fired.c
+++ test/extensions/dynamic_modules/test_data/c/bootstrap_no_timer_fired.c
@@ -9,7 +9,7 @@ envoy_dynamic_module_type_abi_version_module_ptr envoy_dynamic_module_on_program
   return envoy_dynamic_modules_abi_version;
 }
 
-// A bootstrap extension where envoy_dynamic_module_on_bootstrap_extension_new returns nullptr.
+// A bootstrap extension that is missing envoy_dynamic_module_on_bootstrap_extension_timer_fired.
 
 envoy_dynamic_module_type_bootstrap_extension_config_module_ptr
 envoy_dynamic_module_on_bootstrap_extension_config_new(
@@ -32,8 +32,7 @@ envoy_dynamic_module_on_bootstrap_extension_new(
     envoy_dynamic_module_type_bootstrap_extension_envoy_ptr extension_envoy_ptr) {
   (void)extension_config_ptr;
   (void)extension_envoy_ptr;
-  // Return nullptr to test null extension handling.
-  return NULL;
+  return (envoy_dynamic_module_type_bootstrap_extension_module_ptr)0x2;
 }
 
 void envoy_dynamic_module_on_bootstrap_extension_server_initialized(
@@ -47,11 +46,6 @@ void envoy_dynamic_module_on_bootstrap_extension_worker_thread_initialized(
     envoy_dynamic_module_type_bootstrap_extension_envoy_ptr extension_envoy_ptr,
     envoy_dynamic_module_type_bootstrap_extension_module_ptr extension_module_ptr) {
   (void)extension_envoy_ptr;
-  (void)extension_module_ptr;
-}
-
-void envoy_dynamic_module_on_bootstrap_extension_destroy(
-    envoy_dynamic_module_type_bootstrap_extension_module_ptr extension_module_ptr) {
   (void)extension_module_ptr;
 }
 
@@ -69,6 +63,11 @@ void envoy_dynamic_module_on_bootstrap_extension_shutdown(
   (void)extension_envoy_ptr;
   (void)extension_module_ptr;
   completion_callback(completion_context);
+}
+
+void envoy_dynamic_module_on_bootstrap_extension_destroy(
+    envoy_dynamic_module_type_bootstrap_extension_module_ptr extension_module_ptr) {
+  (void)extension_module_ptr;
 }
 
 void envoy_dynamic_module_on_bootstrap_extension_config_scheduled(
@@ -96,11 +95,5 @@ void envoy_dynamic_module_on_bootstrap_extension_http_callout_done(
   (void)body_chunks_size;
 }
 
-void envoy_dynamic_module_on_bootstrap_extension_timer_fired(
-    envoy_dynamic_module_type_bootstrap_extension_config_envoy_ptr extension_config_envoy_ptr,
-    envoy_dynamic_module_type_bootstrap_extension_config_module_ptr extension_config_module_ptr,
-    envoy_dynamic_module_type_bootstrap_extension_timer_module_ptr timer_ptr) {
-  (void)extension_config_envoy_ptr;
-  (void)extension_config_module_ptr;
-  (void)timer_ptr;
-}
+// NOTE: envoy_dynamic_module_on_bootstrap_extension_timer_fired is intentionally missing.
+
