09bd5daa6e8534ce95462f73fd681535669a5e25
Peter Leng <yleng@pinterest.com>
2026-02-18 13:19:46 -0800

    Comment
    Signed-off-by: Peter Leng <yleng@pinterest.com>

--- test/extensions/common/aws/eventstream/eventstream_parser_fuzz_test.cc
+++ test/extensions/common/aws/eventstream/eventstream_parser_fuzz_test.cc
@@ -10,9 +10,9 @@ using namespace Extensions::Common::Aws::Eventstream;
 DEFINE_FUZZER(const uint8_t* buf, size_t len) {
   const absl::string_view input(reinterpret_cast<const char*>(buf), len);
 
-  // Fuzz parseMessage with arbitrary input (single-pass API)
+  // Fuzz parseMessage with arbitrary input
   auto parse_result = EventstreamParser::parseMessage(input);
-  // Result may be error or incomplete - that's fine for fuzzing
+  // Result may be error or incomplete
 
   // If we got a valid message, verify bytes_consumed is reasonable
   if (parse_result.ok() && parse_result->message.has_value()) {
