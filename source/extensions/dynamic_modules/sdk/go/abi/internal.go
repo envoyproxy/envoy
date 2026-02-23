a27793460eaf614fee6eab726faf764fce1623d0
wbpcode/wangbaiping <wbphub@gmail.com>
2026-02-23 02:55:13 +0000

    dym/gosdk: ensure it works correctly in main platforms (#43368)
    Signed-off-by: wbpcode/wangbaiping <wbphub@gmail.com>

--- source/extensions/dynamic_modules/sdk/go/abi/internal.go
+++ source/extensions/dynamic_modules/sdk/go/abi/internal.go
@@ -2,6 +2,7 @@ package abi
 
 /*
 #cgo darwin LDFLAGS: -Wl,-undefined,dynamic_lookup
+#cgo linux LDFLAGS: -Wl,--unresolved-symbols=ignore-all
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
