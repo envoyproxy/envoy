15abb890c9548a901671580853f2cc8119e4e578
antoniovleonti <leonti@google.com>
2026-02-25 18:58:37 +0000

    add fuzzer for tracing validation code
    Signed-off-by: antoniovleonti <leonti@google.com>

---
+++ test/common/tracing/tracing_validation_fuzz_test.cc
@@ -0,0 +1,17 @@
+#include "source/common/tracing/tracing_validation.h"
+
+#include "absl/strings/string_view.h"
+#include "test/fuzz/fuzz_runner.h"
+
+namespace Envoy {
+namespace Fuzz {
+
+DEFINE_FUZZER(const uint8_t* buf, size_t len) {
+  absl::string_view input(reinterpret_cast<const char*>(buf), len);
+  Envoy::Tracing::isValidTraceParent(input);
+  Envoy::Tracing::isValidTraceState(input);
+  Envoy::Tracing::isValidBaggage(input);
+}
+
+} // namespace Fuzz
+} // namespace Envoy
