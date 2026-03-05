f413f859a6a7a5a673949bf20ea25a2f388d2078
Takeshi Yoneda <tyoneda@netflix.com>
2026-03-05 23:49:34 +0000

    Add metadata for HDS response
    Signed-off-by: Takeshi Yoneda <tyoneda@netflix.com>

--- source/common/upstream/upstream_impl.h
+++ source/common/upstream/upstream_impl.h
@@ -378,6 +378,9 @@ public:
   uint32_t healthFlagsGetAll() const override { return health_flags_; }
   void healthFlagsSetAll(uint32_t bits) override { health_flags_ |= bits; }
 
+  void setLastHealthCheckHttpStatus(uint32_t status) override { last_hc_http_status_ = status; }
+  uint32_t lastHealthCheckHttpStatus() const override { return last_hc_http_status_; }
+
   Host::HealthStatus healthStatus() const override {
     // Evaluate active health status first.
 
@@ -465,6 +468,7 @@ private:
   // flag access? May be we could refactor HealthFlag to contain all these statuses and flags in the
   // future.
   std::atomic<Host::HealthStatus> eds_health_status_{};
+  std::atomic<uint32_t> last_hc_http_status_{0};
 
   struct HostHandleImpl : HostHandle {
     HostHandleImpl(const std::shared_ptr<const HostImplBase>& parent) : parent_(parent) {
