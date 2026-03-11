726f0c690c80d4f47e2ac241a9c719a2e8a13190
Xuyang Tao <taoxuy@google.com>
2026-03-11 19:58:34 +0000

    fix
    Signed-off-by: Xuyang Tao <taoxuy@google.com>

--- source/extensions/stat_sinks/open_telemetry/request_splitter.cc
+++ source/extensions/stat_sinks/open_telemetry/request_splitter.cc
@@ -27,9 +27,9 @@ void RequestSplitter::chunkRequests(
   auto* sm = rm->mutable_scope_metrics(0);
 
   if (max_dp == 0) {
-    auto request = std::make_unique<MetricsExportRequest>();
-    request->mutable_resource_metrics()->Add()->Swap(rm);
-    send_callback(std::move(request));
+    auto current_request = std::make_unique<MetricsExportRequest>();
+    current_request->mutable_resource_metrics()->Add()->Swap(rm);
+    send_callback(std::move(current_request));
     return;
   }
 
@@ -42,37 +42,48 @@ void RequestSplitter::chunkRequests(
   base_sm->mutable_scope()->CopyFrom(sm->scope());
   base_sm->mutable_schema_url()->assign(sm->schema_url());
 
-  MetricsExportRequestPtr request = std::make_unique<MetricsExportRequest>(*base_request);
+  MetricsExportRequestPtr current_request = std::make_unique<MetricsExportRequest>(*base_request);
   uint32_t cur_dp = 0;
 
   for (auto& metric : *sm->mutable_metrics()) {
     opentelemetry::proto::metrics::v1::Metric* current_metric = nullptr;
 
-    auto append_dp =
-        [&](const std::function<void(opentelemetry::proto::metrics::v1::Metric*)>& add_dp_func) {
-          if (cur_dp == max_dp) {
-            send_callback(std::move(request));
-            request = std::make_unique<MetricsExportRequest>(*base_request);
-            cur_dp = 0;
-            current_metric = nullptr;
-          }
+    auto append_dp = [&send_callback, &base_request, &current_request, &cur_dp, &current_metric,
+                      max_dp, &metric](
+                         const std::function<void(opentelemetry::proto::metrics::v1::Metric*)>&
+                             add_dp_func) {
+      if (cur_dp == max_dp) {
+        send_callback(std::move(current_request));
+        current_request = std::make_unique<MetricsExportRequest>(*base_request);
+        cur_dp = 0;
+        current_metric = nullptr;
+      }
 
-          if (current_metric == nullptr) {
-            current_metric =
-                request->mutable_resource_metrics(0)->mutable_scope_metrics(0)->add_metrics();
-            *current_metric = metric;
-            if (current_metric->has_sum()) {
-              current_metric->mutable_sum()->clear_data_points();
-            } else if (current_metric->has_histogram()) {
-              current_metric->mutable_histogram()->clear_data_points();
-            } else if (current_metric->has_gauge()) {
-              current_metric->mutable_gauge()->clear_data_points();
-            }
-          }
+      if (current_metric == nullptr) {
+        current_metric =
+            current_request->mutable_resource_metrics(0)->mutable_scope_metrics(0)->add_metrics();
+        // Copy the metric metadata (name, description, unit, data type oneof) to the new chunk.
+        // The data points themselves will be cleared out below so that we can append only
+        // the remaining data points for this specific chunk.
+        *current_metric = metric;
+        switch (metric.data_case()) {
+        case opentelemetry::proto::metrics::v1::Metric::DataCase::kGauge:
+          current_metric->mutable_gauge()->clear_data_points();
+          break;
+        case opentelemetry::proto::metrics::v1::Metric::DataCase::kSum:
+          current_metric->mutable_sum()->clear_data_points();
+          break;
+        case opentelemetry::proto::metrics::v1::Metric::DataCase::kHistogram:
+          current_metric->mutable_histogram()->clear_data_points();
+          break;
+        default:
+          break;
+        }
+      }
 
-          add_dp_func(current_metric);
-          cur_dp++;
-        };
+      add_dp_func(current_metric);
+      cur_dp++;
+    };
 
     switch (metric.data_case()) {
     case opentelemetry::proto::metrics::v1::Metric::DataCase::kGauge:
@@ -100,7 +111,7 @@ void RequestSplitter::chunkRequests(
   }
 
   if (cur_dp > 0) {
-    send_callback(std::move(request));
+    send_callback(std::move(current_request));
   }
 }
 
