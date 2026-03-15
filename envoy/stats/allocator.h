20e15b28ddd164c9cb9fe52f5a94df3ee36f3292
Joshua Marantz <jmarantz@google.com>
2026-03-15 16:45:03 -0400

    restore allocator.h interface
    Signed-off-by: Joshua Marantz <jmarantz@google.com>

---
+++ envoy/stats/allocator.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// This header is a placeholder which we will carry until June 1, 2026,
+// as we have deprecated the pure interface and impl pattern.
+//
+// Please remoe references to this file and instead include
+// source/common/stats/allocator.h.
+
+namespace Envoy {
+namespace Stats {
+
+class Allocator;
+
+} // namespace Stats
+} // namespace Envoy
