5318c38d9f67c441b2dc66d896fa64be15fc694e
Retr0-XD <88275564+Retr0-XD@users.noreply.github.com>
2026-03-18 18:56:05 +0530

    Add support for substitution formatters in all HttpService uses

--- source/extensions/stat_sinks/open_telemetry/open_telemetry_http_impl.h
+++ source/extensions/stat_sinks/open_telemetry/open_telemetry_http_impl.h
@@ -5,6 +5,7 @@
 
 #include "source/common/http/async_client_impl.h"
 #include "source/common/http/async_client_utility.h"
+#include "source/common/http/http_service_headers.h"
 #include "source/extensions/stat_sinks/open_telemetry/open_telemetry_impl.h"
 
 namespace Envoy {
@@ -21,7 +22,8 @@ class OpenTelemetryHttpMetricsExporter : public OtlpMetricsExporter,
                                          public Logger::Loggable<Logger::Id::stats> {
 public:
   OpenTelemetryHttpMetricsExporter(Upstream::ClusterManager& cluster_manager,
-                                   const envoy::config::core::v3::HttpService& http_service);
+                                   const envoy::config::core::v3::HttpService& http_service,
+                                   Server::Configuration::ServerFactoryContext& server_context);
 
   // OtlpMetricsExporter
   void send(MetricsExportRequestPtr&& metrics) override;
@@ -36,7 +38,7 @@ private:
   envoy::config::core::v3::HttpService http_service_;
   // Track active HTTP requests to cancel them on destruction.
   Http::AsyncClientRequestTracker active_requests_;
-  std::vector<std::pair<const Http::LowerCaseString, const std::string>> parsed_headers_to_add_;
+  std::unique_ptr<Http::HttpServiceHeadersApplicator> headers_applicator_;
 };
 
 } // namespace OpenTelemetry
