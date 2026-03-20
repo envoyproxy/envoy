dad4b6593481e179b26c7c7cd074c01bc14b24d3
antoniovleonti <leonti@google.com>
2026-03-20 15:17:53 +0000

    decouple SubscriptionBase and SubscriptionCallbacks
    Signed-off-by: antoniovleonti <leonti@google.com>

--- source/common/config/resource_type_helper.h
+++ source/common/config/resource_type_helper.h
@@ -8,17 +8,19 @@
 namespace Envoy {
 namespace Config {
 
-template <typename Current> struct SubscriptionBase : public Config::SubscriptionCallbacks {
-public:
-  SubscriptionBase(ProtobufMessage::ValidationVisitor& validation_visitor,
-                   absl::string_view name_field)
+/**
+ * Helper for resource type decoding and name identification.
+ * This class is intended to be used via composition in xDS API implementations.
+ */
+template <typename Current> struct ResourceTypeHelper {
+  ResourceTypeHelper(ProtobufMessage::ValidationVisitor& validation_visitor,
+                     absl::string_view name_field)
       : resource_decoder_(std::make_shared<Config::OpaqueResourceDecoderImpl<Current>>(
             validation_visitor, name_field)) {}
 
   std::string getResourceName() const { return Envoy::Config::getResourceName<Current>(); }
 
-protected:
-  OpaqueResourceDecoderSharedPtr resource_decoder_;
+  const OpaqueResourceDecoderSharedPtr resource_decoder_;
 };
 
 } // namespace Config
---
+++ test/common/config/resource_type_helper_test.cc
@@ -0,0 +1,22 @@
+#include "source/common/config/resource_type_helper.h"
+#include "source/common/protobuf/message_validator_impl.h"
+
+#include "api/envoy/config/cluster/v3/cluster.pb.h"
+#include "api/envoy/config/cluster/v3/cluster.pb.validate.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace Envoy {
+namespace Config {
+namespace {
+
+TEST(ResourceTypeHelperTest, GetResourceName) {
+  ProtobufMessage::NullValidationVisitorImpl validation_visitor;
+  ResourceTypeHelper<envoy::config::cluster::v3::Cluster> helper(validation_visitor, "name");
+  EXPECT_EQ("envoy.config.cluster.v3.Cluster", helper.getResourceName());
+  EXPECT_NE(nullptr, helper.resource_decoder_);
+}
+
+} // namespace
+} // namespace Config
+} // namespace Envoy
