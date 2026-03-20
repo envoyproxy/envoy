fcf9a9704cb1e2f5c26a834026db5be59dd51322
antoniovleonti <leonti@google.com>
2026-03-20 16:55:39 +0000

    fix test
    Signed-off-by: antoniovleonti <leonti@google.com>

--- test/common/config/resource_type_helper_test.cc
+++ test/common/config/resource_type_helper_test.cc
@@ -1,8 +1,9 @@
+#include "envoy/config/cluster/v3/cluster.pb.h"
+#include "envoy/config/cluster/v3/cluster.pb.validate.h"
+
 #include "source/common/config/resource_type_helper.h"
 #include "source/common/protobuf/message_validator_impl.h"
 
-#include "api/envoy/config/cluster/v3/cluster.pb.h"
-#include "api/envoy/config/cluster/v3/cluster.pb.validate.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
