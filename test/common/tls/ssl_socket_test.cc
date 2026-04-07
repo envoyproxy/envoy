85041914fa44166e6988fb7222cd593249a507bd
Xuyang Tao <taoxuy@google.com>
2026-04-07 02:24:54 +0000

    add changelog
    Signed-off-by: Xuyang Tao <taoxuy@google.com>

--- test/common/tls/ssl_socket_test.cc
+++ test/common/tls/ssl_socket_test.cc
@@ -4195,7 +4195,6 @@ TEST_P(SslSocketTest, ShutdownWithoutCloseNotify) {
   dispatcher_->run(Event::Dispatcher::RunType::Block);
 }
 
-
 TEST_P(SslSocketTest, ClientAuthMultipleCAs) {
   const std::string server_ctx_yaml = R"EOF(
   common_tls_context:
@@ -6779,7 +6778,6 @@ TEST_P(SslSocketTest, TestTransportSocketCallback) {
   ssl_socket->setTransportSocketCallbacks(callbacks);
   EXPECT_EQ(ssl_socket->transportSocketCallbacks(), &callbacks);
 }
-
 
 class SslReadBufferLimitTest : public SslSocketTest {
 protected:
