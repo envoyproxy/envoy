4a1c38f1553b4cfc4a79712797529f0fe7a0a659
k1chik <kkukdia@gmail.com>
2026-05-01 22:02:12 -0400

    test: update file_sink_impl_test for http_status_code field
    Signed-off-by: k1chik <kkukdia@gmail.com>

--- test/extensions/health_check/event_sinks/file/file_sink_impl_test.cc
+++ test/extensions/health_check/event_sinks/file/file_sink_impl_test.cc
@@ -64,6 +64,7 @@ TEST(HealthCheckEventFileSink, logTest) {
   cluster_name: fake_cluster
   eject_unhealthy_event:
     failure_type: ACTIVE
+    http_status_code: 503
   timestamp: '2009-02-13T23:31:31.234Z'
   )EOF",
                             event);
@@ -74,7 +75,7 @@ TEST(HealthCheckEventFileSink, logTest) {
       "{\"health_checker_type\":\"HTTP\",\"host\":{\"socket_address\":{"
       "\"protocol\":\"TCP\",\"address\":\"10.0.0.1\",\"port_value\":443,\"resolver_name\":\"\","
       "\"ipv4_compat\":false,\"network_namespace_filepath\":\"\"}},\"cluster_name\":\"fake_"
-      "cluster\",\"eject_unhealthy_event\":{\"failure_type\":\"ACTIVE\"},"
+      "cluster\",\"eject_unhealthy_event\":{\"failure_type\":\"ACTIVE\",\"http_status_code\":503},"
       "\"timestamp\":\"2009-02-13T23:31:31.234Z\"}\n");
 
   envoy::data::core::v3::HealthCheckEvent add_event;
