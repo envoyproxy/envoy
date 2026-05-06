1dd885dc6c3755a5944e5a4d6b7f2302fb0044a3
Todd Greer <toddmgreer@gmail.com>
2026-05-06 23:20:00 +0000

    ext_authz: refactor cache integration tests based on reviews
    Refactored ext_authz_cache_integration_test.cc incorporating your reviews: switched downstream to HTTP2, renamed initializeConfig() to initialize() override, removed redundant comments, and renamed the metadata key to cached_authz_response. Kept createUpstreams() to allocate secondary dynamic ports.
    
    Signed-off-by: Todd Greer <toddmgreer@gmail.com>

--- test/extensions/filters/http/ext_authz/ext_authz_cache_integration_test.cc
+++ test/extensions/filters/http/ext_authz/ext_authz_cache_integration_test.cc
@@ -21,30 +21,28 @@ class ExtAuthzCacheIntegrationTest : public HttpIntegrationTest,
                                      public testing::Test {
 public:
   ExtAuthzCacheIntegrationTest()
-      : HttpIntegrationTest(Http::CodecType::HTTP1, Network::Address::IpVersion::v4) {}
+      : HttpIntegrationTest(Http::CodecType::HTTP2, Network::Address::IpVersion::v4) {}
 
   void createUpstreams() override {
     HttpIntegrationTest::createUpstreams();
-    // Create fake upstream for ext_authz gRPC service
     addFakeUpstream(Http::CodecType::HTTP2);
   }
 
-  void initializeConfig() {
+  void initialize() override {
     config_helper_.addConfigModifier([this](envoy::config::bootstrap::v3::Bootstrap& bootstrap) {
-      // Add ext_authz cluster
       auto* ext_authz_cluster = bootstrap.mutable_static_resources()->add_clusters();
       ext_authz_cluster->MergeFrom(bootstrap.static_resources().clusters()[0]);
       ext_authz_cluster->set_name("ext_authz_cluster");
       ConfigHelper::setHttp2(*ext_authz_cluster);
 
-      // 1. Setup header_to_metadata filter config (Cache Simulator)
+      // 1. Set up header_to_metadata filter config (Fake Cache)
       envoy::extensions::filters::http::header_to_metadata::v3::Config h2m_proto;
       const std::string h2m_yaml = R"YAML(
         request_rules:
         - header: x-simulate-cache
           on_header_present:
             metadata_namespace: envoy.filters.http.ext_authz
-            key: authz_cache
+            key: cached_authz_response
             type: STRING
           remove: true
       )YAML";
@@ -54,10 +52,10 @@ public:
       header_to_metadata_filter.set_name("envoy.filters.http.header_to_metadata");
       header_to_metadata_filter.mutable_typed_config()->PackFrom(h2m_proto);
 
-      // 2. Setup ext_authz filter config
+      // 2. Set up ext_authz filter config
       envoy::extensions::filters::http::ext_authz::v3::ExtAuthz ext_authz_proto;
       ext_authz_proto.mutable_grpc_service()->mutable_envoy_grpc()->set_cluster_name("ext_authz_cluster");
-      ext_authz_proto.set_check_response_metadata_key("authz_cache");
+      ext_authz_proto.set_check_response_metadata_key("cached_authz_response");
       ext_authz_proto.set_transport_api_version(envoy::config::core::v3::ApiVersion::V3);
 
       envoy::extensions::filters::network::http_connection_manager::v3::HttpFilter ext_authz_filter;
@@ -68,6 +66,8 @@ public:
       config_helper_.prependFilter(MessageUtil::getJsonStringFromMessageOrError(ext_authz_filter));
       config_helper_.prependFilter(MessageUtil::getJsonStringFromMessageOrError(header_to_metadata_filter));
     });
+
+    HttpIntegrationTest::initialize();
   }
 
   std::string serializeAndEncode(const envoy::service::auth::v3::CheckResponse& response) {
@@ -78,8 +78,7 @@ public:
 };
 
 TEST_F(ExtAuthzCacheIntegrationTest, CacheHitOKBypassesRPC) {
-  initializeConfig();
-  HttpIntegrationTest::initialize();
+  initialize();
 
   // 1. Prepare cached OK response with header mutations
   envoy::service::auth::v3::CheckResponse cached_response;
@@ -125,8 +124,7 @@ TEST_F(ExtAuthzCacheIntegrationTest, CacheHitOKBypassesRPC) {
 }
 
 TEST_F(ExtAuthzCacheIntegrationTest, CacheHitDeniedBypassesRPC) {
-  initializeConfig();
-  HttpIntegrationTest::initialize();
+  initialize();
 
   // 1. Prepare cached Denied response (403 Forbidden)
   envoy::service::auth::v3::CheckResponse cached_response;
