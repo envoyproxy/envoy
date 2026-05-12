396621e3b8ce2bfc506b819f71a5efb889508879
Derek Argueta <derek.argueta@airbnb.com>
2026-05-11 20:29:27 -0500

    wasm: fix clang-format violations in vm_id_with_env change
    Signed-off-by: Derek Argueta <derek.argueta@airbnb.com>

--- source/extensions/common/wasm/wasm.cc
+++ source/extensions/common/wasm/wasm.cc
@@ -377,12 +377,10 @@ bool createWasm(const PluginSharedPtr& plugin, const Stats::ScopeSharedPtr& scop
   // recreation, the same way a code change does. The env vars hash is appended to vm_id (which is
   // small) rather than to code (which can be O(MB)), with a separator to avoid key collisions.
   const std::size_t env_vars_hash = MessageUtil::hash(vm_config.environment_variables());
-  const std::string vm_id_with_env =
-      absl::StrCat(vm_config.vm_id(), "|", env_vars_hash);
+  const std::string vm_id_with_env = absl::StrCat(vm_config.vm_id(), "|", env_vars_hash);
   auto vm_key = proxy_wasm::makeVmKey(
       vm_id_with_env,
-      THROW_OR_RETURN_VALUE(MessageUtil::anyToBytes(vm_config.configuration()), std::string),
-      code);
+      THROW_OR_RETURN_VALUE(MessageUtil::anyToBytes(vm_config.configuration()), std::string), code);
   auto complete_cb = [cb, vm_key, plugin, scope, &api, &cluster_manager, &dispatcher,
                       &lifecycle_notifier, create_root_context_for_testing,
                       &stats_handler](std::string code) -> bool {
