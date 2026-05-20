f7ac6c437feb76d03ed2fbcbf5020928ee6553e3
wbpcode/wangbaiping <wbphub@gmail.com>
2026-05-20 03:05:14 +0000

    ip tagging: refactor ip tagging to make it lock free
    Signed-off-by: wbpcode/wangbaiping <wbphub@gmail.com>

--- test/extensions/filters/http/ip_tagging/ip_tagging_filter_test.cc
+++ test/extensions/filters/http/ip_tagging/ip_tagging_filter_test.cc
@@ -39,8 +39,8 @@ public:
   }
 
   static void resetTrie(IpTaggingFilterConfig& filter_config) {
-    if (filter_config.trie_) {
-      filter_config.trie_.reset();
+    if (filter_config.static_ip_tags_) {
+      filter_config.static_ip_tags_->trie.reset();
     }
   }
 };
@@ -474,7 +474,7 @@ TEST_F(IpTaggingFilterTest, ReusesIpTagsProviderInstanceForSameFilePath) {
   auto provider2 = IpTaggingFilterConfigPeer::ipTagsProvider(*config2);
   EXPECT_NE(nullptr, provider1);
   EXPECT_NE(nullptr, provider2);
-  EXPECT_EQ(provider1->ipTags(), provider2->ipTags());
+  EXPECT_EQ(provider1->loadedIpTags()->trie, provider2->loadedIpTags()->trie);
 }
 
 TEST_F(IpTaggingFilterTest, DifferentIpTagsProviderInstanceForDifferentFilePath) {
@@ -501,7 +501,7 @@ TEST_F(IpTaggingFilterTest, DifferentIpTagsProviderInstanceForDifferentFilePath)
   auto provider2 = IpTaggingFilterConfigPeer::ipTagsProvider(*config2);
   EXPECT_NE(nullptr, provider1);
   EXPECT_NE(nullptr, provider2);
-  EXPECT_NE(provider1->ipTags(), provider2->ipTags());
+  EXPECT_NE(provider1->loadedIpTags()->trie, provider2->loadedIpTags()->trie);
   ::testing::Mock::VerifyAndClearExpectations(&filter_callbacks_);
 }
 
