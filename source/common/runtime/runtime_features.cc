671bb592d4542a65e2872ec587dcaa11d3c2280e
code <wbphub@gmail.com>
2026-05-22 00:06:14 +0800

    runtime: enable new dns cluster implementation (#45048)
    Commit Message: runtime: enable new dns cluster implementation
    Additional Description:
    
    We have released multiple versions after the new DNS clusters
    implementation. Now it's time to enable it by default.
    
    Risk Level:
    Testing:
    Docs Changes:
    Release Notes:
    Platform Specific Features:
    [Optional Runtime guard:]
    [Optional Fixes #Issue]
    [Optional Fixes commit #PR or SHA]
    [Optional Deprecated:]
    [Optional [API
    Considerations](https://github.com/envoyproxy/envoy/blob/main/api/review_checklist.md):]
    
    ---------
    
    Signed-off-by: wbpcode/wangbaiping <wbphub@gmail.com>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -44,6 +44,7 @@ RUNTIME_GUARD(envoy_reloadable_features_disallow_quic_client_udp_mmsg);
 RUNTIME_GUARD(envoy_reloadable_features_enable_cel_regex_precompilation);
 RUNTIME_GUARD(envoy_reloadable_features_enable_cel_response_path_matching);
 RUNTIME_GUARD(envoy_reloadable_features_enable_compression_bomb_protection);
+RUNTIME_GUARD(envoy_reloadable_features_enable_new_dns_implementation);
 // TODO(adisuissa): enable once the LRS server-self-ads support is fully
 // implemented, and tested.
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_enable_lrs_server_self_ads);
@@ -207,12 +208,6 @@ FALSE_RUNTIME_GUARD(envoy_reloadable_features_getaddrinfo_no_ai_flags);
 // Flip to true after two release periods.
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_remove_legacy_route_formatter);
 
-// TODO(grnmeira):
-// Enables the new DNS implementation, a merged implementation of
-// strict and logical DNS clusters. This new implementation will
-// take over the split ones, and will be used as a base for the
-// implementation of on-demand DNS.
-FALSE_RUNTIME_GUARD(envoy_reloadable_features_enable_new_dns_implementation);
 // Force a local reply from upstream envoy for reverse connections.
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_reverse_conn_force_local_reply);
 // RELEASE_ASSERT when upstream stream detects UAF of downstream response decoder instance.
