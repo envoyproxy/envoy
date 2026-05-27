7d4e98843b938c429c3b066abbe97741e5c3486c
Yanjun Xiang <yanjunxiang@google.com>
2026-05-27 20:36:58 +0000

    Revert "ext_authz: respect keep_empty_value in HeaderValueOption (#45103)"
    This reverts commit c3ab9e5a3d3ab77a3eddec72d6ff8c54ad237ad3.
    
    Signed-off-by: Yanjun Xiang <yanjunxiang@google.com>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -49,7 +49,6 @@ RUNTIME_GUARD(envoy_reloadable_features_enable_new_dns_implementation);
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_enable_lrs_server_self_ads);
 RUNTIME_GUARD(envoy_reloadable_features_enable_new_query_param_present_match_behavior);
 RUNTIME_GUARD(envoy_reloadable_features_ext_authz_http_client_retries_respect_user_retry_on);
-RUNTIME_GUARD(envoy_reloadable_features_ext_authz_respect_keep_empty_value);
 // Ignore the automated "remove this flag" issue: we should keep this for 1 year. Confirm with
 // @yanjunxiang-google before removing.
 RUNTIME_GUARD(envoy_reloadable_features_ext_proc_fail_close_spurious_resp);
