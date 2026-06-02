3d81dea2de7ff659cb28afd551faeabbfb44baf6
Ethan Truong <ethantruong@google.com>
2026-06-02 14:23:57 +0000

    Remove non-ENVOY_BUG lcov exclusion
    Signed-off-by: Ethan Truong <ethantruong@google.com>

--- source/extensions/filters/http/ext_authz/ext_authz.cc
+++ source/extensions/filters/http/ext_authz/ext_authz.cc
@@ -38,8 +38,8 @@ void fillMetadataContext(const std::vector<const MetadataProto*>& source_metadat
   for (const auto& context_key : metadata_context_namespaces) {
     for (const MetadataProto* metadata : source_metadata) {
       if (metadata == nullptr) {
-        continue; // LCOV_EXCL_LINE
-      } // LCOV_EXCL_LINE
+        continue;
+      }
       const auto& filter_metadata = metadata->filter_metadata();
       if (const auto metadata_it = filter_metadata.find(context_key);
           metadata_it != filter_metadata.end()) {
@@ -52,8 +52,8 @@ void fillMetadataContext(const std::vector<const MetadataProto*>& source_metadat
   for (const auto& context_key : typed_metadata_context_namespaces) {
     for (const MetadataProto* metadata : source_metadata) {
       if (metadata == nullptr) {
-        continue; // LCOV_EXCL_LINE
-      } // LCOV_EXCL_LINE
+        continue;
+      }
       const auto& typed_filter_metadata = metadata->typed_filter_metadata();
       if (const auto metadata_it = typed_filter_metadata.find(context_key);
           metadata_it != typed_filter_metadata.end()) {
