3d250313aabedf4fbaeb24c74d034b2a5577c3ef
Ethan Truong <ethantruong@google.com>
2026-06-04 19:56:33 +0000

    Remove tests that covered lines already covered by integration tests
    Signed-off-by: Ethan Truong <ethantruong@google.com>

--- test/extensions/filters/http/ext_proc/matching_utils_test.cc
+++ test/extensions/filters/http/ext_proc/matching_utils_test.cc
@@ -59,30 +59,6 @@ TEST_F(ExpressionManagerTest, RepeatedMatchers) {
   EXPECT_TRUE(test_manager.hasRequestExpr());
 }
 
-TEST_F(ExpressionManagerTest, EvaluateAttributesTypes) {
-  Protobuf::RepeatedPtrField<std::string> matchers;
-  matchers.Add("true");
-  matchers.Add("1");
-  matchers.Add("2u");
-  matchers.Add("'foo'");
-  matchers.Add("b'bar'");
-
-  auto builder = Filters::Common::Expr::getBuilder(context_);
-  ExpressionManager test_manager(builder, context_.local_info_, matchers, {});
-
-  NiceMock<StreamInfo::MockStreamInfo> stream_info;
-  auto activation = Filters::Common::Expr::createActivation(&context_.local_info_, stream_info,
-                                                            nullptr, nullptr, nullptr);
-
-  auto result = test_manager.evaluateRequestAttributes(*activation);
-
-  EXPECT_EQ(result.fields().at("true").bool_value(), true);
-  EXPECT_EQ(result.fields().at("1").number_value(), 1);
-  EXPECT_EQ(result.fields().at("2u").number_value(), 2);
-  EXPECT_EQ(result.fields().at("'foo'").string_value(), "foo");
-  EXPECT_EQ(result.fields().at("b'bar'").string_value(), "bar");
-}
-
 TEST_F(ExpressionManagerTest, EvaluateAttributesEmpty) {
   NiceMock<StreamInfo::MockStreamInfo> stream_info;
   auto activation = Filters::Common::Expr::createActivation(&context_.local_info_, stream_info,
--- test/extensions/filters/http/ext_proc/save_processing_response_test.cc
+++ test/extensions/filters/http/ext_proc/save_processing_response_test.cc
@@ -13,50 +13,7 @@ namespace {
 using envoy::service::ext_proc::v3::ProcessingResponse;
 using envoy::service::ext_proc::v3::StreamedImmediateResponse;
 
-TEST(SaveProcessingResponseTest, SaveStreamingImmediateResponse) {
-  envoy::extensions::http::ext_proc::response_processors::save_processing_response::v3::
-      SaveProcessingResponse config;
-  config.mutable_save_immediate_response()->set_save_response(true);
 
-  SaveProcessingResponse processor(config);
-
-  StreamedImmediateResponse response;
-  response.mutable_headers_response()->set_end_of_stream(true);
-
-  NiceMock<Envoy::StreamInfo::MockStreamInfo> stream_info;
-
-  processor.afterProcessingStreamingImmediateResponse(response, absl::OkStatus(), stream_info);
-
-  auto filter_state = stream_info.filterState()->getDataReadOnly<SaveProcessingResponseFilterState>(
-      SaveProcessingResponseFilterState::kFilterStateName);
-  ASSERT_NE(filter_state, nullptr);
-  EXPECT_EQ(filter_state->response->processing_response.response_case(),
-            ProcessingResponse::ResponseCase::kStreamedImmediateResponse);
-  EXPECT_TRUE(TestUtility::protoEqual(
-      filter_state->response->processing_response.streamed_immediate_response(), response));
-}
-
-TEST(SaveProcessingResponseTest, SaveStreamingImmediateResponseOnError) {
-  envoy::extensions::http::ext_proc::response_processors::save_processing_response::v3::
-      SaveProcessingResponse config;
-  config.mutable_save_immediate_response()->set_save_response(true);
-  config.mutable_save_immediate_response()->set_save_on_error(true);
-
-  SaveProcessingResponse processor(config);
-
-  StreamedImmediateResponse response;
-  response.mutable_headers_response()->set_end_of_stream(true);
-
-  NiceMock<Envoy::StreamInfo::MockStreamInfo> stream_info;
-
-  auto error_status = absl::InternalError("test error");
-  processor.afterProcessingStreamingImmediateResponse(response, error_status, stream_info);
-
-  auto filter_state = stream_info.filterState()->getDataReadOnly<SaveProcessingResponseFilterState>(
-      SaveProcessingResponseFilterState::kFilterStateName);
-  ASSERT_NE(filter_state, nullptr);
-  EXPECT_EQ(filter_state->response->processing_status, error_status);
-}
 
 TEST(SaveProcessingResponseTest, DontSaveStreamingImmediateResponseOnError) {
   envoy::extensions::http::ext_proc::response_processors::save_processing_response::v3::
