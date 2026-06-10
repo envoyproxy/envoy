2f8cb75d3b44b67546a60cdeb2920f7e7cddd670
yanavlasov <yavlasov@google.com>
2026-06-09 20:54:04 -0400

    http2: add header map stats histograms (#45479)
    Add histograms for HTTP/2 header map stats.
    
    * header_count - total count of header entries received (including
    individual ``cookie`` headers)
    * header_list_size - total byte size of header map entries, total length
    of the re-assembled ``cookie``
    * cookie_count - count of individual ``cookie`` headers.
    * cookie_size - size of re-assembled ``cookie`` headers.
    
    Histograms are disabled by default and can be enabled by
    setting the runtime guard
    ``envoy.reloadable_features.http2_record_histograms`` to ``true``.
    
    Risk Level: low
    Testing: unit tests
    Docs Changes: yes
    Release Notes: yes
    Platform Specific Features: no
    Runtime guard: envoy.reloadable_features.http2_record_histograms
    
    ---------
    
    Signed-off-by: Yan Avlasov <yavlasov@google.com>

--- source/common/http/http2/codec_stats.h
+++ source/common/http/http2/codec_stats.h
@@ -13,7 +13,7 @@ namespace Http2 {
 /**
  * All stats for the HTTP/2 codec. @see stats_macros.h
  */
-#define ALL_HTTP2_CODEC_STATS(COUNTER, GAUGE)                                                      \
+#define ALL_HTTP2_CODEC_STATS(COUNTER, GAUGE, HISTOGRAM)                                           \
   COUNTER(cookies_total_bytes_too_large)                                                           \
   COUNTER(dropped_headers_with_underscores)                                                        \
   COUNTER(goaway_sent)                                                                             \
@@ -38,7 +38,12 @@ namespace Http2 {
   GAUGE(pending_send_bytes, Accumulate)                                                            \
   GAUGE(deferred_stream_close, Accumulate)                                                         \
   GAUGE(outbound_frames_active, Accumulate)                                                        \
-  GAUGE(outbound_control_frames_active, Accumulate)
+  GAUGE(outbound_control_frames_active, Accumulate)                                                \
+  HISTOGRAM(cookie_count, Unspecified)                                                             \
+  HISTOGRAM(cookie_size, Bytes)                                                                    \
+  HISTOGRAM(header_count, Unspecified)                                                             \
+  HISTOGRAM(header_list_size, Bytes)
+#define GENERATE_CONSTRUCTOR_HISTOGRAM_PARAM(NAME, ...) Envoy::Stats::Histogram &NAME,
 /**
  * Wrapper struct for the HTTP/2 codec stats. @see stats_macros.h
  */
@@ -46,14 +51,17 @@ struct CodecStats : public ::Envoy::Http::HeaderValidatorStats {
   using AtomicPtr = Thread::AtomicPtr<CodecStats, Thread::AtomicPtrAllocMode::DeleteOnDestruct>;
 
   CodecStats(ALL_HTTP2_CODEC_STATS(GENERATE_CONSTRUCTOR_COUNTER_PARAM,
-                                   GENERATE_CONSTRUCTOR_GAUGE_PARAM)...)
+                                   GENERATE_CONSTRUCTOR_GAUGE_PARAM,
+                                   GENERATE_CONSTRUCTOR_HISTOGRAM_PARAM)...)
       : ::Envoy::Http::HeaderValidatorStats()
-            ALL_HTTP2_CODEC_STATS(GENERATE_CONSTRUCTOR_INIT_LIST, GENERATE_CONSTRUCTOR_INIT_LIST) {}
+            ALL_HTTP2_CODEC_STATS(GENERATE_CONSTRUCTOR_INIT_LIST, GENERATE_CONSTRUCTOR_INIT_LIST,
+                                  GENERATE_CONSTRUCTOR_INIT_LIST) {}
 
   static CodecStats& atomicGet(AtomicPtr& ptr, Stats::Scope& scope) {
     return *ptr.get([&scope]() -> CodecStats* {
       return new CodecStats{ALL_HTTP2_CODEC_STATS(POOL_COUNTER_PREFIX(scope, "http2."),
-                                                  POOL_GAUGE_PREFIX(scope, "http2."))};
+                                                  POOL_GAUGE_PREFIX(scope, "http2."),
+                                                  POOL_HISTOGRAM_PREFIX(scope, "http2."))};
     });
   }
 
@@ -63,7 +71,7 @@ struct CodecStats : public ::Envoy::Http::HeaderValidatorStats {
   }
   void incMessagingError() override { rx_messaging_error_.inc(); }
 
-  ALL_HTTP2_CODEC_STATS(GENERATE_COUNTER_STRUCT, GENERATE_GAUGE_STRUCT)
+  ALL_HTTP2_CODEC_STATS(GENERATE_COUNTER_STRUCT, GENERATE_GAUGE_STRUCT, GENERATE_HISTOGRAM_STRUCT)
 };
 
 } // namespace Http2
