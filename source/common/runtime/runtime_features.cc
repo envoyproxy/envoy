2f8141ce4412c8104583bd605232dfc6f0a34609
yanavlasov <yavlasov@google.com>
2026-06-09 20:54:04 -0400

    http2: add header map stats histograms (#45479)
    Add histograms for HTTP/2 header map stats.
    
    * header_count - total count of header entries received (including
    individual ``cookie`` headers)
    * header_list_size - total byte size of header map entries, total length
    of the re-assembled ``cookie``
    * cookie_count - count of individual ``cookie`` headers.
    * cookie_size - size of re-assembled ``cookie`` headers.
    
    Histograms are disabled by default and can be enabled by
    setting the runtime guard
    ``envoy.reloadable_features.http2_record_histograms`` to ``true``.
    
    Risk Level: low
    Testing: unit tests
    Docs Changes: yes
    Release Notes: yes
    Platform Specific Features: no
    Runtime guard: envoy.reloadable_features.http2_record_histograms
    
    ---------
    
    Signed-off-by: Yan Avlasov <yavlasov@google.com>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -183,6 +183,9 @@ FALSE_RUNTIME_GUARD(envoy_reloadable_features_getaddrinfo_no_ai_flags);
 // Replace with a config option (default off) post CVE release.
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_reject_early_connect_data);
 
+// Enable histograms of HTTP/2 header sizes, including cookie size.
+FALSE_RUNTIME_GUARD(envoy_reloadable_features_http2_record_histograms);
+
 // Block of non-boolean flags. Use of int flags is deprecated. Do not add more.
 ABSL_FLAG(uint64_t, re2_max_program_size_error_level, 100, ""); // NOLINT
 ABSL_FLAG(uint64_t, re2_max_program_size_warn_level,            // NOLINT
