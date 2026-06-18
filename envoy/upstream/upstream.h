e3720db253486623696c60d6d747e9deaba0ec24
Prasad I V <prasad.iv@databricks.com>
2026-06-18 19:39:29 +0000

    upstream: document codec factory lifetime contract
    Note that upstreamHttpClientCodecFactory() must return a factory owned by the
    options object, since ClusterInfoImpl pins it via a shared_ptr aliasing the
    options object.
    
    Signed-off-by: Prasad I V <prasad.iv@databricks.com>

--- envoy/upstream/upstream.h
+++ envoy/upstream/upstream.h
@@ -922,6 +922,9 @@ public:
    * @return an optional upstream (client) HTTP codec factory provided by this options object.
    *         Defaults to none. An options extension attached via typed_extension_protocol_options
    *         can override this to make CodecClientProd build a custom or decorated client codec.
+   *         The returned factory's lifetime must be owned by this options object (e.g. by being
+   *         the object itself or a member of it): ClusterInfoImpl pins it with a shared_ptr
+   *         aliasing the options object, so a factory owned elsewhere would dangle.
    */
   virtual OptRef<const Http::ClientCodecFactory> upstreamHttpClientCodecFactory() const {
     return {};
