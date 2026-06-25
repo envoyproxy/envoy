71cb14ed4542e3e00fd9bed7a0c983fde7ec24a1
Filip Čacký <filip.cacky@cdn77.com>
2026-06-25 11:48:41 +0200

    test/geoip: Cleanup mocks and extension tests (#45128)
    Commit Message: test/geoip: Cleanup mocks and extension tests
    Additional Description:
    
    - Move geoip mocks from `test/extensions/filters/http/geoip/` to
    `test/mocks/geoip`.
    - Guard network geoip tests with `envoy_extension_cc_test`.
    - List used extension_names in integration tests.
    
    Risk Level: Low
    Testing: Fixed tests for macOS, passes on Linux
    Docs Changes: N/A
    Release Notes: N/A
    Platform Specific Features: N/A
    
    ---------
    
    Signed-off-by: Filip Cacky <filip.cacky@cdn77.com>

--- test/extensions/filters/network/geoip/config_test.cc
+++ test/extensions/filters/network/geoip/config_test.cc
@@ -3,7 +3,7 @@
 
 #include "source/extensions/filters/network/geoip/config.h"
 
-#include "test/extensions/filters/http/geoip/mocks.h"
+#include "test/mocks/geoip/mocks.h"
 #include "test/mocks/server/factory_context.h"
 #include "test/test_common/registry.h"
 #include "test/test_common/utility.h"
@@ -11,9 +11,6 @@
 #include "absl/types/optional.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-
-// Import the shared geoip mocks from the HTTP filter tests.
-using Envoy::Extensions::HttpFilters::Geoip::DummyGeoipProviderFactory;
 
 namespace Envoy {
 namespace Extensions {
@@ -25,7 +22,7 @@ class GeoipConfigTest : public testing::Test {
 public:
   void initializeProviderFactory() { registration_.emplace(dummy_factory_); }
 
-  DummyGeoipProviderFactory dummy_factory_;
+  Geolocation::DummyGeoipProviderFactory dummy_factory_;
   NiceMock<Server::Configuration::MockFactoryContext> context_;
   absl::optional<Registry::InjectFactory<Geolocation::GeoipProviderFactory>> registration_;
 };
@@ -36,7 +33,7 @@ TEST_F(GeoipConfigTest, CreateFilterFactory) {
     provider:
         name: "envoy.geoip_providers.dummy"
         typed_config:
-          "@type": type.googleapis.com/test.extensions.filters.http.geoip.DummyProvider
+          "@type": type.googleapis.com/test.mocks.geoip.DummyProvider
 )EOF";
 
   envoy::extensions::filters::network::geoip::v3::Geoip proto_config;
@@ -66,7 +63,7 @@ TEST_F(GeoipConfigTest, FilterIsNotTerminal) {
     provider:
         name: "envoy.geoip_providers.dummy"
         typed_config:
-          "@type": type.googleapis.com/test.extensions.filters.http.geoip.DummyProvider
+          "@type": type.googleapis.com/test.mocks.geoip.DummyProvider
 )EOF";
 
   GeoipFilterFactory factory;
@@ -82,7 +79,7 @@ TEST_F(GeoipConfigTest, CreateFilterFactoryWithClientIp) {
     provider:
         name: "envoy.geoip_providers.dummy"
         typed_config:
-          "@type": type.googleapis.com/test.extensions.filters.http.geoip.DummyProvider
+          "@type": type.googleapis.com/test.mocks.geoip.DummyProvider
     client_ip: "192.168.1.100"
 )EOF";
 
