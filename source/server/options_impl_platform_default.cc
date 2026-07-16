61d4b92ca93c3c9d010918531b4af862e1d578f6
jukie <10012479+jukie@users.noreply.github.com>
2026-07-16 08:40:39 -0600

    set hardware_concurrency floor to 1 in default getCpuCount
    Signed-off-by: jukie <10012479+jukie@users.noreply.github.com>

--- source/server/options_impl_platform_default.cc
+++ source/server/options_impl_platform_default.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <thread>
 
 #include "source/common/common/logger.h"
@@ -7,7 +8,8 @@ namespace Envoy {
 
 uint32_t OptionsImplPlatform::getCpuCount() {
   ENVOY_LOG(debug, "CPU number provided by HW thread count (instead of cpuset).");
-  return std::thread::hardware_concurrency();
+  // hardware_concurrency() may return 0 if not computable; floor at 1.
+  return std::max(1U, std::thread::hardware_concurrency());
 }
 
 } // namespace Envoy
