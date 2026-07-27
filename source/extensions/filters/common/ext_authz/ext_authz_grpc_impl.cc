c524571ffad517ba650d9563f85afa89194e2cfb
Yan Avlasov <yavlasov@google.com>
2026-07-27 19:33:45 +0000

    Fix ext_authz UAF crash after rejecting client request
    Fix: [CVE-2026-50572](https://github.com/envoyproxy/envoy/security/advisories/GHSA-q8wp-gf7q-m8cv)
    
    Signed-off-by: Yan Avlasov <yavlasov@google.com>
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- source/extensions/filters/common/ext_authz/ext_authz_grpc_impl.cc
+++ source/extensions/filters/common/ext_authz/ext_authz_grpc_impl.cc
@@ -157,8 +157,9 @@ void GrpcClientImpl::onSuccess(std::unique_ptr<envoy::service::auth::v3::CheckRe
     authz_response->dynamic_metadata = response->dynamic_metadata();
   }
 
-  callbacks_->onComplete(std::move(authz_response));
+  RequestCallbacks* callbacks = callbacks_;
   callbacks_ = nullptr;
+  callbacks->onComplete(std::move(authz_response));
 }
 
 void GrpcClientImpl::onFailure(Grpc::Status::GrpcStatus status, const std::string&,
@@ -169,8 +170,9 @@ void GrpcClientImpl::onFailure(Grpc::Status::GrpcStatus status, const std::strin
   Response response{};
   response.status = CheckStatus::Error;
   response.grpc_status = status;
-  callbacks_->onComplete(std::make_unique<Response>(response));
+  RequestCallbacks* callbacks = callbacks_;
   callbacks_ = nullptr;
+  callbacks->onComplete(std::make_unique<Response>(response));
 }
 
 } // namespace ExtAuthz
