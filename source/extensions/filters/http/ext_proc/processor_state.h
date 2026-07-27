61d8a5ff0a54cbf9cf8df9f59f2ba70d742b7217
Yanjun Xiang <yanjunxiang@google.com>
2026-07-27 20:04:57 +0000

    address comments
    Signed-off-by: Yanjun Xiang <yanjunxiang@google.com>

--- source/extensions/filters/http/ext_proc/processor_state.h
+++ source/extensions/filters/http/ext_proc/processor_state.h
@@ -661,8 +661,8 @@ public:
     return !local_response_started_ && ProcessorState::noExternalProcess();
   }
 
-  // Standalone mode override is only supported when Envoy is in the waiting for
-  // request header response state, and the old request body mode is STREAMED;
+  // Standalone mode override is only supported when Envoy is in the waiting for request
+  // header response state, and the old request body mode is not FULL_DUPLEX_STREAMED;
   // and the new request body mode is FULL_DUPLEX_STREAMED, and the new request
   // trailer mode is SEND.
   bool handleStandaloneModeOverride(
