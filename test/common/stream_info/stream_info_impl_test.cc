a96d5a8b2c5ba169945beaa674a7ffd0b0865c14
Biren Roy <birenroy@google.com>
2026-07-31 12:04:02 +0000

    Added a missing )
    Signed-off-by: Biren Roy <birenroy@google.com>

--- test/common/stream_info/stream_info_impl_test.cc
+++ test/common/stream_info/stream_info_impl_test.cc
@@ -50,7 +50,7 @@ protected:
         // with --config=docker-clang-libc++
         sizeof(stream_info) == 656 ||
         // with protobuf v35
-        sizeof(stream_info) == 672 || sizeof(stream_info) == 696
+        sizeof(stream_info) == 672 || sizeof(stream_info) == 696)
         << "If adding fields to StreamInfoImpl, please check to see if you "
            "need to add them to setFromForRecreateStream or setFrom! Current size "
         << sizeof(stream_info);
