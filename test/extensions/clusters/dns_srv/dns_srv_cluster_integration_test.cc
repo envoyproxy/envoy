a735b5994d8fce719c7b303e5fa3ec57be1acddd
Mikhail Galanin <mikhail.galanin@yahoo.com>
2026-08-03 19:59:42 +0000

    format
    Signed-off-by: Mikhail Galanin <mikhail.galanin@yahoo.com>

--- test/extensions/clusters/dns_srv/dns_srv_cluster_integration_test.cc
+++ test/extensions/clusters/dns_srv/dns_srv_cluster_integration_test.cc
@@ -43,7 +43,8 @@ public:
 
           envoy::extensions::clusters::dns_srv::v3::DnsSrvClusterConfig dns_srv_cluster{};
           dns_srv_cluster.set_srv_name("_local_service._tcp.service.consul.");
-          ASSERT_TRUE(cluster.mutable_cluster_type()->mutable_typed_config()->PackFrom(dns_srv_cluster));
+          ASSERT_TRUE(
+              cluster.mutable_cluster_type()->mutable_typed_config()->PackFrom(dns_srv_cluster));
 
           cluster.mutable_typed_dns_resolver_config()->set_name("envoy.network.dns_resolver.cares");
           ASSERT_TRUE(cluster.mutable_typed_dns_resolver_config()->mutable_typed_config()->PackFrom(
