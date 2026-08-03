617eb9df50c8770bd97535dade346a7cef7bf550
Keerthana G R <Keerthana.G.R@ibm.com>
2026-08-03 15:13:37 +0530

    Update quic_platform_test.cc
    quic: skip stack trace validation in quic_platform_test on optimized s390x builds
    
    Signed-off-by: Keerthana G R <Keerthana.G.R@ibm.com>

--- test/common/quic/platform/quic_platform_test.cc
+++ test/common/quic/platform/quic_platform_test.cc
@@ -146,7 +146,8 @@ TEST_F(QuicPlatformTest, QuicServerStats) {
 }
 
 TEST_F(QuicPlatformTest, QuicStackTraceTest) {
-#if !defined(ENVOY_CONFIG_COVERAGE) && !defined(GCC_COMPILER) && !(defined(__s390x__) && defined(NDEBUG))
+#if !defined(ENVOY_CONFIG_COVERAGE) && !defined(GCC_COMPILER) &&                                   \
+    !(defined(__s390x__) && defined(NDEBUG))
   // This doesn't work in coverage build because part of the stacktrace will be overwritten by
   // __llvm_coverage_mapping
   // Stack trace under gcc with optimizations on (-c opt) doesn't include the test name
