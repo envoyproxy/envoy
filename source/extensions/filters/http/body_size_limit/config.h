6d396b91b0221e6d002986d576e4f7231c03abd9
code <wbphub@gmail.com>
2026-08-11 14:52:40 +0800

    fix main to resolve conflit merging (#46625)
    Signed-off-by: wbpcode <wbphub@gmail.com>

--- source/extensions/filters/http/body_size_limit/config.h
+++ source/extensions/filters/http/body_size_limit/config.h
@@ -27,8 +27,8 @@ private:
 
   absl::StatusOr<Http::FilterFactoryCb> createHttpFilterFactoryFromProtoTyped(
       const envoy::extensions::filters::http::body_size_limit::v3::BodySizeLimit& proto_config,
-      const std::string& stats_prefix,
-      Server::Configuration::ServerFactoryContext& context) override;
+      Server::Configuration::ServerFactoryContext& context,
+      Server::Configuration::ExtraFactoryContext& extra_context) override;
 };
 
 using UpstreamBodySizeLimitFilterFactory = BodySizeLimitFilterFactory;
