8b611589cd35cc6e5055bf7a51047e4a40773760
water <672684719@qq.com>
2026-08-17 12:29:02 +0800

    fix: support P-384 and P-521 ECDSA certificates in QUIC certificate selection

--- source/common/quic/quic_server_transport_socket_factory.cc
+++ source/common/quic/quic_server_transport_socket_factory.cc
@@ -177,7 +177,7 @@ QuicServerTransportSocketFactory::getTlsCertificateAndKey(absl::string_view sni,
   auto ctx =
       std::dynamic_pointer_cast<Extensions::TransportSockets::Tls::ServerContextImpl>(ssl_ctx);
   auto [tls_context, ocsp_staple_action] =
-      ctx->findTlsContext(sni, Ssl::CurveNIDVector{NID_X9_62_prime256v1} /* TODO: ecdsa_capable */,
+      ctx->findTlsContext(sni, Ssl::CurveNIDVector{NID_X9_62_prime256v1, NID_secp384r1, NID_secp521r1} /* TODO: ecdsa_capable */,
                           false /* TODO: ocsp_capable */, cert_matched_sni);
 
   // Thread safety note: accessing the tls_context requires holding a shared_ptr to the ``ssl_ctx``.
