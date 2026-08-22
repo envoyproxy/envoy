06b5b65ecc2f5b184a5d224b9bae5b17636677fc
wbpcode <wbphub@gmail.com>
2026-08-22 01:33:44 +0000

    router: use the unified http filter factory base for router
    Signed-off-by: wbpcode <wbphub@gmail.com>

--- source/extensions/filters/http/router/config.cc
+++ source/extensions/filters/http/router/config.cc
@@ -5,20 +5,26 @@
 
 #include "source/common/router/router.h"
 #include "source/common/router/shadow_writer_impl.h"
+#include "source/server/generic_factory_context.h"
 
 namespace Envoy {
 namespace Extensions {
 namespace HttpFilters {
 namespace RouterFilter {
 
-absl::StatusOr<Http::FilterFactoryCb> RouterFilterConfig::createFilterFactoryFromProtoTyped(
+absl::StatusOr<Http::FilterFactoryCb> RouterFilterConfig::createHttpFilterFactoryFromProtoTyped(
     const envoy::extensions::filters::http::router::v3::Router& proto_config,
-    const std::string& stat_prefix, Server::Configuration::FactoryContext& context) {
-  Stats::StatNameManagedStorage prefix(stat_prefix, context.scope().symbolTable());
+    Server::Configuration::ServerFactoryContext& context,
+    Server::Configuration::ExtraFactoryContext& extra_context) {
+  // The stat prefix name must be created in the symbol table of the same scope that will be used
+  // to create the stats.
+  Stats::Scope& scope = extra_context.scopeOr(context);
+  Stats::StatNameManagedStorage prefix(extra_context.stats_prefix, scope.symbolTable());
+  Server::GenericFactoryContextImpl generic_context(context, scope, extra_context.visitor,
+                                                    extra_context.init_manager);
   auto config_or_error = Router::FilterConfig::create(
-      prefix.statName(), context,
-      std::make_unique<Router::ShadowWriterImpl>(context.serverFactoryContext().clusterManager()),
-      proto_config);
+      prefix.statName(), generic_context,
+      std::make_unique<Router::ShadowWriterImpl>(context.clusterManager()), proto_config);
   RETURN_IF_NOT_OK_REF(config_or_error.status());
   Router::FilterConfigSharedPtr filter_config(std::move(*config_or_error));
 
