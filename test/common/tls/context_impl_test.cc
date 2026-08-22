3ee5bb68b3944eb1abce1dea8dbe714adeb71060
Peng Gao <pengg@google.com>
2026-08-22 06:53:46 -0400

    regenerate test certificates (#46889)
    Regenerate test certificates, which were last
    regenerated on August 21 2024
    Tests are failing due to expired certs.
    
    Fixed certs.sh to work with newer openssl that doesn't allow negative
    -days command.
    
    Additional Description:
    Risk Level: low
    Testing: unit tests
    Docs Changes: n/a
    Release Notes: n/a
    Platform Specific Features: n/a
    Fixes #46881
    
    ---------
    
    Signed-off-by: Peng Gao <pengg@google.com>

--- test/common/tls/context_impl_test.cc
+++ test/common/tls/context_impl_test.cc
@@ -23,6 +23,7 @@
 #include "test/common/tls/test_data/no_san_cert_info.h"
 #include "test/common/tls/test_data/san_dns3_cert_info.h"
 #include "test/common/tls/test_data/san_ip_cert_info.h"
+#include "test/common/tls/test_data/selfsigned_cert_info.h"
 #include "test/common/tls/test_data/unittest_cert_info.h"
 #include "test/mocks/init/mocks.h"
 #include "test/mocks/local_info/mocks.h"
@@ -2808,9 +2809,13 @@ common_tls_context:
   std::string expected_metric_name =
       absl::StrCat("ssl.certificate.", actual_cert_name, ".expiration_unix_time_seconds");
 
+  auto cert_expiry =
+      TestUtility::parseTime(TEST_SELFSIGNED_CERT_NOT_AFTER, "%b %d %H:%M:%S %Y GMT");
+  uint64_t expected_expiry = absl::ToUnixSeconds(cert_expiry);
+
   auto gauge_opt = store.findGaugeByString(expected_metric_name);
   EXPECT_TRUE(gauge_opt.has_value());
-  EXPECT_EQ(gauge_opt->get().value(), 1787339648);
+  EXPECT_EQ(gauge_opt->get().value(), expected_expiry);
 }
 
 TEST_F(CertificateExpirationMetricsTest, ClientCertificateExpirationMetrics) {
@@ -2841,9 +2846,13 @@ common_tls_context:
   std::string expected_metric_name =
       absl::StrCat("ssl.certificate.", actual_cert_name, ".expiration_unix_time_seconds");
 
+  auto cert_expiry =
+      TestUtility::parseTime(TEST_SELFSIGNED_CERT_NOT_AFTER, "%b %d %H:%M:%S %Y GMT");
+  uint64_t expected_expiry = absl::ToUnixSeconds(cert_expiry);
+
   auto gauge_opt = store.findGaugeByString(expected_metric_name);
   EXPECT_TRUE(gauge_opt.has_value());
-  EXPECT_EQ(gauge_opt->get().value(), 1787339648);
+  EXPECT_EQ(gauge_opt->get().value(), expected_expiry);
 }
 
 } // namespace Tls
