5d740faa0da72cd38c14ddb287698294d6a772da
code <wbphub@gmail.com>
2026-09-10 23:30:11 +0800

    runtime: deprecate the validate_upstream_headers flag (#47253)
    Commit Message: runtime: deprecate the validate_upstream_headers flag
    Additional Description:
    
    To close #30429
    
    Risk Level:
    Testing:
    Docs Changes:
    Release Notes:
    Platform Specific Features:
    [Optional Runtime guard:]
    [Optional Fixes #Issue]
    [Optional Fixes commit #PR or SHA]
    [Optional Deprecated:]
    [Optional [API
    Considerations](https://github.com/envoyproxy/envoy/blob/main/api/review_checklist.md):]
    
    Signed-off-by: wbpcode <wbphub@gmail.com>

--- test/integration/http_integration.h
+++ test/integration/http_integration.h
@@ -9,6 +9,7 @@
 #include "envoy/extensions/filters/http/upstream_codec/v3/upstream_codec.pb.h"
 
 #include "source/common/http/codec_client.h"
+#include "source/common/http/header_utility.h"
 #include "source/common/network/filter_impl.h"
 #include "source/extensions/early_data/default_early_data_policy.h"
 
@@ -386,6 +387,14 @@ protected:
   // Set this to true when sending malformed requests to avoid test client codec rejecting it.
   // This flag is only valid when UHV build flag is enabled.
   bool disable_client_header_validation_{false};
+
+  // Stops the codecs from validating the request headers they encode, so that a test can use
+  // Envoy's own client codecs to send a deliberately malformed request at Envoy. Only needed in
+  // non-UHV builds, where the codecs do this check themselves; the destructor restores it.
+  // TODO(yanavlasov): fold this into `disable_client_header_validation_`.
+  void disableCodecHeaderValidation() {
+    Http::HeaderUtility::disable_request_header_validation_for_tests_ = true;
+  }
 
 #ifdef ENVOY_ENABLE_QUIC
   quic::DeterministicConnectionIdGenerator connection_id_generator_{
