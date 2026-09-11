9d50ab8db05f9e81c2c143b958f88d5964064236
code <wbphub@gmail.com>
2026-09-12 00:09:11 +0800

    update runtime flag comment to keep it for another year (#47230)
    Commit Message: runtime: removed ext_proc_fail_close_spurious_resp flag
    and legacy code paths
    Additional Description: 
    To close #42977. This flag been there for one year+, per the comment, it
    should be good to remove.
    Risk Level: Low
    Testing: Existing tests.
    Docs Changes: N/A
    Release Notes: Added.
    Platform Specific Features: N/A
    
    Signed-off-by: wbpcode <wbphub@gmail.com>

--- source/common/runtime/runtime_features.cc
+++ source/common/runtime/runtime_features.cc
@@ -63,7 +63,7 @@ RUNTIME_GUARD(envoy_reloadable_features_enable_new_dns_implementation);
 FALSE_RUNTIME_GUARD(envoy_reloadable_features_enable_lrs_server_self_ads);
 RUNTIME_GUARD(envoy_reloadable_features_enable_new_query_param_present_match_behavior);
 RUNTIME_GUARD(envoy_reloadable_features_enable_send_rst_on_user_space_socket);
-// Ignore the automated "remove this flag" issue: we should keep this for 1 year. Confirm with
+// Ignore the automated "remove this flag" issue: never remove this before 2027/10. Confirm with
 // @yanjunxiang-google before removing.
 RUNTIME_GUARD(envoy_reloadable_features_ext_proc_fail_close_spurious_resp);
 RUNTIME_GUARD(envoy_reloadable_features_ext_proc_inject_data_with_state_update);
