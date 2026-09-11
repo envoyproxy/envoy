6f1355b585ffd24a99effb90050f823b5246b2ab
code <wbphub@gmail.com>
2026-09-11 20:47:51 +0800

    http: add context to allow more complex header formatter (#47362)
    Commit Message: http: add context to allow more complex header formatter
    Additional Description:
    
    A pre-PR to support dynamic modules. This PR added a generic factory
    context as input of the factory and added the ability to handle the
    failure.
    
    Risk Level:
    Testing:
    Docs Changes:
    Release Notes:
    Platform Specific Features:
    [Optional Runtime guard:]
    [Optional Fixes #Issue]
    [Optional Fixes commit #PR or SHA]
    [Optional Deprecated:]
    [Optional [API
    Considerations](https://github.com/envoyproxy/envoy/blob/main/api/review_checklist.md):]
    
    ---------
    
    Signed-off-by: wbpcode <wbphub@gmail.com>

--- source/common/http/http1/settings.h
+++ source/common/http/http1/settings.h
@@ -5,22 +5,30 @@
 #include "envoy/protobuf/message_validator.h"
 #include "envoy/server/factory_context.h"
 
+#include "absl/status/status.h"
+
 namespace Envoy {
 namespace Http {
 namespace Http1 {
 
 /**
+ * @param config the protocol options to parse.
+ * @param context the factory context of whatever owns the options, which also supplies the
+ * validation visitor used for any extension configuration referenced by them.
+ * @param creation_status is set to an error when a configured extension - today only the stateful
+ * header formatter - cannot be created, and default settings are returned in that case. It is left
+ * untouched on success, so it can be threaded through a chain of member initializers.
  * @return Http1Settings An Http1Settings populated from the
  * envoy::config::core::v3::Http1ProtocolOptions config.
  */
 Http1Settings parseHttp1Settings(const envoy::config::core::v3::Http1ProtocolOptions& config,
-                                 Server::Configuration::CommonFactoryContext& context,
-                                 ProtobufMessage::ValidationVisitor& validation_visitor);
+                                 Server::Configuration::GenericFactoryContext& context,
+                                 absl::Status& creation_status);
 
 Http1Settings parseHttp1Settings(const envoy::config::core::v3::Http1ProtocolOptions& config,
-                                 Server::Configuration::CommonFactoryContext& context,
-                                 ProtobufMessage::ValidationVisitor& validation_visitor,
-                                 const Protobuf::BoolValue& hcm_stream_error, bool validate_scheme);
+                                 Server::Configuration::GenericFactoryContext& context,
+                                 const Protobuf::BoolValue& hcm_stream_error, bool validate_scheme,
+                                 absl::Status& creation_status);
 
 } // namespace Http1
 } // namespace Http
