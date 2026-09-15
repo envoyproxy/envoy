a9f683a42d3504cdb28a3beb2b46438ad6e783aa
Yanjun Xiang <yanjunxiang@google.com>
2026-09-15 21:44:43 +0000

    Adding adding injectHeadersToFilterChain() API to send headers
    Signed-off-by: Yanjun Xiang <yanjunxiang@google.com>

--- source/common/http/async_client_impl.h
+++ source/common/http/async_client_impl.h
@@ -207,6 +207,7 @@ private:
   }
   MetadataMapVector& addDecodedMetadata() override { PANIC("not implemented"); }
   void injectDecodedDataToFilterChain(Buffer::Instance&, bool) override {}
+  void injectDecodedHeadersToFilterChain(bool) override {}
   const Buffer::Instance* decodingBuffer() override { return buffered_body_.get(); }
   void modifyDecodingBuffer(std::function<void(Buffer::Instance&)>) override {}
   void sendLocalReply(Code code, absl::string_view body,
