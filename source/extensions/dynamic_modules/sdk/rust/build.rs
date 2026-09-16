5cdc9b6d98904ebb4c55e659036e2c15893d49ae
Anuraag Agrawal <anuraaga@gmail.com>
2026-09-16 17:32:26 +0900

    dynamic_modules: update bindgen and add windows support
    Signed-off-by: Anuraag Agrawal <anuraaga@gmail.com>

--- source/extensions/dynamic_modules/sdk/rust/build.rs
+++ source/extensions/dynamic_modules/sdk/rust/build.rs
@@ -106,7 +106,12 @@ fn main() {
     })
     .derive_partialeq(true)
     .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-    .parse_callbacks(Box::new(TrimEnumNameFromVariantName))
+    .parse_callbacks(Box::new(TrimEnumNameFromVariantName));
+
+  #[cfg(target_os = "windows")]
+  let bindings = bindings.extern_fn_block_attrs("#[link(name = \"envoy\", kind = \"raw-dylib\")]");
+
+  let bindings = bindings
     .generate()
     .expect("Unable to generate bindings");
 
