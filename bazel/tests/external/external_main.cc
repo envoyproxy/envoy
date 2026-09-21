34cb9c39e6d1978c68c0aa01b82d13e614b883cb
phlax <phlax@users.noreply.github.com>
2026-09-21 21:13:42 +0100

    bazel: Switch macro labels to Label() and add external tests (#47556)
    Signed-off-by: Ryan Northey <ryan@synca.io>

---
+++ bazel/tests/external/external_main.cc
@@ -0,0 +1,3 @@
+#include "external_lib.h"
+
+int main() { return External::externalValue() == 3 ? 0 : 1; }
---
+++ bazel/tests/external/select_enable_exceptions_dep.cc
@@ -0,0 +1,7 @@
+#include "select_enable_exceptions_dep.h"
+
+namespace External {
+
+int selectEnableExceptionsValue() { return 1; }
+
+} // namespace External
