808acc50e0cc233e436e6ef602824498cc973528
code <wbphub@gmail.com>
2026-09-25 05:51:05 +0800

    secret: ensure the secret provider be destoryed at main (#47631)
    Fixed a race where a generic secret provider shared with worker threads
    could be destroyed on a
    worker thread.
    
    Risk Level: low.
    Testing: unit.
    Docs Changes: n/a..
    Release Notes: added.
    Platform Specific Features: n/a.
    
    ---------
    
    Signed-off-by: wbpcode <wbphub@gmail.com>
    Signed-off-by: code <wbphub@gmail.com>

--- source/extensions/formatter/generic_secret/config.cc
+++ source/extensions/formatter/generic_secret/config.cc
@@ -140,10 +140,11 @@ Envoy::Formatter::CommandParserPtr GenericSecretFormatterFactory::createCommandP
       }
     }
 
-    auto tls_provider = THROW_OR_RETURN_VALUE(
-        Secret::ThreadLocalGenericSecretProvider::create(
-            std::move(provider), server_context.threadLocal(), server_context.api()),
-        std::unique_ptr<Secret::ThreadLocalGenericSecretProvider>);
+    auto tls_provider =
+        THROW_OR_RETURN_VALUE(Secret::ThreadLocalGenericSecretProvider::create(
+                                  std::move(provider), server_context.threadLocal(),
+                                  server_context.api(), server_context.mainThreadDispatcher()),
+                              Secret::ThreadLocalGenericSecretProviderPtr);
 
     providers.emplace(
         name, std::shared_ptr<Secret::ThreadLocalGenericSecretProvider>(std::move(tls_provider)));
